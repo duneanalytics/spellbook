29d8f4340e8cbe72b964023e4e138c3a9092d148
Mats Julian Olsen <mats@plysjbyen.net>
2020-02-25 12:48:12 +0000

    fix invalid byte array format (#11)

--- schema/compound/view_ctokens.sql
+++ schema/compound/view_ctokens.sql
@@ -6,5 +6,5 @@ CREATE VIEW compound.view_ctokens (symbol, contract_address, decimals, underlyin
    ('cUSDC'::text,'\x39aa39c021dfbae8fac545936693ac917d5e7563'::bytea, 8::numeric, '\xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'::bytea),
    ('cWBTC'::text,'\xc11b1268c1a384e55c48c2391d8d480264a3a7f4'::bytea, 8::numeric, '\x2260fac5e5542a773aa44fbcfedf7c193bc2c599'::bytea),
    ('cZRX'::text, '\xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407'::bytea, 8::numeric, '\xe41d2489571d322189246dafa5ebde1f4699f498'::bytea),
-   ('cDAI'::text, '\x5d3a536e4d6dbd6114cc1ead35777bab948e3643'::bytea, 8::numeric, '\0x6b175474e89094c44da98b954eedeac495271d0f'::bytea) 
+   ('cDAI'::text, '\x5d3a536e4d6dbd6114cc1ead35777bab948e3643'::bytea, 8::numeric, '\x6b175474e89094c44da98b954eedeac495271d0f'::bytea) 
 ;   
