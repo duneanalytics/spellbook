0cef3a9d77a572772d76e655a2fcddc2da4acb0d
Mats Julian Olsen <mats@plysjbyen.net>
2020-03-23 08:36:57 +0100

    New compound format (#18)

--- schema/compound/view_accrue_interest.sql
+++ schema/compound/view_accrue_interest.sql
@@ -10,16 +10,17 @@ SELECT CASE
        "borrowIndex" AS borrow_index,
        c."underlying_token_address" AS token_address,
        evt_tx_hash AS tx_hash,
-       tx.block_time AS block_time
+       evt_block_time AS block_time
 FROM
-  (SELECT *
+  (SELECT "interestAccumulated", "borrowIndex", "totalBorrows", contract_address, evt_tx_hash, evt_block_time
    FROM compound_v2."cErc20_evt_AccrueInterest"
-   UNION SELECT *
+   UNION 
+   SELECT "interestAccumulated", "borrowIndex", "totalBorrows", contract_address, evt_tx_hash, evt_block_time
    FROM compound_v2."cEther_evt_AccrueInterest"
-   UNION SELECT "interestAccumulated", "borrowIndex", "totalBorrows", contract_address, evt_tx_hash, evt_index
+   UNION 
+   SELECT "interestAccumulated", "borrowIndex", "totalBorrows", contract_address, evt_tx_hash, evt_block_time
    FROM compound_v2."CErc20Delegator_evt_AccrueInterest") events
-LEFT JOIN compound_v2.view_ctokens c ON events.contract_address = c.contract_address
-LEFT JOIN ethereum.transactions tx ON events.evt_tx_hash = tx.hash AND block_number >= 7710671
+LEFT JOIN compound.view_ctokens c ON events.contract_address = c.contract_address
 LEFT JOIN erc20.tokens t ON c.underlying_token_address = t.contract_address
 LEFT JOIN
   (SELECT minute,
@@ -31,6 +32,6 @@ LEFT JOIN
    UNION SELECT generate_series('2019-11-18', now(), '1 minute'),
                 '\x6B175474E89094C44Da98b954EedeAC495271d0F'::bytea AS contract_address,
                 'DAI' AS symbol,
-                1 AS price) p ON p.minute = date_trunc('minute', tx.block_time)
+                1 AS price) p ON p.minute = date_trunc('minute', evt_block_time)
 AND p.contract_address = c.underlying_token_address
 ;
