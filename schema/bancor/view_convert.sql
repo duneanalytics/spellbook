6a329fa5c87f3e492c332f78a4407d14de65e514
milkyklim <10698619+milkyklim@users.noreply.github.com>
2020-05-08 00:54:24 +0200

    replace multiple convert events with one convert view

--- schema/bancor/view_convert.sql
+++ schema/bancor/view_convert.sql
@@ -1,22 +1,77 @@
 CREATE OR REPLACE VIEW bancor.view_convert AS
-SELECT "_fromToken" AS source_token_address,
+WITH tmp AS
+  (SELECT "fromToken" AS source_token_address,
+          "toToken" AS target_token_address,
+          "trader" AS trader,
+          "inputAmount" AS source_token_amount,
+          "outputAmount" AS target_token_amount,
+          "conversionFee" AS conversion_fee,
+          contract_address,
+          evt_tx_hash AS tx_hash,
+          evt_block_time AS block_time
+   FROM bancor."BancorConverterWithFee_evt_Conversion"
+   UNION ALL
+   SELECT "fromToken" AS source_token_address,
+          "toToken" AS target_token_address,
+          "trader" AS trader,
+          "inputAmount" AS source_token_amount,
+          "outputAmount" AS target_token_amount,
+          NULL AS conversion_fee,
+          contract_address,
+          evt_tx_hash AS tx_hash,
+          evt_block_time AS block_time
+   FROM bancor."BancorLegacyConverter_evt_Conversion"
+   UNION ALL
+   SELECT "fromToken" AS source_token_address,
+          "toToken" AS target_token_address,
+          "trader" AS trader,
+          "inputAmount" AS source_token_amount,
+          "outputAmount" AS target_token_amount,
+          NULL AS conversion_fee,
+          contract_address,
+          evt_tx_hash AS tx_hash,
+          evt_block_time AS block_time
+   FROM bancor."BancorChange_evt_Change"
+   UNION ALL
+   SELECT "_fromToken" AS source_token_address,
+          "_toToken" AS target_token_address,
+          "_trader" AS trader,
+          "_fromAmount" AS source_token_amount,
+          "_toAmount" AS target_token_amount,
+          NULL AS conversion_fee,
+          contract_address,
+          evt_tx_hash AS tx_hash,
+          evt_block_time AS block_time
+   FROM bancor."BancorNetwork_v1_evt_Conversion"
+),
+conversions AS
+  (SELECT *
+   FROM
+     (SELECT *,
+             row_number() OVER (PARTITION BY (tx_hash,
+                                              source_token_address,
+                                              target_token_address) ORDER BY conversion_fee) AS rn
+      FROM tmp) q
+   WHERE rn = 1
+)
+SELECT source_token_address,
        t1.symbol AS source_token_symbol,
-       "_toToken" AS target_token_address,
+       target_token_address,
        t2.symbol AS target_token_symbol,
-       "_trader" AS trader,
-       "_amount" / 10^t1.decimals AS source_token_amount,
-       "_amount" / 10^t1.decimals * p1.price AS source_usd_amount,
-       "_return" / 10^t2.decimals AS target_token_amount,
-       "_return" / 10^t2.decimals * p2.price AS target_usd_amount,
-       "_conversionFee" / 10^t2.decimals AS conversion_fee,
+       trader,
+       source_token_amount / 10^t1.decimals AS source_token_amount,
+       source_token_amount / 10^t1.decimals * p1.price AS source_usd_amount,
+       target_token_amount / 10^t2.decimals AS target_token_amount,
+       target_token_amount / 10^t2.decimals * p2.price AS target_usd_amount,
+       conversion_fee / 10^t2.decimals AS conversion_fee,
        s.contract_address,
-       s.evt_tx_hash AS tx_hash,
-       s.evt_block_time AS block_time
-FROM bancor."BancorConverter_evt_Conversion" s
-LEFT JOIN erc20.tokens t1 ON s."_fromToken" = t1.contract_address
-LEFT JOIN prices.usd p1 ON p1.minute = date_trunc('minute', s.evt_block_time)
+       tx_hash,
+       block_time
+FROM conversions s
+LEFT JOIN erc20.tokens t1 ON s.source_token_address = t1.contract_address
+LEFT JOIN prices.usd p1 ON p1.minute = date_trunc('minute', s.block_time)
     AND p1.symbol = t1.symbol
-LEFT JOIN erc20.tokens t2 ON s."_toToken" = t2.contract_address
-LEFT JOIN prices.usd p2 ON p2.minute = date_trunc('minute', s.evt_block_time)
+LEFT JOIN erc20.tokens t2 ON s.target_token_address = t2.contract_address
+LEFT JOIN prices.usd p2 ON p2.minute = date_trunc('minute', s.block_time)
     AND p2.symbol = t2.symbol
 ;
