1b690285f61a23651596970864a6854e63bb5d9e
Anxo Rodriguez <anxolin@gmail.com>
2020-05-12 08:33:25 +0200

    Use uppercase for AS keyword

--- schema/gnosis_protocol/view_trades.sql
+++ schema/gnosis_protocol/view_trades.sql
@@ -1,55 +1,55 @@
 CREATE MATERIALIZED VIEW gnosis_protocol.view_trades AS
-WITH reverts as (
+WITH reverts AS (
 SELECT
     reversion.owner,
-    reversion."orderId" as order_id,
-    reversion.evt_block_time as block_time,
+    reversion."orderId" AS order_id,
+    reversion.evt_block_time AS block_time,
     RANK() OVER (
         PARTITION BY solution."batchId", reversion.owner, reversion."orderId"
         ORDER BY reversion.evt_block_number, reversion.evt_index
-    ) as trade_sub_id,
-    solution."batchId" as batch_id
+    ) AS trade_sub_id,
+    solution."batchId" AS batch_id
 FROM gnosis_protocol."BatchExchange_evt_TradeReversion" reversion
 JOIN gnosis_protocol."BatchExchange_call_submitSolution" solution
     ON solution.call_tx_hash=reversion.evt_tx_hash
     AND solution.call_success = true
 ),
-trades as (
+trades AS (
 SELECT
 	  -- id
     floor(extract(epoch from solution.evt_block_time) / 300) - 1 AS batch_id, -- The event time tells us the batch. Between minute 0-4 is resolved batch N-1
     trades."owner" AS trader_hex,    
-    trades."orderId" as order_id,
+    trades."orderId" AS order_id,
     RANK() OVER (
         PARTITION BY solution.evt_block_time, trades.owner, trades."orderId"
         ORDER BY trades.evt_block_number, trades.evt_index
-    ) as trade_sub_id,
+    ) AS trade_sub_id,
     -- Event index
     trades.evt_index AS evt_index_trades,
     solution.evt_index AS evt_index_solution,
     -- dates & block info
     solution.evt_block_number,
-    solution.evt_block_time as block_time,
+    solution.evt_block_time AS block_time,
     to_timestamp((floor(extract(epoch from solution.evt_block_time) / 300)) * 300) AS trade_date,
     -- sell token
-    trades."sellToken" as sell_token_id,
-    sell_token."token" as sell_token,
-    sell_token."symbol" as sell_token_symbol,
-    sell_token."decimals" as sell_token_decimals,
+    trades."sellToken" AS sell_token_id,
+    sell_token."token" AS sell_token,
+    sell_token."symbol" AS sell_token_symbol,
+    sell_token."decimals" AS sell_token_decimals,
     -- sell amounts
-    trades."executedSellAmount" as sell_amount_atoms,
-    trades."executedSellAmount" / 10^(sell_token.decimals) as sell_amount,
+    trades."executedSellAmount" AS sell_amount_atoms,
+    trades."executedSellAmount" / 10^(sell_token.decimals) AS sell_amount,
     -- buy token
-    trades."buyToken" as buy_token_id,
-    buy_token.token as buy_token,    
-    buy_token.symbol as buy_token_symbol,
-    buy_token.decimals as buy_token_decimals,
+    trades."buyToken" AS buy_token_id,
+    buy_token.token AS buy_token,    
+    buy_token.symbol AS buy_token_symbol,
+    buy_token.decimals AS buy_token_decimals,
     -- buy amounts
-    trades."executedBuyAmount" as buy_amount_atoms,
-    trades."executedBuyAmount" / 10^(buy_token.decimals) as buy_amount,
+    trades."executedBuyAmount" AS buy_amount_atoms,
+    trades."executedBuyAmount" / 10^(buy_token.decimals) AS buy_amount,
     -- Tx and block info
-    trades.evt_block_number as block_number,
-    trades.evt_tx_hash as tx_hash
+    trades.evt_block_number AS block_number,
+    trades.evt_tx_hash AS tx_hash
 FROM gnosis_protocol."BatchExchange_evt_Trade" trades
 JOIN gnosis_protocol."BatchExchange_evt_SolutionSubmission" solution
     ON solution.evt_tx_hash=trades.evt_tx_hash
@@ -60,7 +60,7 @@ JOIN gnosis_protocol.view_tokens sell_token
 )
 SELECT
 	trades.*,
-	reverts.block_time as revert_time
+	reverts.block_time AS revert_time
 FROM trades
 LEFT OUTER JOIN reverts
     ON trades.trader_hex = reverts.owner
