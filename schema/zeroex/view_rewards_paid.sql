35ca97192401615470373346731ee9a8da023de1
Alex Kroeger <alexrkroeger@gmail.com>
2020-05-28 16:50:27 -0700

    Added 0x-specific views

---
+++ schema/zeroex/view_rewards_paid.sql
@@ -0,0 +1,9 @@
+DROP VIEW IF EXISTS zeroex.rewards_paid;
+CREATE OR REPLACE VIEW zeroex.rewards_paid AS (
+    SELECT
+        HEX_TO_INT(RIGHT(rp."poolId"::VARCHAR,5)) AS pool_id
+        , epoch - 1 AS epoch_id
+        , rp."membersReward" / 1e18 AS members_reward_eth
+        , rp."operatorReward" / 1e18 AS operator_reward_eth
+    FROM zeroex_v3."StakingProxy_evt_RewardsPaid" rp
+);
---
+++ schema/zeroex/view_staking_parameters.sql
@@ -0,0 +1,12 @@
+DROP VIEW IF EXISTS zeroex.current_staking_params CASCADE;
+CREATE OR REPLACE VIEW zeroex.current_staking_params AS (
+    SELECT
+        evt_block_number AS block_number_set
+        , "epochDurationInSeconds" AS epoch_duraction_in_seconds
+        , "cobbDouglasAlphaNumerator" AS cobb_douglas_alpha_numerator
+        , "cobbDouglasAlphaDenominator" AS cobb_douglas_alpha_denominator
+        , "rewardDelegatedStakeWeight" AS reward_delegated_stake_weight
+    FROM zeroex_v3."StakingProxy_evt_ParamsSet"
+    ORDER BY evt_block_number DESC
+    LIMIT 1
+);
