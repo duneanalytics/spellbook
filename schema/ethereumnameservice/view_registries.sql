cf8c947f493572efed22ebdd85d165201b8a78fb
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-07-06 20:50:41 +0100

    Update expirations and registries

--- schema/ethereumnameservice/view_registries.sql
+++ schema/ethereumnameservice/view_registries.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE VIEW ethereumnameservice.view_registries AS
-SELECT label, TO_TIMESTAMP(min(expires)) AS min_expires, min(evt_block_time) AS min_evt_block_time, TO_TIMESTAMP(max(expires)) AS max_expires, max(evt_block_time) AS max_evt_block_time, count(*) AS count FROM
-(
-    SELECT numeric2bytea(id) AS label, expires, evt_block_time FROM ethereumnameservice."BaseRegistrarImplementation_evt_NameRegistered"
-    UNION
-    SELECT numeric2bytea(id) AS label, expires, evt_block_time FROM ethereumnameservice."BaseRegistrarImplementation_evt_NameRenewed"
-) AS r  GROUP BY label
+SELECT node, label, min(evt_block_time) AS min_evt_block_time, max(evt_block_time) AS max_evt_block_time, count(*)  FROM (
+  SELECT * FROM ethereumnameservice."ENSRegistry_evt_NewOwner"
+  UNION
+  SELECT * FROM ethereumnameservice."ENSRegistryWithFallback_evt_NewOwner"
+) AS r GROUP BY node, label
