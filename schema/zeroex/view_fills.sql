965f72943e873012a9193e11ff1007f7e98ff23e
Alex Kroeger <alexrkroeger@gmail.com>
2020-07-08 03:11:25 -0400

    zeroex: Fixed fills view, added api affiliate data view (#78)
    * Added 0x-specific views
    
    * Fixed epoch views, formatting
    
    * Added 0x to dex trades view
    
    * Adjusted views to fit guidelines
    
    * fixed token parsing and made updates more frequent in fills view, added api affiliate data table
    
    * formatting nit-end on newline
    
    * simplified token parsing for fills view, added drop statement affiliate data table transaction
    
    * Fixed where condition in fills table, made affiliate data comprehensive

--- schema/zeroex/view_fills.sql
+++ schema/zeroex/view_fills.sql
@@ -10,10 +10,10 @@ WITH
             , fills.evt_index
             , fills."makerAddress" AS maker_address
             , fills."takerAddress" AS taker_address
-            , ('\x' || SUBSTRING(fills."makerAssetData"::VARCHAR,35,40))::BYTEA AS maker_token
+            , SUBSTRING(fills."makerAssetData",17,20) AS maker_token
             , mt.symbol AS maker_symbol
             , fills."makerAssetFilledAmount" / (10^mt.decimals) AS maker_asset_filled_amount
-            , ('\x' || SUBSTRING(fills."takerAssetData"::VARCHAR,35,40))::BYTEA AS taker_token
+            , SUBSTRING(fills."takerAssetData",17,20) AS taker_token
             , tt.symbol AS taker_symbol
             , fills."takerAssetFilledAmount" / (10^tt.decimals) AS taker_asset_filled_amount
             , fills."feeRecipientAddress" AS fee_recipient_address
@@ -36,22 +36,22 @@ WITH
             date_trunc('minute', evt_block_time) = tp.minute
             AND CASE
                     -- Set Deversifi ETHWrapper to WETH
-                    WHEN substring(fills."takerAssetData",17,40) IN ('50cb61afa3f023d17276dcfb35abf85c710d1cff','aa7427d8f17d87a28f5e1ba3adbb270badbe1011') THEN 'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+                    WHEN SUBSTRING(fills."takerAssetData",17,20) IN ('\x50cb61afa3f023d17276dcfb35abf85c710d1cff'::BYTEA,'\xaa7427d8f17d87a28f5e1ba3adbb270badbe1011'::BYTEA) THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::BYTEA
                     -- Set Deversifi USDCWrapper to USDC
-                    WHEN substring(fills."takerAssetData",17,40) IN ('69391cca2e38b845720c7deb694ec837877a8e53') THEN 'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-                    ELSE substring(fills."takerAssetData",17,40)
+                    WHEN SUBSTRING(fills."takerAssetData",17,20) IN ('\x69391cca2e38b845720c7deb694ec837877a8e53'::BYTEA) THEN '\xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'::BYTEA
+                    ELSE SUBSTRING(fills."takerAssetData",17,20)
                 END = tp.contract_address
         LEFT JOIN prices.usd mp ON
             DATE_TRUNC('minute', evt_block_time) = mp.minute
             AND CASE
                     -- Set Deversifi ETHWrapper to WETH
-                    WHEN substring(fills."makerAssetData",17,40) IN ('50cb61afa3f023d17276dcfb35abf85c710d1cff','aa7427d8f17d87a28f5e1ba3adbb270badbe1011') THEN 'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+                    WHEN SUBSTRING(fills."makerAssetData",17,20) IN ('\x50cb61afa3f023d17276dcfb35abf85c710d1cff'::BYTEA,'\xaa7427d8f17d87a28f5e1ba3adbb270badbe1011'::BYTEA) THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::BYTEA
                     -- Set Deversifi USDCWrapper to USDC
-                    WHEN substring(fills."makerAssetData",17,40) IN ('69391cca2e38b845720c7deb694ec837877a8e53') THEN 'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-                    ELSE substring(fills."makerAssetData",17,40)
+                    WHEN SUBSTRING(fills."makerAssetData",17,20) IN ('\x69391cca2e38b845720c7deb694ec837877a8e53'::BYTEA) THEN '\xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'::BYTEA
+                    ELSE SUBSTRING(fills."makerAssetData",17,20)
                 END = mp.contract_address
-        LEFT JOIN erc20.tokens mt ON mt.contract_address = SUBSTRING(fills."makerAssetData",17,40)
-        LEFT JOIN erc20.tokens tt ON tt.contract_address = SUBSTRING(fills."takerAssetData",17,40)
+        LEFT JOIN erc20.tokens mt ON mt.contract_address = SUBSTRING(fills."makerAssetData",17,20)
+        LEFT JOIN erc20.tokens tt ON tt.contract_address = SUBSTRING(fills."takerAssetData",17,20)
     )
     , v2_1_fills AS (
         SELECT
@@ -61,10 +61,10 @@ WITH
             , fills.evt_index
             , fills."makerAddress" AS maker_address
             , fills."takerAddress" AS taker_address
-            , ('\x' || SUBSTRING(fills."makerAssetData"::VARCHAR,35,40))::BYTEA AS maker_token
+            , SUBSTRING(fills."makerAssetData",17,20) AS maker_token
             , mt.symbol AS maker_symbol
             , fills."makerAssetFilledAmount" / (10^mt.decimals) AS maker_asset_filled_amount
-            , ('\x' || SUBSTRING(fills."takerAssetData"::VARCHAR,35,40))::BYTEA AS taker_token
+            , SUBSTRING(fills."takerAssetData",17,20) AS taker_token
             , tt.symbol AS taker_symbol
             , fills."takerAssetFilledAmount" / (10^tt.decimals) AS taker_asset_filled_amount
             , fills."feeRecipientAddress" AS fee_recipient_address
@@ -87,22 +87,22 @@ WITH
             date_trunc('minute', evt_block_time) = tp.minute
             AND CASE
                     -- Set Deversifi ETHWrapper to WETH
-                    WHEN substring(fills."takerAssetData",17,40) IN ('50cb61afa3f023d17276dcfb35abf85c710d1cff','aa7427d8f17d87a28f5e1ba3adbb270badbe1011') THEN 'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+                    WHEN SUBSTRING(fills."takerAssetData",17,20) IN ('\x50cb61afa3f023d17276dcfb35abf85c710d1cff'::BYTEA,'\xaa7427d8f17d87a28f5e1ba3adbb270badbe1011'::BYTEA) THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::BYTEA
                     -- Set Deversifi USDCWrapper to USDC
-                    WHEN substring(fills."takerAssetData",17,40) IN ('69391cca2e38b845720c7deb694ec837877a8e53') THEN 'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-                    ELSE substring(fills."takerAssetData",17,40)
+                    WHEN SUBSTRING(fills."takerAssetData",17,20) IN ('\x69391cca2e38b845720c7deb694ec837877a8e53'::BYTEA) THEN '\xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'::BYTEA
+                    ELSE SUBSTRING(fills."takerAssetData",17,20)
                 END = tp.contract_address
         LEFT JOIN prices.usd mp ON
-            date_trunc('minute', evt_block_time) = mp.minute
+            DATE_TRUNC('minute', evt_block_time) = mp.minute
             AND CASE
                     -- Set Deversifi ETHWrapper to WETH
-                    WHEN substring(fills."makerAssetData",17,40) IN ('50cb61afa3f023d17276dcfb35abf85c710d1cff','aa7427d8f17d87a28f5e1ba3adbb270badbe1011') THEN 'c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+                    WHEN SUBSTRING(fills."makerAssetData",17,20) IN ('\x50cb61afa3f023d17276dcfb35abf85c710d1cff'::BYTEA,'\xaa7427d8f17d87a28f5e1ba3adbb270badbe1011'::BYTEA) THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::BYTEA
                     -- Set Deversifi USDCWrapper to USDC
-                    WHEN substring(fills."makerAssetData",17,40) IN ('69391cca2e38b845720c7deb694ec837877a8e53') THEN 'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-                    ELSE substring(fills."makerAssetData",17,40)
+                    WHEN SUBSTRING(fills."makerAssetData",17,20) IN ('\x69391cca2e38b845720c7deb694ec837877a8e53'::BYTEA) THEN '\xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'::BYTEA
+                    ELSE SUBSTRING(fills."makerAssetData",17,20)
                 END = mp.contract_address
-        LEFT JOIN erc20.tokens mt ON mt.contract_address = SUBSTRING(fills."makerAssetData",17,40)
-        LEFT JOIN erc20.tokens tt ON tt.contract_address = SUBSTRING(fills."takerAssetData",17,40)
+        LEFT JOIN erc20.tokens mt ON mt.contract_address = SUBSTRING(fills."makerAssetData",17,20)
+        LEFT JOIN erc20.tokens tt ON tt.contract_address = SUBSTRING(fills."takerAssetData",17,20)
     )
     SELECT * FROM v3_fills
 
@@ -114,5 +114,5 @@ WITH
 CREATE UNIQUE INDEX IF NOT EXISTS zeroex_fills_unique ON zeroex.view_fills (transaction_hash, evt_index);
 CREATE INDEX IF NOT EXISTS zeroex_fills_time_index ON zeroex.view_fills (timestamp);
 
-SELECT cron.schedule('0 * * * *', 'REFRESH MATERIALIZED VIEW CONCURRENTLY zeroex.view_fills');
+SELECT cron.schedule('*/10 * * * *', 'REFRESH MATERIALIZED VIEW CONCURRENTLY zeroex.view_fills');
 COMMIT;
