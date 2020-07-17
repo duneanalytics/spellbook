f1d258fae71e3863ebf5ffdad89fc22728048dcf
dvd-schwrtz <dbschwartz17@gmail.com>
2020-07-17 11:22:46 -0700

    synthetix: add rates, synths, trades (#85)

---
+++ schema/synthetix/rates.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS synthetix.rates (
+    currency_key	bytea		NOT NULL,
+    currency_rate	numeric		NOT NULL,
+    block_time	    timestamptz	NOT NULL,
+    PRIMARY KEY(currency_key, block_time)
+);
+
+CREATE INDEX rates_block_time ON synthetix.rates (block_time);
+CREATE INDEX rates_currency_key ON synthetix.rates (currency_key);
+CREATE INDEX rates_currency_key_block_time ON synthetix.rates (currency_key, block_time DESC) INCLUDE (currency_rate);
+
+INSERT INTO synthetix.rates VALUES (
+    '\x7355534400000000000000000000000000000000000000000000000000000000'::bytea, 1000000000000000000::numeric,	'2019-03-11T22:17:52.000Z'::timestamptz
+) ON CONFLICT DO NOTHING;
+
+CREATE OR REPLACE FUNCTION synthetix.insert_rates(start_ts timestamptz, end_ts timestamptz=now()) RETURNS integer
+LANGUAGE plpgsql AS $function$
+DECLARE r integer;
+BEGIN
+WITH rows AS (
+    INSERT INTO synthetix.rates
+    SELECT currency_key, currency_rate, evt_block_time as block_time
+    FROM synthetix."ExchangeRates_evt_RatesUpdated" r, unnest("currencyKeys", "newRates") as u(currency_key, currency_rate)
+    WHERE evt_block_time >= start_ts
+    AND evt_block_time < end_ts
+    ON CONFLICT DO NOTHING
+    RETURNING 1
+)
+SELECT count(*) INTO r from rows;
+RETURN r;
+END
+$function$;
+
+INSERT INTO cron.job (schedule, command)
+VALUES ('1/5 * * * *', 'SELECT synthetix.insert_rates((SELECT max(block_time) FROM synthetix.rates));')
+ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
+
