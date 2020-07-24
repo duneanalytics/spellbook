46496ca46d18b8525f60a0acff796370b83ad66c
dvd-schwrtz <dbschwartz17@gmail.com>
2020-07-24 09:34:23 -0700

    changes to create a materialized view for synthetix.view_trades

--- schema/synthetix/view_trades.sql
+++ schema/synthetix/view_trades.sql
@@ -1,22 +1,12 @@
-CREATE OR REPLACE VIEW synthetix.view_trades (
-    block_time,
-    project,
-    version,
-    trader_a,
-    trader_b,
-    token_a_amount_raw,
-    token_b_amount_raw,
-    token_a_address,
-    token_b_address,
-    exchange_contract_address,
-    tx_hash,
-    trace_address,
-    evt_index,
-    token_a_amount,
-    token_b_amount,
-    token_a_usd_price,
-    token_b_usd_price
-) AS SELECT
+BEGIN;
+DROP MATERIALIZED VIEW IF EXISTS synthetix.view_trades;
+CREATE MATERIALIZED VIEW synthetix.view_trades AS
+WITH 
+rates AS (
+    SELECT currency_key, currency_rate, evt_block_time
+    FROM synthetix."ExchangeRates_evt_RatesUpdated" r, unnest("currencyKeys", "newRates") as u(currency_key, currency_rate)
+)
+SELECT
     trade.evt_block_time AS block_time,
     'Synthetix' AS project,
     '1' AS version,
@@ -24,32 +14,22 @@ CREATE OR REPLACE VIEW synthetix.view_trades (
     NULL::bytea AS trader_b,
     trade."fromAmount"/1e18 AS token_a_amount_raw,
     trade."toAmount"/1e18 AS token_b_amount_raw,
-    synths_a.address AS token_a_address,
-    synths_b.address AS token_b_address,
+    (SELECT address FROM synthetix.view_synths WHERE trade."fromCurrencyKey" = currency_key AND evt_block_time < trade.evt_block_time ORDER BY evt_block_time DESC LIMIT 1) AS token_a_address,        
+    (SELECT address FROM synthetix.view_synths WHERE trade."toCurrencyKey" = currency_key AND evt_block_time < trade.evt_block_time ORDER BY evt_block_time DESC LIMIT 1) AS token_b_address,
     trade.contract_address AS exchange_contract_address,
     trade.evt_tx_hash AS tx_hash,
     NULL::integer[] AS trace_address,
     trade.evt_index AS evt_index,
-    trade."fromAmount"/1e18 * rates_a.currency_rate/1e18 as token_a_amount,
-    trade."toAmount"/1e18 * rates_a.currency_rate/1e18 as token_b_amount,
-    rates_a.currency_rate/1e18 as token_a_usd_price,
-    rates_b.currency_rate/1e18 as token_b_usd_price
+    trade."fromAmount"/1e18 * (SELECT currency_rate FROM rates WHERE trade."fromCurrencyKey" = currency_key AND evt_block_time < trade.evt_block_time ORDER BY evt_block_time DESC LIMIT 1)/1e18 as token_a_amount,
+    trade."toAmount"/1e18 * (SELECT currency_rate FROM rates WHERE trade."toCurrencyKey" = currency_key AND evt_block_time < trade.evt_block_time ORDER BY evt_block_time DESC LIMIT 1)/1e18 as token_b_amount
 FROM
-    synthetix."Synthetix_evt_SynthExchange" trade
-LEFT JOIN synthetix."view_synths" synths_a
-    ON trade."fromCurrencyKey" = synths_a.currency_key
-    AND trade.evt_block_time >= synths_a.evt_block_time
-    AND trade.evt_block_time < synths_a.max_block_time
-LEFT JOIN synthetix."view_synths" synths_b
-    ON trade."toCurrencyKey" = synths_b.currency_key
-    AND trade.evt_block_time >= synths_b.evt_block_time
-    AND trade.evt_block_time < synths_b.max_block_time
-LEFT JOIN synthetix."view_synths_rates" rates_a
-    ON trade."fromCurrencyKey" = rates_a.currency_key
-    AND trade.evt_block_time >= rates_a.evt_block_time
-    AND trade.evt_block_time < rates_a.max_block_time
-LEFT JOIN synthetix."view_synths_rates" rates_b
-    ON trade."toCurrencyKey" = rates_b.currency_key
-    AND trade.evt_block_time >= rates_b.evt_block_time
-    AND trade.evt_block_time < rates_b.max_block_time
-;
+    synthetix."Synthetix_evt_SynthExchange" trade;
+
+CREATE UNIQUE INDEX IF NOT EXISTS view_trades_id ON synthetix.view_trades (block_time,evt_index);
+CREATE INDEX view_trades_block_time ON synthetix.view_trades (block_time);
+CREATE INDEX view_trades_token_a_address ON synthetix.view_trades (token_a_address);
+CREATE INDEX view_trades_token_b_address ON synthetix.view_trades (token_b_address);
+CREATE INDEX view_trades_token_a_amount ON synthetix.view_trades (token_a_amount);
+
+SELECT cron.schedule('0,30 * * * *', 'REFRESH MATERIALIZED VIEW CONCURRENTLY synthetix.view_trades');
+COMMIT;
