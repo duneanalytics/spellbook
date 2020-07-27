90abc8b08ff04f605ca228311fc80602f917e6a1
dvd-schwrtz <dbschwartz17@gmail.com>
2020-07-26 17:00:41 -0700

    manually add sUSD rates

--- schema/synthetix/view_rates.sql
+++ schema/synthetix/view_rates.sql
@@ -1,3 +1,9 @@
 CREATE OR REPLACE VIEW synthetix.view_rates AS 
 SELECT currency_key, currency_rate, evt_block_time
 FROM synthetix."ExchangeRates_evt_RatesUpdated" r, unnest("currencyKeys", "newRates") as u(currency_key, currency_rate)
+
+UNION 
+
+VALUES
+('\x7355534400000000000000000000000000000000000000000000000000000000'::bytea, 1000000000000000000::bigint,	'2019-03-11T22:17:52.000Z'::timestamptz)
+;
