8f14254834dd11b2ff17a6ef242abcc4d7842f96
dvd-schwrtz <dbschwartz17@gmail.com>
2020-08-06 12:36:57 -0700

    add chainlink rate updates

--- schema/synthetix/rates.sql
+++ schema/synthetix/rates.sql
@@ -23,6 +23,17 @@ WITH rows AS (
     FROM synthetix."ExchangeRates_evt_RatesUpdated" r, unnest("currencyKeys", "newRates") as u(currency_key, currency_rate)
     WHERE evt_block_time >= start_ts
     AND evt_block_time < end_ts
+
+    UNION
+    
+    SELECT
+        cl.current * 1e10 as currency_rate,
+        agg.currency_key,
+        cl.evt_block_time as block_time
+    FROM chainlink."Aggregator_evt_AnswerUpdated" cl
+    INNER JOIN synthetix."ExchangeRates_evt_AggregatorAdded" agg ON agg.address = cl.contract_address;
+    WHERE evt_block_time >= start_ts
+    AND evt_block_time < end_ts
     ON CONFLICT DO NOTHING
     RETURNING 1
 )
@@ -34,4 +45,3 @@ $function$;
 INSERT INTO cron.job (schedule, command)
 VALUES ('1/5 * * * *', 'SELECT synthetix.insert_rates((SELECT max(block_time) FROM synthetix.rates));')
 ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
-
