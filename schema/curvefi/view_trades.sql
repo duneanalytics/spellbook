1b879b026956f41c0734901b1a0ba56c8607b80e
AA <angelangelov97@gmail.com>
2020-10-20 19:50:31 +0300

    fix precisions

--- schema/curvefi/view_trades.sql
+++ schema/curvefi/view_trades.sql
@@ -598,13 +598,13 @@ SELECT
         --change address back to renBTC's, right now Dune only tracks WBTC price
         WHEN bought_id = 0 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
         WHEN bought_id = 1 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
-        WHEN bought_id = 2 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
+        WHEN bought_id = 2 THEN '\xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6'::bytea
     END as token_a_address,
     CASE
         --change address back to renBTC's, right now Dune only tracks WBTC price
         WHEN sold_id = 0 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
         WHEN sold_id = 1 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
-        WHEN sold_id = 2 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
+        WHEN sold_id = 2 THEN '\xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6'::bytea
     END as token_b_address,
     contract_address AS exchange_contract_address,
     evt_tx_hash AS tx_hash,
@@ -626,12 +626,12 @@ SELECT
     tokens_sold AS token_b_amount_raw,
     CASE
         --change address back to renBTC's, right now Dune only tracks WBTC price
-        WHEN bought_id = 0 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
+        WHEN bought_id = 0 THEN '\xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6'::bytea
         WHEN bought_id = 1 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
     END as token_a_address,
     CASE
         --change address back to renBTC's, right now Dune only tracks WBTC price
-        WHEN sold_id = 0 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
+        WHEN sold_id = 0 THEN '\xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6'::bytea
         WHEN sold_id = 1 THEN '\x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'::bytea
     END as token_b_address,
     contract_address AS exchange_contract_address,
