dfb18784498864e6ed4e1f173507982785615588
Mats <mats@plysjbyen.net>
2020-12-03 15:10:44 +0100

    fmt

--- labels/queries/eth2_depositor.sql
+++ labels/queries/eth2_depositor.sql
@@ -5,8 +5,7 @@ SELECT DISTINCT
     'hagaetc' AS author
 FROM
     ethereum. "traces"
-WHERE
-    block_number >= 11182202
-    AND "to" = '\x00000000219ab540356cBB839Cbe05303d7705Fa'
-    AND success = TRUE
-    AND block_time >= '{{timestamp}}'
+WHERE block_number >= 11182202
+AND "to" = '\x00000000219ab540356cBB839Cbe05303d7705Fa'
+AND success = TRUE
+AND block_time >= '{{timestamp}}'
