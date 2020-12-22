54d4ea1e2953c3f8533627b86851b021326a8cce
marcuswin <46599751+marcuswin@users.noreply.github.com>
2020-12-22 15:57:49 +0300

    Refactor susd pool and remove redundant conditions

--- schema/curvefi/view_trades.sql
+++ schema/curvefi/view_trades.sql
@@ -69,154 +69,50 @@ FROM curvefi."susd_v2_evt_TokenExchangeUnderlying"
 UNION
 
 SELECT
-    a.evt_block_time AS block_time,
+    evt_block_time AS block_time,
     'Curve' AS project,
     NULL::text AS version,
     buyer AS trader_a,
     NULL::bytea AS trader_b,
-    (tokens_bought / 4) AS token_a_amount_raw,
-    (tokens_sold / 4) AS token_b_amount_raw,
-    '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea as token_a_address,
-    '\xF61718057901F84C4eEC4339EF8f0D86D2B45600'::bytea as token_b_address,
-    a.contract_address AS exchange_contract_address,
-    a.evt_tx_hash AS tx_hash,
+    tokens_bought AS token_a_amount_raw,
+    tokens_sold AS token_b_amount_raw,
+    CASE
+        WHEN bought_id = 0 THEN '\xF61718057901F84C4eEC4339EF8f0D86D2B45600'::bytea
+        WHEN bought_id = 1 THEN '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea
+    END as token_a_address,
+    CASE
+        WHEN sold_id = 0 THEN '\xF61718057901F84C4eEC4339EF8f0D86D2B45600'::bytea
+        WHEN sold_id = 1 THEN '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea
+    END as token_b_address,
+    contract_address AS exchange_contract_address,
+    evt_tx_hash AS tx_hash,
     NULL::integer[] AS trace_address,
-    a.evt_index AS evt_index
-FROM curvefi."susd_evt_TokenExchange" a
-WHERE bought_id = 0
+    evt_index
+FROM curvefi."susd_evt_TokenExchange"
 
 UNION
 
 SELECT
-    a.evt_block_time AS block_time,
+    evt_block_time AS block_time,
     'Curve' AS project,
     NULL::text AS version,
     buyer AS trader_a,
     NULL::bytea AS trader_b,
-    (tokens_bought / 4) AS token_a_amount_raw,
-    (tokens_sold / 4) AS token_b_amount_raw,
-    '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea as token_a_address,
-    '\xF61718057901F84C4eEC4339EF8f0D86D2B45600'::bytea as token_b_address,
-    a.contract_address AS exchange_contract_address,
-    a.evt_tx_hash AS tx_hash,
+    tokens_bought AS token_a_amount_raw,
+    tokens_sold AS token_b_amount_raw,
+    CASE
+        WHEN bought_id = 0 THEN '\xF61718057901F84C4eEC4339EF8f0D86D2B45600'::bytea
+        WHEN bought_id = 1 THEN '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea
+    END as token_a_address,
+    CASE
+        WHEN sold_id = 0 THEN '\xF61718057901F84C4eEC4339EF8f0D86D2B45600'::bytea
+        WHEN sold_id = 1 THEN '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea
+    END as token_b_address,
+    contract_address AS exchange_contract_address,
+    evt_tx_hash AS tx_hash,
     NULL::integer[] AS trace_address,
-    a.evt_index AS evt_index
-FROM curvefi."susd_evt_TokenExchange" a
-WHERE bought_id = 1
-
-UNION
-
-SELECT
-    a.evt_block_time AS block_time,
-    'Curve' AS project,
-    NULL::text AS version,
-    buyer AS trader_a,
-    NULL::bytea AS trader_b,
-    (tokens_bought / 4) AS token_a_amount_raw,
-    (tokens_sold / 4) AS token_b_amount_raw,
-    '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea as token_a_address,
-    '\xF61718057901F84C4eEC4339EF8f0D86D2B45600'::bytea as token_b_address,
-    a.contract_address AS exchange_contract_address,
-    a.evt_tx_hash AS tx_hash,
-    NULL::integer[] AS trace_address,
-    a.evt_index AS evt_index
-FROM curvefi."susd_evt_TokenExchange" a
-WHERE bought_id = 2
-
-UNION
-
-SELECT
-    a.evt_block_time AS block_time,
-    'Curve' AS project,
-    NULL::text AS version,
-    buyer AS trader_a,
-    NULL::bytea AS trader_b,
-    (tokens_bought / 4) AS token_a_amount_raw,
-    (tokens_sold / 4) AS token_b_amount_raw,
-    '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea as token_a_address,
-    '\xF61718057901F84C4eEC4339EF8f0D86D2B45600'::bytea as token_b_address,
-    a.contract_address AS exchange_contract_address,
-    a.evt_tx_hash AS tx_hash,
-    NULL::integer[] AS trace_address,
-    a.evt_index AS evt_index
-FROM curvefi."susd_evt_TokenExchange" a
-WHERE bought_id = 3
-
-UNION
-
-SELECT
-    a.evt_block_time AS block_time,
-    'Curve' AS project,
-    NULL::text AS version,
-    buyer AS trader_a,
-    NULL::bytea AS trader_b,
-    (tokens_bought / 4) AS token_a_amount_raw,
-    (tokens_sold / 4) AS token_b_amount_raw,
-    '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea as token_a_address,
-    '\x57Ab1ec28D129707052df4dF418D58a2D46d5f51'::bytea as token_b_address,
-    a.contract_address AS exchange_contract_address,
-    a.evt_tx_hash AS tx_hash,
-    NULL::integer[] AS trace_address,
-    a.evt_index AS evt_index
-FROM curvefi."susd_evt_TokenExchangeUnderlying" a
-WHERE bought_id = 0
-
-UNION
-
-SELECT
-    a.evt_block_time AS block_time,
-    'Curve' AS project,
-    NULL::text AS version,
-    buyer AS trader_a,
-    NULL::bytea AS trader_b,
-    (tokens_bought / 4) AS token_a_amount_raw,
-    (tokens_sold / 4) AS token_b_amount_raw,
-    '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea as token_a_address,
-    '\x57Ab1ec28D129707052df4dF418D58a2D46d5f51'::bytea as token_b_address,
-    a.contract_address AS exchange_contract_address,
-    a.evt_tx_hash AS tx_hash,
-    NULL::integer[] AS trace_address,
-    a.evt_index AS evt_index
-FROM curvefi."susd_evt_TokenExchangeUnderlying" a
-WHERE bought_id = 1
-
-UNION
-
-SELECT
-    a.evt_block_time AS block_time,
-    'Curve' AS project,
-    NULL::text AS version,
-    buyer AS trader_a,
-    NULL::bytea AS trader_b,
-    (tokens_bought / 4) AS token_a_amount_raw,
-    (tokens_sold / 4) AS token_b_amount_raw,
-    '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea as token_a_address,
-    '\x57Ab1ec28D129707052df4dF418D58a2D46d5f51'::bytea as token_b_address,
-    a.contract_address AS exchange_contract_address,
-    a.evt_tx_hash AS tx_hash,
-    NULL::integer[] AS trace_address,
-    a.evt_index AS evt_index
-FROM curvefi."susd_evt_TokenExchangeUnderlying" a
-WHERE bought_id = 2
-
-UNION
-
-SELECT
-    a.evt_block_time AS block_time,
-    'Curve' AS project,
-    NULL::text AS version,
-    buyer AS trader_a,
-    NULL::bytea AS trader_b,
-    (tokens_bought / 4) AS token_a_amount_raw,
-    (tokens_sold / 4) AS token_b_amount_raw,
-    '\xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8'::bytea as token_a_address,
-    '\x57Ab1ec28D129707052df4dF418D58a2D46d5f51'::bytea as token_b_address,
-    a.contract_address AS exchange_contract_address,
-    a.evt_tx_hash AS tx_hash,
-    NULL::integer[] AS trace_address,
-    a.evt_index AS evt_index
-FROM curvefi."susd_evt_TokenExchangeUnderlying" a
-WHERE bought_id = 3
+    evt_index
+FROM curvefi."susd_evt_TokenExchangeUnderlying"
 
 UNION
 
@@ -612,9 +508,7 @@ SELECT
     evt_index
 FROM curvefi."sbtc_evt_TokenExchange"
 
-
 UNION
-
 
 SELECT
     evt_block_time AS block_time,
@@ -640,9 +534,7 @@ SELECT
     evt_index
 FROM curvefi."hbtc_evt_TokenExchange"
 
-
 UNION
-
 
 SELECT
     evt_block_time AS block_time,
