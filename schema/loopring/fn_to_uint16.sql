b5f23ddd9ebdd3627e9a67ddcf9cd107db40ee79
Mats <mats@plysjbyen.net>
2021-01-08 09:17:36 +0100

    remove uint-conversion

--- schema/loopring/fn_to_uint16.sql
+++
@@ -1,7 +0,0 @@
-DROP FUNCTION loopring.fn_to_uint16;
-
-CREATE OR REPLACE FUNCTION loopring.fn_to_uint16(data bytea) RETURNS integer AS $$
-BEGIN
-    return get_byte(data, 0) * 256 + get_byte(data, 1);
-END; $$
-LANGUAGE PLPGSQL;
--- schema/loopring/fn_to_uint32.sql
+++
@@ -1,7 +0,0 @@
-DROP FUNCTION loopring.fn_to_uint32;
-
-CREATE OR REPLACE FUNCTION loopring.fn_to_uint32(data bytea) RETURNS integer AS $$
-BEGIN
-    return (get_byte(data, 0) & 127) * 16777216 + get_byte(data, 1) * 65536  + get_byte(data, 2) * 256 + get_byte(data, 3);
-END; $$
-LANGUAGE PLPGSQL;
