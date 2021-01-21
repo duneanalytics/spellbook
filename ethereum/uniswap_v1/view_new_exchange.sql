c45d025ca24f784c04c7d4ea23bf0d3c08ed03e2
Mats <mats@plysjbyen.net>
2021-01-21 10:18:29 +0100

    move uniswap views

--- ethereum/uniswap_v1/view_new_exchange.sql
+++ ethereum/uniswap_v1/view_new_exchange.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW uniswap.view_new_exchange AS
+CREATE OR REPLACE VIEW uniswap_v1.view_new_exchange AS
 SELECT
     t.symbol AS token_symbol,
     e.token AS token_address,
@@ -7,6 +7,5 @@ SELECT
     e.evt_tx_hash AS tx_hash,
     e.evt_block_time AS block_time
 FROM
-    uniswap. "Factory_evt_NewExchange" e
+    uniswap."Factory_evt_NewExchange" e
     LEFT JOIN erc20.tokens t ON t.contract_address = e.token;
-
