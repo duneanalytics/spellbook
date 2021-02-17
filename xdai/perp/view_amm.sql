b14460c18cc22bdb68313c0533a245a53665615a
milkyklim <10698619+milkyklim@users.noreply.github.com>
2021-02-17 20:48:01 +0100

    fix namespace

--- xdai/perp/view_amm.sql
+++ xdai/perp/view_amm.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW view_amm (symbol, base_symbol, quote_symbol, contract_address, quote_token_decimals, quote_token_address) AS VALUES
+CREATE OR REPLACE VIEW perp.view_amm (symbol, base_symbol, quote_symbol, contract_address, quote_token_decimals, quote_token_address) AS VALUES
    ('ETH/USDC'::text, 'ppETH'::text, 'USDC'::text,   '\x8d22f1a9dce724d8c1b4c688d75f17a2fe2d32df'::bytea, 6::numeric, '\xddafbb505ad214d7b80b1f830fccc89b60fb7a83'::bytea),
    ('BTC/USDC'::text, 'ppBTC'::text, 'USDC'::text,   '\x0f346e19f01471c02485df1758cfd3d624e399b4'::bytea, 6::numeric, '\xddafbb505ad214d7b80b1f830fccc89b60fb7a83'::bytea),
    ('YFI/USDC'::text, 'ppYFI'::text, 'USDC'::text,   '\xd41025350582674144102b74b8248550580bb869'::bytea, 6::numeric, '\xddafbb505ad214d7b80b1f830fccc89b60fb7a83'::bytea),
