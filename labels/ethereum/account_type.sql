a676af869a10da34da5ee9ff5708003f0651f0e9
Danning Sui <danningsui@DanningrkLaptop.attlocal.net>
2021-03-08 13:43:40 -0800

    lowercase all the column content

--- labels/ethereum/account_type.sql
+++ labels/ethereum/account_type.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT
     address,
-    'Contract' AS label,
-    'Account Type' AS type,
+    'contract' AS label,
+    'account type' AS type,
     'sui414' AS author
 FROM ethereum."traces"
 WHERE type = 'create' and success is true
