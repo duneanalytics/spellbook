55b3d1c635123debdd6ea5646cd164343eef3478
Mats Olsen <mats@duneanalytics.com>
2021-03-15 14:25:37 +0100

    add back account type

---
+++ labels/ethereum/account_type.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT
+    address,
+    'contract' AS label,
+    'account type' AS type,
+    'sui414' AS author
+FROM ethereum.traces
+WHERE "type" = 'create'
+AND success
+AND block_time >= '{{timestamp}}';
