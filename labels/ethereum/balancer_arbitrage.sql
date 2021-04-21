752aa00c22464b5179f29b771266d0725a812430
Mats Julian Olsen <mats@duneanalytics.com>
2021-04-21 11:01:22 +0000

    Fix balancer_arbitrage.sql

--- labels/ethereum/balancer_arbitrage.sql
+++ labels/ethereum/balancer_arbitrage.sql
@@ -10,8 +10,8 @@ AND t1.token_a_address = t2.token_b_address
 AND t1.token_b_address = t2.token_a_address 
 AND ((t1.project = 'Balancer' AND t2.project = 'Uniswap') or (t1.project = 'Uniswap' AND t2.project = 'Balancer'))
 INNER JOIN ethereum.transactions t ON t.hash = t1.tx_hash
-WHERE t1.block_time >= {{timestamp}}
-AND t2.block_time >= {{timestamp}}
+WHERE t1.block_time >= '{{timestamp}}'
+AND t2.block_time >= '{{timestamp}}'
 UNION ALL
 SELECT
     DISTINCT(t.to) AS address,
@@ -25,5 +25,5 @@ AND t1.token_a_address = t2.token_b_address
 AND t1.token_b_address = t2.token_a_address
 AND ((t1.project = 'Balancer' AND t2.project = 'Sushiswap') or (t1.project = 'Sushiswap' AND t2.project = 'Balancer'))
 INNER JOIN ethereum.transactions t ON t.hash = t1.tx_hash
-WHERE t1.block_time >= {{timestamp}}
-AND t2.block_time >= {{timestamp}}
+WHERE t1.block_time >= '{{timestamp}}'
+AND t2.block_time >= '{{timestamp}}'
