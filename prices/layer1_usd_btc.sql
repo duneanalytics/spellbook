dd49240a48d703fcddc477c733c7c52fe8782db9
Wilhelm <va3093@gmail.com>
2021-04-28 11:20:48 +0100

    remove perms

--- prices/layer1_usd_btc.sql
+++ prices/layer1_usd_btc.sql
@@ -3,6 +3,3 @@ CREATE OR REPLACE VIEW prices.layer1_usd_btc AS
 SELECT *
 FROM prices.layer1_usd
 WHERE symbol = 'BTC';
-
-GRANT SELECT ON prices.layer1_usd_btc TO reader;
-GRANT SELECT, INSERT, UPDATE ON prices.layer1_usd_btc TO dune;
--- prices/layer1_usd_eth.sql
+++ prices/layer1_usd_eth.sql
@@ -3,7 +3,3 @@ CREATE OR REPLACE VIEW prices.layer1_usd_eth AS
 SELECT *
 FROM prices.layer1_usd
 WHERE symbol = 'ETH';
-
-
-GRANT SELECT ON prices.layer1_usd_eth TO reader;
-GRANT SELECT, INSERT, UPDATE ON prices.layer1_usd_eth TO dune;
