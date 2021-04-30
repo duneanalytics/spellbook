5da46cf2209313f9310e24bc4dc27ff4364bf7f4
Vasilije1990 <vas.markovic@gmail.com>
2021-04-30 04:08:30 -0400

    Add view to query token balances per hour

--- ethereum/token_balances/view_token_balances.sql
+++ ethereum/token_balances/view_token_balances.sql
@@ -1,7 +1,37 @@
 
 -- view to query balances over time per hour
 
-create view vasa.balances_per_hour as
+create or replace view vasa.balances_per_hour as
+with hours AS (
+    SELECT generate_series('2015-07-01'::timestamp, date_trunc('hour', NOW()), '1 hour') AS hour_ -- Generate all days since the first contract
+    )
+, token_balances_updated as (
+	select
+	ts,
+	address,
+	contract_address,
+	token,
+	amount,
+	lead(ts, 1, now()) OVER (PARTITION BY contract_address, address ORDER BY ts) AS next_hour
+	from  vasa.token_balances_proposal_2
+)
+, balance_all_days AS (
+    SELECT  d.hour_,
+            address,
+            b.token::text as symbol,
+            b.contract_address,
+            amount AS balance
+    FROM token_balances_updated  b
+    INNER JOIN hours d ON b.ts <= d.hour_ AND d.hour_ < b.next_hour -- Yields an observation for every day after the first transfer until the next day with transfer
+    )
+
+    select * from balance_all_days;
+
+
+-- balance per token per hour
+
+
+create or replace view vasa.balances_per__token_per_hour as
 with hours AS (
     SELECT generate_series('2015-07-01'::timestamp, date_trunc('hour', NOW()), '1 hour') AS hour_ -- Generate all days since the first contract
     )
@@ -18,14 +48,12 @@ with hours AS (
 )
 , balance_all_days AS (
     SELECT  d.hour_,
-            address,
-            erc.symbol,
-            b.contract_address,
-            amount AS balance
+            token,
+            sum(amount) AS balance
     FROM token_balances_updated  b
     INNER JOIN hours d ON b.ts <= d.hour_ AND d.hour_ < b.next_hour -- Yields an observation for every day after the first transfer until the next day with transfer
-    INNER JOIN erc20.tokens erc ON b.contract_address = erc.contract_address
-    ORDER BY 1, 2, 3, 4
+    group by 1,2
     )
 
-    select * from balance_all_days;
+    select * from balance_all_days
+   ;
