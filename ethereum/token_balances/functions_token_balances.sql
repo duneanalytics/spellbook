298a5e2026d5697f99e82288cfd16bc418a2e944
Vasilije1990 <vas.markovic@gmail.com>
2021-04-30 04:49:20 -0400

    Added a function to list token wallets for a time range specified

--- ethereum/token_balances/functions_token_balances.sql
+++ ethereum/token_balances/functions_token_balances.sql
@@ -1,4 +1,4 @@
-create or replace function token_balance_in_time(from_ timestamptz, until_ timestamptz,token_ varchar)
+create or replace function token_balance_over_time(from_ timestamptz, until_ timestamptz,token_ varchar)
    	returns table (
 		time_ timestamptz,
 		balance numeric
@@ -31,6 +31,53 @@ with hours AS (
     FROM token_balances_updated  b
     INNER JOIN hours d ON b.ts <= d.hour_ AND d.hour_ < b.next_hour
     group by 1
+    )
+
+    select * from balance_all_days
+   ;
+end;
+$$
+
+
+
+-- token wallets in a point in time
+
+
+create or replace function token_addresses_over_time(from_ timestamptz, until_ timestamptz,token_ varchar)
+   	returns table (
+		time_ timestamptz,
+		selected_token varchar ,
+        wallet_address bytea
+	)
+   language plpgsql
+  as
+$$
+declare
+-- variable declaration
+begin
+RETURN QUERY
+with hours AS (
+    SELECT generate_series(from_::timestamptz, until_::timestamptz, '1 hour') AS hour_
+    )
+, token_balances_updated as (
+	select
+	ts,
+	address,
+	contract_address,
+	token,
+	amount,
+
+	lead(ts, 1, now()) OVER (PARTITION BY contract_address, address ORDER BY ts) AS next_hour
+	from  vasa.token_balances_proposal_2
+	where token = token_
+)
+, balance_all_days AS (
+    SELECT  d.hour_ as time_,
+    		token,
+             address
+    FROM token_balances_updated  b
+    INNER JOIN hours d ON b.ts <= d.hour_ AND d.hour_ < b.next_hour
+    --group by 1,2
     )
 
     select * from balance_all_days
