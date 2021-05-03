8525cd59745afe910715003a74f5e71ec9fff5f4
Vasilije1990 <vas.markovic@gmail.com>
2021-05-03 02:11:40 -0400

    Fixed and optimised queries a bit and added a test script that verifies results with etherscan

--- ethereum/token_balances/functions_token_balances.sql
+++ ethereum/token_balances/functions_token_balances.sql
@@ -91,49 +91,6 @@ $$
 
 
 
-    create or replace function token_balance_per_address(from_ timestamptz,until_ timestamptz,  address_ bytea)
-   	returns table (
-		time_ timestamptz,
-		address__ bytea,
-		balance numeric
-	)
-   language plpgsql
-  as
-$$
-declare
--- variable declaration
-begin
-RETURN QUERY
-with hours AS (
-    SELECT generate_series(from_::timestamptz, until_::timestamptz, '1 hour') AS hour_
-    )
-, token_balances_updated as (
-	select
-	ts,
-	address,
-	contract_address,
-	token,
-	amount,
-
-	lead(ts, 1, now()) OVER (PARTITION BY contract_address, address ORDER BY ts) AS next_hour
-	from  vasa.token_balances_proposal_2
-	where address = address_
-)
-, balance_all_days AS (
-    SELECT  d.hour_ as time_,
-    address,
-            sum(amount) AS balance
-    FROM token_balances_updated  b
-    INNER JOIN hours d ON b.ts <= d.hour_ AND d.hour_ < b.next_hour
-    group by 1,2
-    )
-
-    select * from balance_all_days
-where balance >   0.0001
-   ;
-end;
-$$
-   ;
 
 
 
@@ -187,7 +144,7 @@ $$
 
 
 
-  create or replace function vasa.token_balance_per_address_json_version(from_ timestamptz,until_ timestamptz,  address_ bytea)
+  create or replace function vasa.token_balance_per_address(from_ timestamptz,until_ timestamptz,  address_ bytea)
    	returns table (
 		time_ timestamptz,
 		address__ bytea,
