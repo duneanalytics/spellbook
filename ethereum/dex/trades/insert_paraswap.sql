829d9a2041648aa41c7455b4ee20f9e755ef1a2d
milkyklim <10698619+milkyklim@users.noreply.github.com>
2021-06-03 16:38:06 +0200

    dex.trades: fix trade_id and end_block for paraswap

--- ethereum/dex/trades/insert_paraswap.sql
+++ ethereum/dex/trades/insert_paraswap.sql
@@ -53,7 +53,7 @@ WITH rows AS (
         tx."to" as tx_to,
         trace_address,
         evt_index,
-        row_number() OVER (PARTITION BY tx_hash, evt_index, trace_address) AS trade_id
+        row_number() OVER (PARTITION BY project, tx_hash, evt_index, trace_address ORDER BY version, category) AS trade_id
 
     FROM (
          
@@ -242,7 +242,7 @@ SELECT dex.insert_paraswap(
     '2021-01-01',
     now(),
     (SELECT max(number) FROM ethereum.blocks WHERE time < '2021-01-01'),
-    (SELECT max(number) FROM ethereum.blocks)
+    SELECT MAX(number) FROM ethereum.blocks WHERE time < now() - interval '20 minutes'
 )
 WHERE NOT EXISTS (
     SELECT *
@@ -258,6 +258,6 @@ VALUES ('*/10 * * * *', $$
         (SELECT max(block_time) - interval '1 days' FROM dex.trades WHERE project='Paraswap'),
         (SELECT now()),
         (SELECT max(number) FROM ethereum.blocks WHERE time < (SELECT max(block_time) - interval '1 days' FROM dex.trades WHERE project='Paraswap')),
-        (SELECT MAX(number) FROM ethereum.blocks));
+        SELECT MAX(number) FROM ethereum.blocks WHERE time < now() - interval '20 minutes');
 $$)
 ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
