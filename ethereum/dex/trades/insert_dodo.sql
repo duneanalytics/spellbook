c2bd8778ed73c52448cfba752b75a0c9913fea55
Wilhelm <va3093@gmail.com>
2021-06-03 10:27:09 +0100

    Revert "Revert "lag dex trades to ensure that we have prices for trades""
    This reverts commit 156bda0573a84d62abee4151ba86b7121a830d96.

--- ethereum/dex/trades/insert_dodo.sql
+++ ethereum/dex/trades/insert_dodo.sql
@@ -261,7 +261,7 @@ SELECT dex.insert_dodo(
     '2021-01-01',
     now(),
     (SELECT max(number) FROM ethereum.blocks WHERE time < '2021-01-01'),
-    (SELECT max(number) FROM ethereum.blocks)
+    SELECT MAX(number) FROM ethereum.blocks where time < now() - interval '20 minutes'
 )
 WHERE NOT EXISTS (
     SELECT *
@@ -275,8 +275,8 @@ INSERT INTO cron.job (schedule, command)
 VALUES ('*/10 * * * *', $$
     SELECT dex.insert_dodo(
         (SELECT max(block_time) - interval '1 days' FROM dex.trades WHERE project='dodo'),
-        (SELECT now()),
+        (SELECT now() - interval '20 minutes'),
         (SELECT max(number) FROM ethereum.blocks WHERE time < (SELECT max(block_time) - interval '1 days' FROM dex.trades WHERE project='dodo')),
-        (SELECT MAX(number) FROM ethereum.blocks));
+        SELECT MAX(number) FROM ethereum.blocks where time < now() - interval '20 minutes');
 $$)
 ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
