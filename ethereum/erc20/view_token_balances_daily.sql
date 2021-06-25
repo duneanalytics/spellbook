9883343a9dfcfa7dd2c40db1fd64b49ff6d5448f
Alan Ghobadi <aalan3@gmail.com>
2021-06-25 14:27:57 +0200

    Create view_token_balances_daily.sql

---
+++ ethereum/erc20/view_token_balances_daily.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW erc20.view_token_balances_daily AS
+SELECT 
+wallet_address,
+token_address,
+t.symbol as token_symbol,
+amount_raw,
+amount_raw / 10^coalesce(t.decimals, null) amount,
+amount_raw / 10^coalesce(t.decimals, null) * p.price amount_usd,
+generate_series(min(timestamp), date_trunc('day', NOW()), '1 day') as day
+FROM erc20.token_balances
+left join erc20.tokens t on t.contract_address = token_address
+left join prices.usd p on p.contract_address = token_address and p.minute = timestamp
+group by 1, 2, 3, 4, 5, 6
