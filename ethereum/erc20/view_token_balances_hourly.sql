43a809ff2c5fe8400a07b81f0c2b84946b8f7ad3
Alan Ghobadi <aalan3@gmail.com>
2021-06-25 14:27:54 +0200

    Change name to hourly

--- ethereum/erc20/view_token_balances_hourly.sql
+++ ethereum/erc20/view_token_balances_hourly.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW erc20.view_token_balances AS
+CREATE OR REPLACE VIEW erc20.view_token_balances_hourly AS
 SELECT 
 wallet_address,
 token_address,
@@ -6,7 +6,7 @@ t.symbol as token_symbol,
 amount_raw,
 amount_raw / 10^coalesce(t.decimals, null) amount,
 amount_raw / 10^coalesce(t.decimals, null) * p.price amount_usd,
-generate_series(min(timestamp), date_trunc('hour', NOW()), '1 hour') as timestamp
+generate_series(min(timestamp), date_trunc('hour', NOW()), '1 hour') as hour
 FROM erc20.token_balances
 left join erc20.tokens t on t.contract_address = token_address
 left join prices.usd p on p.contract_address = token_address and p.minute = timestamp
