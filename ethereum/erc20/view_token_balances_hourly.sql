cdefa5a4aa0e6987ab4b47465b22c08cb9d86f7e
Alan Ghobadi <aalan3@gmail.com>
2021-06-28 17:42:23 +0200

    Create series' in correct way

--- ethereum/erc20/view_token_balances_hourly.sql
+++ ethereum/erc20/view_token_balances_hourly.sql
@@ -1,13 +1,18 @@
 CREATE OR REPLACE VIEW erc20.view_token_balances_hourly AS
-SELECT 
-wallet_address,
-token_address,
-t.symbol as token_symbol,
-amount_raw,
-amount_raw / 10^coalesce(t.decimals, null) amount,
-amount_raw / 10^coalesce(t.decimals, null) * p.price amount_usd,
-generate_series(min(timestamp), date_trunc('hour', NOW()), '1 hour') as hour
-FROM erc20.token_balances
+with hours as (
+    select hour from generate_series('2015-01-01', date_trunc('hour', NOW()), '1 hour') g(hour)
+),
+b as (
+    SELECT 
+    tb.wallet_address,
+    tb.token_address,
+    tb.amount_raw,
+    timestamp as hour,
+    lead(date_trunc('hour', timestamp), 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY date_trunc('hour', timestamp)) AS next_hour 
+    FROM erc20.token_balances tb
+)
+SELECT h.hour, b.wallet_address, b.token_address, b.amount_raw
+FROM b
+INNER JOIN hours h ON b.hour <= h.hour AND h.hour < b.next_hour
 left join erc20.tokens t on t.contract_address = token_address
-left join prices.usd p on p.contract_address = token_address and p.minute = timestamp
-group by 1, 2, 3, 4, 5, 6
+left join prices.usd p on p.contract_address = token_address and p.minute = h.hour
