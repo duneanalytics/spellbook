9b8da1bf6bab921597968d246a3313d2427dd527
masquot <78954627+masquot@users.noreply.github.com>
2021-08-11 16:34:57 +0200

    Integrate erc transfer data for NFTS (#430)
    ## Notes for reviewing and merging
    
    This is a major update based on input of of the community (previous PR https://github.com/duneanalytics/abstractions/pull/414):
    - integration of data for bundle trades enables more sophisticated analysis
    - integration of corresponding erc721, erc1155 or erc20 transfer data for every trade
    
    The table schema has been modified and extended to accomodate extra data points. Therefore, the existing table will need to be deleted, as well as the existing `insert_xxx` functions and cron jobs.
    
    Thank you very much

--- ethereum/nft/trades/insert_foundation.sql
+++ ethereum/nft/trades/insert_foundation.sql
@@ -3,40 +3,98 @@ LANGUAGE plpgsql AS $function$
 DECLARE r integer;
 BEGIN
 
-WITH rows AS (
+-- Get ERC721 and ERC1155 transfer data for every trade transaction
+WITH foundation_erc_union AS (
+SELECT
+    erc721.evt_tx_hash,
+    'erc721' as erc_type,
+    CAST(erc721."tokenId" AS TEXT) AS "tokenId",
+    erc721."from",
+    erc721."to",
+    erc721.contract_address,
+    NULL::numeric AS value
+FROM erc721."ERC721_evt_Transfer" erc721
+INNER JOIN foundation."market_evt_ReserveAuctionFinalized" f ON erc721.evt_tx_hash = f.evt_tx_hash
+WHERE erc721.evt_block_time >= start_ts
+AND erc721.evt_block_time < end_ts
+AND erc721."from" <> '\x0000000000000000000000000000000000000000' -- exclude mints
+UNION ALL
+SELECT
+    erc1155.evt_tx_hash,
+    'erc1155' as erc_type,
+    CAST(erc1155.id AS TEXT) AS "tokenId",
+    erc1155."from",
+    erc1155."to",
+    erc1155.contract_address,
+    erc1155.value
+FROM erc1155."ERC1155_evt_TransferSingle" erc1155
+INNER JOIN foundation."market_evt_ReserveAuctionFinalized" f ON erc1155.evt_tx_hash = f.evt_tx_hash
+WHERE erc1155.evt_block_time >= start_ts
+AND erc1155.evt_block_time < end_ts
+AND erc1155."from" <> '\x0000000000000000000000000000000000000000' -- exclude mints
+),
+-- aggregate NFT transfers per transaction 
+foundation_erc_subsets AS (
+SELECT
+    evt_tx_hash,
+    array_agg("tokenId") AS token_id_array,
+    cardinality(array_agg("tokenId")) AS no_of_transfers,
+    array_agg("from") AS from_array,
+    array_agg("to") AS to_array,
+    array_agg(erc_type) AS erc_type_array,
+    array_agg(contract_address) AS contract_address_array,
+    array_agg(value) AS erc1155_value_array
+FROM foundation_erc_union
+GROUP BY 1
+),
+rows AS (
     INSERT INTO nft.trades (
-	block_time,
-	nft_project_name,
-	nft_token_id,
-	platform,
-	platform_version,
-	category,
-	evt_type,
-	usd_amount,
-	seller,
-	buyer,
-	original_amount,
-	original_amount_raw,
-	original_currency,
-	original_currency_contract,
-	currency_contract,
-	nft_contract_address,
-	exchange_contract_address,
-	tx_hash,
-	block_number,
-	tx_from,
-	tx_to,
-	trace_address,
-	evt_index,
-	trade_id
+        block_time,
+        nft_project_name,
+        nft_token_id,
+        erc_standard,
+        platform,
+        platform_version,
+        trade_type,
+        number_of_items,
+        category,
+        evt_type,
+        usd_amount,
+        seller,
+        buyer,
+        original_amount,
+        original_amount_raw,
+        original_currency,
+        original_currency_contract,
+        currency_contract,
+        nft_contract_address,
+        exchange_contract_address,
+        tx_hash,
+        block_number,
+        nft_token_ids_array,
+        senders_array,
+        recipients_array,
+        erc_types_array,
+        nft_contract_addresses_array,
+        erc_values_array,
+        tx_from,
+        tx_to,
+        trace_address,
+        evt_index,
+        trade_id
     )
 
     SELECT
         trades.evt_block_time AS block_time,
         tokens.name AS nft_project_name,
-        CAST(created."tokenId" AS TEXT) AS nft_token_id,
+        -- Set NFT token ID to `NULL` if the trade consists of multiple NFT transfers
+        CASE WHEN erc.no_of_transfers > 1 THEN NULL ELSE CAST(created."tokenId" AS TEXT) END AS nft_token_id,
+        -- Set ERC standard to `NULL` if the trade consists of multiple NFT transfers
+        CASE WHEN erc.no_of_transfers > 1 THEN NULL ELSE COALESCE(erc.erc_type_array[1], tokens.standard) END AS erc_standard,
         platform,
         platform_version,
+        CASE WHEN erc.no_of_transfers > 1 THEN 'Bundle Trade' ELSE 'Single Item Trade' END AS trade_type,
+        erc.no_of_transfers AS number_of_items,
         category,
         evt_type,
         (trades."f8nFee" + trades."ownerRev" + trades."creatorFee") / 10 ^ 18 * p.price AS usd_amount, --
@@ -51,6 +109,14 @@ WITH rows AS (
         trades.contract_address AS exchange_contract_address, -- Foundation: Market
         trades.evt_tx_hash AS tx_hash,
         trades.evt_block_number AS block_number,
+        -- Sometimes multiple NFT transfers occur in a given trade; the 'array' fields below provide info for these use cases 
+        erc.token_id_array AS nft_token_ids_array,
+        erc.token_id_array AS nft_token_ids_array,
+        erc.from_array AS senders_array,
+        erc.to_array AS recipients_array,
+        erc.erc_type_array AS erc_types_array,
+        erc.contract_address_array AS nft_contract_addresses_array,
+        erc.erc1155_value_array AS erc_values_array,
         tx."from" AS tx_from,
         tx."to" AS tx_to,
         NULL::integer[] AS trace_address,
@@ -70,6 +136,7 @@ WITH rows AS (
         AND tx.block_time < end_ts
         AND tx.block_number >= start_block
         AND tx.block_number < end_block
+    LEFT JOIN foundation_erc_subsets erc ON erc.evt_tx_hash = trades.evt_tx_hash
     LEFT JOIN foundation."market_evt_ReserveAuctionCreated" created ON trades."auctionId" = created."auctionId"
     LEFT JOIN nft.tokens tokens ON tokens.contract_address = created."nftContract"
     LEFT JOIN prices.usd p ON p.minute = date_trunc('minute', trades.evt_block_time)
--- ethereum/nft/trades/insert_rarible.sql
+++ ethereum/nft/trades/insert_rarible.sql
@@ -3,7 +3,7 @@ LANGUAGE plpgsql AS $function$
 DECLARE r integer;
 BEGIN
 
-WITH all_data AS (
+WITH rarible_trades AS (
 -- Get data from various Rarible contracts deployed over time
 -- Oct 2019 fading out in Summer 2020
     SELECT
@@ -127,7 +127,8 @@ WITH all_data AS (
         END, 
         'ExchangeV1_evt_Buy' as category
     FROM rarible."ExchangeV1_evt_Buy"
-    where "buyTokenId" = 0 --buy
+    where "buyTokenId" = 0 AND "sellTokenId" <> 0 
+ --buy
     UNION ALL
 -- from Nov 2020 ongoing
     SELECT
@@ -154,86 +155,138 @@ WITH all_data AS (
     where "sellTokenId" = 0 
     UNION ALL
 -- from 2021-06-15 onwards
--- 'Purchases' in ETH
+    -- ETH purchases of ERC1155 or ERC721 
     SELECT
-        'Rarible' as platform,
-        '2' as platform_version,
+        'Rarible' AS platform,
+        '2' AS platform_version,
         contract_address AS exchange_contract_address,
-        'Trade' as evt_type,
-        tx_hash AS evt_tx_hash,
-        block_time AS evt_block_time,
-        block_number AS evt_block_number,
-        "index" AS evt_index,
-        substring(data FROM 365 FOR 20) AS nft_contract_address,
-        CAST(bytea2numericpy(substring(data FROM 385 FOR 32)) AS TEXT) AS nft_token_id,
-        substring(data FROM 77 FOR 20) AS seller,
-        substring(data FROM 109 FOR 20) AS buyer,
-        bytea2numericpy(substring(data FROM 129 FOR 32)) original_amount_raw,
-        '\x0000000000000000000000000000000000000000'::bytea as original_currency_contract,
-        '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::bytea as currency_contract,
+        'Trade' AS evt_type,
+        evt_tx_hash,
+        evt_block_time,
+        evt_block_number,
+        evt_index,
+        decode(substring("leftAsset"->>'data' FROM 27 FOR 40), 'hex') AS nft_contract_address,
+        CAST(bytea2numericpy(decode(substring("leftAsset"->>'data' FROM 67 FOR 64), 'hex')) AS TEXT) AS nft_token_id,
+        "leftMaker" AS seller,
+        "rightMaker" AS buyer,
+        "newLeftFill" AS original_amount_raw,
+        '\x0000000000000000000000000000000000000000'::bytea AS original_currency_contract,
+        '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::bytea AS currency_contract,
         'Buy' AS category -- 'Purchase'
-    FROM ethereum."logs" 
-    WHERE "contract_address" = '\x9757f2d2b135150bbeb65308d4a91804107cd8d6' 
-    AND topic1 = '\x268820db288a211986b26a8fda86b1e0046281b21206936bb0e61c67b5c79ef4'
-    AND length(data) = 512
-UNION ALL
--- from 2021-06-15 onwards
--- 'Bid Accepted' non-ETH
+    FROM rarible."ExchangeV2_evt_Match"
+    WHERE "rightAsset"->>'assetClass' = '0xaaaebeba' -- ETH
+    AND (
+        "leftAsset"->>'assetClass' = '0x973bb640' 
+        OR 
+        "leftAsset"->>'assetClass' = '0x73ad2146'
+        )
+    UNION ALL
+    -- ERC20 purchases of ERC1155 or ERC721 
     SELECT
-        'Rarible' as platform,
-        '2' as platform_version,
+        'Rarible' AS platform,
+        '2' AS platform_version,
         contract_address AS exchange_contract_address,
-        'Trade' as evt_type,
-         tx_hash AS evt_tx_hash,
-        block_time AS evt_block_time,
-        block_number AS evt_block_number,
-        "index" AS evt_index,
-        substring(data FROM 493 FOR 20) AS nft_contract_address,
-        CAST(bytea2numericpy(substring(data FROM 513 FOR 32)) AS TEXT) AS nft_token_id,
-        substring(data FROM 109 FOR 20) AS seller,
-        substring(data FROM 77 FOR 20) AS buyer,
-        bytea2numericpy(substring(data FROM 161 FOR 32)) AS original_amount_raw,
-        substring(data FROM 365 FOR 20) AS original_currency_contract,
-        substring(data FROM 365 FOR 20) AS currency_contract,
-        'Offer Accepted' AS category -- 'Bid Accepted'
-    FROM ethereum."logs"
-    WHERE "contract_address" = '\x9757f2d2b135150bbeb65308d4a91804107cd8d6'
-    AND topic1 = '\x268820db288a211986b26a8fda86b1e0046281b21206936bb0e61c67b5c79ef4'
-    AND length(data) = 544
-    AND bytea2numericpy(substring(data FROM 225 FOR 32)) = 384
-UNION ALL
--- from 2021-06-15 onwards
--- 'Purchases' in non-ETH currencies
+        'Trade' AS evt_type,
+        evt_tx_hash,
+        evt_block_time,
+        evt_block_number,
+        evt_index,
+        decode(substring("leftAsset"->>'data' FROM 27 FOR 40), 'hex') AS nft_contract_address,
+        CAST(bytea2numericpy(decode(substring("leftAsset"->>'data' FROM 67 FOR 64), 'hex')) AS TEXT) AS nft_token_id,
+        "leftMaker" AS seller,
+        "rightMaker" AS buyer,
+        "newLeftFill" AS original_amount_raw,
+        decode(substring("rightAsset"->>'data' FROM 27 FOR 40), 'hex') AS original_currency_contract,
+        decode(substring("rightAsset"->>'data' FROM 27 FOR 40), 'hex') AS currency_contract,
+        'Buy' AS category -- 'Purchase'
+    FROM rarible."ExchangeV2_evt_Match"
+    WHERE "rightAsset"->>'assetClass' = '0x8ae85d84'
+    AND (
+        "leftAsset"->>'assetClass' = '0x973bb640' 
+        OR
+        "leftAsset"->>'assetClass' = '0x73ad2146' 
+        )
+    UNION ALL
+    -- WETH Bid Accepted
     SELECT
-        'Rarible' as platform,
-        '2' as platform_version,
+        'Rarible' AS platform,
+        '2' AS platform_version,
         contract_address AS exchange_contract_address,
-        'Trade' as evt_type,
-        tx_hash AS evt_tx_hash,
-        block_time AS evt_block_time,
-        block_number AS evt_block_number,
-        "index" AS evt_index,
-        substring(data FROM 365 FOR 20) AS nft_contract_address,
-        CAST(bytea2numericpy(substring(data FROM 385 FOR 32)) AS TEXT) AS nft_token_id,
-        substring(data FROM 77 FOR 20) AS seller,
-        substring(data FROM 109 FOR 20) AS buyer,
-        bytea2numericpy(substring(data FROM 129 FOR 32)) AS original_amount_raw,
-        substring(data FROM 525 FOR 20) AS original_currency_contract,
-        substring(data FROM 525 FOR 20) AS currency_contract,
-        'Buy' AS category -- 'Bid Accepted'
-    FROM ethereum."logs"
-    WHERE "contract_address" = '\x9757f2d2b135150bbeb65308d4a91804107cd8d6'
-    AND topic1 = '\x268820db288a211986b26a8fda86b1e0046281b21206936bb0e61c67b5c79ef4'
-    AND length(data) = 544
-    AND bytea2numericpy(substring(data FROM 225 FOR 32)) = 416
+        'Trade' AS evt_type,
+        evt_tx_hash,
+        evt_block_time,
+        evt_block_number,
+        evt_index,
+        decode(substring("rightAsset"->>'data' FROM 27 FOR 40), 'hex') AS nft_contract_address,
+        CAST(bytea2numericpy(decode(substring("rightAsset"->>'data' FROM 67 FOR 64), 'hex')) AS TEXT) AS nft_token_id,
+        "rightMaker" AS seller,
+        "leftMaker" AS buyer,
+        "newRightFill" AS original_amount_raw,
+        decode(substring("leftAsset"->>'data' FROM 27 FOR 40), 'hex') AS original_currency_contract,
+        decode(substring("leftAsset"->>'data' FROM 27 FOR 40), 'hex') AS currency_contract,
+        'Buy' AS category -- 'Purchase'
+    FROM rarible."ExchangeV2_evt_Match"
+    WHERE "leftAsset"->>'assetClass' = '0x8ae85d84'
+    AND (
+        "rightAsset"->>'assetClass' = '0x973bb640' 
+        OR
+        "rightAsset"->>'assetClass' = '0x73ad2146' 
+        )
+),
+-- Get ERC721 and ERC1155 transfer data for every trade transaction
+rarible_erc_union AS (
+SELECT
+    erc721.evt_tx_hash,
+    'erc721' as erc_type,
+    CAST(erc721."tokenId" AS TEXT) AS "tokenId",
+    erc721."from",
+    erc721."to",
+    erc721.contract_address,
+    NULL::numeric AS value
+FROM erc721."ERC721_evt_Transfer" erc721
+INNER JOIN rarible_trades d ON erc721.evt_tx_hash = d.evt_tx_hash
+WHERE erc721.evt_block_time >= start_ts
+AND erc721.evt_block_time < end_ts
+AND erc721."from" <> '\x0000000000000000000000000000000000000000' -- Exclude mints
+UNION ALL
+SELECT
+    erc1155.evt_tx_hash,
+    'erc1155' as erc_type,
+    CAST(erc1155.id AS TEXT) AS "tokenId",
+    erc1155."from",
+    erc1155."to",
+    erc1155.contract_address,
+    erc1155.value
+FROM erc1155."ERC1155_evt_TransferSingle" erc1155
+INNER JOIN rarible_trades d ON erc1155.evt_tx_hash = d.evt_tx_hash
+WHERE erc1155.evt_block_time >= start_ts
+AND erc1155.evt_block_time < end_ts
+AND erc1155."from" <> '\x0000000000000000000000000000000000000000' -- Exclude mints
+),
+-- aggregate NFT transfers per transaction 
+rarible_erc_subsets AS (
+SELECT
+    evt_tx_hash,
+    array_agg("tokenId") AS token_id_array,
+    cardinality(array_agg("tokenId")) AS no_of_transfers,
+    array_agg("from") AS from_array,
+    array_agg("to") AS to_array,
+    array_agg(erc_type) AS erc_type_array,
+    array_agg(contract_address) AS contract_address_array,
+    array_agg(value) AS erc1155_value_array
+FROM rarible_erc_union
+GROUP BY 1
 ),
 rows AS (
     INSERT INTO nft.trades (
 	block_time,
 	nft_project_name,
 	nft_token_id,
+        erc_standard,
 	platform,
 	platform_version,
+        trade_type,
+        number_of_items,
 	category,
 	evt_type,
 	usd_amount,
@@ -248,24 +301,34 @@ rows AS (
 	exchange_contract_address,
 	tx_hash,
 	block_number,
+        nft_token_ids_array,
+        senders_array,
+        recipients_array,
+        erc_types_array,
+        nft_contract_addresses_array,
+        erc_values_array,
 	tx_from,
 	tx_to,
 	trace_address,
 	evt_index,
 	trade_id
     )
-
     SELECT
         trades.evt_block_time AS block_time,
         tokens.name AS nft_project_name,
-        trades.nft_token_id,
+        -- Set NFT token ID to `NULL` if the trade consists of multiple NFT transfers
+        CASE WHEN erc.no_of_transfers > 1 THEN NULL ELSE trades.nft_token_id END AS nft_token_id,
+        -- Set ERC standard to `NULL` if the trade consists of multiple NFT transfers
+        CASE WHEN erc.no_of_transfers > 1 THEN NULL ELSE COALESCE(erc.erc_type_array[1], tokens.standard) END AS erc_standard,
         trades.platform,
         trades.platform_version,
+        CASE WHEN erc.no_of_transfers > 1 THEN 'Bundle Trade' ELSE 'Single Item Trade' END AS trade_type,
+        erc.no_of_transfers AS number_of_items,
         trades.category,
         trades.evt_type,
         trades.original_amount_raw / 10 ^ erc20.decimals * p.price AS usd_amount,
-        trades.seller,
-        trades.buyer,
+        COALESCE(erc.from_array[1], trades.seller) AS seller,
+        COALESCE(erc.to_array[1], trades.buyer) AS buyer,
         trades.original_amount_raw / 10 ^ erc20.decimals AS original_amount,
         trades.original_amount_raw AS original_amount_raw,
         CASE WHEN trades.original_currency_contract = '\x0000000000000000000000000000000000000000' THEN 'ETH' ELSE erc20.symbol END AS original_currency,
@@ -275,19 +338,28 @@ rows AS (
         trades.exchange_contract_address,
         trades.evt_tx_hash AS tx_hash,
         trades.evt_block_number AS block_number,
+        -- Sometimes multiple NFT transfers occur in a given trade; the 'array' fields below provide info for these use cases 
+        erc.token_id_array AS nft_token_ids_array,
+        erc.token_id_array AS nft_token_ids_array,
+        erc.from_array AS senders_array,
+        erc.to_array AS recipients_array,
+        erc.erc_type_array AS erc_types_array,
+        erc.contract_address_array AS nft_contract_addresses_array,
+        erc.erc1155_value_array AS erc_values_array,
         tx."from" AS tx_from,
         tx."to" AS tx_to,
         NULL::integer[] AS trace_address,
         trades.evt_index,
         row_number() OVER (PARTITION BY platform, trades.evt_tx_hash, trades.evt_index, category ORDER BY platform_version, evt_type) AS trade_id
     FROM
-        all_data trades
+        rarible_trades trades
     INNER JOIN ethereum.transactions tx
         ON trades.evt_tx_hash = tx.hash
         AND tx.block_time >= start_ts
         AND tx.block_time < end_ts
         AND tx.block_number >= start_block
         AND tx.block_number < end_block
+    LEFT JOIN rarible_erc_subsets erc ON erc.evt_tx_hash = trades.evt_tx_hash
     LEFT JOIN erc20.tokens erc20 ON erc20.contract_address = trades.currency_contract
     LEFT JOIN nft.tokens tokens ON tokens.contract_address = trades.nft_contract_address
     LEFT JOIN prices.usd p ON p.minute = date_trunc('minute', trades.evt_block_time)
