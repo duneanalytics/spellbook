3b33c5d9bfe61cb098a2e9968b11b1bfdea7a403
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-10-04 14:26:12 -0300

    balancer: add bpt transfers

---
+++ ethereum/balancer_v2/view_transfers_bpt.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW balancer_v1.view_transfers_bpt AS
+SELECT *
+FROM balancer_v2."WeightedPool_evt_Transfer"
+UNION ALL
+SELECT *
+FROM balancer_v2."StablePool_evt_Transfer"
+UNION ALL
+SELECT *
+FROM balancer_v2."MetaStablePool_evt_Transfer"
+UNION ALL
+SELECT *
+FROM balancer_v2."LiquidityBootstrappingPool_evt_Transfer"
+UNION ALL
+SELECT *
+FROM element."ConvergentCurvePool_evt_Transfer"
+UNION ALL
+SELECT *
+FROM balancer_v2."InvestmentPool_evt_Transfer"
