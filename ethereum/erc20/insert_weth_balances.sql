3d5566620467fa57d09e9c37d41525c6e1390276
masquot <masquot@protonmail.com>
2021-10-12 07:50:23 +0200

    Update table name and cron jobs

--- ethereum/erc20/insert_weth_balances.sql
+++ ethereum/erc20/insert_weth_balances.sql
@@ -1,20 +1,20 @@
-CREATE OR REPLACE FUNCTION dex.insert_weth_balances(time_to_add timestamptz) RETURNS integer
+CREATE OR REPLACE FUNCTION erc20.insert_weth_balances(time_to_add timestamptz) RETURNS integer
 LANGUAGE plpgsql AS $function$
 DECLARE r integer;
 BEGIN
--- The cron job has to run after completion of `dex.insert_weth_balance_changes`
+-- The cron job has to run after completion of `erc20.insert_weth_balance_changes`
 -- Get the token balance for `time_to_add` by accumulating previous balance changes
 WITH balances AS (
     SELECT
         wallet_address,
         token_address,
         SUM(amount_raw) AS amount_raw
-    FROM dex.hourly_balance_changes d1
+    FROM erc20.weth_hourly_balance_changes d1
     -- Sum over all previous balance changes to get current balance
     WHERE hour <= time_to_add
         -- Only add an entry when there was a balance change for `wallet_address`
         -- and `token_address` during the hourly interval stored as `time_to_add`
-        AND EXISTS (SELECT * FROM dex.hourly_balance_changes d2 
+        AND EXISTS (SELECT * FROM erc20.weth_hourly_balance_changes d2 
                     WHERE d2.hour = time_to_add
                         AND d1.wallet_address = d2.wallet_address
                         AND d1.token_address = d2.token_address)
@@ -45,7 +45,7 @@ rows AS (
         FROM balances
     ) b
 
-    ON CONFLICT DO NOTHING
+    ON CONFLICT ON CONSTRAINT token_balances_pkey DO UPDATE SET amount_raw = EXCLUDED.amount_raw, amount = EXCLUDED.amount
     RETURNING 1
 )
 SELECT count(*) INTO r from rows;
@@ -54,10 +54,19 @@ END
 $function$;
 
 
--- Daily cron job updates entries for previous hour
+-- Daily cron job enters entries for previous hour
 INSERT INTO cron.job (schedule, command)
 VALUES ('7 * * * *', $$
-    SELECT dex.insert_weth_balances(
+    SELECT erc20.insert_weth_balances(
         (SELECT date_trunc('hour', now()) - interval '2 hours'));
+$$)
+ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
+
+
+-- Daily cron job `upserts` entries for 24h ago
+INSERT INTO cron.job (schedule, command)
+VALUES ('17 * * * *', $$
+    SELECT erc20.insert_weth_balances(
+        (SELECT date_trunc('hour', now()) - interval '24 hours'));
 $$)
 ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
--- ethereum/erc20/weth_hourly_balance_changes.sql
+++ ethereum/erc20/weth_hourly_balance_changes.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS dex.hourly_balance_changes (
+CREATE TABLE IF NOT EXISTS erc20.weth_hourly_balance_changes (
     hour timestamptz NOT NULL,
     wallet_address bytea NOT NULL,
     token_address bytea NOT NULL,
     amount_raw numeric
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS dex_hourly_balance_changes_hour_wallet_address_token_address_uniq_idx ON dex.hourly_balance_changes (hour, wallet_address, token_address);
-CREATE INDEX IF NOT EXISTS dex_hourly_balance_changes_hour_idx ON dex.hourly_balance_changes USING BRIN (hour);
-CREATE INDEX IF NOT EXISTS dex_hourly_balance_changes_token_address_idx ON dex.hourly_balance_changes (token_address);
-CREATE INDEX IF NOT EXISTS dex_hourly_balance_changes_wallet_address_idx ON dex.hourly_balance_changes (wallet_address);
+CREATE UNIQUE INDEX IF NOT EXISTS erc20_weth_hourly_balance_ch_hour_wallet_token_uniq_idx ON erc20.weth_hourly_balance_changes (hour, wallet_address, token_address);
+CREATE INDEX IF NOT EXISTS erc20_weth_hourly_balance_changes_hour_idx ON erc20.weth_hourly_balance_changes USING BRIN (hour);
+CREATE INDEX IF NOT EXISTS erc20_weth_hourly_balance_changes_token_address_idx ON erc20.weth_hourly_balance_changes (token_address);
+CREATE INDEX IF NOT EXISTS erc20_weth_hourly_balance_changes_wallet_address_idx ON erc20.weth_hourly_balance_changes (wallet_address);
