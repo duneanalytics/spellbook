124a2d4a4005c0fa003f6bbd1c531621f4b49914
masquot <78954627+masquot@users.noreply.github.com>
2021-11-16 08:54:48 +0100

    Add where clause in every CTE and small fixes

--- ethereum/zeroex/view_0x_api_fills.sql
+++ ethereum/zeroex/view_0x_api_fills.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS zeroex.view_0x_api_fills (
     volume_usd float
 );
 
-CREATE OR REPLACE FUNCTION zeroex.insert_0x_api_fills (start_ts timestamptz, end_ts timestamptz=now(), start_block numeric=0, end_block numeric=9e18) RETURNS integer
+CREATE OR REPLACE FUNCTION zeroex.insert_0x_api_fills (start_ts timestamptz, end_ts timestamptz=now()) RETURNS integer
 LANGUAGE plpgsql AS $function$
 DECLARE r integer;
 BEGIN
@@ -36,6 +36,7 @@ WITH zeroex_tx_raw AS (
                 -- contains a bridge order
                 (v3."feeRecipientAddress" = '\x1000000000000000000000000000000000000011'::BYTEA
                     AND SUBSTRING(v3."makerAssetData",1,4) = '\xdc1600f3'::bytea)
+            AND evt_block_time >= start_ts AND evt_block_time < end_ts
 
             UNION
 
@@ -43,6 +44,7 @@ WITH zeroex_tx_raw AS (
                 tx_hash
                 , affiliate_address as affiliate_address
             from zeroex."view_api_affiliate_data"
+            WHERE block_time >= start_ts AND block_time < end_ts
         ),
         zeroex_tx AS (
                     SELECT
@@ -74,6 +76,7 @@ WITH zeroex_tx_raw AS (
                       zeroex_tx.tx_hash IS NOT NULL
                       OR fills."feeRecipientAddress" = '\x86003b044f70dac0abc80ac8957305b6370893ed'::bytea
                   )
+                  AND evt_block_time >= start_ts AND evt_block_time < end_ts
           ),
           v4_rfq_fills_no_bridge AS (
               SELECT fills.evt_tx_hash AS tx_hash
@@ -92,6 +95,7 @@ WITH zeroex_tx_raw AS (
                   , FALSE AS matcha_limit_order_flag
               FROM zeroex."ExchangeProxy_evt_RfqOrderFilled" fills
               LEFT join zeroex_tx on zeroex_tx.tx_hash = fills.evt_tx_hash
+              AND evt_block_time >= start_ts AND evt_block_time < end_ts
         ),
         v4_limit_fills_no_bridge AS (
               SELECT fills.evt_tx_hash AS tx_hash
@@ -110,7 +114,7 @@ WITH zeroex_tx_raw AS (
                   , (fills."feeRecipient" = '\x86003b044f70dac0abc80ac8957305b6370893ed'::bytea) AS matcha_limit_order_flag
               FROM zeroex."ExchangeProxy_evt_LimitOrderFilled" fills
               LEFT join zeroex_tx on zeroex_tx.tx_hash = fills.evt_tx_hash
-
+              AND evt_block_time >= start_ts AND evt_block_time < end_ts
       ),
       -- bridge fills
     	ERC20BridgeTransfer AS (
@@ -131,8 +135,8 @@ WITH zeroex_tx_raw AS (
      		FROM ethereum."logs" logs
         join zeroex_tx on zeroex_tx.tx_hash = logs.tx_hash
     		WHERE topic1 = '\x349fc08071558d8e3aa92dec9396e4e9f2dfecd6bb9065759d1932e7da43b8a9'::bytea
-    	),
-
+                AND block_time >= start_ts AND block_time < end_ts
+        ),
       BridgeFill AS (
     		SELECT 	logs.tx_hash,
     				INDEX AS evt_index,
@@ -151,6 +155,7 @@ WITH zeroex_tx_raw AS (
      		FROM ethereum."logs" logs
         join zeroex_tx on zeroex_tx.tx_hash = logs.tx_hash
     		WHERE topic1 = '\xff3bc5e46464411f331d1b093e1587d2d1aa667f5618f98a95afc4132709d3a9'::bytea
+                AND block_time >= start_ts AND block_time < end_ts
     	),
 
       NewBridgeFill AS (
@@ -172,6 +177,7 @@ WITH zeroex_tx_raw AS (
         join zeroex_tx on zeroex_tx.tx_hash = logs.tx_hash
         WHERE topic1 = '\xe59e71a14fe90157eedc866c4f8c767d3943d6b6b2e8cd64dddcc92ab4c55af8'::bytea
                 and contract_address = '\x22f9dcf4647084d6c31b2765f6910cd85c178c18'::bytea
+                AND block_time >= start_ts AND block_time < end_ts
       ),
       direct_PLP AS (
         SELECT 	plp.evt_tx_hash,
@@ -190,6 +196,7 @@ WITH zeroex_tx_raw AS (
             FALSE AS matcha_limit_order_flag
      		FROM zeroex."ExchangeProxy_evt_LiquidityProviderSwap" plp
         join zeroex_tx on zeroex_tx.tx_hash = plp.evt_tx_hash
+              WHERE evt_block_time >= start_ts AND evt_block_time < end_ts
     	),
 
     	direct_uniswapv2 AS (
@@ -223,6 +230,7 @@ WITH zeroex_tx_raw AS (
     		LEFT JOIN uniswap_v2."Factory_evt_PairCreated" pair ON pair.pair = swap.contract_address
         join zeroex_tx on zeroex_tx.tx_hash = swap.evt_tx_hash
     		WHERE sender = '\xdef1c0ded9bec7f1a1670819833240f027b25eff'::BYTEA
+                AND swap.evt_block_time >= start_ts AND swap.evt_block_time < end_ts
     	),
 
     	direct_sushiswap AS (
@@ -256,6 +264,7 @@ WITH zeroex_tx_raw AS (
     		LEFT JOIN sushi."Factory_evt_PairCreated" pair ON pair.pair = swap.contract_address
         join zeroex_tx on zeroex_tx.tx_hash = swap.evt_tx_hash
     		WHERE sender = '\xdef1c0ded9bec7f1a1670819833240f027b25eff'::BYTEA
+                AND swap.evt_block_time >= start_ts AND swap.evt_block_time < end_ts
     	),
 
       direct_uniswapv3 AS (
@@ -277,6 +286,7 @@ WITH zeroex_tx_raw AS (
     		LEFT JOIN uniswap_v3."Factory_evt_PoolCreated" pair ON pair.pool = swap.contract_address
         join zeroex_tx on zeroex_tx.tx_hash = swap.evt_tx_hash
     		WHERE sender = '\xdef1c0ded9bec7f1a1670819833240f027b25eff'::BYTEA
+                AND swap.evt_block_time >= start_ts AND swap.evt_block_time < end_ts
     	),
 
     	all_tx AS (
@@ -333,14 +343,21 @@ WITH zeroex_tx_raw AS (
       		FROM all_tx
           INNER JOIN ethereum.transactions tx
                                   ON all_tx.tx_hash = tx.hash
+                                    AND tx.block_time >= start_ts
+                                    AND tx.block_time < end_ts
+                                    -- AND tx.block_number >= start_block
+                                    -- AND tx.block_number < end_block
       		LEFT JOIN prices.usd tp ON date_trunc('minute', all_tx.block_time) = tp.minute
       								AND all_tx.taker_token = tp.contract_address
+                                                                AND tp.minute >= start_ts
+                                                                AND tp.minute < end_ts
       		LEFT JOIN prices.usd mp ON DATE_TRUNC('minute', all_tx.block_time) = mp.minute
       								AND all_tx.maker_token = mp.contract_address
+                                                                AND mp.minute >= start_ts
+                                                                AND mp.minute < end_ts
       		LEFT JOIN erc20.tokens mt ON mt.contract_address = all_tx.maker_token
       		LEFT JOIN erc20.tokens tt ON tt.contract_address = all_tx.taker_token
-          WHERE all_tx.block_time >= start_ts
-              AND all_tx.block_time < end_ts
+          -- WHERE all_tx.block_time >= start_ts AND all_tx.block_time < end_ts
       ),
         rows AS (
             INSERT INTO zeroex.view_0x_api_fills (
@@ -389,14 +406,12 @@ WITH zeroex_tx_raw AS (
     END
     $function$;
       
-
 CREATE UNIQUE INDEX IF NOT EXISTS zeroex_api_fills_unique ON zeroex.view_0x_api_fills (tx_hash, evt_index);
 CREATE INDEX IF NOT EXISTS zeroex_api_fills_time_index ON zeroex.view_0x_api_fills (block_time);
 
 --backfill
-SELECT zeroex.insert_0x_api_fills('2020-01-29', (SELECT now()), (SELECT max(number) FROM ethereum.blocks WHERE time < '2020-01-29'), (SELECT MAX(number) FROM ethereum.blocks where time < now() - interval '20 minutes')) WHERE NOT EXISTS (SELECT * FROM zeroex.view_0x_api_fills LIMIT 1);
-
+SELECT zeroex.insert_0x_api_fills('2020-01-29', (SELECT now() - interval '20 minutes')) WHERE NOT EXISTS (SELECT * FROM zeroex.view_0x_api_fills LIMIT 1);
 
 INSERT INTO cron.job (schedule, command)
-VALUES ('*/15 * * * *', $$SELECT zeroex.insert_0x_api_fills((SELECT max(block_time) - interval '2 days' FROM zeroex.view_0x_api_fills), (SELECT now() - interval '20 minutes'), (SELECT max(number) FROM ethereum.blocks WHERE time < (SELECT max(block_time) - interval '2 days' FROM zeroex.view_0x_api_fills)), (SELECT MAX(number) FROM ethereum.blocks where time < now() - interval '20 minutes'));$$)
+VALUES ('15 * * * *', $$SELECT zeroex.insert_0x_api_fills((SELECT max(block_time) - interval '2 days' FROM zeroex.view_0x_api_fills), (SELECT now() - interval '20 minutes'));$$)
 ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
