feef6823db0ff94676a93867998ea8641f242293
dias <dias@dune.xyz>
2021-12-02 16:54:38 +0100

    Changed the columns names to be more descriptive

--- ethereum/lending/liquidation.sql
+++ ethereum/lending/liquidation.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS lending.liquidation (
     tx_hash bytea,
     evt_index integer,
     trace_address integer[],
-    borrower bytea,
+    liquidated_borrower bytea,
     tx_from bytea,
-    asset_address bytea,
-    collateral_address bytea,
-    asset_symbol text,
+    borrowed_asset_address bytea,
+    collateral_asset_address bytea,
+    collateral_asset_symbol text,
     collateral_token_amount numeric,
-    usd_value numeric
+    collateral_usd_value numeric
 );
 
 
@@ -30,12 +30,12 @@ WITH liquidation AS (
         evt_index,
         trace_address,
         tx."from" AS tx_from,
-        borrower,
-        t.symbol AS asset_symbol,
-        asset_address,
-        collateral_address,
+        liquidated_borrower,
+        t.symbol AS collateral_asset_symbol,
+        borrowed_asset_address,
+        collateral_asset_address,
         collateral_amount / 10^t.decimals AS collateral_token_amount,
-        collateral_amount / 10^t.decimals*p.price AS usd_value
+        collateral_amount / 10^t.decimals*p.price AS collateral_usd_value
     FROM (
 
         -- Aave liquidation
@@ -47,15 +47,15 @@ WITH liquidation AS (
             evt_tx_hash AS tx_hash,
             evt_index,
             NULL::integer[] AS trace_address,
-            "_user" AS borrower,
+            "_user" AS liquidated_borrower,
             CASE --Use WETH instead of Aave "mock" address
                 WHEN _reserve = '\xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
                 ELSE _reserve
-            END AS asset_address,
+            END AS borrowed_asset_address,
             CASE --Use WETH instead of Aave "mock" address
                 WHEN _collateral = '\xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
                 ELSE _collateral
-            END AS collateral_address,
+            END AS collateral_asset_address,
             "_liquidatedCollateralAmount" AS collateral_amount
         FROM aave."LendingPool_evt_LiquidationCall"
         WHERE evt_block_time >= start_ts
@@ -71,9 +71,9 @@ WITH liquidation AS (
             evt_tx_hash AS tx_hash,
             evt_index,
             NULL::integer[] AS trace_address,
-            "user" AS borrower,
-            "debtAsset" AS asset_address,
-            "collateralAsset" AS collateral_address,
+            "user" AS liquidated_borrower,
+            "debtAsset" AS borrowed_asset_address,
+            "collateralAsset" AS collateral_asset_address,
             "liquidatedCollateralAmount" AS collateral_amount
         FROM aave_v2."LendingPool_evt_LiquidationCall"
         WHERE evt_block_time >= start_ts
@@ -90,14 +90,14 @@ WITH liquidation AS (
             evt_tx_hash AS tx_hash,
             evt_index,
             NULL::integer[] AS trace_address,
-            borrower,
-            '\x6b175474e89094c44da98b954eedeac495271d0f'::bytea AS asset_address,
-            m."underlying_token_address" AS collateral_address,
+            liquidated_borrower,
+            '\x6b175474e89094c44da98b954eedeac495271d0f'::bytea AS borrowed_asset_address,
+            m."underlying_token_address" AS collateral_asset_address,
             collateral_amount
         FROM (
             -- Liquidation V1.0
             SELECT evt_block_number, evt_block_time, evt_tx_hash, contract_address, evt_index,
-                   "lot" AS collateral_amount, "usr" AS borrower 
+                   "lot" AS collateral_amount, "usr" AS liquidated_borrower 
             FROM makermcd."FLIP_evt_Kick"
             WHERE tab > 0
             AND evt_block_time >= start_ts
@@ -106,7 +106,7 @@ WITH liquidation AS (
             UNION ALL
             -- Liquidation V2.0 
             SELECT evt_block_number, evt_block_time, evt_tx_hash, contract_address, evt_index,
-                   "lot" AS collateral_amount, "usr" AS borrower
+                   "lot" AS collateral_amount, "usr" AS liquidated_borrower
             FROM makerdao."Clipper_evt_Kick"
             WHERE tab > 0
             AND evt_block_time >= start_ts
@@ -124,8 +124,8 @@ WITH liquidation AS (
         AND tx.block_number < end_block
         AND tx.block_time >= start_ts
         AND tx.block_time < end_ts
-    LEFT JOIN erc20.tokens t ON t.contract_address = liquidation.collateral_address
-    LEFT JOIN prices.usd p ON p.minute = date_trunc('minute', liquidation.block_time) AND p.contract_address = liquidation.collateral_address AND p.minute >= start_ts AND p.minute < end_ts
+    LEFT JOIN erc20.tokens t ON t.contract_address = liquidation.collateral_asset_address
+    LEFT JOIN prices.usd p ON p.minute = date_trunc('minute', liquidation.block_time) AND p.contract_address = liquidation.collateral_asset_address AND p.minute >= start_ts AND p.minute < end_ts
     
     UNION ALL
     -- Compound liquidation
@@ -138,12 +138,12 @@ WITH liquidation AS (
         evt_index,
         trace_address,
         tx."from" AS tx_from,
-        borrower,
-        t2.symbol AS asset_symbol,
-        asset_address,
-        collateral_address,
+        liquidated_borrower,
+        t2.symbol AS collateral_asset_symbol,
+        borrowed_asset_address,
+        collateral_asset_address,
         collateral_amount / 10^8 AS collateral_token_amount,
-        collateral_amount / 10^8*dex_p.median_price AS usd_value
+        collateral_amount / 10^8*dex_p.median_price AS collateral_usd_value
     FROM
         (SELECT
             'Compound' AS project,
@@ -153,9 +153,9 @@ WITH liquidation AS (
             evt_tx_hash AS tx_hash,
             evt_index,
             NULL::integer[] AS trace_address,
-            borrower AS borrower,
-            c_asset."asset_underlying_token_address" AS asset_address,
-            c_collateral."collateral_underlying_token_address" AS collateral_address,
+            borrower AS liquidated_borrower,
+            c_asset."asset_underlying_token_address" AS borrowed_asset_address,
+            c_collateral."collateral_underlying_token_address" AS collateral_asset_address,
             CASE --Use WBTC2 instead of WBTC1 cToken for price correctness
                 WHEN c."cTokenCollateral" = '\xc11b1268c1a384e55c48c2391d8d480264a3a7f4' THEN '\xccF4429DB6322D5C611ee964527D42E5d685DD6a'
                 ELSE c."cTokenCollateral"
@@ -180,7 +180,7 @@ WITH liquidation AS (
             AND tx.block_number < end_block
             AND tx.block_time >= start_ts
             AND tx.block_time < end_ts
-        LEFT JOIN erc20.tokens t2 ON t2.contract_address = compound_liq.collateral_address
+        LEFT JOIN erc20.tokens t2 ON t2.contract_address = compound_liq.collateral_asset_address
         LEFT JOIN prices."prices_from_dex_data" dex_p ON dex_p.hour = date_trunc('hour', compound_liq.block_time) AND 
                                                     dex_p.contract_address = compound_liq.c_token_collateral  AND
                                                     dex_p.hour >= start_ts AND  dex_p.hour < end_ts
@@ -195,12 +195,12 @@ rows AS (
        evt_index,
        trace_address,
        tx_from,
-       borrower,
-       asset_address,
-       collateral_address,
-       asset_symbol,
+       liquidated_borrower,
+       borrowed_asset_address,
+       collateral_asset_address,
+       collateral_asset_symbol,
        collateral_token_amount,
-       usd_value
+       collateral_usd_value
     )
     SELECT
        project,
@@ -211,12 +211,12 @@ rows AS (
        evt_index,
        trace_address,
        tx_from,
-       borrower,
-       asset_address,
-       collateral_address,
-       asset_symbol,
+       liquidated_borrower,
+       borrowed_asset_address,
+       collateral_asset_address,
+       collateral_asset_symbol,
        collateral_token_amount,
-       usd_value
+       collateral_usd_value
     FROM liquidation
     ON CONFLICT DO NOTHING
     RETURNING 1
