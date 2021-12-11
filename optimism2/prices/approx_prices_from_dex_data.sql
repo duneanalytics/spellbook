afe2c6d13da373e3ca37be16009d1d086b23ffa3
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-11 18:42:55 -0500

    Update approx_prices_from_dex_data.sql
    update table name

--- optimism2/prices/approx_prices_from_dex_data.sql
+++ optimism2/prices/approx_prices_from_dex_data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS prices.prices_from_dex_data(
+CREATE TABLE IF NOT EXISTS prices.approx_prices_from_dex_data(
     contract_address bytea NOT NULL,
     hour timestamptz NOT NULL,
     median_price numeric,
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS prices.prices_from_dex_data(
     decimals int4
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS prices_prices_from_dex_data_contract_addr_hour_uniq_idx ON prices.prices_from_dex_data (contract_address, hour);
-CREATE INDEX IF NOT EXISTS prices_prices_from_dex_data_hour_idx ON prices.prices_from_dex_data USING BRIN (hour);
-CREATE INDEX IF NOT EXISTS prices_prices_from_dex_data_contract_address_idx ON prices.prices_from_dex_data (contract_address);
-CREATE INDEX IF NOT EXISTS prices_prices_from_dex_data_symbol_idx ON prices.prices_from_dex_data (symbol);
-CREATE INDEX IF NOT EXISTS prices_prices_from_dex_data_median_price_idx ON prices.prices_from_dex_data (median_price);
+CREATE UNIQUE INDEX IF NOT EXISTS prices_approx_prices_from_dex_data_contract_addr_hour_uniq_idx ON prices.approx_prices_from_dex_data (contract_address, hour);
+CREATE INDEX IF NOT EXISTS prices_approx_prices_from_dex_data_hour_idx ON prices.approx_prices_from_dex_data USING BRIN (hour);
+CREATE INDEX IF NOT EXISTS prices_approx_prices_from_dex_data_contract_address_idx ON prices.approx_prices_from_dex_data (contract_address);
+CREATE INDEX IF NOT EXISTS prices_approx_prices_from_dex_data_symbol_idx ON prices.approx_prices_from_dex_data (symbol);
+CREATE INDEX IF NOT EXISTS prices_approx_prices_from_dex_data_median_price_idx ON prices.approx_prices_from_dex_data (median_price);
