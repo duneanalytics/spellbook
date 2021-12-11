19e2e364b93364c1d83845795fdfbaff248bc66a
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-11 17:47:25 -0500

    Update and rename insert_uniswap_v3_poolCreated to insert_uniswap_v3_poolcreated.sql
    Adding backfill logic pulled from other queries

---
+++ optimism2/uniswap_v3/insert_uniswap_v3_poolcreated.sql
@@ -0,0 +1,46 @@
+
+CREATE OR REPLACE FUNCTION uniswap_v3.insert_uniswap_v3_poolcreated(start_ts timestamptz, end_ts timestamptz=now(), start_block numeric=0, end_block numeric=9e18) RETURNS integer
+LANGUAGE plpgsql AS $function$
+DECLARE r integer;
+BEGIN
+WITH rows AS (
+    INSERT INTO uniswap_v3.view_pools(
+    token0 bytea NOT NULL,
+    token1 bytea NOT NULL,
+    fee integer,
+    pool bytea PRIMARY KEY
+    )
+
+    SELECT "token0","token1", fee, pool FROM uniswap_v3."Factory_evt_PoolCreated" pc
+    WHERE NOT EXISTS (SELECT 1 FROM uniswap_v3.view_pools vp WHERE vp.pool = pc.pool LIMIT 1)
+   )
+SELECT count(*) INTO r from rows;
+RETURN r;
+END
+$function$;
+
+-- fill 2021 (post-regenesis 11-11)
+SELECT uniswap_v3.insert_uniswap_v3_poolcreated(
+    '2021-11-10',
+    now(),
+    0,
+    (SELECT MAX(number) FROM optimism.blocks where time < now() - interval '20 minutes')
+) ovm1
+WHERE NOT EXISTS (
+    SELECT *
+    FROM uniswap_v3.insert_uniswap_v3_poolcreated p
+    WHERE block_time > '2021-11-10'
+    AND block_time <= now() - interval '20 minutes'
+    AND ovm1.pool = p.pool
+);
+
+INSERT INTO cron.job (schedule, command)
+VALUES ('27,57 * * * *', $$
+    SELECT uniswap_v3.insert_uniswap_v3_poolcreated(
+        '2021-11-11'::timestamptz,
+        (SELECT now() - interval '20 minutes'),
+        0,
+        (SELECT MAX(number) FROM optimism.blocks where time < now() - interval '20 minutes')
+        );
+$$)
+ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
