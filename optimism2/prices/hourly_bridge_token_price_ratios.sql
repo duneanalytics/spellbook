dfff1b66a02de6624369637db5ea7c17f15b9132
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-12 09:04:03 -0800

    Create hourly_bridge_token_price_ratios.sql
    Create base table to store ratios of bridge tokens (i.e. nETH, hETH) to the underlying token.

---
+++ optimism2/prices/hourly_bridge_token_price_ratios.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS prices.hourly_bridge_token_price_ratios(
+    dt timestamptz NOT NULL,
+    LP_contract bytea NOT NULL,
+    erc20_token bytea NOT NULL,
+    bridge_token bytea NOT NULL,
+    bridge_symbol text, 
+    bridge_decimals numeric,
+    price_ratio numeric, 
+    num_samples numeric,
+    	PRIMARY KEY (dt, LP_contract, erc20_token, bridge_token)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS prices_hourly_bridge_token_price_ratios_dt_idx ON prices.hourly_bridge_token_price_ratios USING BRIN (dt);
+CREATE INDEX IF NOT EXISTS prices_hourly_bridge_token_price_ratios_dt_erc20_token_bridge_token_idx ON prices.hourly_bridge_token_price_ratios (dt, erc20_token, bridge_token);
+CREATE INDEX IF NOT EXISTS prices_hourly_bridge_token_price_ratios_dt_bridge_token_idx ON prices.hourly_bridge_token_price_ratios (dt, bridge_token);
+CREATE INDEX IF NOT EXISTS prices_hourly_bridge_token_price_ratios_dt_erc20_token_idx ON prices.hourly_bridge_token_price_ratios (dt, erc20_token);
