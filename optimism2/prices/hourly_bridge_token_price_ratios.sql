bbe5c0492e95253f7cb18f1bafbd2fd8cf5ba8ee
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-14 10:48:13 -0500

    Update hourly_bridge_token_price_ratios.sql
    Add a unique key, and remove 'lp_contract' from the primary key since it can be null.

--- optimism2/prices/hourly_bridge_token_price_ratios.sql
+++ optimism2/prices/hourly_bridge_token_price_ratios.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS prices.hourly_bridge_token_price_ratios(
+CREATE TABLE IF NOT EXISTS dune_user_generated.test_hourly_bridge_token_price_ratios(
     hour timestamptz NOT NULL,
     lp_contract bytea,
     erc20_token bytea NOT NULL,
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS prices.hourly_bridge_token_price_ratios(
     bridge_decimals numeric,
     price_ratio numeric, 
     sample_size numeric,
-    PRIMARY KEY (hour, LP_contract, erc20_token, bridge_token)
+    PRIMARY KEY (hour, erc20_token, bridge_token),
+    UNIQUE(bridge_token,hour)
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS prices_hourly_bridge_token_price_ratios_hour_bridge_token__hour_uniq_idx ON prices.hourly_bridge_token_price_ratios (bridge_token, hour);
