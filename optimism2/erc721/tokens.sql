4207ba2e7ed6cc79919935244ce0f2a157287529
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-20 21:15:20 -0800

    Create erc721.tokens (sql) (#663)
    * Create erc721.tokens (sql)
    
    Creating a token to project mapping table for ERC721s. Let me know if you all have any alternative ideas (I checked for a way to query tokenURIs, but came up empty).
    
    * Remove empty line and add backslash
    
    Co-authored-by: masquot <78954627+masquot@users.noreply.github.com>

---
+++ optimism2/erc721/tokens.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS erc721.tokens (
+	contract_address bytea UNIQUE,
+	project_name text
+);
+
+BEGIN;
+DELETE FROM erc721.tokens *;
+
+
+COPY erc721.tokens (contract_address, project_name) FROM stdin;
+\\xb8df6cc3050cc02f967db1ee48330ba23276a492	OptiPunk
+\\x52782699900df91b58ecd618e77847c5774dcd2e	Optimistic Bunnies
+\\x006eb613cc586198003a119485594ecbbdf41230	OptimisticLoogies
+\\x5763f564e0b5d8233da0accf2585f2dbef0f0dfa	OldEnglish (OE40)
+\\xeb0d6c099b2fb18da09ad554b7612bfae6a9c9ab	Otter Coloring Book
+\\x8ceac0f1fbac4cf48527b35f8bc629fb30653184	Animal Coloring Book Eraser
+\\xfb0b3e0f27a2a858cc6656627e662b0d3cd5b19b	ETHDubaiTicket
+\.
+
+COMMIT;
+
+CREATE INDEX IF NOT EXISTS erc721_tokens_contract_address_project_name_idx ON erc721.tokens USING btree (contract_address) INCLUDE (project_name);
