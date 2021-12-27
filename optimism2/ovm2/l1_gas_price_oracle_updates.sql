e168236ee2af1ff278f4c9ac38beea1957f6bd55
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-27 00:40:25 -0800

    Add  l1_gas_price_oracle_updates table (#680)
    * Create l1_gas_price_oracle_updates.sql
    
    * Update l1_gas_price_oracle_updates.sql
    
    * Create insert_l1_gas_price_oracle_updates
    
    Test Query: https://dune.xyz/queries/315523
    
    * Create readme.md
    
    * Remove comma
    
    Co-authored-by: masquot <78954627+masquot@users.noreply.github.com>

---
+++ optimism2/ovm2/l1_gas_price_oracle_updates.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS ovm2;
+
+CREATE TABLE IF NOT EXISTS ovm2.l1_gas_price_oracle_updates (
+    block_number numeric PRIMARY KEY,
+    l1_gas_price numeric NOT NULL,
+    block_time timestamptz NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS ovm2_l1_gas_prices_block_number_idx ON ovm2.l1_gas_price_oracle_updates (block_number);
+CREATE INDEX IF NOT EXISTS ovm2_l1_gas_prices_block_time_idx ON ovm2.l1_gas_price_oracle_updates (block_time);
+CREATE INDEX IF NOT EXISTS ovm2_l1_gas_prices_block_number_block_time_idx ON ovm2.l1_gas_price_oracle_updates (block_number, block_time);
