60a515410c64346d2ef5d45882e7fb41a057c16f
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-30 10:51:23 -0800

    Update insert_aave_tokens.sql

--- ethereum/llama/aave_treasury/insert_aave_tokens.sql
+++ ethereum/llama/aave_treasury/insert_aave_tokens.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION llama.insert_aave_tokens(start_block numeric, end_block numeric) RETURNS integer
+CREATE OR REPLACE FUNCTION llama.insert_aave_tokens(start_time numeric, end_time numeric) RETURNS integer
 LANGUAGE plpgsql AS $function$
 DECLARE r integer;
 BEGIN
@@ -12,15 +12,74 @@ WITH rows AS (
       side
     )
 
+SELECT
+token_address, decimals, symbol, erc20_address, erc20_symbol, side
+FROM
+(
+SELECT
+a."aToken" AS token_address,
+e.decimals AS decimals,
+'a' || e.symbol AS symbol,
+a.asset AS erc20_address,
+e.symbol AS erc20_symbol,
+'Deposit' AS side
+FROM aave_v2."LendingPoolConfigurator_evt_ReserveInitialized" a --asset is raw, atoken is atoken
+	WHERE evt_block_time >= start_time
+	AND evt_block_time <= end_time
 
-	--https://dba.stackexchange.com/questions/186218/carry-over-long-sequence-of-missing-values-with-postgres
+LEFT JOIN erc20."tokens" e
+ON a.asset = e."contract_address"
 
-	SELECT 
-  --stuff to insert
+UNION --ammtokens
 
-	FROM events
+SELECT 
+    amm."aTokenAddress" AS token_address,
+    e.decimals AS decimals,
+    'aAmm' || e.symbol AS symbol,
+    amm.asset AS erc20_address,
+    e.symbol AS erc20_symbol,
+    'AMM' AS side
+    FROM aave_v2."AaveLendingPoolV2AMM_call_initReserve" amm
+    LEFT JOIN erc20."tokens" e
+        ON amm.asset = e."contract_address"
+    WHERE e.decimals IS NOT NULL
+	AND evt_block_time >= start_time
+	AND evt_block_time <= end_time
 
-    ON CONFLICT DO NOTHING
+UNION --variable debt
+
+SELECT 
+    vd."variableDebtToken" AS token_address,
+    e.decimals AS decimals,
+    'variableDebt' || e.symbol AS symbol,
+    vd.asset AS erc20_address,
+    e.symbol AS erc20_symbol,
+    'Borrow' AS side
+    FROM aave_v2."LendingPoolConfigurator_evt_ReserveInitialized" vd
+    LEFT JOIN erc20."tokens" e
+        ON vd.asset = e."contract_address"
+    WHERE e.decimals IS NOT NULL
+	AND evt_block_time >= start_time
+	AND evt_block_time <= end_time
+
+UNION --stable debt 
+
+SELECT 
+    sd."stableDebtToken" AS token_address,
+    e.decimals AS decimals,
+    'stableDebt' || e.symbol AS symbol,
+    sd.asset AS erc20_address,
+    e.symbol AS erc20_symbol,
+    'Borrow' AS side
+    FROM aave_v2."LendingPoolConfigurator_evt_ReserveInitialized" sd
+    LEFT JOIN erc20."tokens" e
+        ON sd.asset = e."contract_address"
+    WHERE e.decimals IS NOT NULL
+	AND evt_block_time >= start_time
+	AND evt_block_time <= end_time
+) a
+
+    ON CONFLICT (token_address, erc20_address, side) DO UPDATE SET decimals = EXCLUDED.decimals, symbol = EXCLUDED.symbol, erc20_symbol = EXCLUDED.erc20_symbol
     RETURNING 1
 )
 SELECT count(*) INTO r from rows;
@@ -29,17 +88,18 @@ END
 $function$;
 
 -- Get the table started
-SELECT llama.insert_aave_tokens(1,1000)
+SELECT llama.insert_aave_tokens('2019-01-01'::date,NOW())
 WHERE NOT EXISTS (
     SELECT *
     FROM llama.insert_aave_tokens
-    WHERE block_number = 1000
 );
 
+-- tables are so small, so just run everything rather than building in logic for backfilling tokens. Set a 1 year gap to add some constraint.
 INSERT INTO cron.job (schedule, command)
-VALUES ('5,25,45 * * * *', $$
+VALUES ('15,45 * * * *', $$
     SELECT llama.insert_aave_tokens(
-        (SELECT NOW() - interval '3 days'),
+        (SELECT NOW() - interval '1 year'),
         (SELECT NOW());
+	
 $$)
 ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
