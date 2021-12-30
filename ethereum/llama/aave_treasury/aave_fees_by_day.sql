51206cc10c79f756ef1eb21e9d511d89a79f0453
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-30 11:31:21 -0800

    Update aave_fees_by_day.sql
    add version

--- ethereum/llama/aave_treasury/aave_fees_by_day.sql
+++ ethereum/llama/aave_treasury/aave_fees_by_day.sql
@@ -11,9 +11,11 @@ CREATE TABLE IF NOT EXISTS llama.aave_fees_by_day (
 	swap_fees numeric,
 	lend_burn_fees numeric,
 	deployer_in numeric,
+	version text,
 		PRIMARY KEY (day, contract_address)
 );
 
+CREATE INDEX IF NOT EXISTS llama_aave_treasury_fees_by_day_day_address_version_idx ON llama.aave_fees_by_day (day,contract_address, version);
 CREATE INDEX IF NOT EXISTS llama_aave_treasury_fees_by_day_day_address_idx ON llama.aave_fees_by_day (day,contract_address);
 CREATE INDEX IF NOT EXISTS llama_aave_treasury_fees_by_day_day_idx ON llama.aave_fees_by_day (day);
 CREATE INDEX IF NOT EXISTS llama_aave_treasury_fees_by_day_address_idx ON llama.aave_fees_by_day (contract_address);
