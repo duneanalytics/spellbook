f1693c82789b5d9b6606efb3d812f8389069d368
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2021-12-30 11:00:59 -0800

    Create aave_fees_by_day.sql

---
+++ ethereum/llama/aave_treasury/aave_fees_by_day.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA IF NOT EXISTS llama;
+
+CREATE TABLE IF NOT EXISTS llama.aave_fees_by_day (   
+	day timestamptz,
+	contract_address bytea,
+	borrow_fees_originated numeric,
+	repay_fees numeric,
+	liquidation_fees numeric,
+	flashloan_v1_fees numeric,
+	flashloan_v2_fees numeric,
+	swap_fees numeric,
+	lend_burn_fees numeric,
+	deployer_in numeric,
+		PRIMARY KEY (day, contract_address)
+);
+
+CREATE INDEX IF NOT EXISTS llama_aave_treasury_fees_by_day_day_address_idx ON llama.aave_fees_by_day (day,contract_address);
+CREATE INDEX IF NOT EXISTS llama_aave_treasury_fees_by_day_day_idx ON llama.aave_fees_by_day (day);
+CREATE INDEX IF NOT EXISTS llama_aave_treasury_fees_by_day_address_idx ON llama.aave_fees_by_day (contract_address);
