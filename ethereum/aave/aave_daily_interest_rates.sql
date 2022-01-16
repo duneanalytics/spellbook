5a733894b29b7716e08ea0bce50a4e69bbf2b3cb
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-01-15 16:28:02 -0800

    Update aave_daily_interest_rates.sql

--- ethereum/aave/aave_daily_interest_rates.sql
+++ ethereum/aave/aave_daily_interest_rates.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS aave.aave_daily_interest_rates (
 		PRIMARY KEY (underlying_token, token, day)
 );
 
-CREATE INDEX IF NOT EXISTS llama_aave_daily_interest_rates_underlying_token_day_idx ON llama.aave_daily_interest_rates (underlying_token, token, day);
-CREATE INDEX IF NOT EXISTS llama_aave_daily_interest_rates_underlying_day_idx ON llama.aave_daily_interest_rates (underlying_token, day);
-CREATE INDEX IF NOT EXISTS llama_aave_daily_interest_rates_token_day_idx ON llama.aave_daily_interest_rates (token, day);
-CREATE INDEX IF NOT EXISTS llama_aave_daily_interest_rates_day_idx ON llama.aave_daily_interest_rates (day);
+CREATE INDEX IF NOT EXISTS llama_aave_daily_interest_rates_underlying_token_day_idx ON aave.aave_daily_interest_rates (underlying_token, token, day);
+CREATE INDEX IF NOT EXISTS llama_aave_daily_interest_rates_underlying_day_idx ON aave.aave_daily_interest_rates (underlying_token, day);
+CREATE INDEX IF NOT EXISTS llama_aave_daily_interest_rates_token_day_idx ON aave.aave_daily_interest_rates (token, day);
+CREATE INDEX IF NOT EXISTS llama_aave_daily_interest_rates_day_idx ON aave.aave_daily_interest_rates (day);
