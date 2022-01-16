68946de5878a3b4418fe5702665be075b5875389
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-01-15 16:09:00 -0800

    Update and rename ethereum/llama/aave_treasury/aave_tokens.sql to ethereum/aave/aave_tokens.sql
    Move to aave, add token name and program type

---
+++ ethereum/aave/aave_tokens.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA IF NOT EXISTS aave;
+
+CREATE TABLE IF NOT EXISTS aave.aave_tokens (   
+    token_address bytea PRIMARY KEY,
+    decimals int4,
+    symbol text,
+    erc20_address bytea,
+    erc20_symbol text,
+    side text,
+    token_name text,
+    program_type text,
+        UNIQUE (token_address, erc20_address, side)
+);
+
+CREATE INDEX IF NOT EXISTS llama_aave_treasury_aave_tokens_token_address_idx ON aave.aave_tokens (token_address);
+CREATE INDEX IF NOT EXISTS llama_aave_treasury_aave_tokens_erc20_address_idx ON aave.aave_tokens (erc20_address);
+CREATE INDEX IF NOT EXISTS llama_aave_treasury_aave_tokens_token_and_erc20_address_idx ON aave.aave_tokens (token_address,erc20_address);
+CREATE INDEX IF NOT EXISTS llama_aave_treasury_aave_tokens_program_type_idx ON aave.aave_tokens (program_type);
