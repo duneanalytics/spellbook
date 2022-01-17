1b2d07e685ab7670d22cdcee94edcc3b237cdd47
masquot <78954627+masquot@users.noreply.github.com>
2022-01-17 15:32:38 +0100

    Update insert_hourly_bridge_token_price_ratios.sql

--- optimism2/prices/insert_hourly_bridge_token_price_ratios.sql
+++ optimism2/prices/insert_hourly_bridge_token_price_ratios.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION prices.insert_hourly_bridge_token_price_ratios(start_time timestamp, end_time timestamp=now()) RETURNS integer
+CREATE OR REPLACE FUNCTION prices.insert_hourly_bridge_token_price_ratios(start_time timestamptz, end_time timestamptz=now()) RETURNS integer
 LANGUAGE plpgsql AS $function$
 DECLARE r integer;
 BEGIN
