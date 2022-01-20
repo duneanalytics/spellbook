e277e4e1ff7aaa1ed4cf14c1541f8a6eca47cb1b
lolboysg <72015889+lolboysg@users.noreply.github.com>
2022-01-20 15:47:42 +0100

    Add 1inch to dex.trades on polygon (#625)
    * Add 1inch to dex.trades on Polygon
    
    * Changed insert_1inch.sql from polygon to polygon/dex/trades
    
    * Update srcToken and dstToken so that it changes to matic
    
    * Update trade_id so that it doesn't count evt_index

---
+++ polygon/dex/trades/insert_1inch.sql
@@ -0,0 +1,146 @@
+CREATE OR REPLACE FUNCTION dex.insert_1inch(start_ts timestamptz, end_ts timestamptz=now(), start_block numeric=0, end_block numeric=9e18) RETURNS integer
+LANGUAGE plpgsql AS $function$
+DECLARE r integer;
+BEGIN
+WITH rows AS (
+    INSERT INTO dex.trades (
+        block_time,
+        token_a_symbol,
+        token_b_symbol,
+        token_a_amount,
+        token_b_amount,
+        project,
+        version,
+        category,
+        trader_a,
+        trader_b,
+        token_a_amount_raw,
+        token_b_amount_raw,
+        usd_amount,
+        token_a_address,
+        token_b_address,
+        exchange_contract_address,
+        tx_hash,
+        tx_from,
+        tx_to,
+        trace_address,
+        evt_index,
+        trade_id
+    )
+    SELECT
+        dexs.block_time,
+        pa.symbol AS token_a_symbol,
+        pb.symbol AS token_b_symbol,
+        token_a_amount_raw / 10 ^ pa.decimals AS token_a_amount,
+        token_b_amount_raw / 10 ^ pb.decimals AS token_b_amount,
+        project,
+        version,
+        category,
+        coalesce(trader_a, tx."from") as trader_a, -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
+        trader_b,
+        token_a_amount_raw,
+        token_b_amount_raw,
+        coalesce(
+            usd_amount,
+            token_a_amount_raw / 10 ^ pa.decimals * pa.price,
+            token_b_amount_raw / 10 ^ pb.decimals * pb.price
+        ) as usd_amount,
+        token_a_address,
+        token_b_address,
+        exchange_contract_address,
+        tx_hash,
+        tx."from" as tx_from,
+        tx."to" as tx_to,
+        trace_address,
+        evt_index,
+        row_number() OVER (PARTITION BY project, tx_hash, trace_address  ORDER BY version, category) AS trade_id
+    FROM (
+        -- 1inch v3
+        SELECT
+            evt_block_time AS block_time,
+            '1inch' AS project,
+            '3' AS version,
+            'Aggregator' AS category,
+            sender AS trader_a,
+            NULL::bytea AS trader_b,
+            "returnAmount" AS token_a_amount_raw,
+            "spentAmount" AS token_b_amount_raw,
+            NULL::numeric AS usd_amount,
+            (CASE WHEN "dstToken" = '\xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN '\x0000000000000000000000000000000000001010' ELSE "dstToken" END) AS token_a_address,
+            (CASE WHEN "srcToken" = '\xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN '\x0000000000000000000000000000000000001010' ELSE "srcToken" END) AS token_b_address,
+            contract_address AS exchange_contract_address,
+            evt_tx_hash AS tx_hash,
+            evt_index,
+            NULL::integer[] AS trace_address
+        FROM oneinch."OneInchExchange_evt_Swapped"
+        
+        UNION ALL
+
+        -- 1inch v4
+        SELECT
+            evt_block_time AS block_time,
+            '1inch' AS project,
+            '4' AS version,
+            'Aggregator' AS category,
+            sender AS trader_a,
+            NULL::bytea AS trader_b,
+            "returnAmount" AS token_a_amount_raw,
+            "spentAmount" AS token_b_amount_raw,
+            NULL::numeric AS usd_amount,
+            (CASE WHEN "dstToken" = '\xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN '\x0000000000000000000000000000000000001010' ELSE "dstToken" END) AS token_a_address,
+            (CASE WHEN "srcToken" = '\xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN '\x0000000000000000000000000000000000001010' ELSE "srcToken" END) AS token_b_address,
+            contract_address AS exchange_contract_address,
+            evt_tx_hash AS tx_hash,
+            evt_index,
+            NULL::integer[] AS trace_address
+        FROM oneinch_v4."AggregationRouterV4_evt_Swapped"
+        
+    ) dexs
+    INNER JOIN polygon.transactions tx
+        ON dexs.tx_hash = tx.hash
+        AND tx.block_time >= start_ts
+        AND tx.block_time < end_ts
+        AND tx.block_number >= start_block
+        AND tx.block_number < end_block
+    LEFT JOIN prices.usd pa ON pa.minute = date_trunc('minute', dexs.block_time)
+        AND pa.contract_address = dexs.token_a_address
+        AND pa.minute >= start_ts
+        AND pa.minute < end_ts
+    LEFT JOIN prices.usd pb ON pb.minute = date_trunc('minute', dexs.block_time)
+        AND pb.contract_address = dexs.token_b_address
+        AND pb.minute >= start_ts
+        AND pb.minute < end_ts
+    WHERE dexs.block_time >= start_ts
+    AND dexs.block_time < end_ts
+    ON CONFLICT DO NOTHING
+    RETURNING 1
+)
+SELECT count(*) INTO r from rows;
+RETURN r;
+END
+$function$;
+
+-- fill 2021
+SELECT dex.insert_1inch(
+    '2021-01-01',
+    now(),
+    (SELECT max(number) FROM polygon.blocks WHERE time < '2021-01-01'),
+    (SELECT MAX(number) FROM polygon.blocks where time < now() - interval '20 minutes')
+)
+WHERE NOT EXISTS (
+    SELECT *
+    FROM dex.trades
+    WHERE block_time > '2021-01-01'
+    AND block_time <= now() - interval '20 minutes'
+    AND project = 'Curve'
+);
+
+INSERT INTO cron.job (schedule, command)
+VALUES ('*/10 * * * *', $$
+    SELECT dex.insert_1inch(
+        (SELECT max(block_time) - interval '1 days' FROM dex.trades WHERE project='1inch'),
+        (SELECT now() - interval '20 minutes'),
+        (SELECT max(number) FROM polygon.blocks WHERE time < (SELECT max(block_time) - interval '1 days' FROM dex.trades WHERE project='1inch')),
+        (SELECT MAX(number) FROM polygon.blocks where time < now() - interval '20 minutes'));
+$$)
+ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
