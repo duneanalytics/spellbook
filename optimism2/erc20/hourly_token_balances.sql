6917e1a3b371974b6090d570b4de54186807990d
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-02-01 22:23:36 -0500

    Update hourly_token_balances.sql

--- optimism2/erc20/hourly_token_balances.sql
+++ optimism2/erc20/hourly_token_balances.sql
@@ -15,4 +15,4 @@ CREATE TABLE IF NOT EXISTS erc20.hourly_token_balances (
 
 CREATE INDEX IF NOT EXISTS ovm2_hourly_token_balances_hour_idx ON ovm2.l1_gas_price_oracle_updates (hour);
 CREATE INDEX IF NOT EXISTS ovm2_hourly_token_balances_hour_user_address_idx ON ovm2.l1_gas_price_oracle_updates (hour,user_address);
-CREATE INDEX IF NOT EXISTS ovm2_hourly_token_balances_hour,user_address_token_address_idx ON ovm2.l1_gas_price_oracle_updates (hour,user_address,token_address);
+CREATE INDEX IF NOT EXISTS ovm2_hourly_token_balances_hour_user_address_token_address_idx ON ovm2.l1_gas_price_oracle_updates (hour,user_address,token_address);
