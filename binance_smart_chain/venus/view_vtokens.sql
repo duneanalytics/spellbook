eb6ece3d26f0f408259bba20277c69b2897e4e64
Chris Lcx <alichenxiang@gmail.com>
2022-02-08 21:28:17 +0800

    Update view_vtokens.sql

--- binance_smart_chain/venus/view_vtokens.sql
+++ binance_smart_chain/venus/view_vtokens.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW compound.view_ctokens (symbol, contract_address, decimals, underlying_token_address) AS VALUES
+CREATE OR REPLACE VIEW venus.view_vtokens (symbol, contract_address, decimals, underlying_token_address) AS VALUES
    ('vBNB'::text,   '\xa07c5b74c9b40447a954e1466938b865b6bbea36'::bytea, 8::numeric, '\xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'::bytea),
    ('vUSDT'::text,  '\xfD5840Cd36d94D7229439859C0112a4185BC0255'::bytea, 8::numeric, '\x55d398326f99059fF775485246999027B3197955'::bytea),
    ('vUSDC'::text,  '\xecA88125a5ADbe82614ffC12D0DB554E2e2867C8'::bytea, 8::numeric, '\x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'::bytea),
