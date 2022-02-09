f110b900a472fd2da1fd15983f54f637945d441e
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-02-09 00:02:29 -0500

    Update get_address_from_data.sql

--- ethereum/public/get_address_from_data.sql
+++ ethereum/public/get_address_from_data.sql
@@ -1,14 +1,13 @@
 CREATE OR REPLACE FUNCTION get_address_from_data(bytea) --data from logs (64 bit)
 RETURNS bytea
 STABLE
---STRICT
+
 LANGUAGE plpgsql
 AS $$
 DECLARE
     _log_data bytea := $1;
     addr_data_part bytea := '\x00'::bytea;
-    --nonce INT := $2;
-    --gas_limit INT := $3;
+
 BEGIN
 
 addr_data_part = substring($1 from 13 for 20);
