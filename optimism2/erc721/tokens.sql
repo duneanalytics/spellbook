b541599249ff964be43bb8b337add56cc115d53e
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-02-23 14:58:39 -0500

    Update tokens.sql

--- optimism2/erc721/tokens.sql
+++ optimism2/erc721/tokens.sql
@@ -31,6 +31,8 @@ COPY erc721.tokens (contract_address, project_name) FROM stdin;
 \\xdbfeaae58b6da8901a8a40ba0712beb2ee18368e	Dope Wars Hustlers
 \\xeac4c25aaefe942d408004f5afcce947bae51484	Pegasus Welcome Ticket
 \\xa698713a3bc386970Cdc95A720B5754cC0f96931	Words (viaMirror)
+\\xB5604Fc106074A140DF727Fe28cd68F0dbB6C1B9	PixelConInvaders
+\\x0e55e1913c50e015e0f60386ff56a4bfb00d7110	Dope Wars Gear
 \.
 
 COMMIT;
