9552fa2153e931a4829efdfa9a8c2e8715b49049
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-03-15 03:47:30 -0400

    updating curve
    curve has a weird metapool decimals behavior. This matches the pattern

--- optimism2/dex/insert_curve.sql
+++ optimism2/dex/insert_curve.sql
@@ -27,22 +27,30 @@ WITH rows AS (
         evt_index,
         trade_id
     )
-   WITH curve_pools AS (
+WITH curve_pools AS (
     WITH base_pools AS (
-        SELECT "arg0" AS tokenid, output_0 AS token, contract_address AS pool,
-            MAX("arg0") OVER (PARTITION BY contract_address) AS max_token_id
+        SELECT "arg0" AS tokenid, output_0 AS token, contract_address AS pool
             FROM curvefi."StableSwap_call_coins" WHERE call_success
         )
     , meta_pools AS (
-        SELECT bp.tokenid, bp.token, mp."contract_address" AS pool -- start the pool
+    SELECT tokenid, token, et."contract_address" AS pool
+    FROM (
+        SELECT mp.evt_tx_hash, bp.tokenid + 1 AS tokenid, bp.token
         FROM curvefi."PoolFactory_evt_MetaPoolDeployed" mp
         INNER JOIN base_pools bp
             ON mp.base_pool = bp.pool
         UNION ALL
-        SELECT bp.max_token_id + 1 AS tokenid, mp."coin" AS token, mp."contract_address" AS pool
+        SELECT mp.evt_tx_hash, 0 AS tokenid, mp."coin" AS token
         FROM curvefi."PoolFactory_evt_MetaPoolDeployed" mp
         INNER JOIN base_pools bp
             ON mp.base_pool = bp.pool
+        ) mps
+        -- the exchange address appears as an erc20 minted to itself (not in the deploymeny event)
+        INNER JOIN erc20."ERC20_evt_Transfer" et
+            ON et.evt_tx_hash = mps.evt_tx_hash
+            AND et."from" = '\x0000000000000000000000000000000000000000'
+            AND et."to" = et."contract_address"
+            
         GROUP BY 1,2,3
     )
     SELECT * FROM (
@@ -56,8 +64,9 @@ SELECT
     dexs.block_time,
     erc20a.symbol AS token_a_symbol,
     erc20b.symbol AS token_b_symbol,
-    token_a_amount_raw / 10 ^ erc20a.decimals AS token_a_amount,
-    token_b_amount_raw / 10 ^ erc20b.decimals AS token_b_amount,
+    --metapools seem to always use the added coin's decimals if it's the one that's bought - even if the other token has less decimals (i.e. USDC)
+    token_a_amount_raw / 10 ^ (CASE WHEN pool_type = 'meta' AND bought_id = 0 THEN underlying_decimals ELSE erc20a.decimals END) AS token_a_amount,
+    token_b_amount_raw / 10 ^ (CASE WHEN pool_type = 'meta' AND bought_id = 0 THEN underlying_decimals ELSE erc20b.decimals END)  AS token_b_amount,
     project,
     version,
     category,
@@ -67,8 +76,9 @@ SELECT
     token_b_amount_raw,
     coalesce(
         usd_amount,
-        token_a_amount_raw / 10 ^ erc20a.decimals * pa.median_price,
-        token_b_amount_raw / 10 ^ erc20b.decimals * pb.median_price
+	--metapools seem to always use the added coin's decimals if it's the one that's bought - even if the other token has less decimals (i.e. USDC)
+        token_a_amount_raw / 10 ^ (CASE WHEN pool_type = 'meta' AND bought_id = 0 THEN underlying_decimals ELSE erc20a.decimals END) * pa.median_price,
+        token_b_amount_raw / 10 ^ (CASE WHEN pool_type = 'meta' AND bought_id = 0 THEN underlying_decimals ELSE erc20b.decimals END) * pb.median_price
     ) as usd_amount,
     token_a_address,
     token_b_address,
@@ -78,10 +88,11 @@ SELECT
     tx."to" as tx_to,
     trace_address,
     evt_index,
-    row_number() OVER (PARTITION BY project, tx_hash, evt_index, trace_address ORDER BY version, category) AS trade_id
+    row_number() OVER (PARTITION BY project, tx_hash, dexs.evt_index, trace_address ORDER BY version, category) AS trade_id
     FROM (
-    --Stableswap
+    -- Stableswap
      SELECT
+        'stable' AS pool_type, -- has implications for decimals for curve
         t.evt_block_time AS block_time,
         'Curve' AS project,
         '1' AS version,
@@ -97,7 +108,8 @@ SELECT
         t.contract_address as exchange_contract_address,
         t.evt_tx_hash AS tx_hash,
         NULL::integer[] AS trace_address,
-        t.evt_index
+        t.evt_index, bought_id, sold_id,
+        NULL::numeric AS underlying_decimals --used for metasaps
     FROM curvefi."StableSwap_evt_TokenExchange" t
         INNER JOIN curve_pools ta
             ON t.contract_address = ta.pool
@@ -105,37 +117,37 @@ SELECT
         INNER JOIN curve_pools tb
             ON t.contract_address = tb.pool
             AND t.sold_id = tb.tokenid
-	
-	WHERE t.evt_block_time >= start_ts AND t.evt_block_time < end_ts
-        
     
-    -- UNION ALL
-    --MetaPoolSwap
-    -- SELECT
-    --     t.evt_block_time AS block_time,
-    --     'Curve' AS project,
-    --     '1' AS version,
-    --     'DEX' AS category,
-    --     t.buyer AS trader_a,
-    --     NULL::bytea AS trader_b,
-    --     -- when amount0 is negative it means trader_a is buying token0 from the pool
-    --     "tokens_bought" AS token_a_amount_raw,
-    --     "tokens_sold" AS token_b_amount_raw,
-    --     NULL::numeric AS usd_amount,
-    --     ta.token AS token_a_address,
-    --     tb.token AS token_b_address,
-    --     t.contract_address as exchange_contract_address,
-    --     t.evt_tx_hash AS tx_hash,
-    --     NULL::integer[] AS trace_address,
-    --     t.evt_index
-    -- FROM /*Exchange Underlying*/ t
-    --     INNER JOIN curve_pools ta
-    --         ON t.contract_address = ta.pool
-    --         AND t.bought_id = ta.tokenid
-    --     INNER JOIN curve_pools tb
-    --         ON t.contract_address = tb.pool
-    --         AND t.sold_id = tb.tokenid
---         WHERE t.evt_block_time >= start_ts AND t.evt_block_time < end_ts
+    UNION ALL
+    -- MetaPoolSwap
+    SELECT
+    'meta' AS pool_type, -- has implications for decimals for curve
+        t.evt_block_time AS block_time,
+        'Curve' AS project,
+        '1' AS version,
+        'DEX' AS category,
+        t.buyer AS trader_a,
+        NULL::bytea AS trader_b,
+        -- when amount0 is negative it means trader_a is buying token0 from the pool
+        "tokens_bought" AS token_a_amount_raw,
+        "tokens_sold" AS token_b_amount_raw,
+        NULL::numeric AS usd_amount,
+        ta.token AS token_a_address,
+        tb.token AS token_b_address,
+        t.contract_address as exchange_contract_address,
+        t.evt_tx_hash AS tx_hash,
+        NULL::integer[] AS trace_address,
+        t.evt_index, bought_id, sold_id,
+        CASE WHEN bought_id = 0 THEN ea.decimals ELSE eb.decimals END AS underlying_decimals --used if meta
+    FROM curvefi."MetaPoolSwap_evt_TokenExchangeUnderlying" t
+        INNER JOIN curve_pools ta
+            ON t.contract_address = ta.pool
+            AND t.bought_id = ta.tokenid
+        INNER JOIN curve_pools tb
+            ON t.contract_address = tb.pool
+            AND t.sold_id = tb.tokenid
+        LEFT JOIN erc20.tokens ea ON ea.contract_address = ta.token
+        LEFT JOIN erc20.tokens eb ON eb.contract_address = tb.token
     
     ) dexs
     INNER JOIN optimism.transactions tx
@@ -155,6 +167,7 @@ SELECT
         AND pb.contract_address = dexs.token_b_address
         AND pb.hour >= start_ts
         AND pb.hour < end_ts
+
 
     -- update if we have new info on prices or the erc20
     ON CONFLICT (project, tx_hash, evt_index, trade_id)
