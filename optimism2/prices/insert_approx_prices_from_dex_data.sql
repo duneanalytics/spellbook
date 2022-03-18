13b96a3cca59e1f66365f91f24cd668fcd48b713
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-03-18 00:36:52 -0700

    OP2 - Insert Curve, Zipswap to dex.trades (#897)
    * Create insert_curve.sql
    
    * Create insert_zipswap.sql
    
    * Update insert_zipswap.sql
    
    * Update insert_curve.sql
    
    * Add curve, placeholder for metapools
    
    * insert zipswap + pre swap contract decoding
    
    * fix holdover code from when I was going to do this with logs
    
    * add clipper, zipswap, curve to dex inserts
    
    * Update DEX/Agg Readme
    
    * Update readme.md
    
    * update zipswap swap table
    
    * small zipswap fixes
    
    * updating curve
    
    curve has a weird metapool decimals behavior. This matches the pattern
    
    * add dex filters to price inserts
    
    * Update insert_curve.sql
    
    * Remove redundant line
    
    Co-authored-by: masquot <masquot@protonmail.com>

--- optimism2/prices/insert_approx_prices_from_dex_data.sql
+++ optimism2/prices/insert_approx_prices_from_dex_data.sql
@@ -29,7 +29,7 @@ chainlink_prices AS (
     AND token_a_amount_raw > 100 -- filter out small spam
     AND block_time >= start_time
     AND block_time < end_time
-
+AND project IN ('Uniswap','1inch','0x API','Matcha','Zipswap')
 	GROUP BY 1,2,3,4,5,6 --remove dupes
 
     UNION ALL
@@ -47,6 +47,7 @@ chainlink_prices AS (
     AND token_b_amount_raw > 100 -- filter out small spam
     AND block_time >= start_time
     AND block_time < end_time
+AND project IN ('Uniswap','1inch','0x API','Matcha','Zipswap')
 	
 	GROUP BY 1,2,3,4,5,6 --remove dupes
 ),
