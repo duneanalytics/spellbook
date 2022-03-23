104f213fab157b889223594eae4ee6933e24e6ae
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-03-23 13:31:31 -0400

    Add chainlink re-run to backfill

--- optimism2/dex/adhoc_backfills/prices_backfill_02_01_22.sql
+++ optimism2/dex/adhoc_backfills/prices_backfill_02_01_22.sql
@@ -1,5 +1,16 @@
+--fix chainlink feeds - which stall after mid-Feb
+SELECT chainlink.insert_price_feeds(
+        	'01-01-2021'::date,
+        	now()
+    	)
+	WHERE EXISTS (SELECT 1 FROM prices.approx_prices_from_dex_data
+		WHERE DATE_TRUNC('day',hour) = '03-07-2022'::date AND symbol = 'sETH'
+		AND median_price > 3000
+		);
+
+		
 SELECT prices.insert_approx_prices_from_dex_data(
-        	'02-01-2022'::date,
+        	'01-01-2022'::date,
         	now()
     	)
 	WHERE EXISTS (SELECT 1 FROM prices.approx_prices_from_dex_data
