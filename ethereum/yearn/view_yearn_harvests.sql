c11d33f8379d46b77070c36a9664c229331aeef8
3StepCapital <98933620+3StepCapital@users.noreply.github.com>
2022-03-26 12:08:14 -0500

    Update view_yearn_harvests.sql

--- ethereum/yearn/view_yearn_harvests.sql
+++ ethereum/yearn/view_yearn_harvests.sql
@@ -5,10 +5,10 @@ DROP VIEW IF EXISTS yearn."view_yearn_harvests" cascade;
 CREATE VIEW yearn."view_yearn_harvests" AS(
   SELECT
     CASE
-      WHEN (hvst."profit"/(10^yct."yvault_deposit_token_decimals")) > 0 THEN (hvst."profit"/(10^yct."yvault_deposit_token_decimals"))
+      WHEN hvst."profit" > 0 THEN (hvst."profit"/(10^yct."yvault_deposit_token_decimals"))
       ELSE (hvst."loss"/(10^yct."yvault_deposit_token_decimals"))*-1 END as profit_loss,
-    (hvst."debtPayment"/(10^yct."yvault_deposit_token_decimals")) as debtPayment,
-    (hvst."debtOutstanding"/(10^yct."yvault_deposit_token_decimals")) as debtOutstanding,
+    (hvst."debtPayment"/(10^yct."yvault_deposit_token_decimals")) as debt_payment,
+    (hvst."debtOutstanding"/(10^yct."yvault_deposit_token_decimals")) as debt_outstanding,
     hvst."evt_tx_hash",
     cs."yvault_contract",
     cs."strategy",
@@ -17,8 +17,9 @@ CREATE VIEW yearn."view_yearn_harvests" AS(
     yct."yvault_deposit_token_decimals",
     yct."yvault_deposit_token_symbol"
   FROM
-    yearn."yvault_strat_evt_Harvested" hvst left join yearn."view_yearn_contract_strategy" cs on hvst."contract_address" = cs."strategy"
-    left join yearn."view_yearn_contract_tokens" yct on cs."yvault_contract" = yct."yvault_contract"
+    yearn."yvault_strat_evt_Harvested" hvst 
+  LEFT JOIN yearn."view_yearn_contract_strategy" cs on hvst."contract_address" = cs."strategy"
+  LEFT JOIN yearn."view_yearn_contract_tokens" yct on cs."yvault_contract" = yct."yvault_contract"
   WHERE
     "yvault_deposit_token_symbol" is not null
 )
