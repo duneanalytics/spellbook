a1bef5cd01639980d5011f140d5ec2ad9e3a2efe
3StepCapital <98933620+3StepCapital@users.noreply.github.com>
2022-03-26 11:29:46 -0500

    Update view_iearn_v2_deployments.sql

--- ethereum/yearn/view_iearn_v2_deployments.sql
+++ ethereum/yearn/view_iearn_v2_deployments.sql
@@ -3,12 +3,12 @@ DROP VIEW IF EXISTS iearn_v2."view_iearn_v2_deployments" cascade;
 
 CREATE VIEW iearn_v2."view_iearn_v2_deployments" AS(
     (SELECT
-    ("amount"/10^"yvault_deposit_token_decimals")*-1 as amount,
-    "yvault_contract",
-    "yvault_deposit_token_symbol",
-    "yvault_deposit_token",
-    "call_tx_hash",
-    "transaction_type"
+    (wtd."amount"/10^yct."yvault_deposit_token_decimals")*-1 as amount,
+    yct."yvault_contract",
+    yct."yvault_deposit_token_symbol",
+    yct."yvault_deposit_token",
+    wtd."call_tx_hash",
+    wtd."transaction_type"
     FROM
     (
         (SELECT "amount", "call_tx_hash", "contract_address","call_success",'AAVE' as transaction_type FROM iearn_v2."yToken_call_withdrawAave")
@@ -18,19 +18,20 @@ CREATE VIEW iearn_v2."view_iearn_v2_deployments" AS(
         (SELECT "_amount", "call_tx_hash", "contract_address","call_success",'Compound' as transaction_type FROM iearn_v2."yToken_call_withdrawSomeCompound")
         UNION ALL
         (SELECT "_amount", "call_tx_hash", "contract_address","call_success",'Fulcrum' as transaction_type FROM iearn_v2."yToken_call_withdrawSomeFulcrum")
-    ) wtd left join yearn."view_yearn_contract_tokens" yct on wtd."contract_address" = yct."yvault_contract"
+    ) wtd 
+    LEFT JOIN yearn."view_yearn_contract_tokens" yct on wtd."contract_address" = yct."yvault_contract"
     WHERE
     "call_success" = True)
 
     UNION ALL
 
     (SELECT
-    ("amount"/10^"yvault_deposit_token_decimals") as amount,
-    "yvault_contract",
-    "yvault_deposit_token_symbol",
-    "yvault_deposit_token",
-    "call_tx_hash",
-    "transaction_type"
+    (wtd."amount"/10^yct."yvault_deposit_token_decimals") as amount,
+    yct."yvault_contract",
+    yct."yvault_deposit_token_symbol",
+    yct."yvault_deposit_token",
+    wtd."call_tx_hash",
+    wtd."transaction_type"
     FROM
     (
         (SELECT "amount", "call_tx_hash", "contract_address","call_success",'AAVE' as transaction_type FROM iearn_v2."yToken_call_supplyAave")
@@ -40,7 +41,8 @@ CREATE VIEW iearn_v2."view_iearn_v2_deployments" AS(
         (SELECT "amount", "call_tx_hash", "contract_address","call_success",'Compound' as transaction_type FROM iearn_v2."yToken_call_supplyCompound")
         UNION ALL
         (SELECT "amount", "call_tx_hash", "contract_address","call_success",'Fulcrum' as transaction_type FROM iearn_v2."yToken_call_supplyFulcrum")
-    ) wtd left join yearn."view_yearn_contract_tokens" yct on wtd."contract_address" = yct."yvault_contract"
+    ) wtd 
+    LEFT JOIN yearn."view_yearn_contract_tokens" yct on wtd."contract_address" = yct."yvault_contract"
     WHERE
     "call_success" = True)
 )
