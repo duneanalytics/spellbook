89dca9c86b5ec89b9abc78312010e1b7d7ef0484
3StepCapital <98933620+3StepCapital@users.noreply.github.com>
2022-03-31 08:46:24 -0500

    Update view_yearn_harvests.sql

--- ethereum/yearn/view_yearn_harvests.sql
+++ ethereum/yearn/view_yearn_harvests.sql
@@ -40,8 +40,8 @@ yct."yvault_deposit_token_decimals",
 yct."yvault_deposit_token_symbol"
 FROM 
 yearn."yearn_v1_strat_evt_Harvested" hvst 
-LEFT JOIN dune_user_generated."view_yearn_contract_strategy" cs on hvst."contract_address" = cs."strategy"
-LEFT JOIN dune_user_generated."view_yearn_contract_tokens" yct on cs."yvault_contract" = yct."yvault_contract" 
+LEFT JOIN yearn."view_yearn_contract_strategy" cs on hvst."contract_address" = cs."strategy"
+LEFT JOIN yearn."view_yearn_contract_tokens" yct on cs."yvault_contract" = yct."yvault_contract" 
 WHERE
 "yvault_deposit_token_symbol" is not null
 )
