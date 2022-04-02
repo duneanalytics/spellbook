254293fcb414eb25ae044b6a8607d8c5c611d8b1
3StepCapital <98933620+3StepCapital@users.noreply.github.com>
2022-04-02 13:58:33 -0500

    Update view_yearn_harvests.sql
    Indented it correctly

--- ethereum/yearn/view_yearn_harvests.sql
+++ ethereum/yearn/view_yearn_harvests.sql
@@ -24,25 +24,22 @@ CREATE VIEW yearn."view_yearn_harvests" AS(
     "yvault_deposit_token_symbol" is not null
 UNION ALL 
 
-(
-SELECT 
-CASE 
-    WHEN hvst."wantEarned" > 0 THEN (hvst."wantEarned"/(10^yct."yvault_deposit_token_decimals"))
-    ELSE (hvst."wantEarned"/(10^yct."yvault_deposit_token_decimals"))*-1 END as profit_loss,
-NULL as debt_payment,
-NULL as debt_outstanding,
-hvst."evt_tx_hash",
-cs."yvault_contract",
-cs."strategy",
-cs."yearn_type",
-yct."yvault_deposit_token",
-yct."yvault_deposit_token_decimals",
-yct."yvault_deposit_token_symbol"
-FROM 
-yearn."yearn_v1_strat_evt_Harvested" hvst 
-LEFT JOIN yearn."view_yearn_contract_strategy" cs ON hvst."contract_address" = cs."strategy"
-LEFT JOIN yearn."view_yearn_contract_tokens" yct ON cs."yvault_contract" = yct."yvault_contract" 
-WHERE
-"yvault_deposit_token_symbol" is not null
-)
+  (
+  SELECT 
+    NULL as debt_payment,
+    NULL as debt_outstanding,
+    hvst."evt_tx_hash",
+    cs."yvault_contract",
+    cs."strategy",
+    cs."yearn_type",
+    yct."yvault_deposit_token",
+    yct."yvault_deposit_token_decimals",
+    yct."yvault_deposit_token_symbol"
+  FROM 
+  yearn."yearn_v1_strat_evt_Harvested" hvst 
+  LEFT JOIN yearn."view_yearn_contract_strategy" cs ON hvst."contract_address" = cs."strategy"
+  LEFT JOIN yearn."view_yearn_contract_tokens" yct ON cs."yvault_contract" = yct."yvault_contract" 
+  WHERE
+  "yvault_deposit_token_symbol" is not null
   )
+)
