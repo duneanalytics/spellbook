df7f33ba688fe51d16dc3f186f202fcd2e1a0668
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-04-03 19:26:03 -0700

    update index names

--- optimism2/eth/eth_transfers.sql
+++ optimism2/eth/eth_transfers.sql
@@ -14,21 +14,21 @@ CREATE TABLE IF NOT EXISTS eth.eth_transfers (
 	);
 
 
-CREATE UNIQUE INDEX "eth_transfer_pkey" ON eth."eth_transfer" USING btree (tx_hash, tx_index)
-CREATE INDEX "eth_transfer_contract_address_from_tx_block_time_val_idx" ON eth."eth_transfer" USING btree (contract_address, "from", tx_block_time) INCLUDE (value, value_decimal)
-CREATE INDEX "eth_transfer_contract_address_to_tx_block_time_value_idx" ON eth."eth_transfer" USING btree (contract_address, "to", tx_block_time) INCLUDE (value, value_decimal)
-CREATE INDEX "eth_transfer_tx_block_number_idx" ON eth."eth_transfer" USING brin (tx_block_number)
-CREATE INDEX "eth_transfer_tx_block_time_idx" ON eth."eth_transfer" USING brin (tx_block_time)
-CREATE INDEX "eth_transfer_from_contract_address_tx_block_time_val_idx" ON eth."eth_transfer" USING btree ("from", contract_address, tx_block_time) INCLUDE (value, value_decimal)
-CREATE INDEX "eth_transfer_from_idx" ON eth."eth_transfer" USING btree ("from")
-CREATE INDEX "eth_transfer_to_contract_address_tx_block_time_value_idx" ON eth."eth_transfer" USING btree ("to", contract_address, tx_block_time) INCLUDE (value, value_decimal)
-CREATE INDEX "eth_transfer_incl_value_tx_idx_using_to_contract_addr_ev" ON eth."eth_transfer" USING btree ("to", contract_address, tx_block_time) INCLUDE (value, value_decimal, tx_index)
-CREATE INDEX "eth_transfer_incl_value_tx_idx_using_from_contract_time_" ON eth."eth_transfer" USING btree ("from", contract_address, tx_block_time) INCLUDE (value, value_decimal, tx_index)
-CREATE INDEX "eth_transfer_incl_value_tx_idx_using_contract_from_time_" ON eth."eth_transfer" USING btree (contract_address, "from", tx_block_time) INCLUDE (value, value_decimal, tx_index)
-CREATE INDEX "eth_transfer_incl_value_tx_idx_using_contract_to_time_id" ON eth."eth_transfer" USING btree (contract_address, "to", tx_block_time) INCLUDE (value, value_decimal, tx_index)
+CREATE UNIQUE INDEX "eth_transfer_pkey" ON eth."eth_transfers" USING btree (tx_hash, tx_index)
+CREATE INDEX "eth_transfer_contract_address_from_tx_block_time_val_idx" ON eth."eth_transfers" USING btree (contract_address, "from", tx_block_time) INCLUDE (value, value_decimal)
+CREATE INDEX "eth_transfer_contract_address_to_tx_block_time_value_idx" ON eth."eth_transfers" USING btree (contract_address, "to", tx_block_time) INCLUDE (value, value_decimal)
+CREATE INDEX "eth_transfer_tx_block_number_idx" ON eth."eth_transfers" USING brin (tx_block_number)
+CREATE INDEX "eth_transfer_tx_block_time_idx" ON eth."eth_transfers" USING brin (tx_block_time)
+CREATE INDEX "eth_transfer_from_contract_address_tx_block_time_val_idx" ON eth."eth_transfers" USING btree ("from", contract_address, tx_block_time) INCLUDE (value, value_decimal)
+CREATE INDEX "eth_transfer_from_idx" ON eth."eth_transfers" USING btree ("from")
+CREATE INDEX "eth_transfer_to_contract_address_tx_block_time_value_idx" ON eth."eth_transfers" USING btree ("to", contract_address, tx_block_time) INCLUDE (value, value_decimal)
+CREATE INDEX "eth_transfer_incl_value_tx_idx_using_to_contract_addr_ev" ON eth."eth_transfers" USING btree ("to", contract_address, tx_block_time) INCLUDE (value, value_decimal, tx_index)
+CREATE INDEX "eth_transfer_incl_value_tx_idx_using_from_contract_time_" ON eth."eth_transfers" USING btree ("from", contract_address, tx_block_time) INCLUDE (value, value_decimal, tx_index)
+CREATE INDEX "eth_transfer_incl_value_tx_idx_using_contract_from_time_" ON eth."eth_transfers" USING btree (contract_address, "from", tx_block_time) INCLUDE (value, value_decimal, tx_index)
+CREATE INDEX "eth_transfer_incl_value_tx_idx_using_contract_to_time_id" ON eth."eth_transfers" USING btree (contract_address, "to", tx_block_time) INCLUDE (value, value_decimal, tx_index)
 
-CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id" ON eth.eth_transfer (tx_method_id);
-CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id_block_time" ON eth.eth_transfer (tx_method_id,tx_block_time);
-CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id_to" ON eth.eth_transfer (tx_method_id, "to");
-CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id_to_from" ON eth.eth_transfer (tx_method_id, "to","from");
-CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id_to_block_time" ON eth.eth_transfer (tx_method_id, "to",tx_block_time);
+CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id" ON eth.eth_transfers (tx_method_id);
+CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id_block_time" ON eth.eth_transfers (tx_method_id,tx_block_time);
+CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id_to" ON eth.eth_transfers (tx_method_id, "to");
+CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id_to_from" ON eth.eth_transfers (tx_method_id, "to","from");
+CREATE INDEX IF NOT EXISTS "eth_transfer_tx_method_id_to_block_time" ON eth.eth_transfers (tx_method_id, "to",tx_block_time);
