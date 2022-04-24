c52f912c5c78cc6ec5fea6ec6faee77a0f49c190
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-04-24 08:12:49 -0700

    Update eth_transfers.sql

--- optimism2/eth/eth_transfers.sql
+++ optimism2/eth/eth_transfers.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS eth.eth_transfers (
 	value	numeric,
 	value_decimal	numeric,
 	tx_hash	bytea,
-	trace_address	int8,
+	trace_address	integer[],
 	tx_block_time 	timestamptz,
 	tx_block_number	int8,
 	tx_method_id	bytea,
