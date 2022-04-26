799c58701f43229a48c4b7708a166d4c6a0e7eea
wei3erHase <wei3erhase@defi.sucks>
2022-04-26 18:09:53 +0200

    chore: prettyfying tables

--- ethereum/keep3r_network/job_data.sql
+++ ethereum/keep3r_network/job_data.sql
@@ -1,13 +1,15 @@
-CREATE TABLE keep3r_network.job_data (job, job_name) AS
+CREATE TABLE keep3r_network.job_data (
+    job,
+    job_name
+) AS
 VALUES (
-		'0x5d469e1ef75507b0e0439667ae45e280b9d81b9c'::VARCHAR,
-		'V0-MakerDAOUpkeep'::VARCHAR
-	),
-	(
-		'0x28937b751050fcfd47fd49165c6e1268c296ba19'::VARCHAR,
-		'V1-MakerDAOUpkeep'::VARCHAR
-	),
-	(
-		'0x54a8265adc50fd66fd0f961cfcc8b62de0f2b57f'::VARCHAR,
-		'Kasparov'::VARCHAR
-	);
+    '0x5d469e1ef75507b0e0439667ae45e280b9d81b9c' ::varchar,
+    'V0-MakerDAOUpkeep' ::varchar),
+(
+    '0x28937b751050fcfd47fd49165c6e1268c296ba19' ::varchar,
+    'V1-MakerDAOUpkeep' ::varchar),
+(
+    '0x54a8265adc50fd66fd0f961cfcc8b62de0f2b57f' ::varchar,
+    'Kasparov' ::varchar
+);
+
--- ethereum/keep3r_network/token_data.sql
+++ ethereum/keep3r_network/token_data.sql
@@ -1,9 +1,12 @@
-CREATE TABLE keep3r_network.token_data (address, symbol) AS
+CREATE TABLE keep3r_network.token_data (
+    address,
+    symbol
+) AS
 VALUES (
-		'0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44'::VARCHAR,
-		'KP3R'::VARCHAR
-	),
-	(
-		'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::VARCHAR,
-		'WETH'::VARCHAR
-	);
+    '0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44' ::varchar,
+    'KP3R' ::varchar),
+(
+    '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' ::varchar,
+    'WETH' ::varchar
+);
+
