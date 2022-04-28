5852b4deea3be6571b031d288850664969a604d9
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-04-28 13:23:48 -0400

    add self destruct bool

--- optimism2/ovm2/get_contracts/get_contracts.sql
+++ optimism2/ovm2/get_contracts/get_contracts.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS ovm2.get_contracts (
 	contract_name text,
 	creator_address bytea, 
 	created_time timestamptz,
-	contract_creator_if_factory bytea
+	contract_creator_if_factory bytea,
+	is_self_destruct boolean
 );
 
 CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_address_idx ON ovm2.get_contracts (contract_address);
@@ -15,3 +16,5 @@ CREATE INDEX IF NOT EXISTS ovm2_get_contracts_created_time_idx ON ovm2.get_contr
 CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_project_idx ON ovm2.get_contracts (contract_project);
 CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_address_contract_project_idx ON ovm2.get_contracts (contract_address,contract_project);
 CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_address_contract_project_created_time_idx ON ovm2.get_contracts (contract_address,contract_project,created_time);
+CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_address_contract_project_created_time_destruct_idx ON ovm2.get_contracts (contract_address,contract_project,created_time,is_self_destruct);
+CREATE INDEX IF NOT EXISTS ovm2_get_contracts_destruct_idx ON ovm2.get_contracts (is_self_destruct);
