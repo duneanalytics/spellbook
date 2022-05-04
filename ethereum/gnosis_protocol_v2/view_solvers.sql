42e4e8f5763ac3a8f4cfc4be8c60315d2c2aba07
Benjamin Smith <bh2smith@users.noreply.github.com>
2022-05-04 21:54:42 +0200

    CoW Protocol: Adding new known-solver data (#1013)
    * adding two new solvers
    
    * Update ethereum/gnosis_protocol_v2/view_solvers.sql
    
    * Update ethereum/gnosis_protocol_v2/view_solvers.sql

--- ethereum/gnosis_protocol_v2/view_solvers.sql
+++ ethereum/gnosis_protocol_v2/view_solvers.sql
@@ -32,6 +32,8 @@ known_solver_metadata (address, environment, name) as (
                  (decode('a6ddbd0de6b310819b49f680f65871bee85f517e', 'hex'), 'prod', 'Legacy'),
                  (decode('2d15894fac906386ff7f4bd07fceac43fcf80c73', 'hex'), 'prod', 'DexCowAgg'),
                  (decode('a97DEF4fBCBa3b646dd169Bc2eeE40f0f3fE7771', 'hex'), 'prod', 'BalancerSOR'),
+                 (decode('6fa201C3Aff9f1e4897Ed14c7326cF27548d9c35', 'hex'), 'prod', 'Otex'),
+                 (decode('109BF9E0287Cc95cc623FBE7380dD841d4bdEb03', 'hex'), 'barn', 'Otex'),
                  (decode('70f3c870b6e7e1d566e40c41e2e3d6e895fcee23', 'hex'), 'barn', 'QuasiModo'),
                  (decode('97dd6a023b06ba4722aF8Af775ec3C2361e66684', 'hex'), 'barn', '0x'),
                  (decode('6372bcbf66656e91b9213b61d861b5e815296207', 'hex'), 'barn', 'ParaSwap'),
