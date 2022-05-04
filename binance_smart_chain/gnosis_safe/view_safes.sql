b91e0c6fa2ea8cd82b49207b0901a115fd43deb0
Tobias Schubotz <tobias.schubotz@gmail.com>
2022-05-04 09:50:54 +0200

    Add view_safes for bsc

--- binance_smart_chain/gnosis_safe/view_safes.sql
+++ binance_smart_chain/gnosis_safe/view_safes.sql
@@ -5,23 +5,14 @@ CREATE MATERIALIZED VIEW gnosis_safe.view_safes AS
     SELECT
     	et.from AS address,
     	et.block_time AS creation_time
-    FROM xdai.traces et 
+    FROM bsc.traces et 
     WHERE et.success = True
         AND et.call_type = 'delegatecall' -- The delegate call to the master copy is the Safe address
-        AND (
-            (substring(et."input" for 4) = '\xa97ab18a' -- setup methods of v1.0.0
-                AND et."to" = '\xb6029ea3b2c51d09a50b53ca8012feeb05bda35a') -- mastercopy v1.0.0
-            OR
-            (substring(et."input" for 4) = '\xb63e800d' -- setup methods of v1.1.0, v1.1.1, 1.2.0
-            AND et."to" IN (
-                '\xae32496491b53841efb51829d6f886387708f99b',  -- mastercopy v1.1.0
-                '\x34cfac646f301356faa8b21e94227e3583fe3f5f',  -- mastercopy v1.1.1
-                '\x6851d6fdfafd08c0295c392436245e5bc78b0185')  -- mastercopy v1.2.0
-            )
-        )
-
+        AND substring(et."input" for 4) = '\xb63e800d' -- setup method of v1.1.1
+        AND et."to" = '\x34cfac646f301356faa8b21e94227e3583fe3f5f'  -- mastercopy v1.1.1
+    
     UNION ALL
-
+    
     SELECT contract_address AS address, evt_block_time AS creation_time
     FROM gnosis_safe."GnosisSafeL2_v1_3_0_evt_SafeSetup";
 
--- xdai/gnosis_safe/view_safes_circles.sql
+++ xdai/gnosis_safe/view_safes_circles.sql
@@ -9,11 +9,12 @@ CREATE MATERIALIZED VIEW gnosis_safe.view_safes_circles AS
     WHERE et.success = True
         AND et.call_type = 'delegatecall' -- The delegate call to the master copy is the Safe address
         AND substring(et."input" for 4) = '\xb63e800d' -- setup methods of Circles mastercopy
-        AND et."to" ='\x2CB0ebc503dE87CFD8f0eCEED8197bF7850184ae'  -- Circles mastercopy;
+        AND et."to" ='\x2CB0ebc503dE87CFD8f0eCEED8197bF7850184ae';  -- Circles mastercopy
+
+COMMIT;
 
 CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS view_safes_circles_unique_idx ON gnosis_safe.view_safes_circles (address);
 
 INSERT INTO cron.job (schedule, command)
-VALUES ('0 0 * * *', $$REFRESH MATERIALIZED VIEW CONCURRENTLY gnosis_safe.view_safes_circles$$)
+VALUES ('0 0 * * *', 'REFRESH MATERIALIZED VIEW CONCURRENTLY gnosis_safe.view_safes_circles')
 ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
-COMMIT;
