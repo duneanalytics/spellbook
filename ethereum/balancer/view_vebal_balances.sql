783ead6abc4565f3896aa3ccc2947bfa7d4cbc34
Fábio Mendes <ms.fabiomendes@gmail.com>
2022-05-11 09:36:24 -0300

    fix partition clause

--- ethereum/balancer/view_vebal_balances.sql
+++ ethereum/balancer/view_vebal_balances.sql
@@ -17,11 +17,11 @@ WITH base_locks AS (
     
     decorated_locks AS (
         SELECT
-          provider, unlocked_at, updated_at, FIRST_VALUE(locked_at) OVER (PARTITION BY provider ORDER BY updated_at) AS locked_at
+          provider, unlocked_at, updated_at, FIRST_VALUE(locked_at) OVER (PARTITION BY locked_partition, provider ORDER BY updated_at) AS locked_at
         FROM (
           SELECT
             *,
-            SUM(CASE WHEN locked_at IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY provider ORDER BY updated_at) AS partition
+            SUM(CASE WHEN locked_at IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY provider ORDER BY updated_at) AS locked_partition
           FROM base_locks
         ) AS foo
     ),
