0fedea7747c2f43a2e6268fad311face723df634
jeff-dude <jeff@dune.xyz>
2022-05-18 13:23:37 -0400

    fix delimiters, add commit, fix index field

--- optimism2/ovm2/get_contracts/insert_project_name_mapping.sql
+++ optimism2/ovm2/get_contracts/insert_project_name_mapping.sql
@@ -3,7 +3,6 @@
 CREATE SCHEMA IF NOT EXISTS ovm2;
 
 CREATE TABLE IF NOT EXISTS ovm2.project_name_mappings (
-
   dune_name text,
   mapped_name text,
         UNIQUE (dune_name, mapped_name)
@@ -14,14 +13,12 @@ BEGIN;
 DELETE FROM ovm2.project_name_mappings *;
 
 COPY ovm2.project_name_mappings (dune_name,mapped_name) FROM stdin;
-
 lyra_v1	Lyra
 aave_v3	Aave
 perp_v2	Perpetual Protocol
 synthetix_futures	Kwenta
-zeroex  0x
-uniswap_v3  Uniswap
-
+zeroex	0x
+uniswap_v3	Uniswap
 \.
-
-CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS dune_name_mapped_name_uniq_idx ON ovm2.project_name_mappings (contract_address);
+COMMIT;
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS dune_name_mapped_name_uniq_idx ON ovm2.project_name_mappings (dune_name);
