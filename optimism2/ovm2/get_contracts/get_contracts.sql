0a44df01a8ba2ea864956975e84005633c399aed
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-05-23 12:35:50 -0400

    updating indices

--- optimism2/ovm2/get_contracts/get_contracts.sql
+++ optimism2/ovm2/get_contracts/get_contracts.sql
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS ovm2.get_contracts (
 	is_self_destruct boolean
 );
 
-CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_address_idx ON ovm2.get_contracts (contract_address);
-CREATE INDEX IF NOT EXISTS ovm2_get_contracts_created_time_idx ON ovm2.get_contracts (created_time);
-CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_project_idx ON ovm2.get_contracts (contract_project);
-CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_address_contract_project_idx ON ovm2.get_contracts (contract_address,contract_project);
-CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_address_contract_project_created_time_idx ON ovm2.get_contracts (contract_address,contract_project,created_time);
-CREATE INDEX IF NOT EXISTS ovm2_get_contracts_contract_address_contract_project_created_time_destruct_idx ON ovm2.get_contracts (contract_address,contract_project,created_time,is_self_destruct);
+CREATE INDEX IF NOT EXISTS ovm2_get_contracts_address_idx ON ovm2.get_contracts (contract_address);
+CREATE INDEX IF NOT EXISTS ovm2_get_contracts_created_idx ON ovm2.get_contracts (created_time);
+CREATE INDEX IF NOT EXISTS ovm2_get_contracts_project_idx ON ovm2.get_contracts (contract_project);
+CREATE INDEX IF NOT EXISTS ovm2_get_contracts_address_project_idx ON ovm2.get_contracts (contract_address,contract_project);
+CREATE INDEX IF NOT EXISTS ovm2_get_contracts_address_project_time_idx ON ovm2.get_contracts (contract_address,contract_project,created_time);
+CREATE INDEX IF NOT EXISTS ovm2_get_contracts_address_project_time_destruct_idx ON ovm2.get_contracts (contract_address,contract_project,created_time,is_self_destruct);
 CREATE INDEX IF NOT EXISTS ovm2_get_contracts_destruct_idx ON ovm2.get_contracts (is_self_destruct);
