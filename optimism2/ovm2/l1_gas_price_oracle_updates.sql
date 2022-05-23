e6ba1b555d687ffb9f852375218ba9e7e657325a
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-05-23 12:44:11 -0400

    typo fix

--- optimism2/ovm2/l1_gas_price_oracle_updates.sql
+++ optimism2/ovm2/l1_gas_price_oracle_updates.sql
@@ -10,5 +10,5 @@ CREATE INDEX IF NOT EXISTS ovm2_l1_gas_prices_block_number_idx ON ovm2.l1_gas_pr
 CREATE INDEX IF NOT EXISTS ovm2_l1_gas_prices_block_time_idx ON ovm2.l1_gas_price_oracle_updates (block_time);
 CREATE INDEX IF NOT EXISTS ovm2_l1_gas_prices_block_number_block_time_idx ON ovm2.l1_gas_price_oracle_updates (block_number, block_time);
 
-CREATE UNIQUE INDEX IF NOT EXISTS ovm2_uniq)l1_gas_prices_block_number_idx ON ovm2.l1_gas_price_oracle_updates (block_number);
-CREATE UNIQUE INDEX IF NOT EXISTS ovm2_uniq)l1_gas_prices_block_number_time_idx ON ovm2.l1_gas_price_oracle_updates (block_number, block_time);
+CREATE UNIQUE INDEX IF NOT EXISTS ovm2_uniq_l1_gas_prices_block_number_idx ON ovm2.l1_gas_price_oracle_updates (block_number);
+CREATE UNIQUE INDEX IF NOT EXISTS ovm2_uniq_l1_gas_prices_block_number_time_idx ON ovm2.l1_gas_price_oracle_updates (block_number, block_time);
