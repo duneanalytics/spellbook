e6e9a008e54a9b1166639af4c1ac4e89c395b178
António Mendes <29656081+antonio-mendes@users.noreply.github.com>
2022-05-26 19:08:48 +0100

    Fix whitespace name constraint on ens_names labels (#1079)

--- labels/ethereum/ens_names.sql
+++ labels/ethereum/ens_names.sql
@@ -1,33 +1,42 @@
-SELECT
-    owner AS address,
-    lower(name) AS label,
-    'ens name' AS type,
-    'hagaetc' AS author
-FROM
-    ethereumnameservice."ETHRegistrarController_1_evt_NameRegistered"
-WHERE
-    evt_block_time >= '{{timestamp}}'
-UNION
-SELECT
-    owner AS address,
-    lower(name) AS label,
-    'ens name' AS type,
-    'hagaetc' AS author
+WITH aux AS (
+    SELECT
+        owner AS address,
+        lower(name) AS label,
+        'ens name' AS type,
+        'hagaetc' AS author
+    FROM
+        ethereumnameservice."ETHRegistrarController_1_evt_NameRegistered"
+    WHERE
+        evt_block_time >= '{{timestamp}}'
+    UNION
+    SELECT
+        owner AS address,
+        lower(name) AS label,
+        'ens name' AS type,
+        'hagaetc' AS author
 
-FROM
-    ethereumnameservice."ETHRegistrarController_2_evt_NameRegistered"
-WHERE
-    evt_block_time >= '{{timestamp}}'
-UNION
-SELECT
-    owner AS address,
-    lower(name) AS label,
-    'ens name' AS type,
-    'hagaetc' AS author
+    FROM
+        ethereumnameservice."ETHRegistrarController_2_evt_NameRegistered"
+    WHERE
+        evt_block_time >= '{{timestamp}}'
+    UNION
+    SELECT
+        owner AS address,
+        lower(name) AS label,
+        'ens name' AS type,
+        'hagaetc' AS author
 
-FROM
-    ethereumnameservice."ETHRegistrarController_3_evt_NameRegistered"
-WHERE
-    evt_block_time >= '{{timestamp}}'
-    AND
-    LENGTH(name) < 10000;
+    FROM
+        ethereumnameservice."ETHRegistrarController_3_evt_NameRegistered"
+    WHERE
+        evt_block_time >= '{{timestamp}}'
+        AND
+        LENGTH(name) < 10000
+)
+SELECT
+    address,
+    label,
+    type,
+    author
+FROM aux
+WHERE regexp_replace(btrim(aux.label, ' '::text), '(\s+)'::text, ' '::text, 'g'::text) = aux.label IS TRUE;
