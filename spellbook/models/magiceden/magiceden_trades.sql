507639089cd076c5b42bfc1b0268fa21275f3852
soispoke <thomasthiery@dune.com>
2022-05-28 09:01:58 +0000

    test pr job

--- spellbook/models/magiceden/magiceden_trades.sql
+++ spellbook/models/magiceden/magiceden_trades.sql
@@ -4,7 +4,9 @@
 }}
 
 SELECT blockchain, 'magiceden' as project, '' as version, tx_hash, block_time, amount_usd, amount, token_symbol, token_address FROM 
-(SELECT blockchain, tx_hash, block_time, amount_usd, amount, token_symbol, token_address FROM {{ ref('magiceden_solana_trades') }}) 
+(
+        SELECT blockchain, tx_hash, block_time, amount_usd, amount, token_symbol, token_address FROM {{ ref('magiceden_solana_trades') }}
+) 
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
 where block_time > (select max(block_time) from {{ this }})
