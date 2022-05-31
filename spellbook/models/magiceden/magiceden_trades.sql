7ac6395fc09c0c775d2b98b9207117bf50cd0ee4
soispoke <66172107+soispoke@users.noreply.github.com>
2022-05-31 13:30:43 +0200

    T 12432 performance testing optimize and partition by on dbt models new (#1096)
    This PR results from a discussion we had w/ @aalan3, with the aim of optimizing/reducing build time for incremental tables.
    This resulted in 2 changes:
    - Running OPTIMIZE on incremental tables on-run-end
    - Replacing the incremental filter by a static filter,  namely  `NOW() - interval 2 days`
    
    I've checked that:
    
    * [*] I tested the query on dune.com after compiling the model with dbt compile (compiled queries are written to the target directory)
    * [*] I used "refs" to reference other models in this repo and "sources" to reference raw or decoded tables 
    * [*] the directory tree matches the pattern /sector/blockchain/ e.g. /tokens/ethereum
    * [*] if adding a new model, I added a test
    * [*] the filename is unique and ends with .sql
    * [*] each file has only one view, table or function defined  
    * [*] column names are `lowercase_snake_cased`

--- spellbook/models/magiceden/magiceden_trades.sql
+++ spellbook/models/magiceden/magiceden_trades.sql
@@ -8,7 +8,3 @@ SELECT blockchain, 'magiceden' as project, '' as version, tx_hash, block_time, a
         SELECT blockchain, tx_hash, block_time, amount_usd, amount, token_symbol, token_address, trade_id 
         FROM {{ ref('magiceden_solana_trades') }}
 ) 
-{% if is_incremental() %}
--- this filter will only be applied on an incremental run
-where block_time > (select max(block_time) from {{ this }})
-{% endif %} 
