d081f263f7cc2dc7f281cac29a7fce7bb0f68094
soispoke <66172107+soispoke@users.noreply.github.com>
2022-06-01 17:44:53 +0200

    implement macro to alter tbl properties (#1105)
    This PR implements a macro to alter tblproperties and add metadata to models we want to expose in the Dune App.
    This macro will run when deploying a new version, and is attached to the newly created "Manually Deploy New Version" job that runs the following command: dbt run-operation alter_table_properties
    <img width="1350" alt="Capture d’écran 2022-06-01 à 13 13 59" src="https://user-images.githubusercontent.com/66172107/171391975-ccaffe10-cfb1-4052-b5b4-459a1002ed9f.png">
    
    
    I've checked that:
    
    * [*] I tested the query on dune.com after compiling the model with dbt compile (compiled queries are written to the target directory)
    * [*] I used "refs" to reference other models in this repo and "sources" to reference raw or decoded tables 
    * [*] the directory tree matches the pattern /sector/blockchain/ e.g. /tokens/ethereum
    * [*] if adding a new model, I added a test
    * [*] the filename is unique and ends with .sql
    * [*] each file has only one view, table or function defined  
    * [*] column names are `lowercase_snake_cased`

---
+++ spellbook/macros/alter_table_properties.sql
@@ -0,0 +1,117 @@
+{% macro alter_table_properties() %}
+{%- if target.name == 'prod'-%}
+
+{% set balances_ethereum_erc20_day %}
+ALTER VIEW balances_ethereum.erc20_day SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='sector',
+                                                    'dune.data_explorer.abstraction.name'='balances',
+                                                    'dune.data_explorer.contributors'='["soispoke","dot2dotseurat"]');
+{% endset %}
+
+{% set balances_ethereum_erc20_hour %}
+ALTER VIEW balances_ethereum.erc20_hour SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='sector',
+                                                    'dune.data_explorer.abstraction.name'='balances',
+                                                    'dune.data_explorer.contributors'='["soispoke","dot2dotseurat"]');
+{% endset %}
+
+{% set balances_ethereum_erc20_latest %}
+ALTER VIEW balances_ethereum.erc20_latest SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='sector',
+                                                    'dune.data_explorer.abstraction.name'='balances',
+                                                    'dune.data_explorer.contributors'='["soispoke","dot2dotseurat"]');
+{% endset %}
+
+{% set magiceden_trades %}
+ALTER VIEW magiceden.trades SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["solana"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='project',
+                                                    'dune.data_explorer.abstraction.name'='magiceden',
+                                                    'dune.data_explorer.contributors'='["soispoke"]');
+{% endset %}
+
+{% set nft_trades %}
+ALTER VIEW nft.trades SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum","solana"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='sector',
+                                                    'dune.data_explorer.abstraction.name'='nft',
+                                                    'dune.data_explorer.contributors'='["soispoke"]');
+{% endset %}
+
+{% set opensea_active_traders_day %}
+ALTER VIEW opensea.active_traders_day SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum","solana"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='project',
+                                                    'dune.data_explorer.abstraction.name'='opensea',
+                                                    'dune.data_explorer.contributors'='["rchen8"]');
+{% endset %}
+
+{% set opensea_trades %}
+ALTER VIEW opensea.trades SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum","solana"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='project',
+                                                    'dune.data_explorer.abstraction.name'='opensea',
+                                                    'dune.data_explorer.contributors'='["rchen8","soispoke"]');
+{% endset %}
+
+{% set opensea_txns_day %}
+ALTER VIEW opensea.txns_day SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum","solana"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='project',
+                                                    'dune.data_explorer.abstraction.name'='opensea',
+                                                    'dune.data_explorer.contributors'='["rchen8"]');
+{% endset %}
+
+{% set opensea_volume_day %}
+ALTER VIEW opensea.volume_day SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum","solana"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='project',
+                                                    'dune.data_explorer.abstraction.name'='opensea',
+                                                    'dune.data_explorer.contributors'='["rchen8"]');
+{% endset %}
+
+{% set tokens_ethereum_erc20 %}
+ALTER VIEW tokens_ethereum.erc20 SET TBLPROPERTIES ('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='sector',
+                                                    'dune.data_explorer.abstraction.name'='tokens',
+                                                    'dune.data_explorer.contributors'='["soispoke","dot2dotseurat"]');
+{% endset %}
+
+{% set transfers_ethereum_erc20 %}
+ALTER VIEW transfers_ethereum.erc20 SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='sector',
+                                                    'dune.data_explorer.abstraction.name'='transfers',
+                                                    'dune.data_explorer.contributors'='["soispoke","dot2dotseurat"]');
+{% endset %}
+
+{% do run_query(balances_ethereum_erc20_hour) %}
+{% do run_query(balances_ethereum_erc20_latest) %}
+{% do run_query(magiceden_trades) %}
+{% do run_query(nft_trades) %}
+{% do run_query(opensea_active_traders_day) %}
+{% do run_query(opensea_trades) %}
+{% do run_query(opensea_txns_day) %}
+{% do run_query(opensea_volume_day) %}
+{% do run_query(tokens_ethereum_erc20) %}
+{% do run_query(transfers_ethereum_erc20) %}
+
+{% do log("Tables generated", info=True) %}
+{%- else -%}
+{%- endif -%}
+{% endmacro %}
