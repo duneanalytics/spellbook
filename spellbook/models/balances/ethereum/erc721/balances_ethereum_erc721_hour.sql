c822685e94f37717a7543f97c67d72fc9080a954
hildobby <h.moulie@gmail.com>
2022-06-07 16:02:21 +0200

    add erc721 balances on v2 (#1100)
    * added erc721 agg transfers & balances
    
    * debug compile and sql
    
    * erc20 and erc721 directories
    
    * reworked abstractions and added 2 tests
    
    * added where clause for faster tests
    
    * typo fix
    
    * moved having clauses to the right place
    
    * move where statement before having statement in erc721 tests
    
    * small change
    
    Co-authored-by: Meghan <msheintz@gmail.com>
    Co-authored-by: soispoke <soispoke@protonmail.com>

---
+++ spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_hour.sql
@@ -0,0 +1,32 @@
+{{ config(
+        alias='erc721_hour'
+        )
+}}
+
+with
+    hours as (
+        select
+            explode(
+                sequence(
+                    to_date('2015-01-01'), date_trunc('hour', now()), interval 1 hour
+                )
+            ) as hour
+    )
+
+, daily_balances as
+ (SELECT
+    wallet_address,
+    token_address,
+    tokenId,
+    hour,
+    lead(hour, 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY hour) AS next_hour
+    FROM {{ ref('transfers_ethereum_erc721_rolling_hour') }})
+
+SELECT
+    'ethereum' as blockchain,
+    d.hour,
+    b.wallet_address,
+    b.token_address,
+    b.tokenId
+FROM daily_balances b
+INNER JOIN hours d ON b.hour <= d.hour AND d.hour < b.next_hour
