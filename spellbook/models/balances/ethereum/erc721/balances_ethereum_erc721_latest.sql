812e5b9447f61e09a94eb1965cae8707c1190a27
soispoke <66172107+soispoke@users.noreply.github.com>
2022-06-08 10:42:00 +0200

    Add collections to erc721 balances (#1140)
    * Add collections to erc721 balances
    
    * Update balances_ethereum_erc721_hour.sql
    
    * Update balances_ethereum_erc721_latest.sql

--- spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_latest.sql
+++ spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_latest.sql
@@ -6,5 +6,7 @@ SELECT
     wallet_address,
     token_address,
     tokenId,
+    nft_tokens.name as collection,
     updated_at
 FROM {{ ref('transfers_ethereum_erc721_rolling_hour') }}
+LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft_tokens ON nft_tokens.contract_address = token_address
