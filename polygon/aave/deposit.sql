0f8d448c31a35704d3c2042c47fc140406b548af
Treaqura <93776083+Treaqura@users.noreply.github.com>
2022-06-10 11:21:33 +0200

    Update deposit.sql

--- polygon/aave/deposit.sql
+++ polygon/aave/deposit.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS aave.deposit (
     version text,
     transaction_type text,
     symbol text,
-    contract_address bytea,
-    borrower bytea,
-    "to" bytea,
+    token bytea,
+    depositor bytea,
+    withdrawn_to bytea,
     liquidator bytea,
     amount numeric,
     usd_amount numeric,
@@ -24,9 +24,9 @@ WITH rows AS (
       version,
       transaction_type,
       symbol,
-      contract_address,
-      borrower,
-      "to",
+      token,
+      depositor,
+      withdrawn_to,
       liquidator,
       amount,
       usd_amount,
@@ -40,9 +40,9 @@ WITH rows AS (
       version,
       transaction_type,
       erc20.symbol,
-      deposit.contract_address,
-      borrower,
-      "to",
+      deposit.token,
+      depositor,
+      withdrawn_to,
       liquidator,
       amount / (10^erc20.decimals) AS amount,
       (amount/(10^p.decimals)) * price AS usd_amount,
@@ -56,9 +56,9 @@ FROM (
 SELECT 
     '2' AS version,
     'deposit' AS transaction_type,
-    reserve AS contract_address,
-    "user" AS borrower,
-    NULL::bytea as "to",
+    reserve AS token,
+    "user" AS depositor,
+    NULL::bytea AS withdrawn_to,
     NULL::bytea AS liquidator,
     amount, 
     evt_tx_hash,
@@ -71,10 +71,10 @@ UNION ALL
 -- all withdrawals
 SELECT 
     '2' AS version,
-    'withdrawn' AS transaction_type,
-    reserve AS contract_address,
-    "user" AS borrower,
-    "to" AS "to",
+    'withdraw' AS transaction_type,
+    reserve AS token,
+    "user" AS depositor,
+    "to" AS withdrawn_to,
     NULL::bytea AS liquidator,
     - amount AS amount,
     evt_tx_hash,
@@ -87,9 +87,9 @@ UNION ALL
 SELECT 
     '2' AS version,
     'deposit_liquidation' AS transaction_type,
-    "collateralAsset" AS contract_address,
-    "user" AS borrower,
-    liquidator AS "to",
+    "collateralAsset" AS token,
+    "user" AS depositor,
+    liquidator AS withdrawn_to,
     liquidator AS liquidator,
     - "liquidatedCollateralAmount" AS amount,
     evt_tx_hash,
@@ -102,9 +102,9 @@ UNION ALL
 SELECT 
     '3' AS version,
     'deposit' AS transaction_type,
-    reserve AS contract_address,
-    "user" AS borrower, 
-    NULL::bytea as "to",
+    reserve AS token,
+    "user" AS depositor, 
+    NULL::bytea as withdrawn_to,
     NULL::bytea AS liquidator,
     amount,
     evt_tx_hash,
@@ -117,9 +117,9 @@ UNION ALL
 SELECT 
     '3' AS version,
     'withdrawn' AS transaction_type,
-    reserve AS contract_address,
-    "user" AS borrower,
-    "to" AS "to",
+    reserve AS token,
+    "user" AS depositor,
+    "to" AS withdrawn_to,
     NULL::bytea AS liquidator,
     - amount AS amount,
     evt_tx_hash,
@@ -132,9 +132,9 @@ UNION ALL
 SELECT 
     '3' AS version,
     'deposit_liquidation' AS transaction_type,
-    "collateralAsset" AS contract_address,
-    "user" AS borrower,
-    liquidator AS "to",
+    "collateralAsset" AS token,
+    "user" AS depositor,
+    liquidator AS withdrawn_to,
     liquidator AS liquidator,
     - "liquidatedCollateralAmount" AS amount,
     evt_tx_hash,
@@ -144,10 +144,10 @@ SELECT
 FROM aave_v3."Pool_evt_LiquidationCall"
 ) deposit
 LEFT JOIN erc20."tokens" erc20
-    ON deposit.contract_address = erc20.contract_address
+    ON deposit.token = erc20.contract_address
 LEFT JOIN prices.usd p 
     ON p.minute = date_trunc('minute', deposit.evt_block_time) 
-    AND p.contract_address = deposit.contract_address
+    AND p.contract_address = deposit.token
 WHERE deposit.evt_block_time >= start_ts
 AND deposit.evt_block_time < end_ts
 AND deposit.evt_block_number >= start_block
