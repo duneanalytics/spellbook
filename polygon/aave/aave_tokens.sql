65a83a1e2095c818ae5ad022af0595bb49c989a3
jeff-dude <jeff@dune.xyz>
2022-06-13 15:32:11 -0400

    fix column data type, fix copy command, fix typo in index column

--- polygon/aave/aave_tokens.sql
+++ polygon/aave/aave_tokens.sql
@@ -4,14 +4,14 @@ CREATE TABLE IF NOT EXISTS aave.aave_tokens (
 	decimals int4,
   	underlying_token_address bytea,
   	underlying_token_symbol text,
-  	underlying_token_decimals int 4
+  	underlying_token_decimals int4
 );
 
 BEGIN;
 DELETE FROM aave.aave_tokens *;
 
 
-COPY aave.aave_tokens (address, symbol, decimals,erc20address,erc20symbol,erc20decimals) FROM stdin;
+COPY aave.aave_tokens (token_address, symbol, decimals,underlying_token_address,underlying_token_symbol,underlying_token_decimals) FROM stdin;
 \\x1d2a0e5ec8e5bbdca5cb219e649b565d8e5c3360	amAAVE	18	\\xd6df932a45c0f255f85145f286ea0b292b21c90b	AAVE	18
 \\x27f8d03b3a2196956ed754badc28d73be8830a6e	amDAI	18	\\x8f3cf7ad23cd3cadbd9735aff958023239c6a063	DAI	18
 \\x1a13f4ca1d028320a707d99520abfefca3998b7f	amUSDC	6	\\x2791bca1f2de4661ed88a30c99a7a9449aa84174	USDC	6
@@ -27,4 +27,4 @@ COMMIT;
 CREATE INDEX IF NOT EXISTS llama_aave_tokens_address_decimals_idx ON aave.aave_tokens USING btree (token_address) INCLUDE (decimals);
 CREATE INDEX IF NOT EXISTS llama_aave_tokens_symbol_decimals_idx ON aave.aave_tokens USING btree (symbol) INCLUDE (decimals);
 CREATE INDEX IF NOT EXISTS llama_aave_tokens_erc20address_decimals_idx ON aave.aave_tokens USING btree (underlying_token_address) INCLUDE (underlying_token_decimals);
-CREATE INDEX IF NOT EXISTS llama_aave_tokens_erc20symbol_decimals_idx ON aave.aave_tokens USING btree (underlying_token_symbol) INCLUDE (eunderlying_token_decimals);
+CREATE INDEX IF NOT EXISTS llama_aave_tokens_erc20symbol_decimals_idx ON aave.aave_tokens USING btree (underlying_token_symbol) INCLUDE (underlying_token_decimals);
