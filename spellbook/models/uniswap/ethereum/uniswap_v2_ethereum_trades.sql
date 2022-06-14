5c350911d493c6b80e8019f5c1bb496ee0d706b6
soispoke <66172107+soispoke@users.noreply.github.com>
2022-06-14 20:17:28 +0200

    incremental tables to views finalize transition to new materialization (#1163)

--- spellbook/models/uniswap/ethereum/uniswap_v2_ethereum_trades.sql
+++ spellbook/models/uniswap/ethereum/uniswap_v2_ethereum_trades.sql
@@ -1,11 +1,7 @@
  {{
   config(
         schema = 'uniswap_v2_ethereum', 
-        alias='trades',
-        materialized ='incremental',
-        file_format ='delta',
-        incremental_strategy='merge',
-        unique_key='unique_id'
+        alias='trades'
   )
 }}
 
@@ -64,7 +60,3 @@ LEFT JOIN {{ source('prices', 'usd') }} pa ON pa.minute = date_trunc('minute', d
 LEFT JOIN {{ source('prices', 'usd') }} pb ON pb.minute = date_trunc('minute', dex.block_time)
     AND pb.contract_address = dex.token_b_address
     AND pb.blockchain = 'ethereum'
-{% if is_incremental() %}
--- this filter will only be applied on an incremental run
-WHERE dex.block_time > now() - interval 2 days
-{% endif %} 
