1c74d3900cef433169a0a56ad6ac60e2db915406
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-06-14 11:05:08 -0700

    Add Llama Aave Treasury Tables - Polygon (#701)
    * Create aave_tokens.sql
    
    * Create aave_fees_by_day.sql
    
    * Create insert_aave_fees_by_day.sql
    
    No V1 fees
    
    * Create aave_daily_treasury_events.sql
    
    * Create insert_aave_daily_treasury_events.sql
    
    * Update insert_aave_daily_treasury_events.sql
    
    * Create aave_daily_interest_rates.sql
    
    * Update aave_daily_interest_rates.sql
    
    * Create llama_aave_daily_atoken_balances.sql
    
    * Update llama_aave_daily_atoken_balances.sql
    
    * Create insert_aave_daily_atoken_balances.sql
    
    * Rename llama_aave_daily_atoken_balances.sql to aave_daily_atoken_balances.sql
    
    * Update insert_aave_daily_atoken_balances.sql
    
    * Create insert_aave_daily_interest_rates.sql
    
    * Update insert_aave_daily_interest_rates.sql
    
    * Update insert_aave_daily_atoken_balances.sql
    
    * Update insert_aave_daily_interest_rates.sql
    
    * Create readme.md
    
    * Update insert_aave_fees_by_day.sql
    
    * Rename insert_aave_fees_by_day.sql to insert_aave_treasury_fees_by_day.sql
    
    * Update aave_daily_atoken_balances.sql
    
    * Update aave_daily_interest_rates.sql
    
    * Update aave_daily_treasury_events.sql
    
    * Update aave_fees_by_day.sql
    
    * Update and rename aave_fees_by_day.sql to aave_treasury_fees_by_day.sql
    
    * Update insert_aave_daily_atoken_balances.sql
    
    * Update insert_aave_daily_interest_rates.sql
    
    * Update insert_aave_daily_treasury_events.sql
    
    * Update and rename insert_aave_treasury_fees_by_day.sql to insert_aave_daily_treasury_feesy.sql
    
    * Rename insert_aave_daily_treasury_feesy.sql to insert_aave_daily_treasury_fees.sql
    
    * Update aave_tokens.sql
    
    * Move to aave schema
    
    * Move tokens and rates to top level
    
    * rename folder
    
    * Create aave_daily_liquidity_mining_rates.sql
    
    * Create insert_aave_daily_liquidity_mining_rates.sql
    
    * Update insert_aave_daily_liquidity_mining_rates.sql
    
    * Update insert_aave_daily_treasury_events.sql
    
    * Update aave_tokens.sql
    
    * Update insert_aave_daily_liquidity_mining_rates.sql
    
    * Update insert_aave_daily_liquidity_mining_rates.sql
    
    * Update insert_aave_daily_treasury_events.sql
    
    * Update insert_aave_daily_treasury_fees.sql
    
    * Update insert_aave_daily_atoken_balances.sql
    
    * Update insert_aave_daily_interest_rates.sql
    
    * Update insert_aave_daily_treasury_events.sql
    
    * Update insert_aave_daily_atoken_balances.sql
    
    update date_trunc to day
    
    * Update aave_tokens.sql
    
    address to token_address
    
    * Update insert_aave_daily_atoken_balances.sql
    
    address to token_address
    
    * Update aave_tokens.sql
    
    llama to aave naming
    
    * llama to aave naming
    
    llama to aave naming
    
    * fix column data type, fix copy command, fix typo in index column
    
    * update backfill, update cron command
    
    * update backfill, update cron command
    
    * fix address column, add start_time variable, fix cron job
    
    * remove extra backfill command
    
    * add file to create llama schema and address table on polygon
    
    * fix fees table name, fix cron command, update backfill command
    
    * update fees name, update backfill logic, fix cron command
    
    Co-authored-by: jeff-dude <jeff@dune.xyz>
    Co-authored-by: j__dude <102681548+jeff-dude@users.noreply.github.com>

--- polygon/llama/llama_treasury_addresses.sql
+++ polygon/llama/llama_treasury_addresses.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS llama.llama_treasury_addresses (
 	address bytea UNIQUE,
 	version text,
 	blockchain text,
-	tags integer
+	tags text
 );
 
 BEGIN;
@@ -13,11 +13,10 @@ DELETE FROM llama.llama_treasury_addresses *;
 
 
 COPY llama.llama_treasury_addresses (protocol,address,version,blockchain,tags) FROM stdin;
-\\
-Aave	\\xe3d9988f676457123c5fd01297605efdd0cba1ae	V1	Ethereum	NULL --Aave V1 revenue
-Aave	\\x464c71f6c2f760dda6093dcb91c24c39e5d6e18c	V2	Ethereum	NULL --Aave V2 revenue
-Aave	\\x25F2226B597E8F9514B3F68F00f494cF4f286491	Ecosystem Reserve	Ethereum	Treasury --Aave Treasury
-Aave	\\x7734280A4337F37Fbf4651073Db7c28C80B339e9	MATIC	Polygon	NULL --Aave Polygon revenue
+Aave	\\xe3d9988f676457123c5fd01297605efdd0cba1ae	V1	Ethereum	\N
+Aave	\\x464c71f6c2f760dda6093dcb91c24c39e5d6e18c	V2	Ethereum	\N
+Aave	\\x25F2226B597E8F9514B3F68F00f494cF4f286491	'Ecosystem Reserve'	Ethereum	Treasury
+Aave	\\x7734280A4337F37Fbf4651073Db7c28C80B339e9	MATIC	Polygon	\N
 \.
 
 
@@ -26,4 +25,3 @@ COMMIT;
 CREATE INDEX IF NOT EXISTS llama_treasury_addresses_address_idx ON llama.llama_treasury_addresses (address);
 CREATE INDEX IF NOT EXISTS llama_treasury_addresses_address_protocol_idx ON llama.llama_treasury_addresses (address,protocol);
 CREATE INDEX IF NOT EXISTS llama_treasury_addresses_address_protocol_version_idx ON llama.llama_treasury_addresses (address,protocol,version);
-
