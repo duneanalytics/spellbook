1565c2bf4dedfddd7f89da14f267dd9bef7d9e5c
soispoke <66172107+soispoke@users.noreply.github.com>
2022-06-16 17:44:04 +0200

    add categories to erc721 balances (#1178)
    After having integrated the category column (PR by Hildobby), I think it makes sense to include it in erc721 balances 
    
    I've checked that:
    
    * [x] I tested the query on dune.com after compiling the model with dbt compile (compiled queries are written to the target directory)
    * [ ] I used "refs" to reference other models in this repo and "sources" to reference raw or decoded tables 
    * [ ] the directory tree matches the pattern /sector/blockchain/ e.g. /tokens/ethereum
    * [ ] if adding a new model, I added a test
    * [x] the filename is unique and ends with .sql
    * [x] each file has only one view, table or function defined  
    * [x] column names are `lowercase_snake_cased`

--- spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_latest.sql
+++ spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_latest.sql
@@ -7,6 +7,7 @@ SELECT
     token_address,
     tokenId,
     nft_tokens.name as collection,
+    nft_tokens.category as category,
     updated_at
 FROM {{ ref('transfers_ethereum_erc721_rolling_hour') }}
 LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft_tokens ON nft_tokens.contract_address = token_address
