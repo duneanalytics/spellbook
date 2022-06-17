d941fd34f96c07ed2803c3a32a86dfcd5998f0a9
soispoke <66172107+soispoke@users.noreply.github.com>
2022-06-17 13:14:14 +0200

    add transfers and balances for erc1155 tokens on ethereum

---
+++ spellbook/models/transfers/ethereum/erc1155/transfers_ethereum_erc1155_rolling_hour.sql
@@ -0,0 +1,18 @@
+{{ config(
+        alias ='erc1155_rolling_hour',
+        materialized ='view'
+        )
+}}
+
+        select
+            'ethereum' as blockchain,
+            hour,
+            wallet_address,
+            token_address,
+            tokenId,
+            current_timestamp() as updated_at,
+            row_number() over (partition by token_address, tokenId, wallet_address order by hour desc) as recency_index,
+            sum(amount) over (
+                partition by token_address, wallet_address order by hour
+            ) as amount
+        from {{ ref('transfers_ethereum_erc1155_agg_hour') }}
