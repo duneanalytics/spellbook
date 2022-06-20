ef74ed5e37c72b3dfe1eda7a075f45cac63259da
soispoke <66172107+soispoke@users.noreply.github.com>
2022-06-20 13:18:32 +0200

    Update magiceden_trades.sql
    Fix typo for magic eden materialization

--- spellbook/models/magiceden/magiceden_trades.sql
+++ spellbook/models/magiceden/magiceden_trades.sql
@@ -1,12 +1,12 @@
- {{
-  config(
-        alias='trades', materialize = 'incremental')
+{{ config(
+        alias='trades',
+        materialized ='incremental')
 }}
 
 SELECT blockchain, 'magiceden' as project, '' as version, tx_hash, block_time, amount_usd, amount, token_symbol, token_address, unique_trade_id 
 FROM 
 (
-SELECT blockchain, tx_hash, block_time, amount_usd, amount, token_symbol, token_address, unique_trade_id
+SELECT *
 FROM {{ ref('magiceden_solana_trades') }}
 ) 
 {% if is_incremental() %}
