dd9e8287af581e176949326d42a7978214f80f60
soispoke <66172107+soispoke@users.noreply.github.com>
2022-06-20 16:42:02 +0200

    set metadata to expose seaport view transactions in dune.com

--- spellbook/macros/alter_table_properties.sql
+++ spellbook/macros/alter_table_properties.sql
@@ -145,6 +145,15 @@ ALTER TABLE uniswap_ethereum.trades SET TBLPROPERTIES('dune.public'='true',
                                                     'dune.data_explorer.contributors'='["soispoke"]');
 {% endset %}
 
+{% set seaport_ethereum_view_transactions %}
+ALTER TABLE seaport_ethereum.view_transactions SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["ethereum"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='project',
+                                                    'dune.data_explorer.abstraction.name'='seaport',
+                                                    'dune.data_explorer.contributors'='["sohawk","soispoke"]');
+{% endset %}
+
 {% do run_query(balances_ethereum_erc20_day) %}
 {% do run_query(balances_ethereum_erc20_hour) %}
 {% do run_query(balances_ethereum_erc20_latest) %}
