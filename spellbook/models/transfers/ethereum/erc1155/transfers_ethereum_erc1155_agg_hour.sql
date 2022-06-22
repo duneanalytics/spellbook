6d376c5e126f45296c41cecc5a75183cd1208490
soispoke <66172107+soispoke@users.noreply.github.com>
2022-06-22 10:54:29 +0200

    fix incremental strategy from append to merge into (#1204)

--- spellbook/models/transfers/ethereum/erc1155/transfers_ethereum_erc1155_agg_hour.sql
+++ spellbook/models/transfers/ethereum/erc1155/transfers_ethereum_erc1155_agg_hour.sql
@@ -1,7 +1,9 @@
 {{ config(
         alias ='erc1155_agg_hour',
         materialized ='incremental',
-        file_format ='delta'
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key='unique_transfer_id'
         )
 }}
 
