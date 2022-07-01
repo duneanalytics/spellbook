2c7359051b51eca585ea97ec8c1f155f757b1c73
Meghan Heintz <msheintz@gmail.com>
2022-07-01 10:54:02 -0400

    erc721 category causing dupes (#1231)
    * category causing dupes

--- spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_day.sql
+++ spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_day.sql
@@ -22,14 +22,13 @@ with
     lead(day, 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY day) AS next_day
     FROM {{ ref('transfers_ethereum_erc721_rolling_day') }})
 
-SELECT
+SELECT distinct
     'ethereum' as blockchain,
     d.day,
     b.wallet_address,
     b.token_address,
     b.tokenId,
-    nft_tokens.name as collection,
-    nft_tokens.category as category
+    nft_tokens.name as collection
 FROM daily_balances b
 INNER JOIN days d ON b.day <= d.day AND d.day < b.next_day
 LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft_tokens ON nft_tokens.contract_address = b.token_address
--- spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_hour.sql
+++ spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_hour.sql
@@ -22,14 +22,13 @@ with
     lead(hour, 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY hour) AS next_hour
     FROM {{ ref('transfers_ethereum_erc721_rolling_hour') }})
 
-SELECT
+SELECT distinct
     'ethereum' as blockchain,
     d.hour,
     b.wallet_address,
     b.token_address,
     b.tokenId,
-    nft_tokens.name as collection,
-    nft_tokens.category as category
+    nft_tokens.name as collection
 FROM daily_balances b
 INNER JOIN hours d ON b.hour <= d.hour AND d.hour < b.next_hour
 LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft_tokens ON nft_tokens.contract_address = b.token_address
