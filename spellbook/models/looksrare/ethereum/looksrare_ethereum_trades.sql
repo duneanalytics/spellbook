5c8aefd2efa4d7bd6b12d2effcfdf1ec61a0bc81
soispoke <66172107+soispoke@users.noreply.github.com>
2022-07-04 16:43:53 +0200

    Create burns/mints/fees/trades

---
+++ spellbook/models/looksrare/ethereum/looksrare_ethereum_trades.sql
@@ -0,0 +1,34 @@
+ {{
+  config(
+        alias='trades')
+}}
+
+SELECT blockchain,
+project,
+version,
+block_time,
+token_id,
+collection,
+amount_usd,
+token_standard,a
+trade_type,
+number_of_items,
+trade_category,
+evt_type,
+seller,
+buyer,
+amount_original,
+amount_raw,
+currency_symbol,
+currency_contract,
+currency_contract_original,
+nft_contract_address,
+project_contract_address,
+aggregator_name,
+aggregator_address,
+tx_hash,
+tx_from,
+tx_to,
+unique_trade_id
+FROM ({{ ref('looksrare_ethereum_transactions') }})
+WHERE evt_type = 'Trade'
