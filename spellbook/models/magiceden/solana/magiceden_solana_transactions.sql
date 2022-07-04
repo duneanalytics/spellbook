94db932fa930eb6a2c63d632c8e5f3644f8406da
soispoke <66172107+soispoke@users.noreply.github.com>
2022-07-04 10:38:51 +0200

    magiceden update

--- spellbook/models/magiceden/solana/magiceden_solana_transactions.sql
+++ spellbook/models/magiceden/solana/magiceden_solana_transactions.sql
@@ -7,11 +7,41 @@
         )
 }}
 
-SELECT * FROM {{ ref('magiceden_v1_solana_transactions') }}
-                        UNION
-SELECT * FROM {{ ref('magiceden_v2_solana_transactions') }}
-
-{% if is_incremental() %}
--- this filter will only be applied on an incremental run
-WHERE block_time > now() - interval 2 days
-{% endif %} 
+SELECT 
+  'solana' as blockchain,
+  'magiceden' as project,
+  CASE WHEN (array_contains(account_keys, 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8')) THEN 'v1'
+  WHEN (array_contains(account_keys, 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K')) THEN 'v2' 
+  WHEN (array_contains(account_keys, 'CMX5tvuWs2rBUL3vqVWiARfcDoCKjdeSinCsZdxJmYoF')) THEN 'launchpad_v1'
+  WHEN (array_contains(account_keys, 'CMY8R8yghKfFnHKCWjzrArUpYH4PbJ56aWBr4kCP4DMk')) THEN 'launchpad_v2'
+  WHEN (array_contains(account_keys, 'CMZYPASGWeTz7RNGHaRJfCq2XQ5pYK6nDvVQxzkH51zb')) THEN 'launchpad_v3'
+  END as version,
+  signatures[0] as tx_hash, 
+  block_time,
+  abs(post_balances[0] / 1e9 - pre_balances[0] / 1e9) * p.price AS amount_usd,
+  abs(post_balances[0] / 1e9 - pre_balances[0] / 1e9) AS amount_original,
+  abs(post_balances[0] - pre_balances[0])::string AS amount_raw,
+  p.symbol as currency_symbol,
+  p.contract_address as currency_contract,
+  CASE WHEN (array_contains(account_keys, 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8')) THEN 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8'
+  WHEN (array_contains(account_keys, 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K')) THEN 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K' 
+  END as project_contract_address,
+  CASE WHEN (array_contains(account_keys, 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8')) 
+  OR (array_contains(account_keys, 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K')) 
+  AND ARRAY_CONTAINS(log_messages, 'Program log: Instruction: ExecuteSale') THEN 'Trade' 
+  WHEN (array_contains(account_keys, 'CMX5tvuWs2rBUL3vqVWiARfcDoCKjdeSinCsZdxJmYoF'))
+  OR (array_contains(account_keys, 'CMY8R8yghKfFnHKCWjzrArUpYH4PbJ56aWBr4kCP4DMk'))
+  OR (array_contains(account_keys, 'CMZYPASGWeTz7RNGHaRJfCq2XQ5pYK6nDvVQxzkH51zb'))
+  AND ARRAY_CONTAINS(log_messages, 'Program log: Instruction: MintNft') THEN 'Mint'
+  ELSE 'Transaction' END as evt_type,
+  signatures[0] || '-' || id as unique_trade_id
+FROM {{ source('solana','transactions') }}
+LEFT JOIN {{ source('prices', 'usd') }} p 
+  ON p.minute = date_trunc('minute', block_time)
+  AND p.symbol = 'SOL'        
+WHERE (array_contains(account_keys, 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8'))  -- magic eden v2
+OR (array_contains(account_keys, 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K'))
+OR (array_contains(account_keys, 'CMX5tvuWs2rBUL3vqVWiARfcDoCKjdeSinCsZdxJmYoF'))
+OR (array_contains(account_keys, 'CMY8R8yghKfFnHKCWjzrArUpYH4PbJ56aWBr4kCP4DMk'))
+OR (array_contains(account_keys, 'CMZYPASGWeTz7RNGHaRJfCq2XQ5pYK6nDvVQxzkH51zb'))
+AND block_date > '2022-01-07'
