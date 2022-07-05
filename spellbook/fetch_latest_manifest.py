286a6e986e9ae3d84bca9cc7af58e6f9677209f6
dot2dotseurat <msheintz@gmail.com>
2022-07-05 13:19:38 -0400

    fetch from file

--- spellbook/fetch_latest_manifest.py
+++ spellbook/fetch_latest_manifest.py
@@ -1,3 +1,42 @@
+import argparse
+import logging
+import shutil
+
 import requests as re
 
-print(1)
+
+def fetch_run_ids(token, limit=50):
+    headers = {'Content-Type': 'application/json',
+               'Authorization': f'Token {token}'}
+    response = re.get(f"https://cloud.getdbt.com/api/v2/accounts/58579/runs/?job_definition_id=81672&order_by=-finished_at&limit={limit}", headers=headers)
+    run_ids = [elem['id'] for elem in response.json()['data']]
+    return run_ids
+
+def loop_till_success(token, run_ids):
+    headers = {'Content-Type': 'application/json',
+               'Authorization': f'Token {token}'}
+    success = False
+    while success == False:
+        for run_id in run_ids:
+            print(run_id)
+            response = re.get(f"https://cloud.getdbt.com/api/v2/accounts/58579/runs/{run_id}/artifacts/manifest.json", headers=headers)
+            if response.status_code == 200:
+                success = True
+                with open('manifest.json', 'w') as out_file:
+                    out_file.write(response.text)
+                del response
+    return run_id
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dbt_api_token', type=str)
+    args = parser.parse_args()
+
+    token = args.dbt_api_token
+    run_ids = fetch_run_ids(token=token)
+    loop_till_success(token, run_ids)
+
+if __name__ == "__main__":
+    main()
