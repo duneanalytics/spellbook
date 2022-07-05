4324279d28cb4edac97ae518b35ed6b5f04a31be
dot2dotseurat <msheintz@gmail.com>
2022-07-05 14:20:41 -0400

    filter by success

--- spellbook/fetch_latest_manifest.py
+++ spellbook/fetch_latest_manifest.py
@@ -3,26 +3,23 @@ import argparse
 import requests as re
 
 
-def fetch_run_ids(token, limit=50):
+def fetch_run_id(token, limit=50):
     headers = {'Content-Type': 'application/json',
                'Authorization': f'Token {token}'}
     response = re.get(f"https://cloud.getdbt.com/api/v2/accounts/58579/runs/?job_definition_id=81672&order_by=-finished_at&limit={limit}", headers=headers)
-    run_ids = [elem['id'] for elem in response.json()['data']]
-    return run_ids
+    last_successful_run_id = [elem['id'] for elem in response.json()['data'] if elem['status_humanized'] == 'Success'][0]
+    return last_successful_run_id
 
-def loop_till_success(token, run_ids):
+def fetch_manifest(token, run_id):
     headers = {'Content-Type': 'application/json',
                'Authorization': f'Token {token}'}
-    success = False
-    while success == False:
-        for run_id in run_ids:
-            response = re.get(f"https://cloud.getdbt.com/api/v2/accounts/58579/runs/{run_id}/artifacts/manifest.json", headers=headers)
-            if response.status_code == 200:
-                success = True
-                with open('manifest.json', 'w') as out_file:
-                    out_file.write(response.text)
-                del response
-    return run_id
+    response = re.get(f"https://cloud.getdbt.com/api/v2/accounts/58579/runs/{run_id}/artifacts/manifest.json", headers=headers)
+    if response.status_code == 200:
+        with open('manifest.json', 'w') as out_file:
+            out_file.write(response.text)
+        del response
+    else:
+        print(response.status_code)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -30,8 +27,8 @@ def main():
     args = parser.parse_args()
 
     token = args.dbt_api_token
-    run_ids = fetch_run_ids(token=token)
-    loop_till_success(token, run_ids)
+    run_id = fetch_run_id(token=token)
+    fetch_manifest(token, run_id)
 
 if __name__ == "__main__":
     main()
