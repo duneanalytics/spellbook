69cf1f302a8fddafa09699cb2148691bd4c625b5
dot2dotseurat <msheintz@gmail.com>
2022-07-06 11:31:47 -0400

    using variables

--- spellbook/models/magiceden/solana/magiceden_solana_transactions.sql
+++ spellbook/models/magiceden/solana/magiceden_solana_transactions.sql
@@ -7,14 +7,20 @@
         )
 }}
 
+{% set v1_key = "MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8" %}
+{% set v2_key = "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K" %}
+{% set launchpad_v1_key = "CMX5tvuWs2rBUL3vqVWiARfcDoCKjdeSinCsZdxJmYoF" %}
+{% set launchpad_v2_key = "CMY8R8yghKfFnHKCWjzrArUpYH4PbJ56aWBr4kCP4DMk" %}
+{% set launchpad_v3_key = "CMZYPASGWeTz7RNGHaRJfCq2XQ5pYK6nDvVQxzkH51zb" %}
+
 SELECT 
   'solana' as blockchain,
   'magiceden' as project,
-  CASE WHEN (array_contains(account_keys, 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8')) THEN 'v1'
-  WHEN (array_contains(account_keys, 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K')) THEN 'v2' 
-  WHEN (array_contains(account_keys, 'CMX5tvuWs2rBUL3vqVWiARfcDoCKjdeSinCsZdxJmYoF')) THEN 'launchpad_v1'
-  WHEN (array_contains(account_keys, 'CMY8R8yghKfFnHKCWjzrArUpYH4PbJ56aWBr4kCP4DMk')) THEN 'launchpad_v2'
-  WHEN (array_contains(account_keys, 'CMZYPASGWeTz7RNGHaRJfCq2XQ5pYK6nDvVQxzkH51zb')) THEN 'launchpad_v3'
+  CASE WHEN (array_contains(account_keys, '{{v1_key}}')) THEN 'v1'
+  WHEN (array_contains(account_keys, '{{v2_key}}')) THEN 'v2'
+  WHEN (array_contains(account_keys, '{{launchpad_v1_key}}')) THEN 'launchpad_v1'
+  WHEN (array_contains(account_keys, '{{launchpad_v2_key}}')) THEN 'launchpad_v2'
+  WHEN (array_contains(account_keys, '{{launchpad_v3_key}}')) THEN 'launchpad_v3'
   END as version,
   signatures[0] as tx_hash, 
   block_time,
@@ -24,14 +30,14 @@ SELECT
   p.symbol as currency_symbol,
   p.contract_address as currency_contract,
   'metaplex' as token_standard,
-  CASE WHEN (array_contains(account_keys, 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8')) THEN 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8'
-  WHEN (array_contains(account_keys, 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K')) THEN 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K' 
+  CASE WHEN (array_contains(account_keys, '{{v1_key}}')) THEN '{{v1_key}}'
+  WHEN (array_contains(account_keys, '{{v2_key}}')) THEN '{{v2_key}}'
   END as project_contract_address,
-  CASE WHEN (array_contains(account_keys, 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8')) 
-  OR (array_contains(account_keys, 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K')) THEN 'Trade' 
-  WHEN (array_contains(account_keys, 'CMX5tvuWs2rBUL3vqVWiARfcDoCKjdeSinCsZdxJmYoF'))
-  OR (array_contains(account_keys, 'CMY8R8yghKfFnHKCWjzrArUpYH4PbJ56aWBr4kCP4DMk'))
-  OR (array_contains(account_keys, 'CMZYPASGWeTz7RNGHaRJfCq2XQ5pYK6nDvVQxzkH51zb')) THEN 'Mint'
+  CASE WHEN (array_contains(account_keys, '{{v1_key}}'))
+  OR (array_contains(account_keys, '{{v2_key}}')) THEN 'Trade'
+  WHEN (array_contains(account_keys, '{{launchpad_v1_key}}'))
+  OR (array_contains(account_keys, '{{launchpad_v2_key}}'))
+  OR (array_contains(account_keys, '{{launchpad_v3_key}}')) THEN 'Mint'
   ELSE 'Transaction' END as evt_type,
   signatures[0] || '-' || id || '-' || instructions[0]::string as unique_trade_id
 FROM {{ source('solana','transactions') }}
@@ -39,11 +45,11 @@ LEFT JOIN {{ source('prices', 'usd') }} p
   ON p.minute = date_trunc('minute', block_time)
   AND p.symbol = 'SOL'        
 WHERE (
-array_contains(account_keys, 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8')  -- magic eden v2
-OR array_contains(account_keys, 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K')
-OR array_contains(account_keys, 'CMX5tvuWs2rBUL3vqVWiARfcDoCKjdeSinCsZdxJmYoF')
-OR array_contains(account_keys, 'CMY8R8yghKfFnHKCWjzrArUpYH4PbJ56aWBr4kCP4DMk')
-OR array_contains(account_keys, 'CMZYPASGWeTz7RNGHaRJfCq2XQ5pYK6nDvVQxzkH51zb'))
+array_contains(account_keys, '{{v1_key}}')  -- magic eden v2
+OR array_contains(account_keys, '{{v2_key}}')
+OR array_contains(account_keys, '{{launchpad_v1_key}}')
+OR array_contains(account_keys, '{{launchpad_v2_key}}')
+OR array_contains(account_keys, '{{launchpad_v3_key}}'))
 AND block_date > '2022-01-07'
 AND block_slot > 114980355
 
