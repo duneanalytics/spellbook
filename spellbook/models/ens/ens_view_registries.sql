54c107d615c2e7ed9f6a389eee03af162e9d030b
soispoke <66172107+soispoke@users.noreply.github.com>
2022-07-06 11:04:36 +0200

    alias to spell + add schema in dbt_project

--- spellbook/models/ens/ens_view_registries.sql
+++ spellbook/models/ens/ens_view_registries.sql
@@ -4,7 +4,7 @@ SELECT
   label,
   min(evt_block_time) AS min_evt_block_time, 
   max(evt_block_time) AS max_evt_block_time, 
-  count(*)  
+  count(*) as count_ens_registries
   FROM (
     SELECT * 
     FROM {{source('ethereumnameservice_ethereum', 'ENSRegistry_evt_NewOwner')}}
