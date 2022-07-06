d7833bffa428134e7a99183bfff21ef20746fbe9
soispoke <66172107+soispoke@users.noreply.github.com>
2022-07-06 14:54:29 +0200

    Fix test typos

--- spellbook/tests/ens/ens_view_expirations_test.sql
+++ spellbook/tests/ens/ens_view_expirations_test.sql
@@ -3,13 +3,13 @@
 -- Also manually check etherscan info for the first 5 rows
 WITH unit_tests AS (
     SELECT 
-        CASE WHEN test_data.label = ens_ve.label
-            AND test_data.min_expires = ens_ve.min_expires THEN TRUE 
+        CASE WHEN test_date.label = ens_ve.label
+            AND test_date.min_expires = ens_ve.min_expires THEN TRUE 
         ELSE False 
         END AS min_expiration_test
     FROM {{ ref('ens_view_expirations') }} AS ens_ve
     JOIN {{ ref('ens_view_expirations_postgres') }} AS test_date 
-        ON test_data.label = ens_ve.label
+        ON test_date.label = ens_ve.label
 )
 SELECT
     COUNT(*) AS count_rows,
