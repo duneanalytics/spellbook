71aa404194ab8d061cbc9071cca0b38851e765e5
soispoke <66172107+soispoke@users.noreply.github.com>
2022-07-06 13:46:24 +0200

    added version to unique_trade_id again and distinct to uniswap trades… (#1253)
    … to make sure we have no dupes
    
    Some duplicates were actually cause by txs like this one: https://etherscan.io/tx/0x956dd8bded921b16b93fd0a89d5ae1db53c5ed5fc06d1f548a8c438c17492338
    where both uniswap v3 and v2 contracts were used in the same tx
    
    Brief comments on the purpose of your changes:
    
    
    *For Dune Engine V2*
    I've checked that:
    
    * [ ] I tested the query on dune.com after compiling the model with dbt compile (compiled queries are written to the target directory)
    * [ ] I used "refs" to reference other models in this repo and "sources" to reference raw or decoded tables 
    * [ ] if adding a new model, I added a test
    * [ ] the filename is unique and ends with .sql
    * [ ] each sql file is a select statement and has only one view, table or function defined  
    * [ ] column names are `lowercase_snake_cased`
    
    When you are ready for a review, tag duneanalytics/data-experience. We will re-open your forked pull request as an internal pull request. Then your spells will run in dbt and the logs will be avaiable in Github Actions DBT Slim CI. This job will only run the models and tests changed by your PR compared to the production project.

--- spellbook/models/uniswap/ethereum/uniswap_ethereum_trades.sql
+++ spellbook/models/uniswap/ethereum/uniswap_ethereum_trades.sql
@@ -3,7 +3,7 @@
         alias='trades')
 }}
 
-SELECT blockchain, project, version, block_time, token_a_symbol, token_b_symbol, 
+SELECT DISTINCT blockchain, project, version, block_time, token_a_symbol, token_b_symbol, 
        token_a_amount, token_b_amount, trader_a, trader_b, usd_amount, token_a_address, 
        token_b_address, exchange_contract_address, tx_hash, tx_from, tx_to, unique_trade_id
 FROM (SELECT * FROM {{ ref('uniswap_v2_ethereum_trades') }} 
