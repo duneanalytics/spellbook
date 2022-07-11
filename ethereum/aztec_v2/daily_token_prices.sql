7a2b4d06f49292b876432f9b19f1810d0793b0c9
Jack Yin <jackyin0@gmail.com>
2022-07-11 17:27:06 -0400

    fix unique index bug

--- ethereum/aztec_v2/daily_token_prices.sql
+++ ethereum/aztec_v2/daily_token_prices.sql
@@ -9,6 +9,13 @@ CREATE TABLE IF NOT EXISTS aztec_v2.daily_token_prices (
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS aztec_v2_daily_token_prices_uniq_idx on aztec_v2.daily_token_prices (date, token_address);
-CREATE UNIQUE INDEX IF NOT EXISTS aztec_v2_daily_token_prices_address_idx on aztec_v2.daily_token_prices (token_address);
-CREATE UNIQUE INDEX IF NOT EXISTS aztec_v2_daily_token_prices_symbol_idx on aztec_v2.daily_token_prices (symbol);
-CREATE UNIQUE INDEX IF NOT EXISTS aztec_v2_daily_token_prices_date_idx on aztec_v2.daily_token_prices (date);
+CREATE INDEX IF NOT EXISTS aztec_v2_daily_token_prices_address_idx on aztec_v2.daily_token_prices (token_address);
+CREATE INDEX IF NOT EXISTS aztec_v2_daily_token_prices_symbol_idx on aztec_v2.daily_token_prices (symbol);
+CREATE INDEX IF NOT EXISTS aztec_v2_daily_token_prices_date_idx on aztec_v2.daily_token_prices (date);
+
+/*
+CREATE UNIQUE INDEX IF NOT EXISTS dug_aztec_v2_daily_token_prices_uniq_idx on dune_user_generated.aztec_v2_daily_token_prices (date, token_address);
+CREATE INDEX IF NOT EXISTS dug_aztec_v2_daily_token_prices_address_idx on dune_user_generated.aztec_v2_daily_token_prices (token_address);
+CREATE INDEX IF NOT EXISTS dug_aztec_v2_daily_token_prices_symbol_idx on dune_user_generated.aztec_v2_daily_token_prices (symbol);
+CREATE INDEX IF NOT EXISTS dug_aztec_v2_daily_token_prices_date_idx on dune_user_generated.aztec_v2_daily_token_prices (date);
+*/
