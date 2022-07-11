4d154565ef40df59b68e3da51d524e11619bd7b2
soispoke <66172107+soispoke@users.noreply.github.com>
2022-07-11 14:06:14 +0800

    Fix tests and improve run time

--- spellbook/tests/uniswap/ethereum/uniswap_v3_ethereum_assert_samples.sql
+++ spellbook/tests/uniswap/ethereum/uniswap_v3_ethereum_assert_samples.sql
@@ -6,8 +6,9 @@
 -- Also manually check etherscan info for the first 5 rows
 WITH unit_tests as
 (SELECT case when test_data.token_a_amount = us_trades.token_a_amount AND test_data.token_b_amount = us_trades.token_b_amount then True else False end as price_test
-FROM {{ ref('uniswap_v3_ethereum_trades') }} us_trades
+FROM {{ ref('uniswap_trades') }} us_trades
 JOIN {{ ref('uniswap_v3_ethereum_trades_postgres') }} test_data ON test_data.tx_hash = us_trades.tx_hash
+WHERE us_trades.blockchain = 'ethereum' and us_trades.version = 'v3'
 )
 select count(case when price_test = false then 1 else null end)/count(*) as pct_mismatch, count(*) as count_rows
 from unit_tests
