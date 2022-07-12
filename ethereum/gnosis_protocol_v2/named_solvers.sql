0a58be898df728b67f651517d28ac089e1a8dae9
jeff-dude <jeff@dune.xyz>
2022-07-12 15:21:57 -0400

    attempt another copy command format

--- ethereum/gnosis_protocol_v2/named_solvers.sql
+++ ethereum/gnosis_protocol_v2/named_solvers.sql
@@ -5,10 +5,12 @@ CREATE TABLE IF NOT EXISTS gnosis_protocol_v2.named_solvers
     name        text
 );
 
-COPY gnosis_protocol_v2.named_solvers (address, environment, name)
-    FROM '/ethereum/gnosis_protocol_v2/solver_names.csv'
-    DELIMITER ','
-    CSV HEADER;
+-- COPY gnosis_protocol_v2.named_solvers (address, environment, name)
+--     FROM 'https://raw.githubusercontent.com/duneanalytics/abstractions/a68ea6d2c3045d02c29c974556d0d230533a1263/ethereum/gnosis_protocol_v2/solvers.csv'
+--     DELIMITER ','
+--     CSV HEADER;
+
+\copy gnosis_protocol_v2.named_solvers FROM 'https://raw.githubusercontent.com/duneanalytics/abstractions/a68ea6d2c3045d02c29c974556d0d230533a1263/ethereum/gnosis_protocol_v2/solvers.csv' with (format csv,header true, delimiter ',');
 
 CREATE UNIQUE INDEX IF NOT EXISTS solver_id ON gnosis_protocol_v2.named_solvers (address);
 
