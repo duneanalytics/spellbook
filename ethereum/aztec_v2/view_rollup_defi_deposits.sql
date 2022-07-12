9cb905c5ffdfc2e7f998f4e263ee72f6b254c239
Jack Yin <jackyin0@gmail.com>
2022-07-12 16:02:20 -0400

    added asset address to view

--- ethereum/aztec_v2/view_rollup_defi_deposits.sql
+++ ethereum/aztec_v2/view_rollup_defi_deposits.sql
@@ -12,7 +12,10 @@ select b.*
     , case when b.secondinputinuse then ib.symbol else null end as input_b_symbol
     , case when b.secondoutputinuse then ob.symbol else null end as output_b_symbol
     , defi_deposit_sum * 1.0 / 10 ^ (ia.decimals) as defi_deposit_sum_norm -- defi deposit sum is denominated in input asset a
-    
+    , ia.asset_address as input_a_address
+    , oa.asset_address as output_a_address
+    , case when b.secondinputinuse then ib.asset_address else null end as input_b_address
+    , case when b.secondoutputinuse then ob.asset_address else null end as output_b_address
 from bridge_rollups b
 left join aztec_v2.view_deposit_assets ia on ia.asset_id = b.inputassetida
 left join aztec_v2.view_deposit_assets oa on oa.asset_id = b.outputassetida
