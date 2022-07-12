c5f813b4bc0cc456c5514ec3a51846f0cd395111
jackiep00 <jackyin0@gmail.com>
2022-07-12 12:03:28 -0400

    [Aztec Connect] Basic aztec connect parsing and views (#1275)
    * Basic aztec connect parsing and views
    
    * name indexes, fix update statement
    
    * remove select statement, reference to dune user generated
    
    * comment out test call
    
    * fix unique index bug
    
    * add drop table command to rebuild table as needed
    
    * fix error on parantheses in function creation
    
    * replace reference to dune_user_generated
    
    * remove unique in index creation
    
    * add ; to end command
    
    * remove reference to dune_user_generated, add ; to end command
    
    * add ; to end command
    
    * add ; to end command
    
    * add ; to end command
    
    * remove reference to dune_user_generated, add ; to end command
    
    * add ; to end command
    
    Co-authored-by: jeff-dude <jeff@dune.xyz>

---
+++ ethereum/aztec_v2/view_rollup_inner_proofs.sql
@@ -0,0 +1,17 @@
+-- https://dune.com/queries/1007310
+
+create or replace view aztec_v2.rollup_inner_proofs as 
+with inner_proofs as (
+    select rollupid
+        , call_block_time
+        , (unnest(innerproofs)).*
+    from aztec_v2.rollups_parsed
+)
+select i.*
+    , substring(publicowner from 13 for 20) as publicowner_norm -- publicowner is 32 bytes long, with 12 bytes of padding over the 20 bytes of ethereum address
+    , a.symbol
+    , a.decimals
+    , i.publicvalue * 1.0 / 10 ^ (coalesce(a.decimals, 18)) as publicvalue_norm
+from inner_proofs i
+left join aztec_v2.view_deposit_assets a on i.assetid = a.asset_id
+;
