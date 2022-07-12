7ba4ed840c454b132b62ac5416e2f41f5ec9dc2e
Jack Yin <jackyin0@gmail.com>
2022-07-12 16:02:46 -0400

    renamed view to adhere to convention and added asset address

--- ethereum/aztec_v2/view_rollup_inner_proofs.sql
+++ ethereum/aztec_v2/view_rollup_inner_proofs.sql
@@ -1,6 +1,8 @@
 -- https://dune.com/queries/1007310
 
-create or replace view aztec_v2.rollup_inner_proofs as 
+-- drop view aztec_v2.rollup_inner_proofs
+
+create or replace view aztec_v2.view_rollup_inner_proofs as 
 with inner_proofs as (
     select rollupid
         , call_block_time
@@ -10,6 +12,7 @@ with inner_proofs as (
 select i.*
     , substring(publicowner from 13 for 20) as publicowner_norm -- publicowner is 32 bytes long, with 12 bytes of padding over the 20 bytes of ethereum address
     , a.symbol
+    , a.asset_address
     , a.decimals
     , i.publicvalue * 1.0 / 10 ^ (coalesce(a.decimals, 18)) as publicvalue_norm
 from inner_proofs i
