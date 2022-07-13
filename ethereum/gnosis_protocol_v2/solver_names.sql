505a389b35d06b2a2926e51ba834e4c3e28ca4ac
jeff-dude <jeff@dune.xyz>
2022-07-13 11:47:38 -0400

    format string to read URL correctly

--- ethereum/gnosis_protocol_v2/solver_names.sql
+++ ethereum/gnosis_protocol_v2/solver_names.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS gnosis_protocol_v2.solver_names
     name        text
 );
 
-\copy gnosis_protocol_v2.solver_names FROM 'https://raw.githubusercontent.com/duneanalytics/abstractions/a68ea6d2c3045d02c29c974556d0d230533a1263/ethereum/gnosis_protocol_v2/solvers.csv' with (format csv,header true, delimiter ',');
+\copy gnosis_protocol_v2.solver_names FROM PROGRAM 'curl https://raw.githubusercontent.com/duneanalytics/abstractions/master/ethereum/gnosis_protocol_v2/solver_names.csv' with (format csv,header true, delimiter ',');
 
 CREATE UNIQUE INDEX IF NOT EXISTS solver_id ON gnosis_protocol_v2.solver_names (address);
