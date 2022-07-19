15ec307ab1c1349231dcf3e1f1f5ea5e4d85c49d
dot2dotseurat <msheintz@gmail.com>
2022-07-19 17:26:59 -0400

    unit test typo

--- spellbook/macros/materialization/unit_tests/test_macros.py
+++ spellbook/macros/materialization/unit_tests/test_macros.py
@@ -26,7 +26,7 @@ def test_get_create_global_temp_view_as_sql(jinja_env):
     compare(rendered, expected)
 
 
-def test_create_dt_as(jinja_env):
+def test_create_dt_as_sql(jinja_env):
     template = jinja_env.get_template('create_dt_as.sql')
     rendered = template.module.create_dt_as(file_path='`/tmp/schema/table`', sql='select 1')
     expected = "CREATE OR REPLACE TABLE delta.`/tmp/schema/table` USING DELTA as ( select 1 )"
