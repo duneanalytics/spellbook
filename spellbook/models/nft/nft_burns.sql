1edacaf0aeddf9a535ef73bc97fae06bd4c3ec30
dot2dotseurat <msheintz@gmail.com>
2022-07-20 13:13:19 -0400

    columns specified

--- spellbook/models/nft/nft_burns.sql
+++ spellbook/models/nft/nft_burns.sql
@@ -8,12 +8,120 @@
 }}
 
 
-SELECT * FROM
-(SELECT * FROM {{ ref('opensea_burns') }} 
+SELECT
+    blockchain,
+    project,
+    version,
+    block_time,
+    token_id,
+    collection,
+    amount_usd,
+    token_standard,
+    trade_type,
+    number_of_items,
+    trade_category,
+    evt_type,
+    seller,
+    buyer,
+    amount_original,
+    amount_raw,
+    currency_symbol,
+    currency_contract,
+    nft_contract_address,
+    project_contract_address,
+    aggregator_name,
+    aggregator_address,
+    tx_hash,
+    tx_from,
+    tx_to,
+    unique_trade_id
+FROM
+(SELECT
+        blockchain,
+    project,
+    version,
+    block_time,
+    token_id,
+    collection,
+    amount_usd,
+    token_standard,
+    trade_type,
+    number_of_items,
+    trade_category,
+    evt_type,
+    seller,
+    buyer,
+    amount_original,
+    amount_raw,
+    currency_symbol,
+    currency_contract,
+    nft_contract_address,
+    project_contract_address,
+    aggregator_name,
+    aggregator_address,
+    tx_hash,
+    tx_from,
+    tx_to,
+    unique_trade_id
+FROM {{ ref('opensea_burns') }}
 UNION
-SELECT * FROM {{ ref('looksrare_ethereum_burns') }})
+SELECT
+    blockchain,
+    project,
+    version,
+    block_time,
+    token_id,
+    collection,
+    amount_usd,
+    token_standard,
+    trade_type,
+    number_of_items,
+    trade_category,
+    evt_type,
+    seller,
+    buyer,
+    amount_original,
+    amount_raw,
+    currency_symbol,
+    currency_contract,
+    nft_contract_address,
+    project_contract_address,
+    aggregator_name,
+    aggregator_address,
+    tx_hash,
+    tx_from,
+    tx_to,
+    unique_trade_id
+FROM {{ ref('looksrare_ethereum_burns') }}
 UNION
-SELECT * FROM {{ ref('x2y2_ethereum_burns') }})
+SELECT
+    blockchain,
+    project,
+    version,
+    block_time,
+    token_id,
+    collection,
+    amount_usd,
+    token_standard,
+    trade_type,
+    number_of_items,
+    trade_category,
+    evt_type,
+    seller,
+    buyer,
+    amount_original,
+    amount_raw,
+    currency_symbol,
+    currency_contract,
+    nft_contract_address,
+    project_contract_address,
+    aggregator_name,
+    aggregator_address,
+    tx_hash,
+    tx_from,
+    tx_to,
+    unique_trade_id
+FROM {{ ref('x2y2_ethereum_burns') }})
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
 WHERE block_time > now() - interval 2 days
