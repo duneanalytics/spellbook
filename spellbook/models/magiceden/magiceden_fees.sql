400e0c1ec2eef3b2cb2cd15ef09d97c798f3cbb1
soispoke <66172107+soispoke@users.noreply.github.com>
2022-07-22 17:57:20 +0700

    Major improvements of MagicEden Spells (#1310)
    * Major improvements of MagicEden Spells
    
    * Update magiceden_solana_schema.yml
    
    * Update alter_table_properties.sql
    
    * update incremental filter
    
    * Update magiceden_events.sql

--- spellbook/models/magiceden/magiceden_fees.sql
+++ spellbook/models/magiceden/magiceden_fees.sql
@@ -1,26 +1,31 @@
-{{ config(
-        alias ='trades'
-        )
+ {{
+  config(
+        alias='fees')
 }}
- 
+
 SELECT blockchain,
 project,
 version,
 block_time,
 token_id,
 NULL::string as collection,
-amount_usd,
+platform_fee_amount_raw,
+platform_fee_amount,
+platform_fee_amount_usd,
+platform_fee_percentage,
+royalty_fee_amount_raw,
+royalty_fee_amount,
+royalty_fee_amount_usd,
+royalty_fee_percentage,
+NULL::string as royalty_fee_receive_address,
+royalty_fee_currency_symbol,
 token_standard,
-NULL::string as trade_type,
+trade_type,
 number_of_items,
 NULL::string as trade_category,
 evt_type,
 seller,
 buyer,
-amount_original,
-amount_raw,
-currency_symbol,
-currency_contract,
 NULL::string as nft_contract_address,
 project_contract_address,
 NULL::string as aggregator_name,
@@ -30,4 +35,4 @@ tx_hash,
 NULL::string as tx_from,
 NULL::string as tx_to,
 unique_trade_id
-FROM {{ ref('magiceden_solana_trades') }}
+FROM {{ ref('magiceden_solana_events') }}
