8286ea66190126ccde45bf6897e453ff94ecfcc1
dot2dotseurat <msheintz@gmail.com>
2022-07-27 11:33:20 -0400

    test edit

--- spellbook/tests/nomad/ethereum/nomad_ethereum_assert.sql
+++ spellbook/tests/nomad/ethereum/nomad_ethereum_assert.sql
@@ -6,6 +6,6 @@ FROM {{ ref('nomad_ethereum_view_bridge_transactions') }} nomad_transactions
 JOIN {{ ref('nomad_ethereum_transactions_etherscan') }} test_data ON test_data.tx_hash = nomad_transactions.tx_hash
 WHERE nomad_transactions.block_time >= '2022-07-27' and nomad_transactions.block_time < '2022-07-28'
 )
-select count(case when amount_test = false then 1 else null end)/count(*) as pct_mismatch, count(*) as count_rows
+select *
 from unit_tests
-having count(case when amount_test = false then 1 else null end) > count(*)*0.05
+where amount_test = false
