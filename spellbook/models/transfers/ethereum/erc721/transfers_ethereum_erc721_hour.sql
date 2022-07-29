d88c09a1405ceb0df21859d21639a6f1e11477f2
dot2dotseurat <msheintz@gmail.com>
2022-07-29 16:43:21 -0400

    replace day with hour

--- spellbook/models/transfers/ethereum/erc721/transfers_ethereum_erc721_hour.sql
+++ spellbook/models/transfers/ethereum/erc721/transfers_ethereum_erc721_hour.sql
@@ -1,24 +1,24 @@
 {{ config(materialized='table', alias='erc721_transfer_hour') }}
 
 with one_zero_balances AS
--- sum(amount) ... = 1 for the day the token was recieved
--- sum(amount) ... = 0 for the day the token was sent
+-- sum(amount) ... = 1 for the hour the token was recieved
+-- sum(amount) ... = 0 for the hour the token was sent
 --
 (
        SELECT 'ethereum' as blockchain,
-              date_trunc('day', evt_block_time) as day,
+              date_trunc('hour', evt_block_time) as hour,
               wallet_address,
               token_address,
               tokenId,
               current_timestamp() as updated_at,
-              sum(amount) over (partition by token_address, tokenId, wallet_address order by date_trunc('day', evt_block_time) asc) as balance,
-              row_number() over (partition by token_address, tokenId, wallet_address order by date_trunc('day', evt_block_time) desc) as recency_index
+              sum(amount) over (partition by token_address, tokenId, wallet_address order by date_trunc('hour', evt_block_time) asc) as balance,
+              row_number() over (partition by token_address, tokenId, wallet_address order by date_trunc('hour', evt_block_time) desc) as recency_index
        FROM {{ref('transfers_ethereum_erc721')}})
 
 
 SELECT   blockchain,
-          day,
-          lead(day, 1, now()) OVER (partition BY blockchain, token_address, tokenId ORDER BY day) AS next_day, --day sold
+          hour,
+          lead(hour, 1, now()) OVER (partition BY blockchain, token_address, tokenId ORDER BY hour) AS next_hour, --hour sold
           wallet_address,
           token_address,
           tokenid
