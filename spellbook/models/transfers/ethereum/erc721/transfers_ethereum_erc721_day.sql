7ebc29df0e352016d69c36add9b427cccca1595f
dot2dotseurat <msheintz@gmail.com>
2022-07-29 15:39:12 -0400

    modularize

--- spellbook/models/transfers/ethereum/erc721/transfers_ethereum_erc721_day.sql
+++ spellbook/models/transfers/ethereum/erc721/transfers_ethereum_erc721_day.sql
@@ -1,38 +1,8 @@
 {{ config(materialized='table', alias='erc721_transfer_day') }}
 
-with
-    received_transfers as (
-        select 'receive' || '-' ||  evt_tx_hash || '-' || evt_index || '-' || `to` as unique_tx_id,
-            to as wallet_address,
-            contract_address as token_address,
-            evt_block_time,
-            tokenId,
-            1 as amount
-        from
-            {{ source('erc721_ethereum', 'evt_transfer') }}
-    )
-
-    ,
-    sent_transfers as (
-        select 'send' || '-' || evt_tx_hash || '-' || evt_index || '-' || `from` as unique_tx_id,
-            from as wallet_address,
-            contract_address as token_address,
-            evt_block_time,
-            tokenId,
-            -1 as amount
-        from
-            {{ source('erc721_ethereum', 'evt_transfer') }}
-    )
-
-, transfers as
-(select 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, tokenId, amount, unique_tx_id
-from sent_transfers
-union
-select 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, tokenId, amount, unique_tx_id
-from received_transfers)
 
 
-, one_zero_balances AS
+with one_zero_balances AS
 -- sum(amount) ... = 1 for the hour the token was recieved
 -- sum(amount) ... = 0 for the hour the token was sent
 --
@@ -45,7 +15,7 @@ from received_transfers)
               current_timestamp() as updated_at,
               sum(amount) over (partition by token_address, tokenId, wallet_address order by date_trunc('hour', evt_block_time) asc) as balance,
               row_number() over (partition by token_address, tokenId, wallet_address order by date_trunc('hour', evt_block_time) desc) as recency_index
-       FROM transfers)
+       FROM {{ref('transfers_ethereum_erc721')}})
 
 
 SELECT   blockchain,
