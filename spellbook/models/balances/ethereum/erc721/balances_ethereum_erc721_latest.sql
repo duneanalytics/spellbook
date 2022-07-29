b25cb919cb858403a21d8b1baec79dc6e2938f20
dot2dotseurat <msheintz@gmail.com>
2022-07-29 16:03:40 -0400

    day and hour models

--- spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_latest.sql
+++ spellbook/models/balances/ethereum/erc721/balances_ethereum_erc721_latest.sql
@@ -4,12 +4,12 @@
 }}
 
 SELECT
-    wallet_address,
-    token_address,
-    tokenId,
+    t.wallet_address,
+    t.token_address,
+    t.tokenId,
     nft_tokens.name as collection,
     now() as updated_at
-FROM  {{ref('transfers_ethereum_erc721')}})
-LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft_tokens ON nft_tokens.contract_address = token_address
+FROM  {{ref('transfers_ethereum_erc721')}} as  t
+LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft_tokens ON nft_tokens.contract_address = t.token_address
 group by 1,2,3,4,5
 having sum(amount) = 1
