8181dbf08b84728e7373b16b3d60eefb85815040
jeff-dude <jeff@dune.xyz>
2022-08-03 18:21:49 -0400

    fix unique_trade_id column, move columns to left side

--- spellbook/models/uniswap/ethereum/uniswap_v1_ethereum_trades.sql
+++ spellbook/models/uniswap/ethereum/uniswap_v1_ethereum_trades.sql
@@ -7,50 +7,49 @@
     unique_key = 'unique_trade_id'
     )
 }}
-
 SELECT
-    'ethereum' AS blockchain,
-    'uniswap' AS project, 
-    '1' AS version,
-    dexs.block_time,
-    erc20a.symbol AS token_bought_symbol,
-    erc20b.symbol AS token_sold_symbol,
-    '' AS token_pair --todo: determine value here
-    dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
-    dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount,
-    dexs.token_bought_amount_raw,
-    dexs.token_sold_amount_raw,
-    coalesce(
-        dexs.amount_usd,
-        dexs.token_bought_amount_raw / 10 ^ p_bought.decimals * p_bought.price,
-        dexs.token_sold_amount_raw / 10 ^ p_sold.decimals * p_sold.price
-    ) AS amount_usd,
-    dexs.token_bought_address,
-    dexs.token_sold_address,
-    coalesce(dexs.taker, tx."from") AS taker, -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
-    dexs.maker,
-    dexs.project_contract_address,
-    dexs.tx_hash,
-    tx."from" AS tx_from,
-    tx."to" AS tx_to,
-    dexs.trace_address,
-    dexs.evt_index,
-    'uniswap' ||'-'|| '1' ||'-'|| dexs.tx_hash ||'-'|| dexs.evt_index ||'-'|| dexs.trace_address AS unique_trade_id
+    'ethereum' AS blockchain
+    ,'uniswap' AS project
+    ,'1' AS version
+    ,dexs.block_time
+    ,erc20a.symbol AS token_bought_symbol
+    ,erc20b.symbol AS token_sold_symbol
+    ,'' AS token_pair --todo: determine value here
+    ,dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount
+    ,dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount
+    ,dexs.token_bought_amount_raw
+    ,dexs.token_sold_amount_raw
+    ,coalesce(
+        dexs.amount_usd
+        ,(dexs.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
+        ,(dexs.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
+    ) AS amount_usd
+    ,dexs.token_bought_address
+    ,dexs.token_sold_address
+    ,coalesce(dexs.taker, tx.from) AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
+    ,dexs.maker
+    ,dexs.project_contract_address
+    ,dexs.tx_hash
+    ,tx.from AS tx_from
+    ,tx.to AS tx_to
+    ,dexs.trace_address
+    ,dexs.evt_index
+    ,'uniswap' ||'-'|| '1' ||'-'|| dexs.tx_hash ||'-'|| IFNULL(dexs.evt_index, '') ||'-'|| IFNULL(dexs.trace_address, '') AS unique_trade_id
 FROM (
     -- Uniswap v1 TokenPurchase
     SELECT
-        t.evt_block_time AS block_time,
-        t.buyer AS taker,
-        NULL::bytea AS maker,
-        t.tokens_bought AS token_bought_amount_raw,
-        t.eth_sold AS token_sold_amount_raw,
-        NULL::numeric AS amount_usd,
-        f.token AS token_bought_address,
-        '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::bytea AS token_sold_address, --Using WETH for easier joining with USD price table
-        t.contract_address AS project_contract_address,
-        t.evt_tx_hash AS tx_hash,
-        NULL::integer[] AS trace_address,
-        t.evt_index
+        t.evt_block_time AS block_time
+        ,t.buyer AS taker
+        ,NULL AS maker
+        ,t.tokens_bought AS token_bought_amount_raw
+        ,t.eth_sold AS token_sold_amount_raw
+        ,NULL AS amount_usd
+        ,f.token AS token_bought_address
+        ,'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' AS token_sold_address --Using WETH for easier joining with USD price table
+        ,t.contract_address AS project_contract_address
+        ,t.evt_tx_hash AS tx_hash
+        ,NULL AS trace_address
+        ,t.evt_index
     FROM
         {{ source('uniswap_ethereum', 'Exchange_evt_TokenPurchase') }} t
     INNER JOIN {{ source('uniswap_ethereum', 'Factory_evt_NewExchange') }} f
@@ -63,18 +62,18 @@ FROM (
 
     -- Uniswap v1 EthPurchase
     SELECT
-        t.evt_block_time AS block_time,
-        t.buyer AS taker,
-        NULL::bytea AS maker,
-        t.eth_bought AS token_bought_amount_raw,
-        t.tokens_sold AS token_sold_amount_raw,
-        NULL::numeric AS amount_usd,
-        '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'::bytea AS token_bought_address, --Using WETH for easier joining with USD price table
-        f.token AS token_sold_address,
-        t.contract_address AS project_contract_address,
-        t.evt_tx_hash AS tx_hash,
-        NULL::integer[] AS trace_address,
-        t.evt_index
+        t.evt_block_time AS block_time
+        ,t.buyer AS taker
+        ,NULL AS maker
+        ,t.eth_bought AS token_bought_amount_raw
+        ,t.tokens_sold AS token_sold_amount_raw
+        ,NULL AS amount_usd
+        ,'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' AS token_bought_address --Using WETH for easier joining with USD price table
+        ,f.token AS token_sold_address
+        ,t.contract_address AS project_contract_address
+        ,t.evt_tx_hash AS tx_hash
+        ,NULL AS trace_address
+        ,t.evt_index
     FROM
         {{ source('uniswap_ethereum', 'Exchange_evt_EthPurchase') }} t
     INNER JOIN {{ source('uniswap_ethereum', 'Factory_evt_NewExchange') }} f
