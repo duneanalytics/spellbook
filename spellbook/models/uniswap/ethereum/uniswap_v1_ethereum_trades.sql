01ada188337516f66fb07df891d68d434bd8f43c
jeff-dude <jeff@dune.xyz>
2022-08-03 17:21:05 -0400

    remove comment in config which appears to break it, reformat a bit

--- spellbook/models/uniswap/ethereum/uniswap_v1_ethereum_trades.sql
+++ spellbook/models/uniswap/ethereum/uniswap_v1_ethereum_trades.sql
@@ -1,11 +1,10 @@
-{{
-config(
-    schema = 'uniswap_v1_ethereum', --overwrite dbt project file schema name for version(s) of contracts
-    alias ='trades',
-    materialized ='incremental',
-    file_format ='delta',
-    incremental_strategy='merge',
-    unique_key='unique_trade_id'
+{{ config(
+    schema = 'uniswap_v1_ethereum',
+    alias = 'trades',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = 'unique_trade_id'
     )
 }}
 
