b252e19d4dc7d95273b6c198eb49eec58121cfd9
jeff-dude <jeff@dune.xyz>
2022-08-04 14:44:33 -0400

    update uniswap ethereum to be incremental

--- spellbook/models/uniswap/ethereum/uniswap_ethereum_trades.sql
+++ spellbook/models/uniswap/ethereum/uniswap_ethereum_trades.sql
@@ -1,14 +1,33 @@
- {{ config(
-       alias='trades'
-       )
+{{ config(
+        alias ='trades',
+        materialized ='incremental',
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key='unique_trade_id'
+        )
 }}
 
 SELECT *
 FROM
 (
-       SELECT * FROM {{ ref('uniswap_v1_ethereum_trades') }}
+       SELECT *
+       FROM {{ ref('uniswap_v1_ethereum_trades') }}
+        {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        WHERE block_time > now() - interval 2 days
+        {% endif %} 
        -- UNION
-       -- SELECT * FROM {{ ref('uniswap_v2_ethereum_trades') }}
+       -- SELECT *
+       -- FROM {{ ref('uniswap_v2_ethereum_trades') }}
+       --  {% if is_incremental() %}
+       --  -- this filter will only be applied on an incremental run
+       --  WHERE block_time > now() - interval 2 days
+       --  {% endif %} 
        -- UNION
-       -- SELECT * FROM {{ ref('uniswap_v3_ethereum_trades') }}
+       -- SELECT *
+       -- FROM {{ ref('uniswap_v3_ethereum_trades') }}
+       --  {% if is_incremental() %}
+       --  -- this filter will only be applied on an incremental run
+       --  WHERE block_time > now() - interval 2 days
+       --  {% endif %} 
 )
