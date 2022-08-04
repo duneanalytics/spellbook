f9dc02ab9bc53d89969ba49dd89e30c50f540fd4
jeff-dude <jeff@dune.xyz>
2022-08-04 14:44:07 -0400

    update uniswap trades to be incremental

--- spellbook/models/uniswap/uniswap_trades.sql
+++ spellbook/models/uniswap/uniswap_trades.sql
@@ -1,6 +1,9 @@
-{{config
-        (
-                alias ='trades'
+{{ config(
+        alias ='trades',
+        materialized ='incremental',
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key='unique_trade_id'
         )
 }}
 
@@ -9,4 +12,8 @@ FROM
 (
         SELECT *
         FROM {{ ref('uniswap_ethereum_trades') }}
+        {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        WHERE block_time > now() - interval 2 days
+        {% endif %} 
 )
