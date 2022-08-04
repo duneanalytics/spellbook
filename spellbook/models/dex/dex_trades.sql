ef567c570759ac66388ae89cecc0d36580347400
jeff-dude <jeff@dune.xyz>
2022-08-04 14:36:42 -0400

    init dex trades file

---
+++ spellbook/models/dex/dex_trades.sql
@@ -0,0 +1,18 @@
+{{ config(
+        alias ='trades',
+        materialized ='incremental',
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key='unique_trade_id'
+        )
+}}
+SELECT *
+FROM
+(
+        SELECT *
+        FROM {{ ref('uniswap_trades') }}
+        {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        WHERE block_time > now() - interval 2 days
+        {% endif %} 
+)
