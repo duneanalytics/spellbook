b665ef40b2c9adcd710ffeb84dc3e356d9e4a851
jeff-dude <jeff@dune.xyz>
2022-08-05 11:38:39 -0400

    replace select * with specific columns, add placeholder for future protocols

--- spellbook/models/dex/dex_trades.sql
+++ spellbook/models/dex/dex_trades.sql
@@ -9,10 +9,36 @@
 SELECT *
 FROM
 (
-        SELECT *
+        SELECT
+                blockchain
+                ,project
+                ,version
+                ,block_time
+                ,token_bought_symbol
+                ,token_sold_symbol
+                ,token_pair
+                ,token_bought_amount
+                ,token_sold_amount
+                ,token_bought_amount_raw
+                ,token_sold_amount_raw
+                ,amount_usd
+                ,token_bought_address
+                ,token_sold_address
+                ,taker
+                ,maker
+                ,project_contract_address
+                ,tx_hash
+                ,tx_from
+                ,tx_to
+                ,trace_address
+                ,evt_index
+                ,unique_trade_id
         FROM {{ ref('uniswap_trades') }}
         {% if is_incremental() %}
-        -- this filter will only be applied on an incremental run
-        WHERE block_time > now() - interval 2 days
-        {% endif %} 
+        WHERE block_time > now() - interval 2 days -- this filter will only be applied on an incremental run
+        {% endif %}
+        /*
+        UNION
+        <add future protocols here>
+        */
 )
--- spellbook/models/uniswap/ethereum/uniswap_ethereum_trades.sql
+++ spellbook/models/uniswap/ethereum/uniswap_ethereum_trades.sql
@@ -10,24 +10,90 @@
 SELECT *
 FROM
 (
-       SELECT *
+       SELECT
+                blockchain
+                ,project
+                ,version
+                ,block_time
+                ,token_bought_symbol
+                ,token_sold_symbol
+                ,token_pair
+                ,token_bought_amount
+                ,token_sold_amount
+                ,token_bought_amount_raw
+                ,token_sold_amount_raw
+                ,amount_usd
+                ,token_bought_address
+                ,token_sold_address
+                ,taker
+                ,maker
+                ,project_contract_address
+                ,tx_hash
+                ,tx_from
+                ,tx_to
+                ,trace_address
+                ,evt_index
+                ,unique_trade_id
        FROM {{ ref('uniswap_v1_ethereum_trades') }}
         {% if is_incremental() %}
-        -- this filter will only be applied on an incremental run
-        WHERE block_time > now() - interval 2 days
+        WHERE block_time > now() - interval 2 days -- this filter will only be applied on an incremental run
         {% endif %} 
        -- UNION
-       -- SELECT *
+       -- SELECT
+                -- blockchain
+                -- ,project
+                -- ,version
+                -- ,block_time
+                -- ,token_bought_symbol
+                -- ,token_sold_symbol
+                -- ,token_pair
+                -- ,token_bought_amount
+                -- ,token_sold_amount
+                -- ,token_bought_amount_raw
+                -- ,token_sold_amount_raw
+                -- ,amount_usd
+                -- ,token_bought_address
+                -- ,token_sold_address
+                -- ,taker
+                -- ,maker
+                -- ,project_contract_address
+                -- ,tx_hash
+                -- ,tx_from
+                -- ,tx_to
+                -- ,trace_address
+                -- ,evt_index
+                -- ,unique_trade_id
        -- FROM {{ ref('uniswap_v2_ethereum_trades') }}
        --  {% if is_incremental() %}
-       --  -- this filter will only be applied on an incremental run
-       --  WHERE block_time > now() - interval 2 days
+       --  WHERE block_time > now() - interval 2 days -- this filter will only be applied on an incremental run
        --  {% endif %} 
        -- UNION
-       -- SELECT *
+       -- SELECT
+                -- blockchain
+                -- ,project
+                -- ,version
+                -- ,block_time
+                -- ,token_bought_symbol
+                -- ,token_sold_symbol
+                -- ,token_pair
+                -- ,token_bought_amount
+                -- ,token_sold_amount
+                -- ,token_bought_amount_raw
+                -- ,token_sold_amount_raw
+                -- ,amount_usd
+                -- ,token_bought_address
+                -- ,token_sold_address
+                -- ,taker
+                -- ,maker
+                -- ,project_contract_address
+                -- ,tx_hash
+                -- ,tx_from
+                -- ,tx_to
+                -- ,trace_address
+                -- ,evt_index
+                -- ,unique_trade_id
        -- FROM {{ ref('uniswap_v3_ethereum_trades') }}
        --  {% if is_incremental() %}
-       --  -- this filter will only be applied on an incremental run
-       --  WHERE block_time > now() - interval 2 days
+       --  WHERE block_time > now() - interval 2 days -- this filter will only be applied on an incremental run
        --  {% endif %} 
 )
