34fdba30d3587fb3d7f717b0e4396a1bbd0fb27e
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-08-10 11:51:47 -0400

    add deletes

--- optimism2/chainlink/backfills/2022_08_10_backfill.sql
+++ optimism2/chainlink/backfills/2022_08_10_backfill.sql
@@ -2,14 +2,25 @@
 --arbitrary old date, should hanlde for previous bugs as well
 
 --chainlink
-SELECT chainlink.insert_price_feeds(
-    '2022-03-16'::date,
-    (SELECT max_time FROM ovm2.view_last_updated)
-)
-;
---overall prices
-SELECT prices.insert_approx_prices_from_dex_data(
+
+    --remove bad data
+    DELETE FROM chainlink.view_price_feeds
+    WHERE hour > '2022-03-16'::date;
+    
+    --insert new data
+    SELECT chainlink.insert_price_feeds(
         '2022-03-16'::date,
         (SELECT max_time FROM ovm2.view_last_updated)
     );
+--overall prices
+
+    --remove bad data
+    DELETE FROM prices.approx_prices_from_dex_data
+    WHERE hour > '2022-03-16'::date;
+    
+    --insert new data
+    SELECT prices.insert_approx_prices_from_dex_data(
+            '2022-03-16'::date,
+            (SELECT max_time FROM ovm2.view_last_updated)
+        );
 
