c11bd8651b6a90feb831a6fad06bdff614912309
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-08-10 11:47:21 -0400

    Update 2022_08_10_backfill.sql

--- optimism2/chainlink/backfills/2022_08_10_backfill.sql
+++ optimism2/chainlink/backfills/2022_08_10_backfill.sql
@@ -1,6 +1,15 @@
 --Backfill due to errors caused by Dune delays
+--arbitrary old date, should hanlde for previous bugs as well
+
+--chainlink
 SELECT chainlink.insert_price_feeds(
     '2022-03-16'::date,
     (SELECT MAX(block_time) FROM optimism.transactions)
 )
 ;
+--overall prices
+SELECT prices.insert_approx_prices_from_dex_data(
+        	'2022-03-16'::date,
+        	(SELECT max_time FROM ovm2.view_last_updated)
+    	);
+
