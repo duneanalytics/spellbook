db9e10a3659af155a8a2f0184acbb80d4b5d209b
jeff-dude <jeff@dune.xyz>
2022-08-10 13:58:41 -0400

    build token bought test case, remove old test files

---
+++ spellbook/tests/uniswap/ethereum/uniswap_v1_ethereum_validate_token_bought.sql
@@ -0,0 +1,17 @@
+with unit_test as
+(
+    select
+        case
+            when (uni_v1.token_bought_address = test_data.token_bought_address and uni_v1.token_sold_address = test_data.token_sold_address)
+            then True
+            else False
+        end as token_bought_test
+    from {{ ref('uniswap_v1_ethereum_trades') }} uni_v1
+    join {{ ref('dex_trades_seed') }} test_data
+        on uni_v1.tx_hash = test_data.tx_hash
+        and uni_v1.block_time = test_data.block_time
+)
+select *
+from unit_test
+where token_bought_test = True
+;
