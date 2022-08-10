cafec08fd13b839df77cc29c4e8464cc0828bc8a
jeff-dude <jeff@dune.xyz>
2022-08-10 18:11:20 -0400

    add alias to incoming column, truncate timestamp in join, force failures to return rows

--- spellbook/tests/generic/dex_trades_token_bought.sql
+++ spellbook/tests/generic/dex_trades_token_bought.sql
@@ -1,21 +1,20 @@
 {% test dex_trades_token_bought(model, column_name, dex_trades_seed) %}
 
-with unit_test as
-(
-    select
-        case
-            when {{ column_name }} = seed.token_bought_address
-                then True
-            else False
-        end as token_bought_test
-    from {{ model }} m
-    join {{ dex_trades_seed }} seed
-        on m.tx_hash = seed.tx_hash
-        and m.block_time = seed.block_time
-)
-select *
-from unit_test
-where token_bought_test = True
-;
+    with unit_test as
+    (
+        select
+            case
+                when m.{{ column_name }} = seed.token_bought_address
+                    then True
+                else False
+            end as token_bought_test
+        from {{ model }} m
+        join {{ dex_trades_seed }} seed
+            on m.tx_hash = seed.tx_hash
+            and date_trunc('day',m.block_time) = date_trunc('day',seed.block_time)
+    )
+    select *
+    from unit_test
+    where token_bought_test = False
 
 {% endtest %}
