c36d06fd8abb3a3921354140851a1ab8a6967ebe
Andrey Scherbovich <andrey@safe.global>
2022-08-13 00:22:16 +0200

    Safe (#1352)
    * add safes_ethereum model
    
    * adjustments
    
    * add safe_ethereum_eth_transfers model
    
    * change date to block_time
    
    * add sources file; small adjustments
    
    * update filter to be on block_time
    
    * fix tests
    
    * fix query
    
    * add partition to target, update unique key, add block_date, filter as much as possible
    
    * add partition, update unique key, add block_date, filter as much as possible
    
    * update schema to reflect latest model changes
    
    * add trace address to unique key
    
    * fix schema
    
    * fix eth_transfers unique key
    
    * fixes
    
    * add safe tables to table properties to show in dune explorer
    
    * add safe tables to be optimized on schedule
    
    Co-authored-by: noname <noname@github.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>
    Co-authored-by: = <=>

---
+++ spellbook/tests/safe/safes_check_count.sql
@@ -0,0 +1,16 @@
+-- Check that safes count on a specific date is correct
+
+with test_data as (
+    select count(*) as total
+    from {{ ref('safe_ethereum_safes') }}
+    where creation_time < '2022-08-03'
+),
+
+test_result as (
+    select case when total = 84999 then true else false end as success
+    from test_data
+)
+
+select *
+from test_result
+where success = false
