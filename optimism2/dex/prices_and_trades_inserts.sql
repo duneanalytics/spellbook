1266cda3a47c4064f054b4a336299f9169cd57d2
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-08-12 15:13:07 -0400

    activate kwenta

--- optimism2/dex/prices_and_trades_inserts.sql
+++ optimism2/dex/prices_and_trades_inserts.sql
@@ -20,7 +20,7 @@ VALUES ('15,30,45,59 * * * *', $$
 	SELECT dex.insert_curve( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Curve'), (SELECT max_time FROM ovm2.view_last_updated) );
 	SELECT dex.insert_clipper( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '1'), (SELECT max_time FROM ovm2.view_last_updated) );
 	SELECT dex.insert_clipper_v2( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '2'), (SELECT max_time FROM ovm2.view_last_updated) );
--- 	SELECT dex.insert_kwenta( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Kwenta'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_kwenta( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Kwenta'), (SELECT max_time FROM ovm2.view_last_updated) );
 	SELECT dex.insert_wardenswap( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='WardenSwap' AND version = '2'), (SELECT max_time FROM ovm2.view_last_updated) );
 	SELECT dex.insert_rubicon( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Rubicon'), (SELECT max_time FROM ovm2.view_last_updated) );
 	SELECT dex.insert_velodrome( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Velodrome'), (SELECT max_time FROM ovm2.view_last_updated) );
