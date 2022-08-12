26c8bef3f0b39920c799e5e8123b7f9e87338974
= <=>
2022-08-12 12:18:07 +0200

    fix tests

--- spellbook/tests/safe/safes_check_count.sql
+++ spellbook/tests/safe/safes_check_count.sql
@@ -1,11 +1,16 @@
 -- Check that safes count on a specific date is correct
 
-with unit_tests as (
+with test_data as (
     select count(*) as total
     from {{ ref('safe_ethereum_safes') }}
     where creation_time between '2022-01-01' and '2022-08-03'
+),
+
+test_result as (
+    select case when total = 43704 then true else false end as success
+    from test_data
 )
 
-select case when total = 43704 then true else false end as test
-from unit_tests
-where test = false
+select *
+from test_result
+where success = false
