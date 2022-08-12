df5ecc8aac46b67208c6dc4b68a7a33e31c5eeb4
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-08-11 20:14:12 -0400

    Backfills for OP Prices & DEX Trades (#1366)
    * add backfill
    
    * Create op_last_updated.sql
    
    * Update and rename op_last_updated.sql to view_last_updated.sql
    
    * change now to last updated
    
    * Update 2022_08_10_backfill.sql
    
    * Update 2022_08_10_backfill.sql
    
    * Update 2022_08_10_backfill.sql
    
    * add deletes
    
    * Update 2022_08_10_backfill.sql
    
    * move backfill file
    
    * Update 2022_08_10_backfill.sql
    
    * fix column in dex trades delete
    
    * remove extra ); in script
    
    Co-authored-by: jeff-dude <jeff@dune.xyz>

--- optimism2/dex/prices_and_trades_inserts.sql
+++ optimism2/dex/prices_and_trades_inserts.sql
@@ -3,31 +3,30 @@ VALUES ('15,30,45,59 * * * *', $$
 --Chainlink Updates
 	SELECT chainlink.insert_price_feeds(
         (SELECT MAX(hour) - interval '1 hour' FROM chainlink.view_price_feeds), --buffer in case the db gets stuck
-        now()
+        (SELECT max_time FROM ovm2.view_last_updated) -- when the db was last updated, this handles for the db getting stuck
         );
 --First Prices Run. We expect this to only pull in Chainlink updates
 	SELECT prices.insert_approx_prices_from_dex_data(
         	(SELECT MAX(hour) - interval '1 hour' FROM prices.approx_prices_from_dex_data),
-        	now()
+        	(SELECT max_time FROM ovm2.view_last_updated) -- when the db was last updated, this handles for the db getting stuck
     	);
 	
 ----------
 --DEX Inserts. These should only pull in prices where there is a Chainlink oracle.
-	SELECT dex.insert_uniswap_v3( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Uniswap' AND version = '3'), now() );
-	SELECT dex.insert_oneinch( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='1inch'), now(), 0);
-	SELECT dex.insert_zeroex( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project IN ('0x API', 'Matcha')), now() );
-	SELECT dex.insert_zipswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Zipswap'), now() );
-	SELECT dex.insert_curve( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Curve'), now() );
-	SELECT dex.insert_clipper( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '1'), now() );
-	SELECT dex.insert_clipper_v2( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '2'), now() );
--- 	SELECT dex.insert_kwenta( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Kwenta'), now() );
-	SELECT dex.insert_wardenswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='WardenSwap' AND version = '2'), now() );
-	SELECT dex.insert_rubicon( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Rubicon'), now() );
-	SELECT dex.insert_velodrome( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Velodrome'), now() );
-	SELECT dex.insert_beethoven_x( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Beethoven X'), now() );
-	SELECT dex.insert_sushiswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Sushiswap'), now() );
-	SELECT dex.insert_slingshot( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Slingshot'), now() );
-    );
+	SELECT dex.insert_uniswap_v3( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Uniswap' AND version = '3'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_oneinch( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='1inch'), (SELECT max_time FROM ovm2.view_last_updated), 0);
+	SELECT dex.insert_zeroex( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project IN ('0x API', 'Matcha')), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_zipswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Zipswap'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_curve( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Curve'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_clipper( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '1'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_clipper_v2( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '2'), (SELECT max_time FROM ovm2.view_last_updated) );
+-- 	SELECT dex.insert_kwenta( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Kwenta'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_wardenswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='WardenSwap' AND version = '2'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_rubicon( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Rubicon'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_velodrome( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Velodrome'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_beethoven_x( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Beethoven X'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_sushiswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Sushiswap'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_slingshot( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Slingshot'), (SELECT max_time FROM ovm2.view_last_updated) );
 	--ADD REMAINING DEX INSERTS HERE
 --END DEX Inserts
 ----------
@@ -35,17 +34,17 @@ VALUES ('15,30,45,59 * * * *', $$
 -- Second Prices Run. We expect this to pull in prices for all tokens that interacted with a token included in Chainlink oralces.
 	SELECT prices.insert_approx_prices_from_dex_data(
         	(SELECT MAX(hour) - interval '2 hours' FROM prices.approx_prices_from_dex_data),
-        	(SELECT now() )
+        	(SELECT max_time FROM ovm2.view_last_updated)
     	);
 -- Backfill DEX Trades Run. This updates usd_amount based on our latest prices run.
 	SELECT dex.backfill_insert_missing_prices(
 		(SELECT max(block_time) - interval '2 hours' FROM dex.trades), --adding extra 1hr buffer for safety
-		now()
+		(SELECT max_time FROM ovm2.view_last_updated)
 		);
 -- Third Prices Run. We expect this to pull in the remining prices (Oracles + Interacted with Tokens + Next level of tokens).
 	SELECT prices.insert_approx_prices_from_dex_data(
         	(SELECT MAX(hour) - interval '3 hours' FROM prices.approx_prices_from_dex_data),
-        	now()
+        	(SELECT max_time FROM ovm2.view_last_updated)
     	);
 	
 $$)
@@ -57,12 +56,12 @@ VALUES ('7 1 * * *', $$
 --Chainlink Updates
 	SELECT chainlink.insert_price_feeds(
         (SELECT MAX(hour) - interval '30 days' FROM chainlink.view_price_feeds), --buffer in case the db gets stuck
-        now()
+        (SELECT max_time FROM ovm2.view_last_updated)
         );
 -- Third Prices Run. We expect this to pull in the remining prices (Oracles + Interacted with Tokens + Next level of tokens).
 	SELECT prices.insert_approx_prices_from_dex_data(
         	(SELECT MAX(hour) - interval '30 days' FROM prices.approx_prices_from_dex_data),
-        	(SELECT now() )
+        	(SELECT max_time FROM ovm2.view_last_updated)
     	);
 	
 $$)
