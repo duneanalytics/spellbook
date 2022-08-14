4793f05efab083aa689fa68fe73e57fa04d94515
jeff-dude <jeff@dune.xyz>
2022-08-14 17:16:01 -0400

    update block_date logic

--- spellbook/tests/generic/dex_trades_token_bought.sql
+++ spellbook/tests/generic/dex_trades_token_bought.sql
@@ -9,14 +9,14 @@
                 else False
             end as token_bought_test
         from {{ model }} m
-        join {{ dex_trades_seed }} seed
+        inner join {{ dex_trades_seed }} seed
             on m.tx_hash = seed.tx_hash
             and m.evt_index = seed.evt_index
             and m.block_date = seed.block_date
             and m.blockchain = seed.blockchain
             and m.project = seed.project
             and m.version = seed.version
-        where m.block_date >= (select coalesce(max(block_date), '1900-01-01 00:00') from {{ model }})
+            and m.block_date = seed.block_date
     )
     select *
     from unit_test
