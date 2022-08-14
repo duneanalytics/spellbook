99b2e0131a15c9475a1accc211e1dc3d851cd0d1
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-08-14 11:34:49 -0400

    OP2 force newer dex runs & Activate Kwenta (#1373)
    * force newer dex runs
    
    * Update insert_kwenta.sql
    
    * activate kwenta

--- optimism2/dex/prices_and_trades_inserts.sql
+++ optimism2/dex/prices_and_trades_inserts.sql
@@ -13,20 +13,20 @@ VALUES ('15,30,45,59 * * * *', $$
 	
 ----------
 --DEX Inserts. These should only pull in prices where there is a Chainlink oracle.
-	SELECT dex.insert_uniswap_v3( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Uniswap' AND version = '3'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_oneinch( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='1inch'), (SELECT max_time FROM ovm2.view_last_updated), 0);
-	SELECT dex.insert_zeroex( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project IN ('0x API', 'Matcha')), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_zipswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Zipswap'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_curve( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Curve'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_clipper( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '1'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_clipper_v2( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '2'), (SELECT max_time FROM ovm2.view_last_updated) );
--- 	SELECT dex.insert_kwenta( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Kwenta'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_wardenswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='WardenSwap' AND version = '2'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_rubicon( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Rubicon'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_velodrome( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Velodrome'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_beethoven_x( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Beethoven X'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_sushiswap( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Sushiswap'), (SELECT max_time FROM ovm2.view_last_updated) );
-	SELECT dex.insert_slingshot( (SELECT max(block_time) - interval '1 hour' FROM dex.trades WHERE project='Slingshot'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_uniswap_v3( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Uniswap' AND version = '3'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_oneinch( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='1inch'), (SELECT max_time FROM ovm2.view_last_updated), 0);
+	SELECT dex.insert_zeroex( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project IN ('0x API', 'Matcha')), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_zipswap( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Zipswap'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_curve( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Curve'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_clipper( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '1'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_clipper_v2( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Clipper' AND version = '2'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_kwenta( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Kwenta'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_wardenswap( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='WardenSwap' AND version = '2'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_rubicon( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Rubicon'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_velodrome( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Velodrome'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_beethoven_x( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Beethoven X'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_sushiswap( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Sushiswap'), (SELECT max_time FROM ovm2.view_last_updated) );
+	SELECT dex.insert_slingshot( (SELECT COALESCE(max(block_time),'2021-11-11'::date) - interval '1 hour' FROM dex.trades WHERE project='Slingshot'), (SELECT max_time FROM ovm2.view_last_updated) );
 	--ADD REMAINING DEX INSERTS HERE
 --END DEX Inserts
 ----------
