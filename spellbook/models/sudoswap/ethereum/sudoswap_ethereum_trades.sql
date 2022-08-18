a1bae49dd45ed1e505a0b89e196a55c9d597edfa
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2022-08-18 12:24:08 -0400

    unique id and explode

--- spellbook/models/sudoswap/ethereum/sudoswap_ethereum_trades.sql
+++ spellbook/models/sudoswap/ethereum/sudoswap_ethereum_trades.sql
@@ -8,18 +8,18 @@ SELECT
       project,
       version,
       block_time,
-      token_id,
+      explode(token_id) as token_id,
       collection,
-      amount_usd,
+      amount_usd/cardinality(token_id) as amount_usd,
       token_standard,
       trade_type,
-      number_of_items,
+      number_of_items/cardinality(token_id),
       trade_category,
       evt_type,
       seller,
       buyer,
-      amount_original,
-      amount_raw,
+      amount_original/cardinality(token_id) as amount_original,
+      amount_raw/cardinality(token_id) as amount_raw,
       currency_symbol,
       currency_contract,
       nft_contract_address,
@@ -30,6 +30,6 @@ SELECT
       block_number,
       tx_from,
       tx_to,
-      unique_trade_id
+      concat(token_id::string, '-', unique_trade_id) as unique_trade_id
 FROM ({{ ref('sudoswap_ethereum_events') }})
 WHERE evt_type = 'Trade'
