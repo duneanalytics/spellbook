8744963d84144e871f46b36b49821c93acfad7e6
jeff-dude <jeff@dune.xyz>
2022-08-22 10:44:17 -0400

    add 'OR REPLACE' on creation

--- optimism2/public/bytea2numeric.sql
+++ optimism2/public/bytea2numeric.sql
@@ -1,5 +1,5 @@
-DROP FUNCTION bytea2numeric(bytea, boolean, text);
-CREATE FUNCTION bytea2numeric(a bytea, signed boolean = true, byteorder text = 'big')
+-- DROP FUNCTION bytea2numeric(bytea, boolean, text);
+CREATE OR REPLACE FUNCTION bytea2numeric(a bytea, signed boolean = true, byteorder text = 'big')
     RETURNS numeric
 AS $$
     return int.from_bytes(a, byteorder=byteorder, signed=signed)
--- optimism2/public/numeric2bytea.sql
+++ optimism2/public/numeric2bytea.sql
@@ -1,5 +1,5 @@
-DROP FUNCTION numeric2bytea(numeric, integer, boolean, text);
-CREATE FUNCTION numeric2bytea (a numeric, _length integer = 32, signed boolean = true, byteorder text = 'big')
+-- DROP FUNCTION numeric2bytea(numeric, integer, boolean, text);
+CREATE OR REPLACE FUNCTION numeric2bytea (a numeric, _length integer = 32, signed boolean = true, byteorder text = 'big')
   RETURNS bytea
 AS $$
     return int(a).to_bytes(_length, byteorder=byteorder, signed=signed)
