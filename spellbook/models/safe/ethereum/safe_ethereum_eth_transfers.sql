d7608081b41ef0350347ce6f3592152e039c7913
= <=>
2022-08-23 22:42:55 +0200

    fix duplicated unique key

--- spellbook/models/safe/ethereum/safe_ethereum_eth_transfers.sql
+++ spellbook/models/safe/ethereum/safe_ethereum_eth_transfers.sql
@@ -3,7 +3,7 @@
         materialized='incremental',
         alias='eth_transfers',
         partition_by = ['block_date'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'],
+        unique_key = ['block_date', 'tx_hash', 'trace_address', 'amount_raw'],
         on_schema_change='fail',
         file_format ='delta',
         incremental_strategy='merge'
@@ -21,7 +21,8 @@ from {{ source('ethereum', 'traces') }} et
 join {{ ref('safe_ethereum_safes') }} s on et.from = s.address
     and et.from != et.to -- exclude calls to self to guarantee unique key property
     and success = true
-    and (lower(call_type) not in ('delegatecall', 'callcode', 'staticcall') or call_type is null)    
+    and (lower(call_type) not in ('delegatecall', 'callcode', 'staticcall') or call_type is null)
+    and value > 0 -- exclue 0 value traces
 where et.block_time > '2018-11-24' -- for initial query optimisation
 {% if is_incremental() %}
 -- to prevent potential counterfactual safe deployment issues we take a bigger interval
@@ -42,6 +43,7 @@ join {{ ref('safe_ethereum_safes') }} s on et.to = s.address
     and et.from != et.to -- exclude calls to self to guarantee unique key property
     and success = true
     and (lower(call_type) not in ('delegatecall', 'callcode', 'staticcall') or call_type is null)
+    and value > 0 -- exclue 0 value traces
 where et.block_time > '2018-11-24' -- for initial query optimisation
 {% if is_incremental() %}
 -- to prevent potential counterfactual safe deployment issues we take a bigger interval
