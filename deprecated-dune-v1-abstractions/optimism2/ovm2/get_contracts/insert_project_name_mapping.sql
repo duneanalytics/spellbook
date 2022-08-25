2eadf09548f256d0fc4a9e33ed166ada6da4791e
masquot <78954627+masquot@users.noreply.github.com>
2022-08-25 22:07:44 +0200

    Move OP2 table uptables to new directory structure (#1432)

--- deprecated-dune-v1-abstractions/optimism2/ovm2/get_contracts/insert_project_name_mapping.sql
+++ deprecated-dune-v1-abstractions/optimism2/ovm2/get_contracts/insert_project_name_mapping.sql
@@ -52,10 +52,11 @@ Instadapp	InstaDapp
 Lifi	LiFi
 Optimistic Exporer	Optimistic Explorer - Get Started NFT
 ironbank	Iron Bank
-Ironbank	Iron Bank
 iron_bank	Iron Bank
-Iron_bank	Iron Bank
+bluesweep	BlueSweep
+hidden_hand	Hidden Hand
 \.
+
 COMMIT;
 CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS dune_name_mapped_name_uniq_idx ON ovm2.project_name_mappings (dune_name);
 CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS dune_name_mapped_name_low_uniq_idx ON ovm2.project_name_mappings (LOWER(dune_name));
