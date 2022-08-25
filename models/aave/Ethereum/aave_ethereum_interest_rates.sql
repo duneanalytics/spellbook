dbc1965cabbb99811fdf80105f262ee2dd320b50
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-08-25 18:44:04 +0530

    Added interests abstraction for aave on ethereum

---
+++ models/aave/Ethereum/aave_ethereum_interest_rates.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view', alias='aave_interest') }}
+
+select reserve, date_trunc('hour',evt_block_time) as hour, 
+avg(liquidityRate) / 1e27 as deposit_apy, 
+avg(stableBorrowRate) / 1e27 as stable_borrow_apy, 
+avg(variableBorrowRate) / 1e27 as variable_borrow_apy
+from {{ source('aave_v2_ethereum', 'LendingPool_evt_ReserveDataUpdated') }}
+group by 1,2
