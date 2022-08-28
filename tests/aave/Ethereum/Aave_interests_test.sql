25737f28791865165242877b325b38e33d3cbf41
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-08-28 17:50:30 +0530

    Update Aave_interests_test.sql

--- tests/aave/Ethereum/Aave_interests_test.sql
+++ tests/aave/Ethereum/Aave_interests_test.sql
@@ -3,15 +3,15 @@ WITH unit_test1
                  WHEN variable_borrow_apy == 0.024106036652769853 THEN TRUE
                  ELSE FALSE
                END AS test
-        FROM   {{ ref('aave_ethereum_interest_rates' )}}
+        FROM   {{ ref('aave_v2_ethereum_interest_rates' )}}
         WHERE  reserve = '0xdac17f958d2ee523a2206206994597c13d831ec7'
                AND evt_block_time = '2022-08-22 12:00'),
     unit_test2
     AS (SELECT CASE WHEN deposit_apy == 0.004223674732695224 THEN TRUE
                  ELSE FALSE
                END AS test
-        FROM   {{ ref('aave_ethereum_interest_rates' )}}
-        WHERE  reserve = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+        FROM   {{ ref('aave_v2_ethereum_interest_rates' )}}
+        WHERE  symbol = 'USDC'
                AND evt_block_time = '2022-08-25 09:00')
 SELECT *
 FROM   (SELECT *
