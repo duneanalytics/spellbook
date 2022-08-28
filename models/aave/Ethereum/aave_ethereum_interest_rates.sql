16c38f6e9305da1a130c7bbab6fbd95c8e868395
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-08-28 16:33:31 +0530

    Update aave_ethereum_interest_rates.sql

--- models/aave/Ethereum/aave_ethereum_interest_rates.sql
+++ models/aave/Ethereum/aave_ethereum_interest_rates.sql
@@ -1,6 +1,7 @@
 {{ config(materialized='view', alias='aave_interest') }}
 
-select reserve, date_trunc('hour',evt_block_time) as hour, 
+select reserve, 
+date_trunc('hour',evt_block_time) as hour, 
 avg(liquidityRate) / 1e27 as deposit_apy, 
 avg(stableBorrowRate) / 1e27 as stable_borrow_apy, 
 avg(variableBorrowRate) / 1e27 as variable_borrow_apy
