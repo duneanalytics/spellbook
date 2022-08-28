7241fe08dded513e2c7d31af465c77f90f91fb97
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-08-28 17:54:04 +0530

    Update Aave_interests_test.sql

--- tests/aave/Ethereum/Aave_interests_test.sql
+++ tests/aave/Ethereum/Aave_interests_test.sql
@@ -1,22 +1,22 @@
-WITH unit_test1
-    AS (SELECT CASE
-                 WHEN variable_borrow_apy == 0.024106036652769853 THEN TRUE
-                 ELSE FALSE
-               END AS test
-        FROM   {{ ref('aave_v2_ethereum_interest_rates' )}}
-        WHERE  reserve = '0xdac17f958d2ee523a2206206994597c13d831ec7'
-               AND evt_block_time = '2022-08-22 12:00'),
+with unit_test1
+    as (select case
+                 when variable_borrow_apy == 0.024106036652769853 then true
+                 else false
+               end as test
+        from   {{ ref('aave_v2_ethereum_interest_rates' )}}
+        where  reserve = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+               and evt_block_time = '2022-08-22 12:00'),
     unit_test2
-    AS (SELECT CASE WHEN deposit_apy == 0.004223674732695224 THEN TRUE
-                 ELSE FALSE
-               END AS test
-        FROM   {{ ref('aave_v2_ethereum_interest_rates' )}}
-        WHERE  symbol = 'USDC'
-               AND evt_block_time = '2022-08-25 09:00')
-SELECT *
-FROM   (SELECT *
-       FROM   unit_test1
-       UNION
-       SELECT *
-       FROM   unit_test2)
-WHERE  test = FALSE
+    as (select case when deposit_apy == 0.004223674732695224 then true
+                 else false
+               end as test
+        from   {{ ref('aave_v2_ethereum_interest_rates' )}}
+        where  symbol = 'USDC'
+               and evt_block_time = '2022-08-25 09:00')
+select *
+from   (select *
+       from   unit_test1
+       union
+       select *
+       from   unit_test2)
+where  test = false
