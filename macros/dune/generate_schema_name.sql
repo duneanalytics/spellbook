5d835d1a050bb7c196e7f024ca941cd615d3496e
Meghan <msheintz@gmail.com>
2022-09-02 10:49:28 -0400

    wizard test schema and tests

--- macros/dune/generate_schema_name.sql
+++ macros/dune/generate_schema_name.sql
@@ -5,6 +5,10 @@
 
         {{ custom_schema_name | trim }}
 
+    {%- elif target.schema == "github_actions" -%}
+
+        {{ 'test_schema' }}
+
     {%- elif custom_schema_name is none -%}
 
         {{ default_schema }}
