8cc7173d1f85441927413f9e6999705380185dd6
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-09-02 14:30:01 +0530

    Update aave_interests_test.sql
    replaced evt_block_time with hour 
    corrected ==
    corrected unit_test2

--- tests/aave/ethereum/aave_interests_test.sql
+++ tests/aave/ethereum/aave_interests_test.sql
@@ -1,18 +1,18 @@
 with unit_test1
     as (select case
-                 when variable_borrow_apy == 0.024106036652769853 then true
+                 when variable_borrow_apy = 0.024106036652769853 then true
                  else false
                end as test
         from   {{ ref('aave_v2_ethereum_interest_rates' )}}
         where  reserve = '0xdac17f958d2ee523a2206206994597c13d831ec7'
-               and evt_block_time = '2022-08-22 12:00'),
+               and hour = '2022-08-22 12:00'),
     unit_test2
-    as (select case when deposit_apy == 0.004223674732695224 then true
+    as (select case when deposit_apy = 0.004223674732695223 then true
                  else false
                end as test
         from   {{ ref('aave_v2_ethereum_interest_rates' )}}
         where  symbol = 'USDC'
-               and evt_block_time = '2022-08-25 09:00')
+               and hour = '2022-08-25 09:00')
 select *
 from   (select *
        from   unit_test1
