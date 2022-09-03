db59c37aba4a1a976e28cd1ed774c3a86201fb95
soispoke <66172107+soispoke@users.noreply.github.com>
2022-09-03 08:38:40 +0800

    fix platform fee raw type

--- models/sudoswap/ethereum/sudoswap_ethereum_events.sql
+++ models/sudoswap/ethereum/sudoswap_ethereum_events.sql
@@ -346,7 +346,7 @@ WITH
             , aggregator_address
             , aggregator_name
             , platform_fee_amount/number_of_items as platform_fee_amount
-            , platform_fee_amount_raw/number_of_items as platform_fee_amount_raw
+            , cast(platform_fee_amount_raw/number_of_items as double) as platform_fee_amount_raw
             , platform_fee_amount_usd/number_of_items as platform_fee_amount_usd
             , platform_fee_percentage
             , pool_fee_amount/number_of_items as pool_fee_amount
