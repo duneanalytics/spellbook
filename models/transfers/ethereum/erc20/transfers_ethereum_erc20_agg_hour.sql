1b4c1c20eb0fde5ce37a695354e463b4e0a8f9d7
Alan Ghobadi <aalan3@gmail.com>
2022-09-05 17:14:07 +0200

    Transfers optimize (#1503)
    * Use numbers in group by
    
    * Don't use date_trunc in where filter

--- models/transfers/ethereum/erc20/transfers_ethereum_erc20_agg_hour.sql
+++ models/transfers/ethereum/erc20/transfers_ethereum_erc20_agg_hour.sql
@@ -1,5 +1,5 @@
 {{ config(
-        alias ='erc20_agg_hour', 
+        alias ='erc20_agg_hour',
         materialized ='incremental',
         file_format ='delta',
         incremental_strategy='merge',
@@ -20,11 +20,6 @@ from {{ ref('transfers_ethereum_erc20') }} tr
 left join {{ ref('tokens_ethereum_erc20') }} t on t.contract_address = tr.token_address
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
-where date_trunc('hour', tr.evt_block_time) > now() - interval 2 days
+where tr.evt_block_time > now() - 'interval 1 week'
 {% endif %}
-group by
-    date_trunc('hour', tr.evt_block_time),
-  tr.wallet_address,
-  tr.token_address,
-  t.symbol,
-  tr.wallet_address || '-' || tr.token_address || '-' || date_trunc('hour', tr.evt_block_time)
+group by 1, 2, 3, 4, 5, 6
