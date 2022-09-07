196a991fa002ca6a24eae9c33f1e42edc0e0e585
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-09-08 00:04:15 +0530

    Adding aave interests abstraction in Dune v2 (#1434)
    * Added interests abstraction for aave on ethereum
    
    * Added tests for the aave interests abstraction
    
    * Added base sources file for aave abstractions
    
    * Add aave schema
    
    * Update models/base_sources/aave_base_sources.yml
    
    Co-authored-by: Chuxin Huang <30974572+chuxinh@users.noreply.github.com>
    
    * Update aave_base_sources.yml
    
    * Update aave_ethereum_schema.yml
    
    * Update models/aave/aave_ethereum_schema.yml
    
    Co-authored-by: Chuxin Huang <30974572+chuxinh@users.noreply.github.com>
    
    * Update aave_ethereum_interest_rates.sql
    
    * Update aave_ethereum_schema.yml
    
    Changing model name and adding symbol
    
    * Update and rename aave_ethereum_interest_rates.sql to aave_v2_ethereum_interest_rates.sql
    
    Changing model name and adding symbol
    
    * Update Aave_interests_test.sql
    
    * Update Aave_interests_test.sql
    
    * Rename models/aave/Ethereum/aave_v2_ethereum_interest_rates.sql to models/aave/ethereum/aave_v2_ethereum_interest_rates.sql
    
    Renaming to smaller case for consistancy
    
    * Rename tests/aave/Ethereum/Aave_interests_test.sql to tests/aave/ethereum/aave_interests_test.sql
    
    Changing file names to lower case for consistency
    
    * Update aave_v2_ethereum_interest_rates.sql
    
    Indent the model code
    
    * Update and rename models/aave/aave_ethereum_schema.yml to models/aave/ethereum/aave_ethereum_schema.yml
    
    Moving the schema file to source directory and changing the meta
    
    * Rename models/base_sources/aave_base_sources.yml to models/aave/ethereum/aave_ethereum_sources.yml
    
    Moving the source file to aave ethereum folder
    
    * Update aave_interests_test.sql
    
    replaced evt_block_time with hour 
    corrected ==
    corrected unit_test2
    
    * Update aave_ethereum_sources.yml
    
    Added tokens_ethereum_erc20 abstraction to sources
    
    * Update aave_v2_ethereum_interest_rates.sql
    
    * Update aave_ethereum_sources.yml
    
    * Update aave_v2_ethereum_interest_rates.sql
    
    * Update aave_v2_ethereum_interest_rates.sql
    
    Co-authored-by: Chuxin Huang <30974572+chuxinh@users.noreply.github.com>

---
+++ models/aave/ethereum/aave_v2_ethereum_interest_rates.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='view', alias='interest') }}
+
+select 
+  a.reserve, 
+  t.symbol,
+  date_trunc('hour',a.evt_block_time) as hour, 
+  avg(a.liquidityRate) / 1e27 as deposit_apy, 
+  avg(a.stableBorrowRate) / 1e27 as stable_borrow_apy, 
+  avg(a.variableBorrowRate) / 1e27 as variable_borrow_apy
+from {{ source('aave_v2_ethereum', 'LendingPool_evt_ReserveDataUpdated') }} a
+left join {{ ref('tokens_ethereum_erc20') }} t
+on a.reserve=t.contract_address
+group by 1,2,3
