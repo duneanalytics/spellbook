cb2b1c4fb830e2363457d75c9b0ab279a9e5c591
soispoke <66172107+soispoke@users.noreply.github.com>
2022-09-07 15:03:39 +0200

    fix send and receive, amount signs

--- models/transfers/ethereum/erc721/transfers_ethereum_erc721.sql
+++ models/transfers/ethereum/erc721/transfers_ethereum_erc721.sql
@@ -1,21 +1,9 @@
 {{ config(materialized='view', alias='erc721') }}
 
 with
-    sent_transfers as (
-        select 'send' || '-' ||  evt_tx_hash || '-' || evt_index || '-' || `to` as unique_tx_id,
-            to as wallet_address,
-            contract_address as token_address,
-            evt_block_time,
-            tokenId,
-            -1 as amount
-        from
-            {{ source('erc721_ethereum', 'evt_transfer') }}
-    )
-
-    ,
     received_transfers as (
-        select 'receive' || '-' || evt_tx_hash || '-' || evt_index || '-' || `from` as unique_tx_id,
-            from as wallet_address,
+        select 'receive' || '-' ||  evt_tx_hash || '-' || evt_index || '-' || `to` as unique_tx_id,
+            to as wallet_address,
             contract_address as token_address,
             evt_block_time,
             tokenId,
@@ -23,9 +11,21 @@ with
         from
             {{ source('erc721_ethereum', 'evt_transfer') }}
     )
+
+    ,
+    sent_transfers as (
+        select 'send' || '-' || evt_tx_hash || '-' || evt_index || '-' || `from` as unique_tx_id,
+            from as wallet_address,
+            contract_address as token_address,
+            evt_block_time,
+            tokenId,
+            -1 as amount
+        from
+            {{ source('erc721_ethereum', 'evt_transfer') }}
+    )
     
 select 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, tokenId, amount, unique_tx_id
-from sent_transfers
+from received_transfers
 union
 select 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, tokenId, amount, unique_tx_id
-from received_transfers
+from sent_transfers
