d626e745b8603ee9e040ab7746402584d7580062
chuxinh <chuxin@optimism.io>
2022-09-08 16:21:47 +1000

    add ovm1 data

---
+++ models/ovm1/ovm1_user_address_daily_transactions.sql
@@ -0,0 +1,22 @@
+{{ config(alias='user_address_daily_transactions') }}
+select 
+  *
+from ({{ ref('ovm1_q1_user_address_daily_transactions') }})
+
+union all 
+
+select 
+  *
+from ({{ ref('ovm1_q2_user_address_daily_transactions') }})
+
+union all
+
+select 
+  *
+from ({{ ref('ovm1_q3_user_address_daily_transactions') }})
+
+union all
+
+select 
+  *
+from ({{ ref('ovm1_q4_user_address_daily_transactions') }})
