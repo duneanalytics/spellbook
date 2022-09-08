cffe2d574f0d4e2e44970ea459fdf20f267ed556
soispoke <66172107+soispoke@users.noreply.github.com>
2022-09-08 16:44:55 +0200

    fix models' names

--- models/ovm1/optimism/ovm1_optimism_user_address_daily_transactions.sql
+++ models/ovm1/optimism/ovm1_optimism_user_address_daily_transactions.sql
@@ -1,22 +1,22 @@
 {{ config(alias='user_address_daily_transactions', materialized = 'table') }}
 select 
   *
-from ({{ ref('ovm1_q1_user_address_daily_transactions') }})
+from ({{ ref('ovm1_optimism_q1_user_address_daily_transactions') }})
 
 union all 
 
 select 
   *
-from ({{ ref('ovm1_q2_user_address_daily_transactions') }})
+from ({{ ref('ovm1_optimism_q1_user_address_daily_transactions') }})
 
 union all
 
 select 
   *
-from ({{ ref('ovm1_q3_user_address_daily_transactions') }})
+from ({{ ref('ovm1_optimism_q1_user_address_daily_transactions') }})
 
 union all
 
 select 
   *
-from ({{ ref('ovm1_q4_user_address_daily_transactions') }})
+from ({{ ref('ovm1_optimism_q1_user_address_daily_transactions') }})
