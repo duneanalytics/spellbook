80bad24ab334f14944a5ade2a61b76d3df05cfe1
Spring Zhang <spring.okay@gmail.com>
2022-09-09 00:29:34 +0800

    Add gnosis safe airdrop address data

--- macros/alter_table_properties.sql
+++ macros/alter_table_properties.sql
@@ -415,6 +415,15 @@ ALTER TABLE airdrop_optimism.addresses_1 SET TBLPROPERTIES('dune.public'='true',
                                                     'dune.data_explorer.contributors'='["soispoke"]');
 {% endset %}
 
+{% set gnosis_safe_airdrop %}
+ALTER TABLE airdrop_ethereum.gnosis_safe_airdrop SET TBLPROPERTIES('dune.public'='true',
+                                                    'dune.data_explorer.blockchains'='["gnosis"]',
+                                                    'dune.data_explorer.category'='abstraction',
+                                                    'dune.data_explorer.abstraction.type'='sector',
+                                                    'dune.data_explorer.abstraction.name'='airdrop',
+                                                    'dune.data_explorer.contributors'='["springzh"]');
+{% endset %}
+
 {% set ens_view_expirations %}
 ALTER VIEW ens.view_expirations SET TBLPROPERTIES('dune.public'='true',
                                                     'dune.data_explorer.blockchains'='["ethereum"]',
@@ -654,6 +663,7 @@ ALTER VIEW archipelago_ethereum.fees  SET TBLPROPERTIES ('dune.public'='true',
 {% do run_query(safe_ethereum_eth_transfers) %}
 {% do run_query(prices_tokens) %}
 {% do run_query(airdrop_optimism_addresses_1) %}
+{% do run_query(gnosis_safe_airdrop) %}
 {% do run_query(tornado_cash_deposits) %}
 {% do run_query(tornado_cash_withdrawals) %}
 {% do run_query(tokens_erc20) %}
