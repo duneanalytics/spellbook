8bf46d7b44725434b441bb07fcb5b6f98fe01501
masquot <78954627+masquot@users.noreply.github.com>
2022-09-09 11:41:13 +0200

    Add cryptopunks to engine v2 + extend generic test framework (#1523)
    ### Adding a Generic Test Framework
    
    This code builds upon the work of @jeff-dude who introduced generic tests in the context of spellbook. This PR continues his work by adding a generic test that can be applied in many contexts: `tests/generic/compare_column_values_to_seed_values.sql`. This results in the following process:
    
    #### Steps to add unit tests to a model
    1. Create a CSV seed file with sample rows specific to the project you are integrating into spellbook.
    E.g. `seeds/cryptopunks/cryptopunks_trades_buys_bids_seed.csv`
    Every row in this file should contain expected values that you get from a trusted place such as a project's reporting. In this case, we used the [CryptoPunks project's website detail pages](https://cryptopunks.app/cryptopunks/details/5293). It's important here to **make sure that the column names in the seed file match those in the model**.
    
    2. Add the test in the schema.yml file for the model
    E.g. `models/cryptopunks/ethereum/cryptopunks_ethereum_schema.yml`
    We added the following lines to test if `seller` values from seed and model match up:
    
    ```
    - &seller
      name: seller
      description: "Seller wallet address"
      tests:
        - compare_column_values_to_seed_values:
            seed_file_location: ref('cryptopunks_trades_buys_bids_seed')
    ```
    
    
    ### Why is this PR WIP?
    
    The transformation logic in [`cryptopunks_ethereum_events.sql`](./models/cryptopunks/ethereum/cryptopunks_ethereum_events.sql) needs a little more work review. I does not handle certain edge cases such as sales initiated by aggregators / vaults.
    
    
    ### General checks:
    * [x] I tested the query on dune.com after compiling the model with dbt compile (compiled queries are written to the target directory)
    * [x] I used "refs" to reference other models in this repo and "sources" to reference raw or decoded tables 
    * [x] if adding a new model, I added a test
    * [x] the filename is unique and ends with .sql
    * [x] each sql file is a select statement and has only one view, table or function defined  
    * [x] column names are `lowercase_snake_cased`
    * [x] if adding a new model, I edited the dbt project YAML file with new directory path for both models and seeds (if applicable)
    * [x] if adding a new model, I edited the alter table macro to display new database object (table or view) in UI explorer
    * [x] if adding a new materialized table, I edited the optimize table macro
    
    Join logic:
    * [x] if joining to base table (i.e. ethereum transactions or traces), I looked to make it an inner join if possible
    
    Incremental logic:
    * [ ] I used is_incremental & not is_incremental jinja block filters on both base tables and decoded tables
      * [ ] where block_time >= date_trunc("day", now() - interval '1 week')
    * [x] if joining to base table (i.e. ethereum transactions or traces), I applied join condition where block_time >= date_trunc("day", now() - interval '1 week')
    * [x] if joining to prices view, I applied join condition where minute >= date_trunc("day", now() - interval '1 week')

---
+++ models/cryptopunks/ethereum/cryptopunks_ethereum_events.sql
@@ -0,0 +1,134 @@
+{{ config(
+        alias ='events',
+        materialized ='incremental',
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key='unique_trade_id'
+        )
+}}
+
+WITH buys AS (
+  SELECT
+    `type`,
+    punkIndex,
+    CASE
+      WHEN (buy_value = 0 AND `to` = '0x0000000000000000000000000000000000000000') THEN lag(bid_value) OVER (
+        PARTITION BY punkIndex
+        ORDER BY
+          evt_block_number ASC,
+          evt_index ASC
+      )
+      ELSE buy_value
+    END AS `enhanced_buy_value`,
+    `from`,
+    `bid_from`,
+    CASE
+      WHEN (buy_value = 0 AND `to` = '0x0000000000000000000000000000000000000000') THEN lag(`bid_from`) OVER (
+        PARTITION BY punkIndex
+        ORDER BY
+          evt_block_number ASC,
+          evt_index ASC
+      )
+      ELSE `to`
+    END AS `enhanced_to`,
+    evt_block_time,
+    evt_block_number,
+    evt_index,
+    evt_tx_hash,
+    contract_address,
+    CASE WHEN (buy_value = 0 AND `to` = '0x0000000000000000000000000000000000000000') THEN "Offer Accepted" ELSE "Buy" END AS trade_category
+  FROM
+    (
+      SELECT
+        "PunkBought" AS `type`,
+        punkIndex,
+        value AS buy_value,
+        NULL AS bid_value,
+        toAddress as `to`,
+        fromAddress AS `from`,
+        NULL AS `bid_from`,
+        evt_block_number,
+        evt_index,
+        evt_block_time,
+        evt_tx_hash,
+        contract_address
+      FROM
+        {{ source('cryptopunks_ethereum','CryptoPunksMarket_evt_PunkBought') }}
+      UNION ALL
+        -- `from` is address that placed the bid
+      SELECT
+        "PunkBidEntered" AS `type`,
+        punkIndex,
+        NULL AS buy_value,
+        value AS bid_value,
+        NULL,
+        NULL AS `from`,
+        fromAddress AS `bid_from`,
+        evt_block_number,
+        evt_index,
+        evt_block_time,
+        evt_tx_hash,
+        contract_address
+      FROM
+        {{ source('cryptopunks_ethereum','CryptoPunksMarket_evt_PunkBidEntered') }}
+    )
+)
+SELECT
+    "ethereum" as blockchain,
+    "cryptopunks" as project,
+    "v1" as `version`,
+    buys.evt_block_time AS block_time,
+    buys.punkIndex as token_id,
+    "CryptoPunks" AS `collection`,
+    buys.enhanced_buy_value / power(10,18) * p.price AS amount_usd,
+    "erc20" AS token_standard,
+    NULL::string AS trade_type,
+    NULL::string AS number_of_items,
+    buys.trade_category,
+    buys.from AS seller,
+    CASE WHEN buys.`enhanced_to`= agg.contract_address THEN erc.to
+      ELSE buys.`enhanced_to` END AS buyer,
+    "Trade" as evt_type,
+    buys.enhanced_buy_value / power(10,18) AS amount_original,
+    buys.enhanced_buy_value AS amount_raw,
+    "ETH" AS currency_symbol,
+    "0x0000000000000000000000000000000000000000" AS currency_contract,
+    "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb" AS nft_contract_address,
+    "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb" AS project_contract_address,
+    agg.name as aggregator_name,
+    agg.contract_address as aggregator_address,
+    buys.evt_block_number AS block_number,
+    buys.evt_tx_hash AS tx_hash,
+    tx.from as tx_from,
+    tx.to as tx_to,
+    0::double AS platform_fee_amount_raw,
+    0::double AS platform_fee_amount,
+    0::double AS platform_fee_amount_usd,
+    0::double AS platform_fee_percentage,
+    0::double AS royalty_fee_amount_raw,
+    0::double AS royalty_fee_amount,
+    0::double AS royalty_fee_amount_usd,
+    0::double  AS royalty_fee_percentage,
+    NULL::string as royalty_fee_receive_address,
+    NULL::string as royalty_fee_currency_symbol,
+    "cryptopunks" || '-' || buys.evt_tx_hash || '-' || buys.punkIndex || '-' ||  buys.from || '-' || buys.evt_index || '-' || "" as unique_trade_id
+FROM buys
+INNER JOIN {{ source('ethereum','transactions') }} tx ON buys.evt_tx_hash = tx.hash
+{% if is_incremental() %}
+    AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+{% endif %}
+LEFT JOIN {{ ref('nft_ethereum_aggregators') }} agg ON agg.contract_address = tx.to
+LEFT JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', buys.evt_block_time)
+    AND p.contract_address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    AND p.blockchain = "ethereum"
+{% if is_incremental() %}
+    AND p.minute >= date_trunc("day", now() - interval '1 week')
+{% endif %}
+LEFT JOIN {{ source('erc20_ethereum','evt_transfer') }} erc ON erc.evt_block_time=buys.evt_block_time
+    AND erc.contract_address="0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb"
+    AND erc.evt_tx_hash=buys.evt_tx_hash
+    AND erc.from=buys.`enhanced_to`
+    {% if is_incremental() %}
+    AND erc.evt_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+WHERE buys.type = "PunkBought"
