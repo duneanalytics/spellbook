16e8b44acce591ffc35e440176f128bbfd36d4bf
Hildebert Moulié <h.moulie@gmail.com>
2022-09-10 17:36:23 +0200

    add foundation to optimize tables

--- macros/optimize_tables.sql
+++ macros/optimize_tables.sql
@@ -91,6 +91,10 @@ OPTIMIZE x2y2_ethereum.events;
 OPTIMIZE archipelago_ethereum.events;
 {% endset %}
 
+{% set foundation_ethereum_events %}
+OPTIMIZE foundation_ethereum.events;
+{% endset %}
+
 
 {% do run_query(looksrare_ethereum_events) %}
 {% do run_query(magiceden_solana_events) %}
@@ -113,5 +117,6 @@ OPTIMIZE archipelago_ethereum.events;
 {% do run_query(uniswap_v2_ethereum_trades) %}
 {% do run_query(x2y2_ethereum_events) %}
 {% do run_query(archipelago_ethereum_events) %}
+{% do run_query(foundation_ethereum_events) %}
 {% do log("Tables Optimized", info=True) %}
 {% endmacro %}
