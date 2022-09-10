06278ebe8ef3b1e383f3074ef7a5ce6669876e35
rplust <rplusttplusr@gmail.com>
2022-09-10 13:52:21 +0800

    Add unit test for perpetual_trades

---
+++ tests/generic/perpetual_trades_market_source.sql
@@ -0,0 +1,25 @@
+{% test perpetual_trades_market_address(model, column_name, perpetual_trades_seed) %}
+
+    with unit_test as
+    (
+        select
+            case
+                when m.{{ column_name }} = seed.{{ column_name }}
+                    then True
+                else False
+            end as market_address_test
+        from {{ model }} m
+        inner join {{ perpetual_trades_seed }} seed
+            on m.tx_hash = seed.tx_hash
+            and m.block_time = seed.block_time
+            and m.blockchain = seed.blockchain
+            and m.underlying_market = seed.underlying_market
+            and m.market = s.market
+            and m.project = seed.project
+            and m.version = seed.version
+    )
+    select *
+    from unit_test
+    where market_address_test = False
+
+{% endtest %}
