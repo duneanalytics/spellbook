2057fc3998a9a75c583e92ad947a3f40ccdb85ef
Hildebert Moulié <h.moulie@gmail.com>
2022-09-10 17:35:28 +0200

    commit above for wrong pr, fixed

--- macros/optimize_tables.sql
+++ macros/optimize_tables.sql
@@ -103,10 +103,6 @@ OPTIMIZE element_avalanche_c.events;
 OPTIMIZE archipelago_ethereum.events;
 {% endset %}
 
-{% set foundation_ethereum_events %}
-OPTIMIZE foundation_ethereum.events;
-{% endset %}
-
 
 {% do run_query(looksrare_ethereum_events) %}
 {% do run_query(magiceden_solana_events) %}
@@ -132,6 +128,5 @@ OPTIMIZE foundation_ethereum.events;
 {% do run_query(uniswap_v2_ethereum_trades) %}
 {% do run_query(x2y2_ethereum_events) %}
 {% do run_query(archipelago_ethereum_events) %}
-{% do run_query(foundation_ethereum_events) %}
 {% do log("Tables Optimized", info=True) %}
 {% endmacro %}
