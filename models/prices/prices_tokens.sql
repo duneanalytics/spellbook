6ae64f1885119b1ff1158a19e8644cd8bda772f5
Alan Ghobadi <aalan3@gmail.com>
2022-09-14 14:34:06 +0200

    Revert "feat: add ETHW token on trx-tron chain (#1546)" (#1554)
    This reverts commit d41d9fa469bd3b84deb2c0c16ef7db732534a9c5.

--- models/prices/prices_tokens.sql
+++ models/prices/prices_tokens.sql
@@ -603,7 +603,7 @@ VALUES
     ("usdc-usd-coin", "solana", "USDC", "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", 6),
     ("usdt-tether", "solana", "USDT", "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", 6),
     ("seth2-stakewise", "ethereum", "SETH2", "0xfe2e637202056d30016725477c5da089ab0a043a", 18),
-    ("ethw-ethereum-pow-iou", "trx-tron", "ETHW", "TKqaZCWo6g4cY5P2pkykHQNBpo5GyVQrrn", 18)
     ("knight-citadao","ethereum","KNIGHT","0x3541A5C1b04AdABA0B83F161747815cd7B1516bC",18)
+
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
 
