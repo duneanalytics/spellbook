a53dd1495b16c28e0ef0cc9fb4ef9055c208ca50
dhruvmehtaaa <dhruvmehtaalt@gmail.com>
2022-09-16 12:47:39 +0530

    Spellbook Workshop Submission

--- macros/alter_table_properties.sql
+++ macros/alter_table_properties.sql
@@ -685,7 +685,17 @@ ALTER VIEW archipelago_ethereum.fees  SET TBLPROPERTIES ('dune.public'='true',
                                                         'dune.data_explorer.contributors'='["0xRob"]');
 {% endset %}
 
+{% set balancer_ethereum_balances %}
+ALTER VIEW balancer_ethereum.balances SET TBLPROPERTIES('dune.public'='true',
+                                                   'dune.data_explorer.blockchains'='["ethereum"]',
+                                                   'dune.data_explorer.category'='abstraction',
+                                                   'dune.data_explorer.abstraction.type'='project',
+                                                   'dune.data_explorer.abstraction.name'=balancer,
+                                                   'dune.data_explorer.contributors'='["dhr2323"]');
+{% endset %}
 
+
+{% do run_query(balancer_ethereum_balances) %}
 {% do run_query(balances_ethereum_erc20_day) %}
 {% do run_query(balances_ethereum_erc20_hour) %}
 {% do run_query(balances_ethereum_erc20_latest) %}
