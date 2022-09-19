63a9d6ef185400aa427fca7ac3f956edbc57e6d3
Jack Yin <jackyin0@gmail.com>
2022-09-19 18:21:42 -0400

    adding aztec contract labels to dune v2

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -11,6 +11,8 @@ SELECT * FROM {{ ref('labels_funds') }}
 UNION
 SELECT * FROM {{ ref('labels_submitted_contracts') }}
 UNION
+SELECT blockchain, address, name, category, contributor, source, created_at, updated_at FROM {{ ref('aztec_v2_contract_labels') }}
+UNION
 -- Query Labels
 SELECT * FROM {{ ref('labels_nft') }}
 UNION
