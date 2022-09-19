735f0ccaa965a0358b76af38bde29991921c0b1f
Ben Smith <bh2smith@gmail.com>
2022-09-19 17:03:15 +0200

    first pass through the construction of trades table

---
+++ models/cow_protocol/ethereum/cow_protocol_ethereum_trades.sql
@@ -0,0 +1,162 @@
+{{
+    config(
+        materialized='incremental',
+        alias='trades',
+        -- technically it is possible that the same order could be touched twice in a single block...
+        -- but this is the best we can do without keeping the event index
+        unique_key = ['tx_hash', 'order_uid'],
+        on_schema_change='fail',
+        file_format ='delta',
+        incremental_strategy='merge'
+    )
+}}
+
+-- Find the PoC Query here:
+WITH
+-- First subquery joins buy and sell token prices from prices.usd.
+-- Also deducts fee from sell amount.
+trades_with_prices AS (
+    SELECT evt_block_time           as block_time,
+           evt_tx_hash              as tx_hash,
+           owner                    as trader,
+           orderUid                 as order_uid,
+           sellToken                as sell_token,
+           buyToken                 as buy_token,
+           (sellAmount - feeAmount) as sell_amount,
+           buyAmount                as buy_amount,
+           feeAmount                as fee_amount,
+           ps.price                  as sell_price,
+           pb.price                  as buy_price
+    FROM {{ source('gnosis_protocol_v2_ethereum', 'GPv2Settlement_evt_Trade') }}
+             LEFT OUTER JOIN {{ ref('prices_usd') }} as ps
+                             ON sellToken = ps.contract_address
+                                 AND ps.minute = date_trunc('minute', evt_block_time)
+             LEFT OUTER JOIN {{ ref('prices_usd') }} as pb
+                             ON pb.contract_address = (
+                                 CASE
+                                     WHEN buyToken = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+                                         THEN '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+                                     ELSE buyToken
+                                     END)
+                                 AND pb.minute = date_trunc('minute', evt_block_time)
+    {% if is_incremental() %}
+    WHERE evt_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+),
+-- Second subquery gets token symbol and decimals from tokens.erc20 (to display units bought and sold)
+trades_with_token_units as (
+    SELECT block_time,
+           tx_hash,
+           order_uid,
+           trader,
+           sell_token                        as sell_token_address,
+           (CASE
+                WHEN ts.symbol IS NULL THEN sell_token
+                ELSE ts.symbol
+               END)                          as sell_token,
+           buy_token                         as buy_token_address,
+           (CASE
+                WHEN tb.symbol IS NULL THEN buy_token
+                WHEN buy_token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN 'ETH'
+                ELSE tb.symbol
+               END)                          as buy_token,
+           sell_amount / pow(10, ts.decimals) as units_sold,
+           sell_amount                       as atoms_sold,
+           buy_amount / pow(10, tb.decimals)  as units_bought,
+           buy_amount                        as atoms_bought,
+           -- We use sell value when possible and buy value when not
+           fee_amount / pow(10, ts.decimals)  as fee,
+           fee_amount                        as fee_atoms,
+           sell_price,
+           buy_price
+    FROM trades_with_prices
+             LEFT OUTER JOIN {{ ref('tokens_erc20') }} ts
+                             ON s.contract_address = sell_token
+             LEFT OUTER JOIN {{ ref('tokens_erc20') }} tb
+                             ON b.contract_address =
+                                (CASE
+                                     WHEN buy_token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+                                         THEN '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+                                     ELSE buy_token
+                                    END)
+),
+-- This, independent, aggregation defines a mapping of order_uid and trade
+order_id_and_trade as (
+    select
+        orderUid as order_id,
+        explode(trades) as trade
+    from {{ source('gnosis_protocol_v2_ethereum', 'GPv2Settlement_call_settle') }}
+     join (
+        select evt_tx_hash, evt_index, orderUid
+        from {{ source('gnosis_protocol_v2_ethereum', 'GPv2Settlement_evt_Trade') }}
+                 distribute by evt_tx_hash
+            sort by evt_index
+        ) as _
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+    on call_tx_hash = evt_tx_hash
+),
+-- TODO - replace this with abstraction `cow_protocol_ethereum_order_id_app_data_map
+uid_to_app_id as (
+    select
+        distinct order_id as uid, -- remove duplicate occurrences of order_id (when order is partially fillable)
+                 get_json_object(trade, '$.appData') as app_data,
+                 get_json_object(trade, '$.receiver') as receiver
+    from order_id_and_trade
+),
+
+
+valued_trades as (
+    SELECT block_time,
+           tx_hash,
+           order_uid,
+           trader,
+           sell_token_address,
+           sell_token,
+           buy_token_address,
+           buy_token,
+           case
+                 when lower(buy_token) > lower(sell_token) then concat(sell_token, '-', buy_token)
+                 else concat(buy_token, '-', sell_token)
+               end as token_pair,
+           units_sold,
+           atoms_sold,
+           units_bought,
+           atoms_bought,
+           (CASE
+                WHEN sell_price IS NOT NULL THEN
+                    -- Choose the larger of two prices when both not null.
+                    CASE
+                        WHEN buy_price IS NOT NULL and buy_price * units_bought > sell_price * units_sold
+                            then buy_price * units_bought
+                        ELSE sell_price * units_sold
+                        END
+                WHEN sell_price IS NULL AND buy_price IS NOT NULL THEN buy_price * units_bought
+                ELSE NULL::numeric
+               END)                                        as usd_value,
+           buy_price,
+           buy_price * units_bought                        as buy_value_usd,
+           sell_price,
+           sell_price * units_sold                         as sell_value_usd,
+           fee,
+           fee_atoms,
+           (CASE
+                WHEN sell_price IS NOT NULL THEN
+                    CASE
+                        WHEN buy_price IS NOT NULL and buy_price * units_bought > sell_price * units_sold
+                            then buy_price * units_bought * fee / units_sold
+                        ELSE sell_price * fee
+                        END
+                WHEN sell_price IS NULL AND buy_price IS NOT NULL
+                    THEN buy_price * units_bought * fee / units_sold
+                ELSE NULL::numeric
+               END)                                        as fee_usd,
+           app_data,
+           receiver
+    FROM trades_with_token_units
+             JOIN uid_to_app_id
+                  ON uid = order_uid
+)
+select * from valued_trades
+order by block_time desc
