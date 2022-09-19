06fe4768cdd131f6183ba720b406cd68da7b832e
soispoke <66172107+soispoke@users.noreply.github.com>
2022-09-19 15:26:17 +0100

    remove from alter properties macro and add post hook in model

--- models/nomad/ethereum/nomad_ethereum_view_bridge_transactions.sql
+++ models/nomad/ethereum/nomad_ethereum_view_bridge_transactions.sql
@@ -1,13 +1,17 @@
 {{
   config(
-            alias='view_bridge_transactions',
-            post_hook = "ALTER VIEW {{ this }} SET TBLPROPERTIES ('dune.public'='true',
-            'dune.data_explorer.blockchains'='[ethereum,solana]',
-            'dune.data_explorer.category'='abstraction',
-            'dune.data_explorer.abstraction.type'='project',
-            'dune.data_explorer.abstraction.name'='nomad',
-            'dune.data_explorer.contributors'='[hey]')"
-        )
+      alias='view_bridge_transactions',
+      post_hook = "
+      {%- if target.name == 'prod'-%} 
+      ALTER VIEW {{ this }} SET TBLPROPERTIES ('dune.public'='true',
+      'dune.data_explorer.blockchains'='[\"ethereum\"]',
+      'dune.data_explorer.category'='abstraction',
+      'dune.data_explorer.abstraction.type'='project',
+      'dune.data_explorer.abstraction.name'='nomad',
+      'dune.data_explorer.contributors'='[\"springzh\"]')
+      {%- else -%}
+      {%- endif -%}
+      ")
 }}
 
 with nomad_bridge_domains(domain_id, domain_name, domain_type) as (
