38c14db6f31c974799fd5797dae98b10fd944746
michael <mmm@ib.xyz>
2022-09-19 11:09:05 +0800

    fix compile error

--- macros/alter_table_properties.sql
+++ macros/alter_table_properties.sql
@@ -730,6 +730,24 @@ ALTER VIEW archipelago_ethereum.fees  SET TBLPROPERTIES ('dune.public'='true',
                                                         'dune.data_explorer.contributors'='["0xRob"]');
 {% endset %}
 
+{% set ironbank_ethereum_itokens %}
+ALTER VIEW ironbank_ethereum.itokens  SET TBLPROPERTIES ('dune.public'='true',
+                                                        'dune.data_explorer.blockchains'='["ethereum"]',
+                                                        'dune.data_explorer.category'='abstraction',
+                                                        'dune.data_explorer.abstraction.type'='project',
+                                                        'dune.data_explorer.abstraction.name'='ironbank',
+                                                        'dune.data_explorer.contributors'='["michael"]');
+{% endset %}
+
+{% set ironbank_optmism_itokens %}
+ALTER VIEW ironbank_optimism.itokens  SET TBLPROPERTIES ('dune.public'='true',
+                                                        'dune.data_explorer.blockchains'='["optimism"]',
+                                                        'dune.data_explorer.category'='abstraction',
+                                                        'dune.data_explorer.abstraction.type'='project',
+                                                        'dune.data_explorer.abstraction.name'='ironbank',
+                                                        'dune.data_explorer.contributors'='["michael"]');
+{% endset %}
+
 {% set ovm1_optimism_erc20_balances %}
 ALTER TABLE ovm1_optimism.erc20_balances SET TBLPROPERTIES ('dune.public'='true',
                                                 'dune.data_explorer.blockchains'='["optimism"]',
