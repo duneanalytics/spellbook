08ca7038b35590fbc65729a596770e4d42423255
Gent Rexha <gent@cow.fi>
2022-09-19 12:33:04 +0200

    Fixed unit tests.

--- tests/cow_protocol/ethereum/cow_protocol_ethereum_assert_solvers.sql
+++ tests/cow_protocol/ethereum/cow_protocol_ethereum_assert_solvers.sql
@@ -1,15 +1,16 @@
 -- Given a list of solvers, when we look at the active solvers, then we should see only 1 per each env and name
 WITH unit_test1
-    AS (SELECT *
+    AS (SELECT COUNT(address) as cnt
         FROM   {{ ref('cow_protocol_ethereum_solvers' )}}
-        WHERE  active = true AND COUNT(address) > 1
-        GROUP BY environment, name),
+        WHERE  active = true
+        GROUP BY environment, name
+        HAVING cnt > 1),
 -- Given a list of solvers, , then we should never see a solver who's both true and false for active
     unit_test2
-    AS (SELECT count(address)
+    AS (SELECT count(address) as cnt
         FROM   {{ ref('cow_protocol_ethereum_solvers' )}}
-        WHERE  COUNT(address) > 1
-        GROUP BY address, environment, name)
+        GROUP BY address, environment, name
+        HAVING cnt > 1)
 SELECT *
 FROM   (SELECT *
        FROM   unit_test1
