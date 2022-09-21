26e82c1a265e44b5e88f2a10d80a5966b7e13c05
hildobby <h.moulie@gmail.com>
2022-09-21 17:15:27 +0200

    Add OFAC sanction list (#1582)
    Brief comments on the purpose of your changes:
    
    
    *For Dune Engine V2*
    I've checked that:
    General checks:
    * [X] I tested the query on dune.com after compiling the model with dbt compile (compiled queries are written to the target directory)
    * [X] I used "refs" to reference other models in this repo and "sources" to reference raw or decoded tables 
    * [X] if adding a new model, I added a test
    * [X] the filename is unique and ends with .sql
    * [X] each sql file is a select statement and has only one view, table or function defined  
    * [X] column names are `lowercase_snake_cased`
    * [X] if adding a new model, I edited the dbt project YAML file with new directory path for both models and seeds (if applicable)
    * [X] if adding a new model, I edited the alter table macro to display new database object (table or view) in UI explorer
    * [X] if adding a new materialized table, I edited the optimize table macro
    
    Join logic:
    * [X] if joining to base table (i.e. ethereum transactions or traces), I looked to make it an inner join if possible
    
    Incremental logic:
    * [X] I used is_incremental & not is_incremental jinja block filters on both base tables and decoded tables
      * [X] where block_time >= date_trunc("day", now() - interval '1 week')
    * [X] if joining to base table (i.e. ethereum transactions or traces), I applied join condition where block_time >= date_trunc("day", now() - interval '1 week')
    * [X] if joining to prices view, I applied join condition where minute >= date_trunc("day", now() - interval '1 week')

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -15,6 +15,8 @@ SELECT * FROM {{ ref('labels_funds') }}
 UNION
 SELECT * FROM {{ ref('labels_submitted_contracts') }}
 UNION
+SELECT * FROM {{ ref('labels_ofac_sanctionned_ethereum') }}
+UNION
 -- Query Labels
 SELECT * FROM {{ ref('labels_nft') }}
 UNION
