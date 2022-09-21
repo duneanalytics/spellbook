0f03596e11c80615ad9c2ee5aedd9662c2649723
Ben Smith <bh2smith@gmail.com>
2022-09-21 08:55:01 +0200

    update macros for batches table

--- macros/optimize_tables.sql
+++ macros/optimize_tables.sql
@@ -99,8 +99,12 @@ OPTIMIZE archipelago_ethereum.events;
 OPTIMIZE labels.all;
 {% endset %}
 
-{% set cow_protocol_ethereum_table_optimizer %}
+{% set cow_protocol_ethereum_trades %}
 OPTIMIZE cow_protocol_ethereum.trades;
+{% endset %}
+
+{% set cow_protocol_ethereum_batches %}
+OPTIMIZE cow_protocol_ethereum.batches;
 {% endset %}
 
 
@@ -127,6 +131,7 @@ OPTIMIZE cow_protocol_ethereum.trades;
 {% do run_query(x2y2_ethereum_events) %}
 {% do run_query(archipelago_ethereum_events) %}
 {% do run_query(labels_all) %}
-{% do run_query(cow_protocol_ethereum_table_optimizer) %}
+{% do run_query(cow_protocol_ethereum_trades) %}
+{% do run_query(cow_protocol_ethereum_batches) %}
 {% do log("Tables Optimized", info=True) %}
 {% endmacro %}
