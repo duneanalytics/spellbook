7ac09a6588592d300e393339e499a15df51c1c24
Ben Smith <bh2smith@gmail.com>
2022-09-22 09:31:46 +0200

    first proposed version of aggregator_trades, amm_trades and and dex_trades

---
+++ models/dex/dex_aggregator_trades.sql
@@ -0,0 +1,43 @@
+{{ config(
+        alias ='aggregator_trades',
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "sector",
+                                "dex",
+                                \'["bh2smith"]\') }}'
+        )
+}}
+SELECT *
+FROM
+    (
+--         SELECT
+--             blockchain,
+--             project,
+--             version,
+--             block_date,
+--             block_time,
+--             token_bought_symbol,
+--             token_sold_symbol,
+--             token_pair,
+--             token_bought_amount,
+--             token_sold_amount,
+--             token_bought_amount_raw,
+--             token_sold_amount_raw,
+--             amount_usd,
+--             token_bought_address,
+--             token_sold_address,
+--             taker,
+-- -- I don't believe this is necessary here... unless for 1inch, the maker is the router contract..
+--             NULL as maker,
+--             project_contract_address,
+--             tx_hash,
+--             tx_from,
+--             tx_to,
+--             trace_address,
+--             evt_index,
+--             unique_trade_id
+--         FROM {{ ref('cow_protocol_trades') }}
+        /*
+        UNION
+        <add future protocols here>
+        */
+    )
