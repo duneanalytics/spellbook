6a1ba26c9ebe04aa591223d92c7f802cd3ce1529
hildobby <h.moulie@gmail.com>
2022-09-22 13:45:53 +0200

    Add bridge addresses (#1597)
    * added bridges
    
    * cex>bridges
    
    * added ;
    
    * added missing row
    
    * add missing column
    
    * remove duplicates
    
    * remove arbitrum bridge because already part of the dune submitted contracts
    
    Co-authored-by: soispoke <66172107+soispoke@users.noreply.github.com>

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -15,6 +15,8 @@ SELECT * FROM {{ ref('labels_funds') }}
 UNION
 SELECT * FROM {{ ref('labels_submitted_contracts') }}
 UNION
+SELECT * FROM {{ ref('labels_bridges') }}
+UNION
 SELECT * FROM {{ ref('labels_ofac_sanctionned_ethereum') }}
 UNION
 -- Query Labels
