092551f8796bf2b7e6303ab502c6b773c34d464b
Abimbola Adebayo <bim.adebayo@gmail.com>
2022-09-23 00:27:31 +0100

    add more tests

--- models/balancer/ethereum/balancer_ethereum_balances.sql
+++ models/balancer/ethereum/balancer_ethereum_balances.sql
@@ -1,6 +1,6 @@
 {{ config(
     alias = 'balances',
-    post_hook='{{ expose_spells(\'["ethereum"]\',
+    post_hook = '{{ expose_spells(\'["ethereum"]\',
                                             "sector",
                                             "balancer",
                                             \'["justabi"]\') }}'
