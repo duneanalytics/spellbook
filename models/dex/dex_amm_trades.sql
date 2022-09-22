4fbdf9409807a6609874328bfaada023670edc7c
Ben Smith <bh2smith@gmail.com>
2022-09-22 10:07:08 +0200

    remove contributor to minimize diff

--- models/dex/dex_amm_trades.sql
+++ models/dex/dex_amm_trades.sql
@@ -3,7 +3,7 @@
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "sector",
                                 "dex",
-                                \'["jeff-dude", "bh2smith"]\') }}'
+                                \'["jeff-dude"]\') }}'
         )
 }}
 SELECT *
