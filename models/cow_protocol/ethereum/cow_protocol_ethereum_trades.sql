071d5414bd9ce74a69e12041e81a2c8920c0603f
Benjamin Smith <bh2smith@users.noreply.github.com>
2022-09-22 17:31:38 +0200

    Update models/cow_protocol/ethereum/cow_protocol_ethereum_trades.sql

--- models/cow_protocol/ethereum/cow_protocol_ethereum_trades.sql
+++ models/cow_protocol/ethereum/cow_protocol_ethereum_trades.sql
@@ -2,7 +2,7 @@
         alias='trades',
         materialized='incremental',
         unique_key = ['tx_hash', 'order_uid', 'evt_index'],
-        on_schema_change='fail',
+        on_schema_change='sync_all_columns',
         file_format ='delta',
         incremental_strategy='merge',
         post_hook='{{ expose_spells(\'["ethereum"]\',
