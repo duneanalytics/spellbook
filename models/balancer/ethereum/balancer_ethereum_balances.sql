8f13b4efa0a57d2a6f66851878230120c9556a22
1chioku <102664310+1chioku@users.noreply.github.com>
2022-09-23 06:48:49 +0400

    Spellbook Workshop Submission (Updates & Optmiz)

--- models/balancer/ethereum/balancer_ethereum_balances.sql
+++ models/balancer/ethereum/balancer_ethereum_balances.sql
@@ -2,7 +2,7 @@
 
 WITH calendar AS -- This CTE generates a series of continuous day values
     (
-    SELECT explode(sequence(to_date('2020-07-04'), current_date, interval 1 day)) AS day-- 2020-07-04 is the day on which the first Balancer V1 pool was created
+    SELECT explode(sequence(to_date('2020-02-28'), current_date, interval 1 day)) AS day -- 2020-02-28 is the day on which the first Balancer V1 pool was created by the factory contract
     ),
     
 balancer_v1_pools AS -- This CTE returns the list of all pools that have been created by the Balancer V1 factory contract
@@ -28,21 +28,39 @@ balancer_inflows AS -- This CTE returns all the inflows of ERC20 to Balancer V1 
         date_trunc('day', e.evt_block_time) AS day,
         e.contract_address AS token,
         SUM(e.value) AS amount
-    FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
+    FROM
+        (
+        SELECT -- This subquery optimises the greater query by decreasing the data set from erc20_ethereum.evt_transfer
+            `to` AS address,
+            evt_block_time,
+            contract_address,
+            value
+        FROM {{ source('erc20_ethereum', 'evt_transfer') }}
+        WHERE evt_block_time >= '2020-02-28 00:00' -- 2020-02-28 is the day on which the first Balancer V1 pool was created by the factory contract
+        ) e
     INNER JOIN balancer_v1_pools p
-        ON e.`to` = p.pools
+        ON e.address = p.pools
     -- WHERE e.contract_address IN (SELECT * FROM balancer_supported_ERC20) -- This is a temporary placeholder for an ERC20 filter
     GROUP BY 1, 2, 3
 
     UNION ALL
 
     SELECT -- This query returns all the inflows of ERC20 to the Balancer V2 vault contract
-        e.`to` as pool,
+        e.address as pool,
         date_trunc('day', e.evt_block_time) AS day,
         e.contract_address AS token,
         SUM(e.value) AS amount
-    FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
-    WHERE e.`to` = '0xba12222222228d8ba445958a75a0704d566bf2c8' -- Balancer V2 vault contract
+    FROM
+        (
+        SELECT -- This subquery optimises the greater query by decreasing the data set from erc20_ethereum.evt_transfer
+            `to` AS address,
+            evt_block_time,
+            contract_address,
+            value
+        FROM {{ source('erc20_ethereum', 'evt_transfer') }}
+        WHERE evt_block_time >= '2021-04-21 00:00' -- 2021-04-21 is the day on which the first Balancer V2 inbound transaction was effected
+        ) e
+    WHERE e.address = '0xba12222222228d8ba445958a75a0704d566bf2c8' -- Balancer V2 vault contract
     -- AND e.contract_address IN (SELECT * FROM balancer_supported_ERC20) -- This is a temporary placeholder for an ERC20 filter
     GROUP BY 1, 2, 3
     ),
@@ -54,21 +72,39 @@ balancer_outflows AS -- This CTE returns all the outflows of ERC20 from Balancer
         date_trunc('day', e.evt_block_time) AS day,
         e.contract_address AS token,
         -SUM(e.value) AS amount
-    FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
+    FROM
+        (
+        SELECT -- This subquery optimises the greater query by decreasing the data set from erc20_ethereum.evt_transfer
+            `from` AS address,
+            evt_block_time,
+            contract_address,
+            value
+        FROM {{ source('erc20_ethereum', 'evt_transfer') }}
+        WHERE evt_block_time >= '2020-02-28 00:00' -- 2020-02-28 is the day on which the first Balancer V1 pool was created by the factory contract
+        ) e
     INNER JOIN balancer_v1_pools p
-        ON e.`from` = p.pools
+        ON e.address = p.pools
     -- WHERE e.contract_address IN (SELECT * FROM balancer_supported_ERC20) -- This is a temporary placeholder for an ERC20 filter
     GROUP BY 1, 2, 3
 
     UNION ALL
 
     SELECT -- This query returns all the outflows of ERC20 from the Balancer V2 vault contract
-        e.`from` as pool,
+        e.address as pool,
         date_trunc('day', e.evt_block_time) AS day,
         e.contract_address AS token,
         -SUM(e.value) AS amount
-    FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
-    WHERE e.`from` = '0xba12222222228d8ba445958a75a0704d566bf2c8' -- Balancer V2 vault contract
+    FROM
+        (
+        SELECT -- This subquery optimises the greater query by decreasing the data set from erc20_ethereum.evt_transfer
+            `from` AS address,
+            evt_block_time,
+            contract_address,
+            value
+        FROM {{ source('erc20_ethereum', 'evt_transfer') }}
+        WHERE evt_block_time >= '2021-04-21 00:00' -- 2021-04-21 is the day on which the first Balancer V2 inbound transaction was effected
+        ) e
+    WHERE e.address = '0xba12222222228d8ba445958a75a0704d566bf2c8' -- Balancer V2 vault contract
     -- AND e.contract_address IN (SELECT * FROM balancer_supported_ERC20) -- This is a temporary placeholder for an ERC20 filter
     GROUP BY 1, 2, 3
     ) ,
@@ -89,16 +125,16 @@ running_cumulative_balances_by_token AS  -- This CTE returns all the cumulative 
             y.token,
             Lead(y.day, 1, now()) OVER (PARTITION BY y.token, y.pool ORDER BY y.day ASC) AS day_of_next_change, -- Extrapolated from last values
             SUM(y.amount) OVER (PARTITION BY y.pool, y.token ORDER BY y.day ASC) AS cumulative_amount
-        FROM -- This sub query performs a SUM function on inflows and outflows to get net daily flows to/from Balancer
+        FROM
             (
-            SELECT
+            SELECT  -- This sub query performs a SUM function on inflows and outflows to get net daily flows to/from Balancer
                 x.pool,
                 x.day,
                 x.token,
                 SUM(COALESCE(x.amount, 0)) AS amount
-            FROM  -- This sub query aggregates all inflows and outflows from the Balancer inflow and outflow CTEs
+            FROM
                 (
-                SELECT *
+                SELECT * -- This sub query aggregates all inflows and outflows from the Balancer inflow and outflow CTEs
                 FROM balancer_inflows
                 
                 UNION ALL
