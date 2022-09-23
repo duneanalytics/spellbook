dad78de2cb4ee54d8a06a41815938b524cb568c8
Meghan <msheintz@gmail.com>
2022-09-23 09:13:07 -0400

    elif

--- scripts/pr_job_utils.py
+++ scripts/pr_job_utils.py
@@ -29,7 +29,7 @@ class PRJobDepedencyManager:
             raise Exception(bash_response)
         if 'No nodes selected!' in bash_response:
             modified_objects = []
-        if 'does not match any nodes' in bash_response:
+        elif 'does not match any nodes' in bash_response:
             modified_objects = []
         else:
             modified_names = bash_response.split('\n')
