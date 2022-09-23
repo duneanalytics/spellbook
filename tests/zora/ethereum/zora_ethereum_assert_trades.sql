cec8a9f99ebc8c65029115a744da8d70758bf918
Hildebert Moulié <h.moulie@gmail.com>
2022-09-23 16:47:16 +0200

    fix2

--- tests/zora/ethereum/zora_ethereum_assert_trades.sql
+++ tests/zora/ethereum/zora_ethereum_assert_trades.sql
@@ -116,15 +116,6 @@ WITH raw_events AS (
   FROM {{ source('zora_ethereum','Market_evt_BidFinalized') }}
   WHERE evt_block_time >= '2021-01-30'
   AND evt_block_time < NOW() - interval '1 day' -- allow some head desync
-  UNION
-  SELECT evt_block_time AS raw_block_time
-  , evt_tx_hash AS raw_tx_hash
-  , tokenContract AS raw_nft_contract_address
-  , tokenId AS raw_token_id
-  , evt_tx_hash || tokenContract || tokenId AS raw_unique_trade_id
-  FROM 
-  WHERE evt_block_time >= '2021-01-30'
-  AND evt_block_time < NOW() - interval '1 day' -- allow some head desync
   )
 
 , processed_events AS (
