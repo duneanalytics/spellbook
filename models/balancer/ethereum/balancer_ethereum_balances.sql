99082fe431c56c575f21bfb073601bc69c124f8d
Deepanjal Gupta <deepanjalgupta@192.168.1.6>
2022-09-25 20:50:14 +0530

    added the sql + souce file code

--- models/balancer/ethereum/balancer_ethereum_balances.sql
+++ models/balancer/ethereum/balancer_ethereum_balances.sql
@@ -1,34 +1,35 @@
-    WITH pools AS (
+    WITH pools AS 
+    (
         SELECT pool as pools
-        FROM balancer."BFactory_evt_LOG_NEW_POOL"
+        FROM {{ source('balancer_ethereum', 'BFactory_evt_LOG_NEW_POOL') }}
     ),
     
     joins AS (
         SELECT p.pools as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, SUM(value) AS amount
-        FROM erc20."ERC20_evt_Transfer" e
-        INNER JOIN pools p ON e."to" = p.pools
+        FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
+        INNER JOIN pools p ON e.`to` = p.pools
         GROUP BY 1, 2, 3
 
         UNION ALL
 
-        SELECT e."to" as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, SUM(value) AS amount
-        FROM erc20."ERC20_evt_Transfer" e
-        WHERE e."to" = '\xBA12222222228d8Ba445958a75a0704d566BF2C8'
+        SELECT e.`to` as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, SUM(value) AS amount
+        FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
+        WHERE e.`to` = '0xba12222222228d8ba445958a75a0704d566bf2c8'
         GROUP BY 1, 2, 3
 
     ),
 
     exits AS (
         SELECT p.pools as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, -SUM(value) AS amount
-        FROM erc20."ERC20_evt_Transfer" e
-        INNER JOIN pools p ON e."from" = p.pools   
+        FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
+        INNER JOIN pools p ON e.from = p.pools   
         GROUP BY 1, 2, 3
 
         UNION ALL
 
-        SELECT e."from" as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, -SUM(value) AS amount
-        FROM erc20."ERC20_evt_Transfer" e
-        WHERE e."from" = '\xBA12222222228d8Ba445958a75a0704d566BF2C8'
+        SELECT e.from as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, -SUM(value) AS amount
+        FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
+        WHERE e.from = '0xba12222222228d8ba445958a75a0704d566bf2c8'
         GROUP BY 1, 2, 3
     ),
     
@@ -44,7 +45,7 @@
     
     cumulative_balance_by_token AS (
         SELECT 
-            pool, 
+            pool,  
             token, 
             day, 
             LEAD(day, 1, now()) OVER (PARTITION BY token, pool ORDER BY day) AS day_of_next_change,
@@ -53,7 +54,7 @@
     ),
     
     calendar AS (
-        SELECT generate_series('2020-01-01'::timestamp, CURRENT_DATE, '1 day'::interval) AS day
+        SELECT explode(sequence(to_date('2022-01-01'), current_date, interval 1 day))
     ),
     
     running_cumulative_balance_by_token AS (
