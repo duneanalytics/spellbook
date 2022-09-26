5d36452cd3e443d898208cb713c15d475a5862b7
soispoke <66172107+soispoke@users.noreply.github.com>
2022-09-26 09:53:57 +0100

    update/upgrade sources for contract labels

--- models/labels/contracts/labels_contracts.sql
+++ models/labels/contracts/labels_contracts.sql
@@ -1,4 +1,4 @@
-{{config(alias='submitted_contracts',
+{{config(alias='contracts',
         post_hook='{{ expose_spells(\'["ethereum", "arbitrum", "gnosis", "optimism", "bnb", "avalanche_c"]\',
                                     "sector",
                                     "labels",
@@ -8,59 +8,59 @@
 SELECT array('ethereum') as blockchain,
        address, 
        concat(upper(substring(namespace,1,1)),substring(namespace,2)) || ': ' || name as name,
-       'submitted_contracts' as category,
+       'contracts' as category,
        'soispoke' as contributor,
        'query' AS source,
-       date('2022-09-23') as created_at,
+       date('2022-09-26') as created_at,
        now() as modified_at
-FROM {{ source('ethereum','contracts_submitted') }} 
+FROM {{ source('ethereum','contracts') }} 
 UNION 
 SELECT array('gnosis') as blockchain,
        address, 
        concat(upper(substring(namespace,1,1)),substring(namespace,2)) || ': ' || name as name,
-       'submitted_contracts' as category,
+       'contracts' as category,
        'soispoke' as contributor,
        'query' AS source,
-       date('2022-09-23') as created_at,
+       date('2022-09-26') as created_at,
        now() as modified_at
-FROM {{ source('gnosis','contracts_submitted') }} 
+FROM {{ source('gnosis','contracts') }} 
 UNION 
 SELECT array('avalanche_c') as blockchain,
        address, 
        concat(upper(substring(namespace,1,1)),substring(namespace,2)) || ': ' || name as name,
-       'submitted_contracts' as category,
+       'contracts' as category,
        'soispoke' as contributor,
        'query' AS source,
-       date('2022-09-23') as created_at,
+       date('2022-09-26') as created_at,
        now() as modified_at
-FROM {{ source('avalanche_c','contracts_submitted') }} 
+FROM {{ source('avalanche_c','contracts') }} 
 UNION 
 SELECT array('arbitrum') as blockchain,
        address, 
        concat(upper(substring(namespace,1,1)),substring(namespace,2)) || ': ' || name as name,
-       'submitted_contracts' as category,
+       'contracts' as category,
        'soispoke' as contributor,
        'query' AS source,
-       date('2022-09-23') as created_at,
+       date('2022-09-26') as created_at,
        now() as modified_at
-FROM {{ source('arbitrum','contracts_submitted') }} 
+FROM {{ source('arbitrum','contracts') }} 
 UNION 
 SELECT array('bnb') as blockchain,
        address, 
        concat(upper(substring(namespace,1,1)),substring(namespace,2)) || ': ' || name as name,
-       'submitted_contracts' as category,
+       'contracts' as category,
        'soispoke' as contributor,
        'query' AS source,
-       date('2022-09-23') as created_at,
+       date('2022-09-26') as created_at,
        now() as modified_at
-FROM {{ source('bnb','contracts_submitted') }} 
+FROM {{ source('bnb','contracts') }} 
 UNION 
 SELECT array('optimism') as blockchain,
        address, 
        concat(upper(substring(namespace,1,1)),substring(namespace,2)) || ': ' || name as name,
-       'submitted_contracts' as category,
+       'contracts' as category,
        'soispoke' as contributor,
        'query' AS source,
-       date('2022-09-23') as created_at,
+       date('2022-09-26') as created_at,
        now() as modified_at
-FROM {{ source('optimism','contracts_submitted') }} 
+FROM {{ source('optimism','contracts') }} 
