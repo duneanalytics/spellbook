5fc18b9bc57cfa9acf38925af083cee624fa44b3
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-09-26 17:49:02 +0530

    Update aave_supply_test.sql
    Change model name

--- tests/aave/ethereum/aave_supply_test.sql
+++ tests/aave/ethereum/aave_supply_test.sql
@@ -3,7 +3,7 @@ with unit_test1
                  when usd_amount = -21150 then true
                  else false
                end as test
-        from   {{ ref('aave_v2_supply' )}}
+        from   {{ ref('aave_v2_ethereum_supply' )}}
         where  token_address = '0x514910771af9ca656af840dff83e8264ecf986ca'
                and depositor = '0x4767192455266e422386d14991d697a418c63225'
                and evt_block_time = '2022-09-21 18:09'),
@@ -12,7 +12,7 @@ with unit_test1
                  when amount = 250 then true
                  else false
                end as test
-        from   {{ ref('aave_v2_supply' )}}
+        from   {{ ref('aave_v2_ethereum_supply' )}}
         where  symbol = 'SNX'
                and depositor = '0x1e18b8b6f27568023e0b577cbee1889391b2f444'
                and evt_block_time = '2022-09-24 00:05')
