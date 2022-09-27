b21a46b12a86519857a827cb188dc1d558ce9aee
soispoke <66172107+soispoke@users.noreply.github.com>
2022-09-27 14:04:49 +0100

    Update foundation_ethereum_events.sql

--- models/foundation/ethereum/foundation_ethereum_events.sql
+++ models/foundation/ethereum/foundation_ethereum_events.sql
@@ -206,6 +206,7 @@ LEFT JOIN {{ source('ethereum','traces') }} ett ON ett.block_time=t.block_time
     AND cast(ett.value as string) = cast(t.royalty_fee_amount_raw as string)
     AND ett.to!=t.project_contract_address
     AND t.royalty_fee_amount/t.amount_original < 0.5
+    and ett.success = true
     {% if is_incremental() %}
     -- this filter will only be applied on an incremental run
     AND ett.block_time >=  date_trunc("day", now() - interval '1 week')
