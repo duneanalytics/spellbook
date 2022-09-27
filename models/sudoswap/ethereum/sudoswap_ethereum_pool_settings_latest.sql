f81da23b89f41b7f74208c623fffe494a812eda6
0xRob <0xRobin5@gmail.com>
2022-09-27 16:05:06 +0200

    test witouth incremental latest table

--- models/sudoswap/ethereum/sudoswap_ethereum_pool_settings_latest.sql
+++ models/sudoswap/ethereum/sudoswap_ethereum_pool_settings_latest.sql
@@ -35,10 +35,10 @@ with
             AND tx.block_time >= '{{project_start_date}}'
             AND evt.evt_block_time >= '{{project_start_date}}'
             {% endif %}
-            {% if is_incremental() %}
-            AND tx.block_time >= date_trunc("day", now() - interval '1 week')
-            AND evt.evt_block_time >= date_trunc("day", now() - interval '1 week')
-            {% endif %}
+--            {% if is_incremental() %}
+--            AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+--            AND evt.evt_block_time >= date_trunc("day", now() - interval '1 week')
+--            {% endif %}
         ) foo
     WHERE ordering = 1
 )
@@ -60,10 +60,10 @@ with
             AND tx.block_time >= '{{project_start_date}}'
             AND evt.evt_block_time >= '{{project_start_date}}'
             {% endif %}
-            {% if is_incremental() %}
-            AND tx.block_time >= date_trunc("day", now() - interval '1 week')
-            AND evt.evt_block_time >= date_trunc("day", now() - interval '1 week')
-            {% endif %}
+--            {% if is_incremental() %}
+--            AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+--            AND evt.evt_block_time >= date_trunc("day", now() - interval '1 week')
+--            {% endif %}
         ) foo
     WHERE ordering = 1
 )
@@ -85,10 +85,10 @@ with
             AND tx.block_time >= '{{project_start_date}}'
             AND evt.evt_block_time >= '{{project_start_date}}'
             {% endif %}
-            {% if is_incremental() %}
-            AND tx.block_time >= date_trunc("day", now() - interval '1 week')
-            AND evt.evt_block_time >= date_trunc("day", now() - interval '1 week')
-            {% endif %}
+--            {% if is_incremental() %}
+--            AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+--            AND evt.evt_block_time >= date_trunc("day", now() - interval '1 week')
+--            {% endif %}
         ) foo
     WHERE ordering = 1
 )
@@ -120,30 +120,30 @@ with
     FROM
       {{ source('sudo_amm_ethereum','LSSVMPairFactory_call_createPairETH') }}
     WHERE call_success
-    {% if is_incremental() %}
-    AND call_block_time >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
+--    {% if is_incremental() %}
+--    AND call_block_time >= date_trunc("day", now() - interval '1 week')
+--    {% endif %}
 )
 
 -- we need to backfill columns from the existing data in order to have full rows
-{% if is_incremental() %}
-, full_settings_backfilled as (
-    select
-     coalesce(t1.pool_address,t3.pool_address) as pool_address
-    ,coalesce(t2.bonding_curve, t3.bonding_curve) as bonding_curve
-    ,coalesce(t1.pool_fee, t2.pool_fee, t3.pool_fee) as pool_fee
-    ,coalesce(t1.delta, t2.delta, t3.delta) as delta
-    ,coalesce(t1.spot_price, t2.spot_price, t3.spot_price) as spot_price
-    ,coalesce(t1.latest_update_time,t3.creation_block_time) as latest_update_time
-    from latest_settings t1
-    left join {{ this }} t2 ON
-        t1.pool_address = t2.pool_address
-    left join initial_settings t3
-    ON t1.pool_address = t3.pool_address
-)
-{% endif %}
+--{% if is_incremental() %}
+--, full_settings_backfilled as (
+--    select
+--     coalesce(t1.pool_address,t3.pool_address) as pool_address
+--    ,coalesce(t2.bonding_curve, t3.bonding_curve) as bonding_curve
+--    ,coalesce(t1.pool_fee, t2.pool_fee, t3.pool_fee) as pool_fee
+--    ,coalesce(t1.delta, t2.delta, t3.delta) as delta
+--    ,coalesce(t1.spot_price, t2.spot_price, t3.spot_price) as spot_price
+--    ,coalesce(t1.latest_update_time,t3.creation_block_time) as latest_update_time
+--    from latest_settings t1
+--    left join {{ this }} t2 ON
+--        t1.pool_address = t2.pool_address
+--    left join initial_settings t3
+--    ON t1.pool_address = t3.pool_address
+--)
+--{% endif %}
 
-{% if not is_incremental() %}
+--{% if not is_incremental() %}
 , full_settings_backfilled as (
     select
      coalesce(t1.pool_address,t3.pool_address) as pool_address
@@ -156,7 +156,7 @@ with
     left join initial_settings t3
     ON t1.pool_address = t3.pool_address
 )
-{% endif %}
+--{% endif %}
 
 
 select * from full_settings_backfilled
