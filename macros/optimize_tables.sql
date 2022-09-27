af5648c5b408a230baa400418052ab95fae0b99e
0xRob <0xRobin5@gmail.com>
2022-09-27 12:50:00 +0200

    combine OPTIMIZEs

--- macros/optimize_tables.sql
+++ macros/optimize_tables.sql
@@ -27,8 +27,12 @@ OPTIMIZE safe_ethereum.safes;
 OPTIMIZE seaport_ethereum.transfers;
 {% endset %}
 
-{% set sudoswap_ethereum_events %}
+{% set sudoswap_ethereum %}
 OPTIMIZE sudoswap_ethereum.events;
+OPTIMIZE sudoswap_ethereum.pool_creations;
+OPTIMIZE sudoswap_ethereum.pool_settings_latest;
+OPTIMIZE sudoswap_ethereum.pool_balance_agg_day;
+OPTIMIZE sudoswap_ethereum.pool_trades_agg_day;
 {% endset %}
 
 {% set cryptopunks_ethereum_events %}
@@ -151,14 +155,6 @@ OPTIMIZE cow_protocol_ethereum.trades;
 OPTIMIZE cow_protocol_ethereum.batches;
 {% endset %}
 
-{% set sudoswap_ethereum_pool_balance_changes %}
-OPTIMIZE sudoswap_ethereum.pool_balance_changes;
-{% endset %}
-
-{% set sudoswap_ethereum_pool_trades %}
-OPTIMIZE sudoswap_ethereum.pool_trades;
-{% endset %}
-
 
 {% do run_query(looksrare_ethereum_events) %}
 {% do run_query(magiceden_solana_events) %}
@@ -168,7 +164,7 @@ OPTIMIZE sudoswap_ethereum.pool_trades;
 {% do run_query(safe_ethereum_safes) %}
 {% do run_query(ens_resolver_addresses) %}
 {% do run_query(seaport_ethereum_transfers) %}
-{% do run_query(sudoswap_ethereum_events) %}
+{% do run_query(sudoswap_ethereum) %}
 {% do run_query(cryptopunks_ethereum_events) %}
 {% do run_query(element_ethereum_events) %}
 {% do run_query(element_bnb_events) %}
@@ -196,7 +192,5 @@ OPTIMIZE sudoswap_ethereum.pool_trades;
 {% do run_query(labels_all) %}
 {% do run_query(cow_protocol_ethereum_trades) %}
 {% do run_query(cow_protocol_ethereum_batches) %}
-{% do run_query(sudoswap_ethereum_pool_balance_changes) %}
-{% do run_query(sudoswap_ethereum_pool_trades) %}
 {% do log("Tables Optimized", info=True) %}
 {% endmacro %}
