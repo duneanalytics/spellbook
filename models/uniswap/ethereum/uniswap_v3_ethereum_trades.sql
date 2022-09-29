ee3be08964de91d8e049a2564992826e1f0cb593
jeff-dude <jeff@dune.xyz>
2022-09-28 20:46:11 -0400

    fix datatype issues

--- models/uniswap/ethereum/uniswap_v3_ethereum_trades.sql
+++ models/uniswap/ethereum/uniswap_v3_ethereum_trades.sql
@@ -22,12 +22,12 @@ WITH dexs AS
         t.evt_block_time AS block_time
         ,t.recipient AS taker
         ,'' AS maker
-        ,CASE WHEN amount0 < 0 THEN abs(amount0) ELSE abs(amount1) END AS token_bought_amount_raw -- when amount0 is negative it means trader_a is buying token0 from the pool
-        ,CASE WHEN amount0 < 0 THEN abs(amount1) ELSE abs(amount0) END AS token_sold_amount_raw
+        ,CASE WHEN amount0 < '0' THEN abs(amount0) ELSE abs(amount1) END AS token_bought_amount_raw -- when amount0 is negative it means trader_a is buying token0 from the pool
+        ,CASE WHEN amount0 < '0' THEN abs(amount1) ELSE abs(amount0) END AS token_sold_amount_raw
         ,NULL AS amount_usd
-        ,CASE WHEN amount0 < 0 THEN f.token0 ELSE f.token1 END AS token_bought_address
-        ,CASE WHEN amount0 < 0 THEN f.token1 ELSE f.token0 END AS token_sold_address
-        ,t.contract_address as project_contract_address
+        ,CASE WHEN amount0 < '0' THEN f.token0 ELSE f.token1 END AS token_bought_address
+        ,CASE WHEN amount0 < '0' THEN f.token1 ELSE f.token0 END AS token_sold_address
+        ,CAST(t.contract_address as string) as project_contract_address
         ,t.evt_tx_hash AS tx_hash
         ,'' AS trace_address
         ,t.evt_index
