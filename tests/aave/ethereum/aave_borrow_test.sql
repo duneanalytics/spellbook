6db54cd9e55b2043e62a532371225f57550ee5c6
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-09-30 14:48:09 +0530

    Update aave_borrow_test.sql
    Updated the test for new model

--- tests/aave/ethereum/aave_borrow_test.sql
+++ tests/aave/ethereum/aave_borrow_test.sql
@@ -3,16 +3,17 @@ with unit_test1
                  when usd_amount = 499785 then true
                  else false
                end as test
-        from   {{ ref('aave_v2_ethereum_borrow' )}}
+        from   {{ ref('aave_ethereum_borrow' )}}
         where  token_address = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
                and borrower = '0xec46dd165ee2d4af460a9c3d01b5a4c9516c9c3f'
-               and evt_block_time = '2022-09-22 10:45'),
+               and evt_block_time = '2022-09-22 10:45'
+               and evt_tx_hash = '0x4c052a2865b828ef00dd2840870c08d2074930571c00c1f57f579acbab3e25c8'),
     unit_test2
     as (select case
                  when amount = -100 then true
                  else false
                end as test
-        from   {{ ref('aave_v2_ethereum_borrow' )}}
+        from   {{ ref('aave_ethereum_borrow' )}}
         where  symbol = 'DAI'
                and repayer = '0x0d67d6aab7dab14da3b72ca70e0b83b74ad7e88f'
                and evt_block_time = '2022-09-22 19:40')
