b48d6c804ce5b59c4209b0425902ce73b7bafa3c
Huang Geyang <Sukebeta@outlook.com>
2022-09-30 20:19:32 +0800

    Add airswap to dex trades (#1669)
    * Add and implement dex.trades airswap v2 engine
    
    * Add dex_ethereum_airswap_trades to macro
    
    * Remove column unique_trade_id and refactor the model directory
    
    * Remove trace_address from unique_key
    
    * Add trade_id as part of unique_key as records may have same evt_index and tx_hash
    
    * add the airswap project in the dbt_project.yml file
    
    * Rename and add airswap_ethereum_trades model to the meta table dex.trades

--- models/dex/dex_trades.sql
+++ models/dex/dex_trades.sql
@@ -3,7 +3,7 @@
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "sector",
                                 "dex",
-                                \'["jeff-dude"]\') }}'
+                                \'["jeff-dude", "hosuke"]\') }}'
         )
 }}
 SELECT *
@@ -62,4 +62,32 @@ FROM
                 trace_address,
                 evt_index
         FROM {{ ref('curvefi_ethereum_trades') }}
+
+        UNION
+
+        SELECT
+                blockchain,
+                project,
+                version,
+                block_date,
+                block_time,
+                token_bought_symbol,
+                token_sold_symbol,
+                token_pair,
+                token_bought_amount,
+                token_sold_amount,
+                token_bought_amount_raw,
+                token_sold_amount_raw,
+                amount_usd,
+                token_bought_address,
+                token_sold_address,
+                taker,
+                maker,
+                project_contract_address,
+                tx_hash,
+                tx_from,
+                tx_to,
+                trace_address,
+                evt_index
+        FROM {{ ref('airswap_ethereum_trades') }}
 )
