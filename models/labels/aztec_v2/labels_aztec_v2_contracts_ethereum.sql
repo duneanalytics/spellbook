93f133ff495daa81ca79cc8468b0a83ddb2ec271
Henrystats <65398020+henrystats@users.noreply.github.com>
2022-10-01 03:57:07 +0100

    testing new branch

--- models/labels/aztec_v2/labels_aztec_v2_contracts_ethereum.sql
+++ models/labels/aztec_v2/labels_aztec_v2_contracts_ethereum.sql
@@ -88,7 +88,7 @@ with
                 '0xB4319947947781FFe91dDf96A32aF2D4693FEf64'
               ),
               (
-                'Unknown4',
+                'Unknown5',
                 'Bridge',
                 '1.0',
                 'UniswapDCABridge',
