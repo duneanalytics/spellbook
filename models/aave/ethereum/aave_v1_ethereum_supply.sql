3d01a43964256d5400a74e89c971c56e42efb3d0
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-10-04 22:38:43 +0530

    Add aave supply and borrow abstractions in dune v2 (#1631)
    * Create aave_v2_supply.sql
    
    Add aave supply abstraction
    
    * Update aave_ethereum_sources.yml
    
    Update the sources for aave_v2_supply
    
    * Update aave_ethereum_schema.yml
    
    Update the schema for aave_v2_supply
    
    * Create aave_supply_test.sql
    
    Added tests for aave_v2_supply
    
    * Update aave_ethereum_sources.yml
    
    Correct indentation
    
    * Update aave_v2_supply.sql
    
    correct the reference
    
    * Update aave_v2_supply.sql
    
    Correct reference
    
    * Update aave_ethereum_sources.yml
    
    Add prices table to sources
    
    * Update aave_ethereum_sources.yml
    
    Remove prices table
    
    * Create aave_v2_borrow.sql
    
    Add the aave borrow model
    
    * Update aave_v2_borrow.sql
    
    Add configuration
    
    * Update aave_ethereum_sources.yml
    
    Added the sources for aave_v2_borrow
    
    * Update aave_ethereum_schema.yml
    
    Added aave_v2_borrow in the schema
    
    * Update aave_ethereum_schema.yml
    
    Correcting indentation
    
    * Update aave_v2_borrow.sql
    
    Correcting error
    
    * Create aave_borrow_test
    
    Added tests for aave_v2_borrow model
    
    * Rename aave_borrow_test to aave_borrow_test.sql
    
    rename file
    
    * Update aave_v2_ethereum_interest_rates.sql
    
    Changing the configuration to make the abstraction public
    
    * Rename aave_v2_borrow.sql to aave_v2_ethereum_borrow.sql
    
    Change the model name
    
    * Rename aave_v2_supply.sql to aave_v2_ethereum_supply.sql
    
    Change the model name
    
    * Update aave_ethereum_schema.yml
    
    Change model names
    
    * Update aave_supply_test.sql
    
    Change model name
    
    * Update aave_borrow_test.sql
    
    Change model name
    
    * Update aave_v2_ethereum_supply.sql
    
    Changing the address format
    
    * Update aave_v2_ethereum_borrow.sql
    
    * Update aave_v2_ethereum_borrow.sql
    
    Add blockchain
    
    * Update aave_v2_ethereum_supply.sql
    
    Add blockchain
    
    * Update aave_ethereum_schema.yml
    
    Change sector from dex to lending
    
    * Update aave_v2_ethereum_borrow.sql
    
    Edit aave v2 to only include v2 transactions
    
    * Create aave_v1_ethereum_borrow.sql
    
    Add aave v1 borrow model
    
    * Create aave_v1_ethereum_supply.sql
    
    Add aave v1 supply model
    
    * Update aave_v2_ethereum_supply.sql
    
    Remove v1 transactions
    
    * Update aave_v1_ethereum_supply.sql
    
    Add comments
    
    * Update aave_ethereum_schema.yml
    
    Add v1 models to schema
    
    * Create aave_ethereum_borrow.sql
    
    Create a borrow table for all versions
    
    * Update aave_ethereum_borrow.sql
    
    Correct model
    
    * Create aave_ethereum_supply.sql
    
    Add model for all versions
    
    * Update aave_ethereum_schema.yml
    
    Add aave supply and borrow models in schema
    
    * Update aave_ethereum_borrow.sql
    
    correcting syntex
    
    * Update aave_ethereum_supply.sql
    
    correcting syntax
    
    * Update aave_ethereum_sources.yml
    
    Made the corrections
    
    * Update aave_ethereum_schema.yml
    
    Adding description for token amounts
    
    * Update aave_ethereum_sources.yml
    
    Making corrections
    
    * Update aave_v1_ethereum_borrow.sql
    
    Removing extra line
    
    * Update aave_v1_ethereum_supply.sql
    
    Removing extra line
    
    * Update aave_v2_ethereum_borrow.sql
    
    Removing extra line
    
    * Update aave_v2_ethereum_supply.sql
    
    Removing extra line
    
    * Update aave_borrow_test.sql
    
    Updated the test for new model
    
    * Update aave_supply_test.sql
    
    Updating tests for new model
    
    * Update aave_supply_test.sql
    
    * Update aave_supply_test.sql
    
    correcting error
    
    * standarize and finalize aave models
    
    Co-authored-by: jeff-dude <jeff@dune.xyz>

---
+++ models/aave/ethereum/aave_v1_ethereum_supply.sql
@@ -0,0 +1,86 @@
+{{ config(
+    schema = 'aave_v1_ethereum'
+    , alias='supply'
+    , post_hook='{{ expose_spells(\'["ethereum"]\',
+                                  "project",
+                                  "aave_v1",
+                                  \'["batwayne", "chuxinh"]\') }}'
+  )
+}}
+
+{% set aave_mock_address = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' %}
+{% set weth_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' %}
+
+SELECT 
+      version,
+      transaction_type,
+      erc20.symbol,
+      deposit.token as token_address, 
+      depositor,
+      withdrawn_to,
+      liquidator,
+      amount / concat('1e',erc20.decimals) AS amount,
+      (amount / concat('1e',p.decimals)) * price AS usd_amount,
+      evt_tx_hash,
+      evt_index,
+      evt_block_time,
+      evt_block_number 
+FROM (
+ SELECT 
+    '1' AS version,
+    'deposit' AS transaction_type,
+    CASE
+        WHEN _reserve = '{{aave_mock_address}}' THEN '{{weth_address}}' --Using WETH instead of Aave "mock" address
+        ELSE _reserve
+    END AS token,
+    _user AS depositor, 
+    NULL::string AS withdrawn_to,
+    NULL::string AS liquidator,
+    _amount AS amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+FROM {{ source('aave_ethereum','LendingPool_evt_Deposit') }}
+UNION ALL 
+SELECT 
+    '1' AS version,
+    'withdraw' AS transaction_type,
+    CASE
+        WHEN _reserve = '{{aave_mock_address}}' THEN '{{weth_address}}' --Using WETH instead of Aave "mock" address
+        ELSE _reserve
+    END AS token,
+    _user AS depositor,
+    _user AS withdrawn_to,
+    NULL::string AS liquidator,
+    - _amount AS amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+FROM {{ source('aave_ethereum','LendingPool_evt_RedeemUnderlying') }}
+UNION ALL 
+SELECT 
+    '1' AS version,
+    'deposit_liquidation' AS transaction_type,
+    CASE
+        WHEN _reserve = '{{aave_mock_address}}' THEN '{{weth_address}}' --Using WETH instead of Aave "mock" address
+        ELSE _collateral
+    END AS token,
+    _user AS depositor,
+    _liquidator AS withdrawn_to,
+    _liquidator AS liquidator,
+    - _liquidatedCollateralAmount AS amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+FROM {{ source('aave_ethereum','LendingPool_evt_LiquidationCall') }}
+) deposit
+LEFT JOIN {{ ref('tokens_ethereum_erc20') }} erc20
+    ON deposit.token = erc20.contract_address
+LEFT JOIN {{ source('prices','usd') }} p 
+    ON p.minute = date_trunc('minute', deposit.evt_block_time) 
+    AND p.contract_address = deposit.token 
+    AND p.blockchain = 'ethereum'
+;
