f3bcb0b158101e8562af136c6624355d5d425d86
0xRob <0xRobin5@gmail.com>
2022-10-05 16:51:19 +0200

    remove dynamic column extraction

--- tests/generic/check_seed.sql
+++ tests/generic/check_seed.sql
@@ -1,11 +1,16 @@
 -- this tests checks a model for every row in a seed file.
 -- you need to specify the matching columns and the columns to check for equality.
 
-{% test check_seed(model, seed_file_location) %}
-    {%- set unique_columns = config.get('unique_key') -%}
-    {%- set seed_check_columns = dbt_util.get_filtered_columns_in_relation(from=seed_file_location, except=unique_columns) -%}
-    {%- set seed_matching_columns = dbt_util.get_filtered_columns_in_relation(from=seed_file_location, except=seed_check_columns) -%}
+{% test check_seed(model, seed_file, match_columns=[], check_columns=[]) %}
+    {# --jinja comment
+--    I wish we could do something dynamic like this below, but it is prohibited by having a local database connection.. :(
+--    {%- set unique_columns = config.get('unique_key') -%}
+--    {%- set seed_check_columns = dbt_utils.get_filtered_columns_in_relation(from=seed_file, except=unique_columns) -%}
+--    {%- set seed_matching_columns = dbt_utils.get_filtered_columns_in_relation(from=seed_file, except=seed_check_columns) -%}
+    --jinja comment #}
 
+    {%- set seed_check_columns = check_columns -%}
+    {%- set seed_matching_columns = match_columns -%}
 
     -- check if the matching columns return singular results
     with matching_count_test as (
@@ -14,17 +19,17 @@
         count(model.{{seed_matching_columns[0]}}) as result,
         1 as expected,
         {%- for column_name in seed_matching_columns %}
-        seed.{{column_name}} {%- if not loop.last %},{% endif -%}
+        seed.{{column_name}} {% if not loop.last %},{% endif %}
         {% endfor -%}
-        from seed_file_location seed
-        left join model model
+        from {{seed_file}} seed
+        left join {{model}} model
         ON 1=1
         {%- for column_name in seed_matching_columns %}
         AND seed.{{column_name}} = model.{{column_name}}
         {% endfor -%}
         GROUP BY
         {%- for column_name in seed_matching_columns %}
-        seed.{{column_name}} {%- if not loop.last %},{% endif -%}
+        seed.{{column_name}} {% if not loop.last %},{% endif %}
         {% endfor -%}
     ) ,
 
@@ -39,10 +44,11 @@
             model.{{checked_column}} as result,
             seed.{{checked_column}} as expected,
             {%- for column_name in seed_matching_columns %}
-            seed.{{column_name}} {%- if not loop.last %},{% endif -%}
+            seed.{{column_name}} {% if not loop.last %},{% endif %}
             {% endfor -%}
-            from seed_file_location seed
-            left join model model
+
+            from {{seed_file}} seed
+            left join {{model}} model
             ON 1=1
             {%- for column_name in seed_matching_columns %}
             AND seed.{{column_name}} = model.{{column_name}}
@@ -52,6 +58,7 @@
         UNION ALL
         {% endif -%}
         {% endfor -%}
+
     )
 
 
