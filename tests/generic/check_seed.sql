08c631843f41912935a0920d1c35d8c8fbaad990
0xRob <0xRobin5@gmail.com>
2022-10-05 15:46:01 +0200

    check seed test

---
+++ tests/generic/check_seed.sql
@@ -0,0 +1,66 @@
+-- this tests checks a model for every row in a seed file.
+-- you need to specify the matching columns and the columns to check for equality.
+
+{% test check_seed(model, seed_file_location) %}
+    {%- set unique_columns = config.get('unique_key') -%}
+    {%- set seed_check_columns = dbt_util.get_filtered_columns_in_relation(from=seed_file_location, except=unique_columns) -%}
+    {%- set seed_matching_columns = dbt_util.get_filtered_columns_in_relation(from=seed_file_location, except=seed_check_columns) -%}
+
+
+    -- check if the matching columns return singular results
+    with matching_count_test as (
+        select
+        'mathing count' as test_description,
+        count(model.{{seed_matching_columns[0]}}) as result,
+        1 as expected,
+        {%- for column_name in seed_matching_columns %}
+        seed.{{column_name}} {%- if not loop.last %},{% endif -%}
+        {% endfor -%}
+        from seed_file_location seed
+        left join model model
+        ON 1=1
+        {%- for column_name in seed_matching_columns %}
+        AND seed.{{column_name}} = model.{{column_name}}
+        {% endfor -%}
+        GROUP BY
+        {%- for column_name in seed_matching_columns %}
+        seed.{{column_name}} {%- if not loop.last %},{% endif -%}
+        {% endfor -%}
+    ) ,
+
+    equality_tests as
+    (
+        {%- for checked_column in seed_check_columns %}
+        select
+        'equality test for column: {{checked_column}}' as test_description
+        ,test.*
+        from (
+            select
+            model.{{checked_column}} as result,
+            seed.{{checked_column}} as expected,
+            {%- for column_name in seed_matching_columns %}
+            seed.{{column_name}} {%- if not loop.last %},{% endif -%}
+            {% endfor -%}
+            from seed_file_location seed
+            left join model model
+            ON 1=1
+            {%- for column_name in seed_matching_columns %}
+            AND seed.{{column_name}} = model.{{column_name}}
+            {% endfor -%}
+        ) test
+        {%- if not loop.last %}
+        UNION ALL
+        {% endif -%}
+        {% endfor -%}
+    )
+
+
+    select * from (
+        select *
+        from matching_count_test
+        union all
+        select *
+        from equality_tests
+    ) all
+    where result != expected
+{% endtest %}
