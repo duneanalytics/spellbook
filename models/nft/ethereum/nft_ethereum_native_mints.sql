de6990ab4a52f9078bf51cf12fcb1136ab492aff
UmerHA <40663591+UmerHA@users.noreply.github.com>
2022-10-06 01:19:12 +0200

    wip commit 2
    - used sources & refs
    - align table aliases with other nft tables
    - added columns

--- models/nft/ethereum/nft_ethereum_native_mints.sql
+++ models/nft/ethereum/nft_ethereum_native_mints.sql
@@ -12,33 +12,33 @@ select
   'ethereum' as blockchain,
   null as project,
   null as version,
-  trf.evt_block_time as block_time,
-  trf.tokenId as token_id,
-  tk.name as collection,
+  erc721.evt_block_time as block_time,
+  erc721.tokenId as token_id,
+  tokens_nft.name as collection,
   0 as amount_usd, -- to do
   'erc721' as token_standard,
-  'Single Item Trade' as trade_type, -- to verify
-  1 as number_of_items, -- to verify
+  'Single Item Trade' as trade_type,
+  1 as number_of_items,
   'Buy' as trade_category, -- to verify
   'Mint' as evt_type,
-  trf.`from` as seller,
-  trf.`to` as buyer,
+  erc721.`from` as seller,
+  erc721.`to` as buyer,
   0 as amount_original, -- to do
   0 as amount_raw, -- to do
   'ETH' as currency_symbol,
   '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' as currency_contract,
-  trf.contract_address as nft_contract_address,
+  erc721.contract_address as nft_contract_address,
   null as project_contract_address,
   null as aggregator_name,
   null as aggregator_address,
-  trf.evt_tx_hash as tx_hash,
-  trf.evt_block_number as block_number,
-  trf.`from` as tx_from,
-  trf.`to` as tx_to,
-  'TODO' as unique_trade_id -- to do
+  erc721.evt_tx_hash as tx_hash,
+  erc721.evt_block_number as block_number,
+  erc721.`from` as tx_from,
+  erc721.`to` as tx_to,
+  erc721.contract_address || '-' || erc721.evt_tx_hash || '-' || erc721.tokenId || '-' || erc721.`from` as unique_trade_id -- to verify
 from
-  erc721_ethereum.evt_Transfer as trf
-  left join tokens.nft as tk on trf.contract_address = tk.contract_address
+  {{ source('erc721_ethereum','evt_transfer') }} erc721
+  left join {{ ref('tokens_nft') }} tokens_nft on erc721.contract_address = tokens_nft.contract_address
 where
   `from` = '0x0000000000000000000000000000000000000000'
 --limit
@@ -49,3 +49,10 @@ where
 -- to do: where to include in tranform pipelin leading to nft.mints?
 --			a) in nft_mints
 --			b) in nft_ethereum_events
+
+-- to do: get amount_original, amount_raw
+--			-> join on eth.tx; then amount_raw = value/(#mints in tx) ? 
+--			-> amount_original = amount_raw / 10e18 ?
+
+-- to do: get amount_usd
+--			-> join on prices table by block_time?
