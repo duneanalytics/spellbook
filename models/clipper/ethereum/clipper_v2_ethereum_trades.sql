a19d170ab76a125acc88cf3f0ab617bfbf6a47cf
0xRob <0xRobin5@gmail.com>
2022-10-05 13:41:00 +0200

    incremental filters on decoded event tables

--- models/clipper/ethereum/clipper_v2_ethereum_trades.sql
+++ models/clipper/ethereum/clipper_v2_ethereum_trades.sql
@@ -26,6 +26,13 @@ WITH event_data as (
         '' AS trace_address,
         evt_index
     FROM  {{ source('clipper_ethereum', 'ClipperCaravelExchange_evt_Swapped') }}
+    WHERE 1=1
+    {% if not is_incremental() %}
+    AND tx.block_time >= '{{project_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+    AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
 )
 
 SELECT
