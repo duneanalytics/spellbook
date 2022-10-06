07324e1f0fcce3e0b2cfb0585cc91703d55a3d39
0xRob <0xRobin5@gmail.com>
2022-10-06 17:24:00 +0200

    fix + add default label

--- models/labels/ens/labels_ens.sql
+++ models/labels/ens/labels_ens.sql
@@ -6,7 +6,22 @@
 }}
 
 
-
+-- as default label, take the ENS reverse record or the latest resolver record
+SELECT *
+FROM (
+       SELECT
+       array('ethereum') as blockchain,
+       coalesce(rev.address, res.address) as address
+       coalesce(rev.name, last(res.name,true) over (order by res.latest_tx_block_time asc)) as name
+       'ENS' as category,
+       '0xRob' as contributor,
+       'query' AS source,
+       date('2022-10-06') as created_at,
+       now() as modified_at
+    FROM {{ ref('ens_resolver_latest') }} res
+    OUTER JOIN {{ ref('ens_reverse_latest') }} rev
+    ON res.address = rev.address
+) ens
 SELECT array('ethereum') as blockchain,
        address,
        name,
@@ -25,4 +40,7 @@ SELECT array('ethereum') as blockchain,
        'query' AS source,
        date('2022-10-06') as created_at,
        now() as modified_at
-FROM {{ ref('ens_resolver_latest') }}
+FROM {{ ref('ens_reverse_latest') }}
+UNION
+
+
