a05be2bf1d767e409f5fc599364118b48e3f54e5
UmerHA <40663591+UmerHA@users.noreply.github.com>
2022-10-06 12:36:21 +0200

    Added a test

---
+++ tests/nft/ethereum/nft_ethereum_native_mints.sql
@@ -0,0 +1,33 @@
+-- Check if all ENS mints are taken into account
+with
+  ens_mints_ctn as (
+    select
+      count(*) as ctn,
+      'hi_im_a_dummy' as dummy
+    from
+      ethereum.logs
+    where
+      1 = 1
+      and contract_address = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85' -- contract = ENS
+      and topic1 = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- event type = transfer
+      and topic2 = '0x0000000000000000000000000000000000000000000000000000000000000000' -- seller = null address
+      and block_time < now() - interval '1 day' -- allow some head desync
+  ),
+  eth_native_mints_ctn as (
+    select
+      count(*) as ctn,
+      'hi_im_a_dummy' as dummy
+    from
+      {{ ref('nft_ethereum_native_mints') }}
+    where
+      1 = 1
+      and contract_address = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85' -- ENS
+      and block_time < now() - interval '1 day' -- allow some head desync
+  )
+select
+  *
+from
+  ens_mints_ctn c1
+  inner join eth_native_mints_ctn c2 on c1.dummy = c2.dummy
+where
+  c1.ctn <> c2.ctn
