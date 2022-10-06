1121a2c1cacfe94d51df85e6e5fec8596503d394
jeff-dude <jeff@dune.xyz>
2022-10-06 14:10:52 -0400

    formatting

--- models/addresses_events/ethereum/addresses_events_ethereum_first_funded_by.sql
+++ models/addresses_events/ethereum/addresses_events_ethereum_first_funded_by.sql
@@ -1,25 +1,33 @@
-{{ config(alias='first_funded_by', materialized = 'table', file_format = 'delta',
-        post_hook='{{ expose_spells(\'["ethereum"]\',
+{{ config
+(
+    alias='first_funded_by'
+    , materialized = 'table'
+    , file_format = 'delta'
+    , post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "sector",
                                     "addresses_events",
-                                    \'["hildobby"]\') }}') }}
+                                    \'["hildobby"]\') }}'
+)
+}}
 
 SELECT b.to AS address
-, MIN(a.from) AS first_funded_by
-, MIN(a.block_time) AS block_time
-, MIN(a.block_number) AS block_number
-, MIN(a.tx_hash) AS tx_hash
+    , MIN(a.from) AS first_funded_by
+    , MIN(a.block_time) AS block_time
+    , MIN(a.block_number) AS block_number
+    , MIN(a.tx_hash) AS tx_hash
 FROM {{ source('ethereum', 'traces') }} a
 JOIN (
     SELECT to
-    , MIN(block_number) AS first_block
+        , MIN(block_number) AS first_block
     FROM {{ source('ethereum', 'traces') }}
     WHERE success
     AND (call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR call_type IS NULL)
     AND value > 0
     GROUP BY to
-    ) AS b ON a.to = b.to AND a.block_number = b.first_block
+    ) AS b 
+    ON a.to = b.to AND a.block_number = b.first_block
 WHERE a.success
-AND (a.call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR a.call_type IS NULL)
-AND a.value > 0
+    AND (a.call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR a.call_type IS NULL)
+    AND a.value > 0
 GROUP BY b.to
+;
