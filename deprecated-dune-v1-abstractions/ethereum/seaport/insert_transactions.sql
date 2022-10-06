fc143cd188dfe7272b5a1269ee572864338bb300
sohwak <jeremy2kang@gmail.com>
2022-10-07 04:09:09 +0900

    execlude more than 2 currencies used in single txns

--- deprecated-dune-v1-abstractions/ethereum/seaport/insert_transactions.sql
+++ deprecated-dune-v1-abstractions/ethereum/seaport/insert_transactions.sql
@@ -143,6 +143,7 @@ with p1_call as (
           ,max(purchase_method) as purchase_method
       from p1_evt_add a
      group by 1,2,3,4,5,6
+     having count(distinct price_token) = 1  -- execlude more than 2 currencies used in single txns
 )
 ,p1_nft_trades as ( 
     select a.block_time
@@ -349,6 +350,7 @@ with p1_call as (
           ,'Buy Now' as purchase_method
       from p2_evt a
      group by 1,2,3,4,5
+     having count(distinct evt_price_token) > 1 -- execlude more than 2 currencies used in single txns
 )
 ,p2_nft_trades as ( 
     select a.block_time
@@ -554,6 +556,9 @@ with p1_call as (
           ,max(order_type_id) as order_type_id
       from p3_add_rn a
      group by 1,2,3,4,5
+     having count(distinct case when purchase_method = 'Offer Accepted' and sub_type = 'offer' and sub_idx = 1 then token_contract_address::text
+                                when purchase_method = 'Buy Now' and sub_type = 'consideration' then token_contract_address::text
+                           end) = 1  -- execlude more than 2 currencies used in single txns
 )
 ,p3_nft_trades as (
     select a.block_time
@@ -759,6 +764,7 @@ with p1_call as (
       from p4_add_rn a
      where offer_item_type in ('2','3')
      group by 1,2,3,4,5,6
+     having count(distinct price_token) = 1  -- execlude more than 2 currencies used in single txns
 )
 ,p4_nft_trades as ( 
     select a.block_time
