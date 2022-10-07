d28ce9fe2e927d6176941c7dda21e9ee4050fe2d
soispoke <66172107+soispoke@users.noreply.github.com>
2022-10-07 11:53:36 +0100

    Merge branch 'main' into pr/1712

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -21,6 +21,8 @@ SELECT * FROM {{ ref('labels_multisig_ethereum') }}
 UNION
 SELECT * FROM {{ ref('labels_hackers_ethereum') }}
 UNION
+SELECT * FROM {{ ref('labels_validators_bnb') }}
+UNION
 SELECT blockchain, address, name, category, contributor, source, created_at, updated_at FROM {{ ref('labels_aztec_v2_contracts_ethereum') }}
 UNION
 -- Query Labels
