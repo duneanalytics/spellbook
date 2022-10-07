2d24403be2ef069e71bace4d647fd22042d63c78
0xRob <0xRobin5@gmail.com>
2022-10-07 16:21:07 +0200

    materialize history, view latest

--- models/ens/ens_resolver_latest.sql
+++ models/ens/ens_resolver_latest.sql
@@ -1,9 +1,5 @@
 {{ config(
     alias = 'resolver_latest',
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    unique_key = ['node'],
     post_hook='{{ expose_spells(\'["ethereum"]\',
                             "project",
                             "ens",
@@ -11,34 +7,16 @@
     )
 }}
 
-with latest_resolver_records as (
 select
-    node
+    name
     ,address
+    ,node
     ,block_time
     ,tx_hash
-    from(
-        select
-        a as address
-        ,node
-        ,evt_block_time as block_time
-        ,evt_tx_hash as tx_hash
-        ,evt_index
-        ,row_number() over (partition by node order by evt_block_time desc, evt_index desc) as ordering
-        from {{ source('ethereumnameservice_ethereum','PublicResolver_evt_AddrChanged') }}
-        {% if is_incremental() %}
-        WHERE evt_block_time >= date_trunc("day", now() - interval '1 week')
-        {% endif %}
-    ) foo
-    where ordering = 1
-   )
-
-select
-    n.name
-    ,r.address
-    ,r.node
-    ,r.block_time as latest_tx_block_time
-    ,r.tx_hash as latest_tx_hash
-from latest_resolver_records r
-inner join {{ ref('ens_node_names')}} n
-ON r.node = n.node
+    ,evt_index
+from(
+     *
+    ,row_number() over (partition by node order by block_time desc, evt_index desc) as ordering
+    from {{ ref('ens_resolver_records')}}
+) f
+where ordering = 1
