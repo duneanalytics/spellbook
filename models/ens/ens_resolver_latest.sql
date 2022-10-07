15b98f5d4ad774c3a8b35300b2b47981ff6ae425
0xRob <0xRobin5@gmail.com>
2022-10-07 16:50:15 +0200

    fix

--- models/ens/ens_resolver_latest.sql
+++ models/ens/ens_resolver_latest.sql
@@ -15,6 +15,7 @@ select
     ,tx_hash
     ,evt_index
 from(
+     select
      *
     ,row_number() over (partition by node order by block_time desc, evt_index desc) as ordering
     from {{ ref('ens_resolver_records')}}
