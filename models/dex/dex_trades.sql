93c660fbb769400b32f82773adc95631d261bbe8
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-10-08 16:10:41 -0400

    expose OP logo in dex.trades

--- models/dex/dex_trades.sql
+++ models/dex/dex_trades.sql
@@ -1,6 +1,6 @@
 {{ config(
         alias ='trades',
-        post_hook='{{ expose_spells(\'["ethereum"]\',
+        post_hook='{{ expose_spells(\'["ethereum","optimism"]\',
                                 "sector",
                                 "dex",
                                 \'["jeff-dude", "hosuke"]\') }}'
