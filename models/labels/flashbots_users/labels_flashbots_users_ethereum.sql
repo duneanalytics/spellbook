9f757b20c457fec4b98ac6b1c60a2a139b678dd6
Hildebert Moulié <h.moulie@gmail.com>
2022-10-08 17:24:49 +0200

    added flashbots user addresses

---
+++ models/labels/flashbots_users/labels_flashbots_users_ethereum.sql
@@ -0,0 +1,15 @@
+{{config(alias='flashbots_users_ethereum',
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                    "sector",
+                                    "labels",
+                                    \'["hildobby"]\') }}')}}
+
+SELECT DISTINCT array('ethereum') AS blockchain
+, account_address AS address
+, 'Flashbots user' AS name
+, 'flashbots_user' AS category
+, 'hildobby' AS contributor
+, 'query' AS source
+, date('2022-10-08') AS created_at
+, NOW() AS modified_at
+FROM {{ ref('flashbots_arbitrages') }}
