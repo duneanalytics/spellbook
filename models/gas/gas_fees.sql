6a9e4a105316c6cb16084d618a00c4e23753fb2f
soispoke <66172107+soispoke@users.noreply.github.com>
2022-10-11 16:21:15 +0100

    add avalanche gas spell

--- models/gas/gas_fees.sql
+++ models/gas/gas_fees.sql
@@ -51,4 +51,26 @@ FROM
                 gas_limit,
                 gas_usage_percent,
                 transaction_type
+        FROM {{ ref('gas_avalanche_c_fees') }}
+        UNION
+        SELECT
+                blockchain,
+                block_number,
+                block_time,
+                block_date,
+                tx_hash,
+                native_token_symbol,
+                tx_amount_native,
+                tx_amount_usd,
+                tx_fee_native,
+                tx_fee_usd,
+                burned_native,
+                burned_usd,
+                validator,
+                gas_price_gwei,
+                gas_price_usd,
+                gas_used,
+                gas_limit,
+                gas_usage_percent,
+                transaction_type
         FROM {{ ref('gas_bnb_fees') }})
