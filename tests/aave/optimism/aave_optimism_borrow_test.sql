e388c707f0e50e11eb1131e30180c5f20e53421b
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-10-12 12:27:38 +0530

    Update aave_optimism_borrow_test.sql

--- tests/aave/optimism/aave_optimism_borrow_test.sql
+++ tests/aave/optimism/aave_optimism_borrow_test.sql
@@ -1,22 +1,22 @@
 with unit_test1
     as (select case
-                 when usd_amount = 19436.28 then true
+                 when usd_amount = 0.999989 then true
                  else false
                end as test
         from   {{ ref('aave_optimism_borrow' )}}
-        where  token_address = '0x68f180fcce6836688e9084f035309e29bf0a2095'
-               and borrower = '0x290e28bf4fbb5abf8d7830496847d783f5a67007'
-               and evt_block_time = '2022-10-09 08:22'
-               and evt_tx_hash = '0x2b4f4770d317e5760c369ff4f3fbb17995ceb4a876ecd308008b4956fd8e7d40'),
+        where  token_address = '0x7f5c764cbc14f9669b88837ca1490cca17c31607'
+               and borrower = '0xf2eebb119be9313bafc22a031a29a94afc79e3c9'
+               and evt_block_time = '2022-10-11 16:05'
+               and evt_tx_hash = '0x27b52c00cf1b28b955b48c72b406b3f41f6abe6d4dfa1ea16b682fca414cb3da'),
     unit_test2
     as (select case
-                 when amount = -20.25 then true
+                 when amount = -1 then true
                  else false
                end as test
         from   {{ ref('aave_optimism_borrow' )}}
-        where  symbol = 'USDC'
-               and repayer = '0x70371a494f73a8df658c5cd29e2c1601787e1009'
-               and evt_block_time = '2022-10-09 06:39')
+        where  symbol = 'DAI'
+               and repayer = '0xa9f12ca1b27941c05e3998279c06b81b8e33ca81'
+               and evt_block_time = '2022-10-11 10:11')
 select *
 from   (select *
        from   unit_test1
