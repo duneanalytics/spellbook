9b46312957b2a7c51a4c34eb2618902f220921c9
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-10-12 12:41:02 +0530

    Update aave_optimism_supply_test.sql

--- tests/aave/optimism/aave_optimism_supply_test.sql
+++ tests/aave/optimism/aave_optimism_supply_test.sql
@@ -1,22 +1,22 @@
 with unit_test1
     as (select case
-                 when usd_amount = 12.34988436 then true
+                 when usd_amount = 1.31305 then true
                  else false
                end as test
         from   {{ ref('aave_optimism_supply' )}}
-        where  token_address = '0x68f180fcce6836688e9084f035309e29bf0a2095'
-               and depositor = '0x88b0ea576428da635d0fa9deb686765c90cfde2e'
-               and evt_block_time = '2022-10-09 06:32'
-               and evt_tx_hash = '0x17e3aa6de17838e3b9d6b41c7c4db396ae7c176fdd1b630d071cd21ed616fe37'),
+        where  token_address = '0x4200000000000000000000000000000000000006'
+               and depositor = '0x76d3030728e52deb8848d5613abade88441cbc59'
+               and evt_block_time = '2022-10-10 10:27'
+               and evt_tx_hash = '0x792ac57b533bc85114f70792b78891b4e1a8daf206c2e0c75a426457657777b4'),
     unit_test2
     as (select case
-                 when amount = -21 then true
+                 when amount = -20 then true
                  else false
                end as test
         from   {{ ref('aave_optimism_supply' )}}
         where  symbol = 'USDC'
                and depositor = '0x4ecb5300d9ec6bca09d66bfd8dcb532e3192dda1'
-               and evt_block_time = '2022-10-09 05:01')
+               and evt_block_time = '2022-10-10 10:10')
 select *
 from   (select *
        from   unit_test1
