e111675b216c24bc4dad3a1e2bb488cc6e355aa6
soispoke <66172107+soispoke@users.noreply.github.com>
2022-10-13 17:43:09 +0100

    cache macro

--- macros/optimize_spell.sql
+++
@@ -1,6 +0,0 @@
-{% macro optimize_spell(this, materialization) %}
-{%- if target.name == 'prod' and materialization in ('table', 'incremental') -%}
-        OPTIMIZE {{this}};
-{%- else -%}
-{%- endif -%}
-{%- endmacro -%}
