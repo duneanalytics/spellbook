45926bcfa8ed1559f3f47745c542e3245fb19bc3
Ben Smith <bh2smith@gmail.com>
2022-10-13 11:41:37 +0200

    reference events as sources

--- tests/cow_protocol/ethereum/cow_protocol_ethereum_assert_batches.sql
+++ tests/cow_protocol/ethereum/cow_protocol_ethereum_assert_batches.sql
@@ -4,11 +4,14 @@ with batches_with_trade_count as (
         s.evt_tx_hash,
         s.evt_block_time,
         count(t.evt_index) as num_trades
-    from gnosis_protocol_v2_ethereum.GPv2Settlement_evt_Trade t
-    inner join gnosis_protocol_v2_ethereum.GPv2Settlement_evt_Settlement s
+    from {{ source('gnosis_protocol_v2_ethereum','GPv2Settlement_evt_Trade') }} t
+    inner join {{ source('gnosis_protocol_v2_ethereum','GPv2Settlement_evt_Settlement') }} s
         on s.evt_tx_hash = t.evt_tx_hash
     group by s.evt_tx_hash, s.evt_block_time
 )
 
 select * from batches_with_trade_count
 where evt_tx_hash not in (select tx_hash from {{ ref('cow_protocol_ethereum_batches' )}})
+-- The reference table is only refreshed once in a while,
+-- so we impose a time constraint on this test.
+and evt_block_time < date(now())
