505f14e4e50290a7d7287a5556701712a06b4cc4
soispoke <66172107+soispoke@users.noreply.github.com>
2022-10-13 14:17:04 +0200

    Avalanche gas spell (#1751)
    * gas ethereum fees spell
    
    * Update gas_ethereum_fees.sql
    
    * Update dbt_project.yml
    
    * Update gas_ethereum_fees.sql
    
    * Update gas_ethereum_fees.sql
    
    * fix burned eth by including all txns type
    
    * replace eth by native to standardize across chains
    
    * add blockchain column
    
    * standardize ETH and BNB gas spells
    
    * Update gas_bnb_fees.sql
    
    * Update gas_fees.sql
    
    * fix typo
    
    * native token symbol fixes
    
    * add avalanche gas spell
    
    * Update gas_avalanche_c_fees.sql
    
    * for loop + indexation fix

--- models/gas/gas_fees.sql
+++ models/gas/gas_fees.sql
@@ -6,49 +6,39 @@
                                 \'["soispoke"]\') }}'
         )
 }}
+
+{% set gas_fees_models = [
+'gas_ethereum_fees',
+'gas_bnb_fees',
+'gas_avalanche_c_fees',
+] %}
+
 SELECT *
-FROM
-    (
-        SELECT
-                blockchain,
-                block_number,
-                block_time,
-                block_date,
-                tx_hash,
-                native_token_symbol,
-                tx_amount_native,
-                tx_amount_usd,
-                tx_fee_native,
-                tx_fee_usd,
-                burned_native,
-                burned_usd,
-                validator,
-                gas_price_gwei,
-                gas_price_usd,
-                gas_used,
-                gas_limit,
-                gas_usage_percent,
-                transaction_type
-        FROM {{ ref('gas_ethereum_fees') }}
-        UNION
-        SELECT
-                blockchain,
-                block_number,
-                block_time,
-                block_date,
-                tx_hash,
-                native_token_symbol,
-                tx_amount_native,
-                tx_amount_usd,
-                tx_fee_native,
-                tx_fee_usd,
-                burned_native,
-                burned_usd,
-                validator,
-                gas_price_gwei,
-                gas_price_usd,
-                gas_used,
-                gas_limit,
-                gas_usage_percent,
-                transaction_type
-        FROM {{ ref('gas_bnb_fees') }})
+FROM (
+    {% for gas_model in gas_fees_models %}
+    SELECT
+        blockchain,
+        block_number,
+        block_time,
+        block_date,
+        tx_hash,
+        native_token_symbol,
+        tx_amount_native,
+        tx_amount_usd,
+        tx_fee_native,
+        tx_fee_usd,
+        burned_native,
+        burned_usd,
+        validator,
+        gas_price_gwei,
+        gas_price_usd,
+        gas_used,
+        gas_limit,
+        gas_usage_percent,
+        transaction_type
+    FROM {{ ref(gas_model) }}
+    {% if not loop.last %}
+    UNION
+    {% endif %}
+    {% endfor %}
+)
