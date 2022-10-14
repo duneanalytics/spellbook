14ef8f3f827b7e70d943040301075fda65a3019c
soispoke <66172107+soispoke@users.noreply.github.com>
2022-10-14 17:36:50 +0100

    Update optimize_and_cache_spell.sql

--- macros/optimize_and_cache_spell.sql
+++ macros/optimize_and_cache_spell.sql
@@ -1,6 +1,6 @@
 {% macro optimize_and_cache_spell(this, materialization) %}
 {%- if target.name == 'prod' and materialization in ('table', 'incremental') -%}
-        OPTIMIZE {{this}}
+        OPTIMIZE {{this}};
         CACHE TABLE {{this}};
 {%- else -%}
 {%- endif -%}
