44ee05cea6f6395ebc93333f73ccf3e99129ee78
jeff-dude <jeff@dune.xyz>
2022-10-14 13:49:44 -0400

    revert cache changes

--- macros/optimize_spell.sql
+++ macros/optimize_spell.sql
@@ -1,7 +1,6 @@
-{% macro optimize_and_cache_spell(this, materialization) %}
+{% macro optimize_spell(this, materialization) %}
 {%- if target.name == 'prod' and materialization in ('table', 'incremental') -%}
         OPTIMIZE {{this}};
-        CACHE TABLE {{this}};
 {%- else -%}
 {%- endif -%}
 {%- endmacro -%}
