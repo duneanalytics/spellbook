77d67738c455c2e35765eec1a8f155d990f1cc90
0xMiso <businessmiso18@gmail.com>
2022-10-17 00:18:52 +0200

    Reaper Vaults

--- tests/reaper/TEST_all_vaults.sql
+++ tests/reaper/TEST_all_vaults.sql
@@ -1,10 +1,9 @@
 with unit_test as(
     select
-        case when lower(test.symbol) = lower(actual.symbol) then true else false end as smybol_test,
-        case when test.decimal = actual.decimal then true else false end as decimal_test
+        case when lower(test.symbol) = lower(actual.symbol) then true else false end as smybol_test
     from {{ref('reaper_optimism_all_vaults')}} as actual
     inner join {{ref('reaper_optimism_all_vaults_postgres')}} as test
         on lower(actual.contract_address) = lower(test.contract_address)
 )
 select * from unit_test
-where symbol_test = false or decimal_test = false
+where symbol_test = false
