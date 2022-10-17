c59d2b8cc80714567845e3b6fc08dec0eff24cce
Abimbola Adebayo <bim.adebayo@gmail.com>
2022-10-16 19:29:38 -0500

    add in jeff as contributor from v1

--- models/hashflow/ethereum/hashflow_ethereum_trades.sql
+++ models/hashflow/ethereum/hashflow_ethereum_trades.sql
@@ -8,7 +8,7 @@
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "hashflow",
-                                \'["justabi"]\') }}'
+                                \'["justabi", "jeff-dude"]\') }}'
     )
 }}
 
