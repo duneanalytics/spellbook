b4339d92bda4f9080ef4aee525429b1f38871f38
Soham Pathak <97793359+SohamP99@users.noreply.github.com>
2022-10-17 23:00:03 +0530

    Add aave supply and borrow abstractions for optimism in Dune v2 (#1733)
    * Update aave_v3_optimism_interest_rates.sql
    
    * Update the schema to add borrow and supply modes
    
    * Update the sources for supply and borrow models
    
    * Add borrow and supply model files
    
    * Add the test files for borrow and supply models.
    
    * Update aave_optimism_borrow.sql
    
    * Update aave_optimism_interest_rates.sql
    
    * Add freshness tests for the sources
    
    * Added explaination for interest rate mode
    
    * Update aave_optimism_supply.sql
    
    * Update aave_v3_optimism_borrow.sql
    
    * Update aave_v3_optimism_supply.sql
    
    * Update supply model to add ETH token prices
    
    * Update supply model to add ETH token prices
    
    * Update aave_optimism_borrow_test.sql
    
    * Update aave_optimism_supply_test.sql

---
+++ models/aave/optimism/aave_optimism_supply.sql
@@ -0,0 +1,32 @@
+{{ config(
+      alias='supply'
+      , post_hook='{{ expose_spells(\'["optimism"]\',
+                                  "project",
+                                  "aave",
+                                  \'["batwayne", "chuxinh"]\') }}'
+  )
+}}
+
+SELECT *
+FROM 
+(
+      SELECT 
+            version,
+            transaction_type,
+            symbol,
+            token_address, 
+            depositor,
+            withdrawn_to,
+            liquidator,
+            amount,
+            usd_amount,
+            evt_tx_hash,
+            evt_index,
+            evt_block_time,
+            evt_block_number 
+      FROM {{ ref('aave_v3_optimism_supply') }}
+      /*
+      UNION ALL
+      < add new version as needed
+      */
+)
