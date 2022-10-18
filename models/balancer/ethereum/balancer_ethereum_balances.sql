d92e035860611f3d1e591a54d5d2499414dadffa
Meghan <msheintz@gmail.com>
2022-10-18 16:03:13 -0500

    test bug to force failure

--- models/balancer/ethereum/balancer_ethereum_balances.sql
+++ models/balancer/ethereum/balancer_ethereum_balances.sql
@@ -17,7 +17,7 @@ WITH pools AS (
 
 joins AS (
     SELECT p.pools as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, SUM(value) AS amount
-    FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
+    FROM erc20_ethereum.evt_transfer e
     INNER JOIN pools p ON e.`to` = p.pools
     GROUP BY 1, 2, 3
     UNION ALL
