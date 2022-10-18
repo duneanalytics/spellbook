7d203ab8e5c2f951136801c9adc50f08aaf6c2cc
jeff-dude <jeff@dune.xyz>
2022-10-18 16:29:03 -0400

    update curvefi trades to match dex trades

--- models/curvefi/curvefi_trades.sql
+++ models/curvefi/curvefi_trades.sql
@@ -7,62 +7,43 @@
         )
 }}
 
+{% set curvefi_trade_models = [
+'curvefi_ethereum_trades'
+,'curvefi_optimism_trades'
+] %}
+
+
 SELECT *
-FROM
-(
-        SELECT
-                blockchain
-                ,project
-                ,version
-                ,block_date
-                ,block_time
-                ,token_bought_symbol
-                ,token_sold_symbol
-                ,token_pair
-                ,token_bought_amount
-                ,token_sold_amount
-                ,token_bought_amount_raw
-                ,token_sold_amount_raw
-                ,amount_usd
-                ,token_bought_address
-                ,token_sold_address
-                ,taker
-                ,maker
-                ,project_contract_address
-                ,tx_hash
-                ,tx_from
-                ,tx_to
-                ,trace_address
-                ,evt_index
-        FROM {{ ref('curvefi_ethereum_trades') }}
-        UNION
-        SELECT
-                blockchain
-                ,project
-                ,version
-                ,block_date
-                ,block_time
-                ,token_bought_symbol
-                ,token_sold_symbol
-                ,token_pair
-                ,token_bought_amount
-                ,token_sold_amount
-                ,token_bought_amount_raw
-                ,token_sold_amount_raw
-                ,amount_usd
-                ,token_bought_address
-                ,token_sold_address
-                ,taker
-                ,maker
-                ,project_contract_address
-                ,tx_hash
-                ,tx_from
-                ,tx_to
-                ,trace_address
-                ,evt_index
-        FROM {{ ref('curvefi_optimism_trades') }}
-        /*
-        UNION
-        <add future blockchains here>
-        */
+FROM (
+    {% for curvefi_model in curvefi_trade_models %}
+    SELECT
+        blockchain,
+        project,
+        version,
+        block_date,
+        block_time,
+        token_bought_symbol,
+        token_sold_symbol,
+        token_pair,
+        token_bought_amount,
+        token_sold_amount,
+        token_bought_amount_raw,
+        token_sold_amount_raw,
+        amount_usd,
+        token_bought_address,
+        token_sold_address,
+        taker,
+        maker,
+        project_contract_address,
+        tx_hash,
+        tx_from,
+        tx_to,
+        trace_address,
+        evt_index
+    FROM {{ ref(curvefi_model) }}
+    {% if not loop.last %}
+    UNION
+    {% endif %}
+    {% endfor %}
 )
+;
