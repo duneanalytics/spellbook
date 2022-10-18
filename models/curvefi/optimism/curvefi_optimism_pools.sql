c0aa3b7020f0f746f487597494201643a210ff9b
jeff-dude <jeff@dune.xyz>
2022-10-18 17:13:49 -0400

    formatting, clean config block

--- models/curvefi/optimism/curvefi_optimism_pools.sql
+++ models/curvefi/optimism/curvefi_optimism_pools.sql
@@ -1,10 +1,9 @@
 {{ config(
-    schema = 'curvefi_optimism',
     alias = 'pools',
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['tokenid', 'token', 'pool'],
+    unique_key = ['version', 'tokenid', 'token', 'pool'],
     post_hook='{{ expose_spells(\'["optimism"]\',
                                 "project",
                                 "curvefi",
@@ -24,34 +23,42 @@ WITH base_pools AS (
         FROM {{ source('curvefi_optimism', 'StableSwap_call_coins') }}
         WHERE call_success
     GROUP BY 1,2,3 --unique
-    )
+)
 , meta_pools AS (
     -- Meta Pools are "Base Pools" + 1 extra token (i.e. sUSD + 3pool = sUSD Metapool)
-SELECT tokenid, token, et.`contract_address` AS pool
-FROM (
-    SELECT mp.evt_tx_hash, bp.tokenid + 1 AS tokenid, bp.token, mp.evt_block_number
-    FROM {{ source('curvefi_optimism', 'PoolFactory_evt_MetaPoolDeployed') }} mp
-    INNER JOIN base_pools bp
-        ON mp.base_pool = bp.pool
-
-    {% if is_incremental() %}
-    WHERE evt_block_time >= date_trunc('day', now() - interval '1 week')
-    {% endif %}
+    SELECT
+        tokenid
+        , token
+        , et.`contract_address` AS pool
+    FROM
+    (
+        SELECT
+            mp.evt_tx_hash
+            , bp.tokenid + 1 AS tokenid
+            , bp.token
+            , mp.evt_block_number
+        FROM {{ source('curvefi_optimism', 'PoolFactory_evt_MetaPoolDeployed') }} mp
+        INNER JOIN base_pools bp
+            ON mp.base_pool = bp.pool
+        {% if is_incremental() %}
+        WHERE mp.evt_block_time >= date_trunc('day', now() - interval '1 week')
+        {% endif %}
+        GROUP BY 1,2,3,4 --unique
     
-GROUP BY 1,2,3,4 --unique
-    
-    UNION ALL
-    SELECT mp.evt_tx_hash, 0 AS tokenid, mp.`coin` AS token, mp.evt_block_number
-    FROM {{ source('curvefi_optimism', 'PoolFactory_evt_MetaPoolDeployed') }} mp
-    INNER JOIN base_pools bp
-        ON mp.base_pool = bp.pool
-    
-    {% if is_incremental() %}
-    WHERE evt_block_time >= date_trunc('day', now() - interval '1 week')
-    {% endif %}
-    
-GROUP BY 1,2,3,4 --unique
-    
+        UNION ALL
+        
+        SELECT
+            mp.evt_tx_hash
+            , 0 AS tokenid
+            , mp.`coin` AS token
+            , mp.evt_block_number
+        FROM {{ source('curvefi_optimism', 'PoolFactory_evt_MetaPoolDeployed') }} mp
+        INNER JOIN base_pools bp
+            ON mp.base_pool = bp.pool
+        {% if is_incremental() %}
+        WHERE mp.evt_block_time >= date_trunc('day', now() - interval '1 week')
+        {% endif %}
+        GROUP BY 1,2,3,4 --unique
     ) mps
     -- the exchange address appears as an erc20 minted to itself (not in the deploymeny event)
     INNER JOIN {{ source('erc20_optimism','evt_Transfer') }} et
@@ -65,32 +72,53 @@ GROUP BY 1,2,3,4 --unique
         {% if is_incremental() %}
         AND et.evt_block_time >= date_trunc('day', now() - interval '1 week')
         {% endif %}
-        
-        
     GROUP BY 1,2,3
 )
-
 , basic_pools AS (
-SELECT pos AS tokenid, col AS token, pool
-    FROM (
+    SELECT
+        pos AS tokenid
+        , col AS token
+        , pool
+    FROM
+    (
         SELECT 
-        posexplode(_coins), output_0 AS pool
+            posexplode(_coins)
+            , output_0 AS pool
         FROM {{ source('curvefi_optimism', 'PoolFactory_call_deploy_plain_pool') }}
         WHERE call_success
         {% if is_incremental() %}
         AND call_block_time >= date_trunc('day', now() - interval '1 week')
         {% endif %}
-        ) a
+    ) a
     GROUP BY 1,2,3
 )
-
-
-SELECT version, tokenid, token, pool FROM (
-    SELECT 'Base Pool' AS version, tokenid, token, pool FROM base_pools
+SELECT
+    version
+    , tokenid
+    , token
+    , pool 
+FROM
+(
+    SELECT
+        'Base Pool' AS version
+        , tokenid
+        , token
+        , pool 
+    FROM base_pools
     UNION ALL
-    SELECT 'Meta Pool' AS version, tokenid, token, pool FROM meta_pools
+    SELECT
+        'Meta Pool' AS version
+        , tokenid
+        , token
+        , pool
+    FROM meta_pools
     UNION ALL
-    SELECT 'Basic Pool' AS version, tokenid, token, pool FROM basic_pools
-    ) a
+    SELECT
+        'Basic Pool' AS version
+        , tokenid
+        , token
+        , pool
+    FROM basic_pools
+) a
 GROUP BY 1,2,3,4 --unique
--- ORDER BY pool ASC, tokenid ASC
+;
