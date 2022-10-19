52da90e653add245f087c9ec6d43f5bef99ec3d9
UmerHA <40663591+UmerHA@users.noreply.github.com>
2022-10-19 10:45:32 +0200

    Added defi addresses

--- models/addresses/ethereum/addresses_ethereum_defi.sql
+++ models/addresses/ethereum/addresses_ethereum_defi.sql
@@ -1,14 +1,10 @@
-
-
 SELECT address, project, project_type
 FROM (VALUES
-    -- Source: https://etherscan.io/directory/Exchanges/DEX -- to do: includes all entries from source 
+	-- dexes
     ('0xc36442b4a4522e871399cd717abdd847ab11fe88', 'uniswap_v3', 'dex')
+    , ('0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45', 'uniswap_v3', 'dex')
     , ('0x58a3c68e2d3aaf316239c003779f71acb870ee47', 'curvefi', 'dex')
-  
-  
-    -- Source: XXX -- to do: find source for lending protocols
+	-- lending protocols
     , ('0x58a3c68e2d3aaf316239c003779f71acb870ee47', 'alpha_homora', 'lending')
-  
+    , ('0x5f5cd91070960d13ee549c9cc47e7a4cd00457bb', 'alpha_homora_v2', 'lending')
     ) AS tmp_table (address, project, project_type)
-			
