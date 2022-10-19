ec4da587a715666c9bcac247f8fc695573798cd3
UmerHA <40663591+UmerHA@users.noreply.github.com>
2022-10-19 20:44:46 +0200

    Corrected alpha_homora address

--- models/addresses/ethereum/addresses_ethereum_defi.sql
+++ models/addresses/ethereum/addresses_ethereum_defi.sql
@@ -11,6 +11,6 @@ FROM (VALUES
     , ('0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45', 'uniswap_v3', 'dex')
     , ('0x58a3c68e2d3aaf316239c003779f71acb870ee47', 'curvefi', 'dex')
 	-- lending protocols
-    , ('0x58a3c68e2d3aaf316239c003779f71acb870ee47', 'alpha_homora', 'lending')
+    , ('0xba5ebaf3fc1fcca67147050bf80462393814e54b', 'alpha_homora', 'lending')
     , ('0x5f5cd91070960d13ee549c9cc47e7a4cd00457bb', 'alpha_homora_v2', 'lending')
     ) AS tmp_table (address, project, project_type)
