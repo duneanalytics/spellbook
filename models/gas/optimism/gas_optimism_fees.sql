a7b6ccbc5472306098e85ad98dde3043725ff159
soispoke <66172107+soispoke@users.noreply.github.com>
2022-10-24 19:52:46 +0100

    fix optimism fees

--- models/gas/optimism/gas_optimism_fees.sql
+++ models/gas/optimism/gas_optimism_fees.sql
@@ -24,17 +24,15 @@ SELECT
      cast(NULL as double) AS burned_native, -- Not applicable for L2s
      cast(NULL as double) AS burned_usd, -- Not applicable for L2s
      cast(NULL as string) as validator, -- Not applicable for L2s
+     txns.gas_price/1e9 as gas_price_gwei,
+     txns.gas_price/1e18 * p.price as gas_price_usd,
+     txns.gas_used as gas_used,
+     txns.gas_limit as gas_limit,
+     txns.gas_used / txns.gas_limit * 100 as gas_usage_percent,
      l1_gas_price/1e9 as l1_gas_price_gwei,
      l1_gas_price/1e18 * p.price as l1_gas_price_usd,
      l1_gas_used,
      l1_fee_scalar,
-     txns.gas_price/1e9 as l2_gas_price_gwei,
-     txns.gas_price/1e18 * p.price as l2_gas_price_usd,
-     txns.gas_used as l2_gas_used,
-     cast(NULL as bigint) as l1_gas_limit, --Not applicable for L2s
-     txns.gas_limit as l2_gas_limit,
-     cast(NULL as double) as l1_gas_usage_percent, --Not applicable for L2s
-     txns.gas_used / txns.gas_limit * 100 as l2_gas_usage_percent,
      (l1_gas_price * txns.gas_used)/1e18 as tx_fee_equivalent_on_l1_native,
      (l1_gas_price * txns.gas_used)/1e18 * p.price as tx_fee_equivalent_on_l1_usd,
      (length( decode(unhex(substring(data,3)), 'US-ASCII') ) - length(replace(decode(unhex(substring(data,3)), 'US-ASCII') , chr(0), ''))) as num_zero_bytes,
