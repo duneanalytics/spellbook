6d0bb586e74ca723045c3e33ba4615d86723cb00
jeff-dude <jeff@dune.xyz>
2022-10-28 14:50:16 -0400

    clean up reaper models

--- tests/reaper/TEST_all_vaults.sql
+++
@@ -1,9 +0,0 @@
-with unit_test as(
-    select
-        case when lower(test.symbol) = lower(actual.symbol) then true else false end as symbol_test
-    from {{ref('reaper_optimism_all_vaults')}} as actual
-    inner join {{ref('reaper_optimism_all_vaults_postgres')}} as test
-        on lower(actual.contract_address) = lower(test.contract_address)
-)
-select * from unit_test
-where symbol_test = false
