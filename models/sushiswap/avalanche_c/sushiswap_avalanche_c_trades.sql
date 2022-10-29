99de93ca9787b515d0e6a1ed181c72c1d34b9732
Huang Geyang <Sukebeta@outlook.com>
2022-10-29 20:26:14 +0800

    Add sources in sql

--- models/sushiswap/avalanche_c/sushiswap_avalanche_c_trades.sql
+++ models/sushiswap/avalanche_c/sushiswap_avalanche_c_trades.sql
@@ -9,6 +9,162 @@
     ,post_hook='{{ expose_spells(\'["avalanche_c"]\',
                                       "project",
                                       "sushiswap",
-                                    \'["hosuke"]\') }}'
+                                    \'["hosuke", "zhongyiio"]\') }}'
     )
 }}
+
+WITH decodes_with_output_amounts AS (
+
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           output_amounts,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapETHForExactTokens') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+    UNION ALL
+
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           output_amounts,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapExactETHForTokens') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+    UNION ALL
+
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           output_amounts,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapExactTokensForETH') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+    UNION ALL
+
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           output_amounts,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapExactTokensForTokens') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+    UNION ALL
+
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           output_amounts,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapTokensForExactETH') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+    UNION ALL
+
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           output_amounts,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapTokensForExactTokens') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+
+),
+
+decodes_without_amounts AS (
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapExactETHForTokensSupportingFeeOnTransferTokens') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+    UNION ALL
+
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapExactTokensForETHSupportingFeeOnTransferTokens') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+    UNION ALL
+
+    SELECT call_block_number,
+           call_block_time,
+           call_success,
+           call_trace_address,
+           call_tx_hash,
+           contract_address,
+           deadline,
+           path,
+           `to`
+    FROM {{ source('sushiswap_v2_avalanche_c', 'SushiSwapRouter_call_swapExactTokensForTokensSupportingFeeOnTransferTokens') }}
+    {% if is_incremental() %}
+    WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+)
+
+SELECT 0;
