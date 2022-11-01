2fce5fc901acc1a847ba82763a7914787d496e46
Zhongyi <hi@daizhongyi.com>
2022-11-01 14:39:51 +0800

    Kyberswap DMMPool trades.

---
+++ models/kyberswap/avalanche_c/kyberswap_avalanche_c_trades.sql
@@ -0,0 +1,101 @@
+{{ config(
+    schema = 'kyberswap_avalanche_c',
+    alias = 'trades',
+    partition_by = ['block_date'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address'],
+    post_hook='{{ expose_spells(\'["avalanche_c"]\',
+                                "project",
+                                "kyberswap",
+                                \'["zhongyiio", "hosuke"]\') }}'
+    )
+}}
+
+{% set project_start_date = '2022-05-23' %}
+
+WITH
+
+kyberswap_dex AS (
+    SELECT
+        t.evt_block_time                                                    AS block_time
+        ,t.`to`                                                             AS taker
+        ,''                                                                 AS maker
+        ,CASE WHEN t.amount0Out = 0 THEN t.amount1Out ELSE t.amount0Out END AS token_bought_amount_raw
+        ,CASE WHEN t.amount0In = 0 THEN t.amount1In ELSE t.amount0In END    AS token_sold_amount_raw
+        ,cast(NULL as double)                                               AS amount_usd
+        ,CASE WHEN t.amount0Out = 0 THEN p.token1 ELSE p.token0 END         AS token_bought_address
+        ,CASE WHEN t.amount0In = 0 THEN p.token1 ELSE p.token0 END          AS token_sold_address
+        ,t.contract_address                                                 AS project_contract_address
+        ,t.evt_tx_hash                                                      AS tx_hash
+        ,''                                                                 AS trace_address
+        ,t.evt_index
+    FROM {{ source('kyber_avalanche_c', 'DMMPool_evt_Swap') }} t
+    INNER JOIN {{ source('kyber_avalanche_c', 'DMMFactory_evt_PoolCreated') }} p
+        ON t.contract_address = p.pool
+)
+
+SELECT
+    'avalanche_c'                                                       AS blockchain
+    ,'kyberswap'                                                         AS project
+    ,'dmm'                                                                AS version
+    ,try_cast(date_trunc('DAY', kyberswap_dex.block_time) AS date)       AS block_date
+    ,kyberswap_dex.block_time
+    ,erc20a.symbol                                                      AS token_bought_symbol
+    ,erc20b.symbol                                                      AS token_sold_symbol
+    ,case
+         when lower(erc20a.symbol) > lower(erc20b.symbol) then concat(erc20b.symbol, '-', erc20a.symbol)
+         else concat(erc20a.symbol, '-', erc20b.symbol)
+     end                                                                AS token_pair
+    ,kyberswap_dex.token_bought_amount_raw / power(10, erc20a.decimals)  AS token_bought_amount
+    ,kyberswap_dex.token_sold_amount_raw / power(10, erc20b.decimals)    AS token_sold_amount
+    ,kyberswap_dex.token_bought_amount_raw
+    ,kyberswap_dex.token_sold_amount_raw
+    ,coalesce(kyberswap_dex.amount_usd
+            ,(kyberswap_dex.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
+            ,(kyberswap_dex.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
+     )                                                                  AS amount_usd
+    ,kyberswap_dex.token_bought_address
+    ,kyberswap_dex.token_sold_address
+    ,coalesce(kyberswap_dex.taker, tx.from)                              AS taker
+    ,kyberswap_dex.maker
+    ,kyberswap_dex.project_contract_address
+    ,kyberswap_dex.tx_hash
+    ,tx.from                                                            AS tx_from
+    ,tx.to                                                              AS tx_to
+    ,kyberswap_dex.trace_address
+    ,kyberswap_dex.evt_index
+FROM kyberswap_dex
+INNER JOIN {{ source('avalanche_c', 'transactions') }} tx
+    ON kyberswap_dex.tx_hash = tx.hash
+    {% if is_incremental() %}
+    AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+    {% else %}
+    AND tx.block_time >= '{{project_start_date}}'
+    {% endif %}
+LEFT JOIN {{ ref('tokens_erc20') }} erc20a
+    ON erc20a.contract_address = kyberswap_dex.token_bought_address
+    and erc20a.blockchain = 'avalanche_c'
+LEFT JOIN {{ ref('tokens_erc20') }} erc20b
+    ON erc20b.contract_address = kyberswap_dex.token_sold_address
+    AND erc20b.blockchain = 'avalanche_c'
+LEFT JOIN {{ source('prices', 'usd') }} p_bought
+    ON p_bought.minute = date_trunc('minute', kyberswap_dex.block_time)
+    AND p_bought.contract_address = kyberswap_dex.token_bought_address
+    AND p_bought.blockchain = 'avalanche_c'
+    {% if is_incremental() %}
+    AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
+    {% else %}
+    AND p_bought.minute >= '{{project_start_date}}'
+    {% endif %}
+LEFT JOIN {{ source('prices', 'usd') }} p_sold
+    ON p_sold.minute = date_trunc('minute', kyberswap_dex.block_time)
+    AND p_sold.contract_address = kyberswap_dex.token_sold_address
+    AND p_sold.blockchain = 'avalanche_c'
+    {% if is_incremental() %}
+    AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
+    {% else %}
+    AND p_sold.minute >= '{{project_start_date}}'
+    {% endif %}
+;
