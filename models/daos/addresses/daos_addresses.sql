aec24e9ac39782d431dcb58e31276391bef49740
soispoke <66172107+soispoke@users.noreply.github.com>
2022-11-01 11:25:24 +0000

    Update daos_addresses.sql

--- models/daos/addresses/daos_addresses.sql
+++ models/daos/addresses/daos_addresses.sql
@@ -1,6 +1,6 @@
 {{ config(
     alias = 'addresses',
-    materialized = 'table',
+    materialized = 'view',
     file_format = 'delta',
     post_hook='{{ expose_spells(\'["ethereum", "gnosis"]\',
                                 "sector",
