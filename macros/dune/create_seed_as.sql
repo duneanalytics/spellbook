f10e1b364a67c223670da1681e6104763dd55cc6
Meghan <msheintz@gmail.com>
2022-11-01 12:20:44 -0400

    macros to alter table locations

---
+++ macros/dune/create_seed_as.sql
@@ -0,0 +1,27 @@
+{% macro databricks__create_csv_table(model, agate_table) %}
+  {%- set column_override = model['config'].get('column_types', {}) -%}
+  {%- set quote_seed_column = model['config'].get('quote_columns', None) -%}
+
+  {% set sql %}
+    create table {{ this.render() }} (
+        {%- for col_name in agate_table.column_names -%}
+            {%- set inferred_type = adapter.convert_type(agate_table, loop.index0) -%}
+            {%- set type = column_override.get(col_name, inferred_type) -%}
+            {%- set column_name = (col_name | string) -%}
+            {{ adapter.quote_seed_column(column_name, quote_seed_column) }} {{ type }} {%- if not loop.last -%}, {%- endif -%}
+        {%- endfor -%}
+    )
+    {{ file_format_clause() }} location "{{ 's3a://REPLACE_ME/' +  this.render() | replace(".","/") | replace("_","-") }}"
+    {{ partition_cols(label="partitioned by") }}
+    {{ clustered_cols(label="clustered by") }}
+    {{ location_clause() }}
+    {{ comment_clause() }}
+    {{ tblproperties_clause() }}
+  {% endset %}
+
+  {% call statement('_') -%}
+    {{ sql }}
+  {%- endcall %}
+
+  {{ return(sql) }}
+{% endmacro %}
---
+++ scripts/alter_table_locations.py
@@ -0,0 +1,76 @@
+import argparse
+import ast
+import json
+import subprocess
+from pathlib import Path
+
+from string import Template
+
+
+class TableLocationManager:
+    def __init__(self, s3_base: str):
+        self.s3_base = s3_base
+
+    def fetch_tables_dict(self):
+        """
+        Use dbt ls commands to find json representations of all tables (incremental, static, seeds) that are models
+        :return: tables_dict {"model_name": {node dictionary}}
+        """
+        bash_response = subprocess.run(
+            f'dbt list --output json --select config.materialized:incremental config.materialized:table --exclude resource_type:test',
+            capture_output=True, shell=True).stdout.decode("utf-8")
+        table_strings = bash_response.split('\n')[:-1]
+        tables_dict = {}
+        for table_string in table_strings:
+            tables_dict[json.loads(table_string)['name']] = json.loads(table_string)
+        return tables_dict
+
+    def get_s3_location(self, table_dict):
+        schema = table_dict['config'].get('schema')
+        name = table_dict['config'].get('alias', table_dict['config'].get('name'))
+        s3_location = f's3a://{self.s3_base}/{schema}/{name}'
+        return s3_location
+
+    @staticmethod
+    def get_partitions(table_dict):
+        partition = table_dict['config'].get('partition_by')
+        if partition is not None:
+            return f"PARTITIONED BY ({' '.join(partition)})"
+        else:
+            return ""
+
+    def get_alter_command(self, table_dict):
+        table_name = f"{table_dict['config']['schema']}.{table_dict['config'].get('alias', table_dict['name'])}"
+        s3_path = self.get_s3_location(table_dict)
+        partition= self.get_partitions(table_dict)
+        alter_template = Template("""
+        {% set $var %}
+        ALTER TABLE $table_name $partition SET LOCATION $s3_path;
+        {% endset %}
+        {% do run_query($var) %}
+        """)
+        alter_command = alter_template.substitute(var=table_dict['name'].replace('.', ''),
+                                                  table_name=table_name,
+                                                  partition=partition,
+                                                  s3_path=s3_path)
+        return alter_command
+
+    def generate_macro_file(self, tables_dict):
+        f = open("../macros/dune/alter_table_locations.sql", 'w')
+        f.write("{% macro alter_table_locations() %}")
+        for table, table_dict in tables_dict.items():
+            alter_command = self.get_alter_command(table_dict)
+            f.write(alter_command)
+        f.write("{% endmacro %}")
+        f.close()
+
+    def main(self):
+        tables_dict = self.fetch_tables_dict()
+        self.generate_macro_file(tables_dict)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--s3_base')
+args = parser.parse_args()
+manager = TableLocationManager(s3_base=args.s3_base)
+manager.main()
