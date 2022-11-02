be80ee88b01df38360b22be551f5da1759f8aabb
Meghan Heintz <meghan@dune.com>
2022-11-02 17:51:16 -0400

    Delete test_table.sql

--- models/integration_test/test_table.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized='table', alias='test_table2') }}
-
-select "{{ env_var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') }}" as col1,
-       "{{ var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') }}" as col2
