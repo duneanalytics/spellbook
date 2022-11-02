9066acb14b7c57a6e70deee43effa059f4d11553
Meghan <msheintz@gmail.com>
2022-11-02 11:56:11 -0400

    debugging

--- models/integration_test/test_table.sql
+++ models/integration_test/test_table.sql
@@ -1,3 +1,4 @@
 {{ config(materialized='table', alias='test_table2') }}
 
-select "{{ env_var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') }}" as col
+select "{{ env_var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') }}" as col1,
+       "{{ var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') }}" as col2
