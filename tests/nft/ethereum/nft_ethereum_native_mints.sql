25f93d07d491a31507508f1e8416547003693468
Hildebert Moulié <h.moulie@gmail.com>
2022-11-04 14:20:40 +0100

    fixed test

--- tests/nft/ethereum/nft_ethereum_native_mints.sql
+++ tests/nft/ethereum/nft_ethereum_native_mints.sql
@@ -3,17 +3,17 @@ WITH raw_events AS (
   SELECT evt_tx_hash AS raw_tx_hash
   FROM {{ source('erc721_ethereum','evt_transfer') }}
   WHERE from='0x0000000000000000000000000000000000000000'
-  AND f.evt_block_time < NOW() - interval '1 day' -- allow some head desync
+  AND evt_block_time < NOW() - interval '1 day' -- allow some head desync
   UNION
   SELECT evt_tx_hash AS raw_tx_hash
   FROM {{ source('erc1155_ethereum','evt_transfersingle') }}
   WHERE from='0x0000000000000000000000000000000000000000'
-  AND f.evt_block_time < NOW() - interval '1 day' -- allow some head desync
+  AND evt_block_time < NOW() - interval '1 day' -- allow some head desync
   UNION
   SELECT evt_tx_hash AS raw_tx_hash
   FROM {{ source('erc1155_ethereum','evt_transferbatch') }}
   WHERE from='0x0000000000000000000000000000000000000000'
-  AND f.evt_block_time < NOW() - interval '1 day' -- allow some head desync
+  AND evt_block_time < NOW() - interval '1 day' -- allow some head desync
   )
 
 , processed_events AS (
