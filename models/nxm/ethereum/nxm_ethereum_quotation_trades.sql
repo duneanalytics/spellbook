572e76ef854e766d6c4c09ce8f5d821fe3723d22
zhengxiaofei <xiaofei.zheng@haochezhu.club>
2022-11-04 11:36:17 +0800

    adjust the code

--- models/nxm/ethereum/nxm_ethereum_quotation_trades.sql
+++ models/nxm/ethereum/nxm_ethereum_quotation_trades.sql
@@ -4,7 +4,7 @@
 	materialized = 'incremental',
 	file_format = 'delta',
 	incremental_strategy = 'merge',
-	unique_key = ['block_date','contract_address', 'evt_tx_hash', 'evt_index'],
+	unique_key = ['evt_block_number', 'evt_tx_hash', 'evt_index'],
   post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "nxm",
