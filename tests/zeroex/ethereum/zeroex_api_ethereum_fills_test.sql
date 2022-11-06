1e0803077819d4195806c5bafbd055093824d25a
sui414 <danning@0xproject.com>
2022-11-06 13:57:03 -0800

    add tests kek

---
+++ tests/zeroex/ethereum/zeroex_api_ethereum_fills_test.sql
@@ -0,0 +1,14 @@
+WITH unit_tests AS
+(SELECT CASE WHEN fills.taker_token_amount = fills_sample.taker_token_amount 
+    AND fills.maker_token_amount = fills_sample.maker_token_amount
+    AND fills.volume_usd = fills_sample.volume_usd
+    THEN True ELSE False END AS amount_test
+FROM {{ ref('zeroex_api_ethereum_fills') }} fills
+    JOIN {{ ref('zeroex_api_ethereum_fills_sample') }} fills_sample 
+    ON fills.tx_hash = fills_sample.tx_hash
+    AND fills.evt_index = fills_sample.evt_index
+
+)
+SELECT count(CASE WHEN amount_test = false THEN 1 ELSE NULL END)/count(*) AS pct_mismatch, count(*) AS COUNT_ROWS
+FROM unit_tests
+HAVING count(CASE WHEN amount_test = false THEN 1 ELSE NULL END) > count(*)*0.05
