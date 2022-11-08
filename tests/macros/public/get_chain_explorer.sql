6dac80729cba35056bd2d22272693f516e11a38f
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2022-11-08 12:17:15 -0500

    add macro brackets

--- tests/macros/public/get_chain_explorer.sql
+++ tests/macros/public/get_chain_explorer.sql
@@ -1,7 +1,7 @@
 
 WITH unit_tests as
 (
-    SELECT case when get_chain_explorer('ethereum') = 'https://etherscan.io' then True else False end test
+    SELECT case when {{ get_chain_explorer('ethereum') }} = 'https://etherscan.io' then True else False end test
 )
 select count(case when test = false then 1 else null end) as count_rows
 from unit_tests
--- tests/macros/public/get_href.sql
+++ tests/macros/public/get_href.sql
@@ -1,7 +1,7 @@
 
 WITH unit_tests as
 (
-    SELECT case when get_href('https://etherscan.io/', 'etherscan')= concat('<a href="', link, '"target ="_blank">', text) then True
+    SELECT case when {{ get_href('https://etherscan.io/', 'etherscan') }} = concat('<a href="', link, '"target ="_blank">', text) then True
      else False end test
 )
 select count(case when test = false then 1 else null end) as count_rows
