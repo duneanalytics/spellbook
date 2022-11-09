09ee56e308c4f4c51b36b502d8cd1871f66d955a
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-11-09 11:00:20 -0500

    Update insert_slingshot.sql

--- deprecated-dune-v1-abstractions/optimism2/dex/insert_slingshot.sql
+++ deprecated-dune-v1-abstractions/optimism2/dex/insert_slingshot.sql
@@ -103,12 +103,15 @@ SELECT
         AND pa.contract_address = dexs.token_a_address
         AND pa.hour >= start_ts
         AND pa.hour < end_ts
+	AND pa.symbol NOT IN (SELECT symbol FROM prices.prices_exclude_tokens)
+	
     LEFT JOIN prices.approx_prices_from_dex_data pb
       ON pb.hour = date_trunc('hour', dexs.block_time)
         AND pb.contract_address = dexs.token_b_address
         AND pb.hour >= start_ts
         AND pb.hour < end_ts
-
+	AND pb.symbol NOT IN (SELECT symbol FROM prices.prices_exclude_tokens)
+	
     -- update if we have new info on prices or the erc20
     ON CONFLICT (project, tx_hash, evt_index, trade_id)
     DO UPDATE SET
