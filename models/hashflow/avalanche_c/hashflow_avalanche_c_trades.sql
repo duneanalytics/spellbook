a224d4f0872ccae4dac0ff2aae5fb516e7a05bf7
Huang Geyang <Sukebeta@outlook.com>
2022-11-09 11:08:48 +0800

    Refactor hashflow_trades code structure across chains

--- models/hashflow/avalanche_c/hashflow_avalanche_c_trades.sql
+++ models/hashflow/avalanche_c/hashflow_avalanche_c_trades.sql
@@ -1,6 +1,5 @@
 {{ config(
-    schema = 'hashflow_avalanche_c'
-    ,alias = 'trades'
+    alias = 'trades'
     ,partition_by = ['block_date']
     ,materialized = 'incremental'
     ,file_format = 'delta'
@@ -16,7 +15,7 @@
 {% set project_start_date = '2022-04-11' %}
 
 with dexs AS (
-        select
+        SELECT
             evt_block_time          AS block_time,
             trader                  AS taker,
             ''                      AS maker,
@@ -63,7 +62,7 @@ with dexs AS (
         {% endif %}
 )
 
-select
+SELECT
     'avalanche_c'                                             AS blockchain,
     'hashflow'                                                AS project,
     '1'                                                       AS version,
@@ -71,11 +70,11 @@ select
     dexs.block_time,
     erc20a.symbol                                             AS token_bought_symbol,
     erc20b.symbol                                             AS token_sold_symbol,
-    case
-        when lower(erc20a.symbol) > lower(erc20b.symbol)
-        then concat(erc20b.symbol, '-', erc20a.symbol)
-        else concat(erc20a.symbol, '-', erc20b.symbol)
-        end                                                   AS token_pair,
+    CASE
+        WHEN lower(erc20a.symbol) > lower(erc20b.symbol)
+        THEN concat(erc20b.symbol, '-', erc20a.symbol)
+        ELSE concat(erc20a.symbol, '-', erc20b.symbol)
+        END                                                   AS token_pair,
     dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
     dexs.token_sold_amount_raw / power(10, erc20b.decimals)   AS token_sold_amount,
     dexs.token_bought_amount_raw,
@@ -96,38 +95,38 @@ select
     dexs.trace_address,
     dexs.evt_index
 FROM dexs
-inner join {{ source('avalanche_c', 'transactions') }} tx
-    on dexs.tx_hash = tx.hash
+INNER JOIN {{ source('avalanche_c', 'transactions') }} tx
+    ON dexs.tx_hash = tx.hash
     {% if not is_incremental() %}
-    and tx.block_time >= '{{project_start_date}}'
+    AND tx.block_time >= '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and tx.block_time >= date_trunc("day", now() - interval '1 week')
+    AND tx.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
-left join {{ ref('tokens_erc20') }} erc20a 
-    on erc20a.contract_address = dexs.token_bought_address 
-    and erc20a.blockchain = 'avalanche_c'
-left join {{ ref('tokens_erc20') }} erc20b 
-    on erc20b.contract_address = dexs.token_sold_address 
-    and erc20b.blockchain = 'avalanche_c'
-left join {{ source('prices', 'usd') }} p_bought 
-    on p_bought.minute = date_trunc('minute', dexs.block_time)
-    and p_bought.contract_address = dexs.token_bought_address
-    and p_bought.blockchain = 'avalanche_c'
+LEFT JOIN {{ ref('tokens_erc20') }} erc20a
+    ON erc20a.contract_address = dexs.token_bought_address
+    AND erc20a.blockchain = 'avalanche_c'
+LEFT JOIN {{ ref('tokens_erc20') }} erc20b
+    ON erc20b.contract_address = dexs.token_sold_address
+    AND erc20b.blockchain = 'avalanche_c'
+LEFT JOIN {{ source('prices', 'usd') }} p_bought
+    ON p_bought.minute = date_trunc('minute', dexs.block_time)
+    AND p_bought.contract_address = dexs.token_bought_address
+    AND p_bought.blockchain = 'avalanche_c'
     {% if not is_incremental() %}
-    and p_bought.minute >= '{{project_start_date}}'
+    AND p_bought.minute >= '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and p_bought.minute >= date_trunc("day", now() - interval '1 week')
+    AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
     {% endif %}
-left join {{ source('prices', 'usd') }} p_sold 
-    on p_sold.minute = date_trunc('minute', dexs.block_time)
-    and p_sold.contract_address = dexs.token_sold_address
-    and p_sold.blockchain = 'avalanche_c'
+LEFT JOIN {{ source('prices', 'usd') }} p_sold
+    ON p_sold.minute = date_trunc('minute', dexs.block_time)
+    AND p_sold.contract_address = dexs.token_sold_address
+    AND p_sold.blockchain = 'avalanche_c'
     {% if not is_incremental() %}
-    and p_sold.minute >= '{{project_start_date}}'
+    AND p_sold.minute >= '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and p_sold.minute >= date_trunc("day", now() - interval '1 week')
+    AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
     {% endif %}
     
--- models/hashflow/hashflow_trades.sql
+++ models/hashflow/hashflow_trades.sql
@@ -1,9 +1,9 @@
 {{ config(
         alias='trades',
-        post_hook='{{ expose_spells(\'["avalanche_c"]\',
+        post_hook='{{ expose_spells(\'["ethereum", "avalanche_c"]\',
                         "project",
                         "hashflow",
-                        \'["hosuke"]\') }}'
+                        \'["justabi", "jeff-dude", "hosuke"]\') }}'
         )
 }}
 
@@ -35,6 +35,34 @@ FROM
                 ,trace_address
                 ,evt_index
         FROM {{ ref('hashflow_avalanche_c_trades') }}
+
+        UNION
+
+        SELECT
+                blockchain
+                ,project
+                ,version
+                ,block_date
+                ,block_time
+                ,token_bought_symbol
+                ,token_sold_symbol
+                ,token_pair
+                ,token_bought_amount
+                ,token_sold_amount
+                ,token_bought_amount_raw
+                ,token_sold_amount_raw
+                ,amount_usd
+                ,token_bought_address
+                ,token_sold_address
+                ,taker
+                ,maker
+                ,project_contract_address
+                ,tx_hash
+                ,tx_from
+                ,tx_to
+                ,trace_address
+                ,evt_index
+        FROM {{ ref('hashflow_ethereum_trades') }}
         /*
         UNION
         <add future protocols versions here>
