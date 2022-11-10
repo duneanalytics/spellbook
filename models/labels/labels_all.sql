86603d6962206626286d42ae1226dcd4d32caebd
soispoke <66172107+soispoke@users.noreply.github.com>
2022-11-10 07:53:23 +0000

    Update labels_all.sql

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -13,7 +13,7 @@
  'labels_cex',
  'labels_funds',
  'labels_bridges'
- 'labels_ofac_sanctionned_ethereum'
+ 'labels_ofac_sanctionned_ethereum',
  'labels_multisig_ethereum',
  'labels_hackers_ethereum',
  'labels_mev_ethereum',
