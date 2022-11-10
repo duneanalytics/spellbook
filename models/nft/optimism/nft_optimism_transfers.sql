a115091035bc8c530bde5ca708ca6833466697db
hildobby <h.moulie@gmail.com>
2022-11-10 09:18:03 +0100

    Create nft.transfers: a table combining erc721, erc1155single & erc1155batch transfers (#1871)
    * create nft.transfers
    
    * add to dbt_project
    
    * fix table names
    
    * fix wash trades abstraction
    
    * fix wash trades abstraction
    
    * revert master
    
    * added erc20, 721 & 1155 (single+batch) transfer tables to base_sources
    
    * fix l1 base tables
    
    * lowercased tables names
    
    * fix polygon base table names
    
    * added block_date
    
    * fix
    
    * add block_date
    
    * change unique key
    
    * Update models/nft/avalanche_c/nft_avalanche_c_schema.yml
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/nft/avalanche_c/nft_avalanche_c_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/nft/bnb/nft_bnb_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/nft/gnosis/nft_gnosis_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/nft/nft_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/nft/nft_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/nft/ethereum/nft_ethereum_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/nft/polygon/nft_polygon_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/nft/optimism/nft_optimism_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * added anti join on tx_hash for incremental runs
    
    * added incremental strat for arbitrum
    
    * switch LEFT ANTI JOINs to ANTI JOINs
    
    * merge fix
    
    * Add erc721_arbitrum.evt_Transfer to base sources
    
    * Replace  to
    
    * added incr filters to final table
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: soispoke <66172107+soispoke@users.noreply.github.com>

---
+++ models/nft/optimism/nft_optimism_transfers.sql
@@ -0,0 +1,85 @@
+{{ config(
+        alias ='transfers',
+        partition_by='block_date',
+        materialized='incremental',
+        file_format = 'delta',
+        post_hook='{{ expose_spells(\'["optimism"]\',
+                                    "sector",
+                                    "nft",
+                                    \'["hildobby"]\') }}',
+        unique_key = ['unique_transfer_id']
+)
+}}
+
+ SELECT t.evt_block_time AS block_time
+, date_trunc('day', t.evt_block_time) AS block_date
+, t.evt_block_number AS block_number
+, 'erc721' AS token_standard
+, 'single' AS transfer_type
+, t.evt_index
+, t.contract_address
+, t.tokenId AS token_id
+, 1 AS amount
+, t.from
+, t.to
+, t.evt_tx_hash AS tx_hash
+, 'optimism' || t.evt_tx_hash || '-erc721-' || t.contract_address || '-' || t.tokenId || '-' || t.from || '-' || t.to || '-' || '1' || '-' || t.evt_index AS unique_transfer_id
+FROM {{ source('erc721_optimism','evt_transfer') }} t
+{% if is_incremental() %}
+    ANTI JOIN {{this}} anti_table
+        ON t.evt_tx_hash = anti_table.tx_hash
+{% endif %}
+{% if is_incremental() %}
+WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
+{% endif %}
+UNION
+SELECT t.evt_block_time AS block_time
+, date_trunc('day', t.evt_block_time) AS block_date
+, t.evt_block_number AS block_number
+, 'erc1155' AS token_standard
+, 'single' AS transfer_type
+, t.evt_index
+, t.contract_address
+, t.id AS token_id
+, t.value AS amount
+, t.from
+, t.to
+, t.evt_tx_hash AS tx_hash
+, 'optimism' || t.evt_tx_hash || '-erc721-' || t.contract_address || '-' || t.id || '-' || t.from || '-' || t.to || '-' || t.value || '-' || t.evt_index AS unique_transfer_id
+FROM {{ source('erc1155_optimism','evt_transfersingle') }} t
+{% if is_incremental() %}
+    ANTI JOIN {{this}} anti_table
+        ON t.evt_tx_hash = anti_table.tx_hash
+{% endif %}
+{% if is_incremental() %}
+WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
+{% endif %}
+UNION
+SELECT evt_block_time AS block_time
+, date_trunc('day', evt_block_time) AS block_date
+, evt_block_number AS block_number
+, 'erc1155' AS token_standard
+, 'batch' AS transfer_type
+, evt_index
+, contract_address
+, ids_and_count.ids AS token_id
+, ids_and_count.values AS amount
+, from
+, to
+, evt_tx_hash AS tx_hash
+, 'optimism' || evt_tx_hash || '-erc1155-' || contract_address || '-' || ids_and_count.ids || '-' || from || '-' || to || '-' || ids_and_count.values || '-' || evt_index AS unique_transfer_id
+FROM (
+    SELECT t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, t.evt_index
+    , explode(arrays_zip(t.values, t.ids)) AS ids_and_count
+    FROM {{ source('erc1155_optimism', 'evt_transferbatch') }} t
+    {% if is_incremental() %}
+        ANTI JOIN {{this}} anti_table
+            ON t.evt_tx_hash = anti_table.tx_hash
+    {% endif %}
+    {% if is_incremental() %}
+    WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+    GROUP BY t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, t.evt_index, t.values, t.ids
+    )
+WHERE ids_and_count.values > 0
+GROUP BY evt_block_time, evt_block_number, evt_tx_hash, contract_address, from, to, evt_index, token_id, amount
