f2abb782389044e81ad0cc08d57a44cc004cd461
sohwak <59173567+sohwak@users.noreply.github.com>
2022-11-12 01:49:21 +0900

    Modify `opensea.trades` refer to `seaport.trades` (#1943)
    * modify opensea.trades refer to new seaport
    
    * change trade_type and trade_category
    
    * small fix
    
    * add schema description
    
    * add block_number to source table

--- models/opensea/ethereum/opensea_v3_ethereum_trades.sql
+++ models/opensea/ethereum/opensea_v3_ethereum_trades.sql
@@ -1,9 +1,10 @@
 {{ config(schema='opensea_v3_ethereum'
-         ,alias='events') 
+         ,alias='trades') 
 }}
 
--- opensea.events is compose of mint, burn, and trades. 
--- but now there are only trades.
+-- opensea.trades has the same columns as seaport.trades
+-- only some specified zone_address are recognized as opensea's
+-- project/version : opensea/v3
 -- materialize : view
 
 select blockchain    
@@ -57,4 +58,9 @@ select blockchain
       ,estimated_price
       ,is_private
       ,unique_trade_id
-  from {{ ref('opensea_v3_ethereum_trades') }}
+  from {{ ref('seaport_ethereum_trades') }}
+ where zone_address in ('0xf397619df7bfd4d1657ea9bdd9df7ff888731a11'
+                       ,'0x9b814233894cd227f561b78cc65891aa55c62ad2'
+                       ,'0x004c00500000ad104d7dbd00e3ae0a5c00560c00'
+                       ,'0x110b2b128a9ed1be5ef3232d8e4e41640df5c2cd'
+                       )
