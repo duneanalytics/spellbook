f64ea82ce3e5ac9d705518ef7a419d1bf57a9b59
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2022-11-14 11:48:23 -0500

    syntax fix

--- macros/public/get_href.sql
+++ macros/public/get_href.sql
@@ -1,12 +1,5 @@
 {% macro get_href() %}
       create or replace function get_href(link STRING, text STRING)
-
       returns STRING 
-      as (
-            concat('<a href="'
-            , replace(link,"'",'')
-            , '"target ="_blank">'
-            , replace(text,"'",'')
-            )
-      );
+      return concat('<a href="',link, '"target ="_blank">', text)
 {% endmacro %}
