94f39bfdac254129720412b31369159fb004bc1b
Huang Geyang <Sukebeta@outlook.com>
2022-11-14 17:09:19 +0800

    Add block_date as part of unique_key

--- models/seaport/polygon/seaport_polygon_trades.sql
+++ models/seaport/polygon/seaport_polygon_trades.sql
@@ -4,7 +4,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['tx_hash','evt_index','nft_contract_address','token_id'],
+    unique_key = ['block_date', 'tx_hash','evt_index','nft_contract_address','token_id'],
     post_hook='{{ expose_spells(\'["polygon"]\',
                             "project",
                             "seaport",
