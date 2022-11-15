7af5c55a00325869b184f0e935cdceb308859007
Jacek Trocinski <jacek@hedgehog.app>
2022-11-15 14:01:32 +0100

    Add `view_pools_fees` to query.

--- models/balancer/ethereum/balancer_v2_ethereum_trades.sql
+++ models/balancer/ethereum/balancer_v2_ethereum_trades.sql
@@ -9,7 +9,7 @@
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "balancer_v2",
-                                \'["mendesfabio"]\') }}'
+                                \'["mendesfabio", "jacektrocinski"]\') }}'
     )
 }}
 
@@ -17,24 +17,33 @@
 
 WITH dexs AS (
     SELECT
-        evt_block_time AS block_time,
+        vault_evt_swap.evt_block_time AS block_time,
         '' AS taker,
         '' AS maker,
-        amountOut AS token_bought_amount_raw,
-        amountIn AS token_sold_amount_raw,
+        vault_evt_swap.amountOut AS token_bought_amount_raw,
+        vault_evt_swap.amountIn AS token_sold_amount_raw,
         CAST(NULL as double) AS amount_usd,
-        tokenOut AS token_bought_address,
-        tokenIn AS token_sold_address,
-        poolId AS project_contract_address,
-        evt_tx_hash AS tx_hash,
+        vault_evt_swap.tokenOut AS token_bought_address,
+        vault_evt_swap.tokenIn AS token_sold_address,
+        vault_evt_swap.poolId AS project_contract_address,
+        vault_evt_swap.evt_tx_hash AS tx_hash,
         '' AS trace_address,
-        evt_index
-    FROM {{ source('balancer_v2_ethereum', 'Vault_evt_Swap') }}
-    WHERE tokenIn != SUBSTRING(poolId, 0, 42)
-    AND tokenOut != SUBSTRING(poolId, 0, 42)
-    {% if is_incremental() %}
-    AND evt_block_time >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
+        vault_evt_swap.evt_index
+    FROM {{ source('balancer_v2_ethereum', 'Vault_evt_Swap') }} vault_evt_swap
+    LEFT JOIN {{ ref('balancer_v2_ethereum_pools_fees') }} pools_fees
+        ON pools_fees.contract_address = SUBSTRING(vault_evt_swap.poolId, 0, 42)
+        AND vault_evt_swap.evt_block_time = (
+            SELECT MAX(block_time)
+            FROM balancer_v2.view_pools_fees
+            WHERE block_time <= t.evt_block_time
+            AND contract_address = SUBSTRING(t.`poolId` from 0 for 21)
+        )
+    WHERE
+        vault_evt_swap.tokenIn != SUBSTRING(vault_evt_swap.`poolId`, 0, 42)
+        AND vault_evt_swap.tokenOut != SUBSTRING(vault_evt_swap.`poolId`, 0, 42)
+        {% if is_incremental() %}
+        AND vault_evt_swap.evt_block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
 )
 
 SELECT
@@ -45,22 +54,23 @@ SELECT
     dexs.block_time,
     erc20a.symbol AS token_bought_symbol,
     erc20b.symbol AS token_sold_symbol,
-    case
-        when lower(erc20a.symbol) > lower(erc20b.symbol) then concat(erc20b.symbol, '-', erc20a.symbol)
-        else concat(erc20a.symbol, '-', erc20b.symbol)
-    end as token_pair,
-    dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
-    dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount,
+    CASE
+        WHEN lower(erc20a.symbol) > LOWER(erc20b.symbol) THEN CONCAT(erc20b.symbol, '-', erc20a.symbol)
+        ELSE concat(erc20a.symbol, '-', erc20b.symbol)
+    END AS token_pair,
+    dexs.token_bought_amount_raw / POWER(10, erc20a.decimals) AS token_bought_amount,
+    dexs.token_sold_amount_raw / POWER(10, erc20b.decimals) AS token_sold_amount,
     dexs.token_bought_amount_raw,
     dexs.token_sold_amount_raw,
-    coalesce(
+    COALESCE(
         dexs.amount_usd
-        ,(dexs.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
-        ,(dexs.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
+        ,(dexs.token_bought_amount_raw / POWER(10, p_bought.decimals)) * p_bought.price
+        ,(dexs.token_sold_amount_raw / POWER(10, p_sold.decimals)) * p_sold.price
     ) AS amount_usd,
     dexs.token_bought_address,
     dexs.token_sold_address,
-    coalesce(dexs.taker, tx.from) AS taker, -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
+    -- Subqueries rely on this COALESCE to avoid redundant joins with the transactions table.
+    COALESCE(dexs.taker, tx.from) AS taker, 
     dexs.maker,
     dexs.project_contract_address,
     dexs.tx_hash,
@@ -72,10 +82,10 @@ FROM dexs
 INNER JOIN {{ source('ethereum', 'transactions') }} tx
     ON tx.hash = dexs.tx_hash
     {% if not is_incremental() %}
-    AND tx.block_time >= '{{project_start_date}}'
+    AND tx.block_time >= '{{ project_start_date }}'
     {% endif %}
     {% if is_incremental() %}
-    AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+    AND tx.block_time >= date_trunc("day", NOW() - interval '1 week')
     {% endif %}
 LEFT JOIN {{ ref('tokens_erc20') }} erc20a
     ON erc20a.contract_address = dexs.token_bought_address
@@ -88,19 +98,19 @@ LEFT JOIN {{ source('prices', 'usd') }} p_bought
     AND p_bought.contract_address = dexs.token_bought_address
     AND p_bought.blockchain = 'ethereum'
     {% if not is_incremental() %}
-    AND p_bought.minute >= '{{project_start_date}}'
+    AND p_bought.minute >= '{{ project_start_date }}'
     {% endif %}
     {% if is_incremental() %}
-    AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
+    AND p_bought.minute >= date_trunc("day", NOW() - interval '1 week')
     {% endif %}
 LEFT JOIN {{ source('prices', 'usd') }} p_sold
     ON p_sold.minute = date_trunc('minute', dexs.block_time)
     AND p_sold.contract_address = dexs.token_sold_address
     AND p_sold.blockchain = 'ethereum'
     {% if not is_incremental() %}
-    AND p_sold.minute >= '{{project_start_date}}'
+    AND p_sold.minute >= '{{ project_start_date }}'
     {% endif %}
     {% if is_incremental() %}
-    AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
+    AND p_sold.minute >= date_trunc("day", NOW() - interval '1 week')
     {% endif %}
 ;
