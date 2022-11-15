6f716aa78a89a8b0825ac7a9c8018a040cac5e4a
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2022-11-15 10:24:02 -0500

    Add Zigzag Arbitrum to dex.trades (#1847)
    * Begin setup
    
    Add file structure.  Added v1_trades sql.  Modified dbt_project and added seeds for tests.
    
    * Complete all sql, schema, and sources files
    
    * add zz token and price feed
    
    * Update dex_trades.sql
    
    * minor formatting, fix commas in tokens
    
    * remove test for not null on source
    
    * force evt_index to int
    
    * set evt_index = 1, adjust dex_trades_seeds
    
    * Update zigzag_v1_arbitrum_trades.sql
    
    Change hard coded evt_index to row_number to prevent duplicates
    
    * add contributor, reformat zigzag trades
    
    Co-authored-by: jeff-dude <jeff@dune.xyz>
    Co-authored-by: Augusto <augustog@users.noreply.github.com>

--- models/dex/dex_trades.sql
+++ models/dex/dex_trades.sql
@@ -1,9 +1,9 @@
 {{ config(
         alias ='trades',
-        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism"]\',
+        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum"]\',
                                 "sector",
                                 "dex",
-                                \'["jeff-dude", "hosuke", "0xRob", "pandajackson42", "Henrystats", "scoffie", "zhongyiio", "justabi", "umer_h_adil", "dbustos20"]\') }}'
+                                \'["jeff-dude", "hosuke", "0xRob", "pandajackson42", "Henrystats", "scoffie", "zhongyiio", "justabi", "umer_h_adil", "mtitus6", "dbustos20"]\') }}'
         )
 }}
 
@@ -23,6 +23,7 @@
 ,'platypus_finance_avalanche_c_trades'
 ,'hashflow_ethereum_trades'
 ,'mstable_ethereum_trades'
+,'zigzag_trades'
 ] %}
 
 
