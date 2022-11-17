fe5a831399ee5bfa685c556480f20338d6d1c5b5
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2022-11-17 15:37:18 -0500

    UDFs created and working

--- tests/macros/public/get_chain_explorer_test.sql
+++ tests/macros/public/get_chain_explorer_test.sql
@@ -1,8 +1,7 @@
 
 WITH unit_tests as
 (
-    -- SELECT case when get_chain_explorer('ethereum') = 'https://etherscan.io' then True else False end test
-    SELECT True as test
+    SELECT case when get_chain_explorer('ethereum') = 'https://etherscan.io' then True else False end test
 )
 select count(case when test = false then 1 else null end) as count_rows
 from unit_tests
