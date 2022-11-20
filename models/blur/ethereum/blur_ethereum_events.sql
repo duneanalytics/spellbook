95bd658bdae80517207756eee38d6eb1bef764f2
Hildebert Moulié <h.moulie@gmail.com>
2022-11-20 05:45:58 +0100

    better efficient joins on blur+futureproof for when they launch erc1155 support

--- models/blur/ethereum/blur_ethereum_events.sql
+++ models/blur/ethereum/blur_ethereum_events.sql
@@ -18,9 +18,7 @@ SELECT 'ethereum' AS blockchain
 , bm.evt_block_time AS block_time
 , bm.evt_block_number AS block_number
 , get_json_object(bm.buy, '$.tokenId') AS token_id
-, CASE WHEN erct.evt_block_time IS NOT NULL THEN 'erc721'
-    ELSE 'erc1155'
-    END AS token_standard
+, erct.token_standard
 , nft.name AS collection
 , CASE WHEN get_json_object(bm.buy, '$.amount')=1 THEN 'Single Item Trade'
     ELSE 'Bundle Trade'
@@ -84,29 +82,29 @@ LEFT JOIN {{ source('prices','usd') }} pu ON pu.blockchain='ethereum'
     AND pu.minute >= date_trunc("day", now() - interval '1 week')
     {% endif %}
 LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft ON get_json_object(bm.buy, '$.collection')=nft.contract_address
-LEFT JOIN {{ source('erc721_ethereum','evt_transfer') }} erct ON erct.evt_block_time=bm.evt_block_time
+LEFT JOIN {{ ref('nft_ethereum_transfers') }} erct ON erct.block_time=bm.evt_block_time
     AND get_json_object(bm.buy, '$.collection')=erct.contract_address
-    AND erct.evt_tx_hash=bm.evt_tx_hash
-    AND get_json_object(bm.buy, '$.tokenId')=erct.tokenId
+    AND erct.tx_hash=bm.evt_tx_hash
+    AND get_json_object(bm.buy, '$.tokenId')=erct.token_id
     AND erct.from=get_json_object(bm.sell, '$.trader')
     {% if is_incremental() %}
-    AND erct.evt_block_time >= date_trunc("day", now() - interval '1 week')
+    AND erct.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
-LEFT JOIN {{ source('erc721_ethereum','evt_transfer') }} maker_fix ON maker_fix.evt_block_time=bm.evt_block_time
+LEFT JOIN {{ ref('nft_ethereum_transfers') }} maker_fix ON maker_fix.block_time=bm.evt_block_time
     AND get_json_object(bm.buy, '$.collection')=maker_fix.contract_address
-    AND maker_fix.evt_tx_hash=bm.evt_tx_hash
-    AND get_json_object(bm.buy, '$.tokenId')=maker_fix.tokenId
+    AND maker_fix.tx_hash=bm.evt_tx_hash
+    AND get_json_object(bm.buy, '$.tokenId')=maker_fix.token_id
     AND maker_fix.from=agg.contract_address
     {% if is_incremental() %}
-    AND maker_fix.evt_block_time >= date_trunc("day", now() - interval '1 week')
+    AND maker_fix.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
-LEFT JOIN {{ source('erc721_ethereum','evt_transfer') }} taker_fix ON taker_fix.evt_block_time=bm.evt_block_time
+LEFT JOIN {{ ref('nft_ethereum_transfers') }} taker_fix ON taker_fix.block_time=bm.evt_block_time
     AND get_json_object(bm.buy, '$.collection')=taker_fix.contract_address
-    AND taker_fix.evt_tx_hash=bm.evt_tx_hash
-    AND get_json_object(bm.buy, '$.tokenId')=taker_fix.tokenId
+    AND taker_fix.tx_hash=bm.evt_tx_hash
+    AND get_json_object(bm.buy, '$.tokenId')=taker_fix.token_id
     AND taker_fix.to=agg.contract_address
     {% if is_incremental() %}
-    AND taker_fix.evt_block_time >= date_trunc("day", now() - interval '1 week')
+    AND taker_fix.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
 {% if is_incremental() %}
 WHERE bm.evt_block_time >= date_trunc("day", now() - interval '1 week')
