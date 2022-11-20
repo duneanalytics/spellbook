76b887c511de3f8fc3b1cbfccadf4ff444fea72a
jh <0xroll.onchain@gmail.com>
2022-11-20 16:22:12 +0800

    fix unit_test cols

--- tests/jarvis_network/polygon/jarvis_network_polygon_view_all_transactions.sql
+++ tests/jarvis_network/polygon/jarvis_network_polygon_view_all_transactions.sql
@@ -7,7 +7,7 @@ with unit_test as (
         case when test.collateral_symbol = actual.collateral_symbol then true else false end as collateral_token_test,
         case when test.collateral_token_amount = actual.collateral_token_amount then true else false end as collateral_token_amount_test,
         case when test.net_collateral_amount = actual.net_collateral_amount then true else false end as net_collateral_amount_test,
-        case when ROUND(test.net_amount_usd,4) = ROUND(actual.net_amount_usd,4) then true else false end as net_amount_usd_test,
+        case when ROUND(test.net_collateral_amount_usd,4) = ROUND(actual.net_collateral_amount_usd,4) then true else false end as net_collateral_amount_usd_test,
         case when ROUND(test.fee_amount_usd,4) = ROUND(actual.fee_amount_usd,4) then true else false end as fee_amount_usd_test,
         case when test.evt_index = actual.evt_index then true else false end as evt_index_test
     from {{ref ('jarvis_network_view_all_transactions')}} as actual
@@ -18,4 +18,4 @@ with unit_test as (
 
 select * from unit_test
 where (action_test = false or user_test = false or jfiat_token_test = false or jfiat_token_amount_test = false or collateral_token_test = false or collateral_token_amount_test = false
-or net_collateral_amount_test = false or net_amount_usd_test = false or fee_amount_usd_test = false or evt_index_test = false)
+or net_collateral_amount_test = false or net_collateral_amount_usd_test = false or fee_amount_usd_test = false or evt_index_test = false)
