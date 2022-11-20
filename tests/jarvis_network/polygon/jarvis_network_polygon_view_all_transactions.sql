d73c35bb20118e6b07ba7a71082f96ae6c303ee3
jh <0xroll.onchain@gmail.com>
2022-11-20 23:39:02 +0800

    edit unit test

--- tests/jarvis_network/polygon/jarvis_network_polygon_view_all_transactions.sql
+++ tests/jarvis_network/polygon/jarvis_network_polygon_view_all_transactions.sql
@@ -7,8 +7,8 @@ with unit_test as (
         case when test.collateral_symbol = actual.collateral_symbol then true else false end as collateral_token_test,
         case when test.collateral_token_amount = actual.collateral_token_amount then true else false end as collateral_token_amount_test,
         case when test.net_collateral_amount = actual.net_collateral_amount then true else false end as net_collateral_amount_test,
-        case when ROUND(test.net_collateral_amount_usd,4) = ROUND(actual.net_collateral_amount_usd,4) then true else false end as net_collateral_amount_usd_test,
-        case when ROUND(test.fee_amount_usd,4) = ROUND(actual.fee_amount_usd,4) then true else false end as fee_amount_usd_test
+        case when test.net_collateral_amount_usd = actual.net_collateral_amount_usd then true else false end as net_collateral_amount_usd_test,
+        case when test.fee_amount_usd, = actual.fee_amount_usd then true else false end as fee_amount_usd_test
     from {{ref ('jarvis_network_view_all_transactions')}} as actual
     INNER JOIN {{ref ('jarvis_network_polygon_view_transactions')}} as test
     on (actual.evt_tx_hash = test.evt_tx_hash AND actual.evt_index = test.evt_index)
