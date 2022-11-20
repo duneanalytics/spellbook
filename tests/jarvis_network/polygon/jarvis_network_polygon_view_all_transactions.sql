c1f7e143939ea06b494ab84a82e0bc5ef75ba2b0
jh <0xroll.onchain@gmail.com>
2022-11-20 17:54:50 +0800

    add conditions on unit_test and add more test results

--- tests/jarvis_network/polygon/jarvis_network_polygon_view_all_transactions.sql
+++ tests/jarvis_network/polygon/jarvis_network_polygon_view_all_transactions.sql
@@ -8,14 +8,13 @@ with unit_test as (
         case when test.collateral_token_amount = actual.collateral_token_amount then true else false end as collateral_token_amount_test,
         case when test.net_collateral_amount = actual.net_collateral_amount then true else false end as net_collateral_amount_test,
         case when ROUND(test.net_collateral_amount_usd,4) = ROUND(actual.net_collateral_amount_usd,4) then true else false end as net_collateral_amount_usd_test,
-        case when ROUND(test.fee_amount_usd,4) = ROUND(actual.fee_amount_usd,4) then true else false end as fee_amount_usd_test,
-        case when test.evt_index = actual.evt_index then true else false end as evt_index_test
+        case when ROUND(test.fee_amount_usd,4) = ROUND(actual.fee_amount_usd,4) then true else false end as fee_amount_usd_test
     from {{ref ('jarvis_network_view_all_transactions')}} as actual
     INNER JOIN {{ref ('jarvis_network_polygon_view_transactions')}} as test
-    on actual.evt_tx_hash = test.evt_tx_hash
+    on (actual.evt_tx_hash = test.evt_tx_hash AND actual.evt_index = test.evt_index)
 
 )
 
 select * from unit_test
 where (action_test = false or user_test = false or jfiat_token_test = false or jfiat_token_amount_test = false or collateral_token_test = false or collateral_token_amount_test = false
-or net_collateral_amount_test = false or net_collateral_amount_usd_test = false or fee_amount_usd_test = false or evt_index_test = false)
+or net_collateral_amount_test = false or net_collateral_amount_usd_test = false or fee_amount_usd_test = false)
