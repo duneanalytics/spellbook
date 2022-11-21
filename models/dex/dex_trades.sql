51fd8fcced3c19ac97a388159e0f44be47156963
Hennadii Ivtushok <hivtushok@gmail.com>
2022-11-21 22:10:57 +0100

    Cleanup

--- models/dex/dex_trades.sql
+++ models/dex/dex_trades.sql
@@ -3,7 +3,6 @@
         post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum"]\',
                                 "sector",
                                 "dex",
-
                                 \'["jeff-dude", "hosuke", "0xRob", "pandajackson42", "Henrystats", "scoffie", "zhongyiio", "justabi", "umer_h_adil", "mtitus6", "dbustos20", "tian7"]\') }}'
         )
 }}
