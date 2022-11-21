db519787a59828851b1ca445a3e8a6e0f1a0d182
soispoke <66172107+soispoke@users.noreply.github.com>
2022-11-21 10:14:38 +0100

    Update compound_v2_ethereum_votes.sql

--- models/compound/ethereum/compound_v2_ethereum_votes.sql
+++ models/compound/ethereum/compound_v2_ethereum_votes.sql
@@ -49,7 +49,7 @@ SELECT
 FROM {{ source('compound_v2_ethereum', 'GovernorBravoDelegate_evt_VoteCast') }} vc
 LEFT JOIN cte_sum_votes csv ON vc.proposalId = csv.proposalId
 LEFT JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', evt_block_time)
-    AND p.symbol = 'UNI'
+    AND p.symbol = 'COMP'
     AND p.blockchain ='ethereum'
     {% if is_incremental() %}
     AND p.minute >= date_trunc("day", now() - interval '1 week')
