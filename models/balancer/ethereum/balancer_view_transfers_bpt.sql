2dc7939cee2c019e2ed1298f5e9ef7cb538f9207
Stefenon <victorstefenon0@gmail.com>
2022-11-22 14:25:35 +0100

    Create first version of dynamic view transfers bpt

---
+++ models/balancer/ethereum/balancer_view_transfers_bpt.sql
@@ -0,0 +1,22 @@
+{% set query %}
+    SELECT DISTINCT CONCAT(namespace, '_ethereum', '.', name, '_evt_Transfer') AS event
+    FROM ethereum.contracts c
+    JOIN balancer_v2_ethereum.Vault_evt_PoolRegistered p
+    ON p.poolAddress = c.address
+  {% endset %}
+
+{% set results = run_query(query) %}
+{% if execute %}
+{# Return the first column #}
+{% set transfer_tables = results.columns[0].values() %}
+{% else %}
+{% set transfer_tables = [] %}
+{% endif %}
+
+{% for transfer_table in transfer_tables %}
+SELECT * FROM {{transfer_table}}
+{% if not loop.last %}
+UNION ALL
+{% endif %}
+{% endfor %}
+
