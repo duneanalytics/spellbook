e584cdf1590d968dc71790e6c670348009f9cbfc
jh <0xroll.onchain@gmail.com>
2022-11-22 18:25:42 +0800

    fix syntax in unit_test

--- tests/chainlink/polygon/chainlink_polygon_get_oracle_price.sql
+++ tests/chainlink/polygon/chainlink_polygon_get_oracle_price.sql
@@ -3,7 +3,7 @@ with unit_test as (
         case when test.oracle_price = actual.oracle_price then true else false end                                          AS oracle_price_test,
         case when test.proxy_address = actual.proxy_address then true else false end                                        AS proxy_address_test,
         case when test.aggregator_address = actual.aggregator_address then true else false end                              AS aggregator_address_test,
-        case when test.underlying_token_address = actual.underlying_token_address then true else false end                  AS underlying_token_address_test,
+        case when test.underlying_token_address = actual.underlying_token_address then true else false end                  AS underlying_token_address_test
     from       {{ref ('chainlink_polygon_price_feeds')}} actual
     INNER JOIN {{ref ('chainlink_polygon_get_price')}} test 
     ON (actual.blockchain = test.blockchain AND actual.block_time = test.block_time AND actual.feed_name = test.feed_name)
