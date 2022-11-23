faf204f7a1af8c7746f794ff25f6439a7baa1c23
Stefenon <victorstefenon0@gmail.com>
2022-11-23 20:43:49 +0100

    Apply incremental logic to transfers_bpt

--- models/balancer/arbitrum/balancer_v2_arbitrum_transfers_bpt.sql
+++ models/balancer/arbitrum/balancer_v2_arbitrum_transfers_bpt.sql
@@ -8,6 +8,9 @@
     ) 
 }}
 
+{% set event_signature = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' %}
+{% set project_start_date = '2021-08-26' %}
+
 WITH registered_pools AS (
     SELECT
       DISTINCT poolAddress AS pool_address
@@ -27,5 +30,10 @@ SELECT DISTINCT * FROM (
         bytea2numeric(SUBSTRING(logs.data, 32, 64)) AS value
     FROM {{ source('arbitrum', 'logs') }} logs
     INNER JOIN registered_pools p ON p.pool_address = logs.contract_address
-    WHERE logs.topic1 = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
-    AND logs.block_number >= 224873 ) transfers
+    WHERE logs.topic1 = '{{ event_signature }}'
+        {% if not is_incremental() %}
+        AND logs.block_time >= '{{ project_start_date }}'
+        {% endif %}
+        {% if is_incremental() %}
+        AND logs.block_time >= DATE_TRUNC('day', NOW() - interval '1 week')
+        {% endif %} ) transfers
