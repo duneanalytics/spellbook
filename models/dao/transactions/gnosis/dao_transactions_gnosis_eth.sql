67c54ecd53f63cf86daf2dc7c747921cf524162f
henrystats <65398020+henrystats@users.noreply.github.com>
2022-11-23 18:38:11 +0100

    Seperate mainet tokens from wrapped tokens (#2047)

--- models/dao/transactions/gnosis/dao_transactions_gnosis_eth.sql
+++ models/dao/transactions/gnosis/dao_transactions_gnosis_eth.sql
@@ -27,7 +27,7 @@ transactions as (
         SELECT 
             block_time, 
             tx_hash, 
-            LOWER('0xe91d153e0b41518a2ce8dd3d7944fa863463a97d') as token, 
+            LOWER('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee') as token, 
             value as value, 
             to as dao_wallet_address, 
             'tx-in' as tx_type, 
@@ -52,7 +52,7 @@ transactions as (
         SELECT 
             block_time, 
             tx_hash, 
-            LOWER('0xe91d153e0b41518a2ce8dd3d7944fa863463a97d') as token, 
+            LOWER('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee') as token, 
             value as value, 
             from as dao_wallet_address, 
             'tx_out' as tx_type, 
@@ -82,10 +82,10 @@ SELECT
     t.block_time, 
     t.tx_type,
     t.token as asset_contract_address,
-    COALESCE(er.symbol, t.token) as asset,
+    'xDAI' as asset,
     t.value as raw_value, 
-    t.value/POW(10, COALESCE(er.decimals, 18)) as value, 
-    t.value/POW(10, COALESCE(er.decimals, 18)) * p.price as usd_value, 
+    t.value/POW(10, 18) as value, 
+    t.value/POW(10, 18) * p.price as usd_value, 
     t.tx_hash, 
     t.tx_index,
     t.address_interacted_with,
@@ -96,13 +96,9 @@ INNER JOIN
 dao_tmp dt 
     ON t.dao_wallet_address = dt.dao_wallet_address
 LEFT JOIN 
-{{ ref('tokens_erc20') }} er 
-    ON t.token = er.contract_address
-    AND er.blockchain = 'gnosis'
-LEFT JOIN 
 {{ source('prices', 'usd') }} p 
     ON p.minute = date_trunc('minute', t.block_time)
-    AND p.contract_address = t.token
+    AND p.symbol = 'WXDAI'
     AND p.blockchain = 'gnosis'
     {% if not is_incremental() %}
     AND p.minute >= '{{transactions_start_date}}'
