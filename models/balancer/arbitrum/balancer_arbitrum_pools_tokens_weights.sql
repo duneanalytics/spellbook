9874c5e95d91071f6e6abeddfb222be8fa4bcb9d
Jacek Trocinski <jacek@hedgehog.app>
2022-11-24 16:56:07 +0100

    Add `pools_tokens_weights` for Arbitrum, Polygon, and Optimism.

---
+++ models/balancer/arbitrum/balancer_arbitrum_pools_tokens_weights.sql
@@ -0,0 +1,123 @@
+{{
+    config(
+        alias='pools_tokens_weights',
+        post_hook='{{ expose_spells(\'["arbitrum"]\',
+                                    "project",
+                                    "balancer",
+                                    \'["metacrypto", "jacektrocinski"]\') }}'
+    ) 
+}}
+
+WITH events AS (
+    -- Binds
+    SELECT
+        bind.call_block_number AS block_number,
+        tx.index,
+        bind.call_trace_address,
+        bind.contract_address AS pool,
+        bind.token,
+        bind.denorm
+    FROM {{ source('balancer_v1_arbitrum', 'BPool_call_bind') }} bind
+    INNER JOIN {{ source('arbitrum', 'transactions') }} tx ON tx.hash = bind.call_tx_hash 
+    WHERE bind.call_success = TRUE
+
+    UNION ALL
+
+    -- Rebinds
+    SELECT
+        rebind.call_block_number AS block_number,
+        tx.index,
+        rebind.call_trace_address,
+        rebind.contract_address AS pool,
+        rebind.token,
+        rebind.denorm
+    FROM {{ source('balancer_v1_arbitrum', 'BPool_call_rebind') }} rebind
+    INNER JOIN {{ source('arbitrum', 'transactions') }} tx ON tx.hash = rebind.call_tx_hash 
+    WHERE rebind.call_success = TRUE
+
+    UNION ALL
+    
+    -- Unbinds
+    SELECT
+        unbind.call_block_number AS block_number, 
+        tx.index,
+        unbind.call_trace_address,
+        unbind.contract_address AS pool,
+        unbind.token,
+        '0' AS denorm
+    FROM {{ source('balancer_v1_arbitrum', 'BPool_call_unbind') }} unbind
+    INNER JOIN {{ source('arbitrum', 'transactions') }} tx ON tx.hash = unbind.call_tx_hash 
+    WHERE unbind.call_success = TRUE
+),
+state_with_gaps AS (
+    SELECT
+        events.block_number,
+        events.pool,
+        events.token,
+        events.denorm,
+        LEAD(events.block_number, 1) OVER (
+            PARTITION BY events.pool, events.token 
+            ORDER BY events.block_number, index, call_trace_address
+        ) AS next_block_number
+    FROM events 
+), 
+settings AS (
+    SELECT
+        pool, 
+        token, 
+        denorm
+    FROM state_with_gaps
+    WHERE
+        next_block_number IS NULL
+        AND denorm <> '0'
+),
+sum_denorm AS (
+    SELECT
+        pool,
+        SUM(denorm) AS sum_denorm
+    FROM state_with_gaps
+    WHERE
+        next_block_number IS NULL
+        AND denorm <> '0'
+    GROUP BY pool
+),
+norm_weights AS (
+    SELECT
+        settings.pool AS pool_address,
+        token AS token_address,
+        denorm / sum_denorm AS normalized_weight
+    FROM settings
+    INNER JOIN sum_denorm ON settings.pool = sum_denorm.pool
+)
+SELECT
+    pool_address AS pool_id,
+    token_address,
+    normalized_weight
+FROM norm_weights
+
+UNION ALL
+
+SELECT
+    registered.`poolId` AS pool_id,
+    tokens.token_address,
+    weights.normalized_weight / POWER(10, 18) AS normalized_weight
+FROM {{ source('balancer_v2_arbitrum', 'Vault_evt_PoolRegistered') }} registered
+INNER JOIN {{ source('balancer_v2_arbitrum', 'WeightedPoolFactory_call_create') }} call_create
+    ON call_create.call_tx_hash = registered.evt_tx_hash
+    LATERAL VIEW posexplode(call_create.tokens) tokens AS pos, token_address
+    LATERAL VIEW posexplode(call_create.weights) weights AS pos, normalized_weight
+WHERE tokens.pos = weights.pos
+
+UNION ALL
+
+SELECT
+    registered.`poolId` AS pool_id,
+    tokens.token_address,
+    weights.normalized_weight / POWER(10, 18) AS normalized_weight
+FROM {{ source('balancer_v2_arbitrum', 'Vault_evt_PoolRegistered') }} registered
+INNER JOIN {{ source('balancer_v2_arbitrum', 'WeightedPool2TokensFactory_call_create') }} call_create
+    ON call_create.call_tx_hash = registered.evt_tx_hash
+    LATERAL VIEW posexplode(call_create.tokens) tokens AS pos, token_address
+    LATERAL VIEW posexplode(call_create.weights) weights AS pos, normalized_weight
+WHERE tokens.pos = weights.pos
+;
