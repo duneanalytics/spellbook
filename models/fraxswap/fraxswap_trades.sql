a971672e78c6b27859c0f3d329de98f0dbe895a7
Lexi Kndlexi <93421296+Kndlexi@users.noreply.github.com>
2022-11-24 04:17:09 +0100

    add ethereum fraxswap to dex trades

--- models/fraxswap/fraxswap_trades.sql
+++ models/fraxswap/fraxswap_trades.sql
@@ -1,6 +1,6 @@
 {{ config(
         alias ='trades',
-        post_hook='{{ expose_spells(\'["avalanche_c", "bnb", "polygon"]\',
+        post_hook='{{ expose_spells(\'["avalanche_c", "bnb", "polygon", "ethereum"]\',
                                 "project",
                                 "fraxswap",
                                 \'["zhongyiio", "kndlexi", "hosuke"]\') }}'
@@ -91,6 +91,34 @@ FROM
                 ,trace_address
                 ,evt_index
         FROM {{ ref('fraxswap_polygon_trades') }}
+
+        UNION 
+
+               SELECT
+                blockchain
+                ,project
+                ,version
+                ,block_date
+                ,block_time
+                ,token_bought_symbol
+                ,token_sold_symbol
+                ,token_pair
+                ,token_bought_amount
+                ,token_sold_amount
+                ,token_bought_amount_raw
+                ,token_sold_amount_raw
+                ,amount_usd
+                ,token_bought_address
+                ,token_sold_address
+                ,taker
+                ,maker
+                ,project_contract_address
+                ,tx_hash
+                ,tx_from
+                ,tx_to
+                ,trace_address
+                ,evt_index
+        FROM {{ ref('fraxswap_ethereum_trades') }}
  
         /*
         UNION
