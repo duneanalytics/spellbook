8c6f0bcebb8e633067394d6f25101eebde8bb103
0xRob <83790096+0xRobin@users.noreply.github.com>
2022-11-28 21:09:01 +0100

    Better generic seed checking tests (#1707)
    * check seed test
    
    * remove dynamic column extraction
    
    * default to just warn
    
    * remove older dex_trades_token_bought test
    
    * change wording
    
    * remove dev leftovers
    
    * reduce to 1 join
    
    * Add inner join for a model_sample before left join by seed
    
    * Recover dex_trades_token_bought generic test
    
    * update generic test to be at child model
    
    * move check_seed to macro, introduce check_dex_seed
    
    * check_seed schema
    
    * rename dir
    
    * cleanup
    
    * change macro name collision
    
    * update uniswap models
    
    * update sushiswap models
    
    * update kyberswap
    
    * update fraxswap
    
    * update curve
    
    * update clipper
    
    * update shibaswap
    
    * update swapr
    
    * update defiswap
    
    * update dfx
    
    * update pancakeswap
    
    * update woofi
    
    * update platypus
    
    * update hashflow
    
    * update zigzag
    
    * update gmx
    
    * update biswap
    
    * add comments to show models without new test
    
    * fix commas
    
    * fix compile errors
    
    * fix seed file with test results
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>

--- models/fraxswap/fraxswap_trades.sql
+++ models/fraxswap/fraxswap_trades.sql
@@ -7,36 +7,42 @@
         )
 }}
 
+{% set frax_models = [
+'fraxswap_avalanche_c_trades'
+] %}
+
+
 SELECT *
-FROM
-(
-        SELECT
-                blockchain
-                ,project
-                ,version
-                ,block_date
-                ,block_time
-                ,token_bought_symbol
-                ,token_sold_symbol
-                ,token_pair
-                ,token_bought_amount
-                ,token_sold_amount
-                ,CAST(token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw
-                ,CAST(token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw
-                ,amount_usd
-                ,token_bought_address
-                ,token_sold_address
-                ,taker
-                ,maker
-                ,project_contract_address
-                ,tx_hash
-                ,tx_from
-                ,tx_to
-                ,trace_address
-                ,evt_index
-        FROM {{ ref('fraxswap_avalanche_c_trades') }}
-        /*
-        UNION ALL
-        <add future blockchains here>
-        */
+FROM (
+    {% for dex_model in frax_models %}
+    SELECT
+        blockchain,
+        project,
+        version,
+        block_date,
+        block_time,
+        token_bought_symbol,
+        token_sold_symbol,
+        token_pair,
+        token_bought_amount,
+        token_sold_amount,
+        CAST(token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw,
+        CAST(token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw,
+        amount_usd,
+        token_bought_address,
+        token_sold_address,
+        taker,
+        maker,
+        project_contract_address,
+        tx_hash,
+        tx_from,
+        tx_to,
+        trace_address,
+        evt_index
+    FROM {{ ref(dex_model) }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
 )
+;
