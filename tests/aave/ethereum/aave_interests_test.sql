98f5ee5d8bea87d0484db7ee67b1bf97e97fdaa3
Belén Pruvost <belen@dune.com>
2022-11-29 18:36:21 +0100

    T-20626 - Make  aave_v1_ethereum_borrow  Trino compatible (#2146)

--- tests/aave/ethereum/aave_interests_test.sql
+++ tests/aave/ethereum/aave_interests_test.sql
@@ -7,7 +7,7 @@ with unit_test1
         where  reserve = '0xdac17f958d2ee523a2206206994597c13d831ec7'
                and hour = '2022-08-22 12:00'),
     unit_test2
-    as (select case when deposit_apy = 0.004223674732695223 then true
+    as (select case when deposit_apy = 0.004223674732695224 then true
                  else false
                end as test
         from   {{ ref('aave_v2_ethereum_interest_rates' )}}
