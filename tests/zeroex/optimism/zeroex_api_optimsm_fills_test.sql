cc90468e99d1525c28c269b25cfa8918f24a0c62
rantum <andrew@ecomloop.com>
2022-11-29 12:58:15 -0800

    revise test sql

--- tests/zeroex/optimism/zeroex_api_optimsm_fills_test.sql
+++ tests/zeroex/optimism/zeroex_api_optimsm_fills_test.sql
@@ -3,7 +3,7 @@ WITH unit_tests as
                 and test.taker_token_amount = actual.taker_token_amount 
                 and test.taker_token_address = actual.taker_token_address 
 then True else False end as test
-FROM {{ ref('zeroex_optimism_fills') }} actual
+FROM {{ ref('zeroex_view_0x_api_fills') }} actual
 JOIN {{ ref('zeroex_optimism_fills_sample') }} test ON test.tx_hash = actual.tx_hash
 )
 select count(case when test = false then 1 else null end)/count(*) as pct_mismatch, count(*) as count_rows
