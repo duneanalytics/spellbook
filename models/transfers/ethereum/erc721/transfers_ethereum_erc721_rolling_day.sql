ad218f9786020b731023f1eb898c1adf70f39934
Belén Pruvost <belen@dune.com>
2022-11-30 14:13:56 +0100

    T-21220 - Make balances_ethereum.erc721_latest Trino compatible

--- models/transfers/ethereum/erc721/transfers_ethereum_erc721_rolling_day.sql
+++ models/transfers/ethereum/erc721/transfers_ethereum_erc721_rolling_day.sql
@@ -8,6 +8,6 @@
             wallet_address,
             token_address,
             tokenId,
-            current_timestamp() as updated_at,
+            cast(current_timestamp as timestamp) as updated_at,
             row_number() over (partition by token_address, tokenId order by day desc) as recency_index
         from {{ ref('transfers_ethereum_erc721_agg_day') }}
