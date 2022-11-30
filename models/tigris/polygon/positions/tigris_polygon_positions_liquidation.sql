a428a1cf7c05a3ec287cf99ea1e91c974cac6ce6
jeff-dude <jeff@dune.xyz>
2022-11-30 15:15:44 -0500

    fix rank

--- models/tigris/polygon/positions/tigris_polygon_positions_liquidation.sql
+++ models/tigris/polygon/positions/tigris_polygon_positions_liquidation.sql
@@ -6,83 +6,83 @@
 WITH 
 
 last_margin as (
-        SELECT 
-            evt_block_time,
-            position_id,
-            version,
-            margin,
-            evt_index
-        FROM 
-        (
+    SELECT 
+        evt_block_time,
+        position_id,
+        version,
+        margin,
+        evt_index
+    FROM 
+    (
         SELECT 
             ROW_NUMBER() OVER (PARTITION BY position_id, version ORDER BY evt_index DESC) as rank_,
             * 
         FROM 
         (
-        SELECT 
-            xx.evt_block_time,
-            xx.position_id,
-            xx.version, 
-            xy.margin,
-            xy.evt_index
-        FROM 
-        (
-        SELECT 
-            MAX(evt_block_time) as evt_block_time,
-            position_id,
-            version 
-        FROM 
-        {{ ref('tigris_polygon_positions_margin') }}
-        GROUP BY 2, 3 
-        ) xx 
-        INNER JOIN 
-        {{ ref('tigris_polygon_positions_margin') }} xy 
-            ON xx.evt_block_time = xy.evt_block_time
-            AND xx.position_id = xy.position_id
-            AND xx.version = xy.version
+            SELECT 
+                xx.evt_block_time,
+                xx.position_id,
+                xx.version, 
+                xy.margin,
+                xy.evt_index
+            FROM 
+            (
+            SELECT 
+                MAX(evt_block_time) as evt_block_time,
+                position_id,
+                version 
+            FROM 
+            {{ ref('tigris_polygon_positions_margin') }}
+            GROUP BY 2, 3 
+            ) xx 
+            INNER JOIN 
+            {{ ref('tigris_polygon_positions_margin') }} xy 
+                ON xx.evt_block_time = xy.evt_block_time
+                AND xx.position_id = xy.position_id
+                AND xx.version = xy.version
         ) tmp 
-        WHERE rank_ = 1 
-        ) tmp_2
+    ) tmp_2
+    WHERE rank_ = 1 
 ),
 
 last_leverage as (
-        SELECT 
-            evt_block_time,
-            position_id,
-            version,
-            leverage,
-            evt_index 
-        FROM 
-        (
+    SELECT 
+        evt_block_time,
+        position_id,
+        version,
+        leverage,
+        evt_index 
+    FROM 
+    (
         SELECT 
             ROW_NUMBER() OVER (PARTITION BY position_id, version ORDER BY evt_index DESC) as rank_,
             * 
         FROM 
         (
-        SELECT 
-            xx.evt_block_time,
-            xx.position_id,
-            xx.version, 
-            xy.leverage,
-            xy.evt_index
-        FROM 
-        (
-        SELECT 
-            MAX(evt_block_time) as evt_block_time,
-            position_id,
-            version 
-        FROM 
-        {{ ref('tigris_polygon_positions_leverage') }}
-        GROUP BY 2, 3 
-        ) xx 
-        INNER JOIN 
-        {{ ref('tigris_polygon_positions_leverage') }} xy 
-            ON xx.evt_block_time = xy.evt_block_time
-            AND xx.position_id = xy.position_id
-            AND xx.version = xy.version
+            SELECT 
+                xx.evt_block_time,
+                xx.position_id,
+                xx.version, 
+                xy.leverage,
+                xy.evt_index
+            FROM 
+            (
+            SELECT 
+                MAX(evt_block_time) as evt_block_time,
+                position_id,
+                version 
+            FROM 
+            {{ ref('tigris_polygon_positions_leverage') }}
+            GROUP BY 2, 3 
+            ) xx 
+            INNER JOIN 
+            {{ ref('tigris_polygon_positions_leverage') }} xy 
+                ON xx.evt_block_time = xy.evt_block_time
+                AND xx.position_id = xy.position_id
+                AND xx.version = xy.version
         ) tmp 
-        WHERE rank_ = 1 
-        ) tmp_2
+    ) tmp_2
+    WHERE rank_ = 1 
 )
 
 SELECT 
