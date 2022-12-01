1bc6f6863b007e8ab87abf510584f3d4df202a82
jh <0xroll.onchain@gmail.com>
2022-12-01 23:39:53 +0800

    removing underlying token address for unit_test

--- tests/chainlink/bnb/chainlink_bnb_get_oracle_prices.sql
+++ tests/chainlink/bnb/chainlink_bnb_get_oracle_prices.sql
@@ -2,8 +2,7 @@ with unit_test as (
     select
         case when ABS(test.oracle_price - actual.oracle_price) < 0.00001 then true else false end                           AS oracle_price_test,
         case when test.proxy_address = actual.proxy_address then true else false end                                        AS proxy_address_test,
-        case when test.aggregator_address = actual.aggregator_address then true else false end                              AS aggregator_address_test,
-        case when test.underlying_token_address = actual.underlying_token_address then true else false end                  AS underlying_token_address_test
+        case when test.aggregator_address = actual.aggregator_address then true else false end                              AS aggregator_address_test
     from       {{ref ('chainlink_bnb_price_feeds')}} actual
     INNER JOIN {{ref ('chainlink_get_price_seed')}} test 
     ON (actual.blockchain = test.blockchain     AND 
@@ -16,6 +15,5 @@ select * from unit_test
 where (
        oracle_price_test = false                 OR 
        proxy_address_test   = false              OR
-       aggregator_address_test = false           OR 
-       underlying_token_address_test = false
+       aggregator_address_test = false          
       )
