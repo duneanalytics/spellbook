d56d5a137ddaa2c971dc868bbcf242458d2296b2
Hildebert Moulié <h.moulie@gmail.com>
2022-12-01 12:33:47 -0500

    fix

--- tests/generic/check_nft_seed.sql
+++ tests/generic/check_nft_seed.sql
@@ -5,7 +5,7 @@
     {{ config(severity = 'warn') }}
     {%- set seed_file = ref('nft_trades_seed') -%}
     {%- set seed_check_columns = ['buyer','seller'] -%}
-    {%- set seed_matching_columns = ['block_date','blockchain','project','version','tx_hash','evt_index','nft_contract_address','token_id','sub_type','sub_idx'] -%}
+    {%- set seed_matching_columns = ['block_date','blockchain','project','version','tx_hash','nft_contract_address','token_id'] -%}
     {%- set filter = {'blockchain':blockchain, 'project':project, 'version':version} -%}
 
     {{ check_seed_macro(model,seed_file,seed_matching_columns,seed_check_columns,filter) }}
