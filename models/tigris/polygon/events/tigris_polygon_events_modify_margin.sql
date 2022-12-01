0d2fc52159c789d487bfa5af1c7c70947fe11799
Henrystats <65398020+henrystats@users.noreply.github.com>
2022-12-01 14:43:38 +0100

    fix synthax

--- models/tigris/polygon/events/tigris_polygon_events_modify_margin.sql
+++ models/tigris/polygon/events/tigris_polygon_events_modify_margin.sql
@@ -156,8 +156,6 @@ modify_margin_v8 as (
         WHERE t.evt_tx_hash NOT IN ('0x561cde89720f8af596bf8958dd96339d8b3923094d6d27dd8bf14f5326c9ae25', '0x17e49a19c4feaf014bf485ee2277bfa09375bde9931da9a95222de7a1e704d70', '0x146e22e33c8218ac8c70502b292bbc6d9334983135a1e70ffe0125784bfdcc91')
 )
 
-SELECT * FROM 
-
 SELECT *, 'v5' as version FROM modify_margin_v5
 
 UNION ALL
