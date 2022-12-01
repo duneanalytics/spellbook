5859e3f7ea1df3fba4d6306cd1e63f24c73cc949
soispoke <66172107+soispoke@users.noreply.github.com>
2022-12-01 11:06:46 +0100

    Update gas_bnb_fees.sql

--- models/gas/bnb/gas_bnb_fees.sql
+++ models/gas/bnb/gas_bnb_fees.sql
@@ -17,20 +17,20 @@ SELECT
     txns.from AS tx_sender, 
     txns.to AS tx_receiver,
     'BNB' as native_token_symbol,
-    value/1e18 AS tx_amount_native,
-    value/1e18 * p.price AS tx_amount_usd,
-    (gas_price * txns.gas_used)/1e18 AS tx_fee_native, 
-    (gas_price * txns.gas_used)/1e18 * p.price  AS tx_fee_usd,
-    CASE WHEN block_number >= 13082000 AND txns.to = '0x0000000000000000000000000000000000001000' THEN value/1e18 * 10 / 100 
+    CAST(value AS DOUBLE)/1e18 AS tx_amount_native,
+    CAST(value AS DOUBLE)/1e18 * p.price AS tx_amount_usd,
+    (CAST(CAST(gas_price AS DOUBLE) AS DOUBLE) * CAST(txns.gas_used AS DOUBLE))/1e18 AS tx_fee_native, 
+    (CAST(CAST(gas_price AS DOUBLE) AS DOUBLE) * CAST(txns.gas_used AS DOUBLE))/1e18 * p.price  AS tx_fee_usd,
+    CASE WHEN block_number >= 13082000 AND txns.to = '0x0000000000000000000000000000000000001000' THEN CAST(value AS DOUBLE)/1e18 * 10 / 100 
         ELSE NULL::double END AS burned_native, -- change after BEP95
-    CASE WHEN block_number >= 13082000 AND txns.to = '0x0000000000000000000000000000000000001000' THEN value/1e18 * 10 / 100 * p.price 
+    CASE WHEN block_number >= 13082000 AND txns.to = '0x0000000000000000000000000000000000001000' THEN CAST(value AS DOUBLE)/1e18 * 10 / 100 * p.price 
         ELSE NULL::double END AS burned_usd, -- change after BEP95
     miner AS validator,
-    gas_price /1e9 AS gas_price_gwei,
-    gas_price / 1e18 * p.price AS gas_price_usd,
-    txns.gas_used,
-    txns.gas_used / txns.gas_limit * 100 AS gas_usage_percent,
-    txns.gas_limit,
+    CAST(CAST(gas_price AS DOUBLE) AS DOUBLE) /1e9 AS CAST(CAST(gas_price AS DOUBLE) AS DOUBLE)_gwei,
+    CAST(CAST(gas_price AS DOUBLE) AS DOUBLE) / 1e18 * p.price AS CAST(CAST(gas_price AS DOUBLE) AS DOUBLE)_usd,
+    CAST(txns.gas_used AS DOUBLE),
+    CAST(txns.gas_used AS DOUBLE) / CAST(txns.gas_limit AS DOUBLE) * 100 AS gas_usage_percent,
+    CAST(txns.gas_limit AS DOUBLE),
     difficulty,
     type AS transaction_type
 FROM {{ source('bnb','transactions') }} txns
