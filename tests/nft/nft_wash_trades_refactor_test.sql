18ec230b100bcea203a29170190b3a66aede2b4c
Hildebert Moulié <h.moulie@gmail.com>
2022-12-02 01:15:05 -0500

    fix test

--- tests/nft/nft_wash_trades_refactor_test.sql
+++ tests/nft/nft_wash_trades_refactor_test.sql
@@ -1,8 +1,8 @@
 WITH unit_tests as
 (SELECT
-        case when test_data.back_and_forth_trade = wash_trades.filter2_back_and_forth_trade then True else False end as test1,
-        case when test_data.bought_it_three_times_within_a_week = wash_trades.filter3_bought_or_sold_3x then True else False end as test2,
-        case when test_data.funded_by_same_wallet = wash_trades.filter4_first_funded_by_same_wallet then True else False end as test3,
+        case when test_data.back_and_forth_trade = wash_trades.filter_2_back_and_forth_trade then True else False end as test1,
+        case when test_data.bought_it_three_times_within_a_week = wash_trades.filter_3_bought_or_sold_3x then True else False end as test2,
+        case when test_data.funded_by_same_wallet = wash_trades.filter_4_first_funded_by_same_wallet then True else False end as test3,
         case when test_data.is_wash_trade = wash_trades.is_wash_trade then True else False end as test4
 FROM {{ ref('nft_wash_trades') }} wash_trades
 JOIN {{ ref('nft_wash_trades_refactor_seed') }} test_data ON test_data.unique_trade_id = wash_trades.unique_trade_id
