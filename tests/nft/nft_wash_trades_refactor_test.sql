fbc66f51b571394bd0f16757340ff4637b111577
Hildebert Moulié <h.moulie@gmail.com>
2022-12-02 01:35:39 -0500

    fix test

--- tests/nft/nft_wash_trades_refactor_test.sql
+++ tests/nft/nft_wash_trades_refactor_test.sql
@@ -1,16 +1,24 @@
-WITH unit_tests as
-(SELECT
-        case when test_data.back_and_forth_trade = wash_trades.filter_2_back_and_forth_trade then True else False end as test1,
-        case when test_data.bought_it_three_times_within_a_week = wash_trades.filter_3_bought_or_sold_3x then True else False end as test2,
-        case when test_data.funded_by_same_wallet = wash_trades.filter_4_first_funded_by_same_wallet then True else False end as test3,
-        case when test_data.is_wash_trade = wash_trades.is_wash_trade then True else False end as test4
+WITH unit_tests AS (
+        SELECT
+        CASE WHEN test_data.back_and_forth_trade = wash_trades.filter_2_back_and_forth_trade THEN true
+                ELSE false
+                END AS test_1
+        , CASE WHEN test_data.bought_it_three_times_within_a_week = wash_trades.filter_3_bought_or_sold_3x THEN true
+                ELSE false
+                END AS test_2
+        , CASE WHEN test_data.funded_by_same_wallet = wash_trades.filter_4_first_funded_by_same_wallet THEN true
+                ELSE false
+                END AS test_3
+        , CASE WHEN test_data.is_wash_trade = wash_trades.is_wash_trade THEN true
+                ELSE false
+                END AS test_4
 FROM {{ ref('nft_wash_trades') }} wash_trades
-JOIN {{ ref('nft_wash_trades_refactor_seed') }} test_data ON test_data.unique_trade_id = wash_trades.unique_trade_id
+INNER JOIN {{ ref('nft_wash_trades_refactor_seed') }} test_data ON test_data.unique_trade_id = wash_trades.unique_trade_id
 
 )
-select *
-from unit_tests
-where test1 is false
-   or test2 is false
-   or test3 is false
-   or test4 is false
+SELECT *
+FROM unit_tests
+WHERE test_1 IS false
+   OR test_2 IS false
+   OR test_3 IS false
+   OR test_4 IS false
