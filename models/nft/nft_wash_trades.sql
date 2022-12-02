6e3be0458778969209681a2168287dca25b62f9e
Hildebert Moulié <h.moulie@gmail.com>
2022-12-02 01:14:05 -0500

    rework wash filter into separate cteD

--- models/nft/nft_wash_trades.sql
+++ models/nft/nft_wash_trades.sql
@@ -12,6 +12,125 @@
 }}
 
 
+WITH filter_1 AS (
+    SELECT unique_trade_id
+    , CASE WHEN nftt.buyer=nftt.seller
+        THEN true
+        ELSE false 
+        END AS same_buyer_seller
+    FROM {{ source('table_sample_nft', 'trades_2022_11_20') }} nftt
+    WHERE nftt.blockchain='ethereum'
+        AND nftt.unique_trade_id IS NOT NULL
+        {% if is_incremental() %}
+        AND nftt.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    )
+
+, filter_2 AS (
+    SELECT nftt.unique_trade_id
+    , CASE WHEN COUNT(filter_baf.block_number) > 0
+        THEN true
+        ELSE false 
+        END AS back_and_forth_trade
+    FROM {{ source('table_sample_nft', 'trades_2022_11_20') }} nftt
+    INNER JOIN {{ source('table_sample_nft', 'trades_2022_11_20') }} filter_baf
+        ON filter_baf.seller=nftt.buyer
+        AND filter_baf.buyer=nftt.seller
+        AND filter_baf.nft_contract_address=nftt.nft_contract_address
+        AND filter_baf.token_id=nftt.token_id
+        {% if is_incremental() %}
+        AND filter_baf.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    WHERE nftt.blockchain='ethereum'
+        AND nftt.unique_trade_id IS NOT NULL
+        {% if is_incremental() %}
+        AND nftt.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    GROUP BY nftt.unique_trade_id
+    )
+
+, filter_3_bought AS (
+    SELECT nftt.unique_trade_id
+    , CASE WHEN COUNT(filter_bought_3x.block_number) > 2
+        THEN true
+        ELSE false
+        END AS bought_3x
+    FROM {{ source('table_sample_nft', 'trades_2022_11_20') }} nftt
+    INNER JOIN {{ source('table_sample_nft', 'trades_2022_11_20') }} filter_bought_3x
+        ON filter_bought_3x.nft_contract_address=nftt.nft_contract_address
+        AND filter_bought_3x.token_id=nftt.token_id
+        AND filter_bought_3x.buyer=nftt.buyer
+        AND filter_bought_3x.token_standard IN ('erc721', 'erc20')
+        {% if is_incremental() %}
+        AND filter_bought_3x.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    WHERE nftt.blockchain='ethereum'
+        AND nftt.unique_trade_id IS NOT NULL
+        {% if is_incremental() %}
+        AND nftt.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    GROUP BY nftt.unique_trade_id
+    )
+
+, filter_3_sold AS (
+    SELECT nftt.unique_trade_id
+    , CASE WHEN COUNT(filter_sold_3x.block_number) > 2
+        THEN true
+        ELSE false
+        END AS sold_3x
+    FROM {{ source('table_sample_nft', 'trades_2022_11_20') }} nftt
+    INNER JOIN {{ source('table_sample_nft', 'trades_2022_11_20') }} filter_sold_3x
+        ON filter_sold_3x.nft_contract_address=nftt.nft_contract_address
+        AND filter_sold_3x.token_id=nftt.token_id
+        AND filter_sold_3x.seller=nftt.seller
+        AND filter_sold_3x.token_standard IN ('erc721', 'erc20')
+        {% if is_incremental() %}
+        AND filter_sold_3x.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    WHERE nftt.blockchain='ethereum'
+        AND nftt.unique_trade_id IS NOT NULL
+        {% if is_incremental() %}
+        AND nftt.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    GROUP BY nftt.unique_trade_id
+    )
+
+, filter_4 AS (
+    SELECT nftt.unique_trade_id
+    , CASE WHEN filter_funding_buyer.first_funded_by = filter_funding_seller.first_funded_by
+        OR filter_funding_buyer.first_funded_by = nftt.seller
+        OR filter_funding_seller.first_funded_by = nftt.buyer
+        THEN true
+        ELSE false
+        END AS first_funded_by_same_wallet
+    , filter_funding_buyer.first_funded_by AS buyer_first_funded_by
+    , filter_funding_seller.first_funded_by AS seller_first_funded_by
+    FROM {{ source('table_sample_nft', 'trades_2022_11_20') }} nftt
+        INNER JOIN addresses_events_ethereum.first_funded_by filter_funding_buyer
+        ON filter_funding_buyer.address=nftt.buyer
+        AND filter_funding_buyer.first_funded_by NOT IN (SELECT DISTINCT address FROM labels.bridges)
+        AND filter_funding_buyer.first_funded_by NOT IN (SELECT DISTINCT address FROM labels.cex)
+        AND filter_funding_buyer.first_funded_by NOT IN (SELECT DISTINCT contract_address FROM tornado_cash.withdrawals)
+        {% if is_incremental() %}
+        AND filter_funding_buyer.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    INNER JOIN addresses_events_ethereum.first_funded_by filter_funding_seller
+        ON filter_funding_seller.address=nftt.seller
+        AND filter_funding_seller.first_funded_by NOT IN (SELECT DISTINCT address FROM labels.bridges)
+        AND filter_funding_seller.first_funded_by NOT IN (SELECT DISTINCT address FROM labels.cex)
+        AND filter_funding_seller.first_funded_by NOT IN (SELECT DISTINCT contract_address FROM tornado_cash.withdrawals)
+        {% if is_incremental() %}
+        AND filter_funding_seller.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    WHERE nftt.blockchain='ethereum'
+        AND nftt.unique_trade_id IS NOT NULL
+        AND nftt.buyer IS NOT NULL
+        AND nftt.seller IS NOT NULL
+        {% if is_incremental() %}
+        AND nftt.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
+    )
+
 SELECT nftt.blockchain
     , nftt.project
     , nftt.version
@@ -31,106 +150,41 @@ SELECT nftt.blockchain
     , nftt.block_number
     , nftt.tx_hash
     , nftt.unique_trade_id
-    , filter_funding_buyer.first_funded_by AS buyer_first_funded_by
-    , filter_funding_seller.first_funded_by AS seller_first_funded_by
-    , CASE WHEN nftt.buyer=nftt.seller
+    , buyer_first_funded_by
+    , seller_first_funded_by
+    , CASE WHEN filter_1.same_buyer_seller
         THEN true
         ELSE false 
-        END AS filter1_same_buyer_seller
-    , CASE WHEN COUNT(filter_baf.block_number) > 0
+        END AS filter_1_same_buyer_seller
+    , CASE WHEN filter_2.back_and_forth_trade
         THEN true
         ELSE false 
-        END AS filter2_back_and_forth_trade
-    , CASE WHEN COUNT(filter_bought_3x.block_number) > 2
-        OR COUNT(filter_sold_3x.block_number) > 2
+        END AS filter_2_back_and_forth_trade
+    , CASE WHEN filter_3_bought.bought_3x
+        OR filter_3_sold.sold_3x
         THEN true
         ELSE false
-        END AS filter3_bought_or_sold_3x
-    , CASE WHEN filter_funding_buyer.first_funded_by = filter_funding_seller.first_funded_by
-        OR filter_funding_buyer.first_funded_by = nftt.seller
-        OR filter_funding_seller.first_funded_by = nftt.buyer
+        END AS filter_3_bought_or_sold_3x
+    , CASE WHEN filter_4.first_funded_by_same_wallet
         THEN true
         ELSE false
-        END AS filter4_first_funded_by_same_wallet
-    , CASE WHEN nftt.buyer=nftt.seller
-        OR filter_funding_buyer.first_funded_by = filter_funding_seller.first_funded_by
-        OR filter_funding_buyer.first_funded_by = nftt.seller
-        OR filter_funding_seller.first_funded_by = nftt.buyer
-        OR COUNT(filter_baf.block_number) > 0
-        OR COUNT(filter_bought_3x.block_number) > 2
-        OR COUNT(filter_sold_3x.block_number) > 2
+        END AS filter_4_first_funded_by_same_wallet
+    , CASE WHEN filter_1.same_buyer_seller
+        OR filter_2.back_and_forth_trade
+        OR filter_3_bought.bought_3x
+        OR filter_3_sold.sold_3x
+        OR filter_4.first_funded_by_same_wallet
         THEN true
         ELSE false
         END AS is_wash_trade
 FROM {{ source('table_sample_nft', 'trades_2022_11_20') }} nftt
-LEFT JOIN {{ source('table_sample_nft', 'trades_2022_11_20') }} filter_baf
-    ON filter_baf.seller=nftt.buyer
-    AND filter_baf.buyer=nftt.seller
-    AND filter_baf.nft_contract_address=nftt.nft_contract_address
-    AND filter_baf.token_id=nftt.token_id
-    {% if is_incremental() %}
-    AND filter_baf.block_time >= date_trunc("day", NOW() - interval '1 week')
-    {% endif %}
-LEFT JOIN {{ source('table_sample_nft', 'trades_2022_11_20') }} filter_bought_3x
-    ON filter_bought_3x.nft_contract_address=nftt.nft_contract_address
-    AND filter_bought_3x.token_id=nftt.token_id
-    AND filter_bought_3x.buyer=nftt.buyer
-    AND filter_bought_3x.token_standard IN ('erc721', 'erc20')
-    {% if is_incremental() %}
-    AND filter_bought_3x.block_time >= date_trunc("day", NOW() - interval '1 week')
-    {% endif %}
-LEFT JOIN {{ source('table_sample_nft', 'trades_2022_11_20') }} filter_sold_3x
-    ON filter_sold_3x.nft_contract_address=nftt.nft_contract_address
-    AND filter_sold_3x.token_id=nftt.token_id
-    AND filter_sold_3x.seller=nftt.seller
-    AND filter_sold_3x.token_standard IN ('erc721', 'erc20')
-    {% if is_incremental() %}
-    AND filter_sold_3x.block_time >= date_trunc("day", NOW() - interval '1 week')
-    {% endif %}
-LEFT JOIN {{ source('table_sample_nft', 'addresses_events_ethereum_first_funded_by_GT_2022_11_10') }} filter_funding_buyer
-    ON filter_funding_buyer.address=nftt.buyer
-    AND filter_funding_buyer.first_funded_by NOT IN (SELECT DISTINCT address FROM {{ ref('labels_bridges') }})
-    AND filter_funding_buyer.first_funded_by NOT IN (SELECT DISTINCT address FROM {{ ref('labels_cex') }})
-    AND filter_funding_buyer.first_funded_by NOT IN (SELECT DISTINCT contract_address FROM {{ ref('tornado_cash_withdrawals') }})
-    {% if is_incremental() %}
-    AND filter_funding_buyer.block_time >= date_trunc("day", NOW() - interval '1 week')
-    {% endif %}
-LEFT JOIN {{ source('table_sample_nft', 'addresses_events_ethereum_first_funded_by_GT_2022_11_10') }} filter_funding_seller
-    ON filter_funding_seller.address=nftt.seller
-    AND filter_funding_seller.first_funded_by NOT IN (SELECT DISTINCT address FROM {{ ref('labels_bridges') }})
-    AND filter_funding_seller.first_funded_by NOT IN (SELECT DISTINCT address FROM {{ ref('labels_cex') }})
-    AND filter_funding_seller.first_funded_by NOT IN (SELECT DISTINCT contract_address FROM {{ ref('tornado_cash_withdrawals') }})
-    {% if is_incremental() %}
-    AND filter_funding_seller.block_time >= date_trunc("day", NOW() - interval '1 week')
-    {% endif %}
-{% if is_incremental() %}
-LEFT ANTI JOIN {{this}} nft_wtf ON nftt.unique_trade_id = nft_wtf.unique_trade_id
-{% endif %}
+LEFT JOIN filter_1 ON nftt.unique_trade_id=filter_1.unique_trade_id
+LEFT JOIN filter_2 ON nftt.unique_trade_id=filter_2.unique_trade_id
+LEFT JOIN filter_3_bought ON nftt.unique_trade_id=filter_3_bought.unique_trade_id
+LEFT JOIN filter_3_sold ON nftt.unique_trade_id=filter_3_sold.unique_trade_id
+LEFT JOIN filter_4 ON nftt.unique_trade_id=filter_4.unique_trade_id
 WHERE nftt.blockchain='ethereum'
-AND nftt.unique_trade_id IS NOT NULL
-AND nftt.buyer IS NOT NULL
-AND nftt.seller IS NOT NULL
-    {% if is_incremental() %}
-    AND nftt.block_time >= date_trunc("day", NOW() - interval '1 week')
-    {% endif %}
-GROUP BY nftt.blockchain
-    , nftt.project
-    , nftt.version
-    , nftt.nft_contract_address
-    , nftt.token_id
-    , nftt.token_standard
-    , nftt.trade_category
-    , nftt.buyer
-    , nftt.seller
-    , nftt.project_contract_address
-    , nftt.aggregator_name
-    , nftt.aggregator_address
-    , nftt.tx_from
-    , nftt.tx_to
-    , nftt.block_time
-    , nftt.block_number
-    , nftt.tx_hash
-    , nftt.unique_trade_id
-    , filter_funding_buyer.first_funded_by
-    , filter_funding_seller.first_funded_by
-;
+    AND nftt.unique_trade_id IS NOT NULL
+        {% if is_incremental() %}
+        AND nftt.block_time >= date_trunc("day", NOW() - interval '1 week')
+        {% endif %}
