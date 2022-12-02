ad2f60dc9e2fab4767378e961932dfecf75e84b2
1chioku <102664310+1chioku@users.noreply.github.com>
2022-12-02 12:20:00 +0400

    Backtracked interval

--- models/gmx/arbitrum/glp/gmx_arbitrum_glp_components.sql
+++ models/gmx/arbitrum/glp/gmx_arbitrum_glp_components.sql
@@ -55,7 +55,7 @@ SELECT
     dai_current_price
 FROM {{ref('gmx_arbitrum_glp_components_base')}}
 {% if is_incremental() %}
-WHERE minute >= date_trunc("day", now() - interval '1 day')
+WHERE minute >= date_trunc("day", now() - interval '1 week')
 {% endif %}
 {% if not is_incremental() %}
 WHERE minute >= '{{project_start_date}}'
