12280b09154ff72e70a0577c8d63cd53bae3ef62
crypto586 <96981187+crypto586@users.noreply.github.com>
2022-12-04 18:45:04 +0000

    feat(bnb_token_balances): Adds balance facilities for bnb bep20 tokens

---
+++ models/balances/bnb/bep20/balances_bnb_bep20_day.sql
@@ -0,0 +1,59 @@
+{{ config(
+        alias='bep20_day',
+        post_hook='{{ expose_spells(\'["bnb"]\',
+                                            "sector",
+                                            "balances",
+                                            \'["crypto586"]\') }}'
+        )
+}}
+
+WITH
+
+days AS (
+     -- BSC mainnet launch date
+    SELECT explode(sequence(to_date('2020-08-31'), date_trunc('day', now()), interval 1 day)) AS day
+),
+
+daily_balances AS (
+    SELECT
+        wallet_address,
+        token_address,
+        amount_raw,
+        amount,
+        day,
+        symbol,
+        lead(day, 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY day) AS next_day
+    FROM
+        {{ ref('transfers_bnb_bep20_rolling_day') }}
+)
+
+SELECT
+    'bnb' AS blockchain,
+    d.day,
+    b.wallet_address,
+    b.token_address,
+    b.amount_raw,
+    b.amount,
+    b.amount * p.price AS amount_usd,
+    b.symbol
+FROM
+    daily_balances b
+INNER JOIN
+    days d ON b.day <= d.day
+        AND d.day < b.next_day
+
+LEFT JOIN
+    {{ source('prices', 'usd') }} p ON p.contract_address = b.token_address
+        AND d.day = p.minute
+        AND p.blockchain = 'bnb'
+
+-- Removes rebase tokens from balances (I don't know why actually. I kept the logic from balances_ethereum_erc20_day).
+LEFT JOIN
+    {{ ref('tokens_bnb_rebase') }} AS r ON b.token_address = r.contract_address
+
+-- Removes likely non-compliant tokens due to negative balances
+LEFT JOIN
+    {{ ref('balances_bnb_bep20_noncompliant') }} AS nc ON b.token_address = nc.token_address
+
+WHERE
+    r.contract_address IS NULL AND nc.token_address IS NULL
---
+++ models/balances/bnb/bep20/balances_bnb_bep20_hour.sql
@@ -0,0 +1,55 @@
+{{ config(
+        alias='bep20_hour',
+        post_hook='{{ expose_spells(\'["bnb"]\',
+                                            "sector",
+                                            "balances",
+                                            \'["crypto586"]\') }}'
+        )
+}}
+
+WITH
+hours AS (
+    -- BSC mainnet launch date
+    SELECT explode(sequence(to_date('2020-08-31'), date_trunc('hour', now()), interval 1 hour)) AS hour
+),
+
+hourly_balances AS (
+    SELECT
+        wallet_address,
+        token_address,
+        amount_raw,
+        amount,
+        hour,
+        symbol,
+        lead(hour, 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY hour) AS next_hour
+    FROM
+        {{ ref('transfers_bnb_bep20_rolling_hour') }}
+)
+
+SELECT
+    'bnb' as blockchain,
+    h.hour,
+    b.wallet_address,
+    b.token_address,
+    b.amount_raw,
+    b.amount,
+    b.amount * p.price as amount_usd,
+    b.symbol
+FROM
+    hourly_balances b
+INNER JOIN
+    hours h ON b.hour <= h.hour AND h.hour < b.next_hour
+
+LEFT JOIN
+    {{ source('prices', 'usd') }} p ON p.contract_address = b.token_address
+    AND h.hour = p.minute
+    AND p.blockchain = 'bnb'
+
+-- Removes rebase tokens from balances
+LEFT JOIN {{ ref('tokens_bnb_rebase') }} AS r ON b.token_address = r.contract_address
+
+-- Removes likely non-compliant tokens due to negative balances
+LEFT JOIN {{ ref('balances_bnb_bep20_noncompliant') }} AS nc ON b.token_address = nc.token_address
+
+WHERE
+    r.contract_address IS NULL AND nc.token_address IS NULL
