ce99edcca513a0f21f6c6ca828e01637c88b7b60
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2022-12-04 12:31:18 -0500

    fixed things

--- models/abi/signatures.sql
+++ models/abi/signatures.sql
@@ -5,6 +5,10 @@
         file_format = 'delta',
         incremental_strategy = 'merge',
         unique_key = ['created_at', 'unique_signature_id'],
+        post_hook='{{ expose_spells(\'["ethereum","bnb","avalanche_c","optimism","arbitrum","gnosis","polygon"]\',
+                        "sector",
+                        "abi",
+                        \'["ilemi"]\') }}'
         )
 }}
 
@@ -16,8 +20,8 @@ WITH
             SELECT
                 *
                 , concat(id, signature, type) as unique_signature_id
-            FROM {{ source('{{chain}}','signatures') }}
-            
+            FROM {{chain}}.signatures --can't get source to work with {{chain}} variable
+
             {% if is_incremental() %}
             WHERE created_at >= date_trunc("day", now() - interval '2 days')
             {% endif %}
