c78c67f83a1710681f890f161c5d556fe1a8fdc0
Huang Geyang <Sukebeta@outlook.com>
2022-12-06 22:51:00 +0800

    Refactor long lines shorter

--- models/wombat/bnb/wombat_bnb_trades.sql
+++ models/wombat/bnb/wombat_bnb_trades.sql
@@ -15,24 +15,29 @@
 {% set project_start_date = '2022-04-18' %}
 
 with wombat_swaps_all_pools as (
-	-- main pool
-	select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address from {{ source('wombat_bnb', 'Pool_evt_Swap') }}
-	union all
-	-- side pool
-	select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address from {{ source('wombat_bnb', 'HighCovRatioFeePool_evt_Swap') }}
-	union all
-	-- bnb pool
-	select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address from {{ source('wombat_bnb', 'DynamicPool_evt_Swap') }}
-	union all
-	-- mwom pool
-	select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address from {{ source('wombat_bnb', 'mWOM_Pool_evt_Swap') }}
-	union all
-	-- qwom pool
-	select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address from {{ source('wombat_bnb', 'qWOM_WOMPool_evt_Swap') }}
-	union all
-	-- wmx pool
-	select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address from {{ source('wombat_bnb', 'WMX_WOM_Pool_evt_Swap') }}
-)
+    -- main pool
+    select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address
+    from {{ source('wombat_bnb', 'Pool_evt_Swap') }}
+    union all
+    -- side pool
+    select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address
+    from {{ source('wombat_bnb', 'HighCovRatioFeePool_evt_Swap') }}
+    union all
+    -- bnb pool
+    select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address
+    from {{ source('wombat_bnb', 'DynamicPool_evt_Swap') }}
+    union all
+    -- mwom pool
+    select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address
+    from {{ source('wombat_bnb', 'mWOM_Pool_evt_Swap') }}
+    union all
+    -- qwom pool
+    select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address
+    from {{ source('wombat_bnb', 'qWOM_WOMPool_evt_Swap') }}
+    union all
+    -- wmx pool
+    select toAmount, fromAmount, toToken, fromToken, evt_block_time, evt_tx_hash, evt_index, to, contract_address
+    from {{ source('wombat_bnb', 'WMX_WOM_Pool_evt_Swap') }})
 select
 	'bnb' as blockchain
 	, 'wombat' as project
