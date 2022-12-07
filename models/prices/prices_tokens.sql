21ef0c4ce55258c82a75b2f85144a5cf3bacadec
Ben Smith <bh2smith@gmail.com>
2022-12-07 10:32:25 +0100

    add a couple more

--- models/prices/prices_tokens.sql
+++ models/prices/prices_tokens.sql
@@ -1579,6 +1579,8 @@ VALUES
     ("dai-dai", "gnosis", "DAI", "0x44fa8e6f47987339850636f88629646662444217", 18),
     ("busd-binance-usd", "gnosis", "BUSD", "0xdd96b45877d0e8361a4ddb732da741e97f3191ff", 18),
     ("crv-curve-dao-token", "gnosis", "CRV", "0x712b3d230f3c1c19db860d80619288b1f0bdd0bd", 18),
+    ("link-chainlink", "gnosis", "LINK", "0xe2e73a1c69ecf83f464efce6a5be353a37ca09b2", 18),
+    ("sushi-sushi", "gnosis", "SUSHI", "0x2995d1317dcd4f0ab89f4ae60f3f020a4f17c7ce", 18),
 
     --tokens not yet supported or are not active on coinpaprika are commented out
     ("eth-ethereum", "optimism", "ETH", "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", 18),
