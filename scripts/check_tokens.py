197d32337b676e04c2307babaf5a08d637ccf727
Meghan <msheintz@gmail.com>
2022-12-07 17:32:04 -0500

    intentional error

--- scripts/check_tokens.py
+++ scripts/check_tokens.py
@@ -1,6 +1,6 @@
 from scripts.token_checker import TokenChecker
 
-with open('tests/new_lines.txt') as f:
+WWWwith open('tests/new_lines.txt') as f:
     new_lines = f.read().strip().split('\n')
 new_lines = [new_line.lstrip('+').strip() for new_line in new_lines]
 exceptions = 0
