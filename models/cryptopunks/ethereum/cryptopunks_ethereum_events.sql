0c37cef784f07fbc08530d7e254c78fb7a6c3a3e
cat <cat377550@gmail.com>
2022-12-07 09:48:07 -0800

    udpate schema for new file

--- models/cryptopunks/ethereum/cryptopunks_ethereum_events.sql
+++ models/cryptopunks/ethereum/cryptopunks_ethereum_events.sql
@@ -4,10 +4,6 @@
         file_format ='delta',
         incremental_strategy='merge',
         unique_key='unique_trade_id',
-        post_hook='{{ expose_spells(\'["ethereum"]\',
-                                    "project",
-                                    "cryptopunks",
-                                    \'["masquot", "cat"]\') }}'
         )
 }}
 
