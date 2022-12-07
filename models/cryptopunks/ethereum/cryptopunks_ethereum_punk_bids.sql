b429bd4c0a60bc80eb580b31ae962a007f03b6eb
cat <cat377550@gmail.com>
2022-12-06 20:45:06 -0800

    add punk bids incremental table

---
+++ models/cryptopunks/ethereum/cryptopunks_ethereum_punk_bids.sql
@@ -0,0 +1,36 @@
+{{ config(
+        alias ='punk_bids',
+        partition_by = ['evt_block_time_week'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['evt_block_time_week', 'evt_tx_hash']
+        )
+}}
+
+
+select  'Bid Entered' as event_type
+        , fromAddress as bidder
+        , punkIndex as punk_id
+        , value/1e18 as eth_value
+        , evt_block_time
+        , date_trunc('week',evt_block_time) as evt_block_time_week
+        , evt_block_number
+        , evt_index
+        , evt_tx_hash 
+from {{ source('cryptopunks_ethereum','CryptoPunksMarket_evt_PunkBidEntered') }}
+{% if is_incremental() %} where evt_block_time >= date_trunc('day', now() - interval '1 week') {% endif %}  
+
+union all 
+
+select  'Bid Withdrawn' as event_type
+        , fromAddress as bidder
+        , punkIndex as punk_id
+        , value/1e18 as eth_value
+        , evt_block_time
+        , date_trunc('week',evt_block_time) as evt_block_time_week
+        , evt_block_number
+        , evt_index
+        , evt_tx_hash 
+from {{ source('cryptopunks_ethereum','CryptoPunksMarket_evt_PunkBidWithdrawn') }}
+{% if is_incremental() %} where evt_block_time >= date_trunc('day', now() - interval '1 week') {% endif %}  
