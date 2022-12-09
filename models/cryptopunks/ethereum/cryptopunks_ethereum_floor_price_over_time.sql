252a1acad7fab870a3fedd8257a8a5cdad0f2e4f
Belén Pruvost <belen@dune.com>
2022-12-09 15:24:16 +0100

    T-21622 - Hide more views (#2276)

--- models/cryptopunks/ethereum/cryptopunks_ethereum_floor_price_over_time.sql
+++ models/cryptopunks/ethereum/cryptopunks_ethereum_floor_price_over_time.sql
@@ -1,7 +1,7 @@
 {{ config(
         alias ='floor_price_over_time',
         unique_key='day',
-        post_hook='{{ expose_spells(\'["ethereum"]\',
+        post_hook='{{ expose_spells_hide_trino(\'["ethereum"]\',
                                     "project",
                                     "cryptopunks",
                                     \'["cat"]\') }}'
