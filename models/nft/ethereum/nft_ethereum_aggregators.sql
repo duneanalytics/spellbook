9dc9913ceeb6f5666ad727396332d0d0961b5c6a
hildobby <h.moulie@gmail.com>
2022-12-14 00:23:42 +0100

    adding more aggregators (#2307)
    Co-authored-by: jeff-dude <jeff@dune.xyz>

--- models/nft/ethereum/nft_ethereum_aggregators.sql
+++ models/nft/ethereum/nft_ethereum_aggregators.sql
@@ -17,7 +17,8 @@ FROM
     , ('0x0000000035634b55f3d99b071b5a354f48e10bef', 'Gem') -- Gem Single Contract Checkout 2
     , ('0x00000000a50bb64b4bbeceb18715748dface08af', 'Gem') -- Gem Single Contract Checkout 3
     , ('0xae9c73fd0fd237c1c6f66fe009d24ce969e98704', 'Gem') -- Gem Protection Enabled Address
-    , ('0x9e97195f937c9372fe5fda5e3b86e9b88cbefed7', 'Gem') -- Gem's X2Y2 Batch Buys
+    , ('0x9e97195f937c9372fe5fda5e3b86e9b88cbefed7', 'Gem') -- Gem's X2Y2 Batch Buys (Old)
+    , ('0x539ea5d6ec0093ff6401dbcd14d049c37a77151b', 'Gem') -- Gem's X2Y2 Batch Buys
     , ('0xf22007700b8c443bcb36a39580f7804bffdb1169', 'Gem') -- Gem's Blur Batch Buys
     , ('0x56dd5bbede9bfdb10a2845c4d70d4a2950163044', 'X2Y2') -- X2Y2's OpenSea Sniper
     , ('0x69cf8871f61fb03f540bc519dd1f1d4682ea0bf6', 'Element') -- Element NFT Marketplace Aggregator
@@ -41,4 +42,7 @@ FROM
     , ('0x552b16d19dbad7af2786fe5a40d96d2a5c09428c', 'Alpha Sharks') -- Alpha Sharks 2.0
     , ('0x114e54a100a0415abf9727234c92c83dbcc59abf', 'Alpha Sharks') -- Alpha Sharks 2.1
     , ('0xef1c6e67703c7bd7107eed8303fbe6ec2554bf6b', 'Uniswap') -- Uniswap's Universal Router
+    , ('0x36ab1c395b3711d3d5ed2af8ac8371cc991aa06c', 'Flip') -- Flip
+    , ('0x7db11e30ae8ad7495668701c3f2c1b6d60587eda', 'Flip') -- Flip's LooksRare Checkout
+    , ('0xb123504fa220ba482768dd1e798594c1af88d7dc', 'Tiny Astro') -- Tiny Astro
   ) AS temp_table (contract_address, name)
