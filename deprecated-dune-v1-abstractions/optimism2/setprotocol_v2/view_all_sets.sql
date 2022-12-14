bc847c054ce0e9e6db2e9da2190a17cb242c9f42
Meghan Heintz <meghan@dune.com>
2022-12-14 11:00:49 -0500

    migrate v1 abstractions (#2253)

--- deprecated-dune-v1-abstractions/optimism2/setprotocol_v2/view_all_sets.sql
+++
@@ -1,9 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS setprotocol_v2
-;
-
-create or replace view setprotocol_v2.set_protocol_all_sets as
-select distinct "_setToken" as set_address
-    , "_name" as set_name
-    , "_symbol" as set_symbol
-from setprotocol_v2."SetTokenCreator_evt_SetTokenCreated"
-;
