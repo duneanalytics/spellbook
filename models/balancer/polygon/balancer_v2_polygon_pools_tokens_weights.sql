46e5e38d86b4a32b7741f6f76207037bd5586e4b
Jacek Trocinski <jacek@hedgehog.app>
2022-12-14 16:20:36 +0100

    Remove tick quote marks which are not needed for SparkSQL.

--- models/balancer/polygon/balancer_v2_polygon_pools_tokens_weights.sql
+++ models/balancer/polygon/balancer_v2_polygon_pools_tokens_weights.sql
@@ -17,7 +17,7 @@
 -- Balancer v2 Pools Tokens Weights
 --
 SELECT
-    registered.`poolId` AS pool_id,
+    registered.poolId AS pool_id,
     tokens.token_address,
     weights.normalized_weight / POWER(10, 18) AS normalized_weight
 FROM {{ source('balancer_v2_polygon', 'Vault_evt_PoolRegistered') }} registered
@@ -32,7 +32,7 @@ WHERE tokens.pos = weights.pos
 UNION
 
 SELECT
-    registered.`poolId` AS pool_id,
+    registered.poolId AS pool_id,
     tokens.token_address,
     weights.normalized_weight / POWER(10, 18) AS normalized_weight
 FROM {{ source('balancer_v2_polygon', 'Vault_evt_PoolRegistered') }} registered
@@ -47,7 +47,7 @@ WHERE tokens.pos = weights.pos
 UNION
 
 SELECT
-    registered.`poolId` AS pool_id,
+    registered.poolId AS pool_id,
     tokens.token_address,
     weights.normalized_weight / POWER(10, 18) AS normalized_weight
 FROM {{ source('balancer_v2_polygon', 'Vault_evt_PoolRegistered') }} registered
