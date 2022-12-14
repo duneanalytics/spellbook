d4426e3be64548ec6dcc644f8c02b3ca1b32a50f
Gent Rexha <gent@cow.fi>
2022-12-14 15:38:58 +0100

    Updated docs

--- models/labels/trader_frequencies/trader_frequencies.sql
+++ models/labels/trader_frequencies/trader_frequencies.sql
@@ -1,7 +1,7 @@
 {{
     config(
         alias='trader_frequencies',
-        post_hook='{{ expose_spells_hide_trino(\'["ethereum"]\', "sector", "tx_hash_labels", \'["gentrexha"]\') }}'
+        post_hook='{{ expose_spells(\'["ethereum"]\', "sector", "labels", \'["gentrexha"]\') }}'
     )
 }}
 
