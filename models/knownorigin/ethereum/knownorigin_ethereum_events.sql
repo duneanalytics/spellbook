d018645235c37e8c858aa8ad5e82b689f06a8694
cat <cat377550@gmail.com>
2022-12-15 15:28:20 -0500

    address comments

--- models/knownorigin/ethereum/knownorigin_ethereum_events.sql
+++ models/knownorigin/ethereum/knownorigin_ethereum_events.sql
@@ -26,7 +26,11 @@ with raw_ko_transactions as (
             , 'Buy' as trade_category
             , lower('0xc322cdd03f34b6d25633c2abbc8716a058c7fe9e') as project_contract_address
     from {{ source('knownorigin_ethereum','TokenMarketplaceV2_evt_TokenPurchased') }} a
-    inner join {{ source('erc721_ethereum','evt_transfer') }} b on a.evt_tx_hash = b.evt_tx_hash and a._tokenId = b.tokenId
+    inner join {{ source('erc721_ethereum','evt_transfer') }} b on a.evt_tx_hash = b.evt_tx_hash 
+                and a._tokenId = b.tokenId
+                {% if is_incremental() %}
+                and b.evt_block_time >= date_trunc("day", now() - interval '1 week')
+                {% endif %}
     {% if is_incremental() %}
     where a.evt_block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
@@ -46,7 +50,11 @@ with raw_ko_transactions as (
             , 'Offer Accepted' as trade_category
             , lower('0xc1697d340807324200e26e4617ce9c0070488e23') as project_contract_address
     from {{ source('knownorigin_ethereum','TokenMarketplace_evt_BidAccepted') }} a
-    inner join {{ source('erc721_ethereum','evt_transfer') }} b on a.evt_tx_hash = b.evt_tx_hash and a._tokenId = b.tokenId
+    inner join {{ source('erc721_ethereum','evt_transfer') }} b on a.evt_tx_hash = b.evt_tx_hash 
+                and a._tokenId = b.tokenId
+                {% if is_incremental() %}
+                and b.evt_block_time >= date_trunc("day", now() - interval '1 week')
+                {% endif %}
     {% if is_incremental() %}
     where a.evt_block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
@@ -66,7 +74,11 @@ with raw_ko_transactions as (
             , 'Buy' as trade_category
             , lower('0xfbeef911dc5821886e1dda71586d90ed28174b7d') as project_contract_address    
     from {{ source('knownorigin_ethereum','KnownOriginDigitalAssetV2_evt_Purchase') }} a
-    inner join {{ source('erc721_ethereum','evt_transfer') }} b on a.evt_tx_hash = b.evt_tx_hash and a._tokenId = b.tokenId
+    inner join {{ source('erc721_ethereum','evt_transfer') }} b on a.evt_tx_hash = b.evt_tx_hash 
+                and a._tokenId = b.tokenId
+                {% if is_incremental() %}
+                and b.evt_block_time >= date_trunc("day", now() - interval '1 week')
+                {% endif %}                
     {% if is_incremental() %}
     where a.evt_block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
@@ -76,20 +88,20 @@ with raw_ko_transactions as (
 select  'ethereum' as blockchain
         , 'known origin' as project
         , 'v1' as `version`
-        , cast(date_trunc('day', a.evt_block_time) AS date) as block_date
+        , TRY_CAST(date_trunc('day', a.evt_block_time) AS date) as block_date
         , a.evt_block_time as block_time
         , a.token_id
         , 'Known Origin' as collection
         , (raw_amount/1e18) * ep.price as amount_usd
         , 'erc721' as token_standard
         , 'Single Item Trade' as trade_type 
-        , 1 as number_of_items
+        , CAST(1 AS DECIMAL(38,0)) as number_of_items
         , trade_category 
         , seller 
         , buyer 
         , 'Trade' as evt_type
         , raw_amount/1e18 as amount_original
-        , raw_amount as amount_raw
+        , CAST(raw_amount AS DECIMAL(38,0)) as amount_raw
         , 'ETH' as currency_symbol
         , '0x0000000000000000000000000000000000000000' as currency_contract
         , contract_address as nft_contract_address
@@ -102,7 +114,7 @@ select  'ethereum' as blockchain
         , t.to as tx_to
         , round((2.5 * (raw_amount) / 100), 7) as platform_fee_amount_raw
         , round((2.5 * (raw_amount/1e18) / 100), 7) as platform_fee_amount
-        , round((2.5 * (raw_amount/1e18) * ep.price / 100), 7) as platform_fee_amount_usd
+        , round((2.5 * (raw_amount/1e18) * ep.price / 100), 7) as platform_fee_amount_usd        
         , cast('2.5' as double) as platform_fee_percentage
         , round((12.5 * (raw_amount) / 100), 7) as royalty_fee_amount_raw
         , round((12.5 * (raw_amount/1e18) / 100), 7) as royalty_fee_amount
@@ -113,6 +125,9 @@ select  'ethereum' as blockchain
         , 'knownorigin' || '-' || a.evt_tx_hash || '-' || a.token_id || '-' ||  seller || '-' || a.evt_block_number || '-' || a.evt_index as unique_trade_id
 from (select * from raw_ko_transactions where sale_type = 'Secondary') a
 inner join {{ source('ethereum','transactions') }} t on a.evt_tx_hash = t.hash
+        {% if is_incremental() %}
+        and t.block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
 left outer join
     (   select  minute
                 , price
