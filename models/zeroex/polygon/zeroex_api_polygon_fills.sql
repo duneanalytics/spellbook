fe63ba65595fbe4819085991255b2eb5d508ebcc
RantumBits <rantum@rantum.xyz>
2022-12-15 14:20:04 -0800

    reise fills sql to remove poly.transactions table

--- models/zeroex/polygon/zeroex_api_polygon_fills.sql
+++ models/zeroex/polygon/zeroex_api_polygon_fills.sql
@@ -20,7 +20,7 @@
 
 WITH zeroex_tx AS (
     SELECT tx_hash,
-           max(affiliate_address) as affiliate_address
+           max(affiliate_address) as affiliate_address, tr.to, tr.from
     FROM (
         SELECT tr.tx_hash,
                     '0x' || CASE
@@ -32,7 +32,7 @@ WITH zeroex_tx AS (
                                 THEN SUBSTRING(INPUT
                                         FROM (position('fbc019a7' IN INPUT) + 32)
                                         FOR 40)
-                            END AS affiliate_address, tr.to, tr.from
+                            END AS affiliate_address, 
         FROM {{ source('polygon', 'traces') }} tr
         WHERE tr.to IN (
                 -- exchange contract
