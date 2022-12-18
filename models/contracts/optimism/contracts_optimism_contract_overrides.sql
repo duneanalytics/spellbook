759900f0bb437c341b5ff769441dee1e3b5a6a30
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-12-18 16:03:32 -0500

    add attestation override

--- models/contracts/optimism/contracts_optimism_contract_overrides.sql
+++ models/contracts/optimism/contracts_optimism_contract_overrides.sql
@@ -18,4 +18,5 @@ from
     ('0xc30141B657f4216252dc59Af2e7CdB9D8792e1B0', 'Socket', 'Socket Registry')
     ,('0x81b30ff521D1fEB67EDE32db726D95714eb00637', 'Optimistic Explorer', 'OptimisticExplorerNFT')
     ,('0x998EF16Ea4111094EB5eE72fC2c6f4e6E8647666', 'Quix', 'Seaport')
+    ,('0xEE36eaaD94d1Cc1d0eccaDb55C38bFfB6Be06C77', 'AttestationStation','AttestationStation')
     ) as temp_table(contract_address, contract_project, contract_name)
