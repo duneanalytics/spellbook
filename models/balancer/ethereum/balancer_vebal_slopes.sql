ef2a90d2623fb294f40bdb6dbd4b8fbb78019210
Stefenon <victorstefenon0@gmail.com>
2022-12-19 18:40:24 +0100

    Add incremental logic to vebal_slopes spell

--- models/balancer/ethereum/balancer_vebal_slopes.sql
+++ models/balancer/ethereum/balancer_vebal_slopes.sql
@@ -2,6 +2,10 @@
     config(
         schema = 'balancer_ethereum',
         alias='vebal_slopes',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['block_time', 'wallet_address'],
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "project",
                                     "balancer",
@@ -15,12 +19,18 @@ WITH base_locks AS (
         FROM {{ source('balancer_ethereum', 'veBAL_call_create_lock') }} l
         JOIN {{ source('balancer_ethereum', 'veBAL_evt_Deposit') }} d
         ON d.evt_tx_hash = l.call_tx_hash
+        {% if is_incremental() %}
+        WHERE evt_block_time >= DATE_TRUNC('day', NOW() - interval '1 week')
+        {% endif %}
         
         UNION ALL
         
         SELECT provider, cast(null as numeric(38)) AS locked_at, locktime AS unlocked_at, ts AS updated_at
         FROM {{ source('balancer_ethereum', 'veBAL_evt_Deposit') }}
         WHERE value = 0
+        {% if is_incremental() %}
+        AND evt_block_time >= DATE_TRUNC('day', NOW() - interval '1 week')
+        {% endif %}
     ),
     
     decorated_locks AS (
@@ -46,6 +56,9 @@ WITH base_locks AS (
             evt_block_time AS block_time,
             SUM(value/1e18) AS delta_bpt
         FROM {{ source('balancer_ethereum', 'veBAL_evt_Deposit') }}
+        {% if is_incremental() %}
+        WHERE evt_block_time >= DATE_TRUNC('day', NOW() - interval '1 week')
+        {% endif %}
         GROUP BY 1, 2, 3
     ),
     
@@ -56,6 +69,9 @@ WITH base_locks AS (
             evt_block_time AS block_time,
             -SUM(value/1e18) AS delta_bpt
         FROM {{ source('balancer_ethereum', 'veBAL_evt_Withdraw') }}
+        {% if is_incremental() %}
+        WHERE evt_block_time >= DATE_TRUNC('day', NOW() - interval '1 week')
+        {% endif %}
         GROUP BY 1, 2, 3
     ),
     
