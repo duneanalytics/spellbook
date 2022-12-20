c5ca28fc7b3b872a4fd12b966e492dafc9daf8cb
Victor Stefenon <victorstefenon0@gmail.com>
2022-12-20 23:07:00 +0100

    Update models/balancer/ethereum/balancer_vebal_slopes.sql
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

--- models/balancer/ethereum/balancer_vebal_slopes.sql
+++ models/balancer/ethereum/balancer_vebal_slopes.sql
@@ -135,11 +135,15 @@ SELECT
     a.bpt_balance,
     a.unlocked_at,
     a.slope,
-    a.bias
+    a.bias,
+    date_trunc('day', a.block_time) as block_date
 FROM double_counting a
 INNER JOIN max_updated_at b
 ON a.block_number = b.block_number
 AND a.block_time = b.block_time
 AND a.wallet_address = b.wallet_address
 AND a.updated_at = b.updated_at
-ORDER BY 4, 1
+{% if is_incremental() %}
+WHERE a.block_time >= DATE_TRUNC('day', NOW() - interval '1 week')
+{% endif %}
+ORDER BY a.wallet_address, a.block_number
