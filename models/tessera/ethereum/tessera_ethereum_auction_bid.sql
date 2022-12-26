9e44f0e86146bba6429a6b28f308e481dcea8e81
adrianmarruedo <adrian.marruedo@gmail.com>
2022-12-26 23:56:41 +0100

    feat: initial tessera spellbook

---
+++ models/tessera/ethereum/tessera_ethereum_auction_bid.sql
@@ -0,0 +1,22 @@
+-- PROTOFORM DISTRIBUTION BID. for example LPDA
+{{ config (
+    alias = 'auction_bid',
+    post_hook = '{{ expose_spells(\'["ethereum"]\', "project", "tessera",\'["amadarrrr"]\') }}'
+) }}
+
+WITH lpda_bid AS (
+    SELECT 
+        _user AS user,
+        _vault AS vault_address,
+        _price/POWER(10,18) AS price,
+        _quantity AS amount,
+        _price/POWER(10,18)*_quantity AS volume,
+        evt_block_time AS timestamp,
+        evt_tx_hash AS tx_hash
+    FROM 
+        {{ source('tessera_ethereum','tessera_ethereum_LPDA_evt_BidEntered') }}
+)
+
+SELECT *
+FROM lpda_bid;
+-- union with future distribution modules
---
+++ models/tessera/ethereum/tessera_ethereum_distribution_started.sql
@@ -0,0 +1,21 @@
+{{ config (
+    alias = 'distribution_start',
+    post_hook = '{{ expose_spells(\'["ethereum"]\', "project", "tessera",\'["amadarrrr"]\') }}'
+) }}
+-- PROTOFORM DISTRIBUTION. for example LPDA
+WITH lpda_creations AS (
+    SELECT 
+        _id AS id,
+        _token AS token,
+        _vault AS vault,
+        'LPDA' AS type,
+        _lpdaInfo AS info,
+        evt_block_time AS timestamp,
+        evt_tx_hash AS tx_hash
+    FROM 
+        {{ source('tessera_ethereum','tessera_ethereum_LPDA_evt_CreatedLPDA') }}
+)
+
+SELECT *
+FROM lpda_creations;
+-- union with future distribution modules
