15d28889a34b4f8c51c3fdf4a95751df52841db2
adrianmarruedo <adrian.marruedo@gmail.com>
2022-12-27 16:09:03 +0100

    chore: test vault deploy spell

---
+++ tests/tessera/ethereum/tessera_ethereum_vault_deploy_test.sql
@@ -0,0 +1,29 @@
+WITH unit_test AS (
+    -- Here we compare test data to actual data, returning TRUE if it matches and FALSE if not; ROUND and LOWER ensure we don’t get false errors due to formatting differences.
+    SELECT
+        CASE
+            WHEN LOWER(test.owner) = LOWER(actual.owner) THEN TRUE
+            ELSE FALSE
+        END AS owner_test,
+        CASE
+            WHEN LOWER(test.origin) = LOWER(actual.origin) THEN TRUE
+            ELSE FALSE
+        END AS origin_test,
+        CASE
+            WHEN LOWER(test.vault) = LOWER(actual.vault) THEN TRUE
+            ELSE FALSE
+        END AS vault_test,
+    FROM
+        {{ ref('tessera_ethereum_vault_deploy') }} AS actual
+        INNER JOIN {{ ref('tessera_ethereum_vault_deploy_test_data') }} AS test
+        ON LOWER(actual.tx_hash) = LOWER(test.tx_hash)
+)
+
+SELECT
+    *
+FROM
+    unit_test
+WHERE
+    owner_test = FALSE
+    OR origin_test = FALSE
+    OR vault_test = FALSE
