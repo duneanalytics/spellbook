0b48502e1990a255a5448f0b97705163cc29e848
Meghan <msheintz@gmail.com>
2022-12-28 15:50:35 -0500

    remove selector

--- scripts/validate_source_freshness_checks.py
+++ scripts/validate_source_freshness_checks.py
@@ -33,23 +33,6 @@ class Sources:
         else:
             return
 
-    def generate_selector(self):
-        """
-        Generates a file in the scripts folder. Double check the contents and copy the models new selector
-        to the bottom of selectors.yml in the project root dir.
-        :return:
-        """
-        yaml = YAML()
-        self.selector = yaml.load(Path("selector_template.yml").read_text())
-        model_slug = self.model.split('.')[-1]
-        self.selector['selectors'][0]['name'] = f"{model_slug}_sources"
-        for source in list(set(self.source_node_dependencies)):
-            source_slug = f"{source.split('.')[-2]}.{source.split('.')[-1]}"
-            yaml.indent(mapping=2, sequence=4, offset=2)
-            self.selector['selectors'][0]['definition']['union'].append({'method': 'source', 'value': source_slug})
-        yaml.dump(self.selector, Path(f"{model_slug}_selector.yml"))
-
-
 def missing_freshness_checks(sources, manifest, check_type='warn'):
     sources = list(set(sources))
     source_defintions = {source: manifest['sources'][source] for source in sources}
@@ -69,7 +52,6 @@ with open('../target/manifest.json') as json_file:
     manifest = json.load(json_file)
 
 sources_cls = Sources(model=f'model.spellbook.{args.model}', manifest=manifest)
-sources_cls.generate_selector()
 sources = sources_cls.source_node_dependencies
 sources_missing_checks = missing_freshness_checks(sources, manifest)
 assert len(sources_missing_checks) == 0, MissingSourceFreshnessChecks(sources_missing_checks)
