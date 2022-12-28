7e66c03c74da437df53f9a1724b901b5cbaf77f3
theachenyj <50615263+theachenyj@users.noreply.github.com>
2022-12-29 00:40:10 +0800

    Add tofuNFT into nft.trades on V2 (#2080)
    * Add tofuNFT into nft.trades on V2
    
    * Remove typo lines
    
    * change test date range on tofu_bnb_assert_trades.sql
    
    * fix some bugs
    
    * reanable ci tests
    
    * reanable ci tests
    
    * small tweaks
    
    * add seed test
    
    * more tweaks
    
    * fix syntax
    
    * syntax
    
    * syntax
    
    * add bundle index to unique id
    
    * fix posexplode
    
    * fix posexplode
    
    * fix seed
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: 0xRob <0xRobin5@gmail.com>

--- models/nft/nft_mints.sql
+++ models/nft/nft_mints.sql
@@ -1,6 +1,6 @@
 {{ config(
         alias ='mints',
-        post_hook='{{ expose_spells(\'["ethereum","solana"]\',
+        post_hook='{{ expose_spells(\'["ethereum","solana","bnb"]\',
                     "sector",
                     "nft",
                     \'["soispoke","umer_h_adil","hildobby","0xRob"]\') }}')
