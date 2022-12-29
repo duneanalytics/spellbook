c238355f58eccd9566f671f0bc81f3a6d02a5357
Huang Geyang <Sukebeta@outlook.com>
2022-12-29 17:52:47 +0800

    Reformat aave_v3_tokens for compatibility

--- models/aave/aave_v3_tokens.sql
+++ models/aave/aave_v3_tokens.sql
@@ -1,5 +1,4 @@
 {{ config(
-        schema = 'aave_v3'
         , alias='tokens'
         , materialized = 'incremental'
         , file_format = 'delta'
@@ -20,43 +19,72 @@
     'avalanche_c'
 ] %}
 
+SELECT distinct a.blockchain
+              , a.atoken_address
+              , a.underlying_address
+              , a.atoken_decimals
+              , a.side
+              , a.arate_type
+              , a.atoken_symbol
+              , a.atoken_name
+              , et.decimals AS underlying_decimals
+              , et.symbol   AS underlying_symbol
 
+FROM (
+    {% for aave_v3_chain in aave_v3_decoded_chains %}
+        SELECT
+            '{{aave_v3_chain}}' AS blockchain
+            , contract_address AS atoken_address
+            , underlyingAsset AS underlying_address
+            , aTokenDecimals AS atoken_decimals
+            , 'Supply' AS side
+            , 'Variable' AS arate_type
+            , aTokenSymbol AS atoken_symbol
+            , aTokenName AS atoken_name
+        FROM {{source( 'aave_v3_' + aave_v3_chain, 'AToken_evt_Initialized' ) }}
+        {% if is_incremental() %}
+        WHERE evt_block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
 
-SELECT distinct 
-    a.blockchain
-        , a.atoken_address, a.underlying_address, a.atoken_decimals, a.side, a.arate_type, a.atoken_symbol, a.atoken_name
-        , et.decimals AS underlying_decimals, et.symbol AS underlying_symbol
-    
-    FROM (
-        {% for aave_v3_chain in aave_v3_decoded_chains %}
-        SELECT
-        '{{aave_v3_chain}}' AS blockchain, contract_address as atoken_address, `underlyingAsset` as underlying_address, `aTokenDecimals` as atoken_decimals, 'Supply' as side, 'Variable' as arate_type, aTokenSymbol as atoken_symbol, aTokenName as atoken_name
-            FROM {{source( 'aave_v3_' + aave_v3_chain, 'AToken_evt_Initialized' ) }}
-            {% if is_incremental() %}
-            WHERE evt_block_time >= date_trunc("day", now() - interval '1 week')
-            {% endif %}
         UNION ALL
+
         SELECT
-        '{{aave_v3_chain}}' AS blockchain, contract_address as atoken_address, `underlyingAsset` as underlying_address, `debtTokenDecimals` as atoken_decimals, 'Borrow' as side, 'Stable' as arate_type, debtTokenSymbol as atoken_symbol, debtTokenName as atoken_name
-            FROM {{source( 'aave_v3_' + aave_v3_chain, 'StableDebtToken_evt_Initialized' ) }}
-            WHERE debtTokenName LIKE '%Stable%'
-            {% if is_incremental() %}
-            AND evt_block_time >= date_trunc("day", now() - interval '1 week')
-            {% endif %}
+            '{{aave_v3_chain}}' AS blockchain
+            , contract_address AS atoken_address
+            , underlyingAsset AS underlying_address
+            , debtTokenDecimals AS atoken_decimals
+            , 'Borrow' AS side
+            , 'Stable' AS arate_type
+            , debtTokenSymbol AS atoken_symbol
+            , debtTokenName AS atoken_name
+        FROM {{source( 'aave_v3_' + aave_v3_chain, 'StableDebtToken_evt_Initialized' ) }}
+        WHERE debtTokenName LIKE '%Stable%'
+        {% if is_incremental() %}
+        AND evt_block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+
         UNION ALL
+
         SELECT
-        '{{aave_v3_chain}}' AS blockchain, contract_address as atoken_address, `underlyingAsset` as underlying_address, `debtTokenDecimals` as atoken_decimals, 'Borrow' as side, 'Variable' as arate_type, debtTokenSymbol as atoken_symbol, debtTokenName as atoken_name
-            FROM {{source( 'aave_v3_' + aave_v3_chain, 'VariableDebtToken_evt_Initialized' ) }}
-            WHERE debtTokenName LIKE '%Variable%'
-            {% if is_incremental() %}
-            AND evt_block_time >= date_trunc("day", now() - interval '1 week')
-            {% endif %}
+            '{{aave_v3_chain}}' AS blockchain
+            , contract_address AS atoken_address
+            , underlyingAsset AS underlying_address
+            , debtTokenDecimals AS atoken_decimals
+            , 'Borrow' AS side
+            , 'Variable' AS arate_type
+            , debtTokenSymbol AS atoken_symbol
+            , debtTokenName AS atoken_name
+        FROM {{source( 'aave_v3_' + aave_v3_chain, 'VariableDebtToken_evt_Initialized' ) }}
+        WHERE debtTokenName LIKE '%Variable%'
+        {% if is_incremental() %}
+        AND evt_block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+
         {% if not loop.last %}
         UNION ALL
         {% endif %}
-        {% endfor %}
-
-        ) a
-    LEFT JOIN {{ ref('tokens_erc20') }} et
-        ON a.underlying_address = et.contract_address
-        AND a.blockchain = et.blockchain
+    {% endfor %}
+    ) a
+LEFT JOIN {{ ref('tokens_erc20') }} et
+    ON a.underlying_address = et.contract_address
+    AND a.blockchain = et.blockchain
