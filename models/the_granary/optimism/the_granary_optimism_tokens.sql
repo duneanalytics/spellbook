c4c4742010f6798b4a08acc9bbbaca784984b9a8
Huang Geyang <Sukebeta@outlook.com>
2022-12-30 16:33:37 +0800

    Reformat granary_tokens and compatibility for Trino

--- models/the_granary/optimism/the_granary_optimism_tokens.sql
+++ models/the_granary/optimism/the_granary_optimism_tokens.sql
@@ -1,6 +1,5 @@
 {{ config(
-    schema = 'the_granary_optimism'
-    , alias='tokens'
+    alias='tokens'
     , materialized = 'incremental'
     , file_format = 'delta'
     , incremental_strategy = 'merge'
@@ -14,28 +13,49 @@
 }}
 
 
-    SELECT distinct 
-        a.blockchain
-            , a.atoken_address, a.underlying_address, a.atoken_decimals, a.side, a.arate_type, a.atoken_symbol, a.atoken_name
-            , et.decimals AS underlying_decimals, et.symbol AS underlying_symbol
-    
-    FROM (
-        SELECT
-        'optimism' AS blockchain, contract_address as atoken_address, `underlyingAsset` as underlying_address, `aTokenDecimals` as atoken_decimals, 'Supply' as side, 'Variable' as arate_type, aTokenSymbol as atoken_symbol, aTokenName as atoken_name
-            FROM {{source( 'the_granary_optimism', 'AToken_evt_Initialized' ) }}
+SELECT distinct a.blockchain
+              , a.atoken_address
+              , a.underlying_address
+              , a.atoken_decimals
+              , a.side
+              , a.arate_type
+              , a.atoken_symbol
+              , a.atoken_name
+              , et.decimals AS underlying_decimals
+              , et.symbol   AS underlying_symbol
+
+FROM (
+        SELECT 'optimism'        AS blockchain,
+               contract_address  AS atoken_address,
+               underlyingAsset   AS underlying_address,
+               aTokenDecimals    AS atoken_decimals,
+               'Supply'          AS side,
+               'Variable'        AS arate_type,
+               aTokenSymbol      AS atoken_symbol,
+               aTokenName        AS atoken_name
+        FROM {{source( 'the_granary_optimism', 'AToken_evt_Initialized' ) }}
             {% if is_incremental() %}
             WHERE evt_block_time >= date_trunc("day", now() - interval '1 week')
             {% endif %}
+
         UNION ALL
+
         SELECT
-        'optimism' AS blockchain, contract_address as atoken_address, `underlyingAsset` as underlying_address, `debtTokenDecimals` as atoken_decimals, 'Borrow' as side, 
-        CASE WHEN debtTokenName LIKE '%Stable%' THEN 'Stable' ELSE 'Variable' END as arate_type, debtTokenSymbol as atoken_symbol, debtTokenName as atoken_name
-            FROM {{source( 'the_granary_optimism', 'DebtToken_evt_Initialized' ) }}
+            'optimism'          AS blockchain,
+            contract_address    AS atoken_address,
+            underlyingAsset     AS underlying_address,
+            debtTokenDecimals   AS atoken_decimals,
+            'Borrow'            AS side,
+            CASE WHEN debtTokenName
+            LIKE '%Stable%' THEN 'Stable'
+            ELSE 'Variable' END AS arate_type,
+            debtTokenSymbol     AS atoken_symbol,
+            debtTokenName       AS atoken_name
+        FROM {{source( 'the_granary_optimism', 'DebtToken_evt_Initialized' ) }}
             {% if is_incremental() %}
             WHERE evt_block_time >= date_trunc("day", now() - interval '1 week')
             {% endif %}
-  
         ) a
-    LEFT JOIN {{ ref('tokens_erc20') }} et
-        ON a.underlying_address = et.contract_address
-        AND et.blockchain = 'optimism'
+LEFT JOIN {{ ref('tokens_erc20') }} et
+    ON a.underlying_address = et.contract_address
+    AND et.blockchain = 'optimism'
