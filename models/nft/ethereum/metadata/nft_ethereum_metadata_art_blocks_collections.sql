cd51cd67373c42c5c1eaa566b3f7a1a1be73c4cb
cat <cat377550@gmail.com>
2023-01-03 16:16:57 -0800

    add new metadata

--- models/nft/ethereum/metadata/nft_ethereum_metadata_art_blocks_collections.sql
+++ models/nft/ethereum/metadata/nft_ethereum_metadata_art_blocks_collections.sql
@@ -374,6 +374,7 @@
         , ('0x99a9b7c1116f9ceeb1652de04d5969cce509b069', 379, 1000000, 'ORI', 'James Merrill', 'Curated', 'N/A', '0x99a9b7c1116f9ceeb1652de04d5969cce509b069-379')
         , ('0x99a9b7c1116f9ceeb1652de04d5969cce509b069', 383, 1000000, 'Pre-Process', 'Casey REAS', 'Curated', 'N/A', '0x99a9b7c1116f9ceeb1652de04d5969cce509b069-383')
         , ('0x99a9b7c1116f9ceeb1652de04d5969cce509b069', 385, 1000000, 'Dipolar', 'Junia Farquhar', 'Curated', 'N/A', '0x99a9b7c1116f9ceeb1652de04d5969cce509b069-385')
+        , ('0x99a9b7c1116f9ceeb1652de04d5969cce509b069', 399, 1000000, 'The Field', 'Beervangeer', 'Curated', 'N/A', '0x99a9b7c1116f9ceeb1652de04d5969cce509b069-399')
 
         , ('0x99a9b7c1116f9ceeb1652de04d5969cce509b069', 375, 1000000, 'Wabi Sabi', 'Kazuhiro Tanimoto', 'Presents', 'N/A', '0x99a9b7c1116f9ceeb1652de04d5969cce509b069-375')
         , ('0x99a9b7c1116f9ceeb1652de04d5969cce509b069', 377, 1000000, 'Ingress', 'Paweł Dudko', 'Presents', 'N/A', '0x99a9b7c1116f9ceeb1652de04d5969cce509b069-377')
