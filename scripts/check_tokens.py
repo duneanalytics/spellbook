09dd2aa4b4347343cdf8f2c0fcaee498801254fc
0xRob <0xRobin5@gmail.com>
2023-01-05 14:05:21 +0100

    use batch api calls for price checker

--- scripts/check_tokens.py
+++ scripts/check_tokens.py
@@ -8,14 +8,15 @@ from token_checker import TokenChecker
 
 logging.basicConfig(stream=sys.stdout, level=logging.WARN)
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--file_name')
 args = parser.parse_args()
 
+
 def filter_non_row_lines(new_lines):
-    filtered_lines = [line for line in new_lines if bool(re.search(r'\((.*?,.*?)\)',line))]
+    filtered_lines = [line for line in new_lines if bool(re.search(r'\((.*?,.*?)\)', line))]
     return filtered_lines
+
 
 with open(f'{args.file_name}') as f:
     new_lines = f.read().strip().split('\n')
@@ -23,10 +24,10 @@ cleaned_lines = [new_line.lstrip('+').strip() for new_line in new_lines]
 filtered_lines = filter_non_row_lines(cleaned_lines)
 exceptions = 0
 
+checker = TokenChecker()
 for new_line in filtered_lines:
     try:
-        checker = TokenChecker(new_line=new_line)
-        checker.validate_token()
+        checker.validate_token(new_line)
         # Sleep to (hopefully) avoid limits
         time.sleep(2)
     except Exception as err:
@@ -38,7 +39,8 @@ for new_line in filtered_lines:
             else:
                 raise err
         else:
-            exceptions+=1
+            exceptions += 1
             logging.error(err)
 if exceptions > 0:
-    raise Exception(f"{exceptions} exception/s. Review logs for details. Some could be due simply to missing data from API.")
+    raise Exception(
+        f"{exceptions} exception/s. Review logs for details. Some could be due simply to missing data from API.")
