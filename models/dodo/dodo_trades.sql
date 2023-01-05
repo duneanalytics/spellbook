e05651014788dad65f8cd9cfc3fddfca63dcd016
owen05 <owen05@users.noreply.github.com>
2023-01-06 04:56:33 +0800

    Update dodo_ethereum_trades & add dodo_bnb_trades & split them to aggregator and pools (#2169)
    * update dodo_ethereum_trades && add dodo_bnb_trades
    
    * split dodo_trades to pools & aggregator on Ethereum and BNBChain
    
    * label different dodoV2 pools
    
    * fix schema & sql
    
    * add dodo_proxies on Ethereum
    
    * Modify dodo_bnb_trades to incremental view
    
    * Apply check_dex_seed and modify dodo_eth_trades to incremental view
    
    * Add DODO bnb trade seeds
    
    * Apply check_dex_seed to DODO bnb trades model
    
    * update dex_trade_seeds
    
    * fix
    
    * Add DODO bnb in project.yml
    
    * remove gitkeep in dodo
    
    * Update models/dodo/bnb/dodo_pools_bnb_trades.sql
    
    * Update check_dex_seed in dodo_ethereum_schema.yml
    
    * fix dodo_ethereum_schema
    
    * Remove schema in the config blocks
    
    * Add dodo_trades files by scoffie
    
    * add dppOracle & dppAdvanced to dodo_bnb
    
    * fix
    
    * update dbt_project on dodo
    
    * fix dodo_bnb_sources.yml
    
    * move cast to base model
    
    * update dex.trades seeds on DODO_BNBChain
    
    * final formatting & cleanup expose spell macro
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>

--- models/dodo/dodo_trades.sql
+++ models/dodo/dodo_trades.sql
@@ -3,7 +3,7 @@
         post_hook='{{ expose_spells(\'["ethereum","bnb"]\',
                                 "project",
                                 "dodo",
-                                \'["scoffie"]\') }}'
+                                \'["scoffie", "owen05"]\') }}'
         )
 }}
 
