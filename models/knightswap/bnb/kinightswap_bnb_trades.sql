872e4c41b996cd7f1e39862f244e13769743d99d
jeff-dude <jeff@dune.xyz>
2023-01-06 12:51:05 -0500

    formatting, move cast() to base model

--- models/knightswap/bnb/kinightswap_bnb_trades.sql
+++ models/knightswap/bnb/kinightswap_bnb_trades.sql
@@ -15,18 +15,18 @@
 {% set project_start_date = '2021-05-22' %}     -- select min(evt_block_time) from knightswap_bnb.KnightPair_evt_Swap
 
 WITH knightswap_dex AS (
-    SELECT  t.evt_block_time                                             AS block_time,
-            `to`                                                         AS taker,
-            sender                                                       AS maker,
-            CASE WHEN amount0Out = 0 THEN amount1Out ELSE amount0Out END AS token_bought_amount_raw,
-            CASE WHEN amount0In = 0 THEN amount1In ELSE amount0In END    AS token_sold_amount_raw,
-            cast(NULL as double)                                         AS amount_usd,
-            CASE WHEN amount0Out = 0 THEN token1 ELSE token0 END         AS token_bought_address,
-            CASE WHEN amount0In = 0 THEN token1 ELSE token0 END          AS token_sold_address,
-            t.contract_address                                           AS project_contract_address,
-            t.evt_tx_hash                                                AS tx_hash,
-            ''                                                           AS trace_address,
-            t.evt_index
+    SELECT  t.evt_block_time                                                    AS block_time,
+            t.to                                                                AS taker,
+            t.sender                                                            AS maker,
+            CASE WHEN t.amount0Out = 0 THEN t.amount1Out ELSE t.amount0Out END  AS token_bought_amount_raw,
+            CASE WHEN t.amount0In = 0 THEN t.amount1In ELSE t.amount0In END     AS token_sold_amount_raw,
+            cast(NULL as double)                                                AS amount_usd,
+            CASE WHEN t.amount0Out = 0 THEN p.token1 ELSE p.token0 END          AS token_bought_address,
+            CASE WHEN t.amount0In = 0 THEN p.token1 ELSE p.token0 END           AS token_sold_address,
+            t.contract_address                                                  AS project_contract_address,
+            t.evt_tx_hash                                                       AS tx_hash,
+            ''                                                                  AS trace_address,
+            t.evt_index                                                         AS evt_index
     FROM {{ source('knightswap_bnb', 'KnightPair_evt_Swap') }} t
     INNER JOIN {{ source('knightswap_bnb', 'KnightFactory_evt_PairCreated') }} p
         ON t.contract_address = p.pair
@@ -38,36 +38,37 @@ WITH knightswap_dex AS (
     {% endif %}
 )
 
-SELECT 'bnb'                                                           AS blockchain,
-       'knightswap'                                                        AS project,
-       '1'                                                             AS version,
-       try_cast(date_trunc('DAY', knightswap_dex.block_time) AS date)      AS block_date,
-       knightswap_dex.block_time,
-       erc20a.symbol                                                   AS token_bought_symbol,
-       erc20b.symbol                                                   AS token_sold_symbol,
-       CASE
-           WHEN lower(erc20a.symbol) > lower(erc20b.symbol) THEN concat(erc20b.symbol, '-', erc20a.symbol)
-           ELSE concat(erc20a.symbol, '-', erc20b.symbol)
-           END                                                         AS token_pair,
-       knightswap_dex.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
-       knightswap_dex.token_sold_amount_raw / power(10, erc20b.decimals)   AS token_sold_amount,
-       knightswap_dex.token_bought_amount_raw,
-       knightswap_dex.token_sold_amount_raw,
-       coalesce(
-               knightswap_dex.amount_usd
-           , (knightswap_dex.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
-           , (knightswap_dex.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
-           )                                                           AS amount_usd,
-       knightswap_dex.token_bought_address,
-       knightswap_dex.token_sold_address,
-       coalesce(knightswap_dex.taker, tx.from)                             AS taker,
-       knightswap_dex.maker,
-       knightswap_dex.project_contract_address,
-       knightswap_dex.tx_hash,
-       tx.from                                                         AS tx_from,
-       tx.to                                                           AS tx_to,
-       knightswap_dex.trace_address,
-       knightswap_dex.evt_index
+SELECT
+    'bnb' AS blockchain,
+    'knightswap' AS project,
+    '1' AS version,
+    try_cast(date_trunc('DAY', knightswap_dex.block_time) AS date) AS block_date,
+    knightswap_dex.block_time,
+    erc20a.symbol AS token_bought_symbol,
+    erc20b.symbol AS token_sold_symbol,
+    CASE
+        WHEN lower(erc20a.symbol) > lower(erc20b.symbol) THEN concat(erc20b.symbol, '-', erc20a.symbol)
+        ELSE concat(erc20a.symbol, '-', erc20b.symbol)
+        END AS token_pair,
+    knightswap_dex.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
+    knightswap_dex.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount,
+    CAST(knightswap_dex.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw,
+    CAST(knightswap_dex.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw,
+    coalesce(
+        knightswap_dex.amount_usd
+        , (knightswap_dex.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
+        , (knightswap_dex.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
+    ) AS amount_usd,
+    knightswap_dex.token_bought_address,
+    knightswap_dex.token_sold_address,
+    coalesce(knightswap_dex.taker, tx.from) AS taker,
+    knightswap_dex.maker,
+    knightswap_dex.project_contract_address,
+    knightswap_dex.tx_hash,
+    tx.from AS tx_from,
+    tx.to AS tx_to,
+    knightswap_dex.trace_address,
+    knightswap_dex.evt_index
 FROM knightswap_dex
 INNER JOIN {{ source('bnb', 'transactions') }} tx
     ON knightswap_dex.tx_hash = tx.hash
