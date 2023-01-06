4e0b389158dd9cfed58acd3455d5b17ce3499e81
jeff-dude <jeff@dune.xyz>
2023-01-06 16:12:50 -0500

    cleanup each blockchain model

--- models/wardenswap/arbitrum/wardenswap_arbitrum_trades.sql
+++ models/wardenswap/arbitrum/wardenswap_arbitrum_trades.sql
@@ -5,27 +5,27 @@
     ,file_format = 'delta'
     ,incremental_strategy = 'merge'
     ,unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address']
-    ,post_hook='{{ expose_spells(\'["bnb"]\',
+    ,post_hook='{{ expose_spells(\'["arbitrum"]\',
                                       "project",
                                       "wardenswap",
                                     \'["codingsh"]\') }}'
     )
 }}
 
-{% set project_start_date = '2022-04-22' %}     -- select min(evt_block_time) from wardenswap_arbitrum.WardenPair_evt_Swap
+{% set project_start_date = '2022-04-22' %}
 
-WITH biswap_dex AS (
-    SELECT  t.evt_block_time                                             AS block_time,
-            `to`                                                         AS taker,
-            sender                                                       AS maker,
-            CASE WHEN amount0Out = 0 THEN amount1Out ELSE amount0Out END AS token_bought_amount_raw,
-            CASE WHEN amount0In = 0 THEN amount1In ELSE amount0In END    AS token_sold_amount_raw,
-            cast(NULL as double)                                         AS amount_usd,
-            CASE WHEN amount0Out = 0 THEN token1 ELSE token0 END         AS token_bought_address,
-            CASE WHEN amount0In = 0 THEN token1 ELSE token0 END          AS token_sold_address,
-            t.contract_address                                           AS project_contract_address,
-            t.evt_tx_hash                                                AS tx_hash,
-            ''                                                           AS trace_address,
+WITH dex AS (
+    SELECT  t.evt_block_time AS block_time,
+            t.to AS taker,
+            t.sender AS maker,
+            CASE WHEN t.amount0Out = 0 THEN t.amount1Out ELSE t.amount0Out END AS token_bought_amount_raw,
+            CASE WHEN t.amount0In = 0 THEN t.amount1In ELSE t.amount0In END AS token_sold_amount_raw,
+            cast(NULL as double) AS amount_usd,
+            CASE WHEN t.amount0Out = 0 THEN p.token1 ELSE p.token0 END AS token_bought_address,
+            CASE WHEN t.amount0In = 0 THEN p.token1 ELSE p.token0 END AS token_sold_address,
+            t.contract_address AS project_contract_address,
+            t.evt_tx_hash AS tx_hash,
+            '' AS trace_address,
             t.evt_index
     FROM {{ source('wardenswap_arbitrum', 'WardenPair_evt_Swap') }} t
     INNER JOIN {{ source('wardenswap_arbitrum', 'WardenFactory_evt_PairCreated') }} p
@@ -38,39 +38,40 @@ WITH biswap_dex AS (
     {% endif %}
 )
 
-SELECT 'arbitrum'                                                           AS blockchain,
-       'wardenswap'                                                        AS project,
-       '1'                                                             AS version,
-       try_cast(date_trunc('DAY', biswap_dex.block_time) AS date)      AS block_date,
-       biswap_dex.block_time,
-       erc20a.symbol                                                   AS token_bought_symbol,
-       erc20b.symbol                                                   AS token_sold_symbol,
-       CASE
-           WHEN lower(erc20a.symbol) > lower(erc20b.symbol) THEN concat(erc20b.symbol, '-', erc20a.symbol)
-           ELSE concat(erc20a.symbol, '-', erc20b.symbol)
-           END                                                         AS token_pair,
-       biswap_dex.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
-       biswap_dex.token_sold_amount_raw / power(10, erc20b.decimals)   AS token_sold_amount,
-       biswap_dex.token_bought_amount_raw,
-       biswap_dex.token_sold_amount_raw,
-       coalesce(
-               biswap_dex.amount_usd
-           , (biswap_dex.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
-           , (biswap_dex.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
-           )                                                           AS amount_usd,
-       biswap_dex.token_bought_address,
-       biswap_dex.token_sold_address,
-       coalesce(biswap_dex.taker, tx.from)                             AS taker,
-       biswap_dex.maker,
-       biswap_dex.project_contract_address,
-       biswap_dex.tx_hash,
-       tx.from                                                         AS tx_from,
-       tx.to                                                           AS tx_to,
-       biswap_dex.trace_address,
-       biswap_dex.evt_index
-FROM biswap_dex
+SELECT 
+    'arbitrum' AS blockchain,
+    'wardenswap' AS project,
+    '1' AS version,
+    try_cast(date_trunc('DAY', dex.block_time) AS date) AS block_date,
+    dex.block_time,
+    erc20a.symbol AS token_bought_symbol,
+    erc20b.symbol AS token_sold_symbol,
+    CASE
+        WHEN lower(erc20a.symbol) > lower(erc20b.symbol) THEN concat(erc20b.symbol, '-', erc20a.symbol)
+        ELSE concat(erc20a.symbol, '-', erc20b.symbol)
+        END AS token_pair,
+    dex.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
+    dex.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount,
+    CAST(dex.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw,
+    CAST(dex.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw,
+    coalesce(
+        dex.amount_usd
+        , (dex.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
+        , (dex.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
+        ) AS amount_usd,
+    dex.token_bought_address,
+    dex.token_sold_address,
+    coalesce(dex.taker, tx.from) AS taker,
+    dex.maker,
+    dex.project_contract_address,
+    dex.tx_hash,
+    tx.from AS tx_from,
+    tx.to AS tx_to,
+    dex.trace_address,
+    dex.evt_index
+FROM dex
 INNER JOIN {{ source('arbitrum', 'transactions') }} tx
-    ON biswap_dex.tx_hash = tx.hash
+    ON dex.tx_hash = tx.hash
     {% if is_incremental() %}
     AND tx.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
@@ -78,14 +79,14 @@ INNER JOIN {{ source('arbitrum', 'transactions') }} tx
     AND tx.block_time >= '{{project_start_date}}'
     {% endif %}
 LEFT JOIN {{ ref('tokens_erc20') }} erc20a
-    ON erc20a.contract_address = biswap_dex.token_bought_address
+    ON erc20a.contract_address = dex.token_bought_address
     AND erc20a.blockchain = 'arbitrum'
 LEFT JOIN {{ ref('tokens_erc20') }} erc20b
-    ON erc20b.contract_address = biswap_dex.token_sold_address
+    ON erc20b.contract_address = dex.token_sold_address
     AND erc20b.blockchain = 'arbitrum'
 LEFT JOIN {{ source('prices', 'usd') }} p_bought
-    ON p_bought.minute = date_trunc('minute', biswap_dex.block_time)
-    AND p_bought.contract_address = biswap_dex.token_bought_address
+    ON p_bought.minute = date_trunc('minute', dex.block_time)
+    AND p_bought.contract_address = dex.token_bought_address
     AND p_bought.blockchain = 'arbitrum'
     {% if is_incremental() %}
     AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
@@ -94,8 +95,8 @@ LEFT JOIN {{ source('prices', 'usd') }} p_bought
     AND p_bought.minute >= '{{project_start_date}}'
     {% endif %}
 LEFT JOIN {{ source('prices', 'usd') }} p_sold
-    ON p_sold.minute = date_trunc('minute', biswap_dex.block_time)
-    AND p_sold.contract_address = biswap_dex.token_sold_address
+    ON p_sold.minute = date_trunc('minute', dex.block_time)
+    AND p_sold.contract_address = dex.token_sold_address
     AND p_sold.blockchain = 'arbitrum'
     {% if is_incremental() %}
     AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
