6512dd10f05e2d39c80460ff69ff88bf900392ff
jeff-dude <jeff@dune.xyz>
2023-01-11 13:06:54 -0500

    begin work to standarize sector-wide spell

--- models/dex/aggregator/dex_aggregator_trades.sql
+++ models/dex/aggregator/dex_aggregator_trades.sql
@@ -6,36 +6,42 @@
                                 \'["bh2smith"]\') }}'
         )
 }}
+
+{% set dex_aggregator_models = [
+ ref('cow_protocol_trades')
+] %}
+
+
 SELECT *
-FROM
-    (
-        SELECT
-            blockchain,
-            project,
-            version,
-            block_date,
-            block_time,
-            token_bought_symbol,
-            token_sold_symbol,
-            token_pair,
-            token_bought_amount,
-            token_sold_amount,
-            token_bought_amount_raw,
-            token_sold_amount_raw,
-            amount_usd,
-            token_bought_address,
-            token_sold_address,
-            taker,
-            maker,
-            project_contract_address,
-            tx_hash,
-            tx_from,
-            tx_to,
-            trace_address,
-            evt_index
-        FROM {{ ref('cow_protocol_trades') }}
-        /*
-        UNION ALL
-        <add future protocols here>
-        */
-    )
+FROM (
+    {% for model in dex_aggregator_models %}
+    SELECT
+          blockchain
+         , project
+         , version
+         , block_date
+         , block_time
+         , token_bought_symbol
+         , token_sold_symbol
+         , token_pair
+         , token_bought_amount
+         , token_sold_amount
+         , token_bought_amount_raw
+         , token_sold_amount_raw
+         , amount_usd
+         , token_bought_address
+         , token_sold_address
+         , taker
+         , maker
+         , project_contract_address
+         , tx_hash
+         , tx_from
+         , tx_to
+         , trace_address
+         , evt_index
+    FROM {{ model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
