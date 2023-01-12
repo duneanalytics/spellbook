c2632215e3459eee9355bc78cc012252f991cf66
Stefenon <victorstefenon0@gmail.com>
2023-01-12 23:09:31 +0100

    Create first version of balancer liquidity spell

---
+++ models/balancer/ethereum/balancer_v2_ethereum_liquidity.sql
@@ -0,0 +1,238 @@
+{{
+    config(
+        schema='balancer_v2_ethereum',
+        alias='liquidity',
+        partition_by = ['block_date'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['day', 'pool_id', 'token_address'],
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                    "project",
+                                    "balancer_v2",
+                                    \'["stefenon"]\') }}'
+    )
+}}
+
+WITH prices AS (
+        SELECT
+            date_trunc('day', minute) AS day,
+            contract_address AS token,
+            AVG(price) AS price
+        FROM
+            {{ sources('prices', 'usd') }}
+        WHERE
+            minute >= DATE_TRUNC('day', NOW() - interval '1 week')
+            AND blockchain = "ethereum"
+        GROUP BY
+            1, 2
+    ),
+    
+ dex_prices_1 AS (
+        SELECT
+            date_trunc('day', HOUR) AS DAY,
+            contract_address AS token,
+            percentile(median_price, 0.5) AS price,
+            sum(sample_size) AS sample_size
+        FROM {{ ref('dex_prices') }}
+        GROUP BY 1, 2
+        HAVING sum(sample_size) > 3
+    ),
+    
+    dex_prices AS (
+        SELECT
+            *,
+            LEAD(DAY, 1, NOW()) OVER (
+                PARTITION BY token
+                ORDER BY
+                    DAY
+            ) AS day_of_next_change
+        FROM
+            dex_prices_1
+    ),
+
+    swaps_changes AS (
+        SELECT
+            day,
+            pool_id,
+            token,
+            SUM(COALESCE(delta, 0)) AS delta
+        FROM
+            (
+                SELECT
+                    date_trunc('day', evt_block_time) AS day,
+                    poolId AS pool_id,
+                    tokenIn AS token,
+                    amountIn AS delta
+                FROM
+                    {{ source('balancer_v2_ethereum', 'Vault_evt_Swap') }}
+                UNION
+                ALL
+                SELECT
+                    date_trunc('day', evt_block_time) AS day,
+                    poolId AS pool_id,
+                    tokenOut AS token,
+                    -amountOut AS delta
+                FROM
+                    {{ source('balancer_v2_ethereum', 'Vault_evt_Swap') }}
+            ) swaps
+        GROUP BY 1, 2, 3
+    ),
+    
+zipped_balance_changes AS (
+        SELECT
+            date_trunc('day', evt_block_time) AS day,
+            poolId AS pool_id,
+            explode(arrays_zip(tokens, deltas, protocolFeeAmounts)) AS zipped
+        FROM {{ source('balancer_v2_ethereum', 'Vault_evt_PoolBalanceChanged') }}
+    ),
+    
+    balances_changes AS (
+        SELECT
+            day,
+            pool_id,
+            zipped.tokens AS token,
+            zipped.deltas - zipped.protocolFeeAmounts AS delta
+        FROM zipped_balance_changes
+        ORDER BY 1, 2, 3
+    ),
+    
+    managed_changes AS (
+        SELECT
+            date_trunc('day', evt_block_time) AS day,
+            poolId AS pool_id,
+            token,
+            cashDelta + managedDelta AS delta
+        FROM {{ source('balancer_v2_ethereum', 'Vault_evt_PoolBalanceManaged') }}
+    ),
+    
+    daily_delta_balance AS (
+        SELECT
+            day,
+            pool_id,
+            token,
+            SUM(COALESCE(amount, 0)) AS amount
+        FROM
+            (
+                SELECT
+                    day,
+                    pool_id,
+                    token,
+                    SUM(COALESCE(delta, 0)) AS amount
+                FROM
+                    balances_changes
+                GROUP BY 1, 2, 3
+                UNION ALL
+                SELECT
+                    day,
+                    pool_id,
+                    token,
+                    delta AS amount
+                FROM
+                    swaps_changes
+                UNION ALL
+                SELECT
+                    day,
+                    pool_id,
+                    token,
+                    delta AS amount
+                FROM
+                    managed_changes
+            ) balance
+        GROUP BY 1, 2, 3
+    ),
+    
+    cumulative_balance AS (
+        SELECT
+            DAY,
+            pool_id,
+            token,
+            LEAD(DAY, 1, NOW()) OVER (PARTITION BY token, pool_id ORDER BY DAY) AS day_of_next_change,
+            SUM(amount) OVER (PARTITION BY pool_id, token ORDER BY DAY ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_amount
+        FROM daily_delta_balance
+    ),
+    
+    calendar AS (
+        SELECT explode(sequence(to_date('2021-04-21'), CURRENT_DATE, interval 1 day)) AS day
+    ),
+    
+   cumulative_usd_balance AS (
+        SELECT
+            c.day,
+            b.pool_id,
+            b.token,
+            cumulative_amount / POWER(10, t.decimals) * COALESCE(p1.price, p2.price, 0) AS amount_usd
+        FROM calendar c
+        LEFT JOIN cumulative_balance b ON b.day <= c.day
+        AND c.day < b.day_of_next_change
+        LEFT JOIN tokens.erc20 t ON t.contract_address = b.token
+        LEFT JOIN prices p1 ON p1.day = b.day
+        AND p1.token = b.token
+        LEFT JOIN dex_prices p2 ON p2.day <= c.day
+        AND c.day < p2.day_of_next_change
+        AND p2.token = b.token
+        WHERE b.token != SUBSTRING(b.pool_id, 0, 42)
+    ),
+    
+zipped_pools_tokens_weights AS (
+        SELECT
+            c.poolId AS pool_id,
+            explode(arrays_zip(cc.tokens, cc.weights)) AS zipped
+        FROM {{ source('balancer_v2_ethereum', 'Vault_evt_PoolRegistered') }} c
+        INNER JOIN {{ source('balancer_v2_ethereum', 'WeightedPoolFactory_call_create') }}  cc
+        ON cc.output_0 = SUBSTRING(c.poolId, 0, 42)
+        
+        UNION ALL
+        
+        SELECT
+            c.poolId AS pool_id,
+            explode(arrays_zip(cc.tokens, cc.weights)) AS zipped
+        FROM {{ source('balancer_v2_ethereum', 'Vault_evt_PoolRegistered') }} c
+        INNER JOIN {{ source('balancer_v2_ethereum', 'WeightedPool2TokensFactory_call_create') }}  cc
+        ON cc.output_0 = SUBSTRING(c.poolId, 0, 42)
+
+        UNION ALL
+        
+        SELECT
+            c.poolId AS pool_id,
+            explode(arrays_zip(cc.tokens, cc.weights)) AS zipped
+        FROM {{ source('balancer_v2_ethereum', 'Vault_evt_PoolRegistered') }} c
+        INNER JOIN {{ source('balancer_v2_ethereum', 'WeightedPoolV2Factory_call_create') }}  cc
+        ON cc.output_0 = SUBSTRING(c.poolId, 0, 42)
+    ),
+    
+    pools_tokens_weights AS (
+        SELECT
+            pool_id,
+            zipped.tokens AS token_address,
+            zipped.weights / 1e18 AS normalized_weight
+        FROM zipped_pools_tokens_weights
+    ),
+    
+    pool_liquidity_estimates AS (
+        SELECT
+            b.day,
+            b.pool_id,
+            SUM(b.amount_usd) / COALESCE(SUM(w.normalized_weight), 1) AS liquidity
+        FROM cumulative_usd_balance b
+        LEFT JOIN {{ ref('balancer_v2_ethereum', 'pools_tokens_weights') }} w ON b.pool_id = w.pool_id
+        AND b.token = w.token_address
+        GROUP BY 1, 2
+    )
+    
+SELECT
+    b.day,
+    b.pool_id,
+    --pool_symbol,
+    token AS token_address,
+    symbol AS token_symbol,
+    coalesce(amount_usd, liquidity * normalized_weight) AS usd_amount
+FROM pool_liquidity_estimates b
+LEFT JOIN cumulative_usd_balance c ON c.day = b.day
+AND c.pool_id = b.pool_id
+LEFT JOIN {{ ref('balancer_v2_ethereum', 'pools_tokens_weights') }} w ON b.pool_id = w.pool_id
+AND w.token_address = c.token
+LEFT JOIN tokens.erc20 t ON t.contract_address = c.token
+ORDER BY 1, 2, 3
+--LEFT JOIN pool_labels p ON p.pool_id = SUBSTRING(b.pool_id, 0, 42) :: bytea
+    
