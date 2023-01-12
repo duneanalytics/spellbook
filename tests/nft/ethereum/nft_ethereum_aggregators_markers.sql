4c547c1fd955a4270352c9431477fd1987bc83aa
0xRob <0xRobin5@gmail.com>
2023-01-12 12:36:46 +0100

    fix test revert fix

--- tests/nft/ethereum/nft_ethereum_aggregators_markers.sql
+++ tests/nft/ethereum/nft_ethereum_aggregators_markers.sql
@@ -3,7 +3,7 @@ WITH unit_test as (
     from {{ ref('nft_ethereum_aggregators_markers') }} a
     inner join {{ ref('nft_ethereum_aggregators_markers') }} b
     on a.router_website != b.router_website
-    and a.hash_marker LIKE '%'|| b.hash_marker and
+    and a.hash_marker LIKE '%'|| b.hash_marker
 )
 
 select * from unit_test
