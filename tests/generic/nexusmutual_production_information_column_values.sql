433e435f8d80e909543b1209b100e2a281714b15
guyhow <guyhowlett@gmail.com>
2023-01-15 14:05:06 +0000

    added static view of product information for nexus mutual
    Data correlates contract address for product with product name, product type and syndicate.
    
    Seed file and unit test included.

---
+++ tests/generic/nexusmutual_production_information_column_values.sql
@@ -0,0 +1,24 @@
+{% test nexusmutual_product_information_column_values(model, column_name, nexusmutual_ethereum_product_information_seed) %}
+
+    with unit_test as
+    (
+        select
+            case
+                when m.{{ column_name }} = seed.{{ column_name }}
+                    then True
+                else False
+            end as generic_column_test
+        from {{ model }} m
+        join {{ nexusmutual_ethereum_production_information_seed }} seed
+            on m.contract_address = seed.contract_address
+            and m.product_name = seed.product_name
+            and m.product_type = seed.product_type
+            and m.date_added = seed.date_added
+            and m.syndicate = seed.syndicate
+    )
+
+    select *
+    from unit_test
+    where generic_column_test = False
+
+{% endtest %} 
