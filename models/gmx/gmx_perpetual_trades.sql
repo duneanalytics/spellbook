25bb5a65e7248988461f2bdb8f11df803095bc24
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-01-17 22:40:49 +0100

    tiny update

--- models/gmx/gmx_perpetual_trades.sql
+++ models/gmx/gmx_perpetual_trades.sql
@@ -7,14 +7,14 @@
         )
 }}
 
-{% set gmx_models = [
+{% set gmx_perp_models = [
 'gmx_avalanche_c_perpetual_trades'
 ,'gmx_arbitrum_perpetual_trades'
 ] %}
 
 SELECT *
 FROM (
-    {% for perpetual_model in gmx_models %}
+    {% for perpetual_model in gmx_perp_models %}
     SELECT
 		blockchain
 		,block_date
