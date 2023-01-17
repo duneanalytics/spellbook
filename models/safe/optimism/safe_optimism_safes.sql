b9539fe7f537c94e6823f2893322f1ff305fce12
Frank Maseo <leodubois@live.fr>
2023-01-17 22:32:19 +0100

    updated safe_optimism_safes.sql

--- models/safe/optimism/safe_optimism_safes.sql
+++ models/safe/optimism/safe_optimism_safes.sql
@@ -20,7 +20,7 @@ select
     '1.3.0' as creation_version, 
     try_cast(date_trunc('day', evt_block_time) as date) as block_date,
     evt_block_time as creation_time,
-    evt_tx_hash as tx_hash,
+    evt_tx_hash as tx_hash
 from {{ source('gnosis_safe_optimism', 'GnosisSafeL2_v1_3_0_evt_SafeSetup') }}
 {% if not is_incremental() %}
 where evt_block_time > '2021-11-17' -- for initial query optimisation 
