ea6dac20fe87893aa1adec64a4659b0d9c6a60ec
bizzy <horlasehinde@gmail.com>
2023-01-18 21:53:03 +0000

    add redeem to compound supply

--- models/compound/ethereum/compound_v2_ethereum_supply.sql
+++ models/compound/ethereum/compound_v2_ethereum_supply.sql
@@ -13,6 +13,7 @@ with mints as (
         '2' as version,
         'deposit' as transaction_type,
         asset_symbol as symbol,
+        asset_address as token_address,
         minter as depositor,
         cast(null as varchar(5)) as withdrawn_to,
         cast(null as varchar(5)) as liquidator,
@@ -33,7 +34,36 @@ with mints as (
         on p.minute = date_trunc('minute', evt_mint.evt_block_time)
         and p.contract_address = ctokens.asset_address
         and p.blockchain = 'ethereum'
+),
+redeems as (
+    select
+        '2' as version,
+        'withdraw' as transaction_type,
+        asset_symbol as symbol,
+        asset_address as token_address,
+        redeemer as depositor,
+        redeemer as withdrawn_to,
+        cast(null as varchar(5)) as liquidator,
+        -cast(redeemAmount as decimal(38, 0)) / decimals_mantissa as amount,
+        -cast(redeemAmount as decimal(38, 0)) / decimals_mantissa * price as usd_amount,
+        evt_tx_hash,
+        evt_index,
+        evt_block_time,
+        evt_block_number
+    from (
+        select * from {{ source('compound_v2_ethereum', 'cErc20_evt_Redeem') }}
+        union all
+        select * from {{ source('compound_v2_ethereum', 'cEther_evt_Redeem') }}
+    ) evt_mint
+    left join {{ ref('compound_v2_ethereum_ctokens') }} ctokens
+        on evt_mint.contract_address = ctokens.ctoken_address
+    left join {{ source('prices', 'usd') }} p
+        on p.minute = date_trunc('minute', evt_mint.evt_block_time)
+        and p.contract_address = ctokens.asset_address
+        and p.blockchain = 'ethereum'
 )
 
 
 select * from mints
+union all
+select * from redeems
