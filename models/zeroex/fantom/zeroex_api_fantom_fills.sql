63ecb52198d53bf64d128e148511457add7423a5
jeff-dude <jeff@dune.xyz>
2023-01-20 11:06:06 -0500

    fix spell naming standards

--- models/zeroex/fantom/zeroex_api_fantom_fills.sql
+++ models/zeroex/fantom/zeroex_api_fantom_fills.sql
@@ -1,5 +1,5 @@
 {{  config(
-        alias='fantom_fills',
+        alias='fills',
         materialized='incremental',
         partition_by = ['block_date'],
         unique_key = ['block_date', 'tx_hash', 'evt_index'],
