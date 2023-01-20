0c40366caa6f452eda689db1af8fc8b4dc8a4498
soispoke <66172107+soispoke@users.noreply.github.com>
2023-01-20 14:03:20 +0100

    Update addresses_ethereum_bridges.sql

--- models/addresses/ethereum/addresses_ethereum_bridges.sql
+++ models/addresses/ethereum/addresses_ethereum_bridges.sql
@@ -127,4 +127,5 @@ FROM (VALUES ("0x8ed95d1746bf1e4dab58d8ed4724f1ef95b20db0", "0x", "Erc20 Bridge 
   ,("0x76943c0d61395d8f2edf9060e1533529cae05de6", "Optimism", "Optimism: Lido Bridge")
   ,("0x324c7ec7fb2bc61646ac2f22f6d06ab29b6c87a3", "Optimism", "Optimism: Teleportr v1")
   ,("0x52ec2f3d7c5977a8e558c8d9c6000b615098e8fc", "Optimism", "Optimism: Teleportr v2")
+  ,("0x0ac2d6f5f5afc669d3ca38f830dad2b4f238ad3f", "Polygon", "ERC20 Bridge")
   ) AS x (address, bridge_name, description)
