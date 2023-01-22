162b37634963a7744206bf5ad6150e42eb79fbcc
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-01-21 20:11:13 -0500

    col names

--- models/transfers/optimism/transfers_optimism_tokens.sql
+++ models/transfers/optimism/transfers_optimism_tokens.sql
@@ -71,7 +71,7 @@ FROM (
         r.evt_block_time AS tx_block_time,
         r.evt_block_number AS tx_block_number,
         r.evt_tx_hash AS tx_hash,
-        
+
         r.evt_index,
         NULL AS trace_address,
 
@@ -131,9 +131,9 @@ FROM (
         token_id,
         transfer_type,
         amount as value,
-        r.evt_block_time AS tx_block_time,
-        r.evt_block_number AS tx_block_number,
-        r.evt_tx_hash AS tx_hash,
+        r.block_time AS tx_block_time,
+        r.block_number AS tx_block_number,
+        r.tx_hash AS tx_hash,
 
         r.evt_index,
         NULL AS trace_address,
