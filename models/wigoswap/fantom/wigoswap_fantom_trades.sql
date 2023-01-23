78501a7928c5a6cf93a679140954a8df6584b7d5
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-01-23 20:28:43 +0100

    fix incremental sql

--- models/wigoswap/fantom/wigoswap_fantom_trades.sql
+++ models/wigoswap/fantom/wigoswap_fantom_trades.sql
@@ -33,10 +33,11 @@ with dexs as (
         {{ source('wigoswap_fantom', 'Pair_evt_Swap') }} t
         inner join {{ source('wigoswap_fantom', 'Factory_evt_PairCreated') }} f 
             on f.pair = t.contract_address
+    {% if not is_incremental() %}
+    WHERE t.evt_block_time >= '{{project_start_date}}'
+    {% endif %}
     {% if is_incremental() %}
     WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
-    {% else %}
-    WHERE t.evt_block_time >= '{{ project_start_date }}'
     {% endif %}
 )
 select
