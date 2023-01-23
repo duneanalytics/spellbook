e0096a388466e6f0a15a96f6fdda7d36befac406
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-01-23 15:30:56 -0500

    add rh

--- models/contracts/optimism/contracts_optimism_contract_creator_address_list.sql
+++ models/contracts/optimism/contracts_optimism_contract_creator_address_list.sql
@@ -398,6 +398,7 @@ FROM (
       ,('0x41BA3387E1a5a592E27B9EE33935957CE5F872C1', 'Avault')
       ,('0x86b86c24c7b1f404ada8b4000acbe04158e096bd', 'Defiedge')
       ,('0xf708929CCfDed4142420592caAef42bD41C8a367', 'Champion Finance')
+      ,('0x221a8Da83f675f1Fd97105a5B2B3bb65916a5101', 'Rabbithole')
   ) as temp_table (creator_address, contract_project)
 
 ) f
