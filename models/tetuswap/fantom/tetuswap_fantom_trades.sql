0b928415d85b0c9fb34a720c0b6d87181af23453
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-01-23 20:29:44 +0100

    fix incremental sql

--- models/tetuswap/fantom/tetuswap_fantom_trades.sql
+++ models/tetuswap/fantom/tetuswap_fantom_trades.sql
@@ -33,10 +33,11 @@ with dexs as (
         {{ source('tetu_io_fantom', 'Pair_evt_Swap') }} t
         inner join {{ source('tetu_io_fantom', 'TetuSwapFactory_evt_PairCreated') }} f 
             on f.pair = t.contract_address
+    {% if not is_incremental() %}
+    WHERE t.evt_block_time >= '{{project_start_date}}'
+    {% endif %}
     {% if is_incremental() %}
     WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
-    {% else %}
-    WHERE t.evt_block_time >= '{{ project_start_date }}'
     {% endif %}
 )
 select
