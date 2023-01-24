547c7b5d2717f76dac4705070864fd0d3a30befa
bizzy <horlasehinde@gmail.com>
2023-01-24 00:42:07 +0000

    add liquidifty bnb trades

---
+++ models/liquidifty/bnb/liquidifty_bnb_trades.sql
@@ -0,0 +1,212 @@
+{{ config(
+    schema = 'liquidifty_bnb',
+    alias = 'trades',
+    post_hook = '{{ expose_spells(\'["bnb"]\',
+                                    "project",
+                                    "liquidifty",
+                                    \'["bizzyvinci"]\') }}'
+)}}
+
+{% set wbnb_address = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c' %}
+
+with v1 as (
+    select
+        'v1' as version,
+        evt_block_time as block_time,
+        token as token_id,
+        'erc721' as token_standard,
+        'Single Item Trade' as trade_type,
+        amount as number_of_items,
+        'Buy' as trade_category,
+        owner as seller,
+        buyer,
+        price as amount_raw,
+        '{{ wbnb_address }}' as currency_contract,
+        collection as nft_contract_address,
+        contract_address  as project_contract_address,
+        evt_tx_hash as tx_hash,
+        evt_block_number as block_number,
+        concat(cast(evt_block_number as varchar), '-', cast(evt_index as varchar)) as unique_trade_id,
+        'native' as currency_token_standard,
+        '1' as orderType
+    from {{ source('liquidifty_bnb', 'MarketplaceV1_evt_Buy') }}
+),
+v2 as (
+    select
+        'v2' as version,
+        evt_block_time as block_time,
+        token as token_id,
+        'erc721' as token_standard,
+        'Single Item Trade' as trade_type,
+        amount as number_of_items,
+        'Buy' as trade_category,
+        owner as seller,
+        buyer,
+        price as amount_raw,
+        case
+            when currency = '0x0000000000000000000000000000000000000000' then '{{ wbnb_address }}'
+            else currency
+        end as currency_contract,
+        collection as nft_contract_address,
+        contract_address  as project_contract_address,
+        evt_tx_hash as tx_hash,
+        evt_block_number as block_number,
+        concat(cast(evt_block_number as varchar), '-', cast(evt_index as varchar)) as unique_trade_id,
+        case
+            when currency = '0x0000000000000000000000000000000000000000' then 'native'
+            else 'erc20'
+        end as currency_token_standard,
+        '1' as orderType
+    from (
+        select * from {{ source('liquidifty_bnb', 'MarketplaceV2_evt_Buy') }}
+        union all
+        select * from {{ source('liquidifty_bnb', 'MarketplaceV2_1_evt_Buy') }}
+        union all
+        select * from {{ source('liquidifty_bnb', 'MarketplaceV2_5_evt_Buy') }}
+    ) a
+),
+stack as (
+    select
+        cast(null as varchar) as version,
+        evt_block_time as block_time,
+        cast(null as varchar) as token_id,
+        'erc721' as token_standard,
+        'Bundle Item Trade' as trade_type,
+        amount as number_of_items,
+        'Buy' as trade_category,
+        owner as seller,
+        buyer,
+        price as amount_raw,
+        case
+            when currency = '0x0000000000000000000000000000000000000000' then '{{ wbnb_address }}'
+            else currency
+        end as currency_contract,
+        collection as nft_contract_address,
+        contract_address  as project_contract_address,
+        evt_tx_hash as tx_hash,
+        evt_block_number as block_number,
+        concat(cast(evt_block_number as varchar), '-', cast(evt_index as varchar)) as unique_trade_id,
+        case
+            when currency = '0x0000000000000000000000000000000000000000' then 'native'
+            else 'erc20'
+        end as currency_token_standard,
+        '1' as orderType
+    from {{ source('liquidifty_bnb', 'MultipleSell_evt_Buy') }}
+),
+v3 as (
+    select
+        'v3' as version,
+        call_block_time as block_time,
+        json_extract_scalar(nft, '$.id') as token_id,
+        case
+            when json_extract_scalar(nft, '$.assetType') = '0' then 'native'
+            when json_extract_scalar(nft, '$.assetType') = '1' then 'erc20'
+            when json_extract_scalar(nft, '$.assetType') = '2' then 'erc721'
+            when json_extract_scalar(nft, '$.assetType') = '3' then 'erc1155'
+        end as token_standard,
+        case
+            when cardinality(orders) > 1 then 'Bundle Trade'
+            else 'Single Item Trade'
+        end as trade_type,
+        json_extract_scalar(nft, '$.amount') as number_of_items,
+        case
+            when orderType = '0' then 'Swap'
+            when orderType = '1' then 'Buy'
+            when orderType = '2' then 'Sell'
+        end as trade_category,
+        json_extract_scalar("order", '$.signer') as seller,
+        cast(null as varchar) as buyer,
+        json_extract_scalar(currency, '$.amount') as amount_raw,
+        case
+            when json_extract_scalar(currency, '$.assetType') = '0' then '{{ wbnb_address }}'
+            else json_extract_scalar(currency, '$.collection')
+        end as currency_contract,
+        json_extract_scalar(nft, '$.collection') as nft_contract_address,
+        contract_address  as project_contract_address,
+        call_tx_hash as tx_hash,
+        call_block_number as block_number,
+        concat(cast(call_block_number as varchar), '-', call_tx_hash, cast(array_join(call_trace_address, ',') as varchar)) as unique_trade_id,
+        case
+            when json_extract_scalar(currency, '$.assetType') = '0' then 'native'
+            when json_extract_scalar(currency, '$.assetType') = '1' then 'erc20'
+            when json_extract_scalar(currency, '$.assetType') = '2' then 'erc721'
+            when json_extract_scalar(currency, '$.assetType') = '3' then 'erc1155'
+        end as currency_token_standard,
+        orderType
+    from (
+        select *,
+            case
+                when orderType = '1' then json_parse(json_extract_scalar("order", '$.bid[0]'))
+                else json_parse(json_extract_scalar("order", '$.ask[0]'))
+            end as nft,
+            case
+                when orderType = '1' then json_parse(json_extract_scalar("order", '$.ask[0]'))
+                else json_parse(json_extract_scalar("order", '$.bid[0]'))
+            end as currency
+        from (
+            select *,
+                json_extract_scalar("order", '$.orderType') as orderType,
+                json_extract_scalar("order", '$.amount') as amount
+            from (
+                select * from {{ source('liquidifty_bnb', 'MarketplaceV3_call_buy') }}
+                union all
+                select * from {{ source('liquidifty_bnb', 'MarketplaceV3_deprecated_call_buy') }}
+            ), unnest(orders) as orders("order")
+            where call_success
+        )
+    )
+)
+
+
+select
+    'bnb' as blockchain,
+    'liquidifty' as project,
+    buys.version,
+    buys.block_time,
+    buys.token_id,
+    nft_tokens.name as collection,
+    cast(buys.amount_raw as decimal(38, 0)) / power(10, bep20.decimals) * prices.price as amount_usd,
+    buys.token_standard as token_standard,
+    buys.trade_type,
+    buys.number_of_items,
+    buys.trade_category,
+    'Trade' as evt_type,
+    buys.seller,
+    case
+        when buys.version = 'v3' then transactions."from"
+        else buys.buyer
+    end as buyer,
+    cast(buys.amount_raw as decimal(38, 0)) / power(10, bep20.decimals) as amount_original,
+    cast(buys.amount_raw as decimal(38, 0)) as amount_raw,
+    bep20.symbol as currency_symbol,
+    buys.currency_contract,
+    buys.nft_contract_address,
+    buys.project_contract_address,
+    cast(null as varchar) as aggregator_name,
+    cast(null as varchar) as aggregator_address,
+    buys.tx_hash,
+    buys.block_number,
+    transactions."from" as tx_from,
+    transactions.to as tx_to,
+    buys.unique_trade_id,
+    buys.currency_token_standard,
+    buys.orderType
+from (
+    select * from v1
+    union all
+    select * from v2
+    union all
+    select * from stack
+    union all
+    select * from v3
+) buys
+left join {{ ref('tokens_bnb_bep20') }} bep20
+    on bep20.contract_address = buys.currency_contract
+left join {{ ref('tokens_bnb_nft') }} nft_tokens
+    on nft_tokens.contract_address = buys.nft_contract_address
+left join {{ source('prices', 'usd') }} as prices
+    on prices.minute = date_trunc('minute', buys.block_time)
+    and prices.contract_address = buys.currency_contract
+    and prices.blockchain = 'bnb'
+inner join {{ source('bnb', 'transactions') }} transactions
+    on transactions.hash = buys.tx_hash
