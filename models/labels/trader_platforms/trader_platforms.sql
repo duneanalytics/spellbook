f4aa4538bd410626f2a95fe1df7ea40efe2589c4
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-01-25 22:28:45 +0100

    fix synthax

--- models/labels/trader_platforms/trader_platforms.sql
+++ models/labels/trader_platforms/trader_platforms.sql
@@ -29,7 +29,7 @@ with
 select
   collect_list(blockchain) as blockchain,
   address,
-  array_join(array_distinct(initcap(project)), ', ') ||' User' as name,
+  collect_list(initcap(project)) ||' User' as name,
   "trader_platforms" AS category,
   "gentrexha" AS contributor,
   "query" AS source,
