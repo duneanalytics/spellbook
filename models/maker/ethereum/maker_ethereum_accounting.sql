3d1fa56cf0bd27b3fee148c8925076fa40b24155
web3lyt <104781728+web3lyt@users.noreply.github.com>
2023-01-26 17:57:50 -0600

    Update maker_ethereum_accounting.sql

--- models/maker/ethereum/maker_ethereum_accounting.sql
+++ models/maker/ethereum/maker_ethereum_accounting.sql
@@ -1,6 +1,6 @@
 {{ config(
         alias ='accounting',
-        partition_by = ['code'],
+        partition_by = ['ts'],
         materialized = 'table',
         file_format = 'delta',
         post_hook='{{ expose_spells(\'["ethereum"]\',
