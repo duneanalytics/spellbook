85a1afea6587298cc7374cf17c79defa49560cbb
Huang Geyang <Sukebeta@outlook.com>
2023-01-28 22:04:23 +0800

    Fix bnb sql

--- models/sushiswap/bnb/sushiswap_bnb_trades.sql
+++ models/sushiswap/bnb/sushiswap_bnb_trades.sql
@@ -39,72 +39,71 @@ WITH sushi_v2_evt_swap AS (
 ),
 
 sushi_v2_pair_trades AS (
-    SELECT
-    'bnb'                                                              AS blockchain,
-    'sushiswap'                                                        AS project,
-    '1'                                                                AS version,
-    try_cast(date_trunc('DAY', sushiswap_dex.block_time) AS date)      AS block_date,
-    sushiswap_dex.block_time,
-    erc20a.symbol                                                      AS token_bought_symbol,
-    erc20b.symbol                                                      AS token_sold_symbol,
-    CASE
-        WHEN lower(erc20a.symbol) > lower(erc20b.symbol) THEN concat(erc20b.symbol, '-', erc20a.symbol)
-        ELSE concat(erc20a.symbol, '-', erc20b.symbol)
-        END                                                            AS token_pair,
-    sushiswap_dex.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
-    sushiswap_dex.token_sold_amount_raw / power(10, erc20b.decimals)   AS token_sold_amount,
-    CAST(sushiswap_dex.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw,
-    CAST(sushiswap_dex.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw,
-    coalesce(
-            sushiswap_dex.amount_usd
-        , (sushiswap_dex.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
-        , (sushiswap_dex.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
-        )                                                              AS amount_usd,
-    sushiswap_dex.token_bought_address,
-    sushiswap_dex.token_sold_address,
-    coalesce(sushiswap_dex.taker, tx.from)                             AS taker,
-    sushiswap_dex.maker,
-    sushiswap_dex.project_contract_address,
-    sushiswap_dex.tx_hash,
-    tx.from                                                            AS tx_from,
-    tx.to                                                              AS tx_to,
-    sushiswap_dex.trace_address,
-    sushiswap_dex.evt_index
-FROM sushi_v2_evt_swap
-INNER JOIN {{ source('bnb', 'transactions') }} tx
-    ON sushiswap_dex.tx_hash = tx.hash
-    {% if is_incremental() %}
-    AND tx.block_time >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
-    {% if not is_incremental() %}
-    AND tx.block_time >= '{{project_start_date}}'
-    {% endif %}
-LEFT JOIN {{ ref('tokens_erc20') }} erc20a
-    ON erc20a.contract_address = sushiswap_dex.token_bought_address
-    AND erc20a.blockchain = 'bnb'
-LEFT JOIN {{ ref('tokens_erc20') }} erc20b
-    ON erc20b.contract_address = sushiswap_dex.token_sold_address
-    AND erc20b.blockchain = 'bnb'
-LEFT JOIN {{ source('prices', 'usd') }} p_bought
-    ON p_bought.minute = date_trunc('minute', sushiswap_dex.block_time)
-    AND p_bought.contract_address = sushiswap_dex.token_bought_address
-    AND p_bought.blockchain = 'bnb'
-    {% if is_incremental() %}
-    AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
-    {% if not is_incremental() %}
-    AND p_bought.minute >= '{{project_start_date}}'
-    {% endif %}
-LEFT JOIN {{ source('prices', 'usd') }} p_sold
-    ON p_sold.minute = date_trunc('minute', sushiswap_dex.block_time)
-    AND p_sold.contract_address = sushiswap_dex.token_sold_address
-    AND p_sold.blockchain = 'bnb'
-    {% if is_incremental() %}
-    AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
-    {% if not is_incremental() %}
-    AND p_sold.minute >= '{{project_start_date}}'
-    {% endif %}
+    SELECT 'bnb'                                                                  AS blockchain,
+           'sushiswap'                                                            AS project,
+           '1'                                                                    AS version,
+           try_cast(date_trunc('DAY', sushi_v2_evt_swap.block_time) AS date)      AS block_date,
+           sushi_v2_evt_swap.block_time,
+           erc20a.symbol                                                          AS token_bought_symbol,
+           erc20b.symbol                                                          AS token_sold_symbol,
+           CASE
+               WHEN lower(erc20a.symbol) > lower(erc20b.symbol) THEN concat(erc20b.symbol, '-', erc20a.symbol)
+               ELSE concat(erc20a.symbol, '-', erc20b.symbol)
+               END                                                                AS token_pair,
+           sushi_v2_evt_swap.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
+           sushi_v2_evt_swap.token_sold_amount_raw / power(10, erc20b.decimals)   AS token_sold_amount,
+           CAST(sushi_v2_evt_swap.token_bought_amount_raw AS DECIMAL(38, 0))      AS token_bought_amount_raw,
+           CAST(sushi_v2_evt_swap.token_sold_amount_raw AS DECIMAL(38, 0))        AS token_sold_amount_raw,
+           coalesce(
+                   sushi_v2_evt_swap.amount_usd
+               , (sushi_v2_evt_swap.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
+               , (sushi_v2_evt_swap.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
+               )                                                                  AS amount_usd,
+           sushi_v2_evt_swap.token_bought_address,
+           sushi_v2_evt_swap.token_sold_address,
+           coalesce(sushi_v2_evt_swap.taker, tx.from)                             AS taker,
+           sushi_v2_evt_swap.maker,
+           sushi_v2_evt_swap.project_contract_address,
+           sushi_v2_evt_swap.tx_hash,
+           tx.from                                                                AS tx_from,
+           tx.to                                                                  AS tx_to,
+           sushi_v2_evt_swap.trace_address,
+           sushi_v2_evt_swap.evt_index
+    FROM sushi_v2_evt_swap
+    INNER JOIN {{ source('bnb', 'transactions') }} tx
+        ON sushi_v2_evt_swap.tx_hash = tx.hash
+        {% if is_incremental() %}
+        AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+        {% if not is_incremental() %}
+        AND tx.block_time >= '{{project_start_date}}'
+        {% endif %}
+    LEFT JOIN {{ ref('tokens_erc20') }} erc20a
+        ON erc20a.contract_address = sushi_v2_evt_swap.token_bought_address
+        AND erc20a.blockchain = 'bnb'
+    LEFT JOIN {{ ref('tokens_erc20') }} erc20b
+        ON erc20b.contract_address = sushi_v2_evt_swap.token_sold_address
+        AND erc20b.blockchain = 'bnb'
+    LEFT JOIN {{ source('prices', 'usd') }} p_bought
+        ON p_bought.minute = date_trunc('minute', sushi_v2_evt_swap.block_time)
+        AND p_bought.contract_address = sushi_v2_evt_swap.token_bought_address
+        AND p_bought.blockchain = 'bnb'
+        {% if is_incremental() %}
+        AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+        {% if not is_incremental() %}
+        AND p_bought.minute >= '{{project_start_date}}'
+        {% endif %}
+    LEFT JOIN {{ source('prices', 'usd') }} p_sold
+        ON p_sold.minute = date_trunc('minute', sushi_v2_evt_swap.block_time)
+        AND p_sold.contract_address = sushi_v2_evt_swap.token_sold_address
+        AND p_sold.blockchain = 'bnb'
+        {% if is_incremental() %}
+        AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+        {% if not is_incremental() %}
+        AND p_sold.minute >= '{{project_start_date}}'
+        {% endif %}
 )
 
 SELECT *
