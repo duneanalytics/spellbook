3152406f3888033875be3a38edb50cecf6f588c9
Huang Geyang <Sukebeta@outlook.com>
2023-01-28 16:52:37 +0800

    Update sharky sql

--- models/sharky/solana/sharky_solana_events.sql
+++ models/sharky/solana/sharky_solana_events.sql
@@ -4,11 +4,11 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_date', 'unique_trade_id'],
+    unique_key = ['block_date', 'tx_hash', 'id'],
     post_hook='{{ expose_spells(\'["solana"]\',
                                     "project",
                                     "sharky",
-                                    \'["ennnas"]\') }}'
+                                    \'["ennnas", "hosuke"]\') }}'
     )
 }}
 
@@ -16,76 +16,86 @@
 {%- set sharky_smart_contract = 'SHARKobtfF1bHhxD2eqftjHBdVSCbKo9JtgK71FhELP' %}
 
 WITH sharky_txs AS (
-        SELECT
-            tx_id AS id,
-            block_time
-        FROM {{ source('solana','account_activity') }}
-        WHERE
-            tx_success
+        SELECT tx_id AS id,
+               block_time
+        FROM {{ source('solana', 'account_activity') }}
+        {% if not is_incremental() %}
+        WHERE block_time >= '{{ project_start_date }}'
+        {% endif %}
+        {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        WHERE block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+          AND tx_success
+          AND address = '{{sharky_smart_contract}}'
+    ),
+
+    events AS (
+        SELECT 'solana'                                                        AS blockchain,
+               'sharky'                                                        AS project,
+               signatures[0]                                                   AS tx_hash,
+               block_date,
+               block_time,
+               CAST(block_slot AS BIGINT)                                      AS block_number,
+               (abs(post_balances[0] - pre_balances[0]) / 1e9) * p.price       AS amount_usd,
+               (abs(post_balances[0] - pre_balances[0]) / 1e9)                 AS amount_original,
+               CAST(abs(post_balances[0] - pre_balances[0]) AS DECIMAL(38, 0)) AS amount_raw,
+               filter(
+                           instructions,
+                           x -> x.executing_account = '{{sharky_smart_contract}}'
+                   )                                                           AS sharky_instructions,
+               CASE
+                   WHEN array_contains(log_messages, 'Program log: Instruction: OfferLoan') THEN 'Offer'
+                   WHEN array_contains(log_messages, 'Program log: Instruction: TakeLoan') THEN 'Take'
+                   WHEN array_contains(log_messages, 'Program log: Instruction: RescindLoan') THEN 'Rescind'
+                   WHEN
+                       (
+                               array_contains(log_messages, 'Program log: Instruction: RepayLoan')
+                               OR array_contains(log_messages, 'Program log: Instruction: RepayLoanEscrow')
+                           ) THEN 'Repay'
+                   WHEN
+                       (
+                               array_contains(log_messages, 'Program log: Instruction: ForecloseLoan')
+                               OR array_contains(log_messages, 'Program log: Instruction: ForecloseLoanEscrow')
+                           ) THEN 'Foreclose'
+                   ELSE 'Other' END                                            AS evt_type,
+               signer                                                          AS user,
+               id
+        FROM sharky_txs stx
+        INNER JOIN {{ source('solana','transactions') }} tx
+            ON stx.id = tx.id
             {% if not is_incremental() %}
-            AND block_time >= '{{ project_start_date }}'
+            AND tx.block_time >= '{{project_start_date}}'
             {% endif %}
             {% if is_incremental() %}
-            -- this filter will only be applied on an incremental run
-            AND block_time >= date_trunc("day", now() - interval '1 week')
+            AND tx.block_time >= date_trunc("day", now() - interval '1 week')
             {% endif %}
-            AND address = '{{sharky_smart_contract}}'
-    ), events AS (
-        SELECT
-            'solana' as blockchain,
-            'sharky' as project,
-            signatures[0] as tx_hash,
-            block_date,
-            block_time,
-            CAST(block_slot AS BIGINT) as block_number,
-            (abs(post_balances[0] - pre_balances[0]) / 1e9) * p.price AS amount_usd,
-            (abs(post_balances[0] - pre_balances[0]) / 1e9) AS amount_original,
-            CAST(abs(post_balances[0] - pre_balances[0]) AS DECIMAL(38,0)) AS amount_raw,
-            filter(
-                instructions,
-                x -> x.executing_account = '{{sharky_smart_contract}}'
-              ) AS sharky_instructions,
-            CASE
-              WHEN array_contains( log_messages, 'Program log: Instruction: OfferLoan') THEN 'Offer'
-              WHEN array_contains( log_messages, 'Program log: Instruction: TakeLoan') THEN 'Take'
-              WHEN array_contains( log_messages, 'Program log: Instruction: RescindLoan') THEN 'Rescind'
-              WHEN
-                  (
-                      array_contains(log_messages, 'Program log: Instruction: RepayLoan')
-                      OR array_contains(log_messages, 'Program log: Instruction: RepayLoanEscrow')
-                  ) THEN 'Repay'
-              WHEN
-                  (
-                    array_contains(log_messages, 'Program log: Instruction: ForecloseLoan')
-                    OR array_contains(log_messages, 'Program log: Instruction: ForecloseLoanEscrow')
-                  ) THEN 'Foreclose'
-            ELSE 'Other' END as evt_type,
-            signer as user,
-            signatures[0] || '-' || id as unique_trade_id
-        FROM {{ source('solana','transactions') }}
-        INNER JOIN sharky_txs USING (block_time, id)
-        LEFT JOIN prices.usd p
+        LEFT JOIN {{ source('prices', 'usd') }} p
             ON p.minute = date_trunc('minute', block_time)
             AND p.blockchain is NULL
             AND p.symbol = 'SOL'
+            {% if not is_incremental() %}
+            AND p.minute >= '{{project_start_date}}'
+            {% endif %}
             {% if is_incremental() %}
             AND p.minute >= date_trunc("day", now() - interval '1 week')
             {% endif %}
 )
-    SELECT
-    *,
-    CASE
-        -- The smart contract was update around the 2022-12-01 and a new account was added before the loan id
-        WHEN evt_type = 'Offer' THEN IF(
-                sharky_instructions[0].account_arguments[2] = 'So11111111111111111111111111111111111111112',
-                sharky_instructions[0].account_arguments[3],
-                sharky_instructions[0].account_arguments[2]
-            )
-        WHEN evt_type = 'Take' THEN IF(
-                sharky_instructions[0].account_arguments[4] = 'So11111111111111111111111111111111111111112',
-                sharky_instructions[0].account_arguments[6],
-                sharky_instructions[0].account_arguments[5]
-            )
-        WHEN (evt_type = 'Rescind' OR evt_type = 'Repay' OR evt_type = 'Foreclose') THEN sharky_instructions[0].account_arguments[0]
-    END as loan_id
-    FROM events
+SELECT *,
+       CASE
+           -- The smart contract was update around the 2022-12-01 and a new account was added before the loan id
+           WHEN evt_type = 'Offer' THEN IF(
+                       sharky_instructions[0].account_arguments[2] = 'So11111111111111111111111111111111111111112',
+                       sharky_instructions[0].account_arguments[3],
+                       sharky_instructions[0].account_arguments[2]
+               )
+           WHEN evt_type = 'Take' THEN IF(
+                       sharky_instructions[0].account_arguments[4] = 'So11111111111111111111111111111111111111112',
+                       sharky_instructions[0].account_arguments[6],
+                       sharky_instructions[0].account_arguments[5]
+               )
+           WHEN (evt_type = 'Rescind' OR evt_type = 'Repay' OR evt_type = 'Foreclose')
+               THEN sharky_instructions[0].account_arguments[0]
+           END as loan_id
+FROM events
+;
