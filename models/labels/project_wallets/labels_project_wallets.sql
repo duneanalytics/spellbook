926115383d1292f30ff6d796863d9039b9786764
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-01-29 11:39:59 -0500

    labels edit

--- models/labels/project_wallets/labels_project_wallets.sql
+++ models/labels/project_wallets/labels_project_wallets.sql
@@ -6,15 +6,19 @@
 )}}
 
 SELECT
+
     'optimism' as blockchain,
     address,
     project_name AS name,
-    'project wallet' AS category
-    'msilb7' AS contributor
+    'project wallet' AS category,
+    'msilb7' AS contributor,
     'static' AS source,
     timestamp('2023-01-28') as created_at,
     NOW() AS updated_at
+
 FROM {{ ref('addresses_optimism_grants_funding') }}
 GROUP BY 1,2,3
 
 -- UNION ALL
+
+-- Add additional project wallet labels here
