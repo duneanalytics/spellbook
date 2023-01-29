e4e0188728acfd3a450705ff20f5b15c08abaa86
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-01-29 23:47:07 +0100

    add tofu arbitrum to nft trades, events & fees

---
+++ models/tofu/tofu_trades.sql
@@ -0,0 +1,52 @@
+{{ config(
+        alias ='trades',
+        post_hook='{{ expose_spells(\'["arbitrum","bnb"]\',
+                                    "project",
+                                    "tofu",
+                                    \'["Henrystats", "theachenyj"]\') }}')
+}}
+
+
+{% set tofu_models = [
+ ref('tofu_bnb_trades')
+,ref('tofu_arbitrum_trades')
+] %}
+
+SELECT *
+FROM (
+    {% for nft_model in tofu_models %}
+    SELECT
+        blockchain,
+        project,
+        version,
+        block_time,
+        token_id,
+        collection,
+        amount_usd,
+        token_standard,
+        trade_type,
+        number_of_items,
+        trade_category,
+        evt_type,
+        seller,
+        buyer,
+        amount_original,
+        amount_raw,
+        currency_symbol,
+        currency_contract,
+        nft_contract_address,
+        project_contract_address,
+        aggregator_name,
+        aggregator_address,
+        tx_hash,
+        block_number,
+        tx_from,
+        tx_to,
+        unique_trade_id
+    FROM {{ nft_model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+
+)
