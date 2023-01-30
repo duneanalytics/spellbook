d104509eebae9c6a7cf27d8e7a9c9abd9a15af60
bizzy <horlasehinde@gmail.com>
2023-01-30 08:51:43 +0000

    fix test: use tx_hash instead of block_time, case different block could have same time

--- models/balancer/ethereum/balancer_ethereum_trades.sql
+++ models/balancer/ethereum/balancer_ethereum_trades.sql
@@ -5,7 +5,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_time', 'evt_index'],
+    unique_key = ['tx_hash', 'evt_index'],
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "balancer",
