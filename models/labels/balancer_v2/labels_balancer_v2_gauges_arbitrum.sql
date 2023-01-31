645813047e9564579e25e1441c9571caa9b01fb6
Jacek Trocinski <jacek@hedgehog.app>
2023-01-31 15:55:28 +0100

    Add pool labels.

--- models/labels/balancer_v2/labels_balancer_v2_gauges_arbitrum.sql
+++ models/labels/balancer_v2/labels_balancer_v2_gauges_arbitrum.sql
@@ -7,7 +7,7 @@
 SELECT
     'arbitrum' AS blockchain,
     gauge.gauge AS address,
-    'arb:' || streamer.pool AS name,
+    'arb:' || pools.name AS name,
     'balancer_v2_gauges' AS category,
     'balancerlabs' AS contributor,
     'query' AS source,
@@ -16,11 +16,12 @@ SELECT
 FROM
     {{ source('balancer_ethereum', 'ArbitrumRootGaugeFactory_evt_ArbitrumRootGaugeCreated') }} gauge
     LEFT JOIN {{ source('balancer_arbitrum', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON gauge.recipient = streamer.streamer
+    LEFT JOIN {{ ref('labels_balancer_v2_pools_arbitrum') }} pools ON pools.address = streamer.pool
 UNION ALL
 SELECT
     'arbitrum' AS blockchain,
     gauge.gauge AS address,
-    'arb:' || streamer.pool AS name,
+    'arb:' || pools.name AS name,
     'balancer_v2_gauges' AS category,
     'balancerlabs' AS contributor,
     'query' AS source,
@@ -29,5 +30,6 @@ SELECT
 FROM
     {{ source('balancer_ethereum', 'CappedArbitrumRootGaugeFactory_evt_GaugeCreated') }} gauge
     INNER JOIN {{ source('balancer_ethereum', 'CappedArbitrumRootGaugeFactory_call_create') }} call ON call.call_tx_hash = gauge.evt_tx_hash
-    LEFT JOIN {{ source('balancer_arbitrum', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON streamer.streamer = call.recipient;
+    LEFT JOIN {{ source('balancer_arbitrum', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON streamer.streamer = call.recipient
+    LEFT JOIN {{ ref('labels_balancer_v2_pools_arbitrum') }} pools ON pools.address = streamer.pool;
 
