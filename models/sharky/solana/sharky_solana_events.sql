424f38388b29ed2f1e56a23a4605349c60ab26b2
Huang Geyang <Sukebeta@outlook.com>
2023-01-31 13:04:41 +0800

    Try model building can finished in 60 mins without sol_price join

--- models/sharky/solana/sharky_solana_events.sql
+++ models/sharky/solana/sharky_solana_events.sql
@@ -50,7 +50,7 @@ WITH sharky_txs AS (
                block_date,
                block_time,
                CAST(block_slot AS BIGINT)                                      AS block_number,
-               (abs(post_balances[0] - pre_balances[0]) / 1e9) * p.price       AS amount_usd,
+--                (abs(post_balances[0] - pre_balances[0]) / 1e9) * p.price       AS amount_usd,
                (abs(post_balances[0] - pre_balances[0]) / 1e9)                 AS amount_original,
                CAST(abs(post_balances[0] - pre_balances[0]) AS DECIMAL(38, 0)) AS amount_raw,
                filter(
