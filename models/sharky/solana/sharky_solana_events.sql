677ca723ed3a7ec2ad7aaf05050dff9ead968bca
Ennio Nasca <nascaennio@gmail.com>
2023-01-31 18:12:55 +0100

    chore: try 2 month of data

--- models/sharky/solana/sharky_solana_events.sql
+++ models/sharky/solana/sharky_solana_events.sql
@@ -12,7 +12,7 @@
     )
 }}
 
-{%- set project_start_date = '2022-12-30' %} -- TODO change back to '2022-04-14'
+{%- set project_start_date = '2022-11-31' %} -- TODO change back to '2022-04-14'
 {%- set sharky_smart_contract = 'SHARKobtfF1bHhxD2eqftjHBdVSCbKo9JtgK71FhELP' %}
 
 WITH sharky_txs AS (
