1da86573b9c97bc4f1338d9ebeb5c42d4a9eb35e
henrystats <65398020+henrystats@users.noreply.github.com>
2023-01-31 21:46:03 +0100

    add equaliser exchange fantom to dex trades - Ready for review  (#2536)
    * create files
    
    * add seeds and add model to dex trades
    
    * fix incremental sql
    
    * rerun gh
    
    * Update sql
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/dex/dex_trades.sql
+++ models/dex/dex_trades.sql
@@ -47,6 +47,7 @@ list of models using old generic test, due to multiple versions in one model:
 ,ref('babyswap_bnb_trades')
 ,ref('apeswap_trades')
 ,ref('ellipsis_finance_trades')
+,ref('equalizer_exchange_fantom_trades')
 ,ref('wigoswap_fantom_trades')
 ,ref('arbswap_trades')
 ] %}
