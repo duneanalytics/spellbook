be3e15f6410127008cc14208c0f96c5ecca048ca
Boxer <Floba.hs@gmail.com>
2023-02-02 02:26:56 +0100

    fix code

--- models/uniswap/arbitrum/uniswap_v3_arbitrum_trades.sql
+++ models/uniswap/arbitrum/uniswap_v3_arbitrum_trades.sql
@@ -38,45 +38,22 @@ WITH dexs AS
     SELECT
         t.evt_block_time AS block_time
         ,t.recipient AS taker
-        ,'' AS maker
         ,'' AS maker -- Uniswap v3 does not have a maker
         ,CASE WHEN amount0 < '0' THEN abs(amount0) ELSE abs(amount1) END AS token_bought_amount_raw -- when amount0 is negative it means trader_a is buying token0 from the pool
-        ,CASE WHEN amount0 < '0' THEN abs(amount1) ELSE abs(amount0) END AS token_sold_amount_raw
         ,CASE WHEN amount0 < '0' THEN abs(amount1) ELSE abs(amount0) END AS token_sold_amount_raw -- when amount0 is negative it means trader_a is buying token1 from the pool
-        ,NULL AS amount_usd
-        ,CASE WHEN amount0 < '0' THEN f.token0 ELSE f.token1 END AS token_bought_address
-        ,CASE WHEN amount0 < '0' THEN f.token1 ELSE f.token0 END AS token_sold_address
-        ,CAST(t.contract_address as string) as project_contract_address
-        ,t.evt_tx_hash AS tx_hash
-        ,'' AS trace_address
-        ,t.evt_index
+        ,NULL AS amount_usd -- Uniswap v3 does not have a price oracle in the Swap event
         ,CASE WHEN amount0 < '0' THEN f.token0 ELSE f.token1 END AS token_bought_address -- when amount0 is negative it means trader_a is buying token0 from the pool
         ,CASE WHEN amount0 < '0' THEN f.token1 ELSE f.token0 END AS token_sold_address -- when amount0 is negative it means trader_a is buying token1 from the pool
         ,CAST(t.contract_address as string) as project_contract_address -- Uniswap v3 pool contract address
         ,t.evt_tx_hash AS tx_hash -- the transaction in which this event was emitted
         ,'' AS trace_address 
         ,t.evt_index -- the index of this event in the transaction
-    FROM
-        {{ source('uniswap_v3_arbitrum', 'Pair_evt_Swap') }} t
-        uniswap_v3_arbitrum.Pair_evt_Swap t -- querying all Uniswap V3 Pools on Arbitrum
-    INNER JOIN 
-        {{ source('uniswap_v3_arbitrum', 'UniswapV3Factory_evt_PoolCreated') }} f
-        ON f.pool = t.contract_address
-    {% if is_incremental() %}
-    WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
-        uniswap_v3_arbitrum.UniswapV3Factory_evt_PoolCreated f -- Joining Uniswap V3 Factory PoolCreated event to get token0 and token1 addresses which are needed for further joins
-        ON f.pool = t.contract_address 
+        FROM
+        {{ source('uniswap_v3_arbitrum', 'Pair_evt_Swap') }} t -- all Uniswap V3 Swap events on Arbitrum
+        INNER JOIN 
+        uniswap_v3_arbitrum.UniswapV3Factory_evt_PoolCreated f ON f.pool = t.contract_address  -- Joining Uniswap V3 Factory PoolCreated event to get token0 and token1 addresses which are needed for further joins
 
 )
-SELECT DISTINCT
-    'arbitrum' AS blockchain
-    ,'uniswap' AS project
-    ,'3' AS version
-    ,TRY_CAST(date_trunc('DAY', dexs.block_time) AS date) AS block_date
-    ,dexs.block_time
-    ,erc20a.symbol AS token_bought_symbol
-    ,erc20b.symbol AS token_sold_symbol
 SELECT
     'arbitrum' AS blockchain -- add the blockchain name to the results
     ,'uniswap' AS project -- add the project name to the results
@@ -88,12 +65,7 @@ SELECT
     ,case
         when lower(erc20a.symbol) > lower(erc20b.symbol) then concat(erc20b.symbol, '-', erc20a.symbol)
         else concat(erc20a.symbol, '-', erc20b.symbol)
-    end as token_pair
-    ,dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount
-    ,dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount
-    ,CAST(dexs.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw
-    ,CAST(dexs.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw
-    end as token_pair -- ordering the symbols of token0 and token1 alphabetically and concatenating them with a dash in order to create consistent token_pairs
+        end as token_pair -- ordering the symbols of token0 and token1 alphabetically and concatenating them with a dash in order to create consistent token_pairs
     ,dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount -- calculating the display amount of token0 that was bought
     ,dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount -- calculating the display amount of token1 that was sold
     ,dexs.token_bought_amount_raw 
@@ -102,20 +74,10 @@ SELECT
         dexs.amount_usd
         ,(dexs.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
         ,(dexs.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
-    ) AS amount_usd
-    ,dexs.token_bought_address
-    ,dexs.token_sold_address
-    ) AS amount_usd -- calculating the amount of USD that was bought or sold by using either the price of the token sold or the token bought from the prices.usd table
+        ) AS amount_usd -- calculating the amount of USD that was bought or sold by using either the price of the token sold or the token bought from the prices.usd table
     ,dexs.token_bought_address -- the address of the token that was bought
     ,dexs.token_sold_address -- the address of the token that was sold
     ,coalesce(dexs.taker, tx.from) AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
-    ,dexs.maker
-    ,dexs.project_contract_address
-    ,dexs.tx_hash
-    ,tx.from AS tx_from
-    ,tx.to AS tx_to
-    ,dexs.trace_address
-    ,dexs.evt_index
     ,dexs.maker 
     ,dexs.project_contract_address -- the address of the Uniswap V3 pool contract
     ,dexs.tx_hash -- the transaction in which this event was emitted
@@ -123,59 +85,19 @@ SELECT
     ,tx.to AS tx_to -- the address that received the top level call in which this transaction
     ,dexs.trace_address 
     ,dexs.evt_index -- the index of this event in the transaction
-FROM dexs
-INNER JOIN 
-    {{ source('arbitrum', 'transactions') }} tx
-    ON tx.hash = dexs.tx_hash
-    {% if not is_incremental() %}
-    AND tx.block_time >= '{{project_start_date}}'
-    {% endif %}
-    {% if is_incremental() %}
-    AND tx.block_time >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
-LEFT JOIN {{ ref('tokens_erc20') }} erc20a
-    ON erc20a.contract_address = dexs.token_bought_address 
-    AND erc20a.blockchain = 'arbitrum'
-LEFT JOIN {{ ref('tokens_erc20') }} erc20b
-    ON erc20b.contract_address = dexs.token_sold_address
-    AND erc20b.blockchain = 'arbitrum'
-LEFT JOIN {{ source('prices', 'usd') }} p_bought
-    ON p_bought.minute = date_trunc('minute', dexs.block_time)
-    AND p_bought.contract_address = dexs.token_bought_address
-    AND p_bought.blockchain = 'arbitrum'
-    {% if not is_incremental() %}
-    AND p_bought.minute >= '{{project_start_date}}'
-    {% endif %}
-    {% if is_incremental() %}
-    AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
-LEFT JOIN {{ source('prices', 'usd') }} p_sold
-    ON p_sold.minute = date_trunc('minute', dexs.block_time)
-    AND p_sold.contract_address = dexs.token_sold_address
-    AND p_sold.blockchain = 'arbitrum'
-    {% if not is_incremental() %}
-    AND p_sold.minute >= '{{project_start_date}}'
+    FROM dexs
+    -- join the tokens_erc20 tables to get the symbol, decimals of the tokens that were bought and sold. No time element so can always do this
+    LEFT JOIN {{ ref('tokens_erc20') }} erc20a          ON erc20a.contract_address = dexs.token_bought_address      AND erc20a.blockchain = 'arbitrum'
+    LEFT JOIN {{ ref('tokens_erc20') }} erc20b          ON erc20b.contract_address = dexs.token_sold_address        AND erc20b.blockchain = 'arbitrum'
 
-    {% if not is_incremental() %} -- if the run is not incremental, join all transactions since the project start date
+    {% if not is_incremental() %} -- if the run is not incremental, join all transactions and prices since the project start date
     INNER JOIN  {{ source('arbitrum', 'transactions') }} tx ON tx.hash = dexs.tx_hash AND tx.block_time >= '{{project_start_date}}'
-    {% endif %}
-    {% if is_incremental() %}
-    AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
-        {% if is_incremental() %} -- if the run is incremental, join only the transactions that happened in the last week
-        INNER JOIN  {{ source('arbitrum', 'transactions') }} tx ON tx.hash = dexs.tx_hash AND tx.block_time >= date_trunc("day", now() - interval '1 week')
-        {% endif %}    
--- join the prices.usd and tokens_erc20 tables to get the symbol, decimals and price of the tokens that were bought and sold
-LEFT JOIN {{ ref('tokens_erc20') }} erc20a          ON erc20a.contract_address = dexs.token_bought_address      AND erc20a.blockchain = 'arbitrum'
-LEFT JOIN {{ ref('tokens_erc20') }} erc20b          ON erc20b.contract_address = dexs.token_sold_address        AND erc20b.blockchain = 'arbitrum'
-
-    {% if not is_incremental() %} -- if the run is not incremental, join all prices.usd starting from the project start date
     LEFT JOIN {{ source('prices', 'usd') }} p_bought ON p_bought.minute = date_trunc('minute', dexs.block_time)  AND p_bought.contract_address = dexs.token_bought_address   AND p_bought.blockchain = 'arbitrum' AND p_bought.minute >= '{{project_start_date}}'
     LEFT JOIN {{ source('prices', 'usd') }} p_sold   ON p_sold.minute   = date_trunc('minute', dexs.block_time)  AND p_sold.contract_address   = dexs.token_bought_address   AND p_sold.blockchain   = 'arbitrum' AND p_sold.minute   >= '{{project_start_date}}'
     {% endif %}
-;
-
-            {% if is_incremental() %} -- if the run is incremental, join only the prices.usd that happened in the last week      
-            LEFT JOIN {{ source('prices', 'usd') }} p_bought ON p_bought.minute = date_trunc('minute', dexs.block_time)  AND p_bought.contract_address = dexs.token_bought_address   AND p_bought.blockchain = 'arbitrum' AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
-            LEFT JOIN {{ source('prices', 'usd') }} p_sold   ON p_sold.minute   = date_trunc('minute', dexs.block_time)  AND p_sold.contract_address   = dexs.token_bought_address   AND p_sold.blockchain   = 'arbitrum' AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
-            {% endif %}
-;
+            
+    {% if is_incremental() %} -- if the run is incremental, join only the transactions and prices of the last week that happened in the last week
+    INNER JOIN  {{ source('arbitrum', 'transactions') }} tx ON tx.hash = dexs.tx_hash AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+    LEFT JOIN {{ source('prices', 'usd') }} p_bought ON p_bought.minute = date_trunc('minute', dexs.block_time)  AND p_bought.contract_address = dexs.token_bought_address   AND p_bought.blockchain = 'arbitrum' AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
+    LEFT JOIN {{ source('prices', 'usd') }} p_sold   ON p_sold.minute   = date_trunc('minute', dexs.block_time)  AND p_sold.contract_address   = dexs.token_bought_address   AND p_sold.blockchain   = 'arbitrum' AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
