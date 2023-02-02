d97271fcb106a9c11c3ac66f997cf6bce5e291fc
Hildebert Moulié <h.moulie@gmail.com>
2023-02-02 22:13:42 +0100

    change to use MIN_BY()

--- models/addresses_events/gnosis/addresses_events_gnosis_first_funded_by.sql
+++ models/addresses_events/gnosis/addresses_events_gnosis_first_funded_by.sql
@@ -9,36 +9,20 @@
 }}
 
 SELECT 'gnosis' AS blokchain
-    , b.to AS address
-    , MIN(a.from) AS first_funded_by
-    , MIN(a.block_time) AS block_time
-    , MIN(a.block_number) AS block_number
-    , MIN(a.tx_hash) AS tx_hash
-FROM {{ source('gnosis', 'traces') }} a
-JOIN
-(
-    SELECT et.to
-        , MIN(et.block_number) AS first_block
-    FROM {{ source('gnosis', 'traces') }} et
-    {% if is_incremental() %}
-    LEFT ANTI JOIN {{this}} ffb
-        ON et.to = ffb.address
-    {% endif %}
-    WHERE et.success
-    AND (et.call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR et.call_type IS NULL)
-    AND et.value > 0
-    {% if is_incremental() %}
-    AND et.block_time >= date_trunc('day', now() - interval '1 week')
-    {% endif %}
-    GROUP BY et.to
-) AS b 
-ON a.to = b.to
-    AND a.block_number = b.first_block
-WHERE a.success
-    AND (a.call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR a.call_type IS NULL)
-    AND a.value > 0
-    {% if is_incremental() %}
-    AND a.block_time >= date_trunc('day', now() - interval '1 week')
-    {% endif %}
-GROUP BY b.to
+, et.to AS address
+, MIN_BY(et.from, et.block_number) AS first_funded_by
+, MIN(et.block_time) AS block_time
+, MIN(et.block_number) AS block_number
+, MIN_BY(et.tx_hash, et.block_number) AS tx_hash
+FROM {{ source('gnosis', 'traces') }} et
+{% if is_incremental() %}
+LEFT ANTI JOIN {{this}} ffb ON et.to = ffb.address
+{% endif %}
+WHERE et.success
+AND (et.call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR et.call_type IS NULL)
+AND CAST(et.value AS double) > 0
+{% if is_incremental() %}
+AND et.block_time >= date_trunc('day', now() - interval '1 week')
+{% endif %}
+GROUP BY et.to
 ;
