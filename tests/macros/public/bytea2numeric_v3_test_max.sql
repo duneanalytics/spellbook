b0e1bf8593c00eef5771b6a2a82da8e6fdc6ee81
0xRob <0xRobin5@gmail.com>
2023-02-04 11:55:53 +0100

    add test

--- tests/macros/public/bytea2numeric_v3_test_max.sql
+++
@@ -1,8 +0,0 @@
-WITH unit_tests as
-(
-    SELECT
-        bytea2numeric_v3(lpad('',64,'F')) as result
-        ,'115792089237316195423570985008687907853269984665640564039457584007913129639935' as expected
-)
-select * from unit_tests
-where result != expected
