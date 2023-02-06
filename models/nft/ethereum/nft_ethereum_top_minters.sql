73172f43bfd5f4efd13f97671241dc198cfdf66a
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-02-06 17:20:11 +0100

    make top minters incremental

--- models/nft/ethereum/nft_ethereum_top_minters.sql
+++ models/nft/ethereum/nft_ethereum_top_minters.sql
@@ -1,31 +1,74 @@
 {{ config(
-      alias = 'top_minters',
-      post_hook='{{ expose_spells(\'["ethereum"]\',
-                                  "sector",
-                                  "nft",
-                                  \'["Henrystats"]\') }}'
-      )
-  }}
+    alias = 'top_minters',
+    materialized='incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['minter', 'nft_contract_address'],
+    post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "sector",
+                                "nft",
+                                \'["Henrystats"]\') }}'
+    )
+}}
 
 WITH 
 
 src_data as (
+    {% if not is_incremental() %}
     SELECT 
         nft_contract_address,
         buyer as minter, 
         SUM(amount_original) as eth_spent, 
-        COUNT(*) as no_minted
+        COUNT(*) as no_minted,
+        NOW() - Interval '5 Minutes' as last_updated
     FROM 
     {{ ref('nft_mints') }}
     WHERE blockchain = 'ethereum'
     AND currency_symbol IN ('WETH', 'ETH')
     AND amount_original IS NOT NULL
-    GROUP BY 1, 2
+    AND block_time < NOW() - Interval '5 Minutes'
+    GROUP BY 1, 2, 5
+    {% endif %}
+    -- incremental run
+    {% if is_incremental() %}
+    SELECT 
+        nft_contract_address,
+        buyer as minter, 
+        SUM(amount_original) as eth_spent, 
+        COUNT(*) as no_minted,
+        NOW() - Interval '5 Minutes' as last_updated
+    FROM 
+    {{ ref('nft_mints') }}
+    WHERE block_time >= (SELECT MAX(last_updated) FROM {{this}})
+    AND blockchain = 'ethereum'
+    AND currency_symbol IN ('WETH', 'ETH')
+    AND amount_original IS NOT NULL
+    AND block_time < NOW() - Interval '5 Minutes'
+    GROUP BY 1, 2, 5 
+
+    UNION ALL 
+
+    SELECT 
+        * 
+    FROM 
+    {{this}}
+    {% endif %}
+), 
+
+combined as (
+    SELECT 
+        nft_contract_address, 
+        minter, 
+        SUM(eth_spent) as eth_spent, 
+        SUM(no_minted) as no_minted, 
+        last_updated
+    FROM 
+    src_data
+    GROUP BY 1, 2, 5
 )
 
 SELECT 
     ROW_NUMBER() OVER (PARTITION BY nft_contract_address ORDER BY no_minted DESC) as rank_,
     * 
 FROM 
-src_data
-QUALIFY rank_ <= 50 
+combined
