344a921d85f09230058c67048e4beb9920c9e2f8
Hildebert Moulié <h.moulie@gmail.com>
2023-02-06 03:58:25 +0100

    fix gnosis

--- models/nft/gnosis/nft_gnosis_transfers.sql
+++ models/nft/gnosis/nft_gnosis_transfers.sql
@@ -70,20 +70,20 @@ WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
 UNION ALL
 
 SELECT 'gnosis' as blockchain
-, evt_block_time AS block_time
-, date_trunc('day', evt_block_time) AS block_date
-, evt_block_number AS block_number
+, t.evt_block_time AS block_time
+, date_trunc('day', t.evt_block_time) AS block_date
+, t.evt_block_number AS block_number
 , 'erc1155' AS token_standard
 , 'batch' AS transfer_type
-, evt_index
-, contract_address
-, ids_and_count.ids AS token_id
-, ids_and_count.values AS amount
-, from
-, to
+, t.evt_index
+, t.contract_address
+, t.ids_and_count.ids AS token_id
+, t.ids_and_count.values AS amount
+, t.from
+, t.to
 , gt.from AS executed_by
-, evt_tx_hash AS tx_hash
-, 'gnosis' || evt_tx_hash || '-erc1155-' || contract_address || '-' || ids_and_count.ids || '-' || from || '-' || to || '-' || ids_and_count.values || '-' || evt_index AS unique_transfer_id
+, t.evt_tx_hash AS tx_hash
+, 'gnosis' || t.evt_tx_hash || '-erc1155-' || t.contract_address || '-' || t.ids_and_count.ids || '-' || t.from || '-' || t.to || '-' || t.ids_and_count.values || '-' || t.evt_index AS unique_transfer_id
 FROM (
     SELECT t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, t.evt_index
     , explode(arrays_zip(t.values, t.ids)) AS ids_and_count
@@ -102,5 +102,5 @@ INNER JOIN gnosis.transactions gt ON gt.block_number = t.evt_block_number
     {% if is_incremental() %}
     AND gt.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
-WHERE ids_and_count.values > 0
+WHERE t.ids_and_count.values > 0
 GROUP BY blockchain, t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, gt.from, t.evt_index, token_id, amount
