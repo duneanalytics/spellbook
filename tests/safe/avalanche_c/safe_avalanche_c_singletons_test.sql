c7e66eb1ab9838a96a523ee73b8f4960ee1cfed9
Tobias Schubotz <tobias.schubotz@gmail.com>
2023-02-07 13:13:21 +0100

    Fix test

--- tests/safe/avalanche_c/safe_avalanche_c_singletons_test.sql
+++ tests/safe/avalanche_c/safe_avalanche_c_singletons_test.sql
@@ -2,7 +2,7 @@
 
 with test_data as (
     select count(*)
-    from safe_arbitrum.singletons
+    from safe_avalanche_c.singletons
     where address in (
         '0xfb1bffc9d739b8d520daf37df666da4c687191ea',
         '0x69f4D1788e39c87893C980c06EdF4b7f686e2938')
