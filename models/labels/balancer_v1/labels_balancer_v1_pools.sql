357f00eb81bf679cc19c0237a84e94497394015b
Stefenon <victorstefenon0@gmail.com>
2023-02-07 23:52:25 +0100

    Create cross blockchain pools V1 labels query

---
+++ models/labels/balancer_v1/labels_balancer_v1_pools.sql
@@ -0,0 +1,7 @@
+{{config(alias='labels_balancer_v1_pools',
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                    "sector",
+                                    "labels",
+                                    \'["balancerlabs"]\') }}')}}
+
+SELECT * FROM  {{ ref('labels_balancer_v1_pools_ethereum') }}
