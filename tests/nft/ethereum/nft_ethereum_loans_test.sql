57175e6fb54a4d9f8977ecdcfb7c4a96ac6df169
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2023-02-08 20:26:16 +0100

    removed initial test

--- tests/nft/ethereum/nft_ethereum_loans_test.sql
+++ tests/nft/ethereum/nft_ethereum_loans_test.sql
@@ -1,18 +1,15 @@
 WITH unit_tests AS (
 
         SELECT
-        CASE WHEN loans.collectionName = test_data.collectionName  THEN true
+        CASE WHEN loans.evt_block_time = test_data.evt_block_time THEN true
                 ELSE false
                 END AS test_1
-        ,CASE WHEN loans.evt_block_time = test_data.evt_block_time THEN true
-                ELSE false
-                END AS test_2
         ,CASE WHEN loans.borrower = test_data.borrower THEN true
                 ELSE false
-                END AS test_3
+                END AS test_2
         ,CASE WHEN loans.lender = test_data.lender THEN true
                 ELSE false
-                END AS test_4
+                END AS test_3
 FROM {{ ref('nft_ethereum_loans') }} loans
 INNER JOIN {{ ref('nft_ethereum_loans_seed') }} test_data ON 
             loans.evt_tx_hash = test_data.evt_tx_hash 
@@ -25,4 +22,3 @@ FROM unit_tests
 WHERE test_1 IS false
    OR test_2 IS false
    OR test_3 IS false
-   OR test_4 IS false
