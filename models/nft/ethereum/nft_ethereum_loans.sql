0cec9f2300e02f40b4de8f2d7976f5d1e2a852c7
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2023-02-08 17:30:08 +0100

    added new version of x2y2

--- models/nft/ethereum/nft_ethereum_loans.sql
+++ models/nft/ethereum/nft_ethereum_loans.sql
@@ -99,14 +99,29 @@ x2y2_base as (
             get_json_object(loanDetail, '$.borrowAsset') as currency,
             get_json_object(loanDetail, '$.repayAmount')  as repayment_raw,
             get_json_object(loanDetail, '$.loanDuration') / 86400 as duration,
-            contract_address, loanId
+            contract_address, loanId, 'v1' as version
         from {{ source('xy3_ethereum','XY3_evt_LoanStarted') }}
+        union all
+        select 
+            evt_tx_hash, evt_block_time, borrower, lender,
+            get_json_object(loanDetail, '$.nftAsset') as collectionContract,
+            get_json_object(loanDetail, '$.nftTokenId') as tokenId,
+            get_json_object(loanDetail, '$.borrowAmount') as principal_raw,
+            get_json_object(loanDetail, '$.borrowAsset') as currency,
+            get_json_object(loanDetail, '$.repayAmount')  as repayment_raw,
+            get_json_object(loanDetail, '$.loanDuration') / 86400 as duration,
+            contract_address, loanId, 'v2' as version
+        from {{ source('xy3_ethereum','XY3_V2_evt_LoanStarted') }}
     ) t
 ),
 
 x2y2 as (
     select l.*, r.evt_block_time as repay_time
-    from x2y2_base l left join {{ source('xy3_ethereum','XY3_evt_LoanRepaid') }} r on l.loanId=r.loanId and l.contract_address=r.contract_address
+    from x2y2_base l left join (
+        select * from {{ source('xy3_ethereum','XY3_evt_LoanRepaid') }}
+        union all
+        select * from {{ source('xy3_ethereum','XY3_V2_evt_LoanRepaid') }}
+    ) r on l.loanId=r.loanId and l.contract_address=r.contract_address
 ),
 
 -- arcade loans
@@ -357,3 +372,4 @@ select l.*, coalesce(t.name, 'Awesome NFT') as collectionName
 from loans_with_prices l
 left join {{ ref('tokens_nft') }} t
     on l.collectionContract=t.contract_address
+order by evt_block_time asc
