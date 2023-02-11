622e475c6c3f772c634cbbca79a36caf5aac4256
jeff-dude <jeff@dune.xyz>
2023-02-11 18:10:34 -0500

    set start date back for full run

--- models/oneinch/ethereum/oneinch_v3_ethereum_trades.sql
+++ models/oneinch/ethereum/oneinch_v3_ethereum_trades.sql
@@ -10,7 +10,7 @@
     )
 }}
 
-{% set project_start_date = '2023-02-01' %} --for testing, use small subset of data
+{% set project_start_date = '2017-01-01' %} --for testing, use small subset of data
 
 {% set final_columns = [
     'block_number'
