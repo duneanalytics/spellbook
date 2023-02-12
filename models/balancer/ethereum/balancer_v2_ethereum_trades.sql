a10a0e7ff319552c59051d6002bf7807fd7d639f
Huang Geyang <Sukebeta@outlook.com>
2023-02-12 20:57:27 +0800

    Fix typo

--- models/balancer/ethereum/balancer_v2_ethereum_trades.sql
+++ models/balancer/ethereum/balancer_v2_ethereum_trades.sql
@@ -62,14 +62,14 @@ select
     end as token_pair,
     token_bought_amount_raw / power(10, erc20a.decimals) as token_bought_amount,
     token_sold_amount_raw / power(10, erc20b.decimals) as token_sold_amount,
-    token_bought_amount_raw,
-    token_sold_amount_raw,
+    CAST(token_bought_amount_raw AS DECIMAL(38,0)) as token_bought_amount_raw,
+    CAST(token_sold_amount_raw AS DECIMAL(38,0)) as token_sold_amount_raw,
     coalesce(
         (token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price,
         (token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
     ) AS amount_usd,
-    CAST(token_bought_amount_raw AS DECIMAL(38,0)) as token_bought_amount_raw,
-    CAST(token_sold_amount_raw AS DECIMAL(38,0)) as token_sold_amount_raw,
+    token_bought_address,
+    token_sold_address,
     tx.from as taker,
     cast(null as varchar(5)) as maker,
     project_contract_address,
