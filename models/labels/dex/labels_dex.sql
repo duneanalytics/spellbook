545d98160df3bbd0e5a6bce64a293ecd7ecf7c9d
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-02-13 18:51:48 -0500

    dex changes again

---
+++ models/labels/dex/labels_dex.sql
@@ -0,0 +1,36 @@
+{{ config(
+    alias = 'all',
+    materialized = 'table',
+    file_format = 'delta',
+    post_hook='{{ expose_spells(\'["ethereum", "solana", "arbitrum", "gnosis", "optimism", "bnb", "avalanche_c"]\',
+                                "sector",
+                                "labels",
+                                \'["ilemi"]\') }}')
+}}
+
+{% set dex_labels = [
+--Persona
+ ref('labels_sandwich_attackers')
+,ref('labels_dex_aggregator_traders')
+,ref('labels_arbitrage_traders')
+,ref('labels_dex_traders')
+,ref('labels_trader_platforms')
+--Usage
+,ref('labels_average_trade_values')
+,ref('labels_trader_age')
+,ref('labels_trader_dex_diversity')
+,ref('labels_trader_frequencies')
+,ref('labels_trader_portfolios')
+] %}
+
+SELECT *
+FROM (
+    {% for dex_model in dex_trade_models %}
+    SELECT
+        *
+    FROM {{ dex_model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
