ff8163c460a2da1b413336e6a5bcecafee8dd8a7
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-02-14 00:16:39 +0100

    fix partitioning

--- models/transfers/ethereum/erc721/transfers_ethereum_erc721_rolling_hour.sql
+++ models/transfers/ethereum/erc721/transfers_ethereum_erc721_rolling_hour.sql
@@ -10,5 +10,5 @@
             tokenId,
             current_timestamp() as updated_at,
             sum(amount) over (partition by wallet_address, token_address, tokenId order by hour) as amount, 
-            row_number() over (partition by token_address, tokenId order by hour desc) as recency_index
+            row_number() over (partition by wallet_address, token_address, tokenId order by hour desc) as recency_index
         from {{ ref('transfers_ethereum_erc721_agg_hour') }}
