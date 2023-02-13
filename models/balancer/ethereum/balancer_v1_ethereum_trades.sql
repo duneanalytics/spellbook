62e4ab45bae9fd8bfb9da8b8a1ac61605fa1867d
Huang Geyang <Sukebeta@outlook.com>
2023-02-13 19:01:04 +0800

    Remove post-hook in version spell

--- models/balancer/ethereum/balancer_v1_ethereum_trades.sql
+++ models/balancer/ethereum/balancer_v1_ethereum_trades.sql
@@ -5,11 +5,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address'],
-    post_hook='{{ expose_spells(\'["ethereum"]\',
-                                "project",
-                                "balancer",
-                                \'["bizzyvinci"]\') }}'
+    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address']
     )
 }}
 
