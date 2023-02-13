eb762e49483a4499039cc4ae3afa8d533ada3c53
jeff-dude <jeff@dune.xyz>
2023-02-13 16:28:34 -0500

    enhance incremental logic

--- models/nft/ethereum/nft_ethereum_wallet_pnl.sql
+++ models/nft/ethereum/nft_ethereum_wallet_pnl.sql
@@ -11,12 +11,10 @@
     )
 }}
 
-WITH 
-
-trades as (
-    SELECT 
-    -- non incremental run
-    {% if not is_incremental() %}
+WITH trades as
+(
+    --sells
+    SELECT
         seller as wallet, 
         nft_contract_address, 
         'Sells' as trade_type,
@@ -24,17 +22,25 @@ trades as (
         COUNT(*) as trades, 
         NOW() - Interval '5 Minutes' as last_updated
     FROM 
-    {{ ref('nft_trades') }}
-    WHERE currency_symbol IN ('ETH', 'WETH')
-    AND blockchain = 'ethereum'
-    AND buyer != seller 
-    AND trade_type = 'Single Item Trade'
-    AND amount_original IS NOT NULL 
-    AND block_time < NOW() - Interval '5 Minutes'
-    GROUP BY 1, 2, 3, 6
+        -- {{ ref('nft_trades') }}
+        nft.trades
+    WHERE
+        currency_symbol IN ('ETH', 'WETH')
+        AND blockchain = 'ethereum'
+        AND buyer != seller 
+        AND number_of_items = 1
+        -- AND trade_type = 'Single Item Trade' --replace with filter above on number_of_items = 1
+        AND amount_original IS NOT NULL 
+        AND block_time < NOW() - Interval '5 Minutes'
+        {% if is_incremental() %}
+        AND block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+    GROUP BY
+        1, 2, 3, 6
 
     UNION ALL 
 
+    --buys
     SELECT 
         buyer as wallet, 
         nft_contract_address, 
@@ -43,102 +49,98 @@ trades as (
         COUNT(*) as trades, 
         NOW() - Interval '5 Minutes' as last_updated
     FROM 
-    {{ ref('nft_trades') }}
-    WHERE currency_symbol IN ('ETH', 'WETH')
-    AND blockchain = 'ethereum'
-    AND buyer != seller 
-    AND trade_type = 'Single Item Trade'
-    AND amount_original IS NOT NULL 
-    AND block_time < NOW() - Interval '5 Minutes'
-    GROUP BY 1, 2, 3, 6
-    {% endif %}
-    -- incremental run 
-    {% if is_incremental() %}
-        seller as wallet, 
-        nft_contract_address, 
-        'Sells' as trade_type,
-        SUM(amount_original) as eth_amount, 
-        COUNT(*) as trades, 
-        NOW() - Interval '5 Minutes' as last_updated
-    FROM 
-    {{ ref('nft_trades') }}
-    WHERE block_time >= (SELECT MAX(last_updated) - Interval '5 Minutes' FROM {{this}})
-    AND currency_symbol IN ('ETH', 'WETH')
-    AND blockchain = 'ethereum'
-    AND buyer != seller 
-    AND trade_type = 'Single Item Trade'
-    AND amount_original IS NOT NULL 
-    AND block_time < NOW() - Interval '5 Minutes'
-    GROUP BY 1, 2, 3, 6
-
-    UNION ALL 
-
-    SELECT 
-        buyer as wallet, 
-        nft_contract_address, 
-        'Buys' as trade_type,
-        -1 * SUM(amount_original) as eth_amount,
-        COUNT(*) as trades, 
-        NOW() - Interval '5 Minutes' as last_updated
-    FROM 
-    {{ ref('nft_trades') }}
-    WHERE block_time >= (SELECT MAX(last_updated) FROM {{this}})
-    AND currency_symbol IN ('ETH', 'WETH')
-    AND blockchain = 'ethereum'
-    AND buyer != seller 
-    AND trade_type = 'Single Item Trade'
-    AND amount_original IS NOT NULL 
-    AND block_time < NOW() - Interval '5 Minutes'
-    GROUP BY 1, 2, 3, 6
-    {% endif %}
+        -- {{ ref('nft_trades') }}
+        nft.trades
+    WHERE
+        currency_symbol IN ('ETH', 'WETH')
+        AND blockchain = 'ethereum'
+        AND buyer != seller 
+        AND number_of_items = 1
+        -- AND trade_type = 'Single Item Trade' --replace with filter above on number_of_items = 1
+        AND amount_original IS NOT NULL 
+        AND block_time < NOW() - Interval '5 Minutes'
+        {% if is_incremental() %}
+        AND block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+    GROUP BY
+        1, 2, 3, 6
 ), 
-
-combined as (
-    SELECT 
-     {% if not is_incremental() %}
+combined as
+(
+    {% if not is_incremental() %}
+    SELECT
         wallet, 
         nft_contract_address, 
         last_updated,
-        COALESCE(SUM(CASE 
-            WHEN trade_type = 'Buys'
-            THEN ABS(eth_amount) 
-            ELSE 0 
-        END), 0) as eth_spent, 
-        COALESCE(SUM(CASE 
-            WHEN trade_type = 'Sells'
-            THEN eth_amount
-            ELSE 0 
-        END), 0) as eth_received, 
+        COALESCE
+        (
+            SUM
+            (
+                CASE 
+                    WHEN trade_type = 'Buys'
+                    THEN ABS(eth_amount) 
+                    ELSE 0 
+                END
+            )
+        , 0
+        ) as eth_spent, 
+        COALESCE
+        (
+            SUM
+            (
+                CASE 
+                    WHEN trade_type = 'Sells'
+                    THEN eth_amount
+                    ELSE 0 
+                END
+            )
+        , 0
+        ) as eth_received, 
         SUM(eth_amount) as pnl, 
         SUM(trades) as trades 
     FROM 
-    trades 
-    GROUP BY 1, 2, 3
-    {% endif %}
-    {% if is_incremental() %}
-    SELECT 
-        prev.wallet, 
-        prev.nft_contract_address,
+        trades 
+    GROUP BY
+        1, 2, 3
+    {% else %}
+    SELECT
+        t.wallet, 
+        t.nft_contract_address,
         t.last_updated,
-        COALESCE(SUM(CASE 
-            WHEN t.trade_type = 'Buys'
-            THEN ABS(t.eth_amount) 
-            ELSE 0 
-        END), 0) + prev.eth_spent as eth_spent,
-        COALESCE(SUM(CASE 
-            WHEN t.trade_type = 'Sells'
-            THEN t.eth_amount
-            ELSE 0 
-        END), 0) + prev.eth_received as eth_received,
-        COALESCE(SUM(t.eth_amount), 0) +  prev.pnl as pnl, 
-        COALESCE(SUM(t.trades), 0) + prev.trades as trades 
+        COALESCE
+        (
+            SUM
+            (
+                CASE 
+                    WHEN t.trade_type = 'Buys'
+                    THEN ABS(t.eth_amount) 
+                    ELSE 0 
+                END
+            )
+        , 0
+        ) + COALESCE(prev.eth_spent, 0) as eth_spent,
+        COALESCE
+        (
+            SUM
+            (
+                CASE 
+                    WHEN t.trade_type = 'Sells'
+                    THEN t.eth_amount
+                    ELSE 0 
+                END
+            )
+        , 0
+        ) + COALESCE(prev.eth_received, 0) as eth_received,
+        SUM(t.eth_amount) + COALESCE(prev.pnl, 0) as pnl, 
+        SUM(t.trades) + COALESCE(prev.trades, 0) as trades
     FROM 
-    {{this}} prev 
-    LEFT JOIN 
-    trades t 
+        trades t
+    LEFT JOIN
+        {{this}} prev
         ON prev.wallet = t.wallet 
         AND prev.nft_contract_address = t.nft_contract_address
-    GROUP BY 1, 2, 3
+    GROUP BY
+        1, 2, 3
     {% endif %}
 )
 
@@ -146,4 +148,5 @@ SELECT
     ROW_NUMBER() OVER (PARTITION BY nft_contract_address ORDER BY pnl DESC) as index, 
     * 
 FROM 
-combined
+    combined
+;
