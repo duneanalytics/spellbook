ed9464101ccf4943d667962daac5ab64297f637f
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-02-14 06:56:48 -0500

    repush

--- tests/aave/optimism/aave_optimism_interests_test.sql
+++ tests/aave/optimism/aave_optimism_interests_test.sql
@@ -5,6 +5,7 @@ with unit_test1
                end as test
         from   {{ ref('aave_v3_optimism_interest_rates' )}}
         where  reserve = '0x7f5c764cbc14f9669b88837ca1490cca17c31607'
+        
                and hour = '2022-09-11 03:00'),
     unit_test2
     as (select case when deposit_apy = 0.027884763191936474 then true
@@ -12,6 +13,7 @@ with unit_test1
                end as test
         from   {{ ref('aave_v3_optimism_interest_rates' )}}
         where  symbol = 'sUSD'
+
                and hour = '2022-08-25 09:00')
 select *
 from   (select *
