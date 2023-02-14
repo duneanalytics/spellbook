bb9552c72f3afd55afe10086053fb546b558913c
Hildebert Moulié <h.moulie@gmail.com>
2023-02-14 23:23:30 +0100

    lower letters+futureproof lower

--- models/nft/ethereum/nft_ethereum_aggregators.sql
+++ models/nft/ethereum/nft_ethereum_aggregators.sql
@@ -21,7 +21,7 @@ FROM
     , ('0x539ea5d6ec0093ff6401dbcd14d049c37a77151b', 'Gem') -- Gem's X2Y2 Batch Buys
     , ('0xf22007700b8c443bcb36a39580f7804bffdb1169', 'Gem') -- Gem's Blur Batch Buys
     , ('0x4326275317acc0fae4aa5c68fce4c54c74dc08d3', 'Gem') -- Gem v2 X2Y2
-    , ('0x29aB6d8f7e3D815168D6b40eBB12625b4fE13998', 'Gem') -- Gem v2 
+    , ('0x29ab6d8f7e3d815168d6b40ebb12625b4fe13998', 'Gem') -- Gem v2 
     , ('0x241b8e59e81455e66b9cd0e2ffb2506be1838144', 'Gem') -- Gem v2
     , ('0x56dd5bbede9bfdb10a2845c4d70d4a2950163044', 'X2Y2') -- X2Y2's OpenSea Sniper
     , ('0x69cf8871f61fb03f540bc519dd1f1d4682ea0bf6', 'Element') -- Element NFT Marketplace Aggregator
