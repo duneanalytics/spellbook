effa1363288c3dfbaaae3dae756aff5130a27b8b
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-02-14 06:35:17 -0500

    deposit apy

--- tests/aave/optimism/aave_optimism_interests_test.sql
+++ tests/aave/optimism/aave_optimism_interests_test.sql
@@ -7,7 +7,7 @@ with unit_test1
         where  reserve = '0x7f5c764cbc14f9669b88837ca1490cca17c31607'
                and hour = '2022-09-11 03:00'),
     unit_test2
-    as (select case when deposit_apy = 0.02788476319193648 then true
+    as (select case when deposit_apy = 0.027884763191936474 then true
                  else false
                end as test
         from   {{ ref('aave_v3_optimism_interest_rates' )}}
