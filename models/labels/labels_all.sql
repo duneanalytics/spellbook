46beefe448b9272f27be49e01e11f0adef5b172b
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-02-14 13:27:10 -0500

    trya gain

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -9,38 +9,38 @@
 }}
 
 -- single category labels (no subsets), needs label_type and model_name added still.
+SELECT blockchain, address, name, category, contributor, source, created_at, updated_at, model_name, label_type FROM {{ ref('labels_aztec_v2_contracts_ethereum') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_balancer_v2_pools') }}
+UNION ALL
 SELECT * FROM {{ ref('labels_cex') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_contracts') }}
 UNION ALL
 SELECT * FROM {{ ref('labels_funds') }}
 UNION ALL
-SELECT * FROM {{ ref('labels_ofac_sanctionned_ethereum') }}
-UNION ALL
 SELECT * FROM {{ ref('labels_hackers_ethereum') }}
 UNION ALL
-SELECT blockchain, address, name, category, contributor, source, created_at, updated_at, model_name, label_type FROM {{ ref('labels_aztec_v2_contracts_ethereum') }}
+SELECT * FROM {{ ref('labels_ofac_sanctionned_ethereum') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_project_wallets') }}
 UNION ALL
 SELECT * FROM {{ ref('labels_safe_ethereum') }}
 UNION ALL
 SELECT * FROM {{ ref('labels_tornado_cash') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_contracts') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_balancer_v2_pools') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_project_wallets') }}
 
--- new/standardized labels
-UNION ALL
-SELECT * FROM {{ ref('labels_bridges') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_dex') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_social') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_nft') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_airdrop') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_dao') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_infrastructure') }}
+-- -- new/standardized labels
+-- UNION ALL
+-- SELECT * FROM {{ ref('labels_bridges') }}
+-- UNION ALL
+-- SELECT * FROM {{ ref('labels_dex') }}
+-- UNION ALL
+-- SELECT * FROM {{ ref('labels_social') }}
+-- UNION ALL
+-- SELECT * FROM {{ ref('labels_nft') }}
+-- UNION ALL
+-- SELECT * FROM {{ ref('labels_airdrop') }}
+-- UNION ALL
+-- SELECT * FROM {{ ref('labels_dao') }}
+-- UNION ALL
+-- SELECT * FROM {{ ref('labels_infrastructure') }}
