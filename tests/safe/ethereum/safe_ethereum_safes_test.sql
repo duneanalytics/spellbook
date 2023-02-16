968446c6659d37603cccaca511c142ad587a2143
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2023-02-16 21:26:37 +0100

    Fix test

--- tests/safe/ethereum/safe_ethereum_safes_test.sql
+++ tests/safe/ethereum/safe_ethereum_safes_test.sql
@@ -7,7 +7,7 @@ with test_data as (
 ),
 
 test_result as (
-    select case when total = 85084 then true else false end as success
+    select case when total = 85080 then true else false end as success
     from test_data
 )
 
