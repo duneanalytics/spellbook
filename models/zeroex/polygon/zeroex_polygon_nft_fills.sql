f5ce76021a91dda44b4e904b518f550250cbbd9e
RantumBits <rantum@rantum.xyz>
2023-02-16 07:40:29 -0800

    include 2nd token addresss in native token filter

--- models/zeroex/polygon/zeroex_polygon_nft_fills.sql
+++ models/zeroex/polygon/zeroex_polygon_nft_fills.sql
@@ -29,7 +29,7 @@ WITH tbl_cte_transaction AS
          , 'erc721'         AS label
          , '1'             AS nft_cnt   
          , CASE
-                WHEN erc20Token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+                WHEN erc20Token in ('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', '0x0000000000000000000000000000000000001010') 
                 THEN '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
                 ELSE erc20Token
             END             AS price_label
@@ -57,7 +57,7 @@ WITH tbl_cte_transaction AS
             , 'erc1155'         AS label
             , erc1155FillAmount  as nft_cnt
             , CASE
-                WHEN erc20Token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+                WHEN erc20Token in ('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', '0x0000000000000000000000000000000000001010') 
                 THEN '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
                 ELSE erc20Token
                 END             AS price_label
