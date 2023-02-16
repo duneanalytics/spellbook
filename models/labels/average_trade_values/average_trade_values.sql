61fde87f57257983750385c4f74fbb1f20a64d90
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-02-16 15:45:11 -0500

    Labels relaunch 2023 (Final Review Period Until 2/16 EOD) (#2680)
    * dex changes again
    
    * nft fixes
    
    * reorg
    
    * more fixes
    
    * mooooving
    
    * 3 todos left
    
    * added reame and finished infra
    
    * singles finished
    
    * add accepted value test for categories
    
    * finished draft
    
    * remove collect_set from nft labels
    
    * move test
    
    * fix schemas
    
    * compilation fixes
    
    * remove two more arrays
    
    * add column identifiers to bottom of statics
    
    * fix?
    
    * missed one
    
    * fix comma
    
    * change modified_at to updated_at on ens model
    
    * remove some more arrays
    
    * few more updates
    
    * remove more arrays
    
    * add schema image
    
    * add schema image
    
    * link readme image
    
    * remove one last array lmao
    
    * remove again
    
    * merged in cex
    
    * trya gain
    
    * add back in comments
    
    * some fixes
    
    * fix folder name
    
    * fix folder name

--- models/labels/average_trade_values/average_trade_values.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        alias='average_trade_values',
-        post_hook='{{ expose_spells(\'["ethereum"]\', "sector", "labels", \'["gentrexha"]\') }}'
-    )
-}}
-
-SELECT * FROM {{ ref('average_trade_values_ethereum') }}
