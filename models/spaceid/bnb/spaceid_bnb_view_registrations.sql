868e48fcc511e17d1b2d28975c6184acaa8e7786
Spring Zhang <spring.okay@gmail.com>
2023-02-19 23:02:55 +0800

    Exclude duplicated records from V9 contract

--- models/spaceid/bnb/spaceid_bnb_view_registrations.sql
+++ models/spaceid/bnb/spaceid_bnb_view_registrations.sql
@@ -93,15 +93,18 @@ FROM {{source('spaceid_bnb', 'BNBRegistrarControllerV8_evt_NameRegistered')}}
 
 UNION ALL
 
+-- There are some records in v9 table are duplicated with those in v5 table. So we join to exclude them
 SELECT 'v9' as version,
-    evt_block_time as block_time,
-    name,
-    label,
-    owner,
-    cast(cost as double) as cost,
-    cast(expires as bigint) as expires,
-    contract_address,
-    evt_tx_hash as tx_hash,
-    evt_block_number as block_number,
-    evt_index
-FROM {{source('spaceid_bnb', 'BNBRegistrarControllerV9_evt_NameRegistered')}}
+    v9.evt_block_time as block_time,
+    v9.name,
+    v9.label,
+    v9.owner,
+    cast(v9.cost as double) as cost,
+    cast(v9.expires as bigint) as expires,
+    v9.contract_address,
+    v9.evt_tx_hash as tx_hash,
+    v9.evt_block_number as block_number,
+    v9.evt_index
+FROM {{source('spaceid_bnb', 'BNBRegistrarControllerV9_evt_NameRegistered')}} v9
+LEFT JOIN {{source('spaceid_bnb', 'BNBRegistrarControllerV5_evt_NameRegistered')}} v5 on v9.name = v5.name
+WHERE v5.name is null
