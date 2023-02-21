3a982ae5fc0b1bc4ccd2a09a0c4eeb884e8f29b7
jeff-dude <jeff@dune.xyz>
2023-02-20 19:03:34 -0500

    add uniswap v3

--- models/oneinch/ethereum/oneinch_ethereum_trades.sql
+++ models/oneinch/ethereum/oneinch_ethereum_trades.sql
@@ -15,6 +15,7 @@ ref('oneinch_v1_ethereum_trades')
 ,ref('oneinch_unoswap_v3_ethereum_trades')
 ,ref('oneinch_unoswap_v4_ethereum_trades')
 ,ref('oneinch_unoswap_v5_ethereum_trades')
+,ref('oneinch_uniswap_v3_ethereum_trades')
 ] %}
 
 
