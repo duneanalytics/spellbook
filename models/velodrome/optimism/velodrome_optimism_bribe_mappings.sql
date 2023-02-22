b0a5136a97fed65069fe14dfb34cdce176798ef3
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-02-21 22:09:59 -0500

    push

--- models/velodrome/optimism/velodrome_optimism_bribe_mappings.sql
+++ models/velodrome/optimism/velodrome_optimism_bribe_mappings.sql
@@ -1,7 +1,21 @@
+{{
+    config(
+        schema = 'velodrome_optimism',
+        alias='bribe_mappings',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['pool', 'gauge'],
+        post_hook='{{ expose_spells(\'["optimism"]\',
+                                    "project",
+                                    "velodrome",
+                                    \'["msilb7"]\') }}'
+    ) 
+}}
 
 SELECT
  cg._pool AS pool
-, existing_bribe AS incentives_contract
+, ccb.output_0 AS incentives_contract
 , 'external bribe' as incentives_type
 , evt_block_time
 , evt_block_number
@@ -10,9 +24,25 @@ SELECT
 , evt_index
 
 FROM {{ source('velodrome_optimism','WrappedExternalBribeFactory_call_createBribe') }} ccb
-    LEFT JOIN {{ source('velodrome_optimism','BribeFactory_call_createExternalBribe') }} ceb
-        ON ceb.output_0 = ccb.existing_bribe
-    LEFT JOIN {{ source('velodrome_optimism', 'GaugeFactory_call_createGauge') }} cg
+    INNER JOIN {{ source('velodrome_optimism', 'GaugeFactory_call_createGauge') }} cg
         ON cg._external_bribe = ccb.existing_bribe
+
+WHERE ccb.call_success = true
+
+UNION ALL
+
+SELECT
+ cg._pool AS pool
+, cib.existing_bribe AS incentives_contract
+, 'internal bribe' as incentives_type
+, evt_block_time
+, evt_block_number
+, contract_address,
+, evt_tx_hash
+, evt_index
+
+FROM {{ source('velodrome_optimism','BribeFactory_call_createInternalBribe') }} cib
+    LEFT JOIN {{ source('velodrome_optimism', 'GaugeFactory_call_createGauge') }} cg
+        ON cg._internal_bribe = cib.existing_bribe
 
 WHERE ccb.call_success = true
