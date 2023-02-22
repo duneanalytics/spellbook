aad1bb008ab14840dfa332e9d8a124ddc24831ee
jeff-dude <jeff@dune.xyz>
2023-02-22 12:03:11 -0500

    remove final group by

--- models/nft/fantom/nft_fantom_transfers.sql
+++ models/nft/fantom/nft_fantom_transfers.sql
@@ -111,5 +111,4 @@ INNER JOIN {{ source('fantom', 'transactions') }} ft
     AND ft.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
 WHERE ids_and_count.values > 0
-GROUP BY blockchain, t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, t.evt_index, token_id, amount
 ;
