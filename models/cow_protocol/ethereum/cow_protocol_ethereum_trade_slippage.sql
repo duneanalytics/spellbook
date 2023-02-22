405e30244b78d4c2297f076ff3d546abfc0ecb98
Gent Rexha <gent@cow.fi>
2023-02-22 16:47:57 +0100

    Review feedback

--- models/cow_protocol/ethereum/cow_protocol_ethereum_trade_slippage.sql
+++ models/cow_protocol/ethereum/cow_protocol_ethereum_trade_slippage.sql
@@ -17,7 +17,7 @@ raw_data as (
     CASE when (flags % 2) = 1 THEN (limit_sell_amount / (1.0 + (slippage_bips / 10000.0))) else (limit_sell_amount) end as sell_quote,
     atoms_sold,
     usd_value as trade_usd_value,
-    slippage_bips as slippage_tolerance_bips
+    slippage_bips as tolerance_bips
 from {{ref('cow_protocol_ethereum_app_data')}} as ad
 inner join {{ ref('cow_protocol_ethereum_trades') }} as t on t.app_data = ad.app_hash
 left join (select * from {{ ref('tokens_erc20') }} where blockchain = 'ethereum') A ON A.contract_address=t.buy_token_address
@@ -26,21 +26,21 @@ where slippage_bips is not null
 ),
 
 results as (
-    select order_uid, block_number, block_time, buy_quote, sell_quote, slippage_tolerance_bips, trade_usd_value,
+    select order_uid, block_number, block_time, buy_quote, sell_quote, tolerance_bips, trade_usd_value,
         CASE
             WHEN order_type = 'SELL' THEN (atoms_bought - buy_quote)
             ELSE (sell_quote - atoms_sold)
-        END AS slippage_atoms,
+        END AS amount_atoms,
     100.0 * (CASE
         WHEN order_type = 'SELL'
             THEN ((atoms_bought - buy_quote) / buy_quote)
             ELSE ((sell_quote - atoms_sold) / sell_quote)
-    END) AS slippage_percentage,
+    END) AS amount_percentage,
     CASE
         WHEN order_type = 'SELL'
         THEN (atoms_bought - buy_quote) * (usd_value / atoms_bought)
         ELSE (sell_quote - atoms_sold) * (usd_value / atoms_sold)
-      END AS slippage_usd
+      END AS amount_usd
     from raw_data
 )
 select * from results
