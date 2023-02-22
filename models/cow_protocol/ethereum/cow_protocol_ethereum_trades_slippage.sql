986a5c366ff4acdf2aeb3597fa31cb63f26ec4a6
Gent Rexha <gent@cow.fi>
2023-02-22 14:33:16 +0100

    Further qol updates

--- models/cow_protocol/ethereum/cow_protocol_ethereum_trades_slippage.sql
+++ models/cow_protocol/ethereum/cow_protocol_ethereum_trades_slippage.sql
@@ -1,4 +1,4 @@
-{{ config(alias='trade_slippage',
+{{ config(alias='trades_slippage',
         post_hook='{{ expose_spells_hide_trino(\'["ethereum"]\',
                                     "project",
                                     "cow_protocol",
@@ -8,26 +8,16 @@
 with 
 raw_data as (
     select
+    order_uid
     block_number,
     block_time,
     case when (flags % 2) = 0 then 'SELL' else 'BUY' end  as order_type,
-    limit_buy_amount,
     CASE when (flags % 2) = 0 THEN (limit_buy_amount / (1.0 - (slippage_bips / 10000.0))) else (limit_buy_amount) end as buy_quote,
     atoms_bought,
-    atoms_bought / pow(10, A.decimals) as units_bought,
-    buy_price,
-    A.decimals as buy_decimals,
-    limit_sell_amount,
     CASE when (flags % 2) = 1 THEN (limit_sell_amount / (1.0 + (slippage_bips / 10000.0))) else (limit_sell_amount) end as sell_quote,
     atoms_sold,
-    atoms_sold / pow(10, B.decimals) as units_sold,
-    sell_price,
-    B.decimals as sell_decimals,
-    usd_value,
-    slippage_bips as slippage_tolerance_bips,
-    (1.0 - (slippage_bips / 10000.0)) as buy_tolerance,
-    (1.0 + (slippage_bips / 10000.0)) as sell_tolerance,
-    order_uid
+    usd_value as trade_usd_value,
+    slippage_bips as slippage_tolerance_bips
 from {{ref('cow_protocol_ethereum_app_data')}} as ad
 inner join {{ ref('cow_protocol_ethereum_trades') }} as t on t.app_data = ad.app_hash
 left join (select * from {{ ref('tokens_erc20') }} where blockchain = 'ethereum') A ON A.contract_address=t.buy_token_address
@@ -36,14 +26,14 @@ where slippage_bips is not null
 ),
 
 results as (
-    select order_uid, block_number, block_time, buy_quote, sell_quote, slippage_tolerance_bips, usd_value,
+    select order_uid, block_number, block_time, buy_quote, sell_quote, slippage_tolerance_bips, trade_usd_value,
         CASE
             WHEN order_type = 'SELL' THEN (atoms_bought - buy_quote)
             ELSE (sell_quote - atoms_sold)
         END AS slippage_atoms,
     100.0 * (CASE
         WHEN order_type = 'SELL'
-            THEN (((atoms_bought - buy_quote) / buy_quote))
+            THEN ((atoms_bought - buy_quote) / buy_quote)
             ELSE ((sell_quote - atoms_sold) / sell_quote)
     END) AS slippage_percentage,
     CASE
