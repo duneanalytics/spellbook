b56761625ad683960aa53b53e0f95e8c50948d0f
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-02-22 12:21:45 -0500

    Make OP Aave Tables Incremental (#2626)
    * make aave incremental
    
    * Add uniqueness test
    
    * Add uniqueness test
    
    * Update aave_optimism_interests_test
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

--- models/aave/optimism/aave_v3_optimism_borrow.sql
+++ models/aave/optimism/aave_v3_optimism_borrow.sql
@@ -1,5 +1,9 @@
 {{ config(
     schema = 'aave_v3_optimism'
+    , materialized = 'incremental'
+    , file_format = 'delta'
+    , incremental_strategy = 'merge'
+    , unique_key = ['version', 'token_address', 'evt_tx_hash', 'evt_block_number', 'evt_index']
     , alias='borrow'
     , post_hook='{{ expose_spells(\'["optimism"]\',
                                   "project",
@@ -41,6 +45,9 @@ SELECT
     evt_block_time,
     evt_block_number
 FROM {{ source('aave_v3_optimism','Pool_evt_Borrow') }} 
+{% if is_incremental() %}
+    WHERE evt_block_time >= date_trunc('day', now() - interval '1 week')
+{% endif %}
 UNION ALL 
 SELECT 
     '3' AS version,
@@ -56,6 +63,9 @@ SELECT
     evt_block_time,
     evt_block_number
 FROM {{ source('aave_v3_optimism','Pool_evt_Repay') }}
+{% if is_incremental() %}
+    WHERE evt_block_time >= date_trunc('day', now() - interval '1 week')
+{% endif %}
 UNION ALL
 SELECT 
     '3' AS version,
@@ -71,10 +81,17 @@ SELECT
     evt_block_time,
     evt_block_number
 FROM {{ source('aave_v3_optimism','Pool_evt_LiquidationCall') }}
+{% if is_incremental() %}
+    WHERE evt_block_time >= date_trunc('day', now() - interval '1 week')
+{% endif %}
 ) borrow
 LEFT JOIN {{ ref('tokens_optimism_erc20') }} erc20
     ON borrow.token = erc20.contract_address
 LEFT JOIN {{ source('prices','usd') }} p 
     ON p.minute = date_trunc('minute', borrow.evt_block_time) 
     AND p.symbol = erc20.symbol 
-    AND p.blockchain = 'ethereum' -- Using ETH tokens for USD prices as price data is not available for OP tokens
+    AND p.contract_address = borrow.token
+    AND p.blockchain = 'optimism'
+    {% if is_incremental() %}
+    AND p.minute >= date_trunc('day', now() - interval '1 week')
+    {% endif %}
