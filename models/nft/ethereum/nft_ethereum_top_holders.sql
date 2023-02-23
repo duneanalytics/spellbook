77015a260362f003f24cba0732a8bdb3d15e72c6
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-02-23 23:54:15 +0100

    smol fix

--- models/nft/ethereum/nft_ethereum_top_holders.sql
+++ models/nft/ethereum/nft_ethereum_top_holders.sql
@@ -13,7 +13,7 @@ erc1155_balances as (
     SELECT 
         wallet_address,
         token_address as nft_contract_address,
-        SUM(amount) as balance 
+        COUNT(tokenId) as balance 
     FROM 
     {{ ref('balances_ethereum_erc1155_latest') }}
     GROUP BY 1, 2
