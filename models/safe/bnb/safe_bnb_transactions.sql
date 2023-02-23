1d1c64b2b6f688c1e468cae2799402e5fab75675
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2023-02-23 21:29:00 +0100

    fix

--- models/safe/bnb/safe_bnb_transactions.sql
+++ models/safe/bnb/safe_bnb_transactions.sql
@@ -1,10 +1,9 @@
 {{ 
     config(
         materialized='incremental',
-        alias='safe_transactions_bnb',
+        alias='safe_bnb_transactions',
         partition_by = ['block_date'],
         unique_key = ['block_date', 'tx_hash', 'trace_address'], 
-        on_schema_change='full_refresh',
         file_format ='delta',
         incremental_strategy='merge',
         post_hook='{{ expose_spells(\'["bnb"]\',
