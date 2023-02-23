4d9eb57f2416f851e42ab36b7eb969fafb6e16e9
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2023-02-23 21:31:26 +0100

    fix

--- models/safe/optimism/safe_optimism_transactions.sql
+++ models/safe/optimism/safe_optimism_transactions.sql
@@ -1,10 +1,9 @@
 {{ 
     config(
         materialized='incremental',
-        alias='safe_transactions_optimism',
+        alias='safe_optimism_transactions',
         partition_by = ['block_date'],
         unique_key = ['block_date', 'tx_hash', 'trace_address'], 
-        on_schema_change='fail',
         file_format ='delta',
         incremental_strategy='merge',
         post_hook='{{ expose_spells(\'["optimism"]\',
