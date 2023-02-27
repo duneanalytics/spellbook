96df438651200ed3e1b753d933c05203a3ad081a
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-02-26 23:02:44 -0500

    fix field

--- models/tokens/optimism/tokens_optimism_erc20_bridged_mapping.sql
+++ models/tokens/optimism/tokens_optimism_erc20_bridged_mapping.sql
@@ -41,7 +41,7 @@ FROM (
         UNION ALL
         SELECT l1_token, l2_token, symbol, decimals FROM {{ ref('ovm_optimism_l2_token_factory') }} 
         {% if is_incremental() %}
-        WHERE evt_block_time >= date_trunc("day", now() - interval '1 week')
+        WHERE call_block_time >= date_trunc("day", now() - interval '1 week')
         {% endif %}   
         GROUP BY 1,2,3,4
 
