f8f8c96ec29751b1d6d560c9334a867f53d0c6ee
Gent Rexha <gent@cow.fi>
2023-02-27 18:52:48 +0100

    [tx_hash label] [Easy] Add early investment tx_hash label (#2807)
    * Start of early investment tx_hash label
    
    * Added labels to all tx_hash labels
    
    * Implemented review feedback
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/cow_protocol/tx_hash_labels/cow_protocol_tx_hash_labels_all.sql
+++ models/cow_protocol/tx_hash_labels/cow_protocol_tx_hash_labels_all.sql
@@ -11,6 +11,8 @@
 -- Query Labels
 SELECT * FROM {{ ref('cow_protocol_tx_hash_labels_stable_to_stable') }}
 UNION ALL
+SELECT * FROM {{ ref('cow_protocol_tx_hash_labels_early_investment') }}
+UNION ALL
 SELECT * FROM {{ ref('cow_protocol_tx_hash_labels_staking_token_investment') }}
 UNION ALL
 SELECT * FROM {{ ref('cow_protocol_tx_hash_labels_harvest_yield') }}
