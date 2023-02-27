3896f0e3e73b735e0f6544d6abff1cf2b5c8a632
Spring Zhang <spring.okay@gmail.com>
2023-02-27 15:24:21 +0800

    Added mints file

--- models/fractal/polygon/fractal_polygon_events.sql
+++ models/fractal/polygon/fractal_polygon_events.sql
@@ -16,7 +16,44 @@
 -- Initial date of ListingAdded
 {% set nft_start_date = "2022-12-30" %}
 
-WITH listing_detail AS (
+WITH contract_list as (
+    SELECT distinct erc721TokenAddress as nft_contract_address
+    FROM aavegotchi_polygon.aavegotchi_diamond_evt_ERC721ExecutedListing
+    UNION ALL
+    SELECT distinct erc1155TokenAddress as nft_contract_address
+    FROM aavegotchi_polygon.aavegotchi_diamond_evt_ERC1155ExecutedListing
+),
+
+mints as (
+    SELECT 'mint' AS trade_category,
+        block_time AS evt_block_time,
+        block_number AS evt_block_number,
+        tx_hash AS evt_tx_hash,
+        CAST(NULL AS string) AS contract_address, -- We leave it NULL here and will get its value below by join from transactions table
+        evt_index,
+        'Mint' AS evt_type,
+        `to` AS buyer,
+        CAST(NULL AS string) AS seller,
+        contract_address AS nft_contract_address,
+        token_id,
+        amount AS number_of_items,
+        token_standard,
+        '0x385eeac5cb85a38a9a07a70c73e0a3271cfb54a7' AS currency_contract, -- All sale are in GHST
+        CAST(0 as DECIMAL(38,0)) AS amount_raw, -- It's hard to get the mint price. So handle it similar as in nftb_bnb_events
+        CAST(NULL AS string) AS category,
+        CAST(NULL AS string) AS executed_time
+    FROM {{ ref('nft_polygon_transfers') }}
+    WHERE contract_address IN ( SELECT nft_contract_address FROM contract_list )
+        AND `from` = '0x0000000000000000000000000000000000000000'   -- mint
+        {% if is_incremental() %}
+        AND block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+        {% if not is_incremental() %}
+        AND block_time >= '{{nft_start_date}}'
+        {% endif %}
+),
+
+listing_detail AS (
     SELECT assetContract AS nft_contract_address,
         contract_address,
         evt_block_number,
@@ -38,7 +75,7 @@ WITH listing_detail AS (
     FROM {{ source ('fractal_polygon', 'Marketplace_evt_ListingAdded') }}
 ),
 
-nft_order AS (
+trades AS (
     SELECT 'buy' AS trade_category,
         s.evt_block_time,
         s.evt_block_number,
@@ -48,7 +85,7 @@ nft_order AS (
         s.buyer,
         s.lister AS seller,
         s.assetContract AS nft_contract_address,
-        '' AS collection,
+        CAST(NULL AS string) AS collection,
         l.tokenId AS token_id,
         s.quantityBought AS number_of_items,
         'erc721' AS token_standard, -- 0x73ad2146: erc721; 0x973bb640: erc1155
@@ -58,6 +95,12 @@ nft_order AS (
     INNER JOIN listing_detail l ON s.listingId = l.listingId
     WHERE 1 = 1
         AND s.evt_block_time >= '{{nft_start_date}}'
+),
+
+all_events as (
+    SELECT * FROM mints
+    UNION ALL
+    SELECT * FROM trades
 ),
 
 price_list AS (
@@ -76,10 +119,10 @@ SELECT
     'polygon' AS blockchain,
     'fractal' AS project,
     'v1' AS version,
-    o.evt_tx_hash AS tx_hash,
-    date_trunc('day', o.evt_block_time) AS block_date,
-    o.evt_block_time AS block_time,
-    o.evt_block_number AS block_number,
+    a.evt_tx_hash AS tx_hash,
+    date_trunc('day', a.evt_block_time) AS block_date,
+    a.evt_block_time AS block_time,
+    a.evt_block_number AS block_number,
     amount_raw / power(10, p.decimals) * p.price AS amount_usd,
     amount_raw / power(10, p.decimals) AS amount_original,
     amount_raw,
@@ -87,17 +130,17 @@ SELECT
     p.contract_address AS currency_contract,
     token_id,
     token_standard,
-    o.contract_address AS project_contract_address,
+    a.contract_address AS project_contract_address,
     'Trade' AS evt_type,
-    NULL::string AS collection,
+    CAST(NULL AS string) AS collection,
     CASE WHEN number_of_items = 1 THEN 'Single Item Trade' ELSE 'Bundle Trade' END AS trade_type,
     number_of_items,
-    o.trade_category,
-    o.buyer,
-    o.seller,
-    o.nft_contract_address,
-    NULL::string AS aggregator_name,
-    NULL::string AS aggregator_address,
+    a.trade_category,
+    a.buyer,
+    a.seller,
+    a.nft_contract_address,
+    agg.name AS aggregator_name,
+    agg.contract_address AS aggregator_address,
     t.`from` AS tx_from,
     t.`to` AS tx_to,
     2 * amount_raw / 100 AS platform_fee_amount_raw,
@@ -107,13 +150,14 @@ SELECT
     0 royalty_fee_amount,
     0 AS royalty_fee_amount_usd,
     0 AS royalty_fee_percentage,
-    NULL::double AS royalty_fee_receive_address,
-    NULL::string AS royalty_fee_currency_symbol,
+    CAST(NULL AS double) AS royalty_fee_receive_address,
+    CAST(NULL AS string) AS royalty_fee_currency_symbol,
     evt_tx_hash || '-' || evt_index || '-' || token_id  AS unique_trade_id
-FROM nft_order o
-INNER JOIN {{ source('polygon','transactions') }} t ON o.evt_block_number = t.block_number
-    AND o.evt_tx_hash = t.hash
+FROM all_events a
+INNER JOIN {{ source('polygon','transactions') }} t ON a.evt_block_number = t.block_number
+    AND a.evt_tx_hash = t.hash
     AND t.block_time >= '{{nft_start_date}}'
-LEFT JOIN price_list p ON p.contract_address = o.currency_contract AND p.minute = date_trunc('minute', o.evt_block_time)
+LEFT JOIN price_list p ON p.contract_address = a.currency_contract AND p.minute = date_trunc('minute', a.evt_block_time)
+LEFT JOIN {{ ref('nft_aggregators') }} agg ON agg.blockchain = 'polygon' AND agg.contract_address = t.`to`
 WHERE 1 = 1
-    AND o.evt_block_time >= '{{nft_start_date}}'
+    AND a.evt_block_time >= '{{nft_start_date}}'
---
+++ models/fractal/polygon/fractal_polygon_fees.sql
@@ -0,0 +1,43 @@
+ {{
+  config(
+        alias='fees',
+        post_hook='{{ expose_spells(\'["polygon"]\',
+                                    "project",
+                                    "fractal",
+                                    \'["springzh"]\') }}')
+}}
+
+SELECT blockchain,
+project,
+version,
+block_time,
+token_id,
+collection,
+CAST(platform_fee_amount_raw AS DOUBLE) AS platform_fee_amount_raw,
+platform_fee_amount,
+platform_fee_amount_usd,
+CAST(platform_fee_percentage AS DOUBLE) AS platform_fee_percentage,
+CAST(royalty_fee_amount_raw AS DOUBLE) AS royalty_fee_amount_raw,
+royalty_fee_amount,
+royalty_fee_amount_usd,
+CAST(royalty_fee_percentage AS DOUBLE) AS royalty_fee_percentage,
+royalty_fee_receive_address,
+royalty_fee_currency_symbol,
+token_standard,
+trade_type,
+CAST(number_of_items AS DECIMAL(38,0)) AS number_of_items,
+trade_category,
+evt_type,
+seller,
+buyer,
+nft_contract_address,
+project_contract_address,
+aggregator_name,
+aggregator_address,
+block_number,
+tx_hash,
+tx_from,
+tx_to,
+unique_trade_id
+FROM {{ ref('fractal_polygon_events') }}
+WHERE evt_type = 'Trade' -- There is no platform_fee_amount_raw for Mint type rows
