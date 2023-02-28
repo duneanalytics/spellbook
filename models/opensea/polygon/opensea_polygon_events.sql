59e09f5b695f436ead603c229f9e6724c87d547a
Spring Zhang <spring.okay@gmail.com>
2023-02-28 22:35:41 +0800

    Update opense polygon nft events

--- models/opensea/polygon/opensea_polygon_events.sql
+++ models/opensea/polygon/opensea_polygon_events.sql
@@ -13,95 +13,139 @@
     )
 }}
 
-{% set START_DATE='2021-06-28' %}
-{% set END_DATE='2022-10-07' %}
+{% set nft_start_date='2021-06-28' %}
 
-WITH wyvern_call_data as (
+WITH contract_list AS (
+   SELECT DISTINCT '0x' || right(substring(rightOrder:makerAssetData, 11, 64), 40) AS nft_contract_address
+   FROM  {{ source('opensea_polygon_v2_polygon','ZeroExFeeWrapper_call_matchOrders') }}
+),
+
+mints AS (
+    SELECT 'mint' AS trade_category,
+        t.block_time,
+        t.block_number,
+        t.tx_hash,
+        CAST(NULL AS string) AS contract_address,
+        t.evt_index,
+        'Mint' AS evt_type,
+        t.`to` AS buyer,
+        CAST(NULL AS string) AS seller,
+        t.contract_address AS nft_contract_address,
+        t.token_id,
+        t.amount AS number_of_items,
+        t.token_standard,
+        '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270' AS currency_contract,
+        CAST(0 AS DECIMAL(38,0)) AS amount_raw,
+        CAST(0 AS double) AS platform_fee,
+        CAST(NULL AS string) AS fee_recipient,
+        CAST(0 AS double) AS royalty_fee
+    FROM {{ ref('nft_polygon_transfers') }} t
+    INNER JOIN contract_list c ON t.contract_address = c.nft_contract_address
+        AND t.`from` = '0x0000000000000000000000000000000000000000'   -- mint
+        {% if not is_incremental() %}
+        AND t.block_time >= '{{nft_start_date}}'
+        {% endif %}
+        {% if is_incremental() %}
+        AND t.block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+),
+
+trades AS (
    select 
-      call_tx_hash as tx_hash,
-      call_block_time as block_time,
-      call_block_number as block_number,
-      a.contract_address as project_contract_address,
-      a.leftOrder:makerAddress buyer,
-      a.rightOrder:makerAddress as seller,
-      'Sell' as trade_category, --TODO
-      'Fixed price'as sale_type,
-      paymentTokenAddress as currency_contract,
-      feeData[0]:recipient as fee_recipient,
-      'Trade' as evt_type,
-      0.025 as platform_fee,
-      case when length(feeData[1]:recipient) > 0 then 0.025 else 0 end as royalty_fee,
-      'Single Item Trade' as trade_type,
-      '0x' || right(substring(a.rightOrder:makerAssetData, 11, 64), 40) as nft_contract_address,
+      'buy' AS trade_category,
+      call_block_time AS block_time,
+      call_block_number AS block_number,
+      call_tx_hash AS tx_hash,
+      a.contract_address,
+      CAST(0 as integer) AS evt_index,
+      'Trade' AS evt_type,
+      a.leftOrder:makerAddress AS buyer,
+      a.rightOrder:makerAddress AS seller,
+      '0x' || right(substring(a.rightOrder:makerAssetData, 11, 64), 40) AS nft_contract_address,
       case when length(a.rightOrder:makerAssetData) = 650 then bytea2numeric_v3(substr(a.rightOrder:makerAssetData,332,64))::string
             else bytea2numeric_v3(substr(a.rightOrder:makerAssetData,76,64))::string
-       end as token_id,
+       end AS token_id,
+      least((output_matchedFillResults:left:takerFeePaid)::numeric, (output_matchedFillResults:right:makerFeePaid)::numeric) AS number_of_items,
       case when length(a.rightOrder:makerAssetData) = 650 then 'erc1155'
             else 'erc721' -- 138
-       end as token_standard, 
-      least((output_matchedFillResults:left:takerFeePaid)::numeric, (output_matchedFillResults:right:makerFeePaid)::numeric) as number_of_items,
-      least((output_matchedFillResults:left:makerFeePaid)::decimal(38,0), (output_matchedFillResults:right:takerFeePaid)::decimal(38,0)) as amount_raw,
-      (feeData[0]:paymentTokenAmount)::decimal(38,0) as fee_amount_raw,
-      feeData[1]:recipient as royalty_receive_address,
-      (feeData[1]:paymentTokenAmount)::decimal(38,0) as royalty_amount_raw,
-      a.contract_address as exchange_contract_address
+       end AS token_standard, 
+      paymentTokenAddress AS currency_contract,
+      least((output_matchedFillResults:left:makerFeePaid)::decimal(38,0), (output_matchedFillResults:right:takerFeePaid)::decimal(38,0)) AS amount_raw,
+      2.5 AS platform_fee,
+      feeData[0]:recipient AS fee_recipient,
+      case when length(feeData[1]:recipient) > 0 then 2.5 else 0 end AS royalty_fee
    from {{ source('opensea_polygon_v2_polygon','ZeroExFeeWrapper_call_matchOrders') }} a
    where 1=1
      and call_success
-     and a.call_block_time >= '{{START_DATE}}'
-     and a.call_block_time <= '{{END_DATE}}'
+    {% if not is_incremental() %}
+    AND a.call_block_time >= '{{nft_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+    AND a.call_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+),
+
+all_events AS (
+    SELECT * FROM mints
+    UNION ALL
+    SELECT * FROM trades
 )
 
 SELECT
-  'polygon' as blockchain,
-  'opensea' as project,
-  'v2' as version,
-  project_contract_address,
-  TRY_CAST(date_trunc('DAY', t.block_time) AS date) AS block_date,
-  t.block_time,
-  t.block_number,
-  t.tx_hash,
-  t.nft_contract_address,
-  t.token_standard,
-  CAST(NULL AS string) AS collection, -- Currently there is no data for Polygon
-  t.token_id,
-  t.amount_raw,
-  t.amount_raw / power(10,erc20.decimals) as amount_original,
-  t.amount_raw / power(10,erc20.decimals) * p.price AS amount_usd,
-  t.trade_category,
-  t.trade_type,
-  t.number_of_items,
-  t.seller,
-  t.buyer,
-  t.evt_type,
+  'polygon' AS blockchain,
+  'opensea' AS project,
+  'v2' AS version,
+  a.tx_hash,
+  date_trunc('day', a.block_time) AS block_date,
+  a.block_time,
+  a.block_number,
+  a.amount_raw / power(10,erc20.decimals) * p.price AS amount_usd,
+  a.amount_raw / power(10,erc20.decimals) AS amount_original,
+  a.amount_raw,
   erc20.symbol AS currency_symbol,
-  t.currency_contract,
-  agg.name as aggregator_name,
-  agg.contract_address as aggregator_address,
-  tx.from as tx_from,
-  tx.to as tx_to,
-  CAST(NULL AS VARCHAR(5)) as aggregator_name,
-  CAST(NULL AS VARCHAR(5)) as aggregator_address,
-  CAST(NULL AS VARCHAR(5)) as tx_from,
-  CAST(NULL AS VARCHAR(5)) as tx_to,
-  CAST(round((100 * platform_fee),4) AS DOUBLE) AS platform_fee_percentage,
-  platform_fee * t.amount_raw AS platform_fee_amount_raw,
-  platform_fee * t.amount_raw / power(10,erc20.decimals) AS platform_fee_amount,
-  platform_fee * t.amount_raw / power(10,erc20.decimals) * p.price AS platform_fee_amount_usd,
-  CAST(round((100 * royalty_fee),4) AS DOUBLE) as royalty_fee_percentage,
-  royalty_fee * t.amount_raw AS royalty_fee_amount_raw,
-  royalty_fee * t.amount_raw / power(10,erc20.decimals) AS royalty_fee_amount,
-  royalty_fee * t.amount_raw / power(10,erc20.decimals) * p.price AS royalty_fee_amount_usd,
-  t.fee_recipient as royalty_fee_receive_address,
+  a.currency_contract,
+  a.token_id,
+  a.token_standard,
+  coalesce(a.contract_address, t.`to`) AS project_contract_address,
+  a.evt_type,
+  CAST(NULL AS string) AS collection,
+  CASE WHEN a.number_of_items = 1 THEN 'Single Item Trade' ELSE 'Bundle Trade' END AS trade_type,
+  a.number_of_items,
+  a.trade_category,
+  a.buyer,
+  a.seller,
+  a.nft_contract_address,
+  agg.name AS aggregator_name,
+  agg.contract_address AS aggregator_address,
+  t.`from` AS tx_from,
+  t.`to` AS tx_to,
+  platform_fee * a.amount_raw / 100 AS platform_fee_amount_raw,
+  platform_fee * a.amount_raw / power(10,erc20.decimals) / 100 AS platform_fee_amount,
+  platform_fee * a.amount_raw / power(10,erc20.decimals) * p.price / 100 AS platform_fee_amount_usd,
+  CAST(platform_fee AS double) AS platform_fee_percentage,
+  royalty_fee * a.amount_raw AS royalty_fee_amount_raw,
+  royalty_fee * a.amount_raw / power(10,erc20.decimals) / 100 AS royalty_fee_amount,
+  royalty_fee * a.amount_raw / power(10,erc20.decimals) * p.price / 100 AS royalty_fee_amount_usd,
+  CAST(royalty_fee AS double) AS royalty_fee_percentage,
+  a.fee_recipient AS royalty_fee_receive_address,
   erc20.symbol AS royalty_fee_currency_symbol,
-  'wyvern-opensea' || '-' || t.tx_hash || '-' || token_id as unique_trade_id
-FROM wyvern_call_data t
-INNER JOIN {{ source('polygon','transactions') }} tx ON t.block_number = tx.block_number AND t.tx_hash = tx.hash
-    AND tx.block_time >= '{{START_DATE}}' AND tx.block_time <= '{{END_DATE}}'
-LEFT JOIN {{ ref('nft_aggregators') }} agg ON agg.contract_address = tx.to AND agg.blockchain = 'polygon'
-LEFT JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', t.block_time)
-    AND p.contract_address = t.currency_contract
+  a.tx_hash|| '-' || a.evt_type  || '-' || a.evt_index || '-' || a.token_id  AS unique_trade_id
+FROM all_events a
+INNER JOIN {{ source('polygon','transactions') }} t ON a.block_number = t.block_number AND a.tx_hash = t.hash
+    {% if not is_incremental() %}
+    AND t.block_time >= '{{nft_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+    AND t.block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+LEFT JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', a.block_time)
+    AND p.contract_address = a.currency_contract
     AND p.blockchain ='polygon'
-    AND minute >= '{{START_DATE}}' AND minute <= '{{END_DATE}}'
-LEFT JOIN {{ ref('tokens_erc20') }} erc20 ON erc20.contract_address = t.currency_contract and erc20.blockchain = 'polygon'
-;
+    {% if not is_incremental() %}
+    AND minute >= '{{nft_start_date}}' 
+    {% endif %}
+    {% if is_incremental() %}
+    AND minute >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+LEFT JOIN {{ ref('tokens_erc20') }} erc20 ON erc20.contract_address = a.currency_contract and erc20.blockchain = 'polygon'
+LEFT JOIN {{ ref('nft_aggregators') }} agg ON agg.blockchain = 'polygon' AND agg.contract_address = t.`to`
