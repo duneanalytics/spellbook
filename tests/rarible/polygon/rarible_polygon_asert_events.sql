0ebb6404b370c38dd8ae552e969e9d1927c0b671
Spring Zhang <spring.okay@gmail.com>
2023-02-28 18:06:19 +0800

    update test date

--- tests/rarible/polygon/rarible_polygon_asert_events.sql
+++ tests/rarible/polygon/rarible_polygon_asert_events.sql
@@ -6,8 +6,8 @@ WITH raw_events AS (
         evt_tx_hash || '-Trade-' || evt_index || '-' || CAST(bytea2numeric_v3(substr(leftAsset:data, 3 + 64, 64)) AS string)  AS raw_unique_trade_id
     FROM {{ source ('rarible_polygon', 'Exchange_evt_Match') }}
     WHERE rightAsset:assetClass in ('0xaaaebeba', '0x8ae85d84')
-        AND evt_block_time >= '2023-01-01'
-        AND evt_block_time < '2023-02-01'
+        AND evt_block_time >= '2022-04-01'
+        AND evt_block_time < '2022-05-01'
 ),
 
 processed_events AS (
@@ -15,8 +15,8 @@ processed_events AS (
         tx_hash,
         unique_trade_id
     FROM {{ ref('rarible_polygon_events') }}
-    WHERE block_time >= '2023-01-01'
-        AND block_time < '2023-02-01'
+    WHERE block_time >= '2022-04-01'
+        AND block_time < '2022-05-01'
         AND evt_type = 'Trade'
 )
 
