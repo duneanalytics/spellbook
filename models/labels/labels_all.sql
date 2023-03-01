5e7ac3a81e48ef95600ea6acf61469dfacfa7472
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-03-01 12:19:19 -0500

    forcing full rerun

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -8,25 +8,7 @@
                                 \'["soispoke","hildobby","ilemi"]\') }}')
 }}
 
--- single category labels (no subsets), needs label_type and model_name added still.
-SELECT blockchain, address, name, category, contributor, source, created_at, updated_at, model_name, label_type FROM {{ ref('labels_aztec_v2_contracts_ethereum') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_balancer_v2_pools') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_contracts') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_hackers_ethereum') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_ofac_sanctionned_ethereum') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_project_wallets') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_safe_ethereum') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_tornado_cash') }}
-
 -- new/standardized labels
-UNION ALL
 SELECT * FROM {{ ref('labels_bridges') }}
 UNION ALL
 SELECT * FROM {{ ref('labels_dex') }}
@@ -42,3 +24,21 @@ UNION ALL
 SELECT * FROM {{ ref('labels_infrastructure') }}
 UNION ALL
 SELECT * FROM {{ ref('labels_institution') }}
+
+-- single category labels (no subsets), needs label_type and model_name added still.
+UNION ALL
+SELECT blockchain, address, name, category, contributor, source, created_at, updated_at, model_name, label_type FROM {{ ref('labels_aztec_v2_contracts_ethereum') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_balancer_v2_pools') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_contracts') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_hackers_ethereum') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_ofac_sanctionned_ethereum') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_project_wallets') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_safe_ethereum') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_tornado_cash') }}
