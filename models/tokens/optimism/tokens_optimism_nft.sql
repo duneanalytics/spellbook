1d7b6e661eff322c6f0712508e44c5b92dcda38f
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-01 12:37:21 -0500

    pooly

--- models/tokens/optimism/tokens_optimism_nft.sql
+++ models/tokens/optimism/tokens_optimism_nft.sql
@@ -266,5 +266,6 @@ FROM
 ,('0xd8266CD02173f185b2a01D0bf3d654dF1Cd00714', 'Beefy Finance x Layer3', 'erc1155')
 ,('0x1e2eaf72bd96784d1e64e2ac3d8ec258b889f8f0', '1st Impact', 'erc721')
 ,('0xe30b4a83afbe78165841e10164441ef0f0d07eba', 'OptiChilds', 'erc721')
+,('0x1af7bBb04793E6eb5F7eCB7A0f5B2f697d0398C4', 'Pixel Pooly', 'erc721')
 
 ) as temp_table (contract_address, name, standard)
