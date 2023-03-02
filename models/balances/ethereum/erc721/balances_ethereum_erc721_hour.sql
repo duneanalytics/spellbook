2762a747d1cc18d62f4d19472904cd6dae80cfad
Boxer <Floba.hs@gmail.com>
2023-03-03 00:12:30 +0100

    fixing more column names

--- models/balances/ethereum/erc721/balances_ethereum_erc721_hour.sql
+++ models/balances/ethereum/erc721/balances_ethereum_erc721_hour.sql
@@ -10,7 +10,7 @@
 with
     hours as (
         select
-            max(number) || '_' || 0 as evt_block_number_evt_index,  
+            max(number) || '_' || 0 as evt_block_number_index,  
             date_trunc('hour', time) as hour
         from {{ source('ethereum', 'blocks') }}
         group by 2
@@ -20,14 +20,14 @@ with
 SELECT
     'ethereum' as blockchain,
     d.hour,
-    b.evt_block_number_evt_index,
+    b.evt_block_number_index,
     b.wallet_address,
     b.token_address,
     b.tokenId,
     b.num_tokens,
     nft_tokens.name as collection
 FROM hours d
-INNER JOIN {{ ref('transfers_ethereum_erc721_agg') }} b ON (b.evt_block_number_evt_index <= d.evt_block_number_evt_index AND d.evt_block_number_evt_index < b.next_evt)
+INNER JOIN {{ ref('transfers_ethereum_erc721_agg') }} b ON (b.evt_block_number_index <= d.evt_block_number_index AND d.evt_block_number_index < b.next_evt)
 LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft_tokens ON (nft_tokens.contract_address = b.token_address)
 where num_tokens = 1
 
--- models/balances/ethereum/erc721/balances_ethereum_erc721_latest.sql
+++ models/balances/ethereum/erc721/balances_ethereum_erc721_latest.sql
@@ -10,7 +10,7 @@
 with
     minutes as (
         select
-            max(number) || '_' || 0 as evt_block_number_evt_index,  
+            max(number) || '_' || 0 as evt_block_number_index,  
             date_trunc('minute', time) as minute
         from {{ source('ethereum', 'blocks') }}
         group by 2
@@ -20,14 +20,14 @@ with
 SELECT
     'ethereum' as blockchain,
     d.minute,
-    b.evt_block_number_evt_index,
+    b.evt_block_number_index,
     b.wallet_address,
     b.token_address,
     b.tokenId,
     b.num_tokens,
     nft_tokens.name as collection
 FROM minutes d
-INNER JOIN {{ ref('transfers_ethereum_erc721_agg') }} b ON (b.evt_block_number_evt_index <= d.evt_block_number_evt_index AND d.evt_block_number_evt_index < b.next_evt)
+INNER JOIN {{ ref('transfers_ethereum_erc721_agg') }} b ON (b.evt_block_number_index <= d.evt_block_number_index AND d.evt_block_number_index < b.next_evt)
 LEFT JOIN {{ ref('tokens_ethereum_nft') }} nft_tokens ON (nft_tokens.contract_address = b.token_address)
 where num_tokens = 1
 and d.minute = date_trunc('minute', now()) --as recent as the database pipeline allows as we are using the db time as base
