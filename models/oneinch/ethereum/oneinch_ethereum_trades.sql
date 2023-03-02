b8519b18a7f5b6e5011df12e1fe723987172ef2d
jeff-dude <jeff@dune.xyz>
2023-03-02 15:12:23 -0500

    add lop rfq v2

--- models/oneinch/ethereum/oneinch_ethereum_trades.sql
+++ models/oneinch/ethereum/oneinch_ethereum_trades.sql
@@ -22,6 +22,7 @@ ref('oneinch_v1_ethereum_trades')
 ,ref('oneinch_limit_order_protocol_v3_ethereum_trades')
 ,ref('oneinch_limit_order_protocol_embedded_rfq_ethereum_trades')
 ,ref('oneinch_limit_order_protocol_rfq_v1_ethereum_trades')
+,ref('oneinch_limit_order_protocol_rfq_v2_ethereum_trades')
 ] %}
 
 
