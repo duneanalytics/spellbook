0afa2aeea96eda4790461600d8154c397a91e6c7
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-03-01 22:38:01 -0500

    1223

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -22,8 +22,8 @@ UNION ALL
 SELECT * FROM {{ ref('labels_dao') }}
 UNION ALL
 SELECT * FROM {{ ref('labels_infrastructure') }}
-UNION ALL
-SELECT * FROM {{ ref('labels_institution') }}
+-- UNION ALL
+-- SELECT * FROM {{ ref('labels_institution') }}
 
 -- -- single category labels (no subsets), needs label_type and model_name added still.
 -- UNION ALL
