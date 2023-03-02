bd9e8bdd6b6c2a770ab981776c1af9e50f90ebc1
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-02 17:55:36 -0500

    add block_date

--- models/op/token_distributions/optimism/op_token_distributions_optimism_other_distributions_claims.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_other_distributions_claims.sql
@@ -44,7 +44,9 @@ SELECT
         CASE WHEN claim_rank_asc = 1 THEN to_address ELSE NULL END
         is not NULL))[1] as to_address
 FROM (
-    SELECT r.evt_block_time, r.evt_block_number, r.evt_index,
+    SELECT 
+    DATE_TRUNC('day',r.evt_block_time) AS block_date, 
+    r.evt_block_time, r.evt_block_number, r.evt_index,
         tf.`from` AS from_address, tf.to AS to_address, tx.to AS tx_to_address, tx.`from` AS tx_from_address, r.evt_tx_hash,
         'Project' as from_label, 'Parter Fund' AS from_type, 'Aave' AS from_name, 
         tf.to as user_address, lbl_to.address_descriptor AS to_type
