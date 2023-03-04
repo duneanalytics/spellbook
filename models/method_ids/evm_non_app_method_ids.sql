bb1a2d070c711f6e8064a3fe88a5276b0df5dcb0
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-04 18:23:37 -0500

    yee

--- models/method_ids/evm_non_app_method_ids.sql
+++ models/method_ids/evm_non_app_method_ids.sql
@@ -9,9 +9,22 @@
         )
 }}
 
--- Generic EVM methods
--- Is there a list of supported evm chains that we can pull from here?
-SELECT array('ethereum','optimism','arbitrum','polygon','gnosis','avalanche_c','fantom','goerli','bnb') as blockchains, method_id, method_descriptor
+
+{% set all_chains_array = [
+    'ethereum'
+    ,'optimism'
+    ,'arbitrum'
+    ,'polygon'
+    ,'gnosis'
+    ,'avalanche_c'
+    ,'fantom'
+    ,'goerli'
+    ,'bnb'
+    ] %}
+
+WITH aggrregate_methods AS (
+-- Generic EVM methods, we read null array as "all"
+SELECT NULL as blockchains, method_id, method_descriptor
     FROM (values
          ('0x095ea7b3','ERC20 Approval') --'ERC20 Approval'
         ,('0xa9059cbb','ERC20 Transfer') --'ERC20 Transfer'
@@ -23,7 +36,7 @@ SELECT array('ethereum','optimism','arbitrum','polygon','gnosis','avalanche_c','
         ,('0xa22cb465','ERC721/ERC1155 Approval') --'ERC721 Approval'
         ,('0x60806040','Contract Creation'), ('0x60c06040','Contract Creation') --'Contract Creation'
         ) a (method_id, method_descriptor)
-)
+
 UNION ALL --Optimism-Specific Methods
 
 SELECT array('optimism') AS blockchains, method_id, method_descriptor
@@ -44,3 +57,21 @@ SELECT array('arbitrum') AS blockchains, method_id, method_descriptor
         ,('0x7b3a3c8b','Bridge Out (L2 to L1)') --OutboundTransfer (ERC20)
         ,('0x2e567b36','Bridge In (L1 to L2)') --finalizeInboundTransfer (ERC20)
         ) a (method_id, method_descriptor)
+
+
+)
+
+
+SELECT *
+FROM (
+    {% for chain in all_chains_array %}
+    SELECT chain AS blockchain, method_id, method_descriptor
+    FROM aggrregate_methods
+    WHERE
+        blockchains IS NULL --If Null, make an entry for all chains
+        OR array_contains(blockchains,chain)
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
