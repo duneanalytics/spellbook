61f7689ace9916facafbbf75d6a923f8eec14ac6
Meghan Heintz <meghan@dune.com>
2023-03-09 14:13:22 -0500

    trailing comma

--- macros/mark_as_spell.sql
+++ macros/mark_as_spell.sql
@@ -2,7 +2,7 @@
 {%- if target.name == 'prod'-%}
         ALTER {{"view" if materialization == "view" else "table"}} {{ this }}
         SET TBLPROPERTIES (
-        'dune.data_explorer.category'='abstraction',
+        'dune.data_explorer.category'='abstraction'
         )
 {%- else -%}
 {%- endif -%}
