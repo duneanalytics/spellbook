b455818ea6c5a21e8f44d796f21f4d94648bf1c8
Meghan <msheintz@gmail.com>
2023-03-09 11:58:45 -0500

    wip

--- macros/mark_as_spell.sql
+++ macros/mark_as_spell.sql
@@ -1,6 +1,6 @@
-{% macro mark_as_spell() %}
+{% macro mark_as_spell(this, materialization) %}
 {%- if target.name == 'prod'-%}
-        ALTER {{"view" if model.config.materialized == "view" else "table"}} {{ this }}
+        ALTER {{"view" if materialization == "view" else "table"}} {{ this }}
         SET TBLPROPERTIES (
         'dune.data_explorer.category'='abstraction',
         )
