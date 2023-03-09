05e111e8cb33536cf957c82c2b1a6cbab2ab3636
chef-seaweed <121569855+chef-seaweed@users.noreply.github.com>
2023-03-09 11:17:39 +0800

    Update mmpool to trades

--- models/pancakeswap/pancakeswap_trades.sql
+++ models/pancakeswap/pancakeswap_trades.sql
@@ -10,6 +10,8 @@
 {% set pancake_models = [
     ref('pancakeswap_v2_bnb_trades')
 ,   ref('pancakeswap_v2_ethereum_trades')
+,   ref('pancakeswap_v2_bnb_mmpool')
+,   ref('pancakeswap_v2_ethereum_mmpool')
 ] %}
 
 
