f8014f760d33f7a3bb906fd01609b328f8775bca
Huang Geyang <Sukebeta@outlook.com>
2023-03-09 19:05:33 +0800

    Fix alias name and remove cast() in roll-up spells

--- models/pancakeswap/bnb/pancakeswap_v2_bnb_amm_trades.sql
+++ models/pancakeswap/bnb/pancakeswap_v2_bnb_amm_trades.sql
@@ -1,6 +1,6 @@
 {{ config(
     schema = 'pancakeswap_v2_bnb',
-    alias = 'trades',
+    alias = 'amm_trades',
     partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
