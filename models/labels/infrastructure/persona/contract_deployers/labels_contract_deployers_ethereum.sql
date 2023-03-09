95bdec8deceedf437cd87df8d6584370bb43c53f
Huang Geyang <Sukebeta@outlook.com>
2023-03-09 14:58:57 +0800

    Redesign labels_contract_deployers_ethereum

--- models/labels/infrastructure/persona/contract_deployers/labels_contract_deployers_ethereum.sql
+++ models/labels/infrastructure/persona/contract_deployers/labels_contract_deployers_ethereum.sql
@@ -1,18 +1,27 @@
-{{config(alias='contract_deployers_ethereum',
+{{
+    config(
+        alias='contract_deployers_ethereum',
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "sector",
                                     "labels",
-                                    \'["hildobby"]\') }}')}}
+                                    \'["hildobby", "hosuke"]\') }}'
+    )
+}}
 
-SELECT distinct 'ethereum' AS blockchain
-, creation.`from` AS address
-, 'Contract Deployer' as name
-, 'infrastructure' as category
-, 'hildobby' as contributor
-, 'query' AS source
-, date('2023-03-03') AS created_at
-, NOW() as modified_at
-, 'contract_deployers' AS model_name
-, 'persona' as label_type
-FROM {{ source('ethereum', 'creation_traces') }} creation
-LEFT ANTI JOIN {{this}} anti_table ON creation.`from`=anti_table.`from`
+WITH creation AS (
+    SELECT distinct 'ethereum'           AS blockchain
+                  , creation.`from`      AS address
+                  , 'Contract Deployer'  as name
+                  , 'infrastructure'     as category
+                  , 'hildobby'           as contributor
+                  , 'query'              AS source
+                  , date('2023-03-03')   AS created_at
+                  , NOW()                as modified_at
+                  , 'contract_deployers' AS model_name
+                  , 'persona'            as label_type
+    FROM {{ source('ethereum', 'creation_traces') }}
+)
+SELECT *
+FROM creation
+LEFT ANTI JOIN {{ source('ethereum', 'creation_traces') }} anti_table
+ON creation.address = anti_table.address
