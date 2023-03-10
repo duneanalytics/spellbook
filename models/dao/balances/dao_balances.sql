065480682f78df96744fccd62181a6ae94c9007c
Meghan <msheintz@gmail.com>
2023-03-10 14:31:39 -0500

    change materialization

--- models/dao/balances/dao_balances.sql
+++ models/dao/balances/dao_balances.sql
@@ -1,12 +1,16 @@
 {{ config(
     alias = 'balances',
-    materialized = 'view',
+    materialized = 'incremental',
     file_format = 'delta',
     post_hook='{{ expose_spells(\'["ethereum", "gnosis", "polygon"]\',
                                 "sector",
                                 "dao",
                                 \'["Henrystats"]\') }}')
 }}
+
+-- HACK: This model should be a view but it's syntax isn't supported in DuneSQL
+-- To maintain compatibility on both engines, we will materialize it as a table
+-- To add it to the hourly schedule, we'll mark it as incremental despite not having incremental logic
 
 
 {% set project_start_date = '2018-10-27' %}
