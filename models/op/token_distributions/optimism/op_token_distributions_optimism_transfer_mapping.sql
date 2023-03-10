e0409212b9e2e152004ec14a866bc774760a9216
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-10 13:25:05 -0500

    we did it?

--- models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
@@ -1,9 +1,10 @@
 {{ config(
     alias = 'transfer_mapping',
+    partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['evt_block_time', 'evt_block_number', 'evt_tx_hash', 'evt_index'],
+    unique_key = ['block_date','evt_block_time', 'evt_block_number', 'evt_tx_hash', 'evt_index'],
     post_hook='{{ expose_spells(\'["optimism"]\',
                                 "project",
                                 "op_token",
@@ -29,9 +30,8 @@ WITH all_labels AS (
         SELECT * FROM {{ ref('op_token_distributions_optimism_other_tags') }}
 )
 
--- , outgoing_distributions AS (
-    -- WITH 
-,tfers AS (
+, outgoing_distributions AS (
+    WITH tfers AS (
         -- transfers out
             SELECT
                 evt_block_time, evt_block_number, evt_index,
@@ -132,16 +132,16 @@ WITH all_labels AS (
             
             )
 
-        -- SELECT
-        --     evt_block_time, evt_block_number, evt_index,
-        --     from_address, to_address, tx_to_address, tx_from_address, evt_tx_hash,
-        --     from_type, to_type, from_label, from_name, to_label, o.to_name, op_amount_decimal, tx_method
-        -- FROM {{ ref('op_token_distributions_optimism_other_distributions_claims') }} o
-        -- {% if is_incremental() %} 
-        --     where o.evt_block_time >= date_trunc('day', now() - interval '1 week')
-        -- {% endif %}
+        SELECT
+            evt_block_time, evt_block_number, evt_index,
+            from_address, to_address, tx_to_address, tx_from_address, evt_tx_hash,
+            from_type, to_type, from_label, from_name, to_label, o.to_name, op_amount_decimal, tx_method
+        FROM {{ ref('op_token_distributions_optimism_other_distributions_claims') }} o
+        {% if is_incremental() %} 
+            where o.evt_block_time >= date_trunc('day', now() - interval '1 week')
+        {% endif %}
 
-        -- UNION ALL
+        UNION ALL
         
         SELECT
             t.evt_block_time, t.evt_block_number, t.evt_index,
@@ -149,90 +149,92 @@ WITH all_labels AS (
             t.from_type, t.to_type, t.from_label, t.from_name, t.to_label, t.to_name, t.op_amount_decimal, t.tx_method
         
         FROM tfers t
-        -- LEFT JOIN {{ ref('op_token_distributions_optimism_other_distributions_claims') }} o --don't double count - at the amount level b/c there could be multiple claims in one tx
-        --     ON t.evt_block_number = o.evt_block_number
-        --     AND t.evt_block_time = o.evt_block_time
-        --     AND t.evt_tx_hash = o.evt_tx_hash
-        --     AND (
-        --         t.evt_tfer_index = o.min_evt_tfer_index
-        --         OR
-        --         t.evt_tfer_index = o.max_evt_tfer_index
-        --         )
-        --     {% if is_incremental() %} 
-        --     and o.evt_block_time >= date_trunc('day', now() - interval '1 week')
-        --     {% endif %}
-        -- WHERE o.evt_block_number IS NULL
--- )
+        LEFT JOIN {{ ref('op_token_distributions_optimism_other_distributions_claims') }} o --don't double count - at the amount level b/c there could be multiple claims in one tx
+            ON t.evt_block_number = o.evt_block_number
+            AND t.evt_block_time = o.evt_block_time
+            AND t.evt_tx_hash = o.evt_tx_hash
+            AND (
+                t.evt_tfer_index = o.min_evt_tfer_index
+                OR
+                t.evt_tfer_index = o.max_evt_tfer_index
+                )
+            {% if is_incremental() %} 
+            and o.evt_block_time >= date_trunc('day', now() - interval '1 week')
+            {% endif %}
+        WHERE o.evt_block_number IS NULL
+)
 
 
--- , distributions AS (
+, distributions AS (
 
--- SELECT 
---      evt_block_time, evt_block_number, evt_index, evt_tx_hash
---     --
---     , from_address, to_address
---     , tx_to_address, tx_from_address
---     --
---     , from_type, to_type
---     , from_label, to_label
---     , from_name, to_name
---     , op_amount_decimal, tx_method,
+SELECT 
+     evt_block_time, evt_block_number, evt_index, evt_tx_hash
+    --
+    , from_address, to_address
+    , tx_to_address, tx_from_address
+    --
+    , from_type, to_type
+    , from_label, to_label
+    , from_name, to_name
+    , op_amount_decimal, tx_method,
 
---     CASE WHEN to_label = 'Other' THEN op_amount_decimal ELSE 0 END AS op_claimed,
+    CASE WHEN to_label = 'Other' THEN op_amount_decimal ELSE 0 END AS op_claimed,
     
---     CASE WHEN  to_label IN ('Other','Deployed') AND from_label != 'Deployed' THEN op_amount_decimal
---          WHEN (from_name != to_name) AND from_label = 'Project' AND to_label = 'Project' THEN op_amount_decimal --handle for distirbutions to other projects (i.e. Uniswap to Gamma)
---         ELSE 0 END
---     AS op_deployed,
+    CASE WHEN  to_label IN ('Other','Deployed') AND from_label != 'Deployed' THEN op_amount_decimal
+         WHEN (from_name != to_name) AND from_label = 'Project' AND to_label = 'Project' THEN op_amount_decimal --handle for distirbutions to other projects (i.e. Uniswap to Gamma)
+        ELSE 0 END
+    AS op_deployed,
     
---     CASE WHEN from_label = '{{foundation_label}}' AND from_type = '{{grants_descriptor}}' AND to_label = 'Project' THEN op_amount_decimal
---         ELSE 0 END
---     AS op_to_project,
+    CASE WHEN from_label = '{{foundation_label}}' AND from_type = '{{grants_descriptor}}' AND to_label = 'Project' THEN op_amount_decimal
+        ELSE 0 END
+    AS op_to_project,
     
---     CASE WHEN from_label = 'Project' AND to_label = 'Project'
---             AND from_name != to_name THEN op_amount_decimal
---         ELSE 0 END
---     AS op_between_projects,
+    CASE WHEN from_label = 'Project' AND to_label = 'Project'
+            AND from_name != to_name THEN op_amount_decimal
+        ELSE 0 END
+    AS op_between_projects,
             
---     CASE WHEN from_label='Deployed' and to_label='Project' AND from_name = to_name THEN op_amount_decimal
---         ELSE 0 END
---     AS op_incoming_clawback --Project's deployer back to the OG project wallet
+    CASE WHEN from_label='Deployed' and to_label='Project' AND from_name = to_name THEN op_amount_decimal
+        ELSE 0 END
+    AS op_incoming_clawback --Project's deployer back to the OG project wallet
             
---     FROM outgoing_distributions od
+    FROM outgoing_distributions od
 
--- )
+)
 
--- SELECT 
---     DATE_TRUNC('day',evt_block_time) AS block_date
---     , evt_block_time, evt_block_number, evt_index, evt_tx_hash
---     --
---     , from_address, to_address
---     , tx_to_address, tx_from_address
---     --
---     , from_type, to_type
---     , d.from_label, d.to_label
---     , COALESCE(dfrom.address_name, d.from_name) AS from_name
---     , COALESCE(dto.address_name, dtxto.address_name, d.to_name) AS to_name
---     --
---     , op_amount_decimal, tx_method
---     --
---     , cast(op_claimed as double) AS op_claimed
---     , cast(op_deployed as double) as op_deployed
---     , cast(op_to_project as double) as op_to_project
---     , cast(op_between_projects as double) as op_between_projects
---     , cast(op_incoming_clawback as double) as op_incoming_clawback
---     --
---     , d.to_name AS og_to_name --keep original name in case we want it
---     , d.from_name AS og_from_name --keep original name in case we want it
+
+
+SELECT 
+    DATE_TRUNC('day',evt_block_time) AS block_date
+    , evt_block_time, evt_block_number, evt_index, evt_tx_hash
+    --
+    , from_address, to_address
+    , tx_to_address, tx_from_address
+    --
+    , from_type, to_type
+    , d.from_label, d.to_label
+    , COALESCE(dfrom.address_name, d.from_name) AS from_name
+    , COALESCE(dto.address_name, dtxto.address_name, d.to_name) AS to_name
+    --
+    , op_amount_decimal, tx_method
+    --
+    , cast(op_claimed as double) AS op_claimed
+    , cast(op_deployed as double) as op_deployed
+    , cast(op_to_project as double) as op_to_project
+    , cast(op_between_projects as double) as op_between_projects
+    , cast(op_incoming_clawback as double) as op_incoming_clawback
+    --
+    , d.to_name AS og_to_name --keep original name in case we want it
+    , d.from_name AS og_from_name --keep original name in case we want it
     
--- FROM distributions d
--- -- read in other tags
--- LEFT JOIN other_tags dto
---     ON dto.address = d.to_address
---     AND d.to_name = 'Other' -- don't overwrite existing mapping
--- LEFT JOIN other_tags dtxto
---     ON dtxto.address = d.tx_to_address
---     AND d.to_name = 'Other' -- don't overwrite existing mapping
--- LEFT JOIN other_tags dfrom
---     ON dfrom.address = d.from_address
---     AND d.from_name = 'Other' -- don't overwrite existing mapping
+FROM distributions d
+-- read in other tags
+LEFT JOIN other_tags dto
+    ON dto.address = d.to_address
+    AND d.to_name = 'Other' -- don't overwrite existing mapping
+LEFT JOIN other_tags dtxto
+    ON dtxto.address = d.tx_to_address
+    AND d.to_name = 'Other' -- don't overwrite existing mapping
+LEFT JOIN other_tags dfrom
+    ON dfrom.address = d.from_address
+    AND d.from_name = 'Other' -- don't overwrite existing mapping
