63fc024256d30a1f1b5f27d34e0a34dd8f52822a
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-10 09:12:31 -0500

    test

--- models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
@@ -4,7 +4,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_date', 'evt_block_time', 'evt_block_number', 'evt_tx_hash', 'evt_index'],
+    unique_key = ['evt_block_time', 'evt_block_number', 'evt_tx_hash', 'evt_index'],
     post_hook='{{ expose_spells(\'["optimism"]\',
                                 "project",
                                 "op_token",
@@ -156,74 +156,76 @@ WITH all_labels AS (
         WHERE o.evt_block_number IS NULL
 )
 
-, distributions AS (
+SELECT * FROM outgoing_distributions
 
-SELECT 
-     evt_block_time, evt_block_number, evt_index, evt_tx_hash
-    --
-    , from_address, to_address
-    , tx_to_address, tx_from_address
-    --
-    , from_type, to_type
-    , from_label, to_label
-    , from_name AS og_from_name, to_name AS og_to_name
-    , op_amount_decimal, tx_method,
+-- , distributions AS (
 
-    CASE WHEN to_label = 'Other' THEN op_amount_decimal ELSE 0 END AS op_claimed,
+-- SELECT 
+--      evt_block_time, evt_block_number, evt_index, evt_tx_hash
+--     --
+--     , from_address, to_address
+--     , tx_to_address, tx_from_address
+--     --
+--     , from_type, to_type
+--     , from_label, to_label
+--     , from_name AS og_from_name, to_name AS og_to_name
+--     , op_amount_decimal, tx_method,
+
+--     CASE WHEN to_label = 'Other' THEN op_amount_decimal ELSE 0 END AS op_claimed,
     
-    CASE WHEN  to_label IN ('Other','Deployed') AND from_label != 'Deployed' THEN op_amount_decimal
-         WHEN (from_name != to_name) AND from_label = 'Project' AND to_label = 'Project' THEN op_amount_decimal --handle for distirbutions to other projects (i.e. Uniswap to Gamma)
-        ELSE 0 END
-    AS op_deployed,
+--     CASE WHEN  to_label IN ('Other','Deployed') AND from_label != 'Deployed' THEN op_amount_decimal
+--          WHEN (from_name != to_name) AND from_label = 'Project' AND to_label = 'Project' THEN op_amount_decimal --handle for distirbutions to other projects (i.e. Uniswap to Gamma)
+--         ELSE 0 END
+--     AS op_deployed,
     
-    CASE WHEN from_label = '{{foundation_label}}' AND from_type = '{{grants_descriptor}}' AND to_label = 'Project' THEN op_amount_decimal
-        ELSE 0 END
-    AS op_to_project,
+--     CASE WHEN from_label = '{{foundation_label}}' AND from_type = '{{grants_descriptor}}' AND to_label = 'Project' THEN op_amount_decimal
+--         ELSE 0 END
+--     AS op_to_project,
     
-    CASE WHEN from_label = 'Project' AND to_label = 'Project'
-            AND from_name != to_name THEN op_amount_decimal
-        ELSE 0 END
-    AS op_between_projects,
+--     CASE WHEN from_label = 'Project' AND to_label = 'Project'
+--             AND from_name != to_name THEN op_amount_decimal
+--         ELSE 0 END
+--     AS op_between_projects,
             
-    CASE WHEN from_label='Deployed' and to_label='Project' AND from_name = to_name THEN op_amount_decimal
-        ELSE 0 END
-    AS op_incoming_clawback --Project's deployer back to the OG project wallet
+--     CASE WHEN from_label='Deployed' and to_label='Project' AND from_name = to_name THEN op_amount_decimal
+--         ELSE 0 END
+--     AS op_incoming_clawback --Project's deployer back to the OG project wallet
             
-    FROM outgoing_distributions od
+--     FROM outgoing_distributions od
 
-)
+-- )
 
-SELECT 
-    DATE_TRUNC('day',evt_block_time) AS block_date
-    , evt_block_time, evt_block_number, evt_index, evt_tx_hash
-    --
-    , from_address, to_address
-    , tx_to_address, tx_from_address
-    --
-    , from_type, to_type
-    , d.from_label, d.to_label
-    , COALESCE(dfrom.address_name, d.og_from_name) AS from_name
-    , COALESCE(dto.address_name, dtxto.address_name, d.og_to_name) AS to_name
-    --
-    , op_amount_decimal, tx_method
-    --
-    , cast(op_claimed as double) AS op_claimed
-    , cast(op_deployed as double) as op_deployed
-    , cast(op_to_project as double) as op_to_project
-    , cast(op_between_projects as double) as op_between_projects
-    , cast(op_incoming_clawback as double) as op_incoming_clawback
-    --
-    , d.og_to_name
-    , d.og_from_name
+-- SELECT 
+--     DATE_TRUNC('day',evt_block_time) AS block_date
+--     , evt_block_time, evt_block_number, evt_index, evt_tx_hash
+--     --
+--     , from_address, to_address
+--     , tx_to_address, tx_from_address
+--     --
+--     , from_type, to_type
+--     , d.from_label, d.to_label
+--     , COALESCE(dfrom.address_name, d.og_from_name) AS from_name
+--     , COALESCE(dto.address_name, dtxto.address_name, d.og_to_name) AS to_name
+--     --
+--     , op_amount_decimal, tx_method
+--     --
+--     , cast(op_claimed as double) AS op_claimed
+--     , cast(op_deployed as double) as op_deployed
+--     , cast(op_to_project as double) as op_to_project
+--     , cast(op_between_projects as double) as op_between_projects
+--     , cast(op_incoming_clawback as double) as op_incoming_clawback
+--     --
+--     , d.og_to_name
+--     , d.og_from_name
     
-FROM distributions d
--- read in other tags
-LEFT JOIN other_tags dto
-    ON dto.address = d.to_address
-    AND d.og_to_name = 'Other' -- don't overwrite existing
-LEFT JOIN other_tags dtxto
-    ON dtxto.address = d.tx_to_address
-    AND d.og_to_name = 'Other' -- don't overwrite existing
-LEFT JOIN other_tags dfrom
-    ON dfrom.address = d.from_address
-    AND d.og_from_name = 'Other' -- don't overwrite existing
+-- FROM distributions d
+-- -- read in other tags
+-- LEFT JOIN other_tags dto
+--     ON dto.address = d.to_address
+--     AND d.og_to_name = 'Other' -- don't overwrite existing
+-- LEFT JOIN other_tags dtxto
+--     ON dtxto.address = d.tx_to_address
+--     AND d.og_to_name = 'Other' -- don't overwrite existing
+-- LEFT JOIN other_tags dfrom
+--     ON dfrom.address = d.from_address
+--     AND d.og_from_name = 'Other' -- don't overwrite existing
