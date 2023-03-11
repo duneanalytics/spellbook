00635bc96984180804f2f742cc14fca9563869f1
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-11 12:02:35 -0500

    move disperses to contracts folder

--- models/op/token_distributions/optimism/op_token_distributions_optimism_all_distributions_labels.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_all_distributions_labels.sql
@@ -6,7 +6,7 @@
 WITH all_labels AS (
     SELECT address, label, proposal_name, address_descriptor, project_name FROM {{ ref('op_token_distributions_optimism_project_wallets') }}
     UNION ALL
-    SELECT address, label, 'Utility Contract' AS proposal_name, address_descriptor, address_descriptor AS project_name FROM {{ ref('op_token_distributions_optimism_disperse_contracts') }}
+    SELECT contract_address as address, 'Utility' as label, 'Utility Contract' AS proposal_name, contract_name AS address_descriptor, contract_name AS project_name FROM {{ ref('contracts_optimism_disperse_contracts') }}
     UNION ALL
     SELECT address, 'CEX' as label, distinct_name AS proposal_name, cex_name AS address_descriptor, cex_name AS project_name FROM {{ ref('addresses_optimism_cex') }}
         WHERE address NOT IN (SELECT address FROM {{ ref('op_token_distributions_optimism_project_wallets') }})
