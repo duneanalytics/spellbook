3c710f9814b7ab70ba8802c66461e9fa2b3ab096
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-11 20:53:32 -0500

    adds

--- models/labels/labels_all.sql
+++ models/labels/labels_all.sql
@@ -28,6 +28,8 @@ UNION ALL
 SELECT * FROM {{ ref('labels_safe_ethereum') }}
 UNION ALL
 SELECT * FROM {{ ref('labels_tornado_cash') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_likely_bot_labels') }}
 
 -- new/standardized labels
 UNION ALL
