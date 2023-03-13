835f32064c4f9cb22d42950abdc513097b9b9841
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2023-03-13 16:18:32 +0100

    Add spell for erc20 transfers on Avalanche C

---
+++ models/transfers/avalanche_c/erc20/transfers_avalanche_c_erc20.sql
@@ -0,0 +1,65 @@
+{{ config(materialized='view', alias='erc20',
+        post_hook='{{ expose_spells(\'["avalanche_c"]\',
+                                    "sector",
+                                    "transfers",
+                                    \'["soispoke", "dot2dotseurat", "tschubotz"]\') }}') }}
+
+with
+    sent_transfers as (
+        select
+            CAST('send' AS VARCHAR(4)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(`to` AS VARCHAR(100)) as unique_transfer_id,
+            `to` as wallet_address,
+            contract_address as token_address,
+            evt_block_time,
+            value as amount_raw
+        from
+            {{ source('erc20_avalanche_c', 'evt_transfer') }}
+    )
+
+    ,
+    received_transfers as (
+        select
+        CAST('receive' AS VARCHAR(7)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(`from` AS VARCHAR(100)) as unique_transfer_id,
+        `from` as wallet_address,
+        contract_address as token_address,
+        evt_block_time,
+        '-' || CAST(value AS VARCHAR(100)) as amount_raw
+        from
+            {{ source('erc20_avalanche_c', 'evt_transfer') }}
+    )
+
+    ,
+    deposited_wavax as (
+        select
+            CAST('deposit' AS VARCHAR(7)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(dst AS VARCHAR(100)) as unique_transfer_id,
+            dst as wallet_address,
+            contract_address as token_address,
+            evt_block_time,
+            wad as amount_raw
+        from
+            {{ source('wavax_avalanche_c', 'wavax_evt_deposit') }}
+    )
+
+    ,
+    withdrawn_wavax as (
+        select
+            CAST('withdrawn' AS VARCHAR(9)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(src AS VARCHAR(100)) as unique_transfer_id,
+            src as wallet_address,
+            contract_address as token_address,
+            evt_block_time,
+            '-' || CAST(wad AS VARCHAR(100)) as amount_raw
+        from
+            {{ source('wavax_avalanche_c', 'wavax_evt_withdrawal') }}
+    )
+    
+select unique_transfer_id, 'avalanche_c' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
+from sent_transfers
+union
+select unique_transfer_id, 'avalanche_c' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
+from received_transfers
+union
+select unique_transfer_id, 'avalanche_c' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
+from deposited_wavax
+union
+select unique_transfer_id, 'avalanche_c' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
+from withdrawn_wavax
