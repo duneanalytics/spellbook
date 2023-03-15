c6f3ebffd514515478e920b50f23d30c902786ed
Huang Geyang <Sukebeta@outlook.com>
2023-03-15 11:30:18 +0800

    Rename directory and try to fix CWQ.sql

--- models/coinbase_wallet_quests/optimism/coinbase_wallet_quests_optimism_rewards_transfers.sql
+++ models/coinbase_wallet_quests/optimism/coinbase_wallet_quests_optimism_rewards_transfers.sql
@@ -16,14 +16,17 @@
 
 WITH distributions AS (
 
-SELECT
-qa.distributor_address, qa.quest_name
-, qa.rewards_token, r.to AS quester_address
-, r.evt_tx_hash AS tx_hash, r.evt_index, r.evt_block_time AS block_time, r.evt_block_number AS block_number
-, r.value AS rewards_token_value_raw
+SELECT qa.distributor_address
+     , qa.quest_name
+     , qa.rewards_token
+     , r.to               AS quester_address
+     , r.evt_tx_hash      AS tx_hash
+     , r.evt_index
+     , r.evt_block_time   AS block_time
+     , r.evt_block_number AS block_number
+     , r.value            AS rewards_token_value_raw
 FROM {{source('erc20_optimism','evt_Transfer')}} r
-
-    INNER JOIN {{ref('coinbase_wallet_quests_optimism_distributor_addresses')}} qa
+INNER JOIN {{ref('coinbase_wallet_quests_optimism_distributor_addresses')}} qa
     ON r.`from` = distributor_address
     AND r.contract_address = rewards_token
 
@@ -32,7 +35,7 @@ WHERE evt_block_time >= cast('{{project_start_date}}' as timestamp) --arbitrary
 -- for quest addresses we've seen before, pull incremental, else pull everything (controls for if we first see a distributor address later)
 AND 1 = (
         CASE WHEN evt_block_time >= date_trunc('day', now() - interval '1 week') THEN 1
-             WHEN distributor_address NOT IN (SELECT distributor_address FROM {{this}} GROUP BY 1) --we don't have this loaded in yet.
+             WHEN distributor_address NOT IN (SELECT distributor_address FROM {{this}} GROUP BY 1) THEN 1--we don't have this loaded in yet.
              ELSE 0
         END
         )
