2583d1554cbebad94255897a5d6d006458df34da
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-15 10:58:03 -0400

    rm bankless mapping

--- models/op/token_distributions/optimism/op_token_distributions_optimism_project_name_mapping.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_project_name_mapping.sql
@@ -10,7 +10,6 @@ SELECT
 FROM (values
         --set up a query to check that project_names here have a match in Dune project names & labels (some may not exist, like Karma)
          ('xToken Terminal / Gamma Strategies', 'xToken')
-        ,('Bankless Academy', 'Bankless')
         ,('Rainbow Wallet', 'Rainbow')
         ,('Karma 1', 'Karma')
         ,('Karma 2', 'Karma')
