17dd20cc74353761871fca3716a35cf75b2ea7f7
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-15 10:35:34 -0400

    break query in to cte

--- models/labels/__single_category_labels__/likely_bots/optimism/labels_optimism_likely_bot_addresses.sql
+++ models/labels/__single_category_labels__/likely_bots/optimism/labels_optimism_likely_bot_addresses.sql
@@ -12,7 +12,55 @@
 -- This could/should become a spell with some kind of modular logic approach so that we can plug in new detection logic over time (i.e. many of X method, or Y project's contracts)
 -- the core of this logic is on transaction frequency and sender concentration The "sender concentration" piece will get tested by mutlisigs / smart contract wallets.
 
-WITH  bot_addrs AS (
+WITH sender_transfer_rates AS (
+    -- For each transaction sender, get their hourly transaction data
+    SELECT `from` AS sender, DATE_TRUNC('hour',block_time) AS hr
+            , MIN(block_time) AS min_block_time
+            , MAX(block_time) AS max_block_time
+            , COUNT(*) AS hr_txs
+            , SUM(CASE WHEN to IN (SELECT address FROM {{ ref('labels_optimism_likely_bot_contracts') }} WHERE name != 'chain ops bot') THEN 1 ELSE 0 END) AS bot_concentration_txs
+            
+            , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ source('erc20_optimism','evt_Transfer') }} r WHERE t.hash = r.evt_tx_hash AND t.block_number = r.evt_block_number) THEN 1 ELSE 0 END) AS num_erc20_tfer_txs
+            , SUM(CASE WHEN EXISTS (SELECT 1 FROM nft.transfers r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND blockchain = 'optimism') THEN 1 ELSE 0 END) AS num_nft_tfer_txs
+
+            , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ source('erc20_optimism','evt_Transfer') }} r WHERE t.hash = r.evt_tx_hash AND t.block_number = r.evt_block_number) THEN 1 
+                    WHEN EXISTS (SELECT 1 FROM {{ ref('nft_transfers') }} r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND blockchain = 'optimism') THEN 1 
+                ELSE 0 END) AS num_token_tfer_txs
+    
+            , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ ref('dex_trades') }} r WHERE t.hash = r.tx_hash AND t.block_time = r.block_time AND blockchain = 'optimism') THEN 1 ELSE 0 END) AS num_dex_trade_txs
+            , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ ref('perpetual_trades') }} r WHERE t.hash = r.tx_hash AND t.block_time = r.block_time AND blockchain = 'optimism') THEN 1 ELSE 0 END) AS num_perp_trade_txs
+            , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ ref('nft_trades') }} r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND blockchain = 'optimism') THEN 1 ELSE 0 END) AS num_nft_trade_txs
+            
+            FROM {{ source('optimism','transactions') }} t
+
+        GROUP BY 1,2
+
+)
+
+, first_pass_throughput_filter AS (
+(
+    -- Filter down this list a bit to help with later mappings
+    SELECT sender, DATE_TRUNC('week',hr) AS wk, SUM(hr_txs) AS wk_txs, MAX(hr_txs) AS max_hr_txs, SUM(bot_concentration_txs) AS bot_concentration_txs,
+        cast(COUNT(*) as double) /cast(7.0*24.0 as double) AS pct_weekly_hours_active,
+        MIN(min_block_time) AS min_block_time,
+        MAX(max_block_time) AS max_block_time,
+        
+        SUM(num_erc20_tfer_txs) AS num_erc20_tfer_txs,
+        SUM(num_nft_tfer_txs) AS num_nft_tfer_txs,
+        SUM(num_token_tfer_txs) AS num_token_tfer_txs,
+        SUM(num_dex_trade_txs) AS num_dex_trade_txs,
+        SUM(num_perp_trade_txs) AS num_perp_trade_txs,
+        SUM(num_nft_trade_txs) AS num_nft_trade_txs
+        FROM sender_transfer_rates e
+        GROUP BY 1,2
+        HAVING MAX(hr_txs) >= 20 --had some high-ish frequency - gte 20 txs per hour at least once
+        OR
+        SUM(bot_concentration_txs) >= 0 --had a tx to a bot address
+
+)
+
+
+,  bot_addresses AS (
 SELECT *,
     cast(num_erc20_tfer_txs as double) / cast( num_txs as double) AS pct_erc20_tfer_txs,
     cast(num_nft_tfer_txs as double) / cast( num_txs as double) AS pct_nft_tfer_txs,
@@ -41,45 +89,7 @@ FROM (
             ,SUM(num_perp_trade_txs) AS num_perp_trade_txs
             ,SUM(num_nft_trade_txs) AS num_nft_trade_txs
             
-        FROM (
-            SELECT sender, DATE_TRUNC('week',hr) AS wk, SUM(hr_txs) AS wk_txs, MAX(hr_txs) AS max_hr_txs, SUM(bot_concentration_txs) AS bot_concentration_txs,
-                cast(COUNT(*) as double) /cast(7.0*24.0 as double) AS pct_weekly_hours_active,
-                MIN(min_block_time) AS min_block_time,
-                MAX(max_block_time) AS max_block_time,
-                
-                SUM(num_erc20_tfer_txs) AS num_erc20_tfer_txs,
-                SUM(num_nft_tfer_txs) AS num_nft_tfer_txs,
-                SUM(num_token_tfer_txs) AS num_token_tfer_txs,
-                SUM(num_dex_trade_txs) AS num_dex_trade_txs,
-                SUM(num_perp_trade_txs) AS num_perp_trade_txs,
-                SUM(num_nft_trade_txs) AS num_nft_trade_txs
-                FROM (
-                    SELECT `from` AS sender, DATE_TRUNC('hour',block_time) AS hr
-                        , MIN(block_time) AS min_block_time
-                        , MAX(block_time) AS max_block_time
-                        , COUNT(*) AS hr_txs
-                        , SUM(CASE WHEN to IN (SELECT address FROM {{ ref('labels_optimism_likely_bot_contracts') }} WHERE name != 'chain ops bot') THEN 1 ELSE 0 END) AS bot_concentration_txs
-                        
-                        , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ source('erc20_optimism','evt_Transfer') }} r WHERE t.hash = r.evt_tx_hash AND t.block_number = r.evt_block_number) THEN 1 ELSE 0 END) AS num_erc20_tfer_txs
-                        , SUM(CASE WHEN EXISTS (SELECT 1 FROM nft.transfers r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND blockchain = 'optimism') THEN 1 ELSE 0 END) AS num_nft_tfer_txs
-            
-                        , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ source('erc20_optimism','evt_Transfer') }} r WHERE t.hash = r.evt_tx_hash AND t.block_number = r.evt_block_number) THEN 1 
-                                WHEN EXISTS (SELECT 1 FROM {{ ref('nft_transfers') }} r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND blockchain = 'optimism') THEN 1 
-                            ELSE 0 END) AS num_token_tfer_txs
-                
-                        , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ ref('dex_trades') }} r WHERE t.hash = r.tx_hash AND t.block_time = r.block_time AND blockchain = 'optimism') THEN 1 ELSE 0 END) AS num_dex_trade_txs
-                        , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ ref('perpetual_trades') }} r WHERE t.hash = r.tx_hash AND t.block_time = r.block_time AND blockchain = 'optimism') THEN 1 ELSE 0 END) AS num_perp_trade_txs
-                        , SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ ref('nft_trades') }} r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND blockchain = 'optimism') THEN 1 ELSE 0 END) AS num_nft_trade_txs
-                        
-                        FROM {{ source('optimism','transactions') }} t
-
-                    GROUP BY 1,2
-                    ) e
-                GROUP BY 1,2
-                HAVING MAX(hr_txs) >= 20 --had some high-ish frequency - gte 20 txs per hour at least once
-                OR
-                SUM(bot_concentration_txs) >= 0 --had a tx to a bot address
-            ) f
+        FROM first_pass_throughput_filter f
             GROUP BY 1
             -- various cases to detect bots
             HAVING (MAX(wk_txs) >= 2000 AND MAX(max_hr_txs) >= 100) --frequency (gt 2k txs in one week and gt 100 txs in one hour)
@@ -112,16 +122,16 @@ select
   'persona' as label_type
 
   FROM (
-
+    -- show ther entire list as one label
     SELECT
     sender AS address,
     'likely bots' AS category,
     'likely bot addresses' AS name
 
-    from bot_addrs
+    from bot_addresses
 
   UNION ALL
-
+    -- create another set of labels, cohorted by "type"
     SELECT
     sender AS address,
     'likely bot types' AS category,
@@ -135,6 +145,6 @@ select
     ELSE 'non-token bot address'
     END AS name
 
-  from bot_addrs
+  from bot_addresses
 
     ) a
