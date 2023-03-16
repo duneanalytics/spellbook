7925228b9e8c0899814e4ee2486bdebb5f4f618d
Hildebert Moulié <h.moulie@gmail.com>
2023-03-16 19:13:26 +0100

    fix

--- models/dex/dex_pools.sql
+++ models/dex/dex_pools.sql
@@ -1,6 +1,6 @@
 {{ config(
         alias ='trades',
-        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "fantom", "polygon"]\',
+        post_hook='{{ expose_spells(\'["ethereum", "arbitrum", "polygon"]\',
                                 "sector",
                                 "dex",
                                 \'["hildobby"]\') }}'
