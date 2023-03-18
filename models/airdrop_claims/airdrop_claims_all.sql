dfccceb0a10ec82ee7a88a0f0e11b621411c271b
Hildebert Moulié <h.moulie@gmail.com>
2023-03-18 21:31:42 +0100

    fix

--- models/airdrop_claims/airdrop_claims_all.sql
+++ models/airdrop_claims/airdrop_claims_all.sql
@@ -9,29 +9,28 @@
 
 
 {% set airdrop_claims_models = [
-    ref('airdrop_claims_ethereum_uniswap')
+    ref('airdrop_claims_ethereum_ampleforth')
+    , ref('airdrop_claims_ethereum_apecoin')
+    , ref('airdrop_claims_ethereum_benddao')
     , ref('airdrop_claims_ethereum_blur_1')
-    , ref('airdrop_claims_ethereum_looksrare')
+    , ref('airdrop_claims_ethereum_cow_protocol')
+    , ref('airdrop_claims_ethereum_dappradar')
+    , ref('airdrop_claims_ethereum_dydx')
     , ref('airdrop_claims_ethereum_ens')
     , ref('airdrop_claims_ethereum_forta_network')
-    , ref('airdrop_claims_ethereum_sudoswap')
-    , ref('airdrop_claims_ethereum_oneinch')
-    , ref('airdrop_claims_ethereum_dappradar')
-    , ref('airdrop_claims_ethereum_x2y2')
-    , ref('airdrop_claims_ethereum_hop_protocol')
-    , ref('airdrop_claims_ethereum_uniswap')
-    , ref('airdrop_claims_ethereum_gas_dao')
-    , ref('airdrop_claims_ethereum_apecoin')
-    , ref('airdrop_claims_ethereum_gitcoin')
-    , ref('airdrop_claims_ethereum_ribbon')
-    , ref('airdrop_claims_ethereum_dydx')
-    , ref('airdrop_claims_ethereum_tornado_cash')
-    , ref('airdrop_claims_ethereum_paraswap')
     , ref('airdrop_claims_ethereum_galxe')
-    , ref('airdrop_claims_ethereum_cow_protocol')
-    , ref('airdrop_claims_ethereum_ampleforth')
+    , ref('airdrop_claims_ethereum_gas_dao')
+    , ref('airdrop_claims_ethereum_gitcoin')
+    , ref('airdrop_claims_ethereum_hop_protocol')
+    , ref('airdrop_claims_ethereum_looksrare')
+    , ref('airdrop_claims_ethereum_oneinch')
     , ref('airdrop_claims_ethereum_paladin')
-    , ref('airdrop_claims_ethereum_benddao')
+    , ref('airdrop_claims_ethereum_paraswap')
+    , ref('airdrop_claims_ethereum_ribbon')
+    , ref('airdrop_claims_ethereum_sudoswap')
+    , ref('airdrop_claims_ethereum_tornado_cash')
+    , ref('airdrop_claims_ethereum_uniswap')
+    , ref('airdrop_claims_ethereum_x2y2')
     , ref('airdrop_claims_optimism_optimism_1')
 ] %}
 
@@ -48,7 +47,6 @@ FROM (
     , recipient
     , contract_address
     , tx_hash
-    , evt_index
     , quantity
     , token_address
     , token_symbol
