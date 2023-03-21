f75dc1c03b3e6e2ea9d125e05e48ff3dbda22521
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-03-21 17:56:05 -0400

    add time tables

---
+++ models/time/time_hour.sql
@@ -0,0 +1,13 @@
+{{ config(
+        alias = 'time',
+        materialized = 'table',
+        file_format = 'delta',
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                    "sector",
+                                    "time",
+                                    \'["ilemi"]\') }}'
+        )
+}}
+
+--later for duneSQL use this syntax https://dune.com/queries/1764158?d=11
+SELECT explode(sequence(to_date('1999-01-01'), to_date('2100-01-01'), interval 1 hour))
