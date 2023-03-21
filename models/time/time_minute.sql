0b2647e672cca6df33fe02209ca8ce6c98a27bbf
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-03-21 18:29:49 -0400

    hour and minute need more gran

--- models/time/time_minute.sql
+++ models/time/time_minute.sql
@@ -10,4 +10,8 @@
 }}
 
 --later for duneSQL use this syntax https://dune.com/queries/1764158?d=11
-SELECT explode(sequence(to_date('1999-01-04'), to_date('2100-01-01'), interval 1 minute))
+SELECT explode(sequence(
+    to_timestamp('1999-01-04 00:00:00'), 
+    to_timestamp('2100-01-01 23:59:00'), 
+    interval '1' minute
+))
