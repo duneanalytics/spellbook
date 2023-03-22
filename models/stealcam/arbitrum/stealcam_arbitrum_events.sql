d45f5c79fca5746bd289f8cb6b633be90c89ad93
Hildebert Moulié <h.moulie@gmail.com>
2023-03-22 19:30:30 +0100

    fix

--- models/stealcam/arbitrum/stealcam_arbitrum_events.sql
+++ models/stealcam/arbitrum/stealcam_arbitrum_events.sql
@@ -29,27 +29,27 @@ SELECT 'arbitrum' AS blockchain
 , 'Stealcam' AS collection
 , sc.id AS token_id
 , 'erc721' AS token_standard
-, 1 AS number_of_items
+, CAST(1 AS DECIMAL(38,0)) AS number_of_items
 , '0x82af49447d8a07e3bd95bd0d56f35241523fbab1' AS currency_contract
 , 'ETH' AS currency_symbol
-, sc.value AS amount_raw
-, sc.value/POWER(10, 18) AS amount_original
-, pu.price*sc.value/POWER(10, 18) AS amount_usd
+, CAST(sc.value AS DECIMAL(38,0)) AS amount_raw
+, CAST(sc.value/POWER(10, 18) AS DECIMAL(38,0)) AS amount_original
+, CAST(pu.price*sc.value/POWER(10, 18) AS DECIMAL(38,0)) AS amount_usd
 , sc.contract_address AS project_contract_address
 , CAST(NULL AS string) AS aggregator_name
 , CAST(NULL AS string) AS aggregator_address
 , sc.evt_tx_hash AS tx_hash
 , at.from AS tx_from
 , at.to AS tx_to
-, COALESCE(sc.value-(roy.value+not_fee.value), 0) AS platform_fee_amount_raw
-, COALESCE((sc.value-(roy.value+not_fee.value))/POWER(10, 18), 0) AS platform_fee_amount
-, COALESCE(pu.price*(sc.value-(roy.value+not_fee.value))/POWER(10, 18), 0) AS platform_fee_amount_usd
-, COALESCE(100*(sc.value-(roy.value+not_fee.value))/sc.value, 0) AS platform_fee_percentage
+, CAST(COALESCE(sc.value-(roy.value+not_fee.value), 0) AS double) AS platform_fee_amount_raw
+, CAST(COALESCE((sc.value-(roy.value+not_fee.value))/POWER(10, 18), 0) AS double) AS platform_fee_amount
+, CAST(COALESCE(pu.price*(sc.value-(roy.value+not_fee.value))/POWER(10, 18), 0) AS double) AS platform_fee_amount_usd
+, CAST(COALESCE(100*(sc.value-(roy.value+not_fee.value))/sc.value, 0) AS double) AS platform_fee_percentage
 , CASE WHEN sc.value-(roy.value+not_fee.value) > 0 THEN 'ETH' ELSE NULL END AS royalty_fee_currency_symbol
-, COALESCE(roy.value, 0) AS royalty_fee_amount_raw
-, COALESCE(roy.value/POWER(10, 18), 0) AS royalty_fee_amount
-, COALESCE(roy.value/POWER(10, 18)/pu.price, 0) AS royalty_fee_amount_usd
-, COALESCE(100*roy.value/sc.value, 0) AS royalty_fee_percentage
+, CAST(COALESCE(roy.value, 0) AS double) AS royalty_fee_amount_raw
+, CAST(COALESCE(roy.value/POWER(10, 18), 0) AS double) AS royalty_fee_amount
+, CAST(COALESCE(roy.value/POWER(10, 18)/pu.price, 0) AS double) AS royalty_fee_amount_usd
+, CAST(COALESCE(100*roy.value/sc.value, 0) AS double) AS royalty_fee_percentage
 , m._creator AS royalty_fee_receive_address
 , 'arbitrum-stealcam-' || sc.evt_tx_hash || '-' || sc.evt_index AS unique_trade_id
 FROM {{ source('stealcam_arbitrum', 'Stealcam_evt_Stolen') }} sc
@@ -61,7 +61,7 @@ INNER JOIN {{ source('arbitrum', 'transactions') }} at ON at.block_number=sc.evt
     {% if not is_incremental() %}
     AND at.block_time >= '{{project_start_date}}'
     {% endif %}
-LEFT JOIN prices.usd_forward_fill pu ON pu.blockchain = 'ethereum'
+LEFT JOIN {{ source('prices', 'usd_forward_fill') }} pu ON pu.blockchain = 'ethereum'
     AND pu.contract_address='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
     AND pu.minute=date_trunc('minute', sc.evt_block_time)
     {% if is_incremental() %}
