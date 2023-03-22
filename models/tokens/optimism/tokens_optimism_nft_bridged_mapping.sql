0d78202c3273d05d82bd4e6f9f517c65dbaa0317
0xRob <0xRobin5@gmail.com>
2023-03-22 12:26:24 +0100

    clean tokens.nft and deprecate nft token category

--- models/tokens/optimism/tokens_optimism_nft_bridged_mapping.sql
+++ models/tokens/optimism/tokens_optimism_nft_bridged_mapping.sql
@@ -7,14 +7,13 @@
                                     \'["chuxin"]\') }}'
   )
 }}
-select 
-  n.category as category
-  ,b.`remoteToken` as contract_address
+select
+   b.`remoteToken` as contract_address
   ,n.name
   ,n.standard
   ,n.symbol
   ,b.`localToken` as contract_address_l1
-from {{ source('optimism_ethereum','L1ERC721Bridge_evt_ERC721BridgeInitiated') }} as b 
-left join {{ ref('tokens_ethereum_nft')}} as n 
+from {{ source('optimism_ethereum','L1ERC721Bridge_evt_ERC721BridgeInitiated') }} as b
+left join {{ ref('tokens_ethereum_nft')}} as n
   on n.contract_address = b.`localToken`
 group by 1, 2, 3, 4, 5, 6
--- models/tokens/tokens_nft.sql
+++ models/tokens/tokens_nft.sql
@@ -4,74 +4,66 @@
                                     "tokens",
                                     \'["0xManny","hildobby","soispoke","dot2dotseurat"]\') }}')}}
 
-SELECT 
-'avalanche_c' as blockchain, 
-contract_address, 
-name, 
+SELECT
+'avalanche_c' as blockchain,
+contract_address,
+name,
 symbol,
-standard, 
-category 
+standard
 FROM  {{ ref('tokens_avalanche_c_nft') }}
             UNION
 SELECT
-'ethereum' as blockchain, 
-contract_address, 
-name, 
+'ethereum' as blockchain,
+contract_address,
+name,
 symbol,
-standard, 
-category 
+standard
 FROM  {{ ref('tokens_ethereum_nft') }}
             UNION
 SELECT
-'gnosis' as blockchain, 
-contract_address, 
-name, 
+'gnosis' as blockchain,
+contract_address,
+name,
 symbol,
-standard, 
-CAST(NULL AS VARCHAR(5)) as category
+standard
 FROM  {{ ref('tokens_gnosis_nft') }}
             UNION
 SELECT
-'optimism' as blockchain, 
-contract_address, 
-name, 
+'optimism' as blockchain,
+contract_address,
+name,
 CAST(NULL AS VARCHAR(5)) as symbol,
-standard,
-CAST(NULL AS VARCHAR(5)) as category
+standard
 FROM  {{ ref('tokens_optimism_nft') }}
             UNION
 SELECT
-'optimism' as blockchain, 
-contract_address, 
-name, 
+'optimism' as blockchain,
+contract_address,
+name,
 symbol,
-standard, 
-category 
+standard
 FROM  {{ ref('tokens_optimism_nft_bridged_mapping') }}
             UNION
 SELECT
-'bnb' as blockchain, 
-contract_address, 
-name, 
+'bnb' as blockchain,
+contract_address,
+name,
 CAST(NULL AS VARCHAR(5)) as symbol,
-standard, 
-CAST(NULL AS VARCHAR(5)) as category
+standard
 FROM  {{ ref('tokens_bnb_nft') }}
             UNION
 SELECT
-'fantom' as blockchain, 
-contract_address, 
-name, 
+'fantom' as blockchain,
+contract_address,
+name,
 symbol,
-standard, 
-category 
+standard
 FROM  {{ ref('tokens_fantom_nft') }}
             UNION
 SELECT
-'arbitrum' as blockchain, 
-contract_address, 
-name, 
+'arbitrum' as blockchain,
+contract_address,
+name,
 symbol,
-standard, 
-category 
+standard
 FROM  {{ ref('tokens_arbitrum_nft') }}
