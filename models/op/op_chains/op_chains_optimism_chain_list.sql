34684b7524f32d90f9fe45f8b7e23aef0660be8c
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-23 10:56:02 -0400

    dbt project

--- models/op/op_chains/op_chains_optimism_chain_list.sql
+++ models/op/op_chains/op_chains_optimism_chain_list.sql
@@ -1,5 +1,5 @@
 {{ config(
-        schema = 'op_chains_optimism',
+        schema = 'op_chains',
         alias='chain_list'
         , unique_key = ['blockchain', 'chain_id']
         , post_hook='{{ expose_spells(\'["optimism"]\',
