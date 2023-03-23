6d3f2cae972b2f02efda4980efc5c6513feaf359
0xRob <0xRobin5@gmail.com>
2023-03-23 19:24:27 +0100

    fix tokens_nft jinja

--- models/tokens/tokens_nft.sql
+++ models/tokens/tokens_nft.sql
@@ -20,7 +20,7 @@ SELECT *
 FROM (
     {% for source in sources %}
     SELECT
-    {{ source[0] }} as blockchain,
+    '{{ source[0] }}' as blockchain,
     contract_address,
     name,
     symbol,
