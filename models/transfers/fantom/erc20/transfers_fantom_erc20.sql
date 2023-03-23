aaf90db8f7f08dd3e33dbba5cd2116874f05a972
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2023-03-23 09:49:35 +0100

    Make query ready for Dune SQL

--- models/transfers/fantom/erc20/transfers_fantom_erc20.sql
+++ models/transfers/fantom/erc20/transfers_fantom_erc20.sql
@@ -7,8 +7,8 @@
 with
     sent_transfers as (
         select
-            CAST('send' AS VARCHAR(4)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(`to` AS VARCHAR(100)) as unique_transfer_id,
-            `to` as wallet_address,
+            'send-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' || CAST(to AS VARCHAR(100)) as unique_transfer_id,
+            to as wallet_address,
             contract_address as token_address,
             evt_block_time,
             value as amount_raw
@@ -19,8 +19,8 @@ with
     ,
     received_transfers as (
         select
-        CAST('receive' AS VARCHAR(7)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(`from` AS VARCHAR(100)) as unique_transfer_id,
-        `from` as wallet_address,
+        'receive-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' || CAST("from" AS VARCHAR(100)) as unique_transfer_id,
+        "from" as wallet_address,
         contract_address as token_address,
         evt_block_time,
         '-' || CAST(value AS VARCHAR(100)) as amount_raw
