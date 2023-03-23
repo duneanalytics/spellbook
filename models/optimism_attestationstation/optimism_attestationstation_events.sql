a9a1a215715cb891d217fcf620be8bd586acae58
chuxinh <chuxin@optimism.io>
2023-03-23 11:32:19 -0400

    add attestationstation spell

--- models/optimism_attestationstation/optimism_attestationstation_events.sql
+++ models/optimism_attestationstation/optimism_attestationstation_events.sql
@@ -12,8 +12,27 @@
     )
 }}
 select 
+    date_trunc('day', evt_block_time) as block_date
+    ,evt_tx_hash as tx_hash
+    ,evt_block_number as block_number
+    ,evt_block_time as block_time
+    ,evt_index
+    ,about as recipient
+    ,creator as issuer
+    ,contract_address
+    ,key as key_raw
+    ,unhex(
+        if(
+            substring(key, 1, 6) in ("0xab7e", "0x9e43"),
+            hex(key), 
+            substring(key, 3)
+        )
+    ) as key 
+    ,val as val_raw
+    ,unhex(substring(val, 3)) as val
 from {{source('optimism_attestationstation','OptimismAttestationStation_evt_AttestationSubmitted')}}
-where true
-{% if is_incremental() %}
-and evt_block_time >= date_trunc('day', now() - interval '1 week')
-{% endif %}
+where 
+    true
+    {% if is_incremental() %}
+    and evt_block_time >= date_trunc('day', now() - interval '1 week')
+    {% endif %}
