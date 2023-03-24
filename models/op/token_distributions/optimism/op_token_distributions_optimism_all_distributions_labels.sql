9690cb3737d91d0c09888b6d55cbfabcf2afdec8
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-24 19:45:11 -0400

    remap disperse

--- models/op/token_distributions/optimism/op_token_distributions_optimism_all_distributions_labels.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_all_distributions_labels.sql
@@ -19,7 +19,7 @@ WITH all_labels AS (
     SELECT
         contract_address as address
         , 'Utility' as label, 'Utility Contract' AS proposal_name
-        , override_name AS address_descriptor, contract_name AS project_name
+        , contract_name AS address_descriptor, contract_name AS project_name
         FROM {{ ref('contracts_optimism_disperse_contracts') }}
         WHERE contract_address NOT IN (SELECT address FROM {{ ref('op_token_distributions_optimism_project_wallets') }})
 
