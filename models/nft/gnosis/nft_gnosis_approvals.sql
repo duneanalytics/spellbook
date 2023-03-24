dc16399025b097096ec6a15771fbb9c01d0d317a
Hildebert Moulié <h.moulie@gmail.com>
2023-03-24 04:18:04 +0100

    create nft.approvals

---
+++ models/nft/gnosis/nft_gnosis_approvals.sql
@@ -0,0 +1,94 @@
+{{ config(
+        alias ='approvals',
+        partition_by='block_date',
+        materialized='incremental',
+        file_format = 'delta',
+        unique_key = ['unique_approval_id']
+)
+}}
+
+SELECT 'gnosis' AS blockchain
+, app.evt_block_time AS block_time
+, date_trunc('day', app.evt_block_time) AS block_date
+, app.evt_block_number AS block_number
+, app.owner AS address
+, 'erc721' AS token_standard
+, false AS approval_for_all
+, app.contract_address
+, app.tokenId AS token_id
+, approved 
+, NULL AS approved_for_all
+, app.evt_tx_hash AS tx_hash
+, et.from AS tx_from
+, et.to AS tx_to
+, app.evt_index
+, 'gnosis' || '-' || app.evt_tx_hash || '-' || app.evt_index AS unique_approval_id
+FROM {{ source('erc721_gnosis','evt_Approval') }} app
+INNER JOIN {{ source('gnosis', 'transactions') }} et ON et.block_number=app.evt_block_number
+    AND et.hash=app.evt_tx_hash
+    {% if is_incremental() %}
+    AND et.block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+{% if is_incremental() %}
+WHERE app.evt_block_time >= date_trunc("day", now() - interval '1 week')
+{% endif %}
+
+UNION ALL
+
+SELECT 'gnosis' AS blockchain
+, app.evt_block_time AS block_time
+, date_trunc('day', app.evt_block_time) AS block_date
+, app.evt_block_number AS block_number
+, app.owner AS address
+, 'erc721' AS token_standard
+, true AS approval_for_all
+, app.contract_address
+, NULL AS token_id
+, NULL AS approved 
+, approved AS approved_for_all
+, app.evt_tx_hash AS tx_hash
+, et.from AS tx_from
+, et.to AS tx_to
+, app.evt_index
+, 'gnosis' || '-' || app.evt_tx_hash || '-' || app.evt_index AS unique_approval_id
+FROM {{ source('erc721_gnosis','evt_ApprovalForAll') }} app
+INNER JOIN {{ source('gnosis', 'transactions') }} et ON et.block_number=app.evt_block_number
+    AND et.hash=app.evt_tx_hash
+    {% if is_incremental() %}
+    AND et.block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+{% if is_incremental() %}
+WHERE app.evt_block_time >= date_trunc("day", now() - interval '1 week')
+{% endif %}
+
+UNION ALL
+
+SELECT 'gnosis' AS blockchain
+, app.evt_block_time AS block_time
+, date_trunc('day', app.evt_block_time) AS block_date
+, app.evt_block_number AS block_number
+, app.operator AS address
+, 'erc1155' AS token_standard
+, true AS approval_for_all
+, app.contract_address
+, NULL AS token_id
+, NULL AS approved 
+, approved AS approved_for_all
+, app.evt_tx_hash AS tx_hash
+, et.from AS tx_from
+, et.to AS tx_to
+, app.evt_index
+, 'gnosis' || '-' || app.evt_tx_hash || '-' || app.evt_index AS unique_approval_id
+FROM {{ source('erc1155_gnosis','evt_ApprovalForAll') }} app
+{% if is_incremental() %}
+ANTI JOIN {{this}} anti_table
+    ON .evt_tx_hash = anti_table.tx_hash
+{% endif %}
+INNER JOIN {{ source('gnosis', 'transactions') }} et ON et.block_number=app.evt_block_number
+    AND et.hash=app.evt_tx_hash
+    {% if is_incremental() %}
+    AND et.block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+{% if is_incremental() %}
+WHERE app.evt_block_time >= date_trunc("day", now() - interval '1 week')
+{% endif %}
---
+++ models/nft/nft_approvals.sql
@@ -0,0 +1,45 @@
+{{ config(
+        alias ='approvals',
+        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "polygon", "fantom", "goerli"]\',
+                                    "sector",
+                                    "nft",
+                                    \'["hildobby"]\') }}'
+)
+}}
+
+{% set nft_models = [
+ ref('nft_ethereum_approvals')
+,ref('nft_bnb_approvals')
+,ref('nft_avalanche_c_approvals')
+,ref('nft_gnosis_approvals')
+,ref('nft_optimism_approvals')
+,ref('nft_arbitrum_approvals')
+,ref('nft_polygon_approvals')
+,ref('nft_fantom_approvals')
+,ref('nft_goerli_approvals')
+] %}
+
+SELECT *
+FROM (
+    {% for nft_model in nft_models %}
+    SELECT
+          block_time
+, block_date
+, block_number
+, address
+, token_standard
+, approval_for_all
+, contract_address
+, token_id
+, approved 
+, approved_for_all
+, tx_hash
+, tx_from
+, tx_to
+, evt_index
+    FROM {{ nft_model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
