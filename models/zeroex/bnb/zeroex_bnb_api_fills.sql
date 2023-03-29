2900fa6b48eba8ffe4d093091136a741d917dbaf
RantumBits <rantum@rantum.xyz>
2023-03-29 16:45:10 -0700

    add chain flag on erc20 join

--- models/zeroex/bnb/zeroex_bnb_api_fills.sql
+++ models/zeroex/bnb/zeroex_bnb_api_fills.sql
@@ -444,5 +444,5 @@ AND mp.minute >= date_trunc('day', now() - interval '1 week')
 AND mp.minute >= '{{zeroex_v3_start_date}}'
 {% endif %}
 
-LEFT OUTER JOIN {{ ref('tokens_erc20') }} ts ON ts.contract_address = taker_token
-LEFT OUTER JOIN {{ ref('tokens_erc20') }} ms ON ms.contract_address = maker_token
+LEFT OUTER JOIN {{ ref('tokens_erc20') }} ts ON ts.contract_address = taker_token and ts.blockchain = 'bnb'
+LEFT OUTER JOIN {{ ref('tokens_erc20') }} ms ON ms.contract_address = maker_token and ts.blockchain = 'bnb'
