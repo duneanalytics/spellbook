70cfd818122f806ea0189db5efb4e318432f9131
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-30 09:18:29 -0400

    [Hotfix - Easy] OP ATST Handle for Invisible Characters (#3077)
    * Update optimism_attestationstation_optimism_events.sql
    
    * add val string
    
    * make comma space
    
    * rerun
    
    * add byte2numericval
    
    * bytev3
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/optimism_attestationstation/optimism/optimism_attestationstation_optimism_events.sql
+++ models/optimism_attestationstation/optimism/optimism_attestationstation_optimism_events.sql
@@ -11,31 +11,45 @@
                                 \'["chuxin"]\') }}'
     )
 }}
-select 
-    date_trunc('day', evt_block_time) as block_date
-    ,evt_tx_hash as tx_hash
-    ,evt_block_number as block_number
-    ,evt_block_time as block_time
-    ,evt_index
-    ,about as recipient
-    ,creator as issuer
-    ,contract_address
-    ,key as key_raw
-    ,decode(
-        unhex(
-          if (
-            substring(key, 1, 6) in ("0xab7e", "0x9e43"),
-            hex(key),
-            substring(key, 3)
-          )
-        ),
-        "utf8"
-      ) as key
-    ,val as val_raw
-    ,split(unhex(substring(val, 3)), ",") as val
-from {{source('attestationstation_optimism','AttestationStation_evt_AttestationCreated')}}
-where 
-    true
-    {% if is_incremental() %}
-    and evt_block_time >= date_trunc('day', now() - interval '1 week')
-    {% endif %}
+SELECT
+  *
+  , concat_ws(', ', val) AS val_string
+
+  FROM (
+    select 
+        date_trunc('day', evt_block_time) as block_date
+        ,evt_tx_hash as tx_hash
+        ,evt_block_number as block_number
+        ,evt_block_time as block_time
+        ,evt_index
+        ,about as recipient
+        ,creator as issuer
+        ,contract_address
+        ,key as key_raw
+        ,
+        REGEXP_REPLACE( --Replace invisible characters
+            decode(
+                unhex(
+                  if (
+                    substring(key, 1, 6) in ("0xab7e", "0x9e43"), --Handle for Clique
+                    hex(key),
+                    substring(key, 3)
+                  )
+                ),
+                "utf8"
+              ) 
+        , '[[:cntrl:]]', '')
+        as key
+        ,val as val_raw
+
+        ,split(unhex(substring(val, 3)), ",") as val
+
+        ,bytea2numeric_v3(substring(val, 3)) AS val_byte2numeric
+
+    from {{source('attestationstation_optimism','AttestationStation_evt_AttestationCreated')}}
+    where 
+        true
+        {% if is_incremental() %}
+        and evt_block_time >= date_trunc('day', now() - interval '1 week')
+        {% endif %}
+  ) a
