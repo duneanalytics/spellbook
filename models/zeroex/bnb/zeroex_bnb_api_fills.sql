6dc4de001333e4d09b3cd6c2142351fb2aa43edc
RantumBits <rantum@rantum.xyz>
2023-03-29 17:05:35 -0700

    update blockchain on maker

--- models/zeroex/bnb/zeroex_bnb_api_fills.sql
+++ models/zeroex/bnb/zeroex_bnb_api_fills.sql
@@ -445,4 +445,4 @@ AND mp.minute >= '{{zeroex_v3_start_date}}'
 {% endif %}
 
 LEFT OUTER JOIN {{ ref('tokens_erc20') }} ts ON ts.contract_address = taker_token and ts.blockchain = 'bnb'
-LEFT OUTER JOIN {{ ref('tokens_erc20') }} ms ON ms.contract_address = maker_token and ts.blockchain = 'bnb'
+LEFT OUTER JOIN {{ ref('tokens_erc20') }} ms ON ms.contract_address = maker_token and ms.blockchain = 'bnb'
