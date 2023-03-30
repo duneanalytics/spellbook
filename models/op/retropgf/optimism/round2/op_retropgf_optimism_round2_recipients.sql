9e38659d23d95e51856883721457c8ed7d659617
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-30 14:24:26 -0400

    replace

--- models/op/retropgf/optimism/round2/op_retropgf_optimism_round2_recipients.sql
+++ models/op/retropgf/optimism/round2/op_retropgf_optimism_round2_recipients.sql
@@ -8,11 +8,12 @@
 {% set op_token = '0x4200000000000000000000000000000000000042' %}
 
 with attestations as (
-    SELECT *
+    SELECT *,
+        REGEXP_REPLACE(key, '[[:cntrl:]]', '') AS key_mapped
     FROM {{ ref('optimism_attestationstation_optimism_events') }}
     
-    WHERE from_hex(issuer) = '0x60c5c9c98bcbd0b0f2fd89b24c16e533baa8cda3'
-    AND key IN ('retropgf.round-2.name','retropgf.round-2.award','retropgf.round-2.category')
+    WHERE issuer = '0x60c5c9c98bcbd0b0f2fd89b24c16e533baa8cda3'
+    AND REGEXP_REPLACE(key, '[[:cntrl:]]', '') IN ('retropgf.round-2.name','retropgf.round-2.award','retropgf.round-2.category')
     AND block_date BETWEEN cast('2023-03-30' as date) AND cast('2023-05-01' as date)
     )
 
@@ -22,8 +23,8 @@ SELECT
     , nm.val AS recipient_name, ca.val AS recipient_category, aw.val AS award_amount
     , '{{op_token}}' AS award_token
     
-    FROM (SELECT * FROM attestations where key = 'retropgf.round-2.name') nm
-    LEFT JOIN (SELECT * FROM attestations where key = 'retropgf.round-2.award') aw
+    FROM (SELECT * FROM attestations where key_mapped = 'retropgf.round-2.name') nm
+    LEFT JOIN (SELECT * FROM attestations where key_mapped = 'retropgf.round-2.award') aw
         ON aw.recipient = nm.recipient
-    LEFT JOIN (SELECT * FROM attestations where key = 'retropgf.round-2.category') ca
+    LEFT JOIN (SELECT * FROM attestations where key_mapped = 'retropgf.round-2.category') ca
         ON ca.recipient = nm.recipient
--- models/op/retropgf/optimism/round2/op_retropgf_optimism_round2_voters.sql
+++ models/op/retropgf/optimism/round2/op_retropgf_optimism_round2_voters.sql
@@ -7,10 +7,12 @@
 
 with attestations as (
     SELECT *
+        --replace invisible characters
+        , REGEXP_REPLACE(key, '[[:cntrl:]]', '') AS key_mapped
     FROM {{ ref('optimism_attestationstation_optimism_events') }}
     
     WHERE issuer = '0x60c5c9c98bcbd0b0f2fd89b24c16e533baa8cda3'
-    AND key = 'retropgf.round-2.can_vote'
+    AND REGEXP_REPLACE(key, '[[:cntrl:]]', '') = 'retropgf.round-2.can_vote'
     AND block_date BETWEEN cast('2023-02-01' as date) AND cast('2023-04-01' as date)
     )
 
@@ -18,4 +20,4 @@ with attestations as (
 SELECT 
     v.block_date, v.recipient AS voter, nm.issuer, nm.val AS can_vote
     
-    FROM (SELECT * FROM attestations where key = 'retropgf.round-2.can_vote') v
+    FROM (SELECT * FROM attestations where key_mapped = 'retropgf.round-2.can_vote') v
