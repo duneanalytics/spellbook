4cd537bbfc575acdeb1474ae4ac53b3f85c30da8
Kofi Kufuor <kofikufuor@Kofis-MacBook-Pro.local>
2023-03-31 15:43:16 +0100

    Add flashloan user labels

---
+++ models/labels/addresses/infrastructure/persona/flashloans/labels_flashloans_ethereum.sql
@@ -0,0 +1,56 @@
+{{config(alias='flashloans_ethereum',
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                    "sector",
+                                    "labels",
+                                    \'["niftytable"]\') }}')}}
+
+SELECT DISTINCT
+  target AS address,
+  'flashloan' AS category,
+  'persona' AS label_type,
+  'flashloan_users' AS model_name,
+  'ethereum' AS blockchain,
+  'Aave v2 Flashloan User' AS name
+FROM
+  {{ source('aave_v2_ethereum','LendingPool_evt_FlashLoan') }}
+WHERE
+  amount != cast(0 as uint256)
+
+UNION ALL
+SELECT DISTINCT
+  target AS address,
+  'flashloan' AS category,
+  'persona' AS label_type,
+  'flashloan_users' AS model_name,
+  'ethereum' AS blockchain,
+  'Aave v3 Flashloan User' AS name
+FROM
+  {{ source('aave_v3_ethereum','Pool_evt_FlashLoan') }}
+WHERE
+  amount != cast(0 as uint256)
+
+UNION ALL
+SELECT DISTINCT
+  recipient AS address,
+  'flashloan' AS category,
+  'persona' AS label_type,
+  'flashloan_users' AS model_name,
+  'ethereum' AS blockchain,
+  'Balancer v2 Flashloan User' AS name
+FROM
+  {{ source('balancer_v2_ethereum','Vault_evt_FlashLoan') }}
+WHERE
+  amount != cast(0 as uint256)
+
+UNION ALL
+SELECT DISTINCT
+  recipient AS address,
+  'flashloan' AS category,
+  'persona' AS label_type,
+  'flashloan_users' AS model_name,
+  'ethereum' AS blockchain,
+  'Uniswap v3 Flashloan User' AS name
+FROM
+  {{ source('uniswap_v3_ethereum','Pair_evt_Flash') }}
+WHERE
+  amount0 != cast(0 as uint256)
