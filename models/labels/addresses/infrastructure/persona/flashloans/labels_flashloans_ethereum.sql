4f7c59b79278d3f906cb04c5145305861d646ef9
Huang Geyang <Sukebeta@outlook.com>
2023-04-04 17:51:38 +0800

    Add blockchain column

--- models/labels/addresses/infrastructure/persona/flashloans/labels_flashloans_ethereum.sql
+++ models/labels/addresses/infrastructure/persona/flashloans/labels_flashloans_ethereum.sql
@@ -1,56 +1,59 @@
-{{config(alias='flashloans_ethereum',
-        post_hook='{{ expose_spells(\'["ethereum"]\',
+{{config(
+    alias='flashloans_ethereum',
+    post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "sector",
                                     "labels",
-                                    \'["niftytable"]\') }}')}}
+                                    \'["niftytable"]\') }}'
+    )
+}}
 
-SELECT DISTINCT
-  target AS address,
-  'flashloan' AS category,
-  'persona' AS label_type,
-  'flashloan_users' AS model_name,
-  'ethereum' AS blockchain,
-  'Aave v2 Flashloan User' AS name
+SELECT DISTINCT 'ethereum'               AS blockchain,
+                target                   AS address,
+                'flashloan'              AS category,
+                'persona'                AS label_type,
+                'flashloan_users'        AS model_name,
+                'ethereum'               AS blockchain,
+                'Aave v2 Flashloan User' AS name
 FROM
-  {{ source('aave_v2_ethereum','LendingPool_evt_FlashLoan') }}
+    {{ source('aave_v2_ethereum', 'LendingPool_evt_FlashLoan') }}
 WHERE
-  amount != 0
-
-UNION ALL
-SELECT DISTINCT
-  target AS address,
-  'flashloan' AS category,
-  'persona' AS label_type,
-  'flashloan_users' AS model_name,
-  'ethereum' AS blockchain,
-  'Aave v3 Flashloan User' AS name
-FROM
-  {{ source('aave_v3_ethereum','Pool_evt_FlashLoan') }}
-WHERE
-  amount != 0
+    amount != 0
 
 UNION ALL
-SELECT DISTINCT
-  recipient AS address,
-  'flashloan' AS category,
-  'persona' AS label_type,
-  'flashloan_users' AS model_name,
-  'ethereum' AS blockchain,
-  'Balancer v2 Flashloan User' AS name
+SELECT DISTINCT 'ethereum'               AS blockchain,
+                target                   AS address,
+                'flashloan'              AS category,
+                'persona'                AS label_type,
+                'flashloan_users'        AS model_name,
+                'ethereum'               AS blockchain,
+                'Aave v3 Flashloan User' AS name
 FROM
-  {{ source('balancer_v2_ethereum','Vault_evt_FlashLoan') }}
+    {{ source('aave_v3_ethereum', 'Pool_evt_FlashLoan') }}
 WHERE
-  amount != 0
+    amount != 0
 
 UNION ALL
-SELECT DISTINCT
-  recipient AS address,
-  'flashloan' AS category,
-  'persona' AS label_type,
-  'flashloan_users' AS model_name,
-  'ethereum' AS blockchain,
-  'Uniswap v3 Flashloan User' AS name
+SELECT DISTINCT 'ethereum'                   AS blockchain,
+                recipient                    AS address,
+                'flashloan'                  AS category,
+                'persona'                    AS label_type,
+                'flashloan_users'            AS model_name,
+                'ethereum'                   AS blockchain,
+                'Balancer v2 Flashloan User' AS name
 FROM
-  {{ source('uniswap_v3_ethereum','Pair_evt_Flash') }}
+    {{ source('balancer_v2_ethereum', 'Vault_evt_FlashLoan') }}
 WHERE
-  amount0 != 0
+    amount != 0
+
+UNION ALL
+SELECT DISTINCT 'ethereum'                  AS blockchain,
+                recipient                   AS address,
+                'flashloan'                 AS category,
+                'persona'                   AS label_type,
+                'flashloan_users'           AS model_name,
+                'ethereum'                  AS blockchain,
+                'Uniswap v3 Flashloan User' AS name
+FROM
+    {{ source('uniswap_v3_ethereum', 'Pair_evt_Flash') }}
+WHERE
+    amount0 != 0
