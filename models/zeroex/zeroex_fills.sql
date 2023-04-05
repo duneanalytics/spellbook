ed4e94747f375f97972a40b4be0b609bf5ac69ee
RantumBits <rantum@rantum.xyz>
2023-04-05 15:12:25 -0700

    update seed

---
+++ models/zeroex/zeroex_fills.sql
@@ -0,0 +1,27 @@
+{{ config(
+        alias ='fills',
+        post_hook='{{ expose_spells(\'["ethereum","arbitrum", "optimism", "polygon","fantom","avalanche_c"]\',
+                                "project",
+                                "zeroex",
+                                \'["rantum","bakabhai993"]\') }}'
+        )
+}}
+
+{% set zeroex_models = [  
+ref('zeroex_ethereum_fills')
+,ref('zeroex_polygon_fills')
+] %}
+
+
+SELECT *
+FROM (
+    {% for model in zeroex_models %}
+    SELECT
+    *
+    FROM {{ model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
+;
