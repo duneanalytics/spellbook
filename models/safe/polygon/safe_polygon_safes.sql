d0aded4d54d21c67c8e5aab6a187f56902d8b6e5
jeff-dude <jeff@dune.xyz>
2023-04-06 14:52:14 -0400

    minor change to >= usage

--- models/safe/polygon/safe_polygon_safes.sql
+++ models/safe/polygon/safe_polygon_safes.sql
@@ -47,5 +47,5 @@ where et.success = true
     and et.block_time > '2021-03-07' -- for initial query optimisation    
     {% endif %}
     {% if is_incremental() %}
-    and et.block_time > date_trunc("day", now() - interval '1 week')
+    and et.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
