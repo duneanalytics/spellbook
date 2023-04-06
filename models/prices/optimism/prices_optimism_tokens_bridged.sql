69866be28fdbd91aad23c7f588207a4fab7904e2
0xRob <83790096+0xRobin@users.noreply.github.com>
2023-04-06 21:22:57 +0200

    auto include bridge erc20s in optimism prices (#3106)

---
+++ models/prices/optimism/prices_optimism_tokens_bridged.sql
@@ -0,0 +1,17 @@
+{{ config(
+        schema='prices_optimism',
+        alias ='tokens_bridged',
+        materialized='table',
+        file_format = 'delta',
+        tags=['static']
+        )
+}}
+SELECT
+    e.token_id
+    , 'optimism' as blockchain
+    , e.symbol as symbol
+    , o.l2_token as contract_address
+    , e.decimals
+FROM {{ ref('tokens_optimism_erc20_bridged_mapping') }} o
+INNER JOIN {{ ref('prices_ethereum_tokens') }} e
+ON e.contract_address = o.l1_token
