51fa300bb4020367899581b74cd1537b6f6a29d2
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-04-07 13:48:58 -0400

    Add Likely Bot labels to OP (#2941)
    * add likely bot labels
    
    * adds
    
    * update ref
    
    * fix {
    
    * convert to spark
    
    * spark function converts
    
    * paren
    
    * cols
    
    * model name
    
    * break query in to cte
    
    * add event to curve
    
    * Revert "add event to curve"
    
    This reverts commit eb36d83bfbb58fbf3625614dd509081e185105ba.
    
    * fix rogue parenthases
    
    * fix category choice
    
    * Fix labels_lens_poster_frequencies
    
    * Revert "Fix labels_lens_poster_frequencies"
    
    This reverts commit b8a4bd45951773c2d16f54adb655e9ac29064f25.
    
    * test
    
    * Redirectory
    
    * fix jinja
    
    ---------
    
    Co-authored-by: Meghan Heintz <meghan@dune.com>
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

---
+++ models/labels/addresses/__single_category_labels__/likely_bots/labels_likely_bot_labels.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        alias='likely_bot_labels',
+        post_hook='{{ expose_spells(\'["optimism"]\', 
+        "sector", 
+        "labels", 
+        \'["msilb7"]\') }}'
+    )
+}}
+
+SELECT * FROM {{ ref('labels_optimism_likely_bot_addresses') }}
+UNION ALL
+SELECT * FROM {{ ref('labels_optimism_likely_bot_contracts') }}
