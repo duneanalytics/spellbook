0aee61635556225e043f8fcef4163398a70b6a28
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-04-07 15:22:36 -0400

    add token balances Solana

---
+++ models/solana_utils/solana_utils_latest_balances.sql
@@ -0,0 +1,34 @@
+ {{
+  config(
+        alias='latest_balances',
+        materialized='incremental',
+        file_format = 'delta',
+        incremental_strategy='merge',
+        unique_key = ['token_mint_address', 'address'],
+        post_hook='{{ expose_spells(\'["solana"]\',
+                                    "sector",
+                                    "solana_utils",
+                                    \'["ilemi"]\') }}')
+}}
+
+WITH 
+      updated_balances as (
+            SELECT
+                  address 
+                  , token_mint_address
+                  , post_balance as sol_balance
+                  , post_token_balance as token_balance
+                  row_number() OVER (partition by address order by block_slot desc) as latest_balance
+            FROM {{ source('solana','account_activity') }}
+            WHERE tx_success 
+            {% if is_incremental() %}
+            AND block_time >= date_trunc("day", now() - interval '1 week')
+            {% endif %}
+      )
+
+--is there a way to then only keep the most recent "updated_at" for each address?
+SELECT 
+      *
+      , now() as updated_at 
+FROM updated_balances
+WHERE latest_balance = 1
