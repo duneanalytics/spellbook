f0b74db8895c17d2aa7efd59cc315a34061ea53d
Hildebert Moulié <h.moulie@gmail.com>
2023-04-08 12:18:46 +0200

    core

---
+++ models/airdrop/airdrop_claims.sql
@@ -0,0 +1,44 @@
+{{ config(
+        alias ='claims',
+        post_hook='{{ expose_spells(\'["ethereum", "optimism", "arbitrum", "avalanche_c", "bnb", "gnosis"]\',
+                                      "sector",
+                                      "airdrop",
+                                    \'["hildobby"]\') }}'
+    )
+}}
+
+
+{% set airdrop_claims_models = [
+    ref('airdrop_ethereum_claims')
+    , ref('airdrop_optimism_claims')
+    , ref('airdrop_arbitrum_claims')
+    , ref('airdrop_avalanche_c_claims')
+    , ref('airdrop_bnb_claims')
+    , ref('airdrop_gnosis_claims')
+] %}
+
+
+SELECT *
+FROM (
+    {% for airdrop_claims_model in airdrop_claims_models %}
+    SELECT
+    blockchain
+    , block_time
+    , block_number
+    , project
+    , airdrop_identifier
+    , recipient
+    , contract_address
+    , tx_hash
+    , amount_raw
+    , amount_original
+    , amount_usd
+    , token_address
+    , token_symbol
+    , evt_index
+    FROM {{ airdrop_claims_model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
