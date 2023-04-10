85b02280a375b732da9b6efaa18a6107c67611a7
Spring Zhang <spring.okay@gmail.com>
2023-04-11 04:06:19 +0800

    Opensea wyvern polygon trades (#2857)
    * added opensea polygon events
    
    * update events
    
    * Update opense polygon nft events
    
    * event unique id
    
    * Added test file
    
    * fix unique id
    
    * remove mint, adjust others
    
    * Syntax adjust
    
    * update platform fee
    
    * fix typo
    
    * Update fees
    
    * fix config block
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/opensea/opensea_trades.sql
+++ models/opensea/opensea_trades.sql
@@ -1,6 +1,6 @@
 {{ config(
         alias ='trades',
-        post_hook='{{ expose_spells(\'["ethereum","solana", "optimism"]\',
+        post_hook='{{ expose_spells(\'["ethereum","solana", "optimism", "polygon"]\',
                                             "project",
                                             "opensea",
                                             \'["soispoke"]\') }}'
@@ -36,7 +36,7 @@ tx_to,
 unique_trade_id
 FROM {{ ref('opensea_ethereum_trades') }}
 
-            UNION ALL
+UNION ALL
 
 SELECT blockchain,
 project,
@@ -66,6 +66,37 @@ CAST(NULL AS VARCHAR(5)) as tx_from,
 CAST(NULL AS VARCHAR(5)) as tx_to,
 unique_trade_id
 FROM {{ ref('opensea_solana_trades') }}
+
+UNION ALL
+
+SELECT blockchain,
+project,
+version,
+block_time,
+token_id,
+collection,
+amount_usd,
+token_standard,
+trade_type,
+number_of_items,
+trade_category,
+evt_type,
+seller,
+buyer,
+amount_original,
+amount_raw,
+currency_symbol,
+currency_contract,
+nft_contract_address,
+project_contract_address,
+aggregator_name,
+aggregator_address,
+block_number,
+tx_hash,
+tx_from,
+tx_to,
+unique_trade_id
+FROM {{ ref('opensea_polygon_trades') }}
 
 UNION ALL
 
