8a00566089b15f61411820825a26e7916c481f20
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-04-10 12:17:23 -0400

    Add OP RetroPGF Attestation Decoding (#3088)
    * init
    
    * commit
    
    * mod reward
    
    * replace
    
    * schema fixes
    
    * rename
    
    * can-vote
    
    * can-vote
    
    * add cast for amount
    
    * add to labels
    
    * schema
    
    * update
    
    * more commas
    
    * voter
    
    * Update config block
    
    * fix recipient to submitter_address
    
    * clarify submitter address
    
    * fix val source
    
    * structure cleanup, static tags
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>

---
+++ models/op/retropgf/optimism/op_retropgf_optimism_voters.sql
@@ -0,0 +1,30 @@
+{{ config(
+        schema = 'op_retropgf_optimism'
+        , alias='voters'
+        , post_hook='{{ expose_spells(\'["optimism"]\',
+                                  "project",
+                                  "op_retropgf",
+                                  \'["msilb7"]\') }}'
+  )
+}}
+{% set sources = [
+     ('Round #2',   ref('op_retropgf_optimism_round2_voters'))
+] %}
+
+SELECT *
+FROM (
+    {% for source in sources %}
+    SELECT
+    'optimism' AS blockchain,
+    '{{ source[0] }}' as round_name,
+    block_date,
+    voter,
+    issuer,
+    can_vote
+
+    FROM {{ source[1] }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
