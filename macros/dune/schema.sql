97928f77ca7ef828617a503139d65d8592f68e87
Meghan <msheintz@gmail.com>
2023-04-12 14:41:49 -0400

    remove logging statements

--- macros/dune/schema.sql
+++ macros/dune/schema.sql
@@ -4,9 +4,7 @@
 
 {% macro default__create_schema(relation) -%}
   {% set s3_bucket = 'trino-dev-datasets-118330671040' %}
-  {% do log('default__create_schema', info=true) %}
   {%- call statement('create_schema') -%}
    CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket}}/hive')
   {% endcall %}
-  {% do log('end', info=true) %}
 {% endmacro %}
