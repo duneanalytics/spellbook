ce7576d789f84187a133ef3c5144a28ef22110fe
opar-xu <opar.xu@binance.com>
2023-04-12 15:52:36 +0800

    update schema

--- models/onepunchswap/bnb/onepunchswap_bnb_trades.sql
+++ models/onepunchswap/bnb/onepunchswap_bnb_trades.sql
@@ -9,7 +9,7 @@
     post_hook='{{ expose_spells(\'["bnb"]\',
                                 "project",
                                 "onepunchswap",
-                                \'["onepunchteam"]\') }}'
+                                \'["sunopar"]\') }}'
     )
 }}
 
