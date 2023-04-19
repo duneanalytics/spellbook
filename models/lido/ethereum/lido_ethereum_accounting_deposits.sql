4518dc56ab80af7257a21eb90392ae34cd696b59
gregshestakov <gregshestakov@gmail.com>
2023-04-19 13:34:14 +0300

    Revert "Changed deposits calculation"
    This reverts commit 76ebc4afe6ad6fe4d923d4407ecddb2a3dee32de.

--- models/lido/ethereum/lido_ethereum_accounting_deposits.sql
+++ models/lido/ethereum/lido_ethereum_accounting_deposits.sql
@@ -12,14 +12,10 @@
 --https://dune.com/queries/2011901
 --ref{{'lido_ethereum_accounting_deposits'}} 
 
- 
-  select    block_time AS period,
-            cast(value as double) as amount_staked,
-            LOWER('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') AS token, --ETH
-            tx_hash
-  from {{source('ethereum','traces')}}
-  where `to` = lower('0x00000000219ab540356cBB839Cbe05303d7705Fa')
-    and from = lower('0xae7ab96520de3a18e5e111b5eaab095312d7fe84')
-    AND (LOWER(call_type) NOT IN ('delegatecall', 'callcode', 'staticcall') or call_type is null)
-    AND success
-  
+	SELECT 
+        block_time AS period,
+        amount_staked*POWER(10,18) AS amount_staked,
+        LOWER('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') AS token, --ETH
+        tx_hash
+    FROM {{ ref('staking_ethereum_deposits') }} 
+    WHERE depositor_entity = 'Lido'
