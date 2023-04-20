e002c65a1eaf93e9057fb47a79018dce2193c358
RantumBits <rantum@rantum.xyz>
2023-04-20 16:13:02 -0700

    revise trace address in underlying models

--- models/zeroex/zeroex_trades.sql
+++ models/zeroex/zeroex_trades.sql
@@ -40,7 +40,7 @@ FROM (
       tx_hash  as tx_hash,
       tx_from  as tx_from,
       tx_to  as tx_to,
-      CAST(ARRAY(-1) as array<bigint>) as trace_address,
+      trace_address,
       evt_index  as evt_index
 
     FROM {{ model }}
