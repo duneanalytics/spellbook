793e9f8d67e0b094f344817c4109e470667a7e4d
0xRob <0xRobin5@gmail.com>
2023-04-24 13:41:41 +0200

    fix for superrare erc721

--- models/_sector/nft/trades/ethereum/marketplaces/superrare_ethereum_trades_base.sql
+++ models/_sector/nft/trades/ethereum/marketplaces/superrare_ethereum_trades_base.sql
@@ -20,7 +20,7 @@ with all_superrare_sales as (
             , `_buyer` as buyer
             , `_amount` as price_raw
             , evt_tx_hash
-            , '' as currency_contract
+            , '0x0000000000000000000000000000000000000000' as currency_contract
             , evt_index as sub_tx_trade_id
     from {{ source('superrare_ethereum','SuperRareMarketAuction_evt_Sold') }}
     {% if is_incremental() %}
@@ -37,7 +37,7 @@ with all_superrare_sales as (
             , `_buyer`
             , `_amount`
             , evt_tx_hash
-            , ''
+            , '0x0000000000000000000000000000000000000000'
             , evt_index
     from {{ source('superrare_ethereum','SuperRare_evt_Sold') }}
     {% if is_incremental() %}
@@ -54,7 +54,7 @@ with all_superrare_sales as (
             , `_bidder`
             , `_amount`
             , evt_tx_hash
-            , ''
+            , '0x0000000000000000000000000000000000000000'
             , evt_index
     from {{ source('superrare_ethereum','SuperRareMarketAuction_evt_AcceptBid') }}
     {% if is_incremental() %}
@@ -71,7 +71,7 @@ with all_superrare_sales as (
             , `_bidder`
             , `_amount`
             , evt_tx_hash
-            , ''
+            , '0x0000000000000000000000000000000000000000'
             , evt_index
     from {{ source('superrare_ethereum','SuperRare_evt_AcceptBid') }}
     {% if is_incremental() %}
@@ -140,7 +140,7 @@ with all_superrare_sales as (
             , concat('0x',substring(topic3 from 27 for 40)) as buyer
             , bytea2numeric_v3(substring(data from 67 for 64)) as price_raw
             , tx_hash
-            , ''
+            , '0x0000000000000000000000000000000000000000'
             , index
     from {{ source('ethereum','logs') }}
     where contract_address = lower('0x8c9f364bf7a56ed058fc63ef81c6cf09c833e656')
@@ -162,7 +162,7 @@ with all_superrare_sales as (
             , concat('0x',substring(topic3 from 27 for 40)) as buyer
             , bytea2numeric_v3(substring(data from 3 for 64)) as price_raw
             , tx_hash
-            , ''
+            , '0x0000000000000000000000000000000000000000'
             , index
     from {{ source('ethereum','logs') }}
     where contract_address =  lower('0x65b49f7aee40347f5a90b714be4ef086f3fe5e2c')
@@ -190,8 +190,8 @@ SELECT
     a.buyer,
     CAST(a.price_raw AS DECIMAL(38,0)) as price_raw,
     case
-    when a.currency_contract = '0xba5bde662c17e2adff1075610382b9b691296350' then a.currency_contract -- RARE
-    else '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'   -- WETH
+    when a.currency_contract = '0x0000000000000000000000000000000000000000' then '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' -- WETH
+    else a.currency_contract
     end as currency_contract,
     a.contract_address as nft_contract_address,
     cast(NULL as varchar(1)) as project_contract_address,
