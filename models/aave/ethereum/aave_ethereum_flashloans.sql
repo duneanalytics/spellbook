dac01899773ec273715bcca92dff6922eb9ed578
Hildebert Moulié <h.moulie@gmail.com>
2023-04-26 01:55:47 +0200

    fix

--- models/aave/ethereum/aave_ethereum_flashloans.sql
+++ models/aave/ethereum/aave_ethereum_flashloans.sql
@@ -1,5 +1,10 @@
 {{ config(
       alias='flashloans'
+      , partition_by = ['block_date']
+      , materialized = 'incremental'
+      , file_format = 'delta'
+      , incremental_strategy = 'merge'
+      , unique_key = ['tx_hash', 'evt_index']
       , post_hook='{{ expose_spells(\'["ethereum"]\',
                                   "project",
                                   "aave",
@@ -7,8 +12,15 @@
   )
 }}
 
+{% set aave_models = [
+ref('aave_v1_ethereum_flashloans')
+, ref('aave_v2_ethereum_flashloans')
+, ref('aave_v3_ethereum_flashloans')
+] %}
+
 SELECT *
 FROM (
+    {% for aave_model in aave_models %}
       SELECT blockchain
       , project
       , version
@@ -23,41 +35,12 @@ FROM (
       , currency_symbol
       , recipient
       , contract_address
-      FROM {{ ref('aave_v1_ethereum_flashloans') }}
-
-      UNION ALL
-
-      SELECT blockchain
-      , project
-      , version
-      , block_time
-      , block_number
-      , amount
-      , amount_usd
-      , tx_hash
-      , evt_index
-      , fee
-      , currency_contract
-      , currency_symbol
-      , recipient
-      , contract_address
-      FROM {{ ref('aave_v2_ethereum_flashloans') }} 
-
-      UNION ALL
-
-      SELECT blockchain
-      , project
-      , version
-      , block_time
-      , block_number
-      , amount
-      , amount_usd
-      , tx_hash
-      , evt_index
-      , fee
-      , currency_contract
-      , currency_symbol
-      , recipient
-      , contract_address
-      FROM {{ ref('aave_v3_ethereum_flashloans') }} 
+    FROM {{ aave_model }}
+    {% if is_incremental() %}
+    WHERE block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %} 
 )
