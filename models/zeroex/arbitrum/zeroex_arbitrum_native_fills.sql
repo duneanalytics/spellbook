dab3a99bee8f4bde1f2a7cf66864c92ed0525a8d
RantumBits <rantum@rantum.xyz>
2023-04-25 10:56:58 -0700

    update fee_recipient datatype

--- models/zeroex/arbitrum/zeroex_arbitrum_native_fills.sql
+++ models/zeroex/arbitrum/zeroex_arbitrum_native_fills.sql
@@ -95,7 +95,7 @@ WITH
           , fills.takerToken AS taker_token
           , tt.symbol AS taker_symbol
           , fills.takerTokenFilledAmount / (10^tt.decimals) AS taker_asset_filled_amount
-          , cast(NULL as varbinary) AS fee_recipient_address
+          , cast(NULL as bytea) AS fee_recipient_address
           , CASE
                   WHEN tp.symbol = 'USDC' THEN (fills.takerTokenFilledAmount / 1e6) ----don't multiply by anything as these assets are USD
                   WHEN mp.symbol = 'USDC' THEN (fills.makerTokenFilledAmount / 1e6) ----don't multiply by anything as these assets are USD
@@ -154,7 +154,7 @@ WITH
           , fills.takerToken AS taker_token
           , tt.symbol AS taker_symbol
           , fills.takerTokenFilledAmount / (10^tt.decimals) AS taker_asset_filled_amount
-          , cast(NULL as varbinary) AS fee_recipient_address
+          , cast(NULL as bytea) AS fee_recipient_address
           , CASE
                   WHEN tp.symbol = 'USDC' THEN (fills.takerTokenFilledAmount / 1e6) ----don't multiply by anything as these assets are USD
                   WHEN mp.symbol = 'USDC' THEN (fills.makerTokenFilledAmount / 1e6) ----don't multiply by anything as these assets are USD
