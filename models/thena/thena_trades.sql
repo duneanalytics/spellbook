03711276a024b0f94d2398a8dbee2eed398b721c
chef-seaweed <hsrvc.eth@gmail.com>
2023-04-25 18:06:47 +0800

    Update dbt_project

--- models/thena/thena_trades.sql
+++ models/thena/thena_trades.sql
@@ -1,6 +1,6 @@
 {{ config(
         alias ='trades',
-        post_hook='{{ expose_spells(\'["bnb","ethereum"]\',
+        post_hook='{{ expose_spells(\'["bnb"]\',
                                 "project",
                                 "thena",
                                 \'["hsrvc"]\') }}'
