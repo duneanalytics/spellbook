bfa481256e8ad567084e91a1feab1a17d52f4484
gregshestakov <gregshestakov@gmail.com>
2023-04-26 17:26:57 +0300

    fix tests

--- tests/lido_liquidity/kyberswap/lido_liquidity_kyberswap_ethereum_test.sql
+++ tests/lido_liquidity/kyberswap/lido_liquidity_kyberswap_ethereum_test.sql
@@ -1,16 +1,10 @@
 with tests as (
-select  'Check WETH:wstETH 0.008 pool' as test_name, 
-case when abs(main_token_reserve - 5430.91) < 1 then true else false end as success
+select case when count(*) = 31 then true else false end AS success 
 from {{ref('lido_liquidity_kyberswap_ethereum')}}
 where pool = lower('0xebfe63ba0264ad639b3c41d2bfe1ad708f683bc8')
-  and date_trunc('day',time) = '2023-03-13'
-union all
+  and extract('month',time) = 3
+  and extract('year',time) = 2023
 
-select 'Check USDC:wstETH pool' as test_name, 
-case when abs(paired_token_reserve - 8056414.65) < 1 then true else false end as success
-from {{ref('lido_liquidity_kyberswap_ethereum')}}
-where pool = lower('0xe6bcb55f45af6a2895fadbd644ced981bfa825cb')
-  and date_trunc('day',time) = '2023-02-01'
 )
 
 select *
