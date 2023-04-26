6f69407b03b827b80342b2fb1d8abd23ca517d85
gregshestakov <gregshestakov@gmail.com>
2023-04-26 12:31:20 +0300

    Add lido liquidity initial model

---
+++ models/lido_liquidity/lido_liquidity.sql
@@ -0,0 +1,39 @@
+{{ config(
+        alias ='lido_liquidity',
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "project",
+                                "lido",
+                                \'["ppclunghe", "gregshestakovlido"]\') }}'
+        )
+}}
+
+{% set lido_liquidity_models = [
+ ref('lido_liquidity_kyberswap_ethereum')
+] %}
+
+
+SELECT *
+FROM (
+    {% for k_model in lido_liquidity_models %}
+    SELECT pool_name, 
+           pool, 
+           chain, 
+           project, 
+           fee, 
+           time, 
+           main_token, 
+           main_token_symbol,
+           paired_token, 
+           paired_token_symbol, 
+           main_token_reserve, 
+           paired_token_reserve,
+           main_token_usd_reserve, 
+           paired_token_usd_reserve, 
+           trading_volume
+    FROM {{ k_model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
+;
