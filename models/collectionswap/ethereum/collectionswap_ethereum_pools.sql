42f52590f63b16ccf0d07c58d56eecb87d0e965a
0xRob <0xRobin5@gmail.com>
2023-04-27 14:13:06 +0200

    fix pools

--- models/collectionswap/ethereum/collectionswap_ethereum_pools.sql
+++ models/collectionswap/ethereum/collectionswap_ethereum_pools.sql
@@ -4,39 +4,36 @@
         )
 }}
 
--- ethereum
-WITH pool_info as (
-    select
+select
      poolAddress as pool_address
     ,collection as nft_contract_address
     ,token_address
     ,evt_tx_hash as create_tx_hash
     ,evt_block_time as create_block_time
-    from {{ source('collectionswap_ethereum','CollectionPoolFactory_evt_NewPool') }} e
-    inner join (
-        select
-        output_pool
-        ,get_json_object(params, '$.token') AS token_address
-        from {{ source('collectionswap_ethereum','CollectionPoolFactory_call_createPoolERC20') }}
-        where call_success
-        union all
-        select
-        output_pool
-        ,'0x0000000000000000000000000000000000000000' AS token_address
-        from {{ source('collectionswap_ethereum','CollectionPoolFactory_call_createPoolETH') }}
-        where call_success
-        union all
-        select
-        output_pool
-        ,get_json_object(params, '$.token') AS token_address
-        from {{ source('collectionswap_ethereum','CollectionPoolFactory_call_createPoolERC20Filtered') }}
-        where call_success
-        union all
-        select
-        output_pool
-        ,'0x0000000000000000000000000000000000000000' AS token_address
-        from {{ source('collectionswap_ethereum','CollectionPoolFactory_call_createPoolETHFiltered') }}
-        where call_success
-    ) c
-    on e.poolAddress = c.output_pool
-)
+from {{ source('collectionswap_ethereum','CollectionPoolFactory_evt_NewPool') }} e
+inner join (
+    select
+    output_pool
+    ,get_json_object(params, '$.token') AS token_address
+    from {{ source('collectionswap_ethereum','CollectionPoolFactory_call_createPoolERC20') }}
+    where call_success
+    union all
+    select
+    output_pool
+    ,'0x0000000000000000000000000000000000000000' AS token_address
+    from {{ source('collectionswap_ethereum','CollectionPoolFactory_call_createPoolETH') }}
+    where call_success
+    union all
+    select
+    output_pool
+    ,get_json_object(params, '$.token') AS token_address
+    from {{ source('collectionswap_ethereum','CollectionPoolFactory_call_createPoolERC20Filtered') }}
+    where call_success
+    union all
+    select
+    output_pool
+    ,'0x0000000000000000000000000000000000000000' AS token_address
+    from {{ source('collectionswap_ethereum','CollectionPoolFactory_call_createPoolETHFiltered') }}
+    where call_success
+) c
+on e.poolAddress = c.output_pool
