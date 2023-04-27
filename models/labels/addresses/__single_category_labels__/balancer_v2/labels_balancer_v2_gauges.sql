053e98f669ba51b678cb9cb7f4b7db1ff2ba1df4
Huang Geyang <Sukebeta@outlook.com>
2023-04-27 12:31:07 +0800

    Update roll-up spell

--- models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges.sql
+++ models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges.sql
@@ -4,10 +4,31 @@
                                     "labels",
                                     \'["jacektrocinski"]\') }}')}}
 
-SELECT * FROM  {{ ref('labels_balancer_v2_gauges_ethereum') }}
-UNION
-SELECT * FROM  {{ ref('labels_balancer_v2_gauges_polygon') }}
-UNION
-SELECT * FROM  {{ ref('labels_balancer_v2_gauges_arbitrum') }}
-UNION
-SELECT * FROM  {{ ref('labels_balancer_v2_gauges_optimism') }}
+{% set gauges_models = [
+    ref('labels_balancer_v2_gauges_ethereum')
+    , ref('labels_balancer_v2_gauges_polygon')
+    , ref('labels_balancer_v2_gauges_arbitrum')
+    , ref('labels_balancer_v2_gauges_optimism')
+] %}
+
+SELECT *
+FROM (
+    {% for gauges_model in gauges_models %}
+    SELECT
+          blockchain
+         , address
+         , name
+         , category
+         , contributor
+         , source
+         , created_at
+         , updated_at
+         , model_name
+         , label_type
+    FROM {{ gauges_model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
+;
