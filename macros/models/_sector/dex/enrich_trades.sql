c347ef42567a362fdafd42ffc9556bd5a124e697
Huang Geyang <Sukebeta@outlook.com>
2023-05-03 14:36:45 +0800

    Reformat enrich_trades

--- macros/models/_sector/dex/enrich_trades.sql
+++ macros/models/_sector/dex/enrich_trades.sql
@@ -36,37 +36,36 @@ WITH base_union AS (
 ),
 
 enrichments AS (
-SELECT
-    base.blockchain,
-    base.project,
-    base.version,
-    base.block_date,
-    base.block_time,
-    erc20a.symbol AS token_bought_symbol,
-    erc20b.symbol AS token_sold_symbol,
-    case
-        when lower(erc20a.symbol) > lower(erc20b.symbol) then concat(erc20b.symbol, '-', erc20a.symbol)
-        else concat(erc20a.symbol, '-', erc20b.symbol)
-    end AS token_pair,
-    base.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
-    base.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount,
-    base.token_bought_amount_raw,
-    base.token_sold_amount_raw,
-    coalesce(
-        base.amount_usd,
-        base.token_bought_amount_raw / power(10, erc20a.decimals) * pa.price,
-        base.token_sold_amount_raw / power(10, erc20b.decimals) * pb.price
-    ) AS amount_usd,
-    base.token_bought_address,
-    base.token_sold_address,
-    coalesce(base.taker, tx.from) AS taker,
-    base.maker,
-    base.project_contract_address,
-    base.tx_hash,
-    tx.from AS tx_from,
-    tx.to AS tx_to,
-    base.trace_address,
-    base.evt_index
+SELECT base.blockchain,
+       base.project,
+       base.version,
+       base.block_date,
+       base.block_time,
+       erc20a.symbol                                             AS token_bought_symbol,
+       erc20b.symbol                                             AS token_sold_symbol,
+       case
+           when lower(erc20a.symbol) > lower(erc20b.symbol) then concat(erc20b.symbol, '-', erc20a.symbol)
+           else concat(erc20a.symbol, '-', erc20b.symbol)
+           end                                                   AS token_pair,
+       base.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
+       base.token_sold_amount_raw / power(10, erc20b.decimals)   AS token_sold_amount,
+       base.token_bought_amount_raw,
+       base.token_sold_amount_raw,
+       coalesce(
+               base.amount_usd,
+               base.token_bought_amount_raw / power(10, erc20a.decimals) * pa.price,
+               base.token_sold_amount_raw / power(10, erc20b.decimals) * pb.price
+           )                                                     AS amount_usd,
+       base.token_bought_address,
+       base.token_sold_address,
+       coalesce(base.taker, tx.from)                             AS taker,
+       base.maker,
+       base.project_contract_address,
+       base.tx_hash,
+       tx.from                                                   AS tx_from,
+       tx.to                                                     AS tx_to,
+       base.trace_address,
+       base.evt_index
 FROM base_union base
 INNER JOIN {{ transactions_model }} tx
 ON tx.block_number = base.block_number
