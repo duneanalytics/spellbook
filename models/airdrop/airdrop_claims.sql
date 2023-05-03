d87de46687181c6ce6e85aa6ea71fa9847063009
Hildebert Moulié <h.moulie@gmail.com>
2023-05-03 11:18:59 +0200

    fix

--- models/airdrop/airdrop_claims.sql
+++ models/airdrop/airdrop_claims.sql
@@ -26,23 +26,6 @@ SELECT *
 FROM (
     {% for airdrop_claims_model in airdrop_claims_models %}
     SELECT
-<<<<<<< HEAD
-    blockchain
-    , block_time
-    , block_number
-    , project
-    , airdrop_identifier
-    , recipient
-    , contract_address
-    , tx_hash
-    , amount_raw
-    , amount_original
-    , amount_usd
-    , token_address
-    , token_symbol
-    , evt_index
-    FROM {{ airdrop_claims_model }}
-=======
         blockchain
         , block_time
         , block_number
@@ -61,13 +44,8 @@ FROM (
     {% if is_incremental() %}
     WHERE block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
->>>>>>> main
     {% if not loop.last %}
     UNION ALL
     {% endif %}
     {% endfor %}
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> main
