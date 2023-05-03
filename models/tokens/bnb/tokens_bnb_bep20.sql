4a2d5cf60858cd3f97bea053a759c805d34b2b6b
chef-seaweed <hsrvc.eth@gmail.com>
2023-05-03 15:17:17 +0800

    Add EDU and wBETH to tokens

--- models/tokens/bnb/tokens_bnb_bep20.sql
+++ models/tokens/bnb/tokens_bnb_bep20.sql
@@ -5531,4 +5531,5 @@ SELECT LOWER(contract_address) AS contract_address, symbol, decimals
 ,('0x2dfF88A56767223A5529eA5960Da7A3F5f766406', 'ID', 18)
 ,('0xb64e280e9d1b5dbec4accedb2257a87b400db149', 'LVL', 18)
 ,('0xf4c8e32eadec4bfe97e0f595add0f4450a863a11', 'THE', 18)
+,('0xbdeae1ca48894a1759a8374d63925f21f2ee2639', 'EDU', 18)
 ) AS temp_table (contract_address, symbol, decimals)
