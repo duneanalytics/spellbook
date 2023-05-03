982370c55eadab08d78fc1b39c1eed321db67fdc
0xRob <0xRobin5@gmail.com>
2023-05-03 14:14:42 +0200

    fix refs

--- models/_sector/nft/trades/nft_ethereum_trades_beta.sql
+++ models/_sector/nft/trades/nft_ethereum_trades_beta.sql
@@ -45,7 +45,7 @@ enriched_trades as (
         transactions_model=source('ethereum','transactions'),
         tokens_nft_model=ref('tokens_ethereum_nft'),
         tokens_erc20_model=ref('tokens_ethereum_erc20'),
-        prices_model=cte_prices_patch,
+        prices_model='cte_prices_patch',
         aggregators=ref('nft_ethereum_aggregators'),
         aggregator_markers=ref('nft_ethereum_aggregators_markers')
     )
