2b1813d76803ff570e5180fc01cd67bec09a6091
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-05-10 14:49:47 +0100

    [CoW] ETH Flow - add column environment (#3321)
    * add column environment
    
    * fix duplicate anchor

--- models/cow_protocol/ethereum/cow_protocol_ethereum_eth_flow_orders.sql
+++ models/cow_protocol/ethereum/cow_protocol_ethereum_eth_flow_orders.sql
@@ -22,6 +22,12 @@ eth_flow_orders as (
         evt_block_time as block_time,
         evt_block_number as block_number,
         evt_tx_hash as tx_hash,
+        case
+            when event.contract_address = '0x40a50cf069e992aa4536211b23f286ef88752187'
+                then 'prod'
+            when event.contract_address = '0xd02de8da0b71e1b59489794f423fabba2adc4d93'
+                then 'barn'
+        end as environment,
         -- This validity is always infinite. Instead we unpack this from the data field.
         -- from_unixtime(get_json_object(event.order, '$.validTo')) as valid_to,
         from_unixtime(conv(substring(data, 19, 8), 16, 10)) as valid_to,
