2cfd50194c01d6369dab51fc847bdc2d918c6fce
0xRob <0xRobin5@gmail.com>
2023-05-11 13:05:24 +0200

    Big cleanup part 2

--- models/superrare/superrare_ethereum_usernames.sql
+++ models/superrare/superrare_ethereum_usernames.sql
@@ -1,4 +1,4 @@
-{{config(alias='usernames')}}
+{{config(schema='superrare_ethereum', alias='usernames')}}
 
 
 SELECT blockchain, address, name, category, contributor, source, created_at, updated_at
