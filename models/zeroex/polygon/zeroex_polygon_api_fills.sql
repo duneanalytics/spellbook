daadd78c0f259e36b48e3abae4eb98f42217f477
RantumBits <rantum@rantum.xyz>
2023-05-16 14:54:45 -0700

    update from in taker

--- models/zeroex/polygon/zeroex_polygon_api_fills.sql
+++ models/zeroex/polygon/zeroex_polygon_api_fills.sql
@@ -348,7 +348,7 @@ SELECT distinct
         try_cast(date_trunc('day', all_tx.block_time) AS date) AS block_date,
         maker,
         CASE
-            WHEN taker = '0xdef1c0ded9bec7f1a1670819833240f027b25eff' THEN all_tx.from
+            WHEN taker = '0xdef1c0ded9bec7f1a1670819833240f027b25eff' THEN tx.from
             ELSE taker
         END AS taker, -- fix the user masked by ProxyContract issue
         taker_token,
