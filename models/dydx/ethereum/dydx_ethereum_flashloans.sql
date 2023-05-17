a691e978e4d03fca98ae42c3da7b58268ef3dc78
Hildebert Moulié <h.moulie@gmail.com>
2023-05-17 19:25:09 +0200

    make incremental

--- models/dydx/ethereum/dydx_ethereum_flashloans.sql
+++ models/dydx/ethereum/dydx_ethereum_flashloans.sql
@@ -1,8 +1,9 @@
 {{ config(
       alias='flashloans'
-      , materialized = 'view'
+      , materialized = 'incremental'
       , file_format = 'delta'
-      , unique_key = ['blockchain', 'tx_hash', 'evt_index']
+      , incremental_strategy = 'merge'
+      , unique_key = ['tx_hash', 'evt_index']
       , post_hook='{{ expose_spells(\'["ethereum"]\',
                                   "project",
                                   "dydx",
@@ -18,18 +19,18 @@ WITH flashloans AS (
         , d.evt_index
         , CASE WHEN MIN(CAST(json_extract(json_extract(d.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0)) - CAST(json_extract(json_extract(w.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0))) < 0 THEN 0
             ELSE MIN(CAST(json_extract(json_extract(d.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0)) - CAST(json_extract(json_extract(w.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0))) END AS fee
-        , CASE WHEN d.market = 0 THEN '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-            WHEN d.market = 1 THEN '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359'
-            WHEN d.market = 2 THEN '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-            WHEN d.market = 3 THEN '0x6b175474e89094c44da98b954eedeac495271d0f'
+        , CASE WHEN CAST(d.market AS int) = 0 THEN '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+            WHEN CAST(d.market AS int) = 1 THEN '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359'
+            WHEN CAST(d.market AS int) = 2 THEN '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+            WHEN CAST(d.market AS int) = 3 THEN '0x6b175474e89094c44da98b954eedeac495271d0f'
             END AS currency_contract
-        , CASE WHEN d.market = 0 THEN 'ETH'
-            WHEN d.market = 1 THEN 'SAI'
-            WHEN d.market = 2 THEN 'USDC'
-            WHEN d.market = 3 THEN 'DAI'
+        , CASE WHEN CAST(d.market AS int) = 0 THEN 'ETH'
+            WHEN CAST(d.market AS int) = 1 THEN 'SAI'
+            WHEN CAST(d.market AS int) = 2 THEN 'USDC'
+            WHEN CAST(d.market AS int) = 3 THEN 'DAI'
             END AS currency_symbol
-        , CASE WHEN d.market IN (0, 1, 3) THEN 18
-            WHEN d.market = 2 THEN 6
+        , CASE WHEN CAST(d.market AS int) IN (0, 1, 3) THEN 18
+            WHEN CAST(d.market AS int) = 2 THEN 6
             END AS currency_decimals
         , d.contract_address
     FROM {{ source('dydx_ethereum','SoloMargin_evt_LogDeposit') }} d
@@ -39,6 +40,12 @@ WITH flashloans AS (
         AND w.market = d.market
         AND d.accountOwner = w.accountOwner
         AND w.evt_index < d.evt_index
+        {% if is_incremental() %}
+        AND w.evt_block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+    {% if is_incremental() %}
+    AND d.evt_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
     GROUP BY 1, 2, 3, 4, 5, 7, 8, 9, 10
     )
 
