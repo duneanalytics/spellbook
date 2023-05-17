b19e50408585e56a6c39478a6fbcce11e9501863
0xRob <83790096+0xRobin@users.noreply.github.com>
2023-05-17 12:09:42 +0200

    Restructure looksrare to new NFT trades setup (#3332)
    * restructure looksrare
    
    * bugfix
    
    * bugfix
    
    * bugfix
    
    * add seed tests
    
    * fix seed
    
    * seed schema
    
    * cleanup
    
    * lr seed schema

--- models/_sector/nft/trades/nft_ethereum_trades_beta.sql
+++ models/_sector/nft/trades/nft_ethereum_trades_beta.sql
@@ -22,6 +22,8 @@
     ,('cryptopunks',    'v1',   ref('cryptopunks_ethereum_base_trades'))
     ,('sudoswap',    'v1',   ref('sudoswap_ethereum_base_trades'))
     ,('collectionswap',    'v1',   ref('collectionswap_ethereum_base_trades'))
+    ,('looksrare',    'v1',   ref('looksrare_v1_ethereum_base_trades'))
+    ,('looksrare',    'v2',   ref('looksrare_v2_ethereum_base_trades'))
 ] %}
 
 -- We should remove this CTE and include ETH into the general prices table once everything is migrated
