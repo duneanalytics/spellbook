d143fe0387f5005013ff191160f4c68b158ed27a
Huang Geyang <Sukebeta@outlook.com>
2023-05-18 03:17:24 +0800

    Change back to spark sql with incremental filter

--- models/dydx/ethereum/dydx_ethereum_flashloans.sql
+++ models/dydx/ethereum/dydx_ethereum_flashloans.sql
@@ -14,23 +14,23 @@
 WITH flashloans AS (
     SELECT d.evt_block_time AS block_time
         , d.evt_block_number AS block_number
-        , CAST(json_extract(json_extract(d.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0)) AS amount_raw
+        , CAST(get_json_object(get_json_object(d.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0)) AS amount_raw
         , d.evt_tx_hash AS tx_hash
         , d.evt_index
-        , CASE WHEN MIN(CAST(json_extract(json_extract(d.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0)) - CAST(json_extract(json_extract(w.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0))) < 0 THEN 0
-            ELSE MIN(CAST(json_extract(json_extract(d.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0)) - CAST(json_extract(json_extract(w.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0))) END AS fee
-        , CASE WHEN CAST(d.market AS int) = 0 THEN '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-            WHEN CAST(d.market AS int) = 1 THEN '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359'
-            WHEN CAST(d.market AS int) = 2 THEN '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-            WHEN CAST(d.market AS int) = 3 THEN '0x6b175474e89094c44da98b954eedeac495271d0f'
+        , CASE WHEN MIN(CAST(get_json_object(get_json_object(d.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0)) - CAST(get_json_object(get_json_object(w.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0))) < 0 THEN 0
+            ELSE MIN(CAST(get_json_object(get_json_object(d.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0)) - CAST(get_json_object(get_json_object(w.update, '$.deltaWei'), '$.value') AS DECIMAL(38,0))) END AS fee
+        , CASE WHEN d.market = 0 THEN '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+            WHEN d.market = 1 THEN '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359'
+            WHEN d.market = 2 THEN '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+            WHEN d.market = 3 THEN '0x6b175474e89094c44da98b954eedeac495271d0f'
             END AS currency_contract
-        , CASE WHEN CAST(d.market AS int) = 0 THEN 'ETH'
-            WHEN CAST(d.market AS int) = 1 THEN 'SAI'
-            WHEN CAST(d.market AS int) = 2 THEN 'USDC'
-            WHEN CAST(d.market AS int) = 3 THEN 'DAI'
+        , CASE WHEN d.market = 0 THEN 'ETH'
+            WHEN d.market = 1 THEN 'SAI'
+            WHEN d.market = 2 THEN 'USDC'
+            WHEN d.market = 3 THEN 'DAI'
             END AS currency_symbol
-        , CASE WHEN CAST(d.market AS int) IN (0, 1, 3) THEN 18
-            WHEN CAST(d.market AS int) = 2 THEN 6
+        , CASE WHEN d.market IN (0, 1, 3) THEN 18
+            WHEN d.market = 2 THEN 6
             END AS currency_decimals
         , d.contract_address
     FROM {{ source('dydx_ethereum','SoloMargin_evt_LogDeposit') }} d
@@ -42,10 +42,8 @@ WITH flashloans AS (
         AND w.evt_index < d.evt_index
         {% if is_incremental() %}
         AND w.evt_block_time >= date_trunc("day", now() - interval '1 week')
+        AND d.evt_block_time >= date_trunc("day", now() - interval '1 week')
         {% endif %}
-    {% if is_incremental() %}
-    AND d.evt_block_time >= date_trunc("day", now() - interval '1 week')
-    {% endif %}
     GROUP BY 1, 2, 3, 4, 5, 7, 8, 9, 10
     )
 
