37c38337f624fe8eef75521bd97deb30a2f051f7
0xRob <83790096+0xRobin@users.noreply.github.com>
2023-05-17 12:01:52 +0200

    Restructure Cryptopunks to new NFT trades setup (#3286)
    * cryptopunks restructuring
    
    * add to trades_beta
    
    * fix join logic
    
    * fix call reference
    
    * fix bid pricing
    
    * fix bid buyer
    
    * syntax
    
    * fix
    
    * allow 0.1% error and fix seed value

--- models/_sector/nft/trades/nft_ethereum_trades_beta.sql
+++ models/_sector/nft/trades/nft_ethereum_trades_beta.sql
@@ -19,6 +19,7 @@
     ,('zora',    'v1',   ref('zora_v1_ethereum_base_trades'))
     ,('zora',    'v2',   ref('zora_v2_ethereum_base_trades'))
     ,('zora',    'v3',   ref('zora_v3_ethereum_base_trades'))
+    ,('cryptopunks',    'v1',   ref('cryptopunks_ethereum_base_trades'))
 ] %}
 
 -- We should remove this CTE and include ETH into the general prices table once everything is migrated
