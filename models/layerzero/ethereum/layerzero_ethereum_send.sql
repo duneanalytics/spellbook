776bf43771e0ebc02b5b9b92bf7b30f26e02b4de
BennyFeng <326489067@qq.com>
2023-05-18 19:13:46 +0800

    LayerZero ethereum endpoint send

--- models/layerzero/ethereum/layerzero_ethereum_send.sql
+++ models/layerzero/ethereum/layerzero_ethereum_send.sql
@@ -17,7 +17,7 @@
 {% set endpoint_contract = "0x66a71dcef29a0ffbdbe3c6a460a3b5bc225cd675" %}
 {% set native_token_contract = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2" %}
 
-WITH chain_list(chain_name, chain_id, endpoint_address) as (
+WITH chain_list(chain_name, chain_id, endpoint_address) AS (
     values
     ('Ethereum', 101, '0x66a71dcef29a0ffbdbe3c6a460a3b5bc225cd675'),
     ('BNB Chain', 102, '0x3c2269811836af69497e5f486a85d7316753cf62'),
@@ -44,7 +44,8 @@ WITH chain_list(chain_name, chain_id, endpoint_address) as (
     ('zkSync Era', 165, '0x9b896c0e23220469c7ae69cb4bbae391eaa4c8da'),
     ('Moonriver', 167, '0x7004396c99d5690da76a7c59057c5f3a53e01704')
 ),
-send_call_detail AS (
+
+send_detail AS (
     SELECT CAST(101 AS integer) AS source_chain_id,
         s.call_tx_hash as tx_hash,
         s.call_block_number as block_number,
@@ -57,18 +58,13 @@ send_call_detail AS (
         s._zroPaymentAddress AS zro_payment_address,
         t.`from` AS user_address,
         t.`to` AS transaction_contract_address,
+        CAST(t.value AS DOUBLE) AS transaction_value,
         CASE WHEN len(_destination) >= 82
             THEN '0x' || right(_destination, 40)
             ELSE '' END AS local_contract_address, -- 
         CASE WHEN len(_destination) >= 82
             THEN substring(_destination, 1, len(_destination) - 40)
-            ELSE _destination END AS remote_contract_address,
-        CASE WHEN et.evt_index IS NULL
-            THEN '{{ native_token_contract }}'
-            ELSE et.contract_address END AS token_contract_address,
-        CASE WHEN et.evt_index IS NULL
-            THEN t.value
-            ELSE et.value END AS amount_raw
+            ELSE _destination END AS remote_contract_address
     FROM {{ source ('layerzero_ethereum', 'Endpoint_call_send') }} s
     INNER JOIN {{ source('ethereum','transactions') }} t on t.block_number = s.call_block_number
         AND t.hash = s.call_tx_hash
@@ -78,16 +74,8 @@ send_call_detail AS (
         {% if is_incremental() %}
         AND t.block_time >= date_trunc("day", now() - interval '1 week')
         {% endif %}
-    LEFT JOIN {{ source('erc20_ethereum', 'evt_transfer') }} et on et.evt_block_number = t.block_number
-        AND et.evt_tx_hash = t.hash
-        AND et.`from` = t.`from`
-        {% if not is_incremental() %}
-        AND et.evt_block_time >= '{{transaction_start_date}}'
-        {% endif %}
-        {% if is_incremental() %}
-        AND et.evt_block_time >= date_trunc("day", now() - interval '1 week')
-        {% endif %}
     WHERE s.call_success
+        AND s.call_block_time >= '{{transaction_start_date}}'
         {% if not is_incremental() %}
         AND s.call_block_time >= '{{transaction_start_date}}'
         {% endif %}
@@ -96,59 +84,125 @@ send_call_detail AS (
         {% endif %}
 ),
 
-transfer_amount_detail AS (
-    -- Native transfer
-    SELECT s.source_chain_id,
+send_summary AS (
+    SELECT block_number,
+        tx_hash,
+        user_address,
+        transaction_contract_address,
+        transaction_value
+    FROM send_detail
+    GROUP BY 1,2,3,4,5
+),
+
+-- Destination gas(Endpoint send value)
+destination_gas_detail AS (
+    SELECT s.block_number,
         s.tx_hash,
-        s.block_number,
-        s.destination_chain_id,
-        s.contract_address,
-        s.block_time,
         s.trace_address,
-        s.adapter_params,
-        s.refund_address,
-        s.zro_payment_address,
-        s.user_address,
-        s.transaction_contract_address,
-        s.local_contract_address,
-        s.remote_contract_address,
-        s.token_contract_address,
-        (s.amount_raw - t.value) AS amount_raw -- The transfer amount is obtained by subtracting the gas fee from the transaction amount if it is native token transferred.
-    FROM send_call_detail s
-    INNER JOIN {{ source('ethereum', 'traces') }} t ON t.block_number = s.block_number
-        AND t.tx_hash = s.tx_hash
-        AND cast(t.value as double) > 0
-        AND cardinality(t.trace_address) > 0  -- exclude the main call record
-        AND t.to = '{{ endpoint_contract }}' -- Use the value of traces transferred to the endpoint contract as gas fees.
+        CAST(e.value as double) AS destination_gas
+    FROM send_detail s
+    INNER JOIN {{ source('ethereum', 'traces') }} e on e.block_number = s.block_number
+        AND e.tx_hash = s.tx_hash
+        AND e.trace_address = s.trace_address
         {% if not is_incremental() %}
-        AND t.block_time >= '{{transaction_start_date}}'
+        AND e.block_time >= '{{transaction_start_date}}'
         {% endif %}
         {% if is_incremental() %}
-        AND t.block_time >= date_trunc("day", now() - interval '1 week')
+        AND e.block_time >= date_trunc("day", now() - interval '1 week')
         {% endif %}
-    WHERE s.token_contract_address = '{{ native_token_contract }}'
+),
+
+destination_trace_address_summary AS (
+    SELECT block_number,
+        tx_hash,
+        array_agg(trace_address[0]) AS endpoint_root_trace_address
+    FROM destination_gas_detail
+    group by 1, 2
+),
+
+destination_gas_summary AS (
+    SELECT block_number,
+        tx_hash,
+        sum(CAST(destination_gas as double)) AS amount_destination_gas
+    FROM destination_gas_detail
+    group by 1, 2
+),
+
+native_transfer_value_summary AS (
+    SELECT s.block_number,
+        s.tx_hash,
+        dg.endpoint_root_trace_address,
+        SUM(CAST(e.value as double)) AS amount_native_value
+    FROM send_summary s
+    INNER JOIN destination_trace_address_summary dg ON dg.block_number = s.block_number
+        AND dg.tx_hash = s.tx_hash
+    INNER JOIN {{ source('ethereum', 'traces') }} e ON e.block_number = dg.block_number
+        AND e.tx_hash = dg.tx_hash
+        AND ARRAY_CONTAINS(dg.endpoint_root_trace_address, e.trace_address[0]) IS NOT TRUE
+        AND e.`from` = s.transaction_contract_address
+        AND e.call_type = 'call'
+        AND cast(e.value as double) > 0
+        AND cardinality(e.trace_address) > 0
+        AND e.block_time >= '{{transaction_start_date}}'
+        {% if not is_incremental() %}
+        AND e.block_time >= '{{transaction_start_date}}'
+        {% endif %}
+        {% if is_incremental() %}
+        AND e.block_time >= date_trunc("day", now() - interval '1 week')
+        {% endif %}
+    GROUP BY 1, 2, 3
+),
+
+trans_detail AS (
+    -- ERC20 transfer: Endpoint send value is equal to transaction value
+    SELECT t.block_number,
+        t.tx_hash,
+        t.user_address,
+        t.transaction_contract_address,
+        t.transaction_value,
+        'erc20' AS transfer_type,
+        t.currency_contract,
+        t.amount_raw
+    FROM (
+        SELECT s.*,
+            et.contract_address AS currency_contract,
+            CAST(et.value AS double)AS amount_raw, -- Transfer amount of the transaction
+            row_number() OVER(PARTITION BY et.evt_tx_hash ORDER BY et.evt_index DESC) AS rn
+        FROM send_summary s
+        INNER JOIN destination_gas_summary dgs ON dgs.block_number = s.block_number
+            AND dgs.tx_hash = s.tx_hash
+            AND dgs.amount_destination_gas = s.transaction_value
+        INNER JOIN {{ source('erc20_ethereum', 'evt_transfer') }} et on et.evt_block_number = s.block_number
+            AND et.evt_tx_hash = s.tx_hash
+            AND et.evt_block_time >= '{{transaction_start_date}}'
+            {% if not is_incremental() %}
+            AND et.evt_block_time >= '{{transaction_start_date}}'
+            {% endif %}
+            {% if is_incremental() %}
+            AND et.evt_block_time >= date_trunc("day", now() - interval '1 week')
+            {% endif %}
+    ) t
+    WHERE t.rn = 1
     
     UNION ALL
-    
-    -- ERC20 transfer
-    SELECT s.source_chain_id,
+
+    -- Native transfer: The transaction amount != endpoint gas amount + transfer amount
+    SELECT s.block_number,
         s.tx_hash,
-        s.block_number,
-        s.destination_chain_id,
-        s.contract_address,
-        s.block_time,
-        s.trace_address,
-        s.adapter_params,
-        s.refund_address,
-        s.zro_payment_address,
         s.user_address,
         s.transaction_contract_address,
-        s.local_contract_address,
-        s.remote_contract_address,
-        s.token_contract_address,
-        s.amount_raw
-    FROM send_call_detail s
-    WHERE s.token_contract_address <> '{{ native_token_contract }}'
+        s.transaction_value,
+        'native' AS transfer_type,
+        '{{native_token_contract}}' AS currency_contract,
+        s.transaction_value - dgs.amount_destination_gas AS amount_raw -- Transfer amount of the transaction
+    FROM send_summary s
+    INNER JOIN destination_gas_summary dgs ON dgs.block_number = s.block_number
+        AND dgs.tx_hash = s.tx_hash
+        AND dgs.amount_destination_gas > 0
+    INNER JOIN native_transfer_value_summary nvs ON nvs.block_number = s.block_number
+        AND nvs.tx_hash = s.tx_hash
+        AND nvs.amount_native_value > 0
+    WHERE s.transaction_value = dgs.amount_destination_gas + nvs.amount_native_value
 )
 
 SELECT s.source_chain_id,
@@ -157,27 +211,32 @@ SELECT s.source_chain_id,
     cld.chain_name AS destination_chain_name,
     s.tx_hash,
     s.block_number,
-    s.contract_address as endpoint_contract,
+    s.contract_address AS endpoint_contract,
     date_trunc('day', s.block_time) AS block_date,
+    s.block_time,
     s.trace_address,
     s.adapter_params,
     s.refund_address,
     s.zro_payment_address,
-    s.block_time,
     s.user_address,
     s.transaction_contract_address AS transaction_contract,
     s.local_contract_address AS source_bridge_contract,
     s.remote_contract_address AS destination_bridge_contract,
-    CASE WHEN erc.symbol = 'WETH' THEN 'ETH' ELSE erc.symbol END AS currency_symbol,
-    s.token_contract_address as currency_contract,
-    COALESCE(s.amount_raw,0) / power(10, erc.decimals) * p.price AS amount_usd,
-    COALESCE(s.amount_raw,0) / power(10, erc.decimals) AS amount_original,
-    COALESCE(s.amount_raw,0) AS amount_raw
-FROM transfer_amount_detail s
+    t.transfer_type,
+    CASE WHEN erc.symbol = 'WETH' AND t.transfer_type = 'native'
+        THEN 'ETH'
+        ELSE erc.symbol END AS currency_symbol,
+    t.currency_contract,
+    COALESCE(t.amount_raw,0) / power(10, erc.decimals) * p.price AS amount_usd,
+    COALESCE(t.amount_raw,0) / power(10, erc.decimals) AS amount_original,
+    COALESCE(t.amount_raw,0) AS amount_raw
+FROM send_detail s
+INNER JOIN trans_detail t ON s.block_number = t.block_number
+    AND s.tx_hash = t.tx_hash
 LEFT JOIN chain_list cls ON cls.chain_id = s.source_chain_id
 LEFT JOIN chain_list cld ON cld.chain_id = s.destination_chain_id
-LEFT JOIN {{ ref('tokens_erc20') }} erc ON erc.blockchain = 'ethereum' AND erc.contract_address = s.token_contract_address
-LEFT JOIN {{ source('prices', 'usd') }} p ON p.contract_address = s.token_contract_address
+LEFT JOIN tokens.erc20 erc ON erc.blockchain = 'ethereum' AND erc.contract_address = t.currency_contract
+LEFT JOIN prices.usd p ON p.contract_address = t.currency_contract
     AND p.minute = date_trunc('minute', s.block_time)
     {% if not is_incremental() %}
     AND p.minute >= '{{transaction_start_date}}'
