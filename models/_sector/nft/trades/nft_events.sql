9c9ca409e258580dfcc4fd5d2bb68f5276cdbaf9
0xRob <0xRobin5@gmail.com>
2023-05-18 12:53:04 +0200

    deprecate restructured models

--- models/_sector/nft/trades/nft_events.sql
+++ models/_sector/nft/trades/nft_events.sql
@@ -8,4 +8,9 @@
                     \'["soispoke","0xRob", "hildobby"]\') }}')
 }}
 
+SELECT * FROM {{ ref('nft_ethereum_trades_beta_ported')}}
+UNION ALL
 SELECT * FROM {{ref('nft_events_old')}}
+WHERE (project, version) not in (SELECT distinct project, version FROM {{ref('nft_ethereum_trades_beta_ported')}})
+
+
