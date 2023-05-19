4453f5bc6e708172855fa63020ae5d2f33025ade
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-05-19 16:51:12 -0400

    mod frax

--- models/contracts/optimism/contracts_optimism_project_name_mappings.sql
+++ models/contracts/optimism/contracts_optimism_project_name_mappings.sql
@@ -86,5 +86,6 @@ from (
     ,('Biconomy - Hyphen', 'Biconomy')
     ,('angle', 'Angle Protocol')
     ,('overnight', 'Overnight+')
+    ,('Frax Finance', 'Frax')
 
     ) as temp_table (dune_name, mapped_name)
