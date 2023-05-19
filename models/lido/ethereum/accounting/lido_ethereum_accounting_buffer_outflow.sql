666336013b45b967b6b7c8ca4c53a6620c7021c0
ppclunghe <pipistrella@list.ru>
2023-05-19 10:04:14 +0200

    schema fix

--- models/lido/ethereum/accounting/lido_ethereum_accounting_buffer_outflow.sql
+++ models/lido/ethereum/accounting/lido_ethereum_accounting_buffer_outflow.sql
@@ -12,5 +12,5 @@
 --https://dune.com/queries/2488552
 --ref{{'lido_ethereum_accounting_buffer_outflow'}}
 
-SELECT evt_block_time, amountOfETHLocked as amount, LOWER('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') AS token,  evt_tx_hash
+SELECT evt_block_time as period, amountOfETHLocked as amount, LOWER('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') AS token,  evt_tx_hash
 FROM {{source('lido_ethereum','WithdrawalQueueERC721_evt_WithdrawalsFinalized')}}
