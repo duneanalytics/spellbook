1a49dfbdf087b9c12b861519268e0b046092d59a
ppclunghe <pipistrella@list.ru>
2023-05-19 09:39:49 +0200

    add buffer flow to the model, fix revenue

---
+++ models/lido/ethereum/accounting/lido_ethereum_accounting_buffer_outflow.sql
@@ -0,0 +1,16 @@
+{{ config(
+        alias ='buffer_outflow',
+        partition_by = ['period'],
+        materialized = 'table',
+        file_format = 'delta',
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "project",
+                                "lido",
+                                \'["gregshestakovlido", "ppclunghe", "xadcv"]\') }}'
+        )
+}}
+--https://dune.com/queries/2488552
+--ref{{'lido_ethereum_accounting_buffer_outflow'}}
+
+SELECT evt_block_time, amountOfETHLocked as amount, LOWER('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') AS token,  evt_tx_hash
+FROM {{source('lido_ethereum','WithdrawalQueueERC721_evt_WithdrawalsFinalized')}}
