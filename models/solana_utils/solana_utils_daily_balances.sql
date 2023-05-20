710f4613dfd342c45f342398d12df969dfce49dd
Huang Geyang <Sukebeta@outlook.com>
2023-05-21 03:07:19 +0800

    Try to fix the incremental merge checking bug

--- models/solana_utils/solana_utils_daily_balances.sql
+++ models/solana_utils/solana_utils_daily_balances.sql
@@ -4,7 +4,7 @@
         materialized='incremental',
         file_format = 'delta',
         incremental_strategy='merge',
-        unique_key = ['token_mint_address', 'address','day'],
+        unique_key = ['token_mint_address', 'address', 'day'],
         post_hook='{{ expose_spells(\'["solana"]\',
                                     "sector",
                                     "solana_utils",
@@ -16,7 +16,7 @@ WITH
             SELECT
                   address 
                   , date_trunc('day', block_time) as day
-                  , token_mint_address
+                  , coalesce(token_mint_address, '') as token_mint_address
                   , cast(post_balance as double)/1e9 as sol_balance --lamport -> sol 
                   , post_token_balance as token_balance --tokens are already correct decimals in this table
                   , token_balance_owner
