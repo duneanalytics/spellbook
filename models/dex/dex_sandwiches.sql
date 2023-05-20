ff7bdd29ef72e7114609f893cd97e035f8f9a0a6
Hildebert Moulié <h.moulie@gmail.com>
2023-05-21 01:00:02 +0200

    add mev txs

---
+++ models/dex/dex_sandwiches.sql
@@ -0,0 +1,150 @@
+{{ config(
+        alias ='sandwiches',
+        partition_by = ['block_date'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['blockchain', 'tx_hash', 'evt_index'],
+        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "fantom", "polygon"]\',
+                                "sector",
+                                "dex",
+                                \'["hildobby"]\') }}'
+        )
+}}
+
+{% set chains = [
+    'ethereum'
+    , 'bnb'
+    , 'avalanche_c'
+    , 'gnosis'
+    , 'optimism'
+    , 'arbitrum'
+    , 'fantom'
+    , 'polygon'
+    ] %}
+
+SELECT *
+FROM (
+    {% for chain in chains %}
+    SELECT
+        blockchain,
+        project,
+        version,
+        block_date,
+        block_time,
+        token_bought_symbol,
+        token_sold_symbol,
+        token_pair,
+        token_bought_amount,
+        token_sold_amount,
+        token_bought_amount_raw,
+        token_sold_amount_raw,
+        amount_usd,
+        token_bought_address,
+        token_sold_address,
+        taker,
+        maker,
+        project_contract_address,
+        tx_hash,
+        tx_from,
+        tx_to,
+        trace_address,
+        evt_index
+    FROM {{ chain }}
+    {% if not loop.last %}
+    {% if is_incremental() %}
+    WHERE block_date >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
+
+WITH trades AS (
+    SELECT {{ chain }} AS blockchain
+    , dt.project
+    , dt.version
+    , dt.block_time
+    , t.block_number
+    , dt.token_sold_address
+    , dt.token_bought_address
+    , dt.token_sold_symbol
+    , dt.token_bought_symbol
+    , dt.taker
+    , dt.tx_hash
+    , dt.tx_from
+    , dt.project_contract_address
+    , dt.evt_index
+    , t.index
+    , MIN(et.gas_price) AS gas_price
+    , MIN((t.gas_price/POWER(10, 18))*t.gas_used) AS tx_fee
+    , SUM(COALESCE(dt.token_sold_amount_raw, 0)) AS token_sold_amount_raw
+    , SUM(COALESCE(dt.token_bought_amount_raw, 0)) AS token_bought_amount_raw
+    , SUM(COALESCE(dt.token_sold_amount, 0)) AS token_sold_amount
+    , SUM(COALESCE(dt.token_bought_amount, 0)) AS token_bought_amount
+    , SUM(COALESCE(dt.amount_usd, 0)) AS amount_usd
+    FROM {{ ref('dex_trades') }} dt
+    INNER JOIN {{ source({{ chain }}, 'transactions') }} t ON t.block_time=dt.block_time
+        AND t.hash=dt.tx_hash
+    WHERE dt.blockchain = {{ chain }}
+    {% if not loop.last %}
+    {% if is_incremental() %}
+    AND block_date >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+    )
+
+, sandwiches AS (
+    SELECT s1.blockchain
+    , s1.project
+    , s1.version
+    , s1.block_time
+    , s1.block_number
+    , CASE WHEN s1.token_bought_amount_raw > 0 THEN s2.token_sold_amount_raw/s1.token_bought_amount_raw END AS ratio_bought_sold
+    , s2.token_bought_amount_raw-s1.token_sold_amount_raw AS profit_amount_raw
+    , s2.token_bought_amount-s1.token_sold_amount AS profit_amount
+    , s1.token_bought_address AS currency_address
+    , s1.token_sold_symbol AS currency_symbol
+    , s1.token_bought_amount_raw-s2.token_sold_amount_raw AS profit_traded_currency_amount_raw
+    , s1.token_bought_amount-s2.token_sold_amount AS profit_traded_currency_amount
+    , s1.token_sold_address AS traded_currency_address
+    , s1.token_bought_symbol AS traded_currency_symbol
+    , s1.taker AS frontrun_taker
+    , s1.tx_from AS frontrun_tx_from
+    , s2.taker AS backrun_taker
+    , s2.tx_from AS backrun_tx_from
+    , s1.index AS frontrun_index
+    , s2.index AS backrun_index
+    , s1.project_contract_address AS sandwiched_pool
+    , s1.tx_hash AS frontrun_tx_hash
+    , s2.tx_hash AS backrun_tx_hash
+    , s2.index-s1.index-1 AS amount_trades_between
+    , s1.gas_price
+    , s1.tx_fee AS frontrun_tx_fee
+    , s2.tx_fee AS backrun_tx_fee
+    FROM trades s1
+    INNER JOIN trades s2 ON s1.block_time=s2.block_time
+        AND s1.tx_hash!=s2.tx_hash
+        AND s1.index<s2.index
+        AND s1.project_contract_address=s2.project_contract_address
+        AND (s1.tx_from=s2.tx_from OR s1.taker=s2.taker)
+        AND s1.token_sold_address=s2.token_bought_address
+        AND s1.token_bought_address=s2.token_sold_address
+        AND s2.token_sold_amount BETWEEN s1.token_bought_amount*0.9 AND s1.token_bought_amount*1.1
+        AND s2.token_bought_amount > s1.token_sold_amount
+    INNER JOIN trades v ON v.block_time=s1.block_time
+        AND v.project_contract_address=s2.project_contract_address
+        AND v.evt_index>s1.evt_index
+        AND v.evt_index<s2.evt_index
+        AND v.token_sold_address=s1.token_sold_address
+        AND v.token_bought_address=s1.token_bought_address
+    )
+
+SELECT s1.*
+FROM sandwiches s1
+LEFT JOIN sandwiches s2 ON s1.block_time=s2.block_time
+    AND s1.frontrun_tx_hash=s2.backrun_tx_hash
+WHERE s2.backrun_index IS NULL
