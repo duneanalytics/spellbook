a681df4996f5a50d53e33e2b5de438d7eec07b06
Hildebert Moulié <h.moulie@gmail.com>
2023-05-21 01:14:22 +0200

    fix tests

--- models/dex/dex_sandwiches.sql
+++ models/dex/dex_sandwiches.sql
@@ -4,7 +4,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain', 'tx_hash', 'evt_index'],
+        unique_key = ['blockchain', 'frontrun_tx_hash', 'frontrun_index'],
         post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "fantom", "polygon"]\',
                                 "sector",
                                 "dex",
