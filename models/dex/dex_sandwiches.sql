9e65efd336f55ab1c4df32270ab1aa28104f8a22
Hildebert Moulié <h.moulie@gmail.com>
2023-05-21 12:32:51 +0200

    fix

--- models/dex/dex_sandwiches.sql
+++ models/dex/dex_sandwiches.sql
@@ -291,14 +291,15 @@ WITH trades AS (
     , s1.block_date
     , s1.block_time
     , s1.block_number
-    , CASE WHEN s1.token_bought_amount_raw > 0 THEN s2.token_sold_amount_raw/s1.token_bought_amount_raw END AS ratio_bought_sold
+    , CASE WHEN s1.token_bought_amount_raw > 0 THEN s2.token_sold_amount_raw/s1.token_bought_amount_raw END AS ratio_traded_token
+    , CASE WHEN s1.token_sold_amount_raw > 0 THEN s2.token_bought_amount_raw/s1.token_sold_amount_raw END AS profit_percentage_of_initial
     , s2.token_bought_amount_raw-s1.token_sold_amount_raw AS profit_amount_raw
     , s2.token_bought_amount-s1.token_sold_amount AS profit_amount
-    , s1.token_bought_address AS currency_address
+    , s1.token_sold_address AS currency_address
     , s1.token_sold_symbol AS currency_symbol
     , s1.token_bought_amount_raw-s2.token_sold_amount_raw AS profit_traded_currency_amount_raw
     , s1.token_bought_amount-s2.token_sold_amount AS profit_traded_currency_amount
-    , s1.token_sold_address AS traded_currency_address
+    , s1.token_bought_address AS traded_currency_address
     , s1.token_bought_symbol AS traded_currency_symbol
     , s1.taker AS frontrun_taker
     , s1.tx_from AS frontrun_tx_from
