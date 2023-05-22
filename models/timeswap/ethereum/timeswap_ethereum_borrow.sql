aef76cd156b1a7a79bfed068d0636d77dc125b07
RaveenaBhasin <raveenabhasin15@gmail.com>
2023-05-22 16:39:37 +0530

    fix: fix runtime error caused by double quotes

--- models/timeswap/ethereum/timeswap_ethereum_borrow.sql
+++ models/timeswap/ethereum/timeswap_ethereum_borrow.sql
@@ -8,26 +8,26 @@
 }}
 
 SELECT
-  b.evt_tx_hash as "Transaction_Hash",
-  b.evt_block_time as "Time",
-  b.isToken0 as "Token_0",
-  b.maturity as "maturity",
-  b.strike as "strike",
-  b.from as "User_Address",
-  i.pool_pair as "Pool_Pair",
-  i.chain as "Chain",
+  b.evt_tx_hash as Transaction_Hash,
+  b.evt_block_time as Time,
+  b.isToken0 as Token_0,
+  b.maturity as maturity,
+  b.strike as strike,
+  b.from as User_Address,
+  i.pool_pair as Pool_Pair,
+  i.chain as Chain,
   CAST(
     CASE
       WHEN CAST(b.isToken0 AS BOOLEAN) = true THEN CAST(b.tokenAmount AS DOUBLE) / power(10,i.token0_decimals)
       ELSE CAST(b.tokenAmount AS DOUBLE) / power(10,i.token1_decimals)
     END as DOUBLE
-  ) as "Token_Amount",
+  ) as Token_Amount,
   CAST(
     CASE
       WHEN CAST(b.isToken0 AS BOOLEAN) = true THEN CAST(b.tokenAmount AS DOUBLE) / power(10,i.token0_decimals) * p.price
       ELSE CAST(b.tokenAmount AS DOUBLE) / power(10,i.token1_decimals) * p.price
     END as DOUBLE
-  ) as "USD_Amount"
+  ) as USD_Amount
   FROM {{ source('timeswap_ethereum', 'TimeswapV2PeripheryUniswapV3BorrowGivenPrincipal_evt_BorrowGivenPrincipal') }} b
   JOIN {{ ref('timeswap_ethereum_pools') }} i ON CAST(b.contract_address AS VARCHAR(100)) = i.borrow_contract_address
   JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', b.evt_block_time)
@@ -36,26 +36,26 @@ SELECT
 UNION
  
 SELECT
-  b.evt_tx_hash as "Transaction_Hash",
-  b.evt_block_time as "Time",
-  b.isToken0 as "Token_0",
-  b.maturity as "maturity",
-  b.strike as "strike",
-  b.from as "User_Address",
-  i.pool_pair as "Pool_Pair",
-  i.chain as "Chain",
+  b.evt_tx_hash as Transaction_Hash,
+  b.evt_block_time as Time,
+  b.isToken0 as Token_0,
+  b.maturity as maturity,
+  b.strike as strike,
+  b.from as User_Address,
+  i.pool_pair as Pool_Pair,
+  i.chain as Chain,
   CAST(
     CASE
       WHEN CAST(b.isToken0 AS BOOLEAN) = true THEN CAST(b.tokenAmount AS DOUBLE) / power(10,i.token0_decimals)
       ELSE CAST(b.tokenAmount AS DOUBLE) / power(10,i.token1_decimals)
     END as DOUBLE
-  ) as "Token_Amount",
+  ) as Token_Amount,
   CAST(
     CASE
       WHEN CAST(b.isToken0 AS BOOLEAN) = true THEN CAST(b.tokenAmount AS DOUBLE) / power(10,i.token0_decimals) * p.price
       ELSE CAST(b.tokenAmount AS DOUBLE) / power(10,i.token1_decimals) * p.price
     END as DOUBLE
-  ) as "USD_Amount"
+  ) as USD_Amount
   FROM {{ source('timeswap_ethereum', 'TimeswapV2PeripheryUniswapV3BorrowGivenPrincipal_evt_BorrowGivenPrincipal') }} b
   JOIN {{ ref('timeswap_ethereum_pools') }} i ON CAST(b.contract_address AS VARCHAR(100)) = i.borrow_contract_address
   JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', b.evt_block_time)
--- models/timeswap/ethereum/timeswap_ethereum_lend.sql
+++ models/timeswap/ethereum/timeswap_ethereum_lend.sql
@@ -8,26 +8,26 @@
 }}
 
 SELECT
-  l.evt_tx_hash as "Transaction_Hash",
-  l.evt_block_time as "Time",
-  l.isToken0 as "Token_0",
-  l.maturity as "maturity",
-  l.strike as "strike",
-  l.from as "User_Address",
-  i.pool_pair as "Pool_Pair",
-  i.chain as "Chain",
+  l.evt_tx_hash as Transaction_Hash,
+  l.evt_block_time as Time,
+  l.isToken0 as Token_0,
+  l.maturity as maturity,
+  l.strike as strike,
+  l.from as User_Address,
+  i.pool_pair as Pool_Pair,
+  i.chain as Chain,
   CAST(
     CASE
       WHEN CAST(l.isToken0 AS BOOLEAN) = true THEN CAST(l.tokenAmount AS DOUBLE) / power(10,i.token0_decimals)
       ELSE CAST(l.tokenAmount AS DOUBLE) / power(10,i.token1_decimals)
     END as DOUBLE
-  ) as "Token_Amount",
+  ) as Token_Amount,
   CAST(
     CASE
       WHEN CAST(l.isToken0 AS BOOLEAN) = true THEN CAST(l.tokenAmount AS DOUBLE) / power(10,i.token0_decimals) * p.price
       ELSE CAST(l.tokenAmount AS DOUBLE) / power(10,i.token1_decimals) * p.price
     END as DOUBLE
-  ) as "USD_Amount"
+  ) as USD_Amount
   FROM {{ source('timeswap_ethereum', 'TimeswapV2PeripheryUniswapV3LendGivenPrincipal_evt_LendGivenPrincipal') }} l
   JOIN {{ ref('timeswap_ethereum_pools') }} i ON CAST(l.contract_address AS VARCHAR(100)) = i.borrow_contract_address
   JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', l.evt_block_time)
@@ -36,26 +36,26 @@ SELECT
 UNION
  
 SELECT
-  l.evt_tx_hash as "Transaction_Hash",
-  l.evt_block_time as "Time",
-  l.isToken0 as "Token_0",
-  l.maturity as "maturity",
-  l.strike as "strike",
-  l.from as "User_Address",
-  i.pool_pair as "Pool_Pair",
-  i.chain as "Chain",
+  l.evt_tx_hash as Transaction_Hash,
+  l.evt_block_time as Time,
+  l.isToken0 as Token_0,
+  l.maturity as maturity,
+  l.strike as strike,
+  l.from as User_Address,
+  i.pool_pair as Pool_Pair,
+  i.chain as Chain,
   CAST(
     CASE
       WHEN CAST(l.isToken0 AS BOOLEAN) = true THEN CAST(l.tokenAmount AS DOUBLE) / power(10,i.token0_decimals)
       ELSE CAST(l.tokenAmount AS DOUBLE) / power(10,i.token1_decimals)
     END as DOUBLE
-  ) as "Token_Amount",
+  ) as Token_Amount,
   CAST(
     CASE
       WHEN CAST(l.isToken0 AS BOOLEAN) = true THEN CAST(l.tokenAmount AS DOUBLE) / power(10,i.token0_decimals) * p.price
       ELSE CAST(l.tokenAmount AS DOUBLE) / power(10,i.token1_decimals) * p.price
     END as DOUBLE
-  ) as "USD_Amount"
+  ) as USD_Amount
   FROM {{ source('timeswap_ethereum', 'TimeswapV2PeripheryUniswapV3LendGivenPrincipal_evt_LendGivenPrincipal') }} l
   JOIN {{ ref('timeswap_ethereum_pools') }} i ON CAST(l.contract_address AS VARCHAR(100)) = i.borrow_contract_address
   JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', l.evt_block_time)
