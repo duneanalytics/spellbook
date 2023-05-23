89790316a8d08e2eded875cf679c6a01084cb8ab
Rantum <rantum@rantum.xyz>
2023-05-23 10:25:53 -0700

    zeroex - fix for txs with multiple affiliate addresses [poly, eth] (#3356)
    * initial Socialtokens view
    
    * Rename socialTokens to socialtokens.sql
    
    * Addded new tokens to list
    
    * Update socialtokens.sql
    
    * rmv social tokens file
    
    * add zeroex to dex_aggregator.trades
    
    * add distinct on poly
    
    * update schema
    
    * test remove exclude
    
    * test remove exclude
    
    * update aff address select
    
    * revise edits to correct files
    
    * update to/from
    
    * update from in taker
    
    * revise seed
    
    * revise seed
    
    * revise seed
    
    * revise taker
    
    * remove invalid tx from seed
    
    * single affiiliate address
    
    * remove exclude tag
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/zeroex/ethereum/zeroex_ethereum_api_fills.sql
+++ models/zeroex/ethereum/zeroex_ethereum_api_fills.sql
@@ -461,7 +461,7 @@ SELECT
         maker_token_amount_raw / pow(10, mp.decimals) AS maker_token_amount,
         maker_token_amount_raw,
         all_tx.type,
-        affiliate_address,
+        max(affiliate_address) over (partition by all_tx.tx_hash) as affiliate_address,
         swap_flag,
         matcha_limit_order_flag,
         CASE WHEN maker_token IN ('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2','0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48','0xdac17f958d2ee523a2206206994597c13d831ec7',
