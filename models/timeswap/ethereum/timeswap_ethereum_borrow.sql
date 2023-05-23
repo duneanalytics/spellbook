f44f04be42863a60eba9e54528ea220a54d4e4d3
RaveenaBhasin <raveenabhasin15@gmail.com>
2023-05-23 10:23:25 +0530

    feat: add incremental logic in lend and borrow model

--- models/timeswap/ethereum/timeswap_ethereum_borrow.sql
+++ models/timeswap/ethereum/timeswap_ethereum_borrow.sql
@@ -1,5 +1,9 @@
 {{ config(
     alias = 'borrow',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['Transaction_Hash'],
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "timeswap",
@@ -32,6 +36,9 @@ SELECT
   LEFT JOIN {{ ref('timeswap_ethereum_pools') }} i ON CAST(b.contract_address AS VARCHAR(100)) = i.borrow_contract_address
   LEFT JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', b.evt_block_time)
   WHERE p.symbol=i.token0_symbol AND p.blockchain = 'ethereum' AND b.isToken0 = true
+  {% if is_incremental() %}
+    AND b.evt_block_time >= date_trunc("day", now() - interval '1 week')
+  {% endif %}
 
 UNION
  
@@ -60,3 +67,6 @@ SELECT
   LEFT JOIN {{ ref('timeswap_ethereum_pools') }} i ON CAST(b.contract_address AS VARCHAR(100)) = i.borrow_contract_address
   LEFT JOIN {{ source('prices', 'usd') }} p ON p.minute = date_trunc('minute', b.evt_block_time)
   WHERE p.symbol=i.token1_symbol AND p.blockchain = 'ethereum' AND b.isToken0 = false
+  {% if is_incremental() %}
+    AND b.evt_block_time >= date_trunc("day", now() - interval '1 week')
+  {% endif %}
