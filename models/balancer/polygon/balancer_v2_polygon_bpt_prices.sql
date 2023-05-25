96387a50312c6cc85cc1bfca3692d5c9c40cb666
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-05-25 15:57:34 -0400

    Update balancer_v2_polygon_bpt_prices.sql

--- models/balancer/polygon/balancer_v2_polygon_bpt_prices.sql
+++ models/balancer/polygon/balancer_v2_polygon_bpt_prices.sql
@@ -2,6 +2,8 @@
     config(
         schema = 'balancer_v2_polygon',
         alias='bpt_prices',
+        materialized = 'table',
+        file_format = 'delta',
         post_hook='{{ expose_spells_hide_trino(\'["polygon"]\',
                                     "project",
                                     "balancer_v2",
