f9a16e3e167fca9d97b3d6ea312977a67beb2692
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-05-25 16:07:37 -0400

    Update balancer_v2_ethereum_bpt_prices.sql

--- models/balancer/ethereum/balancer_v2_ethereum_bpt_prices.sql
+++ models/balancer/ethereum/balancer_v2_ethereum_bpt_prices.sql
@@ -1,10 +1,10 @@
 {{
     config(
         schema = 'balancer_v2_ethereum',
-        alias='bpt_prices',
-        materialized = 'table',
+        alias = 'bpt_prices',
+        materialized = 'view',
         file_format = 'delta',
-        post_hook='{{ expose_spells_hide_trino(\'["ethereum"]\',
+        post_hook = '{{ expose_spells_hide_trino(\'["ethereum"]\',
                                     "project",
                                     "balancer_v2",
                                     \'["victorstefenon", "thetroyharris"]\') }}'
