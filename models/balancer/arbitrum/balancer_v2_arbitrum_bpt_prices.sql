8fe83a30e3823af5ded265133619d94d14af6e04
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-05-25 16:07:53 -0400

    Update balancer_v2_arbitrum_bpt_prices.sql

--- models/balancer/arbitrum/balancer_v2_arbitrum_bpt_prices.sql
+++ models/balancer/arbitrum/balancer_v2_arbitrum_bpt_prices.sql
@@ -1,10 +1,10 @@
 {{
     config(
         schema = 'balancer_v2_arbitrum',
-        alias='bpt_prices',
-        materialized = 'table',
+        alias = 'bpt_prices',
+        materialized = 'view',
         file_format = 'delta',
-        post_hook='{{ expose_spells_hide_trino(\'["arbitrum"]\',
+        post_hook = '{{ expose_spells_hide_trino(\'["arbitrum"]\',
                                     "project",
                                     "balancer_v2",
                                     \'["victorstefenon", "thetroyharris"]\') }}'
