799a35ba4b7c954c3367c178a3a47357cc282126
hildobby <h.moulie@gmail.com>
2023-05-25 20:38:00 +0200

    Create `airdrop.claims` with 38 airdrops (#2979)
    * create airdrop_claims.all
    
    * fix
    
    * more fixes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixx
    
    * add safe
    
    * fix
    
    * refactor
    
    * fix
    
    * add galxe to dbt_project
    
    * fix
    
    * fix
    
    * cast as doubles and remove post airdrop token movements
    
    * add 13 airdrops
    
    * fix
    
    * add readme draft
    
    * estend ci timeout
    
    * change from quantity to (amount_raw, amount_original & amount_usd)
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add earliest price for pre-trading claims
    
    * fix
    
    * fix
    
    * dex.prices for tokens not on coinpaprika
    
    * fix
    
    * fix
    
    * turn finished airdrops into static tables
    
    * add genie/uniswap airdrop
    
    * added gnosis, bnb and avalanche airdrops so it`s easy to work with going forward
    
    * extend ci timeout
    
    * fixes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * change ci timeout back to 1h
    
    * fox
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

---
+++ models/cow_protocol/ethereum/cow_protocol_ethereum_airdrop_claims.sql
@@ -0,0 +1,45 @@
+{{
+    config(
+        alias='airdrop_claims',
+        materialized = 'table',
+        file_format = 'delta',
+        tags=['static'],
+        unique_key = ['recipient', 'tx_hash', 'evt_index'],
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "project",
+                                "cow_protocol",
+                                \'["hildobby"]\') }}'
+    )
+}}
+
+{% set cow_token_address = '0xdef1ca1fb7fbcdc777520aa7f396b4e015f497ab' %}
+
+WITH early_price AS (
+    SELECT MIN(minute) AS minute
+    , MIN_BY(price, minute) AS price
+    FROM {{ source('prices', 'usd') }}
+    WHERE blockchain = 'ethereum'
+    AND contract_address='{{cow_token_address}}'
+    )
+
+SELECT 'ethereum' AS blockchain
+, t.evt_block_time AS block_time
+, t.evt_block_number AS block_number
+, 'CoW Protocol' AS project
+, 'CoW Protocol Airdrop' AS airdrop_identifier
+, t.claimant AS recipient
+, t.contract_address
+, t.evt_tx_hash AS tx_hash
+, CAST(t.claimedAmount AS DECIMAL(38,0)) AS amount_raw
+, CAST(t.claimedAmount/POWER(10, 18) AS double) AS amount_original
+, CASE WHEN t.evt_block_time >= (SELECT minute FROM early_price) THEN CAST(pu.price*t.claimedAmount/POWER(10, 18) AS double)
+    ELSE CAST((SELECT price FROM early_price)*t.claimedAmount/POWER(10, 18) AS double)
+    END AS amount_usd
+, '{{cow_token_address}}' AS token_address
+, 'COW' AS token_symbol
+, t.evt_index
+FROM {{ source('cow_protocol_ethereum', 'CowProtocolVirtualToken_evt_Claimed') }} t
+LEFT JOIN {{ ref('prices_usd_forward_fill') }} pu ON pu.blockchain = 'ethereum'
+    AND pu.contract_address='{{cow_token_address}}'
+    AND pu.minute=date_trunc('minute', t.evt_block_time)
+WHERE t.evt_block_time BETWEEN '2022-02-11' AND '2022-03-26'
---
+++ models/tornado_cash/ethereum/tornado_cash_ethereum_airdrop_claims.sql
@@ -0,0 +1,45 @@
+{{
+    config(
+        alias='airdrop_claims',
+        materialized = 'table',
+        file_format = 'delta',
+        tags=['static'],
+        unique_key = ['recipient', 'tx_hash', 'evt_index'],
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "project",
+                                "tornado_cash",
+                                \'["hildobby"]\') }}'
+    )
+}}
+
+{% set torn_token_address = '0x77777feddddffc19ff86db637967013e6c6a116c' %}
+
+WITH early_price AS (
+    SELECT MIN(minute) AS minute
+    , MIN_BY(price, minute) AS price
+    FROM {{ source('prices', 'usd') }}
+    WHERE blockchain = 'ethereum'
+    AND contract_address='{{torn_token_address}}'
+    )
+
+SELECT 'ethereum' AS blockchain
+, t.evt_block_time AS block_time
+, t.evt_block_number AS block_number
+, 'Tornado Cash' AS project
+, 'Tornado Cash Airdrop' AS airdrop_identifier
+, t.from AS recipient
+, t.contract_address
+, t.evt_tx_hash AS tx_hash
+, CAST(t.value AS DECIMAL(38,0)) AS amount_raw
+, CAST(t.value/POWER(10, 18) AS double) AS amount_original
+, CASE WHEN t.evt_block_time >= (SELECT minute FROM early_price) THEN CAST(pu.price*t.value/POWER(10, 18) AS double)
+    ELSE CAST((SELECT price FROM early_price)*t.value/POWER(10, 18) AS double)
+    END AS amount_usd
+, '{{torn_token_address}}' AS token_address
+, 'TORN' AS token_symbol
+, t.evt_index
+FROM {{ source('erc20_ethereum', 'evt_transfer') }} t
+LEFT JOIN {{ ref('prices_usd_forward_fill') }} pu ON pu.blockchain = 'ethereum'
+    AND pu.contract_address='{{torn_token_address}}'
+    AND pu.minute=date_trunc('minute', t.evt_block_time)
+WHERE t.evt_block_time BETWEEN '2020-12-18' AND '2021-12-13'
