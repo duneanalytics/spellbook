34d53710878d53940c78a31b593079f576d29c1f
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-05-25 18:29:55 -0400

    keep name checks exact

--- models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
@@ -195,7 +195,7 @@ SELECT
     
     -- When tokens go to a 'Deployed' address, we assume deployed. Or to an end user from an address we don't already know to be deployed.
     CASE WHEN  ( to_label LIKE '%Other%' OR to_label LIKE '%Deployed%') ) AND from_label NOT LIKE '%Deployed%' THEN op_amount_decimal
-         WHEN (from_name NOT LIKE '%' || to_name || '%') AND from_label LIKE '%Project%' AND to_label LIKE '%Project%' THEN op_amount_decimal --handle for distirbutions to other projects (i.e. Uniswap to Gamma)
+         WHEN (from_name != to_name) AND from_label LIKE '%Project%' AND to_label LIKE '%Project%' THEN op_amount_decimal --handle for distirbutions to other projects (i.e. Uniswap to Gamma)
         ELSE 0 END
     AS op_deployed,
     
@@ -206,13 +206,13 @@ SELECT
     
     -- Tokens being transferred between projects (i.e. Uniswap to Gamma)
     CASE WHEN from_label LIKE '%Project%' AND to_label LIKE '%Project%'
-            AND from_name NOT LIKE '%' || to_name || '%' THEN op_amount_decimal
+            AND from_name != to_name THEN op_amount_decimal
         ELSE 0 END
     AS op_between_projects,
     
     -- Tokens going from being deployed back to the project
     CASE
-        WHEN from_label NOT LIKE '%Project%' and to_label LIKE'%Project%' AND from_name LIKE '%' || to_name || '%' THEN op_amount_decimal --Projects Clawback
+        WHEN from_label NOT LIKE '%Project%' and to_label LIKE'%Project%' AND from_name = to_name THEN op_amount_decimal --Projects Clawback
         WHEN from_type = 'OP Foundation Airdrops' AND to_label = '{{foundation_label}}' THEN op_amount_decimal --Airdrop Clawback
         ELSE 0 END
     AS op_incoming_clawback, --Project's deployer back to the OG project wallet
