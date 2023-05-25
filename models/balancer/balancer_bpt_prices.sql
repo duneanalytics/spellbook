2decd09f940f029af64153800747b49fc7a1d078
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-05-25 15:51:00 -0400

    Update balancer_bpt_prices.sql

--- models/balancer/balancer_bpt_prices.sql
+++ models/balancer/balancer_bpt_prices.sql
@@ -1,19 +1,19 @@
 {{ config(
-        schema = 'balancer',
-        alias ='bpt_prices',
-        post_hook='{{ expose_spells(\'["ethereum", "arbitrum", "optimism", "polygon", "gnosis"]\',
-                                "project",
-                                "balancer",
-                                \'["thetroyharris"]\') }}'
-        )
+    schema = 'balancer',
+    alias ='bpt_prices',
+    post_hook='{{ expose_spells(\'["ethereum", "arbitrum", "optimism", "polygon", "gnosis"]\',
+                            "project",
+                            "balancer",
+                            \'["thetroyharris"]\') }}'
+    )
 }}
 
 {% set balancer_models = [
-ref('balancer_v2_ethereum_bpt_prices'),
-ref('balancer_v2_optimism_bpt_prices'),
-ref('balancer_v2_arbitrum_bpt_prices'),
-ref('balancer_v2_polygon_bpt_prices'),
-ref('balancer_v2_gnosis_bpt_prices')
+    ref('balancer_v2_ethereum_bpt_prices'),
+    ref('balancer_v2_optimism_bpt_prices'),
+    ref('balancer_v2_arbitrum_bpt_prices'),
+    ref('balancer_v2_polygon_bpt_prices'),
+    ref('balancer_v2_gnosis_bpt_prices')
 ] %}
 
 
