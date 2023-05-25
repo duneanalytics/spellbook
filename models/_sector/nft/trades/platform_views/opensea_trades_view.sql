44268cb6b894e399075b0a55aa59444ffde5f48c
0xRob <83790096+0xRobin@users.noreply.github.com>
2023-05-25 20:36:20 +0200

    fix NFT platform views on Dune explorer (#3410)
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/_sector/nft/trades/platform_views/opensea_trades_view.sql
+++ models/_sector/nft/trades/platform_views/opensea_trades_view.sql
@@ -4,8 +4,8 @@
         alias ='trades',
         materialized = 'view',
         post_hook='{{ expose_spells(\'["ethereum", "solana", "polygon"]\',
-                                    "sector",
-                                    "nft",
+                                    "project",
+                                    "opensea",
                                     \'["0xRob"]\') }}')
 }}
 
