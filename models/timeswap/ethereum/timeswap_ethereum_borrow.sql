4baf7011a8fea74e57d8074b71c00c8f464c7e72
Raveena Bhasin <73697080+RaveenaBhasin@users.noreply.github.com>
2023-05-26 23:03:13 +0530

    Add Spell for Timeswap Labs (#3387)
    * feat: initialize spell for timeswap
    
    * update: update spell and schema adhering to required checks
    
    * feat: add models to schema
    
    * add missing description to borrow model
    
    * fix: fix view error in schema
    
    * fix: fix error in lend and borrow model
    
    * fix: fix runtime error caused by double quotes
    
    * fix: fix error caused by from
    
    * fix: use left join instead of join
    
    * fix: syntax error in lend model
    
    * feat: add incremental logic in lend and borrow model
    
    * fix: fix contract address in lend model
    
    * fix: fix join issue and update pool params
    
    * fix: fix datatype length error
    
    * feat: add spell for arbitrum
    
    * feat: add spell for polygon chain
    
    * fix: update column names in schema to lower_snake_case
    
    * Lower case transaction_hash
    
    * Add uniqueness tests
    
    * fix: fix uniqueness test keyword 'column_names' to combination_of_columns' & update addresses to lowercase
    
    * fix: fix unresolved column name in polygon borrow model
    
    * update: add transaction_type column in models
    
    * fix: fix chain name in post hook and lowercase hardcoded txn type
    
    * update: use incremental logic with prices table
    
    * fix: fix ambigous column in borrow model
    
    * add back incremental filter on source
    
    * fix: remove incremental logic from call based model in polygon
    
    * fix: fix error in borrow model polygon
    
    * add back incremental logic
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>

---
+++ models/timeswap/ethereum/timeswap_ethereum_borrow.sql
@@ -0,0 +1,84 @@
+{{ config(
+    alias = 'borrow',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['transaction_hash', 'pool_pair', 'maturity', 'strike'],
+    post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "project",
+                                "timeswap",
+                                \'["raveena15, varunhawk19"]\') }}'
+    )
+}}
+
+SELECT
+  b.evt_tx_hash as transaction_hash,
+  b.evt_block_time as time,
+  b.isToken0 as token_0,
+  'borrow' as transaction_type,
+  b.maturity as maturity,
+  b.strike as strike, 
+  i.pool_pair as pool_pair,
+  i.chain as chain,
+  CAST(
+    CASE
+      WHEN CAST(b.isToken0 AS BOOLEAN) = true THEN CAST(b.tokenAmount AS DOUBLE) / power(10,i.token0_decimals)
+      ELSE CAST(b.tokenAmount AS DOUBLE) / power(10,i.token1_decimals)
+    END as DOUBLE
+  ) as token_amount,
+  CAST(
+    CASE
+      WHEN CAST(b.isToken0 AS BOOLEAN) = true THEN CAST(b.tokenAmount AS DOUBLE) / power(10,i.token0_decimals) * p.price
+      ELSE CAST(b.tokenAmount AS DOUBLE) / power(10,i.token1_decimals) * p.price
+    END as DOUBLE
+  ) as usd_amount
+FROM {{ source('timeswap_ethereum', 'TimeswapV2PeripheryUniswapV3BorrowGivenPrincipal_evt_BorrowGivenPrincipal') }} b
+JOIN {{ ref('timeswap_ethereum_pools') }} i 
+  ON CAST(b.maturity as VARCHAR(100)) = i.maturity 
+  and cast(b.strike as VARCHAR(100)) = i.strike
+JOIN {{ source('prices', 'usd') }} p 
+  ON p.symbol=i.token0_symbol 
+  and p.blockchain = 'ethereum' 
+  and b.isToken0 = true
+  AND p.minute = date_trunc('minute',b.evt_block_time)
+  {% if is_incremental() %}
+  AND p.minute >= date_trunc("day", now() - interval '1 week')
+WHERE b.evt_block_time >= date_trunc("day", now() - interval '1 week')
+  {% endif %}
+
+UNION
+ 
+SELECT
+  b.evt_tx_hash as transaction_hash,
+  b.evt_block_time as time,
+  b.isToken0 as token_0,
+  'borrow' as transaction_type,
+  b.maturity as maturity,
+  b.strike as strike, 
+  i.pool_pair as pool_pair,
+  i.chain as chain,
+  CAST(
+    CASE
+      WHEN CAST(b.isToken0 AS BOOLEAN) = true THEN CAST(b.tokenAmount AS DOUBLE) / power(10,i.token0_decimals)
+      ELSE CAST(b.tokenAmount AS DOUBLE) / power(10,i.token1_decimals)
+    END as DOUBLE
+  ) as token_amount,
+  CAST(
+    CASE
+      WHEN CAST(b.isToken0 AS BOOLEAN) = true THEN CAST(b.tokenAmount AS DOUBLE) / power(10,i.token0_decimals) * p.price
+      ELSE CAST(b.tokenAmount AS DOUBLE) / power(10,i.token1_decimals) * p.price
+    END as DOUBLE
+  ) as usd_amount
+FROM {{ source('timeswap_ethereum', 'TimeswapV2PeripheryUniswapV3BorrowGivenPrincipal_evt_BorrowGivenPrincipal') }} b
+JOIN {{ ref('timeswap_ethereum_pools') }} i 
+  ON CAST(b.maturity as VARCHAR(100)) = i.maturity 
+  and cast(b.strike as VARCHAR(100)) = i.strike
+JOIN {{ source('prices', 'usd') }} p 
+  ON p.symbol=i.token1_symbol 
+  and p.blockchain = 'ethereum' 
+  and b.isToken0 = false
+  AND p.minute = date_trunc('minute',b.evt_block_time)
+  {% if is_incremental() %}
+  AND p.minute >= date_trunc("day", now() - interval '1 week')
+WHERE b.evt_block_time >= date_trunc("day", now() - interval '1 week')
+  {% endif %}
