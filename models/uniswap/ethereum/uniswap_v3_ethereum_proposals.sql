eea705760e3a15b2ab0cb9f55ccd6cd521389e75
Augusto <augustog@users.noreply.github.com>
2023-05-26 14:17:17 +0200

    Convert dao proposals and dao votes models to table

--- models/uniswap/ethereum/uniswap_v3_ethereum_proposals.sql
+++ models/uniswap/ethereum/uniswap_v3_ethereum_proposals.sql
@@ -2,10 +2,8 @@
     schema = 'uniswap_v3_ethereum',
     alias = 'proposals',
     partition_by = ['block_date'],
-    materialized = 'incremental',
+    materialized = 'table',
     file_format = 'delta',
-    incremental_strategy = 'merge',
-    unique_key = ['created_at', 'blockchain', 'project', 'version', 'tx_hash'],
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "uniswap_v3",
@@ -69,6 +67,3 @@ LEFT JOIN cte_sum_votes csv ON csv.proposalId = pcr.id
 LEFT JOIN {{ source('uniswap_v3_ethereum', 'GovernorBravoDelegate_evt_ProposalCanceled') }} pca ON pca.id = pcr.id
 LEFT JOIN {{ source('uniswap_v3_ethereum', 'GovernorBravoDelegate_evt_ProposalExecuted') }} pex ON pex.id = pcr.id
 LEFT JOIN {{ source('uniswap_v3_ethereum', 'GovernorBravoDelegate_evt_ProposalQueued') }} pqu ON pex.id = pcr.id
-{% if is_incremental() %}
-WHERE pcr.evt_block_time > (select max(created_at) from {{ this }})
-{% endif %}
