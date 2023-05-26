6bb95cacd6a6ed0d771e975d8588478f04050643
Hildebert Moulié <h.moulie@gmail.com>
2023-05-26 15:43:31 +0200

    account for different number of columns across trnasactions tables

--- models/evms/evms_transactions.sql
+++ models/evms/evms_transactions.sql
@@ -8,18 +8,257 @@
         )
 }}
 
-SELECT 'ethereum' AS blockchain, * FROM {{source('ethereum', 'transactions')}}
+SELECT 'ethereum' AS blockchain
+, access_list
+, block_hash
+, data
+, "from"
+, hash
+, to
+, block_number
+, block_time
+, gas_limit
+, gas_price
+, gas_used
+, index
+, max_fee_per_gas
+, max_priority_fee_per_gas
+, nonce
+, priority_fee_per_gas
+, success
+, "type"
+, value
+, NULL AS l1_tx_origin
+, NULL AS l1_fee_scalar
+, NULL AS l1_block_number
+, NULL AS l1_fee
+, NULL AS l1_gas_price
+, NULL AS l1_gas_used
+, NULL AS l1_timestamp
+, NULL AS effective_gas_price
+FROM {{source('ethereum', 'transactions')}}
+
 UNION ALL
-SELECT 'polygon' AS blockchain, * FROM {{ source('polygon', 'transactions') }}
+
+SELECT 'polygon' AS blockchain
+, access_list
+, block_hash
+, data
+, "from"
+, hash
+, to
+, block_number
+, block_time
+, gas_limit
+, gas_price
+, gas_used
+, index
+, max_fee_per_gas
+, max_priority_fee_per_gas
+, nonce
+, priority_fee_per_gas
+, success
+, "type"
+, value
+, NULL AS l1_tx_origin
+, NULL AS l1_fee_scalar
+, NULL AS l1_block_number
+, NULL AS l1_fee
+, NULL AS l1_gas_price
+, NULL AS l1_gas_used
+, NULL AS l1_timestamp
+, NULL AS effective_gas_price
+FROM {{ source('polygon', 'transactions') }}
+
 UNION ALL
-SELECT 'bnb' AS blockchain, * FROM {{ source('bnb', 'transactions') }}
+
+
+SELECT 'bnb' AS blockchain
+, access_list
+, block_hash
+, data
+, "from"
+, hash
+, to
+, block_number
+, block_time
+, gas_limit
+, gas_price
+, gas_used
+, index
+, max_fee_per_gas
+, max_priority_fee_per_gas
+, nonce
+, priority_fee_per_gas
+, success
+, "type"
+, value
+, NULL AS l1_tx_origin
+, NULL AS l1_fee_scalar
+, NULL AS l1_block_number
+, NULL AS l1_fee
+, NULL AS l1_gas_price
+, NULL AS l1_gas_used
+, NULL AS l1_timestamp
+, NULL AS effective_gas_price
+FROM {{ source('bnb', 'transactions') }}
+
 UNION ALL
-SELECT 'avalanche_c' AS blockchain, * FROM {{ source('avalanche_c', 'transactions') }}
+
+SELECT 'avalanche_c' AS blockchain
+, access_list
+, block_hash
+, data
+, "from"
+, hash
+, to
+, block_number
+, block_time
+, gas_limit
+, gas_price
+, gas_used
+, index
+, max_fee_per_gas
+, max_priority_fee_per_gas
+, nonce
+, priority_fee_per_gas
+, success
+, "type"
+, value
+, NULL AS l1_tx_origin
+, NULL AS l1_fee_scalar
+, NULL AS l1_block_number
+, NULL AS l1_fee
+, NULL AS l1_gas_price
+, NULL AS l1_gas_used
+, NULL AS l1_timestamp
+, NULL AS effective_gas_price
+FROM {{ source('avalanche_c', 'transactions') }}
+
 UNION ALL
-SELECT 'gnosis' AS blockchain, * FROM {{ source('gnosis', 'transactions') }}
+
+SELECT 'gnosis' AS blockchain
+, access_list
+, block_hash
+, data
+, "from"
+, hash
+, to
+, block_number
+, block_time
+, gas_limit
+, gas_price
+, gas_used
+, index
+, max_fee_per_gas
+, max_priority_fee_per_gas
+, nonce
+, priority_fee_per_gas
+, success
+, "type"
+, value
+, NULL AS l1_tx_origin
+, NULL AS l1_fee_scalar
+, NULL AS l1_block_number
+, NULL AS l1_fee
+, NULL AS l1_gas_price
+, NULL AS l1_gas_used
+, NULL AS l1_timestamp
+, NULL AS effective_gas_price
+FROM {{ source('gnosis', 'transactions') }}
+
 UNION ALL
-SELECT 'fantom' AS blockchain, * FROM {{ source('fantom', 'transactions') }}
+
+SELECT 'fantom' AS blockchain
+, access_list
+, block_hash
+, data
+, "from"
+, hash
+, to
+, block_number
+, block_time
+, gas_limit
+, gas_price
+, gas_used
+, index
+, max_fee_per_gas
+, max_priority_fee_per_gas
+, nonce
+, priority_fee_per_gas
+, success
+, "type"
+, value
+, NULL AS l1_tx_origin
+, NULL AS l1_fee_scalar
+, NULL AS l1_block_number
+, NULL AS l1_fee
+, NULL AS l1_gas_price
+, NULL AS l1_gas_used
+, NULL AS l1_timestamp
+, NULL AS effective_gas_price
+FROM {{ source('fantom', 'transactions') }}
+
 UNION ALL
-SELECT 'optimism' AS blockchain, * FROM {{ source('optimism', 'transactions') }}
+
+SELECT 'optimism' AS blockchain
+, access_list
+, block_hash
+, data
+, "from"
+, hash
+, to
+, block_number
+, block_time
+, gas_limit
+, gas_price
+, gas_used
+, index
+, max_fee_per_gas
+, max_priority_fee_per_gas
+, nonce
+, priority_fee_per_gas
+, success
+, "type"
+, value
+,l1_tx_origin
+, l1_fee_scalar
+, l1_block_number
+, l1_fee
+, l1_gas_price
+, l1_gas_used
+, l1_timestamp
+, NULL AS effective_gas_price
+FROM {{ source('optimism', 'transactions') }}
+
 UNION ALL
-SELECT 'arbitrum' AS blockchain, * FROM {{ source('arbitrum', 'transactions') }}
+
+SELECT 'arbitrum' AS blockchain
+, access_list
+, block_hash
+, data
+, "from"
+, hash
+, to
+, block_number
+, block_time
+, gas_limit
+, gas_price
+, gas_used
+, index
+, max_fee_per_gas
+, max_priority_fee_per_gas
+, nonce
+, priority_fee_per_gas
+, success
+, "type"
+, value
+, NULL AS l1_tx_origin
+, NULL AS l1_fee_scalar
+, NULL AS l1_block_number
+, NULL AS l1_fee
+, NULL AS l1_gas_price
+, gas_used_for_l1 AS l1_gas_used
+, NULL AS l1_timestamp
+, effective_gas_price
+FROM {{ source('arbitrum', 'transactions') }}
