37da55971e9bc519c9f3767fc2003852e30019ae
0xroll <96259407+0xroll@users.noreply.github.com>
2023-05-30 01:50:55 +0800

    Update tokens_arbitrum_erc20.sql
    adding winr into tokens.erc20

--- models/tokens/arbitrum/tokens_arbitrum_erc20.sql
+++ models/tokens/arbitrum/tokens_arbitrum_erc20.sql
@@ -116,5 +116,6 @@ FROM (VALUES
         ,('0x65c936f008bc34fe819bce9fa5afd9dc2d49977f', 'Y2K', 18)
         ,('0xda51015b73ce11f77a115bb1b8a7049e02ddecf0', 'NEU', 18)
         ,('0x7a5D193fE4ED9098F7EAdC99797087C96b002907', 'plsARB', 18)
-        ,('0x088cd8f5eF3652623c22D48b1605DCfE860Cd704', 'VELA', 18)
+        ,('0x088cd8f5eF3652623c22D48b1605DCfE860Cd704', 'VELA', 18),
+        ,('0xd77b108d4f6cefaa0cae9506a934e825becca46e', 'WINR', 18)
      ) AS temp_table (contract_address, symbol, decimals)
