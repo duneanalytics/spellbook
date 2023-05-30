b06a95427203bff7a4c34fdb4b3a7ddf32284358
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-05-30 12:34:10 -0400

    add holo

--- models/contracts/optimism/contracts_optimism_contract_creator_address_list.sql
+++ models/contracts/optimism/contracts_optimism_contract_creator_address_list.sql
@@ -480,6 +480,8 @@ WITH curated_list AS (
       ,('0x6b8a8035601Da0c9d31c5fc11fA66Aae485654fc', 'Cozy Finance')
       ,('0xa2bcad247c64da44739e141b491fe4238a4d0480', 'Lyra Finance')
       ,('0x7d5501e0bc56d0e4de909b748f48c768c9fde841', 'fxdx')
+      ,('0xdeadfed00b7848545d8fe5cb4e7c51ed37e5a2fa', 'Holograph')
+      ,('0x0c8af56f7650a6e3685188d212044338c21d3f73', 'Holograph')
 
   ) as temp_table (creator_address, contract_project)
 
