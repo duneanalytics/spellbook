d56761263ecb2912fbbaecb4374e43b6b7a84cd7
Hildebert Moulié <h.moulie@gmail.com>
2023-05-31 01:06:02 +0200

    rerun

--- models/dex/dex_sandwiches.sql
+++ models/dex/dex_sandwiches.sql
@@ -4,7 +4,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain', 'sandwiched_pool', 'frontrun_tx_hash', 'frontrun_index', 'frontrun_taker', 'currency_address'],
+        unique_key = ['blockchain', 'sandwiched_pool', 'frontrun_tx_hash', 'frontrun_taker', 'frontrun_index', 'currency_address'],
         post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "fantom", "polygon"]\',
                                 "sector",
                                 "dex",
