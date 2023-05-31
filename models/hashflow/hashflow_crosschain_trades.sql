9d0d46152fc384278cdfde67ebc924a28e32dea1
Broderick Bonelli <broderickbonelli@gmail.com>
2023-05-31 05:02:59 -0700

    list out columns on hashflow_crosschain_trades.sql model

--- models/hashflow/hashflow_crosschain_trades.sql
+++ models/hashflow/hashflow_crosschain_trades.sql
@@ -18,7 +18,21 @@
 
 {% for ref in cross_chain_models %}
 SELECT 
-    *
+    block_date
+    ,block_time
+    ,token_bought_symbol
+    ,token_sold_symbol
+    ,token_bought_amount
+    ,token_sold_amount
+    ,token_bought_amount_raw
+    ,token_sold_amount_raw
+    ,amount_usd
+    ,token_bought_address
+    ,token_sold_address
+    ,trader
+    ,tx_hash
+    ,source_chain
+    ,destination_chain
 FROM {{ ref }}
 {% if not loop.last %}
     UNION ALL
