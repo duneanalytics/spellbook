ddd0500263ca38455557360dab5aebf7df035a8b
Hildebert Moulié <h.moulie@gmail.com>
2023-06-02 01:04:56 +0200

    fix

--- models/gas/ethereum/gas_ethereum_fees_traces.sql
+++ models/gas/ethereum/gas_ethereum_fees_traces.sql
@@ -9,31 +9,40 @@
 }}
 
 SELECT 'ethereum' AS blockchain
-, etg.block_time
-, date_trunc('day', etg.block_time) AS block_date
-, etg.block_number
-, etg.tx_hash
-, MAX(etg.to) AS trace_to
-, etg.trace
-, SUM(etg.gas_used) AS gas_used
-, (SUM(etg.gas_used)*etc.gas_price)/1e18 AS spent_gas_fee
-, (AVG(pu.price)*SUM(etg.gas_used)*etc.gas_price)/POWER(10, 18) AS spent_gas_fee_usd
+, traces.block_time
+, date_trunc('day', traces.block_time) AS block_date
+, traces.block_number
+, traces.tx_hash
+, MAX(traces.from) AS trace_from
+, MAX(traces.to) AS trace_to
+, txs.from AS tx_from
+, txs.to AS tx_to
+, traces.trace
+, bytearray_substring(traces.input,1,4) AS trace_method
+, bytearray_substring(txs.data,1,4) AS tx_method
+, SUM(traces.gas_used) AS gas_used
+, txs.gas_used AS tx_gas_used
+, SUM(traces.gas_used)/txs.gas_used AS gas_used_tx_percentage
+, txs.gas_price AS tx_gas_price
+, (SUM(traces.gas_used)*txs.gas_price)/1e18 AS spent_gas_fee
+, (AVG(pu.price)*SUM(traces.gas_used)*txs.gas_price)/POWER(10, 18) AS spent_gas_fee_usd
 FROM (
-     SELECT et.to 
+     SELECT et.from
+     , et.to 
      , et.tx_hash
      , et.trace_address AS trace
      , et.gas_used
      , et.block_time
      , et.block_number
      FROM {{ source('ethereum','traces') }} et
-     WHERE et.to IS NOT NULL
      {% if is_incremental() %}
      AND et.block_time >= date_trunc("day", NOW() - interval '1' week)
      {% endif %}
      
      UNION ALL
      
-     SELECT CAST(NULL AS varchar(1)) AS to 
+     SELECT CAST(NULL AS varchar(1)) AS from 
+     , CAST(NULL AS varchar(1)) AS to 
      , et.tx_hash
      , slice(et.trace_address, 1, cardinality(et.trace_address) - 1) AS trace
      , -et.gas_used AS gas_used
@@ -41,17 +50,17 @@ FROM (
      , et.block_number
      FROM {{ source('ethereum','traces') }} et
      WHERE cardinality(et.trace_address) > 0
-     AND et.block_time >= NOW() - interval '3' day
      {% if is_incremental() %}
      AND et.block_time >= date_trunc("day", NOW() - interval '1' week)
      {% endif %}
-     ) etg
-LEFT JOIN ethereum.transactions etc ON etc.block_time=etg.block_time
-          AND etc.hash=etg.tx_hash
-LEFT JOIN {{ source('prices', 'usd') }} pu ON pu.minute=date_trunc('minute', etg.block_time)
+     ) traces
+LEFT JOIN {{ source('ethereum','transactions') }} txs ON txs.block_time=traces.block_time
+          AND txs.hash=traces.tx_hash
+LEFT JOIN {{ source('prices', 'usd') }} pu ON pu.minute=date_trunc('minute', traces.block_time)
      AND pu.blockchain='ethereum'
      AND pu.contract_address='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
      {% if is_incremental() %}
      AND pu.minute >= date_trunc("day", NOW() - interval '1' week)
      {% endif %}
-GROUP BY etg.tx_hash, etg.trace, etc.gas_price, etg.block_time, etg.block_number
+GROUP BY traces.tx_hash, traces.trace, txs.gas_price, traces.block_time, traces.block_number
+, txs.from, txs.to, txs.gas_used, txs.gas_price, txs.data, traces.input
