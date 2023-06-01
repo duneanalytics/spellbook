2c6fa845e453ad6ea5286855f47446f41b879467
Hildebert Moulié <h.moulie@gmail.com>
2023-06-01 12:44:32 +0200

    use crosschain cex addresses table for spell instead of per chain with table name coalescing

--- models/labels/addresses/__single_category_labels__/cex_users/labels_cex_users.sql
+++ models/labels/addresses/__single_category_labels__/cex_users/labels_cex_users.sql
@@ -34,8 +34,8 @@ FROM (
         'cex_users_withdrawals' model_name,
         'persona' as label_type
     FROM {{source('erc20_' + chain, 'evt_transfer')}} t
-    INNER JOIN {{ref('cex_'+ chain +'_addresses')}} c
-        ON t.from = c.address
+    INNER JOIN {{ref('cex_addresses')}} c ON t.blockchain = c.blockchain
+        AND t.from = c.address
 
     {% if not loop.last %}
     UNION ALL
