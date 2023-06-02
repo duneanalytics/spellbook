0006ecaf94373992e6dc1967e07fc276e7aa9bfc
Hildebert Moulié <h.moulie@gmail.com>
2023-06-02 16:47:22 +0200

    fix

--- models/gas/ethereum/gas_ethereum_fees_traces.sql
+++ models/gas/ethereum/gas_ethereum_fees_traces.sql
@@ -15,17 +15,28 @@ WITH traces AS (
      , MAX(traces.from) AS trace_from
      , MAX(traces.to) AS trace_to
      , traces.trace
+     , MAX(traces.input) AS trace_input
      , substring(MAX(traces.input),1,10) AS trace_method
      , SUM(traces.gas_used) AS gas_used
+     , SUM(traces.gas_used_trace) AS gas_used_trace
+     , MAX(traces.trace_type) AS trace_type
+     , MAX(traces.trace_value) AS trace_value
+     , MAX(traces.trace_success) AS trace_success
+     , MAX(traces.tx_success) AS tx_success
      FROM (
           SELECT from
-          , to 
+          , to
           , tx_hash
           , trace_address AS trace
           , gas_used
+          , gas_used AS gas_used_trace
           , block_time
           , block_number
           , input
+          , type AS trace_type
+          , value AS trace_value
+          , success AS trace_success
+          , tx_success
           FROM {{ source('ethereum','traces') }}
           {% if is_incremental() %}
           WHERE block_time >= date_trunc("day", NOW() - interval '1' week)
@@ -37,10 +48,15 @@ WITH traces AS (
           , CAST(NULL AS varchar(1)) AS to 
           , tx_hash
           , slice(trace_address, 1, cardinality(trace_address) - 1) AS trace
-          , -gas_used AS gas_used
+          , CAST(NULL AS double) AS gas_used
+          , -gas_used AS gas_used_trace
           , block_time
           , block_number
           , CAST(NULL AS varchar(1)) AS input
+          , CAST(NULL AS varchar(1)) AS trace_type
+          , CAST(NULL AS varchar(1)) AS trace_value
+          , CAST(NULL AS varchar(1)) AS trace_success
+          , CAST(NULL AS varchar(1)) AS tx_success
           FROM {{ source('ethereum','traces') }}
           WHERE cardinality(trace_address) > 0
           {% if is_incremental() %}
@@ -61,16 +77,25 @@ SELECT 'ethereum' AS blockchain
 , txs.to AS tx_to
 , traces.trace
 , traces.trace_method
+, traces.trace_input
 , substring(txs.data,1,10) AS tx_method
 , traces.gas_used
+, traces.gas_used_trace
 , txs.gas_used AS tx_gas_used
 , traces.gas_used/txs.gas_used AS gas_used_tx_percentage
 , txs.gas_price AS tx_gas_price
+, traces.trace_type
+, traces.trace_value
+, traces.trace_success
+, traces.tx_success
 , (traces.gas_used*txs.gas_price)/POWER(10, 18) AS spent_gas_fee
 , (pu.price*traces.gas_used*txs.gas_price)/POWER(10, 18) AS spent_gas_fee_usd
 FROM traces
 LEFT JOIN {{ source('ethereum','transactions') }} txs ON txs.block_time=traces.block_time
      AND txs.hash=traces.tx_hash
+     {% if is_incremental() %}
+     AND txs.block_time >= date_trunc("day", NOW() - interval '1' week)
+     {% endif %}
 LEFT JOIN {{ source('prices', 'usd') }} pu ON pu.minute=date_trunc('minute', traces.block_time)
      AND pu.blockchain='ethereum'
      AND pu.contract_address='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
