bcdbe1e53b511450525224b522b0cc2f4e469b3f
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-03 08:07:46 -0400

    lower addr

--- models/addresses/ethereum/addresses_ethereum_l2_batch_submitters.sql
+++ models/addresses/ethereum/addresses_ethereum_l2_batch_submitters.sql
@@ -4,7 +4,7 @@
                                     "addresses",
                                     \'["msilb7"]\') }}') }}
 
-SELECT lower(address) as address, protocol_name, submitter_type, role_type, version, description
+SELECT address, protocol_name, submitter_type, role_type, version, description
 FROM (VALUES
        ("0x5e4e65926ba27467555eb562121fac00d24e9dd2", 'Optimism', 'Canonical Transaction Chain','to_address','2','Optimism: Canonical Transaction Chain')
       ,("0xbe5dab4a2e9cd0f27300db4ab94bee3a233aeb19", "Optimism", "State Commitment Chain","to_address","2","Optimism: State Commitment Chain")
@@ -14,9 +14,9 @@ FROM (VALUES
       ,("0x6887246668a3b87f54deb3b94ba47a6f63f32985", "Optimism", "Canonical Transaction Chain","from_address","","Optimism: Sequencer")
       ,("0xe969c2724d2448f1d1a6189d3e2aa1f37d5998c1", "Optimism", "State Commitment Chain","from_address","","Optimism: State Root Proposer")
 
-      ,("0xff00000000000000000000000000000000000010", "Optimism", "L1BatchInbox","to_address","Bedrock","Optimism: BatchInbox")
-      ,("0x6887246668a3b87F54DeB3b94Ba47a6f63F32985", "Optimism", "L1BatchInbox","from_address","Bedrock","Optimism: BatchInbox")
---       ,("", "Optimism", "OutputOracle","to_address","Bedrock","Optimism: OutputOracle")
+      ,("0xff00000000000000000000000000000000000010", "Optimism", "L1BatchInbox","to_address","Bedrock","Optimism: L1BatchInbox")
+      ,("0x6887246668a3b87f54deb3b94ba47a6f63f32985", "Optimism", "L1BatchInbox","from_address","Bedrock","Optimism: L1BatchInbox")
+--       ,("", "Optimism", "L1OutputOracle","to_address","Bedrock","Optimism: L1OutputOracle")
 
       ,("0xa4b10ac61e79ea1e150df70b8dda53391928fd14", "Arbitrum","SequencerInbox","from_address","","Arbitrum: Sequencer")
       ,("0xcce5c6cff61c49b4d53dd6024f8295f3c5230513", "Arbitrum","SequencerInbox","from_address","","Arbitrum: Sequencer 2")
@@ -33,4 +33,5 @@ FROM (VALUES
       ,("0x56a76bcc92361f6df8d75476fed8843edc70e1c9", "Metis", "Canonical Transaction Chain","to_address","1","")
       ,("0x6a1db7d799fba381f2a518ca859ed30cb8e1d41a", "Metis", "Canonical Transaction Chain","to_address","2","")
       ,("0xf209815e595cdf3ed0aaf9665b1772e608ab9380", "Metis", "State Commitment Chain","to_address","","")
+      
       ) AS x (address, protocol_name, submitter_type, role_type, version, description)
