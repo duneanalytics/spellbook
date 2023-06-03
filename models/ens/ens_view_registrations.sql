974a706dd969c1a861fef82d2dda53b2d85ee6a3
web3lyt <104781728+web3lyt@users.noreply.github.com>
2023-06-03 00:54:29 -0500

    Update ens_view_registrations.sql

--- models/ens/ens_view_registrations.sql
+++ models/ens/ens_view_registrations.sql
@@ -2,12 +2,15 @@
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "project",
                                     "ens",
-                                    \'["antonio-mendes","mewwts"]\') }}')}}
-SELECT *
+                                    \'["antonio-mendes","mewwts","steakhouse"]\') }}')}}
+SELECT *, NULL AS premium
 FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_1_evt_NameRegistered')}}
 UNION 
-SELECT *
+SELECT *, NULL AS premium
 FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_2_evt_NameRegistered')}}
 UNION 
-SELECT *
+SELECT *, NULL AS premium
 FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_3_evt_NameRegistered')}}
+UNION 
+SELECT contract_address, evt_tx_hash, evt_index, evt_block_time, evt_block_number, baseCost + premium AS cost, expires, label, name, owner, premium
+FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_4_evt_NameRegistered')}}
