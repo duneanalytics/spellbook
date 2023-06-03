1a0b90abac3aa0856e2e0063473f8a2cbec93e63
web3lyt <104781728+web3lyt@users.noreply.github.com>
2023-06-03 00:55:01 -0500

    Update ens_view_renewals.sql

--- models/ens/ens_view_renewals.sql
+++ models/ens/ens_view_renewals.sql
@@ -10,4 +10,7 @@ SELECT *
 FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_2_evt_NameRenewed')}}
 UNION 
 SELECT *
-FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_3_evt_NameRenewed')}} ;
+FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_3_evt_NameRenewed')}}
+UNION
+SELECT *
+FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_4_evt_NameRenewed')}} ;
