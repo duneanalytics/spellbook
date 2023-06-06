d87f6d5694f86006112d8360f5a95463e4d7d209
Huang Geyang <Sukebeta@outlook.com>
2023-06-06 23:27:43 +0800

    Reformat

--- models/spiritswap/fantom/spiritswap_fantom_pools.sql
+++ models/spiritswap/fantom/spiritswap_fantom_pools.sql
@@ -11,16 +11,16 @@
     )
 }}
 
-SELECT 'fantom' AS blockchain
-, 'spiritswap' AS project
-, '1' AS version
-, pair AS pool
-, 0.3 AS fee
-, token0
-, token1
-, evt_block_time AS creation_block_time
-, evt_block_number AS creation_block_number
-, contract_address
+SELECT 'fantom'         AS blockchain
+     , 'spiritswap'     AS project
+     , '1'              AS version
+     , pair             AS pool
+     , 0.3              AS fee
+     , token0
+     , token1
+     , evt_block_time   AS creation_block_time
+     , evt_block_number AS creation_block_number
+     , contract_address
 FROM {{ source('spiritswap_fantom', 'Factory_evt_PairCreated') }}
 {% if is_incremental() %}
 WHERE evt_block_time >= date_trunc("day", now() - interval '1 week')
