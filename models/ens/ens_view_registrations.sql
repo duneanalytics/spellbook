28e6be039043a8da392e7f6075889ebd82446dde
web3lyt <104781728+web3lyt@users.noreply.github.com>
2023-06-06 09:20:10 -0500

    Update ens_view_registrations.sql

--- models/ens/ens_view_registrations.sql
+++ models/ens/ens_view_registrations.sql
@@ -12,5 +12,5 @@ UNION
 SELECT *, NULL AS premium
 FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_3_evt_NameRegistered')}}
 UNION 
-SELECT contract_address, evt_tx_hash, evt_index, evt_block_time, evt_block_number, CAST(CAST(baseCost AS INT) + CAST(premium AS INT) AS VARCHAR) AS cost, expires, label, name, owner, premium
+SELECT contract_address, evt_tx_hash, evt_index, evt_block_time, evt_block_number, baseCost + premium AS cost, expires, label, name, owner, premium
 FROM {{source('ethereumnameservice_ethereum', 'ETHRegistrarController_4_evt_NameRegistered')}}
