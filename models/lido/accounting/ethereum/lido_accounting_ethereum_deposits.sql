5b88df3402e5d6d9b32b8d5a09ffce9c555450ab
ppclunghe <pipistrella@list.ru>
2023-06-08 21:19:53 +0200

    incremental deposits fix

--- models/lido/accounting/ethereum/lido_accounting_ethereum_deposits.sql
+++ models/lido/accounting/ethereum/lido_accounting_ethereum_deposits.sql
@@ -1,16 +1,17 @@
 {{ config(
         alias ='deposits',
         partition_by = ['day'],
-        materialized = 'table',
+        materialized = 'incremental',
         file_format = 'delta',
+        incremental_strategy = 'merge',
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "lido_accounting",
-                                \'["pipistrella", "adcv", "zergil1397", "lido"]\') }}'
+                                \'["ppclunghe", "gregshestakovlido"]\') }}'
         )
 }}
---https://dune.com/queries/2011901
---ref{{'lido_accounting_ethereum_deposits'}}
+
+{% set project_start_date = '2020-12-18' %} 
 
 	SELECT  block_time as period, 
         sum(cast(value as DOUBLE)) as amount_staked, 
@@ -18,7 +19,12 @@
         tx_hash,
         date_trunc('day', block_time) as day 
         FROM  {{source('ethereum','traces')}} 
-        WHERE to = LOWER('0x00000000219ab540356cbb839cbe05303d7705fa')
+        {% if is_incremental() %}
+        WHERE date_trunc('hour', block_time) >= date_trunc("hour", now() - interval '7 days')
+        {% else %}
+        WHERE date_trunc('hour', block_time) >= '{{ project_start_date }}' 
+        {% endif %}  
+        AND to = LOWER('0x00000000219ab540356cbb839cbe05303d7705fa')
         AND call_type = 'call'
         AND success = True 
         AND `from` in (LOWER('0xae7ab96520de3a18e5e111b5eaab095312d7fe84'), LOWER('0xB9D7934878B5FB9610B3fE8A5e441e8fad7E293f'), LOWER('0xFdDf38947aFB03C621C71b06C9C70bce73f12999'))
