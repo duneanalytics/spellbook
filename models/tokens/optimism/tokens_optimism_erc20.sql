82b5adc0459b360f2497fa10ca4dc4b682d41118
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2023-06-09 14:43:47 -0400

    Revert recent optimism spell changes / additions (#3504)
    * Revert "Add Labels for Bedrock Addresses & Configure L2 Labels (#3463)"
    
    This reverts commit d2807effc9f9a669fb0b249a32727454b5da327b.
    
    * Revert "[easy] OP Updates + ERC20 Mapping Fix (#3377)"
    
    This reverts commit ef80056f47a4f1793dde626d0d45f670f8fc926c.

--- models/tokens/optimism/tokens_optimism_erc20.sql
+++ models/tokens/optimism/tokens_optimism_erc20.sql
@@ -4,25 +4,23 @@
                                     "tokens",
                                     \'["msilb7"]\') }}')}}
 
-SELECT distinct
-contract_address, symbol, decimals, token_type, token_mapping_source,
+SELECT *,
 CASE WHEN token_type IN ('underlying') THEN 1
 ELSE 0 --double counted (breakdown, receipt) or no price
 END
 AS is_counted_in_tvl
 FROM (
 	SELECT
-	coalesce( c.contract_address, t.contract_address, b.contract_address) AS contract_address
+	c.contract_address
 	, coalesce(t.symbol,b.symbol) as symbol
 	, coalesce(t.decimals,b.decimals) as decimals
 	, coalesce(t.token_type,b.token_type) AS token_type
 	, coalesce(t.token_mapping_source, b.token_mapping_source) AS token_mapping_source
 
 	FROM {{ ref('tokens_optimism_erc20_transfer_source')}} c
-	FULL OUTER JOIN  {{ref('tokens_optimism_erc20_curated')}} t
+	LEFT JOIN  {{ref('tokens_optimism_erc20_curated')}} t
 	ON c.contract_address = t.contract_address
-	FULL OUTER JOIN {{ ref('tokens_optimism_erc20_generated')}} b
+	LEFT JOIN {{ ref('tokens_optimism_erc20_generated')}} b
 	ON c.contract_address = b.contract_address
-	OR t.contract_address = b.contract_address
 	-- Eventually we can also try to map sectors here (i.e. stablecoin, liquid staking)
 ) a
