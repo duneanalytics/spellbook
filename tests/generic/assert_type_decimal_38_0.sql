a32379409adb502055a812f7399c8d00bc09af76
Huang Geyang <Sukebeta@outlook.com>
2023-06-10 01:37:00 +0800

    Update test

--- tests/generic/assert_type_decimal_38_0.sql
+++ tests/generic/assert_type_decimal_38_0.sql
@@ -1,9 +1,10 @@
 {% test assert_type_decimal_38_0(model, column_name) %}
-    with all_values as (
+    with arithmetic_test as (
         select {{column_name}}
         from {{ model }}
-        union all
-        select cast (0 as decimal (38, 0)) as {{column_name}}
+        where  {{column_name}} + cast (0 as decimal (38, 0)) <> cast(({{column_name}} + 0) as decimal (38, 0))
+            or {{column_name}} + cast (0 as decimal (38, 0)) <=  {{column_name}} + cast (-1 as decimal (38, 0))
+            or {{column_name}} + cast (0 as decimal (38, 0)) is null
     )
-    select {{column_name}} from all_values limit 0
+    select {{column_name}} from arithmetic_test limit 100
 {% endtest %}
