ef10bdef0201782f015c51e642d1ef7c90fba28d
Huang Geyang <Sukebeta@outlook.com>
2023-06-10 00:25:55 +0800

    Add assert_type_decimal_38_0 macro

---
+++ macros/test-helpers/assert_type_decimal_38_0.sql
@@ -0,0 +1,9 @@
+{% macro assert_type_decimal_38_0(model, column_name) %}
+    with all_values as (
+        select {{column_name}}
+        from {{ model }}
+        union all
+        select cast (0 as decimal (38, 0)) as {{column_name}}
+    )
+    select {{column_name}} from all_values limit 0
+{% endmacro %}
