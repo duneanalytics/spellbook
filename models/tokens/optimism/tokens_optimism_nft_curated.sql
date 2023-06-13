e2b3f0aa6de3d3c5c984f161c980df47d1ce0a62
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-13 19:09:06 -0400

    add

--- models/tokens/optimism/tokens_optimism_nft_curated.sql
+++ models/tokens/optimism/tokens_optimism_nft_curated.sql
@@ -291,5 +291,6 @@ FROM
 ,('0x2335022c740d17c2837f9C884Bfe4fFdbf0A95D5', 'Optimist NFT')
 ,('0x1B36291fF8F503CfB4E3baBe198a40398BCF54AD', 'Bedrock')
 ,('0x1846fb181a12644dec6af707b23b39d2d19c1879', 'Gitcoin Passport Score')
+,('0xA077FA66Cf4C8044Ca4680db80B07CE85E8Af0B4', 'Getaverse SBT')
 
 ) as temp_table (contract_address, name)
