426d3b0069a26f5e763ec9c50392fcad6edb1bc7
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-06-13 15:48:13 -0400

    Update balancer_v2_ethereum_trades.sql

--- models/balancer/ethereum/balancer_v2_ethereum_trades.sql
+++ models/balancer/ethereum/balancer_v2_ethereum_trades.sql
@@ -6,7 +6,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address'],
-    post_hook= '{{ expose_spells(\'["ethereum"]\',
+    post_hook = '{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "balancer_v2",
                                 \'["mendesfabio", "jacektrocinski", "thetroyharris"]\') }}'
