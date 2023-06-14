c42855e80e08076964db91958bf8400bd2b204a2
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-13 23:16:02 -0400

    alias

--- models/contracts/optimism/contracts_optimism_contract_mapping.sql
+++ models/contracts/optimism/contracts_optimism_contract_mapping.sql
@@ -164,7 +164,7 @@ SELECT *
       ON nd.creator_address = t.creator_address
 
     -- Don't pull contracts that are in the incremental group (prevent dupes)
-    WHERE contract_address NOT IN (
+    WHERE ct.ontract_address NOT IN (
       SELECT address FROM {{ source('optimism', 'creation_traces') }} WHERE ct.block_time >= date_trunc('day', now() - interval '1 week')
     )
 
