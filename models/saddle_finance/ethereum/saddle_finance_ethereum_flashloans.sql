eb38c5bdf7c50810fdd0b7859a8db5f3ed4e90fb
Hildebert Moulié <h.moulie@gmail.com>
2023-06-15 15:13:15 +0200

    saddle flashloans

---
+++ models/saddle_finance/ethereum/saddle_finance_ethereum_flashloans.sql
@@ -0,0 +1,52 @@
+{{ config(
+      alias='flashloans'
+      , materialized = 'incremental'
+      , file_format = 'delta'
+      , incremental_strategy = 'merge'
+      , unique_key = ['tx_hash', 'evt_index']
+      , post_hook='{{ expose_spells(\'["ethereum"]\',
+                                  "project",
+                                  "saddle_finance",
+                                  \'["hildobby"]\') }}'
+  )
+}}
+
+WITH flashloans AS (
+    SELECT flash.evt_block_time AS block_time
+    , flash.evt_block_number AS block_number
+    , flash.amount AS amount_raw
+    , flash.evt_tx_hash AS tx_hash
+    , flash.evt_index
+    , flash.amountFee AS fee_raw
+    , get_json_object(pool.poolData, "$.tokens[" || CAST(flash.tokenIndex AS string) || "]") AS currency_contract
+    , flash.receiver AS recipient
+    , flash.contract_address
+    FROM {{ source('saddle_finance_ethereum','SwapFlashLoan_evt_FlashLoan') }} flash
+    INNER JOIN {{ source('saddle_finance_ethereum','PoolRegistry_evt_AddPool') }} pool ON pool.poolAddress=flash.contract_address
+    {% if is_incremental() %}
+    WHERE flash.evt_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+    )
+
+SELECT 'ethereum' AS blockchain
+, 'Saddle' AS project
+, '1' AS version
+, flash.block_time
+, flash.block_number
+, flash.amount_raw/POWER(10, tok.decimals) AS amount
+, pu.price*(flash.amount_raw/POWER(10, tok.decimals)) AS amount_usd
+, flash.tx_hash
+, flash.evt_index
+, flash.fee_raw/POWER(10, tok.decimals) AS fee
+, flash.currency_contract
+, tok.symbol AS currency_symbol
+, flash.recipient
+, flash.contract_address
+FROM flashloans flash
+LEFT JOIN {{ ref('tokens_ethereum_erc20') }} tok ON tok.contract_address=flash.currency_contract
+LEFT JOIN {{ source('prices','usd') }} pu ON pu.blockchain = 'ethereum'  
+  AND pu.contract_address=flash.currency_contract
+  AND pu.minute = date_trunc('minute', flash.block_time)
+  {% if is_incremental() %}
+  AND pu.minute >= date_trunc("day", now() - interval '1 week')
+  {% endif %}
---
+++ models/saddle_finance/saddle_finance_flashloans.sql
@@ -0,0 +1,45 @@
+{{ config(
+      alias='flashloans'
+      , materialized = 'incremental'
+      , file_format = 'delta'
+      , incremental_strategy = 'merge'
+      , unique_key = ['blockchain', 'tx_hash', 'evt_index']
+      , post_hook='{{ expose_spells(\'["ethereum", "optimism", "arbitrum"]\',
+                                  "project",
+                                  "saddle_finance",
+                                  \'["hildobby"]\') }}'
+  )
+}}
+
+{% set saddle_finance_models = [
+ref('saddle_finance_arbitrum_flashloans')
+, ref('saddle_finance_ethereum_flashloans')
+, ref('saddle_finance_optimism_flashloans')
+] %}
+
+SELECT *
+FROM (
+    {% for saddle_finance_model in saddle_finance_models %}
+      SELECT blockchain
+      , project
+      , version
+      , block_time
+      , block_number
+      , amount
+      , amount_usd
+      , tx_hash
+      , evt_index
+      , fee
+      , currency_contract
+      , currency_symbol
+      , recipient
+      , contract_address
+    FROM {{ saddle_finance_model }}
+    {% if is_incremental() %}
+    WHERE block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %} 
+)
