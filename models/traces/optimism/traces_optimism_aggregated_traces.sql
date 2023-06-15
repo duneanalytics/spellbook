fb06b3de7b8b0cc03187b43c73392188506ab970
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-15 08:53:47 -0400

    uniques

--- models/traces/optimism/traces_optimism_aggregated_traces.sql
+++ models/traces/optimism/traces_optimism_aggregated_traces.sql
@@ -27,6 +27,8 @@ DATE_TRUNC('day', block_time) AS block_date
 	|| '-' || coalesce(cast(trace_to as varchar(100)),'null_trace_to')
 	|| '-' || coalesce(cast(trace_method_id as varchar(100)),'null_trace_method_id')
 	|| '-' || coalesce(cast(call_type as varchar(100)),'null_call_type')
+	|| '-' || coalesce(cast(trace_success as varchar(100)),'null_call_type')
+	|| '-' || coalesce(cast(tx_success as varchar(100)),'null_call_type')
 as unique_id
 
 FROM (
@@ -62,6 +64,9 @@ FROM (
 	, case when t.gas_price = 0 THEN 0 ELSE
 		cast(t.l1_fee + (t.gas_used * cast(t.gas_price as double)) as double)
 		END AS tx_gas_fee_eth
+	
+	, r.success AS trace_success
+	, r.tx_success
     
 	FROM {{ source('optimism', 'traces') }} r
     
