b6f57e6d34cc724e4959508ea4d17f0e5f1f7545
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-15 08:14:30 -0400

    uniques

--- models/quests/quests_participants.sql
+++ models/quests/quests_participants.sql
@@ -8,8 +8,8 @@
     )
 }}
 
-SELECT quester_address, block_number, block_time, quest_name, rewards_token as token_address, null as token_id FROM {{ ref('coinbase_wallet_quests_optimism_rewards_transfers') }}
+SELECT 'optimism' as blockchain, 'coinbase wallet quests' as platform, quester_address, block_number, block_time, quest_name, rewards_token as token_address, null as token_id FROM {{ ref('coinbase_wallet_quests_optimism_rewards_transfers') }}
 UNION ALL
-SELECT quester_address, block_number, block_time, contract_project as quest_name, nft_contract_address as token_address, tokenId as token_id FROM {{ ref('optimism_quests_optimism_quest_completions') }}
+SELECT 'optimism' as blockchain, 'optimism quests' as platform, quester_address, block_number, block_time, contract_project as quest_name, nft_contract_address as token_address, tokenId as token_id FROM {{ ref('optimism_quests_optimism_quest_completions') }}
 UNION ALL
-SELECT token_transfer_to as quester_address, block_number, block_time, null as quest_name, nft_contract_address as token_address, tokenId AS token_id FROM {{ ref('galxe_optimism_nft_mints') }}
+SELECT 'optimism' as blockchain, 'galxe' as platform, token_transfer_to as quester_address, block_number, block_time, null as quest_name, nft_contract_address as token_address, tokenId AS token_id FROM {{ ref('galxe_optimism_nft_mints') }}
