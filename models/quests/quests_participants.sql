bba11fc662ad99aea827be69fa20af309867f048
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-16 09:44:54 -0400

    add manifold

--- models/quests/quests_participants.sql
+++ models/quests/quests_participants.sql
@@ -9,7 +9,10 @@
 }}
 
 SELECT 'optimism' as blockchain, 'coinbase wallet quests' as platform, quester_address, block_number, block_time, quest_name, rewards_token as token_address, 0 as token_id FROM {{ ref('coinbase_wallet_quests_optimism_rewards_transfers') }}
+    GROUP BY 1,2,3,4,5,6,7,8 -- handle for multiple transfers in one tx
 UNION ALL
 SELECT 'optimism' as blockchain, 'optimism quests' as platform, quester_address, block_number, block_time, contract_project as quest_name, nft_contract_address as token_address, tokenId as token_id FROM {{ ref('optimism_quests_optimism_quest_completions') }}
+    GROUP BY 1,2,3,4,5,6,7,8 -- handle for multiple transfers in one tx
 UNION ALL
-SELECT 'optimism' as blockchain, 'galxe' as platform, token_transfer_to as quester_address, block_number, block_time, null as quest_name, nft_contract_address as token_address, tokenId AS token_id FROM {{ ref('galxe_optimism_nft_mints') }}
+SELECT distinct 'optimism' as blockchain, 'galxe' as platform, token_transfer_to as quester_address, block_number, block_time, null as quest_name, nft_contract_address as token_address, tokenId AS token_id FROM {{ ref('galxe_optimism_nft_mints') }}
+    GROUP BY 1,2,3,4,5,6,7,8 -- handle for multiple transfers in one tx
