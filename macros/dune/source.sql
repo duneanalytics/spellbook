388db3d9c9437513e5caf9319a9cad0b5ec7666b
Alex Courouble <alexandre@dune.com>
2023-06-19 15:52:34 +0100

    Only add delta_prod to source if we are running on trino

--- macros/dune/source.sql
+++ macros/dune/source.sql
@@ -1,8 +1,9 @@
 {% macro source(source_name, table_name) %}
-
   {% set rel = builtins.source(source_name, table_name) %}
-  {% set newrel = rel.replace_path(database="delta_prod") %}
-  {% do return(newrel) %}
-  {% do {{ log("TEST CHANGE MACRO", info=True) }}%}
-
+  {%- if env_var('DBT_DUNE_SQL', 'False') == 'True' -%}
+    {% set newrel = rel.replace_path(database="delta_prod") %}
+    {% do return(newrel) %}
+  {%- else -%}
+    {% do return(newrel) %}
+  {%- endif -%}
 {% endmacro %}
