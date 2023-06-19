7f44c469692de209887d05eff5eee48e4322b2f3
Alex Courouble <alexandre@dune.com>
2023-06-19 15:22:51 +0100

    DBT macro to override source database

---
+++ macros/dune/source.sql
@@ -0,0 +1,8 @@
+{% macro source(source_name, table_name) %}
+
+  {% set rel = builtins.source(source_name, table_name) %}
+  {% set newrel = rel.replace_path(database="delta_prod") %}
+  {% do return(newrel) %}
+  {% do {{ log("TEST CHANGE MACRO", info=True) }}%}
+
+{% endmacro %}
