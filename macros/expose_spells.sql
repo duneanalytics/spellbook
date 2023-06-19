e37da90dae81cadc4bd6dbbc2bbe5411dfa2d014
Alex Courouble <alexandre@dune.com>
2023-06-19 16:32:58 +0100

    Updates expose spell and mark as spell macros

--- macros/expose_spells.sql
+++ macros/expose_spells.sql
@@ -1,15 +1,26 @@
 {% macro expose_spells(blockchains, spell_type, spell_name, contributors) %}
 {%- if target.name == 'prod' or True-%}
-        ALTER {{"view" if model.config.materialized == "view" else "table"}} {{ this }}
-        SET TBLPROPERTIES (
-        'dune.public'='true',
-        'dune.data_explorer.blockchains'= '{{ blockchains }}',     -- e.g., ["ethereum","solana"]
-        'dune.data_explorer.category'='abstraction',
-        'dune.data_explorer.abstraction.type'= '{{ spell_type }}', -- 'project' or 'sector'
-        'dune.data_explorer.abstraction.name'= '{{ spell_name }}', -- 'aave' or 'uniswap'
-        'dune.data_explorer.contributors'= '{{ contributors }}',   -- e.g., ["soispoke","jeff_dude"]
-        'dune.vacuum' = '{"enabled":true}'
-        )
-{%- else -%}
+        {%- if env_var('DBT_DUNE_SQL', 'False') != 'True' -%}
+                ALTER {{"view" if model.config.materialized == "view" else "table"}} {{ this }}
+                SET TBLPROPERTIES (
+                'dune.public'='true',
+                'dune.data_explorer.blockchains'= '{{ blockchains }}',     -- e.g., ["ethereum","solana"]
+                'dune.data_explorer.category'='abstraction',
+                'dune.data_explorer.abstraction.type'= '{{ spell_type }}', -- 'project' or 'sector'
+                'dune.data_explorer.abstraction.name'= '{{ spell_name }}', -- 'aave' or 'uniswap'
+                'dune.data_explorer.contributors'= '{{ contributors }}',   -- e.g., ["soispoke","jeff_dude"]
+                'dune.vacuum' = '{"enabled":true}'
+                )
+        {%- else -%}
+                ALTER {{"view" if model.config.materialized == "view" else "table"}} {{ this }}
+                SET PROPERTIES extra_properties = map_from_entries(ARRAY[
+                ROW('dune.public', 'true'),
+                ROW('dune.data_explorer.blockchains', '{{ blockchains }}'),     -- e.g., ["ethereum","solana"]
+                ROW('dune.data_explorer.category', 'abstraction'),
+                ROW('dune.data_explorer.abstraction.type', '{{ spell_type }}'), -- 'project' or 'sector'
+                ROW('dune.data_explorer.abstraction.name', '{{ spell_name }}'), -- 'aave' or 'uniswap'
+                ROW('dune.data_explorer.contributors','{{ contributors }}')   -- e.g., ["soispoke","jeff_dude"]
+                ])
+        {%- endif -%}
 {%- endif -%}
 {%- endmacro -%}
--- macros/mark_as_spell.sql
+++ macros/mark_as_spell.sql
@@ -1,9 +1,15 @@
 {% macro mark_as_spell(this, materialization) %}
 {%- if target.name == 'prod' or True-%}
-        ALTER {{"view" if materialization == "view" else "table"}} {{ this }}
-        SET TBLPROPERTIES (
-        'dune.data_explorer.category'='abstraction'
-        )
-{%- else -%}
+        {%- if env_var('DBT_DUNE_SQL', 'False') != 'True' -%}
+                ALTER {{"view" if materialization == "view" else "table"}} {{ this }}
+                SET TBLPROPERTIES (
+                'dune.data_explorer.category'='abstraction'
+                )
+        {%- else -%}
+                ALTER {{"view" if materialization == "view" else "table"}} {{ this }}
+                SET PROPERTIES extra_properties = map_from_entries (ARRAY[
+                ROW('dune.data_explorer.category', 'abstraction')
+                ])
+        {%- endif -%}
 {%- endif -%}
 {%- endmacro -%}
