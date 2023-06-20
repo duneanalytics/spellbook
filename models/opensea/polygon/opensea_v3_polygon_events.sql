5fcceb893a0b259e254d2f2dbd920d3549fb2188
sohwak <59173567+sohwak@users.noreply.github.com>
2023-06-21 00:34:05 +0900

    Fix `opensea_v3_polygon.events` to include OS trades (#3574)
    * fix opensea-v3-polygon to include OS trades
    
    * rerun
    
    * rerun
    
    * rerun
    
    * rerun
    
    * rerun
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/opensea/polygon/opensea_v3_polygon_events.sql
+++ models/opensea/polygon/opensea_v3_polygon_events.sql
@@ -723,7 +723,6 @@ select  -- basic info
                                       ,'0x110b2b128a9ed1be5ef3232d8e4e41640df5c2cd'
                                       ,'0x000000e7ec00e7b300774b00001314b8610022b8' -- newly added on seaport v1.4
                                       )
-   and  (    fee_wallet_name = 'opensea'
-          or right_hash = '360c6ebe' -- opensea hash
-        )
-  
+  --  and  (    fee_wallet_name = 'opensea'
+  --         or right_hash = '360c6ebe' -- opensea hash
+  --       )
