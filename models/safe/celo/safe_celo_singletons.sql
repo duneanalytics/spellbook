2260e0b345794b525e9f87560279b444831a6f5d
danielpartida <partidapd@gmail.com>
2023-06-22 16:26:21 +0200

    Add Safe celo spells, schema and sources
    Spells implemented are:
    safe_celo_safes.sql
    safe_celo_singletons.sql
    safe_celo_transactions.sql
    safe_celo_celo_transafers.sql
    
    Source implemented is safe_celo_sources.yml
    
    Schema implemented is safe_celo_schema.yml

---
+++ models/safe/celo/safe_celo_singletons.sql
@@ -0,0 +1,15 @@
+{{
+    config(
+        materialized='table',
+        alias='singletons',
+        post_hook='{{ expose_spells(\'["celo"]\',
+                                    "project",
+                                    "safe",
+                                    \'["danielpartida"]\') }}'
+    )
+}}
+
+
+-- Fetch all known singleton addresses used via the factory.
+select distinct singleton as address
+from {{ source('gnosis_safe_celo', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
