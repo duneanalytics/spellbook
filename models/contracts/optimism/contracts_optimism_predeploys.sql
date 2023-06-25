1b39c734f0d10b812da851b45cf72340c65fac25
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-25 13:02:56 -0400

    predeploy

--- models/contracts/optimism/contracts_optimism_predeploys.sql
+++ models/contracts/optimism/contracts_optimism_predeploys.sql
@@ -1,17 +1,37 @@
  {{
   config(
         alias='predeploys',
-        materialized ='incremental',
-        file_format ='delta',
-        incremental_strategy='merge',
-        unique_key='contract_address',
         post_hook='{{ expose_spells(\'["optimism"]\',
                                     "sector",
                                     "contracts",
                                     \'["msilb7", "chuxin"]\') }}'
   )
 }}
-
+        -- this is a static table
+        -- materialized ='incremental',
+        -- file_format ='delta',
+        -- incremental_strategy='merge',
+        -- unique_key='contract_address',
+with contracts AS (
+SELECT *, ROW_NUMBER() OVER (PARTITION BY contract_address ORDER BY pref_rnk ASC) AS c_rank
+FROM (
+  -- other predeploys
+  select 
+     cast(NULL as string) as trace_creator_address
+    ,cast(NULL as string) as creator_address
+    ,cast(NULL as string) as contract_factory
+    ,contract_address
+    ,'ovm' as contract_project
+    ,contract_name
+    ,to_timestamp('2021-07-06 00:00:00') as created_time
+    ,false as is_self_destruct
+    ,'other predeploys' as source
+    ,cast(NULL as string) as creation_tx_hash
+    , 1 as pref_rnk
+  from {{ ref('contracts_optimism_system_predeploys') }} as c
+    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+  
+  union all
   -- ovm 1.0 contracts
 
   select 
@@ -25,21 +45,9 @@
     ,false as is_self_destruct
     ,'ovm1 contracts' as source
     ,cast(NULL as string) as creation_tx_hash
+    , 2 as pref_rnk
   from {{ source('ovm1_optimism', 'contracts') }} as c
-  where 
-    true
-    {% if is_incremental() %} -- this filter will only be applied on an incremental run 
-    and not exists (
-      select 1
-      from {{ this }} as gc
-      where 
-        gc.contract_address = c.contract_address
-        and (
-          (gc.contract_project = c.contract_project) or (gc.contract_project is NULL)
-        )
-    )
-    {% endif %}
-    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11
 
   union all 
   --synthetix genesis contracts
@@ -55,19 +63,10 @@
     ,false as is_self_destruct
     ,'synthetix contracts' as source
     ,cast(NULL as string) as creation_tx_hash
+    , 3 as pref_rnk
   from {{ source('ovm1_optimism', 'synthetix_genesis_contracts') }} as snx
-  where 
-    true
-    {% if is_incremental() %} -- this filter will only be applied on an incremental run 
-    and not exists (
-      select 1 
-      from {{ this }} as gc
-      where 
-        gc.contract_address = snx.contract_address
-        and gc.contract_project LIKE 'Synthetix%' --future proof in case this name changes
-    )
-    {% endif %}
-    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11
 
     union all 
   --uniswap pools from ovm1
@@ -83,19 +82,11 @@
     ,false as is_self_destruct
     ,'ovm1 uniswap pools' as source
     ,cast(NULL as string) as creation_tx_hash
+    , 4 as pref_rnk
   from {{ ref('uniswap_optimism_ovm1_pool_mapping') }} as uni
-  where 
-    true
-    {% if is_incremental() %} -- this filter will only be applied on an incremental run 
-    and not exists (
-      select 1 
-      from {{ this }} as gc
-      where 
-        gc.contract_address = lower(newaddress)
-        and gc.contract_project LIKE 'Uniswap%' --future proof in case this name changes
-    )
-    {% endif %}
-    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+  ) a
 )
 ,get_contracts as (
   select 
@@ -123,6 +114,7 @@
     {% endfor %}
   from get_contracts
   where contract_address is not NULL 
+  AND c_rank = 1 -- get first instance, no dupes
   group by 1
 )
 select 
@@ -145,23 +137,14 @@ select
   ,cast( coalesce(co.contract_name, c.contract_name) as varchar(250)) as contract_name
   ,coalesce(c.creator_address, ovm1c.creator_address) as creator_address
   ,coalesce(c.created_time, to_timestamp(ovm1c.created_time)) as created_time
-  ,coalesce(c.contract_factory, 
-  {% if is_incremental() %}
-    th.contract_creator_if_factory
-    {% else -%}
-    NULL
-  {% endif %}
-  ) as contract_creator_if_factory
+  ,c.contract_factory as contract_creator_if_factory
   ,coalesce(c.is_self_destruct, false) as is_self_destruct
   ,c.creation_tx_hash
 from cleanup as c 
+
 left join {{ source('ovm1_optimism', 'contracts') }} as ovm1c
   on c.contract_address = ovm1c.contract_address --fill in any missing contract creators
 left join {{ ref('contracts_optimism_project_name_mappings') }} as dnm -- fix names for decoded contracts
   on lower(c.contract_project) = lower(dnm.dune_name)
 left join {{ ref('contracts_optimism_contract_overrides') }} as co --override contract maps
   on lower(c.contract_address) = lower(co.contract_address)
-{% if is_incremental() %} -- this filter will only be applied on an incremental run 
-left join {{ this }} th -- grab if the contract was previously picked up as factory created
-  ON lower(th.contract_address) = lower(c.contract_address)
-{% endif %}
