f70092806e6b483ada5a443a81ee79f4e0d1f359
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-25 17:23:10 -0400

    update chain info table

--- models/chain_info/chain_info_dune_chains.sql
+++ models/chain_info/chain_info_dune_chains.sql
@@ -8,15 +8,21 @@
 
 
 SELECT dune_name
+     , chain_name
      , chain_id
+     , chain_type
 FROM (
   VALUES
-    ('ethereum', 1),
-    ('optimism', 10),
-    ('bnb', 56),
-    ('gnosis', 100),
-    ('polygon', 137),
-    ('fantom', 250),
-    ('arbitrum', 42161),
-    ('avalanche_c', 43114)
-) AS dune_chain_name_to_chain_id (dune_name, chain_id)
+    ('ethereum',	'Ethereum Mainnet',	1,	'Mainnet'),
+    ('optimism',	'OP Mainnet',	10,		'Mainnet'),
+    ('bnb',		'BNB Chain',	56,		'Mainnet'),
+    ('gnosis',		'Gnosis Chain',	100,		'Mainnet'),
+    ('polygon',		'Polygon Mainnet',	137,		'Mainnet'),
+    ('fantom',		'Fantom Mainnet',	250,	'Mainnet'),
+    ('arbitrum',	'Arbitrum One',	42161,		'Mainnet'),
+    ('avalanche_c',	'Avalanche C-Chain',	43114,	'Mainnet'),
+    ('celo',		'Celo Mainnet',	42220,	'Mainnet'),
+
+    ('goerli',		'Ethereum Goerli',	5,	'Testnet')
+
+) AS dune_chain_name_to_chain_id (dune_name, chain_name, chain_id, chain_type)
