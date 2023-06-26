016aa800f9c9742b2770939ce72211d666cd556c
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-06-25 23:25:34 -0400

    source

--- models/contracts/contracts_predeploys.sql
+++ models/contracts/contracts_predeploys.sql
@@ -20,13 +20,13 @@ SELECT * FROM
     ,trace_creator_address
     ,contract_address
     ,contract_project
-    ,token_symbol
     ,contract_name
     ,creator_address
     ,created_time
     ,contract_creator_if_factory
     ,is_self_destruct
     ,creation_tx_hash
+    ,source
     FROM {{ pre[1] }}
     {% if not loop.last %}
     {% if is_incremental() %}
