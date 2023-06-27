1b5d8906933573d2090c77a620ce4feb7900ea68
Alex Courouble <alexandre@dune.com>
2023-06-26 20:17:51 -0700

    trailing /

--- macros/dune/schema.sql
+++ macros/dune/schema.sql
@@ -3,8 +3,8 @@
 {% endmacro %}
 
 {% macro default__create_schema(relation) -%}
-  {% set s3_bucket = 'prod-spellbook-trino-118330671040' %}
+  {% set s3_bucket = 'trino-dev-datasets-118330671040' %}
   {%- call statement('create_schema') -%}
-   CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket}}/hive')
+   CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket}}/hive/')
   {% endcall %}
 {% endmacro %}
