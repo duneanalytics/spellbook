c7104e5380c59c8d37fbdfe3fc2641464c8f7137
Alex Courouble <alexandre@dune.com>
2023-06-29 11:44:06 -0700

    Removes /hive from schema and table location

--- macros/dune/schema.sql
+++ macros/dune/schema.sql
@@ -5,6 +5,6 @@
 {% macro default__create_schema(relation) -%}
   {% set s3_bucket = 'prod-spellbook-trino-118330671040' %}
   {%- call statement('create_schema') -%}
-   CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket}}/hive/')
+   CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket}}/')
   {% endcall %}
 {% endmacro %}
