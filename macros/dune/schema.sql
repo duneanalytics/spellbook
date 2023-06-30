414f7329ad98823119e446da9dce934ed832ad89
Alex Courouble <alexandre@dune.com>
2023-06-29 23:03:56 -0700

    remove dev if

--- macros/dune/schema.sql
+++ macros/dune/schema.sql
@@ -4,10 +4,7 @@
 
 {% macro default__create_schema(relation) -%}
   {% set s3_bucket = var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') %}
-  {%- if target.name == 'dev' -%}
-    {%- set catalog = 'hive/' -%}
-  {%- endif -%}
   {%- call statement('create_schema') -%}
-   CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket}}/{catalog}')
+   CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket}}/')
   {% endcall %}
 {% endmacro %}
