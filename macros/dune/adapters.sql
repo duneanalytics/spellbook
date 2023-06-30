36013bc86399e3984dee57faaf9982475b2265ef
Alex Courouble <alexandre@dune.com>
2023-06-29 23:26:01 -0700

    Add env to CI

--- macros/dune/adapters.sql
+++ macros/dune/adapters.sql
@@ -8,7 +8,8 @@
 {% endmacro %}
 
 {% macro create_table_properties(properties, relation) %}
-  {% set s3_bucket = var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') %}
+  {# {% set s3_bucket = var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') %} #}
+  {% set s3_bucket = 'prod-spellbook-trino-118330671040' %}
   {% set modified_identifier = relation.identifier | replace("__dbt_tmp", "") %}
   {%- set unique_location = modified_identifier ~ '_' ~ time_salted_md5_prefix() -%}
       WITH (
