ca75cab9ff2281b7b786ee0435a27c14415f27e4
jeff-dude <jeff@dune.xyz>
2023-06-30 12:53:46 -0400

    update back to spellbook-local

--- macros/dune/no-relation-listing.sql
+++ macros/dune/no-relation-listing.sql
@@ -5,7 +5,7 @@
  #}
 {% macro get_catalog(information_schema, schemas) -%}
   {% do log(target) %}
-  {%- if (var('no-relation-listing', 'false').lower() == 'true') or (target.profile_name == 'spellbook') -%}
+  {%- if (var('no-relation-listing', 'false').lower() == 'true') or (target.profile_name == 'spellbook-local') -%}
     {{ return([]) }}
   {%- else -%}
     {{ return(adapter.dispatch('get_catalog')(information_schema, schemas)) }}
@@ -14,7 +14,7 @@
 
 {% macro list_schemas(database) -%}
   {% do log(target) %}
-  {%- if (var('no-relation-listing', 'false').lower() == 'true') or (target.profile_name == 'spellbook') -%}
+  {%- if (var('no-relation-listing', 'false').lower() == 'true') or (target.profile_name == 'spellbook-local') -%}
     {{ return([]) }}
   {%- else -%}
     {{ return(adapter.dispatch('list_schemas')(database)) }}
@@ -22,7 +22,7 @@
 {%- endmacro %}
 
 {% macro list_relations_without_caching(schema_relation) %}
-  {%- if (var('no-relation-listing', 'false').lower() == 'true') or (target.profile_name == 'spellbook') -%}
+  {%- if (var('no-relation-listing', 'false').lower() == 'true') or (target.profile_name == 'spellbook-local') -%}
     {{ return([]) }}
   {%- else -%}
     {{ return(adapter.dispatch('list_relations_without_caching')(schema_relation)) }}
