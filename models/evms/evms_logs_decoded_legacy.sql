4f78e02ccecafadddf0e1e5d633d5ceb5bf8ef28
Hildebert Moulié <h.moulie@gmail.com>
2023-07-01 12:26:49 +0200

    add legacy files and dunesqlify the rest

--- models/evms/evms_logs_decoded_legacy.sql
+++ models/evms/evms_logs_decoded_legacy.sql
@@ -1,6 +1,5 @@
 {{ config(
-        tags = ['dunesql'],
-        alias = alias('logs_decoded'),
+        alias = alias('logs_decoded', legacy_model=True),
         unique_key=['blockchain', 'tx_hash'],
         post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo"]\',
                                     "sector",
--- models/evms/evms_logs_legacy.sql
+++ models/evms/evms_logs_legacy.sql
@@ -1,6 +1,5 @@
 {{ config(
-        tags = ['dunesql'],
-        alias = alias('logs'),
+        alias = alias('logs', legacy_model=True),
         unique_key=['blockchain', 'tx_hash'],
         post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo"]\',
                                     "sector",
