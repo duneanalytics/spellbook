ba239ee0cbe46acef9ca791c653bf3aea7fe86f2
Alex Courouble <alexandre@dune.com>
2023-07-02 22:56:41 -0700

    Only assign the dbt s3 bucket for dunesql matrix

--- macros/dune/schema.sql
+++ macros/dune/schema.sql
@@ -3,7 +3,7 @@
 {% endmacro %}
 
 {% macro default__create_schema(relation) -%}
-  {% set s3_bucket = 'prod-spellbook-trino-118330671040' %}
+  {% set s3_bucket = var('DBT_ENV_CUSTOM_ENV_S3_BUCKET', 'local') %}
   {%- call statement('create_schema') -%}
    CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket}}/')
   {% endcall %}
