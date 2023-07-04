72e723c31982641515d4058e40bfe113aec590a5
Alex Courouble <alexandre@dune.com>
2023-07-03 22:04:46 -0700

    Generates legacy files for tokens_nft

---
+++ models/tokens/optimism/tokens_optimism_nft_legacy.sql
@@ -0,0 +1,20 @@
+{{ config(
+        alias = alias('nft', legacy_model=True)
+        , materialized = 'table'
+        , post_hook='{{ expose_spells(\'["optimism"]\',
+                                "sector",
+                                "tokens",
+                                \'["0xRob"]\') }}'
+        )
+}}
+
+SELECT
+    c.contract_address
+  , coalesce(t.name,b.name) as name
+  , coalesce(t.symbol,b.symbol) as symbol
+  , c.standard
+FROM {{ ref('tokens_optimism_nft_standards')}} c
+LEFT JOIN  {{ref('tokens_optimism_nft_curated')}} t
+    ON c.contract_address = t.contract_address
+LEFT JOIN {{ ref('tokens_optimism_nft_bridged_mapping')}} b
+    ON c.contract_address = b.contract_address
