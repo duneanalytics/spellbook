50719d046a408e7f45638b76d50fb1a520458859
viniabussafi <vinicius@balancerlabs.dev>
2023-07-04 10:26:50 +0100

    again

--- models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges_polygon.sql
+++ models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges_polygon.sql
@@ -1,39 +1,38 @@
-{{config(alias='balancer_v2_gauges_polygon',
-        post_hook='{{ expose_spells(\'["polygon"]\',
+{{config(alias='balancer_v2_gauges_optimism',
+        post_hook='{{ expose_spells(\'["optimism"]\',
                                     "sector",
                                     "labels",
                                     \'["jacektrocinski"]\') }}')}}
 
 SELECT
-    'polygon' AS blockchain,
+    'optimism' AS blockchain,
     gauge.gauge AS address,
-    'pol:' || pools.name  AS name,
+    'op:' || pools.name AS name,
     'balancer_v2_gauges' AS category,
     'balancerlabs' AS contributor,
     'query' AS source,
     TIMESTAMP('2022-01-13') AS created_at,
     NOW() AS updated_at,
-    'balancer_v2_gauges_polygon' AS model_name,
+    'balancer_v2_gauges_optimism' AS model_name,
     'identifier' AS label_type
 FROM
-    {{ source('balancer_ethereum', 'PolygonRootGaugeFactory_evt_PolygonRootGaugeCreated') }} gauge
-    LEFT JOIN {{ source('balancer_polygon', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON gauge.recipient = streamer.streamer
-    LEFT JOIN {{ ref('labels_balancer_v2_pools_polygon') }} pools ON pools.address = streamer.pool
+    {{ source('balancer_ethereum', 'OptimismRootGaugeFactory_evt_OptimismRootGaugeCreated') }} gauge
+    LEFT JOIN {{ source('balancer_optimism', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON gauge.recipient = streamer.streamer
+    LEFT JOIN {{ ref('labels_balancer_v2_pools_optimism') }} pools ON pools.address = streamer.pool
 UNION ALL
 SELECT
-    'polygon' AS blockchain,
+    'optimism' AS blockchain,
     gauge.gauge AS address,
-    'pol:' || pools.name  AS name,
+    'op:' || pools.name AS name,
     'balancer_v2_gauges' AS category,
     'balancerlabs' AS contributor,
     'query' AS source,
     TIMESTAMP('2022-01-13') AS created_at,
     NOW() AS updated_at,
-    'balancer_v2_gauges_polygon' AS model_name,
+    'balancer_v2_gauges_optimism' AS model_name,
     'identifier' AS label_type
 FROM
-    {{ source('balancer_ethereum', 'CappedPolygonRootGaugeFactory_evt_GaugeCreated') }} gauge
-    INNER JOIN {{ source('balancer_ethereum', 'CappedPolygonRootGaugeFactory_call_create') }} call ON call.call_tx_hash = gauge.evt_tx_hash
-    LEFT JOIN {{ source('balancer_polygon', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON streamer.streamer = call.recipient
-    LEFT JOIN {{ ref('labels_balancer_v2_pools_polygon') }} pools ON pools.address = streamer.pool;
-
+    {{ source('balancer_ethereum', 'CappedOptimismRootGaugeFactory_evt_GaugeCreated') }} gauge
+    INNER JOIN {{ source('balancer_ethereum', 'CappedOptimismRootGaugeFactory_call_create') }} call ON call.call_tx_hash = gauge.evt_tx_hash
+    LEFT JOIN {{ source('balancer_optimism', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON streamer.streamer = call.recipient
+    LEFT JOIN {{ ref('labels_balancer_v2_pools_optimism') }} pools ON pools.address = streamer.pool;
