c67a5175781ba35d6deb2dd9e7588dd07400ef8b
viniabussafi <vinicius@balancerlabs.dev>
2023-07-04 10:21:28 +0100

    retry

--- models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges_optimism.sql
+++ models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges_optimism.sql
@@ -36,3 +36,4 @@ FROM
     INNER JOIN {{ source('balancer_ethereum', 'CappedOptimismRootGaugeFactory_call_create') }} call ON call.call_tx_hash = gauge.evt_tx_hash
     LEFT JOIN {{ source('balancer_optimism', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON streamer.streamer = call.recipient
     LEFT JOIN {{ ref('labels_balancer_v2_pools_optimism') }} pools ON pools.address = streamer.pool;
+
