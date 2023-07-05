13c4fb17f355d88c0dfb79d6c931baa7ee801793
Jonas Irgens Kylling <jonas@dune.xyz>
2023-07-05 10:32:31 +0200

    Make nft_transfers match Spark

--- macros/models/_sector/nft/nft_transfers.sql
+++ macros/models/_sector/nft/nft_transfers.sql
@@ -1,13 +1,15 @@
 {% macro nft_transfers(blockchain, base_transactions, erc721_transfers, erc1155_single, erc1155_batch ) %}
+{%- set token_standard_721 = 'bep721' if blockchain == 'bnb' else 'erc721' -%}
+{%- set token_standard_1155 = 'bep1155' if blockchain == 'bnb' else 'erc1155' -%}
+{%- set spark_mode = True -%} {# TODO: Potential bug. Consider disabling #}
 SELECT
     *
-    , blockchain ||'-'|| cast(block_number as varchar) ||'-'|| cast(tx_hash as varchar) ||'-'|| cast(evt_index as varchar) AS unique_transfer_id
 FROM(
      SELECT '{{blockchain}}' as blockchain
     , t.evt_block_time AS block_time
     , date_trunc('day', t.evt_block_time) AS block_date
     , t.evt_block_number AS block_number
-    , case when '{{blockchain}}' = 'bnb' then 'bep721' else 'erc721' end AS token_standard
+    , '{{token_standard_721}}' AS token_standard
     , 'single' AS transfer_type
     , t.evt_index
     , t.contract_address
@@ -17,6 +19,7 @@ FROM(
     , t.to
     , et."from" AS executed_by
     , t.evt_tx_hash AS tx_hash
+    , '{{blockchain}}' || cast(t.evt_tx_hash as varchar) || '-{{token_standard_721}}-' || cast(t.contract_address as varchar) || '-' || cast(t.tokenId as varchar) || '-' || cast(t."from" as varchar) || '-' || cast(t.to as varchar) || '-1-' || cast(t.evt_index as varchar) AS unique_transfer_id
     FROM {{ erc721_transfers }} t
         {% if is_incremental() %}
             LEFT JOIN {{this}} anti_table
@@ -38,7 +41,7 @@ FROM(
     , t.evt_block_time AS block_time
     , date_trunc('day', t.evt_block_time) AS block_date
     , t.evt_block_number AS block_number
-    , case when '{{blockchain}}' = 'bnb' then 'bep1155' else 'erc1155' end AS token_standard
+    , '{{token_standard_1155}}' AS token_standard
     , 'single' AS transfer_type
     , t.evt_index
     , t.contract_address
@@ -48,6 +51,7 @@ FROM(
     , t.to
     , et."from" AS executed_by
     , t.evt_tx_hash AS tx_hash
+    , '{{blockchain}}' || cast(t.evt_tx_hash as varchar) || '-{{token_standard_1155}}-' || cast(t.contract_address as varchar) || '-' || cast(t.id as varchar) || '-' || cast(t."from" as varchar) || '-' || cast(t.to as varchar) || '-' || cast(t.value as varchar) || '-' || cast(t.evt_index as varchar) AS unique_transfer_id
     FROM {{ erc1155_single }} t
         {% if is_incremental() %}
             LEFT JOIN {{this}} anti_table
@@ -69,7 +73,7 @@ FROM(
     , t.evt_block_time AS block_time
     , date_trunc('day', t.evt_block_time) AS block_date
     , t.evt_block_number AS block_number
-    , case when '{{blockchain}}' = 'bnb' then 'bep1155' else 'erc1155' end AS token_standard
+    , '{{token_standard_1155}}'  AS token_standard
     , 'batch' AS transfer_type
     , t.evt_index
     , t.contract_address
@@ -79,6 +83,7 @@ FROM(
     , t.to
     , et."from" AS executed_by
     , t.evt_tx_hash AS tx_hash
+    , '{{blockchain}}' || cast(t.evt_tx_hash as varchar) || '-{{token_standard_1155}}-' || cast(t.contract_address as varchar) || '-' || cast(t.id as varchar) || '-' || cast(t."from" as varchar) || '-' || cast(t.to as varchar) || '-' || cast(t.value as varchar) || '-' || cast(t.evt_index as varchar) AS unique_transfer_id
     FROM (
         SELECT t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t."from", t.to, t.evt_index
         , value, id
@@ -90,13 +95,23 @@ FROM(
         WHERE t.evt_block_time >= date_trunc('day', now() - interval '7' day)
             AND anti_table.tx_hash is null
         {% endif %}
+        {% if spark_mode == True %}
+        {# This deduplicates rows. Double check if this is correct or not #}
+        GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
+        {% endif %}
         ) t
     INNER JOIN {{ base_transactions }} et ON et.block_number = t.evt_block_number
         AND et.hash = t.evt_tx_hash
         {% if is_incremental() %}
         AND et.block_time >= date_trunc('day', now() - interval '7' day)
         {% endif %}
-    WHERE t.value > cast(0 as uint256)
+    {% if spark_mode == True %}
+    {# TODO: This is a bug. In the comparsion t.value > 0, spark converts t.value to an integer before the comparison,
+    or null (i.e., false) if it overflows) #}
+    WHERE t.value > uint256 '0' and t.value < uint256 '{{2**31}}'
+    {% else %}
+    WHERE t.value > uint256 '0'
+    {% endif %}
 )
 
 {% endmacro %}
