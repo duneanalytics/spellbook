2a8db7be27bd534babfa8e39271e55f4e43ccb73
Jonas Irgens Kylling <jkylling@users.noreply.github.com>
2023-07-05 21:06:33 +0200

    Only check first degree children and parents for DuneSQL and Spark overlap (#3662)
    * Only check first degree children and parents for DuneSQL and Spark overlap
    
    It's sufficient to check that the first layer of parents or children are non-overlapping. This makes the error messages smaller.
    
    * Only show matching models

--- scripts/dunesql_spark_empty_intersection.sh
+++ scripts/dunesql_spark_empty_intersection.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-res=$(comm -12 <( dbt ls --exclude tag:dunesql | sort | grep -v "^source" ) <( dbt ls --select +tag:dunesql+ | sort | grep -v "^source" ))
+res=$(comm -12 <( dbt ls --select resource_type:model --exclude tag:dunesql | sort ) <( dbt ls --exclude resource_type:source --select 1+tag:dunesql+1,resource_type:model | sort ))
 if [ -z "$res" ]; then
   exit 0
 else
