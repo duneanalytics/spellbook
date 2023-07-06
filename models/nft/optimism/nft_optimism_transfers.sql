51ec4b76aa85419ef9f5a59c1e2da13977ec3bc3
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-06 17:42:36 -0400

    mod

--- models/nft/optimism/nft_optimism_transfers.sql
+++ models/nft/optimism/nft_optimism_transfers.sql
@@ -1,5 +1,6 @@
 {{ config(
-        alias = alias('transfers', legacy_model=True),
+        alias = alias('transfers'),
+        tags = ['dunesql'],
         partition_by='block_date',
         materialized='incremental',
         file_format = 'delta',
