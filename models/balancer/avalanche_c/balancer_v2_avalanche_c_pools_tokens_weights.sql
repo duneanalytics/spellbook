1b50c2f91846ecb48518ae0620e53ee76990963a
viniabussafi <vinicius@balancerlabs.dev>
2023-07-06 16:03:17 +0100

    new

--- models/balancer/avalanche_c/balancer_v2_avalanche_c_pools_tokens_weights.sql
+++ models/balancer/avalanche_c/balancer_v2_avalanche_c_pools_tokens_weights.sql
@@ -1,12 +1,12 @@
 {{
     config(
-        schema='balancer_v2_arbitrum',
+        schema='balancer_v2_avalanche_c',
         alias='pools_tokens_weights',
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
         unique_key = ['pool_id', 'token_address'],
-        post_hook='{{ expose_spells(\'["arbitrum"]\',
+        post_hook='{{ expose_spells(\'["avalanche_c"]\',
                                     "project",
                                     "balancer_v2",
                                     \'["metacrypto", "jacektrocinski"]\') }}'
@@ -20,8 +20,8 @@ SELECT
     registered.poolId AS pool_id,
     tokens.token_address,
     weights.normalized_weight / POWER(10, 18) AS normalized_weight
-FROM {{ source('balancer_v2_arbitrum', 'Vault_evt_PoolRegistered') }} registered
-INNER JOIN {{ source('balancer_v2_arbitrum', 'WeightedPoolFactory_call_create') }} call_create
+FROM {{ source('balancer_v2_avalanche_c', 'Vault_evt_PoolRegistered') }} registered
+INNER JOIN {{ source('balancer_v2_avalanche_c', 'WeightedPoolFactory_call_create') }} call_create
     ON call_create.output_0 = SUBSTRING(registered.poolId, 0, 42)
     LATERAL VIEW posexplode(call_create.tokens) tokens AS pos, token_address
     LATERAL VIEW posexplode(call_create.normalizedWeights) weights AS pos, normalized_weight
