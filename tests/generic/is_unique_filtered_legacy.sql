e7f0e605f84b63c5cca1d8727fe4038c5c9d01d3
0xRob <83790096+0xRobin@users.noreply.github.com>
2023-07-06 11:43:35 +0200

    Seed and generic test support for DuneSQL (#3676)
    * seed test support for dunesql
    
    * forgot one _legacy
    
    * add comment

---
+++ tests/generic/is_unique_filtered_legacy.sql
@@ -0,0 +1,13 @@
+{% test is_unique_filtered_legacy(model, column_name) %}
+
+select
+    {{ column_name }} as unique_field,
+    count(*) as n_records
+
+from {{ model }}
+where {{ column_name }} is not null
+    and block_date >= NOW() - interval '2 days'
+group by {{ column_name }}
+having count(*) > 1
+
+{% endtest %}
