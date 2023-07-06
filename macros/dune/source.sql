798c3678a03def4b9ed46223a394809141600ca7
0xRob <83790096+0xRobin@users.noreply.github.com>
2023-07-06 14:42:40 +0200

    Update macros/dune/source.sql
    Co-authored-by: Jonas Irgens Kylling <jkylling@users.noreply.github.com>

--- macros/dune/source.sql
+++ macros/dune/source.sql
@@ -1,6 +1,6 @@
 {% macro source(source_name, table_name) %}
   {% set rel = builtins.source(source_name, table_name) %}
-  {%- if target.type == 'trino'-%}
+  {%- if target.type == 'trino' -%}
     {% set newrel = rel.replace_path(database="delta_prod") %}
     {% do return(newrel) %}
   {%- else -%}
