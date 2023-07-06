0d495a1b0adc048e983c5fede76667aeefb0a450
0xRob <83790096+0xRobin@users.noreply.github.com>
2023-07-06 19:48:05 +0200

    migrate foundation_ethereum_base_trades (#3664)
    * migrate foundation_ethereum_base_trades
    
    * migrate foundation seed test
    
    * fix check_seed_macro
    
    * tag test with sources as dunesql
    
    * exclusion tag in git CI seed step
    
    * try different approach for sources
    
    * typo
    
    * Update macros/dune/source.sql
    
    Co-authored-by: Jonas Irgens Kylling <jkylling@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jonas Irgens Kylling <jkylling@users.noreply.github.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/_sector/nft/trades/nft_ethereum_trades_beta.sql
+++ models/_sector/nft/trades/nft_ethereum_trades_beta.sql
@@ -12,7 +12,7 @@
 {% set base_models = [
      ('archipelago',    'v1',   ref('archipelago_ethereum_base_trades'))
     ,('superrare',    'v1',   ref('superrare_ethereum_base_trades'))
-    ,('foundation',    'v1',   ref('foundation_ethereum_base_trades'))
+    ,('foundation',    'v1',   ref('foundation_ethereum_base_trades_legacy'))
     ,('blur',    'v1',   ref('blur_ethereum_base_trades'))
     ,('element',    'v1',   ref('element_ethereum_base_trades'))
     ,('x2y2',    'v1',   ref('x2y2_ethereum_base_trades'))
