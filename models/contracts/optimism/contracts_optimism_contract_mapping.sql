339f8de026a0e113c5a763631b13d7b558e28720
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-07 19:34:31 -0400

    map

--- models/contracts/optimism/contracts_optimism_contract_mapping.sql
+++ models/contracts/optimism/contracts_optimism_contract_mapping.sql
@@ -336,7 +336,7 @@ WHERE contract_order = 1
   from creator_contracts as cc 
   left join {{ source('optimism', 'contracts') }} as oc 
     on cc.contract_address = oc.address 
-  group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+  group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
 
   
   union all
