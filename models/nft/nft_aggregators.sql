fcc2bb9ee46772b0bab7695834b7bd91ff115a06
0xRob <0xRobin5@gmail.com>
2023-07-07 11:08:21 +0200

    migrate models to dunesql

--- models/nft/nft_aggregators.sql
+++ models/nft/nft_aggregators.sql
@@ -1,17 +1,18 @@
 {{ config(
-        alias ='aggregators',
+        tags = ['dunesql'],
+        alias = alias('aggregators'),
         post_hook='{{ expose_spells(\'["avalanche_c","bnb","ethereum","polygon", "optimism"]\',
                                     "sector",
                                     "nft",
                                     \'["soispoke","hildobby", "chuxin"]\') }}')
 }}
 
-SELECT 'avalanche_c' as blockchain, * FROM  {{ ref('nft_avalanche_c_aggregators_legacy') }}
+SELECT 'avalanche_c' as blockchain, * FROM  {{ ref('nft_avalanche_c_aggregators') }}
 UNION ALL
-SELECT 'bnb' as blockchain, * FROM  {{ ref('nft_bnb_aggregators_legacy') }}
+SELECT 'bnb' as blockchain, * FROM  {{ ref('nft_bnb_aggregators') }}
 UNION ALL
-SELECT 'ethereum' as blockchain, * FROM  {{ ref('nft_ethereum_aggregators_legacy') }}
+SELECT 'ethereum' as blockchain, * FROM  {{ ref('nft_ethereum_aggregators') }}
 UNION ALL
-SELECT 'polygon' as blockchain, * FROM  {{ ref('nft_polygon_aggregators_legacy') }}
+SELECT 'polygon' as blockchain, * FROM  {{ ref('nft_polygon_aggregators') }}
 UNION ALL
-SELECT 'optimism' as blockchain, * FROM  {{ ref('nft_optimism_aggregators_legacy') }}
+SELECT 'optimism' as blockchain, * FROM  {{ ref('nft_optimism_aggregators') }}
