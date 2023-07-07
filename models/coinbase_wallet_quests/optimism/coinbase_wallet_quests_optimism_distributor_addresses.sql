cee869bc5f26bd1f275f9adc1ea3302d19ada742
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-07 11:27:16 -0400

    cast

--- models/coinbase_wallet_quests/optimism/coinbase_wallet_quests_optimism_distributor_addresses.sql
+++ models/coinbase_wallet_quests/optimism/coinbase_wallet_quests_optimism_distributor_addresses.sql
@@ -11,7 +11,9 @@
 {% set op_token = 0x4200000000000000000000000000000000000042 %}
 
 WITH quest_addresses AS (
-SELECT distributor_address, rewards_token, quest_name
+SELECT cast(distributor_address as varbinary) AS distributor_address
+    , cast(rewards_token as varbinary) AS rewards_token
+    , quest_name
 FROM (values
     --  (0x9fFD84fA80932Fa55E761B06398aA2577815c459,'Admin & Gas Fee') --ignore
      (0xf42279467D821bCDf40b50E9A5d2cACCc4Cf5b30,'{{op_token}}','Quest 1 - DEX')
