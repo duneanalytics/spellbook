e38330d451d28153c764828507849b7580f53127
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-07 11:11:42 -0400

    test

--- models/tokens/optimism/tokens_optimism_nft.sql
+++ models/tokens/optimism/tokens_optimism_nft.sql
@@ -13,8 +13,9 @@ SELECT
   , coalesce(t.name,b.name) as name
   , coalesce(t.symbol,b.symbol) as symbol
   , c.standard
+
 FROM {{ ref('tokens_optimism_nft_standards')}} c
-LEFT JOIN  {{ref('tokens_optimism_nft_curated')}} t
+LEFT JOIN {{ref('tokens_optimism_nft_curated')}} t
     ON c.contract_address = t.contract_address
 LEFT JOIN {{ ref('tokens_optimism_nft_bridged_mapping')}} b
     ON c.contract_address = b.contract_address
