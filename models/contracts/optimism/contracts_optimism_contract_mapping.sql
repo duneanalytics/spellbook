af5f3a64730e3364a66b91e49900f86e597c6673
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-07 14:46:05 -0400

    timestamp

--- models/contracts/optimism/contracts_optimism_contract_mapping.sql
+++ models/contracts/optimism/contracts_optimism_contract_mapping.sql
@@ -492,7 +492,9 @@ FROM (
     ,c.token_symbol
     ,cast( coalesce(co.contract_name, c.contract_name) as varchar) as contract_name
     ,coalesce(c.creator_address, ovm1c.creator_address) as creator_address
-    ,coalesce(c.created_time, from_iso8601_timestamp(cast(ovm1c.created_time as varchar)) ) as created_time
+    ,coalesce(c.created_time
+      , cast( from_iso8601_timestamp(cast(ovm1c.created_time as varchar) ) AT TIME ZONE 'UTC' AS TIMESTAMP)
+       ) as created_time
     ,coalesce(c.contract_factory, 
     {% if is_incremental() %}
       th.contract_creator_if_factory
