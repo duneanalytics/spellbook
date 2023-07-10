fd6840274c2b72ad065b34d26cae3935b5d757cc
0xRob <0xRobin5@gmail.com>
2023-07-10 11:20:36 +0200

    fix element base trades legacy

--- models/_sector/nft/trades/ethereum/platforms/element_ethereum_base_trades_legacy.sql
+++ models/_sector/nft/trades/ethereum/platforms/element_ethereum_base_trades_legacy.sql
@@ -10,7 +10,7 @@
 }}
 
 WITH base_trades as (
-    {{ element_v1_base_trades(
+    {{ element_v1_base_trades_legacy(
           erc721_sell_order_filled = source('element_ex_ethereum','OrdersFeature_evt_ERC721SellOrderFilled')
         , erc721_buy_order_filled = source('element_ex_ethereum','OrdersFeature_evt_ERC721BuyOrderFilled')
         , erc1155_sell_order_filled = source('element_ex_ethereum','OrdersFeature_evt_ERC1155SellOrderFilled')
