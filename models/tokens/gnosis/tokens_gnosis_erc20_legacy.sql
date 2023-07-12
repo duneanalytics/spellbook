09599476842581914e6683e39ba508fd1f71c8e3
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-07-12 07:10:12 -0700

    script to add legacy tag (#3721)

--- models/tokens/gnosis/tokens_gnosis_erc20_legacy.sql
+++ models/tokens/gnosis/tokens_gnosis_erc20_legacy.sql
@@ -1,4 +1,4 @@
-{{ config( alias = alias('erc20', legacy_model=True), tags=['static'])}}
+{{ config( alias = alias('erc20', legacy_model=True), tags=['static', 'legacy'])}}
 
 SELECT LOWER(contract_address) as contract_address, symbol, decimals
 FROM (VALUES ('0x3f9463bdb502ec2079bf39da6c924d4022ff9f4c', 'biubiu.tools', 18),
