5cf042583456fb528dd5ec2d221798abcc412c01
Ben Smith <bh2smith@gmail.com>
2023-07-12 11:50:58 +0200

    migrate cow.solvers on both networks

--- tests/cow_protocol/ethereum/cow_protocol_ethereum_assert_solvers.sql
+++ tests/cow_protocol/ethereum/cow_protocol_ethereum_assert_solvers.sql
@@ -4,13 +4,13 @@ WITH unit_test1
         FROM   {{ ref('cow_protocol_ethereum_solvers' )}}
         WHERE  active = true
         GROUP BY environment, name
-        HAVING cnt > 1),
+        HAVING COUNT(address) > 1),
 -- Given a list of solvers, , then we should never see a solver who's both true and false for active
     unit_test2
     AS (SELECT count(address) as cnt
         FROM   {{ ref('cow_protocol_ethereum_solvers' )}}
         GROUP BY address, environment, name
-        HAVING cnt > 1)
+        HAVING COUNT(address) > 1)
 SELECT *
 FROM   (SELECT *
        FROM   unit_test1
