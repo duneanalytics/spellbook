f6bc0745ab2bab9906bad2e63a2ea8a4e66ebcc4
Ben Smith <bh2smith@gmail.com>
2023-07-12 11:39:55 +0200

    reducing diff

--- models/cow_protocol/ethereum/cow_protocol_ethereum_app_data.sql
+++ models/cow_protocol/ethereum/cow_protocol_ethereum_app_data.sql
@@ -8,39 +8,37 @@
 
 -- Find the PoC Query here: https://dune.com/queries/1751965
 with
-  partially_unpacked_app_content AS (
-    SELECT
-      app_hash,
-      content.appCode AS app_code,
-      content.environment,
-      content.metadata.orderClass.orderClass AS order_class,
-      content.metadata.quote,
-      content.metadata.referrer,
-      content.metadata.utm
-    FROM {{ source('cowswap', 'raw_app_data') }}
+  partially_unpacked_app_content as (
+    select
+        distinct app_hash,
+        content.appCode AS app_code,
+        content.environment,
+        content.metadata.orderClass.orderClass as order_class,
+        content.metadata.quote,
+        content.metadata.referrer,
+        content.metadata.utm
+    from {{ source('cowswap', 'raw_app_data') }}
   ),
-  unpacked_referrer_app_data AS (
-    SELECT
-      app_hash,
-      app_code,
-      environment,
-      order_class,
-      quote,
-      LOWER(COALESCE(referrer.address, referrer.referrer)) AS referrer,
-      utm
-    FROM
-      partially_unpacked_app_content
+  unpacked_referrer_app_data as (
+    select
+        app_hash,
+        app_code,
+        environment,
+        order_class,
+        quote,
+        coalesce(referrer.address, referrer.referrer)) as referrer,
+        utm
+    from partially_unpacked_app_content
   ),
-  results AS (
-    SELECT
-      app_hash,
-      app_code,
-      environment,
-      order_class,
-      referrer,
-      TRY_CAST(quote.slippageBips AS INTEGER) AS slippage_bips,
-      utm
-    FROM
-      unpacked_referrer_app_data
-  )
-SELECT * FROM results
+  results as (
+    select
+        app_hash,
+        app_code,
+        environment,
+        order_class,
+        from_hex(referrer) as referrer,
+        cast(quote.slippageBips as integer) as slippage_bips,
+        utm
+    from unpacked_referrer_app_data
+)
+select * from results
