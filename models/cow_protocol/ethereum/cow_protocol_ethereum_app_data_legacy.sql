5ffcfbe97ff573b86b506448196fb007d11dcf4c
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-07-13 20:05:34 +0200

    [CoW] Migrate and add utm to cow.app_data (#3714)
    * migrate app_data and add field utm
    
    * reducing diff
    
    * extra bracket
    
    * alias thing
    
    * fix the types

--- models/cow_protocol/ethereum/cow_protocol_ethereum_app_data_legacy.sql
+++ models/cow_protocol/ethereum/cow_protocol_ethereum_app_data_legacy.sql
@@ -1,5 +1,4 @@
-{{ config(alias=alias('app_data'),
-        tags=['dunesql'],
+{{ config(alias=alias('app_data', legacy_model=True),
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "project",
                                     "cow_protocol",
@@ -8,9 +7,9 @@
 
 -- Find the PoC Query here: https://dune.com/queries/1751965
 with
-  partially_unpacked_app_content as (
+partially_unpacked_app_content as (
     select
-        distinct from_hex(app_hash) as app_hash,
+        distinct app_hash,
         content.appCode as app_code,
         content.environment,
         content.metadata.orderClass.orderClass as order_class,
@@ -18,8 +17,9 @@ with
         content.metadata.referrer,
         content.metadata.utm
     from {{ source('cowswap', 'raw_app_data') }}
-  ),
-  unpacked_referrer_app_data as (
+),
+
+unpacked_referrer_app_data as (
     select
         app_hash,
         app_code,
@@ -27,19 +27,24 @@ with
         order_class,
         quote,
         -- different app data versions put referrer in two possible places.
-        from_hex(coalesce(referrer.address, referrer.referrer)) as referrer,
+        lower(coalesce(referrer.address, referrer.referrer)) as referrer,
         utm
     from partially_unpacked_app_content
-  ),
-  results as (
+),
+
+results as (
     select
         app_hash,
         app_code,
         environment,
         order_class,
-        from_hex(referrer) as referrer,
-        cast(quote.slippageBips as integer) as slippage_bips,
+        referrer,
+        cast(quote.slippageBips as integer) slippage_bips,
         utm
+        -- There is only one App Data using buyAmount/sellAmount fields.
+        -- cast(quote.sellAmount as double) sell_amount,
+        -- cast(quote.buyAmount as double) buy_amount
     from unpacked_referrer_app_data
 )
+
 select * from results
