b89a4a66a5c7ef704a910c4e32a9121d7c197c57
Hildebert Moulié <h.moulie@gmail.com>
2023-07-13 03:12:32 +0200

    LooksRare Seaport-routed trades

---
+++ models/looksrare/ethereum/looksrare_seaport_ethereum_base_trades.sql
@@ -0,0 +1,42 @@
+{{ config(
+    schema = 'blur_v2_ethereum',
+    tags = ['dunesql'],
+    alias = alias('base_trades'),
+    partition_by = ['block_date'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_number','tx_hash','sub_tx_trade_id'],
+    )
+}}
+
+{% set looksrare_seaport_start_date = '2023-06-28' %}
+
+SELECT 'looksrare' AS project
+, 'seaport' AS project_version
+, s.evt_block_number AS block_number
+, s.evt_tx_hash AS tx_hash
+, 'buy' AS trade_category
+, 'secondary' AS trade_type
+, from_hex(json_extract_scalar(s.offer[1], '$.token')) AS nft_contract_address
+, CAST(json_extract_scalar(s.offer[1], '$.identifier') AS UINT256) AS nft_token_id
+, CAST(json_extract_scalar(s.offer[1], '$.amount') AS UINT256) AS nft_amount
+, (CAST(json_extract_scalar(s.consideration[1], '$.amount') AS double)+CAST(try(json_extract_scalar(s.consideration[2], '$.amount')) AS double)) AS price_raw
+, 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2 AS currency_contract
+--, 'ETH' AS currency_symbol
+, CAST(json_extract_scalar(s.consideration[2], '$.amount') AS double) AS platform_fee_amount_raw
+, from_hex(json_extract_scalar(s.consideration[2], '$.recipient')) AS platform_fee_address
+, CAST(NULL AS double) AS royalty_fee_amount_raw
+, from_hex(NULL) AS royalty_fee_address
+, s.contract_address AS project_contract_address
+, s.evt_index as sub_tx_trade_id
+, "RIGHT"(CAST(txs.data AS varchar), 20) AS tx_data_end
+FROM {{ source('seaport_ethereum','Seaport_evt_OrderFulfilled') }} s
+WHERE s.contract_address = 0x00000000000000adc04c56bf30ac9d3c0aaf14dc
+AND s.zone = 0x0000000000000000000000000000000000000000
+AND try(from_hex(json_extract_scalar(s.consideration[2], '$.recipient')))  = 0x1838de7d4e4e42c8eb7b204a91e28e9fad14f536
+{% if is_incremental() %}
+AND evt_block_time >= date_trunc("day", now() - interval '1 week')
+{% else %}
+AND evt_block_time >= '{{looksrare_seaport_start_date}}'
+{% endif %}
