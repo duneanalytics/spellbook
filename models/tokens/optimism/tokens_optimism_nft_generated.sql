024702405fcd645cc0cb13b8e44845c02a33533d
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-15 12:39:02 -0400

    adds

--- models/tokens/optimism/tokens_optimism_nft_generated.sql
+++ models/tokens/optimism/tokens_optimism_nft_generated.sql
@@ -12,6 +12,7 @@ select
    b.edition_address as contract_address
   ,b.edition_name AS name
   ,b.edition_symbol AS symbol
+  ,b.royalty_pct
   ,'Sound.xyz' as platform
 from {{ ref('sound_xyz_optimism_edition_metadata') }} as b
 GROUP BY 1,2,3
@@ -22,6 +23,29 @@ select
    b.edition_address as contract_address
   ,b.edition_name AS name
   ,b.edition_symbol AS symbol
+  ,b.royalty_pct
   ,'Zora' as platform
 from {{ ref('zora_optimism_edition_metadata') }} as b
+GROUP BY 1,2,3
+
+UNION ALL
+
+select
+   b.nft_contract_address as contract_address
+  ,b.name AS name
+  ,b.symbol AS symbol
+  ,b.royalty_pct
+  ,'Decent.XYZ' as platform
+from {{ ref('decent_optimism_edition_metadata') }} as b
+GROUP BY 1,2,3
+
+UNION ALL
+
+select
+   b.edition_address as contract_address
+  ,b.name AS name
+  ,b.symbol AS symbol
+  ,b.royalty_pct
+  ,'Mirror' as platform
+from {{ ref('mirror_optimism_edition_metadata') }} as b
 GROUP BY 1,2,3
