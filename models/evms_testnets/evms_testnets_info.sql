fd861fd98c16e1dc733c513c02f83560464036c7
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-14 22:09:02 -0400

    cast

--- models/evms_testnets/evms_testnets_info.sql
+++ models/evms_testnets/evms_testnets_info.sql
@@ -9,6 +9,6 @@
 
 SELECT chain_id, blockchain, name, chain_type, rollup_type, native_token_symbol, wrapped_native_token_address, explorer_link, first_block_time
 FROM (VALUES
-        (1, 'goerli', 'Ethereum Goerli', 'Layer 1', NULL, 'ETH', 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2, 'https://goerli.etherscan.io', timestamp '2019-01-31 03:10')
+        (1, 'goerli', 'Ethereum Goerli', 'Layer 1', cast(NULL as varchar), 'ETH', 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2, 'https://goerli.etherscan.io', timestamp '2019-01-31 03:10')
 
         ) AS temp_table (chain_id, blockchain, name, chain_type, rollup_type, native_token_symbol, wrapped_native_token_address, explorer_link, first_block_time)
