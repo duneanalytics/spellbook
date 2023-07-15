2e3dacff32eac2f2f624dc4a842efa8f00b7336f
Huang Geyang <Sukebeta@outlook.com>
2023-07-16 02:54:37 +0800

    Remove quotes and add tags for models/addresses/*

--- models/addresses/optimism/addresses_optimism_fee_vaults.sql
+++ models/addresses/optimism/addresses_optimism_fee_vaults.sql
@@ -1,13 +1,14 @@
 {{config(alias = alias('fee_vaults'),
+        tags=['static', 'dunesql'],
         post_hook='{{ expose_spells(\'["optimism"]\',
                                     "sector",
                                     "addresses",
                                     \'["msilb7"]\') }}')}}
 
 -- https://github.com/ethereum-optimism/optimism/blob/develop/op-bindings/predeploys/addresses.go
-SELECT  lower(address) as address, vault_name
+SELECT  address as address, vault_name
 FROM (VALUES
-      ("0x4200000000000000000000000000000000000011","SequencerFeeVault")
-     ,("0x4200000000000000000000000000000000000019","BaseFeeVault")
-     ,("0x420000000000000000000000000000000000001a","L1FeeVault")
+      (0x4200000000000000000000000000000000000011,'SequencerFeeVault')
+     ,(0x4200000000000000000000000000000000000019,'BaseFeeVault')
+     ,(0x420000000000000000000000000000000000001a,'L1FeeVault')
     ) AS x (address, vault_name)
