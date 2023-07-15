df027e8d9f774c2437b54b74be1fa6b1d89afb2b
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-15 18:54:46 -0400

    sources

--- models/worldcoin/optimism/worldcoin_optimism_accounts.sql
+++ models/worldcoin/optimism/worldcoin_optimism_accounts.sql
@@ -15,7 +15,7 @@
 WITH wld_deployers AS (
     SELECT
     to as worldcoin_deployer_address
-    FROM optimism.transactions
+    FROM {{ source('optimism','transactions') }}
     WHERE "from" = 0x86c5608362b3fbbeb721140472229392f754ef87
     AND value > cast(0 as uint256)
     AND block_number >= 105870092 --first transfer
@@ -31,13 +31,13 @@ worldcoin_deployer_address
 , ct.block_number AS created_block_number
 
 FROM wld_deployers w
-INNER JOIN optimism.transactions t
+INNER JOIN {{ source('optimism','transactions') }} t
     ON t."from" = worldcoin_deployer_address
     AND t.block_number >= 105870092 --first transfer
     {% if is_incremental() %}
     AND t.block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
-INNER JOIN optimism.creation_traces ct
+INNER JOIN {{ source('optimism','creation_traces') }}  ct
     ON ct.tx_hash = t.hash
     AND ct.block_number = t.block_number
     AND ct.block_number >= 105870092 --first transfer
