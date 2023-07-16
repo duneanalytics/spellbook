0199a361bc45d3ab49990e0aa55730ee95286b1c
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-15 21:42:57 -0400

    tag

--- models/galxe/optimism/galxe_optimism_nft_mints_legacy.sql
+++ models/galxe/optimism/galxe_optimism_nft_mints_legacy.sql
@@ -1,5 +1,6 @@
 {{ config(
     alias = alias('nft_mints', legacy_model=True),
+    tags=['legacy']
     partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
