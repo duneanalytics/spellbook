5170972aa656b086ea04379fb91e9d71ba8a0ebc
Ben Smith <bh2smith@gmail.com>
2023-07-17 13:34:59 +0200

    migrate cow batches table

--- tests/cow_protocol/ethereum/cow_protocol_ethereum_assert_trades.sql
+++ tests/cow_protocol/ethereum/cow_protocol_ethereum_assert_trades.sql
@@ -2,4 +2,4 @@
 select evt_tx_hash
 from {{ source('gnosis_protocol_v2_ethereum', 'GPv2Settlement_evt_Trade') }}
 where evt_tx_hash not in (select tx_hash from {{ ref('cow_protocol_ethereum_batches')}})
-and evt_block_time < date(now()) - interval '1 day'
+and evt_block_time < date(now()) - interval '1' day
