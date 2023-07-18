a1dc6aae23b4c47ded5aeebd259fedc4762ec5bc
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-07-18 16:49:00 +0100

    add new pairs for legacy

--- models/tigris/arbitrum/events/tigris_v2_arbitrum_events_asset_added_legacy.sql
+++ models/tigris/arbitrum/events/tigris_v2_arbitrum_events_asset_added_legacy.sql
@@ -12,4 +12,14 @@ SELECT
     _name as pair 
 FROM 
 {{ source('tigristrade_v2_arbitrum', 'PairsContract_evt_AssetAdded') }}
+
+UNION
+
+ SELECT 
+     evt_tx_hash, 
+     _asset as asset_id, 
+     _name as pair 
+ FROM 
+ {{ source('tigristrade_v2_arbitrum', 'PairsContract_v2_evt_AssetAdded') }}
+
 ;
