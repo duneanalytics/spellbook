1829995ecfefd91b03cf63d9f0e2633dccd251b8
Huang Geyang <Sukebeta@outlook.com>
2023-07-18 10:24:35 +0800

    Migrate models/addresses_events_testnets/*

--- models/addresses_events_testnets/goerli/addresses_events_testnets_goerli_first_funded_by.sql
+++ models/addresses_events_testnets/goerli/addresses_events_testnets_goerli_first_funded_by.sql
@@ -1,6 +1,7 @@
 {{ config(
     schema = 'addresses_events_testnets_goerli'
     , alias = alias('first_funded_by')
+    , tags = ['dunesql']
     , materialized = 'incremental'
     , file_format = 'delta'
     , incremental_strategy = 'merge'
@@ -16,13 +17,16 @@ SELECT 'goerli' AS blockchain
 , MIN_BY(et.tx_hash, et.block_number) AS tx_hash
 FROM {{ source('goerli', 'traces') }} et
 {% if is_incremental() %}
-LEFT ANTI JOIN {{this}} ffb ON et.to = ffb.address
+LEFT JOIN {{this}} ffb ON et.to = ffb.address
+WHERE ffb.address IS NULL
+{% else %}
+WHERE 1 = 1
 {% endif %}
-WHERE et.success
+AND et.success
 AND (et.call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR et.call_type IS NULL)
 AND CAST(et.value AS double) > 0
 {% if is_incremental() %}
-AND et.block_time >= date_trunc('day', now() - interval '1 week')
+AND et.block_time >= date_trunc('day', now() - interval '7' day)
 {% endif %}
 GROUP BY et.to
 ;
