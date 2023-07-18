26e59170894d104595ab4a8869cc27f21b8d9e0d
0xRob <0xRobin5@gmail.com>
2023-07-18 16:24:48 +0200

    run test compile in CI

--- models/some_new_dunesql_model.sql
+++ models/some_new_dunesql_model.sql
@@ -1,7 +1,8 @@
 {{  config(
         schema = 'test_new_dunesql',
         tags = ['dunesql'],
-        alias = alias('new_model')
+        alias = alias('new_model'),
+        materialized = 'table'
     )
 }}
 select 'test' as test
