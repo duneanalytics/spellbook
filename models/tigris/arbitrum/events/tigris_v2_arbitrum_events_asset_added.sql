337195adcd0cc631fb0925d0acceed60f2faa513
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-07-18 18:46:04 +0100

    remove tx hash as it's not needed and dupes

--- models/tigris/arbitrum/events/tigris_v2_arbitrum_events_asset_added.sql
+++ models/tigris/arbitrum/events/tigris_v2_arbitrum_events_asset_added.sql
@@ -6,7 +6,6 @@
  }}
 
 SELECT 
-    evt_tx_hash, 
     _asset as asset_id, 
     _name as pair 
 FROM 
@@ -15,7 +14,6 @@ FROM
 UNION
 
 SELECT 
-    evt_tx_hash, 
     _asset as asset_id, 
     _name as pair 
 FROM 
