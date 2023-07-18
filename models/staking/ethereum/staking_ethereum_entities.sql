6453201bc28ff8a6aa11f636d5cef4843c5850d4
Huang Geyang <Sukebeta@outlook.com>
2023-07-19 00:44:41 +0800

    Fix config blocks

--- models/staking/ethereum/staking_ethereum_entities.sql
+++ models/staking/ethereum/staking_ethereum_entities.sql
@@ -1,6 +1,6 @@
 {{ config(
     alias = alias('entities'),
-    tags = ['dunesql']
+    tags = ['dunesql'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
