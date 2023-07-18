d3ac4308f6cab353346fc3017fda92f169567e8f
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-07-18 09:09:19 -0400

    Update balancer_bpt_prices_legacy.sql

--- models/balancer/balancer_bpt_prices_legacy.sql
+++ models/balancer/balancer_bpt_prices_legacy.sql
@@ -1,6 +1,5 @@
 {{ config(
 	tags=['legacy'],
-	
     schema = 'balancer',
     alias = alias('bpt_prices', legacy_model=True),
     post_hook='{{ expose_spells(\'["ethereum", "arbitrum", "polygon", "gnosis", "optimism"]\',
