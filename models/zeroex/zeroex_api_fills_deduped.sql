c61c9410eff0cba441845ccd19d9f374963d12b2
RantumBits <rantum@rantum.xyz>
2023-07-18 16:26:16 -0400

    use legacy files

--- models/zeroex/zeroex_api_fills_deduped.sql
+++ models/zeroex/zeroex_api_fills_deduped.sql
@@ -1,5 +1,4 @@
 {{ config(
-        tags=['dunesql'],
         alias = alias('api_fills_deduped'),
         post_hook='{{ expose_spells(\'["ethereum","arbitrum", "optimism", "polygon","fantom","avalanche_c"]\',
                                 "project",
@@ -8,6 +7,10 @@
         )
 }}
 
+/********************************************************
+spells with issues, to be excluded in short term:
+,ref('zeroex_polygon_api_fills_deduped') contains duplicates
+********************************************************/
 
 {% set zeroex_models = [  
 ref('zeroex_arbitrum_api_fills_deduped')
@@ -16,7 +19,6 @@ ref('zeroex_arbitrum_api_fills_deduped')
 ,ref('zeroex_fantom_api_fills_deduped')
 ,ref('zeroex_optimism_api_fills_deduped')
 ,ref('zeroex_bnb_api_fills_deduped')
-,ref('zeroex_polygon_api_fills_deduped')
 ] %}
 
 
