734a383091614283376509c90d5fcb7be62b8883
Huang Geyang <Sukebeta@outlook.com>
2023-07-18 10:30:37 +0800

    Fix from

--- models/addresses_events_testnets/goerli/addresses_events_testnets_goerli_first_funded_by.sql
+++ models/addresses_events_testnets/goerli/addresses_events_testnets_goerli_first_funded_by.sql
@@ -11,7 +11,7 @@
 
 SELECT 'goerli' AS blockchain
 , et.to AS address
-, MIN_BY(et.from, et.block_number) AS first_funded_by
+, MIN_BY(et."from", et.block_number) AS first_funded_by
 , MIN(et.block_time) AS block_time
 , MIN(et.block_number) AS block_number
 , MIN_BY(et.tx_hash, et.block_number) AS tx_hash
@@ -29,4 +29,3 @@ AND CAST(et.value AS double) > 0
 AND et.block_time >= date_trunc('day', now() - interval '7' day)
 {% endif %}
 GROUP BY et.to
-;
