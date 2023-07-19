30cb126ba53727ba5e591b680de5d96dfd6740ce
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-19 16:09:47 -0400

    Migrate static addresses (#3803)
    * migrate static addresses
    
    * update
    
    * map
    
    * add

--- models/op/token_distributions/optimism/op_token_distributions_optimism_project_name_mapping.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_project_name_mapping.sql
@@ -1,26 +1,44 @@
 -- Map governance proposal names to Dune project names
 {{ config(
+    tags = ['dunesql','static'],
     alias = alias('project_name_mapping')
     )
 }}
 
-SELECT
-        proposal_name, project_name
+WITH unified_mappings AS (
+        SELECT proposal_name, project_name,
+                ROW_NUMBER() OVER (PARTITION BY lower(proposal_name) ORDER BY project_name ASC) AS rn
+        FROM (
+                SELECT proposal_name, project_name
 
-FROM (values
-        --set up a query to check that project_names here have a match in Dune project names & labels (some may not exist, like Karma)
-         ('xToken Terminal / Gamma Strategies', 'xToken')
-        ,('Rainbow Wallet', 'Rainbow')
-        ,('Karma 1', 'Karma')
-        ,('Karma 2', 'Karma')
-        ,('Safe', 'Gnosis Safe')
-        ,('Okex', 'OKX')
-        ,('Overtime Markets', 'Thales')
-        ,('Quests on Coinbase Wallet - Quest #1 DEX Swap','Quests on Coinbase Wallet')
-        ,('Quests on Coinbase Wallet - Quest #2 Delegation','Quests on Coinbase Wallet')
-        ,('Uniswap V3','Uniswap')
-        ,('SushiSwap', 'Sushi')
-        ,('Karma delegate registry', 'Karma')
-        ,('Rabbit Hole','Rabbithole')
-        
-) a (proposal_name, project_name)
+                FROM (values
+                        --set up a query to check that project_names here have a match in Dune project names & labels (some may not exist, like Karma)
+                        ('xToken Terminal / Gamma Strategies', 'xToken')
+                        ,('Rainbow Wallet', 'Rainbow')
+                        ,('Karma 1', 'Karma')
+                        ,('Karma 2', 'Karma')
+                        ,('Safe', 'Gnosis Safe')
+                        ,('Okex', 'OKX')
+                        ,('Overtime Markets', 'Thales')
+                        ,('Quests on Coinbase Wallet - Quest #1 DEX Swap','Quests on Coinbase Wallet')
+                        ,('Quests on Coinbase Wallet - Quest #2 Delegation','Quests on Coinbase Wallet')
+                        ,('Uniswap V3','Uniswap')
+                        ,('SushiSwap', 'Sushi')
+                        ,('Karma delegate registry', 'Karma')
+                        ,('Rabbit Hole','Rabbithole')
+                        ,('Lyra','Lyra Finance')
+                        
+                ) a (proposal_name, project_name)
+
+                UNION ALL
+
+                SELECT
+                dune_name AS proposal_name, mapped_name AS project_name
+                FROM {{ ref('contracts_optimism_project_name_mappings') }}
+
+        ) u
+)
+
+SELECT proposal_name, project_name
+FROM unified_mappings
+WHERE rn = 1 --ensure no dupes
