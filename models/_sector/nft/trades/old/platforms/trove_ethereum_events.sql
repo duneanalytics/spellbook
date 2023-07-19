2b31fd23755df8f6d032d9762b9fcb5441dded54
0xRob <0xRobin5@gmail.com>
2023-07-19 14:42:56 +0200

    partition on date

--- models/_sector/nft/trades/old/platforms/trove_ethereum_events.sql
+++ models/_sector/nft/trades/old/platforms/trove_ethereum_events.sql
@@ -58,7 +58,7 @@ select
     'ethereum' as blockchain,
     'trove' as project,
     'v2' as version,
-    date_trunc('day',mp.block_time) as block_date,
+    cast(date_trunc('day',mp.block_time) as date) as block_date,
     mp.block_time,
     token_id,
     nft_tokens.name as collection,
