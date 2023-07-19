d4bd9e7c72c3fb512dfe4652eacc8f1c59627ff3
ppclunghe <pipistrella@list.ru>
2023-07-19 11:42:28 +0200

    fix

--- models/lido/liquidity/arbitrum/lido_liquidity_arbitrum_kyberswap_pools.sql
+++ models/lido/liquidity/arbitrum/lido_liquidity_arbitrum_kyberswap_pools.sql
@@ -88,7 +88,7 @@ left join pools on 1=1
       AND contract_address IN (SELECT address  FROM tokens)
   )
 
-/*  
+  
  , tokens_prices_hourly AS (
         select 
         time, 
@@ -124,7 +124,6 @@ left join pools on 1=1
       
   )
   
-*/
 
 , swap_events as (
     select 
@@ -219,7 +218,7 @@ left join pools on 1=1
 
 )
 
-/*
+
 , swap_events_hourly as (
     select hour, pool, token0, token1, sum(amount0) as amount0, sum(amount1) as amount1 from (
     select 
@@ -250,7 +249,7 @@ select  distinct date_trunc('day', time) as time, pool, sum(volume) as volume
 from trading_volume_hourly
 group by 1,2
 )
-*/
+
 , all_metrics as (
 select l.pool, pools.blockchain, pools.project, pools.fee, cast(l.time as date) as "time", 
     case when l.token0 = 0x5979D7b546E38E414F7E9822514be443A4800529 then l.token0 else l.token1 end main_token,
@@ -261,15 +260,14 @@ select l.pool, pools.blockchain, pools.project, pools.fee, cast(l.time as date) 
     case when l.token0 = 0x5979D7b546E38E414F7E9822514be443A4800529 then amount1/power(10, p1.decimals)  else amount0/power(10, p0.decimals)  end paired_token_reserve,
     case when l.token0 = 0x5979D7b546E38E414F7E9822514be443A4800529 then p0.price*amount0/power(10, p0.decimals) else p1.price*amount1/power(10, p1.decimals) end as main_token_usd_reserve,
     case when l.token0 = 0x5979D7b546E38E414F7E9822514be443A4800529 then p1.price*amount1/power(10, p1.decimals) else p0.price*amount0/power(10, p0.decimals) end as paired_token_usd_reserve,
-  --  volume 
-    double '0' as trading_volume
+    volume as trading_volume
 from pool_liquidity l 
 left join pools on l.pool = pools.address
 left join tokens t0 on l.token0 = t0.address
 left join tokens t1 on l.token1 = t1.address
 left join tokens_prices_daily p0 on l.time = p0.time and l.token0 = p0.token
 left join tokens_prices_daily p1 on l.time = p1.time and l.token1 = p1.token
---left join trading_volume tv on l.time = tv.time and l.pool = tv.pool
+left join trading_volume tv on l.time = tv.time and l.pool = tv.pool
 ) 
 
 select CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(blockchain,CONCAT(' ', project)) ,' '), paired_token_symbol),':') , main_token_symbol, ' ', format('%,.3f',round(coalesce(fee,0),4))) as pool_name,* 
