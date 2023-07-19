ccd1804b9a3eb3a14c3db4cc0eaaca892d0d511b
0xbitfly <0xbitfly@gmail.com>
2023-07-19 10:44:26 +0800

    mini upgrade with only one chain

--- models/account_abstraction/erc4337/account_abstraction_erc4337_userops.sql
+++ models/account_abstraction/erc4337/account_abstraction_erc4337_userops.sql
@@ -9,12 +9,7 @@
 }}
 
 {% set erc4337_models = [
-  -- ref('account_abstraction_erc4337_ethereum_userops')
  ref('account_abstraction_erc4337_polygon_userops')
--- , ref('account_abstraction_erc4337_arbitrum_userops')
--- , ref('account_abstraction_erc4337_optimism_userops')
--- , ref('account_abstraction_erc4337_avalanche_c_userops')
--- , ref('account_abstraction_erc4337_gnosis_userops')
 ] %}
 
 SELECT *
