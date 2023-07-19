0bc98849e077e403583be80413070490f0c36242
Huang Geyang <Sukebeta@outlook.com>
2023-07-19 13:13:40 +0800

    Remove comments

--- models/labels/addresses/infrastructure/identifier/eth_stakers/labels_eth_stakers.sql
+++ models/labels/addresses/infrastructure/identifier/eth_stakers/labels_eth_stakers.sql
@@ -16,7 +16,7 @@ WITH identified_stakers AS (
     , NOW() AS updated_at
     , 'eth_stakers' AS model_name
     , 'identifier' as label_type
-    FROM {{ ref('staking_ethereum_entities') }} -- {{ ref('staking_ethereum_entities')}}
+    FROM {{ ref('staking_ethereum_entities') }}
     )
 
 , unidentified_stakers AS (
