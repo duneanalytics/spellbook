efc40dbc052382b226d77bb131c6ec6c8419f592
0xRob <0xRobin5@gmail.com>
2023-07-19 14:04:58 +0200

    2 partitioning examples

---
+++ models/some_model_partitioned_by_date.sql
@@ -0,0 +1,11 @@
+{{ config(
+        alias = alias('partitioned_by_time'),
+        tags = ['dunesql'],
+        partition_by = ['partition_column']
+        )
+}}
+
+select
+    cast(date_trunc('day', block_time) as date) as partition_column
+from {source('ethereum','transactions')}
+limit 20
---
+++ models/some_model_partitioned_by_timestamp.sql
@@ -0,0 +1,11 @@
+{{ config(
+        alias = alias('partitioned_by_time'),
+        tags = ['dunesql'],
+        partition_by = ['partition_column']
+        )
+}}
+
+select
+    date_trunc('day', block_time) as partition_column
+from {source('ethereum','transactions')}
+limit 20
