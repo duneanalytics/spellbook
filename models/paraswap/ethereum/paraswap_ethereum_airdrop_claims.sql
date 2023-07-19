ed4e42b189670249399c3cf28ab9f15fe870e759
BennyFeng <bennyfeng@yeah.net>
2023-07-19 19:59:57 +0800

    migrate to dunesql

--- models/paraswap/ethereum/paraswap_ethereum_airdrop_claims.sql
+++ models/paraswap/ethereum/paraswap_ethereum_airdrop_claims.sql
@@ -2,7 +2,6 @@
     config(
         schema = 'paraswap_ethereum',
         alias = alias('airdrop_claims'),
-        tags = ['dunesql'],
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
@@ -23,7 +22,7 @@ WITH more_prices AS (
     , MAX_BY(median_price, hour) AS max_price
     FROM {{ ref('dex_prices') }}
     WHERE blockchain = 'ethereum'
-    AND contract_address={{psp_token_address}}
+    AND contract_address='{{psp_token_address}}'
     )
 
 SELECT 'ethereum' AS blockchain
@@ -31,7 +30,7 @@ SELECT 'ethereum' AS blockchain
 , t.evt_block_number AS block_number
 , 'Paraswap' AS project
 , 'Paraswap Airdrop' AS airdrop_identifier
-, "from" AS recipient
+, from AS recipient
 , t.contract_address
 , t.evt_tx_hash AS tx_hash
 , CAST(t.value AS DECIMAL(38,0)) AS amount_raw
@@ -40,19 +39,19 @@ SELECT 'ethereum' AS blockchain
     WHEN t.evt_block_time < (SELECT min_hour FROM more_prices) THEN CAST((SELECT min_price FROM more_prices)*t.value/POWER(10, 18) AS double)
     WHEN t.evt_block_time > (SELECT max_hour FROM more_prices) THEN CAST((SELECT max_price FROM more_prices)*t.value/POWER(10, 18) AS double)
     END AS amount_usd
-, {{psp_token_address}} AS token_address
+, '{{psp_token_address}}' AS token_address
 , 'PSP' AS token_symbol
 , t.evt_index
 FROM {{ source('erc20_ethereum', 'evt_transfer') }} t
 LEFT JOIN {{ ref('dex_prices') }} pu ON pu.blockchain = 'ethereum'
-    AND pu.contract_address={{psp_token_address}}
+    AND pu.contract_address='{{psp_token_address}}'
     AND pu.hour = date_trunc('hour', t.evt_block_time)
     {% if is_incremental() %}
-    AND pu.hour >= date_trunc('day', now() - interval '7' day)
+    AND pu.hour >= date_trunc("day", now() - interval '1 week')
     {% endif %}
-WHERE t.contract_address = {{psp_token_address}}
-AND t."from" = 0x090e53c44e8a9b6b1bca800e881455b921aec420
-AND t.evt_block_time > date('2021-11-15')
+WHERE t.contract_address = '{{psp_token_address}}'
+AND t.from = '0x090e53c44e8a9b6b1bca800e881455b921aec420'
+AND t.evt_block_time > '2021-11-15'
 {% if is_incremental() %}
-AND t.evt_block_time >= date_trunc('day', now() - interval '7' day)
+AND t.evt_block_time >= date_trunc("day", now() - interval '1 week')
 {% endif %}
