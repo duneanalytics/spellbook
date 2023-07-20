linkpoolio/dune-spellbook

Add Chainlink OCR to arbitrum, avalanche_c, bnb, fantom, gnosis, optimism, and polygon

models/chainlink/arbitrum/chainlink_arbitrum_ocr_operator_admin_meta.sql
@@ -1,0 +1,19 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["arbitrum"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}'),
+  (0xe9e11963f61322299f9919ff1dda01a825e82dbc, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/avalanche_c/chainlink_avalanche_c_ocr_operator_admin_meta.sql
@@ -1,0 +1,19 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["avalanche_c"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}'),
+  (0xe9e11963f61322299f9919ff1dda01a825e82dbc, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/fantom/chainlink_fantom_ocr_operator_admin_meta.sql
@@ -1,0 +1,19 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["fantom"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}'),
+  (0xe9e11963f61322299f9919ff1dda01a825e82dbc, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/gnosis/chainlink_gnosis_ocr_operator_admin_meta.sql
@@ -1,0 +1,19 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["gnosis"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}'),
+  (0xe9e11963f61322299f9919ff1dda01a825e82dbc, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/gnosis/chainlink_gnosis_ocr_operator_node_meta.sql
@@ -1,0 +1,21 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_node_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["gnosis"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT node_address, operator_name
+FROM (VALUES 
+  (0xcC29be4Ca92D4Ecc43C8451fBA94C200B83991f6, '{{linkpool}}'),
+  (0x1589d072aC911a55c2010D97839a1f61b1e3323A, '{{linkpool}}')
+) AS tmp_node_meta(node_address, operator_name)
+
+

models/chainlink/optimism/chainlink_optimism_ocr_operator_admin_meta.sql
@@ -1,0 +1,19 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["optimism"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}'),
+  (0xe9e11963f61322299f9919ff1dda01a825e82dbc, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/polygon/chainlink_polygon_ocr_operator_admin_meta.sql
@@ -1,0 +1,19 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["polygon"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}'),
+  (0xe9e11963f61322299f9919ff1dda01a825e82dbc, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/polygon/chainlink_polygon_ocr_operator_node_meta.sql
@@ -1,0 +1,21 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_node_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["polygon"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT node_address, operator_name
+FROM (VALUES 
+  (0xcC29be4Ca92D4Ecc43C8451fBA94C200B83991f6, '{{linkpool}}'),
+  (0x1589d072aC911a55c2010D97839a1f61b1e3323A, '{{linkpool}}')
+) AS tmp_node_meta(node_address, operator_name)
+
+

duneanalytics/spellbook

models/chainlink/arbitrum/chainlink_arbitrum_ocr_operator_admin_meta.sql
@@ -1,0 +1,18 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["arbitrum"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/avalanche_c/chainlink_avalanche_c_ocr_operator_admin_meta.sql
@@ -1,0 +1,18 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["avalanche_c"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/fantom/chainlink_fantom_ocr_operator_admin_meta.sql
@@ -1,0 +1,18 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["fantom"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/gnosis/chainlink_gnosis_ocr_operator_admin_meta.sql
@@ -1,0 +1,18 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["gnosis"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/gnosis/chainlink_gnosis_ocr_operator_node_meta.sql
@@ -1,0 +1,20 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_node_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["gnosis"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT node_address, operator_name
+FROM (VALUES 
+  (0x11eB6a69A56DF3a89b99c4b1484691Af4AB0c508, '{{linkpool}}')
+) AS tmp_node_meta(node_address, operator_name)
+
+

models/chainlink/optimism/chainlink_optimism_ocr_operator_admin_meta.sql
@@ -1,0 +1,18 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["optimism"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/polygon/chainlink_polygon_ocr_operator_admin_meta.sql
@@ -1,0 +1,18 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_admin_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["polygon"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT admin_address, operator_name
+FROM (VALUES 
+  (0x797de2909991C66C66D8e730C8385bbab8D18eA6, '{{linkpool}}')
+) AS tmp_node_meta(admin_address, operator_name)

models/chainlink/polygon/chainlink_polygon_ocr_operator_node_meta.sql
@@ -1,0 +1,22 @@
+{{
+  config(
+    tags=['dunesql'],
+    alias='ocr_operator_node_meta',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["polygon"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+  )
+}}
+
+{% set linkpool = 'LinkPool' %}
+
+SELECT node_address, operator_name
+FROM (VALUES 
+  (0xa1ab1c841898Fe94900d00d9312ba954e4F81501, '{{linkpool}}'),
+  (0x9F9922d4bBa463EfBBcF8563282723d98587f7eb, '{{linkpool}}'),
+  (0xf03b7095B089A4e601fB13F2BF6af518eb199a0b, '{{linkpool}}')
+) AS tmp_node_meta(node_address, operator_name)
+
+