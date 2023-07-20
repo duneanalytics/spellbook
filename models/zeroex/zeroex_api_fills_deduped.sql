886a90b1c6012e12226eba7c101b39351f201647
RantumBits <rantum@rantum.xyz>
2023-07-20 14:43:27 -0400

    remove tags

--- models/zeroex/zeroex_api_fills_deduped.sql
+++ models/zeroex/zeroex_api_fills_deduped.sql
@@ -1,5 +1,4 @@
 {{ config(
-        tags=['dunesql'],
         alias = alias('api_fills_deduped'),
         post_hook='{{ expose_spells(\'["ethereum","arbitrum", "optimism", "polygon","fantom","avalanche_c"]\',
                                 "project",
@@ -20,7 +19,6 @@ ref('zeroex_arbitrum_api_fills_deduped')
 ,ref('zeroex_fantom_api_fills_deduped')
 ,ref('zeroex_optimism_api_fills_deduped')
 ,ref('zeroex_bnb_api_fills_deduped')
-,ref('zeroex_polygon_api_fills_deduped')
 ] %}
 
 
