b0dcf29a5a72b75bf99ddf40052a7012c72c1e91
danielpartida <partidapd@gmail.com>
2023-07-21 11:27:50 +0200

    Implement singletons sql file

---
+++ models/safe/base/safe_base_singletons.sql
@@ -0,0 +1,15 @@
+{{
+    config(
+        materialized='table',
+        alias='singletons',
+        post_hook='{{ expose_spells(\'["base"]\',
+                                    "project",
+                                    "safe",
+                                    \'["danielpartida"]\') }}'
+    )
+}}
+
+
+-- Fetch all known singleton/mastercopy addresses used via factories.
+select distinct singleton as address
+from {{ source('gnosis_safe_base', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
