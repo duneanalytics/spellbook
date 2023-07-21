60effc4ed4216a7510fb737f69ebedaf4910b884
RantumBits <rantum@rantum.xyz>
2023-07-21 12:48:50 -0400

    dunesql for tokens.erc20

--- models/tokens/tokens_erc20.sql
+++ models/tokens/tokens_erc20.sql
@@ -1,5 +1,5 @@
 {{ config( alias = alias('erc20'),
-        tags=['static'],
+        tags=['static', 'dunesql'],
         post_hook='{{ expose_spells(\'["arbitrum","avalanche_c","bnb","ethereum","optimism", "gnosis", "fantom", "polygon"]\',
                                     "sector",
                                     "tokens",
