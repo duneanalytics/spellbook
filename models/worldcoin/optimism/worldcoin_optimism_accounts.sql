fa79e36f347e41a31554262d3106b1e9268dda19
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-22 13:55:54 -0400

    update logic

--- models/worldcoin/optimism/worldcoin_optimism_accounts.sql
+++ models/worldcoin/optimism/worldcoin_optimism_accounts.sql
@@ -10,49 +10,43 @@
                                     "worldcoin",
                                     \'["msilb7"]\') }}')}}
 
--- Source of TX From & Logic 
--- https://cointelegraph.com/news/worldcoin-confirms-it-is-the-cause-of-mysterious-safe-deployments
-WITH wld_deployers AS (
-    SELECT
-    to as worldcoin_deployer_address
-    FROM {{ source('optimism','transactions') }}
-    WHERE "from" IN (
-                 0x86c5608362b3fbbeb721140472229392f754ef87
-                ,0x80dc00811e7c4a03c1f1599d3dc8febaad87bf87
-                )
-    AND value > cast(0 as uint256)
-    AND block_number >= 105870092 --first transfer
-    -- don't do incremental here, we want to always build this subaccount list (maybe an eventual spell)
-    GROUP BY 1
-)
+-- Addresses Sourced from https://dune.com/queries/2456211
 
-SELECT 
-worldcoin_deployer_address
-, ct.address AS account_address
-, ct.block_time AS created_time
-, ct.tx_hash AS creation_tx_hash
-, ct.block_number AS created_block_number
+SELECT
+    created_tx_from AS worldcoin_deployer_address,
+    contract_address AS account_address,
+    created_time,
+    creation_tx_hash,
+    created_block_number,
+    contract_project,
+    contract_name,
+    trace_creator_address
 
-FROM wld_deployers w
-INNER JOIN {{ source('optimism','transactions') }} t
-    ON t."from" = worldcoin_deployer_address
-    AND t.block_number >= 105870092 --first transfer
+
+FROM {{ ref('contracts_optimism_contract_mapping') }}
+where 1=1 -- limit by time
+    and created_time > from_iso8601_timestamp('2023-06-01')
     {% if is_incremental() %}
-    AND t.block_time >= date_trunc('day', now() - interval '7' day)
+    AND created_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
-INNER JOIN {{ source('optimism','creation_traces') }}  ct
-    ON ct.tx_hash = t.hash
-    AND ct.block_number = t.block_number
-    AND ct.block_number >= 105870092 --first transfer
-    AND ct."from" IN (  --safe factories
-                        0xa6b71e26c5e0845f74c812102ca7114b6a896ab2
-                        ,0xc22834581ebc8527d974f8a1c97e1bea4ef910bc
-                        )
-    {% if is_incremental() %}
-    AND ct.block_time >= date_trunc('day', now() - interval '7' day)
-    {% endif %}
-
-WHERE 1=1
-{% if is_incremental() %}
-AND t.block_time >= date_trunc('day', now() - interval '7' day)
-{% endif %}
+    and created_tx_from IN (
+        -- relayers (user tx)
+          0x65bf36d6499a504100eb504f0719271f5c4174ec
+        , 0xabe494eaa4ed80de8583c49183e9cbdadbc3b954
+        , 0xb54a5205ee454f48ddfc23ca26a3836ba3dacc07
+        , 0x4399fa85585f90da110d5ba150ff96c763bc0aba
+        , 0xd8f7d2d62514895475afe0c7d75f31390dd40de4
+        -- deployer wallets (safe deployments)
+        , 0x91a3060513f25b44bacb46762160f6794bbd42d2
+        , 0xe74E592E1dD43ccfb55bcd7999DEe0dfeE7acae3
+        , 0x279A5453597E07505d574233fd16fbc670838fe7
+        , 0x71730945b56A1472874EDf3E40795D74EF350416
+        , 0x85E38523A65a5c9F628c5477033C742da1c218cc
+        , 0x36C334Bd446ad62e602672828D21029ab48DD27F
+        , 0x1d234d6D83B0535Ce28D11BBBBbC9426868D4Cb9
+        , 0x9F4665f0ce1a377abDbD638F81F0C5883fbedCDb
+        , 0x77Db88C013D5149d994B17F6586Cd71c3FFD2503
+        , 0xc5a53b78734284D683d623C5C06D2e646abCec3f
+        -- later adds
+        , 0x67Fa7f957F13e64c986ce1c7ef75De5c716E2367
+        )
