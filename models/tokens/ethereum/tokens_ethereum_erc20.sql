b7188750184dad3a1fcfb2ea7572640a6c7a46c0
0xSerPablo <131652619+0xSerPablo@users.noreply.github.com>
2023-07-22 12:53:36 +0200

    Update tokens_ethereum_erc20.sql
    Add below two tokens to token ERC20 on ETH:
    
    CHIKUN: https://etherscan.io/token/0x9a7703338730b82a803ba050df55f9b3959f3fb2#readContract
    
    COCKGG:https://etherscan.io/token/0xed6bf9e8431a7f8b03a54864702b50140cb54fb0#readContract

--- models/tokens/ethereum/tokens_ethereum_erc20.sql
+++ models/tokens/ethereum/tokens_ethereum_erc20.sql
@@ -28784,4 +28784,6 @@ FROM (VALUES
         ,(0xd822aa7a904deb860a44e0bb455ca11a9cfe1ed6, 'POEPOE', 9)
         ,(0x703293d32aea894cca91d6c282db9efffe47c308, 'AER', 18)
         ,(0x63a2ef2c7d20f8226d1c8c4b71e6832bdca47d48, 'DOGINU', 9)
+        ,(0xed6bf9e8431a7f8b03a54864702b50140cb54fb0, 'COCKGG', 9)
+        ,(0x9a7703338730b82a803ba050df55f9b3959f3fb2, 'CHIKUN', 9)        
      ) AS temp_table (contract_address, symbol, decimals)
