c29a11213673036d3fcb65dd4ba2e59657e273a9
mc <mattcole1093@gmail.com>
2023-07-24 13:16:07 -0400

    Add gho to tokens list (#3821)
    * add AAVE, EURS to arbitrum_erc20.sql
    
    * adding remaining aave v3 assets to erc20
    
    * adding GHO

--- models/tokens/ethereum/tokens_ethereum_erc20.sql
+++ models/tokens/ethereum/tokens_ethereum_erc20.sql
@@ -28784,4 +28784,5 @@ FROM (VALUES
         ,(0xd822aa7a904deb860a44e0bb455ca11a9cfe1ed6, 'POEPOE', 9)
         ,(0x703293d32aea894cca91d6c282db9efffe47c308, 'AER', 18)
         ,(0x63a2ef2c7d20f8226d1c8c4b71e6832bdca47d48, 'DOGINU', 9)
+        ,(0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f, 'GHO', 18)
      ) AS temp_table (contract_address, symbol, decimals)
