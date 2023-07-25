38170e9acacf14a293bf217ad6a9bd1e23dbe2e5
jeff-dude <jeff@dune.xyz>
2023-07-25 09:19:41 -0400

    alias, partition & filter placement

--- models/dex/dex_prices_latest_legacy.sql
+++ models/dex/dex_prices_latest_legacy.sql
@@ -1,7 +1,7 @@
 {{ config(
-        alias = alias('prices_latest'),
+        alias = alias('prices_latest', legacy_model=True),
         tags = ['legacy'],
-        partition_by = ['block_time'],
+        partition_by = ['block_month'],
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
@@ -14,12 +14,14 @@
 }}
 
 SELECT
+    block_month,
     block_time,
     token_address,
     token_price_usd,
     token_price_usd_raw
 FROM (
     SELECT
+        block_month,
         block_time,
         token_address,
         amount_usd / token_amount AS token_price_usd,
@@ -30,27 +32,32 @@ FROM (
         ROW_NUMBER() OVER (PARTITION BY token_address ORDER BY block_time DESC) AS rn
     FROM (
         SELECT
+            CAST(date_trunc('month', block_time) as DATE) as block_month,
             block_time,
             token_bought_address AS token_address,
             token_bought_amount AS token_amount,
             token_bought_amount_raw AS token_amount_raw,
             amount_usd
         FROM {{ ref('dex_trades_legacy') }}
-        {% if is_incremental() %}
-        WHERE block_time >= date_trunc("day", now() - interval '1 week')
-        {% endif %}
+        WHERE
+            amount_usd > 0
+            {% if is_incremental() %}
+            AND block_time >= date_trunc("day", now() - interval '1 week')
+            {% endif %}
         UNION ALL
         SELECT
+            CAST(date_trunc('month', block_time) as DATE) as block_month,
             block_time,
             token_sold_address AS token_address,
             token_sold_amount AS token_amount,
             token_sold_amount_raw AS token_amount_raw,
             amount_usd
         FROM {{ ref('dex_trades_legacy') }}
-        {% if is_incremental() %}
-        WHERE block_time >= date_trunc("day", now() - interval '1 week')
-        {% endif %}
+        WHERE
+            amount_usd > 0
+            {% if is_incremental() %}
+            AND block_time >= date_trunc("day", now() - interval '1 week')
+            {% endif %}
     ) subquery
 ) most_recent
-WHERE rn = 1 
-AND amount_usd > 0
+WHERE rn = 1
