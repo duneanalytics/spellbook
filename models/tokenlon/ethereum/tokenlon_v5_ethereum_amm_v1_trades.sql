220fb52430271e53c5f20f3fb1f835ba13170d59
izayl <izayl@163.com>
2023-07-26 16:22:37 +0800

    fix: mismatched input '<EOF>'.

--- models/tokenlon/ethereum/tokenlon_v5_ethereum_amm_v1_trades.sql
+++ models/tokenlon/ethereum/tokenlon_v5_ethereum_amm_v1_trades.sql
@@ -112,4 +112,3 @@ LEFT JOIN {{ source('prices', 'usd') }} p_sold
     {% if is_incremental() %}
     AND p_sold.minute >= date_trunc('day', now() - interval '7' day)
     {% endif %}
-;
--- models/tokenlon/ethereum/tokenlon_v5_ethereum_rfq_v1_trades_legacy.sql
+++ models/tokenlon/ethereum/tokenlon_v5_ethereum_rfq_v1_trades_legacy.sql
@@ -106,4 +106,3 @@ LEFT JOIN {{ source('prices', 'usd') }} p_sold
     {% if is_incremental() %}
     AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
     {% endif %}
-;
