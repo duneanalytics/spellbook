9fb3d63a4c4180215db6018ac516810c07fdf3ee
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-07-27 19:23:25 +0100

    new tests draft

---
+++ tests/tigris/arbitrum/tigris_arbitrum_asset_events.sql
@@ -0,0 +1,92 @@
+WITH 
+
+open_position_events as (
+    SELECT 
+        'open_position' as evt_type, 
+        evt_tx_hash,
+        _id as position_id, 
+        evt_block_time,
+        CONCAT(CAST(id as VARCHAR), 'arbitrum', 'v1.2') as position_data 
+    FROM 
+    {{ source('tigristrade_arbitrum', 'TradingV2_evt_PositionOpened') }} 
+
+    UNION ALL 
+
+    SELECT 
+        'open_position' as evt_type, 
+        evt_tx_hash,
+        _id as position_id, 
+        evt_block_time,
+        CONCAT(CAST(id as VARCHAR), 'arbitrum', 'v1.3') as position_data 
+    FROM 
+    {{ source('tigristrade_arbitrum', 'TradingV3_evt_PositionOpened') }} 
+
+    UNION ALL 
+
+    SELECT 
+        'open_position' as evt_type, 
+        evt_tx_hash,
+        _id as position_id, 
+        evt_block_time,
+        CONCAT(CAST(id as VARCHAR), 'arbitrum', 'v1.4') as position_data 
+    FROM 
+    {{ source('tigristrade_arbitrum', 'TradingV4_evt_PositionOpened') }} 
+
+    UNION ALL 
+
+    SELECT 
+        'open_position' as evt_type, 
+        evt_tx_hash,
+        _id as position_id, 
+        evt_block_time,
+        CONCAT(CAST(id as VARCHAR), 'arbitrum', 'v1.5') as position_data 
+    FROM 
+    {{ source('tigristrade_arbitrum', 'TradingV5_evt_PositionOpened') }} 
+
+    UNION ALL 
+
+    SELECT 
+        'open_position' as evt_type, 
+        evt_tx_hash,
+        id as position_id, 
+        evt_block_time,
+        CONCAT(CAST(id as VARCHAR), 'arbitrum', 'v2.1') as position_data 
+    FROM 
+    {{ source('tigristrade_v2_arbitrum', 'Trading_evt_PositionOpened') }}
+
+    UNION ALL 
+
+    SELECT 
+        'open_position' as evt_type, 
+        evt_tx_hash,
+        id as position_id, 
+        evt_block_time,
+        CONCAT(CAST(id as VARCHAR), 'arbitrum', 'v2.2') as position_data 
+    FROM 
+    {{ source('tigristrade_v2_arbitrum', 'TradingV2_evt_PositionOpened') }}
+
+    UNION ALL 
+
+    SELECT 
+        'open_position' as evt_type, 
+        evt_tx_hash,
+        id as position_id, 
+        evt_block_time,
+        CONCAT(CAST(id as VARCHAR), 'arbitrum', 'v2.3') as position_data 
+    FROM 
+    {{ source('tigristrade_v2_arbitrum', 'TradingV3_evt_PositionOpened') }}
+),
+
+all_events as (
+    SELECT * FROM open_position_events
+)
+
+SELECT 
+    ae.*,
+    ta.trade_type
+FROM 
+all_events
+LEFT JOIN 
+ref('tigris_arbitrum_trades') ta
+    ON ae.evt_tx_hash = ta.evt_tx_hash
+WHERE ta.trade_type IS NULL 
