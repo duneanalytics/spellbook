dde7937b46cda78d2947cf42c6464eb61de31ef8
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-07-27 11:31:04 -0700

    Script to check for missing legacy models

---
+++ scripts/missing_legacy_model.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+res=$(comm -13 <( dbt ls --resource-type model --select resource_type:model,tag:legacy | sed 's/_legacy//g' |sed 's/.legacy//g' | sort ) <( dbt ls --resource-type model --select tag:dunesql,resource_type:model | sort ))
+if [ -z "$res" ]; then
+  exit 0
+else
+  echo "Legacy models missing:"
+  echo "$res"
+  echo "If you added a new DuneSQL model, please add a dummy spark model with the same name."
+  exit 1
+fi
