c00457ff090b47648668e50ebb79dcb86bc9f707
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-07-26 20:47:28 -0700

    Remove all non-legacy models

--- models/cow_protocol/tx_hash_labels/stable_to_stable/cow_protocol_tx_hash_labels_stable_to_stable.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        alias = alias('tx_hash_labels_stable_to_stable'),
-        post_hook='{{ expose_spells(\'["ethereum"]\', "sector", "tx_hash_labels", \'["gentrexha"]\') }}'
-    )
-}}
-
-SELECT * FROM {{ ref('cow_protocol_tx_hash_labels_stable_to_stable_ethereum') }}
--- models/cow_protocol/tx_hash_labels/treasury_management/cow_protocol_tx_hash_labels_treasury_management.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        alias = alias('tx_hash_labels_treasury_management'),
-        post_hook='{{ expose_spells(\'["ethereum"]\', "sector", "tx_hash_labels", \'["gentrexha"]\') }}'
-    )
-}}
-
-SELECT * FROM {{ ref('cow_protocol_tx_hash_labels_treasury_management_ethereum') }}
