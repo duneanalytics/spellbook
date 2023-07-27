214f2410a3aa9bfd2220cb1d9cd2aa056b17b1a0
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-07-27 12:50:32 -0700

    compare schemas

--- scripts/missing_legacy_model.sh
+++ scripts/missing_legacy_model.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-res=$(comm -13 <( dbt ls --resource-type model --select resource_type:model,tag:legacy --output name | sed 's/_legacy//g' | sort ) <( dbt ls --resource-type model --select tag:dunesql,resource_type:model --output name | sort ))
+res=$(comm -13 <( dbt ls --resource-type model --select resource_type:model,tag:legacy --output json | jq '.config.schema' | sort | uniq ) <( dbt ls --resource-type model --select tag:dunesql,resource_type:model --output json | jq '.config.schema' | sort | uniq ))
 if [ -z "$res" ]; then
   exit 0
 else
-  echo "Legacy models missing:"
+  echo "Legacy schema missing:"
   echo "$res"
-  echo "If you added a new DuneSQL model, please add a dummy spark model with the same name."
+  echo "If you added a new DuneSQL model, please add a dummy spark model with the same name and schema to force schema creation on spark."
   exit 1
 fi
