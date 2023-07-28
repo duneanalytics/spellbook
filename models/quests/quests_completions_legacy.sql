29dec05d916ff3412e185ece9653e7605211dbda
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-28 19:18:22 -0400

    wee

--- models/quests/quests_completions_legacy.sql
+++ models/quests/quests_completions_legacy.sql
@@ -7,5 +7,13 @@
 
 
 -- DUMMY TABLE, WILL BE REMOVED SOON
+-- fitting the same schema because of downstream tables (yolo)
 select 
-    1
+    '1' as blockchain,
+    '1' as platform, 
+    0x as quester_address, 
+    1 as block_number, 
+    timestamp '2023-01-01' as block_time, 
+    '1' as quest_name, 
+    0x as rewards_token as token_address,
+    '1' as token_id
