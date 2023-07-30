8db4967a8b1d14102c981860d271aff23e830061
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-30 13:23:13 -0400

    rm ref

--- models/tokens/base/tokens_base_nft.sql
+++ models/tokens/base/tokens_base_nft.sql
@@ -17,5 +17,5 @@ SELECT
 FROM {{ ref('tokens_base_nft_standards')}} c
 LEFT JOIN {{ref('tokens_base_nft_curated')}} t
     ON c.contract_address = t.contract_address
--- LEFT JOIN {{ ref('tokens_base_nft_bridged_mapping')}} b
+-- LEFT JOIN 'tokens_base_nft_bridged_mapping b
 --     ON c.contract_address = b.contract_address
