aeae9212f4ce895c0995169c3e3c5516978e5f22
Synthquest <babaloo222@proton.me>
2023-07-30 08:58:37 -0400

    update tag

--- models/chainlink/CCIP/chainlink_CCIP_events.sql
+++ models/chainlink/CCIP/chainlink_CCIP_events.sql
@@ -1,4 +1,5 @@
-{{ config(tags=['chainlink', 'CCIP'],
+{{ config(
+        tags=['dunesql'],
         alias = alias('CCIP_events'),
         post_hook='{{ expose_spells(\'["ethereum", "avalanche_c", "optimism", "polygon"]\',
                                 "project",
