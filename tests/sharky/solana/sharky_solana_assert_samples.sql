f1183e668042419aa653f000fc0e9f91c459b02b
Ennio Nasca <nascaennio@gmail.com>
2023-07-30 17:36:10 +0200

    test: cast to timestamp

--- tests/sharky/solana/sharky_solana_assert_samples.sql
+++ tests/sharky/solana/sharky_solana_assert_samples.sql
@@ -1,4 +1,12 @@
-WITH unit_tests as
+WITH
+test_data AS (
+    SELECT
+        CAST(block_time AS TIMESTAMP) AS block_time,
+        tx_hash,
+        amount
+    FROM {{ ref('sharky_solana_loans_solscan') }}
+),
+unit_tests AS
 (
     SELECT 
         case 
@@ -6,10 +14,12 @@ WITH unit_tests as
             then True 
             else False 
         end as amount_test
-    FROM {{ ref('sharky_solana_events') }} sharky_events
-    JOIN {{ ref('sharky_solana_loans_solscan') }} test_data
-        ON test_data.tx_hash = sharky_events.tx_hash
-        AND test_data.block_time = sharky_events.block_time
+    FROM {{ ref('sharky_solana_events') }} AS sharky_events
+    JOIN test_data
+        ON (
+            test_data.tx_hash = sharky_events.tx_hash
+            AND test_data.block_time = sharky_events.block_time
+        )
     WHERE 
         sharky_events.block_time > TIMESTAMP '2023-01-01'
         and sharky_events.block_time < TIMESTAMP '2023-01-05'
