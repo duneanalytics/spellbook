79670012525753df6addf830a0e1315d97bd2965
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-07-30 11:36:39 -0400

    adds

---
+++ models/tokens/tokens_erc20_all.sql
@@ -0,0 +1,31 @@
+{{ config(
+        alias = alias('erc20_all')
+        ,tags=['dunesql']
+        ,materialized='incremental'
+        ,file_format = 'delta'
+        ,unique_key = ['blockchain','contract_address']
+        ,post_hook='{{ expose_spells(\'["arbitrum","avalanche_c","bnb","ethereum","optimism", "gnosis", "fantom", "polygon"]\',
+                                    "sector",
+                                    "tokens",
+                                    \'["msilb7"]\') }}')}}
+
+
+-- This is a table to store all contracts that are ERC20s. 
+-- This should be combined in to the main ERC20 table, such that it mirrors how the NFT table works.
+-- But to avoid screwing anything up, keeping this as separate in the interim.
+-- This table is REQUIRED for contracts mapping spells.
+
+-- DO NOT DELETE
+
+SELECT
+
+blockchain, token_address AS contract_address, 'erc20' as standard
+
+FROM {{ ref('transfers_erc20')}} tr 
+
+WHERE 1=1
+{% if is_incremental() %}
+and tr.evt_block_time >= date_trunc('day', now() - interval '7' day)
+{% endif %}
+
+GROUP BY 1,2
