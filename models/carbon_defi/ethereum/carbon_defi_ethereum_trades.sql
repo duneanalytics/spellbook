5c4e1a10c1a0ed33774676c8f3b78fb633e68185
Tiago Silva <tfn.da.silva@gmail.com>
2023-07-31 17:19:29 +0100

    Change date_trunc interval for incremental

--- models/carbon_defi/ethereum/carbon_defi_ethereum_trades.sql
+++ models/carbon_defi/ethereum/carbon_defi_ethereum_trades.sql
@@ -38,7 +38,7 @@ SELECT
     t.evt_index
 FROM {{ source('carbon_defi_ethereum', 'CarbonController_evt_TokensTraded') }} t
     {% if is_incremental() %}
-    WHERE t.evt_block_time >= date_trunc("day", now() - interval '7' day)
+    WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
     {% if not is_incremental() %}
     WHERE t.evt_block_time >= TIMESTAMP '{{project_start_date}}'
@@ -81,7 +81,7 @@ INNER JOIN {{ source('ethereum', 'transactions') }} tx
     AND tx.block_time >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    AND tx.block_time >= date_trunc('day', now() - interval '7' day)
+    AND tx.block_time >= date_trunc('day', now() - interval '1 week')
     {% endif %}
 LEFT JOIN {{ ref('tokens_erc20') }} erc20a
     ON erc20a.contract_address = dexs.token_bought_address
@@ -97,7 +97,7 @@ LEFT JOIN {{ source('prices', 'usd') }} p_bought
     AND p_bought.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    AND p_bought.minute >= date_trunc('day', now() - interval '7' day)
+    AND p_bought.minute >= date_trunc('day', now() - interval '1 week')
     {% endif %}
 LEFT JOIN {{ source('prices', 'usd') }} p_sold
     ON p_sold.minute = date_trunc('minute', dexs.block_time)
@@ -107,5 +107,5 @@ LEFT JOIN {{ source('prices', 'usd') }} p_sold
     AND p_sold.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    AND p_sold.minute >= date_trunc('day', now() - interval '7' day)
+    AND p_sold.minute >= date_trunc('day', now() - interval '1 week')
     {% endif %}
