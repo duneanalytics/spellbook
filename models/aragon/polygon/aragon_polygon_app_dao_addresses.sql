8f42f46136a851f8ff3048b1811eac3a63a5baed
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-07-31 19:50:56 +0100

    fix polygon

--- models/aragon/polygon/aragon_polygon_app_dao_addresses.sql
+++ models/aragon/polygon/aragon_polygon_app_dao_addresses.sql
@@ -20,8 +20,8 @@ SELECT
     dao, 
     dao as dao_wallet_address, 
     evt_block_time as created_block_time, 
-    CAST(date_trunc('day', block_time) as DATE) as created_date, 
-    CAST(date_trunc('month', block_time) as DATE) as block_month, 
+    CAST(date_trunc('day', evt_block_time) as DATE) as created_date, 
+    CAST(date_trunc('month', evt_block_time) as DATE) as block_month, 
     'aragon_app' as product 
 FROM 
 {{ source('aragon_app_polygon', 'DAORegistry_evt_DAORegistered') }}
