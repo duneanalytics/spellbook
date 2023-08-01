7857ff6a5a601c51523f973fd135d5e3fdb24616
RantumBits <rantum@rantum.xyz>
2023-08-01 12:24:16 -0700

    remove lines

--- models/zeroex/celo/zeroex_celo_api_fills_deduped.sql
+++ models/zeroex/celo/zeroex_celo_api_fills_deduped.sql
@@ -20,7 +20,12 @@ AS
     FROM {{ ref('zeroex_celo_api_fills') }}
     WHERE 1=1
     AND swap_flag = true
-      
+    {% if is_incremental() %}
+    AND block_time >= date_trunc('day', now() - interval '7' day)
+    {% endif %}
+    {% if not is_incremental() %}
+    AND block_time >= cast('{{zeroex_v3_start_date}}' as date)
+    {% endif %}    
 )
 , fills_first_last
 AS
@@ -64,11 +69,11 @@ SELECT  a.blockchain
       , a.block_time
       , b.taker_symbol AS taker_symbol
       , b.maker_symbol AS maker_symbol
-      , CASE WHEN lower(b.taker_symbol) > lower(b.maker_symbol) THEN concat(b.maker_symbol, '-', b.taker_symbol) ELSE concat(b.taker_symbol, '-', b.maker_symbol) END AS token_pair
+     -- , CASE WHEN lower(b.taker_symbol) > lower(b.maker_symbol) THEN concat(b.maker_symbol, '-', b.taker_symbol) ELSE concat(b.taker_symbol, '-', b.maker_symbol) END AS token_pair
       , b.taker_token_amount
       , b.maker_token_amount
-      , CAST(b.taker_token_amount_raw AS DECIMAL(38,0)) AS taker_token_amount_raw
-      , CAST(b.maker_token_amount_raw AS DECIMAL(38,0)) AS maker_token_amount_raw
+     -- , CAST(b.taker_token_amount_raw AS DECIMAL(38,0)) AS taker_token_amount_raw
+     -- , CAST(b.maker_token_amount_raw AS DECIMAL(38,0)) AS maker_token_amount_raw
       , a.volume_usd
       , b.taker_token
       , b.maker_token
@@ -79,7 +84,7 @@ SELECT  a.blockchain
       , a.tx_from
       , a.tx_to
       , b.evt_index
-      , CAST(ARRAY[-1] as array<bigint>) as trace_address
+     -- , CAST(ARRAY[-1] as array<bigint>) as trace_address
       , a.type
       , a.swap_flag
       , b.fills_within
