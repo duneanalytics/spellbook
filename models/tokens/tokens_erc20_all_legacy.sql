fdc406b8acfd9d79e23baec9fc3620d54f19f284
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-08-01 11:00:15 -0400

    fix

--- models/tokens/tokens_erc20_all_legacy.sql
+++ models/tokens/tokens_erc20_all_legacy.sql
@@ -1,25 +1,8 @@
 {{ config(
         alias = alias('erc20_all',legacy_model=True)
         ,tags=['legacy']
+)
 }}
 
 
         SELECT '1' as blockchain, '0x' as contract_address, 'erc20' as standard
-
-        from {{ source('erc20_' + chain , 'evt_transfer') }} tr 
-
-        WHERE 1=1
-        {% if is_incremental() %}
-        and tr.evt_block_time >= date_trunc('day', now() - interval '7' day)
-        AND contract_address NOT IN (
-                                SELECT contract_address
-                                from {{this}} t
-                                WHERE t.blockchain = '{{chain}}'
-                                )
-        {% endif %}
-        GROUP BY 1,2,3 --uniques
-        
-        {% if not loop.last %}
-        UNION ALL
-        {% endif %}
-        {% endfor %}
