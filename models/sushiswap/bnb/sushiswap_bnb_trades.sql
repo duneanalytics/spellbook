56e22c8e6af718d1d2ffcf5fe512146fa6ee61c8
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-08-02 14:44:04 +0200

    Remove double quotes around 'day'

--- models/sushiswap/bnb/sushiswap_bnb_trades.sql
+++ models/sushiswap/bnb/sushiswap_bnb_trades.sql
@@ -29,7 +29,7 @@ with dexs as (
         inner join {{ source('sushi_bnb', 'UniswapV2Factory_evt_PairCreated') }} f 
             on f.pair = t.contract_address
     {% if is_incremental() %}
-    WHERE t.evt_block_time >= date_trunc("day", now() - interval '7' day)
+    WHERE t.evt_block_time >= date_trunc('day', now() - interval '7' day)
     {% else %}
     WHERE t.evt_block_time >= TIMESTAMP '{{ project_start_date }}'
     {% endif %}
@@ -72,7 +72,7 @@ inner join {{ source('bnb', 'transactions') }} tx
     and tx.block_time >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and tx.block_time >= date_trunc("day", now() - interval '7' day)
+    and tx.block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 left join {{ ref('tokens_erc20') }} erc20a
     on erc20a.contract_address = dexs.token_bought_address
@@ -88,7 +88,7 @@ left join {{ source('prices', 'usd') }} p_bought
     and p_bought.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and p_bought.minute >= date_trunc("day", now() - interval '7' day)
+    and p_bought.minute >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 left join {{ source('prices', 'usd') }} p_sold
     on p_sold.minute = date_trunc('minute', dexs.block_time)
@@ -98,6 +98,6 @@ left join {{ source('prices', 'usd') }} p_sold
     and p_sold.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and p_sold.minute >= date_trunc("day", now() - interval '7' day)
+    and p_sold.minute >= date_trunc('day', now() - interval '7' day)
     {% endif %}
     
