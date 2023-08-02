ab1a63d571a31c08aac72baa446a9d579831e475
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-08-02 15:27:00 +0200

    bad legacy ref in dex_trades.sql

--- models/dex/dex_trades.sql
+++ models/dex/dex_trades.sql
@@ -15,7 +15,7 @@
  
 {% set dex_trade_models = [
  ref('uniswap_trades')
-,ref('sushiswap_trades_legacy')
+,ref('sushiswap_trades')
 ,ref('kyberswap_trades')
 ,ref('fraxswap_trades')
 ,ref('curvefi_trades')
