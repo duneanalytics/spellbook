cda9d1f24d305687d3dec9518efe22ad02b2aaba
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-08-02 15:55:06 -0400

    Create decoded_legacy.sql

---
+++ models/contracts/decoded_legacy.sql
@@ -0,0 +1,163 @@
+{{  config(
+        schema = 'contracts',
+        alias = alias('decoded', legacy_model=True),
+        tags = ['legacy'],
+        file_format = 'delta',
+        materialized = 'incremental',
+        incremental_strategy = 'merge',
+        unique_key = ['blockchain', 'abi_id', 'address', 'created_at']
+    )
+}}
+        
+        
+SELECT 
+    'arbitrum' AS blockchain
+    , abi_id
+    , abi
+    , CAST(address AS VARCHAR) AS address
+    , CAST("from" AS VARCHAR) AS "from"
+    , CAST(code AS VARCHAR) AS code
+    , name
+    , namespace
+    , dynamic
+    , base
+    , factory
+    , detection_source
+    , created_at 
+FROM {{ source('arbitrum', 'contracts') }}
+{% if is_incremental() %}
+WHERE created_at >= now() - interval '7' day
+{% endif %}
+UNION ALL
+SELECT 
+    'avalanche_c' AS blockchain
+    , abi_id
+    , abi
+    , CAST(address AS VARCHAR) AS address
+    , CAST("from" AS VARCHAR) AS "from"
+    , CAST(code AS VARCHAR) AS code
+    , name
+    , namespace
+    , dynamic
+    , base
+    , factory
+    , detection_source
+    , created_at
+FROM {{ source('avalanche_c', 'contracts') }}
+{% if is_incremental() %}
+WHERE created_at >= now() - interval '7' day
+{% endif %}
+UNION ALL
+SELECT
+    'bnb' AS blockchain
+    , abi_id
+    , abi
+    , CAST(address AS VARCHAR) AS address
+    , CAST("from" AS VARCHAR) AS "from"
+    , CAST(code AS VARCHAR) AS code
+    , name
+    , namespace
+    , dynamic
+    , base
+    , factory
+    , detection_source
+    , created_at
+FROM {{ source('bnb', 'contracts') }}
+{% if is_incremental() %}
+WHERE created_at >= now() - interval '7' day
+{% endif %}
+UNION ALL
+SELECT
+    'ethereum' AS blockchain
+    , abi_id
+    , abi
+    , CAST(address AS VARCHAR) AS address
+    , CAST("from" AS VARCHAR) AS "from"
+    , CAST(code AS VARCHAR) AS code
+    , name
+    , namespace
+    , dynamic
+    , base
+    , factory
+    , detection_source
+    , created_at 
+FROM {{ source('ethereum', 'contracts') }}
+{% if is_incremental() %}
+WHERE created_at >= now() - interval '7' day
+{% endif %}
+UNION ALL
+SELECT
+    'fantom' AS blockchain
+    , abi_id
+    , abi
+    , CAST(address AS VARCHAR) AS address
+    , CAST("from" AS VARCHAR) AS "from"
+    , CAST(code AS VARCHAR) AS code
+    , name
+    , namespace
+    , dynamic
+    , base
+    , factory
+    , detection_source
+    , created_at 
+FROM {{ source('fantom', 'contracts') }}
+{% if is_incremental() %}
+WHERE created_at >= now() - interval '7' day
+{% endif %}
+UNION ALL
+SELECT 
+    'gnosis' AS blockchain
+    , abi_id
+    , abi
+    , CAST(address AS VARCHAR) AS address
+    , CAST("from" AS VARCHAR) AS "from"
+    , CAST(code AS VARCHAR) AS code
+    , name
+    , namespace
+    , dynamic
+    , base
+    , factory
+    , detection_source
+    , created_at 
+FROM {{ source('gnosis', 'contracts') }}
+{% if is_incremental() %}
+WHERE created_at >= now() - interval '7' day
+{% endif %}
+UNION ALL
+SELECT
+    'optimism' AS blockchain
+    , abi_id
+    , abi
+    , CAST(address AS VARCHAR) AS address
+    , CAST("from" AS VARCHAR) AS "from"
+    , CAST(code AS VARCHAR) AS code
+    , name
+    , namespace
+    , dynamic
+    , base
+    , factory
+    , detection_source
+    , created_at 
+FROM {{ source('optimism', 'contracts') }}
+{% if is_incremental() %}
+WHERE created_at >= now() - interval '7' day
+{% endif %}
+UNION ALL
+SELECT 
+    'polygon' AS blockchain
+    , abi_id
+    , abi
+    , CAST(address AS VARCHAR) AS address
+    , CAST("from" AS VARCHAR) AS "from"
+    , CAST(code AS VARCHAR) AS code
+    , name
+    , namespace
+    , dynamic
+    , base
+    , factory
+    , detection_source
+    , created_at 
+FROM {{ source('polygon', 'contracts') }}
+{% if is_incremental() %}
+WHERE created_at >= now() - interval '7' day
+{% endif %}
