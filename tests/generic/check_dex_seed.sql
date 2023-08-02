9bc0892540e843458f76b75837d2f561513a8afd
Huang Geyang <Sukebeta@outlook.com>
2023-08-03 02:40:11 +0800

    Remove block_date in check_dex_seed

--- tests/generic/check_dex_seed.sql
+++ tests/generic/check_dex_seed.sql
@@ -3,7 +3,7 @@
 {% test check_dex_seed(model, blockchain=None, project=None, version=None) %}
     {%- set seed_file = ref('dex_trades_seed') -%}
     {%- set seed_check_columns = ['token_bought_address','token_sold_address'] -%}
-    {%- set seed_matching_columns = ['block_date','blockchain','project','version','tx_hash','evt_index'] -%}
+    {%- set seed_matching_columns = ['blockchain','project','version','tx_hash','evt_index'] -%}
     {%- set filter = {'blockchain':blockchain, 'project':project, 'version':version} -%}
     {{ check_seed_macro(model,seed_file,seed_matching_columns,seed_check_columns,filter) }}
     
