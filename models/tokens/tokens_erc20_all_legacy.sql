9fdab4d59a0e7eca7eb54a623e67c3b8353ae9d5
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-08-02 07:46:02 -0400

    mods

--- models/tokens/tokens_erc20_all_legacy.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-        alias = alias('erc20_all',legacy_model=True)
-        ,tags=['legacy']
-)
-}}
-
-
-        SELECT '1' as blockchain, '0x' as contract_address, 'erc20' as standard
