2fa041a91176708e9aef61acac030a4de1da6cd6
chef-seaweed <121569855+chef-seaweed@users.noreply.github.com>
2023-08-02 13:50:50 +0800

    Fix sparksql "from"

--- models/maverick/bnb/maverick_v1_bnb_trades_legacy.sql
+++ models/maverick/bnb/maverick_v1_bnb_trades_legacy.sql
@@ -65,11 +65,11 @@ SELECT
     ) AS amount_usd
     ,dexs.token_bought_address
     ,dexs.token_sold_address
-    ,coalesce(dexs.taker, tx."from") AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
+    ,coalesce(dexs.taker, tx.from) AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
     ,dexs.maker
     ,dexs.project_contract_address
     ,dexs.tx_hash
-    ,tx."from" AS tx_from
+    ,tx.from AS tx_from
     ,tx.to AS tx_to
     ,dexs.trace_address
     ,dexs.evt_index
