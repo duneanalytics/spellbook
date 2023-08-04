f455f86a670cd3edbd94a0ec6a3c8d35f68c7acd
Ennio Nasca <nascaennio@gmail.com>
2023-08-04 17:47:57 +0200

    chore: revert using instruction_calls

--- models/sharky/solana/sharky_solana_events.sql
+++ models/sharky/solana/sharky_solana_events.sql
@@ -21,24 +21,16 @@
 {%- set sharky_smart_contract = 'SHARKobtfF1bHhxD2eqftjHBdVSCbKo9JtgK71FhELP' %}
 
 WITH sharky_txs AS (
-        SELECT
-            UPPER(SUBSTR(CAST(data AS VARCHAR), 3)) AS ix_data,
-            block_slot AS block_number,
-            block_time,
-            account_arguments,
-            tx_id AS id,
-            block_date,
-            tx_signer AS user,
-            ARRAY[CAST( ROW(data, account_arguments) AS ROW(data VARBINARY, account_arguments ARRAY<VARCHAR>) )] AS sharky_instructions
-        FROM {{ source('solana', 'instruction_calls') }}
-        WHERE
-            tx_success
+        SELECT DISTINCT tx_id AS id,
+               block_time
+        FROM {{ source('solana', 'account_activity') }}
+        WHERE tx_success
             {% if not is_incremental() %}
             AND block_time >= TIMESTAMP '{{ project_start_date }}'
             {% else %}
             AND block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
             {% endif %}
-            AND executing_account = '{{sharky_smart_contract}}'
+            AND address = '{{sharky_smart_contract}}'
     ),
     sol_price AS (
         SELECT minute,
@@ -52,6 +44,65 @@ WITH sharky_txs AS (
             {% else %}
             AND minute >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
             {% endif %}
+    ),
+    filtered_txs AS (
+        SELECT signatures,
+               block_date,
+               block_time,
+               block_slot,
+               post_balances,
+               pre_balances,
+               account_keys,
+               instructions,
+               signer,
+               id
+        FROM {{ source('solana','transactions') }} tx
+        {% if not is_incremental() %}
+        WHERE tx.block_time >= TIMESTAMP '{{ project_start_date }}'
+        {% else %}
+        WHERE tx.block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+        {% endif %}
+    ),
+    raw_events AS (
+        SELECT 'solana'                                                                                     AS blockchain,
+               'sharky'                                                                                     AS project,
+               pre_balances,
+               post_balances,
+               account_keys,
+               filtered_txs.signatures[1]                                                                   AS tx_hash,
+               filtered_txs.block_date,
+               filtered_txs.block_time,
+               CAST(filtered_txs.block_slot AS BIGINT)                                                      AS block_number,
+               filter(
+                           filtered_txs.instructions,
+                           x -> x.executing_account = '{{sharky_smart_contract}}'
+                   )                                                                                        AS sharky_instructions,
+               filtered_txs.signer                                                                          AS user,
+               filtered_txs.id
+        FROM sharky_txs
+        INNER JOIN filtered_txs
+            ON sharky_txs.block_time = filtered_txs.block_time
+            AND sharky_txs.id = filtered_txs.id
+    ),
+    decoded_events AS (
+        SELECT
+            blockchain,
+            project,
+            pre_balances,
+            post_balances,
+            account_keys,
+            tx_hash,
+            block_date,
+            block_time,
+            block_number,
+            user,
+            id,
+            ARRAY[sharky_instructions[rownumber]] AS sharky_instructions,
+            -- We need to decode the instruction data to identify the instruction since the payload changes the base58
+            -- encoded string. We start from the third character to remove the 0x prefix
+            UPPER(SUBSTR(CAST(fromBase58(data) AS VARCHAR), 3)) AS ix_data
+        FROM raw_events,
+        UNNEST(sharky_instructions) WITH ORDINALITY AS s(data, executing_account, account_arguments, inner_ix, rownumber)
     ),
     events AS (
         SELECT *,
@@ -79,35 +130,35 @@ WITH sharky_txs AS (
                             OR starts_with(ix_data, '471B11834E493E5C') -- ExtendLoanV3
                         ) THEN 'Extend'
             END AS evt_type
-        FROM sharky_txs
-    ), loans AS (
+        FROM decoded_events
+), loans AS (
         SELECT *,
                CASE
                    WHEN evt_type = 'Offer' THEN
                        CASE
-                           WHEN block_number > 164178914 THEN account_arguments[4] -- upgrade to version 3.0 2PXg25v2YGfvFCyX9najJ2mNS5QADvVYNUTtXYYhbyN3cHERqEgbnuDnVKnStaQAMa1edRhM9GAZaHYMPdR9jX8o
+                           WHEN block_number > 164178914 THEN sharky_instructions[1].account_arguments[4] -- upgrade to version 3.0 2PXg25v2YGfvFCyX9najJ2mNS5QADvVYNUTtXYYhbyN3cHERqEgbnuDnVKnStaQAMa1edRhM9GAZaHYMPdR9jX8o
                            ELSE sharky_instructions[1].account_arguments[3]
                        END
                    WHEN evt_type = 'Take' THEN
                        CASE
-                           WHEN block_number > 206343732 THEN account_arguments[5] -- upgrade to version 6.0 3Hk3RgT1aBDZAboPvjCA8XwENGaY5daCtf2AjLV2KdMgQy9ZbPju7TPdq6KFQhYhxjn6GNSaZG1ke2eQjt18tJsa
-                           WHEN block_number > 184280858 THEN account_arguments[5] -- upgrade to version 5.0 5BjeJ7Wnf7QxHSBEhXN4htn4mECD36SfsEUZbMvzhAF5sMPMCCV1jcu2rigNV8SZohVL7J3ctLLgXZ4gVzzPkmJ7
-                           WHEN block_number > 177070316 THEN account_arguments[6] -- upgrade to version 4.0 zFA9ffgu3Z1ETH2LdH3o6C6xzKWdJFcgA3qoXo84S6zQ2iJyaRfe61VXw6BBFGM5rFsHFm9fKSUrxEuUTy7grWR
-                           WHEN block_number > 164178914 THEN account_arguments[7] -- upgrade to version 3.0 2PXg25v2YGfvFCyX9najJ2mNS5QADvVYNUTtXYYhbyN3cHERqEgbnuDnVKnStaQAMa1edRhM9GAZaHYMPdR9jX8o
-                           WHEN block_number > 132405709 THEN account_arguments[6] -- upgrade to version 2.0 2oNjJSxAM72Y6t7ALKXBiMDjtwhmxEfbk8jBz4es3U6XpBfw2Jvgcu3pSvxuHUkDWbVE48xxpjwgpuxrNnPFjvxm
-                           ELSE account_arguments[7]
+                           WHEN block_number > 206343732 THEN sharky_instructions[1].account_arguments[5] -- upgrade to version 6.0 3Hk3RgT1aBDZAboPvjCA8XwENGaY5daCtf2AjLV2KdMgQy9ZbPju7TPdq6KFQhYhxjn6GNSaZG1ke2eQjt18tJsa
+                           WHEN block_number > 184280858 THEN sharky_instructions[1].account_arguments[5] -- upgrade to version 5.0 5BjeJ7Wnf7QxHSBEhXN4htn4mECD36SfsEUZbMvzhAF5sMPMCCV1jcu2rigNV8SZohVL7J3ctLLgXZ4gVzzPkmJ7
+                           WHEN block_number > 177070316 THEN sharky_instructions[1].account_arguments[6] -- upgrade to version 4.0 zFA9ffgu3Z1ETH2LdH3o6C6xzKWdJFcgA3qoXo84S6zQ2iJyaRfe61VXw6BBFGM5rFsHFm9fKSUrxEuUTy7grWR
+                           WHEN block_number > 164178914 THEN sharky_instructions[1].account_arguments[7] -- upgrade to version 3.0 2PXg25v2YGfvFCyX9najJ2mNS5QADvVYNUTtXYYhbyN3cHERqEgbnuDnVKnStaQAMa1edRhM9GAZaHYMPdR9jX8o
+                           WHEN block_number > 132405709 THEN sharky_instructions[1].account_arguments[6] -- upgrade to version 2.0 2oNjJSxAM72Y6t7ALKXBiMDjtwhmxEfbk8jBz4es3U6XpBfw2Jvgcu3pSvxuHUkDWbVE48xxpjwgpuxrNnPFjvxm
+                           ELSE sharky_instructions[1].account_arguments[7]
                        END
                    WHEN (
                        evt_type = 'Rescind'
                        OR evt_type = 'Repay'
                        OR evt_type = 'Foreclose'
                        OR evt_type = 'Extend'
-                   ) THEN account_arguments[1]
+                   ) THEN sharky_instructions[1].account_arguments[1]
                END as loan_id
         FROM events
         WHERE evt_type IS NOT NULL
 ), final_event AS (
-        SELECT sharky_loan.*, block_time, id,
+        SELECT *,
                CASE
                     WHEN (
                             evt_type = 'Offer'
@@ -122,17 +173,16 @@ WITH sharky_txs AS (
                     WHEN (
                             evt_type = 'Offer'
                             OR evt_type = 'Take'
-                        ) THEN account_arguments[array_position(account_arguments, loan_id) + 1]
-                    WHEN evt_type = 'Rescind' THEN account_arguments[5]
+                        ) THEN sharky_instructions[1].account_arguments[array_position(sharky_instructions[1].account_arguments, loan_id) + 1]
+                    WHEN evt_type = 'Rescind' THEN sharky_instructions[1].account_arguments[5]
                 END AS escrow
             FROM loans
-        ) AS sharky_loan
-        INNER JOIN {{ source('solana', 'transactions') }} USING (block_time, id)
+        )
 )
 SELECT
-    'solana' AS blockchain,
-    'sharky' AS project,
-    id AS tx_hash,
+    blockchain,
+    project,
+    tx_hash,
     block_date,
     CAST(date_trunc('month', block_date) AS DATE) AS block_month,
     block_time,
