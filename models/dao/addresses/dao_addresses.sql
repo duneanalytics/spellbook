34223b162e3d9f860911c8dfdaa3e85bc83765a6
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-08-07 20:24:15 +0100

    update transactions and balances

--- models/dao/addresses/dao_addresses.sql
+++ models/dao/addresses/dao_addresses.sql
@@ -3,7 +3,7 @@
     alias = alias('addresses'),
     materialized = 'view',
     file_format = 'delta',
-    post_hook='{{ expose_spells(\'["ethereum", "gnosis", "polygon"]\',
+    post_hook='{{ expose_spells(\'["ethereum", "gnosis", "polygon", "base"]\',
                                 "sector",
                                 "dao",
                                 \'["Henrystats"]\') }}')
@@ -12,7 +12,8 @@
 {% set addresses_models = [
 ref('dao_addresses_ethereum')
 ,ref('dao_addresses_gnosis')
-,ref('dao_addresses_polygon')
+,ref('dao_addresses_polygon'),
+,ref('dao_addresses_base'),
 ] %}
 
 
