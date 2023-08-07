d12a02cb8e26e12566c63fe5641c7744cec0a9fe
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-08-07 13:54:33 -0400

    mod

---
+++ models/decent/optimism/decent_optimism_edition_metadata_legacy.sql
@@ -0,0 +1,8 @@
+{{
+    config(
+        alias = alias('edition_metadata', legacy_model=True)
+        ,tags = ['legacy']
+    )
+}}
+
+SELECT 1 as edition_address
---
+++ models/mirror/optimism/mirror_optimism_edition_metadata_legacy.sql
@@ -0,0 +1,8 @@
+{{
+    config(
+         tags = ['legacy']
+        , alias = alias('edition_metadata',legacy_model=True)
+    )
+}}
+
+SELECT 1 as edition_address
