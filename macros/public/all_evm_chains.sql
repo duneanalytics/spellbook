045fdfbb9b0d67641b0a56a666131c27de28da29
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-08-07 15:28:35 -0400

    mod

--- macros/public/all_evm_chains.sql
+++ macros/public/all_evm_chains.sql
@@ -21,23 +21,3 @@
 
     {{ return(mainnets_testnets_chains) }}
 {% endmacro %}
-
-{% macro all_evm_testnet_chains() %}
-   {{ return(['goerli']) }}
-{% endmacro %}
-
-{% macro all_evm_mainnets_testnets_chains() %}
-    {% set mainnet_chains = all_evm_chains() %}
-    {% set testnet_chains = all_evm_testnet_chains() %}
-    {% set mainnets_testnets_chains = [] %}
-
-    {% for chain in mainnet_chains %}
-        {% do mainnets_testnets_chains.append(chain) %}
-    {% endfor %}
-
-    {% for chain in testnet_chains %}
-        {% do mainnets_testnets_chains.append(chain) %}
-    {% endfor %}
-
-    {{ return(mainnets_testnets_chains) }}
-{% endmacro %}
