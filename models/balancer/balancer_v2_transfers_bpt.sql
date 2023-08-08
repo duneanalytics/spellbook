50b172e7f2d48e43ca5681d719a99b579de36891
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-08-08 12:46:15 -0400

    Update balancer_v2_transfers_bpt.sql

--- models/balancer/balancer_v2_transfers_bpt.sql
+++ models/balancer/balancer_v2_transfers_bpt.sql
@@ -30,8 +30,8 @@ FROM (
       , evt_index
       , evt_block_time
       , evt_block_number
-      , "from"
-      , to
+      , transfer_from
+      , transfer_to
       , value
     FROM {{ model }}
     {% if not loop.last %}
