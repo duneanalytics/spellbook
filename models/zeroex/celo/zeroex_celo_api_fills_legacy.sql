55256bf639190bf0d563812967902d7ad40604e3
RantumBits <rantum@rantum.xyz>
2023-08-08 15:13:41 -0700

    model name

--- models/zeroex/celo/zeroex_celo_api_fills_legacy.sql
+++ models/zeroex/celo/zeroex_celo_api_fills_legacy.sql
@@ -1,7 +1,7 @@
 {{  config(
     tags=['legacy'],
-        alias = alias('api_fills_celo', legacy_model=True),
-        schema = 'zeroex_celo_api_fills'
+        alias = alias('api_fills_celo', legacy_model=True)
+        
     )
 }}
 
