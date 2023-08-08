eae335da62407a7bc8ca9c13da94f7ec12f50eac
jeff-dude <jeff@dune.xyz>
2023-08-08 18:01:43 -0400

    migrate optimism

--- models/curvefi/optimism/curvefi_optimism_gauge_mappings.sql
+++ models/curvefi/optimism/curvefi_optimism_gauge_mappings.sql
@@ -1,5 +1,6 @@
 {{
     config(
+        tags = ['dunesql'],
         schema = 'curvefi_optimism',
         alias = alias('gauge_mappings'),
         materialized = 'incremental',
@@ -14,26 +15,43 @@
 }}
 
 SELECT distinct 
-        'optimism' as blockchain, '1' as version
-        , a.pool AS pool_contract, a.gauge AS incentives_contract, 'rewards gauge' AS incentives_type
-        , a.evt_block_time, a.evt_block_number, a.contract_address, a.evt_tx_hash, a.evt_index
-FROM (
-    SELECT 
-    _lp_token As pool, _gauge AS gauge,
-    evt_block_time, evt_block_number, contract_address, evt_tx_hash, evt_index
-    FROM {{ source ('curvefi_optimism', 'Vyper_contract_evt_DeployedGauge') }}
-    {% if is_incremental() %}
-    WHERE evt_block_time >= NOW() - interval '1 week'
-    {% endif %}
+        'optimism' as blockchain
+        , '1' as version
+        , a.pool AS pool_contract
+        , a.gauge AS incentives_contract
+        , 'rewards gauge' AS incentives_type
+        , a.evt_block_time
+        , a.evt_block_number
+        , a.contract_address
+        , a.evt_tx_hash
+        , a.evt_index
+FROM
+    (
+        SELECT 
+            _lp_token As pool
+            , _gauge AS gauge
+            , evt_block_time
+            , evt_block_number
+            , contract_address
+            , evt_tx_hash
+            , evt_index
+        FROM {{ source ('curvefi_optimism', 'Vyper_contract_evt_DeployedGauge') }}
+        {% if is_incremental() %}
+        WHERE evt_block_time >= date_trunc('day', now() - interval '7' day)
+        {% endif %}
 
-    UNION ALL 
+        UNION ALL 
 
-    SELECT 
-    pool, gauge,
-    evt_block_time, evt_block_number, contract_address, evt_tx_hash, evt_index
-    FROM {{ source ('curvefi_optimism', 'PoolFactory_evt_LiquidityGaugeDeployed') }}
-    {% if is_incremental() %}
-    WHERE evt_block_time >= NOW() - interval '1 week'
-    {% endif %}
+        SELECT 
+            pool
+            , gauge
+            , evt_block_time
+            , evt_block_number
+            , contract_address
+            , evt_tx_hash
+            , evt_index
+        FROM {{ source ('curvefi_optimism', 'PoolFactory_evt_LiquidityGaugeDeployed') }}
+        {% if is_incremental() %}
+        WHERE evt_block_time >= date_trunc('day', now() - interval '7' day)
+        {% endif %}
 ) a
-
--- models/curvefi/optimism/curvefi_optimism_trades.sql
+++ models/curvefi/optimism/curvefi_optimism_trades.sql
@@ -1,10 +1,11 @@
 {{ config(
+    tags = ['dunesql'],
     alias = alias('trades'),
-    partition_by = ['block_date'],
+    partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address']
+    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index']
     )
 }}
 
@@ -30,7 +31,6 @@ SELECT
     tb.token AS token_sold_address,
     project_contract_address,
     tx_hash,
-    trace_address,
     evt_index,
     bought_id,
     sold_id
@@ -41,19 +41,18 @@ SELECT
             t.evt_block_time AS block_time,
             t.evt_block_number AS block_number,
             t.buyer AS taker,
-            '' AS maker,
+            CAST(NULL as varbinary) AS maker,
             -- when amount0 is negative it means taker is buying token0 from the pool
             tokens_bought AS token_bought_amount_raw,
             tokens_sold AS token_sold_amount_raw,
             t.contract_address as project_contract_address,
             t.evt_tx_hash AS tx_hash,
-            '' AS trace_address,
             t.evt_index,
             bought_id,
             sold_id
         FROM {{ source('curvefi_optimism', 'StableSwap_evt_TokenExchange') }} t
         {% if is_incremental() %}
-        WHERE t.evt_block_time >= date_trunc('day', now() - interval '1 week')
+        WHERE t.evt_block_time >= date_trunc('day', now() - interval '7' day)
         {% endif %}
 
         UNION ALL
@@ -64,19 +63,18 @@ SELECT
             t.evt_block_time AS block_time,
             t.evt_block_number,
             t.buyer AS taker,
-            '' AS maker,
+            CAST(NULL as varbinary) AS maker,
             -- when amount0 is negative it means taker is buying token0 from the pool
             tokens_bought AS token_bought_amount_raw,
             tokens_sold AS token_sold_amount_raw,
             t.contract_address as project_contract_address,
             t.evt_tx_hash AS tx_hash,
-            '' AS trace_address,
             t.evt_index,
             bought_id,
             sold_id
         FROM {{ source('curvefi_optimism', 'MetaPoolSwap_evt_TokenExchangeUnderlying') }} t
         {% if is_incremental() %}
-        WHERE t.evt_block_time >= date_trunc('day', now() - interval '1 week')
+        WHERE t.evt_block_time >= date_trunc('day', now() - interval '7' day)
         {% endif %}
 
         UNION ALL
@@ -87,38 +85,39 @@ SELECT
             t.evt_block_time AS block_time,
             t.evt_block_number,
             t.buyer AS taker,
-            '' AS maker,
+            CAST(NULL as varbinary) AS maker,
             -- when amount0 is negative it means taker is buying token0 from the pool
             tokens_bought AS token_bought_amount_raw,
             tokens_sold AS token_sold_amount_raw,
             t.contract_address as project_contract_address,
             t.evt_tx_hash AS tx_hash,
-            '' AS trace_address,
             t.evt_index, 
             bought_id, 
             sold_id
         FROM {{ source('curvefi_optimism', 'MetaPoolSwap_evt_TokenExchange') }} t
         -- handle for dupes due to decoding issues
         WHERE NOT EXISTS (
-            SELECT 1 FROM {{ source('curvefi_optimism', 'MetaPoolSwap_evt_TokenExchangeUnderlying') }} s 
+            SELECT 1
+            FROM {{ source('curvefi_optimism', 'MetaPoolSwap_evt_TokenExchangeUnderlying') }} s 
             WHERE t.evt_block_number = s.evt_block_number
-            AND t.evt_tx_hash = s.evt_tx_hash
-            AND t.evt_index = s.evt_index
-            {% if is_incremental() %}
-            AND s.evt_block_time >= date_trunc('day', now() - interval '1 week')
-            {% endif %}
+                AND t.evt_tx_hash = s.evt_tx_hash
+                AND t.evt_index = s.evt_index
+                {% if is_incremental() %}
+                AND s.evt_block_time >= date_trunc('day', now() - interval '7' day)
+                {% endif %}
         )
         AND NOT EXISTS (
-            SELECT 1 FROM {{ source('curvefi_optimism', 'StableSwap_evt_TokenExchange') }} s 
+            SELECT 1
+            FROM {{ source('curvefi_optimism', 'StableSwap_evt_TokenExchange') }} s 
             WHERE t.evt_block_number = s.evt_block_number
-            AND t.evt_tx_hash = s.evt_tx_hash
-            AND t.evt_index = s.evt_index
-            {% if is_incremental() %}
-            AND s.evt_block_time >= date_trunc('day', now() - interval '1 week')
-            {% endif %}
+                AND t.evt_tx_hash = s.evt_tx_hash
+                AND t.evt_index = s.evt_index
+                {% if is_incremental() %}
+                AND s.evt_block_time >= date_trunc('day', now() - interval '7' day)
+                {% endif %}
         )
         {% if is_incremental() %}
-        AND t.evt_block_time >= date_trunc('day', now() - interval '1 week')
+        AND t.evt_block_time >= date_trunc('day', now() - interval '7' day)
         {% endif %}
 
         UNION ALL
@@ -129,29 +128,28 @@ SELECT
             t.evt_block_time AS block_time,
             t.evt_block_number AS block_number,
             t.buyer AS taker,
-            '' AS maker,
+            CAST(NULL as varbinary) AS maker,
             -- when amount0 is negative it means taker is buying token0 from the pool
             tokens_bought AS token_bought_amount_raw,
             tokens_sold AS token_sold_amount_raw,
             t.contract_address as project_contract_address,
             t.evt_tx_hash AS tx_hash,
-            '' AS trace_address,
             t.evt_index,
             bought_id,
             sold_id
         FROM {{ source('curvefi_optimism', 'wstETH_swap_evt_TokenExchange') }} t --Should be Stableswap, but mis-decoded
-        
         WHERE NOT EXISTS (
-            SELECT 1 FROM {{ source('curvefi_optimism', 'StableSwap_evt_TokenExchange') }} s 
+            SELECT 1
+            FROM {{ source('curvefi_optimism', 'StableSwap_evt_TokenExchange') }} s 
             WHERE t.evt_block_number = s.evt_block_number
-            AND t.evt_tx_hash = s.evt_tx_hash
-            AND t.evt_index = s.evt_index
-            {% if is_incremental() %}
-            AND s.evt_block_time >= date_trunc('day', now() - interval '1 week')
-            {% endif %}
+                AND t.evt_tx_hash = s.evt_tx_hash
+                AND t.evt_index = s.evt_index
+                {% if is_incremental() %}
+                AND s.evt_block_time >= date_trunc('day', now() - interval '7' day)
+                {% endif %}
         )
         {% if is_incremental() %}
-        AND t.evt_block_time >= date_trunc('day', now() - interval '1 week')
+        AND t.evt_block_time >= date_trunc('day', now() - interval '7' day)
         {% endif %}
 
     ) cp
@@ -170,7 +168,8 @@ SELECT DISTINCT
     'optimism' AS blockchain,
     'curve' AS project,
     '1' AS version,
-    TRY_CAST(date_trunc('DAY', dexs.block_time) AS date) AS block_date,
+    CAST(date_trunc('DAY', dexs.block_time) AS date) AS block_date,
+    CAST(date_trunc('MONTH', dexs.block_time) AS date) AS block_month,
     dexs.block_time,
     COALESCE(erc20a.symbol,p_bought.symbol)AS token_bought_symbol,
     COALESCE(erc20b.symbol,p_sold.symbol) AS token_sold_symbol
@@ -179,26 +178,25 @@ SELECT DISTINCT
         else concat(COALESCE(erc20a.symbol,p_bought.symbol), '-', COALESCE(erc20b.symbol,p_sold.symbol))
     end as token_pair,
     --On Sell: Metapools seem to always use the added coin's decimals if it's the one that's bought - even if the other token has less decimals (i.e. USDC)
-    --On Buy: Metapools seem to always use the curve pool token's decimals (18) if bought_id = 0
-    dexs.token_bought_amount_raw / POWER(10 , (CASE WHEN pool_type = 'meta' AND bought_id = 0 THEN 18 ELSE COALESCE(erc20a.decimals,p_bought.decimals) END) ) AS token_bought_amount,
-    dexs.token_sold_amount_raw / POWER(10 , (CASE WHEN pool_type = 'meta' AND bought_id = 0 THEN COALESCE(erc20a.decimals,p_bought.decimals) ELSE COALESCE(erc20b.decimals,p_sold.decimals) END) )  AS token_sold_amount,
-    CAST(dexs.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw,
-    CAST(dexs.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw,
+    --On Buy: Metapools seem to always use the curve pool token's decimals (18) if bought_id = CAST(0 as INT256)
+    dexs.token_bought_amount_raw / POWER(10 , (CASE WHEN pool_type = 'meta' AND bought_id = CAST(0 as INT256) THEN 18 ELSE COALESCE(erc20a.decimals,p_bought.decimals) END) ) AS token_bought_amount,
+    dexs.token_sold_amount_raw / POWER(10 , (CASE WHEN pool_type = 'meta' AND bought_id = CAST(0 as INT256) THEN COALESCE(erc20a.decimals,p_bought.decimals) ELSE COALESCE(erc20b.decimals,p_sold.decimals) END) )  AS token_sold_amount,
+    dexs.token_bought_amount_raw  AS token_bought_amount_raw,
+    dexs.token_sold_amount_raw  AS token_sold_amount_raw,
     coalesce(
 	    --On Sell: Metapools seem to always use the added coin's decimals if it's the one that's bought - even if the other token has less decimals (i.e. USDC)
-	    --On Buy: Metapools seem to always use the curve pool token's decimals (18) if bought_id = 0
-        dexs.token_bought_amount_raw / POWER(10 , CASE WHEN pool_type = 'meta' AND bought_id = 0 THEN 18 ELSE COALESCE(erc20a.decimals,p_bought.decimals) END) * p_bought.price,
-        dexs.token_sold_amount_raw / POWER(10 , CASE WHEN pool_type = 'meta' AND bought_id = 0 THEN COALESCE(erc20a.decimals,p_bought.decimals) ELSE COALESCE(erc20b.decimals,p_sold.decimals) END) * p_sold.price
+	    --On Buy: Metapools seem to always use the curve pool token's decimals (18) if bought_id = CAST(0 as INT256)
+        dexs.token_bought_amount_raw / POWER(10 , CASE WHEN pool_type = 'meta' AND bought_id = CAST(0 as INT256) THEN 18 ELSE COALESCE(erc20a.decimals,p_bought.decimals) END) * p_bought.price,
+        dexs.token_sold_amount_raw / POWER(10 , CASE WHEN pool_type = 'meta' AND bought_id = CAST(0 as INT256) THEN COALESCE(erc20a.decimals,p_bought.decimals) ELSE COALESCE(erc20b.decimals,p_sold.decimals) END) * p_sold.price
     ) as amount_usd,
     dexs.token_bought_address,
     dexs.token_sold_address,
-    coalesce(dexs.taker, tx.from) as taker, -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
+    coalesce(dexs.taker, tx."from") as taker, -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
     dexs.maker,
     dexs.project_contract_address,
     dexs.tx_hash,
-    tx.from as tx_from,
+    tx."from" as tx_from,
     tx.to as tx_to,
-    dexs.trace_address,
     dexs.evt_index,
     dexs.pool_type
 FROM dexs
@@ -206,10 +204,9 @@ INNER JOIN {{ source('optimism', 'transactions') }} tx
     ON dexs.tx_hash = tx.hash
     AND dexs.block_number = tx.block_number
     {% if not is_incremental() %}
-    AND tx.block_time >= '{{project_start_date}}'
-    {% endif %}
-    {% if is_incremental() %}
-    AND tx.block_time >= date_trunc('day', now() - interval '1' week)
+    AND tx.block_time >= TIMESTAMP '{{project_start_date}}'
+    {% else %}
+    AND tx.block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 LEFT JOIN {{ ref('tokens_erc20') }} erc20a
     ON erc20a.contract_address = dexs.token_bought_address
@@ -222,19 +219,17 @@ LEFT JOIN {{ source('prices', 'usd') }} p_bought
     AND p_bought.contract_address = dexs.token_bought_address
     AND p_bought.blockchain = 'optimism'
     {% if not is_incremental() %}
-    AND p_bought.minute >= '{{project_start_date}}'
-    {% endif %}
-    {% if is_incremental() %}
-    AND p_bought.minute >= date_trunc('day', now() - interval '1 week')
+    AND p_bought.minute >= TIMESTAMP '{{project_start_date}}'
+    {% else %}
+    AND p_bought.minute >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 LEFT JOIN {{ source('prices', 'usd') }} p_sold
     ON p_sold.minute = date_trunc('minute', dexs.block_time)
     AND p_sold.contract_address = dexs.token_sold_address
     AND p_sold.blockchain = 'optimism'
     {% if not is_incremental() %}
-    AND p_sold.minute >= '{{project_start_date}}'
+    AND p_sold.minute >= TIMESTAMP '{{project_start_date}}'
+    {% else %}
+    AND p_sold.minute >= date_trunc('day', now() - interval '7' day)
     {% endif %}
-    {% if is_incremental() %}
-    AND p_sold.minute >= date_trunc('day', now() - interval '1 week')
-    {% endif %}
-;
+
