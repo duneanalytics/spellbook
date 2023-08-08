f373cbd44983e3dd7752c4f523fa3d37992eb2c6
RantumBits <rantum@rantum.xyz>
2023-08-08 15:04:41 -0700

    add legacy model =true

--- models/zeroex/celo/zeroex_celo_api_fills_legacy.sql
+++ models/zeroex/celo/zeroex_celo_api_fills_legacy.sql
@@ -1,6 +1,6 @@
 {{  config(
     tags=['legacy'],
-        alias = alias('api_fills_celo'),
+        alias = alias('api_fills_celo', legacy_model=True),
         schema = 'zeroex_celo_api_fills'
     )
 }}
