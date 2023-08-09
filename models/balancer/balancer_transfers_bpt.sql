e376b27f78c760e19fa7d41f7afff34676a7de6d
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-08-09 10:57:10 -0400

    Update balancer_transfers_bpt.sql

--- models/balancer/balancer_transfers_bpt.sql
+++ models/balancer/balancer_transfers_bpt.sql
@@ -2,7 +2,7 @@
     schema = 'balancer',
     tags = ['dunesql'],
     alias = alias('transfers_bpt'),
-    post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "ethereum", "gnosis", "optimism", "polygon"]\',
+    post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "ethereum", "gnosis", "optimism", "polygon"]\',
                                 "project",
                                 "balancer",
                                 \'["thetroyharris", "victorstefenon"]\') }}'
@@ -12,6 +12,7 @@
 {% set balancer_models = [
     ref('balancer_v2_arbitrum_transfers_bpt'),
     ref('balancer_v2_avalanche_c_transfers_bpt'),
+    ref('balancer_v2_base_transfers_bpt'),
     ref('balancer_v2_ethereum_transfers_bpt'),
     ref('balancer_v2_gnosis_transfers_bpt'),
     ref('balancer_v2_optimism_transfers_bpt'),
