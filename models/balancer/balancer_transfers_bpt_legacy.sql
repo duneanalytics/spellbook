314d3eeaba713da39da7c658adad3f8e7b6b3bac
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-08-09 10:57:50 -0400

    Update balancer_transfers_bpt_legacy.sql

--- models/balancer/balancer_transfers_bpt_legacy.sql
+++ models/balancer/balancer_transfers_bpt_legacy.sql
@@ -2,7 +2,7 @@
     schema = 'balancer',
 tags=['legacy'],
     alias = alias('transfers_bpt', legacy_model=True),
-    post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "ethereum", "gnosis", "optimism", "polygon"]\',
+    post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "ethereum", "gnosis", "optimism", "polygon"]\',
                                 "project",
                                 "balancer",
                                 \'["thetroyharris", "victorstefenon"]\') }}'
@@ -12,6 +12,7 @@ tags=['legacy'],
 {% set balancer_models = [
     ref('balancer_v2_arbitrum_transfers_bpt_legacy'),
     ref('balancer_v2_avalanche_c_transfers_bpt_legacy'),
+    ref('balancer_v2_base_transfers_bpt_legacy'),
     ref('balancer_v2_ethereum_transfers_bpt_legacy'),
     ref('balancer_v2_gnosis_transfers_bpt_legacy'),
     ref('balancer_v2_optimism_transfers_bpt_legacy'),
