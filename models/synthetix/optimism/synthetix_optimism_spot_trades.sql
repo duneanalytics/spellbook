f151da1e662939bb4f316e2ff69b694cf6afd741
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-08-10 15:33:53 +0200

    T-32182 Migrate synthetix

--- models/synthetix/optimism/synthetix_optimism_spot_trades.sql
+++ models/synthetix/optimism/synthetix_optimism_spot_trades.sql
@@ -1,11 +1,11 @@
-{{ config(
+{{ config(tags=['dunesql'],
     schema = 'synthetix_optimism',
     alias = alias('spot_trades'),
-    partition_by = ['block_date'],
+    partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address'],
+    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index'],
     post_hook='{{ expose_spells(\'["optimism"]\',
                                 "project",
                                 "synthetix",
@@ -22,19 +22,18 @@ WITH dexs AS
          t.evt_block_time AS block_time
         , t.evt_block_number
         ,'toAddress' AS taker
-        ,'' AS maker
+        ,CAST(NULL AS VARBINARY) AS maker
         ,t.toAmount AS token_bought_amount_raw 
         ,t.fromAmount AS token_sold_amount_raw
-        ,cast(NULL as double) AS amount_usd
-        ,'' AS token_bought_address
-        ,'' AS token_sold_address
+        ,NULL AS amount_usd
+        ,0x AS token_bought_address
+        ,0x AS token_sold_address
         -- we need to map token symbols. This is not ideal.
         ,trim('\u0000' from unhex(substring(toCurrencyKey, 3))) AS token_bought_symbol
         ,trim('\u0000' from unhex(substring(fromCurrencyKey, 3))) AS token_sold_symbol
 
         ,t.contract_address as project_contract_address
         ,t.evt_tx_hash AS tx_hash
-        ,'' AS trace_address
         ,t.evt_index
     FROM
         {{ source('synthetix_optimism', 'SNX_evt_SynthExchange') }} t
@@ -47,7 +46,8 @@ SELECT
      'optimism' AS blockchain
     ,'synthetix' AS project
     ,'1' AS version
-    ,TRY_CAST(date_trunc('DAY', dexs.block_time) AS date) AS block_date
+    ,TRY_CAST(date_trunc('day', dexs.block_time) AS date) AS block_date
+    ,CAST(date_trunc('month', dexs.block_time) AS date) AS block_month
     ,dexs.block_time
     ,erc20a.symbol AS token_bought_symbol
     ,erc20b.symbol AS token_sold_symbol
@@ -57,8 +57,8 @@ SELECT
     end as token_pair
     ,dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount
     ,dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount
-    ,CAST(dexs.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw
-    ,CAST(dexs.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw
+    ,dexs.token_bought_amount_raw  AS token_bought_amount_raw
+    ,dexs.token_sold_amount_raw  AS token_sold_amount_raw
     ,coalesce(
         dexs.amount_usd
         ,(dexs.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
@@ -66,20 +66,19 @@ SELECT
     ) AS amount_usd
     ,erc20a.contract_address AS token_bought_address
     ,erc20b.contract_address AS token_sold_address
-    ,tx.from AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
+    ,tx."from" AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
     ,dexs.maker
     ,dexs.project_contract_address
     ,dexs.tx_hash
-    ,tx.from AS tx_from
+    ,tx."from" AS tx_from
     ,tx.to AS tx_to
-    ,dexs.trace_address
     ,dexs.evt_index
 FROM dexs
 INNER JOIN {{ source('optimism', 'transactions') }} tx
     ON tx.hash = dexs.tx_hash
     AND tx.block_number = dexs.evt_block_number
     {% if not is_incremental() %}
-    AND tx.block_time >= '{{project_start_date}}'
+    AND tx.block_time >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     AND tx.block_time >= date_trunc('day', now() - interval '1' month)
@@ -95,7 +94,7 @@ LEFT JOIN {{ source('prices', 'usd') }} p_bought
     AND p_bought.contract_address = erc20a.contract_address
     AND p_bought.blockchain = 'optimism'
     {% if not is_incremental() %}
-    AND p_bought.minute >= '{{project_start_date}}'
+    AND p_bought.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     AND p_bought.minute >= date_trunc('day', now() - interval '1 month')
@@ -105,9 +104,8 @@ LEFT JOIN {{ source('prices', 'usd') }} p_sold
     AND p_sold.contract_address = erc20b.contract_address
     AND p_sold.blockchain = 'optimism'
     {% if not is_incremental() %}
-    AND p_sold.minute >= '{{project_start_date}}'
+    AND p_sold.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     AND p_sold.minute >= date_trunc('day', now() - interval '1 month')
     {% endif %}
-;
