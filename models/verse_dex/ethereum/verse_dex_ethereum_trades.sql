e9912ee548523bdabf5a0a922840d5450d8dcbcb
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-08-10 18:18:00 +0200

    double quotes fix

--- models/verse_dex/ethereum/verse_dex_ethereum_trades.sql
+++ models/verse_dex/ethereum/verse_dex_ethereum_trades.sql
@@ -33,7 +33,7 @@ with dexs as (
         inner join {{ source('verse_dex_ethereum', 'Factory_evt_PairCreated') }} f 
             on f.pair = t.contract_address
     {% if is_incremental() %}
-    WHERE t.evt_block_time >= date_trunc("day", now() - interval '7' day)
+    WHERE t.evt_block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 )
 select
@@ -74,7 +74,7 @@ inner join {{ source('ethereum', 'transactions') }} tx
     and tx.block_time >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and tx.block_time >= date_trunc("day", now() - interval '7' day)
+    and tx.block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 left join {{ ref('tokens_erc20') }} erc20a
     on erc20a.contract_address = dexs.token_bought_address
@@ -90,7 +90,7 @@ left join {{ source('prices', 'usd') }} p_bought
     and p_bought.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and p_bought.minute >= date_trunc("day", now() - interval '7' day)
+    and p_bought.minute >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 left join {{ source('prices', 'usd') }} p_sold
     on p_sold.minute = date_trunc('minute', dexs.block_time)
@@ -100,5 +100,5 @@ left join {{ source('prices', 'usd') }} p_sold
     and p_sold.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
-    and p_sold.minute >= date_trunc("day", now() - interval '7' day)
+    and p_sold.minute >= date_trunc('day', now() - interval '7' day)
     {% endif %}
