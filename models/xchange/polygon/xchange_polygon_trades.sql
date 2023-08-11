9dd1aadc6162b04be6dc6ddfa7b1a6905805e457
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-08-11 16:43:51 +0200

    remove trace address

--- models/xchange/polygon/xchange_polygon_trades.sql
+++ models/xchange/polygon/xchange_polygon_trades.sql
@@ -5,7 +5,7 @@
     ,materialized = 'incremental'
     ,file_format = 'delta'
     ,incremental_strategy = 'merge'
-    ,unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address']
+    ,unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index']
     ,post_hook='{{ expose_spells(\'["polygon"]\',
                                 "project",
                                 "xchange",
