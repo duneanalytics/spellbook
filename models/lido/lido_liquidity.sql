8de14485390a13a362f7a6102878b987a33041fd
ppclunghe <pipistrella@list.ru>
2023-08-11 20:36:14 +0200

    fix liquidity

--- models/lido/lido_liquidity.sql
+++ models/lido/lido_liquidity.sql
@@ -37,6 +37,36 @@ from day_seq
 cross join unnest(day) as days(day)
   )
 
+, volumes as (
+select u.call_block_time as time,  
+cast(output_0 as double) as steth, cast(_wstETHAmount as double) as wsteth 
+from  {{source('lido_ethereum','WstETH_call_unwrap')}} u 
+where call_success = TRUE 
+union all
+select u.call_block_time, cast(_stETHAmount as double) as steth, cast(output_0 as double) as wsteth 
+from  {{source('lido_ethereum','WstETH_call_wrap')}} u
+where call_success = TRUE 
+)
+
+
+, wsteth_rate as (
+SELECT
+  day, rate as rate0, value_partition, first_value(rate) over (partition by value_partition order by day) as rate,
+  lead(day,1,date_trunc('day', now() + interval '1' day)) over(order by day) as next_day
+  
+FROM (
+select day, rate,
+sum(case when rate is null then 0 else 1 end) over (order by day) as value_partition
+from (
+select  date_trunc('day', d.day) as day, 
+       sum(cast(steth as double))/sum(cast(wsteth as double))  AS rate
+from dates  d
+left join volumes v on date_trunc('day', v.time)  = date_trunc('day', d.day) 
+group by 1
+))
+
+)
+
 , pools as (  
 SELECT *
 FROM (
@@ -66,9 +96,10 @@ FROM (
 )
 
 , pools_per_dates as (
-  select day, pool
+  select day, pool,rate
   from dates
   join (select distinct pool from pools) on 1=1
+  left join wsteth_rate on dates.day = wsteth_rate.day
 )
 
 
@@ -82,7 +113,7 @@ SELECT pool_name,
            main_token_symbol,
            paired_token, 
            paired_token_symbol, 
-           main_token_reserve, 
+           case when main_token_symbol = 'stETH' then main_token_reserve* rate else main_token_reserve end as main_token_reserve, 
            paired_token_reserve,
            main_token_usd_reserve, 
            paired_token_usd_reserve, 
