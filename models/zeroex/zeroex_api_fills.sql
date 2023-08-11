e09c30625bbad1b2178e3d6cdcc4e2728c65c7fb
RantumBits <rantum@rantum.xyz>
2023-08-11 12:37:15 -0700

    use legacy models in multichain

--- models/zeroex/zeroex_api_fills.sql
+++ models/zeroex/zeroex_api_fills.sql
@@ -9,13 +9,13 @@
 }}
 
 {% set zeroex_models = [  
-ref('zeroex_arbitrum_api_fills')
-,ref('zeroex_avalanche_c_api_fills')
-,ref('zeroex_ethereum_api_fills')
-,ref('zeroex_fantom_api_fills')
-,ref('zeroex_optimism_api_fills')
-,ref('zeroex_polygon_api_fills')
-,ref('zeroex_bnb_api_fills')
+ref('zeroex_arbitrum_api_fills_legacy')
+,ref('zeroex_avalanche_c_api_fills_legacy')
+,ref('zeroex_ethereum_api_fills_legacy')
+,ref('zeroex_fantom_api_fills_legacy')
+,ref('zeroex_optimism_api_fills_legacy')
+,ref('zeroex_polygon_api_fills_legacy')
+,ref('zeroex_bnb_api_fills_legacy')
 ,ref('zeroex_celo_api_fills')
 ] %}
 
