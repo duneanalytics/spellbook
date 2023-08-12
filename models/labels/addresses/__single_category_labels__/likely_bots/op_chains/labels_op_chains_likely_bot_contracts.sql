fdd04a304ebdd8c4f52da35c831946860088f692
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-08-12 16:48:20 -0400

    add chain

--- models/labels/addresses/__single_category_labels__/likely_bots/op_chains/labels_op_chains_likely_bot_contracts.sql
+++ models/labels/addresses/__single_category_labels__/likely_bots/op_chains/labels_op_chains_likely_bot_contracts.sql
@@ -25,12 +25,14 @@ SELECT *,
 
 FROM (
     {% for chain in op_chains %}
-        SELECT to AS contract, 
+        SELECT 
+            '{{chain}}' as blockchain,
+            to AS contract, 
             SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ source('erc20_' + chain,'evt_transfer') }} r WHERE t.hash = r.evt_tx_hash AND t.block_number = r.evt_block_number) THEN 1 ELSE 0 END) AS num_erc20_tfer_txs,
-            SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ ref('nft_transfers') }} r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND blockchain = '{{chain}}') THEN 1 ELSE 0 END) AS num_nft_tfer_txs,
+            SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ ref('nft_transfers') }} r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND r.blockchain = '{{chain}}') THEN 1 ELSE 0 END) AS num_nft_tfer_txs,
             
             SUM(CASE WHEN EXISTS (SELECT 1 FROM {{ source('erc20_' + chain,'evt_transfer') }} r WHERE t.hash = r.evt_tx_hash AND t.block_number = r.evt_block_number) THEN 1 
-                    WHEN EXISTS (SELECT 1 FROM {{ ref('nft_transfers') }} r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND blockchain = '{{chain}}') THEN 1
+                    WHEN EXISTS (SELECT 1 FROM {{ ref('nft_transfers') }} r WHERE t.hash = r.tx_hash AND t.block_number = r.block_number AND r.blockchain = '{{chain}}') THEN 1
                 ELSE 0 END) AS num_token_tfer_txs,
                 
             0 /*SUM(CASE WHEN EXISTS (SELECT 1 FROM [[ ref('dex_trades') ]] r WHERE t.hash = r.tx_hash AND t.block_time = r.block_time AND blockchain = '{{chain}}') THEN 1 ELSE 0 END)*/ AS num_dex_trade_txs,
