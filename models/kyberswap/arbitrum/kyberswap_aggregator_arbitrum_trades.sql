3761f6c0134c93ca86c8d5a5c63f0b9a4931233e
nhd98z <nhd98z@gmail.com>
2023-08-13 00:34:09 +0700

    imitate dodo

--- models/kyberswap/arbitrum/kyberswap_aggregator_arbitrum_trades.sql
+++ models/kyberswap/arbitrum/kyberswap_aggregator_arbitrum_trades.sql
@@ -1,7 +1,5 @@
 {{ config
 (
-    tags=['dunesql'],
-
     alias = alias('aggregator_trades'),
     partition_by = ['block_date'],
     materialized = 'incremental',
@@ -52,71 +50,70 @@ SELECT
     ,case
         when lower(erc20a.symbol) > lower(erc20b.symbol) then concat(erc20b.symbol, '-', erc20a.symbol)
         else concat(erc20a.symbol, '-', erc20b.symbol)
-        end as token_pair
+    end as token_pair
     ,dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount
     ,dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount
     ,CAST(dexs.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw
     ,CAST(dexs.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw
     ,coalesce(
         dexs.amount_usd
-    -- TODO: need to check if 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
         ,(dexs.token_bought_amount_raw / power(10, (CASE dexs.token_bought_address WHEN '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN 18 ELSE p_bought.decimals END))) * (CASE dexs.token_bought_address WHEN '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN  p_eth.price ELSE p_bought.price END)
         ,(dexs.token_sold_amount_raw / power(10, (CASE dexs.token_sold_address WHEN '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN 18 ELSE p_sold.decimals END))) * (CASE dexs.token_sold_address WHEN '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' THEN  p_eth.price ELSE p_sold.price END)
     ) as amount_usd
     ,dexs.token_bought_address
     ,dexs.token_sold_address
---    ,coalesce(dexs.taker, tx.from) AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
-    ,dexs.taker AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
+    ,coalesce(dexs.taker, tx.from) AS taker -- subqueries rely on this COALESCE to avoid redundant joins with the transactions table
     ,dexs.maker
     ,dexs.project_contract_address
     ,dexs.tx_hash
---    ,tx.from AS tx_from
+    ,tx.from AS tx_from
     ,tx.to AS tx_to
     ,dexs.trace_address
     ,dexs.evt_index
 FROM dexs
-    INNER JOIN {{ source('arbitrum', 'transactions')}} tx ON dexs.tx_hash = tx.hash
-        {% if not is_incremental() %}
-            AND tx.block_time >= '{{project_start_date}}'
-        {% endif %}
-        {% if is_incremental() %}
-            AND tx.block_time >= date_trunc("day", now() - interval '1 week')
-        {% endif %}
-    LEFT JOIN {{ ref('tokens_erc20') }} erc20a
-        ON erc20a.contract_address = dexs.token_bought_address
-        AND erc20a.blockchain = 'arbitrum'
-    LEFT JOIN {{ ref('tokens_erc20') }} erc20b
-        ON erc20b.contract_address = dexs.token_sold_address
-        AND erc20b.blockchain = 'arbitrum'
-    LEFT JOIN {{ source('prices', 'usd') }} p_bought
-        ON p_bought.minute = date_trunc('minute', dexs.block_time)
-        AND p_bought.contract_address = dexs.token_bought_address
-        AND p_bought.blockchain = 'arbitrum'
-        {% if not is_incremental() %}
-            AND p_bought.minute >= '{{project_start_date}}'
-        {% endif %}
-        {% if is_incremental() %}
-            AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
-        {% endif %}
-    LEFT JOIN {{ source('prices', 'usd') }} p_sold
-        ON p_sold.minute = date_trunc('minute', dexs.block_time)
-        AND p_sold.contract_address = dexs.token_sold_address
-        AND p_sold.blockchain = 'arbitrum'
-        {% if not is_incremental() %}
-            AND p_sold.minute >= '{{project_start_date}}'
-        {% endif %}
-        {% if is_incremental() %}
-            AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
-        {% endif %}
-    LEFT JOIN {{ source('prices', 'usd') }} p_eth
-        ON p_eth.minute = date_trunc('minute', dexs.block_time)
-        AND p_eth.blockchain is null
-        AND p_eth.symbol = 'ETH'
-        {% if not is_incremental() %}
-            AND p_eth.minute >= '{{project_start_date}}'
-        {% endif %}
-        {% if is_incremental() %}
-            AND p_eth.minute >= date_trunc("day", now() - interval '1 week')
-        {% endif %}
-    WHERE dexs.token_bought_address <> dexs.token_sold_address
+INNER JOIN {{ source('arbitrum', 'transactions')}} tx
+    ON dexs.tx_hash = tx.hash
+    {% if not is_incremental() %}
+    AND tx.block_time >= '{{project_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+    AND tx.block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+LEFT JOIN {{ ref('tokens_erc20') }} erc20a
+    ON erc20a.contract_address = dexs.token_bought_address
+    AND erc20a.blockchain = 'arbitrum'
+LEFT JOIN {{ ref('tokens_erc20') }} erc20b
+    ON erc20b.contract_address = dexs.token_sold_address
+    AND erc20b.blockchain = 'arbitrum'
+LEFT JOIN {{ source('prices', 'usd') }} p_bought
+    ON p_bought.minute = date_trunc('minute', dexs.block_time)
+    AND p_bought.contract_address = dexs.token_bought_address
+    AND p_bought.blockchain = 'arbitrum'
+    {% if not is_incremental() %}
+    AND p_bought.minute >= '{{project_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+    AND p_bought.minute >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+LEFT JOIN {{ source('prices', 'usd') }} p_sold
+    ON p_sold.minute = date_trunc('minute', dexs.block_time)
+    AND p_sold.contract_address = dexs.token_sold_address
+    AND p_sold.blockchain = 'arbitrum'
+    {% if not is_incremental() %}
+    AND p_sold.minute >= '{{project_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+    AND p_sold.minute >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+LEFT JOIN {{ source('prices', 'usd') }} p_eth
+    ON p_eth.minute = date_trunc('minute', dexs.block_time)
+    AND p_eth.blockchain is null
+    AND p_eth.symbol = 'ETH'
+    {% if not is_incremental() %}
+    AND p_eth.minute >= '{{project_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+    AND p_eth.minute >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+WHERE dexs.token_bought_address <> dexs.token_sold_address
 ;
