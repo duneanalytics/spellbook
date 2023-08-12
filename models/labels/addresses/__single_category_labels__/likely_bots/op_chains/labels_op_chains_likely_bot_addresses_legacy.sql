259aca0940c82982beb38aae8e098d4025daca22
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-08-12 10:26:52 -0400

    op_chains bots

---
+++ models/labels/addresses/__single_category_labels__/likely_bots/op_chains/labels_op_chains_likely_bot_addresses_legacy.sql
@@ -0,0 +1,9 @@
+{{
+    config(
+	tags=['legacy'],
+	
+        alias = alias('likely_bot_addresses', legacy_model=True)
+    )
+}}
+
+SELECT 1
---
+++ models/labels/addresses/__single_category_labels__/likely_bots/op_chains/labels_op_chains_likely_bot_contracts_legacy.sql
@@ -0,0 +1,10 @@
+{{
+    config(
+	tags=['legacy'],
+	
+        alias = alias('likely_bot_contracts', legacy_model=True)
+    )
+}}
+
+
+SELECT 1
