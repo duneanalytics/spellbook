146e1397c139826b4ad94403f66204c14061530d
chef-seaweed <129702169+hsrvc@users.noreply.github.com>
2023-08-13 20:53:01 +0800

    remove to keep clean

--- models/trader_joe/avalanche_c/trader_joe_avalanche_c_trades_legacy.sql
+++ models/trader_joe/avalanche_c/trader_joe_avalanche_c_trades_legacy.sql
@@ -4,11 +4,6 @@
         )
 }}
 
-{% set trader_joe_models = [
-ref('trader_joe_v1_avalanche_c_trades_legacy')
-,ref('trader_joe_v2_avalanche_c_trades_legacy')
-] %}
-
 -- DUMMY TABLE, WILL BE REMOVED SOON
 select 
     1
--- models/trader_joe/bnb/trader_joe_bnb_trades_legacy.sql
+++ models/trader_joe/bnb/trader_joe_bnb_trades_legacy.sql
@@ -5,11 +5,6 @@
     )
 }}
 
-{% set trader_joe_models = [
-    ref('trader_joe_v2_bnb_trades_legacy')
-,   ref('trader_joe_v2_1_bnb_trades_legacy')  
-] %}
-
 -- DUMMY TABLE, WILL BE REMOVED SOON
 select 
     1
