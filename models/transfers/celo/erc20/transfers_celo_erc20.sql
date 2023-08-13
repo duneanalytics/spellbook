abe146fb7368bac6aee06ad2e0c6e4224dd16d16
tomfutago <tomfutago@gmail.com>
2023-08-13 14:46:47 +0100

    amended transfers_celo_erc20, added test

--- models/transfers/celo/erc20/transfers_celo_erc20.sql
+++ models/transfers/celo/erc20/transfers_celo_erc20.sql
@@ -1,67 +1,89 @@
 {{ config(
-    tags = ['dunesql'],
-    materialized='view', alias = alias('erc20'),
-        post_hook='{{ expose_spells(\'["celo"]\',
-                                    "sector",
-                                    "transfers",
-                                    \'["soispoke", "dot2dotseurat", "tschubotz", "tomfutago"]\') }}') }}
+    alias=alias('erc20'),
+    tags=['dunesql'],
+    materialized='incremental',
+    file_format='delta',
+    incremental_strategy='merge',
+    unique_key='unique_transfer_id',
+    post_hook='{{ expose_spells(\'["celo"]\',
+                                "sector",
+                                "transfers",
+                                \'["soispoke", "dot2dotseurat", "tschubotz", "tomfutago"]\') }}') }}
 
 with
     sent_transfers as (
         select
-            'send-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' || CAST(to AS VARCHAR(100)) as unique_transfer_id,
+            'send-' || cast(evt_tx_hash as varchar) || '-' || cast(evt_index as varchar) || '-' || cast("to" as varchar) as unique_transfer_id,
             to as wallet_address,
             contract_address as token_address,
             evt_block_time,
-            value as amount_raw
+            cast(value as double) as amount_raw
         from
             {{ source('erc20_celo', 'evt_transfer') }}
-    )
-
-    ,
+        where 1=1
+            {% if is_incremental() %} -- this filter will only be applied on an incremental run
+            and evt_block_time >= date_trunc('day', now() - interval '7' day)
+            {% endif %}
+    ),
+    
     received_transfers as (
         select
-        'receive-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' || CAST("from" AS VARCHAR(100)) as unique_transfer_id,
-        "from" as wallet_address,
-        contract_address as token_address,
-        evt_block_time,
-        '-' || CAST(value AS VARCHAR(100)) as amount_raw
+            'receive-' || cast(evt_tx_hash as varchar) || '-' || cast (evt_index as varchar) || '-' || cast("from" as varchar) as unique_transfer_id,
+            "from" as wallet_address,
+            contract_address as token_address,
+            evt_block_time,
+            (-1) * cast(value as double) as amount_raw
         from
             {{ source('erc20_celo', 'evt_transfer') }}
-    )
+        where 1=1
+            {% if is_incremental() %} -- this filter will only be applied on an incremental run
+            and evt_block_time >= date_trunc('day', now() - interval '7' day)
+            {% endif %}
 
-    ,
+    ),
+
     deposited_wcelo as (
         select
-            'deposit-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' ||  CAST(dst AS VARCHAR(100)) as unique_transfer_id,
-            dst as wallet_address,
+            'deposit-' || cast(tx_hash as varchar) || '-' || cast (index as varchar) || '-' ||  cast(bytearray_substring(topic1,13,20) as varchar) as unique_transfer_id,
+            bytearray_substring(topic1,13,20) as wallet_address,
             contract_address as token_address,
-            evt_block_time,
-            wad as amount_raw
+            block_time as evt_block_time,
+            cast(bytearray_to_uint256(data) as double) as amount_raw
         from
-            {{ source('celo_celo', 'wcelo_evt_deposit') }}
-    )
+            {{ source('celo', 'logs') }}
+        where contract_address = 0x3Ad443d769A07f287806874F8E5405cE3Ac902b9 --Wrapped Celo
+            and topic0 = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef --deposit
+            and bytearray_substring(topic1,13,20) <> 0x0000000000000000000000000000000000000000
+            {% if is_incremental() %} -- this filter will only be applied on an incremental run
+            and block_time >= date_trunc('day', now() - interval '7' day)
+            {% endif %}
+    ),
 
-    ,
     withdrawn_wcelo as (
         select
-            'withdraw-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' ||  CAST(src AS VARCHAR(100)) as unique_transfer_id,
-            src as wallet_address,
+            'withdraw-' || cast(tx_hash as varchar) || '-' || cast (index as varchar) || '-' ||  cast(bytearray_substring(topic1,13,20) as varchar) as unique_transfer_id,
+            bytearray_substring(topic1,13,20) as wallet_address,
             contract_address as token_address,
-            evt_block_time,
-            '-' || CAST(wad AS VARCHAR(100)) as amount_raw
+            block_time as evt_block_time,
+            (-1) * cast(bytearray_to_uint256(data) as double) as amount_raw
         from
-            {{ source('celo_celo', 'wcelo_evt_withdrawal') }}
+            {{ source('celo', 'logs') }}
+        where contract_address = 0x3Ad443d769A07f287806874F8E5405cE3Ac902b9 --Wrapped Celo
+            and topic0 = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef --withdrawal
+            and bytearray_substring(topic1,13,20) = 0x0000000000000000000000000000000000000000
+            {% if is_incremental() %} -- this filter will only be applied on an incremental run
+            and block_time >= date_trunc('day', now() - interval '7' day)
+            {% endif %}
     )
     
-select unique_transfer_id, 'celo' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
+select unique_transfer_id, 'celo' as blockchain, wallet_address, token_address, evt_block_time, cast(amount_raw as varchar(100)) as amount_raw
 from sent_transfers
 union
-select unique_transfer_id, 'celo' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
+select unique_transfer_id, 'celo' as blockchain, wallet_address, token_address, evt_block_time, cast(amount_raw as varchar(100)) as amount_raw
 from received_transfers
 union
-select unique_transfer_id, 'celo' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
+select unique_transfer_id, 'celo' as blockchain, wallet_address, token_address, evt_block_time, cast(amount_raw as varchar(100)) as amount_raw
 from deposited_wcelo
 union
-select unique_transfer_id, 'celo' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
+select unique_transfer_id, 'celo' as blockchain, wallet_address, token_address, evt_block_time, cast(amount_raw as varchar(100)) as amount_raw
 from withdrawn_wcelo
