957d491dc3bd396cd2fc43c2e61cf56bd95dfe01
sharkxff <xffzheng@163.com>
2023-08-14 18:27:22 +0800

    update

--- tests/nexusmutual/ethereum/nexusmutual_ethereum_quotation_trades_test.sql
+++ tests/nexusmutual/ethereum/nexusmutual_ethereum_quotation_trades_test.sql
@@ -5,7 +5,7 @@ WITH unit_tests as
 then True else False end as test
 FROM {{ ref('nexusmutual_ethereum_quotation_trades') }} actual
 JOIN {{ ref('nexusmutual_ethereum_trades_seed') }} test 
-    ON test.tx_hash = actual.tx_hash AND test.evt_index = actual.evt_index
+    ON test.evt_tx_hash = actual.evt_tx_hash AND test.evt_block_number = actual.evt_block_number
 )
 select count(case when test = false then 1 else null end)/count(*) as pct_mismatch, count(*) as count_rows
 from unit_tests
