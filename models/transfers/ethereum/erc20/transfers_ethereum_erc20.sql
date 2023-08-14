96baa5469254772df07ac69f2ed01a3bd8497075
lajarre <alexandre.hajjar@gmail.com>
2023-08-14 18:05:43 +0100

    Fix backquotes

--- models/transfers/ethereum/erc20/transfers_ethereum_erc20.sql
+++ models/transfers/ethereum/erc20/transfers_ethereum_erc20.sql
@@ -9,8 +9,8 @@
 with
     sent_transfers as (
         select
-            CAST('send' AS VARCHAR(4)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(`to` AS VARCHAR(100)) as unique_transfer_id,
-            `to` as wallet_address,
+            CAST('send' AS VARCHAR(4)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST("to" AS VARCHAR(100)) as unique_transfer_id,
+            "to" as wallet_address,
             contract_address as token_address,
             evt_block_time,
             value as amount_raw
@@ -21,8 +21,8 @@ with
     ,
     received_transfers as (
         select
-        CAST('receive' AS VARCHAR(7)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(`from` AS VARCHAR(100)) as unique_transfer_id,
-        `from` as wallet_address,
+        CAST('receive' AS VARCHAR(7)) || CAST('-' AS VARCHAR(1)) || CAST(evt_tx_hash AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(evt_index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST("from" AS VARCHAR(100)) as unique_transfer_id,
+        "from" as wallet_address,
         contract_address as token_address,
         evt_block_time,
         '-' || CAST(value AS VARCHAR(100)) as amount_raw
