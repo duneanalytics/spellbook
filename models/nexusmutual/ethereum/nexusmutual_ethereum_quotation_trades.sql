9ba8db303b1a74a04dffe1a3f77353730632cc3d
zhengxiaofei <xffzheng@163.com>
2023-08-14 18:19:32 +0800

    explanatory tag

--- models/nexusmutual/ethereum/nexusmutual_ethereum_quotation_trades.sql
+++ models/nexusmutual/ethereum/nexusmutual_ethereum_quotation_trades.sql
@@ -1,6 +1,6 @@
 {{ config(
     alias = alias('quotation_trades'),
-    tags=['dunesql'],
+    -- tags=['dunesql'],
     partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
