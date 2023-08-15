7d9f7fd6042f1eb8dff3f7847ded672a5f4d4b5d
denver <denverbaumgartner@gmail.com>
2023-08-15 19:19:33 -0400

    trino syntax

--- models/rubicon/arbitrum/rubicon_arbitrum_trades.sql
+++ models/rubicon/arbitrum/rubicon_arbitrum_trades.sql
@@ -72,7 +72,7 @@ SELECT
     dexs.maker,
     dexs.project_contract_address,
     dexs.tx_hash,
-    tx.from AS tx_from,
+    tx."from" AS tx_from,
     tx.to AS tx_to,
     dexs.trace_address,
     dexs.evt_index
