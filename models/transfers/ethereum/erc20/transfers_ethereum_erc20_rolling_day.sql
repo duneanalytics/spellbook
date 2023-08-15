f241a7311085f1e6e856ee97d0b8ae2acdfe8763
lajarre <alexandre.hajjar@gmail.com>
2023-08-15 11:59:04 +0100

    Positive as bool

--- models/transfers/ethereum/erc20/transfers_ethereum_erc20_rolling_day.sql
+++ models/transfers/ethereum/erc20/transfers_ethereum_erc20_rolling_day.sql
@@ -11,7 +11,7 @@
             symbol,
             NOW() as last_updated,
             row_number() over (partition by token_address, wallet_address order by day desc) as recency_index,
-            sum(amount_raw) over (
+            sum(case amount_positive then amount_raw else cast(0 as uint256) end) - sum(case amount_positive then cast(0 as uint256) else amount_raw end) over (
                 partition by token_address, wallet_address order by day
             ) as amount_raw,
             sum(amount) over (
