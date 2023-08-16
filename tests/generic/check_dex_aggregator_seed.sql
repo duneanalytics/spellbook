b58cad981302f0a9db29f6350dec0f235232728c
antonio-mendes <soeiromendes.antonio@gmail.com>
2023-08-16 15:35:22 +0100

    fix problems with seed for dex_aggregator still including trace_address

--- tests/generic/check_dex_aggregator_seed.sql
+++ tests/generic/check_dex_aggregator_seed.sql
@@ -4,7 +4,7 @@
 
     {%- set seed_file = ref('dex_aggregator_seed') -%}
     {%- set seed_check_columns = ['token_bought_address','token_sold_address'] -%}
-    {%- set seed_matching_columns = ['block_date','blockchain','project','version','tx_hash','evt_index','trace_address'] -%}
+    {%- set seed_matching_columns = ['block_date','blockchain','project','version','tx_hash','evt_index'] -%}
     {%- set filter = {'blockchain':blockchain, 'project':project, 'version':version} -%}
 
     {{ check_seed_macro(model,seed_file,seed_matching_columns,seed_check_columns,filter) }}
