39f60da796ed3b69e4a32e9bdf456107c27a2620
lajarre <alexandre.hajjar@gmail.com>
2023-08-16 23:32:37 +0100

    Move erc20 noncompliant to 1100

--- tests/balances/ethereum/balances_ethereum_erc20_noncompliant_assert_LT_1100.sql
+++ tests/balances/ethereum/balances_ethereum_erc20_noncompliant_assert_LT_1100.sql
@@ -1,7 +1,7 @@
--- There were 347 noncompliant tokens at time of model creation
--- This tests confirms there are not more then 500.
+-- There were 1005 noncompliant tokens at time of model creation
+-- This tests confirms there are not more then 1100.
 -- If this threshold is exceeded, we should investigate as to why.
 
 select count(*) as count
 from {{ ref('balances_ethereum_erc20_noncompliant') }}
-having count(*) > 500
+having count(*) > 1100
