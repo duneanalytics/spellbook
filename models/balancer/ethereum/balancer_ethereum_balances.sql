7e1da819faf2b4110bce97be6777d445c448708d
Vinicius Abussafi | Balancer <131974393+viniabussafi@users.noreply.github.com>
2023-08-16 08:48:00 +0100

    Update balancer_ethereum_balances.sql

--- models/balancer/ethereum/balancer_ethereum_balances.sql
+++ models/balancer/ethereum/balancer_ethereum_balances.sql
@@ -24,7 +24,7 @@ joins AS (
     UNION ALL
     SELECT e."to" as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, SUM(CAST(value as int256)) AS amount
     FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
-    WHERE CAST(e."to" as VARCHAR)= '{{balancer_contract}}'
+    WHERE e."to" = {{balancer_contract}}
     GROUP BY 1, 2, 3
 ),
 
@@ -36,7 +36,7 @@ exits AS (
     UNION ALL
     SELECT e."from" as pool, date_trunc('day', e.evt_block_time) AS day, e.contract_address AS token, -SUM(CAST(value as int256)) AS amount
     FROM {{ source('erc20_ethereum', 'evt_transfer') }} e
-    WHERE CAST(e."from" as VARCHAR) = '{{balancer_contract}}'
+    WHERE e."from" = {{balancer_contract}}
     GROUP BY 1, 2, 3
 ),
 
