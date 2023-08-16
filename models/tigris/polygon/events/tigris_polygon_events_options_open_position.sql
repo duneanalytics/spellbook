6dd3d84a320da097ac53786ecede9054739532b1
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-08-16 20:49:56 +0100

    rewrite

--- models/tigris/polygon/events/tigris_polygon_events_options_open_position.sql
+++ models/tigris/polygon/events/tigris_polygon_events_options_open_position.sql
@@ -1,12 +1,12 @@
 {{ config(
     tags=['dunesql'],
     schema = 'tigris_polygon',
-    alias = alias('open_position'),
-    partition_by = ['day'],
+    alias = alias('options_open_position'),
+    partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['evt_block_time', 'evt_tx_hash', 'position_id']
+    unique_key = ['evt_block_time', 'evt_tx_hash', 'position_id', 'positions_contract']
     )
 }}
 
@@ -16,7 +16,7 @@ pairs as (
         SELECT 
             * 
         FROM 
-        {{ ref('tigris_v1_polygon_events_asset_added') }}
+        {{ ref('tigris_polygon_events_asset_added') }}
 ), 
 
 {% set open_position_tables = [
@@ -24,11 +24,13 @@ pairs as (
 
 ] %}
 
-open_position AS (
+open_position_v2 AS (
     {% for open_position in open_position_tables %}
         SELECT
             '{{ 'v2.' + loop.index | string }}' as version,
-            TRY_CAST(date_trunc('DAY', t.evt_block_time) AS date) as day, 
+            '2' as protocol_version,
+            CAST(date_trunc('DAY', t.evt_block_time) AS date) as day, 
+            CAST(date_trunc('MONTH', t.evt_block_time) AS date) as block_month, 
             evt_block_time,
             evt_tx_hash,
             evt_index, 
@@ -43,10 +45,12 @@ open_position AS (
             split_part( human_readable_seconds( CAST(json_extract_scalar(tradeInfo, '$.duration') as double) ),',',1) as options_period,
             from_hex(json_extract_scalar(tradeInfo, '$.referrer')) as referral,
             trader,
-            CAST(orderType as VARCHAR) as order_type 
+            CAST(orderType as VARCHAR) as order_type,
+            contract_address as project_contract_address
         FROM {{ source('tigristrade_v2_polygon', open_position) }} t
         INNER JOIN pairs ta
-            ON t.asset = ta.asset_id
+            ON CAST(json_extract_scalar(tradeInfo, '$.asset') as double) = CAST(ta.asset_id as double)
+            AND ta.protocol_version = '1'
         {% if is_incremental() %}
         WHERE t.evt_block_time >= date_trunc('day', now() - interval '7' day)
         {% endif %}
@@ -56,5 +60,12 @@ open_position AS (
     {% endfor %}
 )
 
-SELECT *
-FROM limit_orders
+SELECT 
+    a.*,
+    c.positions_contract 
+FROM 
+open_position_v2 a 
+INNER JOIN 
+{{ ref('tigris_polygon_events_contracts_positions') }} c 
+    ON a.project_contract_address = c.trading_contract
+    AND a.version = c.trading_contract_version
