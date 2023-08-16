244a7909d430ca7a2a1771c032914eca0829a0af
Hildebert Moulié <h.moulie@gmail.com>
2023-08-16 13:49:59 +0200

    add more entities

--- models/staking/ethereum/staking_ethereum_entities.sql
+++ models/staking/ethereum/staking_ethereum_entities.sql
@@ -121,6 +121,7 @@ FROM
     , (0x8103151e2377e78c04a3d2564e20542680ed3096, 'Node DAO', 'Node DAO', 'Liquid Staking')
     , (0x4befa2aa9c305238aa3e0b5d17eb20c045269e9d, 'RockX', 'RockX', 'Staking Pools')
     , (0xea674fdde714fd979de3edf0f56aa9716b898ec8, 'Ethermine', 'Ethermine', 'Staking Pools')
+    , (0x25e821b7197b146f7713c3b89b6a4d83516b912d, 'ether.fi', 'ether.fi', 'Liquid Staking')
     ) 
     x (address, entity, entity_unique_name, category)
 
