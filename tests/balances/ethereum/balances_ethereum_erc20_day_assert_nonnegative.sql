41cd3968c2256f2e1f4887a4a9bc6fc5d9a496bd
lajarre <alexandre.hajjar@gmail.com>
2023-08-17 14:46:36 +0100

    Reduce precision on nonnegative test

--- tests/balances/ethereum/balances_ethereum_erc20_day_assert_nonnegative.sql
+++ tests/balances/ethereum/balances_ethereum_erc20_day_assert_nonnegative.sql
@@ -3,7 +3,7 @@
 
 select amount
 from {{ ref('balances_ethereum_erc20_day') }} bal
-where round(amount/power(10, 18), 6) < 0
+where round(amount/power(10, 18), 0) < 0
 -- limiting to a selection of tokens because we haven't filtered out all non-compliant tokens
 and symbol in ('AAVE', 'DAI', 'UNI', 'LINK')
 and bal.day > now() - interval '2' day
