4eecc01d6d601b6d301003ed309e0d21fc3cbe75
Chris <96254264+addmorebass@users.noreply.github.com>
2023-08-17 09:19:55 -0500

    Added tokemak_address for view_tokemak_addresses

--- models/tokemak/ethereum/tokemak_ethereum_view_tokemak_addresses.sql
+++ models/tokemak/ethereum/tokemak_ethereum_view_tokemak_addresses.sql
@@ -7,14 +7,14 @@
              \'["addmorebass"]\') }}'
 ) }}
 
-WITH tokemak_ethereum_view_tokemak_addresses(address) AS (
-    SELECT '0x9e0bcE7ec474B481492610eB9dd5D69EB03718D5' AS address /*deployer*/
+WITH tokemak_ethereum_view_tokemak_addresses(tokemak_address) AS (
+    SELECT '0x9e0bcE7ec474B481492610eB9dd5D69EB03718D5' AS tokemak_address /*deployer*/
     UNION 
-    SELECT '0x90b6C61B102eA260131aB48377E143D6EB3A9d4B' AS address/*coordinator*/
+    SELECT '0x90b6C61B102eA260131aB48377E143D6EB3A9d4B' AS tokemak_address/*coordinator*/
     UNION 
-    SELECT '0xA86e412109f77c45a3BC1c5870b880492Fb86A14' AS address/*manager*/
+    SELECT '0xA86e412109f77c45a3BC1c5870b880492Fb86A14' AS tokemak_address/*manager*/
     UNION 
-    SELECT '0x8b4334d4812c530574bd4f2763fcd22de94a969b' as address /*treasury*/
+    SELECT '0x8b4334d4812c530574bd4f2763fcd22de94a969b' as tokemak_address /*treasury*/
 )
 
-SELECT address FROM tokemak_ethereum_view_tokemak_addresses
+SELECT tokemak_address FROM tokemak_ethereum_view_tokemak_addresses
