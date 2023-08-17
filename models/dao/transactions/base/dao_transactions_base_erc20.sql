b114d4282e819915d1adcf59007cf779de5d64e6
henrystats <65398020+henrystats@users.noreply.github.com>
2023-08-17 18:31:12 +0100

    Aragon app base - Reopen - Already approved - Ready for review  (#4100)
    * add aragon app base
    
    * fix typo
    
    * fix naming
    
    * update transactions and balances
    
    * fix synthax error
    
    * fix synthax error
    
    * another typo fix
    
    * fix array synthax
    
    * fix synthax
    
    * force push
    
    * remove null varbinary casts and add txns seed
    
    * comment comment in jinja
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

--- models/dao/transactions/base/dao_transactions_base_erc20.sql
+++ models/dao/transactions/base/dao_transactions_base_erc20.sql
@@ -1,15 +1,15 @@
 {{ config(
     tags = ['dunesql'],
-    alias = alias('transactions_ethereum_eth'),
+    alias = alias('transactions_base_erc20'),
     partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_date', 'blockchain', 'dao_creator_tool', 'dao', 'dao_wallet_address', 'tx_hash', 'tx_index', 'tx_type', 'trace_address', 'address_interacted_with', 'value', 'asset_contract_address', 'block_month']
+    unique_key = ['block_date', 'blockchain', 'dao_creator_tool', 'dao', 'dao_wallet_address', 'tx_hash', 'tx_index', 'tx_type', 'address_interacted_with', 'value', 'asset_contract_address']
     )
 }}
 
-{% set transactions_start_date = '2018-10-27' %}
+{% set transactions_start_date = '2023-08-02' %}
 
 WITH 
 
@@ -20,59 +20,55 @@ dao_tmp as (
             dao, 
             dao_wallet_address
         FROM 
-        {{ ref('dao_addresses_ethereum') }}
-        WHERE dao_wallet_address IS NOT NULL
+        {{ ref('dao_addresses_base') }}
+        WHERE dao_wallet_address IS NOT NULL 
         AND dao_wallet_address NOT IN (0x0000000000000000000000000000000000000001, 0x000000000000000000000000000000000000dead, 0x)
 ), 
 
 transactions as (
         SELECT 
-            block_time, 
-            tx_hash, 
-            0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee as token, 
+            evt_block_time as block_time, 
+            evt_tx_hash as tx_hash, 
+            contract_address as token, 
             value as value, 
             "to" as dao_wallet_address, 
-            'tx_in' as tx_type, 
-            tx_index,
-            COALESCE("from", 0x) as address_interacted_with,
-            trace_address
+            'tx_in' as tx_type,
+            evt_index as tx_index,
+            "from" as address_interacted_with,
+            CAST(NULL as array<bigint>) as trace_address
         FROM 
-        {{ source('ethereum', 'traces') }}
+        {{ source('erc20_base', 'evt_transfer') }}
         {% if not is_incremental() %}
-        WHERE block_time >= DATE '{{transactions_start_date}}'
+        WHERE evt_block_time >= DATE '{{transactions_start_date}}'
         {% endif %}
         {% if is_incremental() %}
-        WHERE block_time >= date_trunc('day', now() - interval '7' Day)
+        WHERE evt_block_time >= date_trunc('day', now() - interval '7' Day)
         {% endif %}
         AND "to" IN (SELECT dao_wallet_address FROM dao_tmp)
-        AND (LOWER(call_type) NOT IN ('delegatecall', 'callcode', 'staticcall') or call_type IS NULL)
-        AND success = true 
-        AND CAST(value as double) != 0 
+        AND contract_address != 0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000
 
         UNION ALL 
 
         SELECT 
-            block_time, 
-            tx_hash, 
-            0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee as token, 
+            evt_block_time as block_time, 
+            evt_tx_hash as tx_hash, 
+            contract_address as token, 
             value as value, 
             "from" as dao_wallet_address, 
-            'tx_out' as tx_type,
-            tx_index,
-            COALESCE("to", 0x) as address_interacted_with,
-            trace_address
+            'tx_out' as tx_type, 
+            evt_index as tx_index, 
+            "to" as address_interacted_with,
+            CAST(NULL as array<bigint>) as trace_address
         FROM 
-        {{ source('ethereum', 'traces') }}
+        {{ source('erc20_base', 'evt_transfer') }}
         {% if not is_incremental() %}
-        WHERE block_time >= DATE '{{transactions_start_date}}'
+        WHERE evt_block_time >=  DATE'{{transactions_start_date}}'
         {% endif %}
         {% if is_incremental() %}
-        WHERE block_time >= date_trunc('day', now() - interval '7' Day)
+        WHERE evt_block_time >= date_trunc('day', now() - interval '7' Day)
         {% endif %}
         AND "from" IN (SELECT dao_wallet_address FROM dao_tmp)
-        AND (LOWER(call_type) NOT IN ('delegatecall', 'callcode', 'staticcall') or call_type IS NULL)
-        AND success = true 
-        AND CAST(value as double) != 0 
+        AND contract_address != 0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000
 )
 
 SELECT 
@@ -85,10 +81,10 @@ SELECT
     t.block_time, 
     t.tx_type,
     t.token as asset_contract_address,
-    'ETH' asset,
+    COALESCE(er.symbol, CAST(NULL as VARCHAR)) as asset,
     CAST(t.value AS double) as raw_value, 
-    t.value/POW(10, 18) as value, 
-    t.value/POW(10, 18) * p.price as usd_value,
+    t.value/POW(10, COALESCE(er.decimals, 18)) as value, 
+    t.value/POW(10, COALESCE(er.decimals, 18)) * p.price as usd_value,
     -- COALESCE(p.price, dp.median_price) as usd_value, 
     t.tx_hash, 
     t.tx_index,
@@ -100,10 +96,14 @@ INNER JOIN
 dao_tmp dt 
     ON t.dao_wallet_address = dt.dao_wallet_address
 LEFT JOIN 
+{{ ref('tokens_erc20') }} er
+    ON t.token = er.contract_address
+    AND er.blockchain = 'base'
+LEFT JOIN 
 {{ source('prices', 'usd') }} p 
     ON p.minute = date_trunc('minute', t.block_time)
-    AND p.symbol = 'WETH'
-    AND p.blockchain = 'ethereum'
+    AND p.contract_address = t.token
+    AND p.blockchain = 'base'
     {% if not is_incremental() %}
     AND p.minute >= DATE '{{transactions_start_date}}'
     {% endif %}
@@ -111,13 +111,14 @@ LEFT JOIN
     AND p.minute >= date_trunc('day', now() - interval '7' Day)
     {% endif %}
 {#
--- LEFT JOIN 
+-- LEFT JOIN comment out until dex_prices is migrated
 -- {{ ref('dex_prices') }} dp 
 --     ON dp.hour = date_trunc('hour', t.block_time)
---     AND dp.contract_address = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
---     AND dp.blockchain = 'ethereum'
+--     AND dp.contract_address = t.token 
+--     AND dp.blockchain = 'base'
 --     AND dp.hour >= DATE '{{transactions_start_date}}'
 --     {% if is_incremental() %}
 --     AND dp.hour >= date_trunc('day', now() - interval '7' Day)
 --     {% endif %}
 #}
+
