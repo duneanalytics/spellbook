5567c7207cc2f528390f936e347fb4630925a55c
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-08-17 15:36:05 -0400

    Revert "Merge branch 'main' into tt"
    This reverts commit c1f5bb04300dada0b601302599925a78e4e09e5c, reversing
    changes made to fd29d07cd19ac805603369046a53c47924cdeab0.

--- models/prices/prices_usd_latest_historical.sql
+++ models/prices/prices_usd_latest_historical.sql
@@ -5,13 +5,12 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['unique_key']
+        unique_key = ['blockchain','contract_address','decimals','symbol']
         )
 }}
 
 SELECT
-{{ dbt_utils.generate_surrogate_key(['pu.blockchain', 'pu.contract_address', 'pu.decimals', 'pu.symbol']) }} as unique_key
-, pu.blockchain
+  pu.blockchain
 , pu.contract_address
 , pu.decimals
 , pu.symbol
@@ -21,4 +20,4 @@ FROM {{ source('prices', 'usd') }} pu
 {% if is_incremental() %}
     WHERE minute >= date_trunc('day', now() - interval '2' day)
 {% endif %}
-GROUP BY 1,2,3,4,5
+GROUP BY 1,2,3,4
