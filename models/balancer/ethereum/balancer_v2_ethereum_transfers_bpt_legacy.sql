a661388b81dfec8b56e8e1edf1a01d13457da9a4
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-08-18 10:48:10 -0400

    Migrate transfers bpt pools fees dunesql (#4114)
    * Update balancer_arbitrum_schema.yml
    
    * Update balancer_v2_arbitrum_transfers_bpt.sql
    
    * Update balancer_v2_arbitrum_transfers_bpt_legacy.sql
    
    * Create balancer_v2_avalanche_c_transfers_bpt.sql
    
    * Create balancer_v2_avalanche_c_transfers_bpt_legacy.sql
    
    * Update balancer_avalanche_c_schema.yml
    
    * Update balancer_schema.yml
    
    * Create balancer_transfers_bpt.sql
    
    * Create balancer_transfers_bpt_legacy.sql
    
    * Create balancer_base_schema.yml
    
    * Create balancer_base_sources.yml
    
    * Create balancer_v2_base_transfers_bpt.sql
    
    * Create balancer_v2_base_transfers_bpt_legacy.sql
    
    * Update balancer_ethereum_schema.yml
    
    * Update balancer_v2_ethereum_transfers_bpt_legacy.sql
    
    * Update balancer_v2_ethereum_transfers_bpt.sql
    
    * Update balancer_gnosis_schema.yml
    
    * Create balancer_v2_gnosis_transfers_bpt.sql
    
    * Create balancer_v2_gnosis_transfers_bpt_legacy.sql
    
    * Update balancer_v2_gnosis_transfers_bpt_legacy.sql
    
    * Update balancer_optimism_schema.yml
    
    * Update balancer_v2_optimism_transfers_bpt.sql
    
    * Update balancer_v2_optimism_transfers_bpt_legacy.sql
    
    * Update balancer_polygon_schema.yml
    
    * Update balancer_v2_polygon_transfers_bpt.sql
    
    * Update balancer_v2_polygon_transfers_bpt_legacy.sql
    
    * Migrate `pools_fees` to DuneSQL
    
    * Add migrated `pools_fees` model to Avalanche, Base, and create an aggregate spell.
    
    * Migrate `balancer_v2_trades` to DuneSQL
    
    * Update balancer_v2_arbitrum_trades.sql
    
    * Update balancer_v2_arbitrum_trades.sql
    
    * Update balancer_ethereum_trades.sql
    
    * Update balancer_v2_optimism_trades.sql
    
    * Update balancer_ethereum_trades.sql
    
    * Update balancer_v2_ethereum_trades.sql
    
    * Revert "Update balancer_v2_ethereum_trades.sql"
    
    This reverts commit c03026ce46460baaf4265f406e86dd9e8e259055.
    
    * Revert "Update balancer_ethereum_trades.sql"
    
    This reverts commit d037954c625d0871f04f9f86f70093e1385b3acb.
    
    * Revert "Update balancer_v2_optimism_trades.sql"
    
    This reverts commit aae6872f9bffecae740b5fefc619cab63bb85c49.
    
    * Revert "Update balancer_ethereum_trades.sql"
    
    This reverts commit f41a6071a21a01bed218794b1a4d6750de41ae0a.
    
    * Revert "Update balancer_v2_arbitrum_trades.sql"
    
    This reverts commit d6ff85ae752df3c040060afdbfab2fb9f561abf0.
    
    * Revert "Update balancer_v2_arbitrum_trades.sql"
    
    This reverts commit b1e3362991d382614832470e8887eb7d5c0d0c42.
    
    * Revert "Migrate `balancer_v2_trades` to DuneSQL"
    
    This reverts commit 83ca53416ecb93a4ac1189bdc40ba4d6e3ef34e1.
    
    * Requested changes.
    
    * Fix merge conflict issues.
    
    * Update balancer_base_schema.yml
    
    * Update balancer_base_sources.yml
    
    * Change `transfers_bpt` to partition by month.
    
    * Update balancer_avalanche_c_schema.yml
    
    * Update balancer_base_schema.yml
    
    * Change casting to make union work.
    
    * Revert "Change casting to make union work."
    
    This reverts commit 1d1511e4e20118f50799a27912313577e1e57e3f.

--- models/balancer/ethereum/balancer_v2_ethereum_transfers_bpt_legacy.sql
+++ models/balancer/ethereum/balancer_v2_ethereum_transfers_bpt_legacy.sql
@@ -4,7 +4,7 @@
 	
         schema='balancer_v2_ethereum',
         alias = alias('transfers_bpt', legacy_model=True),
-        partition_by = ['block_date'],
+        partition_by = ['block_month'],
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
@@ -31,11 +31,13 @@ WITH registered_pools AS (
 
 SELECT DISTINCT * FROM (
     SELECT
+	'ethereum' AS blockchain,
         logs.contract_address,
         logs.tx_hash AS evt_tx_hash,
         logs.index AS evt_index,
         logs.block_time AS evt_block_time,
         TRY_CAST(date_trunc('DAY', logs.block_time) AS date) AS block_date,
+        TRY_CAST(date_trunc('MONTH', logs.block_time) AS date) AS block_month,
         logs.block_number AS evt_block_number,
         CONCAT('0x', SUBSTRING(logs.topic2, 27, 40)) AS from,
         CONCAT('0x', SUBSTRING(logs.topic3, 27, 40)) AS to,
