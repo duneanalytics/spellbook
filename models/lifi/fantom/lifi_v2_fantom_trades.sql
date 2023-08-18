a6d385d3c7d1fb6c0caf10eb46dc3df7a2fe21e0
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-08-18 20:06:01 +0100

    TINY FIXES AND CLEAN UP

--- models/lifi/fantom/lifi_v2_fantom_trades.sql
+++ models/lifi/fantom/lifi_v2_fantom_trades.sql
@@ -69,8 +69,8 @@ SELECT
     end as token_pair,
     dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
     dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount,
-    CAST(dexs.token_bought_amount_raw AS UINT256) AS token_bought_amount_raw,
-    CAST(dexs.token_sold_amount_raw AS UINT256) AS token_sold_amount_raw,
+    CAST(dexs.token_bought_amount_raw) AS token_bought_amount_raw,
+    CAST(dexs.token_sold_amount_raw AS) AS token_sold_amount_raw,
     coalesce(
         dexs.amount_usd
         ,(dexs.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
@@ -83,14 +83,14 @@ SELECT
     dexs.project_contract_address,
     dexs.tx_hash,
     tx."from" AS tx_from,
-    tx."to" AS tx_to,
+    tx.to AS tx_to,
     dexs.trace_address,
     dexs.evt_index
 from dexs
 inner join {{ source('fantom', 'transactions') }} tx
     on dexs.tx_hash = tx.hash
     {% if not is_incremental() %}
-    and tx.block_time >= DATE '{{project_start_date}}'
+    and tx.block_time >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     and tx.block_time >= date_trunc('day', now() - interval '7' Day)
@@ -106,7 +106,7 @@ left join {{ source('prices', 'usd') }} p_bought
     and p_bought.contract_address = dexs.token_bought_address
     and p_bought.blockchain = 'fantom'
     {% if not is_incremental() %}
-    and p_bought.minute >= DATE '{{project_start_date}}'
+    and p_bought.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     and p_bought.minute >= date_trunc('day', now() - interval '7' Day)
@@ -116,7 +116,7 @@ left join {{ source('prices', 'usd') }} p_sold
     and p_sold.contract_address = dexs.token_sold_address
     and p_sold.blockchain = 'fantom'
     {% if not is_incremental() %}
-    and p_sold.minute >= DATE '{{project_start_date}}'
+    and p_sold.minute >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     and p_sold.minute >= date_trunc('day', now() - interval '7' Day)
