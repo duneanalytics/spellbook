a304823a14540fa4037ca9d7fc493e69b260aac2
Hildebert Moulié <h.moulie@gmail.com>
2023-08-19 15:29:14 +0200

    fix

--- models/dex/dex_sandwiches.sql
+++ models/dex/dex_sandwiches.sql
@@ -1,5 +1,6 @@
 {{ config(
         alias = alias('sandwiches'),
+        tags = ['dunesql'],
         partition_by = ['block_date'],
         materialized = 'incremental',
         file_format = 'delta',
