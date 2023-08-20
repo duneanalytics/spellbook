909056c32681d24c69fc21ca5122919bf3634225
Hildebert Moulié <h.moulie@gmail.com>
2023-08-20 12:04:11 +0200

    more efficient

--- macros/models/_project/seaport/seaport_traces.sql
+++ macros/models/_project/seaport/seaport_traces.sql
@@ -19,66 +19,7 @@ WITH base_data AS (
     {% endif %}
     )
 
-
-SELECT '{{blockchain}}' AS blockchain
-, date_trunc('day', block_time) AS block_date
-, block_time
-, block_number
-, 'consideration' AS trace_side
-, order_hash
-, tx_hash
-, CASE json_extract_scalar(consideration_item, '$.itemType')
-    WHEN '0' THEN 'ETH'
-    WHEN '1' THEN '{{token_standard_start}}' || '20'
-    WHEN '2' THEN '{{token_standard_start}}' || '721'
-    WHEN '3' THEN '{{token_standard_start}}' || '1155'
-    END AS token_standard
-, consideration_index AS trace_index
-, seaport_contract_address
-, from_hex(json_extract_scalar(consideration_item, '$.token')) AS token_address
-, CAST(json_extract_scalar(consideration_item, '$.amount') AS UINT256) AS amount
-, json_extract_scalar(consideration_item, '$.identifier') AS identifier
-, from_hex(json_extract_scalar(consideration_item, '$.recipient')) AS recipient
-, offerer
-, zone
-FROM (
-    SELECT block_time
-    , block_number
-    , order_hash
-    , tx_hash
-    , offerer
-    , seaport_contract_address
-    , consideration_index
-    , consideration_item
-    , zone
-    FROM base_data
-    CROSS JOIN UNNEST(consideration) WITH ordinality AS t (consideration_item, consideration_index)
-    )
-
-UNION ALL
-
-SELECT '{{blockchain}}' AS blockchain
-, date_trunc('day', block_time) AS block_date
-, block_time
-, block_number
-, 'offer' AS trace_side
-, order_hash
-, tx_hash
-, CASE json_extract_scalar(offer_item, '$.itemType')
-    WHEN '0' THEN 'ETH'
-    WHEN '1' THEN '{{token_standard_start}}' || '20'
-    WHEN '2' THEN '{{token_standard_start}}' || '721'
-    WHEN '3' THEN '{{token_standard_start}}' || '1155'
-    END AS token_standard
-, offer_index AS trace_index
-, seaport_contract_address
-, from_hex(json_extract_scalar(offer_item, '$.token')) AS token_address
-, CAST(json_extract_scalar(offer_item, '$.amount') AS UINT256) AS amount
-, json_extract_scalar(offer_item, '$.identifier') AS identifier
-, recipient
-, offerer
-, zone
-FROM (
+, exploded_traces AS (
     SELECT block_time
     , block_number
     , order_hash
@@ -86,11 +27,52 @@ FROM (
     , recipient
     , offerer
     , seaport_contract_address
-    , offer_index
-    , offer_item
+    , 'offer' AS trace_side
+    , trace_index
+    , trace_item
     , zone
     FROM base_data
-    CROSS JOIN UNNEST(offer) WITH ordinality AS t (offer_item, offer_index)
+    CROSS JOIN UNNEST(offer) WITH ordinality AS t (trace_item, trace_index)
+    
+    UNION ALL
+
+    SELECT block_time
+    , block_number
+    , order_hash
+    , tx_hash
+    , from_hex(json_extract_scalar(consideration_item, '$.recipient')) AS recipient
+    , offerer
+    , seaport_contract_address
+    , 'consideration' AS trace_side
+    , trace_index
+    , trace_item
+    , zone
+    FROM base_data
+    CROSS JOIN UNNEST(consideration) WITH ordinality AS t (trace_item, trace_index)
     )
+
+SELECT '{{blockchain}}' AS blockchain
+, date_trunc('day', block_time) AS block_date
+, block_time
+, block_number
+, order_hash
+, tx_hash
+, CASE json_extract_scalar(trace_item, '$.itemType')
+    WHEN '0' THEN 'native'
+    WHEN '1' THEN '{{token_standard_start}}' || '20'
+    WHEN '2' THEN '{{token_standard_start}}' || '721'
+    WHEN '3' THEN '{{token_standard_start}}' || '1155'
+    END AS token_standard
+, from_hex(json_extract_scalar(trace_item, '$.token')) AS token_address
+, CAST(json_extract_scalar(trace_item, '$.amount') AS UINT256) AS amount
+, json_extract_scalar(trace_item, '$.identifier') AS identifier
+, recipient
+, offerer
+, seaport_contract_address
+, trace_side
+, trace_index
+, trace_item
+, zone
+FROM exploded_traces
 
 {% endmacro %}
