4cfc44a4266cb8387c036995ae094b2829a9f0e1
Hildebert Moulié <h.moulie@gmail.com>
2023-08-20 12:12:59 +0200

    add base

---
+++ models/seaport/chains/seaport_base_traces.sql
@@ -0,0 +1,19 @@
+{{ config(
+        tags = ['dunesql'],
+        schema = 'seaport_base',
+        alias =alias('traces'),
+        partition_by=['block_date'],
+        materialized='incremental',
+        file_format = 'delta',
+        unique_key = ['block_number', 'order_hash', 'trace_side', 'trace_index'],
+        post_hook='{{ expose_spells(\'["base"]\',
+                                "project",
+                                "seaport",
+                                \'["hildobby"]\') }}'
+        )
+}}
+
+{{seaport_traces(
+    blockchain='base'
+    , seaport_events = source('seaport_base', 'Seaport_evt_OrderFulfilled')
+)}}
