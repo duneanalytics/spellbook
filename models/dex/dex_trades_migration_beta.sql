c32ecb803ad0619c2c3a7cd3f791f0fc5b8df003
Alan Ghobadi <aalan3@gmail.com>
2023-08-21 15:31:38 +0200

    Don't include zeroex for now

--- models/dex/dex_trades_migration_beta.sql
+++ models/dex/dex_trades_migration_beta.sql
@@ -22,6 +22,7 @@
 ,ref('ellipsis_finance_trades')
 ,ref('synthetix_spot_trades')
 ,ref('balancer_trades')
+,ref('zeroex_native_trades')
  #}
 
 {% set dex_trade_models = [
@@ -69,7 +70,6 @@
 ,ref('nomiswap_bnb_trades')
 ,ref('zipswap_trades')
 ,ref('kyberswap_trades')
-,ref('zeroex_native_trades')
 ] %}
 
 
