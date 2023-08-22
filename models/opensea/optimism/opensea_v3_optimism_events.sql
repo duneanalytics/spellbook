8aafe588a89bfa97a9eb9629357ec11306bc26e0
0xRob <0xRobin5@gmail.com>
2023-08-22 14:22:13 +0200

    temp fix

--- models/opensea/optimism/opensea_v3_optimism_events.sql
+++ models/opensea/optimism/opensea_v3_optimism_events.sql
@@ -19,19 +19,20 @@ WITH fee_wallets as (
     ) as foo(wallet_address, wallet_name)
 )
 , trades as (
-     seaport_v3_trades(
+    -- todo: replace opensea_optimism by seaport_optimism schema once decode is fixed
+     {{ seaport_v3_trades(
      blockchain = 'optimism'
      ,source_transactions = source('optimism','transactions')
-     ,Seaport_evt_OrderFulfilled = source('seaport_optimism','Seaport_evt_OrderFulfilled')
-     ,Seaport_call_matchAdvancedOrders = source('seaport_optimism','Seaport_call_matchAdvancedOrders')
-     ,Seaport_call_matchOrders = source('seaport_optimism','Seaport_call_matchOrders')
+     ,Seaport_evt_OrderFulfilled = source('opensea_optimism','Seaport_evt_OrderFulfilled')
+     ,Seaport_call_matchAdvancedOrders = source('opensea_optimism','Seaport_call_matchAdvancedOrders')
+     ,Seaport_call_matchOrders = source('opensea_optimism','Seaport_call_matchOrders')
      ,fee_wallet_list_cte = 'fee_wallets'
      ,native_token_address = '0x0000000000000000000000000000000000000000'
      ,alternative_token_address = '0x4200000000000000000000000000000000000006'
      ,native_token_symbol = 'ETH'
      ,start_date = '2022-07-01'
     )
-
+  }}
 )
 
 select *
