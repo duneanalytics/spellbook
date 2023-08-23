2876918ea92b773631e138cbc60486b7f4e453e6
henrystats <65398020+henrystats@users.noreply.github.com>
2023-08-23 16:36:04 +0100

    Add Tigris Options Trade & Enrich Models - Ready for review  (#4099)
    * blank push
    
    * rewrite
    
    * fix typo
    
    * fix version
    
    * test polygon options models
    
    * fix typo
    
    * add arbitrum options
    
    * consolidate versions in blockchain perps
    
    * smol fix
    
    * add protocol version to positions leverage
    
    * add protocol version on polygon
    
    * missing limit cancelled
    
    * add fees distributed events
    
    * fix synthax
    
    * create options & perpetual trades table on arbit
    
    * fix fees
    
    * add seeds
    
    * fix version
    
    * fix trade
    
    * add polygon models
    
    * final updates
    
    * fix... so proud of myself for completing this
    
    * typo fix
    
    * blank on tokenserc20 and add tigris to perp table
    
    * add columns
    
    * add column to main models
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

---
+++ models/tigris/polygon/events/tigris_polygon_events_fees_distributed.sql
@@ -0,0 +1,75 @@
+{{ config(
+    tags=['dunesql'],
+    schema = 'tigris_polygon',
+    alias = alias('events_fees_distributed'),
+    partition_by = ['block_month'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['evt_block_time', 'evt_tx_hash', 'version', 'protocol_version']
+    )
+}}
+
+WITH 
+
+{% set fees_distributed_v1_evt_tables = [
+    'TradingV7_evt_FeesDistributed'
+    ,'TradingV8_evt_FeesDistributed'
+] %}
+
+{% set fees_distributed_v2_evt_tables = [
+    'Trading_evt_FeesDistributed',
+    'TradingV2_evt_FeesDistributed',
+    'TradingV3_evt_FeesDistributed'
+] %}
+
+fees_v1 AS (
+    {% for fees_evt in fees_distributed_v1_evt_tables %}
+        SELECT
+            '{{ 'v1.' + (loop.index + 6) | string }}' as version,
+            '1' as protocol_version,
+            CAST(date_trunc('DAY', evt_block_time) AS date) as day, 
+            CAST(date_trunc('MONTH', evt_block_time) AS date) as block_month, 
+            evt_block_time,
+            evt_index,
+            evt_tx_hash,
+            _botFees/1e18 + _burnFees/1e18 + _daoFees/1e18 + _refFees/1e18 as fees,
+            contract_address as project_contract_address
+        FROM {{ source('tigristrade_polygon', fees_evt) }}
+        {% if is_incremental() %}
+        WHERE evt_block_time >= date_trunc('day', now() - interval '7' day) 
+        {% endif %}
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+    {% endfor %}
+),
+
+
+fees_v2 AS (
+    {% for fees_evt in fees_distributed_v2_evt_tables %}
+        SELECT
+            '{{ 'v2.' + loop.index | string }}' as version,
+            '2' as protocol_version,
+            CAST(date_trunc('DAY', evt_block_time) AS date) as day, 
+            CAST(date_trunc('MONTH', evt_block_time) AS date) as block_month, 
+            evt_block_time,
+            evt_index,
+            evt_tx_hash,
+            botFees/1e18 + burnFees/1e18 + daoFees/1e18 + refFees/1e18 as fees,
+            contract_address as project_contract_address
+        FROM {{ source('tigristrade_v2_polygon', fees_evt) }}
+        {% if is_incremental() %}
+        WHERE evt_block_time >= date_trunc('day', now() - interval '7' day) 
+        {% endif %}
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+    {% endfor %}
+)
+
+SELECT * FROM fees_v1
+
+UNION ALL 
+
+SELECT * FROM fees_v2 
