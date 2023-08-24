773ed2fb5c41c76cedb186798c1e1cdeea707441
Hildebert Moulié <h.moulie@gmail.com>
2023-08-24 19:35:54 +0200

    change to correct table for base

--- models/zora/chains/zora_base_mints.sql
+++ models/zora/chains/zora_base_mints.sql
@@ -14,7 +14,7 @@
     , erc721_mints = source('zora_base', 'ERC721Drop_evt_Sale')
     , erc721_fee = source('zora_base', 'ERC721Drop_evt_MintFeePayout')
     , erc721_zora_transfers = source('zora_base', 'ERC721Drop_evt_Transfer')
-    , erc1155_mints = source('zora_base', 'Zora1155_evt_Purchased')
+    , erc1155_mints = source('zora_base', 'ZoraCreator1155_evt_Purchased')
     , erc1155_royalties = source('zora_base', 'Zora1155_evt_UpdatedRoyalties')
     , zora_protocol_rewards = source('zora_base', 'ProtocolRewards_call_depositRewards')
 )}}
