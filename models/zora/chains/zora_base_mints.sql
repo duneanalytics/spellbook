f937e2ce875bef5d3b4516fd17c2a990149db192
Hildebert Moulié <h.moulie@gmail.com>
2023-08-25 00:45:50 +0200

    fix

--- models/zora/chains/zora_base_mints.sql
+++ models/zora/chains/zora_base_mints.sql
@@ -15,6 +15,6 @@
     , erc721_fee = source('zora_base', 'ERC721Drop_evt_MintFeePayout')
     , erc721_zora_transfers = source('zora_base', 'ERC721Drop_evt_Transfer')
     , erc1155_mints = source('zora_base', 'ZoraCreator1155_evt_Purchased')
-    , erc1155_royalties = source('zora_base', 'Zora1155_evt_UpdatedRoyalties')
+    , erc1155_royalties = source('zora_base', 'ZoraCreator1155_evt_UpdatedRoyalties')
     , zora_protocol_rewards = source('zora_base', 'ProtocolRewards_call_depositRewards')
 )}}
--- models/zora/chains/zora_goerli_mints.sql
+++ models/zora/chains/zora_goerli_mints.sql
@@ -14,7 +14,7 @@
     , erc721_mints = source('zora_goerli', 'ERC721Drop_evt_Sale')
     , erc721_fee = source('zora_goerli', 'ERC721Drop_evt_MintFeePayout')
     , erc721_zora_transfers = source('zora_goerli', 'ERC721Drop_evt_Transfer')
-    , erc1155_mints = source('zora_goerli', 'Zora1155_evt_Purchased')
-    , erc1155_royalties = source('zora_goerli', 'Zora1155_evt_UpdatedRoyalties')
+    , erc1155_mints = source('zora_goerli', 'ZoraCreator1155Impl_evt_Purchased')
+    , erc1155_royalties = source('zora_goerli', 'ZoraCreator1155Impl_evt_UpdatedRoyalties')
     , zora_protocol_rewards = source('zora_goerli', 'ProtocolRewards_call_depositRewards')
 )}}
