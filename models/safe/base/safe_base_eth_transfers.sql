efe3ccd901f5eda328a90765b3eb01b61f085cfb
Huang Geyang <Sukebeta@outlook.com>
2023-08-24 16:21:18 +0800

    Migrate safe_base_eth_transfers

--- models/safe/base/safe_base_eth_transfers.sql
+++ models/safe/base/safe_base_eth_transfers.sql
@@ -1,8 +1,9 @@
 {{
     config(
         materialized='incremental',
-        alias='eth_transfers',
-        partition_by = ['block_date'],
+        tags = ['dunesql'],
+        alias= alias('eth_transfers'),
+        partition_by = ['block_month'],
         unique_key = ['block_date', 'tx_hash', 'trace_address', 'amount_raw'],
         on_schema_change='fail',
         file_format ='delta',
@@ -10,29 +11,30 @@
         post_hook='{{ expose_spells(\'["base"]\',
                                     "project",
                                     "safe",
-                                    \'["danielpartida"]\') }}'
+                                    \'["danielpartida", "hosuke"]\') }}'
     )
 }}
 
 select
     s.address,
     try_cast(date_trunc('day', et.block_time) as date) as block_date,
+    CAST(date_trunc('month', et.block_time) as DATE) as block_month,
     et.block_time,
-    -et.value as amount_raw,
+    -CAST(et.value AS INT256) as amount_raw,
     et.tx_hash,
     array_join(et.trace_address, ',') as trace_address
 from {{ source('base', 'traces') }} et
 join {{ ref('safe_base_safes') }} s on et.from = s.address
-    and et.from != et.to -- exclude calls to self to guarantee unique key property
+    and et."from" != et.to -- exclude calls to self to guarantee unique key property
     and et.success = true
     and (lower(et.call_type) not in ('delegatecall', 'callcode', 'staticcall') or et.call_type is null)
-    and cast(et.value as decimal(38,0)) > 0 -- value is of type string. exclude 0 value traces
+    and et.value > UINT256 '0' -- et.value is uint256 type
 {% if not is_incremental() %}
-where et.block_time > date('2023-07-01') -- for initial query optimisation
+where et.block_time > timestamp '2023-07-01' -- for initial query optimisation
 {% endif %}
 {% if is_incremental() %}
 -- to prevent potential counterfactual safe deployment issues we take a bigger interval
-where et.block_time > date_trunc("day", now() - interval '10 days')
+where et.block_time > date_trunc('day', now() - interval '10' day)
 {% endif %}
 
 union all
@@ -40,20 +42,21 @@ union all
 select
     s.address,
     try_cast(date_trunc('day', et.block_time) as date) as block_date,
+    CAST(date_trunc('month', et.block_time) as DATE) as block_month,
     et.block_time,
-    et.value as amount_raw,
+    CAST(et.value AS INT256) as amount_raw,
     et.tx_hash,
     array_join(et.trace_address, ',') as trace_address
 from {{ source('base', 'traces') }} et
 join {{ ref('safe_base_safes') }} s on et.to = s.address
-    and et.from != et.to -- exclude calls to self to guarantee unique key property
+    and et."from" != et.to -- exclude calls to self to guarantee unique key property
     and et.success = true
     and (lower(et.call_type) not in ('delegatecall', 'callcode', 'staticcall') or et.call_type is null)
-    and cast(et.value as decimal(38,0)) > 0 -- value is of type string. exclude 0 value traces
+    and et.value > UINT256 '0' -- et.value is uint256 type
 {% if not is_incremental() %}
-where et.block_time > date('2023-07-01') -- for initial query optimisation
+where et.block_time > timestamp '2023-07-01' -- for initial query optimisation
 {% endif %}
 {% if is_incremental() %}
 -- to prevent potential counterfactual safe deployment issues we take a bigger interval
-where et.block_time > date_trunc("day", now() - interval '10 days')
+where et.block_time > date_trunc('day', now() - interval '10' day)
 {% endif %}
