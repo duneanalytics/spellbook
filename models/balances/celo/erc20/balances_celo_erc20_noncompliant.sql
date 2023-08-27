4baeffe61e9e98fce6e1e2be0ac7db943d7572b8
tomfutago <tomfutago@gmail.com>
2023-08-27 10:29:38 +0100

    updated config and where clause

--- models/balances/celo/erc20/balances_celo_erc20_noncompliant.sql
+++ models/balances/celo/erc20/balances_celo_erc20_noncompliant.sql
@@ -1,10 +1,18 @@
-{{ config(
+{{ 
+    config(
+        tags = ['dunesql'],
         alias = alias('erc20_noncompliant'),
-        materialized ='table',
-        file_format = 'delta'
-) 
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['token_address'],
+        post_hook='{{ expose_spells(\'["celo"]\',
+                                    "sector",
+                                    "balances",
+                                    \'["soispoke", "dot2dotseurat", "tomfutago"]\') }}'
+    )
 }}
 
 select distinct token_address
 from {{ ref('transfers_celo_erc20_rolling_day') }}
-where round(amount/power(10, 18), 6) < -0.001
+where amount < -0.001
