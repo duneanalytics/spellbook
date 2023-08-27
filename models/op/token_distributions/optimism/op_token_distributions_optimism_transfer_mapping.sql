8cd5b1ce2f25a97468ab772de70a62dce1beabf3
chuxinh <chuxin@oplabs.co>
2023-08-27 17:22:20 +1000

    syntax

--- models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
@@ -1,5 +1,6 @@
 {{ config(
     alias = alias('transfer_mapping'),
+    tags = ['dunesql'],
     partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
@@ -9,8 +10,7 @@
                                 "project",
                                 "op_token_distributions",
                                 \'["msilb7", "chuxin"]\') }}'
-    ),
-    tags = ['dunesql']
+    )
 }}
 
 
