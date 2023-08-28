d5e2412cf8f2b310813c322e741ea85607a5da7f
chuxinh <chuxin@oplabs.co>
2023-08-29 09:41:38 +1000

    update attestation_events

--- models/optimism_attestationstation/optimism/optimism_attestationstation_optimism_events.sql
+++ models/optimism_attestationstation/optimism/optimism_attestationstation_optimism_events.sql
@@ -18,7 +18,7 @@ SELECT
 
   FROM (
     select 
-        date_trunc('day', evt_block_time) as block_date
+        cast(date_trunc('day', evt_block_time) as date) as block_date
         ,evt_tx_hash as tx_hash
         ,evt_block_number as block_number
         ,evt_block_time as block_time
@@ -27,27 +27,41 @@ SELECT
         ,creator as issuer
         ,contract_address
         ,key as key_raw
-        ,REGEXP_REPLACE(--Replace invisible characters
-            from_utf8(key), '[^\x20-\x7E]','')
-        as key
+        ,regexp_replace(
+          -- Replace invisible characters
+          from_utf8(
+              from_hex(
+                  CASE 
+                      WHEN bytearray_substring(key, 1, 2) IN (0xab7e, 0x9e43) -- Handle for Clique
+                      THEN to_hex(key)
+                      ELSE substring(cast(key as varchar), 3)
+                  END
+              )
+          ),
+          '[^\x20-\x7E]',
+          ''
+        ) as key
         ,val as val_raw
 
         ,split(
                 REGEXP_REPLACE(--Replace invisible characters
                         CASE WHEN cast( REGEXP_REPLACE(from_utf8(val), '[^\x20-\x7E]','') as varchar(100)) != ''
                             THEN cast(from_utf8(val) as varchar(100))
-                            WHEN bytearray_length(val) <= 32 then cast(bytearray_to_uint256(val) as varchar(100))
+                            ELSE cast(bytearray_to_uint256(
+                              if(bytearray_length(val) > 32, bytearray_substring(val, 1, 32), val)
                         END  
                   , '[^\x20-\x7E]','')
               ,',') as val
             
 
-        ,case when bytearray_length(val) <= 32 then bytearray_to_uint256(val) end AS val_byte2numeric
+        ,bytearray_to_uint256(
+          if(bytearray_length(val) > 32, bytearray_substring(val, 1, 32), val)
+        ) AS val_byte2numeric
 
     from {{source('attestationstation_optimism','AttestationStation_evt_AttestationCreated')}}
     where 
         true
         {% if is_incremental() %}
-        and evt_block_time >= date_trunc('day', now() - interval '1 week')
+        and evt_block_time >= date_trunc('day', now() - interval '1' week)
         {% endif %}
   ) a
