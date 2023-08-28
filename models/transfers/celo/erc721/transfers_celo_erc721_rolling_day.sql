ca3f3c114c94b953dd2637623bf4f4856491e34d
tomfutago <tomfutago@gmail.com>
2023-08-28 15:55:40 +0100

    complete erc721 setup

--- models/transfers/celo/erc721/transfers_celo_erc721_rolling_day.sql
+++ models/transfers/celo/erc721/transfers_celo_erc721_rolling_day.sql
@@ -20,8 +20,7 @@ select
   block_day,
   wallet_address,
   token_address,
-  symbol,
-  sum(amount_raw) over (partition by token_address, wallet_address order by block_day) as amount_raw,
+  token_id,
   sum(amount) over (partition by token_address, wallet_address order by block_day) as amount,
   row_number() over (partition by token_address, wallet_address order by block_day desc) as recency_index,
   now() as last_updated
