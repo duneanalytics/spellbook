71fa1f5dbbec7c2151b13538bd0ed4fdcf37c46d
tomfutago <tomfutago@gmail.com>
2023-08-28 17:21:03 +0100

    added erc1155 transfers

---
+++ models/transfers/celo/erc1155/transfers_celo_erc1155_rolling_hour.sql
@@ -0,0 +1,27 @@
+{{ 
+    config(
+        tags = ['dunesql'],
+        alias = alias('erc1155_rolling_hour'),
+        partition_by = ['block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['wallet_address', 'token_address', 'block_hour'],
+        post_hook='{{ expose_spells(\'["celo"]\',
+                                    "sector",
+                                    "transfers",
+                                    \'["tomfutago"]\') }}'
+    )
+}}
+
+select
+  'celo' as blockchain,
+  block_month,
+  block_hour,
+  wallet_address,
+  token_address,
+  token_id,
+  sum(amount) over (partition by token_address, wallet_address order by block_hour) as amount,
+  row_number() over (partition by token_address, wallet_address order by block_hour desc) as recency_index,
+  now() as last_updated
+from {{ ref('transfers_celo_erc1155_agg_hour') }}
