9bf71f635a6e22aeb8597d56a3ea508ad7ba93c6
longnh <hoanglong180695@gmail.com>
2023-08-29 06:51:20 +0700

    agg transfer for satoshi in bitcoin by day

---
+++ models/transfers/bitcoin/satoshi/transfers_bitcoin_satoshi_agg_day.sql
@@ -0,0 +1,21 @@
+{{ config(
+        alias = alias('satoshi_agg_day'),
+        materialized ='incremental',
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key='unique_transfer_id'
+        )
+}}
+
+select
+    'bitcoin' as blockchain,
+    block_date as day,
+    tr.wallet_address,
+    tr.wallet_address || '-' || block_date as unique_transfer_id,
+    sum(tr.amount_raw) as amount_raw,
+from {{ ref('transfers_bitcoin_satoshi') }} tr
+{% if is_incremental() %}
+-- this filter will only be applied on an incremental run
+where tr.evt_block_time >= date_trunc('day', now() - interval '1 week')
+{% endif %}
+group by 1, 2, 3, 4
---
+++ models/transfers/bitcoin/satoshi/transfers_bitcoin_satoshi_rolling_day.sql
@@ -0,0 +1,14 @@
+{{ config(
+        alias = alias('satoshi_rolling_day'))
+}}
+
+select
+    'bitcoin' as blockchain,
+    day,
+    wallet_address,
+    NOW() as last_updated,
+    row_number() over (partition by wallet_address order by day desc) as recency_index,
+    sum(amount_raw) over (
+        partition by wallet_address order by day
+    ) as amount_raw,
+from {{ ref('transfers_bitcoin_satoshi_agg_day') }}
