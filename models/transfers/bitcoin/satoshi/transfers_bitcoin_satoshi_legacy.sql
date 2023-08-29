4787a6430bbf3d7aa7ccdf35f86b39d603649bc9
Longcnttbkhn <hoanglong180695@gmail.com>
2023-08-29 17:58:59 +0700

    fix error: backquoted identifiers are not supported; use double quotes to quote identifiers

--- models/transfers/bitcoin/satoshi/transfers_bitcoin_satoshi_legacy.sql
+++ models/transfers/bitcoin/satoshi/transfers_bitcoin_satoshi_legacy.sql
@@ -8,7 +8,7 @@ with
     input_transfers as (
         select
             CAST('input' AS VARCHAR(5)) || CAST('-' AS VARCHAR(1)) || CAST(tx_id AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(address AS VARCHAR(100)) as unique_transfer_id,
-            `address` as wallet_address,
+            address as wallet_address,
             block_time,
             block_date,
             block_height,
