9041c6474a535c9dbde7ade1a036c182d405ebd2
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-08-29 14:42:00 +0100

    make changes to  and migrate erc20 transfers

--- models/transfers/optimism/erc20/transfers_optimism_erc20.sql
+++ models/transfers/optimism/erc20/transfers_optimism_erc20.sql
@@ -1,65 +1,69 @@
-{{ config(materialized='view', alias = alias('erc20'),
-        post_hook='{{ expose_spells(\'["optimism"]\',
+{{ config(
+    tags=['dunesql'],
+    materialized='view', 
+    alias = alias('erc20'),
+    post_hook='{{ expose_spells(\'["optimism"]\',
                                     "sector",
                                     "transfers",
-                                    \'["soispoke", "dot2dotseurat", "tschubotz"]\') }}') }}
+                                    \'["soispoke", "dot2dotseurat", "tschubotz", "Henrystats"]\') }}') }}
 
-with
-    sent_transfers as (
-        select
-            'send-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' || CAST(to AS VARCHAR(100)) as unique_transfer_id,
-            to as wallet_address,
-            contract_address as token_address,
+WITH 
+
+erc20_transfers  as (
+        SELECT 
+            'receive' as transfer_type, 
+            evt_tx_hash,
+            evt_index, 
             evt_block_time,
+            to as wallet_address, 
+            contract_address as token_address,
             value as amount_raw
-        from
-            {{ source('erc20_optimism', 'evt_transfer') }}
-    )
+        FROM 
+        {{ source('erc20_optimism', 'evt_transfer') }}
 
-    ,
-    received_transfers as (
-        select
-        'receive-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' || CAST("from" AS VARCHAR(100)) as unique_transfer_id,
-        "from" as wallet_address,
-        contract_address as token_address,
-        evt_block_time,
-        '-' || CAST(value AS VARCHAR(100)) as amount_raw
-        from
-            {{ source('erc20_optimism', 'evt_transfer') }}
-    )
+        UNION ALL 
 
-    ,
-    deposited_weth as (
-        select
-            'deposit-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' ||  CAST(dst AS VARCHAR(100)) as unique_transfer_id,
-            dst as wallet_address,
-            contract_address as token_address,
+        SELECT 
+            'send' as transfer_type, 
+            evt_tx_hash,
+            evt_index, 
             evt_block_time,
+            from as wallet_address, 
+            contract_address as token_address,
+            -value as amount_raw
+        FROM 
+        {{ source('erc20_optimism', 'evt_transfer') }}
+),
+
+
+weth_events as (
+        SELECT 
+            'weth_deposit' as transfer_type, 
+            evt_tx_hash, 
+            evt_index, 
+            evt_block_time,
+            dst as wallet_address, 
+            contract_address as token_address, 
             wad as amount_raw
-        from
-            {{ source('weth_optimism', 'weth9_evt_deposit') }}
-    )
+        FROM 
+        {{ source('weth_optimism', 'weth9_evt_deposit') }}
 
-    ,
-    withdrawn_weth as (
-        select
-            'withdraw-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' ||  CAST(src AS VARCHAR(100)) as unique_transfer_id,
-            src as wallet_address,
-            contract_address as token_address,
+        UNION ALL 
+
+        SELECT 
+            'weth_withdraw' as transfer_type, 
+            evt_tx_hash, 
+            evt_index, 
             evt_block_time,
-            '-' || CAST(wad AS VARCHAR(100)) as amount_raw
-        from
-            {{ source('weth_optimism', 'weth9_evt_withdrawal') }}
-    )
-    
-select unique_transfer_id, 'optimism' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
-from sent_transfers
-union
-select unique_transfer_id, 'optimism' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
-from received_transfers
-union
-select unique_transfer_id, 'optimism' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
-from deposited_weth
-union
-select unique_transfer_id, 'optimism' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
-from withdrawn_weth
+            src as wallet_address, 
+            contract_address as token_address, 
+            -wad as amount_raw
+        FROM 
+        {{ source('weth_optimism', 'weth9_evt_withdrawal') }}
+)
+
+SELECT 'optimism' as blockchain, * FROM erc20_transfers
+
+UNION ALL 
+
+SELECT 'optimism' as blockchain, * FROM weth_events
