8cdd91b988b9d5ae8e474f9be69c62dcd903e8a8
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-08-29 15:43:42 -0400

    [Dunesql] Migrate OP Bots Logic (#4056)
    * migrate
    
    * comma
    
    * quote
    
    * rm trades spells
    
    * dunesql
    
    * rerun
    
    * migrate

--- models/labels/addresses/__single_category_labels__/likely_bots/labels_likely_bot_labels.sql
+++ models/labels/addresses/__single_category_labels__/likely_bots/labels_likely_bot_labels.sql
@@ -1,6 +1,7 @@
 {{
     config(
         alias = alias('likely_bot_labels'),
+        tags = ['dunesql'],
         post_hook='{{ expose_spells(\'["optimism"]\', 
         "sector", 
         "labels", 
