f6c85a1ce7ed77ca466a560f49dec6265d0cb944
Huang Geyang <Sukebeta@outlook.com>
2023-08-30 01:16:20 +0800

    Add static tag

--- models/tokemak/ethereum/tokemak_ethereum_view_tokemak_lookup_reactors.sql
+++ models/tokemak/ethereum/tokemak_ethereum_view_tokemak_lookup_reactors.sql
@@ -1,5 +1,5 @@
 {{ config(
-    tags=['dunesql'],
+    tags=['dunesql', 'static'],
     schema='tokemak_ethereum',
     alias = alias('view_tokemak_lookup_reactors'),
     post_hook = '{{ expose_spells(\'["ethereum"]\',
