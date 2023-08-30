6f875b9fc6626c73ac2cd49cd4a69a55db9faa7a
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-08-30 15:33:44 -0400

    [ETH L1 only] Creator Mapping -> All EVMS (#4137)
    * push
    
    * fix bad creation trace mapping
    
    * fix bad wld deployer
    
    * relay
    
    * quote
    
    * init
    
    * source
    
    * comma
    
    * docs
    
    * fix macro
    
    * handle for new chainz
    
    * base nft copy of op
    
    * base nft copy of op
    
    * schema
    
    * proj
    
    * adds
    
    * macro fix
    
    * rerun logic
    
    * rerun logic
    
    * add base
    
    * add base
    
    * fix schema
    
    * fix schema
    
    * mpnth
    
    * month
    
    * source
    
    * rvert
    
    * base
    
    * base
    
    * comma
    
    * base
    
    * base first funded
    
    * base events
    
    * base
    
    * select
    
    * sel
    
    * base nft
    
    * base
    
    * adds
    
    * bridge
    
    * bridge
    
    * mapp
    
    * chain
    
    * chain
    
    * rm ref
    
    * eth tfer
    
    * schema
    
    * add pr
    
    * replace
    
    * push
    
    * rm ref
    
    * try schema
    
    * try schema
    
    * test
    
    * rm ref
    
    * base
    
    * test
    
    * test schema
    
    * list
    
    * try to migrate to dunesql
    
    * not a comma guy
    
    * base
    
    * add
    
    * nft
    
    * adds
    
    * mat
    
    * mat
    
    * approve
    
    * val
    
    * fix
    
    * type
    
    * cast
    
    * date
    
    * comma
    
    * comma
    
    * fix
    
    * fix
    
    * test
    
    * index
    
    * push
    
    * dst
    
    * txt
    
    * push
    
    * push
    
    * Revert "push"
    
    This reverts commit 3a340d2abe7ac492dea99a50929b10759e90b134.
    
    * push
    
    * adds
    
    * base
    
    * wait until migrate
    
    * b
    
    * edit
    
    * chain
    
    * add
    
    * edit
    
    * rm tag missing
    
    * rm base nft
    
    * fix
    
    * simplify tokens
    
    * for
    
    * alias
    
    * fix
    
    * alias
    
    * Update contracts_contract_mapping.sql
    
    * fix for
    
    * mod macro
    
    * rm mod
    
    * mods
    
    * macro
    
    * rm ref
    
    * comment
    
    * schema
    
    * rogue cc
    
    * add
    
    * fix
    
    * mod
    
    * mod
    
    * deploy by chain
    
    * group
    
    * add cg
    
    * predep
    
    * colname
    
    * group
    
    * simplify tokens
    
    * fix token search
    
    * add
    
    * mod token standard by bytecode
    
    * rm nondeterministic call
    
    * when
    
    * mod deploy rank by time for cross-chain
    
    * colname
    
    * map base creators
    
    * try rm cross chain deploy
    
    * try new code mapping model
    
    * group
    
    * groups
    
    * add
    
    * aliased
    
    * col order
    
    * fields
    
    * alias
    
    * try partition
    
    * add
    
    * reduce unions
    
    * add yearn
    
    * test tbl
    
    * adds
    
    * go by chain w/ macro
    
    * quotes
    
    * schema
    
    * schema
    
    * schema
    
    * legacy
    
    * names
    
    * schema
    
    * group by
    
    * updates
    
    * fix
    
    * name
    
    * name
    
    * clean deploy rank
    
    * macro fix
    
    * fix diffs
    
    * comma
    
    * group
    
    * add
    
    * test partition
    
    * add alias
    
    * created_month
    
    * part
    
    * simplify
    
    * alias
    
    * chain
    
    * bnb hook
    
    * add hook to all
    
    * simplify?
    
    * just L1 for mapping
    
    * rm weird update
    
    * rm destructs
    
    * rm op mods
    
    * schema
    
    * fix macro
    
    * commas
    
    * inc
    
    * col
    
    * alias
    
    * alias
    
    * optimize incremental
    
    * sch
    
    * group
    
    * grp
    
    * grp
    
    * equal
    
    * add updated back but efficient
    
    * update
    
    * with fix
    
    * add incremental for the end
    
    * add m3
    
    * schema
    
    * alais
    
    * commas
    
    * wee
    
    * rm legacy
    
    ---------
    
    Co-authored-by: jeff <102681548+jeff-dude@users.noreply.github.com>

---
+++ models/contracts/chains/contracts_ethereum_find_self_destruct_contracts.sql
@@ -0,0 +1,15 @@
+ {{
+  config(
+        tags = ['dunesql'],
+        schema = 'contracts_ethereum',
+        alias = alias('find_self_destruct_contracts'),
+        materialized ='incremental',
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key='contract_address'
+  )
+}}
+
+{{find_self_destruct_contracts_by_chain(
+    chain='ethereum'
+)}}
