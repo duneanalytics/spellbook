bce1ac47483a1f9d08294cf7d1e162b49f660566
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-08-30 15:03:44 +0100

    added token symbols and nft standards

--- models/tokens/tokens_native.sql
+++ models/tokens/tokens_native.sql
@@ -11,4 +11,5 @@ FROM (VALUES
          , ('bnb', 'BNB', 'WBNB', '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', 8)
          , ('fantom', 'FTM', 'WFTM', '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83', 18)
          , ('solana', 'SOL', 'SOL', 'so11111111111111111111111111111111111111112', 18) --not sure if solana decimals are correct here
+         , ('celo', 'CELO', 'CELO', '0x471ece3750da237f93b8e339c536989b8978a438', 18)
      ) AS temp_table (chain, symbol, price_symbol, price_address, decimals)
