c279587d525e4340c2220f48ccb87a5341fc3504
Hildebert Moulié <h.moulie@gmail.com>
2023-08-31 20:55:18 +0200

    Various fixes to seaport tables

--- models/seaport/chains/seaport_avalanche_c_traces.sql
+++ models/seaport/chains/seaport_avalanche_c_traces.sql
@@ -2,11 +2,7 @@
         tags = ['dunesql'],
         schema = 'seaport_avalanche_c',
         alias =alias('traces'),
-        unique_key = ['block_number', 'tx_hash', 'evt_index', 'order_hash', 'trace_side', 'trace_index'],
-        post_hook='{{ expose_spells(\'["avalanche_c"]\',
-                                "project",
-                                "seaport",
-                                \'["hildobby"]\') }}'
+        unique_key = ['block_number', 'tx_hash', 'evt_index', 'order_hash', 'trace_side', 'trace_index']
         )
 }}
 
