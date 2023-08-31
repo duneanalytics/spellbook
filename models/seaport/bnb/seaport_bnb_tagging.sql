fd2b16c7ec85db8c6c1c2686be85bb77f47d1656
hildobby <h.moulie@gmail.com>
2023-08-31 21:36:14 +0200

    Various fixes to seaport tables (#4213)

--- models/seaport/bnb/seaport_bnb_tagging.sql
+++ models/seaport/bnb/seaport_bnb_tagging.sql
@@ -11,5 +11,6 @@ FROM
     , ('bnb', 'zone', 0x000000e7ec00e7b300774b00001314b8610022b8, 'OpenSea', 'Marketplace')
     , ('bnb', 'zone', 0x110b2b128a9ed1be5ef3232d8e4e41640df5c2cd, 'OpenSea', 'Marketplace')
     , ('bnb', 'zone', 0xf397619df7bfd4d1657ea9bdd9df7ff888731a11, 'OpenSea', 'Marketplace')
+    , ('bnb', 'tx_data_salt', 0x360c6ebe, 'OpenSea', 'Marketplace')
     ) 
     x (blockchain, tagging_method, identifier, protocol, protocol_type)
