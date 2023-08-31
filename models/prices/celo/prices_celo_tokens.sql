afd6af99482da50ed52c7366e64b08b4724e7072
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-08-31 11:41:24 +0100

    removed MAI as symbol on site not matching api

--- models/prices/celo/prices_celo_tokens.sql
+++ models/prices/celo/prices_celo_tokens.sql
@@ -21,7 +21,7 @@ FROM
     ('ceur-celo-euro', 'celo', 'cEUR', 0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73, 18), --requested add to coinPaprika 2023-08-10
     --('creal-celo-brazilian-real', 'celo', 'cREAL', 0xe8537a3d056DA446677B9E9d6c5dB704EaAb4787, 18), --listed but no much volume
     ('ico-axelar', 'celo', 'AXL', 0x23ee2343B892b1BB63503a4FAbc840E0e2C6810f, 6), --requested add to coinPaprika 2023-08-10
-    ('mimatic-mimatic', 'celo', 'MAI', 0xb9c8f0d3254007ee4b98970b94544e473cd610ec, 18), --requested add to coinPaprika 2023-08-10
+    --('mimatic-mimatic', 'celo', 'MAI', 0xb9c8f0d3254007ee4b98970b94544e473cd610ec, 18), --requested add to coinPaprika 2023-08-10
     ('wbtc-wrapped-bitcoin', 'celo', 'WBTC', 0xd629eb00deced2a080b7ec630ef6ac117e614f1b, 18),
     ('bifi-beefyfinance', 'celo', 'BIFI', 0x639A647fbe20b6c8ac19E48E2de44ea792c62c5C, 18)
     --('ftm-fantom', 'celo', 'FTM', 0x218c3c3d49d0e7b37aff0d8bb079de36ae61a4c0, 18), --requested add to coinPaprika 2023-08-10
