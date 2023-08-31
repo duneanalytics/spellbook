62121d6e64e5478fb02fd6e92dcf396b2581617f
RantumBits <rantum@rantum.xyz>
2023-08-31 14:59:41 -0700

    comma

--- models/zeroex/celo/zeroex_celo_api_fills_deduped.sql
+++ models/zeroex/celo/zeroex_celo_api_fills_deduped.sql
@@ -72,7 +72,7 @@ SELECT  a.blockchain
       , cast(null as varchar(10)) as version
       , a.block_date
       , a.block_time
-      , CAST(date_trunc('month', a.block_time) as DATE) as block_month, 
+      , CAST(date_trunc('month', a.block_time) as DATE) as block_month 
       , b.taker_symbol AS taker_symbol
       , b.maker_symbol AS maker_symbol
       , CASE
