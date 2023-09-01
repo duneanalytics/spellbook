f1bafaf0bf21d339e3e87c87def15e94787aa7a4
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-09-01 15:46:23 +0100

    Celo erc20 transfer aggs (#4169)
    * add celo erc20 transfer aggs and balances
    
    * updated balances, added recency index
    
    * moved balances schema
    
    * adding balances_celo to dbt_project.yml
    
    * updated config and where clause
    
    * cast type for blank symbol
    
    * amended day and hour sequences
    
    * fixed cycle ref
    
    * fixing cycle again..
    
    * fix noncompliant balances
    
    * removed blances to allow transfer aggs build first
    
    * revert adding balances_celo
    
    * Add uniqueness test
    
    * join to token symbols and amended column name
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

---
+++ models/transfers/celo/erc20/transfers_celo_erc20_rolling_day.sql
@@ -0,0 +1,28 @@
+{{ 
+    config(
+        tags = ['dunesql'],
+        alias = alias('erc20_rolling_day'),
+        partition_by = ['block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['wallet_address', 'token_address', 'block_day'],
+        post_hook='{{ expose_spells(\'["celo"]\',
+                                    "sector",
+                                    "transfers",
+                                    \'["tomfutago"]\') }}'
+    )
+}}
+
+select
+  'celo' as blockchain,
+  block_month,
+  block_day,
+  wallet_address,
+  token_address,
+  symbol,
+  sum(amount_raw) over (partition by token_address, wallet_address order by block_day) as amount_raw,
+  sum(amount) over (partition by token_address, wallet_address order by block_day) as amount,
+  row_number() over (partition by token_address, wallet_address order by block_day desc) as recency_index,
+  now() as last_updated
+from {{ ref('transfers_celo_erc20_agg_day') }}
