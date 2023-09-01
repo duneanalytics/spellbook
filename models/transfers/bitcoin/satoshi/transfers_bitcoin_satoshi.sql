d9aa52b4eab4d69cfae3a8ffa22036f8858fcb1e
longnh <hoanglong180695@gmail.com>
2023-09-02 00:01:57 +0700

    add unique transfer id

--- models/transfers/bitcoin/satoshi/transfers_bitcoin_satoshi.sql
+++ models/transfers/bitcoin/satoshi/transfers_bitcoin_satoshi.sql
@@ -3,7 +3,7 @@
         alias = alias('satoshi'),
         tags = ['dunesql'],
         file_format = 'delta',
-        unique_key = ['type', 'tx_id', 'index', 'wallet_address'],
+        unique_key = ['unique_transfer_id'],
         post_hook='{{ expose_spells(\'["bitcoin"]\',
                                     "sector",
                                     "transfers",
@@ -11,6 +11,7 @@
 with 
     input_transfers as (
         select
+            CAST('input' AS VARCHAR(5)) || CAST('-' AS VARCHAR(1)) || CAST(tx_id AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(address AS VARCHAR(100)) as unique_transfer_id,
             'input' as type,
             tx_id,
             index,
@@ -29,6 +30,7 @@ with
     , 
     output_transfers as (
         select
+            CAST('output' AS VARCHAR(6)) || CAST('-' AS VARCHAR(1)) || CAST(tx_id AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(address AS VARCHAR(100)) as unique_transfer_id,
             'output' as type,
             tx_id,
             index,
@@ -45,8 +47,8 @@ with
         {% endif %}
     )
 
-select type, tx_id, index, 'bitcoin' as blockchain, wallet_address, block_time, block_date, block_height, amount_raw
+select unique_transfer_id, type, tx_id, index, 'bitcoin' as blockchain, wallet_address, block_time, block_date, block_height, amount_raw
 from input_transfers
 union
-select type, tx_id, index, 'bitcoin' as blockchain, wallet_address, block_time, block_date, block_height, amount_raw
+select unique_transfer_id, type, tx_id, index, 'bitcoin' as blockchain, wallet_address, block_time, block_date, block_height, amount_raw
 from output_transfers
