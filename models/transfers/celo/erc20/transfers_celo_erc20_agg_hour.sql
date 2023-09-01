bc753fddee5c70a0901ee2f2075961fe227634d4
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-09-01 10:39:33 +0100

    join to token symbols and amended column name

--- models/transfers/celo/erc20/transfers_celo_erc20_agg_hour.sql
+++ models/transfers/celo/erc20/transfers_celo_erc20_agg_hour.sql
@@ -20,11 +20,11 @@ select
   date_trunc('hour', tr.block_time) as block_hour,
   tr.wallet_address,
   tr.token_address,
-  cast(null as varchar(10)) as symbol, -- t.symbol, -- null until tokens_celo_erc20 spell is created
+  t.symbol,
   sum(tr.amount_raw) as amount_raw,
-  sum(tr.amount_raw / power(10, 18)) as amount --sum(tr.amount_raw / power(10, t.decimals)) as amount
+  sum(tr.amount_raw / power(10, coalesce(t.decimals, 18))) as amount
 from {{ ref('transfers_celo_erc20') }} tr
---left join ref('tokens_celo_erc20') t on t.contract_address = tr.token_address
+left join {{ ref('tokens_celo_erc20') }} t on t.contract_address = tr.token_address
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
 where tr.block_time >= date_trunc('day', now() - interval '7' day)
