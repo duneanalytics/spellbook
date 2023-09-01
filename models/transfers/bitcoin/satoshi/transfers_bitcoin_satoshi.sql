86de49c62237d54ce4a8826850462b35f620cd20
Huang Geyang <Sukebeta@outlook.com>
2023-09-01 21:06:59 +0800

    Refactor transfers_bitcoin_satoshi

--- models/transfers/bitcoin/satoshi/transfers_bitcoin_satoshi.sql
+++ models/transfers/bitcoin/satoshi/transfers_bitcoin_satoshi.sql
@@ -1,36 +1,51 @@
-{{ config(materialized='view', alias = alias('satoshi'),
+{{ config(
+        materialized='incremental',
+        alias = alias('satoshi'),
         tags = ['dunesql'],
+        unique_key = ['type', 'tx_id', 'index', 'wallet_address'],
         post_hook='{{ expose_spells(\'["bitcoin"]\',
                                     "sector",
                                     "transfers",
-                                    \'["longnhbkhn"]\') }}') }}
+                                    \'["longnhbkhn", "hosuke"]\') }}') }}
 with 
     input_transfers as (
         select
-            CAST('input' AS VARCHAR(5)) || CAST('-' AS VARCHAR(1)) || CAST(tx_id AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(address AS VARCHAR(100)) as unique_transfer_id,
+            'input' as type,
+            tx_id,
+            index,
             address as wallet_address,
             block_time,
             block_date,
             block_height,
             -1 * value as amount_raw
         from
-            {{ source('bitcoin', 'inputs') }} where address is not null
+            {{ source('bitcoin', 'inputs') }} 
+        where address is not null
+        {% if is_incremental() %}
+        and block_time >= date_trunc('day', now() - interval '7' day)
+        {% endif %}
     )
     , 
     output_transfers as (
         select
-            CAST('output' AS VARCHAR(6)) || CAST('-' AS VARCHAR(1)) || CAST(tx_id AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(index AS VARCHAR(100)) || CAST('-' AS VARCHAR(1)) || CAST(address AS VARCHAR(100)) as unique_transfer_id,
+            'output' as type,
+            tx_id,
+            index,
             address as wallet_address,
             block_time,
             block_date,
             block_height,
             value as amount_raw
         from
-            {{ source('bitcoin', 'outputs') }} where address is not null
+            {{ source('bitcoin', 'outputs') }} 
+        where address is not null
+        {% if is_incremental() %}
+        and block_time >= date_trunc('day', now() - interval '7' day)
+        {% endif %}
     )
 
-select unique_transfer_id, 'bitcoin' as blockchain, wallet_address, block_time, block_date, block_height, amount_raw
+select type, tx_id, index, 'bitcoin' as blockchain, wallet_address, block_time, block_date, block_height, amount_raw
 from input_transfers
-union
-select unique_transfer_id, 'bitcoin' as blockchain, wallet_address, block_time, block_date, block_height, amount_raw
+union all
+select type, tx_id, index, 'bitcoin' as blockchain, wallet_address, block_time, block_date, block_height, amount_raw
 from output_transfers
