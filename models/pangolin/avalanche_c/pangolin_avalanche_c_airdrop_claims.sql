c435d5f4e0badb38a128da55b59a2b133cab15be
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-03 01:59:06 +0100

    rewrite amount

--- models/pangolin/avalanche_c/pangolin_avalanche_c_airdrop_claims.sql
+++ models/pangolin/avalanche_c/pangolin_avalanche_c_airdrop_claims.sql
@@ -31,7 +31,7 @@ SELECT 'avalanche_c' AS blockchain
 , t.claimer AS recipient
 , t.contract_address
 , t.evt_tx_hash AS tx_hash
-, CAST(t.amount AS UINT256) AS amount_raw
+, t.amount AS amount_raw
 , CAST(t.amount/POWER(10, 18) AS double) AS amount_original
 , CASE WHEN t.evt_block_time >= (SELECT minute FROM early_price) THEN CAST(pu.price*t.amount/POWER(10, 18) AS double)
     ELSE CAST((SELECT price FROM early_price)*t.amount/POWER(10, 18) AS double)
@@ -43,4 +43,4 @@ FROM {{ source('pangolin_exchange_avalanche_c', 'Airdrop_evt_PngClaimed') }} t
 LEFT JOIN {{ ref('prices_usd_forward_fill') }} pu ON pu.blockchain = 'avalanche_c'
     AND pu.contract_address= {{png_token_address}}
     AND pu.minute=date_trunc('minute', t.evt_block_time)
-WHERE t.evt_block_time BETWEEN CAST('2021-02-09' as timestamp) AND CAST('2021-03-10' as timestamp)
+WHERE t.evt_block_time BETWEEN timestamp '2021-02-09' AND timestamp '2021-03-10'
