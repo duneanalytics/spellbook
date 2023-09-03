189f7767cb828b84d2462945947f762f6f7902da
antonio-mendes <soeiromendes.antonio@gmail.com>
2023-09-03 18:43:32 +0100

    Temporarily exclude model breaking incremental job

--- models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
+++ models/op/token_distributions/optimism/op_token_distributions_optimism_transfer_mapping.sql
@@ -1,6 +1,6 @@
 {{ config(
     alias = alias('transfer_mapping'),
-    tags = ['dunesql'],
+    tags = ['prod_exclude'],
     partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
