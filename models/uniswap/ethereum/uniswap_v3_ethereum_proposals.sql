a14d2a2be978830f32812905e26016a027f78cd8
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-09-05 13:55:42 +0100

    another test..

--- models/uniswap/ethereum/uniswap_v3_ethereum_proposals.sql
+++ models/uniswap/ethereum/uniswap_v3_ethereum_proposals.sql
@@ -14,7 +14,7 @@
 {% set blockchain = 'ethereum' %}
 {% set project = 'uniswap' %}
 {% set project_version = 'v3' %}
-{% set dao_name = 'DAO Uniswap' %}
+{% set dao_name = 'DAO_Uniswap' %}
 {% set dao_address = '0x408ed6354d4973f66138c91495f2f2fcbd8724c3' %}
 
 with cte_support as (SELECT 
