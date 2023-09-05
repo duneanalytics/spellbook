2e48219e002adf3c6cc1df021a51359e56d6c5e1
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-06 00:19:31 +0100

    erc20 legacy update

--- models/transfers/base/erc20/transfers_base_erc20_legacy.sql
+++ models/transfers/base/erc20/transfers_base_erc20_legacy.sql
@@ -1,67 +1,11 @@
 {{ config(
 	tags=['legacy'],
-	materialized='view', alias = alias('erc20', legacy_model=True),
-        post_hook='{{ expose_spells(\'["base"]\',
-                                    "sector",
-                                    "transfers",
-                                    \'["soispoke", "dot2dotseurat", "tschubotz"]\') }}') }}
+    schema = 'base_transfers',
+    alias = alias('erc20', legacy_model=True),
+    ) }}
 
-with
-    sent_transfers as (
-        select
-            'send-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' || CAST(to AS VARCHAR(100)) as unique_transfer_id,
-            to as wallet_address,
-            contract_address as token_address,
-            evt_block_time,
-            value as amount_raw
-        from
-            {{ source('erc20_base', 'evt_transfer') }}
-    )
 
-    ,
-    received_transfers as (
-        select
-        'receive-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' || CAST("from" AS VARCHAR(100)) as unique_transfer_id,
-        "from" as wallet_address,
-        contract_address as token_address,
-        evt_block_time,
-        '-' || CAST(value AS VARCHAR(100)) as amount_raw
-        from
-            {{ source('erc20_base', 'evt_transfer') }}
-    )
-
-    ,
-    deposited_weth as (
-        select
-            'deposit-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' ||  CAST(dst AS VARCHAR(100)) as unique_transfer_id,
-            dst as wallet_address,
-            contract_address as token_address,
-            evt_block_time,
-            wad as amount_raw
-        from
-            {{ source('weth_base', 'weth9_evt_deposit') }}
-    )
-
-    ,
-    withdrawn_weth as (
-        select
-            'withdraw-' || cast(evt_tx_hash as varchar(100)) || '-' || cast (evt_index as varchar(100)) || '-' ||  CAST(src AS VARCHAR(100)) as unique_transfer_id,
-            src as wallet_address,
-            contract_address as token_address,
-            evt_block_time,
-            '-' || CAST(wad AS VARCHAR(100)) as amount_raw
-        from
-            {{ source('weth_base', 'weth9_evt_withdrawal') }}
-    )
-
-select unique_transfer_id, 'base' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
-from sent_transfers
-union all
-select unique_transfer_id, 'base' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
-from received_transfers
-union all
-select unique_transfer_id, 'base' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
-from deposited_weth
-union all
-select unique_transfer_id, 'base' as blockchain, wallet_address, token_address, evt_block_time, CAST(amount_raw AS VARCHAR(100)) as amount_raw
-from withdrawn_weth
+select
+'1' as unique_transfer_id
+, 'base' as blockchain, '0x' as wallet_address, '0x' as token_address
+, timestamp '2023-01-01' as evt_block_time, '1' as amount_raw
