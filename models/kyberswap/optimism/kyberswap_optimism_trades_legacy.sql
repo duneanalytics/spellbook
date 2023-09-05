9180cb93de6fb93f958a35ca66c9c8032ec61cf3
nhd98z <nhd98z@gmail.com>
2023-09-05 11:02:28 +0700

    fix optimism test

--- models/kyberswap/optimism/kyberswap_optimism_trades_legacy.sql
+++ models/kyberswap/optimism/kyberswap_optimism_trades_legacy.sql
@@ -144,49 +144,49 @@ kyberswap_dex AS (
 
 SELECT
     'optimism'                                                         AS blockchain
-     ,'kyberswap'                                                          AS project
-     ,'dmm'                                                                AS version
-     ,try_cast(date_trunc('DAY', kyberswap_dex.block_time) AS date)        AS block_date
-     ,kyberswap_dex.block_time
-     ,erc20a.symbol                                                        AS token_bought_symbol
-     ,erc20b.symbol                                                        AS token_sold_symbol
-     ,CASE
-          WHEN lower(erc20a.symbol) > lower(erc20b.symbol) THEN concat(erc20b.symbol, '-', erc20a.symbol)
-          ELSE concat(erc20a.symbol, '-', erc20b.symbol)
-    END                                                                  AS token_pair
-     ,kyberswap_dex.token_bought_amount_raw / power(10, erc20a.decimals)   AS token_bought_amount
-     ,kyberswap_dex.token_sold_amount_raw / power(10, erc20b.decimals)     AS token_sold_amount
-     ,CAST(kyberswap_dex.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw
-     ,CAST(kyberswap_dex.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw
-     ,coalesce(kyberswap_dex.amount_usd
-    ,(kyberswap_dex.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
-    ,(kyberswap_dex.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
-    )                                                                   AS amount_usd
-     ,kyberswap_dex.token_bought_address
-     ,kyberswap_dex.token_sold_address
-     ,coalesce(kyberswap_dex.taker, tx.from)                              AS taker
-     ,kyberswap_dex.maker
-     ,kyberswap_dex.project_contract_address
-     ,kyberswap_dex.tx_hash
-     ,tx.from                                                             AS tx_from
-     ,tx.to                                                               AS tx_to
-     ,kyberswap_dex.trace_address
-     ,kyberswap_dex.evt_index
+    ,'kyberswap'                                                          AS project
+    ,'dmm'                                                                AS version
+    ,try_cast(date_trunc('DAY', kyberswap_dex.block_time) AS date)        AS block_date
+    ,kyberswap_dex.block_time
+    ,erc20a.symbol                                                        AS token_bought_symbol
+    ,erc20b.symbol                                                        AS token_sold_symbol
+    ,CASE
+         WHEN lower(erc20a.symbol) > lower(erc20b.symbol) THEN concat(erc20b.symbol, '-', erc20a.symbol)
+         ELSE concat(erc20a.symbol, '-', erc20b.symbol)
+     END                                                                  AS token_pair
+    ,kyberswap_dex.token_bought_amount_raw / power(10, erc20a.decimals)   AS token_bought_amount
+    ,kyberswap_dex.token_sold_amount_raw / power(10, erc20b.decimals)     AS token_sold_amount
+    ,CAST(kyberswap_dex.token_bought_amount_raw AS DECIMAL(38,0)) AS token_bought_amount_raw
+    ,CAST(kyberswap_dex.token_sold_amount_raw AS DECIMAL(38,0)) AS token_sold_amount_raw
+    ,coalesce(kyberswap_dex.amount_usd
+            ,(kyberswap_dex.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
+            ,(kyberswap_dex.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
+     )                                                                   AS amount_usd
+    ,kyberswap_dex.token_bought_address
+    ,kyberswap_dex.token_sold_address
+    ,coalesce(kyberswap_dex.taker, tx.from)                              AS taker
+    ,kyberswap_dex.maker
+    ,kyberswap_dex.project_contract_address
+    ,kyberswap_dex.tx_hash
+    ,tx.from                                                             AS tx_from
+    ,tx.to                                                               AS tx_to
+    ,kyberswap_dex.trace_address
+    ,kyberswap_dex.evt_index
 FROM kyberswap_dex
-         INNER JOIN {{ source('optimism', 'transactions') }} tx
-ON kyberswap_dex.tx_hash = tx.hash
+INNER JOIN {{ source('optimism', 'transactions') }} tx
+    ON kyberswap_dex.tx_hash = tx.hash
     {% if is_incremental() %}
     AND tx.block_time >= date_trunc("day", now() - interval '1 week')
     {% else %}
     AND tx.block_time >= '{{project_start_date}}'
     {% endif %}
-    LEFT JOIN {{ ref('tokens_erc20_legacy') }} erc20a
+LEFT JOIN {{ ref('tokens_erc20_legacy') }} erc20a
     ON erc20a.contract_address = kyberswap_dex.token_bought_address
     and erc20a.blockchain = 'optimism'
-    LEFT JOIN {{ ref('tokens_erc20_legacy') }} erc20b
+LEFT JOIN {{ ref('tokens_erc20_legacy') }} erc20b
     ON erc20b.contract_address = kyberswap_dex.token_sold_address
     AND erc20b.blockchain = 'optimism'
-    LEFT JOIN {{ source('prices', 'usd') }} p_bought
+LEFT JOIN {{ source('prices', 'usd') }} p_bought
     ON p_bought.minute = date_trunc('minute', kyberswap_dex.block_time)
     AND p_bought.contract_address = kyberswap_dex.token_bought_address
     AND p_bought.blockchain = 'optimism'
@@ -195,7 +195,7 @@ ON kyberswap_dex.tx_hash = tx.hash
     {% else %}
     AND p_bought.minute >= '{{project_start_date}}'
     {% endif %}
-    LEFT JOIN {{ source('prices', 'usd') }} p_sold
+LEFT JOIN {{ source('prices', 'usd') }} p_sold
     ON p_sold.minute = date_trunc('minute', kyberswap_dex.block_time)
     AND p_sold.contract_address = kyberswap_dex.token_sold_address
     AND p_sold.blockchain = 'optimism'
@@ -205,5 +205,5 @@ ON kyberswap_dex.tx_hash = tx.hash
     AND p_sold.minute >= '{{project_start_date}}'
     {% endif %}
 WHERE kyberswap_dex.token_bought_address != '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
-   OR kyberswap_dex.token_sold_address != '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+    OR kyberswap_dex.token_sold_address != '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 ;
