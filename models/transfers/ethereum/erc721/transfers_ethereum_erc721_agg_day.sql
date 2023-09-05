482360fa0aabc3ef50f8d83f9daaaed6fa603f69
primatepngs <142123212+primatepngs@users.noreply.github.com>
2023-09-05 00:40:47 -0700

    Attempt fix for transfers_ethereum_erc721_agg_day.
    Signed-off-by: primatepngs <142123212+primatepngs@users.noreply.github.com>

--- models/transfers/ethereum/erc721/transfers_ethereum_erc721_agg_day.sql
+++ models/transfers/ethereum/erc721/transfers_ethereum_erc721_agg_day.sql
@@ -13,12 +13,12 @@ select
     wallet_address,
     token_address,
     tokenId,
-    CONCAT(wallet_address, '-', date_trunc('day', evt_block_time), '-', CAST(token_address AS VARCHAR), '-', tokenId as unique_transfer_id,
+    CONCAT(wallet_address, '-', date_trunc('day', evt_block_time), '-', CAST(token_address AS VARCHAR), '-', tokenId) as unique_transfer_id,
     SUM(amount) as amount
 from {{ ref('transfers_ethereum_erc721') }}
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
-where evt_block_time >= date_trunc('day', now() - interval '1 week')
+where evt_block_time >= date_trunc('day', cast(now() as timestamp) - interval '1' week)
 {% endif %}
 group by 1,2,3,4,5,6
 -- having sum(amount) = 1 commenting this out as it seems to affect the rolling models
