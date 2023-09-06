5f042b0bc6d9a846cf5f559d77dd5a8f14755eee
longnh <hoanglong180695@gmail.com>
2023-09-07 06:12:36 +0700

    calculate trading profit of bitcoin wallet

--- models/transfers/bitcoin/transfers_bitcoin_satoshi.sql
+++ models/transfers/bitcoin/transfers_bitcoin_satoshi.sql
@@ -46,21 +46,32 @@ with
         and block_time >= date_trunc('day', now() - interval '7' day)
         {% endif %}
     )
+    , transfer_btc as (
+        select any_value(type) as type, 
+            tx_id, index, 'bitcoin' as blockchain, 
+            any_value(wallet_address) as wallet_address, 
+            any_value(block_time) as block_time, 
+            any_value(block_date) as block_date, 
+            any_value(block_height) as block_height, 
+            any_value(amount_raw) as amount_raw
+        from input_transfers group by tx_id, index
+        union
+        select any_value(type) as type, 
+            tx_id, index, 'bitcoin' as blockchain, 
+            any_value(wallet_address) as wallet_address, 
+            any_value(block_time) as block_time, 
+            any_value(block_date) as block_date, 
+            any_value(block_height) as block_height, 
+            any_value(amount_raw) as amount_raw
+        from output_transfers group by tx_id, index
+    )
 
-select any_value(type) as type, 
-    tx_id, index, 'bitcoin' as blockchain, 
-    any_value(wallet_address) as wallet_address, 
-    any_value(block_time) as block_time, 
-    any_value(block_date) as block_date, 
-    any_value(block_height) as block_height, 
-    any_value(amount_raw) as amount_raw
-from input_transfers group by tx_id, index
-union
-select any_value(type) as type, 
-    tx_id, index, 'bitcoin' as blockchain, 
-    any_value(wallet_address) as wallet_address, 
-    any_value(block_time) as block_time, 
-    any_value(block_date) as block_date, 
-    any_value(block_height) as block_height, 
-    any_value(amount_raw) as amount_raw
-from output_transfers group by tx_id, index
+SELECT t.type, t.tx_id, t.index, t.blockchain,
+    t.wallet_address, t.block_time, t.block_date,
+    t.block_height, t.amount_raw,
+    -1 * t.amount_raw * p.price as amount_transfer_usd
+FROM transfer_btc t
+LEFT JOIN {{ source('prices', 'usd') }} p
+    ON date_trunc('minute', t.block_time) = p.minute
+    AND p.symbol='BTC'
+    AND p.blockchain is null
