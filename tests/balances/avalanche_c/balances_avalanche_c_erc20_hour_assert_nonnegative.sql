316c4d3e1472bc6dea2db27171448084482c99cb
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-06 18:05:39 +0100

    update test

--- tests/balances/avalanche_c/balances_avalanche_c_erc20_hour_assert_nonnegative.sql
+++ tests/balances/avalanche_c/balances_avalanche_c_erc20_hour_assert_nonnegative.sql
@@ -1,7 +1,8 @@
 -- copied over from balances ethereum tests 
+
 select amount
-from {{ ref('balances_polygon_erc20_day') }} bal
+from {{ ref('balances_avalanche_c_erc20_hour') }} bal
 where round(amount/power(10, 18), 6) < 0
 and symbol in ('AAVE', 'DAI', 'UNI', 'LINK')
-and bal.day > now() - interval '2' Day
+and bal.hour > now() - interval '1' Day
 
