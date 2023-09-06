6d3a76efe3cd9cd3dffe313e0d0d8e37b3f27511
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-06 05:10:46 +0100

    rewrite asset test

--- tests/balances/bnb/balances_bnb_bep20_hour_assert_nonnegative.sql
+++ tests/balances/bnb/balances_bnb_bep20_hour_assert_nonnegative.sql
@@ -2,7 +2,7 @@
 
 select amount
 from {{ ref('balances_bnb_bep20_hour') }} bal
-where round(amount/power(10, 18), 6) < 0
+where bal.hour > now() - interval '1' Day
+and round(amount/power(10, 18), 6) < 0
 and symbol in ('AAVE', 'DAI', 'UNI', 'LINK')
-and bal.hour > now() - interval '1' Day
 
