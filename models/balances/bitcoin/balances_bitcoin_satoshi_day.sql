598b04243d6f3f1c16db09df251352e798bfb8f1
longnh <hoanglong180695@gmail.com>
2023-09-06 23:30:34 +0700

    Calculate profit of wallet

--- models/balances/bitcoin/balances_bitcoin_satoshi_day.sql
+++ models/balances/bitcoin/balances_bitcoin_satoshi_day.sql
@@ -21,6 +21,7 @@ with
     wallet_address,
     amount_raw,
     amount_raw as amount,
+    amount_transfer_usd,
     day,
     lead(day, 1, date(now())) OVER (PARTITION BY wallet_address ORDER BY day) AS next_day
     FROM {{ ref('transfers_bitcoin_satoshi_rolling_day') }})
@@ -31,7 +32,10 @@ SELECT
     b.wallet_address,
     b.amount_raw,
     b.amount,
-    b.amount * p.price as amount_usd
+    p.price as price_btc,
+    b.amount_transfer_usd,
+    b.amount * p.price as amount_usd,
+    b.amount * p.price + b.amount_transfer_usd as profit
 FROM daily_balances b
 INNER JOIN days d ON b.day <= d.day AND d.day < b.next_day
 LEFT JOIN {{ source('prices', 'usd') }} p
