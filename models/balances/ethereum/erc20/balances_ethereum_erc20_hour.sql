2a016ad8b43d158958bb0aa155210628c7dac640
lajarre <alexandre.hajjar@gmail.com>
2023-09-11 22:05:10 +0100

    Replace large hourly sequence with tomfutago's trick

--- models/balances/ethereum/erc20/balances_ethereum_erc20_hour.sql
+++ models/balances/ethereum/erc20/balances_ethereum_erc20_hour.sql
@@ -10,29 +10,37 @@
 }}
 
 with
-    hours as (
-        select
-            "hour"
-        from
-            unnest (
-                sequence(
-                    cast('2015-01-01' as timestamp),
-                    date_trunc('hour', localtimestamp),
-                    interval '1' hour
-                )
-            ) as _u ("hour")
+years as (
+  select year
+  from (
+    values (
+      sequence(timestamp '2015-01-01', cast(date_trunc('year', now()) as timestamp), interval '1' year)
     )
-
-, hourly_balances as
- (SELECT
-    wallet_address,
-    token_address,
-    amount_raw,
-    amount,
-    hour,
-    symbol,
-    lead(hour, 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY hour) AS next_hour
-    FROM {{ ref('transfers_ethereum_erc20_rolling_hour') }})
+  ) s(year_array)
+  cross join unnest(year_array) as d(year)
+),
+_hours as (
+  select date_add('hour', s.n, y.year) as hour
+  from years y
+  cross join unnest(sequence(1, 9000)) s(n)
+  where s.n <= date_diff('hour', y.year, y.year + interval '1' year)
+),
+hours as (
+  select hour from _hours
+  where hour < localtimestamp
+  order by 1
+),
+hourly_balances as (
+  SELECT
+  wallet_address,
+  token_address,
+  amount_raw,
+  amount,
+  hour,
+  symbol,
+  lead(hour, 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY hour) AS next_hour
+  FROM {{ ref('transfers_ethereum_erc20_rolling_hour') }}
+)
 
 SELECT
     'ethereum' as blockchain,
