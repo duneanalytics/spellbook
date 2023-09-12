b582cff7e95c7bb20ca1b6dca39711fb63b84c61
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-09-12 13:40:02 -0400

    alias

--- models/method_ids/evm_smart_account_method_ids.sql
+++ models/method_ids/evm_smart_account_method_ids.sql
@@ -19,7 +19,7 @@ SELECT NULL as blockchains, method_id, method_descriptor, contract_project
     FROM (values
          (0x6a761202, 'execTransaction', 'Gnosis Safe')
         ,(0x1fad948c, 'handleOps', 'Erc4337')
-        ) a (method_id, method_descriptor)
+        ) a (method_id, method_descriptor, contract_project)
 )
 
 
