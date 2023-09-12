33ab658664923770e8c481d4444286168e075dc2
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-09-12 13:28:02 -0400

    schema

--- models/method_ids/evm_smart_account_method_ids_legacy.sql
+++ models/method_ids/evm_smart_account_method_ids_legacy.sql
@@ -1,7 +1,7 @@
 
 {{ config(
         schema = 'method_ids',
-        tags = ['static'],
+        tags = ['legacy','static'],
         alias = alias('evm_smart_account_method_ids', legacy_model=True),
         post_hook='{{ expose_spells(\'["ethereum","optimism","arbitrum","polygon","gnosis","avalanche_c","fantom","goerli","bnb","base","celo"]\',
                                 "sector",
@@ -11,28 +11,4 @@
 }}
 
 
-{% set all_chains_array = all_evm_mainnets_testnets_chains() %}
-
-WITH aggrregate_methods AS (
--- Generic EVM methods, we read null array as "all"
-SELECT NULL as blockchains, method_id, method_descriptor, contract_project
-    FROM (values
-         ('0x6a761202', 'execTransaction', 'Gnosis Safe')
-        ,('0x1fad948c', 'handleOps', 'ERC4337')
-        ) a (method_id, method_descriptor)
-)
-
-
-SELECT *
-FROM (
-    {% for chain in all_chains_array %}
-    SELECT '{{chain}}' AS blockchain, method_id, method_descriptor, contract_project
-    FROM aggrregate_methods
-    WHERE
-        blockchains IS NULL --If Null, make an entry for all chains
-        OR blockchains = '{{chain}}'
-    {% if not loop.last %}
-    UNION ALL
-    {% endif %}
-    {% endfor %}
-)
+SELECT 1
