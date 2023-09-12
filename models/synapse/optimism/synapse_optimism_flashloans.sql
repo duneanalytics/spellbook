1e4a3d4cbe6756be37afb8f69377d1da4bd13779
henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-12 21:11:29 +0100

    Flashloans Migration - #7 - Synapse (#4300)

--- models/synapse/optimism/synapse_optimism_flashloans.sql
+++ models/synapse/optimism/synapse_optimism_flashloans.sql
@@ -1,5 +1,7 @@
 {{ config(
-    schema = 'synapse_optimism'
+    tags=['dunesql']
+    , partition_by = ['block_month']
+    , schema = 'synapse_optimism'
     , alias = alias('flashloans')
     , materialized = 'incremental'
     , file_format = 'delta'
@@ -14,6 +16,7 @@
 SELECT '{{blockchain}}' AS blockchain
 , 'Synapse' AS project
 , 1 AS version
+, CAST(date_trunc('Month', flash.evt_block_time) as date) as block_month
 , flash.evt_block_time AS block_time
 , flash.evt_block_number AS block_number
 , flash.amount/POWER(10, 18) AS amount
@@ -21,17 +24,17 @@ SELECT '{{blockchain}}' AS blockchain
 , flash.evt_tx_hash AS tx_hash
 , flash.evt_index
 , flash.amountFee/POWER(10, 18) AS fee
-, '{{weth_address}}' AS currency_contract
+, {{weth_address}} AS currency_contract
 , 'ETH' AS currency_symbol
 , flash.receiver AS recipient
 , flash.contract_address
 FROM {{ source('synapse_optimism','SwapFlashLoan_evt_FlashLoan') }} flash
 LEFT JOIN {{ source('prices','usd') }} pu ON pu.blockchain = '{{blockchain}}'
-    AND pu.contract_address = '{{weth_address}}'
+    AND pu.contract_address = {{weth_address}}
     AND pu.minute = date_trunc('minute', flash.evt_block_time)
 	{% if is_incremental() %}
-	AND pu.minute >= date_trunc("day", now() - interval '1 week')
+	AND pu.minute >= date_trunc('day', now() - interval '7' Day)
 	{% endif %}
 {% if is_incremental() %}
-WHERE flash.evt_block_time >= date_trunc("day", now() - interval '1 week')
+WHERE flash.evt_block_time >= date_trunc('day', now() - interval '7' Day)
 {% endif %}
