72c3f70a5839cdce4f46b934b771fb6705c12ca2
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-09-13 20:37:59 +0000

    Update dune_link.sql

--- macros/public/dune_link.sql
+++ macros/public/dune_link.sql
@@ -1,18 +1,18 @@
 {% macro dune_link() %}
-  create or replace function dune_link(user_ STRING, dashboard_name_ STRING, unique_parameter_ STRING, link_name_ STRING)
-  returns STRING 
-  return 
-      SELECT
-          concat(
-              '<a href="'
-              , 'https://dune.com/'
-              , user_
-              , '/'
-              , dashboard_name_
-              , '?'
-              , unique_parameter_
-              , '"target = "_blank">'
-              , link_name_
-              , '</a>'
-          ) AS dune_link
+    create or replace function dune_link(user_ STRING, dashboard_name_ STRING, unique_parameter_ STRING, link_name_ STRING)
+    returns STRING 
+    return 
+        SELECT
+            concat(
+                '<a href="'
+                , 'https://dune.com/'
+                , user_
+                , '/'
+                , dashboard_name_
+                , '?'
+                , unique_parameter_
+                , '"target = "_blank">'
+                , link_name_
+                , '</a>'
+            ) AS dune_link
 {% endmacro %}
