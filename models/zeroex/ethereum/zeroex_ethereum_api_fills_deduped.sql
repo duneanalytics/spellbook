9f6776a937ca48c3c6d1a9774a32ad6c5b67964a
Rantum <rantum@rantum.xyz>
2023-09-13 12:58:23 -0700

    zeroex - refactor eth models [FOR REVIEW] (#4075)
    * initial Socialtokens view
    
    * Rename socialTokens to socialtokens.sql
    
    * Addded new tokens to list
    
    * Update socialtokens.sql
    
    * rmv social tokens file
    
    * add zeroex to dex_aggregator.trades
    
    * revise api_fills model
    
    * add dunesql tag
    
    * update substring to bytearray_substring
    
    * cast dates
    
    * cast dates
    
    * remove floats
    
    * remove floats
    
    * cast 0 as int256
    
    * remove dups
    
    * add 0xdac17f958d2ee523a2206206994597c13d831ec7
    
    * subtraction underflow fix
    
    * cast raw amounts
    
    * update schema
    
    * update tag enclosures in schema
    
    * change check_seed_legacy to check_seed
    
    * re-add seed file
    
    * remove changes to profile
    
    * add see schema
    
    * revise dates on seed file
    
    * add deduped model
    
    * swap_flag
    
    * add native_fills model
    
    * change check_seed_legacy to check_seed on native_fills model
    
    * remove seed test
    
    * remove seed test
    
    * remove numeric
    
    * name protocol_fee_paid_eth
    
    * try cast block_time as timestamp
    
    * update test
    
    * cast timestamps
    
    * update native fills
    
    * update schema
    
    * revise poly fills
    
    * revise deduped
    
    * add schema file
    
    * remoe schema
    
    * remove changes to poly
    
    * trace_Address, change to block_month
    
    * re-add poly schema
    
    * uint256
    
    * uint256
    
    * remove
    
    * uint256
    
    * uint256
    
    * uint256
    
    * unint256
    
    * uint 256

--- models/zeroex/ethereum/zeroex_ethereum_api_fills_deduped.sql
+++ models/zeroex/ethereum/zeroex_ethereum_api_fills_deduped.sql
@@ -1,7 +1,8 @@
 {{  config(
+        tags=['dunesql'],
         alias = alias('api_fills_deduped'),
         materialized='incremental',
-        partition_by = ['block_date'],
+        partition_by = ['block_month'],
         unique_key = ['block_date', 'tx_hash', 'evt_index'],
         on_schema_change='sync_all_columns',
         file_format ='delta',
@@ -22,12 +23,12 @@ AS
            , *
     FROM {{ ref('zeroex_ethereum_api_fills') }}
     WHERE 1=1
-    AND swap_flag = 1
+    AND swap_flag = true 
     {% if is_incremental() %}
-    AND block_time >= date_trunc('day', now() - interval '1 week')
+    AND block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
     {% if not is_incremental() %}
-    AND block_time >= '{{zeroex_v3_start_date}}'
+    AND block_time >= cast('{{zeroex_v3_start_date}}' as date)
     {% endif %}    
 )
 , fills_first_last
@@ -70,13 +71,14 @@ SELECT  a.blockchain
       , cast(null as varchar(10)) as version
       , a.block_date
       , a.block_time
+      , a.block_month
       , b.taker_symbol AS taker_symbol
       , b.maker_symbol AS maker_symbol
       , CASE WHEN lower(b.taker_symbol) > lower(b.maker_symbol) THEN concat(b.maker_symbol, '-', b.taker_symbol) ELSE concat(b.taker_symbol, '-', b.maker_symbol) END AS token_pair
       , b.taker_token_amount
       , b.maker_token_amount
-      , CAST(b.taker_token_amount_raw AS DECIMAL(38,0)) AS taker_token_amount_raw
-      , CAST(b.maker_token_amount_raw AS DECIMAL(38,0)) AS maker_token_amount_raw
+      , CAST(b.taker_token_amount_raw AS uint256) AS taker_token_amount_raw
+      , CAST(b.maker_token_amount_raw AS uint256) AS maker_token_amount_raw
       , a.volume_usd
       , b.taker_token
       , b.maker_token
@@ -87,12 +89,11 @@ SELECT  a.blockchain
       , a.tx_from
       , a.tx_to
       , b.evt_index
-      , CAST(ARRAY(-1) as array<bigint>) as trace_address
+      , ARRAY[-1] as trace_address
       , a.type
       , a.swap_flag
       , b.fills_within
       , a.contract_address 
 FROM fills_with_tx_fill_number a
 INNER JOIN deduped_bridge_fills b
-    ON (a.tx_hash = b.tx_hash AND a.evt_index = b.evt_index)
-;
+    ON a.tx_hash = b.tx_hash AND a.evt_index = b.evt_index
