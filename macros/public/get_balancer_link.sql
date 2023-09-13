0db9c14e24a3fd54fb24a4708107479205497ac2
viniabussafi <vinicius@balancerlabs.dev>
2023-09-13 13:53:46 +0200

    minor fix for avalanche and gnosis

--- macros/public/get_balancer_link.sql
+++ macros/public/get_balancer_link.sql
@@ -1,5 +1,10 @@
 {% macro get_balancer_link() %}
       create or replace function get_balancer_link(chain_ STRING, address STRING)
       returns STRING 
-      return concat('<a href="','https://app.balancer.fi/#/',chain_,'/pool/', address, '"target ="_blank">', 'balancer')
+      return 
+      SELECT
+      CASE WHEN 
+      'avalanche_c' = chain_ THEN concat('<a href="','https://app.balancer.fi/#/avalanche/pool/', address, '"target ="_blank">', 'balancer')
+      'gnosis' = chain_ THEN concat('<a href="','https://app.balancer.fi/#/gnosis-chain/pool/', address, '"target ="_blank">', 'balancer')
+      ELSE concat('<a href="','https://app.balancer.fi/#/',chain_,'/pool/', address, '"target ="_blank">', 'balancer')
 {% endmacro %}
