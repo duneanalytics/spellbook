4f8d6a5c3501a3536c7f1ce5b3d0f3898b3f96f4
antonio-mendes <soeiromendes.antonio@gmail.com>
2023-09-13 17:14:25 +0100

    fix testts

--- tests/balances/bnb/balances_bnb_bep20_day_assert_nonnegative.sql
+++ tests/balances/bnb/balances_bnb_bep20_day_assert_nonnegative.sql
@@ -1,7 +1,7 @@
 -- copied over from balances ethereum tests 
 select amount
 from {{ ref('balances_bnb_bep20_day') }} bal
-where bal.day > now() - interval '2' Day
+where bal.block_day > now() - interval '2' Day
 and round(amount/power(10, 18), 6) < 0
 and symbol in ('AAVE', 'DAI', 'UNI', 'LINK')
 
--- tests/balances/bnb/balances_bnb_bep20_hour_assert_nonnegative.sql
+++ tests/balances/bnb/balances_bnb_bep20_hour_assert_nonnegative.sql
@@ -2,7 +2,7 @@
 
 select amount
 from {{ ref('balances_bnb_bep20_hour') }} bal
-where bal.hour > now() - interval '1' Day
+where bal.block_hour > now() - interval '1' Day
 and round(amount/power(10, 18), 6) < 0
 and symbol in ('AAVE', 'DAI', 'UNI', 'LINK')
 
