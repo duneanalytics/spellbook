eab6452504282b761df67d1d05ffeaa15036843e
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-09-13 20:53:01 +0000

    Update dune_link.sql
    Changes

--- macros/public/dune_link.sql
+++ macros/public/dune_link.sql
@@ -1,16 +1,22 @@
 {% macro dune_link() %}
-    create or replace function dune_link(user_ STRING, dashboard_name_ STRING, unique_parameter_ STRING, link_name_ STRING)
+    create or replace function 
+        dune_link(
+            asset_id_ STRING -- This is inputted as username/dashboard OR queries/1234567
+            , parameter_id_ STRING -- This is the special identifier for your parameter found in the URL between either (? or &) and =
+            , parameter_ STRING -- The input you are passing to the parameter, e.g. an EOA address
+            , link_name_ STRING -- The string you would like to have represent your URL.
+        )
     returns STRING 
     return 
         SELECT
             concat(
                 '<a href="'
                 , 'https://dune.com/'
-                , user_
-                , '/'
-                , dashboard_name_
+                , asset_id
                 , '?'
-                , unique_parameter_
+                , parameter_id_
+                , '='
+                , parameter_
                 , '"target = "_blank">'
                 , link_name_
                 , '</a>'
