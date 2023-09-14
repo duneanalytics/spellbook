17ac2ccc330e3d4b9f2b1d0c68307ac049395bd4
m-r-g-t <m-r-g-t@defi.sucks>
2023-09-14 23:10:09 +0100

    feat_keep3r

--- models/keep3r_network/keep3r_network_unbondLiquidityFromJob.sql
+++ models/keep3r_network/keep3r_network_unbondLiquidityFromJob.sql
@@ -8,6 +8,7 @@
                                  \'["0xr3x"]\') }}'
 ) }}
 
+
     SELECT
       call_block_time as block_time,
       (- CAST(amount as double)) / 1e18 as amount,
@@ -19,51 +20,6 @@
         {{ source(
             'keep3r_network_ethereum',
             'Keep3rV1_call_unbondLiquidityFromJob'
-        ) }}
-    WHERE
-      call_success = true
-    UNION ALL
-    SELECT
-      call_block_time as block_time,
-      (- CAST(_amount as double)) / 1e18 as amount,
-      _job as job,
-      _liquidity as liquidity,
-      'unbondLiquidityFromJob' as event,
-      'ethereum' as blockchain
-    FROM
-        {{ source(
-            'keep3r_network_ethereum',
-            'Keep3r_v2_call_unbondLiquidityFromJob'
-        ) }}
-    WHERE
-      call_success = true
-    UNION ALL
-    SELECT
-      call_block_time as block_time,
-      (- CAST(_amount as double)) / 1e18 as amount,
-      _job as job,
-      _liquidity as liquidity,
-      'unbondLiquidityFromJob' as event,
-      'optimism' as blockchain
-    FROM
-        {{ source(
-            'keep3r_network_optimism',
-            'Keep3rSidechain_call_unbondLiquidityFromJob'
-        ) }}
-    WHERE
-      call_success = true
-    UNION ALL
-    SELECT
-      call_block_time as block_time,
-      (- CAST(_amount as double)) / 1e18 as amount,
-      _job as job,
-      _liquidity as liquidity,
-      'unbondLiquidityFromJob' as event,
-      'polygon' as blockchain
-    FROM
-        {{ source(
-            'keep3r_network_polygon',
-            'Keep3rSidechain_call_unbondLiquidityFromJob'
         ) }}
     WHERE
       call_success = true
