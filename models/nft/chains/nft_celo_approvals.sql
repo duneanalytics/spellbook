c51e706af7dc37b8da6cf9ddd29cbc355e0f9e5b
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-09-14 15:24:32 +0100

    test without incremental_predicates, added token_standard to uniqueness

--- models/nft/chains/nft_celo_approvals.sql
+++ models/nft/chains/nft_celo_approvals.sql
@@ -7,8 +7,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        incremental_predicates = ['DBT_INTERNAL_DEST.block_time >= date_trunc(\'day\', now() - interval \'7\' day)'],
-        unique_key = ['tx_hash', 'evt_index', 'token_id']
+        unique_key = ['tx_hash', 'evt_index', 'token_standard', 'token_id']
     )
 }}
 
