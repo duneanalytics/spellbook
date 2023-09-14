d94e889009ba348fed7f715204a114f0bde5cd81
lajarre <alexandre.hajjar@gmail.com>
2023-09-15 00:12:26 +0100

    Make ethereum erc20 transfers incremental materialized and partition by month

--- models/transfers/ethereum/erc20/transfers_ethereum_erc20.sql
+++ models/transfers/ethereum/erc20/transfers_ethereum_erc20.sql
@@ -1,6 +1,11 @@
 {{ config(
         tags = ['dunesql'],
-        materialized='view', alias = alias('erc20'),
+        materialized='incremental',
+        partition_by = ['block_month'],
+        file_format='delta',
+        incremental_strategy='merge',
+        unique_key=['unique_transfer_id', ],
+        alias = alias('erc20'),
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "sector",
                                     "transfers",
@@ -16,6 +21,9 @@ with
             cast(value as double) as amount_raw
         from
             {{ source('erc20_ethereum', 'evt_transfer') }}
+        {% if is_incremental %}
+            where evt_block_time >= date_trunc('day', now() - interval '3' day)
+        {% endif %}
     )
 
     ,
@@ -28,6 +36,9 @@ with
         - cast(value as double) as amount_raw
         from
             {{ source('erc20_ethereum', 'evt_transfer') }}
+        {% if is_incremental %}
+            where evt_block_time >= date_trunc('day', now() - interval '3' day)
+        {% endif %}
     )
 
     ,
@@ -40,6 +51,9 @@ with
             cast(wad as double) as amount_raw
         from
             {{ source('zeroex_ethereum', 'weth9_evt_deposit') }}
+        {% if is_incremental %}
+            where evt_block_time >= date_trunc('day', now() - interval '3' day)
+        {% endif %}
     )
 
     ,
@@ -52,16 +66,19 @@ with
             - cast(wad as double) as amount_raw
         from
             {{ source('zeroex_ethereum', 'weth9_evt_withdrawal') }}
+        {% if is_incremental %}
+            where evt_block_time >= date_trunc('day', now() - interval '3' day)
+        {% endif %}
     )
     
-select unique_transfer_id, 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, amount_raw
+select unique_transfer_id, 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, amount_raw, CAST(date_trunc('month', evt_block_time) as date) as block_month
 from sent_transfers
 union
-select unique_transfer_id, 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, amount_raw
+select unique_transfer_id, 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, amount_raw, CAST(date_trunc('month', evt_block_time) as date) as block_month
 from received_transfers
 union
-select unique_transfer_id, 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, amount_raw
+select unique_transfer_id, 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, amount_raw,CAST(date_trunc('month', evt_block_time) as date) as block_month
 from deposited_weth
 union
-select unique_transfer_id, 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, amount_raw
+select unique_transfer_id, 'ethereum' as blockchain, wallet_address, token_address, evt_block_time, amount_raw,CAST(date_trunc('month', evt_block_time) as date) as block_month
 from withdrawn_weth
