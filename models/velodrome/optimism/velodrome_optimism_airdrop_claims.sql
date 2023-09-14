90228e5faf98a7da9c85514d291b76dccdbaad40
henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-14 13:47:38 +0100

    SPE-58 Migrate Dex Prices & Update Airdrop Claim Models - Ready for review (#4332)
    * Update Airdrop Claim Models
    
    * trigger main model since incremental
    
    * add dex prices
    
    ---------
    
    Co-authored-by: Alex Courouble <56831527+couralex6@users.noreply.github.com>

--- models/velodrome/optimism/velodrome_optimism_airdrop_claims.sql
+++ models/velodrome/optimism/velodrome_optimism_airdrop_claims.sql
@@ -1,5 +1,7 @@
 {{
     config(
+        tags = ['dunesql'],
+        partition_by = ['block_month'],
         schema = 'velodrome_ethereum',
         alias = alias('airdrop_claims'),
         materialized = 'incremental',
@@ -22,10 +24,11 @@ WITH price_bounds AS (
     , MAX_BY(median_price, hour) AS max_price
     FROM {{ ref('dex_prices') }}
     WHERE blockchain = 'optimism'
-    AND contract_address='{{velo_token_address}}'
+    AND contract_address= {{velo_token_address}}
     )
 
 SELECT 'optimism' AS blockchain
+, CAST(date_trunc('month', t.evt_block_time) as date) as block_month
 , t.evt_block_time AS block_time
 , t.evt_block_number AS block_number
 , 'Velodrome' AS project
@@ -33,22 +36,22 @@ SELECT 'optimism' AS blockchain
 , t.to AS recipient
 , t.contract_address
 , t.evt_tx_hash AS tx_hash
-, CAST(t.amount AS DECIMAL(38,0)) AS amount_raw
+, t.amount AS amount_raw
 , CAST(t.amount/POWER(10, 18) AS double) AS amount_original
 , CASE WHEN t.evt_block_time >= (SELECT min_hour FROM price_bounds) AND t.evt_block_time <= (SELECT max_hour FROM price_bounds) THEN CAST(pu.median_price*t.amount/POWER(10, 18) AS double)
     WHEN t.evt_block_time < (SELECT min_hour FROM price_bounds) THEN CAST((SELECT min_price FROM price_bounds)*t.amount/POWER(10, 18) AS double)
     WHEN t.evt_block_time > (SELECT max_hour FROM price_bounds) THEN CAST((SELECT max_price FROM price_bounds)*t.amount/POWER(10, 18) AS double)
     END AS amount_usd
-, '{{velo_token_address}}' AS token_address
+, {{velo_token_address}} AS token_address
 , 'VELO' AS token_symbol
 , t.evt_index
 FROM {{ source('velodrome_optimism', 'MerkleClaim_evt_Claim') }} t
 LEFT JOIN {{ ref('dex_prices') }} pu ON pu.blockchain = 'optimism'
-    AND pu.contract_address='{{velo_token_address}}'
+    AND pu.contract_address= {{velo_token_address}}
     AND pu.hour = date_trunc('hour', t.evt_block_time)
     {% if is_incremental() %}
-    AND pu.hour >= date_trunc("day", now() - interval '1 week')
+    AND pu.hour >= date_trunc('day', now() - interval '7' Day)
     {% endif %}
 {% if is_incremental() %}
-WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
+WHERE t.evt_block_time >= date_trunc('day', now() - interval '7' Day)
 {% endif %}
