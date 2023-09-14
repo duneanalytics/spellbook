c40cbc5e364d18cdc7873929fca1da03803439f8
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-09-14 15:50:38 +0100

    reverted incremental_predicate, removed token_id from unique key

--- models/nft/chains/nft_celo_approvals.sql
+++ models/nft/chains/nft_celo_approvals.sql
@@ -7,7 +7,8 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['tx_hash', 'evt_index', 'token_standard', 'token_id']
+        incremental_predicates = ['DBT_INTERNAL_DEST.block_time >= date_trunc(\'day\', now() - interval \'7\' day)'],
+        unique_key = ['tx_hash', 'evt_index']
     )
 }}
 
