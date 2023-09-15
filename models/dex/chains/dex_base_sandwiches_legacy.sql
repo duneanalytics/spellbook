98012b0e0386577cf18d4776d96becab0a9a5d09
Hildebert Moulié <h.moulie@gmail.com>
2023-09-15 17:33:22 +0800

    fix

--- models/dex/chains/dex_base_sandwiches_legacy.sql
+++ models/dex/chains/dex_base_sandwiches_legacy.sql
@@ -1,17 +1,12 @@
 {{ config(
-	tags=['legacy'],
-
+	    tags=['legacy'],
         schema = 'dex_base',
         alias = alias('sandwiches', legacy_model=True),
-        partition_by = ['block_date'],
-        materialized = 'incremental',
-        file_format = 'delta',
-        incremental_strategy = 'merge',
-        unique_key = ['sandwiched_pool', 'frontrun_tx_hash', 'frontrun_taker', 'frontrun_index', 'currency_address'],
         post_hook='{{ expose_spells(\'["base"]\',
                                 "sector",
                                 "dex",
                                 \'["hildobby"]\') }}'
         )
 }}
+
 SELECT 1
