1ea2530e7aadadaf784ed3cd4c1afa1238f93978
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2023-09-15 14:15:55 +0000

    Update dune_link.sql
    Changing to match what Alex said would work.

--- macros/public/dune_link.sql
+++ macros/public/dune_link.sql
@@ -1,24 +1,14 @@
-{% macro dune_link() %}
-    create or replace function 
-        dune_link(
-            asset_id_ STRING -- This is inputted as username/dashboard OR queries/1234567
-            , parameter_id_ STRING -- This is the special identifier for your parameter found in the URL between either (? or &) and =
-            , parameter_ STRING -- The input you are passing to the parameter, e.g. an EOA address
-            , link_name_ STRING -- The string you would like to have represent your URL.
-        )
-    returns STRING 
-    return 
-        SELECT
-            concat(
-                '<a href="'
-                , 'https://dune.com/'
-                , asset_id
-                , '?'
-                , parameter_id_
-                , '='
-                , parameter_
-                , '"target = "_blank">'
-                , link_name_
-                , '</a>'
-            ) AS dune_link
+{% macro dune_link(asset_id, parameter_id, parameter_input, link_name) %}
+     {{
+        '<a href="'
+        + 'https://dune.com/'
+        + asset_id
+        + '?'
+        + parameter_id
+        + '='
+        + parameter_input
+        + '"target = "_blank">'
+        + link_name
+        + '</a>'
+    }}
 {% endmacro %}
