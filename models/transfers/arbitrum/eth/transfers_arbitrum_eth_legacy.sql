29cfdbf3c8c2df8fdd246555cbdd6161de8a8b8a
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-15 17:50:41 +0100

    remove eth and create erc20 balances

--- models/transfers/arbitrum/eth/transfers_arbitrum_eth_legacy.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-        tags = ['legacy'],
-        alias = alias('eth', legacy_model=True)
-        )
-}}
-
-SELECT 
-1 AS DUMMY 
--- models/transfers/arbitrum/eth/transfers_arbitrum_eth_rolling_hour_legacy.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-        tags = ['legacy'],
-        alias = alias('eth_rolling_hour', legacy_model=True)
-        )
-}}
-
-SELECT 
-1 AS DUMMY 
