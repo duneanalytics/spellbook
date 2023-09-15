29c0f53b7f622fc06468a70b0d0725b067bd2405
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-09-15 11:32:39 +0100

    missing comma

--- models/balances/celo/erc20/balances_celo_erc20_latest.sql
+++ models/balances/celo/erc20/balances_celo_erc20_latest.sql
@@ -17,7 +17,7 @@ select
   symbol,
   max_by(amount_raw, block_hour) as amount_raw,
   max_by(amount, block_hour) as amount,
-  max_by(amount_usd, block_hour) as amount_usd
+  max_by(amount_usd, block_hour) as amount_usd,
   now() as last_updated
 from {{ ref('balances_celo_erc20_hour') }}
 group by 1,2,3,4
