8d5df6ab22628766689fea389aa3f1f5c4a2c950
tomfutago <tomfutago@gmail.com>
2023-09-16 10:58:07 +0100

    token amount decimals def 18, added dex trade seed

--- models/sushiswap/celo/sushiswap_celo_trades.sql
+++ models/sushiswap/celo/sushiswap_celo_trades.sql
@@ -15,10 +15,10 @@
 
 with dexs as (
     -- Sushiswap
-    SELECT
+    select
         t.evt_block_time as block_time,
         t.to as taker,
-        CAST(NULL AS VARBINARY)as maker,
+        cast(null as varbinary) as maker,
         case when amount0Out = UINT256 '0' then amount1Out else amount0Out end as token_bought_amount_raw,
         case when amount0In = UINT256 '0' then amount1In else amount0In end as token_sold_amount_raw,
         null as amount_usd,
@@ -27,14 +27,12 @@ with dexs as (
         t.contract_address as project_contract_address,
         t.evt_tx_hash as tx_hash,
         t.evt_index
-    FROM
-        {{ source('sushi_celo', 'Pair_evt_Swap') }} t
-        inner join {{ source('sushi_celo', 'SushiV2Factory_evt_PairCreated') }} f 
-            on f.pair = t.contract_address
+    from {{ source('sushi_celo', 'Pair_evt_Swap') }} t
+        inner join {{ source('sushi_celo', 'SushiV2Factory_evt_PairCreated') }} f on t.contract_address = f.pair
     {% if is_incremental() %}
-    WHERE t.evt_block_time >= date_trunc('day', now() - interval '7' day)
+    where t.evt_block_time >= date_trunc('day', now() - interval '7' day)
     {% else %}
-    WHERE t.evt_block_time >= TIMESTAMP '{{project_start_date}}'
+    where t.evt_block_time >= timestamp '{{project_start_date}}'
     {% endif %}
 )
 select
@@ -50,29 +48,29 @@ select
         when lower(erc20a.symbol) > lower(erc20b.symbol) then concat(erc20b.symbol, '-', erc20a.symbol)
         else concat(erc20a.symbol, '-', erc20b.symbol)
     end as token_pair,
-    dexs.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
-    dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount,
-    dexs.token_bought_amount_raw  AS token_bought_amount_raw,
-    dexs.token_sold_amount_raw  AS token_sold_amount_raw,
+    dexs.token_bought_amount_raw / power(10, coalesce(erc20a.decimals, 18)) as token_bought_amount,
+    dexs.token_sold_amount_raw / power(10, coalesce(erc20b.decimals, 18)) as token_sold_amount,
+    dexs.token_bought_amount_raw  as token_bought_amount_raw,
+    dexs.token_sold_amount_raw  as token_sold_amount_raw,
     coalesce(
         dexs.amount_usd
         ,(dexs.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
         ,(dexs.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
-    ) AS amount_usd,
+    ) as amount_usd,
     dexs.token_bought_address,
     dexs.token_sold_address,
-    coalesce(dexs.taker, tx."from") AS taker,
+    coalesce(dexs.taker, tx."from") as taker,
     dexs.maker,
     dexs.project_contract_address,
     dexs.tx_hash,
-    tx."from" AS tx_from,
-    tx.to AS tx_to,
+    tx."from" as tx_from,
+    tx.to as tx_to,
     dexs.evt_index
 from dexs
 inner join {{ source('celo', 'transactions') }} tx
     on dexs.tx_hash = tx.hash
     {% if not is_incremental() %}
-    and tx.block_time >= TIMESTAMP '{{project_start_date}}'
+    and tx.block_time >= timestamp '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     and tx.block_time >= date_trunc('day', now() - interval '7' day)
@@ -88,7 +86,7 @@ left join {{ source('prices', 'usd') }} p_bought
     and p_bought.contract_address = dexs.token_bought_address
     and p_bought.blockchain = 'celo'
     {% if not is_incremental() %}
-    and p_bought.minute >= TIMESTAMP '{{project_start_date}}'
+    and p_bought.minute >= timestamp '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     and p_bought.minute >= date_trunc('day', now() - interval '7' day)
@@ -98,7 +96,7 @@ left join {{ source('prices', 'usd') }} p_sold
     and p_sold.contract_address = dexs.token_sold_address
     and p_sold.blockchain = 'celo'
     {% if not is_incremental() %}
-    and p_sold.minute >= TIMESTAMP '{{project_start_date}}'
+    and p_sold.minute >= timestamp '{{project_start_date}}'
     {% endif %}
     {% if is_incremental() %}
     and p_sold.minute >= date_trunc('day', now() - interval '7' day)
