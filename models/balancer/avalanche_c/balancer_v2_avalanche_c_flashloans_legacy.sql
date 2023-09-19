13facf9b2c4285fbd4da0c2c8e7744dc935ccf76
viniabussafi <vinicius@balancerlabs.dev>
2023-09-19 14:28:37 +0200

    legacy models fix

--- models/balancer/avalanche_c/balancer_v2_avalanche_c_flashloans_legacy.sql
+++ models/balancer/avalanche_c/balancer_v2_avalanche_c_flashloans_legacy.sql
@@ -1,16 +1,7 @@
 {{ config(
 	tags=['legacy'],
-	
       schema = 'balancer_v2_avalanche_c'
       , alias = alias('flashloans', legacy_model=True)
-      , materialized = 'incremental'
-      , file_format = 'delta'
-      , incremental_strategy = 'merge'
-      , unique_key = ['tx_hash', 'evt_index']
-      , post_hook='{{ expose_spells(\'["avalanche_c"]\',
-                                  "project",
-                                  "balancer_v2",
-                                  \'["hildobby"]\') }}'
   )
 }}
 
--- models/balancer/base/balancer_v2_base_flashloans_legacy.sql
+++ models/balancer/base/balancer_v2_base_flashloans_legacy.sql
@@ -1,16 +1,7 @@
 {{ config(
 	tags=['legacy'],
-	
       schema = 'balancer_v2_base'
       , alias = alias('flashloans', legacy_model=True)
-      , materialized = 'incremental'
-      , file_format = 'delta'
-      , incremental_strategy = 'merge'
-      , unique_key = ['tx_hash', 'evt_index']
-      , post_hook='{{ expose_spells(\'["base"]\',
-                                  "project",
-                                  "balancer_v2",
-                                  \'["hildobby"]\') }}'
   )
 }}
 
