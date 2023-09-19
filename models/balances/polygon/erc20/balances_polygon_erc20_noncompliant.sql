76bd1b8d60ba538a49006266f9ce1777912ee6ac
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-19 01:16:51 +0100

    Add balances macro

--- models/balances/polygon/erc20/balances_polygon_erc20_noncompliant.sql
+++ models/balances/polygon/erc20/balances_polygon_erc20_noncompliant.sql
@@ -6,8 +6,9 @@
         )
 }}
 
-SELECT  
-    DISTINCT token_address
-FROM 
-{{ ref('transfers_polygon_erc20_rolling_day') }}
-WHERE round(amount/power(10, 18), 6) < -0.001
+
+{{
+    balances_fungible_noncompliant(
+        transfers_rolling_day = ref('transfers_polygon_erc20_rolling_day')
+    )
+}}
