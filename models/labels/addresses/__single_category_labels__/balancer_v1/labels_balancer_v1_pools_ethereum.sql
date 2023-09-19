056da73824248a2a57015da4d4e5544997721a9c
Huang Geyang <Sukebeta@outlook.com>
2023-09-20 04:39:53 +0800

    Migrate labels with dependencies solved (#4371)
    * Migrate labels_balancer_v1_pools
    
    * Migrate labels_balancer_v2_gauges
    
    * Migrate labels_project_wallets
    
    * Migrate labels_safe
    
    * Migrate labels_cex_users
    
    * Migrate labels_op_retropgf
    
    * Fix labels_balancer_v2_gauges_arbitrum
    
    * Remove prod_exclude tag
    
    * Cast denorm to uint256
    
    * Fix uint256 comparison
    
    * Add distinct

--- models/labels/addresses/__single_category_labels__/balancer_v1/labels_balancer_v1_pools_ethereum.sql
+++ models/labels/addresses/__single_category_labels__/balancer_v1/labels_balancer_v1_pools_ethereum.sql
@@ -1,4 +1,5 @@
 {{config(
+    tags=['dunesql'],
     alias = alias('balancer_v1_pools_ethereum'),
     materialized = 'incremental',
     file_format = 'delta',
@@ -13,63 +14,72 @@
 
 WITH events AS (
     -- binds
-    SELECT call_block_number AS block_number, 
-    contract_address AS pool, token, denorm
+    SELECT call_block_number AS block_number,
+           contract_address  AS pool,
+           token,
+           denorm
     FROM {{ source('balancer_v1_ethereum', 'BPool_call_bind') }}
     WHERE call_success
     {% if is_incremental() %}
-        AND call_block_time >= date_trunc("day", now() - interval '1 week')
+        AND call_block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 
     UNION all
 
     -- rebinds
-    SELECT call_block_number AS block_number, 
-    contract_address AS pool, token, denorm
+    SELECT call_block_number AS block_number,
+           contract_address  AS pool,
+           token,
+           denorm
     FROM {{ source('balancer_v1_ethereum', 'BPool_call_rebind') }}
     WHERE call_success
     {% if is_incremental() %}
-        AND call_block_time >= date_trunc("day", now() - interval '1 week')
+        AND call_block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 
     UNION all
-    
+
     -- unbinds
-    SELECT call_block_number AS block_number, 
-    contract_address AS pool, token, 0 AS denorm
+    SELECT call_block_number AS block_number,
+            contract_address AS pool,
+            token,
+            uint256 '0' AS denorm
     FROM {{ source('balancer_v1_ethereum', 'BPool_call_unbind') }}
     WHERE call_success
     {% if is_incremental() %}
-        AND call_block_time >= date_trunc("day", now() - interval '1 week')
+        AND call_block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 ),
 
 state_with_gaps AS (
-    SELECT events.block_number, events.pool, events.token, CAST(events.denorm AS double),
+    SELECT events.block_number
+           , events.pool
+           , events.token
+           , CAST(events.denorm AS uint256) AS denorm,
     LEAD(events.block_number, 1, 99999999) OVER (PARTITION BY events.pool, events.token ORDER BY events.block_number) AS next_block_number
     FROM events
 ),
 
 settings AS (
-    SELECT pool, 
-    coalesce(t.symbol,'?') AS symbol, 
+    SELECT pool,
+    coalesce(t.symbol,'?') AS symbol,
     denorm,
     next_block_number
     FROM state_with_gaps s
     LEFT JOIN {{ ref('tokens_ethereum_erc20') }} t ON s.token = t.contract_address
     WHERE next_block_number = 99999999
-    AND denorm > 0
+    AND denorm > uint256 '0'
 ),
 
 final AS (
-    SELECT 
+    SELECT
       'ethereum' AS blockchain,
       pool AS address,
-      lower(concat(array_join(collect_list(symbol), '/'), ' ', array_join(collect_list(cast(norm_weight AS string)), '/'))) AS name,
+      lower(concat(array_join(array_agg(symbol), '/'), ' ', array_join(array_agg(cast(norm_weight AS varchar)), '/'))) AS name,
       'balancer_v1_pool' AS category,
       'balancerlabs' AS contributor,
       'query' AS source,
-      timestamp('2023-02-02') AS created_at,
+      timestamp '2023-02-02' AS created_at,
       now() AS updated_at,
       'balancer_v1_pools_ethereum' AS model_name,
       'identifier' as label_type
