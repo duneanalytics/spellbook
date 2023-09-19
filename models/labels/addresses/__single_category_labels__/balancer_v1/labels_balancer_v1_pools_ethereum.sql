feae7c80c306e8beb247ed7a68f03e6ad6a2aac1
Huang Geyang <Sukebeta@outlook.com>
2023-09-20 00:00:50 +0800

    Fix uint256 comparison

--- models/labels/addresses/__single_category_labels__/balancer_v1/labels_balancer_v1_pools_ethereum.sql
+++ models/labels/addresses/__single_category_labels__/balancer_v1/labels_balancer_v1_pools_ethereum.sql
@@ -68,7 +68,7 @@ settings AS (
     FROM state_with_gaps s
     LEFT JOIN {{ ref('tokens_ethereum_erc20') }} t ON s.token = t.contract_address
     WHERE next_block_number = 99999999
-    AND denorm > 0
+    AND denorm > uint256 '0'
 ),
 
 final AS (
