17ab944c72e766558ef41f015eedbd207e1c9560
henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-20 13:08:34 +0100

    Create `erc20_transfers`  & `balances_fungible`  macros - Ready for review  (#4366)
    create erc20_transfers and balances_fungible macros

---
+++ macros/models/_sector/transfers/erc20/transfers_erc20_rolling_day.sql
@@ -0,0 +1,16 @@
+{% macro transfers_erc20_rolling_day(transfers_erc20_agg_day) %}
+
+        SELECT
+            blockchain, 
+            block_day, 
+            wallet_address,
+            token_address,
+            symbol, 
+            CAST(NOW() as timestamp) as last_updated,
+            ROW_NUMBER() OVER (PARTITION BY token_address, wallet_address ORDER BY block_day DESC) as recency_index,
+            SUM(amount_raw) OVER (PARTITION BY token_address, wallet_address ORDER BY block_day) as amount_raw, 
+            SUM(amount) OVER (PARTITION BY token_address, wallet_address ORDER BY block_day) as amount          
+        FROM 
+         {{ transfers_erc20_agg_day }}
+
+{% endmacro %}
