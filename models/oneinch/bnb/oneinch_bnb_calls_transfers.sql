7ed5942f52202d5cf4d197d23546c6ac099370d8
grkhr <eduardgorkh@gmail.com>
2023-09-20 13:04:35 +0200

    try year

--- models/oneinch/bnb/oneinch_bnb_calls_transfers.sql
+++ models/oneinch/bnb/oneinch_bnb_calls_transfers.sql
@@ -1,6 +1,6 @@
 {% set blockchain = 'bnb' %}
 -- {% set project_start_date_str = '2021-02-18' %}
-{% set project_start_date_str = '2023-08-18' %}
+{% set project_start_date_str = '2022-08-18' %}
 
 
 
