bb66351cb11c80d79248b1ec63f65a07a52f6eb7
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-20 16:30:05 +0100

    fix

--- models/balances/bnb/bnb/balances_bnb_bnb_day.sql
+++ models/balances/bnb/bnb/balances_bnb_bnb_day.sql
@@ -13,7 +13,6 @@
     balances_fungible_day(
         blockchain = 'bnb',
         first_transaction_date = '2020-08-29',
-        transfers_rolling_day = ref('transfers_bnb_bnb_rolling_day'),
-        balances_noncompliant = ref('balances_bnb_bnb_noncompliant')
+        transfers_rolling_day = ref('transfers_bnb_bnb_rolling_day')
     )
 }}
