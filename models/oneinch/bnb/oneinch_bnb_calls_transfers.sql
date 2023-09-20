2a34660e01cc15aa8397b07d029563256581e4ab
grkhr <eduardgorkh@gmail.com>
2023-09-20 14:29:49 +0200

    try full bnb

--- models/oneinch/bnb/oneinch_bnb_calls_transfers.sql
+++ models/oneinch/bnb/oneinch_bnb_calls_transfers.sql
@@ -1,6 +1,6 @@
 {% set blockchain = 'bnb' %}
 -- {% set project_start_date_str = '2021-02-18' %}
-{% set project_start_date_str = '2021-08-18' %}
+{% set project_start_date_str = '2021-02-18' %}
 
 
 
