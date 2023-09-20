6df60082efac803a0b1ea3bc1448774ce7bd1185
Hildebert Moulié <h.moulie@gmail.com>
2023-09-20 22:56:51 +0200

    fixes

--- macros/models/_sector/dex/dex_sandwiches.sql
+++ macros/models/_sector/dex/dex_sandwiches.sql
@@ -90,6 +90,8 @@ SELECT distinct s1.blockchain
 , s1.token_sold_amount
 , s1.token_bought_amount
 , CASE WHEN s1.tx_from=s2.tx_from THEN 'tx_from' ELSE 'taker' END AS commonality
+, CASE WHEN s1.token_bought_address=s2.token_sold_address THEN 'token_sold' ELSE 'token_bought' END AS sandwiched_token
+, CASE WHEN s1.token_bought_address<s1.token_sold_address THEN 0 ELSE 1 END AS token_order
 FROM trades_with_index s1
 INNER JOIN trades_with_index s2 ON s1.block_number=s2.block_number
     AND s1.project=s2.project
