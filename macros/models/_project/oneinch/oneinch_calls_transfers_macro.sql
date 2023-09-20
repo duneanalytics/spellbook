1e6b94ae30a4de9b02d3a3c4bda10d092b4253b7
grkhr <eduardgorkh@gmail.com>
2023-09-20 19:26:42 +0200

    all except bnb

--- macros/models/_project/oneinch/oneinch_calls_transfers_macro.sql
+++ macros/models/_project/oneinch/oneinch_calls_transfers_macro.sql
@@ -166,7 +166,7 @@ methods as (
         and slice(transfers.trace_address, 1, cardinality(calls.start)) = calls.start 
 )
 
-{% if blockchain in ['polygon','bnb'] %}
+{% if blockchain not in ['bnb'] %}
 
 select 
     *
