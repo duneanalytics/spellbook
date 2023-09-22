e800f91bac186840aa37673a45b06e478976f5cb
Hildebert Moulié <h.moulie@gmail.com>
2023-09-22 11:13:17 +0200

    fix

--- models/dex/chains/dex_ethereum_sandwiches.sql
+++ models/dex/chains/dex_ethereum_sandwiches.sql
@@ -7,6 +7,7 @@
         file_format = 'delta',
         incremental_strategy = 'merge',
         unique_key = ['blockchain', 'tx_hash', 'project_contract_address', 'evt_index']
+        )
 }}
 
 {{dex_sandwiches(
