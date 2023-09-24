b0b2af596a31937f235a8f713e64ec5f4ab7ba2c
tomfutago <tomfutago@gmail.com>
2023-09-24 16:31:14 +0100

    v1 trades

--- models/mento/celo/mento_celo_trades.sql
+++ models/mento/celo/mento_celo_trades.sql
@@ -2,46 +2,45 @@
     config(
         tags = ['dunesql'],
         schema = 'mento_celo',
-        alias = alias('trades')
+        alias = alias('trades'),
+        post_hook = '{{ expose_spells(\'["celo"]\',
+                                    "project",
+                                    "mento",
+                                    \'["tomfutago"]\') }}'
     )
 }}
 
-{% set mento_models = [
-    ref('mento_v1_celo_trades')
+{% set dex_models = [
+  ref('mento_celo_trades_v1')
 ] %}
 
-
-SELECT *
-FROM (
-    {% for dex_model in mento_models %}
-    SELECT
-        blockchain,
-        project,
-        version,
-        block_month,
-        block_date,
-        block_time,
-        token_bought_symbol,
-        token_sold_symbol,
-        token_pair,
-        token_bought_amount,
-        token_sold_amount,
-        token_bought_amount_raw,
-        token_sold_amount_raw,
-        amount_usd,
-        token_bought_address,
-        token_sold_address,
-        taker,
-        maker,
-        project_contract_address,
-        tx_hash,
-        tx_from,
-        tx_to,
-
-        evt_index
-    FROM {{ dex_model }}
-    {% if not loop.last %}
-    UNION ALL
-    {% endif %}
-    {% endfor %}
-)
+{% for dex_model in dex_models %}
+select
+  blockchain,
+  project,
+  version,
+  block_month,
+  block_date,
+  block_time,
+  token_bought_symbol,
+  token_sold_symbol,
+  token_pair,
+  token_bought_amount,
+  token_sold_amount,
+  token_bought_amount_raw,
+  token_sold_amount_raw,
+  amount_usd,
+  token_bought_address,
+  token_sold_address,
+  taker,
+  maker,
+  project_contract_address,
+  tx_hash,
+  tx_from,
+  tx_to,
+  evt_index
+from {{ dex_model }}
+{% if not loop.last %}
+union all
+{% endif %}
+{% endfor %}
