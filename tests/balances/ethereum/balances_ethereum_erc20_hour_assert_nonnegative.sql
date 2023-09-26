a4dad05f3c4aeb58ef138255ed89ecb24281c91a
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-27 00:02:04 +0100

    get tests fail details

--- tests/balances/ethereum/balances_ethereum_erc20_hour_assert_nonnegative.sql
+++ tests/balances/ethereum/balances_ethereum_erc20_hour_assert_nonnegative.sql
@@ -1,7 +1,7 @@
                                     -- Check for negative balances
 -- Some balances are very small negative numbers due to loss of precision from large ints
 
-select amount
+select *
 from {{ ref('balances_ethereum_erc20_hour') }} bal
 where round(amount/power(10, 18), 6) < 0
 -- limiting to a selection of tokens because we haven't filtered out all non-compliant tokens
