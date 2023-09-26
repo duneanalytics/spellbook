e9e64e2f96eed48ac38c1e762ce242a744832e88
Huang Geyang <Sukebeta@outlook.com>
2023-09-26 17:48:31 +0800

    Add labels_debank into labels_social

--- models/labels/addresses/social/labels_social.sql
+++ models/labels/addresses/social/labels_social.sql
@@ -12,7 +12,8 @@
 {% set social_models = [
   ref('labels_ens')
 , ref('labels_three_letter_ens_count')
- ,ref('labels_lens_poster_frequencies')
+, ref('labels_lens_poster_frequencies')
+, ref('labels_debank')
 ] %}
 
 SELECT *
