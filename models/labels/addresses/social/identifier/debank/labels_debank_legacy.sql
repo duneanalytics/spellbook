d2b5bffad0843042889c41d9b05314163c92d8f6
Huang Geyang <Sukebeta@outlook.com>
2023-09-26 16:52:12 +0800

    Add labels_debank

---
+++ models/labels/addresses/social/identifier/debank/labels_debank_legacy.sql
@@ -0,0 +1,10 @@
+{{ config(
+	tags=['legacy'],
+    alias = alias('debank', legacy_model=True),
+     post_hook='{{ expose_spells(\'["ethereum"]\',
+                                    "sector",
+                                    "labels",
+                                    \'["stakeridoo"]\') }}'
+}}
+
+select 1
