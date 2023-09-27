2742723ccdf9862dc7a84cd82115d2f859d9c15a
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-09-27 15:08:46 +0100

    switch to token address

--- tests/balances/ethereum/balances_ethereum_erc20_day_assert_nonnegative.sql
+++ tests/balances/ethereum/balances_ethereum_erc20_day_assert_nonnegative.sql
@@ -5,6 +5,7 @@ select *
 from {{ ref('balances_ethereum_erc20_day') }} bal
 where round(amount/power(10, 18), 6) < 0
 -- limiting to a selection of tokens because we haven't filtered out all non-compliant tokens
-and symbol in ('AAVE', 'DAI', 'UNI', 'LINK')
+and token_address in (0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9, 0x6B175474E89094C44Da98b954EedeAC495271d0F, 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984, 0x514910771AF9Ca656af840dff83E8264EcF986CA)
+and wallet_address != 0x0000000000000000000000000000000000000000
 and bal.block_day > now() - interval '2' Day
 
