2b03a25d5332cac09a78ec537d6f51e756fb1ff5
0xRob <0xRobin5@gmail.com>
2023-09-28 14:28:27 +0200

    fix

--- models/sudoswap/ethereum/sudoswap_ethereum_pool_creations.sql
+++ models/sudoswap/ethereum/sudoswap_ethereum_pool_creations.sql
@@ -23,7 +23,7 @@ WITH
     SELECT
       output_pair AS pool_address,
       _nft AS nft_contract_address,
-      tx.from AS creator_address,
+      tx."from" AS creator_address,
       CASE
         WHEN _bondingCurve = {{linear_bonding_address}} THEN 'linear'
         WHEN _bondingCurve = {{exponential_bonding_address}} THEN 'exponential'
