91e05c2710e0d3dff42e5dcfb30de52cce6f6341
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-09-28 11:24:59 -0700

    Migrate compound

--- models/compound/ethereum/compound_v2_ethereum_proposals.sql
+++ models/compound/ethereum/compound_v2_ethereum_proposals.sql
@@ -1,7 +1,8 @@
 {{ config(
+    tags=['dunesql'],
     schema = 'compound_v2_ethereum',
     alias = alias('proposals'),
-    partition_by = ['block_date'],
+    partition_by = ['block_month'],
     materialized = 'table',
     file_format = 'delta',
     post_hook='{{ expose_spells(\'["ethereum"]\',
@@ -19,9 +20,9 @@
 
 with cte_support as (SELECT 
         voter as voter,
-        CASE WHEN support = 0 THEN sum(votes/1e18) ELSE 0 END AS votes_against,
-        CASE WHEN support = 1 THEN sum(votes/1e18) ELSE 0 END AS votes_for,
-        CASE WHEN support = 2 THEN sum(votes/1e18) ELSE 0 END AS votes_abstain,
+        CASE WHEN support = false THEN sum(votes/1e18) ELSE 0 END AS votes_against,
+        CASE WHEN support = true THEN sum(votes/1e18) ELSE 0 END AS votes_for,
+        0 AS votes_abstain,
         proposalId
 FROM {{ source('compound_v2_ethereum', 'GovernorBravoDelegate_evt_VoteCast') }}
 GROUP BY support, proposalId, voter),
@@ -30,8 +31,8 @@ cte_sum_votes as (
 SELECT COUNT(DISTINCT voter) as number_of_voters,
        SUM(votes_for) as votes_for, 
        SUM(votes_against) as votes_against, 
-       SUM(votes_abstain) as votes_abstain, 
-       SUM(votes_for) + SUM(votes_against) + SUM(votes_abstain) as votes_total,
+       0 as votes_abstain, 
+       SUM(votes_for) + SUM(votes_against) + 0 as votes_total,
        proposalId
 from cte_support
 GROUP BY proposalId)
@@ -42,9 +43,10 @@ SELECT DISTINCT
     '{{project_version}}' as version,
     pcr.evt_block_time as created_at,
     date_trunc('DAY', pcr.evt_block_time) AS block_date,
+    CAST(date_trunc('month', evt_block_time) as date) as block_month
     pcr.evt_tx_hash as tx_hash, -- Proposal Created tx hash
     '{{dao_name}}' as dao_name,
-    '{{dao_address}}' as dao_address,
+    {{dao_address}} as dao_address,
     proposer,
     pcr.id as proposal_id,
     csv.votes_for,
@@ -58,8 +60,8 @@ SELECT DISTINCT
     CASE 
          WHEN pex.id is not null and now() > pex.evt_block_time THEN 'Executed' 
          WHEN pca.id is not null and now() > pca.evt_block_time THEN 'Canceled'
-         WHEN pcr.startBlock < pcr.evt_block_number < pcr.endBlock THEN 'Active'
-         WHEN now() > pqu.evt_block_time AND startBlock > pcr.evt_block_number THEN 'Queued'
+         WHEN pcr.startBlock < CAST(pcr.evt_block_number AS UINT256) AND CAST(pcr.evt_block_number AS UINT256) < pcr.endBlock THEN 'Active'
+         WHEN now() > pqu.evt_block_time AND startBlock > CAST(pcr.evt_block_number AS UINT256) THEN 'Queued'
          ELSE 'Defeated' END AS status,
     description as description
 FROM  {{ source('compound_v2_ethereum', 'GovernorBravoDelegate_evt_ProposalCreated') }} pcr
