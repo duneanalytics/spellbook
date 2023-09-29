ba5efcf9497a50a8337d3c2e82010585531319f9
longnh <hoanglong180695@gmail.com>
2023-09-29 07:43:36 +0700

    update

--- models/balances/bnb/bep20/balances_bnb_bep20_day_new_wallet.sql
+++ models/balances/bnb/bep20/balances_bnb_bep20_day_new_wallet.sql
@@ -43,30 +43,11 @@ with
     group by
       block_day,
       token_address
-  ),
-   token_holder as (
-    select block_day,
-      token_address,
-      count(*) as number_holder
-      from {{ ref('balances_bnb_bep20_day') }}
-      where amount_raw > 0
-      group by token_address, block_day
-
-   ),
-   agg_token as (
-    select n.block_day, n.token_address,
-      n.volumn_new_wallets, n.number_new_wallets,
-      h.number_holder
-      from token_holder h
-      left join agg_new_wallets n
-      on h.token_address = n.token_address
-      and h.block_day = n.block_day
-   )
+  )
 
 select
   d.block_day,
   d.token_address,
-  d.number_holder,
   d.number_new_wallets,
   d.volumn_new_wallets / power(10, 18) as volumn_new_wallets,
   p.price
