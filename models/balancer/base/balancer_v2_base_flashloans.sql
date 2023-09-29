e9573bf5c975d421ef2bf578f4ca40a4df22d6d8
Vinicius Abussafi | Balancer <131974393+viniabussafi@users.noreply.github.com>
2023-09-29 19:41:37 +0200

    Missing Balancer Flashloans spells on base and avalanche (#4369)
    * Balancer macros
    
    * minor fix for avalanche and gnosis
    
    * fixes
    
    * undo changes to get_chain_explorer
    
    * Update public_macros_schema.yml
    
    * Update get_chain_explorer_address.sql
    
    * Update get_chain_explorer_tx_hash.sql
    
    * adds missing spells to base and avalanche
    
    * minor source fix
    
    * fix
    
    * adds legacy spells
    
    * fixes legacy spells
    
    * fix
    
    * legacy models fix
    
    * Update balancer_flashloans.sql
    
    * Update balancer_v2_avalanche_c_flashloans_legacy.sql
    
    * Update balancer_v2_base_flashloans_legacy.sql
    
    ---------
    
    Co-authored-by: António Mendes <29656081+antonio-mendes@users.noreply.github.com>
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

---
+++ models/balancer/base/balancer_v2_base_flashloans.sql
@@ -0,0 +1,52 @@
+{{ config(
+        tags=['dunesql']
+      , partition_by = ['block_month']
+      , schema = 'balancer_v2_base'
+      , alias = alias('flashloans')
+      , materialized = 'incremental'
+      , file_format = 'delta'
+      , incremental_strategy = 'merge'
+      , unique_key = ['tx_hash', 'evt_index']
+      , post_hook='{{ expose_spells(\'["base"]\',
+                                  "project",
+                                  "balancer_v2",
+                                  \'["hildobby", "viniabussafi"]\') }}'
+  )
+}}
+
+WITH flashloans AS (
+    SELECT f.evt_block_time AS block_time
+    , f.evt_block_number AS block_number
+    , f.amount AS amount_raw
+    , f.evt_tx_hash AS tx_hash
+    , f.evt_index
+    , f.feeAmount AS fee
+    , f.token AS currency_contract
+    , erc20.symbol AS currency_symbol
+    , erc20.decimals AS currency_decimals
+    , f.contract_address
+    FROM {{ source('balancer_v2_base','Vault_evt_FlashLoan') }} f
+    LEFT JOIN {{ ref('tokens_base_erc20') }} erc20 ON f.token = erc20.contract_address
+        {% if is_incremental() %}
+        WHERE f.evt_block_time >= date_trunc('day', now() - interval '7' Day)
+        {% endif %}
+    )
+
+SELECT 'base' AS blockchain
+, 'Balancer' AS project
+, '2' AS version
+, CAST(date_trunc('Month', flash.block_time) as date) as block_month
+, flash.block_time
+, flash.block_number
+, flash.amount_raw/POWER(10, flash.currency_decimals) AS amount
+, pu.price*flash.amount_raw/POWER(10, flash.currency_decimals) AS amount_usd
+, flash.tx_hash
+, flash.evt_index
+, flash.fee/POWER(10, flash.currency_decimals) AS fee
+, flash.currency_contract
+, flash.currency_symbol
+, flash.contract_address
+FROM flashloans flash
+LEFT JOIN {{ source('prices','usd') }} pu ON pu.blockchain = 'base'  
+    AND pu.contract_address = flash.currency_contract
+    AND pu.minute = date_trunc('minute', flash.block_time)
