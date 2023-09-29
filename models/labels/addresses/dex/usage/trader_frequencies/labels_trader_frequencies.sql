5bd633bc6e03921ee84a29c568ad41fb3862ad25
Alan Ghobadi <aalan3@gmail.com>
2023-09-29 14:29:17 +0200

    Fix trader_frequencies

--- models/labels/addresses/dex/usage/trader_frequencies/labels_trader_frequencies.sql
+++ models/labels/addresses/dex/usage/trader_frequencies/labels_trader_frequencies.sql
@@ -15,7 +15,10 @@ with
     select
         blockchain,
         taker as address,
-        count(distinct tx_hash) / datediff(max(block_date), min(block_date)) as trades_per_day
+        case
+            when date_diff('day', min(block_date), max(block_date)) = 0 then null
+            else count(distinct tx_hash) / date_diff('day', min(block_date), max(block_date))
+        end as trades_per_day
     from (
         select blockchain, taker, block_date, tx_hash
         from {{ ref('dex_aggregator_trades') }}
@@ -29,19 +32,19 @@ with
  )
 
 select blockchain       AS blockchain,
-       address,
-       case
+        address,
+        case
            when trades_per_day >= 1 then 'Daily Trader'
            when trades_per_day >= 0.142857142857 then 'Weekly Trader'
            when trades_per_day >= 0.0333333333333 then 'Monthly Trader'
            when trades_per_day >= 0.0027397260274 then 'Yearly Trader'
            else 'Sparse Trader'
            end                 AS name,
-       'dex'    AS category,
-       'gentrexha'             AS contributor,
-       'query'                 AS source,
-       timestamp '2022-12-14' AS created_at,
-       now()                   AS updated_at,
+        'dex'    AS category,
+        'gentrexha'             AS contributor,
+        'query'                 AS source,
+        timestamp '2022-12-14' AS created_at,
+        now()                   AS updated_at,
         'trader_frequencies' as model_name,
         'usage' as label_type
 from trader_frequencies
