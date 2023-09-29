6c923ee78eaf958fa524067fb83dfc1834fd40de
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-09-29 00:14:55 -0400

    nft solana trades

---
+++ models/_sector/nft/trades/solana/nft_solana_trades.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        schema = 'nft_solana'
+        , tags = ['dunesql']
+        , alias = alias('trades')
+        ,materialized = 'incremental'
+        ,file_format = 'delta'
+        ,incremental_strategy = 'merge'
+        ,unique_key = ['unique_trade_id','block_slot']
+        ,post_hook='{{ expose_spells(\'["solana"]\',
+                                    "sector",
+                                    "nft",
+                                    \'["ilemi"]\') }}'
+    )
+}}
+
+{% set solana_marketplaces = [
+    ref('magiceden_solana_trades')
+    , ref('tensorswap_solana_trades')
+] %}
+
+
+SELECT *
+FROM (
+    {% for marketplace in solana_marketplaces %}
+    SELECT
+        *
+    FROM {{ marketplace }}
+
+    UNION ALL
+
+    {% endfor %}
+)
