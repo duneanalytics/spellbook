afb415030b900232dd3534ff8cd0418535db2e48
Huang Geyang <Sukebeta@outlook.com>
2023-09-30 12:02:57 +0800

    Migrate models/cow_protocol/tx_hash_labels/offramp/ and onramp/

--- models/cow_protocol/tx_hash_labels/offramp/cow_protocol_tx_hash_labels_offramp.sql
+++ models/cow_protocol/tx_hash_labels/offramp/cow_protocol_tx_hash_labels_offramp.sql
@@ -1,5 +1,5 @@
 {{
-    config(
+        config(tags=['dunesql'],
         alias = alias('tx_hash_labels_offramp'),
         post_hook='{{ expose_spells(\'["ethereum"]\', "sector", "tx_hash_labels", \'["gentrexha"]\') }}'
     )
