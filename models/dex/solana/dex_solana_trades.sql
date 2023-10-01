fecc6263c2da15bc51e1dd2048c6f455dead8535
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-10-01 16:00:37 -0400

    add dex trades

---
+++ models/dex/solana/dex_solana_trades.sql
@@ -0,0 +1,59 @@
+ {{
+  config(
+        tags = ['dunesql'],
+        schema = 'dex_solana',
+        alias = alias('trades'),
+        -- partition_by = ['block_month'],
+        materialized = 'view',
+        -- file_format = 'delta',
+        -- incremental_strategy = 'merge',
+        unique_key = ['tx_id', 'outer_instruction_index', 'inner_instruction_index', 'tx_index','block_month'],
+        -- pre_hook='{{ enforce_join_distribution("PARTITIONED") }}',
+        post_hook='{{ expose_spells(\'["solana"]\',
+                                    "project",
+                                    "dex",
+                                    \'["ilemi"]\') }}')
+}}
+
+{% set solana_dexes = [
+    ref('orca_whirlpool_trades')
+    , ref('raydium_v3_trades')
+    , ref('phoenix_v1_trades')
+    , ref('lifinity_v1_trades')
+    , ref('lifinity_v2_trades')
+] %}
+
+
+{% for dex in solana_dexes %}
+SELECT
+      blockchain
+      , project
+      , version
+      , block_time
+      , trade_source
+      , token_bought_symbol
+      , token_sold_symbol
+      , token_pair
+      , token_bought_amount
+      , token_sold_amount
+      , token_bought_amount_raw
+      , token_sold_amount_raw
+      , amount_usd
+      , fee_tier
+      , fee_usd
+      , token_bought_mint_address
+      , token_sold_mint_address
+      , token_bought_vault
+      , token_sold_vault
+      , project_program_id
+      , trader_id
+      , tx_id
+      , outer_instruction_index
+      , inner_instruction_index
+      , tx_index
+FROM {{ dex }}
+
+{% if not loop.last %}
+UNION ALL
+{% endif %}
+{% endfor %}
