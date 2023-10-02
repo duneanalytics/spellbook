65a6f7d2bc802e482841d5e7ded177e2a649c8ec
Alan Ghobadi <aalan3@gmail.com>
2023-10-02 15:26:27 +0200

    Fix typo

--- macros/incremental_predicate.sql
+++ macros/incremental_predicate.sql
@@ -1,3 +1,3 @@
 {% macro incremental_predicate(column) -%}
-{{column}} >= date_trunc('{{var("DBT_ENV_INCREMENTAL_TIME_UNIT")}}', now() - interval '{{var('DBT_ENV_INCREMENTAL_DAYS')}}' {{var('DBT_ENV_INCREMENTAL_TIME_UNIT')}})
+{{column}} >= date_trunc('{{var("DBT_ENV_INCREMENTAL_TIME_UNIT")}}', now() - interval '{{var('DBT_ENV_INCREMENTAL_TIME')}}' {{var('DBT_ENV_INCREMENTAL_TIME_UNIT')}})
 {%- endmacro -%}
