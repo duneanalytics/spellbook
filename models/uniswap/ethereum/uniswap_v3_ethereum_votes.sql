324ef48b102f7459b63b8cd529b4c17ddb15cad9
Alan Ghobadi <aalan3@gmail.com>
2023-10-02 13:45:53 +0200

    Drop partitioning from votes and proposals

--- models/uniswap/ethereum/uniswap_v3_ethereum_votes.sql
+++ models/uniswap/ethereum/uniswap_v3_ethereum_votes.sql
@@ -2,7 +2,6 @@
     tags = ['dunesql'],
     schema = 'uniswap_v3_ethereum',
     alias = alias('votes'),
-    partition_by = ['block_month'],
     materialized = 'table',
     file_format = 'delta',
     post_hook='{{ expose_spells(\'["ethereum"]\',
@@ -18,13 +17,13 @@
 {% set dao_name = 'DAO: Uniswap' %}
 {% set dao_address = '0x408ed6354d4973f66138c91495f2f2fcbd8724c3' %}
 
-WITH cte_sum_votes as 
-(SELECT sum(votes/1e18) as sum_votes, 
+WITH cte_sum_votes as
+(SELECT sum(votes/1e18) as sum_votes,
         proposalId
 FROM {{ source('uniswap_v3_ethereum', 'GovernorBravoDelegate_evt_VoteCast') }}
 GROUP BY proposalId)
 
-SELECT 
+SELECT
     '{{blockchain}}' as blockchain,
     '{{project}}' as project,
     '{{project_version}}' as version,
@@ -38,7 +37,7 @@ SELECT
     vc.votes/1e18 as votes,
     (votes/1e18) * (100) / (csv.sum_votes) as votes_share,
     p.symbol as token_symbol,
-    p.contract_address as token_address, 
+    p.contract_address as token_address,
     vc.votes/1e18 * p.price as votes_value_usd,
     vc.voter as voter_address,
     CASE WHEN vc.support = 0 THEN 'against'
