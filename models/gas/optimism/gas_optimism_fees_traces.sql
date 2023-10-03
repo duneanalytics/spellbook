61c5586640e28e38bc6d8c2c04bef827f8160c1f
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-10-03 09:01:29 -0700

    SPE-118 Migrate gas_optimism_fees_traces

--- models/gas/optimism/gas_optimism_fees_traces.sql
+++ models/gas/optimism/gas_optimism_fees_traces.sql
@@ -1,4 +1,5 @@
 {{ config(
+    tags=['dunesql'],
     schema = 'gas_optimism',
     alias = alias('fees_traces'),
     partition_by = ['block_date'],
@@ -17,7 +18,7 @@ WITH traces AS (
      , MAX(traces.to) AS trace_to
      , traces.trace
      , MAX(traces.input) AS trace_input
-     , substring(MAX(traces.input),1,10) AS trace_method
+     , bytearray_substring(MAX(traces.input),1,4) AS trace_method
      , SUM(traces.gas_used_original) AS gas_used_original
      , SUM(traces.gas_used_trace) AS gas_used_trace
      , MAX(traces.trace_type) AS trace_type
@@ -25,8 +26,8 @@ WITH traces AS (
      , MAX(traces.trace_success) AS trace_success
      , MAX(traces.tx_success) AS tx_success
      FROM (
-          SELECT from
-          , to
+          SELECT "from"
+          , "to"
           , tx_hash
           , trace_address AS trace
           , gas_used AS gas_used_original
@@ -40,28 +41,28 @@ WITH traces AS (
           , tx_success
           FROM {{ source('optimism','traces') }}
           {% if is_incremental() %}
-          WHERE block_time >= date_trunc("day", NOW() - interval '1 days')
+          WHERE block_time >= date_trunc('day', NOW() - interval '1' day)
           {% endif %}
           
           UNION ALL
           
-          SELECT CAST(NULL AS varchar(1)) AS from 
-          , CAST(NULL AS varchar(1)) AS to 
+          SELECT CAST(NULL AS varbinary) AS "from" 
+          , CAST(NULL AS varbinary) AS "to" 
           , tx_hash
           , slice(trace_address, 1, cardinality(trace_address) - 1) AS trace
-          , CAST(NULL AS double) AS gas_used_original
+          , CAST(NULL AS bigint) AS gas_used_original
           , -gas_used AS gas_used_trace
           , block_time
           , block_number
-          , CAST(NULL AS varchar(1)) AS input
+          , CAST(NULL AS varbinary) AS input
           , CAST(NULL AS varchar(1)) AS trace_type
-          , CAST(NULL AS varchar(1)) AS trace_value
+          , CAST(NULL AS UINT256) AS trace_value
           , CAST(NULL AS boolean) AS trace_success
           , CAST(NULL AS boolean) AS tx_success
           FROM {{ source('optimism','traces') }}
           WHERE cardinality(trace_address) > 0
           {% if is_incremental() %}
-          AND block_time >= date_trunc("day", NOW() - interval '1 days')
+          AND block_time >= date_trunc('day', NOW() - interval '1' day)
           {% endif %}
           ) traces
      GROUP BY traces.tx_hash, traces.trace, traces.block_time, traces.block_number
@@ -78,7 +79,7 @@ SELECT 'optimism' AS blockchain
 , txs.to AS tx_to
 , traces.trace
 , traces.trace_method
-, substring(txs.data,1,10) AS tx_method
+, bytearray_substring(txs.data,1,4) AS tx_method
 , traces.trace_input
 , traces.gas_used_original
 , traces.gas_used_trace
@@ -98,11 +99,11 @@ FROM traces
 INNER JOIN {{ source('optimism','transactions') }} txs ON txs.block_time=traces.block_time
      AND txs.hash=traces.tx_hash
      {% if is_incremental() %}
-     AND txs.block_time >= date_trunc("day", NOW() - interval '1 days')
+     AND txs.block_time >= date_trunc('day', NOW() - interval '1' day)
      {% endif %}
 LEFT JOIN {{ source('prices', 'usd') }} pu ON pu.minute=date_trunc('minute', traces.block_time)
      AND pu.blockchain='optimism'
-     AND pu.contract_address='0x4200000000000000000000000000000000000006'
+     AND pu.contract_address=0x4200000000000000000000000000000000000006
      {% if is_incremental() %}
-     AND pu.minute >= date_trunc("day", NOW() - interval '1' week)
+     AND pu.minute >= date_trunc('day', NOW() - interval '7' day)
      {% endif %}
