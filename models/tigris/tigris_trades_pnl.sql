1e141843a92e80945b9f7ea53efb08a5d1063231
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-10-03 22:55:18 +0100

    expose spell

--- models/tigris/tigris_trades_pnl.sql
+++ models/tigris/tigris_trades_pnl.sql
@@ -6,7 +6,11 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['evt_block_time', 'evt_tx_hash', 'position_id', 'trade_type', 'positions_contract', 'protocol_version']
+    unique_key = ['evt_block_time', 'evt_tx_hash', 'position_id', 'trade_type', 'positions_contract', 'protocol_version'],
+    post_hook='{{ expose_spells(\'["arbitrum", "polygon"]\',
+                                "project",
+                                "tigris",
+                                \'["Henrystats"]\') }}'
     )
 }}
 
