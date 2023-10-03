b29f817545388e6e0e72a63680dbc102fd210210
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-10-02 17:01:54 -0700

    CURRENT_TIMESTAMP > now()

--- models/gmx/avalanche_c/glp/gmx_avalanche_c_glp_components.sql
+++ models/gmx/avalanche_c/glp/gmx_avalanche_c_glp_components.sql
@@ -17,10 +17,10 @@
 with minute as -- This CTE generates a series of minute values
          (
             {% if not is_incremental() %}
-            SELECT minute from unnest(sequence(TIMESTAMP '{{project_start_date}}',TIMESTAMP CURRENT_TIMESTAMP, INTERVAL 1 minute)) AS _u(minute)
+            SELECT minute from unnest(sequence(TIMESTAMP '{{project_start_date}}',TIMESTAMP now(), INTERVAL 1 minute)) AS _u(minute)
             {% endif %}
             {% if is_incremental() %}
-            SELECT minute from unnest(sequence(date_trunc('day', now() - interval '7' day), CURRENT_TIMESTAMP, INTERVAL 1 minute)) AS _u(minute)
+            SELECT minute from unnest(sequence(date_trunc('day', now() - interval '7' day), now(), INTERVAL 1 minute)) AS _u(minute)
             {% endif %}
          ),
      token as -- This CTE create tokens which in GLP pool on Avalanche
