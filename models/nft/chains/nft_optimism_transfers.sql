db7b55e622a5749e8bb2cd2b3cca098d7e9f32ab
Alan Ghobadi <aalan3@gmail.com>
2023-10-03 14:13:25 +0200

    Nft transfer models incremental macro (#4502)
    * Update nft transfers macro to use incremental predicate macro
    
    * Update incremental predicate for models
    
    * Fix missing replacemnt

--- models/nft/chains/nft_optimism_transfers.sql
+++ models/nft/chains/nft_optimism_transfers.sql
@@ -6,7 +6,7 @@
         materialized='incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        incremental_predicates = ['DBT_INTERNAL_DEST.block_time >= date_trunc(\'day\', now() - interval \'7\' day)'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
         unique_key = ['tx_hash', 'evt_index', 'token_id', 'amount'])
 }}
 
