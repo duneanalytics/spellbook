82ea69eecdf934697b0b671dd7ab0d5dca42a5aa
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-10-04 08:52:39 -0400

    add predicate to config

--- models/_sector/nft/trades/solana/platforms/tensorswap_solana_trades.sql
+++ models/_sector/nft/trades/solana/platforms/tensorswap_solana_trades.sql
@@ -6,6 +6,7 @@
         ,materialized = 'incremental'
         ,file_format = 'delta'
         ,incremental_strategy = 'merge'
+        ,incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
         ,unique_key = ['project','trade_category','outer_instruction_index','inner_instruction_index','account_metadata','tx_id']
         ,post_hook='{{ expose_spells(\'["solana"]\',
                                     "project",
