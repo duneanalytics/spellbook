337f02ac6f77131e2f2408523e5472419822fa35
jeff-dude <jeff@dune.xyz>
2023-10-05 14:23:05 -0400

    incremental logic, cleaner aliases in macro

--- macros/models/_sector/dex/dex_enrich_trades.sql
+++ macros/models/_sector/dex/dex_enrich_trades.sql
@@ -28,7 +28,7 @@ WITH base_union AS (
         evt_index
     FROM {{ dex_model[2] }}
     {% if is_incremental() %}
-    WHERE block_time >= date_trunc('day', now() - interval '7' day)
+    WHERE {{incremental_predicate('block_time')}}
     {% endif %}
     {% if not loop.last %}
     UNION ALL
@@ -37,62 +37,63 @@ WITH base_union AS (
 ),
 
 enrichments AS (
-SELECT base.blockchain,
-       base.project,
-       base.version,
-       base.block_date,
-       base.block_month,
-       base.block_time,
-       erc20a.symbol                                             AS token_bought_symbol,
-       erc20b.symbol                                             AS token_sold_symbol,
-       case
-           when lower(erc20a.symbol) > lower(erc20b.symbol) then concat(erc20b.symbol, '-', erc20a.symbol)
-           else concat(erc20a.symbol, '-', erc20b.symbol)
-           end                                                   AS token_pair,
-       base.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount,
-       base.token_sold_amount_raw / power(10, erc20b.decimals)   AS token_sold_amount,
-       base.token_bought_amount_raw,
-       base.token_sold_amount_raw,
-       coalesce(
-               base.amount_usd,
-               base.token_bought_amount_raw / power(10, erc20a.decimals) * pa.price,
-               base.token_sold_amount_raw / power(10, erc20b.decimals) * pb.price
-           )                                                     AS amount_usd,
-       base.token_bought_address,
-       base.token_sold_address,
-       coalesce(base.taker, tx."from")                           AS taker,
-       base.maker,
-       base.project_contract_address,
-       base.tx_hash,
-       tx."from"                                                 AS tx_from,
-       tx.to                                                     AS tx_to,
-       base.evt_index
-FROM base_union base
-INNER JOIN {{ transactions_model }} tx
-    ON tx.hash = base.tx_hash
-    {% if is_incremental() %}
-    AND tx.block_time >= date_trunc('day', now() - interval '7' day)
-    {% endif %}
-LEFT JOIN {{ tokens_erc20_model }} erc20a
-    ON erc20a.contract_address = base.token_bought_address
-    AND erc20a.blockchain = '{{ blockchain }}'
-LEFT JOIN {{ tokens_erc20_model }} erc20b
-    ON erc20b.contract_address = base.token_sold_address
-    AND erc20b.blockchain = '{{ blockchain }}'
-LEFT JOIN {{ prices_model }} pa
-    ON pa.minute = date_trunc('minute', base.block_time)
-    AND pa.contract_address = base.token_bought_address
-    AND pa.blockchain = '{{ blockchain }}'
-    {% if is_incremental() %}
-    AND pa.minute >= date_trunc('day', now() - interval '7' day)
-    {% endif %}
-LEFT JOIN {{ prices_model }} pb
-    ON pb.minute = date_trunc('minute', base.block_time)
-    AND pb.contract_address = base.token_sold_address
-    AND pb.blockchain = '{{ blockchain }}'
-    {% if is_incremental() %}
-    AND pb.minute >= date_trunc('day', now() - interval '7' day)
-    {% endif %}
+    SELECT
+        base.blockchain,
+        base.project,
+        base.version,
+        base.block_date,
+        base.block_month,
+        base.block_time,
+        erc20_bought.symbol AS token_bought_symbol,
+        erc20_sold.symbol AS token_sold_symbol,
+        case
+            when lower(erc20_bought.symbol) > lower(erc20_sold.symbol) then concat(erc20_sold.symbol, '-', erc20_bought.symbol)
+            else concat(erc20_bought.symbol, '-', erc20_sold.symbol)
+            end AS token_pair,
+        base.token_bought_amount_raw / power(10, erc20_bought.decimals) AS token_bought_amount,
+        base.token_sold_amount_raw / power(10, erc20_sold.decimals) AS token_sold_amount,
+        base.token_bought_amount_raw,
+        base.token_sold_amount_raw,
+        coalesce(
+                base.amount_usd,
+                base.token_bought_amount_raw / power(10, erc20_bought.decimals) * p_bought.price,
+                base.token_sold_amount_raw / power(10, erc20_sold.decimals) * p_sold.price
+            ) AS amount_usd,
+        base.token_bought_address,
+        base.token_sold_address,
+        coalesce(base.taker, tx."from") AS taker,
+        base.maker,
+        base.project_contract_address,
+        base.tx_hash,
+        tx."from" AS tx_from,
+        tx.to AS tx_to,
+        base.evt_index
+    FROM base_union base
+    INNER JOIN {{ transactions_model }} tx
+        ON tx.hash = base.tx_hash
+        {% if is_incremental() %}
+        AND {{incremental_predicate('tx.block_time')}}
+        {% endif %}
+    LEFT JOIN {{ tokens_erc20_model }} erc20_bought
+        ON erc20_bought.contract_address = base.token_bought_address
+        AND erc20_bought.blockchain = '{{ blockchain }}'
+    LEFT JOIN {{ tokens_erc20_model }} erc20_sold
+        ON erc20_sold.contract_address = base.token_sold_address
+        AND erc20_sold.blockchain = '{{ blockchain }}'
+    LEFT JOIN {{ prices_model }} p_bought
+        ON p_bought.minute = date_trunc('minute', base.block_time)
+        AND p_bought.contract_address = base.token_bought_address
+        AND p_bought.blockchain = '{{ blockchain }}'
+        {% if is_incremental() %}
+        AND {{incremental_predicate('p_bought.minute')}}
+        {% endif %}
+    LEFT JOIN {{ prices_model }} p_sold
+        ON p_sold.minute = date_trunc('minute', base.block_time)
+        AND p_sold.contract_address = base.token_sold_address
+        AND p_sold.blockchain = '{{ blockchain }}'
+        {% if is_incremental() %}
+        AND {{incremental_predicate('p_sold.minute')}}
+        {% endif %}
 )
 
 select * from enrichments
