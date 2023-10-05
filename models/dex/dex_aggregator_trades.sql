9c4b2b7dca3b4096dd346b1a619026066a9aa892
nhd98z <34548156+nhd98z@users.noreply.github.com>
2023-10-06 01:48:18 +0700

    add kyberswap (#4194)

--- models/dex/dex_aggregator_trades.sql
+++ models/dex/dex_aggregator_trades.sql
@@ -30,6 +30,7 @@ spells with issues, to be excluded in short term:
     ,ref('bebop_trades')
     ,ref('dodo_aggregator_trades')
     ,ref('zeroex_trades')
+    ,ref('kyberswap_aggregator_trades')
 ] %}
 
 {% for aggregator_model in dex_aggregator_models %}
