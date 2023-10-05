73497a44eac82a04fa16fced586adb0b9d71a36e
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-10-05 19:57:33 +0100

    test again

--- models/balances/ethereum/erc20/balances_ethereum_erc20_pnls.sql
+++ models/balances/ethereum/erc20/balances_ethereum_erc20_pnls.sql
@@ -84,14 +84,15 @@ buy_transactions as (
         amount_usd/(COALESCE(token_bought_amount, token_bought_amount_raw/1e18)) as token_price, 
         token_bought_address, 
         token_sold_amount as eth_sold, 
-        amount_usd as usd_value
+        amount_usd as usd_value,
+        ROW_NUMBER() OVER (PARTITION BY trader, token_bought_address ORDER BY block_time) as trade_id
     FROM 
     {{ ref('uniswap_v2_ethereum_trades') }}
     WHERE block_time > CAST(NOW() as timestamp) - Interval '2' Month
     AND token_sold_symbol IN ('WETH', 'ETH')
     AND token_bought_symbol NOT IN ('USDC', 'USDT', 'DAI', 'ARB', 'OP', 'LINK', 'COMP') -- manually removing this for optimization
     {% if is_incremental() %}
-    AND evt_block_time >= date_trunc('day', now() - interval '7' day)
+    AND block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 ), 
 
@@ -106,6 +107,72 @@ market_cap as (
         ON b.token_bought_address = h.token_address
         AND date_trunc('hour', b.block_time) = h.block_hour
         AND h.total_supply >= 0 
+), 
+
+sell_transactions as (
+    SELECT 
+        taker as trader, 
+        tx_hash,
+        tx_from, 
+        block_time, 
+        block_month,
+        COALESCE(token_sold_amount, token_sold_amount_raw/1e18) as token_sold_amount, 
+        amount_usd/(COALESCE(token_sold_amount, token_sold_amount_raw/1e18)) as token_price,
+        SUM(COALESCE(token_sold_amount, token_sold_amount_raw/1e18)) OVER (PARTITION BY trader, token_sold_address ORDER BY block_time) as cum_sold_amount,
+        token_sold_address, 
+        token_bought_amount as eth_bought, 
+        amount_usd as usd_value
+    FROM 
+    {{ ref('uniswap_v2_ethereum_trades') }}
+    WHERE block_time > CAST(NOW() as timestamp) - Interval '2' Month
+    AND token_bought_symbol IN ('WETH', 'ETH')
+    AND token_sold_symbol NOT IN ('USDC', 'USDT', 'DAI', 'ARB', 'OP', 'LINK', 'COMP') -- manually removing this for optimization
+    {% if is_incremental() %}
+    AND block_time >= date_trunc('day', now() - interval '7' day)
+    {% endif %}
+), 
+
+sell_filters as (
+    SELECT 
+        a.*, 
+        m.token_bought_amount,
+        m.trader, 
+        m.token_bought_address,
+        m.tx_hash, 
+        m.token_price as buy_price, 
+        m.usd_value as buy_volume, 
+        m.eth_sold
+    FROM 
+    (
+    SELECT 
+        MAX(st.cum_sold_amount) as sold_amount, 
+        AVG(st.price) as avg_sell_price, 
+        MAX(st.block_time) as last_sell_time
+        m.trade_id
+    FROM 
+    market_cap m 
+    INNER JOIN 
+    sell_transactions st 
+        ON m.token_bought_address = st.token_sold_address
+        AND m.trader = st.trader
+        AND st.block_time > m.block_time
+        AND st.cum_sold_amount <= m.token_bought_amount
+    GROUP BY 3 
+    ) a 
+    INNER JOIN 
+    market_cap m 
+        ON a.trade_id = m.trade_id
+),
+
+trade_data_enriched as (
+    SELECT 
+        trader, 
+        trade_id, 
+        buy_volume, 
+        sold_amount * avg_sell_price as sell_volume, 
+        buy_volume - (sold_amount * avg_sell_price) as trade_pnl
+    FROM 
+    sell_filters
 )
 
-SELECT * FROM market_cap
+SELECT * FROM trade_data_enriched
