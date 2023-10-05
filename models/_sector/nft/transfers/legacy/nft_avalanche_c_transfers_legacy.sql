1771fef7bef7d780734d8dc18df66a61f182ebfb
0xRob <83790096+0xRobin@users.noreply.github.com>
2023-10-05 18:33:11 +0200

    NFT cleanup of transfers and washtrades (#4520)
    * cleanup of transfers and washtrades
    
    * cleanup dbt_project

--- models/_sector/nft/transfers/legacy/nft_avalanche_c_transfers_legacy.sql
+++ models/_sector/nft/transfers/legacy/nft_avalanche_c_transfers_legacy.sql
@@ -1,5 +1,6 @@
 {{ config(
 	tags=['legacy'],
+	    schema = 'nft_avalanche_c',
         alias = alias('transfers', legacy_model=True),
         partition_by=['block_date'],
         materialized='incremental',
@@ -7,12 +8,11 @@
         unique_key = ['unique_transfer_id']
 )
 }}
-
- SELECT 'bnb' AS blockchain
+ SELECT 'avalanche_c' AS blockchain
 , t.evt_block_time AS block_time
 , date_trunc('day', t.evt_block_time) AS block_date
 , t.evt_block_number AS block_number
-, 'bep721' AS token_standard
+, 'erc721' AS token_standard
 , 'single' AS transfer_type
 , t.evt_index
 , t.contract_address
@@ -20,18 +20,18 @@
 , 1 AS amount
 , t.from
 , t.to
-, bt.from AS executed_by
+, at.from AS executed_by
 , t.evt_tx_hash AS tx_hash
-, 'bnb' || t.evt_tx_hash || '-bep721-' || t.contract_address || '-' || t.tokenId || '-' || t.from || '-' || t.to || '-' || '1' || '-' || t.evt_index AS unique_transfer_id
-FROM {{ source('erc721_bnb','evt_transfer') }} t
+, 'avalanche_c' || t.evt_tx_hash || '-erc721-' || t.contract_address || '-' || t.tokenId || '-' || t.from || '-' || t.to || '-' || '1' || '-' || t.evt_index AS unique_transfer_id
+FROM {{ source('erc721_avalanche_c','evt_transfer') }} t
 {% if is_incremental() %}
     ANTI JOIN {{this}} anti_table
         ON t.evt_tx_hash = anti_table.tx_hash
     {% endif %}
-INNER JOIN {{ source('bnb', 'transactions') }} bt ON bt.block_number = t.evt_block_number
-    AND bt.hash = t.evt_tx_hash
+INNER JOIN {{ source('avalanche_c', 'transactions') }} at ON at.block_number = t.evt_block_number
+    AND at.hash = t.evt_tx_hash
     {% if is_incremental() %}
-    AND bt.block_time >= date_trunc("day", now() - interval '1 week')
+    AND at.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
 {% if is_incremental() %}
 WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
@@ -39,11 +39,11 @@ WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
 
 UNION ALL
 
-SELECT 'bnb' AS blockchain
+SELECT 'avalanche_c' AS blockchain
 , t.evt_block_time AS block_time
 , date_trunc('day', t.evt_block_time) AS block_date
 , t.evt_block_number AS block_number
-, 'bep1155' AS token_standard
+, 'erc1155' AS token_standard
 , 'single' AS transfer_type
 , t.evt_index
 , t.contract_address
@@ -51,18 +51,18 @@ SELECT 'bnb' AS blockchain
 , t.value AS amount
 , t.from
 , t.to
-, bt.from AS executed_by
+, at.from AS executed_by
 , t.evt_tx_hash AS tx_hash
-, 'bnb' || t.evt_tx_hash || '-bep1155-' || t.contract_address || '-' || t.id || '-' || t.from || '-' || t.to || '-' || t.value || '-' || t.evt_index AS unique_transfer_id
-FROM {{ source('erc1155_bnb','evt_transfersingle') }} t
+, 'avalanche_c' || t.evt_tx_hash || '-erc1155-' || t.contract_address || '-' || t.id || '-' || t.from || '-' || t.to || '-' || t.value || '-' || t.evt_index AS unique_transfer_id
+FROM {{ source('erc1155_avalanche_c','evt_transfersingle') }} t
 {% if is_incremental() %}
     ANTI JOIN {{this}} anti_table
         ON t.evt_tx_hash = anti_table.tx_hash
     {% endif %}
-INNER JOIN {{ source('bnb', 'transactions') }} bt ON bt.block_number = t.evt_block_number
-    AND bt.hash = t.evt_tx_hash
+INNER JOIN {{ source('avalanche_c', 'transactions') }} at ON at.block_number = t.evt_block_number
+    AND at.hash = t.evt_tx_hash
     {% if is_incremental() %}
-    AND bt.block_time >= date_trunc("day", now() - interval '1 week')
+    AND at.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
 {% if is_incremental() %}
 WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
@@ -70,11 +70,11 @@ WHERE t.evt_block_time >= date_trunc("day", now() - interval '1 week')
 
 UNION ALL
 
-SELECT 'bnb' AS blockchain
+SELECT 'avalanche_c' AS blockchain
 , t.evt_block_time AS block_time
 , date_trunc('day', t.evt_block_time) AS block_date
 , t.evt_block_number AS block_number
-, 'bep1155' AS token_standard
+, 'erc1155' AS token_standard
 , 'batch' AS transfer_type
 , t.evt_index
 , t.contract_address
@@ -82,13 +82,13 @@ SELECT 'bnb' AS blockchain
 , t.ids_and_count.values AS amount
 , t.from
 , t.to
-, bt.from AS executed_by
-, evt_tx_hash AS tx_hash
-, 'bnb' || t.evt_tx_hash || '-bep1155-' || t.contract_address || '-' || t.ids_and_count.ids || '-' || t.from || '-' || t.to || '-' || t.ids_and_count.values || '-' || t.evt_index AS unique_transfer_id
+, at.from AS executed_by
+, t.evt_tx_hash AS tx_hash
+, 'avalanche_c' || t.evt_tx_hash || '-erc1155-' || t.contract_address || '-' || t.ids_and_count.ids || '-' || t.from || '-' || t.to || '-' || t.ids_and_count.values || '-' || t.evt_index AS unique_transfer_id
 FROM (
     SELECT t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, t.evt_index
     , explode(arrays_zip(t.values, t.ids)) AS ids_and_count
-    FROM {{ source('erc1155_bnb', 'evt_transferbatch') }} t
+    FROM {{ source('erc1155_avalanche_c', 'evt_transferbatch') }} t
     {% if is_incremental() %}
         ANTI JOIN {{this}} anti_table
             ON t.evt_tx_hash = anti_table.tx_hash
@@ -98,10 +98,10 @@ FROM (
     {% endif %}
     GROUP BY t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, t.evt_index, t.values, t.ids
     ) t
-INNER JOIN {{ source('bnb', 'transactions') }} bt ON bt.block_number = t.evt_block_number
-    AND bt.hash = t.evt_tx_hash
+INNER JOIN  {{ source('avalanche_c', 'transactions') }} at ON at.block_number = t.evt_block_number
+    AND at.hash = t.evt_tx_hash
     {% if is_incremental() %}
-    AND bt.block_time >= date_trunc("day", now() - interval '1 week')
+    AND at.block_time >= date_trunc("day", now() - interval '1 week')
     {% endif %}
 WHERE ids_and_count.values > 0
-GROUP BY blockchain, t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, bt.from, t.evt_index, token_id, amount
+GROUP BY blockchain, t.evt_block_time, t.evt_block_number, t.evt_tx_hash, t.contract_address, t.from, t.to, at.from, t.evt_index, token_id, amount
--- models/_sector/nft/transfers/nft_transfers.sql
+++ models/_sector/nft/transfers/nft_transfers.sql
@@ -1,12 +1,14 @@
 {{ config(
-	tags=['legacy'],
-        alias = alias('transfers', legacy_model=True),
-        partition_by = ['block_date'],
+        tags = ['dunesql'],
+        schema = 'nft',
+        alias =alias('transfers'),
+        partition_by = ['block_month'],
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain', 'unique_transfer_id'],
-        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "polygon", "fantom", "goerli"]\',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        unique_key = ['tx_hash', 'evt_index', 'token_id', 'amount'],
+        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "polygon", "fantom", "goerli", "base"]\',
                                     "sector",
                                     "nft",
                                     \'["hildobby", "0xRob"]\') }}'
@@ -14,15 +16,16 @@
 }}
 
 {% set nft_models = [
- ref('nft_ethereum_transfers_legacy')
-,ref('nft_bnb_transfers_legacy')
-,ref('nft_avalanche_c_transfers_legacy')
-,ref('nft_gnosis_transfers_legacy')
-,ref('nft_optimism_transfers_legacy')
-,ref('nft_arbitrum_transfers_legacy')
-,ref('nft_polygon_transfers_legacy')
-,ref('nft_fantom_transfers_legacy')
-,ref('nft_goerli_transfers_legacy')
+ ref('nft_ethereum_transfers')
+,ref('nft_bnb_transfers')
+,ref('nft_avalanche_c_transfers')
+,ref('nft_gnosis_transfers')
+,ref('nft_optimism_transfers')
+,ref('nft_arbitrum_transfers')
+,ref('nft_polygon_transfers')
+,ref('nft_fantom_transfers')
+,ref('nft_goerli_transfers')
+,ref('nft_base_transfers')
 ] %}
 
 SELECT *
@@ -31,6 +34,7 @@ FROM (
     SELECT
           blockchain
         , block_time
+        , block_month
         , block_date
         , block_number
         , token_standard
@@ -39,14 +43,14 @@ FROM (
         , contract_address
         , token_id
         , amount
-        , `from`
+        , "from"
         , to
         , executed_by
         , tx_hash
         , unique_transfer_id
     FROM {{ nft_model }}
     {% if is_incremental() %}
-    WHERE block_time >= date_trunc("day", now() - interval '1 week')
+    WHERE {{incremental_predicate('block_time')}}
     {% endif %}
     {% if not loop.last %}
     UNION ALL
