cff07c4630e5655423ade3f76c1f6c528e7a523c
Alan Ghobadi <aalan3@gmail.com>
2023-10-06 11:53:36 +0200

    Fix test

--- tests/transfers/polygon/transfers_polygon_erc20_test.sql
+++ tests/transfers/polygon/transfers_polygon_erc20_test.sql
@@ -7,7 +7,7 @@ with test_data as (
 ),
 
 test_result as (
-    select case when total = 132014788 then true else false end as success
+    select case when total = 131949227 then true else false end as success
     from test_data
 )
 
