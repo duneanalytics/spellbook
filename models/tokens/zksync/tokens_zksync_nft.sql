75f9b67cea154dd5b11cf47ca8962a9a5475c3c9
Landon Gingerich <landon.ging@gmail.com>
2023-10-06 19:27:15 +0000

    Add more nft models for zkSync on `tokens`

---
+++ models/tokens/zksync/tokens_zksync_nft.sql
@@ -0,0 +1,18 @@
+{{ config(tags=['dunesql'],
+        alias = alias('nft')
+        , materialized = 'table'
+        , post_hook='{{ expose_spells(\'["zksync"]\',
+                                "sector",
+                                "tokens",
+                                \'["lgingerich"]\') }}'
+        )
+}}
+
+SELECT
+    c.contract_address
+  , t.name
+  , t.symbol
+  , c.standard
+  FROM {{ ref('tokens_zksync_nft_standards')}} c
+LEFT JOIN  {{ref('tokens_zksync_nft_curated')}} t
+ON c.contract_address = t.contract_address
