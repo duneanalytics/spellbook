6c5e26f8cddd86db2f0fcbef730ce942a016b9ad
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-10-06 18:42:45 -0700

    backquote

--- models/seaport/avalanche_c/seaport_avalanche_c_trades.sql
+++ models/seaport/avalanche_c/seaport_avalanche_c_trades.sql
@@ -216,8 +216,8 @@ with source_avalanche_c_transactions as (
 ,iv_trades as (
   select a.*
           ,n.name AS nft_token_name
-          ,t.`from` as tx_from
-          ,t.`to` as tx_to
+          ,t."from" as tx_from
+          ,t."to" as tx_to
           ,right(t.data,8) as right_hash
           ,case when a.token_contract_address = {{c_native_token_address}} then '{{c_native_symbol}}'
                 else e.symbol
