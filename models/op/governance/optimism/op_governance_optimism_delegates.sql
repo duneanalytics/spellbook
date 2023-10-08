fe2c40319556be188dad9407c5e387fbf98ad4f6
kaiblade <kaibladenet@gmail.com>
2023-10-08 13:26:31 +0100

    edited the op_governance_optimism_delegates model

--- models/op/governance/optimism/op_governance_optimism_delegates.sql
+++ models/op/governance/optimism/op_governance_optimism_delegates.sql
@@ -24,9 +24,9 @@ CAST(previousBalance AS DOUBLE)/1e18 AS previousBalance,
 CAST(newBalance AS DOUBLE)/1e18 - CAST(previousBalance AS DOUBLE)/1e18 AS power_diff
 FROM {{ source('op_optimism', 'GovernanceToken_evt_DelegateVotesChanged') }}
 WHERE CAST(evt_block_time AS DATE) >= DATE'2022-05-26'
-{% if is_incremental() %}
-    AND evt_block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     AND evt_block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 ),
 
 
@@ -34,18 +34,18 @@ rolling_voting_power AS
 (SELECT *,
 SUM(power_diff) OVER (ORDER BY block_time) AS total_voting_power
 FROM delegate_votes_data_raw
-{% if is_incremental() %}
-    WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 ),
 
 voting_power_share_data AS
 (SELECT *, 
 (CAST(newBalance AS DOUBLE)/CAST(total_voting_power AS DOUBLE)) * 100 AS voting_power_share
 FROM rolling_voting_power
-{% if is_incremental() %}
-    WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 ),
 
 combined_delegator_count AS
@@ -58,9 +58,9 @@ fromDelegate AS delegate,
 FROM {{ source('op_optimism', 'GovernanceToken_evt_DelegateChanged') }}
 WHERE fromDelegate != 0x0000000000000000000000000000000000000000
 -- AND CAST(evt_block_time AS DATE) >= DATE'2022-05-26'
-{% if is_incremental() %}
-    AND evt_block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     AND evt_block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 
 UNION 
 
@@ -72,9 +72,9 @@ toDelegate AS delegate,
 1 AS delegator_count
 FROM {{ source('op_optimism', 'GovernanceToken_evt_DelegateChanged') }}
 -- WHERE CAST(evt_block_time AS DATE) >= DATE'2022-05-26'
-{% if is_incremental() %}
-    WHERE evt_block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %} 
+-- {% if is_incremental() %}
+--     WHERE evt_block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %} 
 ),
 
 delegator_count_data AS
@@ -82,9 +82,9 @@ delegator_count_data AS
 SUM(delegator_count) OVER (PARTITION BY delegate ORDER BY block_time) AS number_of_delegators,
 SUM(delegator_count) OVER (ORDER BY block_time) AS total_delegators
 FROM combined_delegator_count
-{% if is_incremental() %}
-    WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 ),
 
 votingPower_delegators_data AS
@@ -95,9 +95,9 @@ LEFT JOIN delegator_count_data del
 ON power.delegate = del.delegate
 AND power.tx_hash = del.tx_hash
 AND power.block_number = del.block_number
-{% if is_incremental() %}
-    AND power.block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     AND power.block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 ),
 
 votingPower_delegators_data_revised AS
@@ -120,9 +120,9 @@ voting_power_share,
 LAST_VALUE(number_of_delegators) IGNORE NULLS OVER (PARTITION BY delegate ORDER BY block_time) AS number_of_delegators,
 LAST_VALUE(total_delegators) IGNORE NULLS OVER (ORDER BY block_time) AS total_delegators
 FROM votingPower_delegators_data
-{% if is_incremental() %}
-    WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 ),
 
 OP_delegates_table_raw AS
@@ -139,18 +139,18 @@ voting_power_share,
 COALESCE(number_of_delegators,1) AS number_of_delegators,
 total_delegators
 FROM votingPower_delegators_data_revised
-{% if is_incremental() %}
-    WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 ), 
 
 op_delegates_table AS
 (SELECT *, 
 (CAST(number_of_delegators AS DOUBLE) / CAST(total_delegators AS DOUBLE))*100 AS total_delegators_share
 FROM OP_delegates_table_raw
-{% if is_incremental() %}
-    WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
-{% endif %}
+-- {% if is_incremental() %}
+--     WHERE block_time >= DATE_TRUNC('day', NOW() - INTERVAL '7' DAY)
+-- {% endif %}
 )
 
 
