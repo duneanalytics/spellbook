ff00618679ed99cd191699d814885f2245eda8c1
Huang Geyang <Sukebeta@outlook.com>
2023-10-09 22:53:07 +0800

    Fix uniswap_v3_ethereum_trades_beta

--- models/_sector/dex/trades/platforms/uniswap/ethereum/uniswap_v3_ethereum_trades_beta.sql
+++ models/_sector/dex/trades/platforms/uniswap/ethereum/uniswap_v3_ethereum_trades_beta.sql
@@ -13,7 +13,7 @@
 -- demo of not breaking existing queries on `uniswap_v3_ethereum.trades`
 -- uniswap_v3_ethereum.base_trades -> dex.trades -> uniswap_v3_ethereum.trades
 select *
-from ref('dex_trades_beta')
+from {{ ref('dex_trades_beta') }}
 where project = 'uniswap'
   and version = '3'
   and blockchain = 'ethereum'
