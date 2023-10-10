f054f3aaa7d801ac9ed010c6adcb731ac89b8bde
Hildebert Moulié <h.moulie@gmail.com>
2023-10-10 03:06:24 +0200

    fix

--- models/staking/ethereum/staking_ethereum_deposits.sql
+++ models/staking/ethereum/staking_ethereum_deposits.sql
@@ -5,7 +5,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['tx_hash', 'evt_index'],
+    unique_key = ['tx_hash', 'evt_index', 'entity'],
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "sector",
                                 "staking",
