48365dbdfb8d28ba1db12165136c1111ca342fcf
alicecha <110170331+alicecha@users.noreply.github.com>
2023-10-10 11:06:01 +0200

    Updated eth transfers script for zksync

--- models/safe/zksync/safe_zksync_eth_transfers.sql
+++ models/safe/zksync/safe_zksync_eth_transfers.sql
@@ -8,10 +8,10 @@
         on_schema_change='fail',
         file_format ='delta',
         incremental_strategy='merge',
-        post_hook='{{ expose_spells(\'["base"]\',
+        post_hook='{{ expose_spells(\'["zksync"]\',
                                     "project",
                                     "safe",
-                                    \'["danielpartida", "hosuke"]\') }}'
+                                    \'["kryptaki"]\') }}'
     )
 }}
 
@@ -23,14 +23,14 @@ select
     -CAST(et.value AS INT256) as amount_raw,
     et.tx_hash,
     array_join(et.trace_address, ',') as trace_address
-from {{ source('base', 'traces') }} et
-join {{ ref('safe_base_safes') }} s on et."from" = s.address
+from {{ source('zksync', 'traces') }} et
+join {{ ref('safe_zksync_safes') }} s on et."from" = s.address
     and et."from" != et.to -- exclude calls to self to guarantee unique key property
     and et.success = true
     and (lower(et.call_type) not in ('delegatecall', 'callcode', 'staticcall') or et.call_type is null)
     and et.value > UINT256 '0' -- et.value is uint256 type
 {% if not is_incremental() %}
-where et.block_time > timestamp '2023-07-01' -- for initial query optimisation
+where et.block_time > timestamp '2023-09-01' -- for initial query optimisation
 {% endif %}
 {% if is_incremental() %}
 -- to prevent potential counterfactual safe deployment issues we take a bigger interval
@@ -47,14 +47,14 @@ select
     CAST(et.value AS INT256) as amount_raw,
     et.tx_hash,
     array_join(et.trace_address, ',') as trace_address
-from {{ source('base', 'traces') }} et
-join {{ ref('safe_base_safes') }} s on et.to = s.address
+from {{ source('zksync', 'traces') }} et
+join {{ ref('safe_zksync_safes') }} s on et.to = s.address
     and et."from" != et.to -- exclude calls to self to guarantee unique key property
     and et.success = true
     and (lower(et.call_type) not in ('delegatecall', 'callcode', 'staticcall') or et.call_type is null)
     and et.value > UINT256 '0' -- et.value is uint256 type
 {% if not is_incremental() %}
-where et.block_time > timestamp '2023-07-01' -- for initial query optimisation
+where et.block_time > timestamp '2023-09-01' -- for initial query optimisation
 {% endif %}
 {% if is_incremental() %}
 -- to prevent potential counterfactual safe deployment issues we take a bigger interval
