9fed483fbfe9a5618fdbd1eb103db08b5b06f60d
alicecha <110170331+alicecha@users.noreply.github.com>
2023-10-10 10:54:08 +0200

    Updated singletons script for zksync

--- models/safe/zksync/safe_zksync_singletons.sql
+++ models/safe/zksync/safe_zksync_singletons.sql
@@ -3,14 +3,14 @@
         materialized='table',
         tags = ['dunesql'],
         alias= alias('singletons'),
-        post_hook='{{ expose_spells(\'["base"]\',
+        post_hook='{{ expose_spells(\'["zksync"]\',
                                     "project",
                                     "safe",
-                                    \'["danielpartida"]\') }}'
+                                    \'["kryptaki"]\') }}'
     )
 }}
 
 
 -- Fetch all known singleton/mastercopy addresses used via factories.
 select distinct singleton as address
-from {{ source('gnosis_safe_base', 'GnosisSafeProxyFactoryv_1_3_0_evt_ProxyCreation') }}
+from {{ source('gnosis_safe_zksync', 'GnosisSafeProxyFactoryv_1_3_0_evt_ProxyCreation') }}
