68a4de37bab3d4709ddf23959e70d27ae2aee52b
Alan Ghobadi <aalan3@gmail.com>
2023-10-12 20:26:46 +0200

    Fix whitespace

--- models/solana_utils/solana_utils_daily_balances.sql
+++ models/solana_utils/solana_utils_daily_balances.sql
@@ -14,13 +14,13 @@
                                     \'["ilemi"]\') }}')
 }}
 
-WITH 
+WITH
       updated_balances as (
             SELECT
                   date_trunc('day', block_time) as day
                   , address
                   , coalesce(token_mint_address,original_token_mint_address) as token_mint_address
-                  , cast(post_balance as double)/1e9 as sol_balance --lamport -> sol 
+                  , cast(post_balance as double)/1e9 as sol_balance --lamport -> sol
                   , coalesce(post_token_balance,0) as token_balance --tokens are already correct decimals in this table
                   , coalesce(token_balance_owner,original_token_balance_owner) as token_balance_owner
                   , block_time
@@ -40,26 +40,26 @@ WITH
                         , max_by(tk.token_balance_owner, tk.created_at) as original_token_balance_owner
                         -- , row_number() over (partition by aa.address, aa.block_slot, aa.tx_id, aa.tx_index order by tk.created_at desc) as latest_tk
                   FROM {{ source('solana','account_activity') }}  aa
-                  LEFT JOIN {{ ref('solana_utils_token_accounts')}} tk ON tk.address = aa.address 
+                  LEFT JOIN {{ ref('solana_utils_token_accounts')}} tk ON tk.address = aa.address
                         AND aa.token_mint_address is null --only join on the empty token mints
                         AND tk.created_at <= aa.block_time --only get token mints that were created at or before this account activity
-                  WHERE tx_success 
+                  WHERE tx_success
                   AND block_time > now() - interval '1' day
                   {% if is_incremental() %}
                   AND block_time >= date_trunc('day', now() - interval '1' day)
                   {% endif %}
                   group by 1,2,3,4,5,6,7
-            ) 
+            )
             -- WHERE latest_tk = 1
       )
 
-SELECT 
+SELECT
       day
       , address
       , sol_balance
       , token_mint_address
       , token_balance
       , token_balance_owner
-      , now() as updated_at 
+      , now() as updated_at
 FROM updated_balances
 WHERE latest_balance = 1
