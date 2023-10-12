e84847ed8d767c5ebdb13a35ba4206d0f199a0e8
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-10-11 22:02:04 -0500

    Update solana_utils_daily_balances.sql

--- models/solana_utils/solana_utils_daily_balances.sql
+++ models/solana_utils/solana_utils_daily_balances.sql
@@ -38,7 +38,7 @@ WITH
                         AND aa.token_mint_address is null --only join on the empty token mints
                         AND tk.created_at <= aa.block_time --only get token mints that were created at or before this account activity
                   WHERE tx_success 
-                  AND block_time > now() - interval '30' day
+                  AND block_time > now() - interval '7' day
                   {% if is_incremental() %}
                   AND block_time >= date_trunc('day', now() - interval '1' day)
                   {% endif %}
