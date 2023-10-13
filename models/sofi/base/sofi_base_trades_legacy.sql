9f1a7bdab49dde62a357b74f16b33f32ba71b02d
Hildebert Moulié <h.moulie@gmail.com>
2023-10-14 00:27:44 +0200

    fix

--- models/sofi/base/sofi_base_trades_legacy.sql
+++ models/sofi/base/sofi_base_trades_legacy.sql
@@ -1,12 +1,7 @@
 {{ config(
     schema = 'sofi_base',
     tags = ['legacy', 'static'],
-    alias = alias('trades', legacy_model=True),
-    partition_by = ['block_month'],
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    unique_key = ['tx_hash','evt_index']
+    alias = alias('trades', legacy_model=True)
     )
 }}
 
