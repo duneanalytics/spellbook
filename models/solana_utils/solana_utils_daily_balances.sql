39262d86bc22d39fc003b662f711cde1dc60d182
Alan Ghobadi <aalan3@gmail.com>
2023-10-13 09:35:35 +0200

    Remove filter

--- models/solana_utils/solana_utils_daily_balances.sql
+++ models/solana_utils/solana_utils_daily_balances.sql
@@ -46,7 +46,6 @@ WITH
                         AND aa.token_mint_address is null --only join on the empty token mints
                         AND tk.created_at <= aa.block_time --only get token mints that were created at or before this account activity
                   WHERE tx_success
-                  AND block_time > now() - interval '1' day
                   {% if is_incremental() %}
                   AND block_time >= date_trunc('day', now() - interval '1' day)
                   {% endif %}
