4a3bb4b17a6a7a7b3f2777112b864de5e3b93e13
Hildebert Moulié <h.moulie@gmail.com>
2023-10-13 12:53:54 +0200

    create is_unique_or_null generic test

---
+++ tests/generic/is_unique_or_null.sql
@@ -0,0 +1,10 @@
+{% test is_unique_or_null(model, column_name) %}
+
+select {{ column_name }} AS unique_field
+, COUNT(*) AS n_records
+FROM {{ model }}
+WHERE {{ column_name }} IS NOT NULL
+GROUP BY {{ column_name }}
+HAVING COUNT(*) > 1
+
+{% endtest %}
