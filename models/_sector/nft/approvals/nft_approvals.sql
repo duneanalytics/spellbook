33b275f95790b0349f9a811e24094e300cd4a03f
Landon Gingerich <landon.ging@gmail.com>
2023-10-13 17:51:57 +0000

    Add dummy legacy tables

--- models/_sector/nft/approvals/nft_approvals.sql
+++ models/_sector/nft/approvals/nft_approvals.sql
@@ -8,7 +8,7 @@
         incremental_strategy = 'merge',
         unique_key = ['blockchain', 'tx_hash', 'evt_index'],
         incremental_predicates = ['DBT_INTERNAL_DEST.block_time >= date_trunc(\'day\', now() - interval \'7\' day)'],
-        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "polygon", "fantom", "goerli", "celo", "base"]\',
+        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "polygon", "fantom", "goerli", "celo", "base", "zksync"]\',
                                     "sector",
                                     "nft",
                                     \'["hildobby", "tomfutago"]\') }}')
