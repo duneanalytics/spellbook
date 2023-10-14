f8c870fa7609d2433fa4d48d05e3b06e08490414
Hildebert Moulié <h.moulie@gmail.com>
2023-10-14 19:26:27 +0200

    fix

---
+++ models/tokens/zksync/tokens_zksync_nft.sql
@@ -0,0 +1,22 @@
+{{ config(tags=['dunesql']
+        , schema = 'tokens_zksync'
+        , alias = alias('nft')
+        , materialized = 'table'
+        , post_hook='{{ expose_spells(\'["zksync"]\',
+                                "sector",
+                                "tokens",
+                                \'["hildobby"]\') }}'
+        )
+}}
+
+SELECT
+    c.contract_address
+  , name--coalesce(t.name,b.name) as name
+  , symbol--coalesce(t.symbol,b.symbol) as symbol
+  , c.standard
+
+FROM {{ ref('tokens_zksync_nft_standards')}} c
+LEFT JOIN {{ref('tokens_zksync_nft_curated')}} t
+    ON c.contract_address = t.contract_address
+-- LEFT JOIN 'tokens_zksync_nft_bridged_mapping b
+--     ON c.contract_address = b.contract_address
