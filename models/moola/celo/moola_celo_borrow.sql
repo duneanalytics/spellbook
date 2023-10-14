ad2699ea04e5067ad666b476a876041a31783f37
tomfutago <tomfutago@gmail.com>
2023-10-14 15:35:15 +0100

    add borrow

--- models/moola/celo/moola_celo_borrow.sql
+++ models/moola/celo/moola_celo_borrow.sql
@@ -1,77 +1,93 @@
-{{ config(
-    tags = ['dunesql']
-    , schema = 'aave_v2_ethereum'
-    , alias = alias('borrow')
-  )
+{{
+    config(
+        tags = ['dunesql'],
+        schema = 'moola_celo',
+        alias = alias('borrow'),
+        partition_by = ['evt_block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['transaction_type', 'evt_tx_hash', 'evt_index'],
+        post_hook = '{{ expose_spells(\'["celo"]\',
+                                    "sector",
+                                    "moola",
+                                    \'["tomfutago"]\') }}'
+    )
 }}
 
-SELECT
-      version,
-      transaction_type,
-      loan_type,
-      erc20.symbol,
-      borrow.token as token_address,
-      borrower,
-      repayer,
-      liquidator,
-      amount / power(10, erc20.decimals) AS amount,
-      (amount / power(10, p.decimals)) * price AS usd_amount,
+select
+  borrow.transaction_type,
+  borrow.loan_type,
+  erc20.symbol,
+  borrow.token_address,
+  borrow.borrower,
+  borrow.repayer,
+  borrow.liquidator,
+  borrow.amount / power(10, erc20.decimals) as amount,
+  (borrow.amount / power(10, p.decimals)) * p.price as usd_amount,
+  borrow.evt_tx_hash,
+  borrow.evt_index,
+  borrow.evt_block_time,
+  borrow.evt_block_number
+from (
+    select
+      'borrow' as transaction_type,
+      case
+        when borrowRateMode = uint256 '1' then 'stable'
+        when borrowRateMode = uint256 '2' then 'variable'
+      end as loan_type,
+      reserve as token_address,
+      user as borrower, 
+      cast(null as varbinary) as repayer,
+      cast(null as varbinary) as liquidator,
+      cast(amount as DOUBLE) as amount,
       evt_tx_hash,
       evt_index,
       evt_block_time,
-      evt_block_number   
-FROM (
-SELECT 
-    '2' AS version,
-    'borrow' AS transaction_type,
-    CASE 
-        WHEN borrowRateMode = UINT256 '1' THEN 'stable'
-        WHEN borrowRateMode = UINT256 '2' THEN 'variable'
-    END AS loan_type,
-    reserve AS token,
-    user AS borrower, 
-    CAST(NULL AS VARBINARY) AS repayer,
-    CAST(NULL AS VARBINARY) AS liquidator,
-    CAST(amount AS DOUBLE) AS amount,
-    evt_tx_hash,
-    evt_index,
-    evt_block_time,
-    evt_block_number
-FROM {{ source('aave_v2_ethereum','LendingPool_evt_Borrow') }} 
-UNION ALL 
-SELECT 
-    '2' AS version,
-    'repay' AS transaction_type,
-    NULL AS loan_type,
-    reserve AS token,
-    user AS borrower,
-    repayer AS repayer,
-    CAST(NULL AS VARBINARY) AS liquidator,
-    - CAST(amount AS DOUBLE) AS amount,
-    evt_tx_hash,
-    evt_index,
-    evt_block_time,
-    evt_block_number
-FROM {{ source('aave_v2_ethereum','LendingPool_evt_Repay') }}
-UNION ALL
-SELECT 
-    '2' AS version,
-    'borrow_liquidation' AS transaction_type,
-    NULL AS loan_type,
-    debtAsset AS token,
-    user AS borrower,
-    liquidator AS repayer,
-    liquidator  AS liquidator,
-    - CAST(debtToCover AS DOUBLE) AS amount,
-    evt_tx_hash,
-    evt_index,
-    evt_block_time,
-    evt_block_number
-FROM {{ source('aave_v2_ethereum','LendingPool_evt_LiquidationCall') }}
-) borrow
-LEFT JOIN {{ ref('tokens_ethereum_erc20') }} erc20
-    ON borrow.token = erc20.contract_address
-LEFT JOIN {{ source('prices','usd') }} p 
-    ON p.minute = date_trunc('minute', borrow.evt_block_time) 
-    AND p.contract_address = borrow.token
-    AND p.blockchain = 'ethereum'
+      evt_block_number
+    from {{ source('moolainterestbearingmoo_celo', 'LendingPool_evt_Borrow') }}
+    {% if is_incremental() %}
+    where {{ incremental_predicate('evt_block_time') }}
+    {% endif %}
+    union all 
+    select
+      'repay' as transaction_type,
+      null as loan_type,
+      reserve as token_address,
+      user as borrower,
+      repayer as repayer,
+      cast(null as varbinary) as liquidator,
+      -1 * cast(amount as DOUBLE) as amount,
+      evt_tx_hash,
+      evt_index,
+      evt_block_time,
+      evt_block_number
+    from {{ source('moolainterestbearingmoo_celo', 'LendingPool_evt_Repay') }}
+    {% if is_incremental() %}
+    where {{ incremental_predicate('evt_block_time') }}
+    {% endif %}
+    union all
+    select
+      'borrow_liquidation' as transaction_type,
+      null as loan_type,
+      debtAsset as token_address,
+      user as borrower,
+      liquidator as repayer,
+      liquidator  as liquidator,
+      -1 * cast(debtToCover as DOUBLE) as amount,
+      evt_tx_hash,
+      evt_index,
+      evt_block_time,
+      evt_block_number
+    from {{ source('moolainterestbearingmoo_celo', 'LendingPool_evt_LiquidationCall') }}
+    {% if is_incremental() %}
+    where {{ incremental_predicate('evt_block_time') }}
+    {% endif %}
+  ) borrow
+  left join {{ ref('tokens_celo_erc20') }} erc20 on borrow.token_address = erc20.contract_address
+  left join {{ source('prices', 'usd') }} p on p.blockchain = 'celo'
+    and date_trunc('minute', borrow.evt_block_time) = p.minute
+    and borrow.token_address = p.contract_address
+    {% if is_incremental() %}
+    and {{ incremental_predicate('p.minute') }}
+    {% endif %}
