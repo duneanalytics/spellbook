141369ee1ac0d3bcda0b09982c1b5ab2d0f21b4d
tomfutago <tomfutago@gmail.com>
2023-10-15 16:29:40 +0100

    call macro on celo

---
+++ models/_sector/nft/mints/native/nft_celo_native_mints.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        tags = ['dunesql'],
+        schema = 'nft_celo',
+        alias = alias('native_mints'),
+        partition_by = ['block_month'],
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash','evt_index','token_id','number_of_items']
+    )
+}}
+
+{% set blockchain = 'celo' %}
+
+{{
+    nft_mints(
+        blockchain = blockchain,
+        src_contracts = source(blockchain, 'contracts'),
+        src_traces = source(blockchain, 'traces'),
+        src_transactions = source(blockchain, 'transactions'),
+        src_prices_usd = source('prices', 'usd'),
+        src_erc20_evt_transfer = source('erc20_' ~ blockchain, 'evt_transfer'),
+        nft_transfers = ref('nft_' ~ blockchain ~ '_transfers'),
+        nft_aggregators = ref('nft_' ~ blockchain ~ '_aggregators'),
+        tokens_nft = ref('tokens_' ~ blockchain ~ '_nft'),
+        default_currency_symbol = 'CELO',
+        default_currency_contract = 0x000000000000000000000000000000000000dead
+    )
+}}
