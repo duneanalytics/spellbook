413066b1fbb06ebcbe455f96db51438a4d7c77c5
Hildebert Moulié <h.moulie@gmail.com>
2023-10-15 19:10:40 +0200

    fix

---
+++ models/staking/ethereum/entities/staking_ethereum_entities_batch_contracts_legacy.sql
@@ -0,0 +1,9 @@
+{{
+  config(
+        schema = 'staking_ethereum',
+        alias = alias('entities_batch_contracts', legacy_model=True),
+        tags=['legacy', 'static']
+        )
+}}
+
+SELECT 1
