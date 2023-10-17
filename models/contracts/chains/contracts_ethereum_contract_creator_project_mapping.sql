f6a9e9d35f27232df0b23b8e946dd94ff55f40af
jeff-dude <jeff@dune.xyz>
2023-10-17 09:43:10 -0400

    update tag to prod_daily, add incr predicates

--- models/contracts/chains/contracts_ethereum_contract_creator_project_mapping.sql
+++ models/contracts/chains/contracts_ethereum_contract_creator_project_mapping.sql
@@ -1,11 +1,12 @@
  {{
   config(
-        tags = ['dunesql', 'daily'],
+        tags = ['dunesql', 'prod_daily'],
         schema = 'contracts_ethereum',
         alias = alias('creator_project_mapping'),
         materialized ='incremental',
         file_format ='delta',
         incremental_strategy='merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.created_time')],
         unique_key='contract_address',
         partition_by = ['created_month'],
         pre_hook='{{ enforce_join_distribution("PARTITIONED") }}'
--- models/contracts/chains/contracts_ethereum_find_self_destruct_contracts.sql
+++ models/contracts/chains/contracts_ethereum_find_self_destruct_contracts.sql
@@ -1,11 +1,12 @@
  {{
   config(
-        tags = ['dunesql'],
+        tags = ['dunesql', 'prod_daily'],
         schema = 'contracts_ethereum',
         alias = alias('find_self_destruct_contracts'),
         materialized ='incremental',
         file_format ='delta',
         incremental_strategy='merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.created_time')],
         unique_key='contract_address'
   )
 }}
