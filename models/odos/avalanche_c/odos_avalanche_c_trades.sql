3713dc8ea6e4f3523952ab901753427d0d286db3
jeff-dude <jeff@dune.xyz>
2023-10-17 14:14:09 -0400

    add the remaining spells

--- models/odos/avalanche_c/odos_avalanche_c_trades.sql
+++ models/odos/avalanche_c/odos_avalanche_c_trades.sql
@@ -13,6 +13,11 @@
     )
 }}
 
+/*
+    note: this spell has not been migrated to dunesql, as there are duplicates issues and issue is not resolved
+        please migrate to dunesql & fix duplicates to ensure up-to-date logic & data
+*/
+
 
 {% set project_start_date = '2022-11-29' %}
 
