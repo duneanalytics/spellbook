810a8481b01f32c4d3fe911b29b997e5f9e1b33e
Alan Ghobadi <aalan3@gmail.com>
2023-10-17 15:16:38 +0200

    elif

--- macros/dune/source.sql
+++ macros/dune/source.sql
@@ -2,7 +2,7 @@
   {% set rel = builtins.source(source_name, table_name) %}
   {%- if source_name == 'dune' -%}
     {% do return(rel) %}
-  {%- else if target.type == 'trino' -%}
+  {%- elif target.type == 'trino' -%}
       {% set newrel = rel.replace_path(database="delta_prod") %}
     {% do return(newrel) %}
   {%- else -%}
