0151b29c14dfdf40a74b9dd4cbb9cf2d0d8ec68c
grkhr <eduardgorkh@gmail.com>
2023-10-18 14:19:57 +0200

    try recent start date

--- macros/models/_project/oneinch/oneinch_calls_transfers_macro.sql
+++ macros/models/_project/oneinch/oneinch_calls_transfers_macro.sql
@@ -5,7 +5,9 @@
     ) 
 %}
 
-{% set project_start_date = "timestamp '" + project_start_date_str + "'" %} 
+-- {% set project_start_date = "timestamp '" + project_start_date_str + "'" %} 
+
+{% set project_start_date = "timestamp '2023-10-01'" %} 
 {% set lookback_days = -7 %}
 {% set transfer_selector = '0xa9059cbb' %}
 {% set transfer_from_selector = '0x23b872dd' %}
