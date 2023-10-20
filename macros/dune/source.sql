3c7df315012f8dc27151bc4fcf51541485c5e200
Alan Ghobadi <aalan3@gmail.com>
2023-10-20 12:38:32 +0200

    Remove formatting to avoid triggering full refresh

--- macros/dune/source.sql
+++ macros/dune/source.sql
@@ -1,7 +1,7 @@
 {% macro source(source_name, table_name) %}
   {% set rel = builtins.source(source_name, table_name) %}
   {%- if target.type == 'trino' -%}
-      {% set newrel = rel.replace_path(database="delta_prod") %}
+    {% set newrel = rel.replace_path(database="delta_prod") %}
     {% do return(newrel) %}
   {%- else -%}
     {% do return(rel) %}
