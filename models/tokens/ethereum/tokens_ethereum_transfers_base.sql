36127d738ee98ab28f34a5aa998e9c7bdeae1aa7
Alan Ghobadi <aalan3@gmail.com>
2023-10-20 10:53:46 +0200

    Add incremental predicate

--- models/tokens/ethereum/tokens_ethereum_transfers_base.sql
+++ models/tokens/ethereum/tokens_ethereum_transfers_base.sql
@@ -5,6 +5,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
         unique_key = ['block_number', 'tx_index', 'evt_index', 'trace_address'],
         )
 }}
