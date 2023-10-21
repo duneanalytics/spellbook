bfb089a4b17110ce596bee558ad1e2f43f8a4092
tomfutago <tomfutago@gmail.com>
2023-10-21 17:12:43 +0100

    init commit

---
+++ models/curvefi/celo/curvefi_celo_pools.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        tags = ['dunesql'],
+        alias = alias('pools'),
+        materialized = 'table',
+        unique_key = ['version', 'tokenid', 'token', 'pool'],
+        post_hook = '{{ expose_spells(\'["celo"]\',
+                                    "project",
+                                    "curvefi",
+                                    \'["tomfutago"]\') }}'
+    )
+}}
+
+select
+  'Base Pool' as version,
+  cast(arg0 as int256) as tokenid,
+  output_0 as token,
+  contract_address as pool
+from {{ source('curvefi_celo', 'StableSwap_call_coins') }}
+where call_success
+  and output_0 is not null
+group by 2,3,4
