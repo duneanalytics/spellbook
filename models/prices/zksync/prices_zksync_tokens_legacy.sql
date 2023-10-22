8ae197836d075c67ce307f1f94a653fbc1db1a83
Landon Gingerich <landon.ging@gmail.com>
2023-10-22 20:22:21 +0000

    remove legacy file

--- models/prices/zksync/prices_zksync_tokens_legacy.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-        schema='prices_zksync',
-        alias = alias('tokens', legacy_model=True),
-        materialized='table',
-        file_format = 'delta',
-        tags=['legacy', 'static']
-        )
-}}
-SELECT 1
