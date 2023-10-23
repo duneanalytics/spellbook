c446a431658640434b14c7a4ca526554b780ad79
Huang Geyang <Sukebeta@outlook.com>
2023-10-23 11:52:46 +0800

    Update models/openocean/optimism/openocean_optimism_trades.sql

--- models/openocean/optimism/openocean_optimism_trades.sql
+++ models/openocean/optimism/openocean_optimism_trades.sql
@@ -6,7 +6,11 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address']
+    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index'],
+    post_hook='{{ expose_spells(\'["optimism"]\',
+                                "project",
+                                "openocean",
+                                \'["ARDev097"]\') }}'
     )
 }}
 
