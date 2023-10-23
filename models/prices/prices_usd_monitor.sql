eeb21a6aa6046a307ebd8b0322f7f84a89a9ab8a
0xRob <0xRobin5@gmail.com>
2023-10-23 13:21:22 +0200

    syntax

--- models/prices/prices_usd_monitor.sql
+++ models/prices/prices_usd_monitor.sql
@@ -10,7 +10,7 @@
 -- monitor to gather quantile information about prices latency
 select
     now() as recorded_at
-    ,cast(qdigest(latency)as varbinary) latency_digest_varbinary
+    ,cast(qdigest(latency) as varbinary) as latency_digest_varbinary
 from (
     select
     blockchain, contract_address, date_diff('second',now(),max(minute))) as latency
