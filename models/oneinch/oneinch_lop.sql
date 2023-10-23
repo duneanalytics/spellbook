9ac60abf9673622a449d578a0e278f037b7e9e84
grkhr <eduardgorkh@gmail.com>
2023-10-23 13:40:47 +0200

    test run query

--- models/oneinch/oneinch_lop.sql
+++ models/oneinch/oneinch_lop.sql
@@ -14,7 +14,7 @@
 
 with
     
-    methods as (
+    _methods as (
         select
             contract_address
             , contract_name
@@ -35,6 +35,8 @@ with
             and json_value(entity, 'lax $.stateMutability') in ('payable', 'nonpayable')
             and position('fill' in lower(json_value(entity, 'lax $.name'))) > 0
     )
+
+    {% set methods = run_query('select * from _methods') %}
 
     , orders as (
         
