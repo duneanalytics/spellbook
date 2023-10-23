e5d9c1593346acb74903118a74e304ce043217c2
Hildebert Moulié <h.moulie@gmail.com>
2023-10-23 18:04:09 +0200

    rm legacy files, and make withdrawal_credentials static

--- models/staking/ethereum/entities/staking_ethereum_entities_bitcoin_suisse_legacy.sql
+++
@@ -1,9 +0,0 @@
-{{
-  config(
-        schema = 'staking_ethereum',
-        alias = alias('entities_bitcoin_suisse', legacy_model=True),
-        tags=['legacy', 'static']
-        )
-}}
-
-SELECT 1
--- models/staking/ethereum/entities/staking_ethereum_entities_withdrawal_credentials_legacy.sql
+++
@@ -1,9 +0,0 @@
-{{
-  config(
-        schema = 'staking_ethereum',
-        alias = alias('entities_withdrawal_credentials', legacy_model=True),
-        tags=['legacy', 'static']
-        )
-}}
-
-SELECT 1
