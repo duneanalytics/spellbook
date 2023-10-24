1377ff7f499bbed0dd98326b1cbb8c3141fb5f03
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-10-24 04:17:45 -0500

    Solana raydium v4 and orca twohop (#4661)
    * init commit
    
    * raydium v4 done I think
    
    * fix
    
    * add twohop
    
    * fix
    
    * fix account_amm
    
    * small edits
    
    * fix dupe account issue

--- models/solana_utils/solana_utils_token_accounts.sql
+++ models/solana_utils/solana_utils_token_accounts.sql
@@ -15,12 +15,12 @@ WITH
       distinct_accounts as (
             SELECT
                   token_mint_address
-                  , token_balance_owner
                   , address 
+                  , max_by(token_balance_owner, block_time) as token_balance_owner --some account created before and then again after token owner schema change, so then it created dupes.
                   , min(block_time) as created_at 
             FROM {{ source('solana','account_activity') }}
             WHERE token_mint_address is not null
-            group by 1,2,3
+            group by 1,2
       )
       
 SELECT * FROM distinct_accounts
