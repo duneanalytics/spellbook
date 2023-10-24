24826768a6358f0c7606e71c8232095e32438966
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-10-24 10:38:21 -0400

    test partition

--- models/evms/evms_transactions.sql
+++ models/evms/evms_transactions.sql
@@ -1,6 +1,7 @@
 {{ config(
         tags = ['dunesql'],
         alias = alias('transactions'),
+        partition_by = ['blockchain'],
         unique_key=['blockchain', 'tx_hash', 'evt_index'],
         post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo", "base", "goerli"]\',
                                     "sector",
