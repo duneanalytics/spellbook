8d0d6eb56f4d1eeaf0e5b012340395229128b412
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-10-24 16:32:28 -0400

    make new spellz

---
+++ models/contracts/chains/contracts_ethereum_contract_creator_project_base_level.sql
@@ -0,0 +1,17 @@
+ {{
+  config(
+        tags = ['dunesql'],
+        schema = 'contracts_ethereum',
+        alias = alias('creator_project_base_level'),
+        materialized ='incremental',
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key='contract_address',
+        partition_by = ['created_month'],
+        pre_hook='{{ enforce_join_distribution("PARTITIONED") }}'
+  )
+}}
+
+{{contracts_creator_project_base_level(
+    chain='ethereum'
+)}}
