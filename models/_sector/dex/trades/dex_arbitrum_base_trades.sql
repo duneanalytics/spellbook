9e662346490aa4c8781a1a756426bc76ad59a249
Huang Geyang <Sukebeta@outlook.com>
2023-10-25 21:09:52 +0800

    Add base union trades

---
+++ models/_sector/dex/trades/dex_arbitrum_base_trades.sql
@@ -0,0 +1,51 @@
+{{ config(
+    tags=['dunesql'],
+    schema = 'dex_arbitrum',
+    alias = 'base_trades',
+    partition_by = ['block_month'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_date', 'project', 'version', 'tx_hash', 'evt_index']
+    )
+}}
+
+
+-- (blockchain, project, project_version, model)
+{% set base_models = [
+    ref('uniswap_arbitrum_v3_base_trades')
+] %}
+
+WITH base_union AS (
+    SELECT * FROM (
+        {% for base_model in base_models %}
+        SELECT
+            blockchain,
+            project,
+            version,
+            block_date,
+            block_month,
+            block_time,
+            token_bought_amount_raw,
+            token_sold_amount_raw,
+            token_bought_address,
+            token_sold_address,
+            taker,
+            maker,
+            project_contract_address,
+            tx_hash,
+            evt_index,
+            row_number() over (partition by tx_hash, evt_index order by tx_hash) as duplicates_rank
+        FROM {{ base_model }}
+        {% if is_incremental() %}
+        WHERE {{ incremental_predicate('block_time') }}
+        {% endif %}
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+        {% endfor %}
+    )
+    WHERE duplicates_rank = 1
+)
+
+{{ add_tx_from_and_to('base_union', 'arbitrum') }}
