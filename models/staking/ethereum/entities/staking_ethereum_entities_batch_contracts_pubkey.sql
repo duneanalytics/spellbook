68b955bc0ae24c66aa5bc659c773d5886b245cb7
hildobby <h.moulie@gmail.com>
2023-10-25 16:00:52 +0200

    Add staking entities (#4621)
    * add entities
    
    * rebrand Huobi to HTX
    
    * add unique or null tests
    
    * clean up
    
    * add manifold finance
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    * fix
    
    * Add Bitpanda, Binance, US & Ether Capital
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * make all category names singular
    
    * rm legacy files, and make withdrawal_credentials static
    
    * add staking_ethereum_entities_legacy.sql back cause labels depend on it
    
    * rename Manifold Finance to MEV Protocol
    
    ---------
    
    Co-authored-by: jeff <102681548+jeff-dude@users.noreply.github.com>

---
+++ models/staking/ethereum/entities/staking_ethereum_entities_batch_contracts_pubkey.sql
@@ -0,0 +1,57 @@
+{{ config(
+    schema = 'staking_ethereum',
+    alias = alias('entities_batch_contracts_pubkey'),
+    tags = ['dunesql'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['pubkey'])
+}}
+
+WITH tagged_entities AS (
+    SELECT funds_origin, entity, category
+    FROM (VALUES
+        (0x301407427168FB51bcc927b9FB76DCd88Fe45681, 'Ether Capital', 'Staking Pool')
+        ) 
+        x (funds_origin, entity, category)
+    )
+
+, batch_contracts AS (
+    SELECT contract
+    FROM (VALUES
+        (0x1e68238ce926dec62b3fbc99ab06eb1d85ce0270) -- Kiln 1
+        , (0x9b8c989ff27e948f55b53bb19b3cc1947852e394) -- Kiln 2
+        , (0x1BDc639EaBF1c5EbC020Bb79E2dD069A8b6fe865) -- BatchDeposit
+        , (0xe8239B17034c372CDF8A5F8d3cCb7Cf1795c4572) -- Batch Deposit
+        ) AS temp_table (contract)
+    )
+
+SELECT d.pubkey
+, e.entity
+, CONCAT(e.entity, ' ', CAST(ROW_NUMBER() OVER (PARTITION BY entity ORDER BY MIN(traces.block_time)) AS VARCHAR)) AS entity_unique_name
+, e.category
+FROM {{ source('eth2_ethereum', 'DepositContract_evt_DepositEvent') }} d
+INNER JOIN {{ source('ethereum', 'traces') }} dep ON dep.to = 0x00000000219ab540356cbb839cbe05303d7705fa
+    AND (dep.call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR dep.call_type IS NULL)
+    AND dep.value > UINT256 '0'
+    AND dep.success
+    AND dep.block_time >= TIMESTAMP '2020-10-14'
+    {% if is_incremental() %}
+    AND dep.block_time >= date_trunc('day', now() - interval '7' day)
+    {% endif %}
+INNER JOIN batch_contracts bc ON bc.contract=dep."from"
+INNER JOIN {{ source('ethereum', 'traces') }} traces ON traces.block_number=d.evt_block_number
+    AND traces.tx_hash=d.evt_tx_hash
+    AND traces.to=dep."from"
+    AND (traces.call_type NOT IN ('delegatecall', 'callcode', 'staticcall') OR traces.call_type IS NULL)
+    AND traces.value > UINT256 '0'
+    AND traces.block_time >= TIMESTAMP '2020-10-14'
+    {% if is_incremental() %}
+    AND traces.block_time >= date_trunc('day', now() - interval '7' day)
+    {% endif %}
+INNER JOIN tagged_entities e ON e.funds_origin=traces."from"
+WHERE d.evt_block_time >= TIMESTAMP '2020-10-14'
+{% if is_incremental() %}
+AND d.evt_block_time >= date_trunc('day', now() - interval '7' day)
+{% endif %}
+GROUP BY 1, 2, 4
--- models/staking/ethereum/staking_ethereum_entities.sql
+++ models/staking/ethereum/staking_ethereum_entities.sql
@@ -6,7 +6,7 @@
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "sector",
                                 "staking",
-                                \'["hildobby", "sankinyue"]\') }}')
+                                \'["hildobby", "sankinyue", "nerolation"]\') }}')
 }}
 
 {% set entities_identifiers_models = [
@@ -17,11 +17,16 @@
      , ('depositor_address', ref('staking_ethereum_entities_darma_capital'))
      , ('tx_from', ref('staking_ethereum_entities_tx_from_addresses'))
      , ('pubkey', ref('staking_ethereum_entities_chorusone'))
+     , ('tx_from', ref('staking_ethereum_entities_batch_contracts_tx_from'))
+     , ('pubkey', ref('staking_ethereum_entities_batch_contracts_pubkey'))
+     , ('withdrawal_credentials', ref('staking_ethereum_entities_withdrawal_credentials'))
+     , ('depositor_address', ref('staking_ethereum_entities_bitcoin_suisse'))
 ] %}
 
 SELECT depositor_address
 , tx_from
 , pubkey
+, withdrawal_credentials
 , entity
 , entity_unique_name
 , category
@@ -30,6 +35,7 @@ FROM (
         SELECT depositor_address
         , from_hex(NULL) AS tx_from
         , from_hex(NULL) AS pubkey
+        , from_hex(NULL) AS withdrawal_credentials
         , entity
         , entity_unique_name
         , category
@@ -45,6 +51,7 @@ FROM (
         SELECT from_hex(NULL) AS depositor_address
         , tx_from
         , from_hex(NULL) AS pubkey
+        , from_hex(NULL) AS withdrawal_credentials
         , entity
         , entity_unique_name
         , category
@@ -59,7 +66,24 @@ FROM (
         {% for entities_identifiers_model in entities_identifiers_models if entities_identifiers_model[0] == 'pubkey' %}
         SELECT from_hex(NULL) AS depositor_address
         , from_hex(NULL) AS tx_from
-        , pubkey AS pubkey
+        , pubkey
+        , from_hex(NULL) AS withdrawal_credentials
+        , entity
+        , entity_unique_name
+        , category
+        FROM {{ entities_identifiers_model[1] }}
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+        {% endfor %}
+
+        UNION ALL
+
+        {% for entities_identifiers_model in entities_identifiers_models if entities_identifiers_model[0] == 'withdrawal_credentials' %}
+        SELECT from_hex(NULL) AS depositor_address
+        , from_hex(NULL) AS tx_from
+        , from_hex(NULL) AS pubkey
+        , withdrawal_credentials
         , entity
         , entity_unique_name
         , category
