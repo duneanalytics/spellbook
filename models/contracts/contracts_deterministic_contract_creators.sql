19250c08296552bfdd64e35e82abccd75f272935
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-10-25 09:08:29 -0400

    deploy

--- models/contracts/contracts_deterministic_contract_creators.sql
+++ models/contracts/contracts_deterministic_contract_creators.sql
@@ -35,6 +35,7 @@ FROM (values
   ,(0xdeaddeaddeaddeaddeaddeaddeaddeaddead0006, 'Genesis Contract')
   ,(0xe1cb04a0fa36ddd16a06ea828007e35e1a3cbc37, 'Singleton Factory')
   ,(0x488E1A80133870CB71EE2b08f926CE329d56B084, 'Deployer')
+  ,(0x0000000000000000000000000000000000008006, 'zkSync Contract Deployer')
   -- excluded creators
   ,(0x36BDE71C97B33Cc4729cf772aE268934f7AB70B2, 'Optimism: CDM Relay')
 
