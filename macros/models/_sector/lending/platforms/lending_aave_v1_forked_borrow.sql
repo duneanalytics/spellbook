f9329c25bde49277c6fb9dd29e35db39f3a2d820
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-10-26 17:26:51 +0100

    lending aave borrow macros

---
+++ macros/models/_sector/lending/platforms/lending_aave_v1_forked_borrow.sql
@@ -0,0 +1,96 @@
+{%
+  macro lending_aave_v1_forked_borrow(
+    blockchain,
+    project,
+    version,
+    aave_mock_address,
+    native_token_address,
+    LendingPool_evt_Borrow,
+    LendingPool_evt_Repay,
+    LendingPool_evt_LiquidationCall
+  )
+%}
+
+with base_borrow as (
+  select
+    'borrow' as transaction_type,
+    case 
+      when _borrowRateMode = uint256 '1' then 'stable'
+      when _borrowRateMode = uint256 '2' then 'variable'
+    end as loan_type,
+    case
+      when _reserve = {{ aave_mock_address }} then {{ native_token_address }} --using native_token_address instead of Aave "mock" address
+      else _reserve
+    end as token_address,
+    _user as borrower,
+    cast(null as varbinary) as repayer,
+    cast(null as varbinary) as liquidator,
+    cast(_amount as double) as amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+  from {{ LendingPool_evt_Borrow }}
+  {% if is_incremental() %}
+  where {{ incremental_predicate('evt_block_time') }}
+  {% endif %}
+  union all
+  select
+    'repay' as transaction_type,
+    null as loan_type,
+    case
+      when _reserve = {{ aave_mock_address }} then {{ native_token_address }} --using native_token_address instead of Aave "mock" address
+      else _reserve
+    end as token_address,
+    _user as borrower,
+    _repayer as repayer,
+    cast(null as varbinary) as liquidator,
+    -1 * cast(_amountMinusFees as double) as amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+  from {{ LendingPool_evt_Repay }}
+  {% if is_incremental() %}
+  where {{ incremental_predicate('evt_block_time') }}
+  {% endif %}
+  union all
+  select
+    'borrow_liquidation' as transaction_type,
+    null as loan_type,
+    case
+      when _reserve = {{ aave_mock_address }} then {{ native_token_address }} --using native_token_address instead of Aave "mock" address
+      else _reserve
+    end as token_address,
+    _user as borrower,
+    _liquidator as repayer,
+    _liquidator as liquidator,
+    -1 * cast(_purchaseAmount as double) as amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+  from {{ LendingPool_evt_LiquidationCall }}
+  {% if is_incremental() %}
+  where {{ incremental_predicate('evt_block_time') }}
+  {% endif %}
+)
+
+select
+  '{{ blockchain }}' as blockchain,
+  '{{ project }}' as project,
+  '{{ version }}' as version,
+  transaction_type,
+  loan_type,
+  token_address,
+  borrower,
+  repayer,
+  liquidator,
+  amount,
+  evt_tx_hash,
+  evt_index,
+  evt_block_time,
+  evt_block_number
+from base_borrow
+
+{% endmacro %}
---
+++ macros/models/_sector/lending/platforms/lending_aave_v3_forked_borrow.sql
@@ -0,0 +1,85 @@
+{%
+  macro lending_aave_v3_forked_borrow(
+    blockchain,
+    project,
+    version,
+    Pool_evt_Borrow,
+    Pool_evt_Repay,
+    Pool_evt_LiquidationCall
+  )
+%}
+
+with base_borrow as (
+  select
+    'borrow' as transaction_type,
+    case 
+      when interestRateMode = 1 then 'stable'
+      when interestRateMode = 2 then 'variable'
+    end as loan_type,
+    reserve as token_address,
+    user as borrower,
+    cast(null as varbinary) as repayer,
+    cast(null as varbinary) as liquidator,
+    cast(amount as double) as amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+  from {{ Pool_evt_Borrow }}
+  {% if is_incremental() %}
+  where {{ incremental_predicate('evt_block_time') }}
+  {% endif %}
+  union all
+  select
+    'repay' as transaction_type,
+    null as loan_type,
+    reserve as token_address,
+    user as borrower,
+    repayer as repayer,
+    cast(null as varbinary) as liquidator,
+    -1 * cast(amount as double) as amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+  from {{ Pool_evt_Repay }}
+  {% if is_incremental() %}
+  where {{ incremental_predicate('evt_block_time') }}
+  {% endif %}
+  union all
+  select
+    'borrow_liquidation' as transaction_type,
+    null as loan_type,
+    debtAsset as token_address,
+    user as borrower,
+    liquidator as repayer,
+    liquidator as liquidator,
+    -1 * cast(debtToCover as double) as amount,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number
+  from {{ Pool_evt_LiquidationCall }}
+  {% if is_incremental() %}
+  where {{ incremental_predicate('evt_block_time') }}
+  {% endif %}
+)
+
+select
+  '{{ blockchain }}' as blockchain,
+  '{{ project }}' as project,
+  '{{ version }}' as version,
+  transaction_type,
+  loan_type,
+  token_address,
+  borrower,
+  repayer,
+  liquidator,
+  amount,
+  evt_tx_hash,
+  evt_index,
+  evt_block_time,
+  evt_block_number
+from base_borrow
+
+{% endmacro %}
