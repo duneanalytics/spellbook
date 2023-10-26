191987297d5ef20cfec03c42c915941380678816
0xRob <0xRobin5@gmail.com>
2023-10-26 12:57:24 +0200

    try incremental idk

--- models/_sector/nft/trades/nft_trades_beta.sql
+++ models/_sector/nft/trades/nft_trades_beta.sql
@@ -3,7 +3,7 @@
     tags = ['dunesql'],
     alias = 'trades_beta',
     partition_by = ['blockchain','project','block_month'],
-    materialized = 'view',
+    materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
     unique_key = ['project','project_version','tx_hash','sub_tx_trade_id']
