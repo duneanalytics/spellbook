8ffce8d56444dca5229a48cbb553dadafe0b8c36
Markus Buhatem Koch <markusbkoch@gmail.com>
2023-10-27 11:26:39 +0100

    fix uniswap trades

--- models/uniswap/ethereum/uniswap_v2_ethereum_trades.sql
+++ models/uniswap/ethereum/uniswap_v2_ethereum_trades.sql
@@ -21,16 +21,23 @@
 WITH dexs AS
 (
     -- Uniswap v2
+    -- Swap events contain amounts in and out for both tokens 0 and 1, so we actually have two "swaps" in each event:
+    -- * Token 0 in, token 1 out
+    -- * Token 1 in, token 0 out
+    -- Most of the time, one of these swaps has empty/zero amounts.
+    -- However, there are some edge cases where both amounts for one of the tokens is non-zero, and both amounts for the other token are zero. eg 0x2643cc80871c4134704863b17f10901ea6d11646fc10162ff1fb504e7137c193
+    -- and others where both amounts for both tokens are non-zero eg 0x5afc81f74fbfca62c7d083cccbadd01158f65bfdb1f2c7fb24e4866c1d96e346
+    -- We need to handle both of these cases, so we use a UNION ALL to get both swaps from each event, but avoiding cluttering the dataset with empty swaps (0 for 0)
     SELECT
         t.evt_block_time AS block_time
         ,t.to AS taker
         ,CAST(NULL as VARBINARY) as maker
-        ,CASE WHEN amount0Out = UINT256 '0' THEN amount1Out ELSE amount0Out END AS token_bought_amount_raw
-        ,CASE WHEN amount0In = UINT256 '0' OR amount1Out = UINT256 '0' THEN amount1In ELSE amount0In END AS token_sold_amount_raw
+        ,amount0Out AS token_bought_amount_raw
+        ,amount1In AS token_sold_amount_raw
         ,NULL AS amount_usd
-        ,CASE WHEN amount0Out = UINT256 '0' THEN f.token1 ELSE f.token0 END AS token_bought_address
-        ,CASE WHEN amount0In = UINT256 '0' OR amount1Out = UINT256 '0' THEN f.token1 ELSE f.token0 END AS token_sold_address
-        ,t.contract_address as project_contract_address
+        ,f.token0 AS token_bought_address
+        ,f.token1 AS token_sold_address
+        ,t.contract_address AS project_contract_address
         ,t.evt_tx_hash AS tx_hash
 
         ,t.evt_index
@@ -42,6 +49,35 @@ WITH dexs AS
         {{weth_ubomb_wash_trading_pair}},
         {{weth_weth_wash_trading_pair}},
         {{feg_eth_wash_trading_pair}})
+    AND (amount0Out > 0 OR amount1In > 0)
+    {% if is_incremental() %}
+    AND t.evt_block_time >= date_trunc("day", now() - interval '1 week')
+    {% endif %}
+
+    UNION ALL
+
+    SELECT
+        t.evt_block_time AS block_time
+        ,t.to AS taker
+        ,CAST(NULL as VARBINARY) as maker
+        ,amount1Out AS token_bought_amount_raw
+        ,amount0In AS token_sold_amount_raw
+        ,NULL AS amount_usd
+        ,f.token1 AS token_bought_address
+        ,f.token0 AS token_sold_address
+        ,t.contract_address AS project_contract_address
+        ,t.evt_tx_hash AS tx_hash
+        ,'' AS trace_address
+        ,t.evt_index
+    FROM
+        {{ source('uniswap_v2_ethereum', 'Pair_evt_Swap') }} t
+    INNER JOIN {{ source('uniswap_v2_ethereum', 'Factory_evt_PairCreated') }} f
+        ON f.pair = t.contract_address
+    WHERE t.contract_address NOT IN (
+        {{weth_ubomb_wash_trading_pair}},
+        {{weth_weth_wash_trading_pair}},
+        {{feg_eth_wash_trading_pair}})
+    AND (amount1Out > 0 OR amount0In > 0)
     {% if is_incremental() %}
     AND t.evt_block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
