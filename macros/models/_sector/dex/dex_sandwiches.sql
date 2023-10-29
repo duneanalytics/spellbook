22d8de3d44c5a399d2315e506f9d11dc1406743b
Hildebert Moulié <h.moulie@gmail.com>
2023-10-29 23:29:46 +0100

    rework sandwich trades to increase accuracy

--- macros/models/_sector/dex/dex_sandwiches.sql
+++ macros/models/_sector/dex/dex_sandwiches.sql
@@ -1,56 +1,75 @@
 {% macro dex_sandwiches(blockchain, transactions) %}
 
-SELECT distinct s1.blockchain
-, s1.project
-, s1.version
-, s1.block_time
-, CAST(date_trunc('month', s1.block_time) AS date) AS block_month
-, tx1.block_number
-, s1.token_sold_address
-, s1.token_bought_address
-, s1.token_sold_symbol
-, s1.token_bought_symbol
-, s1.maker
-, s1.taker
-, s1.tx_hash
-, s1.tx_from
-, s1.tx_to
-, s1.project_contract_address
-, s1.token_pair
-, tx1.index
-, s1.token_sold_amount_raw
-, s1.token_bought_amount_raw
-, s1.token_sold_amount
-, s1.token_bought_amount
-, s1.amount_usd
-, s1.evt_index
---, CASE WHEN s1.tx_from=s2.tx_from THEN 'tx_from' ELSE 'taker' END AS commonality
---, CASE WHEN s1.token_bought_address=s2.token_sold_address THEN 'token_sold' ELSE 'token_bought' END AS sandwiched_token
---, CASE WHEN s1.token_bought_address<s1.token_sold_address THEN 0 ELSE 1 END AS token_order
-FROM {{ ref('dex_trades') }} s1
-INNER JOIN {{ ref('dex_trades') }} s2 ON s1.blockchain='{{blockchain}}'
-    AND s2.blockchain='{{blockchain}}'
-    AND s1.block_time=s2.block_time
-    AND s1.project=s2.project
-    AND s1.version=s2.version
-    AND s1.tx_hash!=s2.tx_hash
-    AND s1.project_contract_address=s2.project_contract_address
+WITH indexed_trades AS (
+    SELECT dt.block_time
+    , tx.block_number
+    , dt.project_contract_address
+    , dt.tx_from
+    , tx.index AS tx_index
+    , dt.token_sold_address
+    , dt.token_bought_address
+    , dt.tx_hash
+    , dt.evt_index
+    FROM {{ ref('dex_trades') }} dt
+    INNER JOIN {{transactions}} tx ON tx.block_time=dt.block_time
+        AND tx.hash=dt.tx_hash
+        {% if is_incremental() %}
+        AND tx.block_time >= date_trunc('day', now() - interval '7' day)
+        {% endif %}
+    WHERE dt.blockchain='ethereum'
     {% if is_incremental() %}
-    AND s1.block_time >= date_trunc('day', now() - interval '7' day)
-    AND s2.block_time >= date_trunc('day', now() - interval '7' day)
+    AND dt.block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
-INNER JOIN {{transactions}} tx1 ON tx1.block_time=s1.block_time
-    AND tx1.hash=s1.tx_hash
-    {% if is_incremental() %}
-    AND tx1.block_time >= date_trunc('day', now() - interval '7' day)
-    {% endif %}
-INNER JOIN {{transactions}} tx2 ON tx2.block_time=s2.block_time
-    AND tx2.hash=s2.tx_hash
-    AND (s1.tx_from=s2.tx_from OR s1.taker=s2.taker)
-    AND ((tx1.index>tx2.index AND s1.token_bought_address=s2.token_sold_address)
-        OR (tx1.index<tx2.index AND s1.token_sold_address=s2.token_bought_address))
-    {% if is_incremental() %}
-    AND tx2.block_time >= date_trunc('day', now() - interval '7' day)
-    {% endif %}
+    )
+
+, sandwiches AS (
+    SELECT DISTINCT s1.block_time
+    , s1.block_number
+    , s1.project_contract_address
+    , s1.tx_index
+    , tx_hash_all AS tx_hash
+    , evt_index_all AS evt_index
+    FROM indexed_trades s1
+    INNER JOIN indexed_trades s2 ON s1.block_number=s2.block_number
+        AND s1.project_contract_address=s2.project_contract_address
+        AND s1.tx_from=s2.tx_from
+        AND s1.tx_index<s2.tx_index-1
+        AND s1.token_sold_address=s2.token_bought_address
+        AND s1.token_bought_address=s2.token_sold_address
+    INNER JOIN indexed_trades victim ON s1.block_number=victim.block_number
+        AND s1.project_contract_address=victim.project_contract_address
+        AND victim.tx_index BETWEEN s1.tx_index AND s2.tx_index
+        AND victim.tx_from!=s1.tx_from
+        AND s1.token_bought_address=victim.token_bought_address
+        AND s1.token_sold_address=victim.token_sold_address
+    CROSS JOIN UNNEST(ARRAY[(s1.tx_hash, s1.evt_index), (s2.tx_hash, s2.evt_index)]) AS t(tx_hash_all, evt_index_all)
+    )
+
+SELECT dt.blockchain
+, dt.project
+, dt.version
+, block_time
+, CAST(date_trunc('month', block_time) AS date) AS block_month
+, s.block_number
+, dt.token_sold_address
+, dt.token_bought_address
+, dt.token_sold_symbol
+, dt.token_bought_symbol
+, dt.maker
+, dt.taker
+, tx_hash
+, dt.tx_from
+, dt.tx_to
+, project_contract_address
+, dt.token_pair
+, s.tx_index
+, dt.token_sold_amount_raw
+, dt.token_bought_amount_raw
+, dt.token_sold_amount
+, dt.token_bought_amount
+, dt.amount_usd
+, evt_index
+FROM {{ ref('dex_trades') }} dt
+INNER JOIN sandwiches s USING (block_time, tx_hash, project_contract_address, evt_index)
 
 {% endmacro %}
