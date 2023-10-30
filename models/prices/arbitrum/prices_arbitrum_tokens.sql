63bbb46e66d6eff9794826de7ea9d38bda504075
Huang Geyang <Sukebeta@outlook.com>
2023-10-30 19:45:09 +0800

    Update models/prices/arbitrum/prices_arbitrum_tokens.sql

--- models/prices/arbitrum/prices_arbitrum_tokens.sql
+++ models/prices/arbitrum/prices_arbitrum_tokens.sql
@@ -126,6 +126,5 @@ FROM
     ('ush-unsheth','arbitrum','USH',0x51a80238b5738725128d3a3e06ab41c1d4c05c74,18),
     ('hdn-hydranet','arbitrum','HDN',0x3404149e9ee6f17fb41db1ce593ee48fbdcd9506,18),
     ('vela-vela-token','arbitrum','VELA',0x088cd8f5ef3652623c22d48b1605dcfe860cd704,18),
-    ('choke-artichoke','arbitrum','CHOKE',0x6fc2680d8ad8e8312191441b4eca9eff8d06b45a,18),
-    ('acid-0xacid','arbitrum','ACID',0x29c1ea5ed7af53094b1a79ef60d20641987c867e,18)
+    ('choke-artichoke','arbitrum','CHOKE',0x6fc2680d8ad8e8312191441b4eca9eff8d06b45a,18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
