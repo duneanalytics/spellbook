e9164fd7117ab8eb3fcb7b9d771051a5b7d78d43
Hildebert Moulié <h.moulie@gmail.com>
2023-10-31 15:11:07 +0100

    readd tx_hash

--- macros/models/_sector/dex/dex_sandwiches.sql
+++ macros/models/_sector/dex/dex_sandwiches.sql
@@ -3,6 +3,7 @@
 -- Checking that each frontrun trade has a matching backrun and at least one victim in between
 WITH indexed_sandwich_trades AS (
     SELECT DISTINCT front.block_time
+    , t.tx_hash_all AS tx_hash
     , front.project_contract_address
     , t.evt_index_all AS evt_index
     FROM {{ ref('dex_trades') }} front
@@ -20,14 +21,14 @@ WITH indexed_sandwich_trades AS (
     INNER JOIN {{ ref('dex_trades') }} victim ON victim.blockchain='{{blockchain}}'
         AND front.block_time=victim.block_time
         AND front.project_contract_address=victim.project_contract_address
-        AND victim.tx_from!=front.tx_from
+        AND front.tx_from!=victim.tx_from
         AND front.token_bought_address=victim.token_bought_address
         AND front.token_sold_address=victim.token_sold_address
         AND victim.evt_index BETWEEN front.evt_index AND back.evt_index
         {% if is_incremental() %}
         AND victim.block_time >= date_trunc('day', now() - interval '7' day)
         {% endif %}
-    CROSS JOIN UNNEST(ARRAY[front.evt_index, back.evt_index]) AS t(evt_index_all)
+    CROSS JOIN UNNEST(ARRAY[(front.tx_hash, front.evt_index), (back.tx_hash, back.evt_index)]) AS t(tx_hash_all, evt_index_all)
     WHERE front.blockchain='{{blockchain}}'
     {% if is_incremental() %}
     AND front.block_time >= date_trunc('day', now() - interval '7' day)
@@ -61,10 +62,11 @@ SELECT dt.blockchain
 , dt.evt_index
 FROM {{ ref('dex_trades') }} dt
 INNER JOIN indexed_sandwich_trades s ON dt.block_time=s.block_time
+    AND dt.tx_hash=s.tx_hash
     AND dt.project_contract_address=s.project_contract_address
     AND dt.evt_index=s.evt_index
-INNER JOIN {{transactions}} tx ON tx.block_time=dt.block_time
-    AND tx.hash=dt.tx_hash
+INNER JOIN {{transactions}} tx ON tx.block_time=s.block_time
+    AND tx.hash=s.tx_hash
     {% if is_incremental() %}
     AND tx.block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
