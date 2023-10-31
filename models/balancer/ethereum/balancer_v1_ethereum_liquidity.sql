481c198d8349275085ddfa56d6cdf84751766c00
viniabussafi <vinicius@balancerlabs.dev>
2023-10-31 19:40:01 +0100

    same

--- models/balancer/ethereum/balancer_v1_ethereum_liquidity.sql
+++ models/balancer/ethereum/balancer_v1_ethereum_liquidity.sql
@@ -121,7 +121,7 @@ prices AS (
             liquidity * normalized_weight AS pool_liquidity_usd
         FROM pool_liquidity_estimates b
         LEFT JOIN cumulative_usd_balance c ON c.day = b.day
-        AND c.pool_id = b.pool_id
+        AND c.pool = b.pool
         LEFT JOIN {{ ref('balancer_v1_ethereum_pools_tokens_weights') }} w ON b.pool = w.pool_id
         AND CAST (w.normalized_weight as DOUBLE) > CAST (0 as DOUBLE)
         LEFT JOIN {{ ref('tokens_ethereum_erc20') }} t ON t.contract_address = w.token_address
