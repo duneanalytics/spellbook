60b49cdce4c1f7688039bd07ed5e7ab9f7078a09
RantumBits <rantum@rantum.xyz>
2023-11-01 13:34:50 -0700

    fix COMP on poly

--- models/prices/polygon/prices_polygon_tokens.sql
+++ models/prices/polygon/prices_polygon_tokens.sql
@@ -83,5 +83,5 @@ FROM
     ('woo-wootrade','polygon','WOO',0x1b815d120b3ef02039ee11dc2d33de7aa4a8c603,18),
     ('route-router-protocol','polygon','ROUTE',0x16eccfdbb4ee1a85a33f3a9b21175cd7ae753db4,18),
     ('pla-playdapp','polygon','PLA',0x8765f05adce126d70bcdf1b0a48db573316662eb,18),
-    ('comp-compoundd', 'ethereum', 'COMP', 0xc00e94cb662c3520282e6f5717214004a7f26888,18)
+    ('comp-compoundd', 'polygon', 'COMP', 0xc00e94cb662c3520282e6f5717214004a7f26888,18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
