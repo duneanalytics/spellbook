fb53897994a239197a145fe0a0f6e2d723c1fefb
fmarrr <maflora89@gmail.com>
2023-11-01 22:33:53 +0000

    add gyroscope polygon and ethereum E-CLPs trades

--- models/gyroscope/ethereum/gyroscope_ethereum_trades.sql
+++ models/gyroscope/ethereum/gyroscope_ethereum_trades.sql
@@ -1,5 +1,5 @@
 {{ config(
-    schema = 'gyroscope_polygon',
+    schema = 'gyroscope_ethereum',
     tags = ['dunesql'],
     alias = alias('trades'),
     partition_by = ['block_month'],
@@ -7,14 +7,14 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index'],
-    post_hook = '{{ expose_spells(\'["polygon"]\',
+    post_hook = '{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "balancer_v2",
                                 \'["fmarrr"]\') }}'
     )
 }}
 
-{% set project_start_date = '2021-06-24' %}
+{% set project_start_date = '2021-04-20' %}
 
 WITH
     swap_fees AS (
@@ -26,8 +26,8 @@ WITH
             bytearray_substring(swaps.poolId, 1, 20) AS contract_address,
             fees.swap_fee_percentage,
             ROW_NUMBER() OVER (PARTITION BY poolId, evt_tx_hash, evt_index ORDER BY block_number DESC, index DESC) AS rn
-        FROM {{ source('balancer_v2_polygon', 'Vault_evt_Swap') }} swaps
-        LEFT JOIN {{ ref('balancer_v2_polygon_pools_fees') }} fees
+        FROM {{ source('balancer_v2_ethereum', 'Vault_evt_Swap') }} swaps
+        LEFT JOIN {{ ref('balancer_v2_ethereum_pools_fees') }} fees
             ON fees.contract_address = bytearray_substring(swaps.poolId, 1, 20)
             AND ARRAY[fees.block_number] || ARRAY[fees.index] < ARRAY[swaps.evt_block_number] || ARRAY[swaps.evt_index]
         {% if is_incremental() %}
@@ -52,7 +52,7 @@ WITH
             swap.evt_index
         FROM
             swap_fees
-            INNER JOIN {{ source('balancer_v2_polygon', 'Vault_evt_Swap') }} swap
+            INNER JOIN {{ source('balancer_v2_ethereum', 'Vault_evt_Swap') }} swap
                 ON swap.evt_block_number = swap_fees.evt_block_number
                 AND swap.evt_tx_hash = swap_fees.evt_tx_hash
                 AND swap.evt_index = swap_fees.evt_index
@@ -71,7 +71,7 @@ WITH
             MAX(bpt_prices.hour) AS bpa_max_block_time
         FROM
             dexs
-            LEFT JOIN {{ ref('balancer_v2_polygon_bpt_prices') }} bpt_prices
+            LEFT JOIN {{ ref('balancer_v2_ethereum_bpt_prices') }} bpt_prices
                 ON bpt_prices.contract_address = CAST(dexs.token_bought_address AS VARCHAR)
                 AND bpt_prices.hour <= dexs.block_time
                 {% if not is_incremental() %}
@@ -92,7 +92,7 @@ WITH
             MAX(bpt_prices.hour) AS bpb_max_block_time
         FROM
             dexs
-            LEFT JOIN {{ ref('balancer_v2_polygon_bpt_prices') }} bpt_prices
+            LEFT JOIN {{ ref('balancer_v2_ethereum_bpt_prices') }} bpt_prices
                 ON bpt_prices.contract_address = CAST(dexs.token_sold_address AS VARCHAR)
                 AND bpt_prices.hour <= dexs.block_time
                 {% if not is_incremental() %}
@@ -104,15 +104,15 @@ WITH
         GROUP BY 1, 2, 3, 4, 5
     ),
 
-    E_CLPs AS (
+E_CLPs AS (
     SELECT
        pool
-    from {{ source('gyroscope_polygon','GyroECLPPoolFactory_evt_PoolCreated') }}
-    ),
+    from {{ source('gyroscope_ethereum','GyroECLPPoolFactory_evt_PoolCreated') }}
+),
 
-    filtered_trades as (
-        SELECT
-    'polygon' AS blockchain,
+filtered_trades as (
+SELECT
+    'ethereum' AS blockchain,
     'balancer' AS project,
     '2' AS version,
     TRY_CAST(DATE_TRUNC('DAY', dexs.block_time) AS date) AS block_date,
@@ -149,7 +149,7 @@ WITH
     dexs.evt_index
 FROM
     dexs
-    INNER JOIN {{ source('polygon', 'transactions') }} tx
+    INNER JOIN {{ source('ethereum', 'transactions') }} tx
         ON tx.hash = dexs.tx_hash
         {% if not is_incremental() %}
         AND tx.block_time >= TIMESTAMP '{{project_start_date}}'
@@ -159,14 +159,14 @@ FROM
         {% endif %}
     LEFT JOIN {{ ref('tokens_erc20') }} erc20a
         ON erc20a.contract_address = dexs.token_bought_address
-        AND erc20a.blockchain = 'polygon'
+        AND erc20a.blockchain = 'ethereum'
     LEFT JOIN {{ ref('tokens_erc20') }} erc20b
         ON erc20b.contract_address = dexs.token_sold_address
-        AND erc20b.blockchain = 'polygon'
+        AND erc20b.blockchain = 'ethereum'
     LEFT JOIN {{ source('prices', 'usd') }} p_bought
         ON p_bought.minute = DATE_TRUNC('minute', dexs.block_time)
         AND p_bought.contract_address = dexs.token_bought_address
-        AND p_bought.blockchain = 'polygon'
+        AND p_bought.blockchain = 'ethereum'
         {% if not is_incremental() %}
         AND p_bought.minute >= TIMESTAMP '{{project_start_date}}'
         {% endif %}
@@ -176,7 +176,7 @@ FROM
     LEFT JOIN {{ source('prices', 'usd') }} p_sold
         ON p_sold.minute = DATE_TRUNC('minute', dexs.block_time)
         AND p_sold.contract_address = dexs.token_sold_address
-        AND p_sold.blockchain = 'polygon'
+        AND p_sold.blockchain = 'ethereum'
         {% if not is_incremental() %}
         AND p_sold.minute >= TIMESTAMP '{{project_start_date}}'
         {% endif %}
@@ -187,7 +187,7 @@ FROM
         ON bpa.evt_block_number = dexs.evt_block_number
         AND bpa.tx_hash = dexs.tx_hash
         AND bpa.evt_index = dexs.evt_index
-    LEFT JOIN {{ ref('balancer_v2_polygon_bpt_prices') }} bpa_bpt_prices
+    LEFT JOIN {{ ref('balancer_v2_ethereum_bpt_prices') }} bpa_bpt_prices
         ON bpa_bpt_prices.contract_address = bpa.contract_address
         AND bpa_bpt_prices.hour = bpa.bpa_max_block_time
         {% if not is_incremental() %}
@@ -200,7 +200,7 @@ FROM
         ON bpb.evt_block_number = dexs.evt_block_number
         AND bpb.tx_hash = dexs.tx_hash
         AND bpb.evt_index = dexs.evt_index
-    LEFT JOIN {{ ref('balancer_v2_polygon_bpt_prices') }} bpb_bpt_prices
+    LEFT JOIN {{ ref('balancer_v2_ethereum_bpt_prices') }} bpb_bpt_prices
         ON bpb_bpt_prices.contract_address = bpb.contract_address
         AND bpb_bpt_prices.hour = bpb.bpb_max_block_time
         {% if not is_incremental() %}
@@ -209,16 +209,16 @@ FROM
         {% if is_incremental() %}
         AND bpa_bpt_prices.hour >= DATE_TRUNC('day', NOW() - interval '7' day)
         {% endif %}
-    where project_contract_address IN (SELECT pool FROM E_CLPs)
-    ),
+    where dexs.project_contract_address IN (SELECT pool FROM E_CLPs)
+),
 
-    new_table_for_dexTrades AS (
+new_table_for_dexTrades AS (
     SELECT *,
         CASE 
             WHEN project_contract_address IN (SELECT address FROM E_CLPs) THEN 'gyroscope' 
             ELSE project 
         END AS project
     FROM filtered_trades
-    )
+)
 
 SELECT * FROM new_table_for_dexTrades
