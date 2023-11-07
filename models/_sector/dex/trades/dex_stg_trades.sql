3778344c4b38acfeca2f3abefd9e82732835c4de
jeff-dude <jeff@dune.xyz>
2023-11-07 18:40:28 -0500

    try dex_stg_trades as a view

--- models/_sector/dex/trades/dex_stg_trades.sql
+++ models/_sector/dex/trades/dex_stg_trades.sql
@@ -1,11 +1,7 @@
 {{ config(
     schema = 'dex'
     , alias = 'stg_trades'
-    , partition_by = ['block_month', 'blockchain', 'project']
-    , materialized = 'incremental'
-    , file_format = 'delta'
-    , incremental_strategy = 'merge'
-    , unique_key = ['blockchain', 'project', 'version', 'tx_hash', 'evt_index']
+    , materialized = 'view'
     )
 }}
 
@@ -44,20 +40,13 @@ with base_union as (
             , evt_index
             , tx_from
             , tx_to
-            , row_number() over (partition by tx_hash, evt_index order by tx_hash) as duplicates_rank
         FROM
             {{ model }}
-        {% if is_incremental() %}
-        WHERE
-            {{ incremental_predicate('block_time') }}
-        {% endif %}
         {% if not loop.last %}
         UNION ALL
         {% endif %}
         {% endfor %}
     )
-    WHERE
-        duplicates_rank = 1
 )
 select
     *
