b359ce94ab05a19aae9a77e2d76ea1fcce96ebc8
alicecha <110170331+alicecha@users.noreply.github.com>
2023-11-09 16:56:19 +0000

    Replaced data source, now fetching directly from ProxyCreation event table to overcome issues with zksync traces data. Updated singleton addresses.

--- models/safe/zksync/safe_zksync_safes.sql
+++ models/safe/zksync/safe_zksync_safes.sql
@@ -17,29 +17,18 @@
 
 select
     'zksync' as blockchain,
-    et."from" as address,
+    proxy as address,
     case
-        when et.to = 0xd9db270c1b5e3bd161e8c8503c55ceabee709552 then '1.3.0'
-        when et.to = 0x69f4d1788e39c87893c980c06edf4b7f686e2938 then '1.3.0'  -- for chains with EIP-155
-        when et.to = 0x3e5c63644e683549055b9be8653de26e0b4cd36e then '1.3.0L2'
-        when et.to = 0xfb1bffc9d739b8d520daf37df666da4c687191ea then '1.3.0L2' -- for chains with EIP-155
+        when singleton = 0xB00ce5CCcdEf57e539ddcEd01DF43a13855d9910 then '1.3.0'
+        when singleton = 0x1727c2c531cf966f902E5927b98490fDFb3b2b70 then '1.3.0L2'
         else 'unknown'
     end as creation_version,
-    try_cast(date_trunc('day', et.block_time) as date) as block_date,
-    CAST(date_trunc('month', et.block_time) as DATE) as block_month,
-    et.block_time as creation_time,
-    et.tx_hash
-from {{ source('zksync', 'traces') }} et
-join {{ ref('safe_zksync_singletons') }} s
-    on et.to = s.address
-where et.success = true
-    and et.call_type = 'delegatecall' -- delegatecall to singleton is Safe (proxy) address
-    and bytearray_substring(et.input, 1, 4) in (
-        0x0ec78d9e, -- setup method v0.1.0
-        0xa97ab18a, -- setup method v1.0.0
-        0xb63e800d -- setup method v1.1.0, v1.1.1, v1.2.0, v1.3.0, v1.3.0L2
-    )
-    and et.gas_used > 10000  -- to ensure the setup call was successful. excludes e.g. setup calls with missing params that fallback
+    try_cast(date_trunc('day', evt_block_time) as date) as block_date,
+    CAST(date_trunc('month', evt_block_time) as DATE) as block_month,
+    evt_block_time as creation_time,
+    evt_tx_hash
+from {{ source('gnosis_safe_zksync', 'GnosisSafeProxyFactoryv1_3_0_evt_ProxyCreation') }}
+where 
     {% if not is_incremental() %}
     and et.block_time > TIMESTAMP '2023-09-01' -- for initial query optimisation
     {% endif %}
