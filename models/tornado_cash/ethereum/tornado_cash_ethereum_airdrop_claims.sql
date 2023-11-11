a202f8170228ad1de3492747fa9c6e03c0224ead
Hildebert Moulié <h.moulie@gmail.com>
2023-11-11 14:48:00 +0100

    fix

--- models/tornado_cash/ethereum/tornado_cash_ethereum_airdrop_claims.sql
+++ models/tornado_cash/ethereum/tornado_cash_ethereum_airdrop_claims.sql
@@ -13,30 +13,6 @@
     )
 }}
 
-WITH dex_prices AS
-  (SELECT block_date,
-          avg(amount_usd / token_sold_amount) AS price
-   FROM dex.trades
-   WHERE token_sold_symbol = 'TORN'
-     AND block_date < cast('2021-03-02' AS timestamp)
-   GROUP BY 1)
-SELECT sum(value / 1e18 * CASE
-                              WHEN p.price IS NOT NULL THEN p.price
-                              ELSE dp.price
-                          END)
-FROM erc20_ethereum.evt_Transfer erc
-LEFT JOIN prices.usd p ON date_trunc('minute', evt_block_time) = MINUTE
-AND p.symbol = 'TORN'
-AND blockchain = 'ethereum'
-AND p.minute >= cast('2021-03-02' AS timestamp)
-LEFT JOIN {{ ref('prices_usd_forward_fill') }} dp ON date_trunc('day', evt_block_time) = dp.minute
-WHERE erc.contract_address = 0x77777feddddffc19ff86db637967013e6c6a116c
-  AND "from" = 0x3eFA30704D2b8BBAc821307230376556cF8CC39e
-
-
-
-
-
 {% set torn_token_address = '0x77777feddddffc19ff86db637967013e6c6a116c' %}
 
 WITH early_price AS (
@@ -64,7 +40,7 @@ SELECT 'ethereum' AS blockchain
 , 'TORN' AS token_symbol
 , t.evt_index
 FROM {{ source('erc20_ethereum', 'evt_transfer') }} t
-LEFT JOIN {{source('prices','usd')}} pu ON pu.blockchain = 'ethereum'
+LEFT JOIN {{ ref('prices_usd_forward_fill') }} pu ON pu.blockchain = 'ethereum'
     AND pu.contract_address = {{torn_token_address}}
     AND pu.minute=date_trunc('minute', t.evt_block_time)
 WHERE t.evt_block_time BETWEEN TIMESTAMP '2020-12-18' AND TIMESTAMP '2021-12-13'
