b0ea532d911b2a0bb8436b9fe2b91993a242e5e4
Daniel Öhrlund <danielohrlund@gmail.com>
2023-11-13 22:05:09 +0100

    remove is_referral check from slugs rewards test

--- tests/_sector/referral/slugs_rewards_test.sql
+++ tests/_sector/referral/slugs_rewards_test.sql
@@ -1,7 +1,6 @@
 WITH unit_test AS (
     SELECT
-        test.reward_amount_raw = actual.reward_amount_raw AS reward_amount_test,
-        test.is_referral = actual.is_referral AS is_referral_test
+        test.reward_amount_raw = actual.reward_amount_raw AS reward_amount_test
     FROM {{ ref('slugs_optimism_rewards') }} AS actual
     INNER JOIN {{ ref('slugs_optimism_rewards_seed') }} AS test
     ON actual.tx_hash = test.tx_hash
@@ -13,4 +12,3 @@ FROM
     unit_test
 WHERE
     reward_amount_test = FALSE
-    OR is_referral_test = FALSE
