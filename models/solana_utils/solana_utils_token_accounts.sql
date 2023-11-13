da911e78cc5733dd445cb940bb23e5c0dd1deade
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-11-13 12:02:59 -0600

    make token_accounts incremental (#4779)
    * make incremental
    
    * remove join
    
    * update config

--- models/solana_utils/solana_utils_token_accounts.sql
+++ models/solana_utils/solana_utils_token_accounts.sql
@@ -2,8 +2,11 @@
   config(
         schema = 'solana_utils',
         alias = 'token_accounts',
-        materialized='table',
+        materialized = 'incremental',
         file_format = 'delta',
+        incremental_strategy = 'merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.created_at')],
+        unique_key = ['token_mint_address','address'],
         post_hook='{{ expose_spells(\'["solana"]\',
                                     "sector",
                                     "solana_utils",
@@ -12,14 +15,16 @@
 
 WITH 
       distinct_accounts as (
-            --force
             SELECT
-                  token_mint_address
-                  , address 
-                  , max_by(token_balance_owner, block_time) as token_balance_owner --some account created before and then again after token owner schema change, so then it created dupes.
-                  , min(block_time) as created_at 
-            FROM {{ source('solana','account_activity') }}
-            WHERE token_mint_address is not null
+                  aa.token_mint_address
+                  , aa.address 
+                  , max_by(aa.token_balance_owner, aa.block_time) as token_balance_owner --some account created before and then again after token owner schema change, so then it created dupes.
+                  , min(aa.block_time) as created_at 
+            FROM {{ source('solana','account_activity') }} aa
+            WHERE aa.token_mint_address is not null
+            {% if is_incremental() %}
+            AND {{incremental_predicate('aa.block_time')}}
+            {% endif %}
             group by 1,2
       )
       
