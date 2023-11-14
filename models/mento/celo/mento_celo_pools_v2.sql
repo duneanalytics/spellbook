33ecd97e13a194a48862c4aa985ad2752a899c89
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-11-14 15:28:50 +0000

    closing bracket

--- models/mento/celo/mento_celo_pools_v2.sql
+++ models/mento/celo/mento_celo_pools_v2.sql
@@ -21,7 +21,7 @@ select
   ec.evt_block_number as creation_block_number,
   ec.contract_address
 from {{ source('mento_celo', 'BiPoolManager_evt_ExchangeCreated') }} ec
-  left join {{ source('mento_celo', 'BiPoolManager_evt_ExchangeDestroyed' }} ed on ec.exchangeId = ed.exchangeId
+  left join {{ source('mento_celo', 'BiPoolManager_evt_ExchangeDestroyed') }} ed on ec.exchangeId = ed.exchangeId
 where ed.exchangeId is null
 {% if is_incremental() %}
   and {{ incremental_predicate('ec.evt_block_time') }}
