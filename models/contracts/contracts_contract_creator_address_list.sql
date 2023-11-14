6158643a776fe152aecb1b3efbc2da5b7b888b1e
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-11-14 11:27:12 -0500

    add cp

--- models/contracts/contracts_contract_creator_address_list.sql
+++ models/contracts/contracts_contract_creator_address_list.sql
@@ -699,6 +699,8 @@ WITH curated_list AS (
       ,(0x3c6D5c150EE29B698c6C821B53886C41d239669c, 'Uniswap')
       ,(0x5ee2b0fd8d964cb50e787db4ff176d7bbb0fd180, 'Layer Zero')
       ,(0x18dc1fccea2f4f6dd22ddbf0bed638bc358db625, 'Uniswap')
+      ,(0x144fe9807c92c64acc4bb8e3b8917723efc0d557, 'Clearpool')
+      ,(0x3bEAAb5E79e2588D575cfBa48a74d59A9E94C4Fa, 'Clearpool')
 
       
   ) as temp_table (creator_address, contract_project)
