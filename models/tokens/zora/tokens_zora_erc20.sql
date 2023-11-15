1e3e2c0fad8ba1d32acc4c0840178bb30a01907d
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-11-15 09:59:52 -0500

    comma

--- models/tokens/zora/tokens_zora_erc20.sql
+++ models/tokens/zora/tokens_zora_erc20.sql
@@ -12,5 +12,5 @@
 
 SELECT contract_address, symbol, decimals
 FROM (VALUES
-        (0x4200000000000000000000000000000000000006, 'WETH', 18),
+        (0x4200000000000000000000000000000000000006, 'WETH', 18)
      ) AS temp_table (contract_address, symbol, decimals)
