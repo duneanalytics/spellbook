e72591bce5674dee335c7a596bb4c65515891139
Alan Ghobadi <aalan3@gmail.com>
2023-11-15 14:33:34 +0100

    Token balances ethereum (#4640)
    * Add base macro for balances
    
    * Add example implementation for ethereum
    
    * Add enrichment macro
    
    * Add enrich model
    
    * Use correct arg name
    
    * Test: remove enforcing join distribution
    
    * Drop alias
    
    * Aggregate
    
    * Rework balances base to select based on token_standard
    
    * Create intermediary tables
    
    * Build base table from intermediary tables
    
    * Revert "Aggregate"
    
    This reverts commit 8a6d0f16a88c760c3fe6556b2e19881ab71ed25f.
    
    * Drop session property
    
    * Add incremental predicate
    
    * Filter out null values
    
    * cast to double and add comment
    
    * Make incremental
    
    * Fix type
    
    * Fix typo
    
    * Remove unnecessary columns
    
    * Correctly join existing balances
    
    * Add incremental predicates
    
    * Coalesce existing balance
    
    * Change base to be prefix
    
    * Fix typo in schemas
    
    * Add base table schemas
    
    * Use correct enrich columns
    
    * Add schema for enriched balances
    
    * Fix typos
    
    * Reference correct model
    
    * add schema, remove tags, unique key test
    
    * reorg file structure, rename base_transfers
    
    ---------
    
    Co-authored-by: Belén Pruvost <belen@dune.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>

---
+++ models/_sector/tokens/ethereum/tokens_ethereum_base_balances_native.sql
@@ -0,0 +1,22 @@
+{{ config(
+        schema = 'tokens_ethereum',
+        alias = 'base_balances_native',
+        partition_by = ['block_date'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        unique_key = ['block_number', 'tx_index', 'token_address', 'wallet_address'],
+        )
+}}
+
+select *
+from (
+    {{
+        balances_base(
+            blockchain='ethereum',
+            transfers_base = ref('tokens_ethereum_base_transfers'),
+            token_standard = 'native'
+        )
+    }}
+)
