66d24dc5625480692687782c527a7a7b18277df1
Landon Gingerich <landon.ging@gmail.com>
2023-11-15 18:59:37 +0000

    ERC-20 deposits

---
+++ models/bridge/zksync/zksync_standard_bridge_flows.sql
@@ -0,0 +1,105 @@
+{{ config(
+    
+    alias = 'standard_bridge_flows',
+    partition_by = ['block_month'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_date', 'blockchain', 'tx_hash', 'evt_index'],
+    post_hook='{{ expose_spells(\'["zksync"]\',
+                                "sector",
+                                "bridge",
+                                \'["lgingerich"]\') }}'
+    )
+}}
+
+WITH bridge_events AS (
+    SELECT
+         block_time
+        ,CAST(DATE_TRUNC('day',block_time) as date) AS block_date
+        ,CAST(DATE_TRUNC('month',block_time) as date) AS block_month
+        ,block_number
+        ,tx_hash
+        ,sender
+        ,receiver
+        ,bridged_token_address
+        ,bridged_token_amount_raw
+        ,source_chain_id
+        ,destination_chain_id
+    FROM (
+        -- Deposit ERC-20
+        SELECT
+             l.block_time
+            ,l.block_number
+            ,l.tx_hash
+            ,bytearray_substring(l.topic1, 13, 20) as sender
+            ,bytearray_substring(l.topic2, 13, 20) as receiver
+            ,bytearray_substring(l.topic3, 13, 20) as bridged_token_address
+            ,bytearray_to_uint256(l.data) as bridged_token_amount_raw
+            ,UINT256 '1' AS source_chain_id
+            ,UINT256 '324' AS destination_chain_id
+        FROM {{ source ('zksync', 'logs') }} l
+        WHERE topic0 = 0xb84fba9af218da60d299dc177abd5805e7ac541d2673cbee7808c10017874f63 
+        AND (topic1 IS NOT NULL) AND (topic2 IS NOT NULL) AND (topic3 IS NOT NULL) AND (data IS NOT NULL)
+        {% if is_incremental() %}
+        AND block_time > NOW() - interval '14' Day
+        {% endif %}       
+        ) a
+)
+
+SELECT
+    'zksync' as blockchain
+    ,'standard_bridge' AS project
+    ,'' as version
+    ,tf.block_time
+    ,tf.block_date
+    ,tf.block_month
+    ,tf.block_number
+    ,tx_hash
+    ,COALESCE(sender,CAST(NULL as VARBINARY)) as sender
+    ,COALESCE(receiver, CAST(NULL as VARBINARY)) as receiver
+    ,erc.symbol AS token_symbol
+    ,CAST(bridged_token_amount_raw as double)/ POWER(10,erc.decimals) AS token_amount
+    ,p.price*( CAST(bridged_token_amount_raw as double) / POWER(10,erc.decimals) ) AS token_amount_usd
+    ,bridged_token_amount_raw as token_amount_raw
+    ,0 AS fee_amount
+    ,0 AS fee_amount_usd
+    ,0 AS fee_amount_raw
+    ,bridged_token_address as token_address
+    ,CAST(NULL as VARBINARY) AS fee_address
+    ,source_chain_id
+    ,destination_chain_id
+    ,cid_source.chain_name AS source_chain_name
+    ,cid_dest.chain_name AS destination_chain_name
+    ,1 AS is_native_bridge
+    ,t."from" AS tx_from
+    ,t.to AS tx_to
+    ,tf.transfer_id
+    ,tf.evt_index
+    ,tf.trace_address
+    ,bytearray_substring(t.data,1,4) AS tx_method_id
+FROM bridge_events tf
+
+LEFT JOIN {{ source('zksync', 'transactions') }} t
+        ON t.block_time = tf.block_time
+        AND t.hash = tf.tx_hash
+        {% if is_incremental() %}
+        AND t.block_time >= (NOW() - interval '14' Day)
+        {% endif %}
+        
+LEFT JOIN {{ ref('tokens_erc20') }} erc
+    ON erc.blockchain = 'zksync'
+    AND erc.contract_address = tf.bridged_token_address
+    
+LEFT JOIN {{ source('prices', 'usd') }} p
+    ON p.minute = DATE_TRUNC('minute',tf.block_time)
+    AND p.blockchain = 'zksync'
+    AND p.contract_address = tf.bridged_token_address
+    {% if is_incremental() %}
+    AND p.minute >= (NOW() - interval '14' Day)
+    {% endif %}
+    
+LEFT JOIN {{ ref('chain_info_chain_ids') }} cid_source
+    ON cid_source.chain_id =  tf.source_chain_id
+LEFT JOIN {{ ref('chain_info_chain_ids') }} cid_dest
+    ON cid_dest.chain_id = tf.destination_chain_id
