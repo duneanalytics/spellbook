4d8dd4f6ae343879ae05a051ed46b02492133889
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-11-16 12:11:04 -0500

    rerun

--- macros/models/_sector/contracts/contracts_base_iterated_creators.sql
+++ macros/models/_sector/contracts/contracts_base_iterated_creators.sql
@@ -2,7 +2,7 @@
 
 
 {% set column_list = [
-    'blockchain', 'trace_creator_address', 'creator_address', 'deployer_address'
+     'blockchain', 'trace_creator_address', 'creator_address', 'deployer_address'
     ,'contract_address', 'created_time', 'created_month', 'created_block_number', 'created_tx_hash'
     ,'top_level_time', 'top_level_block_number', 'top_level_tx_hash', 'top_level_tx_from', 'top_level_tx_to', 'top_level_tx_method_id'
     ,'created_tx_from', 'created_tx_to', 'created_tx_method_id', 'created_tx_index'
