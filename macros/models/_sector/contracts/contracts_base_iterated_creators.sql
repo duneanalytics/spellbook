c2b6f25a44957c12f0c32d8deb8bd28721e35e59
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-11-15 19:34:26 -0500

    group

--- macros/models/_sector/contracts/contracts_base_iterated_creators.sql
+++ macros/models/_sector/contracts/contracts_base_iterated_creators.sql
@@ -195,4 +195,6 @@ SELECT * FROM level{{max_levels - 1}}
             group by 1
           ) ts 
   ON u.contract_address = ts.contract_address
+
+  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
 {% endmacro %}
