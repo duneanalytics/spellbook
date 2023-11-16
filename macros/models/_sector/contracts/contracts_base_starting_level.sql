5a3b9de3b84fdcbfa5ea53d37ce38b92a34f3fa1
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-11-15 23:47:29 -0500

    op allowlist bug

--- macros/models/_sector/contracts/contracts_base_starting_level.sql
+++ macros/models/_sector/contracts/contracts_base_starting_level.sql
@@ -1,7 +1,24 @@
 {% macro contracts_base_starting_level( chain ) %}
 
   select 
-    *
+    blockchain
+    , trace_creator_address
+    , contract_address
+    , created_time
+    , created_block_number
+    , created_tx_hash
+    , top_level_time
+    , top_level_block_number
+    , top_level_tx_hash
+    , top_level_tx_from
+    , top_level_tx_to
+    , top_level_tx_method_id
+    , created_tx_from
+    , created_tx_to
+    , created_tx_method_id
+    , created_tx_index
+    , code
+    , code_bytelength
     ,cast( DATE_TRUNC('month',created_time) as date) AS created_month
   from (
 
@@ -15,15 +32,17 @@
             ,t.block_time as top_level_time
             ,t.block_number as top_level_block_number
             ,t.hash as top_level_tx_hash
-            ,t."from" AS top_level_tx_from
-            ,t.to AS top_level_tx_to
-            ,bytearray_substring(t.data,1,4) AS top_level_tx_method_id
-            ,t."from" AS created_tx_from
-            ,t.to AS created_tx_to
-            ,bytearray_substring(t.data,1,4) AS created_tx_method_id
+    , top_level_tx_from
+     , top_level_tx_to
+    , top_level_tx_method_id
+    , created_tx_from
+     , created_tx_to
+    , created_tx_method_id
             ,t.index as created_tx_index
             ,ct.code
             ,bytearray_length(ct.code) AS code_bytelength
+            --handle for OP allowlist issue
+            ,ROW_NUMBER() OVER (PARTITION BY contract_address ORDER BY block_number DESC, created_tx_index DESC) AS revision
           from {{ source( chain , 'transactions') }} as t 
           inner join  {{ source( chain , 'creation_traces') }} as ct 
             ON t.hash = ct.tx_hash
@@ -41,6 +60,7 @@
             {% endif %}
 
   ) as x
+  WHERE revision = 1
   group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18
 
 
