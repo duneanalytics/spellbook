a325f20e632b5788f6a697d0df4829324b861ddc
RantumBits <rantum@rantum.xyz>
2023-11-15 17:52:52 -0800

    add to crosschain

--- models/labels/addresses/bridge/labels_bridges.sql
+++ models/labels/addresses/bridge/labels_bridges.sql
@@ -2,7 +2,7 @@
     alias = 'bridge',
     materialized = 'table',
     file_format = 'delta',
-    post_hook='{{ expose_spells(\'["ethereum", "fantom"]\',
+    post_hook='{{ expose_spells(\'["ethereum", "fantom","polygon"]\',
                                 "sector",
                                 "labels",
                                 \'["ilemi"]\') }}')
@@ -11,6 +11,7 @@
 {% set bridges_models = [
  ref('labels_bridges_ethereum')
  , ref('labels_bridges_fantom')
+ , ref('labels_bridges_polygon')
 ] %}
 
 SELECT *
