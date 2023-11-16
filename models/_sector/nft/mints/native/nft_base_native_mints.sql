b8b0fccd2315cad336774bf2e154e1a66c4fcf8c
chuxinh <chuxin@oplabs.co>
2023-11-16 13:47:48 -0500

    add nft mints macro and base native mints

---
+++ models/_sector/nft/mints/native/nft_base_native_mints.sql
@@ -0,0 +1,20 @@
+{{ config(
+        
+        schema = 'nft_base',
+        alias = 'native_mints',
+        partition_by = ['block_month'],
+		file_format = 'delta',
+		incremental_strategy = 'merge',
+        unique_key = ['tx_hash','evt_index','token_id','number_of_items']
+        )
+}}
+
+{% macro nft_mints(blockchain, base_contracts, base_traces, eth_address, erc20_transfer, base_transactions ) %}
+
+{{nft_mints(
+    blockchain='base'
+    , base_contracts = source('base','contracts')
+    , base_traces = source('base','traces')
+    , erc20_transfer = source('erc20_base','evt_Transfer')
+    , base_transactions = source('base','transactions')
+)}}
