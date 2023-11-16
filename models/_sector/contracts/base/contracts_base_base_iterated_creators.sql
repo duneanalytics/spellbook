21da761f7d97f54f0ffcd6e2c0ad67572b05b482
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-11-16 00:31:54 -0500

    unique

--- models/_sector/contracts/base/contracts_base_base_iterated_creators.sql
+++ models/_sector/contracts/base/contracts_base_base_iterated_creators.sql
@@ -3,6 +3,7 @@
         schema = 'contracts_base',
         alias = 'base_iterated_creators',
         materialized ='table',
+        unique_key = ['blockchain', 'contract_address', 'created_tx_hash'],
         partition_by = ['created_month'],
   )
 }}
