cdce8e12bc39ea8c9dc5206b263596a9ff73d413
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-11-15 23:03:27 -0500

    var

--- models/evms/base_evm_smart_account_method_ids.sql
+++ models/evms/base_evm_smart_account_method_ids.sql
@@ -8,8 +8,9 @@
 
  --If Null, make an entry for all chains
 SELECT
-    NULL AS blockchains, method_id, method_descriptor, contract_project
+    cast(NULL as varchar) AS blockchains, method_id, method_descriptor, contract_project
     FROM (values
          (0x6a761202, 'execTransaction', 'Gnosis Safe')
         ,(0x1fad948c, 'handleOps', 'Erc4337')
         ) a (method_id, method_descriptor, contract_project)
+-- UNION ALL
