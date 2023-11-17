102b54bd022844875fb0746491d458034813cd52
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-11-17 09:39:36 +0000

    unique_key fix

--- models/_sector/lending/borrow/ethereum/platforms/aave_v1_ethereum_base_borrow.sql
+++ models/_sector/lending/borrow/ethereum/platforms/aave_v1_ethereum_base_borrow.sql
@@ -5,7 +5,8 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['transaction_type', 'token_address', 'evt_tx_hash', 'evt_index']
+    unique_key = ['transaction_type', 'token_address', 'tx_hash', 'evt_index'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
   )
 }}
 
--- models/_sector/lending/borrow/ethereum/platforms/aave_v3_ethereum_base_borrow.sql
+++ models/_sector/lending/borrow/ethereum/platforms/aave_v3_ethereum_base_borrow.sql
@@ -5,7 +5,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['token_address', 'evt_tx_hash', 'evt_block_number', 'evt_index'],
+    unique_key = ['transaction_type', 'token_address', 'tx_hash', 'evt_index'],
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
   )
 }}
