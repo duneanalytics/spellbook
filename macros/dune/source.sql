a384b8d0c84104bc65b1556312dc08d839fca5b1
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2023-11-22 15:12:48 -0800

    source macro update for cross project source

--- macros/dune/source.sql
+++ macros/dune/source.sql
@@ -1,7 +1,10 @@
-{% macro source(source_name, table_name) %}
+{% macro source(source_name, table_name, cross_project = False) %}
   {% set rel = builtins.source(source_name, table_name) %}
-  {%- if target.type == 'trino' -%}
+  {%- if target.type == 'trino' and not cross_project -%}
     {% set newrel = rel.replace_path(database="delta_prod") %}
+    {% do return(newrel) %}
+  {%- elif cross_project -%}
+    {% set newrel = rel.replace_path(database="hive") %}
     {% do return(newrel) %}
   {%- else -%}
     {% do return(rel) %}
