207d2a31995d3c5bfe8e20ee2e7e9dd7198f35ac
Hildebert Moulié <h.moulie@gmail.com>
2023-11-22 21:59:05 +0100

    fix

--- models/cex/cex_addresses.sql
+++ models/cex/cex_addresses.sql
@@ -8,23 +8,5 @@
 }}
 
 
-{% set cex_models = [
-ref('cex_evms_addresses')
-] %}
-
-SELECT *
-FROM (
-    {% for cex_model in cex_models %}
-    SELECT
-        blockchain, 
-        address,
-        cex_name,
-        distinct_name,
-        added_by,
-        added_date
-    FROM {{ cex_model }}
-    {% if not loop.last %}
-    UNION ALL
-    {% endif %}
-    {% endfor %}
-)
+SELECT 'evms' AS blockchain, address, cex_name, distinct_name, added_by, added_date
+FROM {{ ref('cex_evms_addresses') }}
