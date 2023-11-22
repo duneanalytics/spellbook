285e4d3aeef7a031d7b714142d89be41ddea5827
Hildebert Moulié <h.moulie@gmail.com>
2023-11-22 20:09:50 +0100

    fix

--- models/cex/arbitrum/cex_arbitrum_addresses.sql
+++ models/cex/arbitrum/cex_arbitrum_addresses.sql
@@ -4,4 +4,4 @@
         )}}
 
 SELECT 'aribitrum' AS blockchain, address, cex_name, distinct_name, added_by, added_date
-FROM { ref('cex_evms_addresses') }
+FROM {{ ref('cex_evms_addresses') }}
--- models/cex/fantom/cex_fantom_addresses.sql
+++ models/cex/fantom/cex_fantom_addresses.sql
@@ -4,4 +4,4 @@
         )}}
 
 SELECT 'fantom' AS blockchain, address, cex_name, distinct_name, added_by, added_date
-FROM { ref('cex_evms_addresses')}
+FROM {{ ref('cex_evms_addresses')}}
