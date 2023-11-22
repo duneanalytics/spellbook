b2fa6d727c61eba01c893b843f8344654dabc70d
BennyFeng <BennyFeng@users.noreply.github.com>
2023-11-22 23:31:13 +0800

    Add polygon mooar nft models (#4835)
    * Add polygon aurem nft models
    
    * Merge into nft_events
    
    * Add Mooar polygon nft model, a fork of seaport v1.5
    
    * Fix source definition
    
    * Add dew marketplace
    
    * Mooar nft
    
    * Mooar nft on polygon
    
    ---------
    
    Co-authored-by: Spring Zhang <spring.okay@gmail.com>
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

--- models/_sector/nft/trades/old/platforms/mooar_polygon_events.sql
+++ models/_sector/nft/trades/old/platforms/mooar_polygon_events.sql
@@ -1,5 +1,5 @@
 {{ config(
-    schema = 'oneplanet_polygon',
+    schema = 'mooar_polygon',
     alias = 'events',
     
     materialized = 'incremental',
@@ -8,26 +8,24 @@
     unique_key = ['tx_hash', 'evt_index', 'nft_contract_address', 'token_id', 'sub_type', 'sub_idx']
     )
 }}
-
 WITH fee_wallets as (
     select wallet_address, wallet_name from (
-    values (0xf7eB758c21a6d0f9029Da4655B9F24343c3924dB,'oneplanet')
+    values (0xdd55acb1855cfec31d0f08c80da1db9862a50660,'mooar')
     ) as foo(wallet_address, wallet_name)
 )
 , trades as (
-    {{ seaport_v3_fork_trades(
+    {{ seaport_v4_fork_trades(
      blockchain = 'polygon'
      ,source_transactions = source('polygon','transactions')
-     ,Seaport_evt_OrderFulfilled = source('oneplanet_polygon','Seaport_evt_OrderFulfilled')
-     ,Seaport_call_matchAdvancedOrders = source('oneplanet_polygon','Seaport_call_matchAdvancedOrders')
-     ,Seaport_call_matchOrders = source('oneplanet_polygon','Seaport_call_matchOrders')
+     ,Seaport_evt_OrderFulfilled = source('gashero_polygon','MOOAR_evt_OrderFulfilled')
+     ,Seaport_evt_OrdersMatched = source('gashero_polygon','MOOAR_evt_OrdersMatched')
      ,fee_wallet_list_cte = 'fee_wallets'
      ,native_token_address = '0x0000000000000000000000000000000000000000'
      ,alternative_token_address = '0x0000000000000000000000000000000000001010'
      ,native_token_symbol = 'MATIC'
-     ,start_date = '2023-09-03'
-     ,seaport_fork_address = '0xcbbecf690e030d096794f7685a1bf4a58378a575'
-     ,project = 'oneplanet'
+     ,start_date = '2023-08-18'
+     ,Seaport_order_contracts = ['0xaaaaaaaa33d3520a2266ce508bc079fcfe82c8e3']
+     ,project = 'mooar'
      ,version = 'v1'
     )
   }}
