e6329102f84fb447be425eabb7989c2bcc87c54b
tomfutago <tomfutago@gmail.com>
2023-11-22 14:19:16 +0000

    fix tests

--- tests/aave/ethereum/aave_supply_test.sql
+++ tests/aave/ethereum/aave_supply_test.sql
@@ -6,8 +6,8 @@ with unit_test1
         from   {{ ref('aave_ethereum_supply' )}}
         where  token_address = 0x514910771af9ca656af840dff83e8264ecf986ca
                and depositor = 0x4767192455266e422386d14991d697a418c63225
-               and evt_block_time = TIMESTAMP '2022-09-21 18:09'
-               and evt_tx_hash = 0x5be7cab6a33b1f1a050858e18a0a3140092440fbeb9e431c88103762f23d5305),
+               and block_time = TIMESTAMP '2022-09-21 18:09'
+               and tx_hash = 0x5be7cab6a33b1f1a050858e18a0a3140092440fbeb9e431c88103762f23d5305),
     unit_test2
     as (select case
                  when amount = 250 then true
@@ -16,7 +16,7 @@ with unit_test1
         from   {{ ref('aave_ethereum_supply' )}}
         where  symbol = 'SNX'
                and depositor = 0x1e18b8b6f27568023e0b577cbee1889391b2f444
-               and evt_block_time = TIMESTAMP '2022-09-24 00:05')
+               and block_time = TIMESTAMP '2022-09-24 00:05')
 select *
 from   (select *
        from   unit_test1
