9f3869944a601787c084ac6126be1afa771cb956
tomfutago <tomfutago@gmail.com>
2023-11-22 11:24:59 +0000

    ethereum

---
+++ models/_sector/lending/supply/ethereum/platforms/exposed/aave_ethereum_supply.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    schema = 'aave_ethereum',
+    alias = 'supply',
+    materialized = 'view',
+    post_hook = '{{ expose_spells(\'["ethereum"]\',
+                                    "project",
+                                    "aave",
+                                    \'["tomfutago"]\') }}'
+  )
+}}
+
+select
+  blockchain,
+  project,
+  version,
+  transaction_type,
+  loan_type,
+  symbol,
+  token_address,
+  depositor,
+  withdrawn_to,
+  liquidator,
+  amount,
+  usd_amount,
+  block_month,
+  block_time,
+  block_number,
+  tx_hash,
+  evt_index
+from {{ ref('lending_supply') }}
+where blockchain = 'ethereum'
+  and project = 'aave'
