d9b3578ab4bdac29e0ed1b8c21f86a2fb3da6820
Keen <mail@kuan.li>
2023-11-24 14:36:55 +0800

    update rfqv2 fields

--- models/tokenlon/ethereum/tokenlon_v5_ethereum_rfq_v2_trades.sql
+++ models/tokenlon/ethereum/tokenlon_v5_ethereum_rfq_v2_trades.sql
@@ -22,13 +22,13 @@
 WITH dexs AS (
     SELECT 
         evt_block_time        AS block_time,
-        "userAddr"            AS taker,
-        "makerAddr"           AS maker,
-        "takerAssetAmount"    AS token_sold_amount_raw,
-        "makerAssetAmount"    AS token_bought_amount_raw,
+        "user"                AS taker,
+        "maker"               AS maker,
+        "takerTokenAmount"    AS token_sold_amount_raw,
+        "makerTokenAmount"    AS token_bought_amount_raw,
         cast(NULL as double)  AS amount_usd,
-        "takerAssetAddr"      AS token_sold_address,
-        "makerAssetAddr"      AS token_bought_address,
+        "takerToken"          AS token_sold_address,
+        "makerToken"          AS token_bought_address,
         contract_address      AS project_contract_address,
         evt_tx_hash           AS tx_hash,
         CAST(ARRAY[] as array<bigint>) AS trace_address,
@@ -58,8 +58,8 @@ SELECT
     CAST(dexs.token_bought_amount_raw AS UINT256)        AS token_bought_amount_raw,
     CAST(dexs.token_sold_amount_raw AS UINT256)          AS token_sold_amount_raw,
     coalesce(dexs.
-        amount_usd, 
-        (dexs.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price, 
+        amount_usd,
+        (dexs.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price,
         (dexs.token_sold_amount_raw / power(10, p_sold.decimals)) * p_sold.price
     )                                                           AS amount_usd,
     dexs.token_bought_address,
@@ -73,34 +73,34 @@ SELECT
     dexs.trace_address,
     dexs.evt_index
 FROM dexs
-INNER JOIN {{ source('ethereum', 'transactions') }} tx 
+INNER JOIN {{ source('ethereum', 'transactions') }} tx
     ON tx.hash = dexs.tx_hash
     {% if not is_incremental() %}
     AND tx.block_time >= {{project_start_date}}
-    {% endif %} 
-    {% if is_incremental() %}
-    AND tx.block_time >= date_trunc('day', now() - interval '7' day) 
     {% endif %}
-LEFT JOIN {{ ref('tokens_erc20') }} erc20a 
+    {% if is_incremental() %}
+    AND tx.block_time >= date_trunc('day', now() - interval '7' day)
+    {% endif %}
+LEFT JOIN {{ ref('tokens_erc20') }} erc20a
     ON erc20a.contract_address = dexs.token_bought_address
     AND erc20a.blockchain = 'ethereum'
-LEFT JOIN {{ ref('tokens_erc20') }} erc20b 
+LEFT JOIN {{ ref('tokens_erc20') }} erc20b
     ON erc20b.contract_address = dexs.token_sold_address
     AND erc20b.blockchain = 'ethereum'
-LEFT JOIN {{ source('prices', 'usd') }} p_bought 
+LEFT JOIN {{ source('prices', 'usd') }} p_bought
     ON p_bought.minute = date_trunc('minute', dexs.block_time)
     AND p_bought.contract_address = dexs.token_bought_address
-    AND p_bought.blockchain = 'ethereum' 
+    AND p_bought.blockchain = 'ethereum'
     {% if not is_incremental() %}
     AND p_bought.minute >= {{project_start_date}}
-    {% endif %} 
+    {% endif %}
     {% if is_incremental() %}
     AND p_bought.minute >= date_trunc('day', now() - interval '7' day)
     {% endif %}
 LEFT JOIN {{ source('prices', 'usd') }} p_sold
     ON p_sold.minute = date_trunc('minute', dexs.block_time)
     AND p_sold.contract_address = dexs.token_sold_address
-    AND p_sold.blockchain = 'ethereum' 
+    AND p_sold.blockchain = 'ethereum'
     {% if not is_incremental() %}
     AND p_sold.minute >= {{project_start_date}}
     {% endif %}
