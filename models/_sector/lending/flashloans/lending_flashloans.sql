b2d1f597b3b8f8df6c7496f65ab4bef7ca169fbc
tomfutago <tomfutago@gmail.com>
2023-11-24 17:08:21 +0000

    update uniqueness

--- models/_sector/lending/flashloans/lending_flashloans.sql
+++ models/_sector/lending/flashloans/lending_flashloans.sql
@@ -6,7 +6,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['blockchain', 'project', 'version', 'transaction_type', 'token_address', 'tx_hash', 'evt_index'],
+    unique_key = ['blockchain', 'project', 'version', 'tx_hash', 'evt_index'],
   )
 }}
 
