1388de053cb6af55011bc8cb87876e54f02acaae
RantumBits <rantum@rantum.xyz>
2023-11-28 10:03:19 -0800

    migrate eth erc20 transfers

--- models/balances/ethereum/erc20/balances_ethereum_erc20_day.sql
+++ models/balances/ethereum/erc20/balances_ethereum_erc20_day.sql
@@ -1,59 +1,67 @@
 {{ config(
-tags=['prod_exclude'],
+        schema = 'balances_ethereum',
         alias = 'erc20_day',
-        post_hook='{{ expose_spells_hide_trino(\'["ethereum"]\',
-                                            "sector",
-                                            "balances",
-                                            \'["soispoke","dot2dotseurat"]\') }}'
+        post_hook='{{ expose_spells(\'["ethereum"]\',
+                                    "sector",
+                                    "balances",
+                                    \'["rantum"]\') }}'
         )
 }}
+WITH 
 
-/*
-    note: this spell has not been migrated to dunesql, therefore is only a view on spark
-        please migrate to dunesql to ensure up-to-date logic & data
-*/
+time_seq AS (
+    SELECT 
+        sequence(
+        CAST('2021-05-29' as timestamp),
+        date_trunc('day', cast(now() as timestamp)),
+        interval '1' day
+        ) AS time 
+),
 
-with
-    days as (
-        select
-            explode(
-                sequence(
-                    to_date('2015-01-01'), date_trunc('day', now()), interval 1 day
-                )
-            ) as day
-    )
+days AS (
+    SELECT 
+        time.time AS block_day 
+    FROM time_seq
+    CROSS JOIN unnest(time) AS time(time)
+),
 
-, daily_balances as
- (SELECT
-    wallet_address,
-    token_address,
-    amount_raw,
-    amount,
-    day,
-    symbol,
-    lead(day, 1, now()) OVER (PARTITION BY token_address, wallet_address ORDER BY day) AS next_day
-    FROM {{ ref('transfers_ethereum_erc20_rolling_day') }})
+daily_balances as (
+    SELECT
+        blockchain, 
+        block_day, 
+        wallet_address, 
+        token_address, 
+        amount_raw,
+        amount,
+        symbol,
+        LEAD(block_day, 1, current_timestamp) OVER (PARTITION BY token_address, wallet_address ORDER BY block_day) AS next_day
+    FROM 
+    {{ ref('transfers_ethereum_erc20_rolling_day') }}
+)
 
 SELECT
-    'ethereum' as blockchain,
-    d.day,
+    b.blockchain,
+    cast(date_trunc('month', d.block_day) as date) as block_month,
+    d.block_day,
     b.wallet_address,
     b.token_address,
     b.amount_raw,
     b.amount,
     b.amount * p.price as amount_usd,
     b.symbol
-FROM daily_balances b
-INNER JOIN days d ON b.day <= d.day AND d.day < b.next_day
-LEFT JOIN {{ source('prices', 'usd') }} p
+FROM 
+daily_balances b
+INNER JOIN 
+days d 
+    ON b.block_day <= d.block_day 
+    AND d.block_day < b.next_day
+LEFT JOIN 
+{{ source('prices', 'usd') }} p
     ON p.contract_address = b.token_address
-    AND d.day = p.minute
+    AND d.block_day = p.minute
     AND p.blockchain = 'ethereum'
--- Removes rebase tokens from balances
-LEFT JOIN {{ ref('tokens_ethereum_rebase') }}  as r
-    ON b.token_address = r.contract_address
 -- Removes likely non-compliant tokens due to negative balances
-LEFT JOIN {{ ref('balances_ethereum_erc20_noncompliant') }}  as nc
+LEFT JOIN 
+{{ ref('balances_ethereum_erc20_noncompliant') }} nc
     ON b.token_address = nc.token_address
-WHERE r.contract_address is null
-and nc.token_address is null
+WHERE nc.token_address IS NULL
