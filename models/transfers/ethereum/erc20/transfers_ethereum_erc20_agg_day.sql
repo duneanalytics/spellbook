a6dd18d33520fb8c73900f6b6faf83a23c8edff6
RantumBits <rantum@rantum.xyz>
2023-11-29 14:28:56 -0800

    remove eth changes

--- models/transfers/ethereum/erc20/transfers_ethereum_erc20_agg_day.sql
+++ models/transfers/ethereum/erc20/transfers_ethereum_erc20_agg_day.sql
@@ -1,29 +1,29 @@
 {{ config(
-tags=['prod_exclude'],
+        schema = tranfers_ethereum,
         alias = 'erc20_agg_day',
         materialized ='incremental',
+        partition_by = ['block_month'],
         file_format ='delta',
         incremental_strategy='merge',
-        unique_key='unique_transfer_id'
+        unique_key = ['block_day', 'wallet_address', 'token_address']
         )
 }}
-/*
-    note: this spell has not been migrated to dunesql, therefore is only a view on spark
-        please migrate to dunesql to ensure up-to-date logic & data
-*/
+
 select
-    'ethereum' as blockchain,
-    date_trunc('day', tr.evt_block_time) as day,
+    tr.blockchain,
+    date_trunc('day', tr.evt_block_time) as block_day,
+    block_month,
     tr.wallet_address,
     tr.token_address,
     t.symbol,
-    tr.wallet_address || '-' || tr.token_address || '-' || date_trunc('day', tr.evt_block_time) as unique_transfer_id,
     sum(tr.amount_raw) as amount_raw,
     sum(tr.amount_raw / power(10, t.decimals)) as amount
-from {{ ref('transfers_ethereum_erc20') }} tr
-left join {{ ref('tokens_ethereum_erc20') }} t on t.contract_address = tr.token_address
+FROM 
+{{ ref('transfers_ethereum_erc20') }} tr
+LEFT JOIN 
+{{ ref('tokens_ethereum_erc20') }} t on t.contract_address = tr.token_address
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
-where tr.evt_block_time >= date_trunc('day', now() - interval '1 week')
+WHERE tr.evt_block_time >= date_trunc('day', now() - interval '3' Day)
 {% endif %}
-group by 1, 2, 3, 4, 5, 6 
+GROUP BY 1, 2, 3, 4, 5, 6
