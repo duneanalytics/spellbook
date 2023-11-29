63dc8dedbcb138916d611c877423a94a29c342f2
RantumBits <rantum@rantum.xyz>
2023-11-29 13:08:33 -0800

    start date

--- models/balances/ethereum/erc20/balances_ethereum_erc20_hour.sql
+++ models/balances/ethereum/erc20/balances_ethereum_erc20_hour.sql
@@ -14,7 +14,7 @@ WITH
 
 years as (
     select year
-    from (values (sequence(timestamp '2021-05-29', cast(date_trunc('year', now()) as timestamp), interval '1' year))) s(year_array)
+    from (values (sequence(timestamp '2015-05-29', cast(date_trunc('year', now()) as timestamp), interval '1' year))) s(year_array)
       cross join unnest(year_array) as d(year)
 ),
 
