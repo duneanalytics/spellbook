a89ae966ae484dd0ef928105839fdb8c4d743ae1
RantumBits <rantum@rantum.xyz>
2023-11-29 14:14:05 -0800

    block_hour, block_day

--- tests/balances/base/balances_base_erc20_day_assert_samples.sql
+++ tests/balances/base/balances_base_erc20_day_assert_samples.sql
@@ -15,7 +15,7 @@ with sampled_wallets as
 (SELECT case when round(test_data.amount_raw/power(10, 18), 4) = round(token_balances.amount_raw/power(10, 18), 4) then True else False end as amount_raw_test
 FROM {{ ref('balances_base_erc20_daily_entries') }} as test_data
 JOIN sampled_wallets as token_balances
-ON test_data.timestamp = token_balances.day
+ON test_data.timestamp = token_balances.block_day
 AND cast(test_data.wallet_address as varbinary) = cast(token_balances.wallet_address as varbinary)
 AND cast(test_data.token_address as varbinary) = cast(token_balances.token_address as varbinary))
 
