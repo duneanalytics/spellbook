a475ade58aca905e1716a92dd7e7c3638425a112
Hildebert Moulié <h.moulie@gmail.com>
2023-11-29 12:17:51 +0100

    fixes

--- macros/models/_project/zora/zora_mints_macro.sql
+++ macros/models/_project/zora/zora_mints_macro.sql
@@ -9,7 +9,7 @@ SELECT '{{blockchain}}' AS blockchain
 , mints.firstPurchasedTokenId + t.sequence_element AS token_id
 , 1 AS quantity
 , mints.pricePerToken/1e18/mints.quantity AS total_price
-, pu.price*(mints.value/1e18) AS total_price_usd
+, pu.price*(mints.pricePerToken/1e18/mints.quantity) AS total_price_usd
 , mints.to AS recipient
 , mints.evt_tx_hash AS tx_hash
 , mints.evt_index
@@ -21,7 +21,7 @@ INNER JOIN {{transactions}} txs ON txs.block_number=mints.evt_block_number
         AND txs.hash=mints.evt_tx_hash
 INNER JOIN {{ ref('evms_info') }} info ON info.blockchain='{{blockchain}}'
 INNER JOIN {{ ref('prices_usd_forward_fill') }} pu ON pu.blockchain='{{blockchain}}'
-        AND pu.contract_address=0x4200000000000000000000000000000000000006
+        AND pu.contract_address=info.wrapped_native_token_address
         AND pu.minute=date_trunc('minute', mints.evt_block_time)
 CROSS JOIN UNNEST(sequence(1, CAST(quantity AS BIGINT))) AS t(sequence_element)
 {% if is_incremental() %}
@@ -48,7 +48,7 @@ FROM {{erc1155_mints}} mints
 INNER JOIN {{transactions}} txs ON txs.block_number=mints.evt_block_number
         AND txs.hash=mints.evt_tx_hash
 INNER JOIN {{ ref('evms_info') }} info ON info.blockchain='{{blockchain}}'
-INNER JOIN {{ source('prices', 'usd') }} pu ON pu.blockchain='{{blockchain}}'
+INNER JOIN {{ ref('prices_usd_forward_fill') }} pu ON pu.blockchain='{{blockchain}}'
         AND pu.contract_address=info.wrapped_native_token_address
         AND pu.minute=date_trunc('minute', mints.evt_block_time)
 {% if is_incremental() %}
