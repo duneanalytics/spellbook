f087a087c34cc735374976ce7498eea354c05eac
RantumBits <rantum@rantum.xyz>
2023-11-29 13:06:29 -0800

    test update

--- tests/balances/base/balances_base_erc20_hour_assert_specific_wallet.sql
+++ tests/balances/base/balances_base_erc20_hour_assert_specific_wallet.sql
@@ -3,7 +3,7 @@
 WITH unit_tests as
 (SELECT case when round(test_data.amount_raw/power(10, 22), 3) = round(token_balances.amount_raw/power(10, 22), 3) then True else False end as amount_raw_test
 FROM {{ ref('balances_base_erc20_specific_wallet') }} as test_data
-JOIN (select * from {{ ref('balances_base_erc20_hour') }} where wallet_address = 0xff0cefdbd6bf757cc0cc361ddfbde432186ccaa6) as token_balances
+JOIN (select * from {{ ref('balances_base_erc20_hour') }} where wallet_address = 0x1b72bac3772050fdcaf468cce7e20deb3cb02d89) as token_balances
 ON test_data.timestamp = token_balances.block_hour
 AND cast(test_data.wallet_address as varbinary) = token_balances.wallet_address
 AND cast(test_data.token_address as varbinary)  = token_balances.token_address)
