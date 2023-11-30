c64526f778c2097f52bc6ce554d5b9bbcb02ee93
RantumBits <rantum@rantum.xyz>
2023-11-30 09:08:05 -0800

    revise test

--- tests/balances/base/balances_base_erc20_hour_assert_samples.sql
+++ tests/balances/base/balances_base_erc20_hour_assert_samples.sql
@@ -1,27 +1,36 @@
 -- Bootstrapped correctness test against legacy Caladan values.
+WITH sampled_wallets AS (
+    SELECT *
+    FROM {{ ref('balances_base_erc20_hour') }} bal
+    WHERE wallet_address IN (
+            SELECT DISTINCT CAST(wallet_address AS VARBINARY)
+            FROM {{ ref('balances_base_erc20_latest_entries') }}
+        )
+        AND bal.token_address IN (
+            0x50c5725949a6f0c72e6c4a641f24049a917db0cb,
+            0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca,
+            0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
+        ) --'DAI', 'USDbc', 'USDC'
+        AND bal.block_hour > CAST('2023-11-04' AS DATE)
+        AND bal.block_hour < CAST('2023-11-06' AS DATE)
+), unit_tests AS (
+    SELECT
+        CASE
+            WHEN ROUND(CAST(test_data.amount_raw AS DOUBLE) / POWER(10, 22), 3) =
+                 ROUND(CAST(token_balances.amount_raw AS DOUBLE) / POWER(10, 22), 3)
+            THEN TRUE
+            ELSE FALSE
+        END AS amount_raw_test
+    FROM {{ ref('balances_base_erc20_latest_entries') }} AS test_data
+    JOIN sampled_wallets AS token_balances
+    ON test_data.timestamp = token_balances.block_hour
+        AND CAST(test_data.wallet_address AS VARBINARY) = CAST(token_balances.wallet_address AS VARBINARY)
+        AND CAST(test_data.token_address AS VARBINARY) = CAST(token_balances.token_address AS VARBINARY)
+)
 
-with sampled_wallets as
- (
-     select *
-     from {{ ref('balances_base_erc20_hour') }} bal
-     where wallet_address in (select distinct cast(wallet_address as varbinary) from {{ ref('balances_base_erc20_latest_entries') }})
-     and bal.token_address in ( 0x50c5725949a6f0c72e6c4a641f24049a917db0cb,
-                        0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca,
-                         0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913) --'DAI', 'USDbc', 'USDC'
-     and bal.block_hour > cast('2023-11-04' as date) and bal.block_hour < cast('2023-11-06' as date)
- )
-
-, unit_tests as
-(SELECT case when round(test_data.amount_raw/power(10, 22), 3) = round(token_balances.amount_raw/power(10, 22), 3) then True else False end as amount_raw_test
-FROM {{ ref('balances_base_erc20_latest_entries') }} as test_data
-JOIN sampled_wallets as token_balances
-ON test_data.timestamp = token_balances.block_hour
-AND cast(test_data.wallet_address as varbinary) = cast(token_balances.wallet_address as varbinary)
-AND cast(test_data.token_address as varbinary) = cast(token_balances.token_address as varbinary))
-
-select count(case when amount_raw_test = false then 1 else null end)/count(*) as pct_mismatch, count(*) as count_rows
-from unit_tests
+SELECT
+    COUNT(CASE WHEN amount_raw_test = FALSE THEN 1 ELSE NULL END) / COUNT(*) AS pct_mismatch,
+    COUNT(*) AS count_rows
+FROM unit_tests
 -- Having mismatches less than 5% of rows
-having count(case when amount_raw_test = false then 1 else null end) > count(*)*0.05
-
-
+HAVING COUNT(CASE WHEN amount_raw_test = FALSE THEN 1 ELSE NULL END) > COUNT(*) * 0.05;
--- tests/balances/base/balances_base_erc20_hour_assert_specific_wallet.sql
+++ tests/balances/base/balances_base_erc20_hour_assert_specific_wallet.sql
@@ -1,16 +1,26 @@
 -- Bootstrapped correctness test against legacy Caladan values.
+WITH unit_tests AS (
+    SELECT
+        CASE
+            WHEN ROUND(CAST(test_data.amount_raw AS DOUBLE) / POWER(10, 22), 3) =
+                 ROUND(CAST(token_balances.amount_raw AS DOUBLE) / POWER(10, 22), 3)
+            THEN TRUE
+            ELSE FALSE
+        END AS amount_raw_test
+    FROM {{ ref('balances_base_erc20_specific_wallet') }} AS test_data
+    JOIN (
+        SELECT *
+        FROM {{ ref('balances_base_erc20_hour') }}
+        WHERE wallet_address = 0x1b72bac3772050fdcaf468cce7e20deb3cb02d89
+    ) AS token_balances
+    ON test_data.timestamp = token_balances.block_hour
+        AND CAST(test_data.wallet_address AS VARBINARY) = token_balances.wallet_address
+        AND CAST(test_data.token_address AS VARBINARY) = token_balances.token_address
+)
 
-WITH unit_tests as
-(SELECT case when round(test_data.amount_raw/power(10, 22), 3) = round(token_balances.amount_raw/power(10, 22), 3) then True else False end as amount_raw_test
-FROM {{ ref('balances_base_erc20_specific_wallet') }} as test_data
-JOIN (select * from {{ ref('balances_base_erc20_hour') }} where wallet_address = 0x1b72bac3772050fdcaf468cce7e20deb3cb02d89) as token_balances
-ON test_data.timestamp = token_balances.block_hour
-AND cast(test_data.wallet_address as varbinary) = token_balances.wallet_address
-AND cast(test_data.token_address as varbinary)  = token_balances.token_address)
-
-
-select count(case when amount_raw_test = false then 1 else null end)/count(*) as pct_mismatch, count(*) as count_rows
-from unit_tests
+SELECT
+    COUNT(CASE WHEN amount_raw_test = FALSE THEN 1 ELSE NULL END) / COUNT(*) AS pct_mismatch,
+    COUNT(*) AS count_rows
+FROM unit_tests
 -- Having mismatches less than 1% of rows
-having count(case when amount_raw_test = false then 1 else null end) > count(*)*0.01
-
+HAVING COUNT(CASE WHEN amount_raw_test = FALSE THEN 1 ELSE NULL END) > COUNT(*) * 0.01;
