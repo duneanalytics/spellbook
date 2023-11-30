f3b97f6f6c03bc60f082470388c116a8791fd358
RantumBits <rantum@rantum.xyz>
2023-11-30 12:03:58 -0800

    change begin date

--- models/balances/base/eth/balances_base_eth_day.sql
+++ models/balances/base/eth/balances_base_eth_day.sql
@@ -12,7 +12,7 @@ WITH
 time_seq AS (
     SELECT 
         sequence(
-        CAST('2023-08-01' as timestamp),
+        CAST('2023-06-15' as timestamp),
         date_trunc('day', cast(now() as timestamp)),
         interval '1' day
         ) AS time 
