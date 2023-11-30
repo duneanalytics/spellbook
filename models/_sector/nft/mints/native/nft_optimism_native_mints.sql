7a0afe6819f0911e7f75783da4bd6f454ccee7e9
chuxinh <chuxin@oplabs.co>
2023-11-30 17:26:50 -0500

    update incremental

--- models/_sector/nft/mints/native/nft_optimism_native_mints.sql
+++ models/_sector/nft/mints/native/nft_optimism_native_mints.sql
@@ -4,6 +4,7 @@
         alias = 'native_mints',
         partition_by = ['block_month'],
 		file_format = 'delta',
+        materialized = 'incremental',
 		incremental_strategy = 'merge',
         unique_key = ['tx_hash','evt_index','token_id','number_of_items']
         )
