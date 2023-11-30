bed2f58fdf2bcf7116d61f513591b6e3cb5ee46c
RantumBits <rantum@rantum.xyz>
2023-11-29 16:27:54 -0800

    remve base changes

--- models/transfers/base/transfers_base_eth.sql
+++ models/transfers/base/transfers_base_eth.sql
@@ -1,46 +1,77 @@
-{{ 
+{{
     config(
-        alias = 'eth', 
-        schema = 'transfers_base',
+        alias ='eth',
+        
         materialized ='incremental',
         file_format ='delta',
         incremental_strategy='merge',
-        unique_key=['tx_hash', 'trace_address'],
+        unique_key='unique_transfer_id',
         post_hook='{{ expose_spells(\'["base"]\',
                                     "sector",
                                     "transfers",
-                                    \'["msilb7", "chuxin","rantum"]\') }}'
+                                    \'["msilb7", "chuxin"]\') }}'
     )
 }}
+with eth_transfers as (
+    select
+        r."from"
+        ,r.to
+        --Using the ETH deposit placeholder address to match with prices tables
+        ,0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000 as contract_address
+        ,cast(r.value as double) AS value
+        ,cast(r.value as double)/1e18 as value_decimal
+        ,r.tx_hash
+        ,r.trace_address
+        ,r.block_time as tx_block_time
+        ,r.block_number as tx_block_number
+        ,substring(to_hex(t.data), 1, 10) as tx_method_id
+        ,cast(r.tx_hash as varchar) || '-' || array_join(r.trace_address,',') as unique_transfer_id
+        ,t.to AS tx_to
+        ,t."from" AS tx_from
+    from {{ source('base', 'traces') }} as r
+    join {{ source('base', 'transactions') }} as t
+        on r.tx_hash = t.hash
+        and r.block_number = t.block_number
+    where
+        (r.call_type not in ('delegatecall', 'callcode', 'staticcall') or r.call_type is null)
+        and r.tx_success
+        and r.success
+        and r.value > uint256 '0'
+        {% if is_incremental() %} -- this filter will only be applied on an incremental run
+        and r.block_time >= date_trunc('day', now() - interval '7' day)
+        and t.block_time >= date_trunc('day', now() - interval '7' day)
+        {% endif %}
 
-select 
-    'base' as blockchain
-    ,r."from"
-    ,r.to as wallet_address
-    --Using the ETH placeholder address to match with prices tables
-    ,0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee as token_address
-    ,CAST(r.value as double) as amount_raw
-    ,cast(r.value as double)/1e18 as value_decimal
-    ,r.tx_hash
-    ,r.trace_address
-    ,r.block_time as block_time 
-    ,r.block_number as tx_block_number 
-    ,bytearray_substring(t.data, 1, 4) as tx_method_id
-    ,t.to AS tx_to
-    ,t."from" AS tx_from
-    , CAST(date_trunc('month', r.block_time) as date) as block_month
-from {{ source('base', 'traces') }} as r 
-join {{ source('base', 'transactions') }} as t 
-    on r.tx_hash = t.hash
-    and r.block_number = t.block_number
-    {% if is_incremental() %} -- this filter will only be applied on an incremental run
-    and {{incremental_predicate('t.block_time')}}
-    {% endif %}
-where 
-    (r.call_type not in ('delegatecall', 'callcode', 'staticcall') or r.call_type is null)
-    and r.tx_success = true
-    and r.success = true
-    and r.value > uint256 '0'
-    {% if is_incremental() %} -- this filter will only be applied on an incremental run
-    and {{incremental_predicate('r.block_time')}}
-    {% endif %}
+    union all
+    --ETH Transfers from deposits and withdrawals are ERC20 transfers of the 'deadeadead' ETH token. These do not appear in traces.
+
+    select
+        r."from"
+        ,r.to
+        --Using the ETH deposit placeholder address to match with prices tables
+        ,0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000 as contract_address
+        ,cast(r.value as double) AS value
+        ,cast(r.value as double)/1e18 as value_decimal
+        ,r.evt_tx_hash as tx_hash
+        ,array[r.evt_index] as trace_address
+        ,r.evt_block_time as tx_block_time
+        ,r.evt_block_number as tx_block_number
+        ,substring(to_hex(t.data), 1, 10) as tx_method_id
+        ,cast(r.evt_tx_hash as varchar) || '-' || cast(r.evt_index as varchar) as unique_transfer_id
+        ,t.to AS tx_to
+        ,t."from" AS tx_from
+    from {{ source('erc20_base', 'evt_transfer') }} as r
+    join {{ source('base', 'transactions') }} as t
+        on r.evt_tx_hash = t.hash
+        and r.evt_block_number = t.block_number
+    where
+        r.contract_address = 0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000
+        and t.success
+        and r.value > uint256 '0'
+        {% if is_incremental() %} -- this filter will only be applied on an incremental run
+        and r.evt_block_time >= date_trunc('day', now() - interval '7' day)
+        and t.block_time >= date_trunc('day', now() - interval '7' day)
+        {% endif %}
+)
+select *
+from eth_transfers
