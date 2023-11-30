2a199498a10bc71eaccac74c39218fe87110890e
grkhr <eduardgorkh@gmail.com>
2023-12-01 01:15:02 +0200

    add cta

--- models/dex/dex_raw_pool_initializations.sql
+++ models/dex/dex_raw_pool_initializations.sql
@@ -41,6 +41,7 @@ select * from (
             , substr(input, 17, 20) token0
             , substr(input, 49, 20) token1
             , tx_hash
+            , call_trace_address
         from {{ source(blockchain, 'traces') }}
         where 
             substr(input, 1, 4) = 0x485cc955 
