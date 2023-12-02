a2ebd1eecc37051c04d14aae2d67d6ca139c52af
tomfutago <tomfutago@gmail.com>
2023-12-02 15:55:40 +0000

    init commit

--- models/_sector/dex/trades/bnb/dex_bnb_base_trades.sql
+++ models/_sector/dex/trades/bnb/dex_bnb_base_trades.sql
@@ -12,6 +12,8 @@
     , ref('airswap_bnb_base_trades')
     , ref('sushiswap_v1_bnb_base_trades')
     , ref('sushiswap_v2_bnb_base_trades')
+    , ref('biswap_v2_bnb_base_trades')
+    , ref('biswap_v3_bnb_base_trades')
 ] %}
 
 WITH base_union AS (
