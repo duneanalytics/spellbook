fb222b66873ac946a8782750fea7a6a52822e3de
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-12-03 15:41:32 -0500

    refactor op_chains

--- models/op/op_chains/op_chains_optimism_chain_list.sql
+++ models/op/op_chains/op_chains_optimism_chain_list.sql
@@ -2,23 +2,31 @@
      schema = 'op_chains'
         , alias = 'chain_list'
         , unique_key = ['blockchain', 'chain_id']
-        , post_hook='{{ expose_spells(\'["optimism"]\',
+        , post_hook='{{ expose_spells(\'["optimism","base","zora"]\',
                                   "project",
                                   "op_chains",
                                   \'["msilb7"]\') }}'
   )
 }}
 
+{% set op_chains = all_op_chains() %} --macro: all_op_chains.sql
+
+WITH chain_names AS (
+        {% for chain in op_chains %}
+                SELECT chain AS chain_dune_name -- name of the chain's dune database
+        {% if not loop.last %}
+                UNION ALL
+        {% endif %}
+        {% endfor %}
+)
+
 SELECT 
-        lower(blockchain) AS blockchain,
-        blockchain_name,
+        blockchain,
+        name as blockchain_name,
         chain_id,
-        cast(start_date AS date) AS start_date,
-        is_superchain
+        cast(first_block_time AS date) AS start_date,
+        1 as is_superchain
 
-FROM(values
-
-         ('optimism',   'Optimism Mainnet', 10, '2021-06-23',   1)
-        ,('base',       'Base Mainnet',     NULL,   NULL,       1)
-
-) op (blockchain, blockchain_name, chain_id, start_date, is_superchain)
+FROM chain_names c
+        LEFT JOIN {{ ref('evms_info') }} i 
+                ON i.blockchain = c.chain_dune_name
