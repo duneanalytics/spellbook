1277a61ea9e4556647d151c6eb3bdf2622733318
Hildebert Moulié <h.moulie@gmail.com>
2023-12-04 00:38:14 +0100

    fix

--- models/dex/chains/mev/arbitrages/dex_arbitrum_arbitrages.sql
+++ models/dex/chains/mev/arbitrages/dex_arbitrum_arbitrages.sql
@@ -5,7 +5,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain','tx_hash', 'project_contract_address', 'evt_index']
+        unique_key = ['blockchain','tx_hash', 'evt_index']
 )
 }}
 
