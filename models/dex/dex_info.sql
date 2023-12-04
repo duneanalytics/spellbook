3279fb8798749a40f90d65063928b3c0dc34b155
ARDev097 <ardev029@gmail.com>
2023-12-04 09:54:59 +0530

    Added mummy_finance and firebird_finance dex info in dex_info sql file

--- models/dex/dex_info.sql
+++ models/dex/dex_info.sql
@@ -90,6 +90,6 @@ FROM (VALUES
     , ('mauve', 'Mauve', 'Direct', 'mauve_org')
     , ('xchange', 'X7 Finance', 'Direct', 'X7_Finance')
     , ('mento', 'Mento', 'Direct', 'mento')
-    , ('firebird_finance', 'Firebird Finance', 'Aggregator', 'FinanceFirebird')
     , ('mummy_finance', 'Mummy Finance', 'Direct', 'mummyftm')
+    , ('firebird_finance', 'Firebird Finance', 'Aggregator', 'FinanceFirebird')
     ) AS temp_table (project, name, marketplace_type, x_username)
