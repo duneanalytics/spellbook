65d0eed58f8bdff857f1a56d0c8f9563816c26b4
Hildebert Moulié <h.moulie@gmail.com>
2023-12-04 02:29:17 +0100

    fix

--- macros/models/_sector/dex/dex_arbitrages.sql
+++ macros/models/_sector/dex/dex_arbitrages.sql
@@ -24,7 +24,7 @@ WITH multi_trade_txs AS (
     INNER JOIN multi_trade_txs pa USING (block_time, tx_hash)
     WHERE dt.blockchain = '{{blockchain}}'
     {% if is_incremental() %}
-    AND dt.block_time >= date_trunc('day', now() - interval '7' day)
+    AND block_time >= date_trunc('day', now() - interval '7' day)
     {% endif %}
     )
 
