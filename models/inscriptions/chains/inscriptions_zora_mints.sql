c3edb5e21c780bf102b2519fc1db5cb6792cd0c7
Hildebert Moulié <h.moulie@gmail.com>
2023-12-04 18:50:29 +0100

    fix

--- models/inscriptions/chains/inscriptions_zora_mints.sql
+++ models/inscriptions/chains/inscriptions_zora_mints.sql
@@ -13,6 +13,6 @@
 {{inscriptions_mints(
         blockchain='zora'
         , transactions = source('zora','transactions')
-        , first_inscription_block = 
+        , first_inscription_block = 2137565
 )}}
--- First inscription block is , : 
+-- First inscription block is 2137565, 2023-08-02 09:36: https://dune.com/queries/3254037
