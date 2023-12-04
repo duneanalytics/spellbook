5c522c2bbf47b4a434dc5be0994cee9c007263d0
Hildebert Moulié <h.moulie@gmail.com>
2023-12-04 20:27:26 +0100

    fix

--- models/dex/chains/mev/sandwiches/dex_fantom_sandwiched.sql
+++ models/dex/chains/mev/sandwiches/dex_fantom_sandwiched.sql
@@ -8,8 +8,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain', 'tx_hash', 'evt_index'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
+        unique_key = ['blockchain', 'tx_hash', 'evt_index']
 )
 }}
 
