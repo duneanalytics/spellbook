fb4b07350a8ff045de4a1866ab059614c168df65
hildobby <h.moulie@gmail.com>
2023-12-05 19:47:32 +0100

    Create `dex_sandwiched` (#4936)
    * create dex_sandwiched
    
    * fixes
    
    * fix
    
    * check that it`s the same dex pool as sandwich attack
    
    * change incremental predicate, match unique keys and use alias for blockchain
    
    * fix
    
    * rm project_contract_address, fix incremental, add incremental_predicates back
    
    * fixes
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/dex/chains/dex_arbitrum_sandwiches.sql
+++ models/dex/chains/dex_arbitrum_sandwiches.sql
@@ -6,7 +6,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain','tx_hash', 'project_contract_address', 'evt_index']
+        unique_key = ['blockchain', 'tx_hash', 'project_contract_address', 'evt_index']
 )
 }}
 
