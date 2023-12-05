efa8c4c336a13867fa3982d3fe7a1a29e1a96a12
hildobby <h.moulie@gmail.com>
2023-12-05 20:10:36 +0100

    some dex_sandwiches optisations (#4943)

--- models/dex/chains/mev/sandwiches/dex_bnb_sandwiches.sql
+++ models/dex/chains/mev/sandwiches/dex_bnb_sandwiches.sql
@@ -1,16 +1,18 @@
+{% set blockchain = 'bnb' %}
+
 {{ config(
         
-        schema = 'dex_base',
+        schema = 'dex_' + blockchain,
         alias = 'sandwiches',
         partition_by = ['block_month'],
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain', 'tx_hash', 'project_contract_address', 'evt_index']
+        unique_key = ['blockchain', 'tx_hash', 'evt_index']
 )
 }}
 
 {{dex_sandwiches(
-        blockchain='base'
-        , transactions = source('base','transactions')
+        blockchain = blockchain
+        , transactions = source(blockchain,'transactions')
 )}}
