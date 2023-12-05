da8ac73caaaef055f8a891bec99d89f2afa6a5b5
grkhr <eduardgorkh@gmail.com>
2023-12-06 01:37:03 +0200

    tx

--- models/oneinch/oneinch_ar_trades.sql
+++ models/oneinch/oneinch_ar_trades.sql
@@ -143,14 +143,12 @@ from (
         , tx_from
         , tx_to
         , contract_address
-        , call_from
-        , call_to
         , minute
     from {{ ref('oneinch_calls_transfers_amounts') }}
     {% if is_incremental() %}
         where {{ incremental_predicate('block_time') }}
     {% endif %}
-    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    group by 1, 2, 3, 4, 5, 6, 7, 8
 
 )
 where blockchain = 'base'
