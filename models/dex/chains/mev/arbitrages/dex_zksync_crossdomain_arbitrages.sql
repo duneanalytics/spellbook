a29f3bbfd3a95045d1fc8a4bea7519a42925f0f1
Hildebert Moulié <h.moulie@gmail.com>
2023-12-05 23:07:13 +0100

    fix

---
+++ models/dex/chains/mev/arbitrages/dex_zksync_crossdomain_arbitrages.sql
@@ -0,0 +1,20 @@
+{% set blockchain = 'zksync' %}
+
+{{ config(
+        schema = 'dex_' + blockchain,
+        alias = 'crossdomain_arbitrages',
+        partition_by = ['block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['blockchain', 'tx_hash', 'project_contract_address', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+        )
+}}
+
+{{dex_crossdomain_arbitrages(
+        blockchain=blockchain
+        , blocks = source(blockchain,'blocks')
+        , traces = source(blockchain,'traces')
+        , transactions = source(blockchain,'transactions')
+)}}
