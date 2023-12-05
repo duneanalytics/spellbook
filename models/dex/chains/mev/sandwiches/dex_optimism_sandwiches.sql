7d8ae2a5b8cb2b6e79adeb0f8872f47b50942583
hildobby <h.moulie@gmail.com>
2023-12-05 20:10:36 +0100

    some dex_sandwiches optisations (#4943)

--- models/dex/chains/mev/sandwiches/dex_optimism_sandwiches.sql
+++ models/dex/chains/mev/sandwiches/dex_optimism_sandwiches.sql
@@ -1,16 +1,18 @@
+{% set blockchain = 'optimism' %}
+
 {{ config(
         
-        schema = 'dex_arbitrum',
+        schema = 'dex_' + blockchain,
         alias = 'sandwiches',
         partition_by = ['block_month'],
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain', 'tx_hash', 'project_contract_address', 'evt_index']
+        unique_key = ['blockchain', 'tx_hash', 'evt_index']
 )
 }}
 
 {{dex_sandwiches(
-        blockchain='arbitrum'
-        , transactions = source('arbitrum','transactions')
+        blockchain = blockchain
+        , transactions = source(blockchain,'transactions')
 )}}
