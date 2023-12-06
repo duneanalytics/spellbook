ccccbbdfca4754f2fecda270073430cf46e89e76
kaiblade <kaibladenet@gmail.com>
2023-12-06 17:04:30 +0100

    Added a nft label model

--- models/labels/addresses/nft/persona/op_nft_minters/labels_op_nft_minters.sql
+++ models/labels/addresses/nft/persona/op_nft_minters/labels_op_nft_minters.sql
@@ -7,7 +7,7 @@ WITH erc721_mints AS
 SELECT "from" AS minter, COUNT("from") AS mint_count
 FROM (SELECT nft.evt_tx_hash, tx."from"
 FROM {{ source('erc721_optimism', 'evt_Transfer') }}
-JOIN optimism.transactions tx
+JOIN {{ source('optimism', 'transactions') }} tx
 ON nft.evt_tx_hash = tx.hash
 WHERE nft."from" = 0x0000000000000000000000000000000000000000
 )
