9e25c5889938dcb29e27f61a772d4799c447e68b
viniabussafi <vinicius@balancerlabs.dev>
2023-12-06 16:01:21 +0100

    fix post_hook

--- models/balancer/base/balancer_v2_base_liquidity.sql
+++ models/balancer/base/balancer_v2_base_liquidity.sql
@@ -7,10 +7,10 @@
         alias = 'liquidity',
         materialized = 'table',
         file_format = 'delta',
-        post_hook='expose_spells(\'["{{blockchain}}"]\',
+        post_hook='{{ expose_spells(\'["{{blockchain}}"]\',
                         "project",
                         "balancer_v2",
-                        \'["stefenon", "viniabussafi"]\')'
+                        \'["stefenon", "viniabussafi"]\') }}'
     )
 }}
 
