ef23f48a19536aceb455c7fc01f732c80f2b7256
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-12-06 17:27:21 -0500

    add block_slot instead

--- models/tokens/solana/tokens_solana_transfers.sql
+++ models/tokens/solana/tokens_solana_transfers.sql
@@ -6,7 +6,7 @@
         file_format = 'delta',
         incremental_strategy = 'merge',
         incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-        unique_key = ['tx_id','outer_instruction_index','inner_instruction_index','block_time'],
+        unique_key = ['tx_id','outer_instruction_index','inner_instruction_index', 'block_slot'],
         post_hook='{{ expose_spells(\'["solana"]\',
                                     "sector",
                                     "tokens",
@@ -15,6 +15,7 @@
 
 SELECT
     call_block_time as block_time
+    , call_block_slot as block_slot
     , action
     , amount
     , COALESCE(tk_s.token_mint_address, tk_d.token_mint_address) as token_mint_address
@@ -28,32 +29,32 @@ SELECT
     , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
     , call_outer_executing_account as outer_executing_account
 FROM (  
-      SELECT account_source, account_destination, amount, call_tx_id, call_block_time, call_outer_executing_account, call_tx_signer, 'transfer' as action, call_outer_instruction_index, call_inner_instruction_index
+      SELECT account_source, account_destination, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'transfer' as action, call_outer_instruction_index, call_inner_instruction_index
       FROM {{ source('spl_token_solana','spl_token_call_transfer') }}
 
       UNION ALL
 
-      SELECT account_source, account_destination, amount, call_tx_id, call_block_time, call_outer_executing_account, call_tx_signer, 'transfer' as action, call_outer_instruction_index, call_inner_instruction_index
+      SELECT account_source, account_destination, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'transfer' as action, call_outer_instruction_index, call_inner_instruction_index
       FROM {{ source('spl_token_solana','spl_token_call_transferChecked') }}
 
       UNION ALL
 
-      SELECT null, account_account as account_destination, amount, call_tx_id, call_block_time, call_outer_executing_account, call_tx_signer, 'mint' as action, call_outer_instruction_index, call_inner_instruction_index
+      SELECT null, account_account as account_destination, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'mint' as action, call_outer_instruction_index, call_inner_instruction_index
       FROM {{ source('spl_token_solana','spl_token_call_mintTo') }}
 
       UNION ALL
 
-      SELECT null, account_account as account_destination, amount, call_tx_id, call_block_time, call_outer_executing_account, call_tx_signer, 'mint' as action, call_outer_instruction_index, call_inner_instruction_index
+      SELECT null, account_account as account_destination, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'mint' as action, call_outer_instruction_index, call_inner_instruction_index
       FROM {{ source('spl_token_solana','spl_token_call_mintToChecked') }}
 
       UNION ALL
 
-      SELECT account_account as account_source, null, amount, call_tx_id, call_block_time, call_outer_executing_account, call_tx_signer, 'burn' as action, call_outer_instruction_index, call_inner_instruction_index
+      SELECT account_account as account_source, null, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'burn' as action, call_outer_instruction_index, call_inner_instruction_index
       FROM {{ source('spl_token_solana','spl_token_call_burn') }}
 
       UNION ALL
 
-      SELECT account_account as account_source, null, amount, call_tx_id, call_block_time, call_outer_executing_account, call_tx_signer, 'burn' as action, call_outer_instruction_index, call_inner_instruction_index
+      SELECT account_account as account_source, null, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'burn' as action, call_outer_instruction_index, call_inner_instruction_index
       FROM {{ source('spl_token_solana','spl_token_call_burnChecked') }}
 ) tr
 --get token and accounts
@@ -70,6 +71,7 @@ UNION ALL
 --for the reader, note that SOL is special and can be transferred without calling the transfer instruction. It is also minted and burned without instructions. So to get balances, use daily_balances or account_activity instead of transfers.
 SELECT
     call_block_time as block_time
+    , call_block_slot as block_slot
     , 'transfer' as action
     , lamports as amount --1e9
     , 'native' as token_mint_address
@@ -83,7 +85,7 @@ SELECT
     , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
     , call_outer_executing_account as outer_executing_account
 FROM (
-      SELECT account_from, account_to, lamports, call_tx_signer, call_block_time, call_tx_id, call_outer_instruction_index, call_inner_instruction_index, call_outer_executing_account
+      SELECT account_from, account_to, lamports, call_tx_signer, call_block_time, call_block_slot, call_tx_id, call_outer_instruction_index, call_inner_instruction_index, call_outer_executing_account
       FROM {{ source('system_program_solana','system_program_call_Transfer') }}
       WHERE 1=1
       {% if is_incremental() %}
@@ -92,7 +94,7 @@ FROM (
 
       UNION ALL 
       
-      SELECT account_funding_account, account_recipient_account, lamports, call_tx_signer, call_block_time, call_tx_id, call_outer_instruction_index, call_inner_instruction_index, call_outer_executing_account
+      SELECT account_funding_account, account_recipient_account, lamports, call_tx_signer, call_block_time, call_block_slot, call_tx_id, call_outer_instruction_index, call_inner_instruction_index, call_outer_executing_account
       FROM {{ source('system_program_solana','system_program_call_TransferWithSeed') }}
       WHERE 1=1
       {% if is_incremental() %}
