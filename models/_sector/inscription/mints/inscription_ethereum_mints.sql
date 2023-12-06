0badaa11b00a152a60e7d9d164f11226468b5420
Hildebert Moulié <h.moulie@gmail.com>
2023-12-06 18:23:05 +0100

    make chain-specific deploys/mints models views

--- models/_sector/inscription/mints/inscription_ethereum_mints.sql
+++ models/_sector/inscription/mints/inscription_ethereum_mints.sql
@@ -4,9 +4,6 @@
         
         schema = 'inscription_' + blockchain,
         alias = 'mints',
-        materialized = 'incremental',
-        file_format = 'delta',
-        incremental_strategy = 'merge',
         unique_key = ['blockchain', 'tx_hash']
         )
 }}
