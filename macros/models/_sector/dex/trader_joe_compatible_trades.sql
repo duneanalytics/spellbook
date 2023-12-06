70ef0cc3464c09efe62f8508e4d9954d9508984f
chef-seaweed <121569855+chef-seaweed@users.noreply.github.com>
2023-12-06 11:17:58 +0800

    Add Trader Joe v2 macro

--- macros/models/_sector/dex/trader_joe_compatible_trades.sql
+++ macros/models/_sector/dex/trader_joe_compatible_trades.sql
@@ -1,3 +1,58 @@
+{% macro trader_joe_compatible_v2_trades(
+    blockchain = null
+    , project = null
+    , version = null
+    , Pair_evt_Swap = null
+    , Factory_evt_PoolCreated = null
+    , pair_column_name = 'LBPair'
+    )
+%}
+WITH dexs AS
+(
+    SELECT
+        t.evt_block_number AS block_number
+        , t.evt_block_time AS block_time
+        , t.to AS taker
+        , t.contract_address AS maker
+        , amountOut AS token_bought_amount_raw
+        , amountIn AS token_sold_amount_raw
+        , CASE WHEN swapForY = true THEN f.tokenY ELSE f.tokenX END AS token_bought_address -- when swapForY is true it means that tokenY is the token being bought else it's tokenX
+        , CASE WHEN swapForY = true THEN f.tokenX ELSE f.tokenY END AS token_sold_address
+        , t.contract_address AS project_contract_address
+        , t.evt_tx_hash AS tx_hash
+        , t.evt_index AS evt_index
+    FROM
+        {{ Pair_evt_Swap }} t
+    INNER JOIN
+        {{ Factory_evt_PairCreated }} f
+        ON f.{{ pair_column_name }} = t.contract_address
+    {% if is_incremental() %}
+    WHERE
+        {{ incremental_predicate('t.evt_block_time') }}
+    {% endif %}
+)
+
+SELECT
+    '{{ blockchain }}' AS blockchain
+    , '{{ project }}' AS project
+    , '{{ version }}' AS version
+    , CAST(date_trunc('month', dexs.block_time) AS date) AS block_month
+    , CAST(date_trunc('day', dexs.block_time) AS date) AS block_date
+    , dexs.block_time
+    , dexs.block_number
+    , dexs.token_bought_amount_raw
+    , dexs.token_sold_amount_raw
+    , dexs.token_bought_address
+    , dexs.token_sold_address
+    , dexs.taker
+    , dexs.maker
+    , dexs.project_contract_address
+    , dexs.tx_hash
+    , dexs.evt_index
+FROM
+    dexs
+{% endmacro %}
+
 {% macro trader_joe_compatible_v2_1_trades(
     blockchain = null
     , project = null
--- models/_sector/dex/trades/avalanche_c/platforms/trader_joe_v2_avalanche_c_base_trades.sql
+++ models/_sector/dex/trades/avalanche_c/platforms/trader_joe_v2_avalanche_c_base_trades.sql
@@ -9,45 +9,12 @@
     )
 }}
 
-WITH dexs AS
-(
-    SELECT
-        t.evt_block_time AS block_time
-        ,t.evt_block_number AS block_number
-        ,t.recipient AS taker
-        ,CAST(NULL AS VARBINARY) AS maker
-        ,amountOut AS token_bought_amount_raw
-        ,amountIn AS token_sold_amount_raw
-        ,CASE WHEN swapForY = true THEN f.tokenY ELSE f.tokenX END AS token_bought_address -- when swapForY is true it means that tokenY is the token being bought else it's tokenX
-        ,CASE WHEN swapForY = true THEN f.tokenX ELSE f.tokenY END AS token_sold_address
-        ,t.contract_address AS project_contract_address
-        ,t.evt_tx_hash AS tx_hash
-        ,t.evt_index
-    FROM
-        {{ source('trader_joe_avalanche_c', 'LBPair_evt_Swap') }} t
-    INNER JOIN {{ source('trader_joe_avalanche_c', 'LBFactory_evt_LBPairCreated') }} f
-        ON f.LBPair = t.contract_address
-    {% if is_incremental() %}  -- comment to accomodate additions to prices.usd and force full reload
-    WHERE {{ incremental_predicate('t.evt_block_time') }}
-    {% endif %}
-)
-
-SELECT
-    'avalanche_c' AS blockchain
-    ,'trader_joe' AS project
-    ,'2' AS version
-    , CAST(date_trunc('month', dexs.block_time) AS date) AS block_month
-    , CAST(date_trunc('day', dexs.block_time) AS date) AS block_date
-    , dexs.block_time
-    , dexs.block_number
-    , dexs.token_bought_amount_raw
-    , dexs.token_sold_amount_raw
-    , dexs.token_bought_address
-    , dexs.token_sold_address
-    , dexs.taker
-    , dexs.maker
-    , dexs.project_contract_address
-    , dexs.tx_hash
-    , dexs.evt_index
-FROM
-    dexs
+{{
+    trader_joe_compatible_v2_trades(
+        blockchain = 'avalanche_c'
+        , project = 'trader_joe'
+        , version = '2'
+        , Pair_evt_Swap = source('trader_joe_avalanche_c', 'LBPair_evt_Swap')
+        , Factory_evt_PoolCreated = source('trader_joe_avalanche_c', 'LBFactory_evt_LBPairCreated')
+    )
+}}
