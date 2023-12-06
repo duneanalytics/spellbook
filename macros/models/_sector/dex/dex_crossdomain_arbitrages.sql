9cad757fc8cc807c988ea6784470a67ea829810a
Hildebert Moulié <h.moulie@gmail.com>
2023-12-06 23:28:47 +0100

    fix

--- macros/models/_sector/dex/dex_crossdomain_arbitrages.sql
+++ macros/models/_sector/dex/dex_crossdomain_arbitrages.sql
@@ -96,7 +96,7 @@ SELECT dt.blockchain
 FROM {{ ref('dex_trades')}} dt
 INNER JOIN distinct_transactions i ON dt.block_time=i.block_time
     AND i.tx_from=dt.tx_from
-    AND i.tx_from=txs."from"
+    AND i.tx_from=dt.tx_from
     AND i.tx_index IN (txs.index-1, txs.index)
 INNER JOIN {{transactions}} txs ON txs.block_time=dt.block_time
     AND txs.hash=dt.tx_hash
