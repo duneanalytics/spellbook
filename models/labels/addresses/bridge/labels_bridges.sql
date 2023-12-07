a67e611eb2233aa9da5359e62b25d241573b1b6e
kaiblade <kaibladenet@gmail.com>
2023-12-07 17:38:26 +0100

    Adding finishing touches

--- models/labels/addresses/bridge/labels_bridges.sql
+++ models/labels/addresses/bridge/labels_bridges.sql
@@ -17,6 +17,8 @@
  , ref('labels_bridges_arbitrum')
  , ref('labels_bridges_polygon')
  , ref('labels_bridges_optimism')
+ , ref('labels_op_bridge_derived_archetype')
+ , ref('labels_op_bridge_users')
  
 ] %}
 
