29eb2618617607c701a73a620657002e24493357
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-12-07 09:52:02 -0500

    remove native SOL transfers

--- models/tokens/solana/tokens_solana_transfers.sql
+++ models/tokens/solana/tokens_solana_transfers.sql
@@ -68,40 +68,41 @@ AND {{incremental_predicate('call_block_time')}}
 {% endif %}
 -- WHERE call_block_time > now() - interval '30' day
 
-UNION ALL 
+-- -- adding in SOL/native transfers seems to blow out the CI
+-- UNION ALL 
 
---for the reader, note that SOL is special and can be transferred without calling the transfer instruction. It is also minted and burned without instructions. So to get balances, use daily_balances or account_activity instead of transfers.
-SELECT
-    call_block_time as block_time
-    , date_trunc('day', call_block_time) as block_date
-    , call_block_slot as block_slot
-    , 'transfer' as action
-    , lamports as amount --1e9
-    , 'native' as token_mint_address
-    , account_from as from_owner
-    , account_to as to_owner
-    , account_from as from_token_account
-    , account_to as to_token_account
-    , call_tx_signer as tx_signer
-    , call_tx_id as tx_id
-    , call_outer_instruction_index as outer_instruction_index
-    , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
-    , call_outer_executing_account as outer_executing_account
-FROM (
-      SELECT account_from, account_to, lamports, call_tx_signer, call_block_time, call_block_slot, call_tx_id, call_outer_instruction_index, call_inner_instruction_index, call_outer_executing_account
-      FROM {{ source('system_program_solana','system_program_call_Transfer') }}
-      WHERE 1=1
-      {% if is_incremental() %}
-      AND {{incremental_predicate('call_block_time')}}
-      {% endif %}
+-- --for the reader, note that SOL is special and can be transferred without calling the transfer instruction. It is also minted and burned without instructions. So to get balances, use daily_balances or account_activity instead of transfers.
+-- SELECT
+--     call_block_time as block_time
+--     , date_trunc('day', call_block_time) as block_date
+--     , call_block_slot as block_slot
+--     , 'transfer' as action
+--     , lamports as amount --1e9
+--     , 'native' as token_mint_address
+--     , account_from as from_owner
+--     , account_to as to_owner
+--     , account_from as from_token_account
+--     , account_to as to_token_account
+--     , call_tx_signer as tx_signer
+--     , call_tx_id as tx_id
+--     , call_outer_instruction_index as outer_instruction_index
+--     , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
+--     , call_outer_executing_account as outer_executing_account
+-- FROM (
+--       SELECT account_from, account_to, lamports, call_tx_signer, call_block_time, call_block_slot, call_tx_id, call_outer_instruction_index, call_inner_instruction_index, call_outer_executing_account
+--       FROM {{ source('system_program_solana','system_program_call_Transfer') }}
+--       WHERE 1=1
+--       {% if is_incremental() %}
+--       AND {{incremental_predicate('call_block_time')}}
+--       {% endif %}
 
-      UNION ALL 
+--       UNION ALL 
       
-      SELECT account_funding_account, account_recipient_account, lamports, call_tx_signer, call_block_time, call_block_slot, call_tx_id, call_outer_instruction_index, call_inner_instruction_index, call_outer_executing_account
-      FROM {{ source('system_program_solana','system_program_call_TransferWithSeed') }}
-      WHERE 1=1
-      {% if is_incremental() %}
-      AND {{incremental_predicate('call_block_time')}}
-      {% endif %}
-)
+--       SELECT account_funding_account, account_recipient_account, lamports, call_tx_signer, call_block_time, call_block_slot, call_tx_id, call_outer_instruction_index, call_inner_instruction_index, call_outer_executing_account
+--       FROM {{ source('system_program_solana','system_program_call_TransferWithSeed') }}
+--       WHERE 1=1
+--       {% if is_incremental() %}
+--       AND {{incremental_predicate('call_block_time')}}
+--       {% endif %}
+-- )
 -- WHERE call_block_time > now() - interval '30' day
