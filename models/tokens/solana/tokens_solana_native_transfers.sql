d7ceadaf2b8db06b78e81580b97e47ea2cebba8b
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-12-08 01:36:18 -0500

    try 600

--- models/tokens/solana/tokens_solana_native_transfers.sql
+++ models/tokens/solana/tokens_solana_native_transfers.sql
@@ -44,4 +44,4 @@ WHERE 1=1
 {% if is_incremental() %}
 AND {{incremental_predicate('call_block_time')}}
 {% endif %}
-AND call_block_time > now() - interval '300' day
+AND call_block_time > now() - interval '600' day
