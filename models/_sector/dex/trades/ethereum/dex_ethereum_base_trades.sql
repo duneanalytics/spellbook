57740c638c9cbe523b401aa748575b250038914c
tomfutago <tomfutago@gmail.com>
2023-12-10 13:54:31 +0000

    init commit

--- models/_sector/dex/trades/ethereum/dex_ethereum_base_trades.sql
+++ models/_sector/dex/trades/ethereum/dex_ethereum_base_trades.sql
@@ -14,6 +14,7 @@
     , ref('airswap_ethereum_base_trades')
     , ref('sushiswap_v1_ethereum_base_trades')
     , ref('sushiswap_v2_ethereum_base_trades')
+    , ref('bancor_ethereum_base_trades')
 ] %}
 
 WITH base_union AS (
