382d5a53c522457e5e0a9a2bab0e7c1c50ec3fcf
jeff-dude <jeff@dune.xyz>
2023-12-13 17:35:12 -0500

    fix unique keys

--- models/_sector/tokens/tokens_transfers.sql
+++ models/_sector/tokens/tokens_transfers.sql
@@ -6,7 +6,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-    unique_key = ['unique_key'],
+    unique_key = ['unique_key', 'blockchain'],
     post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "bnb", "celo", "ethereum", "fantom", "gnosis", "optimism", "polygon", "zksync", "zora"]\',
                                 "sector",
                                 "tokens",
