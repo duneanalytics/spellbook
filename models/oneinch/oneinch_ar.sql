3908adbc5f0f9431ba4c61c79453c101a870cbe1
Eduard Gorkh <40689054+grkhr@users.noreply.github.com>
2023-12-13 23:19:26 +0200

    1inch: LOP + AR macros updates (2/2; +ethereum&polygon) (#4979)
    * rework LOP + AR macro
    
    * disable zksync
    
    * explicit
    
    * revert dates
    
    * upd
    
    * easy dates
    
    * qqq
    
    * fix
    
    * fikx
    
    * break
    
    * Revert "break"
    
    This reverts commit b134a114695979194b9265dec1b0d5576f3945ef.
    
    * retrigger ci

--- models/oneinch/oneinch_ar.sql
+++ models/oneinch/oneinch_ar.sql
@@ -34,27 +34,35 @@
         , tx_from
         , tx_to
         , tx_success
+        , tx_nonce
+        , gas_price
+        , priority_fee_per_gas
         , contract_name
+        , protocol
         , protocol_version
         , method
+        , call_selector
+        , call_trace_address
         , call_from
         , call_to
-        , call_trace_address
-        , call_selector
-        , src_token_address
-        , dst_token_address
+        , call_success
+        , call_gas_used
+        , call_output
+        , call_error
         , src_receiver
         , dst_receiver
-        , src_amount
-        , dst_amount
-        , dst_amount_min
+        , src_token_address
+        , dst_token_address
+        , src_amount -- will be removed soon
+        , dst_amount -- will be removed soon
+        , dst_amount_min -- will be removed soon
+        , src_amount as src_token_amount
+        , dst_amount as dst_token_amount
+        , dst_amount_min as dst_token_amount_min
         , ordinary
         , pools
         , router_type
-        , call_success
-        , call_gas_used
         , remains
-        , call_output
         , minute
         , block_month
     from {{ ref('oneinch_' + blockchain + '_ar') }}
