c05f990e699028240e41b0feef00ca37a5486850
Alice <110170331+alicecha@users.noreply.github.com>
2023-12-14 11:02:38 +0000

    Update safe_zksync_transactions.sql
    removed duplicated setting

--- models/safe/zksync/safe_zksync_transactions.sql
+++ models/safe/zksync/safe_zksync_transactions.sql
@@ -4,7 +4,6 @@
         schema = 'safe_zksync',
         alias = 'transactions',
         partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'], 
         file_format ='delta',
         incremental_strategy='merge',
         unique_key = ['unique_key'],
