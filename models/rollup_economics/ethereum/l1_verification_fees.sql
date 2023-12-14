0096415587efb78c38350920bccfedb38eaaba1b
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-12-13 19:37:44 -0500

    add for extra rollups

--- models/rollup_economics/ethereum/l1_verification_fees.sql
+++ models/rollup_economics/ethereum/l1_verification_fees.sql
@@ -93,7 +93,9 @@ with verify_txns as (
     t.hash,
     (cast(gas_used as double) * (cast(gas_price as double) / 1e18)) as gas_spent,
     p.price * (cast(gas_used as double) * (cast(gas_price as double) / 1e18)) as gas_spent_usd,
-    768 / cast(1024 AS double) / cast(1024 AS double) AS proof_size_mb
+    768 / cast(1024 AS double) / cast(1024 AS double) AS proof_size_mb,
+    t.gas_used,
+    {{ evm_get_calldata_gas_from_data('t.data') }} AS calldata_gas_used
     FROM {{ source('ethereum','transactions') }} AS t
     INNER JOIN {{ source('prices','usd') }} p
       ON p.minute = date_trunc('minute', t.block_time)
