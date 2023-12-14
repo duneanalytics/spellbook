447611e5fe1a696b5a9d5f348c35ae419114779e
jeff-dude <jeff@dune.xyz>
2023-12-14 10:55:56 -0500

    reorder configs, update for loop approach

--- models/_sector/tokens/tokens_transfers.sql
+++ models/_sector/tokens/tokens_transfers.sql
@@ -1,12 +1,12 @@
 {{config(
     schema = 'tokens',
     alias = 'transfers',
-    partition_by = ['token_standard', 'block_date', 'blockchain'],
+    partition_by = ['blockchain', 'token_standard', 'block_date'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-    unique_key = ['unique_key', 'blockchain'],
+    unique_key = ['blockchain', 'unique_key'],
     post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "bnb", "celo", "ethereum", "fantom", "gnosis", "optimism", "polygon", "zksync", "zora"]\',
                                 "sector",
                                 "tokens",
@@ -14,28 +14,26 @@
 )
 }}
 
-{% 
-    set blockchains = [
-        'arbitrum'
-        ,'avalanche_c'
-        ,'base'
-        ,'bnb'
-        ,'celo'
-        ,'ethereum'
-        ,'fantom'
-        ,'gnosis'
-        ,'optimism'
-        ,'polygon'
-        ,'zksync'
-        ,'zora'
-    ]
-%}
+{% set models = [
+    ref('tokens_arbitrum_transfers')
+    , {{ ref('tokens_avalanche_c_transfers') }}
+    , {{ ref('tokens_base_transfers') }}
+    , {{ ref('tokens_bnb_transfers') }}
+    , {{ ref('tokens_celo_transfers') }}
+    , {{ ref('tokens_ethereum_transfers') }}
+    , {{ ref('tokens_fantom_transfers') }}
+    , {{ ref('tokens_gnosis_transfers') }}
+    , {{ ref('tokens_optimism_transfers') }}
+    , {{ ref('tokens_polygon_transfers') }}
+    , {{ ref('tokens_zksync_transfers') }}
+    , {{ ref('tokens_zora_transfers') }}
+] %}
 
 with base_union as (
     SELECT *
     FROM
     (
-        {% for blockchain in blockchains %}
+        {% for model in models %}
         SELECT
             unique_key
             , blockchain
@@ -58,7 +56,7 @@ with base_union as (
             , usd_price
             , usd_amount
         FROM
-            {{ ref('tokens_' + blockchain + '_transfers') }}
+            {{ model }}
         {% if is_incremental() %}
         WHERE
             {{ incremental_predicate('block_time') }}
