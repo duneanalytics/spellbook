b452c1ed1f0eb841cfe6016987aa80bd06cba0a1
grkhr <eduardgorkh@gmail.com>
2023-12-16 00:58:38 +0200

    schema stuff

---
+++ models/oneinch/oneinch_call_transfers.sql
@@ -0,0 +1,32 @@
+{{  
+    config(
+        schema = 'oneinch',
+        alias = 'call_transfers',
+        materialized = 'view',
+        unique_key = ['blockchain', 'tx_hash', 'call_trace_address', 'transfer_trace_address'],
+        
+    )
+}}
+
+
+
+{% 
+    set blockchains = [
+        'arbitrum',
+        'avalanche_c',
+        'base',
+        'bnb',
+        'ethereum',
+        'fantom',
+        'gnosis',
+        'optimism',
+        'polygon'
+    ]
+%}
+
+
+
+{% for blockchain in blockchains %}
+    select * from {{ ref('oneinch_' + blockchain + '_call_transfers') }}
+    {% if not loop.last %} union all {% endif %}
+{% endfor %}
