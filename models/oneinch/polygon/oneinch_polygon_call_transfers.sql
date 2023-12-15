c3cc97df09f0740a2231103b678dcf9d6cd834fe
grkhr <eduardgorkh@gmail.com>
2023-12-15 16:47:01 +0200

    try all

---
+++ models/oneinch/polygon/oneinch_polygon_call_transfers.sql
@@ -0,0 +1,24 @@
+{% set blockchain = 'polygon' %}
+
+
+
+{{ 
+    config( 
+        tags=['prod_exclude'],
+        schema = 'oneinch_' + blockchain,
+        alias = 'call_transfers',
+        partition_by = ['block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['blockchain', 'tx_hash', 'call_trace_address', 'transfer_trace_address']
+    )
+}}
+
+
+
+{{ 
+    oneinch_call_transfers_macro(
+        blockchain = blockchain
+    )
+}}
