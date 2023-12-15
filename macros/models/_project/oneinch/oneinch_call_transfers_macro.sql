385541863b602295eeab7d2e6eab836bb76cf8f6
grkhr <eduardgorkh@gmail.com>
2023-12-16 01:21:24 +0200

    try

--- macros/models/_project/oneinch/oneinch_call_transfers_macro.sql
+++ macros/models/_project/oneinch/oneinch_call_transfers_macro.sql
@@ -12,7 +12,9 @@ meta as (
     select 
         wrapped_native_token_address
         , first_deploy_at
-    from {{ ref('oneinch_' + blockchain + '_blockchain') }}
+    -- from {{ ref('oneinch_' + blockchain + '_blockchain') }}
+    from {{ref('oneinch_blockchains')}}
+    where blockchain = '{{ blockchain }}'
 )
 
 , calls as (
