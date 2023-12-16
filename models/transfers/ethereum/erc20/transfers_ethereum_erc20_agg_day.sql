60fe596fa68894dcd90bba312d2822807b825677
Henrystats <65398020+henrystats@users.noreply.github.com>
2023-12-16 21:06:00 +0100

    update base tables

--- models/transfers/ethereum/erc20/transfers_ethereum_erc20_agg_day.sql
+++ models/transfers/ethereum/erc20/transfers_ethereum_erc20_agg_day.sql
@@ -1,29 +1,19 @@
 {{ config(
-tags=['prod_exclude'],
+        
         alias = 'erc20_agg_day',
+        partition_by = ['block_month'],
         materialized ='incremental',
         file_format ='delta',
         incremental_strategy='merge',
-        unique_key='unique_transfer_id'
+        unique_key = ['block_day', 'wallet_address', 'token_address']
         )
 }}
-/*
-    note: this spell has not been migrated to dunesql, therefore is only a view on spark
-        please migrate to dunesql to ensure up-to-date logic & data
-*/
-select
-    'ethereum' as blockchain,
-    date_trunc('day', tr.evt_block_time) as day,
-    tr.wallet_address,
-    tr.token_address,
-    t.symbol,
-    tr.wallet_address || '-' || tr.token_address || '-' || date_trunc('day', tr.evt_block_time) as unique_transfer_id,
-    sum(tr.amount_raw) as amount_raw,
-    sum(tr.amount_raw / power(10, t.decimals)) as amount
-from {{ ref('transfers_ethereum_erc20') }} tr
-left join {{ ref('tokens_ethereum_erc20') }} t on t.contract_address = tr.token_address
-{% if is_incremental() %}
--- this filter will only be applied on an incremental run
-where tr.evt_block_time >= date_trunc('day', now() - interval '1 week')
-{% endif %}
-group by 1, 2, 3, 4, 5, 6
+
+
+{{
+    transfers_erc20_agg_day(
+        transfers_erc20 = ref('transfers_ethereum_erc20'),
+        tokens_erc20 = ref('tokens_ethereum_erc20')
+    )
+}}
+
