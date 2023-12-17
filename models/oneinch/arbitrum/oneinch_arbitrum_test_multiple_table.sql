d4b6862b9e4163c5f1f35753832273a494932a2e
grkhr <eduardgorkh@gmail.com>
2023-12-17 20:22:04 +0200

    perf test

---
+++ models/oneinch/arbitrum/oneinch_arbitrum_test_multiple_table.sql
@@ -0,0 +1,24 @@
+{% set blockchain = 'arbitrum' %}
+
+
+
+{{ 
+    config( 
+        schema = 'oneinch_' + blockchain,
+        alias = 'test_multiple_table',
+        partition_by = ['block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'call_trace_address', 'transfer_trace_address']
+    )
+}}
+
+
+
+{{ 
+    oneinch_call_transfers_macro(
+        blockchain = blockchain
+        , blockchain_meta = ref('oneinch_blockchains_table')
+    )
+}}
