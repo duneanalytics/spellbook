b1b9e9e90f769c4191bf25224124566b2c544353
lorenz234 <lorenz_lehmann@yahoo.de>
2023-12-17 16:16:10 +0100

    quick fix

--- models/rollup_economics/ethereum/l1_verification_fees.sql
+++ models/rollup_economics/ethereum/l1_verification_fees.sql
@@ -72,7 +72,9 @@ with verify_txns as (
     t.hash,
     (cast(gas_used as double) * (cast(gas_price as double) / 1e18)) as gas_spent,
     p.price * (cast(gas_used as double) * (cast(gas_price as double) / 1e18)) as gas_spent_usd,
-    768 / cast(1024 AS double) / cast(1024 AS double) AS proof_size_mb
+    652 / cast(1024 AS double) / cast(1024 AS double) AS proof_size_mb,
+    t.gas_used,
+    {{ evm_get_calldata_gas_from_data('t.data') }} AS calldata_gas_used
     FROM {{ source('ethereum','transactions') }} AS t
     INNER JOIN {{ source('prices','usd') }} p
       ON p.minute = date_trunc('minute', t.block_time)
@@ -91,7 +93,9 @@ with verify_txns as (
     t.hash,
     (cast(gas_used as double) * (cast(gas_price as double) / 1e18)) as gas_spent,
     p.price * (cast(gas_used as double) * (cast(gas_price as double) / 1e18)) as gas_spent_usd,
-    768 / cast(1024 AS double) / cast(1024 AS double) AS proof_size_mb
+    3440 / cast(1024 AS double) / cast(1024 AS double) AS proof_size_mb,
+    t.gas_used,
+    {{ evm_get_calldata_gas_from_data('t.data') }} AS calldata_gas_used
     FROM {{ source('ethereum','transactions') }} AS t
     INNER JOIN {{ source('prices','usd') }} p
       ON p.minute = date_trunc('minute', t.block_time)
@@ -110,7 +114,9 @@ with verify_txns as (
     t.hash,
     (cast(gas_used as double) * (cast(gas_price as double) / 1e18)) as gas_spent,
     p.price * (cast(gas_used as double) * (cast(gas_price as double) / 1e18)) as gas_spent_usd,
-    768 / cast(1024 AS double) / cast(1024 AS double) AS proof_size_mb -- change me once mantle proofs are live
+    768 / cast(1024 AS double) / cast(1024 AS double) AS proof_size_mb,  -- change me once mantle proofs are live
+    t.gas_used,
+    {{ evm_get_calldata_gas_from_data('t.data') }} AS calldata_gas_used
     FROM {{ source('ethereum','transactions') }} AS t
     INNER JOIN {{ source('prices','usd') }} p
       ON p.minute = date_trunc('minute', t.block_time)
