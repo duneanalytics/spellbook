9ede2de457f957c5228550f3c788cea6806e11f3
grkhr <eduardgorkh@gmail.com>
2023-12-18 13:32:51 +0200

    easy

--- macros/models/_project/oneinch/oneinch_call_transfers_macro.sql
+++ macros/models/_project/oneinch/oneinch_call_transfers_macro.sql
@@ -30,12 +30,41 @@ meta as (
 )
 
 , transfers as (
-    select * from ({{ oneinch_parsed_transfers_from_calls_macro(blockchain) }})
-    where
+    
+    select
+        '{{ blockchain }}' as blockchain
+        , block_number
+        , block_time
+        , tx_hash
+        , trace_address as transfer_trace_address
+        , if(value > uint256 '0', 0xae, "to") as contract_address
+        , case {{ selector }}
+            when {{ transfer_selector }} then bytearray_to_uint256(substr(input, 37, 32))
+            when {{ transfer_from_selector }} then bytearray_to_uint256(substr(input, 69, 32))
+            else value
+        end as amount
+        , case
+            when {{ selector }} = {{ transfer_selector }} or value > uint256 '0' then "from"
+            when {{ selector }} = {{ transfer_from_selector }} then substr(input, 17, 20)
+        end as transfer_from
+        , case
+            when {{ selector }} = {{ transfer_selector }} then substr(input, 17, 20)
+            when {{ selector }} = {{ transfer_from_selector }} then substr(input, 49, 20)
+            when value > uint256 '0' then "to"
+        end as transfer_to
+    from {{ source(blockchain, 'traces') }}
+    where (
+            {{ selector }} = {{ transfer_selector }} and length(input) = 68
+            or {{ selector }} = {{ transfer_from_selector }} and length(input) = 100
+            or value > uint256 '0'
+        )
+        and call_type = 'call'
+        and tx_success
+        and success
         {% if is_incremental() %}
-            {{ incremental_predicate('block_time') }}
+            and {{ incremental_predicate('block_time') }}
         {% else %}
-            block_time >= (select first_deploy_at from meta)
+            and block_time >= (select first_deploy_at from meta)
         {% endif %}
 )
 
