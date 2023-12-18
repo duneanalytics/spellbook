cb1fb12b238b7d2e1a33ae20348be646d12db1ad
ARDev097 <147229733+ARDev097@users.noreply.github.com>
2023-12-18 22:17:35 +0530

    Added Optimism Hashflow Dex (#4999)
    * add openxswap trades
    
    * add legacy file for openxswap trades
    
    * update legacy file for openxswap trades
    
    * Add seeds records for openxswap
    
    * Add uniqueness and check_dex_seed tests
    
    * updated seeds records for openxswap
    
    * added openocean dex
    
    * Added seed data for openocean dex
    
    * Added seed data for openocean dex
    
    * Updated dex_trades_assert_freshness file
    
    * Updated trades.sql file
    
    * Updated trades.sql file
    
    * Modified schema file
    
    * Modified schema file
    
    * Modified schema file
    
    * Modified schema file
    
    * Modified schema file
    
    * Modified schema file
    
    * Modified seeds file
    
    * Update models/openocean/optimism/openocean_optimism_trades.sql
    
    * modified trades file
    
    * Update models/openocean/optimism/openocean_optimism_trades.sql
    
    * Update models/openocean/optimism/openocean_optimism_schema.yml
    
    * Added Odos Optimism Dex Trades Data
    
    * Added Odos Optimism Dex Trades Data
    
    * Added Odos Optimism Dex Trades Data
    
    * Added Odos Optimism Dex Trades Data
    
    * Added Odos Optimism Dex Trades Data
    
    * Added Odos Optimism Dex Trades Data
    
    * Added Odos Optimism Dex Trades Data
    
    * Added Odos Optimism Dex Trades Data
    
    * Added Odos Optimism Dex Trades Data
    
    * added sepll for woofi dex on optimism
    
    * added sepll for woofi dex on optimism
    
    * added sepll for woofi dex on optimism
    
    * added sepll for woofi dex on optimism
    
    * Removed tag from trades.sql file
    
    * Implement woofi_optimism_base_trades
    
    * Add block_number
    
    * Update dex_trades_seed.csv (Removing zksync line which might be added mistakenly during resolving commits)
    
    * Add woofi_optimism_base_trades_seed
    
    * Add check_seed test to woofi_optimism_base_trades
    
    * Remove dunesql tag
    
    * Remove duped seed definition
    
    * Added mummy_finance dex trade's
    
    * Removed trace_address column
    
    * Implemented macro approach in mummy_finance dex trades
    
    * Implemented macro approach in mummy_finance dex trades
    
    * Implemented macro approach in mummy_finance dex trades
    
    * Make changes in dex_trades_assert_freshness file
    
    * Updated seed file record
    
    * Updated seed file record
    
    * Updated dex_trades_asser_freshness file
    
    * Updated base_trades file
    
    * Updated base_trades file
    
    * Recover tests/dex/dex_trades_assert_freshness.sql
    
    * Added spell for hashflow dex using macro approach
    
    * Fix _schema.yml
    
    * Update models/dex/dex_info.sql
    
    * Updated crosschain_trade file for resolving syntax error
    
    * Fix incremental filters
    
    * Add hashflow_optimism_crosschain_trades to roll-up table and fix alias
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/hashflow/optimism/hashflow_optimism_crosschain_trades.sql
+++ models/hashflow/optimism/hashflow_optimism_crosschain_trades.sql
@@ -1,17 +1,17 @@
 {{
     config(
-    alias = 'crosschain_trades'
+        alias = 'crosschain_trades'
+        ,schema = 'hashflow_optimism'
+        ,partition_by = ['block_date']
         ,materialized='incremental'
         ,incremental_strategy = 'merge'
-        ,file_format = 'delta'
         ,unique_key = ['block_date', 'source_chain', 'tx_hash']
-        ,post_hook='{{ expose_spells(\'["ethereum"]\',
+        ,post_hook='{{ expose_spells(\'["optimism"]\',
                                         "project",
                                         "hashflow",
-                                        \'["BroderickBonelli"]\') }}'
+                                        \'["ARDev097"]\') }}'
     )
 }}
-
 
 with cross_chain_trades AS (
         SELECT
@@ -29,11 +29,11 @@ with cross_chain_trades AS (
                   WHEN dstChainId = 6 OR dstChainId = 106 THEN 'avalanche_c'
                   WHEN dstChainId = 9 OR dstChainId = 109 THEN 'polygon'
                   WHEN dstChainId = 2 OR dstChainId = 102 THEN 'bnb' END AS destination_chain
-            ,'ethereum'                   AS source_chain
+            ,'optimism'                   AS source_chain
         FROM
-            {{ source('hashflow_ethereum', 'Pool_evt_LzTrade') }}
+            {{ source('hashflow_optimism', 'Pool_evt_LzTrade') }}
         {% if is_incremental() %}
-            WHERE evt_block_time >= date_trunc('day', now() - interval '7' day)
+            WHERE {{ incremental_predicate('evt_block_time') }}
         {% endif %}
         
         UNION
@@ -53,11 +53,11 @@ with cross_chain_trades AS (
                   WHEN dstChainId = 4 THEN 'avalanche_c'
                   WHEN dstChainId = 5 THEN 'polygon'
                   WHEN dstChainId = 6 THEN 'bnb' END AS destination_chain
-            ,'ethereum'                   AS source_chain
+            ,'optimism'                   AS source_chain
         FROM
-            {{ source('hashflow_ethereum', 'Pool_evt_XChainTrade') }}
+            {{ source('hashflow_optimism', 'Pool_evt_XChainTrade') }}
         {% if is_incremental() %}
-            WHERE evt_block_time >= date_trunc('day', now() - interval '7' day)
+            WHERE {{ incremental_predicate('evt_block_time') }}
         {% endif %}
 )
 
@@ -68,8 +68,8 @@ SELECT
     ,erc20b.symbol                                                           AS token_sold_symbol
     ,cross_chain_trades.token_bought_amount_raw / power(10, erc20a.decimals) AS token_bought_amount
     ,cross_chain_trades.token_sold_amount_raw / power(10, erc20b.decimals)   AS token_sold_amount
-    ,cross_chain_trades.token_bought_amount_raw                              AS token_bought_amount_raw
-    ,cross_chain_trades.token_sold_amount_raw                                AS token_sold_amount_raw
+    ,CAST(cross_chain_trades.token_bought_amount_raw AS DECIMAL(38,0))       AS token_bought_amount_raw
+    ,CAST(cross_chain_trades.token_sold_amount_raw AS DECIMAL(38,0))         AS token_sold_amount_raw
     ,coalesce(
             cross_chain_trades.amount_usd
         , (cross_chain_trades.token_bought_amount_raw / power(10, p_bought.decimals)) * p_bought.price
@@ -90,15 +90,15 @@ LEFT JOIN {{ ref('tokens_erc20') }} erc20b
 LEFT JOIN {{ source('prices', 'usd') }} p_bought
     ON p_bought.minute = date_trunc('minute', cross_chain_trades.block_time)
     AND p_bought.contract_address = cross_chain_trades.token_bought_address
-    AND p_bought.blockchain = 'ethereum'
+    AND p_bought.blockchain = 'optimism'
     {% if is_incremental() %}
-        AND p_bought.minute >= date_trunc('day', now() - interval '7' day)
+        AND  {{ incremental_predicate('p_bought.minute') }}
     {% endif %}
 LEFT JOIN {{ source('prices', 'usd') }} p_sold
     ON p_sold.minute = date_trunc('minute', cross_chain_trades.block_time)
     AND p_sold.contract_address = cross_chain_trades.token_sold_address
-    AND p_sold.blockchain = 'ethereum'
+    AND p_sold.blockchain = 'optimism'
     {% if is_incremental() %}
-        AND p_sold.minute >= date_trunc('day', now() - interval '7' day)
+        AND {{ incremental_predicate('p_sold.minute') }}
     {% endif %}
     
