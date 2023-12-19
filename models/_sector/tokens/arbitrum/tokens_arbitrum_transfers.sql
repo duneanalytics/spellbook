3e49910b75b65942c157196baa11de20b6fdc67a
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2023-12-19 12:53:17 -0500

    initiate cross chain transfers (#5025)
    * Revert "Revert "Revert "Revert "add crosschain `tokens.transfers` (#4987)" (#4994)" (#4995)" (#5004)"
    
    This reverts commit 83c3d1b0b585675867dc4b3bc5aec11639a4d9f6.
    
    * initiate final stages of tokens transfers
    
    * update wrapped macro column order to match
    
    * exclude larger chains for CI to run
    
    * Revert "exclude larger chains for CI to run"
    
    This reverts commit 3cbc753efd9c3335404a9597fd9f15f75ba972bb.
    
    * remove extra quotes in case statement

--- models/_sector/tokens/arbitrum/tokens_arbitrum_transfers.sql
+++ models/_sector/tokens/arbitrum/tokens_arbitrum_transfers.sql
@@ -5,8 +5,9 @@
 )
 }}
 
-{{transfers_enrich(
-    blockchain='arbitrum',
-    transfers_base = ref('tokens_arbitrum_base_transfers'),
-    native_symbol = 'ETH'
-)}}
+SELECT
+    *
+FROM
+    {{ ref('tokens_transfers') }}
+WHERE
+    blockchain = 'arbitrum'
