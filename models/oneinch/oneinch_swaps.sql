3c8448fb198a0a46fdaf846ee7458017f6237cbb
grkhr <eduardgorkh@gmail.com>
2023-12-19 20:59:56 +0200

    qqq

--- models/oneinch/oneinch_swaps.sql
+++ models/oneinch/oneinch_swaps.sql
@@ -74,6 +74,7 @@ prices as (
         , protocol
         , protocol_version
         , method
+        , order_hash
         , if(protocol = 'AR' or second_side, tx_from, maker) as user
         , receiver
         , if(src_token_address in {{native_addresses}}, wrapped_native_token_address, src_token_address) as src_token_address
@@ -94,7 +95,7 @@ prices as (
 
         -- LOP calls - second side (when a direct call LOP method => users from two sides)
         select *, true as second_side from {{ ref('oneinch_calls') }}
-        where cardinality(call_trace_address) = 0
+        where protocol = 'LOP' and cardinality(call_trace_address) = 0
     )
     join {{ ref('oneinch_blockchains') }} using(blockchain)
     where
@@ -123,6 +124,30 @@ prices as (
         , max(amount) filter(where contract_address = dst_token_address and amount <= dst_token_amount) as dst_token_amount
         , max(amount * price / pow(10, decimals)) filter(where contract_address = src_token_address and amount <= src_token_amount or contract_address = dst_token_address and amount <= dst_token_amount) as sources_amount_usd
         , max(amount * price / pow(10, decimals)) as transfers_amount_usd
+
+        , sum(amount * if(transfer_from = user, price, -price) / pow(10, decimals)) filter(
+            where (
+                src_token_native is not null and transfer_native 
+                or src_token_address = contract_address
+            )  
+                and user in (transfer_from, transfer_to)
+        ) as _amount_usd_from_user
+        , sum(amount * if(transfer_to = user, price, -price) / pow(10, decimals)) filter(
+            where (
+                dst_token_native is not null and transfer_native 
+                or dst_token_address = contract_address
+            )  
+                and user in (transfer_from, transfer_to)
+        ) as _amount_usd_to_user
+        , sum(amount * if(transfer_to = receiver, price, -price) / pow(10, decimals)) filter(
+            where (
+                dst_token_native is not null and transfer_native 
+                or dst_token_address = contract_address
+            )  
+                and receiver in (transfer_from, transfer_to)
+        ) as _amount_usd_to_receiver
+
+        , count(distinct (contract_address, transfer_native)) as tokens
         , count(*) as transfers
     from _calls 
     join (
@@ -148,29 +173,35 @@ select
     , tx_nonce
     , gas_price
     , priority_fee_per_gas
-    , call_gas_used
-    , call_trace_address
-    , call_from
-    , call_to
     , contract_name
     , protocol
     , protocol_version
     , method
+    , call_trace_address
+    , call_from
+    , call_to
+    , call_gas_used
     , user
     , receiver
     , fusion
     , contracts_only
     , second_side
+    , order_hash
     , remains
     , if(src_token_native is null, src_token_address, {{true_native_address}}) as src_token_address
-    , src_token_symbol
+    , coalesce(src_token_native, src_token_symbol) as src_token_symbol
+    , src_token_decimals
     , amounts.src_token_amount
     , if(dst_token_native is null, dst_token_address, {{true_native_address}}) as dst_token_address
-    , dst_token_symbol
+    , coalesce(dst_token_native, dst_token_symbol) as dst_token_symbol
+    , dst_token_decimals
     , amounts.dst_token_amount
-    , amounts.sources_amount_usd
-    , amounts.transfers_amount_usd
-    , amounts.transfers
+    , coalesce(sources_amount_usd, transfers_amount_usd) as amount_usd
+    , sources_amount_usd
+    , transfers_amount_usd
+    , greatest(coalesce(_amount_usd_from_user, 0), coalesce(_amount_usd_to_user, _amount_usd_to_receiver, 0)) as user_amount_usd
+    , tokens
+    , transfers
     , date_trunc('minute', block_time) as minute
     , date(date_trunc('month', block_time)) as block_month
 from calls
