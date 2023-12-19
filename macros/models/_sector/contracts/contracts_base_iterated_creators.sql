8b945bbb64d5db3a0535af562526d282a9429bb8
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-12-18 20:03:42 -0500

    rerun

--- macros/models/_sector/contracts/contracts_base_iterated_creators.sql
+++ macros/models/_sector/contracts/contracts_base_iterated_creators.sql
@@ -308,4 +308,5 @@ WITH deterministic_deployers AS (
   ON u.contract_address = ts.contract_address
 
   GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
+  
 {% endmacro %}
