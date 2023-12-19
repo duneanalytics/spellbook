a8cab314f00ed433d8b8088563714479ea655e5c
grkhr <eduardgorkh@gmail.com>
2023-12-19 21:44:08 +0200

    revert

--- macros/models/_project/oneinch/oneinch_call_transfers_macro.sql
+++ macros/models/_project/oneinch/oneinch_call_transfers_macro.sql
@@ -1,14 +1,10 @@
 {% macro 
     oneinch_call_transfers_macro(
         blockchain
-        , blockchain_meta
     ) 
 %}
 
 
-{% set transfer_selector = '0xa9059cbb' %}
-{% set transfer_from_selector = '0x23b872dd' %}
-{% set selector = 'substr(input, 1, 4)' %}
 
 with
 
@@ -16,9 +12,7 @@ meta as (
     select 
         wrapped_native_token_address
         , first_deploy_at
-    from {{ blockchain_meta }}
-    where blockchain = '{{ blockchain }}'
-    limit 1
+    from {{ ref('oneinch_' + blockchain + '_blockchain') }}
 )
 
 , calls as (
@@ -32,41 +26,13 @@ meta as (
         {% endif %}
 )
 
-, transfers as (    
-    select
-        '{{ blockchain }}' as blockchain
-        , block_number
-        , block_time
-        , tx_hash
-        , trace_address as transfer_trace_address
-        , if(value > uint256 '0', 0xae, "to") as contract_address
-        , case {{ selector }}
-            when {{ transfer_selector }} then bytearray_to_uint256(substr(input, 37, 32))
-            when {{ transfer_from_selector }} then bytearray_to_uint256(substr(input, 69, 32))
-            else value
-        end as amount
-        , case
-            when {{ selector }} = {{ transfer_selector }} or value > uint256 '0' then "from"
-            when {{ selector }} = {{ transfer_from_selector }} then substr(input, 17, 20)
-        end as transfer_from
-        , case
-            when {{ selector }} = {{ transfer_selector }} then substr(input, 17, 20)
-            when {{ selector }} = {{ transfer_from_selector }} then substr(input, 49, 20)
-            when value > uint256 '0' then "to"
-        end as transfer_to
-    from {{ source(blockchain, 'traces') }}
-    where (
-            {{ selector }} = {{ transfer_selector }} and length(input) = 68
-            or {{ selector }} = {{ transfer_from_selector }} and length(input) = 100
-            or value > uint256 '0'
-        )
-        and call_type = 'call'
-        and tx_success
-        and success
+, transfers as (
+    select * from ({{ oneinch_parsed_transfers_from_calls_macro(blockchain) }})
+    where
         {% if is_incremental() %}
-            and {{ incremental_predicate('block_time') }}
+            {{ incremental_predicate('block_time') }}
         {% else %}
-            and block_time >= (select first_deploy_at from meta)
+            block_time >= (select first_deploy_at from meta)
         {% endif %}
 )
 
