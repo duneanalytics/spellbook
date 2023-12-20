45be4197a4068272842dcffa8ecbe1506d93c5c2
grkhr <eduardgorkh@gmail.com>
2023-12-20 14:53:36 +0200

    fixes

--- models/oneinch/oneinch_swaps.sql
+++ models/oneinch/oneinch_swaps.sql
@@ -55,48 +55,43 @@ prices as (
     from {{ ref('tokens_erc20') }}
 )
 
-, _calls as (
-    select 
-        blockchain
-        , block_number
-        , block_time
-        , tx_hash
-        , tx_from
-        , tx_to
-        , tx_nonce
-        , gas_price
-        , priority_fee_per_gas
-        , call_trace_address
-        , call_from
-        , call_to
-        , call_gas_used
-        , contract_name
-        , protocol
-        , protocol_version
-        , method
-        , order_hash
-        , if(protocol = 'AR' or second_side, tx_from, maker) as user
-        , receiver
-        , if(src_token_address in {{native_addresses}}, wrapped_native_token_address, src_token_address) as src_token_address
-        , if(src_token_address in {{native_addresses}}, native_token_symbol) as src_token_native
-        , src_token_amount
-        , if(dst_token_address in {{native_addresses}}, wrapped_native_token_address, dst_token_address) as dst_token_address
-        , if(dst_token_address in {{native_addresses}}, native_token_symbol) as dst_token_native
-        , dst_token_amount
-        , contains(fusion_settlement_addresses, call_from) as fusion
-        , if(protocol = 'LOP' and second_side = false and position('RFQ' in method) > 0, true, false) as contracts_only
-        , second_side
-        , remains
-    from (
-        -- AR + LOP CALLS
-        select *, false as second_side from {{ ref('oneinch_calls') }}
-        
-        union all
+{% set 
+    calls_base_columns = [
+        'blockchain',
+        'block_number',
+        'block_time',
+        'tx_hash',
+        'tx_from',
+        'tx_to',
+        'tx_nonce',
+        'maker',
+        'gas_price',
+        'priority_fee_per_gas',
+        'call_trace_address',
+        'call_from',
+        'call_to',
+        'call_gas_used',
+        'contract_name',
+        'protocol',
+        'protocol_version',
+        'method',
+        'order_hash',
+        'receiver',
+        'fusion',
+        'remains',
+    ]
+%}
 
-        -- LOP calls - second side (when a direct call LOP method => users from two sides)
-        select *, true as second_side from {{ ref('oneinch_calls') }}
-        where protocol = 'LOP' and cardinality(call_trace_address) = 0
-    )
+, calls_base as (
+    select 
+        *
+        , if(src_token_address in {{native_addresses}}, wrapped_native_token_address, src_token_address) as _src_token_address
+        , if(src_token_address in {{native_addresses}}, native_token_symbol) as _src_token_native
+        , src_token_amount as _src_token_amount
+        , if(dst_token_address in {{native_addresses}}, wrapped_native_token_address, dst_token_address) as _dst_token_address
+        , if(dst_token_address in {{native_addresses}}, native_token_symbol) as _dst_token_native
+        , dst_token_amount as _dst_token_amount
+    from {{ ref('oneinch_calls') }}
     join {{ ref('oneinch_blockchains') }} using(blockchain)
     where
         tx_success
@@ -106,13 +101,44 @@ prices as (
         {% endif %}
 )
 
+, _calls as (
+    -- AR + LOP CALLS
+    select 
+        {{ calls_base_columns | join(', ') }}
+        , _src_token_address as src_token_address
+        , _src_token_native as src_token_native
+        , _src_token_amount as src_token_amount
+        , _dst_token_address as dst_token_address
+        , _dst_token_native as dst_token_native
+        , _dst_token_amount as dst_token_amount
+        , false as second_side 
+    from calls_base
+    
+    union all
+
+    -- LOP calls - second side (when a direct call LOP method => users from two sides)
+    select 
+        {{ calls_base_columns | join(', ') }}
+        , _dst_token_address as src_token_address
+        , _dst_token_native as src_token_native
+        , _dst_token_amount as src_token_amount
+        , _src_token_address as dst_token_address
+        , _src_token_native as dst_token_native
+        , _src_token_amount as dst_token_amount
+        , true as second_side 
+    from calls_base
+    where protocol = 'LOP' and cardinality(call_trace_address) = 0
+)
+
 , calls as (
-    select * from _calls
+    select 
+        *
+        , if(protocol = 'AR' or second_side, tx_from, maker) as user
+        , if(protocol = 'LOP' and second_side = false and position('RFQ' in method) > 0, true, false) as contracts_only
+    from _calls
     left join tokens_src using(blockchain, src_token_address)
     left join tokens_dst using(blockchain, dst_token_address)
 )
-
---  max(amount) filter(where contract_address = src_token_address and amount <= src_token_amount) as src_token_amount
 
 , amounts as (
     select 
@@ -149,7 +175,7 @@ prices as (
 
         , count(distinct (contract_address, transfer_native)) as tokens
         , count(*) as transfers
-    from _calls 
+    from calls 
     join (
         select * from {{ ref('oneinch_call_transfers') }}
         {% if is_incremental() %}
@@ -158,7 +184,6 @@ prices as (
     ) using(blockchain, tx_hash, call_trace_address, block_number)
     left join prices using(blockchain, contract_address, minute)
     group by 1, 2, 3, 4
-    
 )
 
 
