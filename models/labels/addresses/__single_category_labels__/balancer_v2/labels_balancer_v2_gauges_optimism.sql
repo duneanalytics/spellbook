1e18470b601f9fc8cf36e8a7f55e0092337845f9
viniabussafi <vinicius@balancerlabs.dev>
2023-12-22 10:51:21 +0100

    fix

--- models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges_optimism.sql
+++ models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges_optimism.sql
@@ -4,7 +4,7 @@
         post_hook='{{ expose_spells(\'["optimism"]\',
                                     "sector",
                                     "labels",
-                                    \'["jacektrocinski"]\') }}')}}
+                                    \'["jacektrocinski", "viniabusafi"]\') }}')}}
 
 SELECT
     'optimism' AS blockchain,
@@ -21,7 +21,9 @@ FROM
     {{ source('balancer_ethereum', 'OptimismRootGaugeFactory_evt_OptimismRootGaugeCreated') }} gauge
     LEFT JOIN {{ source('balancer_optimism', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON gauge.recipient = streamer.streamer
     LEFT JOIN {{ ref('labels_balancer_v2_pools_optimism') }} pools ON pools.address = streamer.pool
+
 UNION ALL
+
 SELECT
     'optimism' AS blockchain,
     gauge.gauge AS address,
@@ -38,3 +40,20 @@ FROM
     INNER JOIN {{ source('balancer_ethereum', 'CappedOptimismRootGaugeFactory_call_create') }} call ON call.call_tx_hash = gauge.evt_tx_hash
     LEFT JOIN {{ source('balancer_optimism', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON streamer.streamer = call.recipient
     LEFT JOIN {{ ref('labels_balancer_v2_pools_optimism') }} pools ON pools.address = streamer.pool
+
+UNION ALL
+
+SELECT distinct
+    'optimism' AS blockchain,
+    call.output_0 AS address,
+    'opt:' || pools.name AS name,
+    'balancer_v2_gauges' AS category,
+    'balancerlabs' AS contributor,
+    'query' AS source,
+    TIMESTAMP '2022-01-13'  AS created_at,
+    NOW() AS updated_at,
+    'balancer_v2_gauges_optimism' AS model_name,
+    'identifier' AS label_type
+FROM {{ source('balancer_ethereum', 'CappedOptimismRootGaugeFactory_call_create') }} call
+    LEFT JOIN {{ source('balancer_optimism', 'ChildChainGaugeFactory_call_create') }} child ON child.output_0 = call.recipient
+    LEFT JOIN {{ ref('labels_balancer_v2_pools_optimism') }} pools ON pools.address = child.pool
--- models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges_polygon.sql
+++ models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_gauges_polygon.sql
@@ -4,7 +4,7 @@
         post_hook='{{ expose_spells(\'["polygon"]\',
                                     "sector",
                                     "labels",
-                                    \'["jacektrocinski"]\') }}')}}
+                                    \'["jacektrocinski", "viniabussafi"]\') }}')}}
 
 SELECT
     'polygon' AS blockchain,
@@ -21,7 +21,9 @@ FROM
     {{ source('balancer_ethereum', 'PolygonRootGaugeFactory_evt_PolygonRootGaugeCreated') }} gauge
     LEFT JOIN {{ source('balancer_polygon', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON gauge.recipient = streamer.streamer
     LEFT JOIN {{ ref('labels_balancer_v2_pools_polygon') }} pools ON pools.address = streamer.pool
+
 UNION ALL
+
 SELECT
     'polygon' AS blockchain,
     gauge.gauge AS address,
@@ -38,3 +40,20 @@ FROM
     INNER JOIN {{ source('balancer_ethereum', 'CappedPolygonRootGaugeFactory_call_create') }} call ON call.call_tx_hash = gauge.evt_tx_hash
     LEFT JOIN {{ source('balancer_polygon', 'ChildChainLiquidityGaugeFactory_evt_RewardsOnlyGaugeCreated') }} streamer ON streamer.streamer = call.recipient
     LEFT JOIN {{ ref('labels_balancer_v2_pools_polygon') }} pools ON pools.address = streamer.pool
+
+UNION ALL
+
+SELECT distinct
+    'polygon' AS blockchain,
+    call.output_0 AS address,
+    'pol:' || pools.name AS name,
+    'balancer_v2_gauges' AS category,
+    'balancerlabs' AS contributor,
+    'query' AS source,
+    TIMESTAMP '2022-01-13'  AS created_at,
+    NOW() AS updated_at,
+    'balancer_v2_gauges_polygon' AS model_name,
+    'identifier' AS label_type
+FROM {{ source('balancer_ethereum', 'CappedPolygonRootGaugeFactory_call_create') }} call
+    LEFT JOIN {{ source('balancer_polygon', 'ChildChainGaugeFactory_call_create') }} child ON child.output_0 = call.recipient
+    LEFT JOIN {{ ref('labels_balancer_v2_pools_polygon') }} pools ON pools.address = child.pool
