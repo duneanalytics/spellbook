881625b478066b047f8869c4637a22b46e635da2
tomfutago <tomfutago@gmail.com>
2023-12-24 11:09:17 +0000

    more chains

--- models/_sector/dex/trades/polygon/platforms/dodo_polygon_base_trades.sql
+++ models/_sector/dex/trades/polygon/platforms/dodo_polygon_base_trades.sql
@@ -1,6 +1,6 @@
 {{
     config(
-        schema = 'dodo_arbitrum',
+        schema = 'dodo_polygon',
         alias = 'base_trades',
         materialized = 'incremental',
         file_format = 'delta',
@@ -14,9 +14,7 @@
     WITH dodo_view_markets (market_contract_address, base_token_symbol, quote_token_symbol, base_token_address, quote_token_address) AS 
     (
         VALUES
-        (0xFE176A2b1e1F67250d2903B8d25f56C0DaBcd6b2, 'WETH', 'USDC', 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1, 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8),
-        (0xe4B2Dfc82977dd2DCE7E8d37895a6A8F50CbB4fB, 'USDT', 'USDC', 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9, 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8),
-        (0xb42a054D950daFD872808B3c839Fbb7AFb86E14C, 'WBTC', 'USDC', 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f, 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8)
+        (0x813fddeccd0401c4fa73b092b074802440544e52, 'USDC', 'USDT', 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174, 0xc2132D05D31c914a87C6611C10748AEb04B58e8F)
     )
     SELECT * FROM dodo_view_markets
 {% endset %}
@@ -24,6 +22,8 @@
 {%
     set config_other_sources = [
         {'version': '2_dvm', 'source': 'DVM_evt_DODOSwap'},
+        {'version': '2_dpp', 'source': 'DPP_evt_DODOSwap'},
+        {'version': '2_dpp', 'source': 'DPPAdvanced_evt_DODOSwap'},
         {'version': '2_dpp', 'source': 'DPPOracle_evt_DODOSwap'},
         {'version': '2_dsp', 'source': 'DSP_evt_DODOSwap'},
     ]
@@ -31,9 +31,10 @@
 
 {{
     dodo_compatible_trades(
-        blockchain = 'arbitrum',
+        blockchain = 'polygon',
         project = 'dodo',
         markets = config_markets,
+        decoded_project = 'dodoex',
         sell_base_token_source = 'DODO_evt_SellBaseToken',
         buy_base_token_source = 'DODO_evt_BuyBaseToken',
         other_sources = config_other_sources
