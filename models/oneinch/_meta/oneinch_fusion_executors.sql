e3d2f418d34fbac1c09b1e9640fdbfbd4ba822a0
grkhr <eduardgorkh@gmail.com>
2023-12-26 21:52:45 +0200

    fix

--- models/oneinch/_meta/oneinch_fusion_executors.sql
+++ models/oneinch/_meta/oneinch_fusion_executors.sql
@@ -14,7 +14,6 @@ executors as (
         promoter as resolver_address
         , promotee as resolver_executor
         , chainId as chain_id
-        , blockchain
         , min(evt_block_time) as first_promoted_at
         , max(evt_block_time) as last_promoted_at
     from (
@@ -24,11 +23,17 @@ executors as (
         select promoter, promotee, chainId, evt_block_time
         from {{ source('oneinch_ethereum', 'FusionWhitelistRegistryV2_evt_Promotion') }}
     )
-    left join {{ ref('oneinch_blockchains') }} using(chain_id)
-    group by 1, 2, 3, 4
+    group by 1, 2, 3
 )
 
 -- output --
 
-select *
+select 
+    blockchain
+    , resolver_address
+    , resolver_executor
+    , chain_id
+    , first_promoted_at
+    , last_promoted_at
 from executors
+left join {{ ref('oneinch_blockchains') }} using(chain_id)
