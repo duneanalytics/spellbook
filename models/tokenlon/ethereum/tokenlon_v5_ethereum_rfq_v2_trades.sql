2cd247362bfaf4568794aa7d807fab9450d3aa48
Keen <mail@kuan.li>
2023-12-28 16:39:05 +0800

    fix: table name

--- models/tokenlon/ethereum/tokenlon_v5_ethereum_rfq_v2_trades.sql
+++ models/tokenlon/ethereum/tokenlon_v5_ethereum_rfq_v2_trades.sql
@@ -102,7 +102,7 @@ LEFT JOIN {{ ref('tokens_erc20') }} erc20a
 LEFT JOIN {{ ref('tokens_erc20') }} erc20b
     ON erc20b.contract_address = dexs.token_sold_address
     AND erc20b.blockchain = 'ethereum'
-LEFT JOIN {{ source('prices', 'usd') }} p_bought
+LEFT JOIN prices p_bought
     ON p_bought.hour = date_trunc('hour', dexs.block_time)
     AND p_bought.contract_address = dexs.token_bought_address
     AND p_bought.blockchain = 'ethereum'
@@ -112,7 +112,7 @@ LEFT JOIN {{ source('prices', 'usd') }} p_bought
     {% if is_incremental() %}
     AND p_bought.hour >= date_trunc('day', now() - interval '7' day)
     {% endif %}
-LEFT JOIN {{ source('prices', 'usd') }} p_sold
+LEFT JOIN prices p_sold
     ON p_sold.hour = date_trunc('hour', dexs.block_time)
     AND p_sold.contract_address = dexs.token_sold_address
     AND p_sold.blockchain = 'ethereum'
