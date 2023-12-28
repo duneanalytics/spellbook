6ebee8e12ddd8c45bd940e129b3e864e29c91a4e
tomfutago <35136350+tomfutago@users.noreply.github.com>
2023-12-28 16:30:07 +0000

    Revert adding ImmortalX to `dex.trades_beta` (#5048)
    * fix
    
    * revert adding to dex.trades_beta
    
    * update dex.info
    
    * adding immortalx back to dex.info
    
    ---------
    
    Co-authored-by: Alex Courouble <56831527+couralex6@users.noreply.github.com>

--- models/_sector/dex/trades/celo/dex_celo_base_trades.sql
+++ models/_sector/dex/trades/celo/dex_celo_base_trades.sql
@@ -12,8 +12,7 @@
     ref('curvefi_celo_base_trades'),
     ref('sushiswap_celo_base_trades'),
     ref('ubeswap_celo_base_trades'),
-    ref('carbonhood_celo_base_trades'),
-    ref('immortalx_celo_base_trades')
+    ref('carbonhood_celo_base_trades')
 ] %}
 
 WITH base_union AS (
