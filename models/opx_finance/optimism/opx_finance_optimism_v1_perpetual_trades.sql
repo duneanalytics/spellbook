661d9f9d53fcdabb970eb679e9caa67efc074953
Huang Geyang <Sukebeta@outlook.com>
2024-01-04 22:34:33 +0800

    Reformat opx_finance_optimism_v1_perpetual_trades

--- models/opx_finance/optimism/opx_finance_optimism_v1_perpetual_trades.sql
+++ models/opx_finance/optimism/opx_finance_optimism_v1_perpetual_trades.sql
@@ -17,84 +17,75 @@
 
 {% set project_start_date = '2022-11-04' %}
 
-WITH all_executed_positions AS
-(SELECT *, 
-'Open' AS trade_type
+WITH all_executed_positions AS (
+SELECT *,
+       'Open' AS trade_type
 FROM {{ source('opx_finance_optimism', 'Vault_evt_IncreasePosition') }}
-
 WHERE evt_tx_hash IN ( SELECT evt_tx_hash FROM {{ source('opx_finance_optimism', 'PositionRouter_evt_ExecuteIncreasePosition') }} )
-{% if not is_incremental() %}
-AND evt_block_time >= DATE '{{project_start_date}}'
-{% endif %}
-{% if is_incremental() %}
-AND evt_block_time >= DATE_TRUNC('DAY', NOW() - INTERVAL '7' Day)
-{% endif %}
+    {% if not is_incremental() %}
+  AND evt_block_time >= DATE '{{project_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+  AND evt_block_time >= DATE_TRUNC('DAY', NOW() - INTERVAL '7' Day)
+    {% endif %}
 
 UNION
 
-SELECT *, 
-'Close' AS trade_type
+SELECT *, 'Close' AS trade_type
 FROM {{ source('opx_finance_optimism', 'Vault_evt_DecreasePosition') }}
 
 WHERE evt_tx_hash IN ( SELECT evt_tx_hash FROM {{ source('opx_finance_optimism', 'PositionRouter_evt_ExecuteDecreasePosition') }} )
-{% if not is_incremental() %}
-AND evt_block_time >= DATE '{{project_start_date}}'
-{% endif %}
-{% if is_incremental() %}
-AND evt_block_time >= DATE_TRUNC('DAY', NOW() - INTERVAL '7' Day)
-{% endif %}
+    {% if not is_incremental() %}
+  AND evt_block_time >= DATE '{{project_start_date}}'
+    {% endif %}
+    {% if is_incremental() %}
+  AND evt_block_time >= DATE_TRUNC('DAY', NOW() - INTERVAL '7' Day)
+    {% endif %}
 ),
 
-margin_fees_info AS
-(SELECT *, 
-LEAD(evt_index, 1, 1000000) OVER (PARTITION BY evt_tx_hash ORDER BY evt_index) AS next_evt_index
-FROM {{ source('opx_finance_optimism', 'Vault_evt_CollectMarginFees') }}
-
-
-{% if not is_incremental() %}
-WHERE evt_block_time >= DATE '{{project_start_date}}'
-{% endif %}
-{% if is_incremental() %}
-WHERE evt_block_time >= DATE_TRUNC('DAY', NOW() - INTERVAL '7' Day)
-{% endif %}
+margin_fees_info AS (
+    SELECT *,
+           LEAD(evt_index, 1, 1000000) OVER (PARTITION BY evt_tx_hash ORDER BY evt_index) AS next_evt_index
+    FROM {{ source('opx_finance_optimism', 'Vault_evt_CollectMarginFees') }}
+        {% if not is_incremental() %}
+    WHERE evt_block_time >= DATE '{{project_start_date}}'
+        {% endif %}
+        {% if is_incremental() %}
+    WHERE evt_block_time >= DATE_TRUNC('DAY', NOW() - INTERVAL '7' Day)
+        {% endif %}
 ),
 
-complete_perp_tx AS
-(SELECT *, index_token || '/USD' AS market
+complete_perp_tx AS (
+SELECT *, index_token || '/USD' AS market
 FROM (SELECT event.*,
-tokens1.symbol AS underlying_asset,
-(CASE 
-WHEN tokens.symbol = 'WETH' THEN 'ETH'
-WHEN tokens.symbol = 'WBTC' THEN 'BTC'
-ELSE tokens.symbol
-END
-) AS index_token,
-trx."from",
-trx.to, 
-fee.feeUsd AS margin_fee
-FROM all_executed_positions event
-
-JOIN {{ source('optimism', 'transactions') }} trx
-ON event.evt_tx_hash = trx.hash
-{% if not is_incremental() %}
-AND evt_block_time >= DATE '{{project_start_date}}'
-{% endif %}
-{% if is_incremental() %}
-AND evt_block_time >= DATE_TRUNC('DAY', NOW() - INTERVAL '7' Day)
-{% endif %}
-
-JOIN margin_fees_info fee
-ON event.evt_tx_hash = fee.evt_tx_hash
-AND event.evt_index >  fee.evt_index
-AND event.evt_index <  fee.next_evt_index
-
-JOIN {{ ref('tokens_optimism_erc20') }} tokens
-ON event.indexToken = tokens.contract_address
-
-JOIN {{ ref('tokens_optimism_erc20') }} tokens1
-ON event.collateralToken = tokens1.contract_address
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-)
+             tokens1.symbol AS underlying_asset,
+             (CASE
+                  WHEN tokens.symbol = 'WETH' THEN 'ETH'
+                  WHEN tokens.symbol = 'WBTC' THEN 'BTC'
+                  ELSE tokens.symbol
+                 END
+                 )          AS index_token,
+             trx."from",
+             trx.to,
+             fee.feeUsd     AS margin_fee
+      FROM all_executed_positions event
+      JOIN {{ source('optimism', 'transactions') }} trx
+      ON event.evt_tx_hash = trx.hash
+          {% if not is_incremental() %}
+          AND evt_block_time >= DATE '{{project_start_date}}'
+          {% endif %}
+          {% if is_incremental() %}
+          AND evt_block_time >= DATE_TRUNC('DAY', NOW() - INTERVAL '7' Day)
+          {% endif %}
+      JOIN margin_fees_info fee
+          ON event.evt_tx_hash = fee.evt_tx_hash
+          AND event.evt_index > fee.evt_index
+          AND event.evt_index < fee.next_evt_index
+      JOIN {{ ref('tokens_optimism_erc20') }} tokens
+          ON event.indexToken = tokens.contract_address
+      JOIN {{ ref('tokens_optimism_erc20') }} tokens1
+          ON event.collateralToken = tokens1.contract_address
+      GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 )
 
 SELECT
