825fea9f5c665fa1d55decc2ad2208de636ee621
Huang Geyang <Sukebeta@outlook.com>
2024-01-05 01:37:30 +0800

    Fix tabs

--- models/_sector/nft/mints/native/nft_polygon_native_mints.sql
+++ models/_sector/nft/mints/native/nft_polygon_native_mints.sql
@@ -3,9 +3,9 @@
         schema = 'nft_polygon',
         alias = 'native_mints',
         partition_by = ['block_month'],
-		file_format = 'delta',
+        file_format = 'delta',
         materialized = 'incremental',
-		incremental_strategy = 'merge',
+        incremental_strategy = 'merge',
         unique_key = ['tx_hash','evt_index','token_id','number_of_items']
         )
 }}
