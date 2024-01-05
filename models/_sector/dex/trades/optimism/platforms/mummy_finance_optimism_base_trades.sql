ec5cc1c784eda06824a84d63fcfbbc8965a36960
tomfutago <tomfutago@gmail.com>
2024-01-05 16:01:37 +0000

    add taker param, update spells to use generic spot trades macro

--- models/_sector/dex/trades/optimism/platforms/mummy_finance_optimism_base_trades.sql
+++ models/_sector/dex/trades/optimism/platforms/mummy_finance_optimism_base_trades.sql
@@ -9,52 +9,11 @@
     )
 }}
 
-{% set mummy_finance_optimism_evt_trade_tables = [
-    source('mummy_finance_optimism', 'Router_evt_Swap')
-] %}
-
-with dexs AS (
-    {% for evt_trade_table in mummy_finance_optimism_evt_trade_tables %}
-        SELECT
-            evt_block_time          AS block_time,
-            account                 AS taker,
-            CAST(NULL as VARBINARY) AS maker,
-            amountIn                AS token_bought_amount_raw,
-            amountOut               AS token_sold_amount_raw,
-            CAST(NULL AS double)    AS amount_usd,
-            tokenIn                 AS token_bought_address,
-            tokenOut                AS token_sold_address,
-            contract_address        AS project_contract_address,
-            evt_tx_hash             AS tx_hash,
-            evt_block_number        AS block_number,
-            evt_index
-        FROM {{ evt_trade_table }}
-        {% if is_incremental() %}
-        WHERE {{incremental_predicate('evt_block_time')}}
-        {% endif %}
-
-        {% if not loop.last %}
-        UNION ALL
-        {% endif %}
-
-    {% endfor %}
-)
-
-SELECT
-    'optimism' AS blockchain,
-    'mummy_finance' AS project,
-    '1' AS version,
-    CAST(date_trunc('DAY', dexs.block_time) AS date) AS block_date,
-    CAST(date_trunc('MONTH', dexs.block_time) AS date) AS block_month,
-    dexs.block_time,
-    dexs.block_number,
-    dexs.token_bought_amount_raw,
-    dexs.token_sold_amount_raw,
-    dexs.token_bought_address,
-    dexs.token_sold_address,
-    dexs.taker,
-    dexs.maker,
-    dexs.project_contract_address,
-    dexs.tx_hash,
-    dexs.evt_index
-FROM dexs
+{{
+    generic_spot_compatible_trades(
+        blockchain = 'optimism',
+        project = 'mummy_finance',
+        version = '1',
+        source_evt_swap = source('mummy_finance_optimism', 'Router_evt_Swap')
+    )
+}}
