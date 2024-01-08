e04d389e8e6df13de8390c5bb246dcf52f23a1e4
tomfutago <35136350+tomfutago@users.noreply.github.com>
2024-01-08 19:25:15 +0000

    Add xchange to `dex.trades_beta` (#5111)
    * init
    
    * add seed placeholders
    
    * more chains
    
    * update seed
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/_sector/dex/trades/bnb/dex_bnb_base_trades.sql
+++ models/_sector/dex/trades/bnb/dex_bnb_base_trades.sql
@@ -28,6 +28,7 @@
     , ref('maverick_bnb_base_trades')
     , ref('nomiswap_bnb_base_trades')
     , ref('kyberswap_bnb_base_trades')
+    , ref('xchange_bnb_base_trades')
     , ref('thena_bnb_base_trades')
     , ref('ellipsis_finance_bnb_base_trades')
     , ref('onepunchswap_bnb_base_trades')
