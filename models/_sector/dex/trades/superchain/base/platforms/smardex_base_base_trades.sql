19c7584d7978b7cbf83c3650918cdad601ed04cc
chain-l <hayagriva.coder@gmail.com>
2024-01-08 17:15:49 +0530

    Add SmarDex & DackieSwap

---
+++ models/_sector/dex/trades/superchain/base/platforms/smardex_base_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'smardex_base',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v2_trades(
+        blockchain = 'base',
+        project = 'smardex',
+        version = '1',
+        Pair_evt_Swap = source('smardex_base', 'SmardexPair_evt_Swap'),
+        Factory_evt_PairCreated = source('smardex_base', 'SmardexFactory_evt_PairCreated')
+    )
+}}
