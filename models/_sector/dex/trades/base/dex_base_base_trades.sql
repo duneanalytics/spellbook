cd3adb406cde0031458911eee80b294fc05c017b
tomfutago <tomfutago@gmail.com>
2024-01-09 16:06:16 +0000

    init

--- models/_sector/dex/trades/base/dex_base_base_trades.sql
+++ models/_sector/dex/trades/base/dex_base_base_trades.sql
@@ -15,6 +15,7 @@
     , ref('balancer_v2_base_base_trades')
     , ref('dodo_base_base_trades')
     , ref('maverick_base_base_trades')
+    , ref('rubicon_base_base_trades')
 ] %}
 
 WITH base_union AS (
