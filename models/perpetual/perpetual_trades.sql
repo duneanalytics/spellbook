8e36e8a818c46a1fc7bca16e3dbed151a6f3d449
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-01-09 10:15:07 -0500

    fix to handle denormalized decoded tables (#5131)

--- models/perpetual/perpetual_trades.sql
+++ models/perpetual/perpetual_trades.sql
@@ -6,6 +6,7 @@
         file_format = 'delta',
         incremental_strategy = 'merge',
 	      unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
         post_hook='{{ expose_spells(\'["optimism","avalanche_c","arbitrum", "polygon","celo"]\',
                                 "sector",
                                 "perpetual",
@@ -14,20 +15,20 @@
 }}
 
 {% set perpetual_trade_models = [
- ref('perpetual_protocol_perpetual_trades')
-,ref('pika_perpetual_trades')
-,ref('synthetix_perpetual_trades')
-,ref('emdx_avalanche_c_perpetual_trades')
-,ref('hubble_exchange_avalanche_c_perpetual_trades')
-,ref('gmx_perpetual_trades')
-,ref('tigris_perpetual_trades')
-,ref('mummy_finance_optimism_perpetual_trades')
-,ref('fxdx_optimism_perpetual_trades')
-,ref('opx_finance_optimism_perpetual_trades')
-,ref('nex_optimism_perpetual_trades')
-,ref('avt_optimism_perpetual_trades')
-,ref('minerva_money_optimism_perpetual_trades')
-,ref('immortalx_perpetual_trades')
+  ref('perpetual_protocol_perpetual_trades')
+  ,ref('pika_perpetual_trades')
+  ,ref('synthetix_perpetual_trades')
+  ,ref('emdx_avalanche_c_perpetual_trades')
+  ,ref('hubble_exchange_avalanche_c_perpetual_trades')
+  ,ref('gmx_perpetual_trades')
+  ,ref('tigris_perpetual_trades')
+  ,ref('mummy_finance_optimism_perpetual_trades')
+  ,ref('fxdx_optimism_perpetual_trades')
+  ,ref('opx_finance_optimism_perpetual_trades')
+  ,ref('nex_optimism_perpetual_trades')
+  ,ref('avt_optimism_perpetual_trades')
+  ,ref('minerva_money_optimism_perpetual_trades')
+  ,ref('immortalx_perpetual_trades')
 ] %}
 
 SELECT *
@@ -58,7 +59,7 @@ FROM
     ,evt_index
   FROM {{ perpetual_model }}
   {% if is_incremental() %}
-  WHERE block_time >= date_trunc('day', now() - interval '7' Day)
+  WHERE {{ incremental_predicate('block_time') }}
   {% endif %}
   {% if not loop.last %}
   UNION ALL
