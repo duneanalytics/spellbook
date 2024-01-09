670986a66486f7090eee6a55137a1f536e421661
chain-l <hayagriva.coder@gmail.com>
2024-01-09 15:14:21 +0530

    Change proposals.sql

--- models/governance/optimism/proposals.sql
+++ models/governance/optimism/proposals.sql
@@ -1,13 +1,10 @@
--- Proposals Available On Agora And Snapshot Platform
-
-{{ config(
-    alias = 'proposals'
+{{ config(tags=['dunesql']
+    ,alias = 'proposal_votes'
     ,materialized = 'incremental'
     ,file_format = 'delta'
-    ,schema = 'governance_optimism_proposals'
+    ,schema = 'governance_optimism_proposal_votes'
     ,incremental_strategy = 'merge'
-    ,unique_key = ['proposal_id']
-    ,incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.start_timestamp')]
+    ,unique_key = ['proposal_id','date_timestamp','tx_hash','voter','choice']
     ,post_hook='{{ expose_spells(\'["optimism"]\',
                                       "sector",
                                       "governance",
@@ -15,50 +12,40 @@
     )
 }}
 
-{% set models = [
-    ref('agora_proposals'),
-    ref('snapshot_proposals')
-] %}
-
-WITH all_proposals AS (
-    SELECT *
-    FROM (
-        {% for model in models %}
-        SELECT
-            proposal_id,
-            proposal_link,
-            proposal_type,
-            proposal_description,
-            start_block,
-            start_timestamp,
-            end_block,
-            end_timestamp,
-            platform,
-            highest_weightage_vote,
-            highest_weightage_voter,
-            highest_weightage_voter_percentage,
-            total_for_votingWeightage,
-            total_abstain_votingWeightage,
-            total_against_votingWeightage,
-            unique_for_votes,
-            unique_abstain_votes,
-            unique_against_votes,
-            unique_votes_count,
-            total_votes_casted,
-            proposal_status
-        FROM
-            {{ model }}
-        {% if is_incremental() %}
-        WHERE
-            {{ incremental_predicate('start_timestamp') }}
-        {% endif %}
-        {% if not loop.last %}
-        UNION ALL
-        {% endif %}
-        {% endfor %}
-    )
-)
-
-SELECT *
+SELECT
+  TRY_CAST(proposalId AS VARBINARY) AS proposal_id,
+  'agora' AS platform,
+  evt_tx_hash AS tx_hash,
+  evt_block_time AS date_timestamp,
+  voter,
+  reason,
+  weight / POWER(10, 18) AS votingWeightage,
+  support AS choice,
+  CASE
+    WHEN support = 0 THEN 'against'
+    WHEN support = 1 THEN 'for'
+    WHEN support = 2 THEN 'abstain'
+    WHEN support = 3 THEN 'voted'
+  END AS choice_name
 FROM
-    all_proposals
+  {{ source('optimism_governor_optimism','OptimismGovernorV5_evt_VoteCast') }}
+UNION ALL
+SELECT
+  proposal AS proposal_id,
+  'snapshot' AS platform,
+  TRY_CAST('' AS VARBINARY) AS tx_hash,
+  from_unixtime(created) AS date_timestamp,
+  voter,
+  reason,
+  vp AS votingWeightage,
+  TRY_CAST(choice AS INT) AS choice,
+  CASE
+    WHEN choice = '1' THEN 'for'
+    WHEN choice = '2' THEN 'against'
+    WHEN choice = '3' THEN 'abstain'
+    WHEN choice = '4' THEN 'voted'
+  END AS choice_name
+FROM
+  {{ source('snapshot','votes') }}
+WHERE
+  "space" = 'opcollective.eth'
