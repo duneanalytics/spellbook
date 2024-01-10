a28c42d55a0694f707e24f765050c4817bae5628
ARDev097 <ardev029@gmail.com>
2024-01-10 10:18:13 +0530

    Modified structure from Superchain to Base

--- models/dex/dex_info.sql
+++ models/dex/dex_info.sql
@@ -95,4 +95,5 @@ FROM (VALUES
     , ('immortalx', 'ImmortalX', 'Perpetual', 'immortalx')
     , ('wardenswap', 'WardenSwap', 'Direct', 'wardenswap')
     , ('scale','Scale','Direct','Equalizer0x')
+    , ('baseswap','BaseSwap','Direct','BaseSwapDex')
     ) AS temp_table (project, name, marketplace_type, x_username)
