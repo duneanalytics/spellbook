a7241147a65b7b7c969e29f1389080fbc79ea4c9
Huang Geyang <Sukebeta@outlook.com>
2024-01-11 00:31:10 +0800

    Reimplement pharaoh_avalanche_c_base_trades

--- models/_sector/dex/trades/avalanche_c/platforms/pharaoh_avalanche_c_base_trades.sql
+++ models/_sector/dex/trades/avalanche_c/platforms/pharaoh_avalanche_c_base_trades.sql
@@ -9,12 +9,59 @@
     )
 }}
 
-{{
-    uniswap_compatible_v3_trades(
-        blockchain = 'avalanche_c'
-        , project = 'pharaoh'
-        , version = '1'
-        , Pair_evt_Swap = source('pharaoh_avalanche_c', 'ClPool_evt_Swap')
-        , Factory_evt_PoolCreated = source('pharaoh_avalanche_c', 'ClPoolFactory_evt_PoolCreated')
-    )
-}}
+WITH dex_raw AS (
+    {{
+        uniswap_compatible_v3_trades(
+            blockchain = 'avalanche_c'
+            , project = 'pharaoh'
+            , version = '1'
+            , Pair_evt_Swap = source('pharaoh_avalanche_c', 'ClPool_evt_Swap')
+            , Factory_evt_PoolCreated = source('pharaoh_avalanche_c', 'ClPoolFactory_evt_PoolCreated')
+        )
+    }}
+)
+
+SELECT
+    dex_raw.blockchain,
+    dex_raw.project,
+    dex_raw.version,
+    dex_raw.block_month,
+    dex_raw.block_date,
+    dex_raw.block_time,
+    dex_raw.block_number,
+    router.amountOut as token_bought_amount_raw,
+    router.inputAmount as token_sold_amount_raw,
+    dex_raw.token_bought_address,
+    dex_raw.token_sold_address,
+    dex_raw.taker,
+    dex_raw.maker,
+    dex_raw.project_contract_address,
+    dex_raw.tx_hash,
+    dex_raw.evt_index
+FROM dex_raw
+INNER JOIN {{ source('glacier_avalanche_c', 'OdosRouterV2_evt_Swap') }} AS router
+ON dex_raw.tx_hash = router.evt_tx_hash
+
+UNION ALL
+
+SELECT
+    dex_raw.blockchain,
+    dex_raw.project,
+    dex_raw.version,
+    dex_raw.block_month,
+    dex_raw.block_date,
+    dex_raw.block_time,
+    dex_raw.block_number,
+    dex_raw.token_bought_amount_raw,
+    dex_raw.token_sold_amount_raw,
+    dex_raw.token_bought_address,
+    dex_raw.token_sold_address,
+    dex_raw.taker,
+    dex_raw.maker,
+    dex_raw.project_contract_address,
+    dex_raw.tx_hash,
+    dex_raw.evt_index
+FROM dex_raw
+LEFT JOIN {{ source('glacier_avalanche_c', 'OdosRouterV2_evt_Swap') }} AS router
+ON dex_raw.tx_hash = router.evt_tx_hash
+WHERE router.evt_tx_hash IS NULL
