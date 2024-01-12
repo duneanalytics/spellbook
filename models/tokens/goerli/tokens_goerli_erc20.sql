4fc9fd2c59031154901aee9dd5b2f13d63f539a5
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-01-12 18:07:58 -0500

    comma

--- models/tokens/goerli/tokens_goerli_erc20.sql
+++ models/tokens/goerli/tokens_goerli_erc20.sql
@@ -2,5 +2,5 @@
 
 SELECT contract_address as contract_address, symbol, decimals
 FROM (VALUES
-       (0xa3e0Dfbf8DbD86e039f7CDB37682A776D66dae4b, 'USDC', 6),
+       (0xa3e0Dfbf8DbD86e039f7CDB37682A776D66dae4b, 'USDC', 6)
        ) AS temp_table (contract_address, symbol, decimals)
--- models/tokens/goerli/tokens_goerli_nft_curated.sql
+++ models/tokens/goerli/tokens_goerli_nft_curated.sql
@@ -8,5 +8,5 @@ SELECT
   contract_address, name, symbol
 FROM
   (VALUES
-  (0xE45a9E55B565b118b10a9815B9B2dE789831c6f1, 'C', 'Wrapped Cryptopunks'),
+  (0xE45a9E55B565b118b10a9815B9B2dE789831c6f1, 'C', 'Wrapped Cryptopunks')
    ) as temp_table (contract_address, name, symbol)
