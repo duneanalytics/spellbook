650dea88cd2595a989183f2ef52a3093050d6f80
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-01-12 17:57:49 -0500

    add goerli tokens tables

---
+++ models/tokens/goerli/tokens_goerli_erc20.sql
@@ -0,0 +1,6 @@
+{{ config( alias = 'erc20', tags=['static'])}}
+
+SELECT contract_address as contract_address, symbol, decimals
+FROM (VALUES
+       (0xa3e0Dfbf8DbD86e039f7CDB37682A776D66dae4b, 'USDC', 6),
+       ) AS temp_table (contract_address, symbol, decimals)
