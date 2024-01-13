f1044ef491aa4ec862c180f400a54965bd9ce17b
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-01-12 20:36:42 -0500

    drop

--- models/_sector/contracts/ethereum/contracts_ethereum_contract_mapping.sql
+++ models/_sector/contracts/ethereum/contracts_ethereum_contract_mapping.sql
@@ -6,7 +6,6 @@
         file_format ='delta',
         incremental_strategy='merge',
         unique_key = ['blockchain', 'contract_address'],
-        on_table_exists = 'drop',
         partition_by =['created_month']
   )
 }}
