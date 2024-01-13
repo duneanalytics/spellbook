a0bd50436650a7a6e24707e07393505ba5ea8f2d
ARDev097 <ardev029@gmail.com>
2024-01-13 15:57:24 +0530

    Added odos aggreagator trades data on optimism

---
+++ models/odos/optimism/odos_optimism_trades.sql
@@ -0,0 +1,51 @@
+{{ config(
+    schema = 'odos_optimism'
+    ,alias = 'trades'
+    ,materialized = 'incremental'
+    ,file_format = 'delta'
+    ,incremental_strategy = 'merge'
+    ,unique_key = ['tx_hash', 'evt_index']
+    ,incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+WITH 
+dexs as (
+        SELECT 
+            evt_block_time as block_time, 
+            evt_block_number as block_number,
+            json_extract_scalar(outputs[1], '$.receiver') AS taker,
+            sender as maker,
+            amountsIn[1] as token_sold_amount_raw, 
+            amountsOut[1] as token_bought_amount_raw, 
+            CAST(NULL as double) as amount_usd, 
+            tokensIn[1] as token_sold_address,
+            json_extract_scalar(outputs[1], '$.tokenAddress') AS token_bought_address, 
+            contract_address as project_contract_address, 
+            evt_tx_hash as tx_hash, 
+            evt_index
+        FROM 
+        {{ source('odos_optimism', 'OdosRouter_evt_Swapped') }}
+        {% if is_incremental() %}
+        WHERE {{incremental_predicate('evt_block_time')}}
+        {% endif %}
+)
+
+SELECT
+    'optimism' AS blockchain,
+    'odos' AS project,
+    '1' AS version,
+    CAST(date_trunc('DAY', dexs.block_time) AS date) AS block_date,
+    CAST(date_trunc('MONTH', dexs.block_time) AS date) AS block_month,
+    dexs.block_time,
+    dexs.block_number,
+    dexs.token_bought_amount_raw,
+    dexs.token_sold_amount_raw,
+    dexs.token_bought_address,
+    dexs.token_sold_address,
+    dexs.taker,
+    dexs.maker,
+    dexs.project_contract_address,
+    dexs.tx_hash,
+    dexs.evt_index
+FROM dexs
