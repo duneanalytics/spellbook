4058aafac9065406788af64359b87219e356e8f8
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-01-13 15:47:57 -0500

    add bnb predeploys

--- models/labels/addresses/infrastructure/identifier/system_addresses/labels_system_addresses.sql
+++ models/labels/addresses/infrastructure/identifier/system_addresses/labels_system_addresses.sql
@@ -7,27 +7,35 @@
 )}}
 
 
+WITH curated AS (
+        
+    SELECT blockchain, address, name, category, contributor, source, created_at, updated_at, model_name, label_type
+    FROM
+    (
+        VALUES
+    --     ,('solana',   'Vote111111111111111111111111111111111111111',  'Solana - Voting Address',          'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-02' ,    now(), 'system_addresses', 'identifier')
+        ('optimism', 0x420000000000000000000000000000000000000f,   'Optimism - L1 Gas Price Oracle',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-02' ,    now(), 'system_addresses', 'identifier')
+        ,('arbitrum', 0x00000000000000000000000000000000000a4b05,   'Arbitrum - ArbOS L1 Data Oracle',        'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-02' ,    now(), 'system_addresses', 'identifier')
+        {% for chain in all_op_chains() %}
+        ,('{{chain}}', 0xdeaddeaddeaddeaddeaddeaddeaddeaddead0001,   'Optimism - L1 Attributes Depositor Contract',        'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-28' ,    now(), 'system_addresses', 'identifier')
+        {% endfor %}
 
-SELECT blockchain, address as address, name, category, contributor, source, created_at, updated_at, model_name, label_type
-FROM
-(
-    VALUES
---     ,('solana',   'Vote111111111111111111111111111111111111111',  'Solana - Voting Address',          'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-02' ,    now(), 'system_addresses', 'identifier')
-    ('optimism', 0x420000000000000000000000000000000000000f,   'Optimism - L1 Gas Price Oracle',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-02' ,    now(), 'system_addresses', 'identifier')
-    ,('arbitrum', 0x00000000000000000000000000000000000a4b05,   'Arbitrum - ArbOS L1 Data Oracle',        'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-02' ,    now(), 'system_addresses', 'identifier')
-    {% for chain in all_op_chains() %}
-    ,('{{chain}}', 0xdeaddeaddeaddeaddeaddeaddeaddeaddead0001,   'Optimism - L1 Attributes Depositor Contract',        'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-28' ,    now(), 'system_addresses', 'identifier')
-    ,('{{chain}}', 0x4200000000000000000000000000000000000015,   'Optimism - L1 Attributes Predeployed Contract',        'infrastructure',   'msilb7',   'static',   TIMESTAMP '2022-12-28' ,    now(), 'system_addresses', 'identifier')
-    {% endfor %}
-    ,('bnb', 0x0000000000000000000000000000000000001007, 'BSC: Governance Hub',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
-    ,('bnb', 0x0000000000000000000000000000000000001006, 'BSC: Relayer Hub',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
-    ,('bnb', 0x0000000000000000000000000000000000001005, 'BSC: Relayer Incentivize',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
-    ,('bnb', 0x0000000000000000000000000000000000001001, 'BSC: SlashIndicator',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
-    ,('bnb', 0x0000000000000000000000000000000000001002, 'BSC: System Reward',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
-    ,('bnb', 0x0000000000000000000000000000000000001003, 'BSC: Tendermint Light Client',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
-    ,('bnb', 0x0000000000000000000000000000000000001004, 'BSC: Token Hub',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
-    ,('bnb', 0x0000000000000000000000000000000000001008, 'BSC: Token Manager',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
-    ,('bnb', 0x0000000000000000000000000000000000001000, 'BSC: Validator Set',   'infrastructure',   'msilb7',   'static',   TIMESTAMP '2024-01-13' ,    now(), 'system_addresses', 'identifier')
+    ) AS temp_table (blockchain, address, name, category, contributor, source, created_at, updated_at, model_name, label_type)
 
-) AS temp_table (blockchain, address, name, category, contributor, source, created_at, updated_at, model_name, label_type)
+)
+
+
+SELECT *
+SELECT blockchain, address, name, category, contributor, source, created_at, updated_at, model_name, label_type
+FROM curated
+
+UNION ALL
+
+SELECT blockchain, contract_address AS address, contract_name as name, 'infrastructure' as category,'contract_mapping' as contributor, 'static' as source, NOW() AS created_at, NOW() AS updated_at, 'system_addresses' AS model_name, 'identifier' as label_type
+FROM {{ ref('contracts_system_predeploys') }}
+LEFT JOIN curated c 
+    ON c.blockchain = pdp.blockchain 
+    AND c.address = pdp.contract_address
+
+WHERE c.address IS NULL
 
