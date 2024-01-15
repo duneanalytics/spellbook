a686617406890604b70961a51ba82be1debaaeb3
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-01-15 09:54:36 -0500

    Contract Updates - Remove 'Drop Table' and Simplify Min Block Time (#5158)
    * simplify initial check
    
    * rm drop tables
    
    * time field
    
    * add predeploy

--- models/_sector/contracts/base/contracts_base_contract_mapping.sql
+++ models/_sector/contracts/base/contracts_base_contract_mapping.sql
@@ -3,7 +3,6 @@
         schema = 'contracts_base',
         alias = 'contract_mapping',
         materialized ='table',
-        on_table_exists = 'drop',
         partition_by =['created_month']
   )
 }}
