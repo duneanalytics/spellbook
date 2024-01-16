8748a1e6970c256e530527dadd3baada235f074f
0xRob <0xRobin5@gmail.com>
2024-01-16 16:13:50 +0100

    fix compile

--- models/_sector/nft/trades/chains/old/nft_old_base_trades.sql
+++ models/_sector/nft/trades/chains/old/nft_old_base_trades.sql
@@ -8,8 +8,7 @@
 
 -- while we refactor more marketplace models, they should be removed here and added to the chain specific base_trades unions.
 {% set nft_models = [
- ref('element_bnb_events')
-,ref('element_avalanche_c_events')
+ref('element_avalanche_c_events')
 ,ref('magiceden_solana_events')
 ,ref('nftearth_optimism_events')
 ,ref('mooar_polygon_events')
