03ad6882704e6205c8036d47d172001c47aec5c9
Huang Geyang <Sukebeta@outlook.com>
2024-01-17 01:43:29 +0800

    Reimplement pharaoh_avalanche_c_base_trades in full

--- models/_sector/dex/trades/avalanche_c/platforms/pharaoh_avalanche_c_base_trades.sql
+++ models/_sector/dex/trades/avalanche_c/platforms/pharaoh_avalanche_c_base_trades.sql
@@ -9,44 +9,58 @@
     )
 }}
 
-WITH dex_raw AS (
-    {{
-        uniswap_compatible_v3_trades(
-            blockchain = 'avalanche_c'
-            , project = 'pharaoh'
-            , version = '1'
-            , Pair_evt_Swap = source('pharaoh_avalanche_c', 'ClPool_evt_Swap')
-            , Factory_evt_PoolCreated = source('pharaoh_avalanche_c', 'ClPoolFactory_evt_PoolCreated')
-        )
-    }}
+WITH dexs AS
+(
+    SELECT
+        t.evt_block_time AS block_time
+        ,t.recipient AS taker
+        ,CAST(NULL AS VARBINARY) AS maker
+        ,CASE
+            WHEN router.evt_tx_hash IS NULL
+                THEN CASE WHEN amount0 < INT256 '0' THEN abs(amount0) ELSE abs(amount1) END
+                ELSE router.amountOut
+            END AS token_bought_amount_raw
+        ,CASE
+            WHEN router.evt_tx_hash IS NULL
+                THEN CASE WHEN amount0 < INT256 '0' THEN abs(amount1) ELSE abs(amount0) END
+                ELSE router.inputAmount
+            END AS token_sold_amount_raw
+        ,NULL AS amount_usd
+        ,CASE WHEN amount0 < INT256 '0' THEN f.token0 ELSE f.token1 END AS token_bought_address
+        ,CASE WHEN amount0 < INT256 '0' THEN f.token1 ELSE f.token0 END AS token_sold_address
+        ,t.contract_address as project_contract_address
+        ,t.evt_tx_hash AS tx_hash
+        ,t.evt_index
+        ,t.evt_block_number AS block_number
+    FROM
+        {{ source('pharaoh_avalanche_c', 'ClPool_evt_Swap') }} t
+    INNER JOIN
+        {{ source('pharaoh_avalanche_c', 'ClPoolFactory_evt_PoolCreated') }} f
+        ON f.pool = t.contract_address
+    LEFT JOIN {{ source('glacier_avalanche_c', 'OdosRouterV2_evt_Swap') }} AS router
+        ON t.evt_tx_hash = router.evt_tx_hash
+        AND t.evt_index + 2 = router.evt_index
+    {% if is_incremental() %}
+    WHERE {{ incremental_predicate('t.evt_block_time') }}
+    {% endif %}
 )
 
 SELECT
-    dex_raw.blockchain,
-    dex_raw.project,
-    dex_raw.version,
-    dex_raw.block_month,
-    dex_raw.block_date,
-    dex_raw.block_time,
-    dex_raw.block_number,
-    CASE
-        WHEN router.evt_tx_hash IS NULL
-            THEN dex_raw.token_bought_amount_raw
-            ELSE router.amountOut
-        END AS token_bought_amount_raw,
-    CASE
-        WHEN router.evt_tx_hash IS NULL
-            THEN dex_raw.token_sold_amount_raw
-            ELSE router.inputAmount
-        END AS token_sold_amount_raw,
-    dex_raw.token_bought_address,
-    dex_raw.token_sold_address,
-    dex_raw.taker,
-    dex_raw.maker,
-    dex_raw.project_contract_address,
-    dex_raw.tx_hash,
-    dex_raw.evt_index
-FROM dex_raw
-LEFT JOIN {{ source('glacier_avalanche_c', 'OdosRouterV2_evt_Swap') }} AS router
-ON dex_raw.tx_hash = router.evt_tx_hash
-AND dex_raw.evt_index+2 = router.evt_index
+    'avalanche_c' AS blockchain
+    , 'pharaoh' AS project
+    , '1' AS version
+    , CAST(date_trunc('month', dexs.block_time) AS date) AS block_month
+    , CAST(date_trunc('day', dexs.block_time) AS date) AS block_date
+    , dexs.block_time
+    , dexs.block_number
+    , dexs.token_bought_amount_raw
+    , dexs.token_sold_amount_raw
+    , dexs.token_bought_address
+    , dexs.token_sold_address
+    , dexs.taker
+    , dexs.maker
+    , dexs.project_contract_address
+    , dexs.tx_hash
+    , dexs.evt_index
+FROM
+    dexs
