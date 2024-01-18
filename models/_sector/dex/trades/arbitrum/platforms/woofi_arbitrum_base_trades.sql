4aa7e0239abd255e32178fe101aea4d46ba4ccc1
ARDev097 <ardev029@gmail.com>
2024-01-18 15:03:38 +0530

    Added seeds records for woofi dex

--- models/_sector/dex/trades/arbitrum/platforms/woofi_arbitrum_base_trades.sql
+++ models/_sector/dex/trades/arbitrum/platforms/woofi_arbitrum_base_trades.sql
@@ -1,1 +1,22 @@
--- Currently Working
+{{
+    config(
+        schema = 'woofi_arbitrum',
+        alias ='base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+
+
+{{
+    woofi_compatible_trades(
+        blockchain = 'arbitrum',
+        project = 'woofi',
+        version = '2',
+        Pair_evt_Swap = source('woofi_swap_arbitrum', 'WooPPV2_1_evt_WooSwap'),
+    )
+}}
