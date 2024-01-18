5f986cb500b8257eb864c68d0ec7d4ab9bbe4c76
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-01-17 22:44:32 -0500

    test zksync

--- models/_sector/contracts/contracts_contract_mapping.sql
+++ models/_sector/contracts/contracts_contract_mapping.sql
@@ -2,7 +2,7 @@
   config(     
         schema = 'contracts',
         alias = 'contract_mapping',
-        post_hook='{{ expose_spells(\'["ethereum", "base", "optimism", "zora", "arbitrum", "celo", "polygon", "bnb", "avalanche_c", "fantom", "gnosis", "goerli"]\',
+        post_hook='{{ expose_spells(\'["ethereum", "base", "optimism", "zora", "arbitrum", "celo", "polygon", "bnb", "avalanche_c", "fantom", "gnosis", "goerli","zksync"]\',
                                     "sector",
                                     "contracts",
                                     \'["msilb7", "chuxin", "tomfutago"]\') }}'
@@ -23,6 +23,7 @@
  , ref('contracts_fantom_contract_mapping_dynamic')
  , ref('contracts_gnosis_contract_mapping')
  , ref('contracts_goerli_contract_mapping')
+ , ref('contracts_zksync_contract_mapping')
 ] %}
 -- todo: add chains for all EVMs in Dune
 
