f271fb4ae5a96245e789dc61fa9e1d27a8eb36f7
chain-l <hayagriva.coder@gmail.com>
2024-01-19 23:49:26 +0530

    Changing uniswap_compatible_trades.sql

--- macros/models/_sector/dex/uniswap_compatible_trades.sql
+++ macros/models/_sector/dex/uniswap_compatible_trades.sql
@@ -59,7 +59,9 @@ FROM
     , version = null
     , Pair_evt_Swap = null
     , Factory_evt_PoolCreated = null
+    , taker_column_name = 'recipient'
     , optional_columns = ['f.fee']
+    , pair_column_name = 'pool'
     )
 %}
 WITH dexs AS
@@ -67,8 +69,8 @@ WITH dexs AS
     SELECT
         t.evt_block_number AS block_number
         , t.evt_block_time AS block_time
-        , t.recipient AS taker
-        , CAST(NULL as VARBINARY) as maker
+        , t.{{ taker_column_name }} AS taker
+        , t.contract_address as maker
         , CASE WHEN amount0 < INT256 '0' THEN abs(amount0) ELSE abs(amount1) END AS token_bought_amount_raw -- when amount0 is negative it means trader_a is buying token0 from the pool
         , CASE WHEN amount0 < INT256 '0' THEN abs(amount1) ELSE abs(amount0) END AS token_sold_amount_raw
         , CASE WHEN amount0 < INT256 '0' THEN f.token0 ELSE f.token1 END AS token_bought_address
@@ -85,7 +87,7 @@ WITH dexs AS
         {{ Pair_evt_Swap }} t
     INNER JOIN
         {{ Factory_evt_PoolCreated }} f
-        ON f.pool = t.contract_address
+        ON f.{{ pair_column_name }} = t.contract_address
     {% if is_incremental() %}
     WHERE
         {{ incremental_predicate('t.evt_block_time') }}
