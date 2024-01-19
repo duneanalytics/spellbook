c214382b0eea5ac5a7a4dc3b77eae83060320fb3
tomfutago <35136350+tomfutago@users.noreply.github.com>
2024-01-19 16:19:36 +0000

    Consolidate lending sector - supply macro (#4853)
    * aave supply macros
    
    * fix macro and add arbitrum
    
    * avax
    
    * base
    
    * celo moola and amend sources
    
    * ethereum
    
    * add fantom
    
    * add optimism
    
    * add polygon
    
    * update seed schema
    
    * dummy seeds
    
    * fix v3 source
    
    * remove column
    
    * fix tests
    
    * filled seeds
    
    * fix seeds
    
    * update macro
    
    * update enrich macro and rearrange exposed views
    
    * fix schema
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

---
+++ models/_sector/lending/supply/ethereum/lending_ethereum_base_supply.sql
@@ -0,0 +1,37 @@
+{{
+  config(
+    schema = 'lending_ethereum',
+    alias = 'base_supply',
+    materialized = 'view'
+  )
+}}
+
+{%
+  set models = [
+    ref('aave_v1_ethereum_base_supply'),
+    ref('aave_v2_ethereum_base_supply'),
+    ref('aave_v3_ethereum_base_supply')
+  ]
+%}
+
+{% for model in models %}
+select
+  blockchain,
+  project,
+  version,
+  transaction_type,
+  token_address,
+  depositor,
+  withdrawn_to,
+  liquidator,
+  amount,
+  block_month,
+  block_time,
+  block_number,
+  tx_hash,
+  evt_index
+from {{ model }}
+{% if not loop.last %}
+union all
+{% endif %}
+{% endfor %}
