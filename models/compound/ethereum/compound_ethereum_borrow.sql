27affa2fb3b76674f2d09400fbbaa2899c70c68e
tomfutago <35136350+tomfutago@users.noreply.github.com>
2024-01-19 13:52:37 +0000

    Add compound to `lending.borrow` (#4899)
    * init commit
    
    * compound eth v2
    
    * eth compound v3
    
    * 3 more chains, exposed v2 view for eth
    
    * quotes
    
    * fix test
    
    * seeds
    
    * move exposed spells back to project locations
    
    * update test
    
    * separate ctokens
    
    * tweak macro, expose main spell
    
    * add incremental_predicate to enrich macro
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

---
+++ models/compound/ethereum/compound_ethereum_borrow.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    schema = 'compound_ethereum',
+    alias = 'borrow',
+    materialized = 'view',
+    post_hook = '{{ expose_spells(\'["ethereum"]\',
+                                    "project",
+                                    "compound",
+                                    \'["bizzyvinci", "hosuke", "tomfutago"]\') }}'
+  )
+}}
+
+select
+  blockchain,
+  project,
+  version,
+  transaction_type,
+  loan_type,
+  symbol,
+  token_address,
+  borrower,
+  repayer,
+  liquidator,
+  amount,
+  usd_amount,
+  block_month,
+  block_time,
+  block_number,
+  tx_hash,
+  evt_index
+from {{ ref('lending_borrow') }}
+where blockchain = 'ethereum'
+  and project = 'compound'
