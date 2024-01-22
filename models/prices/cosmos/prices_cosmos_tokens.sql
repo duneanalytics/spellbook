94a3ebb85b3d19781ff0c2ac38eb712d50a87788
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-01-22 13:37:11 -0500

    add

---
+++ models/prices/cosmos/prices_cosmos_tokens.sql
@@ -0,0 +1,19 @@
+{{ config(
+        schema='prices_cosmos',
+        alias = 'tokens',
+        materialized='table',
+        file_format = 'delta',
+        tags = ['static']
+        )
+}}
+SELECT
+    token_id
+    , blockchain
+    , symbol
+    , contract_address
+    , decimals
+FROM
+(
+    VALUES 
+        ('tia-celestia','cosmos','TIA',0x0000000000000000000000000000000000000000,8) --native token
+) as temp (token_id, blockchain, symbol, contract_address, decimals)
