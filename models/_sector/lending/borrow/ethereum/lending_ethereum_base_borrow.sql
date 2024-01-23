fbd6ece4ca77ed204ad6a865da4eb29bb45e0341
tomfutago <tomfutago@gmail.com>
2024-01-23 17:01:39 +0000

    more borrow models and cleaning up

--- models/_sector/lending/borrow/ethereum/lending_ethereum_base_borrow.sql
+++ models/_sector/lending/borrow/ethereum/lending_ethereum_base_borrow.sql
@@ -12,7 +12,8 @@
     ref('aave_v2_ethereum_base_borrow'),
     ref('aave_v3_ethereum_base_borrow'),
     ref('compound_v2_ethereum_base_borrow'),
-    ref('compound_v3_ethereum_base_borrow')
+    ref('compound_v3_ethereum_base_borrow'),
+    ref('radiant_ethereum_base_borrow')
   ]
 %}
 
