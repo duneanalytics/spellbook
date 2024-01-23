2792cfae7eec6afe0caca52617fdd0955847a881
Alan Ghobadi <aalan3@gmail.com>
2024-01-23 09:13:01 +0100

    Use right join for transfers due to prices performance

--- macros/models/_sector/tokens/transfers_enrich.sql
+++ macros/models/_sector/tokens/transfers_enrich.sql
@@ -28,12 +28,13 @@ SELECT t.blockchain
 , {{case_when_token_standard('(t.amount_raw / power(10, 18)) * prices.price',
     '(t.amount_raw / power(10, tokens_erc20.decimals)) * prices.price',
     'NULL')}} AS usd_amount
-FROM {{transfers_base}} t
-LEFT JOIN {{ref('tokens_erc20')}} tokens_erc20 on tokens_erc20.blockchain = '{{blockchain}}' AND tokens_erc20.contract_address = t.contract_address
-LEFT JOIN {{ source('prices', 'usd') }} prices ON prices.blockchain = '{{blockchain}}'
+FROM {{ source('prices', 'usd') }} prices
+-- Right join due to performance reasons
+RIGHT JOIN JOIN FROM {{transfers_base}} t  ON prices.blockchain = '{{blockchain}}'
     AND (
             prices.contract_address=t.contract_address
             OR t.contract_address IS NULL AND prices.contract_address=(SELECT wrapped_native_token_address FROM {{ ref('evms_info') }} WHERE blockchain='{{blockchain}}')
         )
     AND prices.minute = date_trunc('minute', t.block_time)
+LEFT JOIN {{ref('tokens_erc20')}} tokens_erc20 on tokens_erc20.blockchain = '{{blockchain}}' AND tokens_erc20.contract_address = t.contract_address
 {%- endmacro %}
