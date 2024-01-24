2ec4821a28ef742b06b664c88312ff6437573dfe
Hildebert Moulié <h.moulie@gmail.com>
2024-01-24 13:18:54 +0100

    fix

--- models/staking/ethereum/entities/entity_specific/staking_ethereum_entities_stakewise_v3.sql
+++ models/staking/ethereum/entities/entity_specific/staking_ethereum_entities_stakewise_v3.sql
@@ -9,7 +9,7 @@
 
 SELECT vault AS depositor_address
 , 'StakeWise' AS entity
-, CONCAT('StakeWise v3 Vault ', CAST(ROW_NUMBER() OVER (ORDER BY MIN(txs.block_time)) AS VARCHAR)) AS entity_unique_name
+, CONCAT('StakeWise v3 Vault ', CAST(ROW_NUMBER() OVER (ORDER BY MIN(evt_block_time)) AS VARCHAR)) AS entity_unique_name
 , 'Staking Pool' AS category
 FROM {{ source('stakewise_v3_ethereum', 'VaultsRegistry_evt_VaultAdded') }}
 {% if is_incremental() %}
