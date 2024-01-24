ef867e92cf855cd9a118a400c10cf1fef67cd24a
Landon Gingerich <landon.ging@gmail.com>
2024-01-24 14:35:06 -0600

    Fix token standard in `transfers_base` (#5212)
    * Fix token standard in `transfers_base`
    
    * Add null check
    
    * add prod_exclude tag to non zksync chains
    
    * remove prod_exclude from Zora
    
    * remove remaining prod_exclude flags

--- models/_sector/tokens/ethereum/tokens_ethereum_base_transfers.sql
+++ models/_sector/tokens/ethereum/tokens_ethereum_base_transfers.sql
@@ -1,13 +1,13 @@
 {{ config(
-        schema = 'tokens_ethereum',
-        alias = 'base_transfers',
-        partition_by = ['token_standard', 'block_date'],
-        materialized = 'incremental',
-        file_format = 'delta',
-        incremental_strategy = 'merge',
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-        unique_key = ['unique_key'],
-        )
+    schema = 'tokens_ethereum',
+    alias = 'base_transfers',
+    partition_by = ['token_standard', 'block_date'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    unique_key = ['unique_key'],
+    )
 }}
 
 {{transfers_base(
