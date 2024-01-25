6efe7f61c702cfd35eb979e3c790f67e368a7594
tomfutago <35136350+tomfutago@users.noreply.github.com>
2024-01-25 23:43:21 +0000

    Add spark protocol to lending (#5228)
    * init
    
    * seed
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/_sector/lending/supply/ethereum/lending_ethereum_base_supply.sql
+++ models/_sector/lending/supply/ethereum/lending_ethereum_base_supply.sql
@@ -14,7 +14,8 @@
     ref('compound_v2_ethereum_base_supply'),
     ref('compound_v3_ethereum_base_supply'),
     ref('radiant_ethereum_base_supply'),
-    ref('uwulend_ethereum_base_supply')
+    ref('uwulend_ethereum_base_supply'),
+    ref('spark_ethereum_base_supply')
   ]
 %}
 
