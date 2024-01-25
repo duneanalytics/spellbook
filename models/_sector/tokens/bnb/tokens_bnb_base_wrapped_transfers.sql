9f042820d5685e55279be61e8b9ee90e1df6fcfc
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-01-25 13:55:44 -0500

    break out wrapped into own model (#5234)

--- models/_sector/tokens/bnb/tokens_bnb_base_wrapped_transfers.sql
+++ models/_sector/tokens/bnb/tokens_bnb_base_wrapped_transfers.sql
@@ -1,7 +1,7 @@
 {{config(
     tags = ['base_transfers_macro'],
     schema = 'tokens_bnb',
-    alias = 'base_transfers',
+    alias = 'base_wrapped_transfers',
     partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
@@ -11,11 +11,14 @@
 )
 }}
 
-{{transfers_base(
-    blockchain='bnb',
-    traces = source('bnb','traces'),
-    transactions = source('bnb','transactions'),
-    erc20_transfers = source('erc20_bnb','evt_transfer'),
-    native_contract_address = null
+SELECT *
+FROM
+(
+    {{transfers_base_wrapped_token(
+        blockchain='bnb',
+        transactions = source('bnb','transactions'),
+        wrapped_token_deposit = source('bnb_bnb', 'WBNB_evt_Deposit'),
+        wrapped_token_withdrawal = source('bnb_bnb', 'WBNB_evt_Withdrawal')
+    )
+    }}
 )
-}}
