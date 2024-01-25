2870703dc561d0627d51c754b4541aceafe15ee9
tomfutago <tomfutago@gmail.com>
2024-01-25 12:10:14 +0000

    init

--- models/_sector/lending/borrow/ethereum/lending_ethereum_base_borrow.sql
+++ models/_sector/lending/borrow/ethereum/lending_ethereum_base_borrow.sql
@@ -14,7 +14,8 @@
     ref('compound_v2_ethereum_base_borrow'),
     ref('compound_v3_ethereum_base_borrow'),
     ref('radiant_ethereum_base_borrow'),
-    ref('uwulend_ethereum_base_borrow')
+    ref('uwulend_ethereum_base_borrow'),
+    ref('spark_ethereum_base_borrow')
   ]
 %}
 
