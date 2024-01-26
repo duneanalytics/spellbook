3cdeb384a5469c5e2a1e994f62be78a68dde8471
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-01-26 17:51:48 +0100

    remove incremental model from bpt supply (#5241)

--- models/balancer/polygon/balancer_v2_polygon_bpt_supply.sql
+++ models/balancer/polygon/balancer_v2_polygon_bpt_supply.sql
@@ -4,11 +4,8 @@
     config(
         schema='balancer_v2_' + blockchain,
         alias = 'bpt_supply',
-        materialized = 'incremental',
-        file_format = 'delta',
-        incremental_strategy = 'merge',
-        unique_key = ['day', 'token_address'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
+        materialized = 'table',
+        file_format = 'delta'
     )
 }}
 
