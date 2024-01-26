31ef5e011652bbf35993b59c430ac133f16d51e3
ARDev097 <ardev029@gmail.com>
2024-01-26 15:10:05 +0530

    removed macro file for woofi

--- models/_sector/dex/trades/arbitrum/platforms/woofi_arbitrum_base_trades.sql
+++ models/_sector/dex/trades/arbitrum/platforms/woofi_arbitrum_base_trades.sql
@@ -10,13 +10,41 @@
     )
 }}
 
+WITH dexs AS
+(
+        SELECT
+            t.evt_block_number AS block_number,
+            t.evt_block_time AS block_time,
+            t."from" AS taker,
+            t.to AS maker,
+            t.toAmount AS token_sold_amount_raw,
+            t.fromAmount AS token_bought_amount_raw,
+            t.toToken AS token_sold_address,
+            t.fromToken AS token_bought_address,
+            t.contract_address AS project_contract_address,
+            t.evt_tx_hash AS tx_hash,
+            t.evt_index
+        FROM {{ source('woofi_swap_arbitrum', 'WooPPV2_1_evt_WooSwap') }} t
+        {% if is_incremental() %}
+        WHERE {{ incremental_predicate('evt_block_time') }}
+        {% endif %}
+)
 
-
-{{
-    woofi_compatible_trades(
-        blockchain = 'arbitrum',
-        project = 'woofi',
-        version = '2',
-        Pair_evt_Swap = source('woofi_swap_arbitrum', 'WooPPV2_1_evt_WooSwap'),
-    )
-}}
+SELECT
+    'arbitrum' AS blockchain,
+    'woofi' AS project,
+    '2' AS version,
+    CAST(date_trunc('month', dexs.block_time) AS date) AS block_month,
+    CAST(date_trunc('day', dexs.block_time) AS date) AS block_date,
+    dexs.block_time,
+    dexs.block_number,
+    dexs.token_bought_amount_raw,
+    dexs.token_sold_amount_raw,
+    dexs.token_bought_address,
+    dexs.token_sold_address,
+    dexs.taker,
+    dexs.maker,
+    dexs.project_contract_address,
+    dexs.tx_hash,
+    dexs.evt_index
+FROM dexs
