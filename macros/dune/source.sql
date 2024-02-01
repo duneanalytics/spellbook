267bf279c4a6fc520fb3b69ac924bf22b9b33905
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2024-02-01 15:37:00 -0800

    test

--- macros/dune/source.sql
+++ macros/dune/source.sql
@@ -1,6 +1,6 @@
 {% macro source(source_name, table_name) %}
   {% set rel = builtins.source(source_name, table_name) %}
-  {%- if target.type == 'trino' -%}
+  {%- if target.type == 'trino' and rel.database != 'hive'-%}
     {% set newrel = rel.replace_path(database="delta_prod") %}
     {% do return(newrel) %}
   {%- else -%}
