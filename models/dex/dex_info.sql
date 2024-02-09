4bda5d02b48f485eaae8f2cc3621ccbb6a5d8ef1
jason4276 <jasonreddy4276@gmail.com>
2024-02-09 13:32:43 +0530

    Added plantbaseswap dex on base chain

--- models/dex/dex_info.sql
+++ models/dex/dex_info.sql
@@ -115,4 +115,5 @@ FROM (VALUES
     , ('velocimeter', 'Velocimeter', 'Direct', 'Velocimeter')
     , ('dooar', 'Dooar', 'Direct', 'dooar')
     , ('gridex', 'Gridex', 'Direct', 'GridexProtocol')
+    , ('plantbaseswap','Plantbaseswap','Direct','PlantBaseSwap')
     ) AS temp_table (project, name, marketplace_type, x_username)
