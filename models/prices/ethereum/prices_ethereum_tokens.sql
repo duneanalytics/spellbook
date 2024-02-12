b44376d7045e7706e8fe7bdf70c49469d1dfb8ee
GoodDataBadGuy <157877550+GoodDataBadGuy@users.noreply.github.com>
2024-02-13 05:52:52 +0800

    update prices.usd with LRT token and USDC (#5295)
    * add Fjord Foundry LBP project token to tokens.erc20
    
    * dedup
    
    * update prices.usd with LRT token and USDC
    
    ---------
    
    Co-authored-by: jeff-dude <jeff@dune.xyz>
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/prices/ethereum/prices_ethereum_tokens.sql
+++ models/prices/ethereum/prices_ethereum_tokens.sql
@@ -1640,7 +1640,10 @@ FROM
     ('gyd-gyro-dollar','ethereum','GYD',0xe07F9D810a48ab5c3c914BA3cA53AF14E4491e8A, 18),
     ('ezeth-renzo-restaked-eth','ethereum','EZETH',0xbf5495efe5db9ce00f80364c8b423567e58d2110, 18),
     ('mubi-multibit', 'ethereum', 'MUBI', 0x38e382f74dfb84608f3c1f10187f6bef5951de93, 18),
-    ('peas-peapods', 'ethereum', 'PEAS', 0x02f92800F57BCD74066F5709F1Daa1A4302Df875, 18)
+    ('peas-peapods', 'ethereum', 'PEAS', 0x02f92800F57BCD74066F5709F1Daa1A4302Df875, 18),
+    ('aeth-ankreth', 'ethereum', 'ankrETH', 0xe95a203b1a91a908f9b9ce46459d101078c2c3cb, 18),
+    ('sweth-swell-ethereum', 'ethereum', 'swETH', 0xf951e335afb289353dc249e82926178eac7ded78, 18),
+    ('meth-meth', 'ethereum', 'mETH', 0xd5f7838f5c461feff7fe49ea5ebaf7728bb0adfa, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
 where contract_address not in (
     -- bad price feeds
