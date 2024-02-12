6f9a1ca4aa0ab08fcc477890a1c9dab484428186
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-02-12 13:54:22 +0000

    Update prices_arbitrum_tokens.sql

--- models/prices/arbitrum/prices_arbitrum_tokens.sql
+++ models/prices/arbitrum/prices_arbitrum_tokens.sql
@@ -125,5 +125,7 @@ FROM
     ('ush-unsheth','arbitrum','USH',0x51a80238b5738725128d3a3e06ab41c1d4c05c74,18),
     ('hdn-hydranet','arbitrum','HDN',0x3404149e9ee6f17fb41db1ce593ee48fbdcd9506,18),
     ('vela-vela-token','arbitrum','VELA',0x088cd8f5ef3652623c22d48b1605dcfe860cd704,18),
-    ('choke-artichoke','arbitrum','CHOKE',0x6fc2680d8ad8e8312191441b4eca9eff8d06b45a,18)
+    ('choke-artichoke','arbitrum','CHOKE',0x6fc2680d8ad8e8312191441b4eca9eff8d06b45a,18),
+    ('cbeth-coinbase-wrapped-staked-eth','arbitrum','CBETH',0x82af49447d8a07e3bd95bd0d56f35241523fbab1, 18),        
+    ('aeth-ankreth','arbitrum','ANKRETH',0x82af49447d8a07e3bd95bd0d56f35241523fbab1, 18)        
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
