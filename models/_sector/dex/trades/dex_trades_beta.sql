000e218edbdba9167a51e3a0641ad18402aa9c31
Huang Geyang <Sukebeta@outlook.com>
2024-02-12 17:49:46 +0800

    Rewrite dex_trades_beta

--- models/_sector/dex/trades/dex_trades_beta.sql
+++ models/_sector/dex/trades/dex_trades_beta.sql
@@ -41,7 +41,30 @@ WITH curve AS (
 , as_is_dexs AS (
     {% for model in as_is_models %}
     SELECT
-        *
+        blockchain
+        , project
+        , version
+        , block_month
+        , block_date
+        , block_time
+        , block_number
+        , token_bought_symbol
+        , token_sold_symbol
+        , token_pair
+        , token_bought_amount
+        , token_sold_amount
+        , token_bought_amount_raw
+        , token_sold_amount_raw
+        , amount_usd
+        , token_bought_address
+        , token_sold_address
+        , taker
+        , maker
+        , project_contract_address
+        , tx_hash
+        , tx_from
+        , tx_to
+        , evt_index
     FROM
         {{ model }}
     {% if is_incremental() %}
@@ -54,17 +77,37 @@ WITH curve AS (
     {% endfor %}
 )
 
-SELECT
-    *
-FROM
-    curve
-UNION ALL
-SELECT
-    *
-FROM
-    as_is_dexs
-UNION ALL
-SELECT
-    *
-FROM
-    dexs
+{% set cte_to_union = ['curve', 'as_is_dexs', 'dexs'] %}
+
+{% for cte in cte_to_union %}
+    SELECT
+        blockchain
+        , project
+        , version
+        , block_month
+        , block_date
+        , block_time
+        , block_number
+        , token_bought_symbol
+        , token_sold_symbol
+        , token_pair
+        , token_bought_amount
+        , token_sold_amount
+        , token_bought_amount_raw
+        , token_sold_amount_raw
+        , amount_usd
+        , token_bought_address
+        , token_sold_address
+        , taker
+        , maker
+        , project_contract_address
+        , tx_hash
+        , tx_from
+        , tx_to
+        , evt_index
+    FROM
+        {{ cte }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+{% endfor %}
