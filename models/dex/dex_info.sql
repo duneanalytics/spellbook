df218f7ecf68c5fc5b376b3f0e48444e9a30b9d3
Huang Geyang <Sukebeta@outlook.com>
2024-02-14 04:39:12 +0800

    Finalizing dex.trades_beta (#5303)
    * Add oneinch and zeroex as as-is models into dex.trades_beta
    
    * Add block_number in oneinch_lop_own_trades
    
    * Add block_number in zeroex_native_trades and zeroex_native_fills
    
    * Rewrite dex_trades_beta
    
    * Modify 1inch LOP in dex_info
    
    * Update models/_sector/dex/trades/dex_trades_beta.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * comments for context, update curve macro
    
    * fix curve call, remove prices ref
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>

--- models/dex/dex_info.sql
+++ models/dex/dex_info.sql
@@ -67,7 +67,7 @@ FROM (VALUES
     , ('saddle', 'Saddle', 'Direct', 'saddlefinance')
     , ('oneinch', '1inch', 'Aggregator', '1inch')
     , ('1inch', '1inch', 'Aggregator', '1inch') -- To Fix, should be just oneinch
-    , ('1inch Limit Order Protocol', '1inch Limit Order Protocol', 'Aggregator', '1inch') -- To Fix, should be just oneinch or maybe oneinch_limit_order_protocol
+    , ('1inch LOP', '1inch Limit Order Protocol', 'Aggregator', '1inch') -- To Fix, should be just oneinch or maybe oneinch_limit_order_protocol
     , ('zeroex', '0x', 'Aggregator', '0xProject')
     , ('0x API', '0x API', 'Aggregator', '0xProject') -- To Fix, should be just zeroex
     , ('paraswap', 'ParaSwap', 'Aggregator', 'paraswap')
