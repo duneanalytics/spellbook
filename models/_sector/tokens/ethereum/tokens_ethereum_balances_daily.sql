9a5390c131425eba6d0b08fa0e90ca63bd27d8e7
Alan Ghobadi <aalan3@gmail.com>
2024-02-13 21:34:27 +0100

    Materialize ethereum

--- models/_sector/tokens/ethereum/tokens_ethereum_balances_daily.sql
+++ models/_sector/tokens/ethereum/tokens_ethereum_balances_daily.sql
@@ -1,10 +1,17 @@
 {{ config(
         schema = 'tokens_ethereum',
         alias = 'balances_daily',
-        materialized = 'view'
+        file_format = 'delta',
+        materialized='incremental',
+        incremental_strategy='merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        unique_key = ['unique_key'],
         )
 }}
 
+select *,
+ {{ dbt_utils.generate_surrogate_key(['day', 'type', 'address', 'contract_address', 'token_id']) }} as unique_key
+ FROM (
 {{
     balances_enrich(
         balances_base = ref('tokens_ethereum_base_balances_daily'),
@@ -12,3 +19,4 @@
         daily=true,
     )
 }}
+)
