e99a61fe42d3da2e04936f6f214d5403297b5eaa
Huang Geyang <Sukebeta@outlook.com>
2024-02-14 04:39:12 +0800

    Finalizing dex.trades_beta (#5303)
    * Add oneinch and zeroex as as-is models into dex.trades_beta
    
    * Add block_number in oneinch_lop_own_trades
    
    * Add block_number in zeroex_native_trades and zeroex_native_fills
    
    * Rewrite dex_trades_beta
    
    * Modify 1inch LOP in dex_info
    
    * Update models/_sector/dex/trades/dex_trades_beta.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * comments for context, update curve macro
    
    * fix curve call, remove prices ref
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>

--- models/_sector/dex/trades/dex_trades_beta.sql
+++ models/_sector/dex/trades/dex_trades_beta.sql
@@ -10,7 +10,15 @@
     )
 }}
 
+-- keep existing dbt lineages for the following projects, as the team built themselves and use the spells throughout the entire lineage
+{% set as_is_models = [
+    ref('oneinch_lop_own_trades')
+    , ref('zeroex_native_trades')
+
+] %}
+
 WITH curve AS (
+    -- due to curve having increased complexity to determine token_bought_amount / token_sold_amount, enrich separately
     {{
         enrich_curve_dex_trades(
             base_trades = ref('dex_base_trades')
@@ -18,7 +26,6 @@ WITH curve AS (
             , curve_ethereum = ref('curvefi_ethereum_base_trades')
             , curve_optimism = ref('curvefi_optimism_base_trades')
             , tokens_erc20_model = source('tokens', 'erc20')
-            , prices_model = source('prices', 'usd')
         )
     }}
 )
@@ -31,12 +38,81 @@ WITH curve AS (
         )
     }}
 )
-SELECT
-    *
-FROM
-    curve
-UNION ALL
-SELECT
-    *
-FROM
-    dexs
+, as_is_dexs AS (
+    {% for model in as_is_models %}
+    SELECT
+        blockchain
+        , project
+        , version
+        , block_month
+        , block_date
+        , block_time
+        , block_number
+        , token_bought_symbol
+        , token_sold_symbol
+        , token_pair
+        , token_bought_amount
+        , token_sold_amount
+        , token_bought_amount_raw
+        , token_sold_amount_raw
+        , amount_usd
+        , token_bought_address
+        , token_sold_address
+        , taker
+        , maker
+        , project_contract_address
+        , tx_hash
+        , tx_from
+        , tx_to
+        , evt_index
+    FROM
+        {{ model }}
+    {% if is_incremental() %}
+    WHERE
+        {{ incremental_predicate('block_time') }}
+    {% endif %}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
+
+{% set cte_to_union = [
+    'curve'
+    , 'as_is_dexs'
+    , 'dexs'
+    ] 
+%}
+
+{% for cte in cte_to_union %}
+    SELECT
+        blockchain
+        , project
+        , version
+        , block_month
+        , block_date
+        , block_time
+        , block_number
+        , token_bought_symbol
+        , token_sold_symbol
+        , token_pair
+        , token_bought_amount
+        , token_sold_amount
+        , token_bought_amount_raw
+        , token_sold_amount_raw
+        , amount_usd
+        , token_bought_address
+        , token_sold_address
+        , taker
+        , maker
+        , project_contract_address
+        , tx_hash
+        , tx_from
+        , tx_to
+        , evt_index
+    FROM
+        {{ cte }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+{% endfor %}
