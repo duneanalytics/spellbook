cb462ae60f6a59c32dc24765b52a4689fd0d9d77
viniabussafi <vinicius@balancerlabs.dev>
2024-02-14 17:34:39 +0000

    built-in dex prices

--- macros/models/_project/balancer/balancer_bpt_prices_macro.sql
+++ macros/models/_project/balancer/balancer_bpt_prices_macro.sql
@@ -24,13 +24,63 @@ WITH pool_labels AS (
         GROUP BY 1, 2, 3
     ),
 
+dex_trades as (
+    SELECT 
+        d.token_bought_address as contract_address, 
+        COALESCE(d.amount_usd/d.token_bought_amount, d.amount_usd/(d.token_bought_amount_raw/POW(10, er.decimals))) as price, 
+        d.block_time, 
+        d.blockchain
+    FROM {{ ref('dex_trades') }} d 
+    LEFT JOIN {{ source('tokens', 'erc20') }} er
+        ON d.token_bought_address = er.contract_address
+        AND d.blockchain = er.blockchain
+    WHERE d.amount_usd > 0 
+        AND d.token_bought_amount_raw > UINT256 '0'
+
+    UNION ALL
+
+    SELECT 
+        d.token_sold_address as contract_address, 
+        COALESCE(d.amount_usd/d.token_sold_amount, d.amount_usd/(d.token_sold_amount_raw/POW(10, er.decimals))) as price, 
+        d.block_time, 
+        d.blockchain
+    FROM {{ ref('dex_trades') }} d 
+    LEFT JOIN {{ source('tokens', 'erc20') }} er
+        ON d.token_sold_address = er.contract_address
+        AND d.blockchain = er.blockchain
+    WHERE d.amount_usd > 0 
+        AND d.token_sold_amount_raw > UINT256 '0'
+        {% if is_incremental() %}
+        AND d.block_time >= date_trunc('day', now() - interval '7' Day)
+        {% endif %}
+),
+
+
+    final_dex_prices as(
+    SELECT 
+        CAST(date_trunc('month', hour) as date) as block_month, -- for partitioning 
+        * 
+    FROM 
+    (
+        SELECT 
+            date_trunc('hour', block_time) as hour, 
+            contract_address,
+            blockchain,
+            approx_percentile(price, 0.5) AS median_price,
+            COUNT(price) as sample_size 
+        FROM dex_trades
+        GROUP BY 1, 2, 3
+        HAVING COUNT(price) >= 5 
+    ) tmp
+    ),    
+
     dex_prices_1 AS (
         SELECT
             date_trunc('day', HOUR) AS DAY,
             contract_address AS token,
             approx_percentile(median_price, 0.5) AS price,
             sum(sample_size) AS sample_size
-        FROM {{ ref('dex_prices') }}
+        FROM final_dex_prices
         GROUP BY 1, 2
         HAVING sum(sample_size) > 7
     ),
