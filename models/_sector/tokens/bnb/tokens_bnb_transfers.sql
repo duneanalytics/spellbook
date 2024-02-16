c3b0cdae26660ea7413861ed2880b2777294e4f1
jeff-dude <jeff@dune.xyz>
2024-02-16 12:55:44 -0500

    update target filter to partition column

--- models/_sector/tokens/bnb/tokens_bnb_transfers.sql
+++ models/_sector/tokens/bnb/tokens_bnb_transfers.sql
@@ -6,7 +6,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     unique_key = ['unique_key'],
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
     post_hook='{{ expose_spells(\'["bnb"]\',
                                 "sector",
                                 "tokens",
