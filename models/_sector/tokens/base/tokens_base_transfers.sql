9574a3de28686a07171b7448aa87687b1bd42e37
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-02-16 17:18:37 -0500

    finalize chain-level transfers spells (#5360)
    * reformat macro
    
    * materialize chain level enhance transfers
    
    * update target filter to partition column
    
    * remove cross-chain base level
    
    * rename base chain transfers file
    
    * add start date param for full refresh performance
    
    * fix schema file
    
    * exclude larger models for now
    
    * exclude all but eth/poly
    
    * reorder columns
    
    * update eth/poly schema files
    
    * fix all other chain schema files
    
    * exclude eth
    
    * exclude poly, run only eth
    
    * update unique key to add block_date
    
    * exclude base level
    
    * re-enable all chains

--- models/_sector/tokens/base/tokens_base_transfers.sql
+++ models/_sector/tokens/base/tokens_base_transfers.sql
@@ -1,5 +1,5 @@
 {{config(
-    schema = 'tokens_gnosis',
+    schema = 'tokens_base',
     alias = 'transfers',
     partition_by = ['block_date'],
     materialized = 'incremental',
@@ -7,7 +7,7 @@
     incremental_strategy = 'merge',
     unique_key = ['block_date','unique_key'],
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
-    post_hook='{{ expose_spells(\'["gnosis"]\',
+    post_hook='{{ expose_spells(\'["base"]\',
                                 "sector",
                                 "tokens",
                                 \'["aalan3", "jeff-dude"]\') }}'
@@ -16,10 +16,10 @@
 
 {{
     transfers_enrich(
-        base_transfers = ref('tokens_gnosis_base_transfers')
+        base_transfers = ref('tokens_base_base_transfers')
         , tokens_erc20_model = source('tokens', 'erc20')
         , prices_model = source('prices', 'usd')
         , evms_info_model = ref('evms_info')
-        , transfers_start_date = '2018-10-09'
+        , transfers_start_date = '2023-06-15'
     )
 }}
