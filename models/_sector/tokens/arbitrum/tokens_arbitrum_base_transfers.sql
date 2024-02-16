f4ca8f5a4406e3af91a239f03c3d8b3b69d19f5b
jeff-dude <jeff@dune.xyz>
2024-02-16 18:37:13 -0500

    Revert "finalize chain-level transfers spells (#5360)"
    This reverts commit ca7eac60b8883467979c02f428cf6666216ec2a5.

--- models/_sector/tokens/arbitrum/tokens_arbitrum_base_transfers.sql
+++ models/_sector/tokens/arbitrum/tokens_arbitrum_base_transfers.sql
@@ -1,4 +1,5 @@
 {{config(
+    tags = ['base_transfers_macro'],
     schema = 'tokens_arbitrum',
     alias = 'base_transfers',
     partition_by = ['block_date'],
@@ -6,7 +7,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-    unique_key = ['block_date','unique_key'],
+    unique_key = ['unique_key'],
 )
 }}
 
--- models/_sector/tokens/base/tokens_base_transfers_view.sql
+++ models/_sector/tokens/base/tokens_base_transfers_view.sql
@@ -1,12 +1,12 @@
 {{config(
-    schema = 'tokens_zksync',
+    schema = 'tokens_base',
     alias = 'transfers',
     materialized = 'view',
 )
 }}
 
 {{transfers_enrich(
-    blockchain='zksync',
-    transfers_base = ref('tokens_zksync_base_transfers'),
+    blockchain='base',
+    transfers_base = ref('tokens_base_base_transfers'),
     native_symbol = 'ETH'
 )}}
