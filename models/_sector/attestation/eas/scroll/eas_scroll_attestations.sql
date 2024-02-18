97b195343471f23d6189a732cc2092c762500f22
tomfutago <tomfutago@gmail.com>
2024-02-18 14:36:38 +0000

    add scroll

---
+++ models/_sector/attestation/eas/scroll/eas_scroll_attestations.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    schema = 'eas_scroll',
+    alias = 'attestations',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['schema_uid', 'attestation_uid'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+  )
+}}
+
+{{
+  eas_attestations(
+    blockchain = 'scroll',
+    project = 'eas',
+    version = '1'
+  )
+}}
