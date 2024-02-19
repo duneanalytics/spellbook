e5110c44a64f24204e9faf579d9c5c88c944bdb0
tomfutago <tomfutago@gmail.com>
2024-02-19 16:12:59 +0000

    init

---
+++ models/_sector/lending/supply/scroll/platforms/layer_bank_scroll_base_supply.sql
@@ -0,0 +1,29 @@
+{{
+  config(
+    schema = 'layer_bank_scroll',
+    alias = 'base_supply',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['transaction_type', 'token_address', 'tx_hash', 'evt_index'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+  )
+}}
+
+{%
+  set config_sources = [
+    {'contract': 'lETH'},
+    {'contract': 'lUSDC'},
+    {'contract': 'lwstETH'},
+  ]
+%}
+
+{{
+  lending_compound_v2_compatible_supply(
+    blockchain = 'scroll',
+    project = 'layer_bank',
+    version = '1',
+    decoded_project = 'layer_bank',
+    sources = config_sources
+  )
+}}
