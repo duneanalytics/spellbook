f878e042f958646e1b1cd95ab6b16d5fc9cae249
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-02-21 09:59:11 -0500

    SPE-266 finalize chain-level transfers spells (#5364)
    * Revert "Revert "finalize chain-level transfers spells (#5360)" (#5363)"
    
    This reverts commit 3c2b54a4d6d9b27f1ffb9cfa64dc16a27e7f099f.
    
    * exclude all but bnb
    
    * add block_date to join condition
    
    * add block_date to CTE
    
    * make only base active for a test
    
    * test blockchain param performance
    
    * remove prod_exclude tag, add blockchain variable

--- models/_sector/tokens/base/tokens_base_transfers.sql
+++ models/_sector/tokens/base/tokens_base_transfers.sql
@@ -1,5 +1,5 @@
 {{config(
-    schema = 'tokens_bnb',
+    schema = 'tokens_base',
     alias = 'transfers',
     partition_by = ['block_date'],
     materialized = 'incremental',
@@ -7,7 +7,7 @@
     incremental_strategy = 'merge',
     unique_key = ['block_date','unique_key'],
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
-    post_hook='{{ expose_spells(\'["bnb"]\',
+    post_hook='{{ expose_spells(\'["base"]\',
                                 "sector",
                                 "tokens",
                                 \'["aalan3", "jeff-dude"]\') }}'
@@ -16,11 +16,11 @@
 
 {{
     transfers_enrich(
-        base_transfers = ref('tokens_bnb_base_transfers')
+        base_transfers = ref('tokens_base_base_transfers')
         , tokens_erc20_model = source('tokens', 'erc20')
         , prices_model = source('prices', 'usd')
         , evms_info_model = ref('evms_info')
-        , transfers_start_date = '2020-08-29'
-        , blockchain = 'bnb'
+        , transfers_start_date = '2023-06-15'
+        , blockchain = 'base'
     )
 }}
