ce2285c066aa054845dbe68d991a6fc4497d9482
Tse Kit Yam <tsekityam@users.noreply.github.com>
2024-02-22 05:24:47 +0800

    updated unique keys

--- models/_sector/nft/trades/chains/solana/platforms/magiceden_v3_solana_trades.sql
+++ models/_sector/nft/trades/chains/solana/platforms/magiceden_v3_solana_trades.sql
@@ -7,7 +7,7 @@
         ,file_format = 'delta'
         ,incremental_strategy = 'merge'
         ,incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
-        ,unique_key = ['project','trade_category','outer_instruction_index','inner_instruction_index','account_merkle_tree','leaf_id','tx_id']
+        ,unique_key = ['instruction','account_merkle_tree','leaf_id','tx_id']
         ,post_hook='{{ expose_spells(\'["solana"]\',
                                     "project",
                                     "magiceden",
