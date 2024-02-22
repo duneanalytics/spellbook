3af8e2837e8bd5c184681f05fa74e67074b83307
Landon Gingerich <landon.ging@gmail.com>
2024-02-22 22:56:12 +0000

    fix pancakeswap arb -> zksync reference

--- models/_sector/dex/trades/zksync/dex_zksync_base_trades.sql
+++ models/_sector/dex/trades/zksync/dex_zksync_base_trades.sql
@@ -7,7 +7,7 @@
 
 {% set base_models = [
     ref('maverick_zksync_base_trades')
-    , ref('pancakeswap_v2_arbitrum_base_trades')
+    , ref('pancakeswap_v2_zksync_base_trades')
 ] %}
 
 WITH base_union AS (
