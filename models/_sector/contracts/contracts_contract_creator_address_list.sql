ac396045f862c22aaf715c8c27f58d78a0f00117
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-02-22 09:21:05 -0500

    push

--- models/_sector/contracts/contracts_contract_creator_address_list.sql
+++ models/_sector/contracts/contracts_contract_creator_address_list.sql
@@ -1086,6 +1086,8 @@ WITH curated_list AS (
       ---
 
       ,(0x6ddb6B042b4f3aE8CC73EBd371fCc70762279473, 'Witness Protocol')
+      ,(0xF1C0dF2381Ac4a6Cac9C866DdbdE3c3D641a1337, 'Layer3')
+      ,(0x13845Fc81a84765f417A058b1F4B8e0C98e1F2Ee, 'KyberSwap')
 
   ) as temp_table (creator_address, contract_project)
 )
