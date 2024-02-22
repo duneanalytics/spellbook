644537be512fc9856919cdf835dfde540a4ce16e
ARDev097 <147229733+ARDev097@users.noreply.github.com>
2024-02-22 18:30:18 +0530

    Added sterling finance and sharkyswap dex (#5385)
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/_sector/dex/trades/arbitrum/dex_arbitrum_base_trades.sql
+++ models/_sector/dex/trades/arbitrum/dex_arbitrum_base_trades.sql
@@ -37,6 +37,8 @@
     , ref('woofi_arbitrum_base_trades')
     , ref('zigzag_arbitrum_base_trades')
     , ref('gridex_arbitrum_base_trades')
+    , ref('sterling_finance_arbitrum_base_trades')
+    , ref('sharkyswap_arbitrum_base_trades')
     , ref('uniswap_v2_arbitrum_base_trades')
 ] %}
 
