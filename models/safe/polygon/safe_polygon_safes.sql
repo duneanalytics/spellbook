09e722bd16cbc1f0bc5fa160a2f1b9562f73e34c
Peter <157015397+safeintern@users.noreply.github.com>
2024-02-23 17:37:45 +0800

    Update safe_polygon_safes.sql
    Added authors and tx_success filters

--- models/safe/polygon/safe_polygon_safes.sql
+++ models/safe/polygon/safe_polygon_safes.sql
@@ -11,7 +11,7 @@
         post_hook='{{ expose_spells(\'["polygon"]\',
                                     "project",
                                     "safe",
-                                    \'["tschubotz", "hosuke"]\') }}'
+                                    \'["tschubotz", "hosuke", "danielpartida", "aliceha", "safeintern"]\') }}'
     ) 
 }}
 
