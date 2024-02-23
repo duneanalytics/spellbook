97f0b0a819cedfb7e53b0ee8a1889c3a3518c1e8
Peter <157015397+safeintern@users.noreply.github.com>
2024-02-23 17:37:00 +0800

    Update safe_optimism_safes.sql
    Added authors and tx_success filters

--- models/safe/optimism/safe_optimism_safes.sql
+++ models/safe/optimism/safe_optimism_safes.sql
@@ -11,7 +11,7 @@
         post_hook='{{ expose_spells(\'["optimism"]\',
                                     "project",
                                     "safe",
-                                    \'["frank_maseo", "tschubotz", "hosuke"]\') }}'
+                                    \'["frank_maseo", "tschubotz", "hosuke", "danielpartida", "aliceha", "safeintern"]\') }}'
     ) 
 }}
 
@@ -45,6 +45,7 @@ where et.success = true
         0xb63e800d -- setup method v1.1.0, v1.1.1, v1.2.0, v1.3.0, v1.3.0L2
     )
     and et.gas_used > 10000  -- to ensure the setup call was successful. excludes e.g. setup calls with missing params that fallback
+    and tx_success = true
     {% if not is_incremental() %}
     and et.block_time > TIMESTAMP '2021-11-17' -- for initial query optimisation
     {% endif %}
