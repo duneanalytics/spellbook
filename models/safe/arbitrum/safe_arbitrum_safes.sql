18734e1fa0a4e166716764bdad53f508df880349
Peter <157015397+safeintern@users.noreply.github.com>
2024-02-23 17:31:13 +0800

    Update safe_arbitrum_safes.sql
    Added authors

--- models/safe/arbitrum/safe_arbitrum_safes.sql
+++ models/safe/arbitrum/safe_arbitrum_safes.sql
@@ -11,7 +11,7 @@
         post_hook='{{ expose_spells(\'["arbitrum"]\',
                                     "project",
                                     "safe",
-                                    \'["tschubotz", "hosuke"]\') }}'
+                                    \'["tschubotz", "hosuke", "danielpartida", "aliceha", "safe_intern"]\') }}'
     ) 
 }}
 
