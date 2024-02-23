ff7e86c716da31f9928f4fbd77723177a39e8393
Peter <157015397+safeintern@users.noreply.github.com>
2024-02-23 17:37:16 +0800

    Update safe_goerli_safes.sql

--- models/safe/goerli/safe_goerli_safes.sql
+++ models/safe/goerli/safe_goerli_safes.sql
@@ -11,7 +11,7 @@
         post_hook='{{ expose_spells(\'["goerli"]\',
                                     "project",
                                     "safe",
-                                    \'["tschubotz", "hosuke", "danielpartida", "aliceha", "safe_intern"]\') }}'
+                                    \'["tschubotz", "hosuke", "danielpartida", "aliceha", "safeintern"]\') }}'
     ) 
 }}
 
