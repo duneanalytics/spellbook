20e79963f25aaaafeab122c7480e05c8951f8c17
archie17277 <archie17277@gmail.com>
2024-02-25 17:50:03 +0530

    Added elk_finance dex data on Optimism chain

--- models/dex/dex_info.sql
+++ models/dex/dex_info.sql
@@ -131,4 +131,5 @@ FROM (VALUES
     , ('sharkyswap', 'Sharky Swap', 'Direct', 'SharkySwapFi')
     , ('zebra', 'Zebra', 'Direct', 'zebra')
     , ('scrollswap', 'ScrollSwap', 'Direct', 'scrollswap')
+    , ('elk_finance', 'Elk Finance', 'Direct', 'Elk Finance')   
     ) AS temp_table (project, name, marketplace_type, x_username)
