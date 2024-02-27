f6734db74549f625ca54b6df49f24a4c60c70fba
Henrystats <65398020+henrystats@users.noreply.github.com>
2024-02-27 14:41:27 +0100

    Add paraswap base

--- models/paraswap/base/paraswap_base_trades.sql
+++ models/paraswap/base/paraswap_base_trades.sql
@@ -1,22 +1,12 @@
 {{ config(
-	    
         alias = 'trades',
-        post_hook='{{ expose_spells(\'["avalanche_c","fantom","arbitrum","bnb","ethereum","optimism","polygon","base"]\',
-                                "project",
-                                "paraswap",
-                                \'["Henrystats","springzh"]\') }}'
+        schema = 'paraswap_base'
+        
         )
 }}
 
 {% set paraswap_models = [
-ref('paraswap_avalanche_c_trades')
-,ref('paraswap_fantom_trades')
-,ref('paraswap_arbitrum_trades')
-,ref('paraswap_bnb_trades')
-,ref('paraswap_ethereum_trades')
-,ref('paraswap_optimism_trades')
-,ref('paraswap_polygon_trades')
-,ref('paraswap_base_trades')
+ref('paraswap_v5_base_trades')
 ] %}
 
 
@@ -54,4 +44,4 @@ FROM (
     {% endif %}
     {% endfor %}
 )
-
+;
