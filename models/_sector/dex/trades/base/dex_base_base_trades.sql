368caccdf3d93ea9fcd8084e8a5985aeec091944
intensodefi <intensodefi@protonmail.com>
2024-02-27 15:30:43 +0700

    Add rocketswap to dex.trades

--- models/_sector/dex/trades/base/dex_base_base_trades.sql
+++ models/_sector/dex/trades/base/dex_base_base_trades.sql
@@ -39,6 +39,7 @@
     , ref('oasisswap_base_base_trades')
     , ref('leetswap_v2_base_base_trades')
     , ref('openocean_base_base_trades')
+    , ref('rocketswap_base_base_trades')
 ] %}
 
 WITH base_union AS (
