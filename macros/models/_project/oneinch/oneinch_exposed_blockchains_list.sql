57dbb1114adb38281ee4eb5b0865c37fe297434c
grkhr <eduardgorkh@gmail.com>
2024-02-28 19:25:20 +0400

    surrogate key

--- macros/models/_project/oneinch/oneinch_exposed_blockchains_list.sql
+++ macros/models/_project/oneinch/oneinch_exposed_blockchains_list.sql
@@ -1,5 +1,13 @@
 {% macro oneinch_exposed_blockchains_list() %}
    {{ return([
-        'ethereum'
+        'ethereum', 
+        'optimism', 
+        'arbitrum', 
+        'avalanche_c', 
+        'polygon', 
+        'bnb', 
+        'gnosis', 
+        'fantom', 
+        'base', 
     ]) }}
 {% endmacro %}
