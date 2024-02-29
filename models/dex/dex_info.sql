541261a36830ee295679b9c797d51711c457b25f
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-02-29 21:20:40 +0100

    added to dex info

--- models/dex/dex_info.sql
+++ models/dex/dex_info.sql
@@ -137,4 +137,5 @@ FROM (VALUES
     , ('rocketswap', 'RocketSwap', 'Direct', 'RocketSwap_Labs')
     , ('alienbase', 'AlienBase', 'Direct', 'alienbasedex')
     , ('swapbased', 'SwapBased', 'Direct', 'swap_based')
+    , ('voltswap', 'VoltSwap', 'Direct', 'VoltSwapFinance')
     ) AS temp_table (project, name, marketplace_type, x_username)
