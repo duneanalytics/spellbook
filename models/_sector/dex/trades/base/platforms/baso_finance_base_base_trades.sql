a913d6f76e876c96ad205baa677fe04113008c34
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-02-29 19:00:10 +0100

    added to seed

--- models/_sector/dex/trades/base/platforms/baso_finance_base_base_trades.sql
+++ models/_sector/dex/trades/base/platforms/baso_finance_base_base_trades.sql
@@ -1,20 +1,21 @@
-{{ config(
-    schema = 'baso_finance_base'
-    , alias = 'base_trades'
-    , materialized = 'incremental'
-    , file_format = 'delta'
-    , incremental_strategy = 'merge'
-    , unique_key = ['tx_hash', 'evt_index']
-    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+{{
+    config(
+        schema = 'baso_finance_base',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
     )
 }}
 
 {{
-    uniswap_compatible_v3_trades(
-        blockchain = 'base'
-        , project = 'baso_finance'
-        , version = '1'
-        , Pair_evt_Swap = source('baso_finance_base', 'Pair_evt_Swap')
-        , Factory_evt_PoolCreated = source('baso_finance_base', 'PairFactory_evt_PairCreated')
+    uniswap_compatible_v2_trades(
+        blockchain = 'base',
+        project = 'baso_finance',
+        version = '2',
+        Pair_evt_Swap = source('baso_finance_base', 'Pair_evt_Swap'),
+        Factory_evt_PairCreated = source('baso_finance_base', 'PairFactory_evt_PairCreated')
     )
 }}
