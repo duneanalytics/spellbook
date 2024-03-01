4daf9e6ac814caf6246a01800d964ad45b33c9d4
Alan Ghobadi <aalan3@gmail.com>
2024-03-01 16:36:16 +0100

    Add prices daily

---
+++ models/prices/prices_usd_daily.sql
@@ -0,0 +1,23 @@
+{{ config(
+        schema='prices',
+        alias = 'usd_daily',
+        materialized='table', -- daily refresh
+        post_hook='{{ expose_spells(\'["ethereum", "solana", "arbitrum", "base", "gnosis", "optimism", "bnb", "avalanche_c", "polygon", "scroll", "zksync"]\',
+                                    "sector",
+                                    "prices",
+                                    \'["aalan3"]\') }}'
+        )
+}}
+
+select * from (
+select
+    cast(date_trunc('day', minute) as date) as day,
+    blockchain,
+    contract_address,
+    decimals,
+    symbol,
+    price,
+    row_number() OVER (partition by date_trunc('day', minute), blockchain, contract_address, symbol order by minute) as row_number
+from {{ source('prices', 'usd') }}
+) where row_number = 1
+
