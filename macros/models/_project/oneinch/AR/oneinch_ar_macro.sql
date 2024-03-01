2c5e3ceaf8e872fc7ba048a649b4b2f114117e2e
grkhr <eduardgorkh@gmail.com>
2024-03-01 21:17:54 +0400

    Revert "Revert "Revert "Revert "Revert "try easy dates"""""
    This reverts commit 9e734c685388b69ff3a6eb60c65979502c3ed56f.

--- macros/models/_project/oneinch/AR/oneinch_ar_macro.sql
+++ macros/models/_project/oneinch/AR/oneinch_ar_macro.sql
@@ -77,7 +77,7 @@
         "ExchangeV1": {
             "version": "0.1",
             "blockchains": ["ethereum"],
-            "start": "2014-02-20",
+            "start": "2019-06-03",
             "end": "2020-09-18",
             "methods": {
                 "aggregate": samples["aggregate"],
@@ -86,7 +86,7 @@
         "ExchangeV2": {
             "version": "0.2",
             "blockchains": ["ethereum"],
-            "start": "2014-02-20",
+            "start": "2019-06-10",
             "end": "2020-09-18",
             "methods": {
                 "aggregate": samples["aggregate"],
@@ -95,7 +95,7 @@
         "ExchangeV3": {
             "version": "0.3",
             "blockchains": ["ethereum"],
-            "start": "2014-02-20",
+            "start": "2019-06-18",
             "end": "2020-09-18",
             "methods": {
                 "aggregate": samples["aggregate"],
@@ -104,7 +104,7 @@
         "ExchangeV4": {
             "version": "0.4",
             "blockchains": ["ethereum"],
-            "start": "2014-02-20",
+            "start": "2019-07-18",
             "end": "2020-09-18",
             "methods": {
                 "aggregate": samples["aggregate"],
@@ -113,7 +113,7 @@
         "ExchangeV5": {
             "version": "0.5",
             "blockchains": ["ethereum"],
-            "start": "2014-02-20",
+            "start": "2019-07-18",
             "end": "2020-09-18",
             "methods": {
                 "aggregate": samples["aggregate"],
@@ -122,7 +122,7 @@
         "ExchangeV6": {
             "version": "0.6",
             "blockchains": ["ethereum"],
-            "start": "2014-02-20",
+            "start": "2019-07-19",
             "end": "2020-09-18",
             "methods": {
                 "aggregate": samples["aggregate"],
@@ -131,7 +131,7 @@
         "ExchangeV7": {
             "version": "0.7",
             "blockchains": ["ethereum"],
-            "start": "2014-02-20",
+            "start": "2019-09-17",
             "end": "2019-09-29",
             "methods": {
                 "swap": dict(samples["aggregate"], src_token_amount="fromTokenAmount", dst_token_amount_min="minReturnAmount"),
@@ -140,7 +140,7 @@
         "AggregationRouterV1": {
             "version": "1",
             "blockchains": ["ethereum"],
-            "start": "2014-02-20",
+            "start": "2019-09-28",
             "methods": {
                 "swap": dict(samples["aggregate"], src_token_amount="fromTokenAmount", dst_token_amount_min="minReturnAmount"),
             },
@@ -148,7 +148,7 @@
         "AggregationRouterV2": {
             "version": "2",
             "blockchains": ["ethereum", "bnb"],
-            "start": "2024-02-20",
+            "start": "2020-11-04",
             "methods": {
                 "swap":           samples["swap"],
                 "discountedSwap": samples["swap"],
@@ -157,7 +157,7 @@
         "AggregationRouterV3": {
             "version": "3",
             "blockchains": ["ethereum", "bnb", "polygon", "arbitrum", "optimism"],
-            "start": "2024-02-20",
+            "start": "2021-03-14",
             "methods": {
                 "swap":              samples["swap"],
                 "discountedSwap":    samples["swap"],
@@ -168,7 +168,7 @@
         "AggregationRouterV4": {
             "version": "4",
             "blockchains": ["ethereum", "bnb", "polygon", "arbitrum", "optimism", "avalanche_c", "gnosis", "fantom"],
-            "start": "2024-02-20",
+            "start": "2021-11-05",
             "methods": {
                 "swap":                      samples["swap"],
                 "discountedSwap":            dict(samples["swap"], blockchains=["bnb", "polygon"]),
@@ -185,7 +185,7 @@
         "AggregationRouterV5": {
             "version": "5",
             "blockchains": ["ethereum", "bnb", "polygon", "arbitrum", "optimism", "avalanche_c", "gnosis", "fantom", "base", "zksync"],
-            "start": "2024-02-20",
+            "start": "2022-11-04",
             "methods": {
                 "swap":                      samples["swap"],
                 "clipperSwap":               samples["clipper"],
@@ -202,7 +202,7 @@
         "AggregationRouterV6": {
             "version": "6",
             "blockchains": ["ethereum", "bnb", "polygon", "arbitrum", "optimism", "avalanche_c", "gnosis", "fantom", "base", "zksync"],
-            "start": "2024-02-20",
+            "start": "2024-02-12",
             "methods": {
                 "swap":          dict(samples["swap"], src_token_amount="output_spentAmount"),
                 "clipperSwap":   dict(samples["clipper"], src_token_address="substr(cast(srcToken as varbinary), 13)", blockchains=["ethereum","bnb","polygon","arbitrum","optimism","avalanche_c","gnosis","fantom","base"]),
