4124105ead1f64981df91ad3fe1495f27f9bae3a
Henrystats <65398020+henrystats@users.noreply.github.com>
2024-03-02 16:56:02 +0100

    test uniswap without macro

--- models/_sector/dex/pools/ethereum/platforms/uniswap_v3_ethereum_pools.sql
+++ models/_sector/dex/pools/ethereum/platforms/uniswap_v3_ethereum_pools.sql
@@ -10,17 +10,21 @@
 }}
 
 SELECT 
-1 as temp
-
-{#
--- {{
---     uniswap_v3_compatible_pools(
---         blockchain = 'ethereum'
---         , project = 'uniswap'
---         , version = '3'
---         , Factory_evt_PoolCreated = source('uniswap_v3_ethereum', 'Factory_evt_PoolCreated')
---         , pool_column_name = 'pool'
---         , fee_column_name = 'fee'
---     )
--- }} 
-#}
+    'ethereum' AS blockchain
+    , 'uniswap' AS project
+    , '2' AS version
+    , pool
+    , 0 as fee -- testing to see if this is the issue
+    , array_agg(
+        ROW(f.token0, f.token1)
+    ) AS tokens
+    , 2 AS tokens_in_pool
+    , evt_block_time AS creation_block_time
+    , evt_block_number AS creation_block_number
+    , contract_address
+FROM {{source('uniswap_v2_ethereum', 'Factory_evt_PairCreated')}}
+{% if is_incremental() %}
+WHERE
+{{ incremental_predicate('f.evt_block_time') }} f 
+{% endif %}
+GROUP BY 1, 2, 3, 4, 5, 7, 8, 9, 10
