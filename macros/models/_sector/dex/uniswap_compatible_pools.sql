dd1bc93bc7f4d4638d93fa078560daed8b8c26ca
Henrystats <65398020+henrystats@users.noreply.github.com>
2024-03-03 00:42:52 +0100

    test fix

--- macros/models/_sector/dex/uniswap_compatible_pools.sql
+++ macros/models/_sector/dex/uniswap_compatible_pools.sql
@@ -2,9 +2,9 @@
     blockchain = null
     , project = null
     , version = null
-    , Factory_evt_PairCreated = null
     , hardcoded_fee = null 
     , fee_column_name = null
+    , Factory_evt_PairCreated = null
     , pool_column_name = 'pair'
     , token0_column_name = 'token0'
     , token1_column_name = 'token1'
@@ -26,7 +26,7 @@ SELECT
     , CAST(NULL as BIGINT) as fee 
     {% endif %}
     , array_agg(
-        ROW(f.{{ token0_column_name }}, f.{{ token1_column_name }})
+        ROW(f.{{ token0_column_name }} AS token0, f.{{ token1_column_name }} AS token1)
     ) AS tokens
     , 2 AS tokens_in_pool
     , evt_block_time AS creation_block_time
@@ -36,7 +36,7 @@ FROM
 {{ Factory_evt_PairCreated }} f
 {% if is_incremental() %}
 WHERE
-{{ incremental_predicate('f.evt_block_time') }}
+{{ incremental_predicate('evt_block_time') }}
 {% endif %}
 GROUP BY 1, 2, 3, 4, 5, 7, 8, 9, 10
 
