58f7ef84b7ad7694768bb20e8206ba87cd097d17
Alan Ghobadi <aalan3@gmail.com>
2024-03-05 15:24:56 +0100

    Fix typo

--- models/_sector/tokens/avalanche_c/tokens_avalanche_c_balances.sql
+++ models/_sector/tokens/avalanche_c/tokens_avalanche_c_balances.sql
@@ -10,7 +10,7 @@
 }}
 
 with balances_raw as (
-{{balances_fix_schema(source('tokens_avalanche_c', 'balances_avalanche_c'),'avalanche_c'), '0x0000000000000000000000000000000000000000'}}
+{{balances_fix_schema(source('tokens_avalanche_c', 'balances_avalanche_c'),'avalanche_c', '0x0000000000000000000000000000000000000000')}}
 )
 
 {{
