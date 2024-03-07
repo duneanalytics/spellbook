84835aec2aa13c673555c0a665b37916bdcd5324
pntemi <ploy@rabbithole.gg>
2024-03-06 23:42:15 -0800

    removed incremental predicates because it caused compile error

--- models/boost/arbitrum/boost_arbitrum_deployed.sql
+++ models/boost/arbitrum/boost_arbitrum_deployed.sql
@@ -6,7 +6,6 @@
         file_format='delta',
         incremental_strategy='merge',
         unique_key=['boost_address', 'boost_id'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
     )
 }}
 
