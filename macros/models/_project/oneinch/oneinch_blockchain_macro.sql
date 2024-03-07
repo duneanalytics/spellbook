bc635356e5dbe3ec5a8a7cc8571896814e7afb4a
grkhr <eduardgorkh@gmail.com>
2024-03-07 23:41:48 +0400

    more easy dates

--- macros/models/_project/oneinch/oneinch_blockchain_macro.sql
+++ macros/models/_project/oneinch/oneinch_blockchain_macro.sql
@@ -2,7 +2,7 @@
 
 {% set
     config = {
-        "ethereum":      "1,            'ETH',     0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2, 'https://etherscan.io',         timestamp '2014-03-01 20:11', array[0xa88800cd213da5ae406ce248380802bd53b47647]",
+        "ethereum":      "1,            'ETH',     0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2, 'https://etherscan.io',         timestamp '2024-03-01 20:11', array[0xa88800cd213da5ae406ce248380802bd53b47647]",
         "bnb":           "56,           'BNB',     0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c, 'https://bscscan.com',          timestamp '2024-03-01 14:37', array[0x1d0ae300eec4093cee4367c00b228d10a5c7ac63]",
         "polygon":       "137,          'MATIC',   0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270, 'https://polygonscan.com',      timestamp '2024-03-01 09:39', array[0x1e8ae092651e7b14e4d0f93611267c5be19b8b9f]",
         "arbitrum":      "42161,        'ETH',     0x82af49447d8a07e3bd95bd0d56f35241523fbab1, 'https://arbiscan.io',          timestamp '2024-03-01 10:27', array[0x4bc3e539aaa5b18a82f6cd88dc9ab0e113c63377]",
