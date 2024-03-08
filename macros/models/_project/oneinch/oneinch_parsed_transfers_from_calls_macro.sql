62af144f79b12e68d72812797a92d2b0b8c1aefc
Max-Morrow <amidmoro@gmail.com>
2024-03-08 19:02:40 +0300

    fix & updated wrapper deposits & withdraws

--- macros/models/_project/oneinch/oneinch_parsed_transfers_from_calls_macro.sql
+++ macros/models/_project/oneinch/oneinch_parsed_transfers_from_calls_macro.sql
@@ -16,8 +16,9 @@
 {% set null_address = '0x0000000000000000000000000000000000000000' %}
 
 
-with t as (
-    -- deposit is accounted as ETH
+with
+
+transfers as (
     select
         '{{ blockchain }}' as blockchain
         , block_number
@@ -45,65 +46,39 @@ with t as (
         end as amount
         , case
             when {{ selector }} in ({{ transferFrom_selector }}, {{ burn_selector }}) then substr(input, 17, 20) -- transferFrom, burn
-            when {{ selector }} in ({{ mint_selector }}) then {{ null_address }} -- mint
-            else "from" -- transfer, native, withdraw, deposit
+            when {{ selector }} = {{ mint_selector }} then {{ null_address }} -- mint
+            when {{ selector }} = {{ withdraw_selector }} then "to" -- withdraw
+            else "from" -- transfer, native, deposit
         end as transfer_from
         , case
             when {{ selector }} in ({{ transfer_selector }}, {{ mint_selector }}) then substr(input, 17, 20) -- transfer, mint
             when {{ selector }} = {{ transferFrom_selector }} then substr(input, 49, 20) -- transferFrom
-            when {{ selector }} in ({{ burn_selector }}, {{ withdraw_selector }}, {{ deposit_selector }}) then {{ null_address }} -- burn, withdraw, deposit
-            else "to" -- native
+            when {{ selector }} = {{ burn_selector }} then {{ null_address }} -- burn
+            when {{ selector }} = {{ withdraw_selector }} then "from" -- withdraw
+            else "to" -- native, deposit
         end as transfer_to
     from {{ source(blockchain, 'traces') }}
-    where (
-            {{ selector }} = {{ transfer_selector }} and length(input) = 68 -- transfer
-            or {{ selector }} = {{ transferFrom_selector }} and length(input) = 100 -- transferFrom
-            or {{ selector }} = {{ mint_selector }} and length(input) = 68 -- mint
-            or {{ selector }} = {{ burn_selector }} and length(input) = 68 -- burn
-            or {{ selector }} = {{ deposit_selector }} and length(input) = 4 -- deposit
-            or {{ selector }} = {{ withdraw_selector }} and length(input) = 36 -- withdraw
-            or value > uint256 '0'
+    where
+        (
+            length(input) = 68 and {{ selector }} in ({{ transfer_selector }}, {{ mint_selector }}, {{ burn_selector }}) -- transfer, mint, burn
+            or length(input) = 100 and {{ selector }} = {{ transferFrom_selector }} -- transferFrom
+            or length(input) = 36 and {{ selector }} = {{ withdraw_selector }}  -- withdraw
+            or value > uint256 '0' -- native, deposit
         )
         and call_type = 'call'
         and tx_success
         and success
 )
+-- the wrapper deposit includes two transfers: native and wrapper
 
+-- output
 
+select *
+from transfers
 
-, deposit_as_wrapped as (
-    select 
-        blockchain
-        , block_number
-        , block_time
-        , tx_hash
-        , transfer_trace_address
-        , type
-        , "to" as contract_address
-        , value as amount
-        , {{ null_address }} as transfer_from
-        , "from" as transfer_to
-    from t
-    where type = 'deposit' and value > uint256 '0'
-)
+union all
 
-
-
-select 
-    blockchain
-    , block_number
-    , block_time
-    , tx_hash
-    , transfer_trace_address
-    , type
-    , contract_address
-    , amount
-    , transfer_from
-    , transfer_to
-from t
-
-union all 
-
+-- adding wrapper transfers when deposit
 select
     blockchain
     , block_number
@@ -111,10 +86,11 @@ select
     , tx_hash
     , transfer_trace_address
     , type
-    , contract_address
+    , transfer_to as contract_address
     , amount
-    , transfer_from
-    , transfer_to
-from deposit_as_wrapped
+    , transfer_to as transfer_from
+    , transfer_from as transfer_to
+from transfers
+where type = 'deposit'
 
 {% endmacro %}
