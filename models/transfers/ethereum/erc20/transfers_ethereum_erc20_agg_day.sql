b155dbdfffb38a5d827a521a493b8b35544d905c
RantumBits <rantum@rantum.xyz>
2024-03-08 09:50:22 -0800

    remove unneeed files

--- models/transfers/ethereum/erc20/transfers_ethereum_erc20_agg_day.sql
+++ models/transfers/ethereum/erc20/transfers_ethereum_erc20_agg_day.sql
@@ -1,27 +1,29 @@
 {{ config(
-        schema = tranfers_ethereum,
+tags=['prod_exclude'],
         alias = 'erc20_agg_day',
         materialized ='incremental',
-        partition_by = ['block_month'],
         file_format ='delta',
         incremental_strategy='merge',
-        unique_key = ['block_day', 'wallet_address', 'token_address']
+        unique_key='unique_transfer_id'
         )
 }}
-
+/*
+    note: this spell has not been migrated to dunesql, therefore is only a view on spark
+        please migrate to dunesql to ensure up-to-date logic & data
+*/
 select
-    tr.blockchain,
-    date_trunc('day', tr.evt_block_time) as block_day,
-    block_month,
+    'ethereum' as blockchain,
+    date_trunc('day', tr.evt_block_time) as day,
     tr.wallet_address,
     tr.token_address,
     t.symbol,
+    tr.wallet_address || '-' || tr.token_address || '-' || date_trunc('day', tr.evt_block_time) as unique_transfer_id,
     sum(tr.amount_raw) as amount_raw,
     sum(tr.amount_raw / power(10, t.decimals)) as amount
 from {{ ref('transfers_ethereum_erc20') }} tr
 left join {{ source('tokens_ethereum', 'erc20') }} t on t.contract_address = tr.token_address
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
-WHERE tr.evt_block_time >= date_trunc('day', now() - interval '3' Day)
+where tr.evt_block_time >= date_trunc('day', now() - interval '1 week')
 {% endif %}
-GROUP BY 1, 2, 3, 4, 5, 6
+group by 1, 2, 3, 4, 5, 6
--- models/transfers/ethereum/erc20/transfers_ethereum_erc20_rolling_hour.sql
+++ models/transfers/ethereum/erc20/transfers_ethereum_erc20_rolling_hour.sql
@@ -1,17 +1,24 @@
 {{ config(
-        schema = tranfers_ethereum,
-        alias = 'erc20_rolling_hour')
+tags=['prod_exclude'],
+        alias = 'erc20_rolling_hour'
+        )
 }}
-
-        SELECT
-            blockchain, 
-            block_hour, 
+/*
+    note: this spell has not been migrated to dunesql, therefore is only a view on spark
+        please migrate to dunesql to ensure up-to-date logic & data
+*/
+        select
+            'ethereum' as blockchain,
+            hour,
             wallet_address,
             token_address,
-            symbol, 
-            CAST(NOW() as timestamp) as last_updated,
-            ROW_NUMBER() OVER (PARTITION BY token_address, wallet_address ORDER BY block_hour DESC) as recency_index,
-            SUM(amount_raw) OVER (PARTITION BY token_address, wallet_address ORDER BY block_hour) as amount_raw, 
-            SUM(amount) OVER (PARTITION BY token_address, wallet_address ORDER BY block_hour) as amount          
-        FROM 
-        {{ ref('transfers_ethereum_erc20_agg_hour') }}
+            symbol,
+            NOW() as last_updated,
+            row_number() over (partition by token_address, wallet_address order by hour desc) as recency_index,
+            sum(amount_raw) over (
+                partition by token_address, wallet_address order by hour
+            ) as amount_raw,
+            sum(amount) over (
+                partition by token_address, wallet_address order by hour
+            ) as amount
+        from {{ ref('transfers_ethereum_erc20_agg_hour') }}
