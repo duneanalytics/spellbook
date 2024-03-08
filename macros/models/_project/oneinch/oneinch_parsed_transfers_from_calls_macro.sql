600d520a2f6361f7dfeb6281bdb7d35ad85ecfb0
grkhr <eduardgorkh@gmail.com>
2024-03-08 17:22:03 +0400

    oneinch_parsed_transfers_from_calls_macro fix

--- macros/models/_project/oneinch/oneinch_parsed_transfers_from_calls_macro.sql
+++ macros/models/_project/oneinch/oneinch_parsed_transfers_from_calls_macro.sql
@@ -16,48 +16,105 @@
 {% set null_address = '0x0000000000000000000000000000000000000000' %}
 
 
+with t as (
+    -- deposit is accounted as ETH
+    select
+        '{{ blockchain }}' as blockchain
+        , block_number
+        , block_time
+        , tx_hash
+        , trace_address as transfer_trace_address
+        , "from"
+        , "to"
+        , value
+        , case
+            when {{ selector }} = {{ transfer_selector }} then 'transfer'
+            when {{ selector }} = {{ transferFrom_selector }} then 'transferFrom'
+            when {{ selector }} = {{ mint_selector }} then 'mint'
+            when {{ selector }} = {{ burn_selector }} then 'burn'
+            when {{ selector }} = {{ deposit_selector }} then 'deposit'
+            when {{ selector }} = {{ withdraw_selector }} then 'withdraw'
+            else 'native'
+        end as type
+        , if(value > uint256 '0', 0xae, "to") as contract_address
+        , case
+            when {{ selector }} in ({{ transfer_selector }}, {{ mint_selector }}, {{ burn_selector }}) then bytearray_to_uint256(substr(input, 37, 32)) -- transfer, mint, burn
+            when {{ selector }} = {{ transferFrom_selector }} then bytearray_to_uint256(substr(input, 69, 32)) -- transferFrom
+            when {{ selector }} = {{ withdraw_selector }} then bytearray_to_uint256(substr(input, 5, 32)) -- withdraw
+            else value -- native, deposit
+        end as amount
+        , case
+            when {{ selector }} in ({{ transferFrom_selector }}, {{ burn_selector }}) then substr(input, 17, 20) -- transferFrom, burn
+            when {{ selector }} in ({{ mint_selector }}) then {{ null_address }} -- mint
+            else "from" -- transfer, native, withdraw, deposit
+        end as transfer_from
+        , case
+            when {{ selector }} in ({{ transfer_selector }}, {{ mint_selector }}) then substr(input, 17, 20) -- transfer, mint
+            when {{ selector }} = {{ transferFrom_selector }} then substr(input, 49, 20) -- transferFrom
+            when {{ selector }} in ({{ burn_selector }}, {{ withdraw_selector }}, {{ deposit_selector }}) then {{ null_address }} -- burn, withdraw, deposit
+            else "to" -- native
+        end as transfer_to
+    from {{ source(blockchain, 'traces') }}
+    where (
+            {{ selector }} = {{ transfer_selector }} and length(input) = 68 -- transfer
+            or {{ selector }} = {{ transferFrom_selector }} and length(input) = 100 -- transferFrom
+            or {{ selector }} = {{ mint_selector }} and length(input) = 68 -- mint
+            or {{ selector }} = {{ burn_selector }} and length(input) = 68 -- burn
+            or {{ selector }} = {{ deposit_selector }} and length(input) = 4 -- deposit
+            or {{ selector }} = {{ withdraw_selector }} and length(input) = 36 -- withdraw
+            or value > uint256 '0'
+        )
+        and call_type = 'call'
+        and tx_success
+        and success
+)
 
-select
-    '{{ blockchain }}' as blockchain
+
+
+, deposit_as_wrapped as (
+    select 
+        blockchain
+        , block_number
+        , block_time
+        , tx_hash
+        , transfer_trace_address
+        , type
+        , "to" as contract_address
+        , value as amount
+        , {{ null_address }} as transfer_from
+        , "from" as transfer_to
+    from t
+    where type = 'deposit' and value > uint256 '0'
+)
+
+
+
+select 
+    blockchain
     , block_number
     , block_time
     , tx_hash
-    , trace_address as transfer_trace_address
-    , case 
-        when {{ selector }} in ({{ deposit_selector }}, {{ withdraw_selector }}) then "to" -- deposit, withdraw
-        when value > uint256 '0' then 0xae -- native
-        else "to" -- transfer, transferFrom, mint, burn
-    end as contract_address
-    , case
-        when {{ selector }} in ({{ transfer_selector }}, {{ mint_selector }}, {{ burn_selector }}) then bytearray_to_uint256(substr(input, 37, 32)) -- transfer, mint, burn
-        when {{ selector }} = {{ transferFrom_selector }} then bytearray_to_uint256(substr(input, 69, 32)) -- transferFrom
-        when {{ selector }} = {{ withdraw_selector }} then bytearray_to_uint256(substr(input, 5, 32)) -- withdraw
-        else value -- native, deposit
-    end as amount
-    , case
-        when {{ selector }} in ({{ transferFrom_selector }}, {{ burn_selector }}) then substr(input, 17, 20) -- transferFrom, burn
-        when {{ selector }} in ({{ mint_selector }}, {{ deposit_selector }}) then {{ null_address }} -- mint, deposit
-        else "from" -- transfer, native, withdraw
-    end as transfer_from
-    , case
-        when {{ selector }} in ({{ transfer_selector }}, {{ mint_selector }}) then substr(input, 17, 20) -- transfer, mint
-        when {{ selector }} = {{ transferFrom_selector }} then substr(input, 49, 20) -- transferFrom
-        when {{ selector }} in ({{ burn_selector }}, {{ withdraw_selector }}) then {{ null_address }} -- burn, withdraw
-        when {{ selector }} = {{ deposit_selector }} then "from"
-        else "to" -- native
-    end as transfer_to
-from {{ source(blockchain, 'traces') }}
-where (
-        {{ selector }} = {{ transfer_selector }} and length(input) = 68 -- transfer
-        or {{ selector }} = {{ transferFrom_selector }} and length(input) = 100 -- transferFrom
-        or {{ selector }} = {{ mint_selector }} and length(input) = 68 -- mint
-        or {{ selector }} = {{ burn_selector }} and length(input) = 68 -- burn
-        or {{ selector }} = {{ deposit_selector }} and length(input) = 4 -- deposit
-        or {{ selector }} = {{ withdraw_selector }} and length(input) = 36 -- withdraw
-        or value > uint256 '0'
-    )
-    and call_type = 'call'
-    and tx_success
-    and success
+    , transfer_trace_address
+    , type
+    , contract_address
+    , amount
+    , transfer_from
+    , transfer_to
+from t
+
+union all 
+
+select
+    blockchain
+    , block_number
+    , block_time
+    , tx_hash
+    , transfer_trace_address
+    , type
+    , contract_address
+    , amount
+    , transfer_from
+    , transfer_to
+from deposit_as_wrapped
 
 {% endmacro %}
--- models/oneinch/oneinch_parsed_transfers_from_calls.sql
+++ models/oneinch/oneinch_parsed_transfers_from_calls.sql
@@ -9,6 +9,6 @@
 -- View for all parsed transfers from calls, for internal usage. Won't be used in lineage.
 
 {% for blockchain in all_evm_chains() %}
-    {{ oneinch_parsed_transfers_from_calls_macro(blockchain) }}
+    select * from ({{ oneinch_parsed_transfers_from_calls_macro(blockchain) }})
     {% if not loop.last %}union all{% endif %}
 {% endfor %}
