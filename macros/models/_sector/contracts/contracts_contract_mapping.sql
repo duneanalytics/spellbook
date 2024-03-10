5d86a60b613f55758451add38bef9a320282e62b
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-03-10 13:21:22 -0400

    grp

--- macros/models/_sector/contracts/contracts_contract_mapping.sql
+++ macros/models/_sector/contracts/contracts_contract_mapping.sql
@@ -127,7 +127,7 @@ WITH get_contracts as (
       ) as t_mapped
     on c.contract_address = t_mapped.contract_address
     AND c.blockchain = t_mapped.blockchain
-  group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28
+  group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29
   ) a
   where contract_address is not NULL 
   group by 1,2
