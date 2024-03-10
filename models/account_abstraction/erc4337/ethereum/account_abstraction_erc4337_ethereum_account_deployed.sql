6c2920949fdd32af9c54a46008ee4a4607eeda66
Peter <49856481+peterrliem@users.noreply.github.com>
2024-03-10 19:51:33 +0800

    Added v_07 of entrypoints for ERC 4337

--- models/account_abstraction/erc4337/ethereum/account_abstraction_erc4337_ethereum_account_deployed.sql
+++ models/account_abstraction/erc4337/ethereum/account_abstraction_erc4337_ethereum_account_deployed.sql
@@ -9,13 +9,14 @@
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "project",
                                     "erc4337",
-                                    \'["0xbitfly"]\') }}'
+                                    \'["0xbitfly", "petertherock"]\') }}'
 )}}
 
 
 {% set erc4337_base_models = [
     ref('account_abstraction_erc4337_ethereum_v0_5_account_deployed')
     , ref('account_abstraction_erc4337_ethereum_v0_6_account_deployed')
+    , ref('account_abstraction_erc4337_ethereum_v0_7_account_deployed')
 ] %}
 
 SELECT  
