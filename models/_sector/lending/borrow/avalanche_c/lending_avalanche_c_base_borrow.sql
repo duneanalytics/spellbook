f8847eee6936432a02f29c669dd65c95e4c3ecfb
tomfutago <35136350+tomfutago@users.noreply.github.com>
2024-03-11 13:11:33 +0000

    Add Granary to lending (#5495)
    * borrow
    
    * flashloans & supply
    
    * fix type
    
    * seed
    
    * fix seed
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/_sector/lending/borrow/avalanche_c/lending_avalanche_c_base_borrow.sql
+++ models/_sector/lending/borrow/avalanche_c/lending_avalanche_c_base_borrow.sql
@@ -10,7 +10,8 @@
   set models = [
     ref('aave_v2_avalanche_c_base_borrow'),
     ref('aave_v3_avalanche_c_base_borrow'),
-    ref('benqi_avalanche_c_base_borrow')
+    ref('benqi_avalanche_c_base_borrow'),
+    ref('granary_avalanche_c_base_borrow')
   ]
 %}
 
