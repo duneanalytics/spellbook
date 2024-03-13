514e72dd31c90485e8c2878d7c510ccbd5b452d5
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-13 12:46:05 +0100

    ADDED BASO FINANCE (BASE) TO DEX.TRADES (#5476)
    * added baso finance
    
    * added to seed
    
    * added to dex info
    
    * removed duplication
    
    * Fix: Removed Filter
    
    * Change version to 1
    
    * Fix project names
    
    * Fix wombat_exchange
    
    * Add SyncSwap in dex.info
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

---
+++ models/_sector/dex/trades/base/platforms/baso_finance_base_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'baso_finance_base',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v2_trades(
+        blockchain = 'base',
+        project = 'baso_finance',
+        version = '1',
+        Pair_evt_Swap = source('baso_finance_base', 'Pair_evt_Swap'),
+        Factory_evt_PairCreated = source('baso_finance_base', 'PairFactory_evt_PairCreated')
+    )
+}}
