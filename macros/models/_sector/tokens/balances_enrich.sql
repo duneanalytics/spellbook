4c005c1ea689cd263409e377d667f5df3eec08ab
Alan Ghobadi <aalan3@gmail.com>
2024-03-14 21:39:53 +0400

    Try with nft metadata but change order

--- macros/models/_sector/tokens/balances_enrich.sql
+++ macros/models/_sector/tokens/balances_enrich.sql
@@ -14,8 +14,14 @@ select
         ELSE balances.balance_raw
     END as balance,
     erc20_tokens.symbol as token_symbol,
-    token_id
+    token_id,
+    nft_tokens.name as collection_name
 from {{balances_raw}} balances
+left join {{ ref('tokens_nft') }} nft_tokens on (
+   nft_tokens.blockchain = balances.blockchain
+   AND nft_tokens.contract_address = balances.token_address
+   AND balances.token_standard in ('erc721', 'erc1155')
+   )
 left join {{ source('tokens', 'erc20') }} erc20_tokens on
     erc20_tokens.blockchain = balances.blockchain
     AND erc20_tokens.contract_address = balances.token_address
