e581b74cac7e12ff28bc816c8376fcd1fa530ae5
jeff-dude <jeff@dune.xyz>
2024-03-15 11:32:11 -0400

    Revert "prep macro for dune catalog uploads"
    This reverts commit 9edb434f435fa10981300105abe983fcaa18253f.

--- macros/dune/source.sql
+++ macros/dune/source.sql
@@ -1,12 +1,9 @@
-{% macro source(source_name, table_name, dune=False) %}
+{% macro source(source_name, table_name) %}
   {% set rel = builtins.source(source_name, table_name) %}
-  {%- if dune -%}
-      {% set newrel = rel.replace_path(database="dune") %}
-      {% do return(newrel) %}
-  {%- elif target.type == "trino" -%}
-      {% set newrel = rel.replace_path(database="delta_prod") %}
-      {% do return(newrel) %}
+  {%- if target.type == 'trino' -%}
+    {% set newrel = rel.replace_path(database="delta_prod") %}
+    {% do return(newrel) %}
   {%- else -%}
-      {% do return(rel) %}
+    {% do return(rel) %}
   {%- endif -%}
 {% endmacro %}
