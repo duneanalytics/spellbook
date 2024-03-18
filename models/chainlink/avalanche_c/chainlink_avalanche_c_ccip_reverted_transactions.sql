a38b16cbbb12289fc829348b9b25d6910533fadf
AnonJon <jon.gregis@gmail.com>
2024-03-18 12:18:48 -0500

    updated ccip v1.2

--- models/chainlink/avalanche_c/chainlink_avalanche_c_ccip_reverted_transactions.sql
+++ models/chainlink/avalanche_c/chainlink_avalanche_c_ccip_reverted_transactions.sql
@@ -1,38 +1,62 @@
 {{
   config(
-    tags = ['prod_exclude'], 
+    
     alias='ccip_reverted_transactions',
+    partition_by=['date_month'],
     materialized='incremental',
     file_format='delta',
     incremental_strategy='merge',
-    unique_key=['tx_hash', 'trace_address', 'node_address']
+    unique_key=['tx_hash', 'tx_index', 'caller_address']
   )
 }}
 
 {% set incremental_interval = '7' %}
 
 WITH
+  avalanche_c_usd AS (
+    SELECT
+      minute as block_time,
+      price as usd_amount
+    FROM
+      {{ source('prices', 'usd') }} price
+    WHERE
+      symbol = 'ETH'
+      {% if is_incremental() %}
+        AND minute >= date_trunc('day', now() - interval '{{incremental_interval}}' day)
+      {% endif %}      
+  ),
   ccip_reverted_transactions AS (
     SELECT
-      tx.tx_hash as tx_hash,
-      tx.block_time as block_time,
-      cast(date_trunc('day', tx.block_time) as date) as date_start,
-      tx."from" as "node_address",
-      tx.trace_address as trace_address
+      tx.hash as tx_hash,
+      tx.index as tx_index,
+      MAX(tx.block_time) as block_time,
+      cast(date_trunc('month', MAX(tx.block_time)) as date) as date_month,
+      tx."from" as "caller_address",
+      MAX(
+        (cast((gas_used) as double) / 1e18) * gas_price
+      ) as token_amount,
+      MAX(avalanche_c_usd.usd_amount) as usd_amount
     FROM
-      {{ ref('chainlink_avalanche_c_ccip_send_traces') }} tx
-      WHERE
-        tx.tx_success = false 
-        {% if is_incremental() %}
-          AND tx.block_time >= date_trunc('day', now() - interval '{{incremental_interval}}' day)
-        {% endif %}    
+      {{ source('avalanche_c', 'transactions') }} tx
+      LEFT JOIN avalanche_c_usd ON date_trunc('minute', tx.block_time) = avalanche_c_usd.block_time
+    WHERE
+      success = false
+      {% if is_incremental() %}
+        AND tx.block_time >= date_trunc('day', now() - interval '{{incremental_interval}}' day)
+      {% endif %}      
+    GROUP BY
+      tx.hash,
+      tx.index,
+      tx."from"
   )
 SELECT
  'avalanche_c' as blockchain,
   block_time,
-  date_start,
-  node_address,
+  date_month,
+  caller_address,
+  token_amount,
+  usd_amount,
   tx_hash,
-  trace_address
+  tx_index
 FROM
   ccip_reverted_transactions
