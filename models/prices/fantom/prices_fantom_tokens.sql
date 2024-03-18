561f3e426f757161e3105cbc98dc63542f799bc2
Tiago Silva <tfn.da.silva@gmail.com>
2024-03-18 12:52:36 +0000

    Fix formatting

--- models/prices/fantom/prices_fantom_tokens.sql
+++ models/prices/fantom/prices_fantom_tokens.sql
@@ -78,9 +78,9 @@ FROM
     --('stg-stargate-finance', 'fantom', 'STG', 0x2f6f07cdcf3588944bf4c42ac74ff24bf56e7590, 18), --not found in API 
     ('spa-spartacus', 'fantom', 'SPA', 0x5602df4a94eb6c680190accfa2a475621e0ddbdc, 9),
     ('space-space-token', 'fantom', 'SPACE', 0x5f7f94a1dd7b15594d17543beb8b30b111dd464c, 18),
-    ('woo-wootrade', 'fantom', 'WOO', 0x6626c47c00f1d87902fc13eecfac3ed06d5e8d8a, 18)
+    ('woo-wootrade', 'fantom', 'WOO', 0x6626c47c00f1d87902fc13eecfac3ed06d5e8d8a, 18),
     --('vemp-vemp', 'fantom', 'VEMP', 0x526f1dc408cfe7fc5330ab9f1e78474ceff2a5dd, 18) --not found in API 
-    ('axlusdc-axelar-wrapped-usdc', 'fantom', 'AXLUSDC', 0x1B6382DBDEa11d97f24495C9A90b7c88469134a4, 6)
-    ('mpx-morphex', 'fantom', 'MPX', 0x66eEd5FF1701E6ed8470DC391F05e27B1d0657eb, 18)
+    ('axlusdc-axelar-wrapped-usdc', 'fantom', 'AXLUSDC', 0x1B6382DBDEa11d97f24495C9A90b7c88469134a4, 6),
+    ('mpx-morphex', 'fantom', 'MPX', 0x66eEd5FF1701E6ed8470DC391F05e27B1d0657eb, 18),
     ('unidx-unidex', 'fantom', 'UNIDX', 0x0483a76D80D0aFEC6bd2afd12C1AD865b9DF1471, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
