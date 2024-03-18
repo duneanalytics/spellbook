baf8bbf94192cf9633d84039d129cf9fd57c9209
Chuxin Huang <chuxin@oplabs.co>
2024-03-19 03:40:48 +1100

    Add Agora V6 in OP Governance (#5598)
    * add agora v6 proposals
    
    * end line
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * replace CRLF
    
    * add line
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * update
    
    * add incremental
    
    * update incremental
    
    * update incremental
    
    * update incremental
    
    * update partition
    
    * add deadline
    
    * add deadline
    
    * add deadline
    
    * add new source
    
    * update logic
    
    * update logic
    
    * update logic
    
    * update logic
    
    * add vote with params
    
    * nits
    
    * add union
    
    * update agora
    
    * update agora
    
    * update agora
    
    * update
    
    * update
    
    * update
    
    * update
    
    * add unique voter count
    
    * update schema
    
    * update schema
    
    * update schema
    
    * nits

--- models/governance/optimism/governance_optimism_proposal_votes.sql
+++ models/governance/optimism/governance_optimism_proposal_votes.sql
@@ -1,4 +1,5 @@
-{{ config(alias = 'proposal_votes'
+{{ config(
+    alias = 'proposal_votes'
     ,materialized = 'incremental'
     ,file_format = 'delta'
     ,schema = 'governance_optimism'
@@ -7,12 +8,37 @@
     ,post_hook='{{ expose_spells(\'["optimism"]\',
                                       "sector",
                                       "governance",
-                                    \'["chain_l"]\') }}'
+                                    \'["chain_l", "chuxin"]\') }}'
     )
 }}
 
 SELECT
-  TRY_CAST(proposalId AS VARBINARY) AS proposal_id,
+  CAST(proposalId as VARCHAR) AS proposal_id,
+  'agora' AS platform,
+  evt_tx_hash AS tx_hash,
+  evt_block_time AS date_timestamp,
+  voter,
+  reason,
+  weight / POWER(10, 18) AS votingWeightage,
+  support AS choice,
+  CASE
+    WHEN support = 0 THEN 'against'
+    WHEN support = 1 THEN 'for'
+    WHEN support = 2 THEN 'abstain'
+    WHEN support = 3 THEN 'voted'
+  END AS choice_name,
+  NULL as params
+FROM
+  {{ source('optimism_governor_optimism','OptimismGovernorV5_evt_VoteCast') }}
+{% if is_incremental() %}
+WHERE
+    {{ incremental_predicate('evt_block_time') }}
+{% endif %}
+
+UNION ALL
+
+SELECT
+  CAST(proposalId as VARCHAR) AS proposal_id,
   'agora' AS platform,
   evt_tx_hash AS tx_hash,
   evt_block_time AS date_timestamp,
@@ -26,13 +52,70 @@ SELECT
     WHEN support = 2 THEN 'abstain'
     WHEN support = 3 THEN 'voted'
   END AS choice_name
+  ,params
 FROM
-  {{ source('optimism_governor_optimism','OptimismGovernorV5_evt_VoteCast') }}
+  {{ source('optimism_governor_optimism','OptimismGovernorV5_evt_VoteCastWithParams') }}
+{% if is_incremental() %}
+WHERE
+    {{ incremental_predicate('evt_block_time') }}
+{% endif %}
+
 UNION ALL
+
 SELECT
-  proposal AS proposal_id,
+  CAST(proposalId as VARCHAR) AS proposal_id,
+  'agora' AS platform,
+  evt_tx_hash AS tx_hash,
+  evt_block_time AS date_timestamp,
+  voter,
+  reason,
+  weight / POWER(10, 18) AS votingWeightage,
+  support AS choice,
+  CASE
+    WHEN support = 0 THEN 'against'
+    WHEN support = 1 THEN 'for'
+    WHEN support = 2 THEN 'abstain'
+    WHEN support = 3 THEN 'voted'
+  END AS choice_name,
+  NULL as params
+FROM
+  {{ source('optimism_governor_optimism','OptimismGovernorV6_evt_VoteCast') }}
+{% if is_incremental() %}
+WHERE
+    {{ incremental_predicate('evt_block_time') }}
+{% endif %}
+
+UNION ALL
+
+SELECT
+  CAST(proposalId as VARCHAR) AS proposal_id,
+  'agora' AS platform,
+  evt_tx_hash AS tx_hash,
+  evt_block_time AS date_timestamp,
+  voter,
+  reason,
+  weight / POWER(10, 18) AS votingWeightage,
+  support AS choice,
+  CASE
+    WHEN support = 0 THEN 'against'
+    WHEN support = 1 THEN 'for'
+    WHEN support = 2 THEN 'abstain'
+    WHEN support = 3 THEN 'voted'
+  END AS choice_name
+  ,params
+FROM
+  {{ source('optimism_governor_optimism','OptimismGovernorV6_evt_VoteCastWithParams') }}
+{% if is_incremental() %}
+WHERE
+    {{ incremental_predicate('evt_block_time') }}
+{% endif %}
+
+UNION ALL
+
+SELECT
+  TRY_CAST(proposal as VARCHAR) AS proposal_id,
   'snapshot' AS platform,
-  TRY_CAST('' AS VARBINARY) AS tx_hash,
+  NULL AS tx_hash,
   from_unixtime(created) AS date_timestamp,
   voter,
   reason,
@@ -43,8 +126,12 @@ SELECT
     WHEN choice = '2' THEN 'against'
     WHEN choice = '3' THEN 'abstain'
     WHEN choice = '4' THEN 'voted'
-  END AS choice_name
+  END AS choice_name,
+  NULL as params
 FROM
   {{ source('snapshot','votes') }}
 WHERE
   "space" = 'opcollective.eth'
+{% if is_incremental() %}
+AND {{ incremental_predicate('from_unixtime(created)') }}
+{% endif %}
