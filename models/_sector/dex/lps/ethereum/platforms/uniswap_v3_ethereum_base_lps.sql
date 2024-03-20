a8543a0fd7b4f2f27479ce07c10dd8d7787a2128
0xrusowsky <0xrusowsky@proton.me>
2024-03-20 20:45:16 +0100

    fix: compilation errors

--- models/_sector/dex/lps/ethereum/platforms/uniswap_v3_ethereum_base_lps.sql
+++ models/_sector/dex/lps/ethereum/platforms/uniswap_v3_ethereum_base_lps.sql
@@ -1,6 +1,6 @@
 {{ config(
     schema = 'uniswap_v3_ethereum'
-    , alias = 'base_trades'
+    , alias = 'base_lps'
     , materialized = 'incremental'
     , file_format = 'delta'
     , incremental_strategy = 'merge'
@@ -10,16 +10,16 @@
 }}
 
 {{
-    uniswap_compatible_v3_trades(
+    uniswap_compatible_v3_lps(
         blockchain = 'ethereum'
         , project = 'uniswap'
         , version = '3'
         , Pair_evt_Mint = source('uniswap_v3_ethereum', 'Pair_evt_Mint')
         , Pair_evt_Burn = source('uniswap_v3_ethereum', 'Pair_evt_Burn')
         , Factory_evt_PoolCreated = source('uniswap_v3_ethereum', 'Factory_evt_PoolCreated')
-        , NonfungibleTokenPositionManager_evt_Transfer = source('uniswap_v3_ethereum', 'NonfungibleTokenPositionManager_evt_Transfer')
         , NonfungibleTokenPositionManager_evt_IncreaseLiquidity = source('uniswap_v3_ethereum', 'NonfungibleTokenPositionManager_evt_IncreaseLiquidity')
         , NonfungibleTokenPositionManager_evt_DecreaseLiquidity = source('uniswap_v3_ethereum', 'NonfungibleTokenPositionManager_evt_DecreaseLiquidity')
         , position_manager_addr = 0xc36442b4a4522e871399cd717abdd847ab11fe88
+        , NonfungibleTokenPositionManager_evt_Transfer = source('uniswap_v3_ethereum', 'NonfungibleTokenPositionManager_evt_Transfer')
     )
 }}
