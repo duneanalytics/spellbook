acf5fe87524dafeede6dee9f871fad97f0d1dca3
Ploy Temiyasathit <ploy_ups103@hotmail.com>
2024-03-20 01:07:34 -0700

    Update models/boost/boost_claimed.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/boost/boost_claimed.sql
+++ models/boost/boost_claimed.sql
@@ -6,6 +6,7 @@
         file_format='delta',
         incremental_strategy='merge',
         unique_key=['boost_address', 'claim_tx_hash', 'action_tx_hash'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
     )
 }}
 
