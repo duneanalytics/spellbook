c474beb98dba3fe3fac694c895dd0878b1d8905e
0xRob <0xRobin5@gmail.com>
2024-03-22 09:55:01 +0100

    test CI

--- models/opensea/ethereum/opensea_v4_ethereum_events.sql
+++ models/opensea/ethereum/opensea_v4_ethereum_events.sql
@@ -1,7 +1,6 @@
 {{ config(
     schema = 'opensea_v4_ethereum',
     alias = 'events',
-    tags = ['prod_exclude'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
