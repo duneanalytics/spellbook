25b8da3785228e3aa0f41156ee636cada3e7aed1
lorenz234 <lorenz_lehmann@yahoo.de>
2024-03-24 16:12:53 +0100

    final fix (hopefully)

--- models/blobs/ethereum/enriched.sql
+++ models/blobs/ethereum/enriched.sql
@@ -4,7 +4,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['tx_hash','blob_index'],
+    unique_key = ['tx_hash','blob_index','blob_kzg_commitment'],
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                     "project",
                                     "blobs",
