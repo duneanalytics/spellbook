0ffdb79b60e6f4174053f4cf0073ac6d2118f7d5
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-03-25 14:33:09 +0000

    Update labels_balancer_v2_pools_ethereum.sql

--- models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_pools_ethereum.sql
+++ models/labels/addresses/__single_category_labels__/balancer_v2/labels_balancer_v2_pools_ethereum.sql
@@ -266,7 +266,7 @@ settings AS (
 SELECT 
   'ethereum' AS blockchain,
   bytearray_substring(pool_id, 1, 20) AS address,
-  CASE WHEN pool_type IN ('stable', 'linear', 'LBP', 'ECLP') 
+  CASE WHEN pool_type IN ('stable', 'linear', 'LBP', 'ECLP', 'FX') 
   THEN lower(pool_symbol)
     ELSE lower(concat(array_join(array_agg(token_symbol ORDER BY token_symbol), '/'), ' ', 
     array_join(array_agg(cast(norm_weight AS varchar) ORDER BY token_symbol), '/')))
