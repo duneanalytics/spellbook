37d6a8ad953dc895acc0b36dacb8ce398ca4a12c
Landon Gingerich <landon.ging@gmail.com>
2024-03-26 18:53:13 +0000

    fix block_time, format

--- models/_sector/nft/trades/chains/zksync/platforms/mint_square_zksync_base_trades.sql
+++ models/_sector/nft/trades/chains/zksync/platforms/mint_square_zksync_base_trades.sql
@@ -1,6 +1,6 @@
 {{ config(
-    schema = 'mintsquare_zksync',
-    alias = 'base_trades',
+    schema = 'mint_square_zksync',
+    aliAS = 'base_trades',
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
@@ -12,41 +12,43 @@ WITH mintsquare_trades AS (
     SELECT *
     , ROW_NUMBER() OVER (PARTITION BY tx_hash, nft_contract_address, nft_token_id ORDER BY evt_index ASC) AS id
     FROM (
-        SELECT ta.evt_block_time AS block_time
-        , ta.tokenId AS nft_token_id
-        , ta.amount AS nft_amount
-        , 'Buy' AS trade_category
-        , ta.maker AS seller
-        , ta.taker AS buyer
-        , ta.price AS price_raw
-        , ta.currency AS currency_contract
-        , ta.collection AS nft_contract_address
-        , ta.contract_address AS project_contract_address
-        , ta.evt_tx_hash AS tx_hash
-        , ta.evt_block_number AS block_number
-        , ta.evt_index
-        , ta.strategy
-        FROM {{ source('mint_square_zksync','Marketplace_evt_TakerAsk') }} ta
+        SELECT 
+            ta.evt_block_time AS block_time
+            , ta.tokenId AS nft_token_id
+            , ta.amount AS nft_amount
+            , 'Buy' AS trade_category
+            , ta.maker AS seller
+            , ta.taker AS buyer
+            , ta.price AS price_raw
+            , ta.currency AS currency_contract
+            , ta.collection AS nft_contract_address
+            , ta.contract_address AS project_contract_address
+            , ta.evt_tx_hash AS tx_hash
+            , ta.evt_block_number AS block_number
+            , ta.evt_index
+            , ta.strategy
+        FROM {{ source('mint_square_zksync','Marketplace_evt_TakerASk') }} ta
         {% if is_incremental() %}
         WHERE ta.{{incremental_predicate('evt_block_time')}}
         {% endif %}
 
         UNION ALL
 
-        SELECT tb.evt_block_time AS block_time
-        , tb.tokenId AS nft_token_id
-        , tb.amount AS nft_amount
-        , 'Offer Accepted' AS trade_category
-        , tb.maker AS seller
-        , tb.taker AS buyer
-        , tb.price AS price_raw
-        , tb.currency AS currency_contract
-        , tb.collection AS nft_contract_address
-        , tb.contract_address AS project_contract_address
-        , tb.evt_tx_hash AS tx_hash
-        , tb.evt_block_number AS block_number
-        , tb.evt_index
-        , tb.strategy
+        SELECT 
+            tb.evt_block_time AS block_time
+            , tb.tokenId AS nft_token_id
+            , tb.amount AS nft_amount
+            , 'Offer Accepted' AS trade_category
+            , tb.maker AS seller
+            , tb.taker AS buyer
+            , tb.price AS price_raw
+            , tb.currency AS currency_contract
+            , tb.collection AS nft_contract_address
+            , tb.contract_address AS project_contract_address
+            , tb.evt_tx_hash AS tx_hash
+            , tb.evt_block_number AS block_number
+            , tb.evt_index
+            , tb.strategy
         FROM {{ source('mint_square_zksync','Marketplace_evt_TakerBid') }} tb
         {% if is_incremental() %}
         WHERE tb.{{incremental_predicate('evt_block_time')}}
@@ -55,14 +57,15 @@ WITH mintsquare_trades AS (
     )
 
 , royalties AS (
-    SELECT evt_block_time AS block_time
-    , evt_tx_hash AS tx_hash
-    , evt_index
-    , collection AS nft_contract_address
-    , tokenId AS nft_token_id
-    , amount
-    , royaltyRecipient
-    , ROW_NUMBER() OVER (PARTITION BY evt_tx_hash, collection, tokenId ORDER BY evt_index ASC) AS id
+    SELECT 
+        evt_block_time AS block_time
+        , evt_tx_hash AS tx_hash
+        , evt_index
+        , collection AS nft_contract_address
+        , tokenId AS nft_token_id
+        , amount
+        , royaltyRecipient
+        , ROW_NUMBER() OVER (PARTITION BY evt_tx_hash, collection, tokenId ORDER BY evt_index ASC) AS id
     FROM {{ source('mint_square_zksync','Marketplace_evt_RoyaltyPayment') }}
     {% if is_incremental() %}
     WHERE {{incremental_predicate('evt_block_time')}}
@@ -70,42 +73,47 @@ WITH mintsquare_trades AS (
     )
 
 , platform_fees AS (
-    SELECT distinct contract_address
-    , cast(output_0 as double)/100 AS fee_percentage
+    SELECT 
+        DISTINCT contract_address
+        , CAST(output_0 AS double)/100 AS fee_percentage
     FROM {{ source('mint_square_zksync','StrategyHighestBidderAuctionSale_call_viewProtocolFee') }}
     UNION ALL
-    SELECT distinct contract_address
-    , cast(output_0 as double)/100 AS fee_percentage
+    SELECT 
+        DISTINCT contract_address
+        , CAST(output_0 AS double)/100 AS fee_percentage
     FROM {{ source('mint_square_zksync','StrategyStandardSaleForFixedPrice_call_viewProtocolFee') }}
     UNION ALL
-    SELECT distinct contract_address
-    , cast(output_0 as double)/100 AS fee_percentage
+    SELECT 
+        DISTINCT contract_address
+        , CAST(output_0 AS double)/100 AS fee_percentage
     FROM {{ source('mint_square_zksync','StrategyAnyItemFromCollectionForFixedPrice_call_viewProtocolFee') }}
     )
 
 
 SELECT
- 'zksync' as blockchain
-, 'mintsquare' as project
-, 'v1' as project_version
-, m.block_time
-, m.block_number
-, m.tx_hash
-, m.project_contract_address
-, m.trade_category
-, 'secondary' as trade_type
-, m.nft_contract_address
-, m.nft_token_id
-, m.nft_amount
-, m.buyer
-, m.seller
-, m.currency_contract
-, m.price_raw
-, CAST(COALESCE((pf.fee_percentage/100) * CAST(m.price_raw as uint256),  DOUBLE '0') as UINT256) AS platform_fee_amount_raw
-, COALESCE(roy.amount, uint256 '0') AS royalty_fee_amount_raw
-, cast(null as varbinary) as platform_fee_address
-, roy.royaltyRecipient AS royalty_fee_address
-, m.evt_index AS sub_tx_trade_id
+    'zksync' AS blockchain
+    , 'mint_square' AS project
+    , 'v1' AS project_version
+    , m.block_time
+    , CAST(date_trunc('day', m.block_time) AS date) AS block_date
+    , CAST(date_trunc('month', m.block_time) AS date) AS block_month
+    , m.block_number
+    , m.tx_hash
+    , m.project_contract_address
+    , m.trade_category
+    , 'secondary' AS trade_type
+    , m.nft_contract_address
+    , m.nft_token_id
+    , m.nft_amount
+    , m.buyer
+    , m.seller
+    , m.currency_contract
+    , m.price_raw
+    , CAST(COALESCE((pf.fee_percentage/100) * CAST(m.price_raw AS uint256),  DOUBLE '0') AS UINT256) AS platform_fee_amount_raw
+    , COALESCE(roy.amount, uint256 '0') AS royalty_fee_amount_raw
+    , CAST(null AS varbinary) AS platform_fee_address
+    , roy.royaltyRecipient AS royalty_fee_address
+    , m.evt_index AS sub_tx_trade_id
 FROM mintsquare_trades m
 LEFT JOIN royalties roy ON roy.block_time=m.block_time
     AND roy.tx_hash = m.tx_hash
