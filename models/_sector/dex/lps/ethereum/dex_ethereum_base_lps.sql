bddfe41f6421b63d624d891216fbda0abdd61993
0xrusowsky <0xrusowsky@proton.me>
2024-03-27 11:27:08 +0100

    fix: remove duplicates

--- models/_sector/dex/lps/ethereum/dex_ethereum_base_lps.sql
+++ models/_sector/dex/lps/ethereum/dex_ethereum_base_lps.sql
@@ -32,13 +32,20 @@ WITH base_union AS (
             , tick_upper
             , tx_hash
             , evt_index
+            , row_number() over (partition by tx_hash, evt_index order by tx_hash) as duplicates_rank
         FROM 
             {{ base_model }}
+        {% if is_incremental() %}
+        WHERE
+            {{ incremental_predicate('block_time') }}
+        {% endif %}
         {% if not loop.last %}
         UNION ALL
         {% endif %}
         {% endfor %}
     )
+    WHERE
+        duplicates_rank = 1
 )
 
 select
