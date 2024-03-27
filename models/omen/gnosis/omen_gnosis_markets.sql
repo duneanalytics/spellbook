7aec2a19e5a0fc8504e80513282bb1a060af5bd7
hugo <hugo.serodio@gnosis.io>
2024-03-27 11:29:25 +0100

    Omen models. First attempt.

---
+++ models/omen/gnosis/omen_gnosis_markets.sql
@@ -0,0 +1,132 @@
+{{ config(
+    schema = 'omen_gnosis',
+    alias = 'markets',
+    
+    partition_by = ['block_day'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_day', 'tx_hash', 'questionId'],
+    post_hook='{{ expose_spells(\'["gnosis"]\',
+                                "project",
+                                "omen",
+                                \'["hdser"]\') }}'
+    )
+}}
+
+{% set project_start_date = '2021-01-01' %}
+
+WITH 
+
+Realitio_LogNewQuestion AS (
+    SELECT
+        block_time,
+        block_hash,
+        tx_hash,
+        contract_address,
+        topic1 AS questionId,
+        VARBINARY_SUBSTRING(topic2, 13, 20) AS user,
+        topic3 AS content_hash,
+        VARBINARY_TO_UINT256(VARBINARY_LTRIM(VARBINARY_SUBSTRING(data, 1, 32))) AS template_id,
+        FROM_UTF8(VARBINARY_SUBSTRING(data, 225)) AS question,
+        VARBINARY_SUBSTRING(VARBINARY_SUBSTRING(data, 65, 32), 13, 20) AS arbitrator,
+        VARBINARY_TO_UINT256(VARBINARY_LTRIM(VARBINARY_SUBSTRING(data, 97, 32))) AS timeout,
+        VARBINARY_TO_UINT256(VARBINARY_LTRIM(VARBINARY_SUBSTRING(data, 129, 32))) AS opening_ts,
+        VARBINARY_TO_UINT256(VARBINARY_LTRIM(VARBINARY_SUBSTRING(data, 161, 32))) AS nonce,
+        VARBINARY_TO_UINT256(VARBINARY_LTRIM(VARBINARY_SUBSTRING(data, 193, 32))) AS created
+    FROM 
+        {{source('gnosis','logs') }}
+    WHERE
+        topic0 = 0xfe2dac156a3890636ce13f65f4fdf41dcaee11526e4a5374531572d92194796c --Realitio_v2_1_evt_LogNewQuestion
+        {% if is_incremental() %}
+        AND block_time >= date_trunc('day', now() - interval '7' day)
+        {% else %}
+        AND block_time >= TIMESTAMP '{{project_start_date}}'
+        {% endif %}
+), 
+
+ConditionPreparation AS (
+    SELECT
+        block_time,
+        block_hash,
+        contract_address,
+        topic1 AS conditionId,
+        VARBINARY_SUBSTRING(topic2, 13, 20) AS oracle,
+        topic3 AS questionId,
+        VARBINARY_TO_UINT256(VARBINARY_LTRIM(VARBINARY_SUBSTRING(data, 1, 32))) AS outcomeSlotCount
+    FROM 
+        {{source('gnosis','logs') }}
+    WHERE
+        topic0 = 0xab3760c3bd2bb38b5bcf54dc79802ed67338b4cf29f3054ded67ed24661e4177 --ConditionalTokens_evt_ConditionPreparation
+         {% if is_incremental() %}
+        AND block_time >= date_trunc('day', now() - interval '7' day)
+        {% else %}
+        AND block_time >= TIMESTAMP '{{project_start_date}}'
+        {% endif %}
+), 
+
+FixedProductMarketMakerCreation AS (
+    SELECT
+        t.block_time,
+        t.block_hash,
+        t.contract_address,
+        t.creator,
+        t.fixedProductMarketMaker,
+        t.conditionalTokens,
+        t.collateralToken,
+        t.fee,
+        s.SEQUENCE_NUMBER AS conditionIds_index,
+        VARBINARY_SUBSTRING(t.data, 161 + 32 * s.SEQUENCE_NUMBER, 32) AS conditionId
+    FROM (
+        SELECT
+            block_time,
+            block_hash,
+            contract_address,
+            VARBINARY_SUBSTRING(topic1, 13, 20)  AS creator,
+            VARBINARY_SUBSTRING(VARBINARY_SUBSTRING(data, 1, 32), 13, 20) AS fixedProductMarketMaker,
+            VARBINARY_SUBSTRING(VARBINARY_SUBSTRING(data, 33, 32), 13, 20) AS conditionalTokens,
+            VARBINARY_SUBSTRING(VARBINARY_SUBSTRING(data, 65, 32), 13, 20) AS collateralToken,
+            VARBINARY_TO_UINT256(VARBINARY_LTRIM(VARBINARY_SUBSTRING(data, 129, 32))) AS fee,
+            VARBINARY_TO_UINT256(VARBINARY_SUBSTRING(data, 161, 32)) AS conditionIds_size,
+            data
+        FROM 
+            {{source('gnosis','logs') }}
+        WHERE
+            topic0 = 0x92e0912d3d7f3192cad5c7ae3b47fb97f9c465c1dd12a5c24fd901ddb3905f43 --FPMMDeterministicFactory_evt_FixedProductMarketMakerCreation
+            {% if is_incremental() %}
+            AND block_time >= date_trunc('day', now() - interval '7' day)
+            {% else %}
+            AND block_time >= TIMESTAMP '{{project_start_date}}'
+            {% endif %}
+        ) AS t
+    CROSS JOIN UNNEST(SEQUENCE(1, TRY_CAST(t.conditionIds_size AS INTEGER)) ) AS s(SEQUENCE_NUMBER)
+),
+
+
+
+prediction_market_info AS (
+  SELECT
+    t1.block_time,
+    DATE_TRUNC('day', t1.block_time) AS block_day,
+    t1.tx_hash,
+    t2.questionId,
+    t2.conditionId,
+    t2.oracle,
+    t2.outcomeSlotCount,
+    t1.question,
+    t3.fixedProductMarketMaker,
+    t3.conditionalTokens,
+    t3.collateralToken,
+    t3.fee,
+    t3.conditionIds_index
+  FROM Realitio_LogNewQuestion AS t1
+  INNER JOIN ConditionPreparation AS t2
+    ON t2.questionId = t1.questionId
+  INNER JOIN FixedProductMarketMakerCreation AS t3
+    ON 
+    t3.conditionId = t2.conditionId
+)
+
+SELECT
+  *
+FROM prediction_market_info
---
+++ models/omen/gnosis/omen_gnosis_trades.sql
@@ -0,0 +1,108 @@
+{{ config(
+    schema = 'omen_gnosis',
+    alias = 'trades',
+    
+    partition_by = ['block_day'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_day', 'tx_hash', 'evt_index'],
+    post_hook='{{ expose_spells(\'["gnosis"]\',
+                                "project",
+                                "omen",
+                                \'["hdser"]\') }}'
+    )
+}}
+
+{% set project_start_date = '2021-01-01' %}
+
+
+WITH
+
+trades AS (
+    SELECT
+        block_time
+        ,DATE_TRUNC('day', block_time) AS block_day
+        ,tx_from 
+        ,tx_to 
+        ,tx_hash 
+        ,index AS evt_index
+        ,contract_address AS evt_contract_address
+        ,varbinary_substring(topic1,13,20)  AS address
+        ,varbinary_to_uint256(varbinary_ltrim(topic2)) AS outcomeIndex
+        ,varbinary_to_uint256(varbinary_ltrim(varbinary_substring(data,1,32)))AS amount
+        ,varbinary_to_uint256(varbinary_ltrim(varbinary_substring(data,33,32))) AS feeAmount
+        ,varbinary_to_uint256(varbinary_ltrim(varbinary_substring(data,65,32))) AS outcomeTokens
+        ,CASE 
+            WHEN topic0 = 0x4f62630f51608fc8a7603a9391a5101e58bd7c276139366fc107dc3b67c3dcf8 
+                THEN 'Buy'
+            ELSE 'Sell'
+        END AS action
+    FROM
+        {{ source('gnosis', 'logs') }}
+    WHERE
+        (
+            topic0 = 0x4f62630f51608fc8a7603a9391a5101e58bd7c276139366fc107dc3b67c3dcf8 -- Buy
+            OR
+            topic0 = 0xadcf2a240ed9300d681d9a3f5382b6c1beed1b7e46643e0c7b42cbe6e2d766b4 -- Sell
+        )
+        {% if is_incremental() %}
+        AND block_time >= date_trunc('day', now() - interval '7' day)
+        {% else %}
+        AND block_time >= TIMESTAMP '{{project_start_date}}'
+        {% endif %}
+
+),
+
+prices AS (
+    SELECT 
+        minute
+        ,contract_address
+        ,decimals
+        ,symbol
+        ,price
+    FROM
+        {{ source('prices', 'usd') }}
+    WHERE
+        blockchain = 'gnosis'
+        {% if is_incremental() %}
+        AND {{ incremental_predicate('minute') }}
+        {% else %}
+        AND minute >= DATE '{{project_start_date}}'
+        {% endif %}
+),
+
+trades_usd AS (
+    SELECT
+        t1.block_time
+        ,t1.block_day
+        ,t1.tx_from 
+        ,t1.tx_to 
+        ,t1.tx_hash 
+        ,t1.evt_index
+        ,t2.fixedProductMarketMaker
+        ,t2.collateralToken
+        ,t3.symbol
+        ,t1.address
+        ,t1.outcomeIndex
+        ,t1.amount/POWER(10,t3.decimals) AS amount
+        ,t1.amount/POWER(10,t3.decimals) * t3.price AS amount_usd
+        ,t1.feeAmount/POWER(10,t3.decimals) AS feeAmount
+        ,t1.feeAmount/POWER(10,t3.decimals) * t3.price AS feeAmount_usd
+        ,t1.outcomeTokens
+        ,t1.action
+    FROM
+        trades t1 
+    LEFT JOIN 
+        {{ ref('omen_gnosis_markets') }} t2
+        ON
+        t2.fixedProductMarketMaker = t1.evt_contract_address
+    LEFT JOIN
+        prices t3
+        ON
+        t3.contract_address = t2.collateralToken
+        AND 
+        t3.minute = date_trunc('minute', t1.block_time)
+)
+
+SELECT * FROM trades
