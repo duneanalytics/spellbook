102e96eb81be2cd0009b43d02fd1fcef13ebb977
Landon Gingerich <landon.ging@gmail.com>
2024-03-27 08:02:56 -0500

    Add Pancakeswap V3 dex.trades to zkSync (#5682)
    * init
    
    * add seed
    
    * fix schema def
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/_sector/dex/trades/zksync/dex_zksync_base_trades.sql
+++ models/_sector/dex/trades/zksync/dex_zksync_base_trades.sql
@@ -8,6 +8,7 @@
 {% set base_models = [
     ref('maverick_zksync_base_trades')
     , ref('pancakeswap_v2_zksync_base_trades')
+    , ref('pancakeswap_v3_zksync_base_trades')
     , ref('syncswap_zksync_base_trades')
     , ref('uniswap_v3_zksync_base_trades')
     , ref('mute_zksync_base_trades')
