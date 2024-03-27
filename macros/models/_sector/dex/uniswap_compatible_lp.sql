98a591e70c6aed62c3ecae6e43c4c3a2aee6e33f
0xrusowsky <0xrusowsky@proton.me>
2024-03-27 11:03:03 +0100

    fix: missing alias

--- macros/models/_sector/dex/uniswap_compatible_lp.sql
+++ macros/models/_sector/dex/uniswap_compatible_lp.sql
@@ -116,7 +116,7 @@ SELECT
     , date_trunc('MONTH', lp_data.block_time) AS block_month
     , lp_data.tx_hash
     , lp_data.evt_index
-    , lp_data.lp_address
+    , lp_data.lp_address as liquidity_provider
     , lp_data.position_id
     , lp_data.tick_lower
     , lp_data.tick_upper
