afa53048aac6f061e1d6c3dc773b2bda966df296
Landon Gingerich <landon.ging@gmail.com>
2024-03-28 18:52:46 +0000

    init

--- models/dex/dex_info.sql
+++ models/dex/dex_info.sql
@@ -146,4 +146,5 @@ FROM (VALUES
     , ('syncswap', 'SyncSwap', 'Direct', 'SyncSwap')
     , ('mute', 'Mute', 'Direct', 'Mute')
     , ('spacefi', 'SpaceFi', 'Direct', 'SpaceFi')
+    , ('vesync', 'veSync', 'Direct', 'veSync')
     ) AS temp_table (project, name, marketplace_type, x_username)
