fe795ae7fff59637c17818acbf51f4b1200ef946
hdser <hugo.serodio@gnosis.io>
2024-03-28 16:36:17 +0100

    fix model

--- models/omen/gnosis/omen_gnosis_liquidity.sql
+++ models/omen/gnosis/omen_gnosis_liquidity.sql
@@ -6,7 +6,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_time', 'tx_hash', 'evt_index', '	amount_index'],
+    unique_key = ['block_time', 'tx_hash', 'evt_index', 'amount_index'],
     post_hook='{{ expose_spells(\'["gnosis"]\',
                                 "project",
                                 "omen",
