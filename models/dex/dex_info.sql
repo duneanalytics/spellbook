358c23c37bed27fd6373496f00f2129ba48a9122
archie17277 <archie17277@gmail.com>
2024-04-03 21:16:55 +0530

    Added auragi swap dex

--- models/dex/dex_info.sql
+++ models/dex/dex_info.sql
@@ -131,4 +131,5 @@ FROM (VALUES
     , ('sharkyswap', 'Sharky Swap', 'Direct', 'SharkySwapFi')
     , ('zebra', 'Zebra', 'Direct', 'zebra')
     , ('scrollswap', 'ScrollSwap', 'Direct', 'scrollswap')
+    , ('auragi', 'Auragi', 'Direct', 'AuragiFinance')
     ) AS temp_table (project, name, marketplace_type, x_username)
