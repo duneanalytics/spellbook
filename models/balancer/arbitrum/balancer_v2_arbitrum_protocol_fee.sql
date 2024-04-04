7caa7b03ae03627059aad4c510623cf47438492e
viniabussafi <vinicius@balancerlabs.dev>
2024-04-04 12:26:34 +0100

    retry

--- models/balancer/arbitrum/balancer_v2_arbitrum_protocol_fee.sql
+++ models/balancer/arbitrum/balancer_v2_arbitrum_protocol_fee.sql
@@ -4,12 +4,8 @@
     config(
         schema = 'balancer_v2_arbitrum',
         alias = 'protocol_fee', 
-        materialized = 'incremental',
-        file_format = 'delta',
-        incremental_strategy = 'merge',
-        unique_key = ['day', 'pool_id', 'token_address'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
-
+        materialized = 'table',
+        file_format = 'delta'
     )
 }}
 
