5e3c5fb9531d98b7621dccb9d6f596810cd663a7
Lydia Puspita <113081740+lydiapuspita@users.noreply.github.com>
2024-04-04 23:00:11 +0700

    Update prices_ethereum_tokens.sql (#5720)
    * Update prices_ethereum_tokens.sql
    
    * Update prices_ethereum_tokens.sql
    
    update (,) comma
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/prices/ethereum/prices_ethereum_tokens.sql
+++ models/prices/ethereum/prices_ethereum_tokens.sql
@@ -1705,6 +1705,8 @@ FROM
     ('tuna-tunachain', 'ethereum', 'TUNA',0xadd353fb2e2c563383ff3272a500f3e7134dafe4,18),
     ('pxeth-pirex-ether', 'ethereum', 'PXETH',0x04c154b66cb340f3ae24111cc767e0184ed00cc6,18),
     ('sdai-savings-dai', 'ethereum', 'SDAI',0x83f20f44975d03b1b09e64809b757c47f942beea,18),
+    ('strk-starknet', 'ethereum', 'STRK',0xCa14007Eff0dB1f8135f4C25B34De49AB0d42766,18),
+    ('ela-elastos', 'ethereum', 'ELA', 0xe6fd75ff38adca4b97fbcd938c86b98772431867,18),
     ('frxeth-frax-ether', 'ethereum', 'FRXETH',0x5e8422345238f34275888049021821e8e08caa1f,18),  
     --('cgpt-chaingpt', 'ethereum', 'CGPT',0x25931894a86d47441213199621f1f2994e1c39aa,18), --not in API 
     ('cswap-chainswap', 'ethereum', 'CSWAP',0xae41b275aaaf484b541a5881a2dded9515184cca,18),
