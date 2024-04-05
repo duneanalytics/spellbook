bef01404ce63497e11683fedd038eea74f78c4f5
tomfutago <tomfutago@gmail.com>
2024-04-05 11:20:34 +0100

    init

---
+++ models/_sector/attestation/eas/celo/eas_celo_schemas.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    schema = 'eas_celo',
+    alias = 'schemas',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['schema_uid'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+  )
+}}
+
+{{
+  eas_schemas(
+    blockchain = 'celo',
+    project = 'eas',
+    version = '1'
+  )
+}}
