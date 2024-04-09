8b8029078593e3228c50aaa78891546181e6f641
AnonJon <jon.gregis@gmail.com>
2024-04-09 13:41:33 -0500

    add in burned event

---
+++ models/chainlink/base/chainlink_base_ccip_tokens_transferred_logs.sql
@@ -0,0 +1,49 @@
+{{
+  config(
+    
+    alias='ccip_tokens_transferred_logs',
+    materialized='view'
+  )
+}}
+
+SELECT
+  'base' as blockchain,
+  block_hash,
+  contract_address,
+  data,
+  topic0,
+  topic1,
+  topic2,
+  topic3,
+  tx_hash,
+  block_number,
+  block_time,
+  index,
+  tx_index,
+  tx_from,
+  bytearray_to_uint256(bytearray_substring(data, 1, 32)) as total_tokens
+FROM
+  {{ source('base', 'logs') }} logs
+WHERE
+  topic0 = 0x9f1ec8c880f76798e7b793325d625e9b60e4082a553c98f42b6cda368dd60008 -- Locked
+UNION ALL
+SELECT
+  'base' as blockchain,
+  block_hash,
+  contract_address,
+  data,
+  topic0,
+  topic1,
+  topic2,
+  topic3,
+  tx_hash,
+  block_number,
+  block_time,
+  index,
+  tx_index,
+  tx_from,
+  bytearray_to_uint256(bytearray_substring(data, 1, 32)) as total_tokens
+FROM
+  {{ source('base', 'logs') }} logs
+WHERE
+  topic0 = 0x696de425f79f4a40bc6d2122ca50507f0efbeabbff86a84871b7196ab8ea8df7 -- Burned(address,uint256)
