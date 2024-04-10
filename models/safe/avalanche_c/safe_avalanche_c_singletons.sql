abd6b308f796c9b69ebb1d363ba53b040ef0f8fe
Peter <49856481+peterrliem@users.noreply.github.com>
2024-04-10 12:02:45 +0800

    Update Singleton v1.4.1

--- models/safe/avalanche_c/safe_avalanche_c_singletons.sql
+++ models/safe/avalanche_c/safe_avalanche_c_singletons.sql
@@ -14,3 +14,8 @@
 -- Fetch all known singleton addresses used via the factory.
 select distinct singleton as address 
 from {{ source('gnosis_safe_avalanche_c', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
+
+union all
+
+select distinct singleton as address
+from {{ source('gnosis_safe_avalanche_c', 'SafeProxyFactory_v_1_4_1_evt_ProxyCreation') }}
