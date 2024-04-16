5430be3b872a5e944ff91d2f769bc0d8f5d480bc
yy-analytics <yieldyak.analytics@gmail.com>
2024-04-16 15:12:51 +0530

    Fixed arbitrum balances dependency mislabelling

--- models/yield_yak/arbitrum/yield_yak_arbitrum_balances.sql
+++ models/yield_yak/arbitrum/yield_yak_arbitrum_balances.sql
@@ -9,9 +9,9 @@
         incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.to_time')]
     )
 }}
--- depends_on: {{ ref('yield_yak_avalanche_c_deposits') }}
--- depends_on: {{ ref('yield_yak_avalanche_c_withdraws') }}
--- depends_on: {{ ref('yield_yak_avalanche_c_reinvests') }}
+-- depends_on: {{ ref('yield_yak_arbitrum_deposits') }}
+-- depends_on: {{ ref('yield_yak_arbitrum_withdraws') }}
+-- depends_on: {{ ref('yield_yak_arbitrum_reinvests') }}
 {{
     yield_yak_balances(
         blockchain = 'arbitrum'
