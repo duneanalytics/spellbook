7248319562f8bb8f03219a13fe132f75a4e97164
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-04-19 17:46:00 +0200

    updated stEth with correct token_id

--- models/prices/gnosis/prices_gnosis_tokens.sql
+++ models/prices/gnosis/prices_gnosis_tokens.sql
@@ -35,5 +35,5 @@ FROM
     ('sdai-savings-dai', 'gnosis', 'SDAI',0xaf204776c7245bF4147c2612BF6e5972Ee483701,18),
     ('eure-monerium-eur-emoney', 'gnosis', 'EURe', 0xcB444e90D8198415266c6a2724b7900fb12FC56E, 18),
     ('olas-autonolas', 'gnosis', 'OLAS', 0xcE11e14225575945b8E6Dc0D4F2dD4C570f79d9f, 18),
-    ('steth-lido-staked-ether-on-xDAI', 'gnosis', 'stETH', 0xae7ab96520de3a18e5e111b5eaab095312d7fe84, 18)
+    ('steth-lido-staked-ether', 'gnosis', 'stETH', 0x3C037849a8ffcf19886e2f5B04f293B7847D0377, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
