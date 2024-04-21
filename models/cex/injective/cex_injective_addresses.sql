e4e109f0ea4c5dbdb3a93ff012d2c73b4a005c82
Hildebert Moulié <h.moulie@gmail.com>
2024-04-22 01:55:06 +0200

    fix

--- models/cex/injective/cex_injective_addresses.sql
+++ models/cex/injective/cex_injective_addresses.sql
@@ -8,5 +8,5 @@
 
 SELECT blockchain, address, cex_name, distinct_name, added_by, added_date
 FROM (VALUES
-    , ('injective', 'inj1uyc234cek2ja9ru7a870cmx2lcavt5um2nk6hh', 'LAToken', 'LAToken 1', 'hildobby', date '2024-04-20')
+    ('injective', 'inj1uyc234cek2ja9ru7a870cmx2lcavt5um2nk6hh', 'LAToken', 'LAToken 1', 'hildobby', date '2024-04-20')
     ) AS x (blockchain, address, cex_name, distinct_name, added_by, added_date)
