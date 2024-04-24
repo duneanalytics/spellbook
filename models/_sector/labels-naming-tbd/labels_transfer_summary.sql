4a5d35406ed7c39a0db12df726405e92065426fb
0xRob <0xRobin5@gmail.com>
2024-04-24 16:26:30 +0200

    init

--- models/_sector/labels-naming-tbd/labels_transfer_summary.sql
+++ models/_sector/labels-naming-tbd/labels_transfer_summary.sql
@@ -10,17 +10,17 @@
 
 select
     l.blockchain
-    ,l.address
+    ,from_hex(l.address) as address
     ,l.custody_owner
     ,l.account_owner
-    ,count(*) filter (where "from" = l.address) as transfers_out
-    ,count(*) filter (where "to" = l.address) as transfers_in
-    ,sum(amount_usd) filter (where "from" = l.address) as usd_out
-    ,sum(amount_usd) filter (where "to" = l.address) as usd_in
+    ,count(*) filter (where "from" = from_hex(l.address)) as transfers_out
+    ,count(*) filter (where "to" = from_hex(l.address)) as transfers_in
+    ,sum(amount_usd) filter (where "from" = from_hex(l.address)) as usd_out
+    ,sum(amount_usd) filter (where "to" = from_hex(l.address)) as usd_in
     ,min_by(tx_hash, block_time) as first_tx_hash
     ,max_by(tx_hash, block_time) as last_tx_hash
 from {{ref('tokens_transfers')}} t
 left join {{source('labels','owner_addresses')}} l
 on t.blockchain = l.blockchain
- and ("from" = l.address or "to" = l.address)
+ and ("from" = from_hex(l.address) or "to" = from_hex(l.address))
 group by 1,2,3,4
