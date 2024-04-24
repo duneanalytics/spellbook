64b3cbc7612c13c0d71b35ef08e3d198e918038c
Daniel <32246390+danielpartida@users.noreply.github.com>
2024-04-24 16:55:55 +0200

    Add zkevm to safe models (#5758)
    * Add v1.4.1 singletons
    
    L1 singleton 0x41675C099F32341bf84BFc5382aF534df5C7461a
    L2 singleton 0x29fcB43b46531BcA003ddC8FCB67FFE91900C76
    
    * Change goerli test
    
    * Declare new Safe models for zkevm
    
    * Implement Safe models for zkevm
    
    * Update crosschain models with zkevm data
    
    Models updated are:
    - safe_native_transfers.all.sql -> Added safe_zkevm_matic_transfers
    - safe_safes_all.sql -> Added safe_zkevm_safes
    - safe_transactions_all.sql -> safe_zkevm_transactions
    
    * Bugfix: Joining correctly on celo transactions for safe_celo_transactions spell
    
    * Fix project start for safe_polygon_matic_transfers.sql
    
    * rename correctly Safe zkevm transactions spell
    
    * Add zkevm base source
    
    * Implement correctly base source
    
    # Conflicts:
    #	sources/_base_sources/zkevm_base_sources.yml
    
    * Fix zkevm schema
    
    * Trigger run again
    
    * Remove singletons spell
    
    * Add singletons spell
    
    * add decoded source tables
    
    * Fix duplicate anchor
    
    * Update sources/safe/zkevm/safe_zkevm_sources.yml
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Adapt test
    
    * Update models/safe/zkevm/safe_zkevm_matic_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Update models/safe/zkevm/safe_zkevm_matic_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Add schema to zkevm Safe data models
    
    * Adapt pot_hook section
    
    * Use same incremental interval in union
    
    * Set project start date
    
    * Add unique combo of columns test
    
    * remove unique_key column on zkevm_transactions
    
    * Revert "remove unique_key column on zkevm_transactions"
    
    This reverts commit 5e44197e8490352cae0bc6471b623813072c0538.
    
    * Delelete unique key on safe_zkevm_transacions
    
    * Update models/safe/zkevm/safe_zkevm_matic_transfers.sql
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    
    * Add increment join
    
    Following suggestion of Hosuke
    
    * Address merge conflicts
    
    * Address one more merge conflict
    
    * Address one more merge conflict
    
    * Quickfix: Change block_time incremental join
    
    * Update models/safe/zkevm/safe_zkevm_transactions.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update models/safe/zkevm/safe_zkevm_transactions.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Address feedback of jeff
    
    * Move unique combo test to safe txs spell
    
    * Update models/safe/zkevm/safe_zkevm_schema.yml
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/safe/zkevm/safe_zkevm_transactions.sql
+++ models/safe/zkevm/safe_zkevm_transactions.sql
@@ -1,22 +1,24 @@
 {{
     config(
         materialized='incremental',
-        
-        schema='safe_celo',
-        alias = 'transactions',
+        schema = 'safe_zkevm',
+        alias= 'transactions',
         partition_by = ['block_month'],
         unique_key = ['block_date', 'tx_hash', 'trace_address'],
         file_format ='delta',
         incremental_strategy='merge',
-        post_hook='{{ expose_spells(\'["celo"]\',
-                                    "project",
-                                    "safe",
-                                    \'["danielpartida", "hosuke"]\') }}'
+        post_hook = '{{ expose_spells(
+                        blockchains = \'["zkevm"]\',
+                        spell_type = "project",
+                        spell_name = "safe",
+                        contributors = \'["danielpartida"]\') }}'
     )
 }}
 
+{% set project_start_date = '2023-09-01' %}
+
 select
-    'celo' as blockchain,
+    'zkevm' as blockchain,
     try_cast(date_trunc('day', tr.block_time) as date) as block_date,
     CAST(date_trunc('month', tr.block_time) as DATE) as block_month,
     tr.block_time,
@@ -42,13 +44,16 @@ select
         when bytearray_substring(tr.input, 1, 4) = 0x5229073f then 'execTransactionFromModuleReturnData'
         else 'unknown'
     end as method
-from {{ source('celo', 'traces') }} tr
-join {{ ref('safe_celo_safes') }} s
+from {{ source('zkevm', 'traces') }} tr
+join {{ ref('safe_zkevm_safes') }} s
     on s.address = tr."from"
-join {{ ref('safe_celo_singletons') }} ss
+join {{ ref('safe_zkevm_singletons') }} ss
     on tr.to = ss.address
-join {{ source('celo', 'transactions') }} et
+join {{ source('zkevm', 'transactions') }} et
     on tr.tx_hash = et.hash
+    {% if is_incremental() %}
+    and {{ incremental_predicate('et.block_time') }}
+    {% endif %}
     and tr.block_number = et.block_number
 where bytearray_substring(tr.input, 1, 4) in (
         0x6a761202, -- execTransaction
@@ -57,8 +62,8 @@ where bytearray_substring(tr.input, 1, 4) in (
     )
     and tr.call_type = 'delegatecall'
     {% if not is_incremental() %}
-    and tr.block_time > TIMESTAMP '2021-06-20' -- for initial query optimisation
+    and tr.block_time > TIMESTAMP '{{project_start_date}}' -- for initial query optimisation
     {% endif %}
     {% if is_incremental() %}
-    and tr.block_time > date_trunc('day', now() - interval '7' day)
+    and {{ incremental_predicate('tr.block_time') }}
     {% endif %}
