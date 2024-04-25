7ca2531139d6ec4a8672edeb26e1619e048ad5c6
ppclunghe <pipistrella@list.ru>
2024-04-25 17:18:14 +0200

    fix comma

--- models/prices/linea/prices_linea_tokens.sql
+++ models/prices/linea/prices_linea_tokens.sql
@@ -19,6 +19,6 @@ FROM
     ('usdc-usd-coin', 'linea', 'USDC', 0x176211869cA2b568f2A7D4EE941E073a821EE1ff, 6),
     ('usdt-tether', 'linea', 'USDT', 0xA219439258ca9da29E9Cc4cE5596924745e12B93, 6),
     ('weth-weth', 'linea', 'WETH', 0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f, 18),
-    ('wsteth-wrapped-liquid-staked-ether-20', 'linea', 'wstETH', 0xB5beDd42000b71FddE22D3eE8a79Bd49A568fC8F, 18),
+    ('wsteth-wrapped-liquid-staked-ether-20', 'linea', 'wstETH', 0xB5beDd42000b71FddE22D3eE8a79Bd49A568fC8F, 18)
 
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
