50835c0ebd3f91543411f0f81fb786b97659de35
0xRob <0xRobin5@gmail.com>
2024-04-25 12:42:49 +0200

    try this

--- models/_sector/labels-naming-tbd/labels_transfer_summary.sql
+++ models/_sector/labels-naming-tbd/labels_transfer_summary.sql
@@ -6,21 +6,24 @@
         )
 }}
 
-
-
+WITH stats_in as (
 select
     l.blockchain
     ,from_hex(l.address) as address
     ,l.custody_owner
     ,l.account_owner
-    ,count(*) filter (where "from" = from_hex(l.address)) as transfers_out
-    ,count(*) filter (where "to" = from_hex(l.address)) as transfers_in
-    ,sum(amount_usd) filter (where "from" = from_hex(l.address)) as usd_out
-    ,sum(amount_usd) filter (where "to" = from_hex(l.address)) as usd_in
+--    ,count(*) filter (where "from" = from_hex(l.address)) as transfers_out
+    ,count(*) as transfers_in
+--    ,sum(amount_usd) filter (where "from" = from_hex(l.address)) as usd_out
+    ,sum(amount_usd) as usd_in
     ,min_by(tx_hash, block_time) as first_tx_hash
     ,max_by(tx_hash, block_time) as last_tx_hash
-from {{ref('tokens_ethereum_transfers')}} t
-inner join {{source('labels','owner_addresses')}} l
+from {{source('labels','owner_addresses')}} l
+left join {{ref('tokens_ethereum_transfers')}} t
 on t.blockchain = l.blockchain
- and ("from" = from_hex(l.address) or "to" = from_hex(l.address))
+ and "to" = from_hex(l.address)
 group by 1,2,3,4
+)
+
+select * from stats_in
+
