01e199cb054d44ba9ac49ed2fcb63b95c5de1c53
thecaptncrunch <nia_whitaker@protonmail.com>
2024-04-26 12:51:46 -0400

    Corrected API

--- models/prices/ethereum/prices_ethereum_tokens.sql
+++ models/prices/ethereum/prices_ethereum_tokens.sql
@@ -1735,7 +1735,7 @@ FROM
     ('rsweth-restaked-swell-eth', 'ethereum', 'RSWETH', 0xfae103dc9cf190ed75350761e95403b7b8afa6c0, 18),
     ('safe-safe', 'ethereum', 'SAFE', 0x5afe3855358e112b5647b952709e6165e1c1eeee, 18),
     ('omni-omni-network', 'ethereum', 'OMNI', 0x36e66fbbce51e4cd5bd3c62b637eb411b18949d4, 18),
-    ('stakestone-ether', 'ethereum', 'STONE', 0x7122985656e38BDC0302Db86685bb972b145bD3C, 18)
+    ('stone-stakestone-ether', 'ethereum', 'STONE', 0x7122985656e38BDC0302Db86685bb972b145bD3C, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
 where contract_address not in (
     -- bad price feeds
