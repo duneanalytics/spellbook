592e095a33dd61e565d6578f09abe68b03cade32
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-04-29 16:45:10 +0100

    Add missing tokens to prices.usd (#5850)
    * Update prices_arbitrum_tokens.sql
    
    * Update prices_gnosis_tokens.sql
    
    * Update prices_optimism_tokens_curated.sql

--- models/prices/optimism/prices_optimism_tokens_curated.sql
+++ models/prices/optimism/prices_optimism_tokens_curated.sql
@@ -117,6 +117,7 @@ FROM
     ('grain-granary', 'GRAIN', 0xfD389Dc9533717239856190F42475d3f263a270d,18),
     ('usdc-usd-coin', 'USDC', 0x0b2c639c533813f4aa9d7837caf62653d097ff85, 6),
     ('tbtc-tbtc', 'TBTC', 0x6c84a8f1c29108f47a79964b5fe888d4f4d0de40, 18),
-    ('axlusdc-axelar-wrapped-usdc', 'AXLUSDC', 0xeb466342c4d449bc9f53a865d5cb90586f405215, 6)
+    ('axlusdc-axelar-wrapped-usdc', 'AXLUSDC', 0xeb466342c4d449bc9f53a865d5cb90586f405215, 6),
+    ('beets-beethoven-x', 'BEETS', 0xb4bc46bc6cb217b59ea8f4530bae26bf69f677f0, 18)
 
 ) as temp (token_id, symbol, contract_address, decimals)
