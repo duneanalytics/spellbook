fd4dfc1d1ca2870ef90785a020e3c1bbfd583f83
0xRob <0xRobin5@gmail.com>
2024-04-30 11:39:31 +0200

    add daily spell

--- models/_sector/labels-naming-tbd/labels_transfer_summary.sql
+++ models/_sector/labels-naming-tbd/labels_transfer_summary.sql
@@ -29,6 +29,10 @@ select
     ,l.address
     ,count(*) as transfers_out
     ,sum(amount_usd)  as usd_out
+    ,min_by(tx_hash, block_time) as first_tx_hash
+    ,min(block_time) as first_block_time
+    ,max_by(tx_hash, block_time) as last_tx_hash
+    ,max(block_time) as last_block_time
 from {{source('labels','owner_addresses')}} l
 left join {{ref('tokens_ethereum_transfers')}} t
 on t.blockchain = l.blockchain
@@ -45,8 +49,10 @@ select
     ,transfers_out
     ,usd_in
     ,usd_out
-    ,first_tx_hash
-    ,last_tx_hash
+    ,case when stats_in.first_block_time < stats_out.first_block_time then stats_in.first_tx_hash
+        else stats_out.first_tx_hash end as first_tx_hash
+    ,case when stats_in.last_block_time > stats_out.last_block_time then stats_in.last_tx_hash
+        else stats_out.last_tx_hash end as last_tx_hash
 from {{source('labels','owner_addresses')}} l
 left join stats_in using (blockchain, address)
 left join stats_out using (blockchain, address)
---
+++ models/_sector/labels-naming-tbd/labels_transfer_summary_daily.sql
@@ -0,0 +1,54 @@
+{{ config(
+        schema = 'labels',
+        alias = 'transfer_summary_daily',
+        partition_by = ['blockchain'],
+        materialized = 'table'
+        )
+}}
+
+WITH stats_in as (
+select
+    l.blockchain
+    ,l.address
+    ,t.block_date
+    ,count(*) as transfers_in
+    ,sum(amount_usd) as usd_in
+from {{source('labels','owner_addresses')}} l
+left join {{ref('tokens_ethereum_transfers')}} t
+on t.blockchain = l.blockchain
+ and "to" = from_hex(l.address)
+group by 1,2,3
+)
+
+,stats_out as (
+select
+    l.blockchain
+    ,l.address
+    ,t.block_date
+    ,count(*) as transfers_out
+    ,sum(amount_usd)  as usd_out
+from {{source('labels','owner_addresses')}} l
+left join {{ref('tokens_ethereum_transfers')}} t
+on t.blockchain = l.blockchain
+ and "from" = from_hex(l.address)
+group by 1,2,3
+)
+
+select
+    l.blockchain
+    ,l.address
+    ,l.custody_owner
+    ,l.account_owner
+    ,coalesce(stats_in.block_date,stats_out.block_date)
+    ,transfers_in
+    ,transfers_out
+    ,usd_in
+    ,usd_out
+from {{source('labels','owner_addresses')}} l
+left join stats_in
+on l.blockchain = stats_in.blockchain
+    and l.address = stats_in.address
+left join stats_out
+on l.blockchain = stats_out.blockchain
+    and l.address = stats_out.address
+    and (stat_in.block_date is null or stats_in.block_date = stats_out.block_date)
