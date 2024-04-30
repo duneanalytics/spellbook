df8b62a6ed595488760b25ab315a1d2fada91d93
0xRob <0xRobin5@gmail.com>
2024-04-30 15:55:28 +0200

    try this

--- macros/models/_sector/nft/platforms/seaport_v4_trades.sql
+++ macros/models/_sector/nft/platforms/seaport_v4_trades.sql
@@ -218,7 +218,7 @@ with source_ethereum_transactions as (
     select a.block_time
             ,a.block_number
             ,a.tx_hash
-            ,coalesce(a.om_evt_index, a.evt_index) as evt_index  -- when order_matched exists, then replace evt_index to its
+            ,coalesce(a.om_evt_index, 0 ) + a.evt_index as evt_index  -- when orders_matched exists, add the evt_indexes to prevent duplication
             ,a.sub_type
             ,a.sub_idx
             ,a.offer_first_item_type
