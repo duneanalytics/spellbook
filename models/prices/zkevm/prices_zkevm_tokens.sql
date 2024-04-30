27734e94cd1433500855a3c28811c5bbe784d487
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-04-30 19:33:10 -0400

    remove invalid id

--- models/prices/zkevm/prices_zkevm_tokens.sql
+++ models/prices/zkevm/prices_zkevm_tokens.sql
@@ -20,7 +20,6 @@ FROM
     ('usdc-usdc','zkevm','USDC',0x37eaa0ef3549a5bb7d431be78a3d99bd360d19e5,6),
     ('dai-dai','zkevm','DAI',0x744c5860ba161b5316f7e80d9ec415e2727e5bd5,18),
     ('rseth-rseth','zkevm','RSETH',0x8c7d118b5c47a5bcbd47cc51789558b98dad17c5,18),
-    ('rwsteth-wrapped-liquid-staked-ether-20','zkevm','WSTETH',0x5d8cff95d7a57c0bf50b30b43c7cc0d52825d4a9,18),
     ('aura-aura-finance','zkevm','AURA',0x1509706a6c66ca549ff0cb464de88231ddbe213b,18),
     ('matic-polygon','zkevm','MATIC',0xa2036f0538221a77a3937f1379699f44945018d0,18),
     ('aeth-ankreth','zkevm','AETH',0x12d8ce035c5de3ce39b1fdd4c1d5a745eaba3b8c,18),
