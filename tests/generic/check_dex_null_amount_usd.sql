2d61eb276e1aa091af2230b309a6d2e21b2d8f56
Huang Geyang <Sukebeta@outlook.com>
2024-05-01 17:36:40 +0800

    Add group_by param in the test

--- tests/generic/check_dex_null_amount_usd.sql
+++ tests/generic/check_dex_null_amount_usd.sql
@@ -1,16 +1,20 @@
-{% test check_dex_null_amount_usd(model) %}
+{% test check_dex_null_amount_usd(model, group_by='blockchain') %}
 
     {%- set threshold = 0.05 -%}
 
     WITH base AS (
         SELECT
+            {{ group_by }},
             COUNT(*) AS total_rows,
             COUNT(CASE WHEN amount_usd IS NULL THEN 1 END) AS null_rows
         FROM {{ model }}
+        GROUP BY {{ group_by }}
     ),
 
     result AS (
         SELECT
+            {{ group_by }},
+            null_rows * 1.0 / total_rows AS null_amount_usd_percentage,
             CASE
                 WHEN null_rows * 1.0 / total_rows > {{ threshold }} THEN
                     'Fail: The percentage of rows with null amount_usd is above the threshold'
