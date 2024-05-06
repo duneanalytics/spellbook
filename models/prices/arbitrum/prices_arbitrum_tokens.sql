359e6f79f0a6c8763c18e268317e2c214bb8779c
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-05-06 13:42:54 +0100

    Update prices_arbitrum_tokens.sql

--- models/prices/arbitrum/prices_arbitrum_tokens.sql
+++ models/prices/arbitrum/prices_arbitrum_tokens.sql
@@ -188,6 +188,6 @@ FROM
     ('lndx-landx-governance-token','arbitrum','LNDX',0xe10d4a4255d2d35c9e23e2c4790e073046fbaf5c, 6),
     ('wom-wombat-exchange','arbitrum','WOM',0x7b5eb3940021ec0e8e463d5dbb4b7b09a89ddf96, 18),
     ('ankr-ankr-network','arbitrum','ANKR',0x46f74778b265df3a15ec9695ccd2fd3869ca848c, 18),
-    ('forex-handlefi','arbitrum','FOREX',0x46f74778b265df3a15ec9695ccd2fd3869ca848c, 18),
+    ('forex-handlefi','arbitrum','FOREX',0xdb298285fe4c5410b05390ca80e8fbe9de1f259b, 18),
     ('susde-ethena-staked-usde','arbitrum','SUSDE',0x211cc4dd073734da055fbf44a2b4667d5e5fe5d2, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
