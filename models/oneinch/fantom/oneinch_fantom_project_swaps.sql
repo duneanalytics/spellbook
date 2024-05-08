78d1afff125e22ee3c8cfc6fdb9c30376efd268a
grkhr <eduardgorkh@gmail.com>
2024-05-08 16:00:05 +0300

    prod exclude

--- models/oneinch/fantom/oneinch_fantom_project_swaps.sql
+++ models/oneinch/fantom/oneinch_fantom_project_swaps.sql
@@ -9,6 +9,7 @@
         partition_by = ['block_month'],
         materialized = 'incremental',
         file_format = 'delta',
+        tags = ['prod_exclude'],
         incremental_strategy = 'merge',
         unique_key = ['blockchain', 'tx_hash', 'call_trace_address']
     )
