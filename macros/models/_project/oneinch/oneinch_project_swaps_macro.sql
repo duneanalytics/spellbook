3c498a74140d0302443423c52e454666d084f18f
grkhr <eduardgorkh@gmail.com>
2024-05-08 17:47:28 +0300

    fix

--- macros/models/_project/oneinch/oneinch_project_swaps_macro.sql
+++ macros/models/_project/oneinch/oneinch_project_swaps_macro.sql
@@ -96,7 +96,7 @@ methods as (
         *
         , array_union(transform(filter(array_distinct(flatten(call_transfer_addresses)), x -> x[2]), x -> (x[1])), if(flags['limits'], array[], array[tx_from])) as users
         , array_agg(
-            cast(row(project, call_trace_address, coalesce(user_amount_usd, amount_usd)) as row(varchar, array(bigint), double))
+            cast(row(project, call_trace_address, coalesce(user_amount_usd, amount_usd)) as row(project varchar, call_trace_address array(bigint), amount_usd double))
         ) over(partition by block_number, tx_hash) as amounts
         , coalesce(if(direct, user_amount_usd, caller_amount_usd), amount_usd) as result_amount_usd
     from (
