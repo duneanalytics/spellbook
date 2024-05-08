8ae3b24f3aafcbc94b185e158f661339746f9ac0
hdser <hugo.serodio@gnosis.io>
2024-05-08 12:28:05 +0200

    Added GBPe token for Gnosis

--- models/prices/gnosis/prices_gnosis_tokens.sql
+++ models/prices/gnosis/prices_gnosis_tokens.sql
@@ -34,6 +34,7 @@ FROM
     ('wsteth-wrapped-liquid-staked-ether-20', 'gnosis', 'WSTETH', 0x6c76971f98945ae98dd7d4dfca8711ebea946ea6, 18),
     ('sdai-savings-dai', 'gnosis', 'SDAI',0xaf204776c7245bF4147c2612BF6e5972Ee483701,18),
     ('eure-monerium-eur-emoney', 'gnosis', 'EURe', 0xcB444e90D8198415266c6a2724b7900fb12FC56E, 18),
+    ('gbpe-monerium-gbp-emoney', 'gnosis', 'GBPe', 0x5Cb9073902F2035222B9749F8fB0c9BFe5527108, 18),
     ('olas-autonolas', 'gnosis', 'OLAS', 0xcE11e14225575945b8E6Dc0D4F2dD4C570f79d9f, 18),
     ('steth-lido-staked-ether', 'gnosis', 'stETH', 0x3C037849a8ffcf19886e2f5B04f293B7847D0377, 18),
     ('crvusd-curvefi-usd-stablecoin', 'gnosis', 'CRVUSD', 0xaBEf652195F98A91E490f047A5006B71c85f058d, 18),
