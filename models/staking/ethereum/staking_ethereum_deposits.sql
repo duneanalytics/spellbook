e5af5a6286762ba1eac102c50ed785fddd4dbf5d
Hildebert Moulié <h.moulie@gmail.com>
2024-05-14 15:19:41 +0200

    trigger flows rerun

--- models/staking/ethereum/staking_ethereum_deposits.sql
+++ models/staking/ethereum/staking_ethereum_deposits.sql
@@ -30,7 +30,7 @@ WITH deposit_events AS (
     , ROW_NUMBER() OVER (PARTITION BY d.evt_block_number, d.evt_tx_hash, from_big_endian_64(reverse(d.amount)) ORDER BY d.evt_index) AS table_merging_deposits_id
     FROM {{ source('eth2_ethereum', 'DepositContract_evt_DepositEvent') }} d
     {% if not is_incremental() %}
-    WHERE d.evt_block_time >= TIMESTAMP '2020-10-13' -- SHOULD BE 2020-10-14 BUT CHANGED TO 2020-10-13 TO TRIGGER TABLE RERUN
+    WHERE d.evt_block_time >= TIMESTAMP '2020-10-14'
     {% endif %}
     {% if is_incremental() %}
     WHERE d.evt_block_time >= date_trunc('day', now() - interval '7' day)
@@ -49,7 +49,7 @@ WITH deposit_events AS (
     AND t.value > UINT256 '0'
     AND success
     {% if not is_incremental() %}
-    AND t.block_time >= TIMESTAMP '2020-10-13' -- SHOULD BE 2020-10-14 BUT CHANGED TO 2020-10-13 TO TRIGGER TABLE RERUN
+    AND t.block_time >= TIMESTAMP '2020-10-14'
     {% endif %}
     {% if is_incremental() %}
     AND t.block_time >= date_trunc('day', now() - interval '7' day)
@@ -79,7 +79,7 @@ FROM deposit_events d
 INNER JOIN {{ source('ethereum', 'transactions') }} et ON et.block_number=d.block_number
     AND et.hash=d.tx_hash
     {% if not is_incremental() %}
-    AND et.block_time >= TIMESTAMP '2020-10-13' -- SHOULD BE 2020-10-14 BUT CHANGED TO 2020-10-13 TO TRIGGER TABLE RERUN
+    AND et.block_time >= TIMESTAMP '2020-10-14'
     {% endif %}
     {% if is_incremental() %}
     AND et.block_time >= date_trunc('day', now() - interval '7' day)
@@ -100,4 +100,3 @@ LEFT JOIN {{ ref('staking_ethereum_entities')}} etes
     OR (etes.pubkey IS NOT NULL AND d.pubkey=etes.pubkey)
     OR (etes.withdrawal_credentials IS NOT NULL AND d.withdrawal_credentials=etes.withdrawal_credentials))
     AND etes.sub_entity IS NOT NULL
-    
