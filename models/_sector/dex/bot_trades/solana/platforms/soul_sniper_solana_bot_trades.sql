fef2adb692d0157e606c06fd9f6ebc6d0f8fa2ff
whale_hunter <143016036+whalehunting@users.noreply.github.com>
2024-05-14 20:31:58 +0200

    fix: rename to soul_sniper

--- models/_sector/dex/bot_trades/solana/platforms/soul_sniper_solana_bot_trades.sql
+++ models/_sector/dex/bot_trades/solana/platforms/soul_sniper_solana_bot_trades.sql
@@ -1,7 +1,7 @@
 {{
     config(
         alias='bot_trades',
-        schema='soulbot_solana',
+        schema='soul_sniper_solana',
         partition_by=['block_month'],
         materialized='incremental',
         file_format='delta',
