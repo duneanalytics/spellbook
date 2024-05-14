0986af54998ddca37d987aed62cb8d8d173f00f7
whale_hunter <143016036+whalehunting@users.noreply.github.com>
2024-05-14 20:42:27 +0200

    fix: remove duplicated column

--- models/_sector/dex/bot_trades/solana/platforms/consortium_key_solana_bot_trades.sql
+++ models/_sector/dex/bot_trades/solana/platforms/consortium_key_solana_bot_trades.sql
@@ -99,8 +99,6 @@ select
     bottrades.tx_id,
     tx_index,
     bottrades.outer_instruction_index,
-    CAST(NULL AS DOUBLE) as fee_usd,
-    CAST(NULL AS DOUBLE) as fee_token_amount,
     coalesce(inner_instruction_index, 0) as inner_instruction_index,
     if(
         inner_instruction_index = highestinnerinstructionindex, true, false
