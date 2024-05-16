301390f2abc6a7d22afa6c6567797367451fd07c
Huang Geyang <Sukebeta@outlook.com>
2024-05-16 16:30:25 +0800

    Update models/safe/base/safe_base_singletons.sql

--- models/safe/base/safe_base_singletons.sql
+++ models/safe/base/safe_base_singletons.sql
@@ -15,7 +15,7 @@
 select distinct singleton as address
 from {{ source('gnosis_safe_base', 'GnosisSafeProxyFactoryv_1_3_0_evt_ProxyCreation') }}
 
-union all
+union
 
 select distinct singleton as address
 from {{ source('gnosis_safe_base', 'SafeProxyFactory_v_1_4_1_evt_ProxyCreation') }}
