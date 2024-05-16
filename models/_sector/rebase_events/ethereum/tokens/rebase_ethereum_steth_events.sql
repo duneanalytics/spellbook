ecf405a224bfd986b314980ca21096e09076e2b5
Hildebert Moulié <h.moulie@gmail.com>
2024-05-16 12:53:26 +0200

    create rebase events tables

---
+++ models/_sector/rebase_events/ethereum/tokens/rebase_ethereum_steth_events.sql
@@ -0,0 +1,35 @@
+{{ config(
+        schema = 'rebase_ethereum',
+        alias ='steth_events',
+        post_hook='{{ expose_spells(\'["rebase", "ethereum", "lido"]\',
+                                    "sector",
+                                    "rebase",
+                                    \'["hildobby"]\') }}'
+)
+}}
+
+SELECT 'ethereum' AS blockchain
+, contract_address AS token_address
+, 'stETH' AS token_symbol
+, evt_block_time AS block_time
+, evt_block_number AS block_number
+, 1 + (postTotalPooledEther - preTotalPooledEther) / (CAST(preTotalPooledEther AS DOUBLE)) * 0.9 AS rebase_rate
+, evt_tx_hash AS tx_hash
+, evt_index
+FROM {{source('lido_ethereum','LegacyOracle_evt_PostTotalShares')}}
+WHERE evt_block_time <= date('2023-05-16')
+
+UNION ALL
+
+SELECT 'ethereum' AS blockchain
+, contract_address AS token_address
+, 'stETH' AS token_symbol
+, evt_block_time AS block_time
+, evt_block_number AS block_number
+, 1 + (((postTotalEther * 1e27) / CAST(postTotalShares AS DOUBLE))
+    - ((preTotalEther * 1e27) / CAST(preTotalShares AS DOUBLE))
+    ) / ((preTotalEther * 1e27) / CAST(preTotalShares AS DOUBLE))
+    AS rebase_rate
+, evt_tx_hash AS tx_hash
+, evt_index
+FROM {{source('lido_ethereum','steth_evt_TokenRebased')}}
