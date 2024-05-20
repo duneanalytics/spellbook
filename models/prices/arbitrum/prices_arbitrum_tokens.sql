971e0a6989d9fcff511cac7bbb300867ba9ef87c
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-05-20 15:57:00 +0100

    add missing tokens (#5946)
    * add missing tokens
    
    * fix typos

--- models/prices/arbitrum/prices_arbitrum_tokens.sql
+++ models/prices/arbitrum/prices_arbitrum_tokens.sql
@@ -190,5 +190,7 @@ FROM
     ('wom-wombat-exchange','arbitrum','WOM',0x7b5eb3940021ec0e8e463d5dbb4b7b09a89ddf96, 18),
     ('ankr-ankr-network','arbitrum','ANKR',0x46f74778b265df3a15ec9695ccd2fd3869ca848c, 18),
     ('forex-handlefi','arbitrum','FOREX',0xdb298285fe4c5410b05390ca80e8fbe9de1f259b, 18),
-    ('susde-ethena-staked-usde','arbitrum','SUSDE',0x211cc4dd073734da055fbf44a2b4667d5e5fe5d2, 18)
+    ('susde-ethena-staked-usde','arbitrum','SUSDE',0x211cc4dd073734da055fbf44a2b4667d5e5fe5d2, 18),
+    ('ethx-stader-ethx','arbitrum','ETHX',0xed65c5085a18fa160af0313e60dcc7905e944dc7, 18),
+    ('gyd-gyro-dollar','arbitrum','GYD',0xca5d8f8a8d49439357d3cf46ca2e720702f132b8, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
