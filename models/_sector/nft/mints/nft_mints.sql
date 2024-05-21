b0ef5aafdccbaeeb2e0f2115ebcc41f66b0a2f2a
0xRob <0xRobin5@gmail.com>
2024-05-21 12:45:59 +0200

    refactor: Update nft_mints SQL query to remove unnecessary code and combine project_mints and native_mints.

--- models/_sector/nft/mints/nft_mints.sql
+++ models/_sector/nft/mints/nft_mints.sql
@@ -22,55 +22,7 @@
 ,ref('nft_zksync_native_mints')
 ] %}
 
-
-{% set project_mints = [
- ref('zora_mints')
-] %}
-
-WITH project_mints as
-(
-    {% for project_mint in project_mints %}
-    SELECT
-        blockchain,
-        project,
-        version,
-        CAST(date_trunc('day', block_time) as date)  as block_date,
-        CAST(date_trunc('month', block_time) as date)  as block_month,
-        block_time,
-        token_id,
-        collection,
-        amount_usd,
-        token_standard,
-        trade_type,
-        number_of_items,
-        trade_category,
-        evt_type,
-        seller,
-        buyer,
-        amount_original,
-        amount_raw,
-        currency_symbol,
-        currency_contract,
-        nft_contract_address,
-        project_contract_address,
-        aggregator_name,
-        aggregator_address,
-        tx_hash,
-        block_number,
-        tx_from,
-        tx_to,
-        evt_index
-    FROM {{ project_mint }}
-    WHERE evt_type = 'Mint'
-    {% if is_incremental() %}
-    AND block_time >= date_trunc('day', now() - interval '7' Day)
-    {% endif %}
-    {% if not loop.last %}
-    UNION ALL
-    {% endif %}
-    {% endfor %}
-)
-, native_mints AS
+WITH native_mints AS
 (
     SELECT *
     FROM
