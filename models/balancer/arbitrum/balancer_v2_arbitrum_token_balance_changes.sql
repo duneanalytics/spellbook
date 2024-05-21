ff7e4ef93af6647e8ef2a1434f852912862cdd62
viniabussafi <vinicius@balancerlabs.dev>
2024-05-21 15:49:40 +0100

    init new macros, test on arb

---
+++ models/balancer/arbitrum/balancer_v2_arbitrum_token_balance_changes.sql
@@ -0,0 +1,20 @@
+{% set blockchain = 'arbitrum' %}
+
+{{
+    config(
+        schema = 'balancer_v2_arbitrum',
+        alias = 'token_balance_changes', 
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['block_date', 'evt_tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.evt_block_time')]
+    )
+}}
+
+{{ 
+    balancer_token_balance_changes_macro(
+        blockchain = blockchain,
+        version = '2'
+    )
+}}
