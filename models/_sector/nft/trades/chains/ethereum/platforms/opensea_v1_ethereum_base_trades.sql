4a9e485fae5580f84106c7b6492cd88356393be0
0xRob <0xRobin5@gmail.com>
2024-05-21 14:21:14 +0200

    fix

--- models/_sector/nft/trades/chains/ethereum/platforms/opensea_v1_ethereum_base_trades.sql
+++ models/_sector/nft/trades/chains/ethereum/platforms/opensea_v1_ethereum_base_trades.sql
@@ -161,5 +161,5 @@ SELECT
   cast(royalty_fee * (t.amount_raw/t.price_correction) as uint256) AS royalty_fee_amount_raw,
   t.fee_recipient as royalty_fee_address,
   cast(null as varbinary) as platform_fee_address,
-  cast(cast(t.order_evt_index as varchar) || cast(t.nft_evt_index as varchar) as bigint) as sub_tx_trade_id -- using this downstream in nft mints
+  coalesce(t.nft_evt_index, t.order_evt_index) as sub_tx_trade_id -- using this downstream in nft mints
 FROM enhanced_trades t
