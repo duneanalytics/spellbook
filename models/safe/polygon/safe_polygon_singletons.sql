59804e731b04c37d36b509d642a103c9730fb99b
Peter <49856481+peterrliem@users.noreply.github.com>
2024-05-22 16:53:42 +0200

    Added v1.4.1

--- models/safe/polygon/safe_polygon_singletons.sql
+++ models/safe/polygon/safe_polygon_singletons.sql
@@ -6,7 +6,7 @@
         post_hook='{{ expose_spells(\'["polygon"]\',
                                     "project",
                                     "safe",
-                                    \'["tschubotz"]\') }}'
+                                    \'["tschubotz", "peterrliem"]\') }}'
     ) 
 }}
 
@@ -30,3 +30,8 @@ union
 
 select distinct singleton as address 
 from {{ source('gnosis_safe_polygon', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
+
+union
+
+select distinct singleton as address 
+from {{ source('gnosis_safe_polygon', 'ProxyFactoryv_1_4_1_evt_ProxyCreation') }}
