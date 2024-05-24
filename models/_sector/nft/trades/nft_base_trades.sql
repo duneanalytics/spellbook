a092149690228e8cee64e5d72c3b69fee9739c29
0xRob <0xRobin5@gmail.com>
2024-05-24 15:15:16 +0200

    include into nft base trades

--- models/_sector/nft/trades/nft_base_trades.sql
+++ models/_sector/nft/trades/nft_base_trades.sql
@@ -17,6 +17,7 @@
  ,ref('nft_bnb_base_trades')
  ,ref('nft_ethereum_base_trades')
  ,ref('nft_old_base_trades')
+ ,ref('nft_solana_base_trades')
  ,ref('nft_optimism_base_trades')
  ,ref('nft_polygon_base_trades')
  ,ref('nft_zksync_base_trades')
