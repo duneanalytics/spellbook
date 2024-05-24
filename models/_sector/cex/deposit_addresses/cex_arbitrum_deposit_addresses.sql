5bdba8ff8d3f49c457c12d23a3016997d129e0f2
Hildebert Moulié <h.moulie@gmail.com>
2024-05-24 18:47:27 +0200

    fix

--- models/_sector/cex/deposit_addresses/cex_arbitrum_deposit_addresses.sql
+++ models/_sector/cex/deposit_addresses/cex_arbitrum_deposit_addresses.sql
@@ -13,6 +13,7 @@
 
 {{cex_deposit_addresses(
         blockchain = blockchain
+        , token_transfers = ref('tokens_' + blockchain + '_transfers')
         , cex_addresses = ref('cex_' + blockchain + '_addresses')
         , cex_flows = ref('cex_' + blockchain + '_flows')
         , first_funded_by = ref('addresses_events_' + blockchain + '_first_funded_by')
