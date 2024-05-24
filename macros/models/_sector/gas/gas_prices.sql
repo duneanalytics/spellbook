f5c03578c81a8d47b9c64e6dc723636d3807537d
Hildebert Moulié <h.moulie@gmail.com>
2024-05-24 17:28:05 +0200

    fix

--- macros/models/_sector/gas/gas_prices.sql
+++ macros/models/_sector/gas/gas_prices.sql
@@ -2,12 +2,12 @@
 
 SELECT '{{blockchain}}' AS blockchain
 , date_trunc('minute', block_time) AS time
-, approx_percentile(gas_price, 0.1) AS median_gas
-, approx_percentile(gas_price, 0.5) AS tenth_percentile_gas
-, approx_percentile(gas_price, 0.9) AS ninetieth_percentile_gas
-, AVG(gas_price) AS avg_gas
-, MIN(gas_price) AS min_gas
-, MAX(gas_price) AS max_gas
+, approx_percentile(gas_price/1e9, 0.1) AS median_gas
+, approx_percentile(gas_price/1e9, 0.5) AS tenth_percentile_gas
+, approx_percentile(gas_price/1e9, 0.9) AS ninetieth_percentile_gas
+, AVG(gas_price/1e9) AS avg_gas
+, MIN(gas_price/1e9) AS min_gas
+, MAX(gas_price/1e9) AS max_gas
 FROM {{transactions}}
 {% if is_incremental() %}
 WHERE {{ incremental_predicate('block_time') }}
