72498abfdf152a804508a5ef838f941c18d4c699
Hildebert Moulié <h.moulie@gmail.com>
2024-05-25 04:12:29 +0200

    add all chains

---
+++ models/_sector/cex/deposit_addresses/cex_deposit_addresses.sql
@@ -0,0 +1,55 @@
+{{ config(
+
+        schema = 'cex',
+        alias ='deposit_addresses',
+        partition_by = ['block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        unique_key = ['blockchain', 'address'],
+        post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "polygon", "fantom", "goerli", "base", "celo", "zksync", "linea", "scroll", "zkevm", "zora"]\',
+                                    "sector",
+                                    "cex",
+                                    \'["hildobby"]\') }}'
+)
+}}
+
+{% set cex_models = [
+    ref('cex_ethereum_deposit_addresses')
+    , ref('cex_bnb_deposit_addresses')
+    , ref('cex_avalanche_c_deposit_addresses')
+    , ref('cex_gnosis_deposit_addresses')
+    , ref('cex_optimism_deposit_addresses')
+    , ref('cex_arbitrum_deposit_addresses')
+    , ref('cex_polygon_deposit_addresses')
+    , ref('cex_fantom_deposit_addresses')
+    , ref('cex_goerli_deposit_addresses')
+    , ref('cex_base_deposit_addresses')
+    , ref('cex_zksync_deposit_addresses')
+    , ref('cex_zora_deposit_addresses')
+    , ref('cex_celo_deposit_addresses')
+    , ref('cex_scroll_deposit_addresses')
+    , ref('cex_zora_deposit_addresses')
+    , ref('cex_zkevm_deposit_addresses')
+    , ref('cex_linea_deposit_addresses')
+] %}
+
+SELECT *
+FROM (
+    {% for cex_model in cex_models %}
+    SELECT blockchain
+    , address
+    , cex_name
+    , creation_block_time
+    , creation_block_number
+    , funded_by_same_cex
+    FROM {{ cex_model }}
+    {% if is_incremental() %}
+    WHERE {{incremental_predicate('block_time')}}
+    {% endif %}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
