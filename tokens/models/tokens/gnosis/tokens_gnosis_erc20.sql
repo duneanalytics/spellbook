1848598485e39975e18488a388c7b1f955537cd4
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-05-27 08:50:29 +0100

    Update tokens_gnosis_erc20.sql

--- tokens/models/tokens/gnosis/tokens_gnosis_erc20.sql
+++ tokens/models/tokens/gnosis/tokens_gnosis_erc20.sql
@@ -7793,4 +7793,5 @@ FROM (VALUES
     , ( 0x7644fa5d0ea14fcf3e813fdf93ca9544f8567655, 'sBAL3', 18)
     , ( 0x4d18815d14fe5c3304e87b3fa18318baa5c23820, 'SAFE', 18)    
     , ( 0x270ba1f35d8b87510d24f693fccc0da02e6e4eeb, 'stataGnoUSDC', 6)       
+    , ( 0x1a8805194d0ef2f73045a00c70da399d9e74221c, 'GNOBBY', 18)    
 ) AS temp_table (contract_address, symbol, decimals)
