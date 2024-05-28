bc44ca0dd31300e6b85be7856ccc06a7bf1c9e3d
Hildebert Moulié <h.moulie@gmail.com>
2024-05-28 18:32:56 +0200

    fix attempt

--- macros/models/_sector/addresses_events/addresses_events_first_received.sql
+++ macros/models/_sector/addresses_events/addresses_events_first_received.sql
@@ -8,7 +8,7 @@ WITH identify_first AS (
     FROM {{token_transfers}} tt
     {% if is_incremental() %}
     LEFT JOIN {{this}} t ON t.address=tt.to
-        AND t.address = NULL
+        AND t.unique_key IS NULL
     WHERE {{ incremental_predicate('tt.block_time') }}
     {% endif %}
     GROUP BY to
