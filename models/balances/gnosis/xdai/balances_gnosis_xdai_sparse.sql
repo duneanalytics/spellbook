7f758e14d43a4deb07a77b4edfaf1a3a7971a3a6
hdser <hugo.serodio@gnosis.io>
2024-05-28 15:57:49 +0200

    fixed suicide

--- models/balances/gnosis/xdai/balances_gnosis_xdai_sparse.sql
+++ models/balances/gnosis/xdai/balances_gnosis_xdai_sparse.sql
@@ -37,60 +37,60 @@ sparse_balances as (
 
 suicide as (
     SELECT
-         block_time
-         ,block_number
+        block_time
+        ,block_number
         ,address
         ,refund_address
     FROM 
-    {{ ref('suicide_gnosis_xdai') }}
+        {{ ref('suicide_gnosis_xdai') }}
 ),
 
-suicide_balances as (
-    SELECT 
+suicide_contract AS (
+    SELECT
+        t1.blockchain, 
+        t1.block_time,
+        t1.block_number,
+        t1.block_month,
+        t2.address,
+        t2.refund_address,
+        t1.token_address, 
+        -t1.amount_raw AS amount_raw
+    FROM
+        sparse_balances t1
+    INNER JOIN
+        suicide t2
+        ON
+        t2.block_number = t1.block_number
+        AND
+        t2.address = t1.wallet_address
+),
+
+suicide_balances AS (
+    SELECT
         blockchain, 
         block_time,
         block_number,
         block_month,
-        wallet_address, 
+        address AS wallet_address,
         token_address, 
-        SUM(amount_raw) AS amount_raw
-    FROM (
-        SELECT
-            t1.blockchain, 
-            t1.block_time,
-            t1.block_number,
-            t1.block_month,
-            t2.address AS wallet_address, 
-            t1.token_address, 
-            -t1.amount_raw AS amount_raw
-        FROM
-            sparse_balances t1
-        INNER JOIN
-            suicide t2
-            ON
-            t2.block_number = t1.block_number
-            
-        
-        UNION ALL
+        amount_raw
+    FROM 
+        suicide_contract
 
-        SELECT
-            t1.blockchain, 
-            t1.block_time,
-            t1.block_number,
-            t1.block_month,
-            t2.refund_address AS wallet_address, 
-            t1.token_address, 
-            t1.amount_raw AS amount_raw
-        FROM
-            sparse_balances t1
-        INNER JOIN
-            suicide t2
-            ON
-            t2.block_number = t1.block_number
-    )
-    GROUP BY 
-        1,2,3,4,5,6
+    UNION ALL
+
+    SELECT
+        blockchain, 
+        block_time,
+        block_number,
+        block_month,
+        refund_address AS wallet_address,
+        token_address, 
+        -amount_raw
+    FROM 
+        suicide_contract
 )
+
 
 
 SELECT
