65bc1aa4f668b3fa359e6fefc1514c42c7eafc6b
Mansi <122999960+IrishLatte19@users.noreply.github.com>
2024-05-28 15:11:08 +0530

    Update prices_polygon_tokens.sql
    miMATIC added

--- models/prices/polygon/prices_polygon_tokens.sql
+++ models/prices/polygon/prices_polygon_tokens.sql
@@ -131,7 +131,8 @@ FROM
     ('mask-mask-network', 'polygon', 'MASK', 0x2b9e7ccdf0f4e5b24757c1e1a80e311e34cb10c7,18),
     ('ice-decentral-games-ice', 'polygon', 'ICE', 0xc6c855ad634dcdad23e64da71ba85b8c51e5ad7c,18),
     ('bonsai-bonsai-token', 'polygon', 'BONSAI', 0x3d2bd0e15829aa5c362a4144fdf4a1112fa29b5c, 18),
-    ('wifi-wifi', 'polygon', 'WIFI', 0xe238ecb42c424e877652ad82d8a939183a04c35f, 18)
+    ('wifi-wifi', 'polygon', 'WIFI', 0xe238ecb42c424e877652ad82d8a939183a04c35f, 18),
+    ('mimatic-mimatic', 'polygon', 'miMATIC', 0xa3Fa99A148fA48D14Ed51d610c367C61876997F1, 18)    
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
 where contract_address not in (
     0xef938b6da8576a896f6e0321ef80996f4890f9c4 -- DG, bad price feed
