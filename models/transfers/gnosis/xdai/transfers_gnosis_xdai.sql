54e34323e56ead5580695ee1b94c55fa4a230f2c
hdser <hugo.serodio@gnosis.io>
2024-05-28 09:25:58 +0200

    fix create/suicide logic

--- models/transfers/gnosis/xdai/transfers_gnosis_xdai.sql
+++ models/transfers/gnosis/xdai/transfers_gnosis_xdai.sql
@@ -13,6 +13,52 @@
 
 WITH 
 
+
+type_create AS (
+    SELECT 
+        tx_hash
+        ,address
+    FROM 
+       {{ source('gnosis', 'traces') }}
+    WHERE
+        type = 'create'
+        AND
+        success
+        {% if is_incremental() %}
+        AND block_time >= date_trunc('day', now() - interval '3' Day)
+        {% endif %}
+),
+
+type_suicide AS (
+    SELECT 
+        tx_hash
+        ,trace_address
+        ,refund_address
+    FROM 
+        {{ source('gnosis', 'traces') }}
+    WHERE
+        type = 'suicide'
+        AND
+        success
+        {% if is_incremental() %}
+        AND block_time >= date_trunc('day', now() - interval '3' Day)
+        {% endif %}
+),
+ 
+same_tx_create_suicide_events AS (
+    SELECT 
+        t1.tx_hash
+        ,t2.trace_address
+        ,t1.address
+        ,t2.refund_address
+    FROM 
+        type_create t1
+    INNER JOIN  
+        type_suicide t2
+        ON
+        t2.tx_hash = t1.tx_hash
+),
+
 xdai_transfers  as (
         SELECT 
             'receive' as transfer_type, 
@@ -52,6 +98,63 @@ xdai_transfers  as (
         {% endif %}
 ),
 
+xdai_transfers_create_suicide_events  as (
+    SELECT 
+        t1.tx_hash,
+        t2.address, 
+        t2.refund_address,
+        SUM(amount_raw) AS amount_raw
+    FROM
+        xdai_transfers t1
+    INNER JOIN
+        same_tx_create_suicide_events t2
+        ON
+        t2.tx_hash = t1.tx_hash
+        AND
+        t2.address = t1.wallet_address
+    GROUP BY 
+        1,2,3
+),
+
+xdai_transfers_from_same_tx_create_suicide_events  as (
+    SELECT 
+        'send' AS transfer_type, 
+        t1.tx_hash,
+        NULL AS trace_address, 
+        t1.block_time,
+        t2.address AS wallet_address, 
+        t1.token_address,
+        -t2.amount_raw AS amount_raw
+    FROM
+        xdai_transfers t1
+    RIGHT JOIN
+        xdai_transfers_create_suicide_events t2
+        ON
+        t2.tx_hash = t1.tx_hash
+    GROUP BY
+        1,2,3,4,5,6,7
+
+
+    UNION ALL
+
+    SELECT 
+        'receive' AS transfer_type, 
+        t1.tx_hash,
+        NULL AS trace_address, 
+        t1.block_time,
+        t2.refund_address AS wallet_address, 
+        t1.token_address,
+        t2.amount_raw AS amount_raw
+    FROM
+        xdai_transfers t1
+    RIGHT JOIN
+        xdai_transfers_create_suicide_events t2
+        ON
+        t2.tx_hash = t1.tx_hash
+    GROUP BY
+        1,2,3,4,5,6,7
+),
+
 gas_fee as (
     SELECT 
         'gas_fee' as transfer_type,
@@ -82,7 +185,7 @@ gas_fee_rewards as (
     INNER JOIN
         {{ source('gnosis', 'blocks') }} t2
         ON
-        t2.time = t1.block_time
+        t2.number = t1.block_number
     {% if is_incremental() %}
         WHERE t1.block_time >= date_trunc('day', now() - interval '3' Day)
     {% endif %}
@@ -150,6 +253,21 @@ SELECT
 FROM 
 xdai_transfers
 
+UNION ALL
+
+SELECT
+    'gnosis' as blockchain, 
+    transfer_type,
+    tx_hash, 
+    trace_address,
+    block_time,
+    CAST(date_trunc('month', block_time) as date) as block_month,
+    wallet_address, 
+    token_address, 
+    amount_raw
+FROM 
+xdai_transfers_from_same_tx_create_suicide_events
+
 UNION ALL 
 
 SELECT 
@@ -194,20 +312,3 @@ SELECT
     amount_raw
 FROM 
 block_reward
-
-/*
-UNION ALL 
-
-SELECT 
-    'gnosis' as blockchain, 
-    transfer_type,
-    tx_hash, 
-    trace_address,
-    block_time,
-    CAST(date_trunc('month', block_time) as date) as block_month,
-    wallet_address, 
-    token_address, 
-    amount_raw
-FROM 
-bridged
-*/
