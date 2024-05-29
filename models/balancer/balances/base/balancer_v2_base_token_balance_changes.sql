ee6fa30a0d29d330aa1272e633aa0ab7d810da64
viniabussafi <vinicius@balancerlabs.dev>
2024-05-29 11:05:50 +0100

    again

--- models/balancer/balances/base/balancer_v2_base_token_balance_changes.sql
+++ models/balancer/balances/base/balancer_v2_base_token_balance_changes.sql
@@ -2,14 +2,14 @@
 
 {{ config(
         schema = 'balancer_v2_base',
-        alias = 'bpt_token_balance_changes',
+        alias = 'token_balance_changes',
         materialized = 'table',
         file_format = 'delta'
     )
 }}
 
 {{ 
-    balancer_bpt_token_balance_changes_macro(
+    balancer_token_balance_changes_macro(
         blockchain = blockchain,
         version = '2'
     )
