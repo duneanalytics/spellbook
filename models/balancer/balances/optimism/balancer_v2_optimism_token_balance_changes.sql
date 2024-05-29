b4ac92fc281b79e26f0989f76ad94cd145cc89f3
viniabussafi <vinicius@balancerlabs.dev>
2024-05-29 10:54:16 +0100

    fix macro

--- models/balancer/balances/optimism/balancer_v2_optimism_token_balance_changes.sql
+++ models/balancer/balances/optimism/balancer_v2_optimism_token_balance_changes.sql
@@ -2,14 +2,14 @@
 
 {{ config(
         schema = 'balancer_v2_optimism',
-        alias = 'bpt_supply',
+        alias = 'bpt_token_balance_changes',
         materialized = 'table',
         file_format = 'delta'
     )
 }}
 
 {{ 
-    balancer_bpt_supply_macro(
+    balancer_bpt_token_balance_changes_macro(
         blockchain = blockchain,
         version = '2'
     )
