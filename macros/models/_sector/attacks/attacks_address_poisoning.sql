23472760242f8c943c5e03982194ee198580a397
Hildebert Moulié <h.moulie@gmail.com>
2024-05-31 05:24:07 +0200

    rewrite

--- macros/models/_sector/attacks/attacks_address_poisoning.sql
+++ macros/models/_sector/attacks/attacks_address_poisoning.sql
@@ -1,76 +1,52 @@
 {% macro attacks_address_poisoning(blockchain, transactions, token_transfers, cex_addresses) %}
 
-WITH only_relevant AS (
-    SELECT tr.tx_from
-    , tr.block_number
-    , tr.tx_index
-    FROM {{token_transfers}} tr
-    INNER JOIN {{transactions}} txs ON txs.block_number = tr.block_number
-        AND txs.index = tr.tx_index
-        AND txs.block_time > NOW() - interval '6' month
-        {% if is_incremental() %}
-        AND {{ incremental_predicate('txs.block_time') }}
-        {% endif %}
-    WHERE tr."from"=tr.tx_from
-    AND tr.block_time > NOW() - interval '6' month
+WITH transfer_recipients AS (
+    SELECT to AS address
+    , varbinary_substring(to, 1, 3) AS address_start
+    , varbinary_substring(to, 18, 3) AS address_end
+    , COUNT(*) AS address_occurence
+    FROM {{token_transfers}}
     {% if is_incremental() %}
-    AND {{ incremental_predicate('tr.block_time') }}
+    WHERE {{ incremental_predicate('block_time') }}
     {% endif %}
     GROUP BY 1, 2, 3
     )
 
-, transfered_to_similar_addresses AS (
-    SELECT attack.block_time
-    , attack.block_number
-    , normal.tx_from AS victim
-    , normal.amount_usd AS amount_usd
-    , normal.amount AS amount
-    , normal.amount_raw AS amount_raw
-    , attack.token_standard AS token_standard
-    , attack.contract_address AS token_address
-    , attack.symbol AS token_symbol
-    , attack.tx_to AS attacker
-    , normal.tx_to AS intended_recipient
-    -- Uncomment if to be made a purely crosschain spell to catch crosschain phishing:
-    --, attack.blockchain AS phished_blockchain
-    --, normal.blockchain AS original_blockchain
-    , attack.tx_hash
-    , attack.tx_index
-    , attack.evt_index
-    FROM {{token_transfers}} attack
-    INNER JOIN only_relevant ora ON ora.tx_from=attack.tx_from
-        AND ora.block_number=attack.block_number
-        AND ora.tx_index=attack.tx_index
-    INNER JOIN {{token_transfers}} normal ON normal.block_time BETWEEN attack.block_time - interval '1' day AND attack.block_time -- To tweak, ideally 3 days
-        AND attack.tx_from=normal.tx_from
-        AND attack.tx_to!=normal.tx_to
-        AND "LEFT"(CAST(attack.tx_to AS varchar), 4)="LEFT"(CAST(normal.tx_to AS varchar), 4)
-        AND "RIGHT"(CAST(attack.tx_to AS varchar), 4)="RIGHT"(CAST(normal.tx_to AS varchar), 4)
-        AND normal.amount_raw > 0
-    WHERE attack.amount_raw > 0
-    AND attack.block_time > NOW() - interval '6' month
-    {% if is_incremental() %}
-    AND {{ incremental_predicate('attack.block_time') }}
-    {% endif %}
+, matching_addresses AS (
+    SELECT CASE WHEN tr1.address_occurence < tr2.address_occurence THEN tr1.address ELSE tr2.address END AS address_attack
+    , CASE WHEN tr1.address_occurence < tr2.address_occurence THEN tr2.address ELSE tr1.address END AS address_normal
+    FROM transfer_recipients tr1
+    INNER JOIN transfer_recipients tr2 ON tr1.address_start=tr2.address_start
+        AND tr1.address_end=tr2.address_end
+        AND tr1.address!=tr2.address
+    GROUP BY 1, 2
     )
-    
+
 SELECT '{{blockchain}}' AS blockchain
-, ttsa.block_time
-, ttsa.block_number
-, ttsa.victim
-, ttsa.amount_usd
-, ttsa.amount
-, ttsa.amount_raw
-, ttsa.token_standard
-, ttsa.token_address
-, ttsa.token_symbol
-, ttsa.intended_recipient
-, ttsa.attacker
-, ttsa.tx_hash
-, ttsa.tx_index
-, ttsa.evt_index
-FROM transfered_to_similar_addresses ttsa
-LEFT JOIN {{cex_addresses}} ca ON ca.address=ttsa.victim -- Exclude CEXs
-    AND ca.address IS NULL
+, 'Address Poisoning' AS attack_type
+, 'Integrity' AS attack_type
+, ma.address_attack AS attacker
+, ma.address_normal AS intended_recipient
+, attack.amount_usd
+, attack.amount
+, attack.amount_raw
+, attack.contract_address
+, attack.token_standard
+, attack.symbol
+, attack.tx_hash
+FROM matching_addresses ma
+INNER JOIN {{token_transfers}} attack ON attack.to = ma.address_attack
+    {% if is_incremental() %}
+    WHERE {{ incremental_predicate('attack.block_time') }}
+    {% endif %}
+INNER JOIN {{token_transfers}} normal ON normal.to = ma.address_normal
+    AND attack.tx_from=normal.tx_from
+    {% if is_incremental() %}
+    WHERE {{ incremental_predicate('attack.block_time') }} - interval '14' day
+    {% endif %}
+    AND attack.block_number > normal.block_number
+INNER JOIN {{token_transfers}} attack_probe ON attack_probe.to = ma.address_attack
+    AND attack_probe.tx_from=attack.tx_from
+    AND attack_probe.block_number BETWEEN normal.block_number AND attack.block_number
 
 {% endmacro %}
