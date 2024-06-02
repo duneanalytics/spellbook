dbe2fa1b2e8e3853babf7fe900f9e95bef43d9ba
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-06-01 23:37:13 -0400

    limit CI test history

--- models/tokens/solana/tokens_solana_transfers.sql
+++ models/tokens/solana/tokens_solana_transfers.sql
@@ -172,4 +172,4 @@ WHERE 1=1
 {% if is_incremental() %}
 AND {{incremental_predicate('call_block_time')}}
 {% endif %}
--- AND call_block_time > now() - interval '600' day
+AND call_block_time > now() - interval '14' day --for faster CI testing
