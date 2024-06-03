78c0d07e2896e8646830c292108bc5fd6d9106f9
Alan Ghobadi <aalan3@gmail.com>
2024-06-03 15:51:12 +0200

    Move op folder to daily spellbook

--- daily_spellbook/models/op/optimism/op_optimism_airdrop_1_claims.sql
+++ daily_spellbook/models/op/optimism/op_optimism_airdrop_1_claims.sql
@@ -1,6 +1,6 @@
 {{
     config(
-        
+
         schema = 'op_optimism',
         alias = 'airdrop_1_claims',
         materialized = 'incremental',
@@ -41,7 +41,7 @@ SELECT 'optimism' AS blockchain
 , 'OP' AS token_symbol
 , t.evt_index
 FROM {{ source('op_optimism', 'MerkleDistributor_evt_Claimed') }} t
-LEFT JOIN {{ ref('prices_usd_forward_fill') }} pu ON pu.blockchain = 'optimism'
+LEFT JOIN {{ ('prices_usd_forward_fill') }} pu ON pu.blockchain = 'optimism'
     AND pu.contract_address= {{op_token_address}}
     AND pu.minute=date_trunc('minute', t.evt_block_time)
     {% if is_incremental() %}
