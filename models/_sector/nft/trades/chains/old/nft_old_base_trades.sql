190f83135807c58f9799db8811b6534cfbb8163d
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-06-03 21:59:53 +0200

    chore: review solana trades setup (#5979)
    * chore: review solana trades setup
    
    * chore: review solana trades setup
    
    * exclude
    
    * syntax
    
    * currency contract
    
    * add types
    
    * fix ref
    
    * fix more types
    
    * fix
    
    * types
    
    * include into nft base trades
    
    * test
    
    * re-enable magiceden model
    
    * revert changes
    
    * remove double refs
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/_sector/nft/trades/chains/old/nft_old_base_trades.sql
+++ models/_sector/nft/trades/chains/old/nft_old_base_trades.sql
@@ -8,11 +8,9 @@
 
 -- while we refactor more marketplace models, they should be removed here and added to the chain specific base_trades unions.
 {% set nft_models = [
-ref('magiceden_solana_events')
-,ref('nftearth_optimism_events')
+ref('nftearth_optimism_events')
 ,ref('mooar_polygon_events')
 ,ref('oneplanet_polygon_events')
-,ref('opensea_solana_events')
 ,ref('quix_seaport_optimism_events')
 ] %}
 
