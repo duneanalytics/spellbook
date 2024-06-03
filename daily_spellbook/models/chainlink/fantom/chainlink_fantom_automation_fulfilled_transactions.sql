c54ad7a6957e0a3107c5aa5b25f626b465a97584
Alan Ghobadi <aalan3@gmail.com>
2024-06-03 12:15:00 +0200

    Add incremental_predicate strategy to more models

--- daily_spellbook/models/chainlink/fantom/chainlink_fantom_automation_fulfilled_transactions.sql
+++ daily_spellbook/models/chainlink/fantom/chainlink_fantom_automation_fulfilled_transactions.sql
@@ -6,7 +6,8 @@
     materialized='incremental',
     file_format='delta',
     incremental_strategy='merge',
-    unique_key=['tx_hash', 'tx_index', 'node_address']
+    unique_key=['tx_hash', 'tx_index', 'node_address'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
   )
 }}
 
