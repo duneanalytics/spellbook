e6818fee14603f005d5111963a11d3735a8119d6
tomfutago <35136350+tomfutago@users.noreply.github.com>
2024-06-03 16:08:16 +0100

    EAS schemas - bug fix (#6042)
    * init
    
    * use uid_column_name param

--- models/_sector/attestation/eas/optimism/eas_optimism_schemas.sql
+++ models/_sector/attestation/eas/optimism/eas_optimism_schemas.sql
@@ -10,40 +10,11 @@
   )
 }}
 
-with eas as (
-  {{
-    eas_schemas(
-      blockchain = 'optimism',
-      project = 'eas',
-      version = '1',
-      decoded_project_name = 'attestationstation_v1'
-    )
-  }}
-)
-, dedupe as (
-  select distinct
-    schema_uid
-  from
-  (
-    select
-      schema_uid
-      , count(1) as count
-    from
-      eas
-    group by
-      schema_uid
-    having
-      count(1) > 1
-    )
-)
-select
-  *
-from
-  eas
-where
-  schema_uid not in (
-    select
-      schema_uid
-    from
-      dedupe
+{{
+  eas_schemas(
+    blockchain = 'optimism',
+    project = 'eas',
+    version = '1',
+    decoded_project_name = 'attestationstation_v1'
   )
+}}
