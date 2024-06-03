ce6635f23cc3450c6e80d6609475e527517f6b9c
max-morrow <amidmoro@gmail.com>
2024-06-03 18:26:58 +0300

    hot fix underflow

--- models/oneinch/oneinch_project_orders.sql
+++ models/oneinch/oneinch_project_orders.sql
@@ -33,9 +33,9 @@ orders as (
                 , call_gas_used
                 , maker
                 , maker_asset
-                , coalesce(making_amount, if(order_start = uint256 '0', maker_max_amount, maker_max_amount - cast(block_unixtime - order_start as double) / (order_end - order_start) * (maker_max_amount - maker_min_amount))) as making_amount
+                , coalesce(making_amount, if(order_start = uint256 '0', maker_max_amount, maker_max_amount - cast(block_unixtime - order_start as double) / (order_end - order_start) * (cast(maker_max_amount as double) - cast(maker_min_amount as double)))) as making_amount
                 , taker_asset
-                , coalesce(taking_amount, if(order_start = uint256 '0', taker_max_amount, taker_max_amount - cast(block_unixtime - order_start as double) / (order_end - order_start) * (taker_max_amount - taker_min_amount))) as taking_amount
+                , coalesce(taking_amount, if(order_start = uint256 '0', taker_max_amount, taker_max_amount - cast(block_unixtime - order_start as double) / (order_end - order_start) * (cast(taker_max_amount as double) - cast(taker_min_amount as double)))) as taking_amount
                 , order_hash
                 , flags
             from {{ ref('oneinch_' + blockchain + '_project_orders') }}
