7910dd9d8cb20b9c5eff754c64483444e143a471
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-06-04 00:19:10 +0100

    Organize Balancer Spells into folders (#6005)
    * test folders
    
    * fix typo
    
    * again
    
    * organize balancer spells into folders
    
    * fix indentation
    
    * fix schema references
    
    * again
    
    * again
    
    * remove legacy model for zkevm
    
    * Remove balancer from dbt_project.yml
    
    * rerun a few schemas
    
    * again
    
    * fix macro
    
    * again
    
    * force run schemas
    
    * rerun
    
    * fix indentation issues and remove legacy trades models
    
    * update post hooks
    
    * fix typo
    
    * again
    
    * again
    
    * again
    
    * move all folders into _project
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

---
+++ models/_project/balancer/balances/balancer_token_balance_changes.sql
@@ -0,0 +1,46 @@
+{{ config(
+    schema = 'balancer',
+    alias = 'token_balance_changes',
+    post_hook='{{ expose_spells(blockchains = \'["arbitrum", "avalanche_c", "base", "ethereum", "gnosis", "optimism", "polygon", "zkevm"]\',
+                                spell_type = "project",
+                                spell_name = "balancer",
+                                contributors = \'["viniabussafi"]\') }}'
+    )
+}}
+
+{% set balancer_models = [
+    ref('balancer_v2_arbitrum_token_balance_changes'),
+    ref('balancer_v2_avalanche_c_token_balance_changes'),
+    ref('balancer_v2_base_token_balance_changes'),
+    ref('balancer_v2_ethereum_token_balance_changes'),
+    ref('balancer_v2_gnosis_token_balance_changes'),
+    ref('balancer_v2_optimism_token_balance_changes'),
+    ref('balancer_v2_polygon_token_balance_changes'),
+    ref('balancer_v2_zkevm_token_balance_changes')
+] %}
+
+SELECT *
+FROM (
+    {% for model in balancer_models %}
+    SELECT
+       block_date
+      , evt_block_time
+      , evt_block_number
+      , blockchain
+      , evt_tx_hash
+      , evt_index
+      , pool_id
+      , pool_address
+      , pool_symbol
+      , pool_type 
+      , version 
+      , token_address
+      , token_symbol
+      , delta_amount_raw
+      , delta_amount
+    FROM {{ model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
