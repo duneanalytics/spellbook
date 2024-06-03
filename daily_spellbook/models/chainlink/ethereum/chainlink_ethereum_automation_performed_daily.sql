f03eb003ddb4130b073ebec5c5a343da3055885c
Alan Ghobadi <aalan3@gmail.com>
2024-06-03 12:18:02 +0200

    More models

--- daily_spellbook/models/chainlink/ethereum/chainlink_ethereum_automation_performed_daily.sql
+++ daily_spellbook/models/chainlink/ethereum/chainlink_ethereum_automation_performed_daily.sql
@@ -7,6 +7,7 @@
     file_format='delta',
     incremental_strategy='merge',
     unique_key=['date_start', 'keeper_address']
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.date_month')]
   )
 }}
 
