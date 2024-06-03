c1b5417a186c4701e452fcaeea1ebebaea625653
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-06-04 00:19:10 +0100

    Organize Balancer Spells into folders (#6005)
    * test folders
    
    * fix typo
    
    * again
    
    * organize balancer spells into folders
    
    * fix indentation
    
    * fix schema references
    
    * again
    
    * again
    
    * remove legacy model for zkevm
    
    * Remove balancer from dbt_project.yml
    
    * rerun a few schemas
    
    * again
    
    * fix macro
    
    * again
    
    * force run schemas
    
    * rerun
    
    * fix indentation issues and remove legacy trades models
    
    * update post hooks
    
    * fix typo
    
    * again
    
    * again
    
    * again
    
    * move all folders into _project
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>

---
+++ models/_project/balancer/pools/balancer_pools_fees.sql
@@ -0,0 +1,41 @@
+{{ config(
+    schema = 'balancer',
+    alias = 'pools_fees',
+    post_hook='{{ expose_spells(blockchains = \'["arbitrum", "avalanche_c", "base", "ethereum", "gnosis", "optimism", "polygon", "zkevm"]\',
+                                spell_type = "project",
+                                spell_name = "balancer",
+                                contributors = \'["thetroyharris", "viniabussafi"]\') }}'
+    )
+}}
+
+{% set balancer_models = [
+    ref('balancer_v2_arbitrum_pools_fees'),
+    ref('balancer_v2_avalanche_c_pools_fees'),
+    ref('balancer_v2_base_pools_fees'),
+    ref('balancer_v2_ethereum_pools_fees'),
+    ref('balancer_v2_gnosis_pools_fees'),
+    ref('balancer_v2_optimism_pools_fees'),
+    ref('balancer_v2_polygon_pools_fees'),
+    ref('balancer_v2_zkevm_pools_fees')
+] %}
+
+
+SELECT *
+FROM (
+    {% for model in balancer_models %}
+    SELECT
+      blockchain
+      , version
+      , contract_address
+      , tx_hash
+      , index
+      , tx_index
+      , block_time
+      , block_number
+      , swap_fee_percentage
+    FROM {{ model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
