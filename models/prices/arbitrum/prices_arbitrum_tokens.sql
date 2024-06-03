7874b8815b8f420c396afb1cb84df61e9db8fda2
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-06-03 16:06:54 +0100

    Add missing tokens on tokens.erc20 and prices.usd (#6053)
    * Update tokens_gnosis_erc20.sql
    
    * Update prices_arbitrum_tokens.sql

--- models/prices/arbitrum/prices_arbitrum_tokens.sql
+++ models/prices/arbitrum/prices_arbitrum_tokens.sql
@@ -193,5 +193,6 @@ FROM
     ('susde-ethena-staked-usde','arbitrum','SUSDE',0x211cc4dd073734da055fbf44a2b4667d5e5fe5d2, 18),
     ('yak-yield-yak','arbitrum','YAK',0x7f4db37d7beb31f445307782bc3da0f18df13696,18),
     ('ethx-stader-ethx','arbitrum','ETHX',0xed65c5085a18fa160af0313e60dcc7905e944dc7, 18),
-    ('gyd-gyro-dollar','arbitrum','GYD',0xca5d8f8a8d49439357d3cf46ca2e720702f132b8, 18)
+    ('gyd-gyro-dollar','arbitrum','GYD',0xca5d8f8a8d49439357d3cf46ca2e720702f132b8, 18),
+    ('a51-a51-finance','arbitrum','A51',0xb3f13b0c61d65d67d7d6215d70c89533ee567a91, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
