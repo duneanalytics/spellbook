60d1c55cdeb1829575b9203a193d8637629ef830
Huang Geyang <Sukebeta@outlook.com>
2024-06-03 23:10:09 +0800

    Add prod_exclude tags to uniswap_*_trades (#6013)
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/uniswap/optimism/uniswap_v3_optimism_trades.sql
+++ models/uniswap/optimism/uniswap_v3_optimism_trades.sql
@@ -1,6 +1,7 @@
 {{ config(
     schema = 'uniswap_v3_optimism',
     alias = 'trades',
+    tags = ['prod_exclude'],
     partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
