b615dd740359aa2c856afa0129f0d029d683094a
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-06-04 15:35:40 -0400

    Revert "Revert "Revert "Organize Balancer Spells into folders (#6005)" (#6074)" (#6075)" (#6077)
    This reverts commit 8e1ef8576c68892f780e742392538bd1feaa7b42.

---
+++ models/balancer/base/balancer_v2_base_liquidity.sql
@@ -0,0 +1,22 @@
+
+{% set blockchain = 'base' %}
+
+{{
+    config(
+        schema = 'balancer_v2_base',
+        alias = 'liquidity',
+        materialized = 'table',
+        file_format = 'delta',
+        post_hook="{{ expose_spells('[\"" + blockchain + '"]' + '\',
+                        "project",
+                        "balancer_v2",
+                        \'["stefenon", "viniabussafi"]\') }}'
+    )
+}}
+
+{{ 
+    balancer_liquidity_macro(
+        blockchain = blockchain,
+        version = '2'
+    )
+}}
