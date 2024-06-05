37eba7eeb6d2c49687960fa03ef9f3155c5e397d
Alan Ghobadi <aalan3@gmail.com>
2024-06-05 12:06:49 +0200

    Use incremental predicate

--- models/tokens/solana/tokens_solana_fungible.sql
+++ models/tokens/solana/tokens_solana_fungible.sql
@@ -1,7 +1,7 @@
 {{ config
 (
   alias = 'fungible',
-  
+
   post_hook='{{ expose_spells(\'["solana"]\',
                                   "sector",
                                   "tokens",
@@ -10,7 +10,7 @@
 }}
 
 
-with 
+with
     tokens as (
         SELECT
         bytearray_to_bigint(bytearray_reverse(bytearray_substring(call_data, 2, 1))) as decimals
@@ -22,20 +22,20 @@ with
         , row_number() over (partition by account_mint order by call_block_time desc) as latest
         FROM (
             SELECT call_data, account_mint, call_tx_id, call_block_time, 'spl_token' as token_version FROM {{ source('spl_token_solana', 'spl_token_call_initializeMint') }}
-            UNION ALL 
+            UNION ALL
             SELECT call_data, account_mint, call_tx_id, call_block_time, 'spl_token' as token_version FROM {{ source('spl_token_solana', 'spl_token_call_initializeMint2') }}
             UNION ALL
             SELECT call_data, account_mint, call_tx_id, call_block_time, 'token2022' as token_version FROM {{ source('spl_token_2022_solana', 'spl_token_2022_call_initializeMint') }}
-            UNION ALL 
-            SELECT call_data, account_mint, call_tx_id, call_block_time, 'token2022' as token_version FROM {{ source('spl_token_2022_solana', 'spl_token_2022_call_initializeMint2') }}      
+            UNION ALL
+            SELECT call_data, account_mint, call_tx_id, call_block_time, 'token2022' as token_version FROM {{ source('spl_token_2022_solana', 'spl_token_2022_call_initializeMint2') }}
         )
         {% if is_incremental() %}
-        where call_block_time >= date_trunc('day', now() - interval '7' day)
+        where {{ incremental_predicate('call_block_time') }}
         {% endif %}
     )
-    
+
     , metadata as (
-        SELECT 
+        SELECT
             meta.call_tx_id
             , meta.call_block_slot
             , meta.call_block_time
@@ -47,7 +47,7 @@ with
             , metadata_program
             , row_number() over (partition by meta.account_mint order by meta.call_block_time desc) as latest
         FROM (
-            SELECT 
+            SELECT
                 call_tx_id
                 , call_outer_instruction_index
                 , call_inner_instruction_index
@@ -58,8 +58,8 @@ with
                 , account_mint
                 , call_executing_account as metadata_program
             FROM  {{ source('mpl_token_metadata_solana', 'mpl_token_metadata_call_CreateMetadataAccount') }}
-            UNION ALL 
-            SELECT 
+            UNION ALL
+            SELECT
                 call_tx_id
                 , call_outer_instruction_index
                 , call_inner_instruction_index
@@ -70,26 +70,26 @@ with
                 , account_mint
                 , call_executing_account as metadata_program
             FROM {{ source('mpl_token_metadata_solana', 'mpl_token_metadata_call_CreateMetadataAccountV2') }}
-            UNION ALL 
-            SELECT  
+            UNION ALL
+            SELECT
                 call_tx_id
                 , call_outer_instruction_index
-                , call_inner_instruction_index                    
+                , call_inner_instruction_index
                 , call_block_slot
                 , call_block_time
                 , json_query(createMetadataAccountArgsV3, 'lax $.CreateMetadataAccountArgsV3.data.DataV2') as args
                 , account_metadata
                 , account_mint
                 , call_executing_account as metadata_program
-            FROM {{ source('mpl_token_metadata_solana', 'mpl_token_metadata_call_CreateMetadataAccountV3') }} 
-        ) meta 
+            FROM {{ source('mpl_token_metadata_solana', 'mpl_token_metadata_call_CreateMetadataAccountV3') }}
+        ) meta
         LEFT JOIN (
-            SELECT account_mintAuthority, account_edition, account_metadata FROM {{ source('mpl_token_metadata_solana', 'mpl_token_metadata_call_CreateMasterEdition') }} 
+            SELECT account_mintAuthority, account_edition, account_metadata FROM {{ source('mpl_token_metadata_solana', 'mpl_token_metadata_call_CreateMasterEdition') }}
             UNION ALL
             SELECT account_mintAuthority, account_edition, account_metadata FROM {{ source('mpl_token_metadata_solana', 'mpl_token_metadata_call_CreateMasterEditionV3') }}
             ) master ON master.account_metadata = meta.account_metadata
         {% if is_incremental() %}
-        WHERE meta.call_block_time >= date_trunc('day', now() - interval '7' day)
+        WHERE {{ incremental_predicate('meta.call_block_time') }}
         {% endif %}
     )
 
@@ -102,7 +102,7 @@ with
                 )) as symbol
             , from_utf8(bytearray_substring(data,1+8+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8,4))) + 4 --end of name and end of length of symbol
                     + bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8,4))),4))) + 4 --start from end of symbol and end of length of uri
-                , bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8,4))) + 4 
+                , bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8,4))) + 4
                     + bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+8,4))),4))),4))) --get length of uri from end of symbol
                 )) as uri
             , tx_id as metadata_tx
@@ -115,7 +115,7 @@ with
         AND bytearray_substring(data,1,1) = 0xd2 --deal with updateField later 0xdd
         AND tx_success
         {% if is_incremental() %}
-        AND block_time >= date_trunc('day', now() - interval '7' day)
+        AND {{ incremental_predicate('block_time') }}
         {% endif %}
     )
 
@@ -128,7 +128,7 @@ with
                 )) as symbol
             , from_utf8(bytearray_substring(data,1+1+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1,4))) + 4 --end of name and end of length of symbol
                     + bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1,4))),4))) + 4 --start from end of symbol and end of length of uri
-                , bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1,4))) + 4 
+                , bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1,4))) + 4
                     + bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1+4+bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+1,4))),4))),4))) --get length of uri from end of symbol
                 )) as uri
             , tx_id as metadata_tx
@@ -141,15 +141,15 @@ with
         AND bytearray_substring(data,1,1) = 0x21
         AND tx_success
         {% if is_incremental() %}
-        AND block_time >= date_trunc('day', now() - interval '7' day)
+        AND {{ incremental_predicate('block_time') }}
         {% endif %}
     )
 
 SELECT
     tk.account_mint as token_mint_address
     , tk.decimals
-    , coalesce(m22.name,mo.name,trim(json_value(args, 'strict $.name'))) as name 
-    , coalesce(m22.symbol,mo.symbol,trim(json_value(args, 'strict $.symbol'))) as symbol 
+    , coalesce(m22.name,mo.name,trim(json_value(args, 'strict $.name'))) as name
+    , coalesce(m22.symbol,mo.symbol,trim(json_value(args, 'strict $.symbol'))) as symbol
     , coalesce(m22.uri,mo.uri,trim(json_value(args, 'strict $.uri'))) as token_uri
     , tk.call_block_time as created_at
     , coalesce(m22.metadata_program,mo.metadata_program,m.metadata_program) as metadata_program
@@ -165,7 +165,7 @@ AND tk.latest = 1
 UNION ALL
 
 --token2022 wrapped sol https://solscan.io/tx/2L1o7sDMCMJ6PYqfNrnY6ozJC1DEx61pRYiLdfCCggxw81naQXsmHKDLn6EhJXmDmDSQ2eCKjUMjZAQuUsyNnYUv
-SELECT 
+SELECT
   trim(token_mint_address) as token_mint_address
   , decimals
   , trim(name) as name
@@ -175,7 +175,7 @@ SELECT
   , metadata_program
   , token_version
   , init_tx
-FROM 
+FROM
 (
   VALUES
 (
@@ -194,7 +194,7 @@ FROM
 UNION ALL
 
 --old wrapped sol is special and doesn't have a init tx (that I can find)
-SELECT 
+SELECT
   trim(token_mint_address) as token_mint_address
   , decimals
   , trim(name) as name
@@ -204,7 +204,7 @@ SELECT
   , metadata_program
   , token_version
   , init_tx
-FROM 
+FROM
 (
   VALUES
 (
