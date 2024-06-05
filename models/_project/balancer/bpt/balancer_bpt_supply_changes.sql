f2e81ec467cf752329f8c222913c7c35fdd6fd82
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-06-05 09:31:52 -0400

    Revert "Revert "Revert "Revert "Organize Balancer Spells into folders (#6005)" (#6074)" (#6075)" (#6077)" (#6078)
    This reverts commit b615dd740359aa2c856afa0129f0d029d683094a.

---
+++ models/_project/balancer/bpt/balancer_bpt_supply_changes.sql
@@ -0,0 +1,44 @@
+{{ config(
+    schema = 'balancer',
+    alias = 'bpt_supply_changes',
+    post_hook='{{ expose_spells(blockchains = \'["arbitrum", "avalanche_c", "base", "ethereum", "gnosis", "optimism", "polygon", "zkevm"]\',
+                                spell_type = "project",
+                                spell_name = "balancer",
+                                contributors = \'["viniabussafi"]\') }}'
+    )
+}}
+
+{% set balancer_models = [
+    ref('balancer_v2_arbitrum_bpt_supply_changes'),
+    ref('balancer_v2_avalanche_c_bpt_supply_changes'),
+    ref('balancer_v2_base_bpt_supply_changes'),
+    ref('balancer_v2_ethereum_bpt_supply_changes'),
+    ref('balancer_v2_gnosis_bpt_supply_changes'),
+    ref('balancer_v2_optimism_bpt_supply_changes'),
+    ref('balancer_v2_polygon_bpt_supply_changes'),
+    ref('balancer_v2_zkevm_bpt_supply_changes')
+] %}
+
+SELECT *
+FROM (
+    {% for model in balancer_models %}
+    SELECT
+        block_date
+      , evt_block_time
+      , evt_block_number
+      , blockchain
+      , evt_tx_hash
+      , evt_index
+      , pool_type
+      , pool_symbol   
+      , version 
+      , label
+      , token_address
+      , delta_amount_raw
+      , delta_amount
+    FROM {{ model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
