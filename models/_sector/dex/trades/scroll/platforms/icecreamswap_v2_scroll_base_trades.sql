eed2af64e3a848154efff5f795b86e4ba1ad98d6
intensodefi <intensodefi@protonmail.com>
2024-06-08 21:51:52 +0700

    fix model

--- models/_sector/dex/trades/scroll/platforms/icecreamswap_v2_scroll_base_trades.sql
+++ models/_sector/dex/trades/scroll/platforms/icecreamswap_v2_scroll_base_trades.sql
@@ -14,7 +14,7 @@
           blockchain = 'scroll'
         , project = 'icecreamswap'
         , version = '2'
-        , Pair_evt_Swap = source('icecreamswap_v2_scroll', 'pool_evt_Swap')
-        , Factory_evt_PairCreated = source('icecreamswap_v2_scroll', 'IceCreamSwapFactory_evt_PairCreated')
+        , Pair_evt_Swap = source('icecreamswap_v2_scroll', 'UniswapV2Factory_evt_Swap')
+        , Factory_evt_PairCreated = source('icecreamswap_v2_scroll', 'UniswapV2Factory_evt_PairCreated')
     )
 }}
