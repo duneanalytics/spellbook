d5f29ca9fab9d0a76997aef953ee4548b8d14fcb
yy-analytics <yieldyak.analytics@gmail.com>
2024-06-09 15:41:39 +0530

    Spacing changes to re-trigger dbt tests in PR

--- models/yield_yak/arbitrum/yield_yak_arbitrum_trades.sql
+++ models/yield_yak/arbitrum/yield_yak_arbitrum_trades.sql
@@ -1,12 +1,13 @@
-{{ config(
-    schema = 'yield_yak_arbitrum',
-    alias = 'trades',
-    partition_by = ['block_month'],
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address'],
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+{{
+    config(
+        schema = 'yield_yak_arbitrum',
+        alias = 'trades',
+        partition_by = ['block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index', 'trace_address'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
     )
 }}
 
