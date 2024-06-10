284c9cbdaff849175c6724fd6d24557bce6d9e60
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2024-06-10 10:45:00 -0700

    Add TAIKO ERC-20 token to Ethereum mainnet (#6109)
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- tokens/models/tokens/ethereum/tokens_ethereum_erc20.sql
+++ tokens/models/tokens/ethereum/tokens_ethereum_erc20.sql
@@ -4560,6 +4560,7 @@ FROM (VALUES
     , ( 0xfc87753df5ef5c368b5fba8d4c5043b77e8c5b39, 'aETH', 18)
     , ( 0xf1617882a71467534d14eee865922de1395c9e89, 'saETH', 18)
     , ( 0x9deb0fc809955b79c85e82918e8586d3b7d2695a, 'GOLD', 18)
-    , ( 0x75e88b8c2d34a52a6d36deada664d7dc9116e4ef, 'ZRS', 18)    
+    , ( 0x75e88b8c2d34a52a6d36deada664d7dc9116e4ef, 'ZRS', 18)
+    , ( 0x10dea67478c5f8c5e2d90e5e9b26dbe60c54d800, 'TAIKO', 18)
     , ( 0x80ac24aA929eaF5013f6436cdA2a7ba190f5Cc0b, 'syrupUSDC', 6)    
 ) AS temp_table (contract_address, symbol, decimals)
