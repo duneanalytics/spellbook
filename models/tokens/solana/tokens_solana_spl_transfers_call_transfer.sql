d06e7c7ef1ac4c12f47fad2d14846f30f4fb54c3
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-06-10 07:57:56 -0400

    add back changes for solana token 2022 (#6107)
    * add back changes for solana
    
    * moving
    
    * removing
    
    * removing
    
    * sources removed
    
    * Fix name
    
    * Update tokens_solana_transfers.sql
    
    * Try splitting up
    
    * Fxi typo
    
    * fix union
    
    * typeing
    
    * Try another split
    
    * Fix typo
    
    * Use correct path
    
    ---------
    
    Co-authored-by: Andrew <47720952+andrewhong5297@users.noreply.github.com>
    Co-authored-by: Alan Ghobadi <aalan3@gmail.com>

---
+++ models/tokens/solana/tokens_solana_spl_transfers_call_transfer.sql
@@ -0,0 +1,33 @@
+ {{
+  config(
+        schema = 'tokens_solana',
+        alias = 'spl_transfers_call_transfer',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        partition_by = ['block_date'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        unique_key = ['tx_id','outer_instruction_index','inner_instruction_index', 'block_slot']
+  )
+}}
+
+SELECT
+      call_block_time as block_time
+    , cast (date_trunc('day', call_block_time) as date) as block_date
+    , call_block_slot as block_slot
+    , 'transfer' as action
+    , bytearray_to_uint256(bytearray_reverse(bytearray_substring(call_data,1+1,8))) as amount
+    , cast(null as double) as fee
+    , account_source as from_token_account
+    , account_destination as to_token_account
+    , 'spl_token' as token_version
+    , call_tx_signer as tx_signer
+    , call_tx_id as tx_id
+    , call_outer_instruction_index as outer_instruction_index
+    , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
+    , call_outer_executing_account as outer_executing_account
+FROM {{ source('spl_token_solana','spl_token_call_transfer') }}
+WHERE 1=1
+{% if is_incremental() %}
+AND {{incremental_predicate('call_block_time')}}
+{% endif %}
