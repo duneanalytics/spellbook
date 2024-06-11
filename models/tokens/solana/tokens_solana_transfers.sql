e50f7756e8c7ca5e933d61f06f85897695cad9b0
Alan Ghobadi <aalan3@gmail.com>
2024-06-11 10:00:05 +0200

    Revert "Revert solana transfers (#6130)" (#6131)
    This reverts commit 26dd8a1f3c4aab89598c88775c382d8424213cd5.

--- models/tokens/solana/tokens_solana_transfers.sql
+++ models/tokens/solana/tokens_solana_transfers.sql
@@ -14,56 +14,69 @@
                                     \'["ilemi"]\') }}')
 }}
 
+
+WITH
+base as (
+    SELECT
+    block_time,
+    , call_block_slot as block_slot
+    , action
+    , amount
+    , fee
+    , from_token_account
+    , to_token_account
+    , token_version
+    , tx_signer
+    , tx_id
+    , outer_instruction_index
+    , inner_instruction_index
+    , outer_executing_account
+FROM {{ ref('tokens_solana_spl_transfers') }}
+{% if is_incremental() %}
+WHERE {{incremental_predicate('block_time')}}
+{% endif %}
+UNION ALL
+    SELECT
+    block_time,
+    , call_block_slot as block_slot
+    , action
+    , amount
+    , fee
+    , from_token_account
+    , to_token_account
+    , token_version
+    , tx_signer
+    , tx_id
+    , outer_instruction_index
+    , inner_instruction_index
+    , outer_executing_account
+FROM {{ ref('tokens_solana_token22_spl_transfers') }}
+{% if is_incremental() %}
+WHERE {{incremental_predicate('block_time')}}
+{% endif %}
+)
+
 SELECT
     call_block_time as block_time
     , cast (date_trunc('day', call_block_time) as date) as block_date
     , call_block_slot as block_slot
     , action
     , amount
+    , fee
     , COALESCE(tk_s.token_mint_address, tk_d.token_mint_address) as token_mint_address
     , tk_s.token_balance_owner as from_owner
     , tk_d.token_balance_owner as to_owner
-    , account_source as from_token_account
-    , account_destination as to_token_account
-    , call_tx_signer as tx_signer
-    , call_tx_id as tx_id
-    , call_outer_instruction_index as outer_instruction_index
-    , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
-    , call_outer_executing_account as outer_executing_account
-FROM (  
-      SELECT account_source, account_destination, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'transfer' as action, call_outer_instruction_index, call_inner_instruction_index
-      FROM {{ source('spl_token_solana','spl_token_call_transfer') }}
-
-      UNION ALL
-
-      SELECT account_source, account_destination, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'transfer' as action, call_outer_instruction_index, call_inner_instruction_index
-      FROM {{ source('spl_token_solana','spl_token_call_transferChecked') }}
-
-      UNION ALL
-
-      SELECT null, account_account as account_destination, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'mint' as action, call_outer_instruction_index, call_inner_instruction_index
-      FROM {{ source('spl_token_solana','spl_token_call_mintTo') }}
-
-      UNION ALL
-
-      SELECT null, account_account as account_destination, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'mint' as action, call_outer_instruction_index, call_inner_instruction_index
-      FROM {{ source('spl_token_solana','spl_token_call_mintToChecked') }}
-
-      UNION ALL
-
-      SELECT account_account as account_source, null, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'burn' as action, call_outer_instruction_index, call_inner_instruction_index
-      FROM {{ source('spl_token_solana','spl_token_call_burn') }}
-
-      UNION ALL
-
-      SELECT account_account as account_source, null, amount, call_tx_id, call_block_time, call_block_slot, call_outer_executing_account, call_tx_signer, 'burn' as action, call_outer_instruction_index, call_inner_instruction_index
-      FROM {{ source('spl_token_solana','spl_token_call_burnChecked') }}
-) tr
+    , from_token_account
+    , to_token_account
+    , token_version
+    , tx_signer
+    , tx_id
+    , outer_instruction_index
+    , inner_instruction_index
+    , outer_executing_account
+FROM base tr
 --get token and accounts
-LEFT JOIN {{ ref('solana_utils_token_accounts') }} tk_s ON tk_s.address = tr.account_source 
-LEFT JOIN {{ ref('solana_utils_token_accounts') }} tk_d ON tk_d.address = tr.account_destination
+INNER JOIN {{ ref('solana_utils_token_accounts') }} tk_s ON tk_s.address = tr.account_source
+INNER JOIN {{ ref('solana_utils_token_accounts') }} tk_d ON tk_d.address = tr.account_destination
 WHERE 1=1
-{% if is_incremental() %}
-AND {{incremental_predicate('call_block_time')}}
-{% endif %}
--- AND call_block_time > now() - interval '600' day
+-- AND call_block_time > now() - interval '90' day --for faster CI testing
