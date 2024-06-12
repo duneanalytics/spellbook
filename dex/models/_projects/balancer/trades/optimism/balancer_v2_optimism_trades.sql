8090ff60a73396c63319d1fafa8f1205670e94c0
jeff-dude <jeff@dune.xyz>
2024-06-12 18:55:33 -0400

    move project views, fix compile errors

--- dex/models/_projects/balancer/trades/optimism/balancer_v2_optimism_trades.sql
+++ dex/models/_projects/balancer/trades/optimism/balancer_v2_optimism_trades.sql
@@ -1,13 +1,13 @@
 {{
     config(
-        schema = 'balancer_v2_avalanche_c',
+        schema = 'balancer_v2_optimism',
         alias = 'trades',
         materialized = 'view',
-        post_hook = '{{ expose_spells(\'["avalanche_c"]\',
+        post_hook = '{{ expose_spells(\'["optimism"]\',
                                 spell_type = "project",
                                 spell_name = "balancer",
                                 contributors = \'["mendesfabio", "jacektrocinski", "thetroyharris", "tomfutago", "viniabussafi"]\') }}'
-   )
+    )
 }}
 
 WITH
@@ -19,7 +19,7 @@ WITH
             swap_fee,
             pool_symbol,
             pool_type
-        FROM {{ ref('balancer_v2_avalanche_c_base_trades') }}
+        FROM {{ ref('balancer_v2_optimism_base_trades') }}
     ),
     dexs AS (
         SELECT
@@ -55,7 +55,7 @@ WITH
             INNER JOIN dexs_base
                 ON dexs.tx_hash = dexs_base.tx_hash
                 AND dexs.evt_index = dexs_base.evt_index
-        WHERE dexs.blockchain = 'avalanche_c'
+        WHERE dexs.blockchain = 'optimism'
             AND dexs.project = 'balancer'
             AND dexs.version = '2'
     ),
@@ -68,7 +68,7 @@ WITH
             dexs.block_time,
             MAX(bpt_prices.day) AS bpa_max_block_date
         FROM dexs
-            LEFT JOIN {{ ref('balancer_v2_avalanche_c_bpt_prices') }} bpt_prices
+            LEFT JOIN {{ source('balancer_v2_optimism', 'bpt_prices') }} bpt_prices
                 ON bpt_prices.contract_address = dexs.token_bought_address
                 AND bpt_prices.day <= DATE_TRUNC('day', dexs.block_time)
         GROUP BY 1, 2, 3, 4, 5
@@ -82,7 +82,7 @@ WITH
             dexs.block_time,
             MAX(bpt_prices.day) AS bpb_max_block_date
         FROM dexs
-            LEFT JOIN {{ ref('balancer_v2_avalanche_c_bpt_prices') }} bpt_prices
+            LEFT JOIN {{ source('balancer_v2_optimism', 'bpt_prices') }} bpt_prices
                 ON bpt_prices.contract_address = dexs.token_sold_address
                 AND bpt_prices.day <= DATE_TRUNC('day', dexs.block_time)
         GROUP BY 1, 2, 3, 4, 5
@@ -132,13 +132,13 @@ FROM dexs
         ON bpa.block_number = dexs.block_number
         AND bpa.tx_hash = dexs.tx_hash
         AND bpa.evt_index = dexs.evt_index
-    LEFT JOIN {{ ref('balancer_v2_avalanche_c_bpt_prices') }} bpa_bpt_prices
+    LEFT JOIN {{ source('balancer_v2_optimism', 'bpt_prices') }} bpa_bpt_prices
         ON bpa_bpt_prices.contract_address = bpa.contract_address
         AND bpa_bpt_prices.day = bpa.bpa_max_block_date
     INNER JOIN bpb
         ON bpb.block_number = dexs.block_number
         AND bpb.tx_hash = dexs.tx_hash
         AND bpb.evt_index = dexs.evt_index
-    LEFT JOIN {{ ref('balancer_v2_avalanche_c_bpt_prices') }} bpb_bpt_prices
+    LEFT JOIN {{ source('balancer_v2_optimism', 'bpt_prices') }} bpb_bpt_prices
         ON bpb_bpt_prices.contract_address = bpb.contract_address
         AND bpb_bpt_prices.day = bpb.bpb_max_block_date
