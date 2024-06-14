ac97a0a30cf5479ba9f3c5093770a32c39a3aeb8
max-morrow <amidmoro@gmail.com>
2024-06-14 17:34:23 +0300

    1inch-U25: upgrade project_swaps & project_orders flow (#6155)
    * huge upgrade project_swaps & project_orders flow
    
    - fixed calls to proxy contracts
    - fixed incorrect parsing contract abi
    
    * fixes
    
    * retrigger ci
    
    * fix bracket
    
    * redesigned lineage to optimize future refills
    
    - created project_calls flow
    - some fixes
    
    * fix & added blockchains project_orders spellbooks
    
    - fix unique keys in project_swaps & project_orders flows
    
    * retrigger
    
    * retrigger
    
    * fix
    
    * retrigger
    
    * fix
    
    * ambigous fix
    
    * fix
    
    * retrigger ci
    
    * retrigger ci
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * true dates returned
    
    ---------
    
    Co-authored-by: grkhr <eduardgorkh@gmail.com>

--- models/oneinch/base/oneinch_base_project_calls.sql
+++ models/oneinch/base/oneinch_base_project_calls.sql
@@ -5,20 +5,20 @@
 {{
     config(
         schema = 'oneinch_' + blockchain,
-        alias = 'project_swaps',
+        alias = 'project_calls',
         partition_by = ['block_month'],
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
         incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-        unique_key = ['blockchain', 'block_number', 'tx_hash', 'call_trace_address', 'call_trade_id']
+        unique_key = ['blockchain', 'block_number', 'tx_hash', 'call_trace_address']
     )
 }}
 
 
 
 {{
-    oneinch_project_swaps_macro(
+    oneinch_project_calls_macro(
         blockchain = blockchain
     )
 }}
