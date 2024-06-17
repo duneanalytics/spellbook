8ed69e22391c5fbdcc556dcf45e8cbe7ed57dfdb
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2024-06-16 20:42:11 -0400

    Small fix.

--- tokens/models/tokens/avalanche_c/tokens_avalanche_c_erc20_stablecoins.sql
+++ tokens/models/tokens/avalanche_c/tokens_avalanche_c_erc20_stablecoins.sql
@@ -12,5 +12,5 @@ FROM (
     VALUES
         (0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E,    'USDC', 6,  'USD Coin')
 		, (0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664,  'USDC', 6,  'USC Coin') --bridged
-		, (0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E,  'USDT', 6,  'Tether')
+		, (0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7,  'USDT', 6,  'Tether')
 ) AS temp_table (contract_address, symbol, decimals, name)
