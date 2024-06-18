788d48cadbdf9f5464e2b55d9f33c172ea330a02
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2024-06-18 12:56:12 -0400

    Small fixes.

--- tokens/models/tokens/linea/tokens_linea_erc20.sql
+++ tokens/models/tokens/linea/tokens_linea_erc20.sql
@@ -7,7 +7,7 @@
   )
 }}
 
-SELECT contract_address, symbol, decimals, name
+SELECT contract_address, symbol, decimals
 FROM (
     VALUES
         (0x176211869ca2b568f2a7d4ee941e073a821ee1ff,    'USDC', 6) --bridged
@@ -16,4 +16,4 @@ FROM (
 		, (0x4af15ec2a0bd43db75dd04e62faa3b8ef36b00d5,  'DAI',  18)
 		, (0xd2bc272EA0154A93bf00191c8a1DB23E67643EC5,  'USDP',  18)
 		, (0xDD3B8084AF79B9BaE3D1b668c0De08CCC2C9429A,  'MIM',  18)
-) AS temp_table (contract_address, symbol, decimals, name)
+) AS temp_table (contract_address, symbol, decimals)
