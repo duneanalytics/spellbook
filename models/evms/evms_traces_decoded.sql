3f4a3a43fcd71cd1223a275f89c9b983bdfc3dd8
hildobby <h.moulie@gmail.com>
2024-06-18 15:38:39 +0200

    Add Linea, Blast, Mantle & zkEVM to evms tables (#6159)
    * remake bridge.flows
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fi
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fi
    
    * Add Linea, Blast, Mantle & zkEVM to evms tables
    
    * fix
    
    * fix
    
    * fix

--- models/evms/evms_traces_decoded.sql
+++ models/evms/evms_traces_decoded.sql
@@ -2,7 +2,7 @@
         schema='evms',
         alias = 'traces_decoded',
         unique_key=['blockchain', 'tx_hash', 'evt_index'],
-        post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo", "base", "goerli", "zksync", "zora", "scroll"]\',
+        post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo", "base", "goerli", "zksync", "zora", "scroll", "linea", "zkevm", "blast", "mantle"]\',
                                     "sector",
                                     "evms",
                                     \'["hildobby"]\') }}'
@@ -24,6 +24,10 @@
      , ('zksync', source('zksync', 'traces_decoded'))
      , ('zora', source('zora', 'traces_decoded'))
      , ('scroll', source('scroll', 'traces_decoded'))
+     , ('linea', source('linea', 'traces_decoded'))
+     , ('zkevm', source('zkevm', 'traces_decoded'))
+     , ('blast', source('blast', 'traces_decoded'))
+     , ('mantle', source('mantle', 'traces_decoded'))
 ] %}
 
 SELECT *
