6e35bf6087a86580c82d7d7c402723d3566581af
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2024-06-18 13:12:05 -0400

    Small tweaks

--- tokens/models/tokens/tokens_stablecoins.sql
+++ tokens/models/tokens/tokens_stablecoins.sql
@@ -12,10 +12,10 @@
 
 SELECT contract_address, symbol, decimals, 'arbitrum' AS blockchain 
 FROM (VALUES
-    (0xaf88d065e77c8cc2239327c5edb3a432268e5831, 'USDC', 6) -- Native
+    (0xaf88d065e77c8cc2239327c5edb3a432268e5831,   'USDC', 6) -- Native
     , (0xff970a61a04b1ca14834a43f5de4533ebddb5cc8, 'USDC', 6) -- Bridged
     , (0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9, 'USDT', 6)
-    , (0xda10009cbd5d07dd0cecc66161fc93d7c9000da1, 'DAI', 18)
+    , (0xda10009cbd5d07dd0cecc66161fc93d7c9000da1, 'DAI',  18)
     , (0x17fc002b466eec40dae837fc4be5c67993ddbd6f, 'FRAX', 18)
 ) AS temp_table (contract_address, symbol, decimals)
 UNION ALL
