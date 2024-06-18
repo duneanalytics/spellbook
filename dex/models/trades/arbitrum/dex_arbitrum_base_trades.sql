e69946bcac25c70c8c176a60c2dc7ada0aaa29db
blanchemaria6 <130497866+blanchemaria6@users.noreply.github.com>
2024-06-18 20:29:10 +0700

    add dackieswap v3 on arb to dex.trades (#6170)
    * add dackieswap v3 on arb to dex.trades
    
    * fix model
    
    * fix seed
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dex/models/trades/arbitrum/dex_arbitrum_base_trades.sql
+++ dex/models/trades/arbitrum/dex_arbitrum_base_trades.sql
@@ -43,6 +43,7 @@
     , ref('auragi_arbitrum_base_trades')
     , ref('wombat_exchange_arbitrum_base_trades')
     , ref('solidly_v3_arbitrum_base_trades')
+    , ref('dackieswap_v3_arbitrum_base_trades')
     , ref('dackieswap_v2_arbitrum_base_trades')
 ] %}
 
