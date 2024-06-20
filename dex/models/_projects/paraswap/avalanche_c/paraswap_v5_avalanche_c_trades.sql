379fbc1f31f2c54e01cf4c0797dd24e53e5f814a
Huang Geyang <Sukebeta@outlook.com>
2024-06-20 23:33:28 +0800

    Remove amount_usd from chain-level spells

--- dex/models/_projects/paraswap/avalanche_c/paraswap_v5_avalanche_c_trades.sql
+++ dex/models/_projects/paraswap/avalanche_c/paraswap_v5_avalanche_c_trades.sql
@@ -60,60 +60,6 @@ dexs as (
         UNION ALL
         {% endif %}
     {% endfor %}
-),
-
--- USDC.e AND USDT.e price are missing before 2022-10
-price_missed_previous AS (
-    WITH usdc_price AS (
-        SELECT minute, contract_address, decimals, symbol, price
-        FROM {{ source('prices', 'usd') }}
-        WHERE contract_address = 0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664 -- USDC.e
-        ORDER BY minute
-        LIMIT 1
-    ),
-
-    usdt_price AS (
-        SELECT minute, contract_address, decimals, symbol, price
-        FROM {{ source('prices', 'usd') }}
-        WHERE contract_address = 0xc7198437980c041c805a1edcba50c1ce5db95118 -- USDT.e
-        ORDER BY minute
-        LIMIT 1
-    )
-
-    SELECT minute, contract_address, decimals, symbol, price
-    FROM usdc_price
-
-    UNION ALL
-
-    SELECT minute, contract_address, decimals, symbol, price
-    FROM usdt_price
-),
-
---  USDC.e AND USDT.e price may be missed for latest swaps
-price_missed_next AS (
-    WITH usdc_price AS (
-        SELECT minute, contract_address, decimals, symbol, price
-        FROM {{ source('prices', 'usd') }}
-        WHERE contract_address = 0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664 -- USDC.e
-        ORDER BY minute DESC
-        LIMIT 1
-    ),
-
-    usdt_price AS (
-        SELECT minute, contract_address, decimals, symbol, price
-        FROM {{ source('prices', 'usd') }}
-        WHERE contract_address = 0xc7198437980c041c805a1edcba50c1ce5db95118 -- USDT.e
-        ORDER BY minute DESC
-        LIMIT 1
-    )
-
-    SELECT minute, contract_address, decimals, symbol, price
-    FROM usdc_price
-
-    UNION ALL
-    
-    SELECT minute, contract_address, decimals, symbol, price
-    FROM usdt_price
 )
 
 SELECT
@@ -133,11 +79,6 @@ SELECT
     dexs.token_sold_amount_raw / power(10, erc20b.decimals) AS token_sold_amount,
     dexs.token_bought_amount_raw,
     dexs.token_sold_amount_raw,
-    coalesce(
-        dexs.amount_usd
-        ,(dexs.token_bought_amount_raw / power(10, erc20a.decimals)) * coalesce(p_bought.price, p_prev1.price, p_next1.price)
-        ,(dexs.token_sold_amount_raw / power(10, erc20b.decimals)) * coalesce(p_sold.price, p_prev2.price, p_next2.price)
-    ) AS amount_usd,
     dexs.token_bought_address,
     dexs.token_sold_address,
     coalesce(dexs.taker, tx."from") AS taker,
@@ -164,31 +105,3 @@ LEFT JOIN {{ source('tokens', 'erc20') }} erc20a
 LEFT JOIN {{ source('tokens', 'erc20') }} erc20b
     ON erc20b.contract_address = dexs.token_sold_address
     AND erc20b.blockchain = 'avalanche_c'
-LEFT JOIN {{ source('prices', 'usd') }} p_bought
-    ON p_bought.minute = date_trunc('minute', dexs.block_time)
-    AND p_bought.contract_address = dexs.token_bought_address
-    AND p_bought.blockchain = 'avalanche_c'
-    {% if not is_incremental() %}
-    AND p_bought.minute >= TIMESTAMP '{{project_start_date}}'
-    {% endif %}
-    {% if is_incremental() %}
-    AND p_bought.minute >= date_trunc('day', now() - interval '7' day)
-    {% endif %}
-LEFT JOIN price_missed_previous p_prev1 ON dexs.token_bought_address = p_prev1.contract_address
-    AND dexs.block_time < p_prev1.minute -- Swap before first price record time
-LEFT JOIN price_missed_next p_next1 ON dexs.token_bought_address = p_next1.contract_address
-    AND dexs.block_time > p_next1.minute -- Swap after last price record time
-LEFT JOIN {{ source('prices', 'usd') }} p_sold
-    ON p_sold.minute = date_trunc('minute', dexs.block_time)
-    AND p_sold.contract_address = dexs.token_sold_address
-    AND p_sold.blockchain = 'avalanche_c'
-    {% if not is_incremental() %}
-    AND p_sold.minute >= TIMESTAMP '{{project_start_date}}'
-    {% endif %}
-    {% if is_incremental() %}
-    AND p_sold.minute >= date_trunc('day', now() - interval '7' day)
-    {% endif %}
-LEFT JOIN price_missed_previous p_prev2 ON dexs.token_sold_address = p_prev2.contract_address
-    AND dexs.block_time < p_prev2.minute -- Swap before first price record time
-LEFT JOIN price_missed_next p_next2 ON dexs.token_sold_address = p_next2.contract_address
-    AND dexs.block_time > p_next2.minute -- Swap after last price record time
