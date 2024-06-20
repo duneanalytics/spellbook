6a0e502572235dc82bf20787593d743a14ac43f4
Huang Geyang <Sukebeta@outlook.com>
2024-06-20 17:25:31 +0800

    Remove mount_usd from arbitrum chain-level spell

--- dex/models/_projects/paraswap/arbitrum/paraswap_arbitrum_trades.sql
+++ dex/models/_projects/paraswap/arbitrum/paraswap_arbitrum_trades.sql
@@ -1,7 +1,6 @@
 {{ config(
         schema='paraswap_arbitrum',
-        alias = 'trades',
-        
+        alias = 'trades'
         )
 }}
 
@@ -28,7 +27,6 @@ FROM (
         token_sold_amount,
         token_bought_amount_raw,
         token_sold_amount_raw,
-        amount_usd,
         token_bought_address,
         token_sold_address,
         taker,
