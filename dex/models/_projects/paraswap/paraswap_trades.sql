93e8c8ca2bdee39fb4d0349e805528bcf1addcde
Huang Geyang <Sukebeta@outlook.com>
2024-06-20 18:23:14 +0800

    Apply trusted tokens

--- dex/models/_projects/paraswap/paraswap_trades.sql
+++ dex/models/_projects/paraswap/paraswap_trades.sql
@@ -19,38 +19,72 @@ ref('paraswap_avalanche_c_trades')
 ,ref('paraswap_base_trades')
 ] %}
 
-
-SELECT *
-FROM (
-    {% for dex_model in paraswap_models %}
-    SELECT
-        blockchain,
-        project,
-        version,
-        block_month,
-        block_date,
-        block_time,
-        token_bought_symbol,
-        token_sold_symbol,
-        token_pair,
-        token_bought_amount,
-        token_sold_amount,
-        token_bought_amount_raw,
-        token_sold_amount_raw,
-        token_bought_address,
-        token_sold_address,
-        taker,
-        maker,
-        project_contract_address,
-        tx_hash,
-        tx_from,
-        tx_to,
-        trace_address,
-        evt_index
-    FROM {{ dex_model }}
-    {% if not loop.last %}
-    UNION ALL
-    {% endif %}
-    {% endfor %}
+WITH basic_trades AS (
+    SELECT *
+    FROM (
+        {% for dex_model in paraswap_models %}
+        SELECT
+            blockchain,
+            project,
+            version,
+            block_month,
+            block_date,
+            block_time,
+            token_bought_symbol,
+            token_sold_symbol,
+            token_pair,
+            token_bought_amount,
+            token_sold_amount,
+            token_bought_amount_raw,
+            token_sold_amount_raw,
+            token_bought_address,
+            token_sold_address,
+            taker,
+            maker,
+            project_contract_address,
+            tx_hash,
+            tx_from,
+            tx_to,
+            trace_address,
+            evt_index
+        FROM {{ dex_model }}
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+        {% endfor %}
+    )
 )
 
+, enrichments_with_prices AS (
+    {{
+        add_amount_usd(
+            trades_cte = 'basic_trades'
+        )
+    }}
+)
+
+SELECT blockchain,
+       project,
+       version,
+       block_month,
+       block_date,
+       block_time,
+       token_bought_symbol,
+       token_sold_symbol,
+       token_pair,
+       token_bought_amount,
+       token_sold_amount,
+       token_bought_amount_raw,
+       token_sold_amount_raw,
+       amount_usd,
+       token_bought_address,
+       token_sold_address,
+       taker,
+       maker,
+       project_contract_address,
+       tx_hash,
+       tx_from,
+       tx_to,
+       trace_address,
+       evt_index
+FROM enrichments_with_prices
