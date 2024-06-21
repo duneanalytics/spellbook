85519874655bbd1df4440b726a34141a7dfcee5b
fyang1024 <fyang1024@gmail.com>
2024-06-21 17:23:50 +1000

    Add QUARTZ token to prices.usd table

--- models/prices/ethereum/prices_ethereum_tokens.sql
+++ models/prices/ethereum/prices_ethereum_tokens.sql
@@ -1794,6 +1794,7 @@ FROM
     ('portal2-portal-coin', 'ethereum', 'PORTAL', 0x1bbe973bef3a977fc51cbed703e8ffdefe001fed, 18),
     ('dough-l3t-h1m-c00k', 'ethereum', 'DOUGH', 0xFc116eA24F002F600e363bdce4b91715fe5e0392, 9),
     ('giv-giv', 'ethereum', 'GIV', 0x900db999074d9277c5da2a43f252d74366230da0, 18)
+    ('sandclock-quartz', 'ethereum', 'QUARTZ', 0xbA8A621b4a54e61C442F5Ec623687e2a942225ef, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
 where contract_address not in (
     -- bad price feeds
