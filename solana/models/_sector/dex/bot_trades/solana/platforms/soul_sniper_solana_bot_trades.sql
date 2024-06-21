5f4575fb8ea05a09cbc3a795bc2db86832aeeae2
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-06-21 14:12:59 +0200

    solana bot_trades improvment (#6226)

--- solana/models/_sector/dex/bot_trades/solana/platforms/soul_sniper_solana_bot_trades.sql
+++ solana/models/_sector/dex/bot_trades/solana/platforms/soul_sniper_solana_bot_trades.sql
@@ -13,7 +13,7 @@
             'tx_index',
             'outer_instruction_index',
             'inner_instruction_index',
-        ],
+        ]
     )
 }}
 
@@ -23,19 +23,7 @@
 
 with
     all_fee_payments as (
-        select distinct
-            signer as user
-        from {{ source('solana', 'account_activity') }} as activity
-        join {{ source("solana", "transactions") }} as transactions on tx_id = id
-        where
-            {% if is_incremental() %}
-            {{ incremental_predicate('activity.block_time') }}
-            {% else %} activity.block_time >= timestamp '{{project_start_date}}'
-            {% endif %}
-            and transactions.block_time >= timestamp '{{project_start_date}}'
-            and tx_success
-            and balance_change > 0
-            and address = '{{fee_receiver}}'
+        select * from {{ref('soul_sniper_solana_bot_users')}}
     ),
     bot_trades as (
         select
@@ -69,20 +57,19 @@ with
         join
             {{ source('solana', 'transactions') }} as transactions
             on (
-                trades.tx_id = id
+                trades.block_time = transactions.block_time
+                and trades.tx_id = id
                 {% if is_incremental() %}
                     and {{ incremental_predicate('transactions.block_time') }}
+                    and {{ incremental_predicate('trades.block_time') }}
                 {% else %}
                     and transactions.block_time >= timestamp '{{project_start_date}}'
+                    and trades.block_time >= timestamp '{{project_start_date}}'
                 {% endif %}
             )
         where
             trades.trader_id != '{{fee_receiver}}'  -- Exclude trades signed by FeeWallet
             and transactions.signer != '{{fee_receiver}}'  -- Exclude trades signed by FeeWallet
-            {% if is_incremental() %}
-                and {{ incremental_predicate('trades.block_time') }}
-            {% else %} and trades.block_time >= timestamp '{{project_start_date}}'
-            {% endif %}
     ),
     highest_inner_instruction_index_for_each_trade as (
         select
