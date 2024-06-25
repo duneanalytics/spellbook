74d40ed918399fcb09ab48a452599f96681eb83f
hildobby <h.moulie@gmail.com>
2024-06-25 21:53:31 +0200

    Blur on Blast Fix (#6251)
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- models/prices/blast/prices_blast_tokens.sql
+++ models/prices/blast/prices_blast_tokens.sql
@@ -17,5 +17,6 @@ FROM
     VALUES
     ('weth-weth','blast','WETH',0x4300000000000000000000000000000000000004,18),
     ('usdb-usdb','blast','USDB',0x4300000000000000000000000000000000000003,18),
+    ('weth-weth', 'blast', 'bpETH', 0xb772d5c5f4a2eef67dfbc89aa658d2711341b8e5, 18),
     ('yolo-yolo-games','blast','YOLO',0xf77dd21c5ce38ac08786be35ef1d1dec1a6a15f3,18)    
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
