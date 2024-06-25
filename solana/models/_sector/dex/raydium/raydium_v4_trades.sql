adc280db6ff0538654b4e2153f44601ce099ae0e
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-06-25 05:44:23 -0400

    change price methodology

--- solana/models/_sector/dex/raydium/raydium_v4_trades.sql
+++ solana/models/_sector/dex/raydium/raydium_v4_trades.sql
@@ -116,7 +116,10 @@ SELECT
     , tb.token_sold_symbol
     , tb.token_sold_amount
     , tb.token_sold_amount_raw
-    , COALESCE(tb.token_sold_amount * p_sold.price, tb.token_bought_amount * p_bought.price) as amount_usd
+    , case when p_sold.price is not null and p_bought.pric eis not null 
+        then least(tb.token_sold_amount * p_sold.price, tb.token_bought_amount * p_bought.price)
+        else COALESCE(tb.token_sold_amount * p_sold.price, tb.token_bought_amount * p_bought.price)
+        end as amount_usd    
     , cast(null as double) as fee_tier
     , cast(null as double) as fee_usd
     , tb.token_sold_mint_address
