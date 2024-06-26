615149f7647fe8779ff9a51ff8825e047fa1da6c
Huang Geyang <Sukebeta@outlook.com>
2024-06-27 01:05:46 +0800

    Init bladeswap_blast_base_trades

---
+++ dex/models/trades/blast/platforms/bladeswap_blast_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'bladeswap_blast',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v2_trades(
+        blockchain = 'blast',
+        project = 'bladeswap',
+        version = '1',
+        Pair_evt_Swap = source('bladeswap_blast', 'XYKPool_evt_Swap'),
+        Factory_evt_PairCreated = source('bladeswap_blast', 'XYKPoolFactory_V2_evt_PoolCreated')
+    )
+}}
