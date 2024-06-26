b079c22ba18167ba626010f59eec8c201da57ba7
Huang Geyang <Sukebeta@outlook.com>
2024-06-27 02:01:46 +0800

    Update bladeswap_blast_base_trades

--- dex/models/trades/blast/platforms/bladeswap_blast_base_trades.sql
+++ dex/models/trades/blast/platforms/bladeswap_blast_base_trades.sql
@@ -10,13 +10,51 @@
     )
 }}
 
-{{
-    uniswap_compatible_v2_trades(
-        blockchain = 'blast',
-        project = 'bladeswap',
-        version = '1',
-        Pair_evt_Swap = source('bladeswap_blast', 'XYKPool_evt_Swap'),
-        Factory_evt_PairCreated = source('bladeswap_blast', 'XYKPoolFactory_V2_evt_PoolCreated'),
-        pair_column_name = 'pool'
-    )
-}}
+WITH pools AS (
+    SELECT
+        bytearray_substring(t1, 13, 20) as token0,
+        bytearray_substring(t2, 13, 20) as token1,
+        pool
+    FROM
+        {{ source('bladeswap_blast', 'XYKPoolFactory_V2_evt_PoolCreated') }}
+)
+, dexs AS (
+    SELECT
+        t.evt_block_number AS block_number
+        , t.evt_block_time AS block_time
+        , t.to AS taker
+        , t.contract_address AS maker
+        , CASE WHEN amount0Out = UINT256 '0' THEN amount1Out ELSE amount0Out END AS token_bought_amount_raw
+        , CASE WHEN amount0In = UINT256 '0' OR amount1Out = UINT256 '0' THEN amount1In ELSE amount0In END AS token_sold_amount_raw
+        , CASE WHEN amount0Out = UINT256 '0' THEN f.token1 ELSE f.token0 END AS token_bought_address
+        , CASE WHEN amount0In = UINT256 '0' OR amount1Out = UINT256 '0' THEN f.token1 ELSE f.token0 END AS token_sold_address
+        , t.contract_address AS project_contract_address
+        , t.evt_tx_hash AS tx_hash
+        , t.evt_index AS evt_index
+    FROM
+        {{ source('bladeswap_blast', 'XYKPool_evt_Swap') }} t
+    INNER JOIN
+        pools f
+        ON f.pool = t.contract_address
+
+)
+
+SELECT
+    'blast' AS blockchain
+    , 'bladeswap' AS project
+    , '1' AS version
+    , CAST(date_trunc('month', dexs.block_time) AS date) AS block_month
+    , CAST(date_trunc('day', dexs.block_time) AS date) AS block_date
+    , dexs.block_time
+    , dexs.block_number
+    , dexs.token_bought_amount_raw
+    , dexs.token_sold_amount_raw
+    , dexs.token_bought_address
+    , dexs.token_sold_address
+    , dexs.taker
+    , dexs.maker
+    , dexs.project_contract_address
+    , dexs.tx_hash
+    , dexs.evt_index
+FROM
+    dexs
