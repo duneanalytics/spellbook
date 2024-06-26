2fcd31e87aa85d10f7b700c2ce337830914dd6e1
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-06-26 17:51:20 +0200

    Solana dex trades: enrichment + base trades setup (#6245)
    * enrichment + base trades setup
    
    * remove excluded ones
    
    * fix
    
    * fix
    
    * fix
    
    * typo
    
    * add comment for block_slot and project_main_id
    
    * add test for column types and uniqueness
    
    * fix col name

--- solana/models/_sector/dex/dex_solana_trades.sql
+++ solana/models/_sector/dex/dex_solana_trades.sql
@@ -1,6 +1,5 @@
 {{
   config(
-
         schema = 'dex_solana',
         alias = 'trades',
         materialized = 'incremental',
@@ -12,53 +11,66 @@
         post_hook='{{ expose_spells(\'["solana"]\',
                                     "project",
                                     "dex",
-                                    \'["ilemi"]\') }}')
+                                    \'["ilemi,0xRob"]\') }}')
 }}
 
-{% set solana_dexes = [
-    ref('orca_whirlpool_trades')
-    , ref('raydium_v3_trades')
-    , ref('raydium_v4_trades')
-    , ref('phoenix_v1_trades')
-    , ref('meteora_v1_solana_trades')
-    , ref('meteora_v2_solana_trades')
-    , ref('goosefx_ssl_v2_solana_trades')
-    , ref('pumpdotfun_solana_trades')
-] %}
+with base_trades as (
+    select * from {{ ref('dex_solana_base_trades')}}
+    {% if is_incremental() %}
+    WHERE {{incremental_predicate('block_time')}}
+    {% endif %}
+)
 
-{% for dex in solana_dexes %}
-SELECT
-      blockchain
+SELECT bt.blockchain
       , project
       , version
       , CAST(date_trunc('month', block_time) AS DATE) as block_month
-      , block_time
+      , bt.block_time
+      , bt.block_slot
       , trade_source
-      , token_bought_symbol
-      , token_sold_symbol
-      , token_pair
-      , token_bought_amount
-      , token_sold_amount
+      , token_bought.symbol as token_bought_symbol
+      , token_sold.symbol as token_sold_symbol
+      , case when lower(token_bought.symbol) > lower(token_sold.symbol)
+            then concat(token_bought.symbol, '-', token_sold.symbol)
+            else concat(token_sold.symbol, '-', token_bought.symbol)
+        end as token_pair
+      , token_bought_amount_raw / pow(10,coalesce(token_bought.decimals, 9)) as token_bought_amount
+      , token_sold_amount_raw / pow(10,coalesce(token_sold.decimals, 9)) as token_sold_amount
       , token_bought_amount_raw
       , token_sold_amount_raw
-      , amount_usd
+      , COALESCE(token_sold_amount_raw / pow(10,coalesce(token_sold.decimals, 9)) * p_sold.price
+               , token_bought_amount_raw / pow(10,coalesce(token_bought.decimals, 9)) * p_bought.price)
+            as amount_usd
       , fee_tier
-      , fee_usd
+      , fee_tier *
+        COALESCE(token_sold_amount_raw / pow(10,coalesce(token_sold.decimals, 9)) * p_sold.price
+               , token_bought_amount_raw / pow(10,coalesce(token_bought.decimals, 9)) * p_bought.price)
+            as fee_usd
       , token_bought_mint_address
       , token_sold_mint_address
       , token_bought_vault
       , token_sold_vault
       , project_program_id
+      , project_main_id
       , trader_id
       , tx_id
       , outer_instruction_index
       , inner_instruction_index
       , tx_index
-FROM {{ dex }}
-{% if is_incremental() %}
-WHERE {{incremental_predicate('block_time')}}
-{% endif %}
-{% if not loop.last %}
-UNION ALL
-{% endif %}
-{% endfor %}
+FROM base_trades bt
+LEFT JOIN {{ ref('tokens_solana_fungible') }} token_bought ON token_bought.token_mint_address = token_bought_mint_address
+LEFT JOIN {{ ref('tokens_solana_fungible') }} token_sold ON token_sold.token_mint_address = token_sold_mint_address
+LEFT JOIN {{ source('prices', 'usd') }} p_bought
+    ON p_bought.blockchain = 'solana'
+    AND date_trunc('minute', bt.block_time) = p_bought.minute
+    AND token_bought_mint_address = toBase58(p_bought.contract_address)
+    {% if is_incremental() %}
+    AND {{incremental_predicate('p_bought.minute')}}
+    {% endif %}
+LEFT JOIN {{ source('prices', 'usd') }} p_sold
+    ON p_sold.blockchain = 'solana'
+    AND date_trunc('minute', bt.block_time) = p_sold.minute
+    AND token_sold_mint_address = toBase58(p_sold.contract_address)
+    {% if is_incremental() %}
+    AND {{incremental_predicate('p_sold.minute')}}
+    {% endif %}
