5891c9adcc175a506e4b26f1a2d59f1fac969c0d
jeff-dude <jeff@dune.xyz>
2024-06-26 10:27:44 -0400

    Revert "Fix price tokens and set up tests (#6262)"
    This reverts commit 625919b13353f53eb77c4374ea8ca21dcd01fa1d.

--- models/prices/prices_tokens.sql
+++ models/prices/prices_tokens.sql
@@ -47,7 +47,6 @@ FROM
         , contract_address
         , decimals
     FROM {{ model }}
-    WHERE contract_address != 0x0000000000000000000000000000000000000000    -- safeguard as native tokens currently have null address
     {% if not loop.last %}
     UNION ALL
     {% endif %}
