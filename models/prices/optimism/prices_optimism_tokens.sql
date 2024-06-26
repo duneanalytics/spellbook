4157eacb1ddcd951f4a60ecbfa3cc22ac6dca527
0xRob <0xRobin5@gmail.com>
2024-06-26 16:38:08 +0200

    use IS DISTINCT FROM

--- models/prices/optimism/prices_optimism_tokens.sql
+++ models/prices/optimism/prices_optimism_tokens.sql
@@ -6,28 +6,19 @@
         
         )
 }}
-
-WITH prices_op AS (
-    SELECT
-        token_id
-        , blockchain
-        , symbol
-        , contract_address
-        , decimals
-    FROM {{ ref('prices_optimism_tokens_curated') }}
-    UNION ALL
-    SELECT
-        token_id
-        , blockchain
-        , symbol
-        , contract_address
-        , decimals
-    FROM {{ ref('prices_optimism_tokens_bridged') }}
-    WHERE contract_address not in (select contract_address from {{ ref('prices_optimism_tokens_curated') }})
-)
 SELECT
-    *
-FROM 
-    prices_op
-WHERE
-    contract_address != 0x0000000000000000000000000000000000000000 -- safeguard as native tokens currently have null address
+    token_id
+    , blockchain
+    , symbol
+    , contract_address
+    , decimals
+FROM {{ ref('prices_optimism_tokens_curated') }}
+UNION ALL
+SELECT
+     token_id
+    , blockchain
+    , symbol
+    , contract_address
+    , decimals
+FROM {{ ref('prices_optimism_tokens_bridged') }}
+WHERE contract_address not in (select contract_address from {{ ref('prices_optimism_tokens_curated') }})
