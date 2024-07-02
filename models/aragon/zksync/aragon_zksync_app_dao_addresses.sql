a0103edbab9b46aed15436ae273a16976997abb3
Henrystats <65398020+henrystats@users.noreply.github.com>
2024-07-02 02:21:30 +0100

    use logs

--- models/aragon/zksync/aragon_zksync_app_dao_addresses.sql
+++ models/aragon/zksync/aragon_zksync_app_dao_addresses.sql
@@ -9,18 +9,23 @@
     )
 }}
 
+{% set project_start_date = '2024-06-01' %}
 
 SELECT 
-    'zksync' as blockchain, 
-    'aragon' as dao_creator_tool, 
-    dao, 
-    dao as dao_wallet_address, 
-    evt_block_time as created_block_time, 
-    CAST(date_trunc('day', evt_block_time) as DATE) as created_date, 
-    CAST(date_trunc('month', evt_block_time) as DATE) as block_month, 
+    'zksync' as blockchain,
+    'aragon' as dao_creator_tool,
+    bytearray_ltrim(topic1) as dao,
+    bytearray_ltrim(topic1) as dao_wallet_address,
+    block_time as created_block_time, 
+    CAST(date_trunc('day', block_time) as DATE) as created_date, 
+    CAST(date_trunc('month', block_time) as DATE) as block_month, 
     'aragon_app' as product 
 FROM 
-{{ source('aragon_app_zksync', 'DAOFactory_evt_DAORegistered') }}
-{% if is_incremental() %}
-WHERE {{incremental_predicate('evt_block_time')}}
+{{ source('zksync', 'logs') }}
+{% if not is_incremental() %}
+WHERE block_time >= DATE '{{project_start_date}}'
 {% endif %}
+{% if is_incremental() %}
+WHERE {{incremental_predicate('block_time')}}
+{% endif %}
+AND topic0 =  0xbc0b11fe649bb4d67c7fb40936163e5423f45c3ae83fbd8f8f8c75e1a3fa97af
