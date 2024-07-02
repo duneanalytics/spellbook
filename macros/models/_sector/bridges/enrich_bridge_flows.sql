f298d7387bd8b68a7ac80ea731b8fe95b01dbf2c
Hildebert Moulié <h.moulie@gmail.com>
2024-07-02 18:34:29 +0200

    fix

--- macros/models/_sector/bridges/enrich_bridge_flows.sql
+++ macros/models/_sector/bridges/enrich_bridge_flows.sql
@@ -3,10 +3,12 @@
 
 SELECT bf.*
 FROM {{base_flows}} bf
+{% if is_incremental() %}
 LEFT JOIN {{this}} t ON t.blockchain=bf.blockchain
     AND t.tx_hash=bf.tx_hash
     AND t.evt_index=bf.evt_index
     AND t.block_number IS NULL
 WHERE {{ incremental_predicate('block_time') }}
+{% endif %}
 
 {% endmacro %}
