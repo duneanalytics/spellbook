0d57c5eba33ef1c5c05614bd16dbd9fc175d2980
Hildebert Moulié <h.moulie@gmail.com>
2024-07-02 17:35:31 +0200

    fixes

--- daily_spellbook/macros/models/sector/addresses_events/addresses_events_first_funded_by.sql
+++ daily_spellbook/macros/models/sector/addresses_events/addresses_events_first_funded_by.sql
@@ -2,15 +2,17 @@
 
 SELECT '{{blockchain}}' as blockchain
 , tt.to AS address
-, MIN_BY(tt."from", tt.block_number) AS first_funded_by
-, MIN_BY(tt.tx_from, tt.block_number) AS first_funding_executed_by
+, MIN_BY(tt."from", (tt.block_number, tt.tx_index)) AS first_funded_by
+, MIN_BY(tt.tx_from, (tt.block_number, tt.tx_index)) AS first_funding_executed_by
 , MIN(tt.block_time) AS block_time
 , MIN(tt.block_number) AS block_number
-, MIN_BY(tt.tx_hash, tt.block_number) AS tx_hash
+, MIN_BY(tt.tx_hash, (tt.block_number, tt.tx_index)) AS tx_hash
 , MIN_BY(tt.tx_index, tt.block_number) AS tx_index
+, MIN_BY(tt.unique_key, (tt.block_number, tt.tx_index)) AS unique_key
 FROM {{token_transfers}} tt
 {% if is_incremental() %}
-LEFT JOIN {{this}} ffb ON tt.to = ffb.address WHERE ffb.address IS NULL
+LEFT JOIN {{this}} ffb ON tt.to = ffb.address
+    AND ffb.address IS NULL
 WHERE {{ incremental_predicate('tt.block_time') }}
 AND tt.token_standard = 'native'
 {% else %}
