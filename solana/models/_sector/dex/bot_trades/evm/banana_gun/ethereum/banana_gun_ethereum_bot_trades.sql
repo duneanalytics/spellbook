a24361f1657ded456bf5358f642c2959e5b6b976
whale_hunter <143016036+whalehunting@users.noreply.github.com>
2024-07-03 20:57:28 +0200

    fix: fix column name in uniqueness test

--- solana/models/_sector/dex/bot_trades/evm/banana_gun/ethereum/banana_gun_ethereum_bot_trades.sql
+++ solana/models/_sector/dex/bot_trades/evm/banana_gun/ethereum/banana_gun_ethereum_bot_trades.sql
@@ -6,7 +6,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-    unique_key = ['blockchain', 'tx_id', 'evt_index']
+    unique_key = ['blockchain', 'tx_hash', 'evt_index']
    )
 }}
 
