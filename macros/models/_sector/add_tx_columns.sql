9d4ea0981410e7a2a576a9bb460b6bbcbf0a0889
Alan Ghobadi <aalan3@gmail.com>
2024-07-04 18:32:08 +0200

    typo

--- macros/models/_sector/add_tx_columns.sql
+++ macros/models/_sector/add_tx_columns.sql
@@ -14,7 +14,7 @@ from {{model_cte}} model
 inner join {{source(blockchain, 'transactions')}} tx
     on
     model.block_date = tx.block_date
-    model.block_number = tx.block_number
+    and model.block_number = tx.block_number
     and model.tx_hash = tx.hash
     {% if is_incremental() %}
     and {{incremental_predicate('tx.block_time')}}
