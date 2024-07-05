dc76aa4d53910289a611ed2ff649d16e20755a89
Alan Ghobadi <aalan3@gmail.com>
2024-07-05 13:52:46 +0200

    fix typo

--- models/_sector/tokens/ethereum/tokens_ethereum_balances_daily_agg_base.sql
+++ models/_sector/tokens/ethereum/tokens_ethereum_balances_daily_agg_base.sql
@@ -3,7 +3,7 @@
         alias = 'balances_daily_agg_base',
         file_format = 'delta',
         materialized='incremental',
-        partition_by = ['day']
+        partition_by = ['day'],
         incremental_strategy='merge',
         incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
         unique_key = ['unique_key'],
