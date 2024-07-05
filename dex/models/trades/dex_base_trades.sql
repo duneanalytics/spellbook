7b7775e715ed056300c82079d048e0b679241bb2
Alan Ghobadi <aalan3@gmail.com>
2024-07-05 11:45:44 +0200

    Cast as uint256 instead

--- dex/models/trades/dex_base_trades.sql
+++ dex/models/trades/dex_base_trades.sql
@@ -42,8 +42,8 @@ with base_union as (
             , block_date
             , block_time
             , block_number
-            , token_bought_amount_raw
-            , token_sold_amount_raw
+            , cast(token_bought_amount_raw as uint256) as token_bought_amount_raw
+            , cast(token_sold_amount_raw as uint256) as token_sold_amount_raw
             , token_bought_address
             , token_sold_address
             , taker
