dddaccf197547a159442537d14f54984b9c999b9
viniabussafi <131974393+viniabussafi@users.noreply.github.com>
2024-07-05 07:36:06 +0100

    make macros balancer_compatible (#6098)
    * make macros balancer_compatible
    
    * fix typo
    
    * test
    
    * again
    
    * again
    
    * again
    
    * oops
    
    * remove models from pr
    
    * update beethoven_x models
    
    * add new macros for beethoven x
    
    * fix schema
    
    * again
    
    * again
    
    * again
    
    * fix
    
    * again
    
    * revert beets liquidity
    
    * add protocol fees collector address for fantom
    
    * fix
    
    * fix upstream refs
    
    * retry
    
    * update bpt prices macro
    
    * filter to remove dupes
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: Alan Ghobadi <aalan3@gmail.com>

--- models/beethoven_x/fantom/beethoven_x_fantom_token_balance_changes.sql
+++ models/beethoven_x/fantom/beethoven_x_fantom_token_balance_changes.sql
@@ -1,17 +1,15 @@
-{{
-    config(
+{{ config(
         schema = 'beethoven_x_fantom',
-        alias = 'bpt_prices',        
+        alias = 'token_balance_changes',
         materialized = 'table',
         file_format = 'delta'
     )
 }}
 
-
 {{ 
-    balancer_v2_compatible_bpt_prices_macro(
+    balancer_v2_compatible_token_balance_changes_macro(
         blockchain = 'fantom',
-        version = '2',        
+        version = '2',
         project_decoded_as = 'beethoven_x',
         base_spells_namespace = 'beethoven_x_fantom',
         pool_labels_spell = ref('labels_beethoven_x_pools_fantom')
