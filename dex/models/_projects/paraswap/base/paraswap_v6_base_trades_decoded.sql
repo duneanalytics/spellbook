22c20ea0334bc3bf84db19b4b3acfef6f9611354
Alan Ghobadi <aalan3@gmail.com>
2024-07-06 16:51:27 +0200

    Fix incremental predicate column

--- dex/models/_projects/paraswap/base/paraswap_v6_base_trades_decoded.sql
+++ dex/models/_projects/paraswap/base/paraswap_v6_base_trades_decoded.sql
@@ -5,7 +5,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.call_block_time')],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.blockTime')],
     unique_key = ['block_date', 'blockchain', 'project', 'version', 'txHash', 'method', 'callTraceAddress']
     )
 }}
