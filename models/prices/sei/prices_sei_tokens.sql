595252635a1e896e314e4914ae06ecf95e8eabae
jeff-dude <jeff@dune.xyz>
2024-07-17 12:18:08 -0400

    fix prices bug

--- models/prices/sei/prices_sei_tokens.sql
+++ models/prices/sei/prices_sei_tokens.sql
@@ -22,4 +22,4 @@ FROM
     , ('usdc-usd-coin', 'USDC', 0x3894085Ef7Ff0f0aeDf52E2A2704928d1Ec074F1, 6)
     , ('usdt-tether', 'USDT', 0xB75D0B03c06A926e488e2659DF1A861F860bD3d1, 6)
     , ('weth-weth', 'WETH', 0x160345fC359604fC6e70E3c5fAcbdE5F7A9342d8, 18)
-) as temp (token_id, blockchain, symbol, contract_address, decimals)
+) as temp (token_id, symbol, contract_address, decimals)
