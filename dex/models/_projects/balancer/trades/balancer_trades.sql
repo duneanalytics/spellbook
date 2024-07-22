9c3cc6c1fb0b1c23b2934b9c347bd9926a8acb0e
viniabussafi <vinicius@balancerlabs.dev>
2024-07-22 15:18:03 +0100

    rebuild balancer.trades

--- dex/models/_projects/balancer/trades/balancer_trades.sql
+++ dex/models/_projects/balancer/trades/balancer_trades.sql
@@ -2,37 +2,59 @@
         schema = 'balancer',
         alias = 'trades',
         materialized = 'view',
-        post_hook='{{ expose_spells(blockchains = \'["arbitrum", "avalanche_c", "base", "ethereum", "gnosis", "optimism", "polygon"]\',
+        post_hook='{{ expose_spells(blockchains = \'["arbitrum", "avalanche_c", "base", "ethereum", "gnosis", "optimism", "polygon", "zkevm"]\',
                                       spell_type = "project", 
                                       spell_name = "balancer", 
-                                      contributors = \'["bizzyvinci", "thetroyharris"]\') }}'
+                                      contributors = \'["bizzyvinci", "thetroyharris", "viniabussafi"]\') }}'
         )
 }}
 
+{% set balancer_models = [
+    ref('balancer_v2_arbitrum_pools_fees'),
+    ref('balancer_v2_avalanche_c_pools_fees'),
+    ref('balancer_v2_base_pools_fees'),
+    ref('balancer_v2_ethereum_pools_fees'),
+    ref('balancer_v2_gnosis_pools_fees'),
+    ref('balancer_v2_optimism_pools_fees'),
+    ref('balancer_v2_polygon_pools_fees'),
+    ref('balancer_v2_zkevm_pools_fees')
+] %}
 
-SELECT  blockchain
-        , project
-        , version
-        , block_month
-        , block_date
-        , block_time
-        , block_number
-        , token_bought_symbol
-        , token_sold_symbol
-        , token_pair
-        , token_bought_amount
-        , token_sold_amount
-        , token_bought_amount_raw
-        , token_sold_amount_raw
-        , amount_usd
-        , token_bought_address
-        , token_sold_address
-        , taker
-        , maker
-        , project_contract_address
-        , tx_hash
-        , tx_from
-        , tx_to
-        , evt_index
-FROM {{ ref('dex_trades') }}
-WHERE project = 'balancer'
+
+SELECT *
+FROM (
+    {% for model in balancer_models %}
+    blockchain,
+    project,
+    version,
+    block_date,
+    block_number,
+    block_month,
+    block_time,
+    token_bought_symbol,
+    token_sold_symbol,
+    token_pair,
+    token_bought_amount,
+    token_sold_amount,
+    token_bought_amount_raw,
+    token_sold_amount_raw,
+    amount_usd,
+    token_bought_address,
+    token_sold_address,
+    taker,
+    maker,
+    project_contract_address,
+    pool_id,
+    swap_fee,
+    pool_symbol,
+    pool_type,
+    tx_hash,
+    tx_from,
+    tx_to,
+    evt_index
+    FROM {{ model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
