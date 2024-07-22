32fda92facaebdf7e8f2dc97b1096c4038871acd
viniabussafi <vinicius@balancerlabs.dev>
2024-07-22 15:23:52 +0100

    change models

--- dex/models/_projects/balancer/trades/balancer_trades.sql
+++ dex/models/_projects/balancer/trades/balancer_trades.sql
@@ -10,51 +10,52 @@
 }}
 
 {% set balancer_models = [
-    ref('balancer_v2_arbitrum_pools_fees'),
-    ref('balancer_v2_avalanche_c_pools_fees'),
-    ref('balancer_v2_base_pools_fees'),
-    ref('balancer_v2_ethereum_pools_fees'),
-    ref('balancer_v2_gnosis_pools_fees'),
-    ref('balancer_v2_optimism_pools_fees'),
-    ref('balancer_v2_polygon_pools_fees'),
-    ref('balancer_v2_zkevm_pools_fees')
+    ref('balancer_arbitrum_trades'),
+    ref('balancer_avalanche_c_trades'),
+    ref('balancer_base_trades'),
+    ref('balancer_ethereum_trades'),
+    ref('balancer_gnosis_trades'),
+    ref('balancer_optimism_trades'),
+    ref('balancer_polygon_trades'),
+    ref('balancer_zkevm_trades')
 ] %}
 
 
 SELECT *
 FROM (
     {% for model in balancer_models %}
-    blockchain,
-    project,
-    version,
-    block_date,
-    block_number,
-    block_month,
-    block_time,
-    token_bought_symbol,
-    token_sold_symbol,
-    token_pair,
-    token_bought_amount,
-    token_sold_amount,
-    token_bought_amount_raw,
-    token_sold_amount_raw,
-    amount_usd,
-    token_bought_address,
-    token_sold_address,
-    taker,
-    maker,
-    project_contract_address,
-    pool_id,
-    swap_fee,
-    pool_symbol,
-    pool_type,
-    tx_hash,
-    tx_from,
-    tx_to,
-    evt_index
-    FROM {{ model }}
-    {% if not loop.last %}
-    UNION ALL
-    {% endif %}
-    {% endfor %}
-)
+        SELECT 
+                blockchain,
+                project,
+                version,
+                block_date,
+                block_number,
+                block_month,
+                block_time,
+                token_bought_symbol,
+                token_sold_symbol,
+                token_pair,
+                token_bought_amount,
+                token_sold_amount,
+                token_bought_amount_raw,
+                token_sold_amount_raw,
+                amount_usd,
+                token_bought_address,
+                token_sold_address,
+                taker,
+                maker,
+                project_contract_address,
+                pool_id,
+                swap_fee,
+                pool_symbol,
+                pool_type,
+                tx_hash,
+                tx_from,
+                tx_to,
+                evt_index
+        FROM {{ model }}
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+        {% endfor %}
+        )
