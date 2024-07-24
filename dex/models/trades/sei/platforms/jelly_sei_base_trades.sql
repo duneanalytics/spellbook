17cba317bdb3ace486cbdb13af28ecf93895d33f
Huang Geyang <Sukebeta@outlook.com>
2024-07-24 20:21:22 +0800

    Implement jelly_sei_base_trades

--- dex/models/trades/sei/platforms/jelly_sei_base_trades.sql
+++ dex/models/trades/sei/platforms/jelly_sei_base_trades.sql
@@ -10,7 +10,8 @@
     )
 }}
 
-SELECT * FROM (
+WITH trades AS (
+    SELECT * FROM (
         SELECT
             swaps.poolId,
             swaps.evt_tx_hash,
@@ -20,11 +21,54 @@ SELECT * FROM (
             fees.swap_fee_percentage,
             ROW_NUMBER() OVER (PARTITION BY poolId, evt_tx_hash, evt_index ORDER BY block_number DESC, index DESC) AS rn
         FROM {{ source('jelly_swap_sei', 'Vault_evt_Swap') }} swaps
-        LEFT JOIN {{ ref(project_decoded_as ~ '_' ~ blockchain ~ '_' ~ pools_fees) }} fees
-            ON fees.contract_address = bytearray_substring(swaps.poolId, 1, 20)
-            AND ARRAY[fees.block_number] || ARRAY[fees.index] < ARRAY[swaps.evt_block_number] || ARRAY[swaps.evt_index]
         {% if is_incremental() %}
         WHERE {{ incremental_predicate('swaps.evt_block_time') }}
         {% endif %}
     ) t
     WHERE t.rn = 1
+)
+
+-- , pools AS (
+--     SELECT
+--         'sei' AS blockchain,
+--         poolId,
+--         poolAddress AS pool_address,
+--     FROM {{ source('jelly_swap_sei', 'Vault_evt_PoolRegistered') }}
+-- )
+
+, dexs AS (
+    SELECT
+        trade.evt_block_number AS block_number,
+        trade.evt_block_time AS block_time,
+        CAST(NULL AS VARBINARY) AS taker,
+        CAST(NULL AS VARBINARY) AS maker,
+        trade.amountOut AS token_bought_amount_raw,
+        trade.amountIn AS token_sold_amount_raw,
+        trade.tokenOut AS token_bought_address,
+        trade.tokenIn AS token_sold_address,
+        trade.contract_address AS project_contract_address,
+        trade.evt_tx_hash AS tx_hash,
+        trade.evt_index
+    FROM trades trade
+    LEFT JOIN pools p
+        ON p.poolId = trade.poolId
+)
+
+SELECT
+    'sei' AS blockchain,
+    'jelly' AS project,
+    '1' AS version,
+    CAST(date_trunc('month', dexs.block_time) AS date) AS block_month,
+    CAST(date_trunc('day', dexs.block_time) AS date) AS block_date,
+    dexs.block_time,
+    dexs.block_number,
+    dexs.token_bought_amount_raw,
+    dexs.token_sold_amount_raw,
+    dexs.token_bought_address,
+    dexs.token_sold_address,
+    dexs.taker,
+    dexs.maker,
+    dexs.project_contract_address,
+    dexs.tx_hash,
+    dexs.evt_index
+FROM dexs
