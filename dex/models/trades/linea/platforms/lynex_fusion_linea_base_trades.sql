0592c1dc242d39560d716720a3109d0b5e30ca96
atarod0 <77494391+atarod0@users.noreply.github.com>
2024-07-25 12:09:59 +0100

    Update lynex_fusion_linea_base_trades.sql

--- dex/models/trades/linea/platforms/lynex_fusion_linea_base_trades.sql
+++ dex/models/trades/linea/platforms/lynex_fusion_linea_base_trades.sql
@@ -11,13 +11,47 @@
 }}
 
 
-{{
-    uniswap_compatible_v3_trades(
-        blockchain = 'linea'
-        , project = 'lynex'
-        , version = 'fusion'
-        , Pair_evt_Swap = source('lynex_linea', 'AlgebraPool_evt_Swap')
-        , Factory_evt_PoolCreated = source('lynex_linea', 'AlgebraFactory_evt_Pool')
-    )
-}}
-    
+WITH dexs AS
+(
+    SELECT
+        t.evt_block_number AS block_number
+        , t.evt_block_time AS block_time
+        , t.recipient AS taker
+        , cast(null as varbinary) as maker
+        , CASE WHEN t.amount0 < INT256 '0' THEN abs(t.amount0) ELSE abs(t.amount1) END AS token_bought_amount_raw -- when amount0 is negative it means trader_a is buying token0 from the pool
+        , CASE WHEN t.amount0 < INT256 '0' THEN abs(t.amount1) ELSE abs(t.amount1) END AS token_sold_amount_raw
+        , CASE WHEN t.amount0 < INT256 '0' THEN f.token0 ELSE f.token1 END AS token_bought_address
+        , CASE WHEN t.amount0 < INT256 '0' THEN f.token1 ELSE f.token0 END AS token_sold_address
+        , t.contract_address as project_contract_address
+        , t.evt_tx_hash AS tx_hash
+        , t.evt_index
+    FROM
+        {{ source('lynex_linea', 'AlgebraPool_evt_Swap') }} t
+    INNER JOIN
+        {{ source('lynex_linea', 'AlgebraFactory_evt_Pool') }} f
+        ON f.pool = t.contract_address
+    {% if is_incremental() %}
+    WHERE
+        {{ incremental_predicate('t.evt_block_time') }}
+    {% endif %}
+)
+
+SELECT
+    'linea' AS blockchain
+    ,'lynex' AS project
+    ,'fusion' AS version
+    , CAST(date_trunc('month', dexs.block_time) AS date) AS block_month
+    , CAST(date_trunc('day', dexs.block_time) AS date) AS block_date
+    , dexs.block_time
+    , dexs.block_number
+    , CAST(dexs.token_bought_amount_raw AS UINT256) AS token_bought_amount_raw
+    , CAST(dexs.token_sold_amount_raw AS UINT256) AS token_sold_amount_raw
+    , dexs.token_bought_address
+    , dexs.token_sold_address
+    , dexs.taker
+    , dexs.maker
+    , dexs.project_contract_address
+    , dexs.tx_hash
+    , dexs.evt_index
+FROM
+    dexs
