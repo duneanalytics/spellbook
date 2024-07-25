6707953c59dd7ff767279d8c2e676c4e841f1376
RantumBits <rantum@rantum.xyz>
2024-07-25 14:26:23 -0700

    requested changes excl incremental on nft.transfers

--- dex/models/_projects/zeroex/arbitrum/zeroex_arbitrum_settler_trades.sql
+++ dex/models/_projects/zeroex/arbitrum/zeroex_arbitrum_settler_trades.sql
@@ -4,7 +4,7 @@
         alias = 'settler_trades',
         materialized='incremental',
         partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'evt_index'],
+        unique_key = ['block_month', 'block_date', 'tx_hash', 'evt_index'],
         on_schema_change='sync_all_columns',
         file_format ='delta',
         incremental_strategy='merge',
