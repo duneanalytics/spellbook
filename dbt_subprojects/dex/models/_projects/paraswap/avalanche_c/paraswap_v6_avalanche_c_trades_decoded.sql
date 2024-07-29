5f6b535dcbab2344c7c33eecefc9b31268f6a91c
alexshchur <60445720+alexshchur@users.noreply.github.com>
2024-07-29 06:36:59 +0000

    check if there's a problem with case sensitivity in sql

--- dbt_subprojects/dex/models/_projects/paraswap/avalanche_c/paraswap_v6_avalanche_c_trades_decoded.sql
+++ dbt_subprojects/dex/models/_projects/paraswap/avalanche_c/paraswap_v6_avalanche_c_trades_decoded.sql
@@ -6,7 +6,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.blockTime')],
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'txHash', 'method', 'callTraceAddress']
+    unique_key = ['block_date', 'blockchain', 'project', 'version', 'txhash', 'method', 'calltraceaddress']
     )
 }}
 
