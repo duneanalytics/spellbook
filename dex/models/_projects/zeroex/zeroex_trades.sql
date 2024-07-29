0bae591c8440003a6b0aed2bdd1a55f882225d8e
RantumBits <rantum@rantum.xyz>
2024-07-29 09:07:46 -0700

    remoove polygon files

--- dex/models/_projects/zeroex/zeroex_trades.sql
+++ dex/models/_projects/zeroex/zeroex_trades.sql
@@ -22,7 +22,7 @@ FROM (
     SELECT
       blockchain  as blockchain,
       '0x API'  as project,
-      case when version is null then '1' else version end as version,
+      version as version,
       block_month as block_month,
       block_date  as block_date,
       block_time  as block_time,
