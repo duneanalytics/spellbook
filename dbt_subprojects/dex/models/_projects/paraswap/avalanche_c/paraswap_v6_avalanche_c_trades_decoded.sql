df21200951323e4e8e3648b3d67df90b161e3ba6
alexshchur <60445720+alexshchur@users.noreply.github.com>
2024-07-30 04:40:22 +0000

    remove trace address from decoded trades table unique constraint (tmp/testing)

--- dbt_subprojects/dex/models/_projects/paraswap/avalanche_c/paraswap_v6_avalanche_c_trades_decoded.sql
+++ dbt_subprojects/dex/models/_projects/paraswap/avalanche_c/paraswap_v6_avalanche_c_trades_decoded.sql
@@ -6,8 +6,22 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.blockTime')],
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'call_tx_hash', 'method', 'call_trace_address']
+    unique_key = ['block_date', 'blockchain', 'project', 'version', 'call_tx_hash', 'method']
     )
 }}
+
+
+--   - name: paraswap_v6_avalanche_c_trades_decoded
+--     description: "Paraswap V6 trades decoded"
+--     tests:
+--       - dbt_utils.unique_combination_of_columns:
+--           combination_of_columns:
+--             - block_date
+--             - blockchain
+--             - project
+--             - version
+--             - call_tx_hash
+--             - method
+--             - call_trace_address  
 
 {{ paraswap_v6_trades_master('avalanche_c', 'paraswap') }}
