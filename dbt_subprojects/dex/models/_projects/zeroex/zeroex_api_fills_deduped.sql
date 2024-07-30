9301c46f2319702bc0cba77f76b9751e63121a06
jeff-dude <jeff@dune.xyz>
2024-07-30 12:56:41 -0400

    Revert "[FOR REVIEW] Add ZeroEx settler trades on Polygon (#6424)"
    This reverts commit cebf82cc2e0defcbd8a371be6705cb7ed9909b5f.

--- dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
+++ dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
@@ -19,7 +19,6 @@
   ,ref('zeroex_optimism_api_fills_deduped')
   ,ref('zeroex_polygon_api_fills_deduped')
   ,ref('zeroex_bnb_api_fills_deduped')
-  ,ref('zeroex_polygon_settler_trades')
   ,ref('zeroex_optimism_settler_trades')
   ,ref('zeroex_bnb_settler_trades')
   ,ref('zeroex_avalanche_c_settler_trades')
