d13a861850798715761816467fb659e5e1f43fb6
alexshchur <60445720+alexshchur@users.noreply.github.com>
2024-07-30 05:22:08 +0000

    drop redundant columns from unique key

--- dbt_subprojects/dex/models/_projects/paraswap/avalanche_c/paraswap_v6_avalanche_c_trades_decoded.sql
+++ dbt_subprojects/dex/models/_projects/paraswap/avalanche_c/paraswap_v6_avalanche_c_trades_decoded.sql
@@ -6,7 +6,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.blockTime')],
-    unique_key = ['call_tx_hash', 'method']
+    unique_key = ['call_tx_hash', 'method', 'call_trace_address']
     )
 }}
 
@@ -15,13 +15,9 @@
 --     description: "Paraswap V6 trades decoded"
 --     tests:
 --       - dbt_utils.unique_combination_of_columns:
---           combination_of_columns:
---             - block_date
---             - blockchain
---             - project
---             - version
+--           combination_of_columns:            
 --             - call_tx_hash
 --             - method
---             - call_trace_address  
+--             - call_trace_address        
 
 {{ paraswap_v6_trades_master('avalanche_c', 'paraswap') }}
