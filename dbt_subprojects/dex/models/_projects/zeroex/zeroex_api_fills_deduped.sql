88b6805bbb60961e7ba6489e894cd6fd17237221
RantumBits <rantum@rantum.xyz>
2024-07-31 15:11:26 -0700

    test remove all but settler trades and bnb_fiils

--- dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
+++ dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
@@ -10,15 +10,7 @@
 
 
 {% set zeroex_models = [  
-  ref('zeroex_arbitrum_api_fills_deduped')
-  ,ref('zeroex_avalanche_c_api_fills_deduped')
-  ,ref('zeroex_base_api_fills_deduped')
-  ,ref('zeroex_celo_api_fills_deduped')
-  ,ref('zeroex_ethereum_api_fills_deduped')
-  ,ref('zeroex_fantom_api_fills_deduped')
-  ,ref('zeroex_optimism_api_fills_deduped')
-  ,ref('zeroex_polygon_api_fills_deduped')
-  
+  ref('zeroex_bnb_api_fills_deduped')
   ,ref('zeroex_base_settler_trades')
 
 ] %}
