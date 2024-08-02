8308bb611e3c6c5ad48b88fca37d6ed166bf7f29
alexshchur <60445720+alexshchur@users.noreply.github.com>
2024-08-02 15:53:22 +0000

    reenable paraswap networks after fixing the issue

--- dbt_subprojects/dex/models/_projects/paraswap/paraswap_trades.sql
+++ dbt_subprojects/dex/models/_projects/paraswap/paraswap_trades.sql
@@ -10,6 +10,13 @@
 
 {% set paraswap_models = [
 ref('paraswap_avalanche_c_trades')
+,ref('paraswap_fantom_trades')
+,ref('paraswap_arbitrum_trades')
+,ref('paraswap_bnb_trades')
+,ref('paraswap_ethereum_trades')
+,ref('paraswap_optimism_trades')
+,ref('paraswap_polygon_trades')
+,ref('paraswap_base_trades')
 ] %}
 
 
