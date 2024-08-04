18648e3b14ff5b1e04d0a3ef9f8ef1029053d7d9
darvin <todarvinrio@gmail.com>
2024-08-04 22:10:10 +0530

    patch borrow

--- macros/models/_sector/lending/lending_compound_compatible_borrow.sql
+++ macros/models/_sector/lending/lending_compound_compatible_borrow.sql
@@ -169,42 +169,6 @@ from base_borrow
 
 with
 
-src_Borrow as (
-  {% for src in sources %}
-    select contract_address, src, amount, evt_tx_hash, evt_index, evt_block_time, evt_block_number
-    from {{ source( decoded_project ~ '_' ~ blockchain, src["contract"] ~ '_evt_Withdraw' )}}
-    where evt_tx_hash not in (
-        select evt_tx_hash
-        from {{ source( decoded_project ~ '_' ~ blockchain, src["contract"] ~ '_evt_Transfer' )}}
-        where "to" = 0x0000000000000000000000000000000000000000
-      )
-    {% if is_incremental() %}
-      and {{ incremental_predicate('evt_block_time') }}
-    {% endif %}
-    {% if not loop.last %}
-    union all
-    {% endif %}
-  {% endfor %}
-),
-
-src_Repay as (
-  {% for src in sources %}
-    select contract_address, "from", dst, amount, evt_tx_hash, evt_index, evt_block_time, evt_block_number
-    from {{ source( decoded_project ~ '_' ~ blockchain, src["contract"] ~ '_evt_Supply' )}}
-    where evt_tx_hash not in (
-        select evt_tx_hash
-        from {{ source( decoded_project ~ '_' ~ blockchain, src["contract"] ~ '_evt_Transfer' )}}
-        where "from" = 0x0000000000000000000000000000000000000000
-      )
-    {% if is_incremental() %}
-      and {{ incremental_predicate('evt_block_time') }}
-    {% endif %}
-    {% if not loop.last %}
-    union all
-    {% endif %}
-  {% endfor %}
-),
-
 src_LiquidationCall as (
   {% for src in sources %}
     select contract_address, borrower, absorber, basePaidOut, evt_tx_hash, evt_index, evt_block_time, evt_block_number
@@ -221,35 +185,108 @@ src_LiquidationCall as (
 ctokens as (
   select * from {{ ref( decoded_project ~ '_' ~ blockchain ~ '_ctokens' ) }}
 ),
+base_withdraw_actions as (
+  {% for src in sources %}
+    select 
+      w.contract_address, w.evt_tx_hash, w.evt_index, w.evt_block_time, w.evt_block_number,
+      
+      w.src as withdraw_from,
+      w.to as withdraw_dst,
+      w.amount as withdraw_amt,
+      
+      t."from" as transfer_from,
+      t.to as transfer_to,
+      t.amount as transfer_amt,
+
+      coalesce(least(t.amount,w.amount),0) as amount_withdrawn,
+      w.amount - coalesce(least(t.amount,w.amount),0) as amount_borrowed,
+      case
+          when t.amount is null then 'borrow'
+          when w.amount = t.amount then 'withdraw'
+          when w.amount < t.amount+10 then 'withdraw' -- accuracy fix
+          else 'borrow + withdraw' 
+      end as action
+    from {{ source( decoded_project ~ '_' ~ blockchain, src["contract"] ~ '_evt_Withdraw' )}} w 
+        left join {{ source( decoded_project ~ '_' ~ blockchain, src["contract"] ~ '_evt_Transfer' )}} t 
+            on w.evt_tx_hash = t.evt_tx_hash
+            and w.contract_address = t.contract_address
+            and w.evt_index+1 = t.evt_index
+    {% if is_incremental() %}
+    where {{ incremental_predicate('w.evt_block_time') }}
+    {% endif %}
+    {% if not loop.last %}
+    union all
+    {% endif %}
+  {% endfor %}
+),
+
+base_supply_actions as (
+  {% for src in sources %}
+    select 
+        s.contract_address, s.evt_tx_hash, s.evt_index, s.evt_block_time, s.evt_block_number,
+        
+        s."from" as supply_from,
+        s.dst as supply_dst,
+        s.amount as supply_amt,
+        
+        t."from" as transfer_from,
+        t.to as transfer_to,
+        t.amount as transfer_amt,
+
+        coalesce(t.amount,0) as amount_supplied,
+        s.amount - coalesce(t.amount,0) as amount_repaid,
+        case
+            when t.amount is null then 'repay'
+            when s.amount = t.amount then 'supply'
+            when s.amount < t.amount+10 then 'supply' -- accuracy fix
+            else 'repay + supply' 
+        end as action
+    from {{ source( decoded_project ~ '_' ~ blockchain, src["contract"] ~ '_evt_Supply' )}} s
+        left join {{ source( decoded_project ~ '_' ~ blockchain, src["contract"] ~ '_evt_Transfer' )}} t 
+            on s.evt_tx_hash = t.evt_tx_hash
+            and s.contract_address = t.contract_address
+            and s.evt_index+1 = t.evt_index
+    {% if is_incremental() %}
+    where {{ incremental_predicate('s.evt_block_time') }}
+    {% endif %}
+    {% if not loop.last %}
+    union all
+    {% endif %}
+  {% endfor %}
+),
 
 base_borrow as (
   select
     'borrow' as transaction_type,
     contract_address as comet_contract_address,
-    src as borrower,
+    withdraw_from as borrower,
     cast(null as varbinary) as on_behalf_of,
     cast(null as varbinary) as repayer,
     cast(null as varbinary) as liquidator,
-    cast(amount as double) as amount,
+    cast(amount_borrowed as double) as amount,
     evt_tx_hash,
     evt_index,
     evt_block_time,
     evt_block_number
-  from src_Borrow
+  from base_withdraw_actions
+  where amount_borrowed is not null
+    and amount_borrowed> 0
   union all
   select
     'repay' as transaction_type,
     contract_address as comet_contract_address,
-    "from" as borrower,
+    supply_from as borrower,
     cast(null as varbinary) as on_behalf_of,
-    dst as repayer,
+    supply_dst as repayer,
     cast(null as varbinary) as liquidator,
-    -1 * cast(amount as double) as amount,
+    -1 * cast(amount_repaid as double) as amount,
     evt_tx_hash,
     evt_index,
     evt_block_time,
     evt_block_number
-  from src_Repay
+  from base_supply_actions
+  where amount_repaid is not null
+    and amount_repaid> 0
   union all
   select
     'borrow_liquidation' as transaction_type,
