8c7a859a2fbf3a08e28c19e3726b3692af8b1c74
0xRob <0xRobin5@gmail.com>
2024-08-05 17:18:39 +0200

    optimize incremental setup

--- dbt_subprojects/hourly_spellbook/macros/blockchain_address_metrics.sql
+++ dbt_subprojects/hourly_spellbook/macros/blockchain_address_metrics.sql
@@ -18,8 +18,8 @@ select
     coalesce(o.blockchain, r.blockchain) as blockchain
     ,coalesce(o.address, r.address) as address
     ,coalesce(o.min_block_time, r.min_block_time) as min_block_time
-from {{this}} o
-full join run r
+from run r
+left join {{this}} o
 on r.blockchain = o.blockchain
 and r.address = o.address
 {% else %}
