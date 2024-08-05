c3abcd4818f4960b7f117b55effe58838735e0b5
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-08-04 20:45:48 -0400

    max by

--- dbt_subprojects/solana/models/_sector/staking/solana/staking_solana_validator_stake_account_epochs.sql
+++ dbt_subprojects/solana/models/_sector/staking/solana/staking_solana_validator_stake_account_epochs.sql
@@ -11,35 +11,19 @@
                                 \'["ilemi"]\') }}')
 }}
 
-with 
-    bal as (
-        --get all stake accounts for each epoch and their balance
-        SELECT
-            b.epoch
-            , b.epoch_time
-            , b.epoch_start_slot
-            , b.epoch_end_slot
-            , b.stake_account
-            , b.vote_account
-            , bal.sol_balance
-            , bal.day
-            , row_number() over (partition by b.stake_account order by bal.day desc) as latest_bal
-        FROM {{ ref('staking_solana_validator_stake_account_epochs_raw')}} b
-        LEFT JOIN solana_utils.daily_balances bal ON bal.address = b.stake_account 
-            AND bal.token_mint_address is null
-            AND bal.day <= date_trunc('day', b.epoch_time)
-        {% if is_incremental() %}
-        WHERE {{incremental_predicate('b.epoch_time')}}
-        {% endif %}
-    )
-
-SELECT 
-    epoch
-    , epoch_time
-    , epoch_start_slot
-    , epoch_end_slot
-    , stake_account
-    , vote_account
-    , sol_balance
-FROM bal
-WHERE latest_bal = 1
+SELECT
+    b.epoch
+    , b.epoch_time
+    , b.epoch_start_slot
+    , b.epoch_end_slot
+    , b.stake_account
+    , b.vote_account
+    , max_by(bal.sol_balance, bal.day) as sol_balance
+FROM {{ ref('staking_solana_validator_stake_account_epochs_raw')}} b
+LEFT JOIN solana_utils.daily_balances bal ON bal.address = b.stake_account 
+    AND bal.token_mint_address is null
+    AND bal.day <= date_trunc('day', b.epoch_time)
+{% if is_incremental() %}
+WHERE {{incremental_predicate('b.epoch_time')}}
+{% endif %}
+GROUP BY 1,2,3,4,5,6
