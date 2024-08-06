eb121416463e97c47bd8906b7ad8eb3d450f5af3
danielpartida <partidapd@gmail.com>
2024-08-06 14:51:35 +0200

    Fix if else clauses

--- dbt_subprojects/hourly_spellbook/models/_project/safe/blast/safe_blast_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/blast/safe_blast_transactions.sql
@@ -61,6 +61,5 @@ where bytearray_substring(tr.input, 1, 4) in (
     {% if not is_incremental() %}
     and tr.block_time > TIMESTAMP '{{project_start_date}}' -- for initial query optimisation
     {% else %}
-    {% if is_incremental() %}
     and tr.block_time > date_trunc('day', now() - interval '7' day)
     {% endif %}
