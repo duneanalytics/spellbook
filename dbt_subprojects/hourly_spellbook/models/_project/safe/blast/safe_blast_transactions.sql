e572d7870f5eca2f6260da875efc5abe4f7e75f9
danielpartida <partidapd@gmail.com>
2024-08-06 14:48:34 +0200

    Fix if else clause

--- dbt_subprojects/hourly_spellbook/models/_project/safe/blast/safe_blast_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/blast/safe_blast_transactions.sql
@@ -60,7 +60,7 @@ where bytearray_substring(tr.input, 1, 4) in (
     and tr.call_type = 'delegatecall'
     {% if not is_incremental() %}
     and tr.block_time > TIMESTAMP '{{project_start_date}}' -- for initial query optimisation
-    {% endif %}
+    {% else %}
     {% if is_incremental() %}
     and tr.block_time > date_trunc('day', now() - interval '7' day)
     {% endif %}
