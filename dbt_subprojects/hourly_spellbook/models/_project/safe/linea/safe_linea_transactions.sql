c840beacd2dbf7593089f0d86a6b343b3bc4d547
Daniel <32246390+danielpartida@users.noreply.github.com>
2024-08-07 10:27:27 +0200

    Update dbt_subprojects/hourly_spellbook/models/_project/safe/linea/safe_linea_transactions.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/hourly_spellbook/models/_project/safe/linea/safe_linea_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/linea/safe_linea_transactions.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='incremental',
-
+schema = 'safe_linea',
         alias= 'transactions',
         partition_by = ['block_month'],
         unique_key = ['block_date', 'tx_hash', 'trace_address'],
