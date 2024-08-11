04cf82e418a966e66227a9ef70273d8ac0a40423
Boxer <Floba.hs@gmail.com>
2024-08-11 10:27:57 +0200

    revert changes to dex base trades global and scope down date

--- dbt_subprojects/dex/macros/models/in-flight-decoding/uniswap_v2_pool_mass_decoding.sql
+++ dbt_subprojects/dex/macros/models/in-flight-decoding/uniswap_v2_pool_mass_decoding.sql
@@ -66,7 +66,7 @@ FROM TABLE (
         SELECT l.* 
         FROM {{logs}} l
         WHERE topic0 = 0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822
-        and date_trunc('day', block_time) > now() - interval '10' day
+        and date_trunc('day', block_time) > now() - interval '5' day
       )
     )
   )
