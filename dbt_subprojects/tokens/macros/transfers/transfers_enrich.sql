1bd7329602f32bad6edc6929ec52e5d183f369bd
jeff-dude <jeff@dune.xyz>
2024-08-12 11:05:23 -0400

    partition by month

--- dbt_subprojects/tokens/macros/transfers/transfers_enrich.sql
+++ dbt_subprojects/tokens/macros/transfers/transfers_enrich.sql
@@ -50,6 +50,7 @@ WITH base_transfers as (
     SELECT
         t.unique_key
         , t.blockchain
+        , t.block_month
         , t.block_date
         , t.block_time
         , t.block_number
@@ -96,6 +97,7 @@ WITH base_transfers as (
 SELECT
     unique_key
     , blockchain
+    , block_month
     , block_date
     , block_time
     , block_number
--- dbt_subprojects/tokens/models/transfers_and_balances/gnosis/tokens_gnosis_base_without_suicide_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/gnosis/tokens_gnosis_base_without_suicide_transfers.sql
@@ -1,7 +1,7 @@
 {{config(
     schema = 'tokens_gnosis',
     alias = 'base_wihout_suicide_transfers',
-    partition_by = ['block_date'],
+    partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
