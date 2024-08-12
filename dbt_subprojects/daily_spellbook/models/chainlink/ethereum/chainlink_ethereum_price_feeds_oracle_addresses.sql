c03594bd8183fec650a80cdf8e1205a504ebdc39
tomfutago <tomfutago@gmail.com>
2024-08-12 15:18:55 +0100

    update config

--- dbt_subprojects/daily_spellbook/models/chainlink/ethereum/chainlink_ethereum_price_feeds_oracle_addresses.sql
+++ dbt_subprojects/daily_spellbook/models/chainlink/ethereum/chainlink_ethereum_price_feeds_oracle_addresses.sql
@@ -1,11 +1,11 @@
 {{
   config(
-    
+    schema = 'chainlink_ethereum',
     alias='price_feeds_oracle_addresses',
     post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "chainlink",
-                                \'["linkpool_ryan","linkpool_jon"]\') }}'
+                                \'["linkpool_ryan","linkpool_jon","tomfutago"]\') }}'
   )
 }}
 
