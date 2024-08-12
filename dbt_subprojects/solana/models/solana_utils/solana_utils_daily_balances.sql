b65e7492f786fa4f602c840ca656999e1bf2e48b
jeff-dude <jeff@dune.xyz>
2024-08-12 16:05:17 -0400

    Revert "test: hardcode small time window"
    This reverts commit 7ccb1046494a306655e4784c78ab493b9f797c1e.

--- dbt_subprojects/solana/models/solana_utils/solana_utils_daily_balances.sql
+++ dbt_subprojects/solana/models/solana_utils/solana_utils_daily_balances.sql
@@ -32,7 +32,6 @@ WITH
             WHERE tx_success
             --many writable accounts are included in account activity but don't see any state changes. So this shrinks the table by 70%.
             AND (aa.balance_change != 0 or aa.token_balance_change != 0)
-            AND block_time >= date '2024-08-01'
             {% if is_incremental() %}
             AND block_time >= date_trunc('day', now() - interval '1' day)
             {% endif %}
