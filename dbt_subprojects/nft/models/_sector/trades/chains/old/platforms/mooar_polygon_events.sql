066a08f6ade3131cb8bd12724bf971e9ff20cdf2
0xRob <0xRobin5@gmail.com>
2024-08-13 11:25:25 +0200

    concile seaport macros

--- dbt_subprojects/nft/models/_sector/trades/chains/old/platforms/mooar_polygon_events.sql
+++ dbt_subprojects/nft/models/_sector/trades/chains/old/platforms/mooar_polygon_events.sql
@@ -1,7 +1,7 @@
 {{ config(
     schema = 'mooar_polygon',
     alias = 'events',
-    
+
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
@@ -14,15 +14,13 @@ WITH fee_wallets as (
     ) as foo(wallet_address, wallet_name)
 )
 , trades as (
-    {{ seaport_v4_fork_trades(
+    {{ seaport_v4_trades(
      blockchain = 'polygon'
      ,source_transactions = source('polygon','transactions')
      ,Seaport_evt_OrderFulfilled = source('gashero_polygon','MOOAR_evt_OrderFulfilled')
      ,Seaport_evt_OrdersMatched = source('gashero_polygon','MOOAR_evt_OrdersMatched')
      ,fee_wallet_list_cte = 'fee_wallets'
-     ,native_token_address = '0x0000000000000000000000000000000000000000'
-     ,alternative_token_address = '0x0000000000000000000000000000000000001010'
-     ,native_token_symbol = 'MATIC'
+     ,native_currency_contract = '0x0000000000000000000000000000000000001010'
      ,start_date = '2023-08-18'
      ,Seaport_order_contracts = ['0xaaaaaaaa33d3520a2266ce508bc079fcfe82c8e3']
      ,project = 'mooar'
--- dbt_subprojects/nft/models/_sector/trades/chains/old/platforms/oneplanet_polygon_events.sql
+++ dbt_subprojects/nft/models/_sector/trades/chains/old/platforms/oneplanet_polygon_events.sql
@@ -1,7 +1,7 @@
 {{ config(
     schema = 'oneplanet_polygon',
     alias = 'events',
-    
+
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
@@ -15,17 +15,13 @@ WITH fee_wallets as (
     ) as foo(wallet_address, wallet_name)
 )
 , trades as (
-    {{ seaport_v3_fork_trades(
+    {{ seaport_v3_trades(
      blockchain = 'polygon'
      ,source_transactions = source('polygon','transactions')
      ,Seaport_evt_OrderFulfilled = source('oneplanet_polygon','Seaport_evt_OrderFulfilled')
      ,Seaport_call_matchAdvancedOrders = source('oneplanet_polygon','Seaport_call_matchAdvancedOrders')
      ,Seaport_call_matchOrders = source('oneplanet_polygon','Seaport_call_matchOrders')
      ,fee_wallet_list_cte = 'fee_wallets'
-     ,native_token_address = '0x0000000000000000000000000000000000000000'
-     ,alternative_token_address = '0x0000000000000000000000000000000000001010'
-     ,native_token_symbol = 'MATIC'
-     ,start_date = '2023-09-03'
      ,seaport_fork_address = '0xcbbecf690e030d096794f7685a1bf4a58378a575'
      ,project = 'oneplanet'
      ,version = 'v1'
