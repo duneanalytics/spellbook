094ba314226f83e7605f879cb9c4b65b6d7518fb
0xRob <0xRobin5@gmail.com>
2024-08-14 10:17:37 +0200

    select fixed columns

--- dbt_subprojects/tokens/models/transfers_and_balances/tokens_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/tokens_transfers.sql
@@ -49,7 +49,28 @@
 SELECT *
 FROM (
     {% for chain in chains %}
-    SELECT *
+    SELECT
+          unique_key
+        , blockchain
+        , block_month
+        , block_date
+        , block_time
+        , block_number
+        , tx_hash
+        , evt_index
+        , trace_address
+        , token_standard
+        , tx_from
+        , tx_to
+        , tx_index
+        , "from"
+        , "to"
+        , contract_address
+        , symbol
+        , amount_raw
+        , amount
+        , price_usd
+        , amount_usd
     FROM {{ ref('tokens_'~chain~'_transfers') }}
     {% if not loop.last %}
     UNION ALL
