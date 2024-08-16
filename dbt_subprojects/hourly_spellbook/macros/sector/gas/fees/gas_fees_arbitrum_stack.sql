5fc4828a5cc5e3ee402e8e83c0ff4facc5668467
jeff-dude <jeff@dune.xyz>
2024-08-16 11:37:22 -0400

    format all other macros to match dencun

--- dbt_subprojects/hourly_spellbook/macros/sector/gas/fees/gas_fees_arbitrum_stack.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/gas/fees/gas_fees_arbitrum_stack.sql
@@ -2,32 +2,33 @@
     blockchain = 'arbitrum'
     ,token_symbol = 'ETH') %}
 -- Arbitrum stack chains expose a effective_gas_price column that should be used in the calculations
+
 SELECT
-     '{{blockchain}}' as blockchain,
-     date_trunc('day', block_time) AS block_date,
-     CAST(date_trunc('month', block_time) AS DATE) AS block_month,
-     block_number,
-     block_time,
-     txns.hash AS tx_hash,
-     txns."from" AS tx_sender,
-     txns.to AS tx_receiver,
-     '{{token_symbol}}' as native_token_symbol,
-     value/1e18 AS tx_amount_native,
-     value/1e18 * p.price AS tx_amount_usd,
-     (effective_gas_price / 1e18) * txns.gas_used as tx_fee_native,
-     (effective_gas_price / 1e18) * txns.gas_used * p.price AS tx_fee_usd,
-     txns.effective_gas_price/1e9 as gas_price_gwei,
-     txns.effective_gas_price/1e18 * p.price as gas_price_usd,
-     txns.gas_price/1e9 as gas_price_bid_gwei,
-     txns.gas_price/1e18 * p.price as gas_price_bid_usd,
-     txns.gas_used as gas_used,
-     txns.gas_limit as gas_limit,
-     CASE
+     '{{blockchain}}' as blockchain
+     ,CAST(date_trunc('month', block_time) AS DATE) AS block_month
+     ,CAST(date_trunc('day', block_time) AS DATE) AS block_date
+     ,block_time
+     ,block_number
+     ,txns.hash AS tx_hash
+     ,txns."from" AS tx_sender
+     ,txns.to AS tx_receiver
+     ,'{{token_symbol}}' as native_token_symbol
+     ,value/1e18 AS tx_amount_native
+     ,value/1e18 * p.price AS tx_amount_usd
+     ,(effective_gas_price / 1e18) * txns.gas_used as tx_fee_native
+     ,(effective_gas_price / 1e18) * txns.gas_used * p.price AS tx_fee_usd
+     ,txns.effective_gas_price/1e9 as gas_price_gwei
+     ,txns.effective_gas_price/1e18 * p.price as gas_price_usd
+     ,txns.gas_price/1e9 as gas_price_bid_gwei
+     ,txns.gas_price/1e18 * p.price as gas_price_bid_usd
+     ,txns.gas_used as gas_used
+     ,txns.gas_limit as gas_limit
+     ,CASE
         WHEN txns.gas_limit = 0 THEN NULL
         WHEN txns.gas_limit != 0 THEN txns.gas_used / txns.gas_limit * 100
-     END AS gas_usage_percent,
-     gas_used_for_l1 as l1_gas_used,
-     type as transaction_type
+     END AS gas_usage_percent
+     ,gas_used_for_l1 as l1_gas_used
+     ,type as transaction_type
 FROM {{ source( blockchain, 'transactions') }} txns
 INNER JOIN {{ source( blockchain, 'blocks') }} blocks
     ON txns.block_number = blocks.number
@@ -40,9 +41,12 @@ LEFT JOIN {{ source('prices', 'usd') }} p
     AND p.symbol = '{{token_symbol}}'
     {% if is_incremental() %}
     AND {{ incremental_predicate('p.minute') }}
+    {% else %}
+    AND p.minute >= date '2024-08-01'
     {% endif %}
 {% if is_incremental() %}
 WHERE {{ incremental_predicate('txns.block_time') }}
+{% else %}
+WHERE txns.block_time >= date '2024-08-01'
 {% endif %}
-
 {% endmacro %}
--- dbt_subprojects/hourly_spellbook/macros/sector/gas/fees/gas_fees_op_stack.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/gas/fees/gas_fees_op_stack.sql
@@ -4,28 +4,28 @@
 -- OP stack chains expose a l1_fee column that should be accounted for in the gas fee
 
 SELECT
-     '{{blockchain}}' as blockchain,
-     date_trunc('day', block_time) AS block_date,
-     CAST(date_trunc('month', block_time) AS DATE) AS block_month,
-     block_number,
-     block_time,
-     txns.hash AS tx_hash,
-     txns."from" AS tx_sender,
-     txns.to AS tx_receiver,
-     '{{token_symbol}}' as native_token_symbol,
-     value/1e18 AS tx_amount_native,
-     value/1e18 * p.price AS tx_amount_usd,
-     (l1_fee/1e18 + ((txns.gas_used/1e18) * txns.gas_price)) as tx_fee_native,
-     (l1_fee/1e18 + ((txns.gas_used/1e18) * txns.gas_price)) * p.price AS tx_fee_usd,
-     txns.gas_price/1e9 as gas_price_gwei,
-     txns.gas_price/1e18 * p.price as gas_price_usd,
-     txns.gas_used as gas_used,
-     txns.gas_limit as gas_limit,
-     CASE
+     '{{blockchain}}' as blockchain
+     ,CAST(date_trunc('month', block_time) AS DATE) AS block_month
+     ,CAST(date_trunc('day', block_time) AS DATE) AS block_date
+     ,block_time
+     ,block_number
+     ,txns.hash AS tx_hash
+     ,txns."from" AS tx_sender
+     ,txns.to AS tx_receiver
+     ,'{{token_symbol}}' as native_token_symbol
+     ,value/1e18 AS tx_amount_native
+     ,value/1e18 * p.price AS tx_amount_usd
+     ,(l1_fee/1e18 + ((txns.gas_used/1e18) * txns.gas_price)) as tx_fee_native
+     ,(l1_fee/1e18 + ((txns.gas_used/1e18) * txns.gas_price)) * p.price AS tx_fee_usd
+     ,txns.gas_price/1e9 as gas_price_gwei
+     ,txns.gas_price/1e18 * p.price as gas_price_usd
+     ,txns.gas_used as gas_used
+     ,txns.gas_limit as gas_limit
+     ,CASE
         WHEN txns.gas_limit = 0 THEN NULL
         WHEN txns.gas_limit != 0 THEN txns.gas_used / txns.gas_limit * 100
-     END AS gas_usage_percent,
-     type as transaction_type
+     END AS gas_usage_percent
+     ,type as transaction_type
 FROM {{ source( blockchain, 'transactions') }} txns
 INNER JOIN {{ source( blockchain, 'blocks') }} blocks
     ON txns.block_number = blocks.number
@@ -38,9 +38,12 @@ LEFT JOIN {{ source('prices', 'usd') }} p
     AND p.symbol = '{{token_symbol}}'
     {% if is_incremental() %}
     AND {{ incremental_predicate('p.minute') }}
+    {% else %}
+    AND p.minute >= date '2024-08-01'
     {% endif %}
 {% if is_incremental() %}
 WHERE {{ incremental_predicate('txns.block_time') }}
+{% else %}
+WHERE txns.block_time >= date '2024-08-01'
 {% endif %}
-
 {% endmacro %}
