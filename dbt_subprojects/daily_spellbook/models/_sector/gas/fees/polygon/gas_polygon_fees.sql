df927138e233876127fd51a1f8dc9e40dceacaa6
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-08-16 10:51:57 +0200

    remove weird stuff

--- dbt_subprojects/daily_spellbook/models/_sector/gas/fees/polygon/gas_polygon_fees.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/gas/fees/polygon/gas_polygon_fees.sql
@@ -38,31 +38,7 @@ SELECT
         WHEN txns.gas_limit = 0 THEN NULL
         WHEN txns.gas_limit != 0 THEN txns.gas_used / txns.gas_limit * 100
      END AS gas_usage_percent,
-     l1_gas_price/1e9 as l1_gas_price_gwei,
-     l1_gas_price/1e18 * p.price as l1_gas_price_usd,
-     l1_gas_used,
-     l1_fee_scalar,
-     (l1_gas_price/1e18 )* txns.gas_used as tx_fee_equivalent_on_l1_native,
-     (l1_gas_price/1e18 )* txns.gas_used * p.price as tx_fee_equivalent_on_l1_usd,
-     (length(from_utf8(data)) - length(replace(from_utf8(data), chr(0), ''))) as num_zero_bytes,
-     bytearray_length(data) - (length(from_utf8(data)) - length(replace(from_utf8(data), chr(0), ''))) as num_nonzero_bytes,
-     -- source: https://www.starburst.io/community/forum/t/count-zero-bytes-and-nonzero-bytes-in-a-bytestring-varbinary/261/2
-     16 * (length(from_utf8(data)) - length(replace(from_utf8(data), chr(0), ''))) --16 * nonzero bytes
-     + 4 * ( bytearray_length(data) - (length(from_utf8(data)) - length(replace(from_utf8(data), chr(0), ''))) ) --4 * zero bytes
-     as calldata_gas,
-     type as transaction_type,
-     l1_fee/1e18 AS l1_data_fee_native,
-     p.price * l1_fee/1e18 AS l1_data_fee_usd,
-      --use gas price pre-Bedrock (no base fee)
-     (COALESCE(CAST(blocks.base_fee_per_gas AS UINT256),txns.gas_price)/1e18)*txns.gas_used AS l2_base_fee_native,
-     p.price * (COALESCE(CAST(blocks.base_fee_per_gas AS UINT256),txns.gas_price)/1e18)*txns.gas_used AS l2_base_fee_usd,
-      --base_fee_per_gas was null pre-bedrock when there was no base fee
-     case when (txns.gas_price = UINT256 '0') or (blocks.base_fee_per_gas IS NULL)then 0 else
-        cast( (txns.gas_price-blocks.base_fee_per_gas/ 1e18)*txns.gas_used as double)
-     end AS l2_priority_fee_native,
-     case when (txns.gas_price = UINT256 '0') or (blocks.base_fee_per_gas IS NULL)then 0 else
-        p.price * cast( (txns.gas_price-blocks.base_fee_per_gas/ 1e18)*txns.gas_used as double)
-     end AS l2_priority_fee_usd
+     type as transaction_type
 
 FROM {{ source('polygon','transactions') }} txns
 JOIN {{ source('polygon','blocks') }} blocks ON blocks.number = txns.block_number
