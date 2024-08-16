bc5a8f718d6f48942571ec9c8b1e66666526ec30
jeff-dude <jeff@dune.xyz>
2024-08-16 12:15:21 -0400

    update existing chains

--- dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/gas_fees.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/gas_fees.sql
@@ -1,7 +1,7 @@
 {{ config(
         schema = 'gas',
         alias = 'fees',
-        post_hook='{{ expose_spells(\'["ethereum","bnb","avalanche_c","optimism","arbitrum","zksync","zora","base","scroll","mantle"]\',
+        post_hook='{{ expose_spells(\'["arbitrum","avalanche_c","base","bnb","ethereum","optimism","scroll","zksync","zora"]\',
                                 "sector",
                                 "gas",
                                 \'["soispoke", "ilemi", "0xRob", "jeff-dude"]\') }}'
@@ -9,46 +9,50 @@
 }}
 
 {% set gas_fees_models = [
-    'gas_ethereum_fees',
-    'gas_bnb_fees',
-    'gas_avalanche_c_fees',
-    'gas_optimism_fees',
-    'gas_arbitrum_fees',
-    'gas_zksync_fees',
-    'gas_zora_fees',
-    'gas_base_fees',
-    'gas_scroll_fees'
+    'gas_arbitrum_fees'
+    ,'gas_avalanche_c_fees'
+    ,'gas_base_fees'
+    ,'gas_bnb_fees'
+    ,'gas_ethereum_fees'
+    ,'gas_optimism_fees'
+    ,'gas_scroll_fees'
+    ,'gas_zksync_fees'
+    ,'gas_zora_fees'
 ] %}
 
 --remove mantle for now:
     --'gas_mantle_fees'
 
-SELECT *
-FROM (
+SELECT
+    *
+FROM
+(
     {% for gas_model in gas_fees_models %}
     SELECT
-        blockchain,
-        block_number,
-        block_time,
-        block_date,
-        tx_hash,
-        tx_sender, 
-        tx_receiver,
-        native_token_symbol,
-        tx_amount_native,
-        tx_amount_usd,
-        tx_fee_native,
-        tx_fee_usd,
-        burned_native,
-        burned_usd,
-        validator,
-        gas_price_gwei,
-        gas_price_usd,
-        gas_used,
-        gas_limit,
-        gas_usage_percent,
-        transaction_type
-    FROM {{ ref(gas_model) }}
+        blockchain
+        ,block_month
+        ,block_date
+        ,block_time
+        ,block_number
+        ,tx_hash
+        ,tx_sender
+        ,tx_receiver
+        ,native_token_symbol
+        ,tx_amount_native
+        ,tx_amount_usd
+        ,tx_fee_native
+        ,tx_fee_usd
+        ,burned_native
+        ,burned_usd
+        ,validator
+        ,gas_price_gwei
+        ,gas_price_usd
+        ,gas_used
+        ,gas_limit
+        ,gas_usage_percent
+        ,transaction_type
+    FROM
+        {{ ref(gas_model) }}
     {% if not loop.last %}
     UNION ALL
     {% endif %}
