cde516b67cff13f1655ee72b5863c5e7ef52b595
tomfutago <tomfutago@gmail.com>
2024-08-18 17:11:49 +0100

    change to view, remove dupes

--- dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_ctf_tokens.sql
+++ dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_ctf_tokens.sql
@@ -2,9 +2,7 @@
   config(
     schema = 'polymarket_polygon',
     alias = 'ctf_tokens',
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
+    materialized = 'view',
     unique_key = ['condition_id','token0','token1'],
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
   )
@@ -12,30 +10,29 @@
 
 with ctf_tokens as (
   select
-    evt_block_time as block_time,
-    evt_block_number as block_number,
-    conditionId as condition_id,
-    token0,
-    token1,
-    evt_index,
-    evt_tx_hash as tx_hash
-  from {{ source('polymarket_polygon', 'CTFExchange_evt_TokenRegistered') }}
-  {% if is_incremental() %}
-  where {{ incremental_predicate('evt_block_time') }}
-  {% endif %}
-  union all
-  select
-    evt_block_time as block_time,
-    evt_block_number as block_number,
-    conditionId as condition_id,
-    token0,
-    token1,
-    evt_index,
-    evt_tx_hash as tx_hash
-  from {{ source('polymarket_polygon', 'NegRiskCtfExchange_evt_TokenRegistered') }}
-  {% if is_incremental() %}
-  where {{ incremental_predicate('evt_block_time') }}
-  {% endif %}
+    *,
+    row_number() over (partition by condition_id, token0, token1 order by block_time) as rn
+  from (
+    select
+      evt_block_time as block_time,
+      evt_block_number as block_number,
+      conditionId as condition_id,
+      token0,
+      token1,
+      evt_index,
+      evt_tx_hash as tx_hash
+    from {{ source('polymarket_polygon', 'CTFExchange_evt_TokenRegistered') }}
+    union all
+    select
+      evt_block_time as block_time,
+      evt_block_number as block_number,
+      conditionId as condition_id,
+      token0,
+      token1,
+      evt_index,
+      evt_tx_hash as tx_hash
+    from {{ source('polymarket_polygon', 'NegRiskCtfExchange_evt_TokenRegistered') }}
+  ) t
 )
 
 select
@@ -48,3 +45,4 @@ select
   t.tx_hash
 from ctf_tokens t
   inner join {{ ref('polymarket_polygon_market_conditions') }} mc on t.condition_id = mc.condition_id
+where t.rn = 1
