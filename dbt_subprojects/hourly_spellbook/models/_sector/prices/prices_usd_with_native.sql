a6d7c8373e9e482002a42f68cbb8c58ee44e30dc
0xRob <0xRobin5@gmail.com>
2024-08-21 15:49:11 +0200

    specify join cols

--- dbt_subprojects/hourly_spellbook/models/_sector/prices/prices_usd_with_native.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/prices/prices_usd_with_native.sql
@@ -6,6 +6,20 @@
 -- this is a TEMPORARY spell that should be incorporated in the general prices models.
 -- more discussion here: https://github.com/duneanalytics/spellbook/issues/6577
 
-select * from {{source('prices','usd')}}
+select
+    minute
+    ,blockchain
+    ,contract_address
+    ,decimals
+    ,symbol
+    ,price
+from {{source('prices','usd')}}
 union all
-select * from {{ref('prices_usd_native')}}
+select
+    minute
+    ,blockchain
+    ,contract_address
+    ,decimals
+    ,symbol
+    ,price
+from {{ref('prices_usd_native')}}
