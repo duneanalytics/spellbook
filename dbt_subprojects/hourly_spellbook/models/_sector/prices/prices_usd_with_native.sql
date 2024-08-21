2b1ebeb3ea4e632f6015f8e0c80b8d73791136d1
0xRob <0xRobin5@gmail.com>
2024-08-21 14:51:22 +0200

    fix compile

--- dbt_subprojects/hourly_spellbook/models/_sector/prices/prices_usd_with_native.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/prices/prices_usd_with_native.sql
@@ -6,6 +6,6 @@
 -- this is a TEMPORARY spell that should be incorporated in the general prices models.
 -- more discussion here: https://github.com/duneanalytics/spellbook/issues/6577
 
-select * from {{ref('prices_usd')}}
+select * from {{source('prices','usd')}}
 union all
 select * from {{ref('prices_usd_native')}}
