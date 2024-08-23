bc6c88b69f71a4db5009b35d4812e9072dac1dc2
jeff-dude <jeff@dune.xyz>
2024-08-23 16:24:23 -0400

    initiate new address level metrics

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/blockchain_metrics/ethereum/ethereum_transaction_address_metrics.sql
@@ -0,0 +1,21 @@
+{{ config
+    (
+        schema = 'ethereum',
+        alias = 'transaction_address_metrics',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['blockchain', 'block_hour', 'from', 'to'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_hour')],
+        post_hook = '{{ expose_spells(\'["ethereum"]\',
+                                        "sector",
+                                        "metrics",
+                                        \'["jeff-dude"]\') }}'
+    )
+}}
+
+{{
+    blockchain_transaction_address_metrics(
+        blockchain = 'ethereum'
+    )
+}}
