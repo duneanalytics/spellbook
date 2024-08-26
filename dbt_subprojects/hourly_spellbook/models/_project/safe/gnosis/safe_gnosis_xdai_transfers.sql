3f1e7da919fbf79e292a5bf2c4a7df8873fe54b8
Huang Geyang <Sukebeta@outlook.com>
2024-08-26 21:23:57 +0800

    Apply macro

--- dbt_subprojects/hourly_spellbook/models/_project/safe/gnosis/safe_gnosis_xdai_transfers.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/gnosis/safe_gnosis_xdai_transfers.sql
@@ -1,18 +1,18 @@
 {{ 
     config(
         materialized='incremental',
-        
+        schema='safe_gnosis',
         alias = 'xdai_transfers',
         partition_by = ['block_month'],
         unique_key = ['block_date', 'address', 'tx_hash', 'trace_address'],
         on_schema_change='fail',
         file_format ='delta',
         incremental_strategy='merge',
-        post_hook='{{ expose_spells(\'["gnosis"]\',
-                                    "project",
-                                    "safe",
-                                    \'["tschubotz", "hosuke"]\') }}'
-    ) 
+        post_hook='{{ expose_spells(blockchains = \'["gnosis"]\',
+                                    spell_type = "project",
+                                    spell_name = "safe",
+                                    contributors = \'["tschubotz", "hosuke"]\') }}'
+    )
 }}
 
 {% set project_start_date = '2020-05-21' %}
@@ -71,31 +71,3 @@ from (
     -- to prevent potential counterfactual safe deployment issues we take a bigger interval
     where et.block_time > date_trunc('day', now() - interval '10' day)
     {% endif %}
-
-    union all
-
-    select 
-        'gnosis' as blockchain,
-        'XDAI' as symbol,
-        s.address, 
-        try_cast(date_trunc('day', a.evt_block_time) as date) as block_date,
-        CAST(date_trunc('month', a.evt_block_time) as DATE) as block_month,
-        a.evt_block_time as block_time, 
-        CAST(a.amount AS INT256) as amount_raw,
-        a.evt_tx_hash as tx_hash,
-        cast(a.evt_index as varchar) as trace_address
-    from {{ source('xdai_gnosis', 'BlockRewardAuRa_evt_AddedReceiver') }} a
-    join {{ ref('safe_gnosis_safes') }} s
-        on a.receiver = s.address
-    {% if not is_incremental() %}
-    where a.evt_block_time > TIMESTAMP '{{project_start_date}}' -- for initial query optimisation
-    {% endif %}
-    {% if is_incremental() %}
-    -- to prevent potential counterfactual safe deployment issues we take a bigger interval
-    where a.evt_block_time > date_trunc('day', now() - interval '10' day)
-    {% endif %}
-) t
-
-left join {{ source('prices', 'usd') }} p on p.blockchain is null
-    and p.symbol = t.symbol
-    and p.minute = date_trunc('minute', t.block_time)
