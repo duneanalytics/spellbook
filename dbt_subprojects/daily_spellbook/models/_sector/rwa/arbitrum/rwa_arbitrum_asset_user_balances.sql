693f610caa758d143e69bad2f1bc490feef78cb4
darvin <todarvinrio@gmail.com>
2024-08-26 14:47:31 +0530

    init balances table

---
+++ dbt_subprojects/daily_spellbook/models/_sector/rwa/arbitrum/rwa_arbitrum_asset_user_balances.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    schema = 'rwa_arbitrum',
+    alias = 'asset_user_balances',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['day', 'address', 'token_address'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+  )
+}}
+
+with 
+rwa_assets as (
+  select 
+    'rwa' as category,
+    project,
+    'asset' as version,
+    token_address
+  from {{ref('rwa_arbitrum_assets')}}
+  where type = 'RWA'
+)
+
+{{
+  balances_something_user_daily(
+        balances_daily_agg = ref('tokens_arbitrum_balances_daily_agg'), 
+        something = 'rwa_assets',
+        start_date = '2023-11-17', 
+        native_token='ETH'
+  )
+}}
