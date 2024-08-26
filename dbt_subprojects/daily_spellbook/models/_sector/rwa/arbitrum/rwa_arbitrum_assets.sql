22e1e6bf8b35f9bede8dbb93f2d9ee077575fb46
darvin <todarvinrio@gmail.com>
2024-08-26 14:47:17 +0530

    assets table column name changes

--- dbt_subprojects/daily_spellbook/models/_sector/rwa/arbitrum/rwa_arbitrum_assets.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/rwa/arbitrum/rwa_arbitrum_assets.sql
@@ -12,17 +12,17 @@
 }}
 
 select  
-    token_contract,
+    token_address,
     symbol,
-    protocol,
+    project,
     type
 from (values
     (0x59D9356E565Ab3A36dD77763Fc0d87fEaf85508C, 'USDM'     , 'Mountain Protocol'   , 'RWA'         ),
     (0xfc90518D5136585ba45e34ED5E1D108BD3950CFa, 'USD+'     , 'Dinari'              , 'RWA'         ),
     (0x9d2f299715d94d8a7e6f5eaa8e654e8c74a988a7, 'FXS'      , 'Frax Finance'        , 'Governance'  )
 ) as t(
-    token_contract,
+    token_address,
     symbol,
-    protocol,
+    project,
     type
 )
