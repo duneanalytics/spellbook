599aa252dc484a6c3775573c3d2c469832f9d1e0
AIDataMaster <cryptouserv1@gmail.com>
2024-08-27 20:16:11 +0300

    updated collateral_tokens_data model

--- dbt_subprojects/daily_spellbook/models/_projects/gmx/tokens/arbitrum/gmx_v2_arbitrum_collateral_tokens_data.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/gmx/tokens/arbitrum/gmx_v2_arbitrum_collateral_tokens_data.sql
@@ -6,8 +6,16 @@
     )
 }}
 
-SELECT 
-    contract_address AS collateral_token, 
-    decimals AS collateral_token_decimals
-FROM 
-    {{ source("ai_data_master", "result_token_information_from_arbitrum_api", database="dune") }}
+{%- set url_path = 'https://arbitrum-api.gmxinfra.io/tokens/dune' -%}
+{%- set blockchain_name = 'arbitrum' -%}
+
+SELECT
+    '{{ blockchain_name }}' AS blockchain,
+    'gmx_v2' AS project,
+    json_extract_scalar(token, '$.symbol') AS symbol,
+    from_hex(json_extract_scalar(token, '$.address')) AS contract_address,
+    CAST(json_extract_scalar(token, '$.decimals') AS INTEGER) AS decimals,
+    COALESCE(CAST(json_extract_scalar(token, '$.synthetic') AS BOOLEAN), false) AS synthetic,
+    CURRENT_TIMESTAMP AS last_update_utc
+FROM
+    UNNEST(CAST(json_parse(http_get('{{ url_path }}')) AS ARRAY(JSON))) AS t(token)
