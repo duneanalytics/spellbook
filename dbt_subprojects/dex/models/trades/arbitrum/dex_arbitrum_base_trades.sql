ee21cc237a0e58353b31268f947d60bd94f3eb51
chef-seaweed <121569855+chef-seaweed@users.noreply.github.com>
2024-08-27 16:58:12 +0800

    Add TraderJoe V2.2

--- dbt_subprojects/dex/models/trades/arbitrum/dex_arbitrum_base_trades.sql
+++ dbt_subprojects/dex/models/trades/arbitrum/dex_arbitrum_base_trades.sql
@@ -15,6 +15,7 @@
     , ref('arbswap_arbitrum_base_trades')
     , ref('trader_joe_v2_arbitrum_base_trades')
     , ref('trader_joe_v2_1_arbitrum_base_trades')
+    , ref('trader_joe_v2_2_arbitrum_base_trades')
     , ref('pancakeswap_v2_arbitrum_base_trades')
     , ref('pancakeswap_v3_arbitrum_base_trades')
     , ref('balancer_v2_arbitrum_base_trades')
