bb737732c9da9f391dd8a748b7ff5af1d3d7f7db
0xRob <0xRobin5@gmail.com>
2024-08-27 15:12:07 +0200

    run full history in CI (will probably timeout)

--- dbt_subprojects/hourly_spellbook/macros/sector/gas/fees/gas_fees.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/gas/fees/gas_fees.sql
@@ -120,9 +120,6 @@ WITH base_model as (
     {%- endif -%}
     {% if is_incremental() %}
     WHERE {{ incremental_predicate('txns.block_time') }}
-    {% else %}
-    WHERE txns.block_time >= date '2024-08-10'
-    OR txns.hash in (select tx_hash from {{ref('evm_gas_fees')}})
     {% endif %}
     )
 
