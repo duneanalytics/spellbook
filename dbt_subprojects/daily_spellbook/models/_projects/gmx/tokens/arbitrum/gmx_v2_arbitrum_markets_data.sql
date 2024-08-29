f5ab413b3ce40943c88058e7cb150c7b26088151
AIDataMaster <cryptouserv1@gmail.com>
2024-08-29 13:07:59 +0300

    Fixed error in markets_data model

--- dbt_subprojects/daily_spellbook/models/_projects/gmx/tokens/arbitrum/gmx_v2_arbitrum_markets_data.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/gmx/tokens/arbitrum/gmx_v2_arbitrum_markets_data.sql
@@ -13,8 +13,8 @@ SELECT
     ERC20_ST.decimals AS short_token_decimals  
 FROM {{ ref('gmx_v2_arbitrum_market_created') }} AS MCE
 LEFT JOIN {{ ref('gmx_v2_arbitrum_erc20') }} AS ERC20_IT
-    ON ERC20_IT.contract_address = MCE.index_token
+    ON TRY_CAST(ERC20_IT.contract_address AS VARCHAR)
 LEFT JOIN {{ ref('gmx_v2_arbitrum_erc20') }} AS ERC20_LT
-    ON ERC20_LT.contract_address = MCE.long_token 
+    ON TRY_CAST(ERC20_LT.contract_address AS VARCHAR) = MCE.long_token 
 LEFT JOIN {{ ref('gmx_v2_arbitrum_erc20') }} AS ERC20_ST
-    ON ERC20_ST.contract_address = MCE.short_token
+    ON TRY_CAST(ERC20_ST.contract_address AS VARCHAR) = MCE.short_token
