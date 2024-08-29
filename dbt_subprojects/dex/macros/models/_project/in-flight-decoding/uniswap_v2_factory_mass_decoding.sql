7d6e02c7a7949a6a4c0f19722100a9a0efa9cad7
Boxer <Floba.hs@gmail.com>
2024-08-29 11:47:50 +0200

    make abi fetching dynamic

--- dbt_subprojects/dex/macros/models/_project/in-flight-decoding/uniswap_v2_factory_mass_decoding.sql
+++ dbt_subprojects/dex/macros/models/_project/in-flight-decoding/uniswap_v2_factory_mass_decoding.sql
@@ -1,8 +1,28 @@
+
+
+
+
+
+{% macro get_abi(signature_id) %}
+    {% set abi_query %}
+        SELECT abi FROM abi.signatures WHERE id = '{{ signature_id }}'
+    {% endset %}
+    {% set results = run_query(abi_query) %}
+    {% if execute %}
+        {% set abi = results.columns[0].values()[0] %}
+        {{ return(abi) }}
+    {% else %}
+        {{ return('') }}
+    {% endif %}
+{% endmacro %}
+
 {% macro uniswap_v2_factory_mass_decoding(
-    logs = null
+    logs = null,
+    signature_id = '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9'
     )
 %}
 
+{% set abi = get_abi(signature_id) %}
 
 SELECT
 token0
@@ -15,47 +35,17 @@ token0
 ,tx_hash
 ,index
 
--- could grab abi from our database instead to make this more dynamic
 FROM TABLE (
     decode_evm_event (
-      abi => '{
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "token0",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "token1",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "address",
-                "name": "pair",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "PairCreated",
-        "type": "event"
-    }',
+      abi => '{{ abi }}',
       input => TABLE (
         SELECT l.* 
         FROM {{logs}} l
-        WHERE topic0 = 0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9
-        and block_date > (Select min(block_date) from {{logs}} where topic0 = 0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9) -- take out limit if you want to use in prod
+        WHERE topic0 = '{{signature_id}}'
+        and block_date > (Select min(block_date) from {{logs}} where topic0 = '{{signature_id}}') -- take out limit if you want to use in prod
       )
     )
   )
 
 {% endmacro %}
+
