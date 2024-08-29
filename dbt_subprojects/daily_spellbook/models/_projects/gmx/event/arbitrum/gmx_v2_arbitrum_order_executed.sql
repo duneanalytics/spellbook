fdc48319b8525b1693cc1c9e3eb393dbff5e4a16
AIDataMaster <cryptouserv1@gmail.com>
2024-08-29 12:04:34 +0300

    Added OrderExecuted Event Model

---
+++ dbt_subprojects/daily_spellbook/models/_projects/gmx/event/arbitrum/gmx_v2_arbitrum_order_executed.sql
@@ -0,0 +1,153 @@
+{{
+  config(
+    schema = 'gmx_v2_arbitrum',
+    alias = 'order_executed',
+    materialized = 'table'
+    )
+}}
+
+{%- set event_name = 'OrderExecuted' -%}
+{%- set blockchain_name = 'arbitrum' -%}
+
+WITH evt_data_1 AS (
+    SELECT 
+        -- Main Variables
+        '{{ blockchain_name }}' AS blockchain,
+        evt_block_time AS block_time,
+        evt_block_number AS block_number, 
+        evt_tx_hash AS tx_hash,
+        evt_index AS index,
+        contract_address,
+        eventName AS event_name,
+        eventData AS data,
+        msgSender AS msg_sender,
+        topic1,
+        CAST(NULL AS varbinary) AS topic2  -- Ensure topic2 is treated as varbinary
+    FROM {{ source('gmx_v2_arbitrum','EventEmitter_evt_EventLog1')}}
+    WHERE eventName = '{{ event_name }}'
+    ORDER BY evt_block_time ASC
+)
+
+, evt_data_2 AS (
+    SELECT 
+        -- Main Variables
+        'arbitrum' AS blockchain,
+        evt_block_time AS block_time,
+        evt_block_number AS block_number, 
+        evt_tx_hash AS tx_hash,
+        evt_index AS index,
+        contract_address,
+        eventName AS event_name,
+        eventData AS data,
+        msgSender AS msg_sender,
+        topic1,
+        topic2
+    FROM {{ source('gmx_v2_arbitrum','EventEmitter_evt_EventLog2')}}
+    WHERE eventName = '{{ event_name }}'
+    ORDER BY evt_block_time ASC
+)
+
+-- unite 2 tables
+, evt_data AS (
+    SELECT * 
+    FROM evt_data_1
+    UNION
+    SELECT *
+    FROM evt_data_2
+)
+
+, parsed_data AS (
+    SELECT
+        tx_hash,
+        index, 
+        json_query(data, 'lax $.addressItems' OMIT QUOTES) AS address_items,
+        json_query(data, 'lax $.uintItems' OMIT QUOTES) AS uint_items,
+        json_query(data, 'lax $.bytes32Items' OMIT QUOTES) AS bytes32_items
+    FROM
+        evt_data
+)
+
+, address_items_parsed AS (
+    SELECT 
+        tx_hash,
+        index,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.key') AS key_name,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.value') AS value
+    FROM 
+        parsed_data,
+        UNNEST(
+            CAST(json_extract(address_items, '$.items') AS ARRAY(JSON))
+        ) AS t(item)
+)
+
+, uint_items_parsed AS (
+    SELECT 
+        tx_hash,
+        index,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.key') AS key_name,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.value') AS value
+    FROM 
+        parsed_data,
+        UNNEST(
+            CAST(json_extract(uint_items, '$.items') AS ARRAY(JSON))
+        ) AS t(item)
+)
+
+, bytes32_items_parsed AS (
+    SELECT 
+        tx_hash,
+        index,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.key') AS key_name,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.value') AS value
+    FROM 
+        parsed_data,
+        UNNEST(
+            CAST(json_extract(bytes32_items, '$.items') AS ARRAY(JSON))
+        ) AS t(item)
+)
+
+, combined AS (
+    SELECT *
+    FROM address_items_parsed
+    UNION ALL    
+    SELECT *
+    FROM uint_items_parsed
+    UNION ALL
+    SELECT *
+    FROM bytes32_items_parsed
+
+)
+
+, evt_data_parsed AS (
+    SELECT
+        tx_hash,
+        index,
+        MAX(CASE WHEN key_name = 'key' THEN value END) AS key,
+        MAX(CASE WHEN key_name = 'account' THEN value END) AS account,
+        MAX(CASE WHEN key_name = 'secondaryOrderType' THEN value END) AS secondary_order_type
+    FROM
+        combined
+    GROUP BY tx_hash, index
+)
+
+-- full data 
+SELECT 
+    blockchain,
+    block_time,
+    block_number,
+    ED.tx_hash,
+    ED.index,
+    contract_address,
+    event_name,
+    msg_sender,
+    topic1, 
+    topic2,
+    
+    key,
+    account,
+    TRY_CAST(secondary_order_type AS INTEGER) AS secondary_order_type
+
+FROM evt_data AS ED
+LEFT JOIN evt_data_parsed AS EDP
+    ON ED.tx_hash = EDP.tx_hash
+        AND ED.index = EDP.index
