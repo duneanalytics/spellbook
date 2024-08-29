bdbd60e2fcce7610cfe86bb1686879bb88e77f3a
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-08-29 13:27:09 -0400

    DUX-621 initiate new address level metrics (#6603)
    * initiate new address level metrics
    
    * try quotes
    
    * update col names
    
    * add all EVM chains
    
    * remove incr on contract creation, fix min block hour
    
    * run full history

--- dbt_subprojects/hourly_spellbook/models/_sector/blockchain_metrics/evms_transaction_address_metrics.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/blockchain_metrics/evms_transaction_address_metrics.sql
@@ -1,10 +1,10 @@
 {{ config(
         schema='evms',
-        alias = 'transaction_metrics',
+        alias = 'transaction_address_metrics',
         post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "blast", "bnb", "celo", "ethereum", "fantom", "gnosis", "linea", "mantle", "optimism", "polygon", "scroll", "sei", "zkevm", "zksync", "zora"]\',
                                     "sector",
                                     "metrics",
-                                    \'["0xRob"]\') }}'
+                                    \'["jeff-dude"]\') }}'
         )
 }}
 
@@ -33,17 +33,18 @@ SELECT *
 FROM (
     {% for chain in chains %}
     SELECT
-         blockchain
-        ,chain_id
-        ,block_hour
-        ,tx_count
-        ,tx_success_count
-        ,tx_success_rate
-        ,avg_block_time_seconds
-        ,tx_per_second
-        ,new_addresses
-        ,new_contracts
-    FROM {{ ref(chain ~ '_transaction_metrics') }}
+        blockchain
+        , chain_id
+        , block_hour
+        , from_address
+        , to_address
+        , tx_count
+        , tx_success_rate
+        , from_is_new_address
+        , from_is_contract
+        , to_is_new_address
+        , to_is_contract
+    FROM {{ ref(chain ~ '_transaction_address_metrics') }}
     {% if not loop.last %}
     UNION ALL
     {% endif %}
