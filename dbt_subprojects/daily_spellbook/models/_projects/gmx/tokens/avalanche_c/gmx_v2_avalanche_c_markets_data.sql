3a24cb660a3a80c21404e96d82cd05b05b31044c
AIDataMaster <cryptouserv1@gmail.com>
2024-08-30 22:57:44 +0300

    Fixed error in column names in markets_data model

--- dbt_subprojects/daily_spellbook/models/_projects/gmx/tokens/avalanche_c/gmx_v2_avalanche_c_markets_data.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/gmx/tokens/avalanche_c/gmx_v2_avalanche_c_markets_data.sql
@@ -11,11 +11,11 @@ SELECT
     CONCAT(ERC20_IT.index_token, '/USD [', ERC20_LT.long_token, '-', ERC20_ST.short_token, ']') AS market_name,
     'GM' AS market_token_symbol,
     18 AS market_token_decimals,
-    ERC20_IT.index_token AS index_token_symbol,
+    ERC20_IT.symbol AS index_token_symbol,
     ERC20_IT.decimals AS index_token_decimals,
-    ERC20_LT.long_token AS long_token_symbol,
+    ERC20_LT.symbol AS long_token_symbol,
     ERC20_LT.decimals AS long_token_decimals,
-    ERC20_ST.short_token AS short_token_symbol,
+    ERC20_ST.symbol AS short_token_symbol,
     ERC20_ST.decimals AS short_token_decimals 
 FROM {{ ref('gmx_v2_avalanche_c_market_created') }} AS MCE
 LEFT JOIN {{ ref('gmx_v2_avalanche_c_erc20') }} AS ERC20_IT
