d7ea43fc04b4648aeb37bfc0c1092c6ffcd23248
Boxer <Floba.hs@gmail.com>
2024-08-30 12:37:18 +0200

    construct full lineage

---
+++ dbt_subprojects/dex/models/_log_decoded_trades/log_decoded_dex_trades.sql
@@ -0,0 +1,45 @@
+{{ config(
+    schema = 'dex'
+    , alias = 'log_decoded_trades'
+    , partition_by = ['block_month', 'blockchain', 'project']
+    , materialized = 'incremental'
+    , file_format = 'delta'
+    , incremental_strategy = 'merge'
+    , unique_key = ['blockchain', 'project', 'version', 'tx_hash', 'evt_index']
+    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    , post_hook='{{ expose_spells(\'[
+                                        "arbitrum"
+                                        , "avalanche_c"
+                                        , "base"
+                                        , "blast"
+                                        , "bnb"
+                                        , "celo"
+                                        , "ethereum"
+                                        , "fantom"
+                                        , "gnosis"
+                                        , "linea"
+                                        , "mantle"
+                                        , "nova"
+                                        , "optimism"
+                                        , "polygon"
+                                        , "scroll"
+                                        , "sei"
+                                        , "zkevm"
+                                        , "zksync"
+                                        , "zora"
+                                    ]\',
+                                    "sector",
+                                    "dex",
+                                    \'["hosuke", "0xrob", "jeff-dude", "tomfutago"]\') }}')
+}}
+
+with dexs AS (
+    {{
+        enrich_dex_trades(
+            base_trades = ref('log_decoded_dex_base_trades')
+            , filter = "project != 'curve'"
+            , tokens_erc20_model = source('tokens', 'erc20')
+        )
+    }}
+)
+
