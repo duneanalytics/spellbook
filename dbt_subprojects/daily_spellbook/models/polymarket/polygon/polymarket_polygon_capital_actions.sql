0b5df2b3ce2881396d7bb036c64f2d6c1994baec
tomfutago <tomfutago@gmail.com>
2024-08-30 14:48:31 +0100

    deposits, withdrawals AND transfers..

--- dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_capital_actions.sql
+++ dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_capital_actions.sql
@@ -35,6 +35,7 @@ select
 from {{ source('tokens_polygon', 'transfers')}}
 where contract_address = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174 -- USDC
   and "to" in (select proxy from safe_proxies)
+  and "from" not in (select proxy from safe_proxies)
   {% if is_incremental() %}
   and {{ incremental_predicate('block_time') }}
   {% endif %}
@@ -56,6 +57,29 @@ select
 from {{ source('tokens_polygon', 'transfers')}}
 where contract_address = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174 -- USDC
   and "from" in (select proxy from safe_proxies)
+  and "to" not in (select proxy from safe_proxies)
+  {% if is_incremental() %}
+  and {{ incremental_predicate('block_time') }}
+  {% endif %}
+
+union all
+
+select distinct
+  block_time,
+  block_number,
+  'transfer' as action, -- transfer between safes
+  "from" as from_address,
+  "to" as to_address,
+  symbol,
+  amount_raw,
+  amount,
+  amount_usd,
+  evt_index,
+  tx_hash
+from {{ source('tokens_polygon', 'transfers')}}
+where contract_address = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174 -- USDC
+  and "from" in (select proxy from safe_proxies)
+  and "to" in (select proxy from safe_proxies)
   {% if is_incremental() %}
   and {{ incremental_predicate('block_time') }}
   {% endif %}
