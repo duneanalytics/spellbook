411b9e3908ee87c8483ad6a9bd7410ad5fc0edf7
AIDataMaster <cryptouserv1@gmail.com>
2024-08-30 20:10:50 +0300

    Added all other events for cross-chain spellbooks

---
+++ dbt_subprojects/daily_spellbook/models/_projects/gmx/event/gmx_v2_order_executed.sql
@@ -0,0 +1,22 @@
+{{ config(
+        schema='gmx_v2',
+        alias = 'order_executed',
+        post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c"]\',
+                                    "project",
+                                    "gmx",
+                                    \'["ai_data_master","gmx-io"]\') }}'
+        )
+}}
+
+{%- set chains = [
+    'arbitrum',
+    'avalanche_c',
+] -%}
+
+{%- for chain in chains -%}
+SELECT *
+FROM {{ ref('gmx_v2_' ~ chain ~ '_order_executed') }}
+{% if not loop.last %}
+UNION ALL
+{% endif %}
+{%- endfor -%}
---
+++ dbt_subprojects/daily_spellbook/models/_projects/gmx/event/gmx_v2_position_decrease.sql
@@ -0,0 +1,22 @@
+{{ config(
+        schema='gmx_v2',
+        alias = 'position_decrease',
+        post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c"]\',
+                                    "project",
+                                    "gmx",
+                                    \'["ai_data_master","gmx-io"]\') }}'
+        )
+}}
+
+{%- set chains = [
+    'arbitrum',
+    'avalanche_c',
+] -%}
+
+{%- for chain in chains -%}
+SELECT *
+FROM {{ ref('gmx_v2_' ~ chain ~ '_position_decrease') }}
+{% if not loop.last %}
+UNION ALL
+{% endif %}
+{%- endfor -%}
