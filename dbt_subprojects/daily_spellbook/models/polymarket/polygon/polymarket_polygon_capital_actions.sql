6a81776da5d77f33899bb6f48f1eeed0767bb85e
tomfutago <tomfutago@gmail.com>
2024-08-30 12:14:35 +0100

    add safe proxies, update user captial actions

--- dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_capital_actions.sql
+++ dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_capital_actions.sql
@@ -14,12 +14,18 @@
   )
 }}
 
+with
+
+safe_proxies as (
+  select * from {{ ref('polymarket_polygon_safe_proxies') }}
+)
+
 select
   block_time,
   block_number,
   'deposit' as action,
   "from" as from_address,
-  to as to_address,
+  "to" as to_address,
   symbol,
   amount_raw,
   amount,
@@ -28,10 +34,7 @@ select
   tx_hash
 from {{ source('tokens_polygon', 'transfers')}}
 where contract_address = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174 -- USDC
-  and to in (
-    0x4bfb41d5b3570defd03c39a9a4d8de6bd8b8982e, -- CTF Exchange
-    0xc5d563a36ae78145c45a50134d48a1215220f80a  -- NegRisk CTF Exchange
-  )
+  and "to" in (select proxy from safe_proxies)
   {% if is_incremental() %}
   and {{ incremental_predicate('block_time') }}
   {% endif %}
@@ -43,7 +46,7 @@ select
   block_number,
   'withdrawal' as action,
   "from" as from_address,
-  to as to_address,
+  "to" as to_address,
   symbol,
   amount_raw,
   amount,
@@ -52,10 +55,7 @@ select
   tx_hash
 from {{ source('tokens_polygon', 'transfers')}}
 where contract_address = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174 -- USDC
-  and "from" in (
-    0x4bfb41d5b3570defd03c39a9a4d8de6bd8b8982e, -- CTF Exchange
-    0xc5d563a36ae78145c45a50134d48a1215220f80a  -- NegRisk CTF Exchange
-  )
+  and "from" in (select proxy from safe_proxies)
   {% if is_incremental() %}
   and {{ incremental_predicate('block_time') }}
   {% endif %}
---
+++ dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_safe_proxies.sql
@@ -0,0 +1,27 @@
+{{
+  config(
+    schema = 'polymarket_polygon',
+    alias = 'safe_proxies',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['proxy'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    post_hook = '{{ expose_spells(blockchains = \'["polygon"]\',
+                                  spell_type = "project",
+                                  spell_name = "polymarket",
+                                  contributors = \'["tomfutago"]\') }}'
+  )
+}}
+
+select
+  evt_block_time as block_time,
+  evt_block_number as block_number,
+  owner,
+  proxy,
+  evt_index,
+  evt_tx_hash as tx_hash
+from {{ source('polymarket_polygon', 'SafeProxyFactory_evt_ProxyCreation') }}
+{% if is_incremental() %}
+and {{ incremental_predicate('evt_block_time') }}
+{% endif %}
