79519af44cdece83b67d001551ef000ab0718a74
Boxer <Floba.hs@gmail.com>
2024-09-01 17:13:39 +0200

    add wallet proxies and filter internal addresses

--- dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_capital_actions.sql
+++ dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_capital_actions.sql
@@ -17,11 +17,25 @@
 with
 
 safe_proxies as (
-  select * from {{ ref('polymarket_polygon_safe_proxies') }}
+  select proxy from {{ ref('polymarket_polygon_safe_proxies') }}
+  UNION ALL 
+  select proxy from {{ ref('polymarket_polygon_magic_wallet_proxies') }}
+),
+
+polymarket_addresses as (
+  select * from (values 
+    (0x4D97DCd97eC945f40cF65F87097ACe5EA0476045), -- Conditional Tokens
+    (0x3A3BD7bb9528E159577F7C2e685CC81A765002E2), -- Wrapped Collateral
+    (0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E), -- CTFExchange
+    (0xC5d563A36AE78145C45a50134d48A1215220f80a), -- NegRiskCTFExchange
+    (0xc288480574783BD7615170660d71753378159c47),  -- Polymarket Rewards
+    (0x94a3db2f861b01c027871b08399e1ccecfc847f6)   -- liq mining merkle distributor
+  ) as t(address)
 )
 
 select
   block_time,
+  block_date,
   block_number,
   'deposit' as action,
   "from" as from_address,
@@ -36,6 +50,8 @@ from {{ source('tokens_polygon', 'transfers')}}
 where contract_address = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174 -- USDC
   and "to" in (select proxy from safe_proxies)
   and "from" not in (select proxy from safe_proxies)
+  and "to" not in (select address from polymarket_addresses)
+  and "from" not in (select address from polymarket_addresses)
   {% if is_incremental() %}
   and {{ incremental_predicate('block_time') }}
   {% endif %}
@@ -44,6 +60,7 @@ union all
 
 select
   block_time,
+  block_date,
   block_number,
   'withdrawal' as action,
   "from" as from_address,
@@ -58,6 +75,8 @@ from {{ source('tokens_polygon', 'transfers')}}
 where contract_address = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174 -- USDC
   and "from" in (select proxy from safe_proxies)
   and "to" not in (select proxy from safe_proxies)
+  and "to" not in (select address from polymarket_addresses)
+  and "from" not in (select address from polymarket_addresses)
   {% if is_incremental() %}
   and {{ incremental_predicate('block_time') }}
   {% endif %}
@@ -66,6 +85,7 @@ union all
 
 select distinct
   block_time,
+  block_date,
   block_number,
   'transfer' as action, -- transfer between safes
   "from" as from_address,
@@ -80,6 +100,8 @@ from {{ source('tokens_polygon', 'transfers')}}
 where contract_address = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174 -- USDC
   and "from" in (select proxy from safe_proxies)
   and "to" in (select proxy from safe_proxies)
+  and "to" not in (select address from polymarket_addresses)
+  and "from" not in (select address from polymarket_addresses)
   {% if is_incremental() %}
   and {{ incremental_predicate('block_time') }}
   {% endif %}
---
+++ dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_magic_wallet_proxies.sql
@@ -0,0 +1,30 @@
+{{
+  config(
+    schema = 'polymarket_polygon',
+    alias = 'magic_wallet_proxies',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['proxy'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    post_hook = '{{ expose_spells(blockchains = \'["polygon"]\',
+                                  spell_type = "project",
+                                  spell_name = "polymarket",
+                                  contributors = \'["tomfutago"]\') }}'
+  )
+}}
+
+select
+  block_time,
+  block_number,
+  null as owner,
+  address as proxy,
+  tx_hash
+from polygon.creation_traces
+where "from" = 0xaB45c5A4B0c941a2F231C04C3f49182e1A254052
+{% if is_incremental() %}
+  and {{ incremental_predicate('evt_block_time') }}
+{% endif %}
+
+
+
