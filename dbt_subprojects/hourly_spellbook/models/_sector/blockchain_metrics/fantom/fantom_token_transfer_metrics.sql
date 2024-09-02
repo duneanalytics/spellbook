263f55675b7e3c1682c79028cf4d14bea0bc715b
0xRob <0xRobin5@gmail.com>
2024-09-02 10:39:23 +0200

    add files

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/blockchain_metrics/fantom/fantom_token_transfer_metrics.sql
@@ -0,0 +1,15 @@
+{{ config(
+        schema='fantom',
+        alias = 'token_transfer_metrics',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['blockchain','block_hour', 'token_address'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_hour')],
+        post_hook='{{ expose_spells(\'["fantom"]\',
+                "sector",
+                "metrics",
+                \'["0xRob"]\') }}')
+}}
+
+{{blockchain_token_transfer_metrics('fantom')}}
---
+++ dbt_subprojects/hourly_spellbook/models/_sector/blockchain_metrics/mantle/mantle_token_transfer_metrics.sql
@@ -0,0 +1,15 @@
+{{ config(
+        schema='mantle',
+        alias = 'token_transfer_metrics',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['blockchain','block_hour', 'token_address'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_hour')],
+        post_hook='{{ expose_spells(\'["mantle"]\',
+                "sector",
+                "metrics",
+                \'["0xRob"]\') }}')
+}}
+
+{{blockchain_token_transfer_metrics('mantle')}}
