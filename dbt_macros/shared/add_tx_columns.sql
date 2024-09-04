b25516013edf0140a503feeca2d3d66a4e932021
Huang Geyang <Sukebeta@outlook.com>
2024-09-05 00:28:30 +0800

    Recover add_tx_columns

--- dbt_macros/shared/add_tx_columns.sql
+++ dbt_macros/shared/add_tx_columns.sql
@@ -20,38 +20,3 @@ inner join {{source(blockchain, 'transactions')}} tx
     and {{incremental_predicate('tx.block_time')}}
     {% endif %}
 {% endmacro %}
-
-##########################################################################################
-#### multichain beta #####################################################################
-##########################################################################################
-
-{% macro add_tx_columns_multichain(
-    model_cte
-    , blockchains
-    , columns = []
-    )
-%}
-
-{% for blockchain in blockchains %}
-select
-    model.*
-    {% for column in columns %}
-    , tx."{{column}}" as tx_{{column}}
-    {% endfor %}
-from (
-    select * from {{model_cte}}
-    where blockchain = '{{blockchain}}'
-) model
-inner join {{source(blockchain, 'transactions')}} tx
-    on
-    model.block_date = tx.block_date
-    and model.block_number = tx.block_number
-    and model.tx_hash = tx.hash
-    {% if is_incremental() %}
-    and {{incremental_predicate('tx.block_time')}}
-    {% endif %}
-{% if not loop.last %}
-UNION ALL
-{% endif %}
-{% endfor %}
-{% endmacro %}
