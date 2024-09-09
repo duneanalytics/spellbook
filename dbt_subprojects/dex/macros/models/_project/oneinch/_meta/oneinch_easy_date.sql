952a859d20db358f0329ae72193359313cf20308
grkhr <eduardgorkh@gmail.com>
2024-09-09 16:29:55 +0300

    Update oneinch_easy_date.sql

--- dbt_subprojects/dex/macros/models/_project/oneinch/_meta/oneinch_easy_date.sql
+++ dbt_subprojects/dex/macros/models/_project/oneinch/_meta/oneinch_easy_date.sql
@@ -1,3 +1,5 @@
+-- for CI purposes to manage from one place
+
 {% macro oneinch_easy_date() %}
-   {{ return("2024-09-06") }} -- "2019-06-01"
+   {{ return("'2024-09-06'") }} -- "'2019-06-01'"
 {% endmacro %}
