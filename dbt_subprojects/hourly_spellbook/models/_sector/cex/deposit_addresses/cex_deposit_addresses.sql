d5f76f836a4dd28b8eebebd92246ecaffa14f75b
Hildebert Moulié <h.moulie@gmail.com>
2024-09-13 18:08:25 +0200

    fix

--- dbt_subprojects/hourly_spellbook/models/_sector/cex/deposit_addresses/cex_deposit_addresses.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/cex/deposit_addresses/cex_deposit_addresses.sql
@@ -100,16 +100,6 @@ WITH new AS (
     HAVING COUNT(DISTINCT cex_name) = 1
     )
 
-SELECT address
-, MAX(cex_name) AS cex_name
-, array_agg(blockchain) AS blockchains
-, MIN_BY(blockchain, creation_block_time) AS first_used_blockchain
-, MIN(creation_block_time) AS creation_block_time
-, MIN(creation_block_number) AS creation_block_number
-, MIN_BY(funded_by_same_cex, creation_block_time) AS funded_by_same_cex
-, MIN_BY(first_funded_by, creation_block_time) AS first_funded_by
-, MIN_BY(is_smart_contract, creation_block_time) AS is_smart_contract
-
 SELECT n.address
 , MAX(n.cex_name) AS cex_name
 , array_union(MAX(t.blockchains), array_agg(n.blockchains)) AS blockchains
