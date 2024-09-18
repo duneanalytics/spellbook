2590b6d7aacc07a578c6168f30382ad0e1532b41
mhmdriyadi <112420584+mhmdriyadi@users.noreply.github.com>
2024-09-19 00:56:58 +0700

    Update prices_bnb_tokens.sql
    Add COCO token price

--- dbt_subprojects/tokens/models/prices/bnb/prices_bnb_tokens.sql
+++ dbt_subprojects/tokens/models/prices/bnb/prices_bnb_tokens.sql
@@ -276,7 +276,8 @@ FROM
     ('bwjup-bsc-wrapped-jupiter', 'bnb', 'bwJUP', 0x0231f91e02debd20345ae8ab7d71a41f8e140ce7, 18),
     ('lovely-lovely-inu-finance', 'bnb', 'LOVELY', 0x9e24415d1e549ebc626a13a482bb117a2b43e9cf, 8),
     --('pvc-pvc-meta','bnb','PVC',0x75ca521892de7f2ecfb070cab545c250d0ceb7e3, 9), --token_id not found 
-    ('bvsd-bvsd','bnb','BVSD',0x7f9ad7a5854658d984924e868187b2135514fb88, 18)
+    ('bvsd-bvsd','bnb','BVSD',0x7f9ad7a5854658d984924e868187b2135514fb88, 18),
+    ('coco-coco-coin', 'bnb', 'COCO', 0xf563e86e461de100cfcfd8b65daa542d3d4b0550, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
 where contract_address not in (
     0x2ab0e9e4ee70fff1fb9d67031e44f6410170d00e -- bXEN has bad price feed.
