95ebba4de6bfd4b32b72e1ae7d1eb79c67728b51
thez0fa <thez0fa@proton.me>
2024-09-19 14:05:45 +0300

    Update prices_avalanche_c_tokens.sql
    add KIMBO

--- dbt_subprojects/tokens/models/prices/avalanche_c/prices_avalanche_c_tokens.sql
+++ dbt_subprojects/tokens/models/prices/avalanche_c/prices_avalanche_c_tokens.sql
@@ -92,5 +92,6 @@ FROM
     ('hon-heroes-of-nft','avalanche_c','HON',0xed2b42d3c9c6e97e11755bb37df29b6375ede3eb,18),
     ('bawls-bawls-onu','avalanche_c','BAWLS',0x2da8312e2c08b79104c6b18ba26bc7065abec704,18),
     ('shrap-shrapnel', 'avalanche_c', 'SHRAP', 0xd402298a793948698b9a63311404fbbee944eafd, 18),
-    ('bal-balancer', 'avalanche_c', 'BAL', 0xe15bcb9e0ea69e6ab9fa080c4c4a5632896298c3, 18)
+    ('bal-balancer', 'avalanche_c', 'BAL', 0xe15bcb9e0ea69e6ab9fa080c4c4a5632896298c3, 18),
+    ('kimbo-kimbo', 'avalanche_c', 'KIMBO', 0x184ff13B3EBCB25Be44e860163A5D8391Dd568c1, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
