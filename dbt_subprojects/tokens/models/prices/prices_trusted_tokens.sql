7b265bdf0244b02c93e6482bf5bb34dca57ca5b6
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2024-09-19 16:10:08 -0400

    DUX-665 trusted tokens: add native (#6774)
    * add native
    
    * remove test, as we already test upstream models

--- dbt_subprojects/tokens/models/prices/prices_trusted_tokens.sql
+++ dbt_subprojects/tokens/models/prices/prices_trusted_tokens.sql
@@ -137,16 +137,35 @@ WITH trusted_tokens AS (
                 , ('zksync', 0xbbeb516fb02a01611cbbe0453fe3c580d7281011)
                 , ('zora', 0x4200000000000000000000000000000000000006)
         ) AS t (blockchain, contract_address)
+), erc20 as (
+        SELECT
+                p.token_id
+                , p.blockchain
+                , p.contract_address
+                , p.symbol
+                , p.decimals
+        FROM
+                {{ ref('prices_tokens') }} AS p
+        INNER JOIN
+                trusted_tokens AS tt
+                ON p.blockchain = tt.blockchain
+                AND p.contract_address = tt.contract_address
+), native_tokens AS (
+        SELECT
+                token_id
+                , blockchain
+                , contract_address
+                , symbol
+                , decimals
+        FROM
+                {{ ref('prices_native_tokens') }}
 )
 SELECT
-        p.token_id
-        , p.blockchain
-        , p.contract_address
-        , p.symbol
-        , p.decimals
+        *
 FROM
-        {{ ref('prices_tokens') }} AS p
-INNER JOIN
-        trusted_tokens AS tt
-        ON p.blockchain = tt.blockchain
-        AND p.contract_address = tt.contract_address
+        erc20
+UNION ALL
+SELECT
+        *
+FROM
+        native_tokens
