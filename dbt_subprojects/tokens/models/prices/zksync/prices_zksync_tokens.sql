9a9740bfc2e4a897f7f0f914cd35d0e52b76970c
zionhaley <zionhaley89@outlook.com>
2024-09-19 14:10:49 +0000

    Update prices.usd to include KOI4 on ZKSync

--- dbt_subprojects/tokens/models/prices/zksync/prices_zksync_tokens.sql
+++ dbt_subprojects/tokens/models/prices/zksync/prices_zksync_tokens.sql
@@ -57,6 +57,6 @@ FROM
     ('kat-karat', 'zksync', 'KAT', 0xCDb7D260c107499C80B4b748e8331c64595972a1, 18),
     ('zkid-zksync-id', 'zksync', 'ZKID', 0x2141d7fe06A1d69c016fC638bA75b6Ef92Fa1435, 18),
     ('zk-zksync', 'zksync', 'ZK', 0x5A7d6b2F92C77FAD6CCaBd7EE0624E64907Eaf3E, 18),
-    ('weeth-wrapped-eeth', 'zksync', 'weETH', 0xc1fa6e2e8667d9be0ca938a54c7e0285e9df924a, 18)
-
+    ('weeth-wrapped-eeth', 'zksync', 'weETH', 0xc1fa6e2e8667d9be0ca938a54c7e0285e9df924a, 18),
+    ('koi4-koi', 'zksync', 'KOI4', 0xa995ad25ce5eb76972ab356168f5e1d9257e4d05, 18),
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
