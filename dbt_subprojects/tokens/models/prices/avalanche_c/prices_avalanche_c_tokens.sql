194e376914f9c1508f795a8c5c0c15bec155c2e2
thez0fa <thez0fa@proton.me>
2024-09-19 14:13:35 +0300

    add STG

--- dbt_subprojects/tokens/models/prices/avalanche_c/prices_avalanche_c_tokens.sql
+++ dbt_subprojects/tokens/models/prices/avalanche_c/prices_avalanche_c_tokens.sql
@@ -94,5 +94,6 @@ FROM
     ('shrap-shrapnel', 'avalanche_c', 'SHRAP', 0xd402298a793948698b9a63311404fbbee944eafd, 18),
     ('bal-balancer', 'avalanche_c', 'BAL', 0xe15bcb9e0ea69e6ab9fa080c4c4a5632896298c3, 18),
     ('kimbo-kimbo', 'avalanche_c', 'KIMBO', 0x184ff13B3EBCB25Be44e860163A5D8391Dd568c1, 18),
-    ('meow-meowcat-avax', 'avalanche_c', 'MEOW', 0x8aD25B0083C9879942A64f00F20a70D3278f6187, 18)
+    ('meow-meowcat-avax', 'avalanche_c', 'MEOW', 0x8aD25B0083C9879942A64f00F20a70D3278f6187, 18),
+    ('stg-stargate-finance', 'avalanche_c', 'STG', 0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
