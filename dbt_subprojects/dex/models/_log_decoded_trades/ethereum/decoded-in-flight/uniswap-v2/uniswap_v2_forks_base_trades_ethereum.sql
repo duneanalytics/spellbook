2ba354a3dc63c50ec055d5fae750efd198131bbf
Huang Geyang <Sukebeta@outlook.com>
2024-09-21 03:01:11 +0800

    Remove inner join to show all uni-v2 forks

--- dbt_subprojects/dex/models/_log_decoded_trades/ethereum/decoded-in-flight/uniswap-v2/uniswap_v2_forks_base_trades_ethereum.sql
+++ dbt_subprojects/dex/models/_log_decoded_trades/ethereum/decoded-in-flight/uniswap-v2/uniswap_v2_forks_base_trades_ethereum.sql
@@ -11,8 +11,7 @@
         )
 }}
 
-WITH all_decoded_trades AS (
-    {{
+{{
         uniswap_v2_forks_trades(
             blockchain = 'ethereum'
             , version = 'null'
@@ -20,28 +19,4 @@ WITH all_decoded_trades AS (
             , Pair_evt_Swap = ref('uniswap_v2_pool_decoding_ethereum')
             , Factory_evt_PairCreated = ref('uniswap_v2_factory_decoding_ethereum')
         )
-    }}
-)
-
-SELECT
-    all_decoded_trades.blockchain
-    , fork_mapping.project_name as project
-    , all_decoded_trades.version
-    , all_decoded_trades.dex_type
-    , factory_address
-    , all_decoded_trades.block_month
-    , all_decoded_trades.block_date
-    , all_decoded_trades.block_time
-    , all_decoded_trades.block_number
-    , all_decoded_trades.token_bought_amount_raw
-    , all_decoded_trades.token_sold_amount_raw
-    , all_decoded_trades.token_bought_address
-    , all_decoded_trades.token_sold_address
-    , all_decoded_trades.taker
-    , all_decoded_trades.maker
-    , all_decoded_trades.project_contract_address
-    , all_decoded_trades.tx_hash
-    , all_decoded_trades.evt_index
-FROM all_decoded_trades
-INNER JOIN {{ ref('uniswap_v2_fork_mapping_ethereum') }} AS fork_mapping
-USING (factory_address)
+}}
