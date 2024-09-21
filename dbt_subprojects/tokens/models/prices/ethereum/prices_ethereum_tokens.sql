5d11e7d9fb64a7d7f4dfd8cb632748ba6a90b340
0xyadday <112420584+mhmdriyadi@users.noreply.github.com>
2024-09-21 19:49:42 +0700

    Update prices_ethereum_tokens.sql
    Add token XBG prices

--- dbt_subprojects/tokens/models/prices/ethereum/prices_ethereum_tokens.sql
+++ dbt_subprojects/tokens/models/prices/ethereum/prices_ethereum_tokens.sql
@@ -1842,7 +1842,8 @@ FROM
     ('bubble-imaginary-ones', 'ethereum', 'BUBBLE', 0xe9689028ede16c2fdfe3d11855d28f8e3fc452a3, 18),
     ('zeta-zetachain', 'ethereum', 'ZETA', 0xf091867ec603a6628ed83d274e835539d82e9cc8, 18),
     ('euri-eurite', 'ethereum', 'EURI', 0x9d1a7a3191102e9f900faa10540837ba84dcbae7, 18),
-    ('sky-sky', 'ethereum', 'SKY', 0x56072C95FAA701256059aa122697B133aDEd9279, 18)
+    ('sky-sky', 'ethereum', 'SKY', 0x56072C95FAA701256059aa122697B133aDEd9279, 18),
+    ('xbg-xborg', 'ethereum', 'XBG', 0xeae00d6f9b16deb1bd584c7965e4c7d762f178a1, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
 where contract_address not in (
     -- bad price feeds
