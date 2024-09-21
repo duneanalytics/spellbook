71821f0bc918d0080fc91a0158c589d04379a702
Rudi Afandi <53332703+rudiafandi16@users.noreply.github.com>
2024-09-21 16:23:58 +0700

    Update prices_optimism_tokens_curated.sql
    Add CYBER price token

--- dbt_subprojects/tokens/models/prices/optimism/prices_optimism_tokens_curated.sql
+++ dbt_subprojects/tokens/models/prices/optimism/prices_optimism_tokens_curated.sql
@@ -134,5 +134,6 @@ FROM
     ('dola-dola', 'DOLA',0x8ae125e8653821e851f12a49f7765db9a9ce7384, 18),
     ('wld-worldcoin', 'WLD',0xdc6ff44d5d932cbd77b52e5612ba0529dc6226f1, 18),
     ('pendle-pendle', 'PENDLE',0xbc7b1ff1c6989f006a1185318ed4e7b5796e66e1, 18),
-    ('trb-tellor', 'TRB', 0xaf8ca653fa2772d58f4368b0a71980e9e3ceb888, 18)
+    ('trb-tellor', 'TRB', 0xaf8ca653fa2772d58f4368b0a71980e9e3ceb888, 18),
+    ('cyber-cyberconnect', 'CYBER', 0x14778860e937f509e651192a90589de711fb88a9, 18)
 ) as temp (token_id, symbol, contract_address, decimals)
