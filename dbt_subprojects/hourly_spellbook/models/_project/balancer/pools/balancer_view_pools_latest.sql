abc9bee4d63af6c910bd022bd8f32838c068bad3
viniabussafi <vinicius@balancerlabs.dev>
2024-09-24 11:33:19 -0300

    fix

--- dbt_subprojects/hourly_spellbook/models/_project/balancer/pools/balancer_view_pools_latest.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/pools/balancer_view_pools_latest.sql
@@ -17,9 +17,9 @@ SELECT
     m.factory_version,
     m.factory_address,
     m.creation_date,
-    SUM(CASE WHEN block_date = (SELECT MAX(block_date) FROM balancer.pools_metrics_daily)
+    SUM(CASE WHEN block_date = (SELECT MAX(block_date) FROM {{ref('balancer_pools_metrics_daily')}})
     THEN tvl_usd END) AS tvl_usd,
-    SUM(CASE WHEN block_date = (SELECT MAX(block_date) FROM balancer.pools_metrics_daily)
+    SUM(CASE WHEN block_date = (SELECT MAX(block_date) FROM {{ref('balancer_pools_metrics_daily')}})
     THEN tvl_eth END) AS tvl_eth,
     SUM(swap_amount_usd) AS swap_volume,
     SUM(CASE WHEN block_date > CURRENT_DATE - INTERVAL '365' day THEN swap_amount_usd END) AS swap_volume_1y,
@@ -32,6 +32,6 @@ SELECT
     SUM(CASE WHEN block_date > CURRENT_DATE - INTERVAL '7' day THEN fee_amount_usd END) AS fees_collected_7d,
     SUM(CASE WHEN block_date > CURRENT_DATE - INTERVAL '1' day THEN fee_amount_usd END) AS fees_collected_1d  
 FROM {{source('balancer', 'factory_pool_mapping')}} m
-LEFT JOIN ref('balancer_pools_metrics_daily') p ON m.blockchain = p.blockchain
+LEFT JOIN {{ref('balancer_pools_metrics_daily')}} p ON m.blockchain = p.blockchain
 AND m.pool_address = p.project_contract_address
 GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
