3591bf1f054ac7e631ef4954a1e610251b0698fc
Synthquest <synthquestdune@gmail.com>
2024-09-24 17:07:30 -0400

    init stablecoin balances evm chains

---
+++ dbt_subprojects/daily_spellbook/models/_sector/stablecoins/blast/stablecoins_blast_balances.sql
@@ -0,0 +1,38 @@
+{{
+  config(
+    schema = 'stablecoins_blast',
+    alias = 'balances',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['day', 'address', 'token_address', 'blockchain'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
+  )
+}}
+
+with
+stablecoin_tokens as (
+  select
+    symbol,
+    contract_address as token_address
+  from 
+    {{ source('tokens_blast', 'erc20_stablecoins')}}
+)
+
+,balances as (
+    {{
+      balances_incremental_subset_daily(
+            blockchain = 'blast',
+            token_list = 'stablecoin_tokens',
+            start_date = '2024-09-01'
+      )
+    }}
+)
+
+select
+    t.symbol
+    ,b.*
+from balances b
+left join stablecoin_tokens t
+    on b.token_address = t.token_address
+ 
