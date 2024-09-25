da77ca33076cf496846a89abe052092e025c7c4c
tomfutago <35136350+tomfutago@users.noreply.github.com>
2024-09-25 16:39:41 +0100

    Nexus Mutual - staking updates (#3) (#6821)
    * fix: reset product_id as non-nullable
    
    * feat: add tranche expiry date & chain level, output full history
    
    * docs: add new columns to schema.yml
    
    * trigger re-run

--- dbt_subprojects/daily_spellbook/models/nexusmutual/ethereum/staking/nexusmutual_ethereum_staking_deposit_extensions.sql
+++ dbt_subprojects/daily_spellbook/models/nexusmutual/ethereum/staking/nexusmutual_ethereum_staking_deposit_extensions.sql
@@ -11,7 +11,7 @@
   )
 }}
 
-with recursive deposit_chain (pool_address, token_id, tranche_id, new_tranche_id, total_amount, block_time, is_active, evt_index, tx_hash, chain_level) as (
+with recursive deposit_chain (pool_address, token_id, tranche_id, new_tranche_id, total_amount, block_time, tranche_expiry_date, is_active, evt_index, tx_hash, chain_level) as (
   select
     pool_address,
     token_id,
@@ -19,6 +19,7 @@ with recursive deposit_chain (pool_address, token_id, tranche_id, new_tranche_id
     tranche_id as new_tranche_id,
     sum(amount) as total_amount,
     max(block_time) as block_time,
+    max_by(tranche_expiry_date, block_time) as tranche_expiry_date,
     max_by(is_active, block_time) as is_active,
     max_by(evt_index, block_time) as evt_index,
     max_by(tx_hash, block_time) as tx_hash,
@@ -36,6 +37,7 @@ with recursive deposit_chain (pool_address, token_id, tranche_id, new_tranche_id
     d.new_tranche_id,
     dc.total_amount + coalesce(d.topup_amount, 0) as total_amount,
     d.block_time,
+    d.tranche_expiry_date,
     d.is_active,
     d.evt_index,
     d.tx_hash,
@@ -54,7 +56,10 @@ select
   tranche_id as init_tranche_id,
   new_tranche_id as current_tranche_id,
   total_amount,
+  tranche_expiry_date,
   is_active,
+  chain_level,
+  rn as token_tranche_rn,
   evt_index,
   tx_hash
 from (
@@ -63,4 +68,3 @@ from (
       row_number() over (partition by pool_address, token_id, tranche_id order by chain_level desc) as rn
     from deposit_chain
   ) t
-where rn = 1
