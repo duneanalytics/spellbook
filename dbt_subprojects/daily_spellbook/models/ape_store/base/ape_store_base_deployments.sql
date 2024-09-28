2b23e0aef688e475d9108ccea1e72ab2de646b3c
whale_hunter <143016036+whalehunting@users.noreply.github.com>
2024-09-28 14:05:37 +0200

    chore: add ape_store_base deployments spellbook

--- dbt_subprojects/daily_spellbook/models/ape_store/base/ape_store_base_deployments.sql
+++ dbt_subprojects/daily_spellbook/models/ape_store/base/ape_store_base_deployments.sql
@@ -1,1 +1,23 @@
+{{
+    config(
+        alias='deployments',
+        schema='ape_store_base',
+        materialized='incremental',
+        file_format='delta',
+        incremental_strategy='merge',
+        unique_key='token'
+    )
+}}
+
+{% set blockchain = 'base' %}
+
+select
+    evt_block_time as block_time,
+    date_trunc('day', evt_block_time) as block_date,
+    date_trunc('month', evt_block_time) as block_month,
+    '{{blockchain}}' as blockchain,
+    token,
+    id,
     evt_tx_from as deployer,
+    evt_tx_hash as tx_hash
+from {{ source("ape_store_base", "Router_evt_CreateToken") }}
