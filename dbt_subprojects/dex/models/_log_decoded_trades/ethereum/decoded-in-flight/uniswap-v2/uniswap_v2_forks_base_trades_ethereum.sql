f70131eb191f6d71e77215ab4e108f5e5623f925
Huang Geyang <Sukebeta@outlook.com>
2024-09-29 21:51:47 +0800

    Remove incremental predicates

--- dbt_subprojects/dex/models/_log_decoded_trades/ethereum/decoded-in-flight/uniswap-v2/uniswap_v2_forks_base_trades_ethereum.sql
+++ dbt_subprojects/dex/models/_log_decoded_trades/ethereum/decoded-in-flight/uniswap-v2/uniswap_v2_forks_base_trades_ethereum.sql
@@ -6,8 +6,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['tx_hash', 'evt_index'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+        unique_key = ['tx_hash', 'evt_index']
         )
 }}
 
