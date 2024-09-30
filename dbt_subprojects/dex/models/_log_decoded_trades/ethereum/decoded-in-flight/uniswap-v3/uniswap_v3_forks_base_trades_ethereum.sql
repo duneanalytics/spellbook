f2deff7ae2de748774ed45a06c8d5f71042136fc
Huang Geyang <Sukebeta@outlook.com>
2024-09-30 17:02:52 +0800

    Revert "Remove incremental predicates"
    This reverts commit f70131eb191f6d71e77215ab4e108f5e5623f925.

--- dbt_subprojects/dex/models/_log_decoded_trades/ethereum/decoded-in-flight/uniswap-v3/uniswap_v3_forks_base_trades_ethereum.sql
+++ dbt_subprojects/dex/models/_log_decoded_trades/ethereum/decoded-in-flight/uniswap-v3/uniswap_v3_forks_base_trades_ethereum.sql
@@ -6,7 +6,8 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['tx_hash', 'evt_index']
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
         )
 }}
 
