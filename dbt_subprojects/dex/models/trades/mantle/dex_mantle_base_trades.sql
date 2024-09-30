58ac29b61371fdcd36b33b3489d08d1cf80569aa
Cuauhtemoc Ramirez Hernandez <cramhd@gmail.com>
2024-09-30 12:04:26 -0600

    add clipper to mantle dex trades

--- dbt_subprojects/dex/models/trades/mantle/dex_mantle_base_trades.sql
+++ dbt_subprojects/dex/models/trades/mantle/dex_mantle_base_trades.sql
@@ -10,6 +10,7 @@
     , ref('fusionx_mantle_base_trades')
     , ref('agni_mantle_base_trades')
     , ref('swaap_v2_mantle_base_trades')
+    , ref('clipper_mantle_base_trades')
 ] %}
 
 WITH base_union AS (
