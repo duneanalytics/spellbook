24b3b37708c2ac0440b4ff576118f7cdc2834592
Hildebert Moulié <h.moulie@gmail.com>
2024-10-02 03:08:27 +0200

    fix

--- dbt_subprojects/daily_spellbook/models/addresses/chains/addresses_blast_info.sql
+++ dbt_subprojects/daily_spellbook/models/addresses/chains/addresses_blast_info.sql
@@ -9,7 +9,7 @@
         file_format = 'delta',
         incremental_strategy = 'merge',
         unique_key = ['address'],
-        merge_update_columns = ['executed_tx_count', 'max_nonce', 'is_smart_contract', 'namespace', 'name', 'first_funded_by', 'received_count', 'sent_count', 'first_received_block_time', 'last_received_block_time', 'first_sent_block_time', 'last_sent_block_time', 'received_volume_usd', 'sent_volume_usd', 'first_tx_block_time', 'last_tx_block_time', 'first_tx_block_number', 'last_tx_block_number', 'last_seen'],
+        merge_update_columns = ['executed_tx_count', 'max_nonce', 'is_smart_contract', 'namespace', 'name', 'first_funded_by', 'first_funded_by_block_time', 'tokens_received_count', 'tokens_received_tx_count', 'tokens_sent_count', 'tokens_sent_tx_count', 'first_transfer_block_time', 'last_transfer_block_time', 'first_received_block_number', 'last_received_block_number', 'first_sent_block_number', 'last_sent_block_number', 'received_volume_usd', 'sent_volume_usd', 'first_tx_block_time', 'last_tx_block_time', 'first_tx_block_number', 'last_tx_block_number', 'last_seen', 'last_seen_block'],
     )
 }}
 
