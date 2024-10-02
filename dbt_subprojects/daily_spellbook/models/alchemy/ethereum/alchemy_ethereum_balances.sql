9d29729227790adf138871582cdfd6ef29740a51
sophie-analytics <sonibaresofiat@gmail.com>
2024-10-02 23:05:08 +0100

    fix syntax error

--- dbt_subprojects/daily_spellbook/models/alchemy/ethereum/alchemy_ethereum_balances.sql
+++ dbt_subprojects/daily_spellbook/models/alchemy/ethereum/alchemy_ethereum_balances.sql
@@ -5,7 +5,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['day', 'address', 'token_address', 'token_id'],
+    unique_key = ['day', 'address', 'token_address', 'unique_key_id'],
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
   )
 }}
@@ -28,7 +28,8 @@ alchemy_addresses as (
 )
 
 select
-  *
+  *,
+  coalesce(token_id, uint256 '1') as unique_key_id ---using this to add a unique key to the table as erc20 has no token_id
 from balances
 where token_address not in (
             0xd74f5255d557944cf7dd0e45ff521520002d5748, --$9.8B were minted in a hack in 2023, all of which are stored in a Safe. Filtering out.
