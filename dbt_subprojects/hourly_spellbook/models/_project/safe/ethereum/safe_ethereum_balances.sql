b1e022fdb9fd49d6f25a9a268f7e5511b1e66516
Peter <157015397+safeintern@users.noreply.github.com>
2024-10-04 03:25:36 +0800

    Added unique_id

--- dbt_subprojects/hourly_spellbook/models/_project/safe/ethereum/safe_ethereum_balances.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/ethereum/safe_ethereum_balances.sql
@@ -5,7 +5,7 @@
      materialized = 'incremental',
      file_format = 'delta',
      incremental_strategy = 'merge',
-     unique_key = ['day', 'address', 'token_address', 'blockchain', 'token_standard', 'token_symbol', 'token_id'],
+     unique_key = ['day', 'address', 'token_address', 'unique_key_id'],
      incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
    )
  }}
@@ -27,43 +27,12 @@ balances as (
              start_date = '2021-07-01'
        )
      }}
- ),
-
-flagged_balances as (
-    select
-        b.day,
-        b.blockchain,
-        b.address,
-        b.token_address,
-        b.token_standard,
-        b.token_id,
-        b.token_symbol,
-        b.balance,
-        b.balance_usd,
-        -- Flag balances that changed on the day as "active"
-        case when lag(b.balance) over (
-            partition by b.address, b.token_address, b.blockchain, b.token_standard, b.token_id
-            order by b.day
-        ) != b.balance or lag(b.balance) over (
-            partition by b.address, b.token_address, b.blockchain, b.token_standard, b.token_id
-            order by b.day
-        ) is null then 1 else 0 end as is_active
-    from balances b
-)
-
+ )
+   
 select
-    day,
-    blockchain,
-    address,
-    token_address,
-    token_standard,
-    token_id,
-    token_symbol,
-    sum(balance) as token_balance,
-    sum(balance_usd) as balance_usd
-from flagged_balances
+  *,
+  coalesce(token_id, uint256 '1') as unique_key_id 
+from balances
 where token_address not in (
-         0xd74f5255d557944cf7dd0e45ff521520002d5748,
-         0xe9689028ede16c2fdfe3d11855d28f8e3fc452a3 
-  )
-group by 1, 2, 3, 4, 5, 6, 7
+            0xd74f5255d557944cf7dd0e45ff521520002d5748, --$9.8B were minted in a hack in 2023, all of which are stored in a Safe. Filtering out.
+            0xe9689028ede16c2fdfe3d11855d28f8e3fc452a3 )
