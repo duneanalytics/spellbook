e85ce149401e3ea5af51af17bd357de233ba1746
Hildebert Moulié <h.moulie@gmail.com>
2024-10-05 18:21:23 +0200

    fix

--- dbt_subprojects/daily_spellbook/models/addresses/chains/addresses_polygon_info.sql
+++ dbt_subprojects/daily_spellbook/models/addresses/chains/addresses_polygon_info.sql
@@ -19,7 +19,7 @@
         , transactions = source(blockchain, 'transactions')
         , token_transfers = source('tokens_' + blockchain, 'transfers')
         , creation_traces = source(blockchain, 'creation_traces')
-        , first_funded_by = source('addresses_events_' + blockchain, '_first_funded_by')
+        , first_funded_by = source('addresses_events_' + blockchain, 'first_funded_by')
         , contracts = source(blockchain, 'contracts')
     )
 }}
