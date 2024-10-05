3aa7d5fdf583977214aa48341058ad925a51125f
Boxer <Floba.hs@gmail.com>
2024-10-05 18:07:12 +0200

    send it

--- dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_hourly_market_prices.sql
+++ dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_hourly_market_prices.sql
@@ -1,12 +1,12 @@
 {{
   config(
     schema = 'polymarket_polygon',
-    alias = 'hourly_market_prices',
+    alias = 'daily_market_prices',
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['hour', 'condition_id', 'token_id'],
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.hour')],
+    unique_key = ['day', 'condition_id', 'token_id'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')],
     post_hook = '{{ expose_spells(blockchains = \'["polygon"]\',
                                   spell_type = "project",
                                   spell_name = "polymarket",
@@ -16,33 +16,49 @@
 
 WITH changed_prices AS (
     SELECT
-        date_trunc('hour', block_time) AS hour,
+        date_trunc('day', block_time) AS day,
         condition_id,
         asset_id AS token_id,
         price,
-        LEAD(CAST(date_trunc('hour', block_time) AS timestamp)) OVER (PARTITION BY condition_id, asset_id ORDER BY block_time ASC) AS next_update_hour
+        LEAD(CAST(date_trunc('day', block_time) AS timestamp)) OVER (PARTITION BY condition_id, asset_id ORDER BY block_time ASC) AS next_update_day
     FROM {{ ref('polymarket_polygon_raw_market_trades') }}
-    WHERE block_time < DATE_TRUNC('hour', NOW()) 
+    WHERE block_time < DATE_TRUNC('day', NOW()) 
 ),
 
 --sequences are limited to 10k so just pulling this in from the transactions table, no other relationship
-hours AS (
-    Select date_trunc('hour', block_time) as hour
+days AS (
+    Select date_trunc('day', block_time) as day
     from {{ source('ethereum', 'transactions') }}
 ),
 
-
+latest_prices AS (
+    SELECT
+        day,
+        condition_id,
+        token_id,
+        price
+    FROM (
+        SELECT
+            day,
+            condition_id,
+            token_id,
+            price,
+            ROW_NUMBER() OVER (PARTITION BY condition_id, token_id, day ORDER BY block_time DESC) AS rn
+        FROM changed_prices
+    ) t
+    WHERE rn = 1
+),
 
 forward_fill AS (
     SELECT
-        CAST(h.hour AS timestamp) AS hour,
-        cp.condition_id,
-        cp.token_id,
-        cp.price
-    FROM hours h
-    LEFT JOIN changed_prices cp
-        ON h.hour >= cp.hour
-        AND (cp.next_update_hour IS NULL OR h.hour < cp.next_update_hour)
+        CAST(d.day AS timestamp) AS day,
+        lp.condition_id,
+        lp.token_id,
+        lp.price
+    FROM days d
+    LEFT JOIN latest_prices lp
+        ON d.day >= lp.day
+        AND (lp.next_update_day IS NULL OR d.day < lp.next_update_day)
 ),
 
 prices AS (
@@ -53,5 +69,5 @@ prices AS (
 
 SELECT * FROM prices
 {% if is_incremental() %}
-WHERE hour >= DATE_TRUNC('day', NOW() - INTERVAL '7' day)
+WHERE day >= DATE_TRUNC('day', NOW() - INTERVAL '2' day)
 {% endif %}
