d0624b2d36d0de168e94f38742863035eea6ded8
jeff-dude <jeff@dune.xyz>
2024-10-07 17:09:02 -0400

    update current to last_x_days

--- dbt_subprojects/hourly_spellbook/models/_metrics/fees/metrics_gas_fees_stats.sql
+++ dbt_subprojects/hourly_spellbook/models/_metrics/fees/metrics_gas_fees_stats.sql
@@ -15,7 +15,7 @@ with source as (
 ), current_day as (
     select
         blockchain
-        , gas_fees_usd as current_day_gas_fees_usd
+        , gas_fees_usd as last_1_days_gas_fees_usd
     from
         source
     where
@@ -24,7 +24,7 @@ with source as (
 ), previous_day as (
     select
         blockchain
-        , gas_fees_usd as previous_day_gas_fees_usd
+        , gas_fees_usd as previous_1_days_gas_fees_usd
     from
         source
     where
@@ -32,17 +32,17 @@ with source as (
         and block_date < date_trunc('day', now()) - interval '1' day
 ), total_current_day_gas_fees as (
     select
-        sum(current_day_gas_fees_usd) AS total_current_day_gas_fees
+        sum(last_1_days_gas_fees_usd) AS total_cross_chain_last_1_days_gas_fees_usd
     from
         current_day
 ), daily_stats as (
     select
         c.blockchain
-        , c.current_day_gas_fees_usd
-        , p.previous_day_gas_fees_usd
-        , ((c.current_day_gas_fees_usd - coalesce(p.previous_day_gas_fees_usd, 0)) / coalesce(p.previous_day_gas_fees_usd, 1)) * 100 AS daily_percent_change
-        , t.total_current_day_gas_fees
-        , (c.current_day_gas_fees_usd / t.total_current_day_gas_fees) * 100 AS percent_of_total_current_day_fees
+        , c.last_1_days_gas_fees_usd
+        , p.previous_1_days_gas_fees_usd
+        , ((c.last_1_days_gas_fees_usd - coalesce(p.previous_1_days_gas_fees_usd, 0)) / coalesce(p.previous_1_days_gas_fees_usd, 1)) * 100 AS daily_percent_change
+        , t.total_cross_chain_last_1_days_gas_fees_usd
+        , (c.last_1_days_gas_fees_usd / t.total_cross_chain_last_1_days_gas_fees_usd) * 100 AS percent_total_last_1_days_fees_usd
     from
         current_day as c
     left join previous_day as p
@@ -52,7 +52,7 @@ with source as (
 ), current_week as (
     select
         blockchain
-        , sum(gas_fees_usd) as current_week_gas_fees_usd
+        , sum(gas_fees_usd) as last_7_days_gas_fees_usd
     from
         source
     where
@@ -63,7 +63,7 @@ with source as (
 ), previous_week as (
     select
         blockchain
-        , sum(gas_fees_usd) as previous_week_gas_fees_usd
+        , sum(gas_fees_usd) as previous_7_days_gas_fees_usd
     from
         source
     where
@@ -73,17 +73,17 @@ with source as (
         blockchain
 ), total_current_week_gas_fees as (
     select
-        sum(current_week_gas_fees_usd) AS total_current_week_gas_fees
+        sum(last_7_days_gas_fees_usd) AS total_cross_chain_last_7_days_gas_fees_usd
     from
         current_week
 ), weekly_stats as (
     select
         c.blockchain
-        , c.current_week_gas_fees_usd
-        , p.previous_week_gas_fees_usd
-        , ((c.current_week_gas_fees_usd - coalesce(p.previous_week_gas_fees_usd, 0)) / coalesce(p.previous_week_gas_fees_usd, 1)) * 100 AS weekly_percent_change
-        , t.total_current_week_gas_fees
-        , (c.current_week_gas_fees_usd / t.total_current_week_gas_fees) * 100 AS percent_of_total_current_week_gas_fees
+        , c.last_7_days_gas_fees_usd
+        , p.previous_7_days_gas_fees_usd
+        , ((c.last_7_days_gas_fees_usd - coalesce(p.previous_7_days_gas_fees_usd, 0)) / coalesce(p.previous_7_days_gas_fees_usd, 1)) * 100 AS weekly_percent_change
+        , t.total_cross_chain_last_7_days_gas_fees_usd
+        , (c.last_7_days_gas_fees_usd / t.total_cross_chain_last_7_days_gas_fees_usd) * 100 AS percent_total_last_7_days_gas_fees_usd
     from
         current_week as c
     left join previous_week as p
@@ -93,7 +93,7 @@ with source as (
 ), current_month as (
     select
         blockchain
-        , sum(gas_fees_usd) as current_month_gas_fees_usd
+        , sum(gas_fees_usd) as last_30_days_gas_fees_usd
     from
         source
     where
@@ -104,7 +104,7 @@ with source as (
 ), previous_month as (
     select
         blockchain
-        , sum(gas_fees_usd) as previous_month_gas_fees_usd
+        , sum(gas_fees_usd) as previous_30_days_gas_fees_usd
     from
         source
     where
@@ -114,17 +114,17 @@ with source as (
         blockchain
 ), total_current_month_gas_fees as (
     select
-        sum(current_month_gas_fees_usd) AS total_current_month_gas_fees
+        sum(last_30_days_gas_fees_usd) AS total_cross_chain_last_30_days_gas_fees_usd
     from
         current_month
 ), monthly_stats as (
     select
         c.blockchain
-        , c.current_month_gas_fees_usd
-        , p.previous_month_gas_fees_usd
-        , ((c.current_month_gas_fees_usd - coalesce(p.previous_month_gas_fees_usd, 0)) / coalesce(p.previous_month_gas_fees_usd, 1)) * 100 AS monthly_percent_change
-        , t.total_current_month_gas_fees
-        , (c.current_month_gas_fees_usd / t.total_current_month_gas_fees) * 100 AS percent_of_total_current_month_gas_fees
+        , c.last_30_days_gas_fees_usd
+        , p.previous_30_days_gas_fees_usd
+        , ((c.last_30_days_gas_fees_usd - coalesce(p.previous_30_days_gas_fees_usd, 0)) / coalesce(p.previous_30_days_gas_fees_usd, 1)) * 100 AS monthly_percent_change
+        , t.total_cross_chain_last_30_days_gas_fees_usd
+        , (c.last_30_days_gas_fees_usd / t.total_cross_chain_last_30_days_gas_fees_usd) * 100 AS percent_total_last_30_days_gas_fees
     from
         current_month as c
     left join previous_month as p
@@ -134,22 +134,22 @@ with source as (
 )
 select
     d.blockchain
-    , d.current_day_gas_fees_usd
-    , d.previous_day_gas_fees_usd
+    , d.last_1_days_gas_fees_usd
+    , d.previous_1_days_gas_fees_usd
     , d.daily_percent_change
-    , d.total_current_day_gas_fees
-    , d.percent_of_total_current_day_gas_fees
-    , w.current_week_gas_fees_usd
-    , w.previous_week_gas_fees_usd
+    , d.total_cross_chain_last_1_days_gas_fees_usd
+    , d.percent_total_last_1_days_fees_usd
+    , w.last_7_days_gas_fees_usd
+    , w.previous_7_days_gas_fees_usd
     , w.weekly_percent_change
-    , w.total_current_week_gas_fees
-    , w.percent_of_total_current_week_gas_fees
-    , m.current_month_gas_fees_usd
-    , m.previous_month_gas_fees_usd
+    , w.total_cross_chain_last_7_days_gas_fees_usd
+    , w.percent_total_last_7_days_gas_fees_usd
+    , m.last_30_days_gas_fees_usd
+    , m.previous_30_days_gas_fees_usd
     , m.monthly_percent_change
-    , m.total_current_month_gas_fees
-    , m.percent_of_total_current_month_gas_fees
-    , m.total_current_month_gas_fees * 12 as gas_fees_usd_run_rate
+    , m.total_cross_chain_last_30_days_gas_fees_usd
+    , m.percent_total_last_30_days_gas_fees
+    , m.last_30_days_gas_fees_usd * 12 as gas_fees_usd_run_rate
 from
     daily_stats as d
 inner join weekly_stats as w
