c96f1dcd4c6bbb75df222bff445e7d7dc810ce6a
jeff-dude <jeff@dune.xyz>
2024-10-08 13:53:08 -0400

    initiate prices usd trusted tokens

---
+++ dbt_subprojects/tokens/models/prices/prices_usd_trusted_tokens.sql
@@ -0,0 +1,28 @@
+{{ config(
+        schema = 'prices'
+        , alias = 'usd_trusted_tokens'
+        , partition_by = ['blockchain']
+        , materialized = 'incremental'
+        , file_format = 'delta'
+        , incremental_strategy = 'merge'
+        , unique_key = ['blockchain', 'contract_address', 'minute']
+        , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.minute')]
+        )
+}}
+
+select
+    p.minute
+    , ptt.blockchain
+    , ptt.contract_address
+    , ptt.symbol
+    , ptt.decimals
+    , p.price
+from
+    {{ source('prices','usd_0003') }} as p
+inner join
+    {{ ref('prices_trusted_tokens') }} as ptt
+    on p.token_id = ptt.token_id
+{% if is_incremental() %}
+where
+    {{ incremental_predicate('p.minute') }}
+{% endif %}
