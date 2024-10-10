0af222fd6c2a68256fb29917a9c46e7d7474aacc
jeff-dude <jeff@dune.xyz>
2024-10-10 15:25:37 -0400

    initiate index metrics spells

---
+++ dbt_subprojects/hourly_spellbook/models/_metrics/transactions/metrics_transactions_index.sql
@@ -0,0 +1,57 @@
+{{ config(
+        schema = 'metrics'
+        , alias = 'transactions_index'
+        , materialized = 'incremental'
+        , file_format = 'delta'
+        , incremental_strategy = 'merge'
+        , unique_key = ['blockchain', 'block_date']
+        , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
+        )
+}}
+
+{% set baseline_date = '2018-01-01' %}
+
+with baseline as (
+    select
+        sum(tx_count) as baseline_tx_count -- sum is required due to blockchain being second unique key in source
+    from
+        {{ ref('metrics_transactions_daily') }}
+    where
+        block_date = date '{{ baseline_date }}'
+), blockchain_baseline as (
+    select
+        blockchain
+        , tx_count as blockchain_baseline_tx_count
+    from
+        {{ ref('metrics_transactions_daily') }}
+    where
+        block_date = date '{{ baseline_date }}'
+), daily as (
+    select
+        blockchain
+        , block_date
+        , tx_count
+    from
+        {{ ref('metrics_transactions_daily') }}
+    where
+        block_date >= date '{{ baseline_date }}'
+        {% if is_incremental() %}
+        and {{ incremental_predicate('block_date') }}
+        {% endif %}
+)
+select
+    d.blockchain
+    , d.block_date
+    , d.tx_count
+    , b.baseline_tx_count
+    , bb.blockchain_baseline_tx_count
+    , (d.tx_count / b.baseline_tx_count) * 100 as tx_index
+    , (d.tx_count / bb.blockchain_baseline_tx_count) * 100 as blockchain_tx_index
+from
+    daily as d
+left join
+    baseline as b
+    on 1 = 1
+left join
+    blockchain_baseline as bb
+    on d.blockchain = bb.blockchain
