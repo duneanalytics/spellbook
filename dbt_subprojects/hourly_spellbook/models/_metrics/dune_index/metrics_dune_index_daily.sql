99e9c0cad5041e3cff4167f2f03121ad1b1e3867
jeff-dude <jeff@dune.xyz>
2024-10-10 16:27:52 -0400

    fix ref names

--- dbt_subprojects/hourly_spellbook/models/_metrics/dune_index/metrics_dune_index_daily.sql
+++ dbt_subprojects/hourly_spellbook/models/_metrics/dune_index/metrics_dune_index_daily.sql
@@ -17,16 +17,16 @@ select
     , tx.tx_index
     , (f.fees_index + tr.transfers_index + tx.tx_index) / 3 as dune_index
     , 100 as baseline
-from {{ ref('metrics_fees_index') }} as f
+from {{ ref('metrics_fees_index_daily') }} as f
 left join
-    {{ ref('metrics_transfers_index') }} as tr
+    {{ ref('metrics_transfers_index_daily') }} as tr
     on f.blockchain = tr.blockchain
     and f.block_date = tr.block_date
     {% if is_incremental() %}
     and {{ incremental_predicate('tr.block_date') }}
     {% endif %}
 left join
-    {{ ref('metrics_transactions_index') }} as tx
+    {{ ref('metrics_transactions_index_daily') }} as tx
     on f.blockchain = tx.blockchain
     and f.block_date = tx.block_date
     {% if is_incremental() %}
