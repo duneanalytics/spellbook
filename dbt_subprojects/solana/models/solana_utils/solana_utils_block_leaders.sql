a89ab63e12022bf24946c4cb8d0eeda68e591054
Boxer <Floba.hs@gmail.com>
2024-10-11 11:27:31 +0200

    create util block_leaders

---
+++ dbt_subprojects/solana/models/solana_utils/solana_utils_block_leaders.sql
@@ -0,0 +1,40 @@
+{{ config(
+    schema = 'solana_utils',
+    alias = 'block_leaders',
+    materialized = 'incremental',
+    partition_by = ['month'],
+    file_format = 'delta',
+    unique_key = ['slot'],
+    incremental_strategy = 'merge',
+    post_hook='{{ expose_spells(\'["solana"]\',
+                                    "sector",
+                                    "solana_utils",
+                                    \'["0xBoxer"]\') }}'
+) }}
+
+SELECT 
+    date_trunc('month', b.time) as month,
+    b.time,
+    b.slot,
+    b.height,
+    b.hash,
+    b.total_transactions,
+    b.successful_transactions,
+    b.failed_transactions,
+    b.total_vote_transactions,
+    b.total_non_vote_transactions,
+    b.successful_vote_transactions,
+    b.successful_non_vote_transactions,
+    b.failed_vote_transactions,
+    b.failed_non_vote_transactions,
+    b.parent_slot,
+    b.previous_block_hash,
+    r.recipient AS leader
+FROM {{ source('solana', 'blocks') }} b 
+LEFT JOIN {{ source('solana', 'rewards') }} r 
+    ON b.slot = r.block_slot 
+    AND r.reward_type = 'Fee'
+
+{% if is_incremental() %}
+    AND {{incremental_predicate('b.time')}}
+{% endif %}
