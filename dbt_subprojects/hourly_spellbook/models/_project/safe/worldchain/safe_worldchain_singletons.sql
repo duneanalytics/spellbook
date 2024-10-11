eb48827cb2d6e4353e83a62ccd0bc0dd762c2f28
danielpartida <partidapd@gmail.com>
2024-10-11 20:55:47 +0200

    Change name of decoded tables to run

--- dbt_subprojects/hourly_spellbook/models/_project/safe/worldchain/safe_worldchain_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/worldchain/safe_worldchain_singletons.sql
@@ -14,8 +14,8 @@
 
 -- Fetch all known singleton/mastercopy addresses used via factories.
 select distinct singleton as address
-from {{ source('gnosis_safe_worldchain', 'SafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
+from {{ source('gnosis_safe_worldchain', 'SafeProxyFactory_v_1_3_0_evt_ProxyCreation') }}
 
 union
 select distinct singleton as address
-from {{ source('gnosis_safe_worldchain', 'SafeProxyFactory_v1_4_1_evt_ProxyCreation') }}
+from {{ source('gnosis_safe_worldchain', 'SafeProxyFactory_v_1_4_1_evt_ProxyCreation') }}
