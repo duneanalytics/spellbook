952bd952c4072780fa0d3d54b617e1d136088b96
Hildebert Moulié <h.moulie@gmail.com>
2024-10-13 04:29:55 +0200

    create get_explorers() macros

---
+++ dbt_macros/deprecated_udfs/get_explorers.sql
@@ -0,0 +1,47 @@
+{% macro get_explorers() %}
+      create or replace function get_explorers(blockchain_ STRING, hash_ STRING, hash_type_ STRING, concise_ BOOLEAN)
+      returns STRING
+      return
+WITH explorers AS (
+    SELECT explorer, emoji, address, transaction
+    FROM (VALUES
+    ('Etherscan', '🔎', 'https://etherscan.io/address/hash_', 'https://etherscan.io/tx/hash_')
+    , ('GitHub', '💻', 'https://github.com/search?q=hash_&type=code', 'https://github.com/search?q=hash_&type=code')
+    , ('Arkham','⬛️', 'https://platform.arkhamintelligence.com/explorer/address/hash_', 'https://platform.arkhamintelligence.com/explorer/tx/hash_')
+    , ('Zerion', '🟦', 'https://app.zerion.io/hash_/overview', NULL)
+    , ('Nansen', '❇️', 'https://app.nansen.ai/profiler?address=hash_', 'https://app.nansen.ai/tx/hash_')
+    , ('Parsec', '⪇', 'https://parsec.fi/address/hash_', 'https://parsec.fi/blockchain_/tx/hash_')
+    , ('Zapper', '⚡️', 'https://zapper.xyz/account/hash_', 'https://zapper.xyz/event/blockchain_/hash_')
+    , ('Blockscout', '🔭', 'https://eth.blockscout.com/address/hash_', 'https://eth.blockscout.com/tx/hash_')
+    , ('Unmarshal', '⛨', 'https://xscan.io/address/hash_/assets?chain=blockchain_', NULL)
+    , ('Dora', '🌼', 'https://www.ondora.xyz/accounts/hash_/all', 'https://www.ondora.xyz/network/blockchain_/interactions/hash_')
+    , ('Ethplorer', '🔷', 'https://ethplorer.io/address/hash_#', 'https://ethplorer.io/tx/hash_#pageTab=transfers')
+    , ('Socketscan', '🔌', NULL, 'https://www.socketscan.io/tx/0xb2c17ecd521f73d8d91aaf574ad43ff1bda3148996079fb2796aabd30dc6ddac')
+    , ('Beaconchain', '📡', 'https://beaconcha.in/address/hash_', 'https://beaconcha.in/tx/hash_')
+    , ('Rated', '🍬', 'https://explorer.rated.network/o/hash_', NULL)
+    , ('Tokenview', '🎟️', 'https://eth.tokenview.io/en/address/hash_', 'https://eth.tokenview.io/en/tx/hash_')
+    , ('Bitquery', '🅱️', 'https://explorer.bitquery.io/ethereum/address/hash_', 'https://explorer.bitquery.io/blockchain_/tx/hash_')
+    , ('OKLink', '🆗', 'https://www.oklink.com/multi-search#key=hash_', 'https://www.oklink.com/eth/tx/hash_')
+    , ('Blockchair', '🧊', 'https://blockchair.com/blockchain_/address/hash_', 'https://blockchair.com/blockchain_/transaction/hash_')
+    , ('Debank', '🔓', 'https://debank.com/profile/hash_', NULL)
+    , ('Ankr', '⚓️', 'https://ankrscan.io/address/hash_', 'https://ankrscan.io/transactions/chain/blockchain_/hash_')
+    , ('ethVM', '🖥️', 'https://www.ethvm.com/address/hash_', 'https://www.ethvm.com/tx/hash_?t=actions')
+    ) AS x (explorer, emoji, address, transaction)
+    )
+
+, results AS (
+    SELECT CASE WHEN concise_ AND hash_type_ = 'tx' THEN get_href(transaction, emoji)
+      WHEN NOT concise_ AND hash_type_ = 'tx' get_href(transaction, explorer)
+      WHEN concise_ AND hash_type_ = 'address' get_href(address, emoji)
+      WHEN NOT concise_ AND hash_type_ = 'address' get_href(address, explorer)
+      WHEN hash_type_ = 'tx' get_href(transaction, explorer)
+      ELSE get_href(address, explorer)
+      END AS link
+    ,regexp_count(substring('{{explorers}}' FROM 1 FOR position(explorer IN '{{explorers}}') - 1), ',') AS ordering
+    FROM explorers
+    WHERE '{{explorers}}' LIKE '%' || explorer || '%'
+    )
+
+SELECT array_join(array_agg(link ORDER BY ordering), ' ') AS links
+FROM results
+{% endmacro %}
