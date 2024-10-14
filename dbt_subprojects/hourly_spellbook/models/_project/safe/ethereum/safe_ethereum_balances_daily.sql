57fedd6997a6decc1e08f921853b8585d1a87e3d
Peter <157015397+safeintern@users.noreply.github.com>
2024-10-14 20:28:34 +0800

    Edited a table name error

--- dbt_subprojects/hourly_spellbook/models/_project/safe/ethereum/safe_ethereum_balances_daily.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/ethereum/safe_ethereum_balances_daily.sql
@@ -10,7 +10,7 @@
 select
   *,
   coalesce(token_id, uint256 '1') as unique_key_id 
-from {{ ref('safe_balances_balances') }}
+from {{ ref('safe_balances') }}
 where token_address not in (
             0xd74f5255d557944cf7dd0e45ff521520002d5748, --$9.8B were minted in a hack in 2023, all of which are stored in a Safe. Filtering out.
             0xe9689028ede16c2fdfe3d11855d28f8e3fc452a3 )
