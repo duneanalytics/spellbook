276b1485d82bfd70ca83ba064c987d42a9aefd8e
Peter <157015397+safeintern@users.noreply.github.com>
2024-10-14 18:02:20 +0800

    Incremental Model

--- dbt_subprojects/hourly_spellbook/models/_project/safe/ethereum/safe_ethereum_balances.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/ethereum/safe_ethereum_balances.sql
@@ -10,7 +10,6 @@
    )
  }}
 
-
 with safes as (
     select
         address,
@@ -18,7 +17,7 @@ with safes as (
     from {{ ref('safe_ethereum_safes') }}
     where blockchain = 'ethereum'
 ),
-    
+
 balances as (
      {{
        balances_incremental_subset_daily(
@@ -27,12 +26,6 @@ balances as (
              start_date = '2021-07-01'
        )
      }}
- )
-   
-select
-  *,
-  coalesce(token_id, uint256 '1') as unique_key_id 
-from balances
-where token_address not in (
-            0xd74f5255d557944cf7dd0e45ff521520002d5748, --$9.8B were minted in a hack in 2023, all of which are stored in a Safe. Filtering out.
-            0xe9689028ede16c2fdfe3d11855d28f8e3fc452a3 )
+)
+
+select * from balances
