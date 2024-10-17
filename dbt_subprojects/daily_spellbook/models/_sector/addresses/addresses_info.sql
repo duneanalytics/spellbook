2098ef0f7230ec34887330e8dff0ef073f8b8507
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-10-17 13:46:59 +0200

    reset git

--- dbt_subprojects/daily_spellbook/models/_sector/addresses/addresses_info.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/addresses/addresses_info.sql
@@ -65,7 +65,50 @@ WITH data AS (
             ])) AS chain_stats
     , MAX(last_seen) AS last_seen
     , MAX(last_seen_block) AS last_seen_block
-    FROM ({{ref('addresses_info_union_view')}})
+    FROM (
+        {% for addresses_model in addresses_models %}
+        SELECT '{{ addresses_model[0] }}' AS blockchain
+        , address
+        , executed_tx_count
+        , max_nonce
+        , is_smart_contract
+        , namespace
+        , name
+        , first_funded_by
+        , first_funded_by_block_time
+        , tokens_received_count
+        , tokens_received_tx_count
+        , tokens_sent_count
+        , tokens_sent_tx_count
+        , first_transfer_block_time
+        , last_transfer_block_time
+        , first_received_block_number
+        , last_received_block_number
+        , first_sent_block_number
+        , last_sent_block_number
+        , received_volume_usd
+        , sent_volume_usd
+        , first_tx_block_time
+        , last_tx_block_time
+        , first_tx_block_number
+        , last_tx_block_number
+        , map_from_entries(array[
+            ('last_seen', CAST(last_seen AS varchar))
+            , ('last_seen_block', CAST(last_seen_block AS varchar))
+            , ('executed_tx_count', CAST(executed_tx_count AS varchar))
+            , ('is_smart_contract', CAST(is_smart_contract AS varchar))
+            , ('tokens_sent_count', CAST(tokens_sent_count AS varchar))
+            , ('tokens_received_count', CAST(tokens_received_count AS varchar))
+            ]) AS chain_stats
+        , last_seen
+        , last_seen_block
+        FROM (select * from {{ addresses_model[1] }}
+        ORDER BY address asc limit 2000000)
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+        {% endfor %}
+    )
     GROUP BY address
     )
 
