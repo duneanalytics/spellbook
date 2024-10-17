baeaaf2b687e6eb53d72081d653dd33d52aa83f2
jeff-dude <jeff@dune.xyz>
2024-10-17 17:55:39 -0400

    separate net transfers, update downstream

--- dbt_subprojects/daily_spellbook/models/_metrics/transactions/metrics_transactions_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/transactions/metrics_transactions_daily.sql
@@ -9,16 +9,45 @@
         )
 }}
 
+with net_transfers_filter as (
+    select 
+        blockchain
+        , block_date
+        , tx_hash
+        , net_transfer_amount_usd
+    from
+        {{ ref('tokens_net_transfers') }}
+    where
+        1 = 1
+        and net_transfer_amount_usd >= 1
+        {% if is_incremental() %}
+        and {{ incremental_predicate('block_date') }}
+        {% endif %}
+), filtered_tx as (
+    select
+        tx.blockchain
+        , date_trunc('day', tx.block_hour) as block_date
+        , tx.tx_count
+    from
+        {{ source('evms', 'transaction_metrics') }} as tx
+    inner join
+        net_transfers_filter as nt
+        on tx.blockchain = nt.blockchain
+        and date_trunc('day', tx.block_hour) = nt.block_date
+        and tx.tx_hash = nt.tx_hash
+    where
+        1 = 1
+        {% if is_incremental() %}
+        and {{ incremental_predicate('tx.block_hour') }}
+        {% endif %}
+)
+
 select
     blockchain
-    , date_trunc('day', block_hour) as block_date
+    , block_date
     , sum(tx_count) as tx_count
 from
-    {{ source('evms', 'transaction_metrics') }}
-{% if is_incremental() %}
-where
-    {{ incremental_predicate('block_hour') }}
-{% endif %}
+    filtered_tx
 group by
     blockchain
-    , date_trunc('day', block_hour)
+    , block_date
