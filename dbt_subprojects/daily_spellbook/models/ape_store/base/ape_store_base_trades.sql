6c4951289e5b54bc1d4b5156b70bfa67eb4a3fef
whale_hunter <143016036+whalehunting@users.noreply.github.com>
2024-10-17 08:47:52 +0200

    fix: add more columsn to uniqueness test

--- dbt_subprojects/daily_spellbook/models/ape_store/base/ape_store_base_trades.sql
+++ dbt_subprojects/daily_spellbook/models/ape_store/base/ape_store_base_trades.sql
@@ -6,7 +6,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-    unique_key = ['block_month', 'blockchain', 'tx_hash', 'tx_index']
+    unique_key = ['block_month', 'blockchain', 'tx_hash', 'tx_index', 'token_bought_address', 'token_sold_address']
    )
 }}
 
