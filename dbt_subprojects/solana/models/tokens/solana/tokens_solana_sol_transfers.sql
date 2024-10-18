698e754e67526220a207172185848d10d68f2ffa
Boxer <Floba.hs@gmail.com>
2024-10-18 13:27:23 +0200

    add sol transfers and add them to transfers + add prices to transfers

---
+++ dbt_subprojects/solana/models/tokens/solana/tokens_solana_sol_transfers.sql
@@ -0,0 +1,74 @@
+{{ config(
+    schema = 'tokens_solana',
+    alias = 'sol_transfers',
+    partition_by = ['block_month'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['tx_id', 'outer_executing_account', 'from_owner', 'to_owner'],
+    post_hook='{{ expose_spells(\'["solana"]\',
+                                "sector",
+                                "tokens",
+                                \'["0xBoxer"]\') }}'
+) }}
+
+WITH transfers AS (
+    SELECT
+        'solana' as blockchain,
+        call_block_time as block_time,
+        date_trunc('day', call_block_time) AS block_date,
+        date_trunc('month', call_block_time) AS block_month,
+        call_block_slot as block_slot,
+        call_tx_id as tx_id,
+        call_tx_index as tx_index,
+        call_inner_instruction_index as inner_instruction_index,
+        call_outer_instruction_index as outer_instruction_index,
+        call_tx_signer as tx_signer,
+        call_account_arguments[1] AS from_owner,
+        call_account_arguments[2] AS to_owner,
+        'native' as token_version,
+        'SOL' as symbol,
+        lamports as amount_raw,
+        lamports / 1e9 as amount,
+        'So11111111111111111111111111111111111111111' as token_mint_address,
+        call_outer_executing_account as outer_executing_account,
+        call_inner_executing_account as inner_executing_account,
+        'transfer' as action
+    FROM {{ source('system_program_solana', 'system_program_call_Transfer') }}
+    {% if is_incremental() %}
+    WHERE {{incremental_predicate('call_block_time')}}
+    {% endif %}
+    {% if not is_incremental() %}
+    WHERE call_block_time > now() - interval '5' hour
+    {% endif %}
+)
+
+SELECT
+    t.blockchain,
+    t.block_month,
+    t.block_date,
+    t.block_time,
+    t.block_slot,
+    t.tx_id,
+    t.tx_index,
+    t.inner_instruction_index,
+    t.outer_instruction_index,
+    t.tx_signer,
+    t.from_owner,
+    t.to_owner,
+    t.token_mint_address,
+    t.symbol,
+    t.amount_raw,
+    t.amount,
+    t.action,
+    t.outer_executing_account,
+    t.inner_executing_account,
+    t.token_version
+FROM transfers t
+where 1=1 
+{% if is_incremental() %}
+AND {{incremental_predicate('block_time')}}
+{% endif %}
+{% if not is_incremental() %}
+AND p.minute > now() - interval '5' hour
+{% endif %}
--- dbt_subprojects/solana/models/tokens/solana/tokens_solana_transfers.sql
+++ dbt_subprojects/solana/models/tokens/solana/tokens_solana_transfers.sql
@@ -11,7 +11,7 @@
         post_hook='{{ expose_spells(\'["solana"]\',
                                     "sector",
                                     "tokens_solana",
-                                    \'["ilemi"]\') }}')
+                                    \'["ilemi", "0xBoxer"]\') }}')
 }}
 
 
@@ -35,6 +35,9 @@ FROM {{ ref('tokens_solana_spl_transfers') }}
 {% if is_incremental() %}
 WHERE {{incremental_predicate('block_time')}}
 {% endif %}
+{% if not is_incremental() %}
+AND block_time > now() - interval '90' day
+{% endif %}
 UNION ALL
     SELECT
     block_time
@@ -54,8 +57,36 @@ FROM {{ ref('tokens_solana_token22_spl_transfers') }}
 {% if is_incremental() %}
 WHERE {{incremental_predicate('block_time')}}
 {% endif %}
+{% if not is_incremental() %}
+AND block_time > now() - interval '90' day
+{% endif %}
+UNION ALL
+    SELECT
+    block_time
+    , block_slot
+    , action
+    , amount
+    , cast (null as double) as fee
+    , from_owner
+    , to_owner
+    , token_version
+    , tx_signer
+    , tx_id
+    , outer_instruction_index
+    , inner_instruction_index
+    , outer_executing_account
+    , cast(null as string) as from_token_account
+    , cast(null as string) as to_token_account
+FROM {{ ref('tokens_solana_sol_transfers') }}
+{% if is_incremental() %}
+WHERE {{incremental_predicate('block_time')}}
+{% endif %}
+{% if not is_incremental() %}
+AND block_time > now() - interval '90' day
+{% endif %}
 )
 
+,final_transfers as (
 SELECT
     block_time
     , cast (date_trunc('day', block_time) as date) as block_date
@@ -79,4 +110,47 @@ FROM base tr
 LEFT JOIN {{ ref('solana_utils_token_accounts') }} tk_s ON tk_s.address = tr.from_token_account
 LEFT JOIN {{ ref('solana_utils_token_accounts') }} tk_d ON tk_d.address = tr.to_token_account
 WHERE 1=1
--- AND call_block_time > now() - interval '90' day --for faster CI testing
+{% if is_incremental() %}
+AND {{incremental_predicate('block_time')}}
+{% endif %}
+{% if not is_incremental() %}
+AND block_time > now() - interval '90' day
+{% endif %}
+)
+
+SELECT 
+    block_time
+    , block_date
+    , block_slot
+    , action
+    , amount
+    , fee
+    , token_mint_address
+    , from_owner
+    , to_owner
+    , from_token_account
+    , to_token_account
+    , token_version
+    , tx_signer
+    , tx_id
+    , outer_instruction_index
+    , inner_instruction_index
+    , outer_executing_account
+ FROM final_transfers
+ LEFT JOIN {{ source('prices', 'usd_forward_fill') }} p
+    ON p.blockchain = 'solana'
+    AND to_base58(p.contract_address) = token_mint_address
+    AND p.minute = date_trunc('minute', block_time)
+    {% if is_incremental() %}
+    AND {{incremental_predicate('p.minute')}}
+    {% endif %}
+    {% if not is_incremental() %}
+    AND p.minute > now() - interval '90' day
+    {% endif %}
+WHERE 1=1
+{% if is_incremental() %}
+AND {{incremental_predicate('block_time')}}
+{% endif %}
+{% if not is_incremental() %}
+AND block_time > now() - interval '90' day
+{% endif %}
