c5acacc9314ee38d1c1a7e8801666382973cf301
jeff-dude <jeff@dune.xyz>
2024-10-18 11:12:49 -0400

    switch source to tx_hash level

--- dbt_subprojects/daily_spellbook/models/_metrics/transactions/metrics_transactions_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/transactions/metrics_transactions_daily.sql
@@ -9,44 +9,53 @@
         )
 }}
 
-with net_transfers_filter as (
+with raw_tx as (
+    select
+        blockchain
+        , block_date
+        , tx_hash
+    from
+        {{ source('evms', 'transactions') }}
+    where
+        1 = 1
+        and block_hour >= timestamp '2024-10-01'
+        {% if is_incremental() %}
+        and {{ incremental_predicate('block_date') }}
+        {% endif %}
+    group by
+        blockchain
+        , block_date
+        , tx_hash
+), net_transfers_filter as (
     select 
         blockchain
         , block_date
         , tx_hash
-        , net_transfer_amount_usd
     from
         {{ ref('tokens_net_transfers') }}
     where
         1 = 1
-        and net_transfer_amount_usd >= 1
+        and net_transfer_amount_usd >= 1 --only include tx's where transfer value is greater at least $1
         {% if is_incremental() %}
         and {{ incremental_predicate('block_date') }}
         {% endif %}
 ), filtered_tx as (
     select
         tx.blockchain
-        , date_trunc('day', tx.block_hour) as block_date
-        , tx.tx_count
+        , tx.block_date
+        , tx.tx_hash
     from
-        {{ source('evms', 'transaction_metrics') }} as tx
+        raw_tx as tx
     inner join
         net_transfers_filter as nt
         on tx.blockchain = nt.blockchain
-        and date_trunc('day', tx.block_hour) = nt.block_date
+        and tx.block_date = nt.block_date
         and tx.tx_hash = nt.tx_hash
-    where
-        1 = 1
-        and tx.block_hour >= timestamp '2024-10-01'
-        {% if is_incremental() %}
-        and {{ incremental_predicate('tx.block_hour') }}
-        {% endif %}
 )
-
 select
     blockchain
     , block_date
-    , sum(tx_count) as tx_count
+    , sum(tx_hash) as tx_count
 from
     filtered_tx
 group by
