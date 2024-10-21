966e5b62ed973c8dc41b26306d62120dd9014715
Boxer <Floba.hs@gmail.com>
2024-10-21 10:49:57 +0200

    fix incremental predicate

--- dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_positions_raw.sql
+++ dbt_subprojects/daily_spellbook/models/polymarket/polygon/polymarket_polygon_positions_raw.sql
@@ -6,7 +6,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     unique_key = ['address', 'token_address', 'token_id', 'day'],
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')],
     post_hook = '{{ expose_spells(blockchains = \'["polygon"]\',
                                   spell_type = "project",
                                   spell_name = "polymarket",
