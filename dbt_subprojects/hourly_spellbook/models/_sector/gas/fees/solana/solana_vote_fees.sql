2a51b9d3c38945139e0f8ffdea97446864a695a1
Boxer <Floba.hs@gmail.com>
2024-10-23 15:51:40 +0200

    add block_hour partition

--- dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/solana/solana_vote_fees.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/solana/solana_vote_fees.sql
@@ -1,7 +1,7 @@
 {{ config(
     schema = 'gas_solana',
     alias = 'vote_fees',
-    partition_by = ['block_date'],
+    partition_by = ['block_date', 'block_hour'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'delete+insert',
