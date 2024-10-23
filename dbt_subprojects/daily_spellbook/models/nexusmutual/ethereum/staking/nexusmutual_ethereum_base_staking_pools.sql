0f8456888f7088a9c5215d635c6d479acba0cf0d
tomfutago <tomfutago@gmail.com>
2024-10-23 11:03:41 +0100

    fix: remove redundant tokencontroller refs

--- dbt_subprojects/daily_spellbook/models/nexusmutual/ethereum/staking/nexusmutual_ethereum_base_staking_pools.sql
+++ dbt_subprojects/daily_spellbook/models/nexusmutual/ethereum/staking/nexusmutual_ethereum_base_staking_pools.sql
@@ -150,24 +150,6 @@ staking_pool_managers_history as (
   from {{ source('nexusmutual_ethereum', 'TokenController_call_assignStakingPoolManager') }}
   where call_success
   union all
-  select
-    call_block_time as block_time,
-    poolId as pool_id,
-    manager,
-    call_trace_address,
-    call_tx_hash as tx_hash
-  from {{ source('nexusmutual_ethereum', 'TokenController2_call_assignStakingPoolManager') }}
-  where call_success
-  union all
-  select
-    call_block_time as block_time,
-    poolId as pool_id,
-    manager,
-    call_trace_address,
-    call_tx_hash as tx_hash
-  from {{ source('nexusmutual_ethereum', 'TokenController3_call_assignStakingPoolManager') }}
-  where call_success
-  union all
   select distinct
     m.call_block_time as block_time,
     sp.poolId as pool_id,
