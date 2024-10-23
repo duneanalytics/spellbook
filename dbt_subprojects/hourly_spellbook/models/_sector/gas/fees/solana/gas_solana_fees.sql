378c76ea2a0696602d79413ec78081ee9148338a
Boxer <Floba.hs@gmail.com>
2024-10-23 15:41:32 +0200

    change gas_solana.fees to a view

--- dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/solana/gas_solana_fees.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/solana/gas_solana_fees.sql
@@ -1,39 +1,58 @@
 {{ config(
     schema = 'gas_solana',
     alias = 'fees',
-    partition_by = ['block_date'],
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'delete+insert',
-    unique_key = ['block_date', 'block_slot', 'tx_index', 'tx_type']
+    materialized = 'view'
 ) }}
 
-WITH combined_fees AS (
+WITH all_fees AS (
     SELECT
-        'normal' as tx_type,
-        *
+        blockchain,
+        block_month,
+        block_date,
+        block_time,
+        block_slot,
+        tx_index,
+        tx_hash,
+        signer,
+        compute_unit_price,
+        compute_limit,
+        currency_symbol,
+        tx_fee_raw,
+        tx_fee,
+        tx_fee_usd,
+        tx_fee_breakdown_raw,
+        tx_fee_breakdown,
+        tx_fee_breakdown_usd,
+        tx_fee_currency,
+        leader,
+        'regular' AS tx_type
     FROM {{ ref('gas_solana_tx_fees') }}
-    {% if is_incremental() %}
-    WHERE {{ incremental_predicate('block_date') }}
-    {% else %}
-    WHERE block_date >= DATE_TRUNC('DAY', CURRENT_DATE) - INTERVAL '30' DAY
-    {% endif %}
 
     UNION ALL
 
     SELECT
-        'vote' as tx_type,
-        f.*
-    FROM {{ ref('gas_solana_tx_fees') }} f
-    JOIN {{ ref('solana_vote_fees') }} vf
-        ON f.tx_hash = vf.tx_hash
-        AND f.block_slot = vf.block_slot
-    {% if is_incremental() %}
-    WHERE {{ incremental_predicate('f.block_date') }}
-    {% else %}
-    WHERE f.block_date >= DATE_TRUNC('DAY', CURRENT_DATE) - INTERVAL '30' DAY
-    {% endif %}
+        blockchain,
+        block_month,
+        block_date,
+        block_time,
+        block_slot,
+        tx_index,
+        tx_hash,
+        signer,
+        compute_unit_price,
+        compute_limit,
+        currency_symbol,
+        tx_fee_raw,
+        tx_fee,
+        tx_fee_usd,
+        tx_fee_breakdown_raw,
+        tx_fee_breakdown,
+        tx_fee_breakdown_usd,
+        tx_fee_currency,
+        leader,
+        tx_type
+    FROM {{ ref('solana_vote_fees') }}
 )
 
 SELECT *
-FROM combined_fees
+FROM all_fees
