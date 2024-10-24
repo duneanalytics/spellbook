3e6e7ad29d6a6c5766be2b53e8b92b5b1f0fb16b
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-10-24 08:56:14 -0400

    need to make this table also refresh fully not incremental

--- dbt_subprojects/solana/models/_sector/staking/solana/staking_solana_validator_stake_account_epochs.sql
+++ dbt_subprojects/solana/models/_sector/staking/solana/staking_solana_validator_stake_account_epochs.sql
@@ -2,10 +2,8 @@
     schema = 'staking_solana'
     , alias = 'validator_stake_account_epochs'
     , materialized = 'incremental'
-    , file_format = 'delta'
-    , incremental_strategy = 'merge'
+    , file_format = 'table'
     , unique_key = ['epoch', 'epoch_time', 'stake_account', 'vote_account']
-    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.epoch_time')]
     , post_hook='{{ expose_spells(\'["solana"]\',
                                 "sector",
                                 "staking",
@@ -24,7 +22,4 @@ FROM {{ ref('staking_solana_validator_stake_account_epochs_raw')}} b
 LEFT JOIN {{ ref('solana_utils_daily_balances')}} bal ON bal.address = b.stake_account 
     AND bal.token_mint_address is null
     AND bal.day <= date_trunc('day', b.epoch_time)
-{% if is_incremental() %}
-WHERE {{incremental_predicate('b.epoch_time')}}
-{% endif %}
 GROUP BY 1,2,3,4,5,6
