9d9dae6fc8b4dcfeab266a819fad1dfecb84ed7b
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-10-24 14:14:30 +0200

    enable compute_unit_price

--- dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/solana/solana_compute_unit_price.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/solana/solana_compute_unit_price.sql
@@ -1,7 +1,6 @@
 {{ config(
     schema = 'gas_solana',
     alias = 'compute_unit_price',
-    tags = ['prod_exclude'],
     partition_by = ['block_date', 'block_hour'],
     materialized = 'incremental',
     file_format = 'delta',
