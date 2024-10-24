68268a47de3b794eb8457e8ae43c5549df99e78b
0xBoxer <71930326+0xBoxer@users.noreply.github.com>
2024-10-24 12:53:19 +0200

    Split Solana fees (#6999)
    * try append incremental
    
    * test
    
    * run delete+insert
    
    * remove predicates
    
    * run merge
    
    * delete+insert with date as partition and incremental filter
    
    * syntax
    
    * set incremental predicates
    
    * revert incremental predicates and enable full history
    
    * init
    
    * fix
    
    * limit to last 30d
    
    * change join conditions and fix vote fess
    
    * change gas_solana.fees to a view
    
    * add block_hour partition
    
    * add block_hour to tables
    
    * readd old model to compare runtimes
    
    * fix
    
    * remove old model
    
    * add comments about decoding pipeline
    
    * add incremental predicates on joins
    
    ---------
    
    Co-authored-by: 0xRob <83790096+0xRobin@users.noreply.github.com>

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/solana/solana_compute_limit.sql
@@ -0,0 +1,32 @@
+{{ config(
+    schema = 'gas_solana',
+    alias = 'compute_limit',
+    partition_by = ['block_date'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'delete+insert',
+    unique_key = ['block_date', 'block_slot', 'tx_id']
+) }}
+
+-- this is just decoding program data, could be moved into decoding pipeline
+
+SELECT
+    tx_id,
+    block_date,
+    block_time,
+    block_slot,
+    tx_index,
+    bytearray_to_bigint(
+        bytearray_reverse(
+            bytearray_substring(data, 2, 8)
+        )
+    ) as compute_limit
+FROM {{ source('solana', 'instruction_calls') }}
+WHERE executing_account = 'ComputeBudget111111111111111111111111111111'
+AND bytearray_substring(data,1,1) = 0x02
+AND inner_instruction_index is null
+{% if is_incremental() %}
+    AND {{ incremental_predicate('block_date') }}
+{% else %}
+    AND block_date >= DATE_TRUNC('DAY', CURRENT_DATE) - INTERVAL '30' DAY
+{% endif %}
