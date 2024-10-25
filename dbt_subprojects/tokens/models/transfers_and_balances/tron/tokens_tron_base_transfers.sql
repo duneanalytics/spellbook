ea2c9508f6033dd2650da9bb8622cf5c68f91363
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-10-25 16:05:30 +0200

    only include trc20 transfers

--- dbt_subprojects/tokens/models/transfers_and_balances/tron/tokens_tron_base_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/tron/tokens_tron_base_transfers.sql
@@ -10,11 +10,34 @@
 )
 }}
 
-{{transfers_base(
-    blockchain='tron',
-    traces = source('tron','traces'),
-    transactions = source('tron','transactions'),
-    erc20_transfers = source('erc20_tron','evt_transfer'),
-    native_contract_address = null
-)
-}}
+-- this is only including TRC20 (ERC20) transfers
+SELECT
+    {{dbt_utils.generate_surrogate_key(['t.block_number', 'tx.index', 't.evt_index'])}} as unique_key
+    , 'tron' as blockchain
+    , cast(date_trunc('month', t.evt_block_time) as date) AS block_month
+    , cast(date_trunc('day', t.evt_block_time) as date) AS block_date
+    , t.evt_block_time AS block_time
+    , t.evt_block_number AS block_number
+    , t.evt_tx_hash AS tx_hash
+    , t.evt_index
+    , CAST(NULL AS ARRAY<BIGINT>) AS trace_address
+    , t.contract_address
+    , 'trc20' AS token_standard
+    , t."from"
+    , t.to
+    , tx."index" AS tx_index
+    , tx."from" as tx_from
+    , tx.to as tx_to
+    , t.contract_address
+    , t.value AS amount_raw
+FROM {{ erc20_transfers }} t
+INNER JOIN {{ transactions }} tx ON
+    tx.block_date = t.block_date -- partition column in raw base tables (transactions)
+    AND tx.block_number = t.block_number
+    AND tx.hash = t.tx_hash
+    {% if is_incremental() %}
+    AND {{incremental_predicate('tx.block_time')}}
+    {% endif %}
+{% if is_incremental() %}
+WHERE {{incremental_predicate('t.evt_block_time')}}
+{% endif %}
