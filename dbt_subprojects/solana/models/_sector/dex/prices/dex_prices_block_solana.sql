bbabaac0c5645b4ff050e7adc91b1e728efec283
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2024-10-25 12:37:23 -0700

    Move to solana subproject

--- dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
+++ dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
@@ -1,6 +1,6 @@
 {{ config(
-    schema = 'dex'
-    , alias = 'prices_block_solana'
+    schema = 'dex_solana'
+    , alias = 'prices_block'
     , partition_by = ['block_month']
     , materialized = 'incremental'
     , file_format = 'delta'
