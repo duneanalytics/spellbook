256a8d92ac94df47788ae7f1a9df134bfc5e1dd7
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-10-30 20:59:35 +0100

    add tron correction to fees metrics

--- dbt_subprojects/daily_spellbook/models/_metrics/fees/metrics_gas_fees_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/fees/metrics_gas_fees_daily.sql
@@ -9,6 +9,7 @@
         )
 }}
 
+with daily_data as (
 select
     blockchain
     , block_date
@@ -22,3 +23,14 @@ where
 group by
     blockchain
     , block_date
+)
+
+select
+    blockchain
+    ,block_date
+    ,case when blockchain = 'tron' then gas_fees_usd * coalesce(trx_ration,0.0) -- apply correction to account for subsidized fees
+    else gas_fees_usd end
+    as gas_fees_usd
+from daily_data
+left join {{ref('tron_fee_correction')}}
+on block_date = day and blockchain = 'tron'
