062957d15e08411d948444d7a0e9e6a7df8f9301
jeff-dude <jeff@dune.xyz>
2024-10-30 11:52:01 -0400

    add solana to fees metrics

--- dbt_subprojects/daily_spellbook/models/_metrics/fees/metrics_gas_fees_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/fees/metrics_gas_fees_daily.sql
@@ -9,16 +9,67 @@
         )
 }}
 
+with evm_fees as (
+    select
+        blockchain
+        , block_date
+        , sum(tx_fee_usd) as gas_fees_usd
+    from
+        {{ source('gas', 'fees') }}
+    {% if is_incremental() %}
+    where
+        {{ incremental_predicate('block_date') }}
+    {% endif %}
+    group by
+        blockchain
+        , block_date
+), solana_regular_fees as (
+    select
+        blockchain
+        , block_date
+        , sum(tx_fee_usd) as gas_fees_usd
+    from
+        {{ source('gas_solana', 'fees') }}
+    {% if is_incremental() %}
+    where
+        {{ incremental_predicate('block_date') }}
+    {% endif %}
+    group by
+        blockchain
+        , block_date
+), solana_vote_fees as (
+    select
+        blockchain
+        , block_date
+        , sum(tx_fee_usd) as gas_fees_usd
+    from
+        {{ source('gas_solana', 'vote_fees') }}
+    {% if is_incremental() %}
+    where
+        {{ incremental_predicate('block_date') }}
+    {% endif %}
+    group by
+        blockchain
+        , block_date
+), solana_fees as (
+    select
+        srf.blockchain
+        , srf.block_date
+        , srf.gas_fees_usd + svf.gas_fees_usd as gas_fees_usd
+    from
+        solana_regular_fees as srf
+    inner join
+        solana_vote_fees as svf
+    on
+        srf.block_date = svf.block_date
+        and srf.blockchain = svf.blockchain
+)
 select
-    blockchain
-    , block_date
-    , sum(tx_fee_usd) as gas_fees_usd
+    *
 from
-    {{ source('gas', 'fees') }}
-{% if is_incremental() %}
-where
-    {{ incremental_predicate('block_date') }}
-{% endif %}
-group by
-    blockchain
-    , block_date
+    evm_fees
+union all
+select
+    *
+from
+    solana_fees
