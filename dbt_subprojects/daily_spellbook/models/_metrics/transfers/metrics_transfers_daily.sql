a048367619b3757844b82cce0249f7bfa88e2ad8
jeff-dude <jeff@dune.xyz>
2024-10-30 22:13:04 -0400

    initiate solana transfers metrics

--- dbt_subprojects/daily_spellbook/models/_metrics/transfers/metrics_transfers_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/transfers/metrics_transfers_daily.sql
@@ -9,12 +9,13 @@
         )
 }}
 
-select 
-    blockchain
-    , block_date
-    , sum(transfer_amount_usd_sent) as transfer_amount_usd_sent
-    , sum(transfer_amount_usd_received) as transfer_amount_usd_received
-    , sum(net_transfer_amount_usd) as net_transfer_amount_usd
+with evm as (
+    select 
+        blockchain
+        , block_date
+        , sum(transfer_amount_usd_sent) as transfer_amount_usd_sent
+        , sum(transfer_amount_usd_received) as transfer_amount_usd_received
+        , sum(net_transfer_amount_usd) as net_transfer_amount_usd
 from
     {{ ref('metrics_net_transfers') }}
 where
@@ -23,5 +24,32 @@ where
     and {{ incremental_predicate('block_date') }}
     {% endif %}
 group by
-    blockchain
-    , block_date
+        blockchain
+        , block_date
+), solana as (
+    select 
+        blockchain
+        , block_date
+        , sum(transfer_amount_usd_sent) as transfer_amount_usd_sent
+        , sum(transfer_amount_usd_received) as transfer_amount_usd_received
+        , sum(net_transfer_amount_usd) as net_transfer_amount_usd
+    from
+        {{ ref('metrics_net_solana_transfers') }}
+    where
+        1 = 1
+        {% if is_incremental() %}
+        and {{ incremental_predicate('block_date') }}
+        {% endif %}
+    group by
+        blockchain
+        , block_date
+)
+select 
+    * 
+from
+    evm
+union all
+select
+    * 
+from
+    solana
