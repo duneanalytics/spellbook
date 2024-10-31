1dfd9393db2db78db0fc16ff36f5b7869e669def
Huang Geyang <Sukebeta@outlook.com>
2024-10-31 17:38:40 +0800

    Implement transfers_enrich_beta

---
+++ dbt_subprojects/tokens/models/transfers_and_balances/blast/tokens_blast_transfers_beta.sql
@@ -0,0 +1,26 @@
+{{config(
+    schema = 'tokens_blast',
+    alias = 'transfers_beta',
+    partition_by = ['block_month'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_date','unique_key'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
+    post_hook='{{ expose_spells(\'["blast"]\',
+                                "sector",
+                                "tokens",
+                                \'["aalan3", "jeff-dude", "hildobby"]\') }}'
+)
+}}
+
+{{
+    transfers_enrich_beta(
+        base_transfers = ref('tokens_blast_base_transfers')
+        , tokens_erc20_model = source('tokens', 'erc20')
+        , prices_model = source('prices', 'hour')
+        , evms_info_model = source('evms','info')
+        , transfers_start_date = '2020-04-22'
+        , blockchain = 'blast'
+    )
+}}
