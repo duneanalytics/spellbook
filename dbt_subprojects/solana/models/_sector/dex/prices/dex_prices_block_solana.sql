4d327d35db5beef233b7b06d75e6a3640213385e
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2024-10-31 09:43:22 -0700

    Update dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
+++ dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
@@ -90,7 +90,7 @@ dex_sold as (
     inner join {{ ref('tokens_solana_fungible') }} as t
         on d.token_sold_mint_address = t.token_mint_address
     left join {{ source('prices', 'trusted_tokens') }} as ptt
-        on d.blockchain = 'solana'
+        on d.blockchain = ptt.blockchain
         and d.token_sold_mint_address = toBase58(ptt.contract_address)
     where
         1 = 1
