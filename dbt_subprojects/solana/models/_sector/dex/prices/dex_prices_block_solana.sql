649e11cb2f31b642442e7187e8642d9f47da208e
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2024-10-31 09:43:52 -0700

    Update dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
+++ dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
@@ -1,7 +1,7 @@
 {{ config(
     schema = 'dex_solana'
     , alias = 'prices_block'
-    , partition_by = ['block_month']
+    , partition_by = ['block_date']
     , materialized = 'incremental'
     , file_format = 'delta'
     , incremental_strategy = 'delete+insert'
