868659453d4f0abc59c364222910849edeb9bdd0
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-10-31 14:04:17 +0100

    clean up test models (#7080)
    * add example models
    
    * fix ref
    
    * error due div by zero
    
    * exclude from PROD
    
    * clean up test models
    
    * actually delete models

--- dbt_subprojects/hourly_spellbook/models/_project/dbt_cloud_test/dbt_cloud_test_good_model.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-        schema = 'dbt_cloud_test'
-        ,tags = ['prod_exclude']
-        ,alias = 'good_model'
-        ,materialized = 'table'
-        )
-}}
-select 'this_model_works' as comment
-, now() as last_executed
