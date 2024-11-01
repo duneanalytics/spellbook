14d60916614827bd86b73565411bae0b8e3b37e2
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-11-01 08:08:53 +0100

    make index models views

--- dbt_subprojects/daily_spellbook/models/_metrics/dune_index/metrics_dune_index_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/dune_index/metrics_dune_index_daily.sql
@@ -1,11 +1,7 @@
 {{ config(
         schema = 'metrics'
         , alias = 'dune_index_daily'
-        , materialized = 'incremental'
-        , file_format = 'delta'
-        , incremental_strategy = 'merge'
-        , unique_key = ['blockchain', 'block_date']
-        , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
+        , materialized = 'view'
         )
 }}
 
