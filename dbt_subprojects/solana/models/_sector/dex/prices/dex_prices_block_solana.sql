331d22430ef94b97971d648244fc2c0fcefd91eb
Alex Courouble <56831527+couralex6@users.noreply.github.com>
2024-11-01 12:42:23 -0700

    Remove ci time limit

--- dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
+++ dbt_subprojects/solana/models/_sector/dex/prices/dex_prices_block_solana.sql
@@ -27,7 +27,6 @@ with dex_trades_raw as (
     where
         1 = 1
         and amount_usd > 0
-        and block_time >= now() - interval '1' month
         {% if is_incremental() %}
         and {{ incremental_predicate('block_time') }}
         {% endif %}
