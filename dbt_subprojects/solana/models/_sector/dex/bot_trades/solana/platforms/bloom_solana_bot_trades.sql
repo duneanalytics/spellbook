723f503ebab52f0de9f02b22093fb511a1ebc2fc
clizzard7 <145153355+clizzard7@users.noreply.github.com>
2024-11-06 21:59:45 +0100

    corrected sql code

--- dbt_subprojects/solana/models/_sector/dex/bot_trades/solana/platforms/bloom_solana_bot_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/bot_trades/solana/platforms/bloom_solana_bot_trades.sql
@@ -1,124 +1,151 @@
-{{
-    config(
-        alias='bot_trades',
-        schema='bloom_solana',
-        partition_by=['block_month'],
-        materialized='incremental',
-        file_format='delta',
-        incremental_strategy='merge',
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-        unique_key=[
-            'blockchain',
-            'tx_id',
-            'tx_index',
-            'outer_instruction_index',
-            'inner_instruction_index',
-        ]
-    )
+{{ config(
+    alias = 'bot_trades',
+    schema = 'bloom_solana',
+    partition_by = ['block_month'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    unique_key = ['blockchain', 'tx_id', 'tx_index', 'outer_instruction_index', 'inner_instruction_index']
+   )
 }}
 
 {% set project_start_date = '2024-10-22' %}
-{% set fee_receiver = '7HeD6sLLqAnKVRuSfc1Ko3BSPMNKWgGTiWLKXJF31vKM' %}
+{% set fee_receiver_1 = '7HeD6sLLqAnKVRuSfc1Ko3BSPMNKWgGTiWLKXJF31vKM' %}
 {% set wsol_token = 'So11111111111111111111111111111111111111112' %}
 
-with
-    all_fee_payments as (
-        select * from {{ref('bloom_solana_bot_users')}}
-    ),
-    bot_trades as (
-        select
-            trades.block_time,
-            cast(date_trunc('day', trades.block_time) as date) as block_date,
-            cast(date_trunc('month', trades.block_time) as date) as block_month,
-            'solana' as blockchain,
-            amount_usd,
-            if(token_sold_mint_address = '{{wsol_token}}', 'Buy', 'Sell') as type,
-            token_bought_amount,
-            token_bought_symbol,
-            token_bought_mint_address as token_bought_address,
-            token_sold_amount,
-            token_sold_symbol,
-            token_sold_mint_address as token_sold_address,
-            0 as fee_usd,
-            0 AS fee_token_amount,
-            'SOL' as fee_token_symbol,
-            '{{wsol_token}}' as fee_token_address,
-            project,
-            version,
-            token_pair,
-            project_program_id as project_contract_address,
-            trader_id as user,
-            trades.tx_id,
-            tx_index,
-            outer_instruction_index,
-            inner_instruction_index
-        from {{ ref('dex_solana_trades') }} as trades
-        join all_fee_payments on trades.trader_id = all_fee_payments.user
-        join
-            {{ source('solana', 'transactions') }} as transactions
-            on (
-                trades.block_time = transactions.block_time
-                and trades.tx_id = id
-                {% if is_incremental() %}
-                    and {{ incremental_predicate('transactions.block_time') }}
-                    and {{ incremental_predicate('trades.block_time') }}
-                {% else %}
-                    and transactions.block_time >= timestamp '{{project_start_date}}'
-                    and trades.block_time >= timestamp '{{project_start_date}}'
-                {% endif %}
-            )
-        where
-            trades.trader_id != '{{fee_receiver}}'  -- Exclude trades signed by FeeWallet
-            and transactions.signer != '{{fee_receiver}}'  -- Exclude trades signed by FeeWallet
-    ),
-    highest_inner_instruction_index_for_each_trade as (
-        select
-            tx_id,
-            outer_instruction_index,
-            max(inner_instruction_index) as highest_inner_instruction_index
-        from bot_trades
-        group by tx_id, outer_instruction_index
-    )
-select
-    block_time,
-    block_date,
-    block_month,
-    'Bloom' as bot,
-    blockchain,
-    amount_usd,
-    type,
-    token_bought_amount,
-    token_bought_symbol,
-    token_bought_address,
-    token_sold_amount,
-    token_sold_symbol,
-    token_sold_address,
-    fee_usd,
-    fee_token_amount,
-    fee_token_symbol,
-    fee_token_address,
-    project,
-    version,
-    token_pair,
-    project_contract_address,
-    user,
-    bot_trades.tx_id,
-    tx_index,
-    bot_trades.outer_instruction_index,
-    coalesce(inner_instruction_index, 0) as inner_instruction_index,
-    if(
-        inner_instruction_index = highest_inner_instruction_index, true, false
-    ) as is_last_trade_in_transaction
-from bot_trades
-join
-    highest_inner_instruction_index_for_each_trade
-    on (
-        bot_trades.tx_id = highest_inner_instruction_index_for_each_trade.tx_id
-        and bot_trades.outer_instruction_index
-        = highest_inner_instruction_index_for_each_trade.outer_instruction_index
-    )
-order by
-    block_time desc,
-    tx_index desc,
-    outer_instruction_index desc,
-    inner_instruction_index desc
+WITH
+  allFeePayments AS (
+    SELECT
+      tx_id,
+      'SOL' AS feeTokenType,
+      balance_change / 1e9 AS fee_token_amount,
+      '{{wsol_token}}' AS fee_token_mint_address
+    FROM
+      {{ source('solana','account_activity') }}
+    WHERE
+      {% if is_incremental() %}
+      {{ incremental_predicate('block_time') }}
+      {% else %}
+      block_time >= TIMESTAMP '{{project_start_date}}'
+      {% endif %}
+      AND tx_success
+      AND balance_change > 0
+      AND address = '{{fee_receiver_1}}'
+        
+  ),
+  botTrades AS (
+    SELECT
+      trades.block_time,
+      CAST(date_trunc('day', trades.block_time) AS date) AS block_date,
+      CAST(date_trunc('month', trades.block_time) AS date) AS block_month,
+      'solana' AS blockchain,
+      amount_usd,
+      IF(
+        token_sold_mint_address = '{{wsol_token}}',
+        'Buy',
+        'Sell'
+      ) AS type,
+      token_bought_amount,
+      token_bought_symbol,
+      token_bought_mint_address AS token_bought_address,
+      token_sold_amount,
+      token_sold_symbol,
+      token_sold_mint_address AS token_sold_address,
+      fee_token_amount * price AS fee_usd,
+      fee_token_amount,
+      IF(feeTokenType = 'SOL', 'SOL', symbol) AS fee_token_symbol,
+      fee_token_mint_address AS fee_token_address,
+      project,
+      version,
+      token_pair,
+      project_program_id AS project_contract_address,
+      trader_id AS user,
+      trades.tx_id,
+      tx_index,
+      outer_instruction_index,
+      inner_instruction_index
+    FROM
+      {{ ref('dex_solana_trades') }} AS trades
+      JOIN allFeePayments AS feePayments ON trades.tx_id = feePayments.tx_id
+      LEFT JOIN {{ source('prices', 'usd') }} AS feeTokenPrices ON (
+        feeTokenPrices.blockchain = 'solana'
+        AND fee_token_mint_address = toBase58 (feeTokenPrices.contract_address)
+        AND date_trunc('minute', block_time) = minute
+        {% if is_incremental() %}
+        AND {{ incremental_predicate('minute') }}
+        {% else %}
+        AND minute >= TIMESTAMP '{{project_start_date}}'
+        {% endif %}
+      )
+      JOIN {{ source('solana','transactions') }} AS transactions ON (
+        trades.tx_id = id
+        {% if is_incremental() %}
+        AND {{ incremental_predicate('transactions.block_time') }}
+        {% else %}
+        AND transactions.block_time >= TIMESTAMP '{{project_start_date}}'
+        {% endif %}
+      )
+    WHERE
+      trades.trader_id != '{{fee_receiver_1}}' -- Exclude trades signed by FeeWallet
+      AND transactions.signer != '{{fee_receiver_1}}' -- Exclude trades signed by FeeWallet
+      {% if is_incremental() %}
+      AND {{ incremental_predicate('trades.block_time') }}
+      {% else %}
+      AND trades.block_time >= TIMESTAMP '{{project_start_date}}'
+      {% endif %}
+  ),
+  highestInnerInstructionIndexForEachTrade AS (
+    SELECT
+      tx_id,
+      outer_instruction_index,
+      MAX(inner_instruction_index) AS highestInnerInstructionIndex
+    FROM
+      botTrades
+    GROUP BY
+      tx_id,
+      outer_instruction_index
+  )
+SELECT
+  block_time,
+  block_date,
+  block_month,
+  'Bloom' as bot,
+  blockchain,
+  amount_usd,
+  type,
+  token_bought_amount,
+  token_bought_symbol,
+  token_bought_address,
+  token_sold_amount,
+  token_sold_symbol,
+  token_sold_address,
+  fee_usd,
+  fee_token_amount,
+  fee_token_symbol,
+  fee_token_address,
+  project,
+  version,
+  token_pair,
+  project_contract_address,
+  user,
+  botTrades.tx_id,
+  tx_index,
+  botTrades.outer_instruction_index,
+  COALESCE(inner_instruction_index, 0) AS inner_instruction_index,
+  IF(
+    inner_instruction_index = highestInnerInstructionIndex,
+    true,
+    false
+  ) AS is_last_trade_in_transaction
+FROM
+  botTrades
+  JOIN highestInnerInstructionIndexForEachTrade ON (
+    botTrades.tx_id = highestInnerInstructionIndexForEachTrade.tx_id
+    AND botTrades.outer_instruction_index = highestInnerInstructionIndexForEachTrade.outer_instruction_index
+  )
+ORDER BY
+  block_time DESC,
+  tx_index DESC,
+  outer_instruction_index DESC,
+  inner_instruction_index DESC
