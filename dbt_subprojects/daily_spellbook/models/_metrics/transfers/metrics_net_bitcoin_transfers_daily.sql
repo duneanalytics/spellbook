5b599a2476e6f8232399c4a275ccbcf84ccbf2be
jeff-dude <jeff@dune.xyz>
2024-11-06 20:55:53 +0700

    initiate net transfers

--- dbt_subprojects/daily_spellbook/models/_metrics/transfers/metrics_net_bitcoin_transfers_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/transfers/metrics_net_bitcoin_transfers_daily.sql
@@ -1,38 +1,36 @@
 {{ config(
         schema = 'metrics'
-        , alias = 'net_bitcoin_transfers'
+        , alias = 'net_bitcoin_transfers_daily'
         , materialized = 'incremental'
         , file_format = 'delta'
         , incremental_strategy = 'merge'
-        , unique_key = ['blockchain', 'block_date', 'tx_hash']
+        , unique_key = ['blockchain', 'block_date']
         , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
         )
 }}
 
+
 with raw_transfers as (
-    /*
-    - get the transfer amount sent per address in a transaction hash, set the amount sent as a negative value
-    - union the transfer amount received per address in a transaction hash, keep the amount received as a positve value
-    */
     select
         'bitcoin' as blockchain
         , block_date
-        , tx_id
-        , "from" as address
+        , wallet_address as address
         , 'sent' as transfer_direction
-        , (sum(amount_usd) * -1) as transfer_amount_usd
+        , (sum(abs(amount_transfer_usd)) * -1) as transfer_amount_usd
     from
-        {{ source('tokens', 'transfers') }}
+        {{ source('transfers_bitcoin', 'satoshi') }}
     where
         1 = 1
+        and type = 'input'
+        and tx_id = 0x06959162c165c7eb10b42d30f70ff82da89b5c1aaff5c8495eaf7cb0fc422670
+        and block_date = date '2024-10-15'
         {% if is_incremental() %}
         and {{ incremental_predicate('block_date') }}
         {% endif %}
     group by
         blockchain
         , block_date
-        , tx_hash
-        , "from"
+        , wallet_address
         , 'sent'
 
     union all
@@ -40,22 +38,23 @@ with raw_transfers as (
     select
         'bitcoin' as blockchain
         , block_date
-        , tx_hash
-        , to as address
+        , wallet_address as address
         , 'received' as transfer_direction
-        , sum(amount_usd) as transfer_amount_usd
+        , sum(abs(amount_transfer_usd)) as transfer_amount_usd
     from
-        {{ source('tokens', 'transfers') }}
+        {{ source('transfers_bitcoin', 'satoshi') }}
     where
         1 = 1
+        and type = 'output'
+        and tx_id = 0x06959162c165c7eb10b42d30f70ff82da89b5c1aaff5c8495eaf7cb0fc422670
+        and block_date = date '2024-10-15'
         {% if is_incremental() %}
         and {{ incremental_predicate('block_date') }}
         {% endif %}
     group by
         blockchain
         , block_date
-        , tx_hash
-        , to
+        , wallet_address
         , 'received'
 ), labels as (
     select
@@ -69,13 +68,9 @@ with raw_transfers as (
         {{ source('labels', 'owner_details') }} as od
         on oa.owner_key = od.owner_key
 ), transfers_amount as (
-    /*
-    - create one column for transfer amount received, one column for transfer amount sent
-    */
     select
         t.blockchain
         , t.block_date
-        , t.tx_hash
         , coalesce(l.owner_key, cast(t.address as varchar)) as address_owner
         , sum(case when t.transfer_direction = 'sent' then t.transfer_amount_usd else 0 end) as transfer_amount_usd_sent
         , sum(case when t.transfer_direction = 'received' then t.transfer_amount_usd else 0 end) as transfer_amount_usd_received
@@ -84,23 +79,17 @@ with raw_transfers as (
     left join
         labels as l
         on t.blockchain = l.blockchain
-        and t.address = l.address
+        and cast(t.address as varbinary) = l.address
     where
         coalesce(l.primary_category, 'n/a') not in ('Hacks and exploits', 'Social Engineering Scams') -- filter out scam addresses
     group by
         t.blockchain
         , t.block_date
-        , t.tx_hash
         , coalesce(l.owner_key, cast(t.address as varchar))
 ), net_transfers as (
-    /*
-    - add amount received and amount sent (since transfer amount sent is set to a negative number, this calculates the net transfer amount received)
-    - for any given address in a single transaction
-    */
     select
         blockchain
         , block_date
-        , tx_hash
         , address_owner
         , sum(coalesce(transfer_amount_usd_sent, 0)) as transfer_amount_usd_sent
         , sum(coalesce(transfer_amount_usd_received, 0)) as transfer_amount_usd_received
@@ -110,15 +99,14 @@ with raw_transfers as (
     group by
         blockchain
         , block_date
-        , tx_hash
         , address_owner
 )
-select 
+select
     blockchain
     , block_date
-    , tx_hash
     , sum(transfer_amount_usd_sent) as transfer_amount_usd_sent
     , sum(transfer_amount_usd_received) as transfer_amount_usd_received
+    , sum(abs(transfer_amount_usd_sent)) + sum(abs(transfer_amount_usd_received)) as transfer_amount_usd
     , sum(net_transfer_amount_usd) as net_transfer_amount_usd
 from
     net_transfers
@@ -127,4 +115,3 @@ where
 group by
     blockchain
     , block_date
-    , tx_hash
