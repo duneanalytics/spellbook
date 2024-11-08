a66e7776a67b10a438f6853cbaf54f5b7627db31
Hildebert Moulié <h.moulie@gmail.com>
2024-11-08 18:48:19 +0100

    add ronin to evms tables

--- dbt_subprojects/daily_spellbook/models/evms/evms_erc20_approvals.sql
+++ dbt_subprojects/daily_spellbook/models/evms/evms_erc20_approvals.sql
@@ -2,7 +2,7 @@
         schema='evms',
         alias = 'erc20_approvals',
         unique_key=['blockchain', 'tx_hash', 'evt_index'],
-        post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo", "base", "zksync", "zora", "scroll", "linea", "zkevm", "blast", "mantle"]\',
+        post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo", "base", "zksync", "zora", "scroll", "linea", "zkevm", "blast", "mantle", "ronin"]\',
                                     "sector",
                                     "evms",
                                     \'["hildobby"]\') }}'
@@ -28,6 +28,7 @@
      , ('blast', source('erc20_blast', 'evt_approval'))
      , ('mantle', source('erc20_mantle', 'evt_approval'))
      , ('sei', source('erc20_sei', 'evt_Approval'))
+     , ('ronin', source('erc20_ronin', 'evt_Approval'))
 ] %}
 
 SELECT *
