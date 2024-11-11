e25cb2f8ccdc16cd9347bf7697ba091ef9d63da9
chef-seaweed <121569855+chef-seaweed@users.noreply.github.com>
2024-11-11 22:50:57 +0800

    Revert "Add hardcode date filter to prevent memory errors"
    This reverts commit e4f46af395dba2ff5fb18f66d4a344fe102a9698.

--- dbt_subprojects/solana/models/_sector/dex/orca_whirlpool/orca_whirlpool_v2_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/orca_whirlpool/orca_whirlpool_v2_base_trades.sql
@@ -208,8 +208,7 @@ with
                 {% if is_incremental() %}
                 AND {{incremental_predicate('call_block_time')}}
                 {% else %}
-                AND call_block_time >= TIMESTAMP '2024-11-01'  -- Hardcoded date filter 
-                --AND call_block_time >= TIMESTAMP '{{project_start_date}}'
+                AND call_block_time >= TIMESTAMP '{{project_start_date}}'
                 {% endif %}
 
                 UNION ALL
@@ -219,8 +218,7 @@ with
                 {% if is_incremental() %}
                 AND {{incremental_predicate('call_block_time')}}
                 {% else %}
-                AND call_block_time >= TIMESTAMP '2024-11-01'  -- Hardcoded date filter 
-                --AND call_block_time >= TIMESTAMP '{{project_start_date}}'
+                AND call_block_time >= TIMESTAMP '{{project_start_date}}'
                 {% endif %}
             )
             sp
@@ -239,8 +237,7 @@ with
             {% if is_incremental() %}
             AND {{incremental_predicate('memo.block_time')}}
             {% else %}
-            AND memo.block_time >= TIMESTAMP '2024-11-01'  -- Hardcoded date filter 
-            --AND memo.block_time >= TIMESTAMP '{{project_start_date}}'
+            AND memo.block_time >= TIMESTAMP '{{project_start_date}}'
             {% endif %}
         --token extension transfer could be preceeded by the memo program and followed by a post execution hook + even potentially more instructions.
         INNER JOIN {{ ref('tokens_solana_transfers') }} tr_1
@@ -252,8 +249,7 @@ with
             {% if is_incremental() %}
             AND {{incremental_predicate('tr_1.block_time')}}
             {% else %}
-            AND tr_1.block_time >= TIMESTAMP '2024-11-01'  -- Hardcoded date filter 
-            --AND tr_1.block_time >= TIMESTAMP '{{project_start_date}}'
+            AND tr_1.block_time >= TIMESTAMP '{{project_start_date}}'
             {% endif %}
         INNER JOIN {{ ref('tokens_solana_transfers') }} tr_2
             ON tr_2.tx_id = sp.call_tx_id
@@ -265,8 +261,7 @@ with
             {% if is_incremental() %}
             AND {{incremental_predicate('tr_2.block_time')}}
             {% else %}
-            AND tr_2.block_time >= TIMESTAMP '2024-11-01'  -- Hardcoded date filter 
-            --AND tr_2.block_time >= TIMESTAMP '{{project_start_date}}'
+            AND tr_2.block_time >= TIMESTAMP '{{project_start_date}}'
             {% endif %}
     )
 
