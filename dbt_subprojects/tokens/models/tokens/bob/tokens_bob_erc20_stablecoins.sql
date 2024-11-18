e46f50147de8f6922a3f4fea2b598a4f90543653
Thomas Sileghem <thomas@interlay.io>
2024-11-18 09:01:30 +0000

    fix bob stablecoin query

--- dbt_subprojects/tokens/models/tokens/bob/tokens_bob_erc20_stablecoins.sql
+++ dbt_subprojects/tokens/models/tokens/bob/tokens_bob_erc20_stablecoins.sql
@@ -21,5 +21,5 @@ FROM (VALUES
     ('bob', 0xf3107eec1e6f067552c035fd87199e1a5169cb20, 'Crypto-backed stablecoin','DLLR', 18, 'Sovryn DLLR'),
     ('bob', 0xc4a20a608616f18aa631316eeda9fb62d089361e, 'Hybrid stablecoin', 'FRAX', 18, 'FRAX'),
     ('bob', 0xe75d0fb2c24a55ca1e3f96781a2bcc7bdba058f0, 'Fiat-backed stablecoin', 'USDC', 6, 'USD Coin'),
-    ('bob', 0x05d032ac25d322df992303dca074ee7392c117b9, 'Fiat-backed stablecoin', 'USDT', 6, 'Tether USD'),
+    ('bob', 0x05d032ac25d322df992303dca074ee7392c117b9, 'Fiat-backed stablecoin', 'USDT', 6, 'Tether USD')
 ) AS temp_table (blockchain, contract_address, backing, symbol, decimals, name)
