afceb91c4bdaa68c0b9d40f421dc3f7327913861
max-morrow <amidmoro@gmail.com>
2024-11-19 20:31:23 +0300

    removed counters

--- dbt_subprojects/daily_spellbook/models/_projects/oneinch/oneinch_project_swaps.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/oneinch/oneinch_project_swaps.sql
@@ -9,13 +9,9 @@
 
 
 
-select
-    *
-    , row_number() over(partition by user, project order by block_time) as user_project_sn
-    , row_number() over(partition by user order by block_time, project) as user_over_sn
-from ({% for blockchain in oneinch_project_swaps_exposed_blockchains_list() %}
+{% for blockchain in oneinch_project_swaps_exposed_blockchains_list() %}
     {{ "-- depends_on: {{ ref('oneinch_' + blockchain + '_project_orders') }}" }}
     
     select * from {{ ref('oneinch_' + blockchain + '_project_swaps') }}
     {% if not loop.last %} union all {% endif %}
-{% endfor %})
+{% endfor %}
