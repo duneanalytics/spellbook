333989ef1b8d42a18fa234d4b7b8aefe50d93ebe
RantumBits <rantum@rantum.xyz>
2024-11-20 09:36:41 -0800

    add mode to cross-chain

--- dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
+++ dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
@@ -1,7 +1,7 @@
 {{ config(
      schema = 'zeroex'
         , alias = 'api_fills_deduped'
-        , post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "bnb", "celo", "ethereum", "fantom", "optimism", "polygon","scroll", "linea","blast","mantle"]\',
+        , post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "bnb", "celo", "ethereum", "fantom", "optimism", "polygon","scroll", "linea","blast","mantle","mode"]\',
                                 "project",
                                 "zeroex",
                                 \'["rantum","bakabhai993"]\') }}'
@@ -33,6 +33,7 @@
   ,ref('zeroex_linea_settler_trades')
   ,ref('zeroex_blast_settler_trades')
   ,ref('zeroex_mantle_settler_trades')
+  ,ref('zeroex_mode_settler_trades')
 ] %}
 
 
