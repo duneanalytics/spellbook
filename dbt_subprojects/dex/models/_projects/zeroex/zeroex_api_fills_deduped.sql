ed9f8e2675e507b7f48b0e5e3a9c7e8b5dc32c1d
RantumBits <rantum@rantum.xyz>
2024-11-20 12:54:40 -0800

    add linea

--- dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
+++ dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
@@ -30,7 +30,7 @@
   ,ref('zeroex_avalanche_c_settler_trades')
   ,ref('zeroex_arbitrum_settler_trades')
   ,ref('zeroex_scroll_settler_trades')
-  ,ref('zeroex_linea_settler_trades')
+  ,ref('zeroex_v2_linea_trades')
   ,ref('zeroex_blast_settler_trades')
   ,ref('zeroex_mantle_settler_trades')
 ] %}
