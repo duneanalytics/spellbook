8c939830e0db5f90b6aeae3071e3f6205202c771
darvin <todarvinrio@gmail.com>
2024-11-23 18:20:53 +0700

    add unique test and comma fix

--- dbt_subprojects/daily_spellbook/models/swell/rsweth/ethereum/swell_rsweth_ethereum_withdrawals.sql
+++ dbt_subprojects/daily_spellbook/models/swell/rsweth/ethereum/swell_rsweth_ethereum_withdrawals.sql
@@ -4,7 +4,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['token_id']
+        unique_key = ['token_id'],
         post_hook='{{ expose_spells(\'["ethereum"]\',
                                 "project",
                                 "swell",
