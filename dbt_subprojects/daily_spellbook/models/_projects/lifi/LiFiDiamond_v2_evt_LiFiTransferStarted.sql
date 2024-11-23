85a029525f13a3ba7493550833c014865c930bda
Phu <33654804+lequangphu@users.noreply.github.com>
2024-11-23 17:46:46 +0700

    Add bridges of LiFi across chains

---
+++ dbt_subprojects/daily_spellbook/models/_projects/lifi/LiFiDiamond_v2_evt_LiFiTransferStarted.sql
@@ -0,0 +1,65 @@
+{{
+    config(
+        schema = 'lifi',
+        alias = 'transfers',
+        materialized = 'incremental',
+        unique_key = ['evt_block_time', 'evt_tx_hash', 'evt_index']
+    )
+}}
+
+{% set chains = [
+    'ethereum',
+    'arbitrum',
+    'avalanche',
+    'bnb',
+    'fantom',
+    'gnosis',
+    'zksync'
+] %}
+
+with chain_transfers as (
+    {% for chain in chains %}
+    select
+        contract_address,
+        evt_tx_hash,
+        evt_index,
+        evt_block_time,
+        evt_block_number,
+        transactionId,
+        bridge,
+        integrator,
+        referrer,
+        sendingAssetId,
+        receiver,
+        minAmount,
+        destinationChainId,
+        source_chain,
+        sender
+    from {{ ref('lifi_' ~ chain ~ '_transfers') }}
+    {% if is_incremental() %}
+    where evt_block_time >= date_trunc('day', now() - interval '7' day)
+    {% endif %}
+
+    {% if not loop.last %}
+    union all
+    {% endif %}
+    {% endfor %}
+)
+
+select
+    contract_address,
+    evt_tx_hash,
+    evt_index,
+    evt_block_time,
+    evt_block_number,
+    transactionId,
+    bridge,
+    integrator,
+    referrer,
+    sendingAssetId,
+    receiver,
+    minAmount,
+    destinationChainId,
+    source_chain,
+    sender
+from chain_transfers
---
+++ dbt_subprojects/daily_spellbook/models/_projects/lifi/zksync/lifi_zksync_transfers.sql
@@ -0,0 +1,38 @@
+{{ config(
+    schema = 'lifi_zksync',
+    alias = 'transfers',
+    materialized = 'view'
+    )
+}}
+
+with source_data as (
+    select
+        contract_address,
+        evt_tx_hash,
+        evt_index,
+        evt_block_time,
+        evt_block_number,
+        json_extract_scalar(bridgeData, '$.transactionId') as transactionId,
+        json_extract_scalar(bridgeData, '$.bridge') as bridge,
+        json_extract_scalar(bridgeData, '$.integrator') as integrator,
+        json_extract_scalar(bridgeData, '$.referrer') as referrer,
+        json_extract_scalar(bridgeData, '$.sendingAssetId') as sendingAssetId,
+        json_extract_scalar(bridgeData, '$.receiver') as receiver,
+        json_extract_scalar(bridgeData, '$.minAmount') as minAmount,
+        json_extract_scalar(bridgeData, '$.destinationChainId') as destinationChainId,
+        'zksync' as source_chain
+    from {{ source('lifi_zksync', 'LiFiDiamond_v2_evt_LiFiTransferStarted') }}
+),
+transactions as (
+    select 
+        "from" as sender,
+        hash as tx_hash
+    from {{ source('zksync', 'transactions') }}
+)
+
+select 
+    s.*,
+    t.sender
+from source_data s
+inner join transactions t
+    on s.evt_tx_hash = t.tx_hash
