2905af8d5aa810adad22b4a145afcf6fb8750294
viniabussafi <vinicius@balancerlabs.dev>
2024-11-25 13:21:37 +0000

    add pools_fees

---
+++ dbt_subprojects/dex/models/_projects/balancer/pools/gnosis/balancer_v3_gnosis_pools_fees.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        schema = 'balancer_v3_gnosis',
+        alias = 'pools_fees',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.evt_block_time')],
+        unique_key = ['block_number', 'tx_hash', 'index']
+    )
+}}
+
+SELECT
+    'gnosis' AS blockchain,
+    '3' AS version, 
+    pool AS pool_address,
+    evt_tx_hash,
+    evt_tx_index,
+    evt_index,
+    evt_block_time,
+    evt_block_number,
+    swapFeePercentage AS swap_fee_percentage
+FROM {{ source ('balancer_v2_gnosis', 'Vault_evt_SwapFeePercentageChanged ') }}
+WHERE 1 = 1 
+{% if is_incremental() %}
+AND {{ incremental_predicate('evt_block_time') }}
+{% endif %}
+
