4e1be9bac581875959b61d36ba8245fbdc4154c5
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-11-25 14:03:10 +0100

    split out transactions

---
+++ dbt_subprojects/daily_spellbook/models/_metrics/transactions/chains/evm/metrics_avalanche_c_transactions_daily.sql
@@ -0,0 +1,30 @@
+{% set blockchain = 'avalanche_c' %}
+
+{{ config(
+        schema = 'metrics_' + blockchain
+        , alias = 'transactions_daily'
+        , materialized = 'incremental'
+        , file_format = 'delta'
+        , incremental_strategy = 'merge'
+        , unique_key = ['blockchain', 'block_date']
+        , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
+        )
+}}
+
+
+select
+        blockchain
+        , block_date
+        , approx_distinct(tx_hash) as tx_count --max 2% error, which is fine
+from
+    {{ source('tokens', 'transfers') }}
+where
+    blockchain = '{{ blockchain }}'
+    and amount_usd >=1
+    {% if is_incremental() %}
+    and {{ incremental_predicate('block_date') }}
+    {% endif %}
+group by
+    blockchain
+    , block_date
+
