cf1454256ff13d0768f642d940c55561f20dff2b
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-11-25 14:09:35 +0100

    setup evm macros

--- dbt_subprojects/daily_spellbook/models/_metrics/fees/chains/evm/metrics_arbitrum_gas_fees_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/fees/chains/evm/metrics_arbitrum_gas_fees_daily.sql
@@ -12,17 +12,4 @@
 }}
 
 
-select
-    blockchain
-    , block_date
-    , sum(tx_fee_usd) as gas_fees_usd
-from
-    {{ source('gas', 'fees') }}
-where blockchain = '{{blockchain}}'
-{% if is_incremental() %}
-and
-    {{ incremental_predicate('block_date') }}
-{% endif %}
-group by
-    blockchain
-    ,block_date
+{{ metrics_fees_evm(blockchain) }}
--- dbt_subprojects/daily_spellbook/models/_metrics/transactions/chains/evm/metrics_base_transactions_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/transactions/chains/evm/metrics_base_transactions_daily.sql
@@ -12,18 +12,4 @@
 }}
 
 
-select
-        blockchain
-        , block_date
-        , approx_distinct(tx_hash) as tx_count --max 2% error, which is fine
-from
-    {{ source('tokens', 'transfers') }}
-where
-    blockchain = '{{ blockchain }}'
-    and amount_usd >=1
-    {% if is_incremental() %}
-    and {{ incremental_predicate('block_date') }}
-    {% endif %}
-group by
-    blockchain
-    , block_date
+{{ metrics_transactions_evm(blockchain) }}
