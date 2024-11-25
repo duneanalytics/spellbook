7635198510e64450430de719dc2a06ff06455718
viniabussafi <vinicius@balancerlabs.dev>
2024-11-25 08:58:55 +0000

    commit v3 macros (sans liquidity)

--- dbt_macros/shared/balancer/balancer_bpt_supply_macro.sql
+++ dbt_macros/shared/balancer/balancer_bpt_supply_macro.sql
@@ -138,3 +138,101 @@ WITH pool_labels AS (
     HAVING SUM(b.supply - COALESCE(preminted_bpts, 0) + COALESCE(adelta, 0)) >= 0  --simple filter to remove outliers
 
 {% endmacro %}
+
+{% macro 
+    balancer_v3_compatible_bpt_supply_macro(
+        blockchain, version, project_decoded_as, base_spells_namespace, pool_labels_spell
+    ) 
+%}
+
+ WITH pool_labels AS (
+        SELECT * FROM (
+            SELECT
+                address,
+                name,
+                pool_type,
+                ROW_NUMBER() OVER (PARTITION BY address ORDER BY MAX(updated_at) DESC) AS num
+            FROM {{ pool_labels_spell }}
+            WHERE blockchain = '{{blockchain}}'
+            GROUP BY 1, 2, 3) 
+        WHERE num = 1
+    ),
+
+  -- Extract mints and burns from transfers
+    transfers AS (
+        SELECT
+            block_date AS day,
+            contract_address AS token,
+            COALESCE(SUM(CASE WHEN t."from" = 0x0000000000000000000000000000000000000000 THEN value / POWER(10, 18) ELSE 0 END), 0) AS mints,
+            COALESCE(SUM(CASE WHEN t.to = 0x0000000000000000000000000000000000000000 THEN value / POWER(10, 18) ELSE 0 END), 0) AS burns
+        FROM  {{ ref(base_spells_namespace + '_transfers_bpt') }} t
+        WHERE blockchain = '{{blockchain}}'   
+        AND version = '{{version}}'
+        GROUP BY 1, 2)
+
+    -- Calculate token balances over time
+    balances AS (
+        SELECT
+            day,
+            token,
+            LEAD(DAY, 1, NOW()) OVER (PARTITION BY token ORDER BY DAY) AS day_of_next_change,
+            SUM(COALESCE(mints, 0) - COALESCE(burns, 0)) OVER (PARTITION BY token ORDER BY DAY ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS supply
+        FROM transfers
+    ),
+
+    -- Calculating Joins(mint) and Exits(burn) via Swap
+    joins AS (
+        SELECT 
+            DATE_TRUNC('day', evt_block_time) AS block_date, 
+            tokenOut,
+            pool_type,
+            SUM(amountOut / POWER(10, 18)) AS ajoins
+        FROM {{ source(project_decoded_as + '_' + blockchain, 'Vault_evt_Swap') }} 
+        LEFT JOIN pool_labels ON BYTEARRAY_SUBSTRING(poolId, 1, 20) = address
+        WHERE tokenOut = BYTEARRAY_SUBSTRING(poolId, 1, 20)       
+        GROUP BY 1, 2, 3
+    ),
+
+    exits AS (
+        SELECT 
+            DATE_TRUNC('day', evt_block_time) AS block_date, 
+            tokenIn,
+            pool_type,
+            SUM(amountIn / POWER(10, 18)) AS aexits
+        FROM {{ source(project_decoded_as + '_' + blockchain, 'Vault_evt_Swap') }} 
+        LEFT JOIN pool_labels ON BYTEARRAY_SUBSTRING(poolId, 1, 20) = address
+        WHERE tokenIn = BYTEARRAY_SUBSTRING(poolId, 1, 20)        
+        GROUP BY 1, 2, 3
+    ),
+
+    joins_and_exits AS (
+        SELECT 
+            j.block_date, 
+            j.tokenOut AS bpt, 
+            SUM(COALESCE(ajoins, 0) - COALESCE(aexits, 0)) OVER (PARTITION BY j.tokenOut ORDER BY j.block_date ASC) AS adelta
+        FROM joins j
+        FULL OUTER JOIN exits e ON j.block_date = e.block_date AND e.tokenIn = j.tokenOut
+    ),
+
+    calendar AS (
+        SELECT 
+            date_sequence AS day
+        FROM unnest(sequence(date('2024-12-01'), date(now()), interval '1' day)) as t(date_sequence)
+    )
+
+    SELECT
+        c.day,
+        l.pool_type,
+        '{{version}}' as version,
+        '{{blockchain}}' as blockchain,
+        b.token AS token_address,
+        COALESCE(SUM(b.supply + COALESCE(adelta, 0)),0) AS supply
+    FROM calendar c 
+    LEFT JOIN balances b ON b.day <= c.day AND c.day < b.day_of_next_change
+    LEFT JOIN joins_and_exits j ON c.day = j.block_date AND b.token = j.bpt
+    LEFT JOIN premints p ON b.token = p.bpt
+    LEFT JOIN pool_labels l ON b.token = l.address
+    GROUP BY 1, 2, 3, 4, 5
+    HAVING SUM(b.supply + COALESCE(adelta, 0)) >= 0  --simple filter to remove outliers
+    
+{% endmacro %}
--- dbt_macros/shared/balancer/balancer_transfers_bpt_macro.sql
+++ dbt_macros/shared/balancer/balancer_transfers_bpt_macro.sql
@@ -36,3 +36,42 @@
 
 
  {% endmacro %}
+
+ {% macro 
+    balancer_v3_compatible_transfers_bpt_macro(
+        blockchain, version, project_decoded_as 
+    ) 
+%}
+
+    WITH registered_pools AS (
+        SELECT
+        DISTINCT pool AS pool_address
+        FROM
+        {{ source(project_decoded_as + '_' + blockchain, 'Vault_evt_PoolRegistered') }}
+    )
+
+    SELECT DISTINCT * FROM (
+        SELECT
+            '{{blockchain}}' AS blockchain,
+            '{{version}}' AS version,
+            transfer.contract_address,
+            transfer.evt_tx_hash,
+            transfer.evt_index,
+            transfer.evt_block_time,
+            TRY_CAST(date_trunc('DAY', transfer.evt_block_time) AS date) AS block_date,
+            TRY_CAST(date_trunc('MONTH', transfer.evt_block_time) AS date) AS block_month,
+            transfer.evt_block_number,
+            transfer."from",
+            transfer.to,
+            transfer.value
+        FROM {{ source('erc20_' + blockchain, 'evt_transfer') }} transfer
+        INNER JOIN registered_pools p ON p.pool_address = transfer.contract_address
+            {% if not is_incremental() %}
+            WHERE transfer.evt_block_time >= TIMESTAMP '2024-12-01'
+            {% endif %}
+            {% if is_incremental() %}
+            WHERE {{ incremental_predicate('evt_block_time') }}
+            {% endif %} ) transfers
+
+
+ {% endmacro %}
