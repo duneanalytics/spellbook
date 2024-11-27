3ff5c5ce701acf69fd266ad27da0b976e9c16f7d
RantumBits <rantum@rantum.xyz>
2024-11-27 00:20:32 -0800

    -endif

--- dbt_subprojects/dex/macros/models/_project/zeroex/zeroex_v2.sql
+++ dbt_subprojects/dex/macros/models/_project/zeroex/zeroex_v2.sql
@@ -83,8 +83,7 @@ WITH tbl_all_logs AS (
         logs.block_time,
         logs.block_number,
         index,
-        varbinary_substring(logs.topic1, 13, 20) end as taker_,
-        {% endif %} 
+        varbinary_substring(logs.topic1, 13, 20) end as taker_, 
         case when (varbinary_substring(logs.topic1, 13, 20) in (tx_from, settler_address)) then logs.contract_address end as taker_token_, 
         case when (varbinary_substring(logs.topic2, 13, 20) in (settler_address, tx_from)) then logs.contract_address end as maker_token_,
         case when (varbinary_substring(logs.topic1, 13, 20) in (tx_from, settler_address)) then try_cast(bytearray_to_uint256(bytearray_substring(DATA, 22,11)) as int256)end as taker_amount_, 
