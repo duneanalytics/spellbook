9bad2c9b75f4e7d9d5a7a7f0699826262758a177
whalehunting <143016036+whalehunting@users.noreply.github.com>
2024-11-30 13:03:11 +0100

    chore: format with sqlfmt

--- dbt_subprojects/dex/models/bot_trades/flokibot/ethereum/flokibot_ethereum_bot_trades.sql
+++ dbt_subprojects/dex/models/bot_trades/flokibot/ethereum/flokibot_ethereum_bot_trades.sql
@@ -1,13 +1,16 @@
-{{ config(
-    alias = 'bot_trades',
-    schema = 'flokibot_ethereum',
-    partition_by = ['block_month'],
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-    unique_key = ['blockchain', 'tx_hash', 'evt_index']
-   )
+{{
+    config(
+        alias='bot_trades',
+        schema='flokibot_ethereum',
+        partition_by=['block_month'],
+        materialized='incremental',
+        file_format='delta',
+        incremental_strategy='merge',
+        incremental_predicates=[
+            incremental_predicate('DBT_INTERNAL_DEST.block_time')
+        ],
+        unique_key=['blockchain', 'tx_hash', 'evt_index'],
+    )
 }}
 
 {% set project_name = 'Floki Trading Bot' %}
@@ -18,178 +21,141 @@
 {% set weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' %}
 {% set fee_token_symbol = 'ETH' %}
 
-WITH
-  bot_contracts AS (
-    SELECT
-      address
-    FROM
-      {{ source('ethereum','creation_traces') }}
-    WHERE
-      ("from" = {{bot_deployer_1}} OR "from" = {{bot_deployer_2}})
-      AND block_time >= TIMESTAMP '{{project_start_date}}'
-  ),
-  bot_trades AS (
-    SELECT
-      trades.block_time,
-      amount_usd,
-      IF(
-        token_sold_address = {{weth}},
-        'Buy',
-        'Sell'
-      ) AS type,
-      token_bought_amount,
-      token_bought_symbol,
-      token_bought_address,
-      token_sold_amount,
-      token_sold_symbol,
-      token_sold_address,
-      project,
-      version,
-      token_pair,
-      project_contract_address,
-      tx_from AS user,
-      tx_to AS bot,
-      trades.tx_hash,
-      evt_index
-    FROM
-      {{ source('dex', 'trades') }} as trades
-      JOIN bot_contracts ON trades.tx_to = bot_contracts.address
-    WHERE
-      trades.blockchain = '{{blockchain}}'
-      {% if is_incremental() %}
-      AND {{ incremental_predicate('trades.block_time') }}
-      {% else %}
-      AND trades.block_time >= TIMESTAMP '{{project_start_date}}'
-      {% endif %}
-  ),
-  highest_event_index_for_each_trade AS (
-    SELECT
-      tx_hash,
-      MAX(evt_index) AS highest_event_index
-    FROM
-      bot_trades
-    GROUP BY
-      tx_hash
-  ),
-  bot_eth_deposits AS (
-    SELECT
-      tx_hash,
-      block_number,
-      CAST(value AS DECIMAL (38, 0)) AS delta_gwei,
-      CAST(value AS DECIMAL (38, 0)) AS deposit_gwei
-    FROM
-      {{ source('ethereum','traces') }}
-      JOIN bot_contracts ON to = bot_contracts.address
-    WHERE
-      {% if is_incremental() %}
-      {{ incremental_predicate('block_time') }}
-      {% else %}
-      block_time >= TIMESTAMP '{{project_start_date}}'
-      {% endif %}
-      AND value > 0
-  ),
-  bot_eth_withdrawals AS (
-    SELECT
-      tx_hash,
-      block_number,
-      CAST(value AS DECIMAL (38, 0)) * -1 AS delta_gwei,
-      0 AS deposit_gwei,
-      block_hash,
-      to
-    FROM
-      {{ source('ethereum','traces') }}
-      JOIN bot_contracts ON "from" = bot_contracts.address
-    WHERE
-      {% if is_incremental() %}
-      {{ incremental_predicate('block_time') }}
-      {% else %}
-      block_time >= TIMESTAMP '{{project_start_date}}'
-      {% endif %}
-      AND value > 0
-  ),
-  botEthTransfers AS (
-    /* Deposits */
-    (
-      SELECT
-        tx_hash,
-        block_number,
-        delta_gwei,
-        deposit_gwei
-      FROM
-        bot_eth_deposits
+with
+    bot_contracts as (
+        select address
+        from {{ source('ethereum', 'creation_traces') }}
+        where
+            ("from" = {{ bot_deployer_1 }} or "from" = {{ bot_deployer_2 }})
+            and block_time >= timestamp '{{project_start_date}}'
+    ),
+    bot_trades as (
+        select
+            trades.block_time,
+            amount_usd,
+            if(token_sold_address = {{ weth }}, 'Buy', 'Sell') as type,
+            token_bought_amount,
+            token_bought_symbol,
+            token_bought_address,
+            token_sold_amount,
+            token_sold_symbol,
+            token_sold_address,
+            project,
+            version,
+            token_pair,
+            project_contract_address,
+            tx_from as user,
+            tx_to as bot,
+            trades.tx_hash,
+            evt_index
+        from {{ source('dex', 'trades') }} as trades
+        join bot_contracts on trades.tx_to = bot_contracts.address
+        where
+            trades.blockchain = '{{blockchain}}'
+            {% if is_incremental() %}
+                and {{ incremental_predicate('trades.block_time') }}
+            {% else %} and trades.block_time >= timestamp '{{project_start_date}}'
+            {% endif %}
+    ),
+    highest_event_index_for_each_trade as (
+        select tx_hash, max(evt_index) as highest_event_index
+        from bot_trades
+        group by tx_hash
+    ),
+    bot_eth_deposits as (
+        select
+            tx_hash,
+            block_number,
+            cast(value as decimal(38, 0)) as delta_gwei,
+            cast(value as decimal(38, 0)) as deposit_gwei
+        from {{ source('ethereum', 'traces') }}
+        join bot_contracts on to = bot_contracts.address
+        where
+            {% if is_incremental() %} {{ incremental_predicate('block_time') }}
+            {% else %} block_time >= timestamp '{{project_start_date}}'
+            {% endif %} and value > 0
+    ),
+    bot_eth_withdrawals as (
+        select
+            tx_hash,
+            block_number,
+            cast(value as decimal(38, 0)) * -1 as delta_gwei,
+            0 as deposit_gwei,
+            block_hash,
+            to
+        from {{ source('ethereum', 'traces') }}
+        join bot_contracts on "from" = bot_contracts.address
+        where
+            {% if is_incremental() %} {{ incremental_predicate('block_time') }}
+            {% else %} block_time >= timestamp '{{project_start_date}}'
+            {% endif %} and value > 0
+    ),
+    botethtransfers as (
+        /* Deposits */
+        (select tx_hash, block_number, delta_gwei, deposit_gwei from bot_eth_deposits)
+        union all
+        /* Withdrawals */
+        (
+            select tx_hash, block_number, delta_gwei, deposit_gwei
+            from bot_eth_withdrawals
+        )
+    ),
+    bot_eth_deltas as (
+        select
+            tx_hash,
+            block_number,
+            sum(delta_gwei) as fee_gwei,
+            sum(deposit_gwei) as deposit_gwei
+        from botethtransfers
+        group by tx_hash, block_number
     )
-    UNION ALL
-    /* Withdrawals */
-    (
-      SELECT
-        tx_hash,
-        block_number,
-        delta_gwei,
-        deposit_gwei
-      FROM
-        bot_eth_withdrawals
+select
+    block_time,
+    date_trunc('day', bot_trades.block_time) as block_date,
+    date_trunc('month', bot_trades.block_time) as block_month,
+    '{{project_name}}' as bot,
+    block_number,
+    '{{blockchain}}' as blockchain,
+    -- Trade
+    amount_usd,
+    type,
+    token_bought_amount,
+    token_bought_symbol,
+    token_bought_address,
+    token_sold_amount,
+    token_sold_symbol,
+    token_sold_address,
+    -- Fees
+    round(
+        cast(fee_gwei as double) / cast(deposit_gwei as double),
+        /* Round feePercentage to 0.01% steps */
+        4
+    ) as fee_percentage_fraction,
+    (fee_gwei / 1e18) * price as fee_usd,
+    fee_gwei / 1e18 fee_token_amount,
+    '{{fee_token_symbol}}' as fee_token_symbol,
+    {{ weth }} as fee_token_address,
+    -- Dex
+    project,
+    version,
+    token_pair,
+    project_contract_address,
+    -- User
+    user as user,
+    bot_trades.tx_hash,
+    evt_index,
+    if(evt_index = highest_event_index, true, false) as is_last_trade_in_transaction
+from bot_trades
+join
+    highest_event_index_for_each_trade
+    on bot_trades.tx_hash = highest_event_index_for_each_trade.tx_hash
+left join bot_eth_deltas on bot_trades.tx_hash = bot_eth_deltas.tx_hash
+left join
+    {{ source('prices', 'usd') }}
+    on (
+        blockchain = '{{blockchain}}'
+        and contract_address = {{ weth }}
+        and minute = date_trunc('minute', block_time)
+        {% if is_incremental() %} and {{ incremental_predicate('minute') }} {% endif %}
     )
-  ),
-  bot_eth_deltas AS (
-    SELECT
-      tx_hash,
-      block_number,
-      SUM(delta_gwei) AS fee_gwei,
-      SUM(deposit_gwei) AS deposit_gwei
-    FROM
-      botEthTransfers
-    GROUP BY
-      tx_hash,
-      block_number
-  )
-SELECT
-  block_time,
-  date_trunc('day', bot_trades.block_time) as block_date,
-  date_trunc('month', bot_trades.block_time) as block_month,
-  '{{project_name}}' as bot,
-  block_number,
-  '{{blockchain}}' AS blockchain,
-  -- Trade
-  amount_usd,
-  type,
-  token_bought_amount,
-  token_bought_symbol,
-  token_bought_address,
-  token_sold_amount,
-  token_sold_symbol,
-  token_sold_address,
-  -- Fees
-  ROUND(
-    CAST(fee_gwei AS DOUBLE) / CAST(deposit_gwei AS DOUBLE),
-    /* Round feePercentage to 0.01% steps */
-    4
-  ) AS fee_percentage_fraction,
-  (fee_gwei / 1e18) * price AS fee_usd,
-  fee_gwei / 1e18 fee_token_amount,
-  '{{fee_token_symbol}}' AS fee_token_symbol,
-  {{weth}} AS fee_token_address,
-  -- Dex
-  project,
-  version,
-  token_pair,
-  project_contract_address,
-  -- User
-  user AS user,
-  bot_trades.tx_hash,
-  evt_index,
-  IF(evt_index = highest_event_index, true, false) AS is_last_trade_in_transaction
-FROM
-  bot_trades
-  JOIN highest_event_index_for_each_trade ON bot_trades.tx_hash = highest_event_index_for_each_trade.tx_hash
-  LEFT JOIN bot_eth_deltas ON bot_trades.tx_hash = bot_eth_deltas.tx_hash
-  LEFT JOIN {{ source('prices', 'usd') }} ON (
-    blockchain = '{{blockchain}}'
-    AND contract_address = {{weth}}
-    AND minute = DATE_TRUNC('minute', block_time)
-    {% if is_incremental() %}
-    AND {{ incremental_predicate('minute') }}
-    {% endif %}  
-  )
-ORDER BY
-  block_time DESC,
-  evt_index DESC
+order by block_time desc, evt_index desc
