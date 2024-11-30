a46d7033506489a110f60443d3ef8caa78e5f326
whalehunting <143016036+whalehunting@users.noreply.github.com>
2024-11-30 12:52:07 +0100

    fix: typo

--- dbt_subprojects/dex/models/bot_trades/flokibot/ethereum/flokibot_ethereum_bot_trades.sql
+++ dbt_subprojects/dex/models/bot_trades/flokibot/ethereum/flokibot_ethereum_bot_trades.sql
@@ -164,8 +164,8 @@ SELECT
     /* Round feePercentage to 0.01% steps */
     4
   ) AS fee_percentage_fraction,
-  (feeGwei / 1e18) * price AS fee_usd,
-  feeGwei / 1e18 fee_token_amount,
+  (fee_gwei / 1e18) * price AS fee_usd,
+  fee_gwei / 1e18 fee_token_amount,
   '{{fee_token_symbol}}' AS fee_token_symbol,
   {{weth}} AS fee_token_address,
   -- Dex
