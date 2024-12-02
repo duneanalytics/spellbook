7ab788373f68cefef9952bcfcbe680ec09f71891
Huang Geyang <Sukebeta@outlook.com>
2024-12-02 23:32:50 +0800

    fix price

--- dbt_subprojects/tokens/models/prices/flare/prices_flare_tokens.sql
+++ dbt_subprojects/tokens/models/prices/flare/prices_flare_tokens.sql
@@ -16,7 +16,5 @@ SELECT
 FROM
 (
     VALUES
-    ('flare-flare', 'WFLR', 0x1D80c49BbBCd1C0911346656B529DF9E5c2F783d, 18)
-    -- Note: Currently only WFLR has a reliable price feed. Other tokens will be added
-    -- when they have established price feeds on major price aggregators.
+    ('flr-flare-network', 'WFLR', 0x1D80c49BbBCd1C0911346656B529DF9E5c2F783d, 18)
 ) as temp (token_id, symbol, contract_address, decimals)
