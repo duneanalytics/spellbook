7e9ff87e4b6d1fdeeb048225244e787996a2ee4a
archie17277 <archie17277@gmail.com>
2024-12-02 10:43:40 +0530

    flashliquidity dex added

---
+++ dbt_subprojects/dex/models/trades/base/platforms/flashliquidity_base_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'flashliquidity_base',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v2_trades(
+        blockchain = 'base',
+        project = 'flashliquidity_base',
+        version = '1',
+        Pair_evt_Swap = source('flashliquidity_base', 'FlashLiquidityPair_evt_Swap'),
+        Factory_evt_PairCreated = source('flashliquidity_base', 'FlashLiquidityFactory_evt_PairCreated')
+    )
+}}
