0473a6407f641c2c5d366ab10456131953f66e38
Huang Geyang <Sukebeta@outlook.com>
2024-12-04 02:35:37 +0800

    Init boba chain (#7209)
    * Init boba chain
    
    * Fix evm info:
    
    * Add native token
    
    * Update token checker
    
    * Remove not existed transfers sources
    
    * Match raw data schema
    
    * Rewrite boba docs block
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/tokens/models/tokens/tokens_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/tokens_erc20.sql
@@ -10,6 +10,7 @@
                                         ,"base"
                                         ,"blast"
                                         ,"bnb"
+                                        ,"boba"
                                         ,"celo"
                                         ,"ethereum"
                                         ,"fantom"
@@ -71,6 +72,7 @@
     ,'tokens_tron': {'blockchain': 'tron', 'model': ref('tokens_tron_erc20')}
     ,'tokens_ronin': {'blockchain': 'ronin', 'model': ref('tokens_ronin_erc20')}
     ,'tokens_bob': {'blockchain': 'bob', 'model': ref('tokens_bob_erc20')}
+    ,'tokens_boba': {'blockchain': 'boba', 'model': ref('tokens_boba_erc20')}
 } %}
 
 with automated_source as (
