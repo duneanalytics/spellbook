f4ac044c5830dc9ca5b71d586ebc868572ba78a1
Huang Geyang <Sukebeta@outlook.com>
2024-12-04 18:39:47 +0800

    Update prices_flare_tokens.sql

--- dbt_subprojects/tokens/models/prices/flare/prices_flare_tokens.sql
+++ dbt_subprojects/tokens/models/prices/flare/prices_flare_tokens.sql
@@ -3,6 +3,8 @@
 {{ config(
     schema = 'prices_' + blockchain,
     alias = 'tokens',
+    materialized = 'table',
+    file_format = 'delta',
     tags = ['static']
     )
 }}
@@ -17,4 +19,7 @@ FROM
 (
     VALUES
     ('flr-flare-network', 'WFLR', 0x1D80c49BbBCd1C0911346656B529DF9E5c2F783d, 18)
+    , ('joule-kinetic', 'JOULE', 0xE6505f92583103AF7ed9974DEC451A7Af4e3A3bE, 18)
+    , ('usdc.e-usd-coin.e', 'USDC.e', 0xFbDa5F676cB37624f28265A144A48B0d6e87d3b6, 6)
+    , ('usdt-tether', 'USDT', 0x0B38e83B86d491735fEaa0a791F65c2B99535396, 6)
 ) as temp (token_id, symbol, contract_address, decimals)
