e968576fc6e7f7541063599a1c625390b24916e2
AIDataMaster <cryptouserv1@gmail.com>
2024-12-04 20:12:56 +0200

    Upgrade Event Models for GMX v2 Project (#7195)
    * Add 5 new Event Models. Update 4 Event Models Based on Changes in Contracts
    
    * Update 4 Event Models
    
    * fix Error
    
    * Final changes on 4 event models
    
    * Empty commit
    
    * fix tests
    
    * fix tests
    
    * fix errors again
    
    * Run Check Again.
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/daily_spellbook/models/_projects/gmx/event/avalanche_c/gmx_v2_avalanche_c_position_fees_info.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/gmx/event/avalanche_c/gmx_v2_avalanche_c_position_fees_info.sql
@@ -135,19 +135,20 @@ WITH evt_data_1 AS (
         tx_hash,
         index,
         
+        MAX(CASE WHEN key_name = 'orderKey' THEN value END) AS order_key,
+        MAX(CASE WHEN key_name = 'positionKey' THEN value END) AS position_key,
+        MAX(CASE WHEN key_name = 'referralCode' THEN value END) AS referral_code,
+
         MAX(CASE WHEN key_name = 'market' THEN value END) AS market,
         MAX(CASE WHEN key_name = 'collateralToken' THEN value END) AS collateral_token,
         MAX(CASE WHEN key_name = 'affiliate' THEN value END) AS affiliate,
         MAX(CASE WHEN key_name = 'trader' THEN value END) AS trader,
         MAX(CASE WHEN key_name = 'uiFeeReceiver' THEN value END) AS ui_fee_receiver,
+
         MAX(CASE WHEN key_name = 'collateralTokenPrice.min' THEN value END) AS collateral_token_price_min,
         MAX(CASE WHEN key_name = 'collateralTokenPrice.max' THEN value END) AS collateral_token_price_max,
         MAX(CASE WHEN key_name = 'tradeSizeUsd' THEN value END) AS trade_size_usd,
-        MAX(CASE WHEN key_name = 'totalRebateFactor' THEN value END) AS total_rebate_factor,
-        MAX(CASE WHEN key_name = 'traderDiscountFactor' THEN value END) AS trader_discount_factor,
-        MAX(CASE WHEN key_name = 'totalRebateAmount' THEN value END) AS total_rebate_amount,
-        MAX(CASE WHEN key_name = 'traderDiscountAmount' THEN value END) AS trader_discount_amount,
-        MAX(CASE WHEN key_name = 'affiliateRewardAmount' THEN value END) AS affiliate_reward_amount,
+
         MAX(CASE WHEN key_name = 'fundingFeeAmount' THEN value END) AS funding_fee_amount,
         MAX(CASE WHEN key_name = 'claimableLongTokenAmount' THEN value END) AS claimable_long_token_amount,
         MAX(CASE WHEN key_name = 'claimableShortTokenAmount' THEN value END) AS claimable_short_token_amount,
@@ -168,10 +169,28 @@ WITH evt_data_1 AS (
         MAX(CASE WHEN key_name = 'totalCostAmount' THEN value END) AS total_cost_amount,
         MAX(CASE WHEN key_name = 'uiFeeReceiverFactor' THEN value END) AS ui_fee_receiver_factor,
         MAX(CASE WHEN key_name = 'uiFeeAmount' THEN value END) AS ui_fee_amount,
-        MAX(CASE WHEN key_name = 'isIncrease' THEN value END) AS is_increase,
-        MAX(CASE WHEN key_name = 'orderKey' THEN value END) AS order_key,
-        MAX(CASE WHEN key_name = 'positionKey' THEN value END) AS position_key,
-        MAX(CASE WHEN key_name = 'referralCode' THEN value END) AS referral_code
+
+        MAX(CASE WHEN key_name = 'totalRebateFactor' THEN value END) AS total_rebate_factor,
+        MAX(CASE WHEN key_name = 'traderDiscountFactor' THEN value END) AS trader_discount_factor,
+        MAX(CASE WHEN key_name = 'totalRebateAmount' THEN value END) AS total_rebate_amount,
+        MAX(CASE WHEN key_name = 'traderDiscountAmount' THEN value END) AS trader_discount_amount,
+        MAX(CASE WHEN key_name = 'affiliateRewardAmount' THEN value END) AS affiliate_reward_amount,
+        
+        MAX(CASE WHEN key_name = 'referral.totalRebateFactor' THEN value END) AS referral_total_rebate_factor,
+        MAX(CASE WHEN key_name = 'referral.adjustedAffiliateRewardFactor' THEN value END) AS referral_adjusted_affiliate_reward_factor,
+        MAX(CASE WHEN key_name = 'referral.traderDiscountFactor' THEN value END) AS referral_trader_discount_factor,
+        MAX(CASE WHEN key_name = 'referral.totalRebateAmount' THEN value END) AS referral_total_rebate_amount,
+        MAX(CASE WHEN key_name = 'referral.traderDiscountAmount' THEN value END) AS referral_trader_discount_amount,
+        MAX(CASE WHEN key_name = 'referral.affiliateRewardAmount' THEN value END) AS referral_affiliate_reward_amount,
+
+        MAX(CASE WHEN key_name = 'pro.traderDiscountFactor' THEN value END) AS pro_trader_discount_factor,
+        MAX(CASE WHEN key_name = 'pro.traderDiscountAmount' THEN value END) AS pro_trader_discount_amount,
+
+        MAX(CASE WHEN key_name = 'liquidationFeeAmount' THEN value END) AS liquidation_fee_amount,
+        MAX(CASE WHEN key_name = 'liquidationFeeReceiverFactor' THEN value END) AS liquidation_fee_receiver_factor,
+        MAX(CASE WHEN key_name = 'liquidationFeeAmountForFeeReceiver' THEN value END) AS liquidation_fee_amount_for_fee_receiver,
+        
+        MAX(CASE WHEN key_name = 'isIncrease' THEN value END) AS is_increase
 
     FROM
         combined
@@ -190,19 +209,19 @@ WITH evt_data_1 AS (
         event_name,
         msg_sender,
         
+        from_hex(order_key) AS order_key,
+        from_hex(position_key) AS position_key,
+        from_hex(referral_code) AS referral_code,
+
         from_hex(market) AS market,
         from_hex(collateral_token) AS collateral_token,
         from_hex(affiliate) AS affiliate,
         from_hex(trader) AS trader,
         from_hex(ui_fee_receiver) AS ui_fee_receiver,
+
         TRY_CAST(collateral_token_price_min AS DOUBLE) AS collateral_token_price_min,
         TRY_CAST(collateral_token_price_max AS DOUBLE) AS collateral_token_price_max,
         TRY_CAST(trade_size_usd AS DOUBLE) AS trade_size_usd,
-        TRY_CAST(total_rebate_factor AS DOUBLE) AS total_rebate_factor,
-        TRY_CAST(trader_discount_factor AS DOUBLE) AS trader_discount_factor,
-        TRY_CAST(total_rebate_amount AS DOUBLE) AS total_rebate_amount,
-        TRY_CAST(trader_discount_amount AS DOUBLE) AS trader_discount_amount,
-        TRY_CAST(affiliate_reward_amount AS DOUBLE) AS affiliate_reward_amount,
         TRY_CAST(funding_fee_amount AS DOUBLE) AS funding_fee_amount,
         TRY_CAST(claimable_long_token_amount AS DOUBLE) AS claimable_long_token_amount,
         TRY_CAST(claimable_short_token_amount AS DOUBLE) AS claimable_short_token_amount,
@@ -223,11 +242,29 @@ WITH evt_data_1 AS (
         TRY_CAST(total_cost_amount AS DOUBLE) AS total_cost_amount,
         TRY_CAST(ui_fee_receiver_factor AS DOUBLE) AS ui_fee_receiver_factor,
         TRY_CAST(ui_fee_amount AS DOUBLE) AS ui_fee_amount,
-        TRY_CAST(is_increase AS BOOLEAN) AS is_increase,
-        from_hex(order_key) AS order_key,
-        from_hex(position_key) AS position_key,
-        from_hex(referral_code) AS referral_code
-        
+
+        TRY_CAST(total_rebate_factor AS DOUBLE) AS total_rebate_factor,
+        TRY_CAST(trader_discount_factor AS DOUBLE) AS trader_discount_factor,
+        TRY_CAST(total_rebate_amount AS DOUBLE) AS total_rebate_amount,
+        TRY_CAST(trader_discount_amount AS DOUBLE) AS trader_discount_amount,
+        TRY_CAST(affiliate_reward_amount AS DOUBLE) AS affiliate_reward_amount,
+
+        TRY_CAST(referral_total_rebate_factor AS DOUBLE) AS referral_total_rebate_factor,
+        TRY_CAST(referral_adjusted_affiliate_reward_factor AS DOUBLE) AS referral_adjusted_affiliate_reward_factor,
+        TRY_CAST(referral_trader_discount_factor AS DOUBLE) AS referral_trader_discount_factor,
+        TRY_CAST(referral_total_rebate_amount AS DOUBLE) AS referral_total_rebate_amount,
+        TRY_CAST(referral_trader_discount_amount AS DOUBLE) AS referral_trader_discount_amount,
+        TRY_CAST(referral_affiliate_reward_amount AS DOUBLE) AS referral_affiliate_reward_amount,
+
+        TRY_CAST(pro_trader_discount_factor AS DOUBLE) AS pro_trader_discount_factor,
+        TRY_CAST(pro_trader_discount_amount AS DOUBLE) AS pro_trader_discount_amount,
+
+        TRY_CAST(liquidation_fee_amount AS DOUBLE) AS liquidation_fee_amount, 
+        TRY_CAST(liquidation_fee_receiver_factor AS DOUBLE) AS liquidation_fee_receiver_factor, 
+        TRY_CAST(liquidation_fee_amount_for_fee_receiver AS DOUBLE) AS liquidation_fee_amount_for_fee_receiver, 
+
+        TRY_CAST(is_increase AS BOOLEAN) AS is_increase
+
     FROM evt_data AS ED
     LEFT JOIN evt_data_parsed AS EDP
         ON ED.tx_hash = EDP.tx_hash
@@ -247,6 +284,10 @@ WITH evt_data_1 AS (
         event_name,
         msg_sender,
         
+        order_key,
+        position_key,
+        referral_code,
+
         ED.market AS market,
         ED.collateral_token,
         affiliate,
@@ -255,12 +296,7 @@ WITH evt_data_1 AS (
         
         collateral_token_price_min / POWER(10, 30 - collateral_token_decimals) AS collateral_token_price_min,
         collateral_token_price_max / POWER(10, 30 - collateral_token_decimals) AS collateral_token_price_max,    
-        trade_size_usd / POWER(10, 30) AS trade_size_usd,
-        total_rebate_factor / POWER(10, 30) AS total_rebate_factor,
-        trader_discount_factor / POWER(10, 30) AS trader_discount_factor,
-        total_rebate_amount / POWER(10, collateral_token_decimals) AS total_rebate_amount,
-        trader_discount_amount / POWER(10, collateral_token_decimals) AS trader_discount_amount,
-        affiliate_reward_amount / POWER(10, collateral_token_decimals) AS affiliate_reward_amount,
+        trade_size_usd / POWER(10, 30) AS trade_size_usd,        
         funding_fee_amount / POWER(10, collateral_token_decimals + 15) AS funding_fee_amount,
         claimable_long_token_amount / POWER(10, long_token_decimals + 15) AS claimable_long_token_amount,
         claimable_short_token_amount / POWER(10, short_token_decimals + 15) AS claimable_short_token_amount,
@@ -281,10 +317,28 @@ WITH evt_data_1 AS (
         total_cost_amount / POWER(10, collateral_token_decimals) AS total_cost_amount,
         ui_fee_receiver_factor / POWER(10, 30) AS ui_fee_receiver_factor,
         ui_fee_amount / POWER(10, collateral_token_decimals) AS ui_fee_amount,
-        is_increase,
-        order_key,
-        position_key,
-        referral_code
+
+        total_rebate_factor / POWER(10, 30) AS total_rebate_factor,
+        trader_discount_factor / POWER(10, 30) AS trader_discount_factor,
+        total_rebate_amount / POWER(10, collateral_token_decimals) AS total_rebate_amount,
+        trader_discount_amount / POWER(10, collateral_token_decimals) AS trader_discount_amount,
+        affiliate_reward_amount / POWER(10, collateral_token_decimals) AS affiliate_reward_amount,
+
+        referral_total_rebate_factor / POWER(10, 30) AS referral_total_rebate_factor,
+        referral_adjusted_affiliate_reward_factor / POWER(10, 30) AS referral_adjusted_affiliate_reward_factor,
+        referral_trader_discount_factor / POWER(10, 30) AS referral_trader_discount_factor,
+        referral_total_rebate_amount / POWER(10, collateral_token_decimals) AS referral_total_rebate_amount,
+        referral_trader_discount_amount / POWER(10, collateral_token_decimals) AS referral_trader_discount_amount,
+        referral_affiliate_reward_amount / POWER(10, collateral_token_decimals) AS referral_affiliate_reward_amount,
+        
+        pro_trader_discount_factor / POWER(10, 30) AS pro_trader_discount_factor,
+        pro_trader_discount_amount / POWER(10, collateral_token_decimals) AS pro_trader_discount_amount,
+
+        liquidation_fee_amount / POWER(10, collateral_token_decimals) AS liquidation_fee_amount, 
+        liquidation_fee_receiver_factor / POWER(10, 30) AS liquidation_fee_receiver_factor, 
+        liquidation_fee_amount_for_fee_receiver / POWER(10, collateral_token_decimals) AS liquidation_fee_amount_for_fee_receiver, 
+
+        is_increase
         
     FROM event_data AS ED
     LEFT JOIN {{ ref('gmx_v2_avalanche_c_markets_data') }} AS MD
@@ -293,10 +347,99 @@ WITH evt_data_1 AS (
         ON ED.collateral_token = CTD.collateral_token
 )
 
+, full_data_2 AS (
+    SELECT 
+        blockchain,
+        block_time,
+        block_date,
+        block_number,
+        tx_hash,
+        index,
+        contract_address,
+        event_name,
+        msg_sender,
+        
+        order_key,
+        position_key,
+        referral_code,
+    
+        market,
+        collateral_token,
+        affiliate,
+        trader,
+        ui_fee_receiver,
+        
+        collateral_token_price_min,
+        collateral_token_price_max,
+        trade_size_usd,
+        funding_fee_amount,
+        claimable_long_token_amount,
+        claimable_short_token_amount,
+        latest_funding_fee_amount_per_size,
+        latest_long_token_claimable_funding_amount_per_size,
+        latest_short_token_claimable_funding_amount_per_size,
+        borrowing_fee_usd,
+        borrowing_fee_amount,
+        borrowing_fee_receiver_factor,
+        borrowing_fee_amount_for_fee_receiver,
+        position_fee_factor,
+        protocol_fee_amount,
+        position_fee_receiver_factor,
+        fee_receiver_amount,
+        fee_amount_for_pool,
+        position_fee_amount_for_pool,
+        position_fee_amount,
+        total_cost_amount,
+        ui_fee_receiver_factor,
+        ui_fee_amount,
+    
+        CASE
+            WHEN total_rebate_factor IS NOT NULL THEN total_rebate_factor
+            WHEN referral_total_rebate_factor IS NOT NULL THEN referral_total_rebate_factor
+            ELSE 0
+        END AS referral_total_rebate_factor,
+        CASE
+            WHEN total_rebate_amount IS NOT NULL THEN total_rebate_amount
+            WHEN referral_total_rebate_amount IS NOT NULL THEN referral_total_rebate_amount
+            ELSE 0
+        END AS referral_total_rebate_amount,
+        CASE 
+            WHEN referral_trader_discount_factor IS NOT NULL THEN referral_trader_discount_factor
+            WHEN trader_discount_factor IS NOT NULL THEN trader_discount_factor * total_rebate_factor
+            ELSE 0
+        END AS referral_trader_discount_factor,
+        CASE 
+            WHEN referral_adjusted_affiliate_reward_factor IS NOT NULL THEN referral_adjusted_affiliate_reward_factor
+            WHEN affiliate_reward_amount IS NOT NULL THEN affiliate_reward_amount
+            ELSE 0
+        END AS referral_adjusted_affiliate_reward_factor,
+        CASE 
+            WHEN referral_affiliate_reward_amount IS NOT NULL THEN referral_affiliate_reward_amount
+            WHEN affiliate_reward_amount IS NOT NULL THEN affiliate_reward_amount
+            ELSE 0
+        END AS referral_affiliate_reward_amount,
+        CASE 
+            WHEN referral_trader_discount_amount IS NOT NULL THEN referral_trader_discount_amount
+            WHEN trader_discount_amount IS NOT NULL THEN trader_discount_amount
+            ELSE 0
+        END AS referral_trader_discount_amount,
+    
+        COALESCE(pro_trader_discount_factor, 0) AS pro_trader_discount_factor,
+        COALESCE(pro_trader_discount_amount, 0) AS pro_trader_discount_amount,
+    
+        COALESCE(liquidation_fee_amount, 0) AS liquidation_fee_amount,
+        COALESCE(liquidation_fee_receiver_factor, 0) AS liquidation_fee_receiver_factor,
+        COALESCE(liquidation_fee_amount_for_fee_receiver, 0) AS liquidation_fee_amount_for_fee_receiver, 
+    
+        is_increase
+    
+    FROM full_data
+)
+
 --can be removed once decoded tables are fully denormalized
 {{
     add_tx_columns(
-        model_cte = 'full_data'
+        model_cte = 'full_data_2'
         , blockchain = blockchain_name
         , columns = ['from', 'to']
     )
