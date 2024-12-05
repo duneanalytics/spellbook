7bbe3f9db791131d4ac66dcb64ada04925ed6f58
jeff-dude <jeff@dune.xyz>
2024-12-05 18:03:10 -0500

    update unique keys to include address

--- dbt_subprojects/daily_spellbook/models/_metrics/fees/chains/evm/metrics_sei_gas_fees_daily.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/fees/chains/evm/metrics_sei_gas_fees_daily.sql
@@ -6,7 +6,7 @@
         , materialized = 'incremental'
         , file_format = 'delta'
         , incremental_strategy = 'merge'
-        , unique_key = ['blockchain', 'block_date']
+        , unique_key = ['blockchain', 'block_date', 'address']
         , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
         )
 }}
