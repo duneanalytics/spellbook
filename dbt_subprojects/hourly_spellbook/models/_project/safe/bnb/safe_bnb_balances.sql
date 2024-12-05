8676479ef84f48011942c8481e87861e20267bf1
Peter <157015397+safeintern@users.noreply.github.com>
2024-12-05 19:15:39 +0800

    Update safe_bnb_balances.sql v2

--- dbt_subprojects/hourly_spellbook/models/_project/safe/bnb/safe_bnb_balances.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/bnb/safe_bnb_balances.sql
@@ -30,7 +30,7 @@ balances as (
 )
 
 select * from balances
-where token_standard in ('native', 'bep20', 'erc20')
+where token_standard in ('native', 'bep20') -- bnb has no erc20
 and token_address not in (
             0xd74f5255d557944cf7dd0e45ff521520002d5748, --$9.8B were minted in a hack in 2023, all of which are stored in a Safe. Filtering out.
             0xe9689028ede16c2fdfe3d11855d28f8e3fc452a3 )
