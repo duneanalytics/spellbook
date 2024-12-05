ef1221609ce810711c1bfae791f48766ee7e7e8d
darvin <todarvinrio@gmail.com>
2024-12-06 03:22:09 +0530

    revert

--- dbt_subprojects/daily_spellbook/models/swell/balances/ethereum/swell_balances_ethereum_core_assets.sql
+++ dbt_subprojects/daily_spellbook/models/swell/balances/ethereum/swell_balances_ethereum_core_assets.sql
@@ -17,13 +17,13 @@
 with
 tokens as (
   select * from (values
-    -- (0xf951E335afb289353dc249e82926178EaC7DEd78,   'swETH', 'Swell LRT'),
-    -- (0xFAe103DC9cf190eD75350761e95403b7b8aFa6c0,  'rswETH', 'Swell LRT'),
-    -- (0x0a6E7Ba5042B38349e437ec6Db6214AEC7B35676,   'SWELL', 'Swell LRT'),
+    (0xf951E335afb289353dc249e82926178EaC7DEd78,   'swETH', 'Swell LRT'),
+    (0xFAe103DC9cf190eD75350761e95403b7b8aFa6c0,  'rswETH', 'Swell LRT'),
+    (0x0a6E7Ba5042B38349e437ec6Db6214AEC7B35676,   'SWELL', 'Swell LRT'),
     (0x358d94b5b2F147D741088803d932Acb566acB7B6,  'rSWELL', 'Swell LRT'),
-    (0x9Ed15383940CC380fAEF0a75edacE507cC775f22, 'earnETH', 'Swell LRT')
-    -- (0x66E47E6957B85Cf62564610B76dD206BB04d831a, 'earnBTC', 'Swell LRT'),
-    -- (0x8DB2350D78aBc13f5673A411D4700BCF87864dDE,   'swBTC', 'Swell LRT')
+    (0x9Ed15383940CC380fAEF0a75edacE507cC775f22, 'earnETH', 'Swell LRT'),
+    (0x66E47E6957B85Cf62564610B76dD206BB04d831a, 'earnBTC', 'Swell LRT'),
+    (0x8DB2350D78aBc13f5673A411D4700BCF87864dDE,   'swBTC', 'Swell LRT')
   ) as t(
     token_address,
     symbol,
