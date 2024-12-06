59ea201137f84628faefd25e8535a22e5c75ba3c
PatelPrinci <principatel@lampros.tech>
2024-12-06 16:32:54 +0530

    changed seed file

--- dbt_subprojects/dex/models/trades/base/platforms/swapline_base_base_trades.sql
+++ dbt_subprojects/dex/models/trades/base/platforms/swapline_base_base_trades.sql
@@ -14,6 +14,7 @@ WITH swap_events AS (
     SELECT
         t.evt_tx_hash AS tx_hash,
         t.evt_index,
+        t.contract_address,
         t.evt_block_time AS block_time,
         t.evt_block_number AS block_number,
         t.to AS token_bought_address,
@@ -26,6 +27,7 @@ pair_creation_events AS (
     SELECT
         t.evt_tx_hash AS tx_hash,
         t.evt_index,
+        t.contract_address,
         t.evt_block_time AS block_time,
         t.evt_block_number AS block_number,
         t.tokenY AS token_bought_address,
@@ -36,6 +38,7 @@ SELECT DISTINCT
     'base' AS blockchain,
     'swapline' AS project,
     '1' AS version,
+    swap.contract_address AS project_contract_address,
     swap.block_time AS block_time,
     CAST(date_trunc('month', swap.block_time) AS date) AS block_month,
     CAST(date_trunc('day', swap.block_time) AS date) AS block_date,
