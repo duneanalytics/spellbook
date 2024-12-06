45068d886cb63a898397dc5469ab4b4b704f0d6a
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-12-06 17:41:55 +0100

    add 4th option

--- dbt_subprojects/tokens/models/transfers_and_balances/ethereum/temp/balaces_daily_by_wallet.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/ethereum/temp/balaces_daily_by_wallet.sql
@@ -10,5 +10,5 @@
 select
 *
 ,cast(varbinary_substring(address, 0,2) as varchar) as address_partition
-from {{ref('tokens_ethereum_balances')}}
-where block_time > now() - interval '30' day
+from {{ref('tokens_ethereum_balances_daily_agg')}}
+where day > now() - interval '30' day
