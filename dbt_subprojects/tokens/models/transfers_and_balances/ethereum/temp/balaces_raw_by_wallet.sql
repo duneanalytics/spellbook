2278d63c3243737386e99cadce0b247f3cc6a43d
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-12-06 16:26:23 +0100

    use daily_agg

--- dbt_subprojects/tokens/models/transfers_and_balances/ethereum/temp/balaces_raw_by_wallet.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/ethereum/temp/balaces_raw_by_wallet.sql
@@ -10,5 +10,5 @@
 select
 *
 ,varbinary_substring(address, 0,2) as address_partition
-from {{ref('tokens_ethereum_balances_daily')}}
+from {{ref('tokens_ethereum_balances_daily_agg')}}
 where day > now() - interval '30' day
