2851d52d8279f60c0b2b5e913100341db91d7498
viniabussafi <vinicius@balancerlabs.dev>
2024-12-09 11:41:55 +0000

    fix protocol_fee

--- dbt_macros/shared/balancer/balancer_protocol_fee_macro.sql
+++ dbt_macros/shared/balancer/balancer_protocol_fee_macro.sql
@@ -173,7 +173,7 @@ WITH pool_labels AS (
         ON r.day = f.day
     LEFT JOIN pool_labels l
         ON BYTEARRAY_SUBSTRING(f.pool_id,1,20) = l.address
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 13
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14
 
 {% endmacro %}
 
