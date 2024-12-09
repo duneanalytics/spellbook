5be7e7c75eec59d1372390cd0c890bcae5e56904
viniabussafi <vinicius@balancerlabs.dev>
2024-12-09 11:35:15 +0000

    fix liquidity macro

--- dbt_macros/shared/balancer/balancer_liquidity_macro.sql
+++ dbt_macros/shared/balancer/balancer_liquidity_macro.sql
@@ -226,7 +226,7 @@ WITH pool_labels AS (
         AND p2.token = b.token
         LEFT JOIN bpt_prices p3 ON p3.day = b.day
         AND p3.token = b.token
-        LEFT JOIN gyro_prices p4 ON p4.token = b.token
+        LEFT JOIN gyro_prices p4 ON p4.token_address = b.token
         WHERE b.token != BYTEARRAY_SUBSTRING(b.pool_id, 1, 20)
     ),
 
