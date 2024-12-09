91830d81f010e108bf56bc4fd4f805ad82cbc86a
viniabussafi <vinicius@balancerlabs.dev>
2024-12-09 11:00:25 +0000

    again2

---
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/static_tokens/balancer_v3_erc4626_tokens_prices.sql
@@ -0,0 +1,34 @@
+{{ config(
+    schema = 'balance_v3',
+    alias = 'erc4626_token_prices',
+    post_hook='{{ expose_spells(blockchains = \'["ethereum", "gnosis"]\',
+                                spell_type = "project",
+                                spell_name = "balancer",
+                                contributors = \'["viniabussafi"]\') }}'
+    )
+}}
+
+{% set balancer_models = [
+    ref('balancer_v3_ethereum_erc4626_tokens_prices'),
+    ref('balancer_v3_gnosis_erc4626_tokens_prices')
+] %}
+
+SELECT *
+FROM (
+    {% for model in balancer_models %}
+    SELECT
+        minute,
+        blockchain,
+        wrapped_token,
+        underlying_token,
+        erc4626_token_symbol,
+        underlying_token_symbol,
+        decimals,
+        median_price,
+        next_change
+    FROM {{ model }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)
