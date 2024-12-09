0228adf93ada1a3c81f96cc008c85c80c316a369
Huang Geyang <Sukebeta@outlook.com>
2024-12-09 23:39:03 +0800

    Update prices

--- dbt_subprojects/tokens/models/prices/flare/prices_flare_tokens.sql
+++ dbt_subprojects/tokens/models/prices/flare/prices_flare_tokens.sql
@@ -22,4 +22,6 @@ FROM
     , ('joule-kinetic', 'JOULE', 0xE6505f92583103AF7ed9974DEC451A7Af4e3A3bE, 18)
     , ('usdc.e-usd-coin.e', 'USDC.e', 0xFbDa5F676cB37624f28265A144A48B0d6e87d3b6, 6)
     , ('usdt-tether', 'USDT', 0x0B38e83B86d491735fEaa0a791F65c2B99535396, 6)
+    , ('phili-inu', 'PHIL', 0x932E691aA8c8306C4bB0b19F3f00a284371be8Ba, 18)
+    , ('flare-inu', 'FINU', 0x2F8d7ff4f55D7B3E10D4910BeB38209291843B36, 18)
 ) as temp (token_id, symbol, contract_address, decimals)
