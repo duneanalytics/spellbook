2bcc973cdc61ab4415c07cdbca0c819873b55355
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-12-09 19:48:13 +0100

    check with {{this}}

---
+++ dbt_subprojects/tokens/models/transfers_and_balances/ethereum/tests/incremental_macro_on_daily_withthis.sql
@@ -0,0 +1,32 @@
+{{
+   config(
+     schema = 'tests',
+     alias = 'macro_on_daily',
+     materialized = 'incremental',
+     file_format = 'delta',
+     incremental_strategy = 'merge',
+     unique_key = ['day', 'address', 'token_address'],
+     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
+   )
+ }}
+
+with safes as (
+    select
+        address,
+        blockchain
+    from safe_ethereum.safes
+    where blockchain = 'ethereum'
+),
+
+balances as (
+     {{
+       balance_macro_copy_withthis(
+             blockchain = 'ethereum',
+             address_list  = 'safes',
+             start_date = '2021-07-01',
+             balances_source = ref('balances_daily')
+       )
+     }}
+)
+
+select * from balances
