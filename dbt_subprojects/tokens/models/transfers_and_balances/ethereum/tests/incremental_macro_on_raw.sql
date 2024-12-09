8c0c68d00d8d541e5d4b04240d8aecd9550bea96
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-12-09 10:01:15 +0100

    add incremental balance models

---
+++ dbt_subprojects/tokens/models/transfers_and_balances/ethereum/tests/incremental_macro_on_raw.sql
@@ -0,0 +1,33 @@
+{{
+   config(
+     schema = 'tests',
+     alias = 'macro_on_draw',
+     materialized = 'incremental',
+     file_format = 'delta',
+     incremental_strategy = 'merge',
+     unique_key = ['day', 'address', 'token_address'],
+     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
+   )
+ }}
+
+with safes as (
+    select
+        address,
+        blockchain
+    from {{ ref('safe_ethereum_safes') }}
+    where blockchain = 'ethereum'
+    limit 100000
+),
+
+balances as (
+     {{
+       balance_macro_copy(
+             blockchain = 'ethereum',
+             address_list  = 'safes',
+             start_date = '2021-07-01',
+             balances_source = ref('balances_raw')
+       )
+     }}
+)
+
+select * from balances
