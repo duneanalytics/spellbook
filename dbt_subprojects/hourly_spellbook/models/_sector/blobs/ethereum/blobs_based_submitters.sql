7911155429fa3cae04362c801cd5b6b7340f719a
Hildebert Moulié <h.moulie@gmail.com>
2024-12-09 18:18:22 +0100

    add taiko and pandasea blobs

--- dbt_subprojects/hourly_spellbook/models/_sector/blobs/ethereum/blobs_based_submitters.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/blobs/ethereum/blobs_based_submitters.sql
@@ -4,3 +4,7 @@
 )}}
 
 SELECT evt_tx_hash AS tx_hash, evt_block_number AS block_number, 'Taiko' AS entity FROM {{ source('taikoxyz_ethereum', 'TaikoL1_evt_BlockProposed')}}
+
+UNION ALL
+
+SELECT evt_tx_hash AS tx_hash, evt_block_number AS block_number, 'Taiko' AS entity FROM {{ source('taikoxyz_ethereum', 'TaikoL1_evt_BlockProposedV2')}}
