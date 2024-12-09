dfd137c0cebe04eb0248a97c7975afd8f1147454
Phu <33654804+lequangphu@users.noreply.github.com>
2024-12-10 00:13:34 +0700

    Bridges of LI.FI across chains, the contract is LiFiDiamond_v2 (#7172)
    * Add Odos on Arbitrum to dex_aggregator
    
    * Change to WETH address on Arbitrum
    
    * Add sources of Odos on Arbitrum
    
    * Add odos/arbitrum seeds
    
    * Change data type
    
    * Add bridges of LiFi across chains
    
    * replace tests which is deprecated
    
    * fix name error of avalanche source
    
    * add index to unique test
    
    * fix concat issue
    
    * fix unique test of the main model
    
    * try to fix unique key
    
    * use surrogate key for data tests
    
    * use generate_surrogate_key inside the models
    
    * fix and remove redundant schema properties
    
    * data_tests
    
    * move generate_surrogate_key to schema.yml
    
    * 1. create macro lifi_extract_bridge_data_macro.sql
    and apply it in the models
    2. update the models' config
    3. flip materialization strategy
    
    * data_ again
    
    * fix naming of the main model
    
    * replace avalanche with avalanche_c
    
    * avalanche_c again
    
    * 1. add block_date column to the macro to use it in
    the add_tx_columns macro
    2. update model of avalanche_c
    
    * rename columns to use add_tx_columns macro
    
    * remove evt_
    
    * a few minor changes
    
    * add amount_usd column
    
    * data_tests again
    
    * map native token to wrapped token to avoid null
    price data
    
    * update the main model and schema
    
    * move type conversion from models to macro
    
    * fix source name
    
    * varbinary type doesn't need single quotes
    
    * correct collumn name
    
    * add post_hook
    
    ---------
    
    Co-authored-by: Huang Geyang <Sukebeta@outlook.com>
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

---
+++ dbt_subprojects/daily_spellbook/models/_projects/lifi/bnb/lifi_bnb_transfers.sql
@@ -0,0 +1,47 @@
+{{ config(
+    schema = 'lifi_bnb',
+    alias = 'transfers',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['transfer_id'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+with source_data as (
+    {{ lifi_extract_bridge_data('bnb') }}
+),
+
+tokens_mapped as (
+    select
+        *,
+        case
+            when sendingAssetId = 0x0000000000000000000000000000000000000000
+            then 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c -- WBNB
+            else sendingAssetId
+        end as sendingAssetId_adjusted
+    from source_data
+),
+
+price_data as (
+    select 
+        tokens_mapped.*,
+        p.price * minAmount / power(10, p.decimals) as amount_usd
+    from tokens_mapped
+    left join {{ source('prices', 'usd') }} p 
+        on p.contract_address = tokens_mapped.sendingAssetId_adjusted
+        and p.blockchain = 'bnb'
+        and p.minute = date_trunc('minute', tokens_mapped.block_time)
+        {% if is_incremental() %}
+        and {{ incremental_predicate('p.minute') }}
+        {% endif %}
+)
+
+{{
+    add_tx_columns(
+        model_cte = 'price_data'
+        , blockchain = 'bnb'
+        , columns = ['from']
+    )
+}}
