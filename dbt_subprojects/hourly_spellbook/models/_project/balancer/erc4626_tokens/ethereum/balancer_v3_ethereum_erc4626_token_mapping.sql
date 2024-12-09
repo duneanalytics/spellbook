4b6db51951a8c81bdb3b2e69ba461cf30d62609c
viniabussafi <vinicius@balancerlabs.dev>
2024-12-09 20:24:20 +0000

    fix references

--- dbt_subprojects/hourly_spellbook/models/_project/balancer/erc4626_tokens/ethereum/balancer_v3_ethereum_erc4626_token_mapping.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/erc4626_tokens/ethereum/balancer_v3_ethereum_erc4626_token_mapping.sql
@@ -9,15 +9,15 @@
 
 WITH aave_tokens AS(
 SELECT 
-    b.erc4626Token AS erc4626_token,
-    a.erc4626TokenName AS erc4626_token_name,
-    a.erc4626TokenSymbol AS erc4626_token_symbol,
+    b.staticAToken AS erc4626_token,
+    a.staticATokenName AS erc4626_token_name,
+    a.staticATokenSymbol AS erc4626_token_symbol,
     b.underlying AS underlying_token,
     t.symbol AS underlying_token_symbol,
     t.decimals AS underlying_token_decimals
 FROM {{ source('aave_ethereum', 'StaticATokenLM_evt_Initialized') }} a
 JOIN {{ source('aave_ethereum', 'StaticATokenFactory_evt_StaticTokenCreated') }} b
-ON b.erc4626Token = a.contract_address
+ON b.staticAToken = a.contract_address
 JOIN {{ source('aave_v3_ethereum', 'VariableDebtToken_evt_Initialized') }} c
 ON a.aToken = c.contract_address
 JOIN {{ source('tokens', 'erc20') }} t
