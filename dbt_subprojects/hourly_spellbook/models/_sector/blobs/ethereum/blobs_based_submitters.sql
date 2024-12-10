08bc6f83645603f52f5e7230daf5decb24616a2b
Hildebert Moulié <h.moulie@gmail.com>
2024-12-10 13:10:42 +0100

    fix

--- dbt_subprojects/hourly_spellbook/models/_sector/blobs/ethereum/blobs_based_submitters.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/blobs/ethereum/blobs_based_submitters.sql
@@ -3,8 +3,11 @@
     alias = 'based_submitters'
 )}}
 
-SELECT evt_tx_hash AS tx_hash, evt_block_number AS block_number, 'Taiko' AS entity FROM {{ source('taikoxyz_ethereum', 'TaikoL1_evt_BlockProposed')}}
+SELECT DISTINCT tx_hash, block_number, entity
+FROM (
+    SELECT evt_tx_hash AS tx_hash, evt_block_number AS block_number, 'Taiko' AS entity FROM {{ source('taikoxyz_ethereum', 'TaikoL1_evt_BlockProposed')}}
 
-UNION ALL
+    UNION ALL
 
-SELECT evt_tx_hash AS tx_hash, evt_block_number AS block_number, 'Taiko' AS entity FROM {{ source('taikoxyz_ethereum', 'TaikoL1_evt_BlockProposedV2')}}
+    SELECT evt_tx_hash AS tx_hash, evt_block_number AS block_number, 'Taiko' AS entity FROM {{ source('taikoxyz_ethereum', 'TaikoL1_evt_BlockProposedV2')}}
+    )
