1e2864f9baa0c988d83b0a4d82c5a9b938a24b45
Huang Geyang <Sukebeta@outlook.com>
2024-12-10 21:09:27 +0800

    Add flare dexes

--- dbt_subprojects/dex/models/trades/flare/dex_flare_base_trades.sql
+++ dbt_subprojects/dex/models/trades/flare/dex_flare_base_trades.sql
@@ -8,6 +8,9 @@
 {% set base_models = [
     ref('enosys_v2_flare_base_trades')
     , ref('enosys_v3_flare_base_trades')
+    , ref('sparkdex_v2_flare_base_trades')
+    , ref('sparkdex_v3_flare_base_trades')
+    , ref('blazeswap_flare_base_trades')
 ] %}
 
 WITH base_union AS (
---
+++ dbt_subprojects/dex/models/trades/flare/platforms/sparkdex_v3_flare_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'sparkdex_v3_flare',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_v3_compatible_trades(
+        blockchain = 'flare',
+        project = 'sparkdex',
+        version = '3',
+        Pool_evt_Swap = source('sparkdex_flare', 'UniswapV3Pool_evt_Swap'),
+        Factory_evt_PoolCreated = source('sparkdex_flare', 'UniswapV3Factory_evt_PoolCreated')
+    )
+}}
