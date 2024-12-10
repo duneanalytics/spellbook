971bfa1819fb0c0126572067b3130454f4e29bb2
Huang Geyang <Sukebeta@outlook.com>
2024-12-10 22:11:58 +0800

    Add sparkdex into dex.info

--- dbt_subprojects/dex/models/dex_info.sql
+++ dbt_subprojects/dex/models/dex_info.sql
@@ -189,4 +189,5 @@ FROM (VALUES
     , ('stablebase', 'StableBase', 'Direct', 'stablebasefi')
     , ('flashliquidity', 'Flashliquidity', 'Direct', 'flashliquidity')
     , ('akronswap', 'Akronswap', 'Direct', 'AkronFinance')
+    , ('sparkdex', 'SparkDEX', 'Direct', 'SparkDexAI')
     ) AS temp_table (project, name, marketplace_type, x_username)
