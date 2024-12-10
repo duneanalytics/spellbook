4a723b03851d7655630c924cb8824429db729dac
alexshchur <60445720+alexshchur@users.noreply.github.com>
2024-12-10 11:34:28 +0000

    fix stats queries (for data cross verification)

--- dbt_subprojects/dex/models/_projects/paraswap/stats.sql
+++ dbt_subprojects/dex/models/_projects/paraswap/stats.sql
@@ -13,7 +13,15 @@ set delta_configs = [
     ['ethereum', 'delta-v2', 'paraswapdelta_ethereum.ParaswapDeltav2_evt_OrderSettled', 'contract_address', 'evt_block_time', 'evt_tx_hash', null],
     ['base', 'delta-v2', 'paraswapdelta_base.ParaswapDeltav2_evt_OrderSettled', 'contract_address', 'evt_block_time', 'evt_tx_hash', null],
 
-    ['ethereum', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap'"],
+    ['ethereum', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap' and blockchain='ethereum'"],
+
+    ['polygon', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap' and blockchain='polygon'"],
+    ['bnb', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap' and blockchain='bnb'"],
+    ['arbitrum', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap' and blockchain='arbitrum'"],
+    ['avalanche_c', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap' and blockchain='avalanche_c'"],
+    ['fantom', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap' and blockchain='fantom'"],
+    ['optimism', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap' and blockchain='optimism'"],
+    ['base', 'augustus', 'dex_aggregator.trades', 'project_contract_address', 'block_time', 'tx_hash', "project='paraswap' and blockchain='base'"],
 
 
     
@@ -21,8 +29,9 @@ set delta_configs = [
 ]
 
 %}  
+with entities as (
     {% for blockchain, entity, table, contract_field_name, time_field_name, hash_field_name, conditional in delta_configs %}
-        select '{{ entity }}' as entity, '{{ blockchain }}' as blockchain, {{contract_field_name}}, {{time_field_name}}, {{hash_field_name}} from {{ table }}
+        select '{{ entity }}' as entity, '{{ blockchain }}' as blockchain, {{contract_field_name}} as contract_address, {{time_field_name}}, {{hash_field_name}} from {{ table }}
         where 
             ({{time_field_name}} BETWEEN {{date_from}} AND {{date_to}})
             {% if conditional %}
@@ -30,3 +39,8 @@ set delta_configs = [
             {% endif %}
         {% if not loop.last %} union all {% endif %}
     {% endfor %}
+)
+select blockchain, entity, contract_address, count(*) as qty
+from entities
+group by 1,2,3
+order by qty desc 
--- dbt_subprojects/dex/target/compiled/dex/models/_projects/paraswap/stats.sql
+++ dbt_subprojects/dex/target/compiled/dex/models/_projects/paraswap/stats.sql
@@ -2,36 +2,98 @@
 
 
   
+with entities as (
     
-        select 'delta-v1-single' as entity, 'ethereum' as blockchain, contract_address, call_block_time, call_tx_hash from paraswapdelta_ethereum.ParaswapDeltav1_call_settleSwap
+        select 'delta-v1-single' as entity, 'ethereum' as blockchain, contract_address as contract_address, call_block_time, call_tx_hash from paraswapdelta_ethereum.ParaswapDeltav1_call_settleSwap
         where 
             (call_block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
             
          union all 
     
-        select 'delta-v1-batch' as entity, 'ethereum' as blockchain, contract_address, call_block_time, call_tx_hash from paraswapdelta_ethereum.ParaswapDeltav1_call_safeSettleBatchSwap
+        select 'delta-v1-batch' as entity, 'ethereum' as blockchain, contract_address as contract_address, call_block_time, call_tx_hash from paraswapdelta_ethereum.ParaswapDeltav1_call_safeSettleBatchSwap
         where 
             (call_block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
             
          union all 
     
-        select 'delta-v2' as entity, 'ethereum' as blockchain, contract_address, evt_block_time, evt_tx_hash from paraswapdelta_ethereum.ParaswapDeltav2_evt_OrderSettled
+        select 'delta-v2' as entity, 'ethereum' as blockchain, contract_address as contract_address, evt_block_time, evt_tx_hash from paraswapdelta_ethereum.ParaswapDeltav2_evt_OrderSettled
         where 
             (evt_block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
             
          union all 
     
-        select 'delta-v2' as entity, 'base' as blockchain, contract_address, evt_block_time, evt_tx_hash from paraswapdelta_base.ParaswapDeltav2_evt_OrderSettled
+        select 'delta-v2' as entity, 'base' as blockchain, contract_address as contract_address, evt_block_time, evt_tx_hash from paraswapdelta_base.ParaswapDeltav2_evt_OrderSettled
         where 
             (evt_block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
             
          union all 
     
-        select 'augustus' as entity, 'ethereum' as blockchain, project_contract_address, block_time, tx_hash from dex_aggregator.trades
+        select 'augustus' as entity, 'ethereum' as blockchain, project_contract_address as contract_address, block_time, tx_hash from dex_aggregator.trades
         where 
             (block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
             
-            AND project='paraswap'
+            AND project='paraswap' and blockchain='ethereum'
+            
+         union all 
+    
+        select 'augustus' as entity, 'polygon' as blockchain, project_contract_address as contract_address, block_time, tx_hash from dex_aggregator.trades
+        where 
+            (block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
+            
+            AND project='paraswap' and blockchain='polygon'
+            
+         union all 
+    
+        select 'augustus' as entity, 'bnb' as blockchain, project_contract_address as contract_address, block_time, tx_hash from dex_aggregator.trades
+        where 
+            (block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
+            
+            AND project='paraswap' and blockchain='bnb'
+            
+         union all 
+    
+        select 'augustus' as entity, 'arbitrum' as blockchain, project_contract_address as contract_address, block_time, tx_hash from dex_aggregator.trades
+        where 
+            (block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
+            
+            AND project='paraswap' and blockchain='arbitrum'
+            
+         union all 
+    
+        select 'augustus' as entity, 'avalanche_c' as blockchain, project_contract_address as contract_address, block_time, tx_hash from dex_aggregator.trades
+        where 
+            (block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
+            
+            AND project='paraswap' and blockchain='avalanche_c'
+            
+         union all 
+    
+        select 'augustus' as entity, 'fantom' as blockchain, project_contract_address as contract_address, block_time, tx_hash from dex_aggregator.trades
+        where 
+            (block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
+            
+            AND project='paraswap' and blockchain='fantom'
+            
+         union all 
+    
+        select 'augustus' as entity, 'optimism' as blockchain, project_contract_address as contract_address, block_time, tx_hash from dex_aggregator.trades
+        where 
+            (block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
+            
+            AND project='paraswap' and blockchain='optimism'
+            
+         union all 
+    
+        select 'augustus' as entity, 'base' as blockchain, project_contract_address as contract_address, block_time, tx_hash from dex_aggregator.trades
+        where 
+            (block_time BETWEEN DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1' day AND DATE_TRUNC('day', CURRENT_TIMESTAMP))
+            
+            AND project='paraswap' and blockchain='base'
             
         
     
+)
+select blockchain, entity, contract_address, count(*) as qty
+from entities
+group by 1,2,3
+order by qty desc
