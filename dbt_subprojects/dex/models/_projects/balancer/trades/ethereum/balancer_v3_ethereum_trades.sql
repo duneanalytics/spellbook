239b8a52e494e166c6bf225d4885ed0babc897cd
viniabussafi <vinicius@balancerlabs.dev>
2024-12-10 07:42:10 +0000

    fix trades

--- dbt_subprojects/dex/models/_projects/balancer/trades/ethereum/balancer_v3_ethereum_trades.sql
+++ dbt_subprojects/dex/models/_projects/balancer/trades/ethereum/balancer_v3_ethereum_trades.sql
@@ -70,7 +70,7 @@ WITH
             dexs.block_time,
             MAX(bpt_prices.day) AS bpa_max_block_date
         FROM dexs
-            LEFT JOIN {{ source('balancer_v3', 'bpt_prices') }} bpt_prices
+            LEFT JOIN {{ source('balancer', 'bpt_prices') }} bpt_prices
                 ON bpt_prices.contract_address = dexs.token_bought_address
                 AND bpt_prices.day <= DATE_TRUNC('day', dexs.block_time)
                 AND bpt_prices.blockchaih = 'ethereum'                
@@ -86,7 +86,7 @@ WITH
             dexs.block_time,
             MAX(bpt_prices.day) AS bpb_max_block_date
         FROM dexs
-            LEFT JOIN {{ source('balancer_v3', 'bpt_prices') }} bpt_prices
+            LEFT JOIN {{ source('balancer', 'bpt_prices') }} bpt_prices
                 ON bpt_prices.contract_address = dexs.token_sold_address
                 AND bpt_prices.day <= DATE_TRUNC('day', dexs.block_time)
                 AND bpt_prices.blockchaih = 'ethereum'
@@ -137,7 +137,7 @@ FROM dexs
         ON bpa.block_number = dexs.block_number
         AND bpa.tx_hash = dexs.tx_hash
         AND bpa.evt_index = dexs.evt_index
-    LEFT JOIN {{ source('balancer_v3', 'bpt_prices') }} bpa_bpt_prices
+    LEFT JOIN {{ source('balancer', 'bpt_prices') }} bpa_bpt_prices
         ON bpa_bpt_prices.contract_address = bpa.contract_address
         AND bpa_bpt_prices.day = bpa.bpa_max_block_date
         AND bpa_bpt_prices.blockchaih = 'ethereum'        
@@ -145,7 +145,7 @@ FROM dexs
         ON bpb.block_number = dexs.block_number
         AND bpb.tx_hash = dexs.tx_hash
         AND bpb.evt_index = dexs.evt_index   
-    LEFT JOIN {{ source('balancer_v3', 'bpt_prices') }} bpb_bpt_prices
+    LEFT JOIN {{ source('balancer', 'bpt_prices') }} bpb_bpt_prices
         ON bpb_bpt_prices.contract_address = bpb.contract_address
         AND bpb_bpt_prices.day = bpb.bpb_max_block_date
         AND bpb_bpt_prices.blockchaih = 'ethereum'
