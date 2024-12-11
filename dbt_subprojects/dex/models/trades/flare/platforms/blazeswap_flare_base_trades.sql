c11c6badb424c7d71b841f33a7278ddc89f0f664
Huang Geyang <Sukebeta@outlook.com>
2024-12-11 15:48:15 +0800

    Revert "Remove blazeswap as source not available yet"
    This reverts commit b962d91328e91c0bb8e18c3e0630291b5a568516.

---
+++ dbt_subprojects/dex/models/trades/flare/platforms/blazeswap_flare_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'blazeswap_flare',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v2_trades(
+        blockchain = 'flare',
+        project = 'blazeswap',
+        version = '2',
+        Pair_evt_Swap = source('blazeswap_flare', 'BlazeSwapPair_evt_Swap'),
+        Factory_evt_PairCreated = source('blazeswap_flare', 'BlazeSwapFactory_evt_PairCreated')
+    )
+}}
