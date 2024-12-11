cb9e353241b0a3dea0b55522d162610b3e1cff0e
imimim-username <100382074+imimim-username@users.noreply.github.com>
2024-12-11 12:49:15 -0500

    Added a few tokens (#7298)
    * Added jUSDC and farmdWETHV3 tokens
    
    * added apxETH to tokens

--- dbt_subprojects/tokens/models/tokens/ethereum/tokens_ethereum_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/ethereum/tokens_ethereum_erc20.sql
@@ -4728,4 +4728,5 @@ FROM (VALUES
     , ( 0x358d94b5b2F147D741088803d932Acb566acB7B6, 'rSWELL', 18)
     , ( 0x9Ed15383940CC380fAEF0a75edacE507cC775f22, 'earnETH', 18)
     , ( 0x66E47E6957B85Cf62564610B76dD206BB04d831a, 'earnBTC', 8)
+    , (0x9Ba021B0a9b958B5E75cE9f6dff97C7eE52cb3E6, 'apxETH', 18)
 ) AS temp_table (contract_address, symbol, decimals)
