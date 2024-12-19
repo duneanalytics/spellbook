4c907d36a169316b0c866dd9e17d9d690ec6253c
alexshchur <60445720+alexshchur@users.noreply.github.com>
2024-12-19 14:42:50 +0000

    feat: expose paraswap v6 decoded trades

--- dbt_subprojects/dex/models/_projects/paraswap/arbitrum/paraswap_v6_arbitrum_trades_decoded.sql
+++ dbt_subprojects/dex/models/_projects/paraswap/arbitrum/paraswap_v6_arbitrum_trades_decoded.sql
@@ -6,7 +6,11 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.blockTime')],
-    unique_key = ['call_tx_hash', 'method', 'call_trace_address']
+    unique_key = ['call_tx_hash', 'method', 'call_trace_address'],    
+    post_hook='{{ expose_spells(\'["arbitrum"]\',
+                                "project",
+                                "paraswap_v6",
+                                \'["eptighte", "mwamedacen"]\') }}'
     )
 }}
 
