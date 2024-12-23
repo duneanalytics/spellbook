f7f4370d0d4c291905df0f2a72c217829b9f1103
viniabussafi <vinicius@balancerlabs.dev>
2024-12-23 08:35:15 +0000

    update tokens.erc20

--- dbt_subprojects/tokens/models/prices/arbitrum/prices_arbitrum_tokens.sql
+++ dbt_subprojects/tokens/models/prices/arbitrum/prices_arbitrum_tokens.sql
@@ -229,6 +229,6 @@ FROM
     ('usdm-mountain-protocol-usd', 'arbitrum', 'USDM', 0x59d9356e565ab3a36dd77763fc0d87feaf85508c, 18),
     ('usdz-anzen-usdz', 'arbitrum', 'USDZ', 0x5018609ab477cc502e170a5accf5312b86a4b94f, 18),
     ('trb-tellor', 'arbitrum', 'TRB', 0xd58d345fd9c82262e087d2d0607624b410d88242, 18),
-    ('mor-morpheusai', 'arbitrum', 'MOR', 0x092baadb7def4c3981454dd9c0a0d7ff07bcfc86, 18)
-    
+    ('mor-morpheusai', 'arbitrum', 'MOR', 0x092baadb7def4c3981454dd9c0a0d7ff07bcfc86, 18),
+    ('usdx-usdxmoney-usdx', 'arbitrum', 'USDX', 0xf3527ef8dE265eAa3716FB312c12847bFBA66Cef, 18)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
