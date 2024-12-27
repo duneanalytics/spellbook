5d6c2a5288c22713d45018cf4e6f94deaa6a265d
0xRob <83790096+0xRobin@users.noreply.github.com>
2024-12-27 15:18:07 +0100

    left anti join

--- dbt_subprojects/tokens/models/prices_v2/minute/prices_v2_dex_minute.sql
+++ dbt_subprojects/tokens/models/prices_v2/minute/prices_v2_dex_minute.sql
@@ -20,7 +20,7 @@ WITH dex_trades_filter_and_unnest as (
     INNER JOIN {{ref('prices_trusted_tokens')}} t
         on t.blockchain = d.blockchain
         and t.contract_address = d.token_sold_address -- the token traded against is trusted
-    INNER JOIN {{ref('prices_trusted_tokens')}} anti_t
+    LEFT JOIN {{ref('prices_trusted_tokens')}} anti_t
         on t.blockchain = d.blockchain
         and t.contract_address = d.token_bought_address -- the subjected token is already in trusted
     WHERE d.amount_usd > 0 and token_bought_amount > 0
@@ -41,7 +41,7 @@ WITH dex_trades_filter_and_unnest as (
     INNER JOIN {{ref('prices_trusted_tokens')}} t
         on t.blockchain = d.blockchain
         and t.contract_address = d.token_bought_address -- the token traded against is trusted
-    INNER JOIN {{ref('prices_trusted_tokens')}} anti_t
+    LEFT JOIN {{ref('prices_trusted_tokens')}} anti_t
         on t.blockchain = d.blockchain
         and t.contract_address = d.token_sold_address -- the subjected token is already in trusted
     WHERE d.amount_usd > 0 and token_sold_amount > 0
