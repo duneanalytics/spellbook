3f6bf2e318202a90c2a58d8091d93c02ca5e4dba
jason <jasonreddy4276@gmail.com>
2024-12-27 11:02:29 +0530

    updated sql file

--- dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/aave/aave_pools_optimism_balances.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/aave/aave_pools_optimism_balances.sql
@@ -1,35 +1,25 @@
-{{
-  config(
-    schema = 'top_protocols_op_balances',
-    alias = 'aave_v3_op_balances',
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    unique_key = ['wallet_address', 'snapshot_day']
-  )
-}}
-
 WITH supply_op AS (
   SELECT
-    CAST(depositor AS varchar) AS wallet_address, -- Explicitly cast depositor
+    depositor AS wallet_address,
     token_address,
     SUM(amount) AS total_supplied,
     CAST(evt_block_time AS DATE) AS snapshot_day
-  FROM {{ source('aave_v3_optimism', 'supply') }}
-  WHERE token_address = CAST(FROM_HEX('4200000000000000000000000000000000000042') AS varbinary)
-  GROUP BY depositor, token_address, CAST(evt_block_time AS DATE)
+  FROM {{ source('aave_v3', 'supply') }}
+  WHERE token_address = '0x4200000000000000000000000000000000000042'
+  GROUP BY wallet_address, token_address, snapshot_day
 ),
 borrow_op AS (
   SELECT
-    CAST(borrower AS varchar) AS wallet_address, -- Explicitly cast borrower
+    borrower AS wallet_address,
     token_address,
     SUM(amount) AS total_borrowed,
     CAST(evt_block_time AS DATE) AS snapshot_day
-  FROM {{ source('aave_v3_optimism', 'borrow') }}
-  WHERE token_address = CAST(FROM_HEX('4200000000000000000000000000000000000042') AS varbinary)
-  GROUP BY borrower, token_address, CAST(evt_block_time AS DATE)
-),
-net_balance AS (
+  FROM {{ source('aave_v3', 'borrow') }}
+  WHERE token_address = '0x4200000000000000000000000000000000000042'
+  GROUP BY wallet_address, token_address, snapshot_day
+)
+SELECT *
+FROM (
   SELECT
     COALESCE(s.wallet_address, b.wallet_address) AS wallet_address,
     'aave' AS protocol_name,
@@ -40,8 +30,5 @@ net_balance AS (
   FULL OUTER JOIN borrow_op b
     ON s.wallet_address = b.wallet_address
     AND s.snapshot_day = b.snapshot_day
-)
-
-SELECT *
-FROM net_balance
-WHERE op_balance > 0
+) net_balance
+WHERE op_balance > 0;
