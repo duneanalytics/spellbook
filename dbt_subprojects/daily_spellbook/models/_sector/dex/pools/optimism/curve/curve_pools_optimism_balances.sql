24ea909b2a134adffed0969d90c94e8d2abe5632
jason <jasonreddy4276@gmail.com>
2025-01-02 15:55:22 +0530

    updated sql file

--- dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/curve/curve_pools_optimism_balances.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/curve/curve_pools_optimism_balances.sql
@@ -10,18 +10,25 @@
     )
 }}
 
+WITH op_pools AS (
+    SELECT DISTINCT
+        CAST(pool AS varchar(42)) as pool_address,
+        tokenid,
+        token
+    FROM {{ source('curve_optimism', 'pools') }}
+    WHERE CAST(token AS varchar(42)) = '0x4200000000000000000000000000000000000042'
+)
+
 SELECT 
-    CAST(p.pool AS varchar(42)) as pool_address,
+    p.pool_address,
     p.tokenid,
     p.token,
     b.balance AS op_balance,
     b.day AS snapshot_day
-FROM 
-    {{ source('curve_optimism', 'pools') }} p
-JOIN 
-    {{ source('tokens_optimism', 'balances_daily') }} b
-ON 
-    CAST(p.pool AS varchar(42)) = CAST(b.address AS varchar(42))
-WHERE 
-    CAST(p.token AS varchar(42)) = '0x4200000000000000000000000000000000000042'
-    AND CAST(b.token_address AS varchar(42)) = '0x4200000000000000000000000000000000000042'
+FROM op_pools p
+JOIN {{ source('tokens_optimism', 'balances_daily') }} b
+    ON p.pool_address = CAST(b.address AS varchar(42))
+WHERE CAST(b.token_address AS varchar(42)) = '0x4200000000000000000000000000000000000042'
+{% if is_incremental() %}
+    AND b.day >= current_date - interval '{{ var("DBT_ENV_INCREMENTAL_TIME") }}' {{ var("DBT_ENV_INCREMENTAL_TIME_UNIT") }}
+{% endif %}
