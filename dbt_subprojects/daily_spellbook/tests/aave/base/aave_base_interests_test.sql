72d89e933a6f97a8da38b1fc7c96f7a4b854cbc9
Michael Ghen <mike@mikeghen.com>
2025-01-02 18:04:55 -0500

    Update test values

--- dbt_subprojects/daily_spellbook/tests/aave/base/aave_base_interests_test.sql
+++ dbt_subprojects/daily_spellbook/tests/aave/base/aave_base_interests_test.sql
@@ -1,21 +1,21 @@
 with unit_test1
     as (select case
-                 when abs(variable_borrow_apy - 0.3076100813979409) / 0.3076100813979409 < 0.001
+                 when abs(variable_borrow_apy - 0.10808360395679557) / 0.10808360395679557 < 0.001
                  then true
                  else false
                end as test
         from   {{ ref('aave_v3_base_interest_rates' )}}
-        where  reserve = 0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca -- USDbC
-               and hour = TIMESTAMP '2023-11-11 08:00'),
+        where  reserve = 0x833589fcd6edb6e08f4c7c32d4f71b54bda02913 -- USDC
+               and hour = TIMESTAMP '2025-01-02 20:00'),
     unit_test2
     as (select case
-                 when abs(deposit_apy - 0.00007259786571901644) / 0.00007259786571901644 < 0.001
+                 when abs(deposit_apy - 0.015712521763084) / 0.015712521763084 < 0.001
                  then true
                  else false
                end as test
         from   {{ ref('aave_v3_base_interest_rates' )}}
         where  symbol = 'WETH' -- 0x4200000000000000000000000000000000000006
-               and hour = TIMESTAMP '2023-09-23 07:00')
+               and hour = TIMESTAMP '2024-08-21 03:00')
 select *
 from   (select *
        from   unit_test1
