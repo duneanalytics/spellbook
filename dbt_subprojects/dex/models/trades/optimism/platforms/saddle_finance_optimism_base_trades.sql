368e174013e537788163fcc5f877f320311e28dd
Huang Geyang <Sukebeta@outlook.com>
2025-01-04 00:28:31 +0800

    Fix stablebase and saddle_finance id mapping (#7400)
    * Fix stablebase mapping
    
    * Fix mapping
    
    * Fix seed
    
    * Fix saddle_finance mapping

--- dbt_subprojects/dex/models/trades/optimism/platforms/saddle_finance_optimism_base_trades.sql
+++ dbt_subprojects/dex/models/trades/optimism/platforms/saddle_finance_optimism_base_trades.sql
@@ -8,6 +8,11 @@
     unique_key = ['tx_hash', 'evt_index']
 ) }}
 
+-- Id mapping:
+-- 0 DAI https://optimistic.etherscan.io/token/0xda10009cbd5d07dd0cecc66161fc93d7c9000da1
+-- 1 USDC.e https://optimistic.etherscan.io/token/0x7f5c764cbc14f9669b88837ca1490cca17c31607
+-- 2 USDT https://optimistic.etherscan.io/token/0x94b008aa00579c1307b0ef2c499ad98a8ce58e58
+
 WITH token_swaps AS (
     SELECT
         evt_block_number AS block_number,
@@ -16,8 +21,16 @@ WITH token_swaps AS (
         evt_tx_to AS taker,
         tokensSold AS token_sold_amount_raw,
         tokensBought AS token_bought_amount_raw,
-        CAST(soldId AS varbinary) AS token_sold_address,
-        CAST(boughtId AS varbinary) AS token_bought_address,
+        CASE soldId
+            WHEN 0 THEN 0xda10009cbd5d07dd0cecc66161fc93d7c9000da1
+            WHEN 1 THEN 0x7f5c764cbc14f9669b88837ca1490cca17c31607
+            WHEN 2 THEN 0x94b008aa00579c1307b0ef2c499ad98a8ce58e58
+        END AS token_sold_address,
+        CASE boughtId
+            WHEN 0 THEN 0xda10009cbd5d07dd0cecc66161fc93d7c9000da1
+            WHEN 1 THEN 0x7f5c764cbc14f9669b88837ca1490cca17c31607
+            WHEN 2 THEN 0x94b008aa00579c1307b0ef2c499ad98a8ce58e58
+        END AS token_bought_address,
         contract_address AS project_contract_address,
         evt_tx_hash AS tx_hash,
         evt_index AS evt_index
