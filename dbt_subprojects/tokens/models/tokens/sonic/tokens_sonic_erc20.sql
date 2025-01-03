70f01d0a48acdd65014c4be7ab51d227cee68e3d
Huang Geyang <Sukebeta@outlook.com>
2025-01-04 00:18:58 +0800

    DUX-762 Init sonic chain (#7341)
    * Init sonic chain
    
    * Update dbt_subprojects/daily_spellbook/models/evms/evms_info.sql
    
    * Add docs
    
    * Modify docs
    
    * Update dbt_subprojects/daily_spellbook/models/evms/evms_info.sql
    
    * Add sonic prices
    
    * Add beets
    
    * fix BEETS
    
    * fix weth address, update prices and trusted tokens, add unique test
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    Co-authored-by: 0xRob <83790096+0xRobin@users.noreply.github.com>
    Co-authored-by: jeff-dude <jeff@dune.xyz>

--- dbt_subprojects/tokens/models/tokens/sonic/tokens_sonic_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/sonic/tokens_sonic_erc20.sql
@@ -12,8 +12,8 @@ SELECT
     , symbol
     , decimals
 FROM (VALUES
-      (0x309C92261178fA0CF748A855e90Ae73FDb79EBc7, 'WETH', 18)
-    , (0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38, 'WS', 18)
+      (0x50c42dEAcD8Fc9773493ED674b675bE577f2634b, 'WETH', 18)
+    , (0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38, 'wS', 18)
     , (0x29219dd400f2Bf60E5a23d13Be72B486D4038894, 'USDC.e', 6)
     , (0xe715cba7b5ccb33790cebff1436809d36cb17e57, 'EURC.e', 6)
     , (0xf2f9f482a395b4dd5b5b92173b7d62f3aff92884, 'DOG', 18)
@@ -23,5 +23,5 @@ FROM (VALUES
     , (0x2030170901a9d87f6bd0ca9b8ad130119c7e1173, 'BABYSONIC', 9)
     , (0xe5da20f15420ad15de0fa650600afc998bbe3955, 'stS', 18)
     , (0xd4a5c68a1ed1fc2bb06cba2d90d6adeee7503671, 'HOOPS', 18)
-
+    , (0x2d0e0814e62d80056181f5cd932274405966e4f0, 'BEETS', 18)
 ) as temp (contract_address, symbol, decimals)
