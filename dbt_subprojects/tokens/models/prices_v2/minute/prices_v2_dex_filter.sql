be3556040ef7256b2d89d2bb367ff7fc6e769432
0xRobin <83790096+0xRobin@users.noreply.github.com>
2025-01-03 16:16:30 +0100

    add dex volume filter

---
+++ dbt_subprojects/tokens/models/prices_v2/minute/prices_v2_dex_filter.sql
@@ -0,0 +1,31 @@
+{{ config(
+    schema='prices_v2'
+    , alias = 'dex_filter'
+    , materialized = 'view'
+    )
+}}
+
+WITH dex_volume_over_10k as (
+    blockchain
+    ,contract_address
+    from(
+        SELECT
+            d.blockchain,
+            d.token_bought_address as contract_address,
+            sum(d.amount_usd) as volume -- in USD
+        FROM {{ source('dex','trades') }} d
+        group by 1,2
+        UNION ALL
+        SELECT
+            d.blockchain,
+            d.token_sold_address as contract_address,
+            sum(d.amount_usd) as volume -- in USD
+        FROM {{ source('dex','trades') }} d
+        group by 1,2
+    )
+    group by 1,2
+    having sum(volume) >= 10000
+)
+
+select *
+from dex_volume_over_10k
