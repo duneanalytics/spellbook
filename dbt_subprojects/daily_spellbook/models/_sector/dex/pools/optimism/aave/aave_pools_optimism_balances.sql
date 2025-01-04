646551a15ef2fe41cd1c5149ef9adff94e215014
jason <jasonreddy4276@gmail.com>
2025-01-04 14:37:32 +0530

    updated sql file

--- dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/aave/aave_pools_optimism_balances.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/aave/aave_pools_optimism_balances.sql
@@ -10,82 +10,76 @@
   )
 }}
 
-WITH supply_events AS (
+WITH raw_supply AS (
   SELECT
     depositor AS pool_address,
     DATE(evt_block_time) AS snapshot_day,
     evt_block_time,
     evt_block_number,
-    evt_tx_hash,
-    amount AS supply_amount,
-    'supply' as event_type
+    amount
   FROM {{ source('aave_v3_optimism', 'supply') }}
   WHERE token_address = 0x4200000000000000000000000000000000000042
 ),
 
-borrow_events AS (
+raw_borrow AS (
   SELECT
     borrower AS pool_address,
     DATE(evt_block_time) AS snapshot_day,
     evt_block_time,
     evt_block_number,
-    evt_tx_hash,
-    amount AS borrow_amount,
-    'borrow' as event_type
+    amount
   FROM {{ source('aave_v3_optimism', 'borrow') }}
   WHERE token_address = 0x4200000000000000000000000000000042
 ),
 
-combined_events AS (
+supply_daily AS (
   SELECT
     pool_address,
     snapshot_day,
-    evt_block_time,
-    evt_block_number,
-    evt_tx_hash,
-    supply_amount AS amount,
-    event_type
-  FROM supply_events
-  
-  UNION ALL
-  
-  SELECT
-    pool_address,
-    snapshot_day,
-    evt_block_time,
-    evt_block_number,
-    evt_tx_hash,
-    -borrow_amount AS amount, -- Negative for borrows
-    event_type
-  FROM borrow_events
-),
-
--- Calculate running balance per day
-daily_balances AS (
-  SELECT
-    pool_address,
-    snapshot_day,
-    SUM(amount) as net_balance
-  FROM combined_events
+    SUM(amount) as total_supplied
+  FROM raw_supply
   GROUP BY 1, 2
 ),
 
--- Final output with metadata
+borrow_daily AS (
+  SELECT
+    pool_address,
+    snapshot_day,
+    SUM(amount) as total_borrowed
+  FROM raw_borrow
+  GROUP BY 1, 2
+),
+
+combined_daily AS (
+  SELECT
+    COALESCE(s.pool_address, b.pool_address) as pool_address,
+    COALESCE(s.snapshot_day, b.snapshot_day) as snapshot_day,
+    COALESCE(s.total_supplied, 0) as total_supplied,
+    COALESCE(b.total_borrowed, 0) as total_borrowed,
+    COALESCE(s.total_supplied, 0) - COALESCE(b.total_borrowed, 0) as net_balance
+  FROM supply_daily s
+  FULL OUTER JOIN borrow_daily b
+    ON s.pool_address = b.pool_address
+    AND s.snapshot_day = b.snapshot_day
+),
+
 final_balances AS (
   SELECT DISTINCT
     pool_address,
-    'aave' AS protocol_name,
-    'v3' AS protocol_version,
+    'aave' as protocol_name,
+    'v3' as protocol_version,
     snapshot_day,
-    net_balance as op_balance
-  FROM daily_balances
+    net_balance as op_balance,
+    ROW_NUMBER() OVER (PARTITION BY pool_address, snapshot_day ORDER BY net_balance DESC) as rn
+  FROM combined_daily
   WHERE net_balance > 0
 )
 
-SELECT 
+SELECT
   pool_address,
   protocol_name,
   protocol_version,
   snapshot_day,
   op_balance
 FROM final_balances
+WHERE rn = 1
