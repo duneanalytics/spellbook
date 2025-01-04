b8df5e6f321a61df071cf5e8bc392ed6d4706039
PatelPrinci <principatel@lampros.tech>
2025-01-04 12:25:05 +0530

    token address issue resolved

--- dbt_subprojects/dex/models/trades/base/platforms/curvefi_base_base_trades.sql
+++ dbt_subprojects/dex/models/trades/base/platforms/curvefi_base_base_trades.sql
@@ -1,50 +1,45 @@
-{{ config(
-    schema = 'curvefi_base',
-    alias = 'base_trades',
-    partition_by = ['block_month'],
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index']
-) }}
+{{
+    config(
+        schema = 'curvefi_base',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
 
 WITH token_swaps AS (
-    SELECT
-        evt_block_number AS block_number,
-        CAST(evt_block_time AS timestamp(3) with time zone) AS block_time,
-        evt_tx_from AS maker,
-        evt_tx_to AS taker,
-        tokens_sold AS token_sold_amount_raw,
-        tokens_bought AS token_bought_amount_raw,
-        CAST(sold_id AS varbinary) AS token_sold_address,
-        CAST(bought_id AS varbinary) AS token_bought_address,
-        contract_address AS project_contract_address,
-        evt_tx_hash AS tx_hash,
-        evt_index AS evt_index
-    FROM
-        {{ source('curvefi_base', 'StableSwap_evt_TokenExchange') }}
-    {% if is_incremental() %}
-    WHERE
-        {{ incremental_predicate('evt_block_time') }}
-    {% endif %}
+  SELECT
+    evt_block_number AS block_number,
+    TRY_CAST(evt_block_time AS TIMESTAMP(3) WITH TIME ZONE) AS block_time,
+    evt_tx_from AS maker,
+    evt_tx_to AS taker,
+    in_amount AS token_sold_amount_raw,
+    out_amount AS token_bought_amount_raw,
+    TRY_CAST(route[1] AS VARBINARY) AS token_sold_address,
+    TRY_CAST(route[CARDINALITY(route)] AS VARBINARY) AS token_bought_address,
+    contract_address AS project_contract_address,
+    evt_tx_hash AS tx_hash,
+    evt_index AS evt_index
+  FROM delta_prod.curvefi_base.CurveRouter_evt_Exchange
 )
-
 SELECT
-    'base' AS blockchain,
-    'curvefi' AS project,
-    '1' AS version,
-    CAST(date_trunc('month', token_swaps.block_time) AS date) AS block_month,
-    CAST(date_trunc('day', token_swaps.block_time) AS date) AS block_date,
-    token_swaps.block_time,
-    token_swaps.block_number,
-    token_swaps.token_sold_amount_raw,
-    token_swaps.token_bought_amount_raw,
-    token_swaps.token_sold_address,
-    token_swaps.token_bought_address,
-    token_swaps.maker,
-    token_swaps.taker,
-    token_swaps.project_contract_address,
-    token_swaps.tx_hash,
-    token_swaps.evt_index
-FROM
-    token_swaps
+  'base' AS blockchain,
+  'curve' AS project,
+  '1' AS version,
+  TRY_CAST(DATE_TRUNC('month', token_swaps.block_time) AS DATE) AS block_month,
+  TRY_CAST(DATE_TRUNC('day', token_swaps.block_time) AS DATE) AS block_date,
+  token_swaps.block_time,
+  token_swaps.block_number,
+  token_swaps.token_sold_amount_raw,
+  token_swaps.token_bought_amount_raw,
+  token_swaps.token_sold_address,
+  token_swaps.token_bought_address,
+  token_swaps.maker,
+  token_swaps.taker,
+  token_swaps.project_contract_address,
+  token_swaps.tx_hash,
+  token_swaps.evt_index
+FROM token_swaps
