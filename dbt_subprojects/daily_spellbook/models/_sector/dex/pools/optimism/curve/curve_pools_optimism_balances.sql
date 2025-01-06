cbf4fce74802473374ddb9939eb7c606f3345fe8
jason <jasonreddy4276@gmail.com>
2025-01-06 10:31:59 +0530

    updated sql file

--- dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/curve/curve_pools_optimism_balances.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/curve/curve_pools_optimism_balances.sql
@@ -7,28 +7,40 @@
     incremental_strategy = 'merge',
     unique_key = ['pool_address', 'snapshot_day'],
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.snapshot_day')]
-    )
+  )
 }}
 
 WITH op_pools AS (
-    SELECT DISTINCT
-        CAST(pool AS varchar(42)) as pool_address,
-        tokenid,
-        token
-    FROM {{ source('curve_optimism', 'pools') }}
-    WHERE CAST(token AS varchar(42)) = '0x4200000000000000000000000000000000000042'
+  SELECT
+    pool AS pool_address,
+    tokenid,
+    token,
+    creation_time
+  FROM {{ source('curve_optimism', 'pools') }}
+  WHERE token = 0x4200000000000000000000000000000042
+),
+
+filtered_balances AS (
+  SELECT
+    address AS pool_address,
+    balance AS op_balance,
+    day AS snapshot_day
+  FROM {{ source('tokens_optimism', 'balances_daily') }}
+  WHERE token_address = 0x4200000000000000000000000000000042
+  {% if is_incremental() %}
+    AND {{ incremental_predicate('day') }}
+  {% else %}
+    AND day >= date '2021-11-11' --first pool initiated
+  {% endif %}
 )
 
 SELECT 
-    p.pool_address,
-    p.tokenid,
-    p.token,
-    b.balance AS op_balance,
-    b.day AS snapshot_day
-FROM op_pools p
-JOIN {{ source('tokens_optimism', 'balances_daily') }} b
-    ON p.pool_address = CAST(b.address AS varchar(42))
-WHERE CAST(b.token_address AS varchar(42)) = '0x4200000000000000000000000000000000000042'
-{% if is_incremental() %}
-    AND b.day >= current_date - interval '{{ var("DBT_ENV_INCREMENTAL_TIME") }}' {{ var("DBT_ENV_INCREMENTAL_TIME_UNIT") }}
-{% endif %}
+  p.pool_address,
+  p.tokenid,
+  p.token,
+  p.creation_time,
+  COALESCE(b.op_balance, 0) AS op_balance,
+  COALESCE(b.snapshot_day, CURRENT_DATE) AS snapshot_day
+FROM filtered_balances b
+RIGHT JOIN op_pools p 
+  ON p.pool_address = b.pool_address
