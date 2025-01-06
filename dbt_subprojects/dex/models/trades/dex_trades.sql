66b964d2ee5eb7891c96fc421c306396f8ff187d
Huang Geyang <Sukebeta@outlook.com>
2025-01-07 01:22:23 +0800

    Refresh dex.trades

--- dbt_subprojects/dex/models/trades/dex_trades.sql
+++ dbt_subprojects/dex/models/trades/dex_trades.sql
@@ -53,6 +53,7 @@ WITH curve AS (
         )
     }}
 )
+
 , balancer_v3 AS (
     -- due to Balancer V3 having trades between ERC4626 tokens, which won't be priced on prices.usd, enrich separately
     {{
