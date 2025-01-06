be8815e49ffa6ac7f484d7ed1dda08e8a159a086
0xRob <83790096+0xRobin@users.noreply.github.com>
2025-01-06 11:34:24 +0100

    add comment docs on the setup

--- dbt_subprojects/hourly_spellbook/models/_sector/dune/dune_blockchains.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/dune/dune_blockchains.sql
@@ -7,5 +7,9 @@
                                     "dune",
                                     \'["0xRob"]\') }}')
 }}
+
+-- because the graphql endpoint often returns http error 403 when querying through http_get(),
+-- we store the results in a matview instead and expose that one here with a view on top.
+-- matview: https://dune.com/queries/4467416
 select * from
 {{ source("dune", "result_blockchains", database="dune") }}
