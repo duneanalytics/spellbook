54cf6ad391bc556982d732b7987bad85cb79c2a7
Huang Geyang <Sukebeta@outlook.com>
2025-01-07 20:34:25 +0800

    feat(dex): implement automatic base_trades change detection
    - Add get_modified_base_trades macro to read changed files from env
    - Update test view to automatically include modified models
    - Enhance workflow to pass modified files information
    
    This change enables fully automated testing of modified base_trades
    models without requiring manual updates to the test view.

---
+++ dbt_subprojects/dex/macros/models/get_modified_base_trades.sql
@@ -0,0 +1,12 @@
+{% macro get_modified_base_trades() %}
+    {% set modified_files = modules.os.environ.get('MODIFIED_BASE_TRADES', '') %}
+    {% set models = [] %}
+    {% if modified_files %}
+        {% set files = modified_files.split(',') %}
+        {% for file in files %}
+            {% set model_name = file.split('/')[-1].replace('.sql', '') %}
+            {% do models.append(model_name) %}
+        {% endfor %}
+    {% endif %}
+    {{ return(models) }}
+{% endmacro %}
