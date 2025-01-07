22b463d23fe8e7ca82c23b38528718c77f6f3562
Huang Geyang <Sukebeta@outlook.com>
2025-01-07 20:20:01 +0800

    refactor(dex): simplify CI test view to focus on amount_usd validation
    - Remove unnecessary transaction columns handling
    - Simplify base_trades selection
    - Keep core functionality for amount_usd validation

--- dbt_subprojects/dex/models/trades/test/dex_ci_trades_test.sql
+++ dbt_subprojects/dex/models/trades/test/dex_ci_trades_test.sql
@@ -7,29 +7,13 @@
 }}
 
 -- This view is only for CI testing purpose
--- It directly processes modified base_trades models and enriches them with transaction and token information
+-- It directly processes modified base_trades models and enriches them with token information
 
 {% set modified_base_trades = dbt_utils.get_modified_models('base_trades') %}
 
 WITH base_union AS (
     {% for model in modified_base_trades %}
-    SELECT
-        blockchain
-        , project
-        , version
-        , block_month
-        , block_date
-        , block_time
-        , block_number
-        , token_bought_amount_raw
-        , token_sold_amount_raw
-        , token_bought_address
-        , token_sold_address
-        , taker
-        , maker
-        , project_contract_address
-        , tx_hash
-        , evt_index
+    SELECT *
     FROM {{ model }}
     WHERE block_date = current_date - interval '1' day
     {% if not loop.last %}
@@ -38,18 +22,9 @@ WITH base_union AS (
     {% endfor %}
 )
 
-, with_tx AS (
-    {{
-        add_tx_columns_dynamic(
-            model_cte = 'base_union'
-            , columns = ['from', 'to', 'index']
-        )
-    }}
-)
-
 {{
     enrich_dex_trades(
-        base_trades = 'with_tx'
+        base_trades = 'base_union'
         , filter = "1=1"  -- Date filter already applied in base_union
         , tokens_erc20_model = source('tokens', 'erc20')
     )
