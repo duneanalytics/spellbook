40a95bb2f502831bda0985742e90500238bacc9f
Huang Geyang <Sukebeta@outlook.com>
2025-01-08 01:15:19 +0800

    fix(dex): add mock tx_from and tx_to in test view
    - Add mock fields for testing only
    - Use evt_tx_hash as a placeholder
    - Keep production code unchanged

--- dbt_subprojects/dex/models/trades/test/dex_ci_trades_test.sql
+++ dbt_subprojects/dex/models/trades/test/dex_ci_trades_test.sql
@@ -12,10 +12,13 @@
 {% set modified_models = get_modified_base_trades() %}
 {% set git_schema = 'git_dunesql_' ~ env_var('GIT_SHA', '') %}
 
-WITH base_union AS (
+WITH base_trades AS (
     {% for file in modified_models %}
     {% set model_name = file.split('/')[-1].replace('.sql', '') %}
-    SELECT *
+    SELECT
+        *,
+        evt_tx_hash as tx_from,  -- For testing only, not used in production
+        evt_tx_hash as tx_to     -- For testing only, not used in production
     FROM delta_prod.test_schema.{{ git_schema }}_{{ model_name }}
     WHERE block_date = current_date - interval '1' day
     {% if not loop.last %}
@@ -27,7 +30,7 @@ WITH base_union AS (
 SELECT * FROM (
     {{
         enrich_dex_trades(
-            base_trades = 'base_union'
+            base_trades = 'base_trades'
             , tokens_erc20_model = source('tokens', 'erc20')
             , filter = "1=1"
         )
