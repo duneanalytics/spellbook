e4e089103e770850509e9733ebc17f1016dabcb2
Huang Geyang <Sukebeta@outlook.com>
2025-01-07 20:37:53 +0800

    fix(dex): use dbt vars instead of environment variables
    - Replace modules.os.environ with dbt var() function
    - Update workflow to pass modified files via --vars
    - Keep the same functionality but use dbt-native approach

--- dbt_subprojects/dex/macros/models/get_modified_base_trades.sql
+++ dbt_subprojects/dex/macros/models/get_modified_base_trades.sql
@@ -1,5 +1,5 @@
 {% macro get_modified_base_trades() %}
-    {% set modified_files = modules.os.environ.get('MODIFIED_BASE_TRADES', '') %}
+    {% set modified_files = var('MODIFIED_BASE_TRADES', '') %}
     {% set models = [] %}
     {% if modified_files %}
         {% set files = modified_files.split(',') %}
