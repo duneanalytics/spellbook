641ae1e69ead3e16cd4d1a016b86e1210313fb46
0xRob <83790096+0xRobin@users.noreply.github.com>
2025-01-08 14:08:40 +0100

    add date

--- dbt_subprojects/tokens/models/prices_v2/prices_v2_day_raw.sql
+++ dbt_subprojects/tokens/models/prices_v2/prices_v2_day_raw.sql
@@ -3,6 +3,7 @@
         alias = 'day_raw',
         materialized = 'incremental',
         file_format = 'delta',
+        partition_by = ['date'],
         incremental_strategy = 'merge',
         unique_key = ['blockchain', 'contract_address', 'timestamp']
         )
@@ -16,6 +17,7 @@ SELECT
     , max_by(price,timestamp) as price
     , max_by(volume,timestamp) as volume
     , max_by(source,timestamp) as source
+    , date
     , max(timestamp) as source_timestamp
 FROM {{ ref('prices_v2_minute_raw') }}
 {% if is_incremental() %}
