0e6019b437dca82a080ba682d95e859f447c49fc
jason <jasonreddy4276@gmail.com>
2025-01-08 12:27:07 +0530

    added optimism: uniswap pools dex balances

---
+++ dbt_macros/shared/balances_subset_daily.sql
@@ -0,0 +1,25 @@
+{%- macro balances_subset_daily(
+        blockchain,
+        token_address,
+        start_date
+    )
+%}
+
+with
+filtered_balances as (
+    select
+        address as pool_address,
+        balance as token_balance,
+        day as snapshot_day
+    from {{ source('tokens_' ~ blockchain, 'balances_daily') }}
+    where
+        token_address = {{ token_address }}
+        {% if is_incremental() %}
+        and {{ incremental_predicate('day') }}
+        {% else %}
+        and day >= cast('{{ start_date }}' as date)
+        {% endif %}
+)
+
+select * from filtered_balances
+{% endmacro %}
