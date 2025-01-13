f7636fa38ec7c96f196c6cfc46a859c797ba4174
Peter <157015397+safeintern@users.noreply.github.com>
2025-01-13 17:12:44 +0800

    Update safe_avalanche_c_singletons.sql

--- dbt_subprojects/hourly_spellbook/models/_project/safe/avalanche_c/safe_avalanche_c_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/avalanche_c/safe_avalanche_c_singletons.sql
@@ -6,12 +6,13 @@
         post_hook='{{ expose_spells(\'["avalanche_c"]\',
                                     "project",
                                     "safe",
-                                    \'["tschubotz"]\') }}'
+                                    \'["tschubotz", "safeintern"]\') }}'
     ) 
 }}
 
 
 -- Fetch all known singleton addresses used via the factory.
+    
 select distinct singleton as address 
 from {{ source('gnosis_safe_avalanche_c', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
 
@@ -19,3 +20,5 @@ union
 
 select distinct singleton as address 
 from {{ source('gnosis_safe_avalanche_c', 'SafeProxyFactory_v1_4_1_evt_ProxyCreation') }}
+
+
