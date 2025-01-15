705cde17cc8b96c69830f2bcb55e0a0faf5821eb
Huang Geyang <Sukebeta@outlook.com>
2025-01-15 21:28:15 +0800

    refactor(dex): update test_reasonable_usd_amount to use enrich_dex_trades
    - Use enrich_dex_trades macro to get token information and USD amount
    - Only check trades from last 24h

--- dbt_subprojects/dex/tests/generic/test_reasonable_usd_amount.sql
+++ dbt_subprojects/dex/tests/generic/test_reasonable_usd_amount.sql
@@ -10,15 +10,32 @@
 
 WITH base_trades AS (
     SELECT
-        *
+        blockchain,
+        project,
+        version,
+        block_month,
+        block_date,
+        block_time,
+        block_number,
+        token_bought_amount_raw,
+        token_sold_amount_raw,
+        token_bought_address,
+        token_sold_address,
+        taker,
+        maker,
+        project_contract_address,
+        tx_hash,
+        evt_index
     FROM {{ model }}
     WHERE block_time >= NOW() - INTERVAL '1' day  -- Only check recent trades
 ),
 
-trades_with_prices AS (
+enriched_trades AS (
     {{
-        add_amount_usd(
-            trades_cte = 'base_trades'
+        enrich_dex_trades(
+            base_trades = 'base_trades'
+            , tokens_erc20_model = source('tokens', 'erc20')
+            , filter = "1=1"
         )
     }}
 ),
@@ -32,7 +49,7 @@ validation AS (
         evt_index,
         block_time,
         amount_usd
-    FROM trades_with_prices
+    FROM enriched_trades
     WHERE amount_usd > {{ max_value }}
     AND amount_usd is not null
 )
