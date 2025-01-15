f98f6914eb592c96a58ed1a1a902f5b2e0ef4a91
beve0x <gk0001va@gmail.com>
2025-01-15 12:34:26 -0700

    moved files

---
+++ dbt_subprojects/tokens/models/transfers_and_balances/net_transfers/chains/evm/daily_address/metrics_sei_transfers_daily_address.sql
@@ -0,0 +1,15 @@
+{% set blockchain = 'sei' %}
+
+{{ config(
+        schema = 'metrics_' + blockchain
+        , alias = 'transfers_daily_address'
+        , materialized = 'incremental'
+        , file_format = 'delta'
+        , incremental_strategy = 'merge'
+        , unique_key = ['blockchain', 'block_date', 'address']
+        , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
+        )
+}}
+
+
+{{ metrics_transfers_evm_address(blockchain) }}
