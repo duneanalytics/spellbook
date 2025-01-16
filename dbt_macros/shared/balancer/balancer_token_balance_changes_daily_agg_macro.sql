fe2d36bcec2648160e15c5419e38c8003f26dbf9
viniabussafi <vinicius@balancerlabs.dev>
2025-01-16 23:51:33 +0000

    propagate

--- dbt_macros/shared/balancer/balancer_token_balance_changes_daily_agg_macro.sql
+++ dbt_macros/shared/balancer/balancer_token_balance_changes_daily_agg_macro.sql
@@ -28,7 +28,7 @@ WITH
         WHERE
         ('{{blockchain}}' != 'fantom' OR
         ('{{blockchain}}' = 'fantom' AND contract_address NOT IN (0xde1e704dae0b4051e80dabb26ab6ad6c12262da0, 0x5ddb92a5340fd0ead3987d3661afcd6104c3b757))
-        OR '{{blockchain}}' = 'fantom' AND contract_address NOT IN (0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38)) --broken price feeds
+        OR '{{blockchain}}' = 'sonic' AND contract_address NOT IN (0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38)) --broken price feeds
         AND blockchain = '{{blockchain}}'
         {% if is_incremental() %}
         AND {{ incremental_predicate('hour') }}
@@ -220,7 +220,7 @@ WITH
         WHERE
         ('{{blockchain}}' != 'fantom' OR
         ('{{blockchain}}' = 'fantom' AND contract_address NOT IN (0xde1e704dae0b4051e80dabb26ab6ad6c12262da0, 0x5ddb92a5340fd0ead3987d3661afcd6104c3b757))
-        OR '{{blockchain}}' = 'fantom' AND contract_address NOT IN (0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38)) --broken price feeds
+        OR '{{blockchain}}' = 'sonic' AND contract_address NOT IN (0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38)) --broken price feeds
         AND blockchain = '{{blockchain}}'
         {% if is_incremental() %}
         AND {{ incremental_predicate('hour') }}
