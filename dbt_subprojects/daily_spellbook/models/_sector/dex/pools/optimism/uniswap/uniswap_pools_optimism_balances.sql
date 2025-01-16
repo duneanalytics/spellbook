c7a300cec06b75cdcef72fe62b0a80e9a0dd7025
jason <jasonreddy4276@gmail.com>
2025-01-16 11:06:51 +0530

    updated sql file

--- dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/uniswap/uniswap_pools_optimism_balances.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/dex/pools/optimism/uniswap/uniswap_pools_optimism_balances.sql
@@ -12,41 +12,35 @@
 
 with op_addresses as (
   select
-    pool as address,  -- Keep as varbinary
-    token0,          -- Keep as varbinary
-    token1,          -- Keep as varbinary
+    pool as address,  
+    token0,          
+    token1,          
     fee as fee_tier,
     creation_block_time as creation_time
   from 
     {{ source('uniswap_v3_optimism', 'pools') }}
   where
-    token0 = from_hex('0x4200000000000000000000000000000000000042')
-    or token1 = from_hex('0x4200000000000000000000000000000000000042')
-),
-
-op_token as (
-  select 
-    from_hex('0x4200000000000000000000000000000000000042') as token_address
+    token0 = 0x4200000000000000000000000000000000000042
+    or token1 = 0x4200000000000000000000000000000000000042
 ),
 
 filtered_balances as (
   {{ balances_incremental_subset_daily(
        blockchain='optimism',
        start_date='2021-11-11',
-       address_list='op_addresses',  
-       token_list='op_token'         
+        address_token_list = 'op_addresses'         
   ) }}
 )
 
 select 
-  lower(to_hex(p.address)) as pool_address,  -- Convert to hex string only in final output
-  lower(to_hex(p.token0)) as token0,         -- Convert to hex string only in final output
-  lower(to_hex(p.token1)) as token1,         -- Convert to hex string only in final output
+  p.address as pool_address, 
+  p.token0 as token0,         
+  p.token1 as token1, 
   p.fee_tier,
   p.creation_time,
   coalesce(b.balance, 0) as op_balance,
   coalesce(b.day, current_date) as snapshot_day
 from 
   filtered_balances b
-right join
-  op_addresses p on b.address = p.address    -- Compare varbinary with varbinary
+left join
+  op_addresses p on b.address = p.address    
