1b678a884f0dd6452990bdf71bb2e09b3c41e358
PatelPrinci <principatel@lampros.tech>
2025-01-18 16:49:00 +0530

    jojo on base

--- dbt_subprojects/dex/models/dex_info.sql
+++ dbt_subprojects/dex/models/dex_info.sql
@@ -195,4 +195,5 @@ FROM (VALUES
     , ('levinswap', 'Levinswap', 'Direct', 'levinswap')
     , ('gravity_finance', 'GravityFinance', 'Direct', 'Gravity_Finance')
     , ('fluid', 'Fluid DEX', 'Direct', '0xfluid')
+    , ('jojo', 'JOJO', 'Direct', 'jojo_exchange')
     ) AS temp_table (project, name, marketplace_type, x_username)
