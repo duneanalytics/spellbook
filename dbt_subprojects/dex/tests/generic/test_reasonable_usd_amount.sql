cfb03189928625a99878ddaf899ef6cc5035739f
Huang Geyang <Sukebeta@outlook.com>
2025-01-18 19:09:28 +0800

    refactor(dex): simplify test_reasonable_usd_amount\n\n- Combine all trade types into a single enrichments CTE\n- Handle Curve trades with CASE statements\n- Add time filter to prices table\n- Optimize JOIN conditions

--- dbt_subprojects/dex/tests/generic/test_reasonable_usd_amount.sql
+++ dbt_subprojects/dex/tests/generic/test_reasonable_usd_amount.sql
@@ -9,37 +9,95 @@
 #}
 
 WITH base_trades AS (
-    SELECT
-        blockchain,
-        project,
-        version,
-        block_month,
-        block_date,
-        block_time,
-        block_number,
-        token_bought_amount_raw,
-        token_sold_amount_raw,
-        token_bought_address,
-        token_sold_address,
-        taker,
-        maker,
-        project_contract_address,
-        tx_hash,
-        evt_index,
-        tx_from,
-        tx_to
+    SELECT *
     FROM {{ model }}
     WHERE block_time >= NOW() - INTERVAL '1' day  -- Only check recent trades
 ),
 
-enriched_trades AS (
-    {{
-        enrich_dex_trades(
-            base_trades = 'base_trades'
-            , tokens_erc20_model = source('tokens', 'erc20')
-            , filter = "1=1"
-        )
-    }}
+enrichments AS (
+    SELECT
+        bt.blockchain,
+        bt.project,
+        bt.version,
+        bt.block_time,
+        bt.tx_hash,
+        bt.evt_index,
+        CASE
+            WHEN bt.project = 'curve' AND curve_optimism.pool_type is not null
+                THEN bt.token_bought_amount_raw / power(10, CASE WHEN curve_optimism.pool_type = 'meta' AND curve_optimism.bought_id = INT256 '0' THEN 18 ELSE erc20_bought.decimals END)
+            WHEN bt.project = 'curve'
+                THEN bt.token_bought_amount_raw / power(10, erc20_bought.decimals)
+            ELSE bt.token_bought_amount_raw / power(10, COALESCE(erc20_bought.decimals, 18))
+        END AS token_bought_amount,
+        CASE
+            WHEN bt.project = 'curve' AND curve_ethereum.swap_type is not null
+                THEN bt.token_sold_amount_raw / power(10, CASE WHEN curve_ethereum.swap_type = 'underlying_exchange_base' THEN 18 ELSE erc20_sold.decimals END)
+            WHEN bt.project = 'curve' AND curve_optimism.pool_type is not null
+                THEN bt.token_sold_amount_raw / power(10, CASE WHEN curve_optimism.pool_type = 'meta' AND curve_optimism.bought_id = INT256 '0' THEN erc20_bought.decimals ELSE erc20_sold.decimals END)
+            WHEN bt.project = 'curve'
+                THEN bt.token_sold_amount_raw / power(10, erc20_sold.decimals)
+            ELSE bt.token_sold_amount_raw / power(10, COALESCE(erc20_sold.decimals, 18))
+        END AS token_sold_amount,
+        bt.token_bought_address,
+        bt.token_sold_address
+    FROM base_trades bt
+    LEFT JOIN {{ source('tokens', 'erc20') }} erc20_bought
+        ON erc20_bought.contract_address = bt.token_bought_address
+        AND erc20_bought.blockchain = bt.blockchain
+    LEFT JOIN {{ source('tokens', 'erc20') }} erc20_sold
+        ON erc20_sold.contract_address = bt.token_sold_address
+        AND erc20_sold.blockchain = bt.blockchain
+    LEFT JOIN {{ ref('curve_ethereum_base_trades') }} curve_ethereum
+        ON curve_ethereum.tx_hash = bt.tx_hash
+        AND curve_ethereum.evt_index = bt.evt_index
+        AND bt.project = 'curve'
+    LEFT JOIN {{ ref('curve_optimism_base_trades') }} curve_optimism
+        ON curve_optimism.tx_hash = bt.tx_hash
+        AND curve_optimism.evt_index = bt.evt_index
+        AND bt.project = 'curve'
+),
+
+prices AS (
+    SELECT
+        blockchain,
+        contract_address,
+        minute,
+        price
+    FROM {{ source('prices', 'usd') }}
+    WHERE minute >= NOW() - INTERVAL '1' day  -- Match base_trades time filter
+),
+
+trusted_tokens AS (
+    SELECT 
+        contract_address,
+        blockchain
+    FROM {{ source('prices', 'trusted_tokens') }}
+),
+
+trades_with_prices AS (
+    SELECT
+        bt.*,
+        COALESCE(
+            CASE WHEN tt_bought.contract_address IS NOT NULL THEN bt.token_bought_amount * pb.price END,
+            CASE WHEN tt_sold.contract_address IS NOT NULL THEN bt.token_sold_amount * ps.price END,
+            bt.token_bought_amount * pb.price,
+            bt.token_sold_amount * ps.price
+        ) AS amount_usd
+    FROM enrichments bt
+    LEFT JOIN prices pb
+        ON bt.token_bought_address = pb.contract_address
+        AND bt.blockchain = pb.blockchain
+        AND pb.minute = date_trunc('minute', bt.block_time)
+    LEFT JOIN prices ps
+        ON bt.token_sold_address = ps.contract_address
+        AND bt.blockchain = ps.blockchain
+        AND ps.minute = date_trunc('minute', bt.block_time)
+    LEFT JOIN trusted_tokens tt_bought
+        ON bt.token_bought_address = tt_bought.contract_address
+        AND bt.blockchain = tt_bought.blockchain
+    LEFT JOIN trusted_tokens tt_sold
+        ON bt.token_sold_address = tt_sold.contract_address
+        AND bt.blockchain = tt_sold.blockchain
 ),
 
 validation AS (
@@ -51,7 +109,7 @@ validation AS (
         evt_index,
         block_time,
         amount_usd
-    FROM enriched_trades
+    FROM trades_with_prices
     WHERE amount_usd > {{ max_value }}
     AND amount_usd is not null
 )
