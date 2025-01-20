710534f64526c7d8cae5d82da111b9839c227055
Boxer <Floba.hs@gmail.com>
2025-01-20 13:11:52 +0100

    init

---
+++ dbt_subprojects/dex/models/automated_trades/multichain/projects/uniswap/uniswap_v2_ethereum_decoded_factory_evt.sql
@@ -0,0 +1,36 @@
+{{ config(
+        schema = 'uniswap_v2_multichain',
+        alias = 'decoded_factory_evt',
+        partition_by = ['block_date'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+        )
+}}
+
+{%
+    set blockchains = [
+        "ethereum",
+        "bnb"
+    ]
+%}
+
+with uniswap_pool_created_logs as (
+    {% for blockchain in blockchains %}      
+        select 
+            '{{blockchain}}' as blockchain,
+            * 
+        from (
+            {{uniswap_v2_factory_event_decoding(
+                logs = source(blockchain, 'logs')
+            )}}
+        )   
+        {% if not loop.last %}
+            union all
+        {% endif %}
+    {% endfor %}
+)
+
+select * from uniswap_pool_created_logs
