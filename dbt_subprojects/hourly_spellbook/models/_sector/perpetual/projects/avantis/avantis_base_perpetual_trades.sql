9d8b37853675db1df5f4377204ed4507946754c5
PatelPrinci <principatel@lampros.tech>
2025-01-23 11:33:59 +0530

    space changed

--- dbt_subprojects/hourly_spellbook/models/_sector/perpetual/projects/avantis/avantis_base_perpetual_trades.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/perpetual/projects/avantis/avantis_base_perpetual_trades.sql
@@ -17,27 +17,27 @@ FROM
 (
     {% for avantis_perpetual_trades in avantis_base_perpetual_trade_models %}
     SELECT
-        blockchain
-		,block_date
-        ,block_month
-        ,block_time
-        ,virtual_asset
-        ,underlying_asset
-        ,market
-        ,market_address
-        ,volume_usd
-        ,fee_usd
-        ,margin_usd
-        ,trade
-        ,project
-        ,version
-        ,frontend
-        ,trader
-        ,volume_raw
-        ,tx_hash
-        ,tx_from
-        ,tx_to
-        ,evt_index
+    blockchain
+    ,block_date
+    ,block_month
+    ,block_time
+    ,virtual_asset
+    ,underlying_asset
+    ,market
+    ,market_address
+    ,volume_usd
+    ,fee_usd
+    ,margin_usd
+    ,trade
+    ,project
+    ,version
+    ,frontend
+    ,trader
+    ,volume_raw
+    ,tx_hash
+    ,tx_from
+    ,tx_to
+    ,evt_index
     FROM {{ avantis_perpetual_trades }}
     {% if not loop.last %}
     UNION ALL
