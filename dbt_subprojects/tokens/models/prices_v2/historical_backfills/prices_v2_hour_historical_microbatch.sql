8559f09e981b81cf014718cced629d59cf882dab
0xRob <83790096+0xRobin@users.noreply.github.com>
2025-01-24 13:37:42 +0100

    try microbatch for historical

--- dbt_subprojects/tokens/models/prices_v2/historical_backfills/prices_v2_hour_historical_microbatch.sql
+++ dbt_subprojects/tokens/models/prices_v2/historical_backfills/prices_v2_hour_historical_microbatch.sql
@@ -1,14 +1,23 @@
 {{ config(
         schema='prices_v2',
-        alias = 'hour',
+        alias = 'hour_historical_microbatch',
         materialized = 'incremental',
         file_format = 'delta',
         partition_by = ['date'],
-        incremental_strategy = 'merge',
-        unique_key = ['blockchain', 'contract_address', 'timestamp'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.timestamp')]
+        incremental_strategy = 'microbatch',
+        begin='2024-09-01',
+        batch_size='month',
+        event_time =' timestamp',
+        unique_key = ['blockchain', 'contract_address', 'timestamp']
         )
 }}
+
+-- this is the max date of the historical batched backfill, after which the incremental model takes over
+-- we currently need to setup to have both monthly historical batches, but smaller incremental batches with a daily lookback.
+-- currently all data in historical is rematerialized in the incremental model to prevent any chance of duplicates.
+-- after this end_date, this model will still run but is setup to produce no outputs.
+-- you should set this date to maximum the start of the current batch, to make sure the last batch is always running empty once in prod.
+{% set end_date = '2025-01-01' %}   -- assume exclusive
 
 -- depends_on: {{ ref('prices_v2_day_sparse') }} (this is needed because it's only used in the conditional block)
 
@@ -28,12 +37,8 @@ WITH sparse_prices as (
             , source_timestamp
         from {{ ref('prices_v2_hour_sparse') }}
         where 1=1
-            and timestamp > now() - interval '30' day
-            {% if is_incremental() %}
-            and {{ incremental_predicate('timestamp') }}
-            {% endif %}
-        -- If we're running incremental, we also need to add the last known prices from before the incremental window, to forward fill them
-        {% if is_incremental() %}
+            and timestamp > now() - interval '90' day   -- todo: remove temp filter
+            and timestamp < timestamp '{{end_date}}' -- don't process any data from end_date on
         UNION ALL
         SELECT * FROM (
             select
@@ -45,11 +50,11 @@ WITH sparse_prices as (
                 , max_by(source,timestamp) as source
                 , max(date) as date
                 , max_by(source_timestamp,timestamp) as source_timestamp
-            from {{ ref('prices_v2_day_sparse') }}  -- because incremental windows always start at start of day, we cheat a little and use day here
-            where not {{ incremental_predicate('timestamp') }} -- not in the current incremental window (so before that)
+            -- we need render() here to not autofilter the model based on the batch timestamps
+            from {{ ref('prices_v2_day_sparse').render() }}  -- because incremental windows always start at start of day, we cheat a little and use day here
+            where timestamp < (select min(timestamp) from {{ref('utils_days')}})
             group by blockchain, contract_address
         )
-        {% endif %}
     )
 )
 
@@ -58,19 +63,10 @@ WITH sparse_prices as (
 , timeseries as (
     select * from (
        select date_add('hour',hour, day) as timestamp
-       from unnest(
-             sequence(cast((select date_trunc('day', min(timestamp)) from sparse_prices) as timestamp)
-                    , cast(date_trunc('day', now()) as timestamp)
-                    , interval '1' day
-                    )
-             ) as foo(day)
+       from (select timestamp as day from {{ref('utils_days')}}) --this is auto-filtered for the microbatch
        cross join unnest(sequence(0, 23)) as h(hour)
    )
-   where 1=1
-       and timestamp <= now()
-       {% if is_incremental() %}
-       and {{ incremental_predicate('timestamp') }}
-       {% endif %}
+   where timestamp <= now() --don't overshoot
 )
 
 SELECT
---
+++ dbt_subprojects/tokens/models/prices_v2/historical_backfills/utils_days.sql
@@ -0,0 +1,37 @@
+{{ config(
+        schema='utils',
+        alias = 'days',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = 'timestamp',
+        event_time = 'timestamp'
+        )
+}}
+
+--todo: move this model in a better place
+{% set start_date = '2000-01-01' %}
+
+
+{% if not is_incremental() %}
+    select timestamp
+    from unnest(
+         sequence(timestamp '{{start_date}}'
+                , cast(date_trunc('day', now()) as timestamp)
+                , interval '1' day
+                )
+         ) as foo(timestamp)
+{% else %}
+    select timestamp
+    from unnest(
+         sequence(cast(date_trunc('day', now() - interval '10' day) as timestamp)
+                , cast(date_trunc('day', now()) as timestamp)
+                , interval '1' day
+                )
+         ) as foo(timestamp)
+    {% if is_incremental() %}
+    where {{ incremental_predicate('timestamp') }}
+    {% endif %}
+{% endif %}
+
+
