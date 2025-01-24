a78f6dd5c217372fa258f177e41ef9d68c531021
RantumBits <rantum@rantum.xyz>
2025-01-23 17:41:44 -0800

    remove fills_within cte

--- dbt_subprojects/dex/macros/models/_project/zeroex/zeroex_v2.sql
+++ dbt_subprojects/dex/macros/models/_project/zeroex/zeroex_v2.sql
@@ -85,34 +85,37 @@ SELECT * FROM settler_txs
 {% macro zeroex_v2_trades(blockchain, start_date) %}
 WITH 
 swap_signatures as (
-    select 0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822 as signature   
-    union select 0x66251e495e6e69e208ab08e2bc259dbe2ef482a8c4a93b8984b03a1eb27e1b9e as signature 
-    union select 0xdde2f3711ab09cdddcfee16ca03e54d21fb8cf3fa647b9797913c950d38ad693 as signature 
-    union select 0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67 as signature
-    union select 0x19b47279256b2a23a1665c810c8d55a1758940ee09377d4f8d26497a3577dc83 as signature
-    union select 0x2170c741c41531aec20e7c107c24eecfdd15e69c9bb0a8dd37b1840b9e0b207b as signature
-    union select 0x143f1f8e861fbdeddd5b46e844b7d3ac7b86a122f36e8c463859ee6811b1f29c as signature --tokenExchange
-    union select 0x8b3e96f2b889fa771c53c981b40daf005f63f637f1869f707052d15a3dd97140 as signature --tokenExchange
-    union select 0xb2e76ae99761dc136e598d4a629bb347eccb9532a5f8bbd72e18467c3c34cc98 as signature --tokenExchange
-    union select 0xac75f773e3a92f1a02b12134d65e1f47f8a14eabe4eaf1e24624918e6a8b269f as signature --otcOrderFilled
-    union select 0x085d06ecf4c34b237767a31c0888e121d89546a77f186f1987c6b8715e1a8caa as signature --BuyGem
-    union select 0xc2c0245e056d5fb095f04cd6373bc770802ebd1e6c918eb78fdef843cdb37b0f as signature --DodoSwap
-    union select 0x103ed084e94a44c8f5f6ba8e3011507c41063177e29949083c439777d8d63f60 as signature
-    union select 0xa4228e1eb11eb9b31069d9ed20e7af9a010ca1a02d4855cee54e08e188fcc32c
-    union select 0x34660fc8af304464529f48a778e03d03e4d34bcd5f9b6f0cfbf3cd238c642f7f
-    union select 0xdc004dbca4ef9c966218431ee5d9133d337ad018dd5b5c5493722803f75c64f7
-    union select 0xa5a79273c52413fd319bf0be43c422824dc76fc4f69c671d8805d1aaf3cecc77
-    union select 0x823eaf01002d7353fbcadb2ea3305cc46fa35d799cb0914846d185ac06f8ad05
-    union select 0x3b841dc9ab51e3104bda4f61b41e4271192d22cd19da5ee6e292dc8e2744f713
-    union select 0x0874b2d545cb271cdbda4e093020c452328b24af12382ed62c4d00f5c26709db
-    union select 0x606ecd02b3e3b4778f8e97b2e03351de14224efaa5fa64e62200afc9395c2499
-    union select 0x176648f1f11cda284c124490086be42a926ddf0ae887ebe7b1d6b337d8942756
-    union select 0x298c349c742327269dc8de6ad66687767310c948ea309df826f5bd103e19d207
-    union select 0xcd3829a3813dc3cdd188fd3d01dcf3268c16be2fdd2dd21d0665418816e46062
-    union select 0xb3e2773606abfd36b5bd91394b3a54d1398336c65005baf7bf7a05efeffaf75b 
-    union select 0xad7d6f97abf51ce18e17a38f4d70e975be9c0708474987bb3e26ad21bd93ca70
-    union select 0x54787c404bb33c88e86f4baf88183a3b0141d0a848e6a9f7a13b66ae3a9b73d1
-    union select 0x6ac6c02c73a1841cb185dff1fe5282ff4499ce709efd387f7fc6de10a5124320
+    SELECT signature FROM (
+        VALUES 
+        (0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822),
+        (0x66251e495e6e69e208ab08e2bc259dbe2ef482a8c4a93b8984b03a1eb27e1b9e),
+        (0xdde2f3711ab09cdddcfee16ca03e54d21fb8cf3fa647b9797913c950d38ad693),
+        (0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67),
+        (0x19b47279256b2a23a1665c810c8d55a1758940ee09377d4f8d26497a3577dc83),
+        (0x2170c741c41531aec20e7c107c24eecfdd15e69c9bb0a8dd37b1840b9e0b207b),
+        (0x143f1f8e861fbdeddd5b46e844b7d3ac7b86a122f36e8c463859ee6811b1f29c), --tokenExchange
+        (0x8b3e96f2b889fa771c53c981b40daf005f63f637f1869f707052d15a3dd97140), --tokenExchange
+        (0xb2e76ae99761dc136e598d4a629bb347eccb9532a5f8bbd72e18467c3c34cc98), --tokenExchange
+        (0xac75f773e3a92f1a02b12134d65e1f47f8a14eabe4eaf1e24624918e6a8b269f), --otcOrderFilled
+        (0x085d06ecf4c34b237767a31c0888e121d89546a77f186f1987c6b8715e1a8caa), --BuyGem
+        (0xc2c0245e056d5fb095f04cd6373bc770802ebd1e6c918eb78fdef843cdb37b0f), --DodoSwap
+        (0x103ed084e94a44c8f5f6ba8e3011507c41063177e29949083c439777d8d63f60),
+        (0xa4228e1eb11eb9b31069d9ed20e7af9a010ca1a02d4855cee54e08e188fcc32c),
+        (0x34660fc8af304464529f48a778e03d03e4d34bcd5f9b6f0cfbf3cd238c642f7f),
+        (0xdc004dbca4ef9c966218431ee5d9133d337ad018dd5b5c5493722803f75c64f7),
+        (0xa5a79273c52413fd319bf0be43c422824dc76fc4f69c671d8805d1aaf3cecc77),
+        (0x823eaf01002d7353fbcadb2ea3305cc46fa35d799cb0914846d185ac06f8ad05),
+        (0x3b841dc9ab51e3104bda4f61b41e4271192d22cd19da5ee6e292dc8e2744f713),
+        (0x0874b2d545cb271cdbda4e093020c452328b24af12382ed62c4d00f5c26709db),
+        (0x606ecd02b3e3b4778f8e97b2e03351de14224efaa5fa64e62200afc9395c2499),
+        (0x176648f1f11cda284c124490086be42a926ddf0ae887ebe7b1d6b337d8942756),
+        (0x298c349c742327269dc8de6ad66687767310c948ea309df826f5bd103e19d207),
+        (0xcd3829a3813dc3cdd188fd3d01dcf3268c16be2fdd2dd21d0665418816e46062),
+        (0xb3e2773606abfd36b5bd91394b3a54d1398336c65005baf7bf7a05efeffaf75b),
+        (0xad7d6f97abf51ce18e17a38f4d70e975be9c0708474987bb3e26ad21bd93ca70),
+        (0x54787c404bb33c88e86f4baf88183a3b0141d0a848e6a9f7a13b66ae3a9b73d1),
+        (0x6ac6c02c73a1841cb185dff1fe5282ff4499ce709efd387f7fc6de10a5124320)
+    ) AS t(signature)
 ),
 tbl_all_logs AS (
     SELECT
@@ -324,29 +327,7 @@ select * from tbl_trades
 {% endmacro %}
 
 {% macro zeroex_v2_trades_detail(blockchain, start_date) %}
-WITH fills AS (
-    WITH signatures AS (
-        SELECT DISTINCT signature
-        FROM {{ source(blockchain, 'logs_decoded') }} l
-        JOIN zeroex_v2_trades tt ON tt.tx_hash = l.tx_hash AND l.block_time = tt.block_time AND l.block_number = tt.block_number
-        WHERE event_name IN ('TokenExchange', 'OtcOrderFilled', 'SellBaseToken', 'Swap', 'BuyGem', 'DODOSwap', 'SellGem', 'Submitted')
-        {% if is_incremental() %}
-            AND {{ incremental_predicate('l.block_time') }}
-        {% else %}
-            AND l.block_time >= DATE '{{start_date}}'
-        {% endif %}
-    )
-    SELECT tt.tx_hash, tt.block_number, tt.block_time, COUNT(*) AS fills_within
-    FROM {{ source(blockchain, 'logs') }} l
-    JOIN signatures ON signature = topic0
-    JOIN zeroex_v2_trades tt ON tt.tx_hash = l.tx_hash AND l.block_time = tt.block_time AND l.block_number = tt.block_number
-    {% if is_incremental() %}
-        WHERE {{ incremental_predicate('l.block_time') }}
-    {% else %}
-        WHERE l.block_time >= DATE '{{start_date}}'
-    {% endif %}
-    GROUP BY 1,2,3
-),
+WITH 
 
 results AS (
     SELECT
@@ -376,12 +357,9 @@ results AS (
         maker_amount / POW(10,COALESCE(tm.decimals,pm.decimals)) AS maker_token_amount,
         maker_amount / POW(10,COALESCE(tm.decimals,pm.decimals)) AS token_bought_amount,
         maker_amount / POW(10,COALESCE(tm.decimals,pm.decimals)) * pm.price AS maker_amount,
-        tag,
-        fills_within
+        tag
     FROM
         zeroex_v2_trades trades
-    LEFT JOIN
-        fills f ON f.tx_hash = trades.tx_hash AND f.block_time = trades.block_time AND f.block_number = trades.block_number
     LEFT JOIN
         {{ source('tokens', 'erc20') }} tt ON tt.blockchain = '{{blockchain}}' AND tt.contract_address = taker_token
     LEFT JOIN
