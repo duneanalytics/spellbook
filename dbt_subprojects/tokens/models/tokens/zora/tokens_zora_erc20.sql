27edd26c00531ea990850ec431f011084c9582c9
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-01-27 20:15:30 +0300

    Adding Tokens for Zora

--- dbt_subprojects/tokens/models/tokens/zora/tokens_zora_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/zora/tokens_zora_erc20.sql
@@ -13,4 +13,6 @@ SELECT
     , decimals
 FROM (VALUES
         (0x4200000000000000000000000000000000000006, 'WETH', 18)
+        (0xa6b280b42cb0b7c4a4f789ec6ccc3a7609a1bc39, 'ENJOY', 18)
+        (0x078540eecc8b6d89949c9c7d5e8e91eab64f6696, 'IMAGINE', 18)
 ) AS temp_table (contract_address, symbol, decimals)
