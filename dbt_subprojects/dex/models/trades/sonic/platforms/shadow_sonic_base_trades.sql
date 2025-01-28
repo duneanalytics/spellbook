92e337c20d45ad91771a0b1120f8f443f5e01e22
disco chuck <uv@househeads.org>
2025-01-28 07:35:28 -0600

    Adding shadow to dex.trades

---
+++ dbt_subprojects/dex/models/trades/sonic/platforms/shadow_sonic_base_trades.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        schema = 'equalizer_sonic',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v2_trades(
+        blockchain = 'sonic',
+        project = 'shadow',
+        version = '1',
+        Pair_evt_Swap = source('shadow_sonic', 'Pair_evt_Swap'),
+        Factory_evt_PairCreated = source('shadow_sonic', 'Core_PairFactory_evt_PairCreated')
+    )
+}}
+
+{{
+    uniswap_compatible_v3_trades(
+        blockchain = 'sonic',
+        project = 'shadow',
+        version = '3',
+        Pair_evt_Swap = source('shadow_sonic', 'RamsesV3Pool_evt_Swap'),
+        Factory_evt_PoolCreated = source('shadow_sonic', 'RamsesV3Factory_evt_PoolCreated')
+    )
+}}
