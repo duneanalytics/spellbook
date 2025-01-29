85a58fe419c2548b0ebd329d0ea82a5963db28a5
Boxer <Floba.hs@gmail.com>
2025-01-29 17:03:53 +0100

    fix tests and files

--- dbt_subprojects/dex/models/automated_trades/dex_automated_trades_unmapped.sql
+++ dbt_subprojects/dex/models/automated_trades/dex_automated_trades_unmapped.sql
@@ -18,25 +18,30 @@ SELECT
         WHEN base_trades.dex_type = 'uni-v3' THEN concat('unknown-uni-v3-', cast(varbinary_substring(base_trades.factory_address, 1, 5) as varchar))
         ELSE concat('unknown-', cast(varbinary_substring(base_trades.factory_address, 1, 5) as varchar))
     END as project,
-    CASE when dex_map.project_name is not NULL then true else false end as project_status,
     base_trades.version,
-    base_trades.dex_type,
     base_trades.factory_address,
+    base_trades.dex_type,
     base_trades.block_month,
     base_trades.block_date,
     base_trades.block_time,
     base_trades.block_number,
+    base_trades.token_bought_symbol,
+    base_trades.token_sold_symbol,
+    base_trades.token_pair,
+    base_trades.token_bought_amount,
+    base_trades.token_sold_amount,
     base_trades.token_bought_amount_raw,
     base_trades.token_sold_amount_raw,
+    base_trades.amount_usd,
     base_trades.token_bought_address,
     base_trades.token_sold_address,
     base_trades.taker,
     base_trades.maker,
     base_trades.project_contract_address,
     base_trades.tx_hash,
-    base_trades.evt_index,
     base_trades.tx_from,
     base_trades.tx_to,
+    base_trades.evt_index,
     base_trades.tx_index
 FROM base_trades
 LEFT JOIN {{ ref('dex_mapping') }} AS dex_map
