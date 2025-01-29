677a2aac97ac4dbae0227fe3d8d70d049d8a053a
Boxer <Floba.hs@gmail.com>
2025-01-29 17:43:45 +0100

    fix unique keys for merging

--- dbt_subprojects/dex/models/automated_trades/multichain/projects/uniswap/v2/old/uniswap_v2_old_chains_decoded_factory_evt.sql
+++ dbt_subprojects/dex/models/automated_trades/multichain/projects/uniswap/v2/old/uniswap_v2_old_chains_decoded_factory_evt.sql
@@ -5,7 +5,7 @@
         materialized = 'incremental',
         file_format = 'delta',
         incremental_strategy = 'merge',
-        unique_key = ['blockchain', 'block_number', 'evt_index'],
+        unique_key = ['blockchain', 'block_number', 'tx_index', 'evt_index'],
         incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
         )
 }}
