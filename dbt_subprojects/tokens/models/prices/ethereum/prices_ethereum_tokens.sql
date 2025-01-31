2c48f8ce6aed6cb204c572b6a60d2b05d588080c
Rudi Afandi <53332703+rudiafandi16@users.noreply.github.com>
2025-01-31 15:58:11 +0700

    Update prices_ethereum_tokens.sql
    Add MOVE token price

--- dbt_subprojects/tokens/models/prices/ethereum/prices_ethereum_tokens.sql
+++ dbt_subprojects/tokens/models/prices/ethereum/prices_ethereum_tokens.sql
@@ -1875,7 +1875,8 @@ FROM
     ('wagmi5-wagmi', 'ethereum', 'WAGMI', 0x92CC36D66e9d739D50673d1f27929a371FB83a67, 18),
     ('deusd-elixir-deusd','ethereum', 'DEUSD', 0x15700b564ca08d9439c58ca5053166e8317aa138, 18),
     ('unibot-unibot','ethereum', 'UNIBOT', 0xf819d9cb1c2a819fd991781a822de3ca8607c3c9, 18),
-    ('morpho-morpho','ethereum', 'MORPHO', 0x58d97b57bb95320f9a05dc918aef65434969c2b2, 18)
+    ('morpho-morpho','ethereum', 'MORPHO', 0x58d97b57bb95320f9a05dc918aef65434969c2b2, 18),
+    ('move-movement', 'ethereum', 'MOVE', 0x3073f7aaa4db83f95e9fff17424f71d4751a3073, 8)
 
    ) as temp (token_id, blockchain, symbol, contract_address, decimals)
 where contract_address not in (
