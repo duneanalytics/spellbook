cfd3f2c762e903d301cfa1438534739354856883
beve0x <gk0001va@gmail.com>
2025-02-04 22:55:24 -0700

    changed unique key in chains

--- dbt_subprojects/tokens/models/transfers_and_balances/blast/tokens_blast_net_value_transfers_asset_daily.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/blast/tokens_blast_net_value_transfers_asset_daily.sql
@@ -6,7 +6,7 @@
         , materialized = 'incremental'
         , file_format = 'delta'
         , incremental_strategy = 'merge'
-        , unique_key = ['blockchain', 'block_date', 'address']
+        , unique_key = ['blockchain', 'block_date', 'contract_address', 'symbol']
         , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
         )
 }}
