14368512926b4832fe7999db1096083710b91d25
beve0x <gk0001va@gmail.com>
2025-02-04 23:24:08 -0700

    fixed unique key error

--- dbt_subprojects/tokens/models/transfers_and_balances/avalanche_c/tokens_avalanche_c_net_value_transfers_asset_daily.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/avalanche_c/tokens_avalanche_c_net_value_transfers_asset_daily.sql
@@ -6,7 +6,7 @@
         , materialized = 'incremental'
         , file_format = 'delta'
         , incremental_strategy = 'merge'
-        , unique_key = ['blockchain', 'block_date', 'contract_address', 'symbol']
+        , unique_key = ['blockchain', 'block_date', 'contract_address']
         , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
         )
 }}
