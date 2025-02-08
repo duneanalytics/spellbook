5f8dd376aa3929af11d201233716f10749bae083
Huang Geyang <Sukebeta@outlook.com>
2025-02-08 17:00:27 +0800

    Draft ci_incremental_predicate

--- dbt_macros/dune/incremental_predicate.sql
+++ dbt_macros/dune/incremental_predicate.sql
@@ -1,3 +1,3 @@
 {% macro incremental_predicate(column) -%}
-{{column}} >= date_trunc('{{var("DBT_ENV_INCREMENTAL_TIME_UNIT")}}', now() - interval '{{var('DBT_ENV_INCREMENTAL_TIME')}}' {{var('DBT_ENV_INCREMENTAL_TIME_UNIT')}})
+{{ ci_incremental_predicate(column) }}
 {%- endmacro -%}
