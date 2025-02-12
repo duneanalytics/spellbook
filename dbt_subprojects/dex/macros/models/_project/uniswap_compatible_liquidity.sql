9befb9556afe1af1c1de10dfa92e25b7030b9b8b
Mansi <122999960+IrishLatte19@users.noreply.github.com>
2025-02-12 22:22:10 +0530

    Update uniswap_compatible_liquidity.sql

--- dbt_subprojects/dex/macros/models/_project/uniswap_compatible_liquidity.sql
+++ dbt_subprojects/dex/macros/models/_project/uniswap_compatible_liquidity.sql
@@ -89,8 +89,8 @@ SELECT
         , salt
         , token0
         , token1
-        , CAST(amount0 AS UINT256) as amount0_raw
-        , CAST(amount1 AS UINT256) as amount1_raw
+        , CAST(amount0 AS BIGINT) as amount0_raw
+        , CAST(amount1 AS BIGINT) as amount1_raw
   FROM  base_amounts base
 
 {% endmacro %}
