d5a3e10abdaa5b437d7c8969a7dfafecdf2400f7
IrishLatte19 <mansibansal.1912@gmail.com>
2025-02-12 20:01:55 +0530

    Uniswap V4 dex liquidity (ethereum)

---
+++ dbt_subprojects/dex/macros/models/_project/uniswap_compatible_liquidity.sql
@@ -0,0 +1,96 @@
+{% macro uniswap_compatible_v4_liquidity(
+    blockchain = null
+    , project = 'uniswap'
+    , version = '4'
+    , PoolManager_evt_ModifyLiquidity = null
+    , PoolManager_evt_Swap = null
+    , PoolManager_evt_Initialize = null
+    , pair_column_name = 'id'
+    )
+%}
+WITH get_recent_sqrtPriceX96 AS
+(
+    SELECT *
+      FROM (
+                SELECT 
+                        ml.*
+                        ,i.currency0 as token0
+                        , i.currency1 as token1
+                        , COALESCE(s.evt_block_time, i.evt_block_time) as most_recent_time
+                        , COALESCE(s.sqrtPriceX96, i.sqrtPriceX96) AS sqrtPriceX96
+                        , ROW_NUMBER() OVER (
+                            PARTITION BY ml.id, ml.evt_block_time
+                            ORDER BY 
+                                CASE WHEN s.sqrtPriceX96 IS NOT NULL THEN s.evt_block_time ELSE i.evt_block_time END DESC
+                        ) AS rn
+                    FROM 
+                        {{ PoolManager_evt_ModifyLiquidity }} ml
+                    LEFT JOIN 
+                        {{ PoolManager_evt_Swap }} s ON ml.evt_block_time > s.evt_block_time AND ml.id = s.id
+                    LEFT JOIN 
+                        {{ PoolManager_evt_Initialize }} i ON ml.evt_block_time >= i.evt_block_time AND i.{{pair_column_name}} = ml.id
+                    {%- if is_incremental() %}
+                        WHERE
+                            {{ incremental_predicate('ml.evt_block_time') }}
+                    {%- endif %}
+            )tbl
+        WHERE rn = 1
+),
+prep_for_calculations AS (
+    SELECT  evt_block_time as block_time
+          , evt_block_number as block_number
+          , id 
+          , evt_tx_hash as tx_hash
+          , evt_index
+          , salt
+          , token0
+          , token1
+          , LOG(sqrtPriceX96/POWER(2, 96), 10)/LOG(1.0001, 10) as tickCurrent
+          , tickLower
+          , tickUpper
+          , SQRT(POWER(1.0001, tickLower)) as sqrtRatioL
+          , SQRT(POWER(1.0001, tickUpper)) sqrtRatioU
+          , sqrtPriceX96/ POWER(2, 96) sqrtPrice
+          , sqrtPriceX96
+          , liquidityDelta
+    FROM get_recent_sqrtPriceX96
+),
+base_amounts AS (
+    SELECT
+          block_time
+        , block_number
+        , id 
+        , tx_hash
+        , evt_index
+        , salt
+        , token0
+        , token1
+        , CASE WHEN sqrtPrice <= sqrtRatioL THEN liquidityDelta * ((sqrtRatioU - sqrtRatioL)/(sqrtRatioL*sqrtRatioU))
+               WHEN sqrtPrice >= sqrtRatioU THEN 0
+               ELSE liquidityDelta * ((sqrtRatioU - sqrtPrice)/(sqrtPrice*sqrtRatioU))
+           END as amount0
+        , CASE WHEN sqrtPrice <= sqrtRatioL THEN 0
+               WHEN sqrtPrice >= sqrtRatioU THEN liquidityDelta*(sqrtRatioU - sqrtRatioL)
+               ELSE liquidityDelta*(sqrtPrice - sqrtRatioL)
+           END as amount1
+    FROM prep_for_calculations pc
+)
+SELECT 
+          {{blockchain}} AS blockchain
+        , {{project}}  AS project
+        , {{version}} AS version
+        , CAST(date_trunc('month', base.block_time) AS date) AS block_month
+        , CAST(date_trunc('day', base.block_time) AS date) AS block_date
+        , block_time
+        , block_number
+        , id
+        , tx_hash
+        , evt_index
+        , salt
+        , token0
+        , token1
+        , amount0 as amount0_raw
+        , amount1 as amount1_raw
+  FROM  base_amounts base
+
+{% endmacro %}
---
+++ dbt_subprojects/dex/models/liquidity/dex.liquidity.sql
@@ -0,0 +1,54 @@
+{{ config(
+    schema = 'dex'
+    , alias = 'liquidity'
+    , partition_by = ['block_month', 'blockchain', 'project']
+    , materialized = 'incremental'
+    , file_format = 'delta'
+    , incremental_strategy = 'merge'
+    , unique_key = ['blockchain', 'project', 'version', 'tx_hash', 'evt_index']
+    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    , post_hook='{{ expose_spells(\'[
+                                      "ethereum"
+                                    ]\',
+                                    "sector",
+                                    "dex",
+                                    \'["irishlatte19"]\') }}')
+}}
+
+WITH dexes AS (
+    {{
+        enrich_dex_liq_with_prices(
+            base_liquidity = ref('dex_base_liquidity')
+            , tokens_erc20_model = source('tokens', 'erc20')
+        )
+    }}
+)
+    {% for dex in dexes %}
+          SELECT
+                 blockchain
+                , project
+                , version
+                , block_month
+                , block_date
+                , block_time
+                , block_number
+                , id
+                , tx_hash
+                , evt_index
+                , salt
+                , token0
+                , token1
+                , amount0
+                , amount1
+                , amount0_usd
+                , amount1_usd
+           FROM
+                {{ dex }}
+          {% if is_incremental() %}
+           WHERE
+               {{ incremental_predicate('block_time') }}
+          {% endif %}
+        {% if not loop.last %}
+           UNION ALL
+        {% endif %}
+    {% endfor %}
