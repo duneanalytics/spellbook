fe3c5b02b36fe97c1e927721261daadb1bc94b13
RantumBits <rantum@rantum.xyz>
2025-02-12 17:08:23 -0800

    re-add new chains

--- dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
+++ dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
@@ -33,6 +33,9 @@
   ,ref('zeroex_linea_settler_trades')
   ,ref('zeroex_blast_settler_trades')
   ,ref('zeroex_mantle_settler_trades')
+  ,ref('zeroex_v2_mode_trades')
+  ,ref('zeroex_v2_unichain_trades')
+  ,ref('zeroex_v2_worldchain_trades')
 ] %}
 
 
