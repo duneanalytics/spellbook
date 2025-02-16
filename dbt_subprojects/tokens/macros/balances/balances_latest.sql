d4d8dc4bd3e933a6e434c1154ce39fad2760e29b
jeff-dude <jeff@dune.xyz>
2025-02-16 16:44:04 -0500

    remove testing filter

--- dbt_subprojects/tokens/macros/balances/balances_latest.sql
+++ dbt_subprojects/tokens/macros/balances/balances_latest.sql
@@ -19,8 +19,6 @@ where
     1 = 1
     {% if is_incremental() %}
     and {{ incremental_predicate('block_time') }}
-    {% else %}
-    and block_time >= date '2025-01-01' -- for testing
     {% endif %}
 group by
     blockchain,
