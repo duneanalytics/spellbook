e37ae3b23b56747d4dc58fe1d2ac50f519847459
Huang Geyang <Sukebeta@outlook.com>
2025-02-17 21:47:54 +0800

    Fix syntax

--- dbt_subprojects/tokens/models/transfers_and_balances/sophon/tokens_sophon_base_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/sophon/tokens_sophon_base_transfers.sql
@@ -1,21 +1,19 @@
-{{config(
-     schema = 'tokens_sophon',
-     alias = 'base_transfers',
-     partition_by = ['block_month'],
-     materialized = 'incremental',
-     file_format = 'delta',
-     incremental_strategy = 'merge',
-     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-     unique_key = ['block_date','unique_key'],
- )
-}}
+{{ config(
+    schema = 'tokens_sophon',
+    alias = 'base_transfers',
+    partition_by = ['block_month'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    unique_key = ['block_date','unique_key'],
+) }}
 
-{{transfers_base(
+{{ transfers_base(
     blockchain='sophon',
     traces = source('sophon','traces'),
     transactions = source('sophon','transactions'),
     erc20_transfers = source('erc20_sophon','evt_transfer'),
     native_contract_address = '0x000000000000000000000000000000000000800a',  -- SOPH
     include_traces = false  -- Set to false for ZK Stack chains
-)
-}}
+) }}
