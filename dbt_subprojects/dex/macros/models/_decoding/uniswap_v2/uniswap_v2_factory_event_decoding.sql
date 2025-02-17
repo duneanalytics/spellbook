d1ff73d6cd3348d50e1c83db1f40f45bb43df8e4
Boxer <Floba.hs@gmail.com>
2025-02-17 15:14:28 +0100

    more factory events

--- dbt_subprojects/dex/macros/models/_decoding/uniswap_v2/uniswap_v2_factory_event_decoding.sql
+++ dbt_subprojects/dex/macros/models/_decoding/uniswap_v2/uniswap_v2_factory_event_decoding.sql
@@ -1,4 +1,6 @@
-{% macro uniswap_v2_factory_event_decoding(logs) %}
+/*
+
+{% macro uniswap_v2_factory_event_decoding_old(logs) %}
 
 {% set abi = '{
     "anonymous": false,
@@ -38,5 +40,138 @@
 {% if is_incremental() %}
 WHERE {{ incremental_predicate('block_time') }}
 {% endif %}
+
+{% endmacro %}
+*/
+
+{% macro uniswap_v2_factory_event_decoding(logs) %}
+
+{% 
+    set abi_config = {
+        '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9': '{
+            "anonymous": false,
+            "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token0",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token1",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "pair",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+            ],
+            "name": "PairCreated",
+            "type": "event"
+        }',
+
+        '0x41f8736f924f57e464ededb08bf71f868f9d142885bbc73a1516db2be21fc428': '{
+            "anonymous": false,
+            "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token0",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token1",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "pair",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint32",
+                "name": "tokenWeight0",
+                "type": "uint32"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint32",
+                "name": "swapFee",
+                "type": "uint32"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+            ],
+            "name": "PairCreated",
+            "type": "event"
+        }',
+
+        '0x3541d8fea55be35f686281f975bf8b7ab8fbb500c1c7ddd6c4e714655e9cd4e2': '{
+            "anonymous": false,
+            "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token0",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token1",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "pair",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint32",
+                "name": "swapFee",
+                "type": "uint32"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+            ],
+            "name": "PairCreated",
+            "type": "event"
+    }'
+} 
+%}
+
+{% for topic0, abi in abi_config.items() %}
+    {{ evm_event_decoding_base(logs, abi, topic0) }} 
+    {% if is_incremental() %}
+    WHERE {{ incremental_predicate('block_time') }}
+    {% endif %}
+    {% if not loop.last %}
+        union all 
+    {% endif %}
+{% endfor %}
 
 {% endmacro %}
