2dffb548444ef8ee3033a01cf6b171a652adf7f6
Huang Geyang <Sukebeta@outlook.com>
2025-02-17 21:53:12 +0800

    Update tokens_sophon_transfers.sql

--- dbt_subprojects/tokens/models/transfers_and_balances/sophon/tokens_sophon_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/sophon/tokens_sophon_transfers.sql
@@ -7,20 +7,20 @@
      incremental_strategy = 'merge',
      unique_key = ['block_date','unique_key'],
      incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
-     post_hook='{{ expose_spells(\'["sophon"]\',
-                                 "sector",
-                                 "tokens",
-                                 \'["hosuke"]\') }}'
- )
+     post_hook='{{ expose_spells(blockchains = \'["sophon"]\',
+                                 spell_type = "sector",
+                                 spell_name = "tokens",
+                                 contributors = \'["hosuke"]\') }}'
+  )
 }}
 
-{{
-     transfers_enrich(
-         base_transfers = ref('tokens_sophon_base_transfers')
-         , tokens_erc20_model = source('tokens', 'erc20')
-         , prices_model = source('prices', 'usd')
-         , evms_info_model = source('evms','info')
-         , transfers_start_date = '2024-10-22'  -- Sophon mainnet launch date (Block #1 timestamp)
-         , blockchain = 'sophon'
-     )
+-- Sophon mainnet launch date: 2024-10-22 (Block #1 timestamp)
+{{ transfers_enrich(
+    base_transfers = ref('tokens_sophon_base_transfers'),
+    tokens_erc20_model = source('tokens', 'erc20'),
+    prices_model = source('prices', 'usd'),
+    evms_info_model = source('evms','info'),
+    transfers_start_date = '2024-10-22',
+    blockchain = 'sophon'
+  ) 
 }}
