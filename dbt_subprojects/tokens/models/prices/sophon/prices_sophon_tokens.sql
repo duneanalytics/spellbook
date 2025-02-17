b1f821257683c03a749f2f171f280d0fbe61160b
Huang Geyang <Sukebeta@outlook.com>
2025-02-17 21:42:25 +0800

    Update sophon prices

--- dbt_subprojects/tokens/models/prices/sophon/prices_sophon_tokens.sql
+++ dbt_subprojects/tokens/models/prices/sophon/prices_sophon_tokens.sql
@@ -18,6 +18,6 @@ SELECT
 FROM
 (
     VALUES
-    ('soph-sophon', 'SOPH', 0x000000000000000000000000000000000000800A, 18)
-    , ('eth-ethereum', 'ETH', 0x72af9F169B619D85A47Dfa8fefbCD39dE55c567D, 18)
+    -- ('soph-sophon', 'SOPH', 0x000000000000000000000000000000000000800A, 18)  -- Temporarily removed as no price data available yet
+    ('eth-ethereum', 'ETH', 0x72af9F169B619D85A47Dfa8fefbCD39dE55c567D, 18)
 ) as temp (token_id, symbol, contract_address, decimals) 
