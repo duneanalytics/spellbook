e8408b48c88b0357a4cb97968351adfb2e023f8b
Boxer <Floba.hs@gmail.com>
2025-02-18 13:48:51 +0100

    add more factory event configs

--- dbt_subprojects/dex/macros/models/_decoding/uniswap_v2/uniswap_v2_factory_event_decoding.sql
+++ dbt_subprojects/dex/macros/models/_decoding/uniswap_v2/uniswap_v2_factory_event_decoding.sql
@@ -46,35 +46,35 @@ WHERE {{ incremental_predicate('block_time') }}
 
 {% macro uniswap_v2_factory_event_decoding(logs) %}
 
-{% 
+{%- 
     set abi_config = {
         '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9': '{
             "anonymous": false,
             "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "token0",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "token1",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "address",
-                "name": "pair",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token0",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token1",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "pair",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
             ],
             "name": "PairCreated",
             "type": "event"
@@ -83,42 +83,42 @@ WHERE {{ incremental_predicate('block_time') }}
         '0x41f8736f924f57e464ededb08bf71f868f9d142885bbc73a1516db2be21fc428': '{
             "anonymous": false,
             "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "token0",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "token1",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "address",
-                "name": "pair",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint32",
-                "name": "tokenWeight0",
-                "type": "uint32"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint32",
-                "name": "swapFee",
-                "type": "uint32"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token0",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token1",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "pair",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint32",
+                    "name": "tokenWeight0",
+                    "type": "uint32"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint32",
+                    "name": "swapFee",
+                    "type": "uint32"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
             ],
             "name": "PairCreated",
             "type": "event"
@@ -127,47 +127,129 @@ WHERE {{ incremental_predicate('block_time') }}
         '0x3541d8fea55be35f686281f975bf8b7ab8fbb500c1c7ddd6c4e714655e9cd4e2': '{
             "anonymous": false,
             "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "token0",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "token1",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "address",
-                "name": "pair",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint32",
-                "name": "swapFee",
-                "type": "uint32"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token0",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token1",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "pair",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint32",
+                    "name": "swapFee",
+                    "type": "uint32"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
             ],
             "name": "PairCreated",
             "type": "event"
-    }'
-} 
-%}
+        }',
 
-{% for topic0, abi in abi_config.items() %}
-    {% if not loop.first %}
+        '0xc4805696c66d7cf352fc1d6bb633ad5ee82f6cb577c453024b6e0eb8306c6fc9': '{
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token0",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token1",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "bool",
+                    "name": "stable",
+                    "type": "bool"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "pair",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "PairCreated",
+            "type": "event"
+        }',
+
+        '0x25bc54a32c894b07fd47ed3cc4296ec7d97a974e5ebd17c9f5163afddaf107fa': '{
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token0",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "token1",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "bool",
+                    "name": "stable",
+                    "type": "bool"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "pair",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "fee",
+                    "type": "uint256"
+                }
+            ],
+            "name": "PairCreated",
+            "type": "event"
+        }'
+    }
+-%}
+
+{%- for topic0, abi in abi_config.items() -%}
+    {%- if not loop.first %}
     union all 
-    {% endif %}
+    {%- endif %}
     select 
         pair
         , token0
@@ -188,6 +270,6 @@ WHERE {{ incremental_predicate('block_time') }}
         WHERE {{ incremental_predicate('block_time') }}
         {% endif %}
     )
-{% endfor %}
+{%- endfor -%}
 
 {% endmacro %}
