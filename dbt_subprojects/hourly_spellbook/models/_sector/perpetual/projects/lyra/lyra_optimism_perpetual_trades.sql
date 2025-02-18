485786e423151620c119ea1390e35f8eec6495e2
PatelPrinci <principatel@lampros.tech>
2025-02-18 18:07:56 +0530

    fixed the name of fxdx v2 base trades file and lyra v1 on optimism

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/perpetual/projects/lyra/lyra_optimism_perpetual_trades.sql
@@ -0,0 +1,46 @@
+{{ config(
+    schema = 'lyra_optimism',
+      alias = 'perpetual_trades',
+    post_hook='{{ expose_spells(blockchains = \'["optimism"]\',
+                                    spell_type = "project",
+                                    spell_name = "lyra",
+                                    contributors = \'["princi"]\') }}'
+        )
+}}
+
+{% set lyra_optimism_perpetual_trade_models = [
+    ref('lyra_v1_optimism_perpetual_trades')
+] %}
+
+SELECT *
+FROM
+(
+    {% for lyra_perpetual_trades in lyra_optimism_perpetual_trade_models %}
+    SELECT
+    blockchain
+    ,block_date
+    ,block_month
+    ,block_time
+    ,virtual_asset
+    ,underlying_asset
+    ,market
+    ,market_address
+    ,volume_usd
+    ,fee_usd
+    ,margin_usd
+    ,trade
+    ,project
+    ,version
+    ,frontend
+    ,trader
+    ,volume_raw
+    ,tx_hash
+    ,tx_from
+    ,tx_to
+    ,evt_index
+    FROM {{ lyra_perpetual_trades }}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+    {% endfor %}
+)   
