a9eea70e623b8373e133fe31af9fd2b248f8c4d3
helanto <helias1990@hotmail.com>
2025-02-19 10:19:36 +0200

    fix: Column rename in gyroscope_arbitrum.gyro2clppoolfactory_call_create

--- dbt_subprojects/dex/models/_projects/balancer/labels/arbitrum/labels_balancer_v2_pools_arbitrum.sql
+++ dbt_subprojects/dex/models/_projects/balancer/labels/arbitrum/labels_balancer_v2_pools_arbitrum.sql
@@ -237,7 +237,7 @@ WITH pools AS (
     ON c.evt_tx_hash = cc.call_tx_hash
     AND bytearray_substring(c.poolId, 1, 20) = cc.output_0
   CROSS JOIN UNNEST(cc.tokens) AS t(tokens)
-/*
+
   UNION ALL
 
   SELECT
@@ -250,8 +250,9 @@ WITH pools AS (
   INNER JOIN {{ source('gyroscope_arbitrum', 'Gyro2CLPPoolFactory_call_create') }} cc
     ON c.evt_tx_hash = cc.call_tx_hash
     AND bytearray_substring(c.poolId, 1, 20) = cc.output_0
-  CROSS JOIN UNNEST(cc.tokens) AS t(tokens)
-*/
+  CROSS JOIN UNNEST(cc.tokens_array_binary) AS t(tokens)
+  WHERE cc.tokens_array_binary IS NOT NULL
+
   UNION ALL
   
   SELECT
