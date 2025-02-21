627ba42b04da28f5ab3f769da515acfebfac8092
jeff-dude <jeff@dune.xyz>
2025-02-21 17:22:51 -0500

    Revert "Using swap call for v4 dex.trades to get a holistic picture of trade volume (#7689)"
    This reverts commit a82f64fc41fa8b670abbf259a66c9ef0d0593b96.

--- dbt_subprojects/dex/models/trades/arbitrum/platforms/uniswap_v4_arbitrum_base_trades.sql
+++ dbt_subprojects/dex/models/trades/arbitrum/platforms/uniswap_v4_arbitrum_base_trades.sql
@@ -14,6 +14,7 @@
         blockchain = 'arbitrum'
         , project = 'uniswap'
         , version = '4'
-        , PoolManager_call_Swap = source('uniswap_v4_arbitrum', 'PoolManager_call_Swap') 
+        , PoolManager_evt_Swap = source('uniswap_v4_arbitrum', 'PoolManager_evt_Swap') 
+        , PoolManager_evt_Initialize = source('uniswap_v4_arbitrum', 'PoolManager_evt_Initialize')
     )
 }}
