a82f64fc41fa8b670abbf259a66c9ef0d0593b96
agaperste <agapehumilduro@gmail.com>
2025-02-21 15:51:19 -0500

    Using swap call for v4 dex.trades to get a holistic picture of trade volume (#7689)
    * take 1 on using swap call for v4 dex trades (testing with 1 day data)
    
    * call_tx_from don't exist for these chains yet, so removing
    
    * passing id in as param seems to be causing error, removing
    
    * not sure how to pass in column names in param that are later created (I think that's error), so making it simple to just list them for now
    
    * oops clicked too fast
    
    * there is no dexs.sender anymore cuz router/sender is only emitted in event, so removing
    
    * rip base also doesn't have call_tx_from in the swap call table T_T
    
    * removing "taker_column_name = 'call_tx_from'" from all v4 chain model now until we have all the field fed in
    
    * using a different way to create the evt_index from call_trace_address, can't directly cast as bigint because concatenated call_trace_address can be more than 24 digits long
    
    * updating seeds, maybe that's it
    
    * using uint256 instead of bigint for artificially created evt_index column
    
    * removing wrong seeds
    
    * removing test filter on time, fingers crossed!
    
    * forcing dedup (maybe something wrong with merge)
    
    * reverting row number fix, didn't fix
    
    * filter source incrementally
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/dex/macros/models/_project/uniswap_compatible_trades.sql
+++ dbt_subprojects/dex/macros/models/_project/uniswap_compatible_trades.sql
@@ -124,51 +124,176 @@ FROM
     blockchain = null
     , project = 'uniswap'
     , version = '4'
-    , PoolManager_evt_Swap = null
-    , PoolManager_evt_Initialize = null
+    , PoolManager_call_Swap = null
     , taker_column_name = null
-    , maker_column_name = 'id'
-    , swap_optional_columns = ['fee']
-    , initialize_optional_columns = ['hooks']
-    , pair_column_name = 'id'
+    , maker_column_name = null
     )
 %}
 WITH dexs AS
 (
-    SELECT
-        t.evt_block_number AS block_number
-        , t.evt_block_time AS block_time
-        , {% if taker_column_name -%} t.{{ taker_column_name }} {% else -%} cast(null as varbinary) {% endif -%} as taker
-        -- In v4, the maker (i.e. what sold the token) is the pool's virtual address. We also pass the pool ID, making it easier to join with Initialize() and retrieve hooked pool metrics.
-        , {% if maker_column_name -%} t.{{ maker_column_name }} {% else -%} cast(null as varbinary) {% endif -%} as maker      
-        -- in v4, when amount is negative, then user are selling the token (so things are done from the perspective of the user instead of the pool)
-        , CASE WHEN t.amount0 < INT256 '0' THEN abs(t.amount1) ELSE abs(t.amount0) END AS token_bought_amount_raw 
-        , CASE WHEN t.amount0 < INT256 '0' THEN abs(t.amount0) ELSE abs(t.amount1) END AS token_sold_amount_raw
-        , CASE WHEN t.amount0 < INT256 '0' THEN f.currency1 ELSE f.currency0 END AS token_bought_address
-        , CASE WHEN t.amount0 < INT256 '0' THEN f.currency0 ELSE f.currency1 END AS token_sold_address
-        , t.contract_address as project_contract_address
-        , t.sender 
-        , t.evt_tx_hash AS tx_hash
-        , t.evt_index
-        {%- if swap_optional_columns %}
-        {%- for optional_column in swap_optional_columns %}
-        , t.{{ optional_column }}
-        {%- endfor %}
-        {%- endif %}
-        {%- if initialize_optional_columns %}
-        {%- for optional_column in initialize_optional_columns %}
-        , f.{{ optional_column }}
-        {%- endfor %}
-        {%- endif %}
-    FROM
-        {{ PoolManager_evt_Swap }} t
-    INNER JOIN
-        {{ PoolManager_evt_Initialize }} f
-        ON f.{{ pair_column_name }} = t.id
-    {%- if is_incremental() %}
-    WHERE
-        {{ incremental_predicate('t.evt_block_time') }}
-    {%- endif %}
+    WITH clean_swaps AS (
+        WITH raw AS (
+            SELECT 
+                call_block_number
+            , call_block_time 
+            , call_tx_hash 
+            , contract_address
+            , call_trace_address
+            
+            -- Applying keccak256(abi.encode(poolKey)) in SQL to create the virtual pool ID
+            , keccak (
+                CONCAT(
+                    LPAD(
+                        FROM_HEX(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.currency0'))
+                        , 32
+                        , 0x00
+                    )
+                    , LPAD(
+                        FROM_HEX(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.currency1'))
+                        , 32
+                        , 0x00
+                    )
+                    , LPAD(
+                        CAST(CAST(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.fee') AS UINT256) AS VARBINARY)
+                        , 32
+                        , 0x00
+                    )
+                    , LPAD(
+                        CAST(CAST(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.tickSpacing') AS INT256) AS VARBINARY)
+                        , 32
+                        , 0x00
+                    )
+                    , LPAD(
+                        FROM_HEX(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.hooks'))
+                        , 32
+                        , 0x00
+                    )
+                )
+            ) AS id
+            
+            , FROM_HEX(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.currency0')) AS currency0
+            , FROM_HEX(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.currency1')) AS currency1
+            , CAST(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.fee') AS UINT256) AS swapFee 
+            , FROM_HEX(JSON_EXTRACT_SCALAR(JSON_PARSE("key"), '$.hooks')) AS hooks
+            , CAST(JSON_EXTRACT(params, '$.zeroForOne') AS BOOLEAN) AS zeroForOne
+            , JSON_EXTRACT(params, '$.amountSpecified') AS amountSpecified
+            , CAST(output_swapDelta AS VARBINARY) AS swapDelta_varbinary
+            
+            FROM {{ PoolManager_call_Swap }}
+            WHERE call_success
+                {%- if is_incremental() %}
+                AND {{ incremental_predicate('call_block_time') }}
+                {%- endif %}
+        )
+
+        , wrangled AS (
+            SELECT *
+            
+            -- The top 16 bytes
+            , CASE 
+                WHEN BITWISE_AND(
+                    VARBINARY_TO_BIGINT(VARBINARY_SUBSTRING(swapDelta_varbinary, 1, 1))
+                    , FROM_BASE('80', 16) -- 0x80 as decimal 128
+                ) = FROM_BASE('80', 16)
+                THEN VARBINARY_TO_INT256(
+                    VARBINARY_CONCAT(
+                        FROM_HEX('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF') -- 16 bytes of 0xFF
+                        , VARBINARY_SUBSTRING(swapDelta_varbinary, 1, 16)           
+                    )
+                )
+                ELSE VARBINARY_TO_INT256(
+                    VARBINARY_CONCAT(
+                        FROM_HEX('0x00000000000000000000000000000000') -- 16 bytes of 0x00
+                        , VARBINARY_SUBSTRING(swapDelta_varbinary, 1, 16)
+                    )
+                )
+            END AS high_bits
+            
+            -- The bottom 16 bytes
+            , CASE 
+                WHEN BITWISE_AND(
+                    VARBINARY_TO_BIGINT(VARBINARY_SUBSTRING(swapDelta_varbinary, 17, 1))
+                    , FROM_BASE('80', 16)
+                ) = FROM_BASE('80', 16)
+                THEN VARBINARY_TO_INT256(
+                    VARBINARY_CONCAT(
+                        FROM_HEX('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF') -- 16 bytes of 0xFF
+                        , VARBINARY_SUBSTRING(swapDelta_varbinary, 17, 16)          
+                    )
+                )
+                ELSE VARBINARY_TO_INT256(
+                    VARBINARY_CONCAT(
+                        FROM_HEX('0x00000000000000000000000000000000') -- 16 bytes of 0x00
+                        , VARBINARY_SUBSTRING(swapDelta_varbinary, 17, 16)
+                    )
+                )
+            END AS low_bits
+            
+            FROM raw
+        )
+
+        , base_data AS (
+            SELECT 
+                *
+            , CAST(CAST(amountSpecified AS VARCHAR) AS INT256) AS amountSpecified_input
+            , CASE WHEN CAST(CAST(amountSpecified AS VARCHAR) AS INT256) < 0 THEN TRUE ELSE FALSE END AS exactInput -- bool exactInput = params.amountSpecified < 0
+            , high_bits AS specifiedCurrency
+            , low_bits AS unspecifiedCurrency
+            FROM wrangled
+        )
+
+        /*
+        Formula for calculating amount0 and amount1 from returned delta:
+            amount0 = (zeroForOne and exact-input) ? specifiedDelta : unspecifiedDelta
+            amount1 = (oneForZero and exact-input) ? specifiedDelta : unspecifiedDelta
+        */
+        SELECT 
+            call_block_number
+        , call_block_time
+        , id
+        , contract_address
+        , call_tx_hash
+        , currency0
+        , currency1
+        , swapFee
+        , hooks
+        , zeroForOne
+        , exactInput
+        , specifiedCurrency AS specifiedDelta
+        , unspecifiedCurrency AS unspecifiedDelta
+        , call_trace_address
+        
+        -- Calculate amount0 and amount1 with formula; signage is from user's perspective
+        , CASE 
+            WHEN zeroForOne AND exactInput THEN specifiedCurrency
+            ELSE unspecifiedCurrency
+        END AS amount0
+        , CASE 
+            WHEN NOT (zeroForOne AND exactInput) THEN specifiedCurrency
+            ELSE unspecifiedCurrency
+        END AS amount1
+
+        FROM base_data
+    )
+
+    SELECT 
+        call_block_number AS block_number
+    , call_block_time AS block_time
+    , {% if taker_column_name -%} t.{{ taker_column_name }} {% else -%} cast(null as varbinary) {% endif -%} as taker
+    , id as maker -- In v4, the maker (i.e. what sold the token) is the pool's virtual address. We also pass the pool ID, making it easier to join with Initialize() and retrieve hooked pool metrics.
+    , CASE WHEN amount0 < INT256 '0' THEN ABS(amount1) ELSE ABS(amount0) END AS token_bought_amount_raw 
+    , CASE WHEN amount0 < INT256 '0' THEN ABS(amount0) ELSE ABS(amount1) END AS token_sold_amount_raw
+    , CASE WHEN amount0 < INT256 '0' THEN currency1 ELSE currency0 END AS token_bought_address
+    , CASE WHEN amount0 < INT256 '0' THEN currency0 ELSE currency1 END AS token_sold_address
+    , contract_address AS project_contract_address
+    , call_tx_hash AS tx_hash
+    , varbinary_to_uint256(xxhash64(to_utf8(array_join(call_trace_address, '')))) as evt_index -- we are using swap call here, so artificially creating evt_index | can't directly cast as bigint because concatenated call_trace_address can be more than 24 digits long
+
+    , swapFee
+    , hooks
+
+    FROM clean_swaps 
+
 )
 
 SELECT
@@ -183,22 +308,13 @@ SELECT
     , CAST(dexs.token_sold_amount_raw AS UINT256) AS token_sold_amount_raw
     , dexs.token_bought_address
     , dexs.token_sold_address
-    , dexs.sender as router
     , dexs.taker
     , dexs.maker
     , dexs.project_contract_address
     , dexs.tx_hash
     , dexs.evt_index
-    {%- if swap_optional_columns %}  
-    {%- for optional_column in swap_optional_columns %}  
-    , dexs.{{ optional_column }}  
-    {%- endfor %}  
-    {%- endif %}  
-    {%- if initialize_optional_columns %}
-    {%- for optional_column in initialize_optional_columns %}
-    , dexs.{{ optional_column }}
-    {%- endfor %}
-    {%- endif %}
+    , dexs.swapFee
+    , dexs.hooks
 FROM
     dexs
 {% endmacro %}
