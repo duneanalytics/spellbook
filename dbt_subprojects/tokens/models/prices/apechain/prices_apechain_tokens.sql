f260850c94a9d05d3cdd3e4753a08020011bdec9
Huang Geyang <Sukebeta@outlook.com>
2025-02-21 17:23:11 +0800

    Add ApeUSD

--- dbt_subprojects/tokens/models/prices/apechain/prices_apechain_tokens.sql
+++ dbt_subprojects/tokens/models/prices/apechain/prices_apechain_tokens.sql
@@ -22,4 +22,5 @@ FROM
     , ('gns-gains-network', 'GNS', 0xe31c676d8235437597581b44c1c4f8a30e90b38a, 18)
     , ('ape-apecoin', 'APE', 0xfc7b0badb1404412a747bc9bb6232e25098be303, 18)
     , ('eth-ethereum', 'ApeETH', 0xcf800f4948d16f23333508191b1b1591daf70438, 18)
+    , ('usdc-usd-coin', 'ApeUSD', 0xa2235d059f80e176d931ef76b6c51953eb3fbef4, 18)
 ) as temp (token_id, symbol, contract_address, decimals)
--- dbt_subprojects/tokens/models/tokens/apechain/tokens_apechain_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/apechain/tokens_apechain_erc20.sql
@@ -16,4 +16,5 @@ FROM (VALUES
     , (0xe31c676d8235437597581b44c1c4f8a30e90b38a, 'GNS', 18)
     , (0xfc7b0badb1404412a747bc9bb6232e25098be303, 'APE', 18)
     , (0xcf800f4948d16f23333508191b1b1591daf70438, 'ApeETH', 18)
+    , (0xa2235d059f80e176d931ef76b6c51953eb3fbef4, 'ApeUSD', 18)
 ) AS temp_table (contract_address, symbol, decimals)
