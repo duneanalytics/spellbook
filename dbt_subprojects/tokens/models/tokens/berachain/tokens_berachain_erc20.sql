4cdf624dfed9b7b0c3545f01a968e72ecf38090a
Rebustron <dimaqwe94@gmail.com>
2025-02-26 15:40:21 +0300

    Update tokens_berachain_erc20.sql

--- dbt_subprojects/tokens/models/tokens/berachain/tokens_berachain_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/berachain/tokens_berachain_erc20.sql
@@ -25,5 +25,5 @@ FROM (VALUES
     , (0x211cc4dd073734da055fbf44a2b4667d5e5fe5d2, 'sUSDe', 18)
     , (0x09d4214c03d01f49544c0448dbe3a27f768f2b34, 'rUSD', 18)
     , (0xc3827a4bc8224ee2d116637023b124ced6db6e90, 'uniBTC', 8)
-    , (0x5b82028cfc477c4e7dda7ff33d59a23fa7be002a, 'MIM', 8)
+    , (0x5b82028cfc477c4e7dda7ff33d59a23fa7be002a, 'MIM', 18)
 ) AS temp_table (contract_address, symbol, decimals) 
