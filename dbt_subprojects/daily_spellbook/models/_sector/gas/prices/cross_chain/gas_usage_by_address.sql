9dbab305319e71e87bf85aeb682677d9740c9164
Daniel Pfeffer <daniel@gashawk.io>
2025-02-26 11:56:11 +0100

    refactor: enhance gas usage by address model with advanced incremental tracking

--- dbt_subprojects/daily_spellbook/models/_sector/gas/prices/cross_chain/gas_usage_by_address.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/gas/prices/cross_chain/gas_usage_by_address.sql
@@ -14,53 +14,82 @@
 
 WITH
 {% if is_incremental() %}
-current_total_metrics AS (
+current_metrics AS (
     -- Get the oldest block_time in the current materialized table
-    SELECT address, blockchain, currency_symbol, number_of_txs, gas_spent_usd_total, gas_spent_native_total, last_block_time_for_total_metrics
+    SELECT address, 
+        blockchain, 
+        currency_symbol, 
+        number_of_txs, 
+        gas_spent_usd_total, 
+        gas_spent_native_total, 
+        gas_spent_native_1_year, 
+        gas_spent_usd_1_year, 
+        last_block_time_of_incremental_update
     FROM {{ this }}
 ),
 
-last_year_window AS (
+last_30d_window AS (
     SELECT 
         blockchain,
-        tx_from as address,
+        tx_from as "address",
         currency_symbol,
         block_time,
         tx_fee as gas_cost_native,
         tx_fee_usd as gas_cost_usd
     FROM {{ source('gas', 'fees') }}
     WHERE currency_symbol is not null
-    AND block_time >= now() - INTERVAL '1' YEAR
+    AND block_time >= now() - INTERVAL '30' DAY
 ),
 
-current_metrics AS (
+window_to_forget AS (
+    SELECT
+        tx_from as "address",
+        blockchain,
+        currency_symbol,
+        SUM(gas_cost_usd) as gas_spent_usd,
+        SUM(gas_cost_native) as gas_spent_native
+    FROM {{ source('gas', 'fees') }} gf
+    LEFT JOIN current_metrics cm
+        ON cm.address = gf.tx_from 
+        AND cm.blockchain = gf.blockchain 
+        AND cm.currency_symbol = gf.currency_symbol
+    WHERE block_time > cm.last_block_time_of_incremental_update - INTERVAL '1' YEAR 
+        AND block_time < cm.last_block_time_of_incremental_update - INTERVAL '1' YEAR + INTERVAL {{var('DBT_ENV_INCREMENTAL_TIME')}}
+    GROUP BY 1, 2, 3
+),
+
+updated_metrics AS (
     SELECT
         address,
         blockchain,
         currency_symbol,
-        COALESCE(ct.number_of_txs, 0) + COUNT(*) FILTER (where block_time > lyw.last_block_time_for_total_metrics) as number_of_txs,
-        COALESCE(ct.gas_spent_usd_total, 0) + SUM(CASE WHEN block_time > lyw.last_block_time_for_total_metrics THEN lyw.gas_cost_usd END) as gas_spent_usd_total,
-        SUM(CASE WHEN block_time >= now() - INTERVAL '1' DAY THEN lyw.gas_cost_usd END) as gas_spent_usd_24_hours,
-        SUM(CASE WHEN block_time >= now() - INTERVAL '7' DAY THEN lyw.gas_cost_usd END) as gas_spent_usd_7_days,
-        SUM(CASE WHEN block_time >= now() - INTERVAL '30' DAY THEN lyw.gas_cost_usd END) as gas_spent_usd_30_days,
-        SUM(lyw.gas_cost_usd) as gas_spent_usd_1_year,
-        COALESCE(ct.gas_spent_native_total, 0) + SUM(CASE WHEN block_time > lyw.last_block_time_for_total_metrics THEN lyw.gas_cost_native END) as gas_spent_native_total,
-        SUM(CASE WHEN block_time >= now() - INTERVAL '1' DAY THEN lyw.gas_cost_native END) as gas_spent_native_24_hours,
-        SUM(CASE WHEN block_time >= now() - INTERVAL '7' DAY THEN lyw.gas_cost_native END) as gas_spent_native_7_days,
-        SUM(CASE WHEN block_time >= now() - INTERVAL '30' DAY THEN lyw.gas_cost_native END) as gas_spent_native_30_days,
-        SUM(lyw.gas_cost_native) as gas_spent_native_1_year,
-        MAX(lyw.block_time) as last_block_time_for_total_metrics
-    FROM last_year_window lyw
-    FULL OUTER JOIN current_total_metrics ct
-    ON ct.address = lyw.address 
-    AND ct.blockchain = lyw.blockchain 
-    AND ct.currency_symbol = lyw.currency_symbol
+        COALESCE(cm.number_of_txs, 0) + COUNT(*) FILTER (where block_time > lw.last_block_time_of_incremental_update) as number_of_txs,
+        COALESCE(cm.gas_spent_usd_total, 0) + SUM(CASE WHEN block_time > lw.last_block_time_of_incremental_update THEN lw.gas_cost_usd END) as gas_spent_usd_total,
+        SUM(CASE WHEN block_time >= now() - INTERVAL '1' DAY THEN lw.gas_cost_usd END) as gas_spent_usd_24_hours,
+        SUM(CASE WHEN block_time >= now() - INTERVAL '7' DAY THEN lw.gas_cost_usd END) as gas_spent_usd_7_days,
+        SUM(CASE WHEN block_time >= now() - INTERVAL '30' DAY THEN lw.gas_cost_usd END) as gas_spent_usd_30_days,
+        SUM(cm.gas_spent_usd_1_year) + SUM(CASE WHEN incremental_predicate('block_time') THEN lw.gas_cost_usd END) - COALESCE(wtf.gas_spent_usd, 0) as gas_spent_usd_1_year,
+        COALESCE(cm.gas_spent_native_total, 0) + SUM(CASE WHEN block_time > lw.last_block_time_of_incremental_update THEN lw.gas_cost_native END) as gas_spent_native_total,
+        SUM(CASE WHEN block_time >= now() - INTERVAL '1' DAY THEN lw.gas_cost_native END) as gas_spent_native_24_hours,
+        SUM(CASE WHEN block_time >= now() - INTERVAL '7' DAY THEN lw.gas_cost_native END) as gas_spent_native_7_days,
+        SUM(CASE WHEN block_time >= now() - INTERVAL '30' DAY THEN lw.gas_cost_native END) as gas_spent_native_30_days,
+        SUM(cm.gas_spent_native_1_year) + SUM(CASE WHEN incremental_predicate('block_time') THEN lw.gas_cost_native END) - COALESCE(wtf.gas_spent_native, 0) as gas_spent_native_1_year,
+        MAX(lw.block_time) as last_block_time_of_incremental_update
+    FROM last_30d_window lw
+    FULL OUTER JOIN current_metrics cm
+        ON cm.address = lw.address 
+        AND cm.blockchain = lw.blockchain 
+        AND cm.currency_symbol = lw.currency_symbol
+    LEFT JOIN window_to_forget wtf
+        ON wtf.address = lw.address 
+        AND wtf.blockchain = lw.blockchain 
+        AND wtf.currency_symbol = lw.currency_symbol
     GROUP BY 1, 2, 3
 )
 
 {% else %}
 
-current_metrics AS (
+updated_metrics AS (
     SELECT
         address,
         blockchain,
@@ -76,12 +105,12 @@ current_metrics AS (
         SUM(CASE WHEN block_time >= now() - INTERVAL '7' DAY THEN gas_cost_native END) as gas_spent_native_7_days,
         SUM(CASE WHEN block_time >= now() - INTERVAL '30' DAY THEN gas_cost_native END) as gas_spent_native_30_days,
         SUM(CASE WHEN block_time >= now() - INTERVAL '1' YEAR THEN gas_cost_native END) as gas_spent_native_1_year,
-        MAX(block_time) as last_block_time_for_total_metrics
+        MAX(block_time) as last_block_time_of_incremental_update
     FROM {{ source('gas', 'fees') }}
     GROUP BY 1, 2, 3
 )
 {% endif %}
 
 SELECT *
-FROM current_metrics
+FROM updated_metrics
 ORDER BY gas_spent_usd_total DESC
