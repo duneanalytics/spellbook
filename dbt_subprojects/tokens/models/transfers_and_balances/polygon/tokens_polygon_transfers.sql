a0b3271bfd8a4eeda54abe74966776858a50f41e
jeff-dude <jeff@dune.xyz>
2025-02-28 14:50:25 -0500

    for CI, make a view to run downstream faster

--- dbt_subprojects/tokens/models/transfers_and_balances/polygon/tokens_polygon_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/polygon/tokens_polygon_transfers.sql
@@ -1,16 +1,7 @@
 {{config(
     schema = 'tokens_polygon',
     alias = 'transfers',
-    partition_by = ['block_month'],
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    unique_key = ['block_date','unique_key'],
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
-    post_hook='{{ expose_spells(\'["polygon"]\',
-                                "sector",
-                                "tokens",
-                                \'["aalan3", "jeff-dude"]\') }}'
+    materialized = 'view',
 )
 }}
 
