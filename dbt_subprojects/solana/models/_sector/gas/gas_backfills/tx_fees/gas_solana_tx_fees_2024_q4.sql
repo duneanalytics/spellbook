7de0afa9daebd2aeeee2ba82d2325a16707b7d46
Alan Ghobadi <aalan3@gmail.com>
2025-03-03 14:42:28 +0100

    Update gas solana fees (#7775)
    * Partition by block_date
    
    * Add q4
    
    * Update current
    
    * Drop block_hour from unique key
    
    * Update view
    
    * Use merge into instead of delete+insert
    
    * Add missing comma

--- dbt_subprojects/solana/models/_sector/gas/gas_backfills/tx_fees/gas_solana_tx_fees_2024_q4.sql
+++ dbt_subprojects/solana/models/_sector/gas/gas_backfills/tx_fees/gas_solana_tx_fees_2024_q4.sql
@@ -1,6 +1,6 @@
 {{ config(
     schema = 'gas_solana',
-    alias = 'tx_fees_2021_q4',
+    alias = 'tx_fees_2024_q4',
     tags = ['static'],
     partition_by = ['block_date'],
     materialized = 'table',
@@ -8,6 +8,6 @@
 ) }}
 
 {{ solana_tx_fees_macro(
-    "cast('2021-10-01' as timestamp)",
-    "cast('2022-01-01' as timestamp)"
+    "cast('2024-10-01' as timestamp)",
+    "cast('2025-01-01' as timestamp)"
 ) }}
