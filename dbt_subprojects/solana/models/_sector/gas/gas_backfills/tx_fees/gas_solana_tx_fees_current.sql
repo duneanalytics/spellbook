f32e7def54244df8193b9a0e5e11b67b962d1bbf
Alan Ghobadi <aalan3@gmail.com>
2025-03-03 14:08:59 +0100

    Drop block_hour from unique key

--- dbt_subprojects/solana/models/_sector/gas/gas_backfills/tx_fees/gas_solana_tx_fees_current.sql
+++ dbt_subprojects/solana/models/_sector/gas/gas_backfills/tx_fees/gas_solana_tx_fees_current.sql
@@ -5,7 +5,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'delete+insert',
-    unique_key = ['block_date', 'block_hour', 'block_slot', 'tx_index']
+    unique_key = ['block_date', 'block_slot', 'tx_index']
 ) }}
 
 {{ solana_tx_fees_macro(
