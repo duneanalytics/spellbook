a6056ea2d54111f474dac2ba90b2949d8d6d4fa8
Alan Ghobadi <aalan3@gmail.com>
2025-03-03 14:26:36 +0100

    Use merge into instead of delete+insert

--- dbt_subprojects/solana/models/_sector/gas/gas_backfills/tx_fees/gas_solana_tx_fees_current.sql
+++ dbt_subprojects/solana/models/_sector/gas/gas_backfills/tx_fees/gas_solana_tx_fees_current.sql
@@ -4,7 +4,8 @@
     partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
-    incremental_strategy = 'delete+insert',
+    incremental_strategy = 'merge',
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
     unique_key = ['block_date', 'block_slot', 'tx_index']
 ) }}
 
