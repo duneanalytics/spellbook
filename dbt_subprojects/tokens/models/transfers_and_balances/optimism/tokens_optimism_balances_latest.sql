c1c38de435cca96c06e01b1f824e10e9927208d0
jeff-dude <jeff@dune.xyz>
2025-03-03 19:52:14 -0500

    fix unique key

--- dbt_subprojects/tokens/models/transfers_and_balances/optimism/tokens_optimism_balances_latest.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/optimism/tokens_optimism_balances_latest.sql
@@ -4,7 +4,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['unique_key', 'block_date_latest'],
+    unique_key = ['unique_key'],
     partition_by = ['block_date_latest'],
     post_hook='{{ expose_spells(\'["optimism"]\',
                                 "sector",
