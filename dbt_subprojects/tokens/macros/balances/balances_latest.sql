ea5c0efe3251de4bba8b7e5587cd24ea87b82041
jeff-dude <jeff@dune.xyz>
2025-03-03 19:53:20 -0500

    force CI to run short timeframe

--- dbt_subprojects/tokens/macros/balances/balances_latest.sql
+++ dbt_subprojects/tokens/macros/balances/balances_latest.sql
@@ -19,6 +19,8 @@ where
     1 = 1
     {% if is_incremental() %}
     and {{ incremental_predicate('block_time') }}
+    {% else %}
+    and block_time >= date '2025-02-01'
     {% endif %}
 group by
     blockchain,
