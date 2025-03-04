9a02354cf9dfa0a0fe5b7d88d5914f88a14f0ed6
Sohan <sohan.j12@gmail.com>
2025-03-04 11:41:12 +0530

    Revert "feat: add ZORDER optimization to gas usage by address model"
    This reverts commit 457ac7e051285c4e70435613694938d6317041d4.

--- dbt_subprojects/daily_spellbook/models/_sector/gas/prices/cross_chain/gas_usage_by_address.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/gas/prices/cross_chain/gas_usage_by_address.sql
@@ -5,13 +5,10 @@
     file_format='delta',
     partition_by = ['blockchain'],
     unique_key=['address', 'blockchain', 'currency_symbol'],
-    post_hook=[
-        '{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "fantom", "polygon", "base", "celo", "zora", "zksync", "scroll", "linea", "zkevm"]\',
+    post_hook='{{ expose_spells(\'["ethereum", "bnb", "avalanche_c", "gnosis", "optimism", "arbitrum", "fantom", "polygon", "base", "celo", "zora", "zksync", "scroll", "linea", "zkevm"]\',
                         "sector",
                         "gas",
-                        \'["gashawk, zohjag"]\') }}',
-                        'OPTIMIZE {{ this }} ZORDER BY (address);'
-                ]
+                        \'["gashawk, zohjag"]\') }}'
 ) }}
 
 WITH 
