fc781018cfd337644e629ed4141b8329d49d1562
Krish <113653936+beve0x@users.noreply.github.com>
2025-03-05 09:43:34 -0700

    Update dbt_subprojects/tokens/models/transfers_and_balances/bitcoin/tokens_bitcoin_net_transfers_daily_asset.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/tokens/models/transfers_and_balances/bitcoin/tokens_bitcoin_net_transfers_daily_asset.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/bitcoin/tokens_bitcoin_net_transfers_daily_asset.sql
@@ -2,10 +2,6 @@
         schema = 'tokens_bitcoin'
         , alias = 'net_transfers_daily_asset'
         , materialized = 'view'
-        , file_format = 'delta'
-        , incremental_strategy = 'merge'
-        , unique_key = ['blockchain', 'block_date', 'contract_address']
-        , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
 )
 }}
 
