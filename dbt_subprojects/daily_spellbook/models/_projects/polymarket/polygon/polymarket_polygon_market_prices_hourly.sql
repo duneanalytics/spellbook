eb4f791af9c433fe1752fa217cae2dcac5e917d1
Boxer <Floba.hs@gmail.com>
2025-03-04 17:13:25 -0700

    set prices to zero or one on market resolve

--- dbt_subprojects/daily_spellbook/models/_projects/polymarket/polygon/polymarket_polygon_market_prices_hourly.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/polymarket/polygon/polymarket_polygon_market_prices_hourly.sql
@@ -40,7 +40,28 @@ forward_fill AS (
     LEFT JOIN changed_prices lp
         ON h.hour >= lp.hour
         AND (lp.next_update_hour IS NULL OR h.hour < lp.next_update_hour)
+),
+
+price_correction AS (
+    SELECT
+        ff.hour,
+        ff.condition_id,
+        ff.token_id,
+        CASE 
+            WHEN ff.hour <= TRY_CAST(SUBSTRING(md.market_end_time FROM 1 FOR 19) AS timestamp) THEN ff.price
+            WHEN ff.hour > TRY_CAST(SUBSTRING(md.market_end_time FROM 1 FOR 19) AS timestamp) THEN
+                CASE 
+                    WHEN md.token_outcome = 'Yes' AND md.outcome = 'yes' THEN 1
+                    WHEN md.token_outcome = 'Yes' AND md.outcome = 'no' THEN 0
+                    WHEN md.token_outcome = 'No' AND md.outcome = 'yes' THEN 0
+                    WHEN md.token_outcome = 'No' AND md.outcome = 'no' THEN 1
+                    ELSE ff.price
+                END
+            ELSE ff.price
+        END AS price
+    FROM forward_fill ff
+    LEFT JOIN {{ ref('polymarket_polygon_market_details') }} md ON ff.token_id = md.token_id
 )
 
-SELECT * FROM forward_fill
+SELECT * FROM price_correction
 WHERE price > 0
