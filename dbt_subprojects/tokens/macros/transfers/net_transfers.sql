488f32558ee6d9c06c86bf8fc60a06b017d4fe1b
beve0x <gk0001va@gmail.com>
2025-03-05 17:30:11 -0700

    added net transfers per asset per address

--- dbt_subprojects/tokens/macros/transfers/net_transfers.sql
+++ dbt_subprojects/tokens/macros/transfers/net_transfers.sql
@@ -312,3 +312,113 @@ group by
     , block_date
     , address_owner
 {% endmacro %}
+
+{% ############################################################################################ %}
+
+{% macro evm_net_transfers_daily_asset_address(blockchain, native_contract_address) %}
+with raw_transfers as (
+    select
+        blockchain
+        , block_date
+        , COALESCE(contract_address, {{ native_contract_address }}) as contract_address
+        , symbol
+        , "from" as address
+        , 'sent' as transfer_direction
+        , (sum(amount_usd) * -1) as transfer_amount_usd
+        , count(*) transfer_count
+    from
+        {{ ref('tokens_'~blockchain~'_transfers') }}
+    where
+        blockchain = '{{blockchain}}'
+        {% if is_incremental() %}
+        and {{ incremental_predicate('block_date') }}
+        {% endif %}
+    group by
+        blockchain
+        , block_date
+        , COALESCE(contract_address, {{ native_contract_address }}) 
+        , symbol
+        , "from"
+        , 'sent'
+
+    union all
+
+    select
+        blockchain
+        , block_date
+        , COALESCE(contract_address, {{ native_contract_address }}) as contract_address
+        , symbol
+        , to as address
+        , 'received' as transfer_direction
+        , sum(amount_usd) as transfer_amount_usd
+        , count(*) transfer_count
+    from
+        {{ ref('tokens_'~blockchain~'_transfers') }}
+    where
+        blockchain = '{{blockchain}}'
+       {% if is_incremental() %}
+        and {{ incremental_predicate('block_date') }}
+        {% endif %}
+    group by
+        blockchain
+        , block_date
+        , COALESCE(contract_address, {{ native_contract_address }})
+        , symbol
+        , to
+        , 'received'
+),  transfers_amount as (
+    select
+        t.blockchain
+        , t.block_date
+        , t.contract_address
+        , t.symbol
+        , t.address
+        , sum(case when t.transfer_direction = 'sent' then t.transfer_amount_usd else 0 end) as transfer_amount_usd_sent
+        , sum(case when t.transfer_direction = 'received' then t.transfer_amount_usd else 0 end) as transfer_amount_usd_received
+        , sum(transfer_count) as transfer_count
+    from
+        raw_transfers as t
+    group by
+        t.blockchain
+        , t.block_date
+        , t.contract_address
+        , t.symbol
+        , t.address
+), net_transfers as (
+    select
+        blockchain
+        , block_date
+        , contract_address
+        , symbol
+        , address
+        , sum(coalesce(transfer_amount_usd_sent, 0)) as transfer_amount_usd_sent
+        , sum(coalesce(transfer_amount_usd_received, 0)) as transfer_amount_usd_received
+        , sum(coalesce(transfer_amount_usd_received, 0)) + sum(coalesce(transfer_amount_usd_sent, 0)) as net_transfer_amount_usd
+        , sum(transfer_count) transfer_count
+    from
+        transfers_amount
+    group by
+        blockchain
+        , block_date
+        , contract_address
+        , symbol
+        , address
+)
+select
+    blockchain
+    , block_date
+    , contract_address
+    , symbol
+    , address
+    , sum(net_transfer_amount_usd) as net_transfer_amount_usd
+from
+    net_transfers
+where
+    net_transfer_amount_usd > 0
+group by
+    blockchain
+    , block_date
+    , contract_address
+    , symbol
+    , address
+{% endmacro %}
---
+++ dbt_subprojects/tokens/models/transfers_and_balances/tokens_net_transfers_daily_asset_address.sql
@@ -0,0 +1,49 @@
+{{ config(
+        schema = 'tokens'
+        , alias = 'net_transfers_daily_asset_address'
+        , materialized = 'view'
+        )
+}}
+
+{% set chains = [
+     'arbitrum'
+    , 'avalanche_c'
+    , 'base'
+    , 'blast'
+    , 'bnb'
+    , 'celo'
+    , 'ethereum'
+    , 'fantom'
+    , 'gnosis'
+    , 'linea'
+    , 'mantle'
+    , 'optimism'
+    , 'polygon'
+    , 'ronin'
+    , 'scroll'
+    , 'sei'
+    , 'tron'
+    , 'zkevm'
+    , 'zksync'
+    , 'zora'
+   
+] %}
+
+SELECT *
+FROM (
+        {% for blockchain in chains %}
+        SELECT
+                
+                /* since solana labels are not used at this time, coalesce again in final view */
+                blockchain
+                , block_date
+                , contract_address
+                , symbol
+                , net_transfer_amount_usd
+                , address
+        FROM {{ ref('tokens_' + blockchain + '_net_transfers_daily_asset_address') }}
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+        {% endfor %}
+)
