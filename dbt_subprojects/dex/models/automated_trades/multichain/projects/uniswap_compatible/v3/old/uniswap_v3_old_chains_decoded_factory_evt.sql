0e0504b50eb414705a694bbda221db585d2755e9
Boxer <Floba.hs@gmail.com>
2025-03-11 17:41:38 +0100

    try this way

--- dbt_subprojects/dex/models/automated_trades/multichain/projects/uniswap_compatible/v3/old/uniswap_v3_old_chains_decoded_factory_evt.sql
+++ dbt_subprojects/dex/models/automated_trades/multichain/projects/uniswap_compatible/v3/old/uniswap_v3_old_chains_decoded_factory_evt.sql
@@ -24,62 +24,54 @@ with factory_events as (
                 logs = source(blockchain, 'logs')
             )}}
         )   
-        {% if not loop.last %}
-            union all
-        {% endif %}
-
-        {% if blockchain == 'optimism' %}
-            {% if loop.last %}
-                union all
-            {% endif %}
-            {% if not is_incremental() %}
-                
-                select 
-                    'optimism' as blockchain,
-                    token0
-                    , token1
-                    , oldaddress as pool
-                , 0x1F98431c8aD98523631AE4a59f267346ea31F984 as contract_address
-                , 0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118 as factory_topic0
-                , 'original uniswap v3 factory event' as factory_info
-                , null as block_time
-                , null as block_date
-                , null as block_month
-                , null as block_number
-                , null as tx_hash
-                , null as tx_from
-                , null as tx_to
-                , null as tx_index
-                , null as evt_index
-            from {{ ref('uniswap_optimism_ovm1_pool_mapping') }}
-
-            union all 
-
-            select
-                'optimism' as blockchain,
-                token0
-                , token1
-                , newaddress as pool
-                , 0x1F98431c8aD98523631AE4a59f267346ea31F984 as contract_address
-                , 0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118 as factory_topic0
-                , 'original uniswap v3 factory event' as factory_info
-                , null as block_time
-                , null as block_date
-                , null as block_month
-                , null as block_number
-                , null as tx_hash
-                , null as tx_from
-                , null as tx_to
-                , null as tx_index
-                , null as evt_index
-            from {{ ref('uniswap_optimism_ovm1_pool_mapping') }}
-            {% endif %}
-            
-        {% endif %}
-        {% if not loop.last %}
+        {% if not loop.last or (not is_incremental() and 'optimism' in blockchains) %}
             union all
         {% endif %}
     {% endfor %}
+    
+    {% if not is_incremental() and 'optimism' in blockchains %}
+        -- Old addresses for optimism
+        select 
+            'optimism' as blockchain,
+            token0
+            , token1
+            , oldaddress as pool
+            , 0x1F98431c8aD98523631AE4a59f267346ea31F984 as contract_address
+            , 0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118 as factory_topic0
+            , 'original uniswap v3 factory event' as factory_info
+            , null as block_time
+            , null as block_date
+            , null as block_month
+            , null as block_number
+            , null as tx_hash
+            , null as tx_from
+            , null as tx_to
+            , null as tx_index
+            , null as evt_index
+        from {{ ref('uniswap_optimism_ovm1_pool_mapping') }}
+        
+        union all
+        
+        -- New addresses for optimism
+        select
+            'optimism' as blockchain,
+            token0
+            , token1
+            , newaddress as pool
+            , 0x1F98431c8aD98523631AE4a59f267346ea31F984 as contract_address
+            , 0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118 as factory_topic0
+            , 'original uniswap v3 factory event' as factory_info
+            , null as block_time
+            , null as block_date
+            , null as block_month
+            , null as block_number
+            , null as tx_hash
+            , null as tx_from
+            , null as tx_to
+            , null as tx_index
+            , null as evt_index
+        from {{ ref('uniswap_optimism_ovm1_pool_mapping') }}
+    {% endif %}
 )
 
 select * from factory_events
