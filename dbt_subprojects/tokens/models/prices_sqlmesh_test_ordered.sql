b5a3ca1595d9c175cced2ed08b37c4375a542e66
0xRob <83790096+0xRobin@users.noreply.github.com>
2025-03-11 14:18:00 +0100

    add ordered models

---
+++ dbt_subprojects/tokens/models/prices_sqlmesh_test_ordered.sql
@@ -0,0 +1,13 @@
+{{ config(
+        schema='prices_test',
+        alias = 'sqlmesh_copy',
+        materialized='table',
+        file_format = 'delta',
+        partition_by = ['year','month','blockchain'],
+        tags = ['static']
+        )
+}}
+
+select * 
+from "hive"."sqlmesh__prices_v2"."prices_v2__day_clickhouse_replica__2289764117__dev"
+order by blockchain, contract_address, timestamp
