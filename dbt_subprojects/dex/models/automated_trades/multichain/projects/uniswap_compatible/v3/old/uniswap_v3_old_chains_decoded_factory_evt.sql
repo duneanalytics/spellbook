1a24acc5327cf20d896ff7f2555fdd0a4a20430f
Boxer <Floba.hs@gmail.com>
2025-03-11 13:39:00 +0100

    handle upstream duplication issue

--- dbt_subprojects/dex/models/automated_trades/multichain/projects/uniswap_compatible/v3/old/uniswap_v3_old_chains_decoded_factory_evt.sql
+++ dbt_subprojects/dex/models/automated_trades/multichain/projects/uniswap_compatible/v3/old/uniswap_v3_old_chains_decoded_factory_evt.sql
@@ -27,50 +27,55 @@ with factory_events as (
         {% if not loop.last %}
             union all
         {% endif %}
-
-        {% if blockchain == 'optimism' %}
-            union all
-            select 
-                'optimism' as blockchain,
-                token0
-                , token1
-                , oldaddress as pool
-                , 0x1F98431c8aD98523631AE4a59f267346ea31F984 as contract_address
-                , 0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118 as factory_topic0
-                , 'original uniswap v3 factory event' as factory_info
-                , null as block_time
-                , null as block_date
-                , null as block_month
-                , null as block_number
-                , null as tx_hash
-                , null as tx_from
-                , null as tx_to
-                , null as tx_index
-                , null as evt_index
-            from {{ ref('uniswap_optimism_ovm1_pool_mapping') }}
-
-            union all 
-
-            select
-                'optimism' as blockchain,
-                token0
-                , token1
-                , newaddress as pool
-                , 0x1F98431c8aD98523631AE4a59f267346ea31F984 as contract_address
-                , 0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118 as factory_topic0
-                , 'original uniswap v3 factory event' as factory_info
-                , null as block_time
-                , null as block_date
-                , null as block_month
-                , null as block_number
-                , null as tx_hash
-                , null as tx_from
-                , null as tx_to
-                , null as tx_index
-                , null as evt_index
-            from {{ ref('uniswap_optimism_ovm1_pool_mapping') }}
-        {% endif %}
     {% endfor %}
+    
+    {% if 'optimism' in blockchains %}
+        union all
+        -- Handle old_address pools
+        select 
+            'optimism' as blockchain,
+            token0
+            , token1
+            , oldaddress as pool
+            , 0x1F98431c8aD98523631AE4a59f267346ea31F984 as contract_address
+            , 0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118 as factory_topic0
+            , 'original uniswap v3 factory event (oldaddress)' as factory_info
+            , null as block_time
+            , null as block_date
+            , null as block_month
+            , null as block_number
+            , null as tx_hash
+            , null as tx_from
+            , null as tx_to
+            , null as tx_index
+            , null as evt_index
+        from {{ ref('uniswap_optimism_ovm1_pool_mapping') }}
+        
+        union all
+        
+        -- Handle new_address pools
+        select 
+            'optimism' as blockchain,
+            token0
+            , token1
+            , newaddress as pool
+            , 0x1F98431c8aD98523631AE4a59f267346ea31F984 as contract_address
+            , 0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118 as factory_topic0
+            , 'original uniswap v3 factory event (newaddress)' as factory_info
+            , null as block_time
+            , null as block_date
+            , null as block_month
+            , null as block_number
+            , null as tx_hash
+            , null as tx_from
+            , null as tx_to
+            , null as tx_index
+            , null as evt_index
+        from {{ ref('uniswap_optimism_ovm1_pool_mapping') }}
+        
+        -- We need entries for both oldaddress and newaddress in the factory events
+        -- This ensures all pools are properly tracked in the system
+    {% endif %}
 )
 
 select * from factory_events
