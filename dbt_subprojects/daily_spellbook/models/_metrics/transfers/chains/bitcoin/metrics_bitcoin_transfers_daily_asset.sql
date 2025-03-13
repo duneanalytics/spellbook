47eae1b78ef9a1fe8f2b5eb15a5a74a6e607d163
beve0x <gk0001va@gmail.com>
2025-03-13 16:22:21 -0700

    asset files datatype change for btc and solana

--- dbt_subprojects/daily_spellbook/models/_metrics/transfers/chains/bitcoin/metrics_bitcoin_transfers_daily_asset.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/transfers/chains/bitcoin/metrics_bitcoin_transfers_daily_asset.sql
@@ -5,5 +5,13 @@
         )
 }}
 
-SELECT *
+SELECT 
+    blockchain
+    , block_date
+    , case
+        when substring(contract_address, 1, 3) = 'bc1' then cast(contract_address as varbinary) --we don't have bech32() function for this address type
+        else from_base58(contract_address) --all other address types *should* be fine to use base58
+      end as contract_address
+    , symbol
+    , net_transfer_amount_usd
 FROM {{ source('tokens_bitcoin', 'net_transfers_daily_asset') }}
