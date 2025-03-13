420e9b34a4d017d36749f7ba7796206cfadc3b31
Krish <113653936+beve0x@users.noreply.github.com>
2025-03-13 15:22:20 -0700

    EVM transfer metrics (#7837)
    * arbitrum metrics
    
    * added other chains
    
    * removed ronin extra
    
    * removed metrics transfers macro
    
    * dummy commit of metrics_dune_index_daily
    
    * dummy edit again to the metrics index file
    
    * removed comment from metrics index
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

---
+++ dbt_subprojects/daily_spellbook/models/_metrics/transfers/chains/evm/polygon/metrics_polygon_transfers_daily_asset.sql
@@ -0,0 +1,11 @@
+{% set blockchain = 'polygon' %}
+
+{{ config(
+        schema = 'metrics_' + blockchain
+        , alias = 'transfers_daily_asset'
+        , materialized = 'view'
+        )
+}}
+
+SELECT *
+FROM {{ source('tokens_polygon', 'net_transfers_daily_asset') }}
