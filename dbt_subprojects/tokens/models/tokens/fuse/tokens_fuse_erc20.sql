707bdbbe9b881944fe0eaff31654fa66bff55b79
Voronor <129545215+voronor@users.noreply.github.com>
2025-03-14 09:18:40 +0100

    feat(tokens): Added tokens to Fuse

--- dbt_subprojects/tokens/models/tokens/fuse/tokens_fuse_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/fuse/tokens_fuse_erc20.sql
@@ -14,5 +14,7 @@ SELECT
 FROM (VALUES
     (0x34ef2cc892a88415e9f02b91bfa9c91fc0be6bd4, 'VOLT', 18),
     (0x0be9e53fd7edac9f859882afdda116645287c629, 'WFUSE', 18),
-    (0xb1dd0b683d9a56525cc096fbf5eec6e60fe79871, 'sFUSE', 18)    
+    (0xb1dd0b683d9a56525cc096fbf5eec6e60fe79871, 'sFUSE', 18),
+    (0xeeeeeb57642040be42185f49c52f7e9b38f8eeee, 'ELK', 18),
+    (0x5dd8015cec49f4db01fd228f688bf30337d3e0a9, 'ELON', 18)
 ) AS temp_table (contract_address, symbol, decimals)
