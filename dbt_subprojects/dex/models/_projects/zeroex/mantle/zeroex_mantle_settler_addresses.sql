37ba7144e9c1754a44efb4f6bea520558f36c39f
RantumBits <rantum@rantum.xyz>
2025-03-21 10:26:51 -0700

    settler address config updates

--- dbt_subprojects/dex/models/_projects/zeroex/mantle/zeroex_mantle_settler_addresses.sql
+++ dbt_subprojects/dex/models/_projects/zeroex/mantle/zeroex_mantle_settler_addresses.sql
@@ -3,13 +3,10 @@
         schema = 'zeroex_mantle',
         alias = 'settler_addresses',
         materialized='incremental',
-        partition_by = ['begin_block_time'],
-        unique_key = ['token_id', 'settler_address', 'begin_block_number', 'begin_block_time'],
+        unique_key = ['settler_address'],
         on_schema_change='sync_all_columns',
         file_format ='delta',
-        incremental_strategy='merge',
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.begin_block_time')]
-    )
+        incremental_strategy='merge'    )
 }}
 
 -- Use the zeroex_settler_addresses macro to generate the settler addresses for mantle
