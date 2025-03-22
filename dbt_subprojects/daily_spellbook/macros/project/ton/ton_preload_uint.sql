52f399ee5c601b90d33bba8eb1c507a2f06b2984
Pavel Shuvalov <pavel.s@ton.org>
2025-03-22 16:29:46 +0400

    fix for load_uint with current offset divisible by 8

--- dbt_subprojects/daily_spellbook/macros/project/ton/ton_preload_uint.sql
+++ dbt_subprojects/daily_spellbook/macros/project/ton/ton_preload_uint.sql
@@ -9,7 +9,7 @@ bitwise_and(
                 {{ state }}.current_cell_data,
                 1 + cast(floor({{ state }}.cursor_bit_offset / 8e0) as bigint),
                 cast(ceil({{ size }} / 8e0) as bigint) +
-                IF(cast(floor({{ state }}.cursor_bit_offset / 8e0) as bigint) = cast(floor(({{ state }}.cursor_bit_offset + {{ size }} - 1) / 8e0) as bigint), 0, 1) {# check that end of the slice in the next octet#}
+                IF(({{ state }}.cursor_bit_offset) % 8 = 0 OR cast(floor({{ state }}.cursor_bit_offset / 8e0) as bigint) = cast(floor(({{ state }}.cursor_bit_offset + {{ size }} - 1) / 8e0) as bigint), 0, 1) {# check that end of the slice in the next octet#}
             )
         ),
         IF(({{ size }} + {{ state }}.cursor_bit_offset) % 8 = 0, 0, 8 - ({{ size }} + {{ state}}.cursor_bit_offset) % 8)),
