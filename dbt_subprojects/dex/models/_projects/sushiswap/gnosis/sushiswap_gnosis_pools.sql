8a3465e041c8dd7509ed3125e23cd5c3f52e2adc
mlaegn <maximilian.laechelin@gnosis.io>
2025-03-25 16:22:40 +0000

    corrected table

--- dbt_subprojects/dex/models/_projects/sushiswap/gnosis/sushiswap_gnosis_pools.sql
+++ dbt_subprojects/dex/models/_projects/sushiswap/gnosis/sushiswap_gnosis_pools.sql
@@ -22,7 +22,7 @@ SELECT
     evt_block_time AS creation_block_time,
     evt_block_number AS creation_block_number,
     contract_address
-FROM {{ source('sushiswap_v2_gnosis', 'UniswapV2Factory_evt_PairCreated') }}
+FROM {{ source('sushiswap_gnosis', 'UniswapV2Factory_evt_PairCreated') }}
 {% if is_incremental() %}
 WHERE evt_block_time >= date_trunc('day', now() - interval '7' day)
 {% endif %}
