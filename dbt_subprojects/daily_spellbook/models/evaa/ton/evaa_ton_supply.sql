284fb9a705d740fdcbae6c1bca1aa5ce3c2e8933
Pavel Shuvalov <pavel.s@ton.org>
2025-03-25 07:01:02 +0400

    Add block_date to unique_key

--- dbt_subprojects/daily_spellbook/models/evaa/ton/evaa_ton_supply.sql
+++ dbt_subprojects/daily_spellbook/models/evaa/ton/evaa_ton_supply.sql
@@ -5,7 +5,7 @@
        , file_format = 'delta'
        , incremental_strategy = 'merge'
        , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
-       , unique_key = ['tx_hash']
+       , unique_key = ['tx_hash', 'block_date']
        , post_hook='{{ expose_spells(\'["ton"]\',
                                    "project",
                                    "evaa",
