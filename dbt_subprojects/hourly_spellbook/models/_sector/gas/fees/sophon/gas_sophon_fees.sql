f8563131a13bd2593147536d2f387d3380af317d
Huang Geyang <Sukebeta@outlook.com>
2025-03-26 00:54:35 +0800

    Fix sql

--- dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/sophon/gas_sophon_fees.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/sophon/gas_sophon_fees.sql
@@ -24,7 +24,7 @@ WITH base_model as (
         ,cast(gas_price as uint256) as gas_price
         ,txns.gas_used as gas_used
         ,cast(gas_price as uint256) * cast(txns.gas_used as uint256) as tx_fee_raw
-        ,case when txns.priority_fee_per_gas is null or base_fee_per_gas = txns.gas_used
+        ,case when txns.priority_fee_per_gas is null or base_fee_per_gas = txns.gas_used or txns.priority_fee_per_gas < 0
             then map(array['base_fee'], array[cast(gas_price as uint256) * cast(txns.gas_used as uint256)])
             else map(
                 array['base_fee', 'priority_fee'],
