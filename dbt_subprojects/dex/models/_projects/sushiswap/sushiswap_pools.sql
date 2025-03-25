6d5da6dc8b57dd1b0baa4b2a01fe4a0131d65e6a
mlaegn <maximilian.laechelin@gnosis.io>
2025-03-25 16:14:56 +0000

    add sushiswap models for gnosis and update dex pools aggregation

---
+++ dbt_subprojects/dex/models/_projects/sushiswap/sushiswap_pools.sql
@@ -0,0 +1,34 @@
+{{ config(
+    schema = 'sushiswap',
+    alias = 'pools',
+    post_hook='{{ expose_spells(\'["gnosis"]\',
+                                "project",
+                                "sushiswap",
+                                \'["mlaegn"]\') }}'
+) }}
+
+{% set sushiswap_models = [
+  ref('sushiswap_gnosis_pools')
+] %}
+
+SELECT *
+FROM (
+    {% for pool_model in sushiswap_models %}
+      SELECT
+          blockchain,
+          project,
+          version,
+          pool,
+          fee,
+          token0,
+          token1,
+          creation_block_time,
+          creation_block_number,
+          contract_address
+      FROM {{ pool_model }}
+      {% if not loop.last %}
+         UNION ALL
+      {% endif %}
+    {% endfor %}
+)
+;
