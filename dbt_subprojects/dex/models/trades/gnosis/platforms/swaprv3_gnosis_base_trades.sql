955914023241442a2559bd1e5b56a1485143ec03
mlaegn <maximilian.laechelin@gnosis.io>
2025-03-31 17:01:31 +0100

    added swaprv3 do dex trades and added source

---
+++ dbt_subprojects/dex/models/trades/gnosis/platforms/swaprv3_gnosis_base_trades.sql
@@ -0,0 +1,19 @@
+{{ config(
+    schema = 'swaprv3_gnosis',
+    alias = 'base_trades',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['tx_hash', 'evt_index'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+) }}
+
+{{
+    uniswap_compatible_v3_trades(
+        blockchain = 'gnosis',
+        project = 'swaprv3',
+        version = '3',
+        Pair_evt_Swap = source('swaprv3_gnosis', 'AlgebraPool_evt_Swap'),
+        Factory_evt_PoolCreated = source('swaprv3_gnosis', 'SwaprV3Factory_evt_Pool')
+    )
+}}
