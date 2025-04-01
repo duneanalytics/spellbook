7f52dbc341dc9f8e74573799e285e12ac5e40bd8
0xRob <83790096+0xRobin@users.noreply.github.com>
2025-04-01 10:51:16 +0200

    git add

---
+++ dbt_subprojects/tokens/tests/generic/test_prices_tokens_against_erc20.sql
@@ -0,0 +1,21 @@
+-- this tests checks a prices.tokens model against a tokens.erc20 model.
+-- making sure we have to correct symbol and decimals for each token.
+{% test check_prices_tokens_against_erc20(model) %}
+With comparison as (
+
+select 
+  blockchain
+  ,contract_address
+  ,t.symbol as prices_symbol
+  ,erc20.symbol as erc20_symbol
+  ,(t.symbol = erc20.symbol) as equal_symbol 
+  ,t.decimals as prices_decimals
+  ,erc20.decimals as erc20_decimals
+  ,(t.decimals = erc20.decimals) as equal_decimals
+from {{ model }} t
+inner join {{ref('tokens','erc20')}} erc20 using (blockchain, contract_address)
+)
+
+select * from comparison
+where not equal_symbol or not equal_decimals
+{% endtest %}
