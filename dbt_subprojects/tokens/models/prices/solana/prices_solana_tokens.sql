c5d25562f162a25e4c5cb26424d595be7fd44020
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-04-01 16:09:20 +0300

    Update prices_solana_tokens.sql

--- dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
+++ dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
@@ -736,5 +736,6 @@ FROM
         ('pyusd-paypal-usd', 'solana', 'PyUSD', '2b1kV6DkPAnxd5ixfnxCpjxmKwqjjaYmCZfHsFu24GXo', 6),
         ('prcl-parcl', 'solana', 'PRCL', '4LLbsb5ReP3yEtYzmXewyGjcir5uXtKFURtaEUVC2AHs', 6),
         ('layer-solayer', 'solana', 'LAYER', 'LAYER4xPpTCb3QL8S9u41EAhAX7mhBn8Q6xMTwY2Yzc', 9),
-        ('drift-drift-protocol', 'solana', 'DRIFT', 'DriFtupJYLTosbwoN8koMbEYSx54aFAVLddWsbksjwg7', 6)
+        ('drift-drift-protocol', 'solana', 'DRIFT', 'DriFtupJYLTosbwoN8koMbEYSx54aFAVLddWsbksjwg7', 6),
+        ('tdccp-tdccp','solana','TDCCP','Hg8bKz4mvs8KNj9zew1cEF9tDw1x2GViB4RFZjVEmfrD',9)      
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
