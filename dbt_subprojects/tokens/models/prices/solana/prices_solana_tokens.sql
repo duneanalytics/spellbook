595f8ba41d8fdca0733d58484920e61fd3a5ec1e
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-04-01 19:35:50 +0300

    Added new token in tokens/models/prices/solana/prices_solana_tokens.sql (#7958)
    * Update prices_solana_tokens.sql
    
    * Update prices_solana_tokens.sql
    
    * Update prices_solana_tokens.sql
    
    * Update PAXG token decimals to 8
    
    ---------
    
    Co-authored-by: 0xRob <83790096+0xRobin@users.noreply.github.com>

--- dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
+++ dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
@@ -736,5 +736,6 @@ FROM
         ('pyusd-paypal-usd', 'solana', 'PyUSD', '2b1kV6DkPAnxd5ixfnxCpjxmKwqjjaYmCZfHsFu24GXo', 6),
         ('prcl-parcl', 'solana', 'PRCL', '4LLbsb5ReP3yEtYzmXewyGjcir5uXtKFURtaEUVC2AHs', 6),
         ('layer-solayer', 'solana', 'LAYER', 'LAYER4xPpTCb3QL8S9u41EAhAX7mhBn8Q6xMTwY2Yzc', 9),
-        ('drift-drift-protocol', 'solana', 'DRIFT', 'DriFtupJYLTosbwoN8koMbEYSx54aFAVLddWsbksjwg7', 6)
+        ('drift-drift-protocol', 'solana', 'DRIFT', 'DriFtupJYLTosbwoN8koMbEYSx54aFAVLddWsbksjwg7', 6),
+        ('paxg-pax-gold','solana','PAXG','4V68SgViyBsEv37ridbG7pxzd1V78fVMHGaCHa2HGZMp',8)      
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
