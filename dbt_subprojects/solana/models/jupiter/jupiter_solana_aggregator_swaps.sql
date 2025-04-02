1bbd609daf642c72400276fc31e7261fecc69d23
beve0x <gk0001va@gmail.com>
2025-04-02 10:13:56 -0600

    applied 4byte fix for 1DEX amm only

--- dbt_subprojects/solana/models/jupiter/jupiter_solana_aggregator_swaps.sql
+++ dbt_subprojects/solana/models/jupiter/jupiter_solana_aggregator_swaps.sql
@@ -133,7 +133,16 @@ with
             a.amm_name
             , toBase58(bytearray_substring(data,1+16,32)) as amm
             , toBase58(bytearray_substring(data,1+48,32)) as input_mint
-            , bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+80,4))) as input_amount
+            , CASE 
+                -- Apply the 4-byte fix for 1DEX AMM after August 23rd, 2024
+                WHEN a.amm = 'DEXYosS6oEGvk8uCDayvwEZz4qEyDJRf9nFgYCaqPMTm' 
+                    AND l.block_time >= TIMESTAMP '2024-08-23 00:00:00' 
+                    THEN 
+                    bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+80,4)))
+                -- Keep the original 8-byte parsing for all other AMMs
+                ELSE 
+                    bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+80,8)))
+            END as input_amount
             , toBase58(bytearray_substring(data,1+88,32)) as output_mint
             , bytearray_to_bigint(bytearray_reverse(bytearray_substring(data,1+120,8))) as output_amount
             , log_index
