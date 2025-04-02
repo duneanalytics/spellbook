c4b5b4661132098034094b6d4509d53cd8148895
Alan Ghobadi <aalan3@gmail.com>
2025-04-02 11:26:04 +0200

    Add worldchain daily balances

---
+++ dbt_subprojects/tokens/models/transfers_and_balances/worldchain/tokens_base_balances_daily_agg.sql
@@ -0,0 +1,13 @@
+{{ config(
+        schema = 'tokens_worldchain',
+        alias = 'balances_daily_agg_worldchain',
+        materialized = 'view'
+        )
+}}
+
+{{
+    balances_enrich(
+        balances_raw = ref('tokens_worldchain_balances_daily_agg_worldchain'),
+        daily=true,
+    )
+}}
