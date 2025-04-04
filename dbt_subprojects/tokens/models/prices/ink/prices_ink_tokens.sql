e6480adf37c313905af1abdae2a2af7205656065
Hopium <135053852+Hopium21@users.noreply.github.com>
2025-04-04 10:20:25 +0200

    Update prices_ink_tokens.sql

--- dbt_subprojects/tokens/models/prices/ink/prices_ink_tokens.sql
+++ dbt_subprojects/tokens/models/prices/ink/prices_ink_tokens.sql
@@ -23,4 +23,5 @@ FROM
     , ('krill-krill', 'KRILL', 0xcb95a3840c8ea5f0d4e78b67ec897df84d17c5e6, 18)
     , ('purple-purple-coin', 'PURPLE', 0xd642b49d10cc6e1bc1c6945725667c35e0875f22, 18)
     , ('usdt-tether', 'USD₮0', 0x0200C29006150606B650577BBE7B6248F58470c1, 6)
+    , ('gminky-gminky', 'GMINKY', 0x44c497597aa32f5F2B7eDB91522f321F04f03cD7, 18)
 ) as temp (token_id, symbol, contract_address, decimals)
