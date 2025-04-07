89c79dcab4e744c7b7fee0daf0185018f186f403
hildobby <h.moulie@gmail.com>
2025-04-07 20:38:46 +0200

    add ink to evms.{} tables (#7977)
    * tag morph blobs
    
    * add cex addresses
    
    * evms.[] add ink

--- dbt_subprojects/daily_spellbook/models/evms/evms_erc721_transfers.sql
+++ dbt_subprojects/daily_spellbook/models/evms/evms_erc721_transfers.sql
@@ -2,7 +2,7 @@
         schema='evms',
         alias = 'erc721_transfers',
         unique_key=['blockchain', 'tx_hash', 'evt_index'],
-        post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo", "base", "zksync", "zora", "scroll", "linea", "zkevm", "blast", "mantle", "ronin"]\',
+        post_hook='{{ expose_spells(\'["ethereum", "polygon", "bnb", "avalanche_c", "gnosis", "fantom", "optimism", "arbitrum", "celo", "base", "zksync", "zora", "scroll", "linea", "zkevm", "blast", "mantle", "ronin", "ink"]\',
                                     "sector",
                                     "evms",
                                     \'["hildobby"]\') }}'
@@ -29,6 +29,7 @@
      , ('mantle', source('erc721_mantle', 'evt_transfer'))
      , ('sei', source('erc721_sei', 'evt_transfer'))
      , ('ronin', source('erc721_ronin', 'evt_transfer'))
+     , ('ink', source('erc721_ink', 'evt_transfer'))
 ] %}
 
 SELECT *
