cf46d44ee43f302bd6def2732aa6aa9ae9b999ce
beve0x <gk0001va@gmail.com>
2025-04-07 13:42:06 -0600

    fixed double encoding

--- dbt_subprojects/daily_spellbook/models/_metrics/transfers/chains/ton/metrics_ton_transfers_daily_asset.sql
+++ dbt_subprojects/daily_spellbook/models/_metrics/transfers/chains/ton/metrics_ton_transfers_daily_asset.sql
@@ -8,7 +8,7 @@
 SELECT 
   blockchain,
   block_date,
-  CAST(contract_address AS varbinary) as contract_address,
+  contract_address,
   symbol,
   net_transfer_amount_usd
 FROM {{ ref('tokens_ton_net_transfers_daily_asset') }}
