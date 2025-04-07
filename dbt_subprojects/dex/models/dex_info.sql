d59ae0152149b9544923110c01448c8296513dfc
sunshinejulia9 <135378933+sunshinejulia9@users.noreply.github.com>
2025-04-08 01:43:17 +0700

    Add squidswap to dex.trades (#7960)
    * Update _schema.yml
    
    * Update dex_ink_base_trades.sql
    
    * Create squidswap_ink_base_trades.sql
    
    * Update _sources.yml
    
    * Update _schema.yml
    
    * Create squidswap_ink_base_trades_seed.csv
    
    * Update dex_info.sql
    
    * Update _sources.yml
    
    * Update squidswap_ink_base_trades_seed.csv
    
    * Update squidswap_ink_base_trades_seed.csv
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/dex/models/dex_info.sql
+++ dbt_subprojects/dex/models/dex_info.sql
@@ -211,5 +211,6 @@ FROM (VALUES
     , ('burrbear', 'BurrBear', 'Direct', 'moneygoesburr')
     , ('reservoir_swap', 'Reservoir Swap', 'Direct', 'reservoir0x')
     , ('inkyswap', 'InkySwap', 'Direct', 'inkyswap')
+    , ('squidswap', 'SquidSwap', 'Direct', 'squidswap_ink')
     , ('swanswap', 'SwanSwap', 'Direct', 'swan_swap')
 ) AS temp_table (project, name, marketplace_type, x_username)
