c5c1cd0a669c1f8c992368d44c562c1625ce0038
Boxer <Floba.hs@gmail.com>
2025-04-08 13:38:18 +0200

    fix naming schema

--- dbt_subprojects/dex/models/_projects/curvefi/sonic/curve_sonic_pools.sql
+++ dbt_subprojects/dex/models/_projects/curvefi/sonic/curve_sonic_pools.sql
@@ -31,8 +31,8 @@ WITH plain_pools AS (
         CAST(NULL as varbinary) AS undercoin2,
         CAST(NULL as varbinary) AS undercoin3
     FROM
-        {{ source('curvefi_sonic', 'StableswapFactory_call_deploy_plain_pool') }} as p
-        LEFT JOIN {{ source('curvefi_sonic', 'StableswapFactory_evt_PlainPoolDeployed') }} dp
+        {{ source('curvefi_sonic', 'curvestableswapfactory_call_deploy_plain_pool') }} as p
+        LEFT JOIN {{ source('curvefi_sonic', 'curvestableswapfactory_evt_PlainPoolDeployed') }} dp
         ON p.call_block_time = dp.evt_block_time
         AND p.call_tx_hash = dp.evt_tx_hash
         AND p._coins = dp.coins
@@ -59,8 +59,8 @@ meta_pools AS (
         r.coin1 as undercoin2,
         r.coin2 as undercoin3
     FROM
-        {{ source('curvefi_sonic', 'StableswapFactory_call_deploy_metapool') }} mc
-        LEFT JOIN {{ source('curvefi_sonic', 'StableswapFactory_evt_MetaPoolDeployed') }} mp
+        {{ source('curvefi_sonic', 'curvestableswapfactory_call_deploy_metapool') }} mc
+        LEFT JOIN {{ source('curvefi_sonic', 'curvestableswapfactory_evt_MetaPoolDeployed') }} mp
         ON mc.call_block_time = mp.evt_block_time
         AND mc.call_tx_hash = mp.evt_tx_hash
         LEFT JOIN plain_pools r ON r.pool_address = mc._base_pool
