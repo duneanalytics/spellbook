1f65be9013b3fc5c0f0395d93fb6cc40425da1dc
Boxer <Floba.hs@gmail.com>
2025-04-08 17:41:24 +0100

    fix downstream

--- dbt_subprojects/solana/models/solana_utils/alt_solana_utils_token_accounts_updates.sql
+++ dbt_subprojects/solana/models/solana_utils/alt_solana_utils_token_accounts_updates.sql
@@ -19,23 +19,23 @@ WITH combined_events AS (
     MAX(CASE WHEN account_mint IS NOT NULL THEN account_mint END)
       OVER (
         PARTITION BY token_account
-        ORDER BY event_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC
+        ORDER BY block_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC
         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
       ) AS last_non_null_account_mint,
 
     -- Capture start time, slot, and indices
-    event_time AS valid_from,
+    block_time AS valid_from,
     block_slot as valid_from_block_slot,
     tx_index as valid_from_tx_index,
     outer_instruction_index as valid_from_outer_index,
     inner_instruction_index as valid_from_inner_index,
 
     -- Calculate end time, slot, and indices using granular LEAD
-    LEAD(event_time, 1) OVER (PARTITION BY token_account ORDER BY event_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to,
-    LEAD(block_slot, 1) OVER (PARTITION BY token_account ORDER BY event_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to_block_slot,
-    LEAD(tx_index, 1) OVER (PARTITION BY token_account ORDER BY event_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to_tx_index,
-    LEAD(outer_instruction_index, 1) OVER (PARTITION BY token_account ORDER BY event_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to_outer_index,
-    LEAD(inner_instruction_index, 1) OVER (PARTITION BY token_account ORDER BY event_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to_inner_index,
+    LEAD(block_time, 1) OVER (PARTITION BY token_account ORDER BY block_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to,
+    LEAD(block_slot, 1) OVER (PARTITION BY token_account ORDER BY block_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to_block_slot,
+    LEAD(tx_index, 1) OVER (PARTITION BY token_account ORDER BY block_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to_tx_index,
+    LEAD(outer_instruction_index, 1) OVER (PARTITION BY token_account ORDER BY block_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to_outer_index,
+    LEAD(inner_instruction_index, 1) OVER (PARTITION BY token_account ORDER BY block_time, block_slot, tx_index, outer_instruction_index, inner_instruction_index ASC) AS valid_to_inner_index,
     
     block_month,
     event_type,
