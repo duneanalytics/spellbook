73002b61dc21ed6bd972f9531c8a3b910fada950
AIDataMaster <cryptouserv1@gmail.com>
2025-04-09 15:08:54 +0300

    added 2 cols to order_created

--- dbt_subprojects/hourly_spellbook/models/_project/gmx/event/arbitrum/gmx_v2_arbitrum_order_created.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/gmx/event/arbitrum/gmx_v2_arbitrum_order_created.sql
@@ -49,13 +49,35 @@ WITH evt_data_1 AS (
     {% endif %}
 )
 
--- unite 2 tables
+, evt_data_3 AS (
+    SELECT 
+        -- Main Variables
+        '{{ blockchain_name }}' AS blockchain,
+        evt_block_time AS block_time,
+        evt_block_number AS block_number, 
+        evt_tx_hash AS tx_hash,
+        evt_index AS index,
+        contract_address,
+        eventName AS event_name,
+        eventData AS data,
+        msgSender AS msg_sender
+    FROM {{ source('gmx_v2_arbitrum','EventEmitter_evt_EventLog')}}
+    WHERE eventName = '{{ event_name }}'
+    {% if is_incremental() %}
+        AND {{ incremental_predicate('evt_block_time') }}
+    {% endif %}
+)
+
+-- unite 3 tables
 , evt_data AS (
     SELECT * 
     FROM evt_data_1
     UNION DISTINCT
     SELECT *
     FROM evt_data_2
+    UNION 
+    SELECT *
+    FROM evt_data_3
 )
 
 , parsed_data AS (
@@ -135,6 +157,19 @@ WITH evt_data_1 AS (
         ) AS t(item)
 )
 
+, bytes32_array_items_parsed AS (
+    SELECT 
+        tx_hash,
+        index,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.key') AS key_name,
+        json_format(json_extract(CAST(item AS VARCHAR), '$.value')) AS value
+    FROM 
+        parsed_data,
+        UNNEST(
+            CAST(json_extract(bytes32_items, '$.arrayItems') AS ARRAY(JSON))
+        ) AS t(item)
+)
+
 , combined AS (
     SELECT *
     FROM address_items_parsed
@@ -150,7 +185,9 @@ WITH evt_data_1 AS (
     UNION ALL
     SELECT *
     FROM bytes32_items_parsed
-
+    UNION ALL
+    SELECT *
+    FROM bytes32_array_items_parsed
 )
 
 , evt_data_parsed AS (
