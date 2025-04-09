b2416716be5866c189cfdcba972ee4a882932130
jeff-dude <jeff@dune.xyz>
2025-04-09 16:37:22 -0400

    coalesce instructions to ensure a value

--- dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_account_raw_data.sql
+++ dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_account_raw_data.sql
@@ -6,113 +6,128 @@
     materialized='incremental',
     file_format='delta',
     incremental_strategy='merge',
-    unique_key=['token_account_prefix', 'token_account', 'block_time', 'block_slot', 'tx_index', 'inner_instruction_index', 'outer_instruction_index', 'event_type'],
+    unique_key=['token_account_prefix', 'token_account', 'event_type', 'block_time', 'block_slot', 'tx_index', 'inner_instruction_index', 'outer_instruction_index'],
     incremental_predicates=[incremental_predicate('DBT_INTERNAL_DEST.block_time')]
   )
 }}
 
---Init events contain mint address, and owner address
---Init v1
-SELECT
-  call_block_time AS block_time
-  , call_block_slot AS block_slot
-  , call_tx_index AS tx_index
-  , call_inner_instruction_index AS inner_instruction_index
-  , call_outer_instruction_index AS outer_instruction_index
-  , account_account AS token_account
-  , SUBSTRING(account_account, 1, 2) AS token_account_prefix
-  , 'init' AS event_type
+with raw_events as (
+  --Init v1, events contain mint address and owner address
+  SELECT
+    call_block_time AS block_time
+    , call_block_slot AS block_slot
+    , call_tx_index AS tx_index
+    , call_inner_instruction_index AS inner_instruction_index
+    , call_outer_instruction_index AS outer_instruction_index
+    , account_account AS token_account
+    , SUBSTRING(account_account, 1, 2) AS token_account_prefix
+    , 'init' AS event_type
+    , account_owner
+    , account_mint
+  FROM {{ source('spl_token_solana', 'spl_token_call_initializeaccount') }}
+  WHERE
+    1=1
+    {% if is_incremental() %}
+    AND {{ incremental_predicate('call_block_time') }}
+    {% endif %}
+
+  UNION ALL
+
+  --Init v2
+  SELECT
+    call_block_time AS block_time
+    , call_block_slot AS block_slot
+    , call_tx_index AS tx_index
+    , call_inner_instruction_index AS inner_instruction_index
+    , call_outer_instruction_index AS outer_instruction_index
+    , account_account AS token_account
+    , SUBSTRING(account_account, 1, 2) AS token_account_prefix
+    , 'init' AS event_type
+    , owner as account_owner
+    , account_mint
+  FROM {{ source('spl_token_solana', 'spl_token_call_initializeaccount2') }}
+  WHERE
+    1=1
+    {% if is_incremental() %}
+    AND {{ incremental_predicate('call_block_time') }}
+    {% endif %}
+
+  UNION ALL
+
+  --Init v3
+  SELECT
+    call_block_time AS block_time
+    , call_block_slot AS block_slot
+    , call_tx_index AS tx_index
+    , call_inner_instruction_index AS inner_instruction_index
+    , call_outer_instruction_index AS outer_instruction_index
+    , account_account AS token_account
+    , SUBSTRING(account_account, 1, 2) AS token_account_prefix
+    , 'init' AS event_type
+    , owner as account_owner
+    , account_mint
+  FROM {{ source('spl_token_solana', 'spl_token_call_initializeaccount3') }}
+  WHERE
+    1=1
+    {% if is_incremental() %}
+    AND {{ incremental_predicate('call_block_time') }}
+    {% endif %}
+
+  UNION ALL
+
+
+  -- Owner Changes: Only owner changes, mint persists, mint address not in data
+  SELECT
+    call_block_time AS block_time
+    , call_block_slot AS block_slot
+    , call_tx_index AS tx_index
+    , call_inner_instruction_index AS inner_instruction_index
+    , call_outer_instruction_index AS outer_instruction_index
+    , account_owned AS token_account
+    , SUBSTRING(account_owned, 1, 2) AS token_account_prefix
+    , 'owner_change' AS event_type
+    , newAuthority as account_owner
+    , null as account_mint
+  FROM {{ source('spl_token_solana', 'spl_token_call_setauthority') }}
+  WHERE
+    json_extract_scalar(authorityType, '$.AuthorityType') = 'AccountOwner'
+    AND 1=1
+    {% if is_incremental() %}
+    AND {{ incremental_predicate('call_block_time') }}
+    {% endif %}
+
+  UNION ALL
+
+  --Closure events only contain the token account address
+  SELECT
+    call_block_time AS block_time
+    , call_block_slot AS block_slot
+    , call_tx_index AS tx_index
+    , call_inner_instruction_index AS inner_instruction_index
+    , call_outer_instruction_index AS outer_instruction_index
+    , account_account AS token_account
+    , SUBSTRING(account_account, 1, 2) AS token_account_prefix
+    , 'close' AS event_type
+    , null as account_owner
+    , null as account_mint
+  FROM {{ source('spl_token_solana', 'spl_token_call_closeaccount') }}
+  WHERE 
+    1=1
+    {% if is_incremental() %}
+    AND {{ incremental_predicate('call_block_time') }}
+    {% endif %}
+)
+--
+select
+  block_time
+  , block_slot
+  , tx_index
+  , COALESCE(inner_instruction_index, -1) as inner_instruction_index --8bil+ rows with null, need to coalesce to avoid nulls in merge statement
+  , COALESCE(outer_instruction_index, -1) as outer_instruction_index --0 rows with null, but keeping for clarity
+  , token_account
+  , token_account_prefix
+  , event_type
   , account_owner
   , account_mint
-FROM {{ source('spl_token_solana', 'spl_token_call_initializeaccount') }}
-WHERE
-  1=1
-  {% if is_incremental() %}
-  AND {{ incremental_predicate('call_block_time') }}
-  {% endif %}
-
-UNION ALL
-
---Init v2
-SELECT
-  call_block_time AS block_time
-  , call_block_slot AS block_slot
-  , call_tx_index AS tx_index
-  , call_inner_instruction_index AS inner_instruction_index
-  , call_outer_instruction_index AS outer_instruction_index
-  , account_account AS token_account
-  , SUBSTRING(account_account, 1, 2) AS token_account_prefix
-  , 'init' AS event_type
-  , owner as account_owner
-  , account_mint
-FROM {{ source('spl_token_solana', 'spl_token_call_initializeaccount2') }}
-WHERE
-  1=1
-  {% if is_incremental() %}
-  AND {{ incremental_predicate('call_block_time') }}
-  {% endif %}
-
-UNION ALL
-
---Init v3
-SELECT
-  call_block_time AS block_time
-  , call_block_slot AS block_slot
-  , call_tx_index AS tx_index
-  , call_inner_instruction_index AS inner_instruction_index
-  , call_outer_instruction_index AS outer_instruction_index
-  , account_account AS token_account
-  , SUBSTRING(account_account, 1, 2) AS token_account_prefix
-  , 'init' AS event_type
-  , owner as account_owner
-  , account_mint
-FROM {{ source('spl_token_solana', 'spl_token_call_initializeaccount3') }}
-WHERE
-  1=1
-  {% if is_incremental() %}
-  AND {{ incremental_predicate('call_block_time') }}
-  {% endif %}
-
-UNION ALL
-
-
--- Owner Changes: Only owner changes, mint persists, mint address not in data
-SELECT
-  call_block_time AS block_time
-  , call_block_slot AS block_slot
-  , call_tx_index AS tx_index
-  , call_inner_instruction_index AS inner_instruction_index
-  , call_outer_instruction_index AS outer_instruction_index
-  , account_owned AS token_account
-  , SUBSTRING(account_owned, 1, 2) AS token_account_prefix
-  , 'owner_change' AS event_type
-  , newAuthority as account_owner
-  , null as account_mint
-FROM {{ source('spl_token_solana', 'spl_token_call_setauthority') }}
-WHERE
-  json_extract_scalar(authorityType, '$.AuthorityType') = 'AccountOwner'
-  AND 1=1
-  {% if is_incremental() %}
-  AND {{ incremental_predicate('call_block_time') }}
-  {% endif %}
-
-UNION ALL
-
---Closure events only contain the token account address
-SELECT
-  call_block_time AS block_time
-  , call_block_slot AS block_slot
-  , call_tx_index AS tx_index
-  , call_inner_instruction_index AS inner_instruction_index
-  , call_outer_instruction_index AS outer_instruction_index
-  , account_account AS token_account
-  , SUBSTRING(account_account, 1, 2) AS token_account_prefix
-  , 'close' AS event_type
-  , null as account_owner
-  , null as account_mint
-FROM {{ source('spl_token_solana', 'spl_token_call_closeaccount') }}
-WHERE 
-  1=1
-  {% if is_incremental() %}
-  AND {{ incremental_predicate('call_block_time') }}
-  {% endif %}
+from
+  raw_events
