6ad1b289baae9e2646214f07a0603a1bc1ab2838
Huang Geyang <Sukebeta@outlook.com>
2025-04-09 10:55:25 +0800

    Revert "Recover cex_evms.sql"
    This reverts commit 48fec290b21146262a779a139d40e5ab615cc3ad.

--- dbt_subprojects/hourly_spellbook/macros/sector/cex/cex_evms.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/cex/cex_evms.sql
@@ -1,71 +1,38 @@
-{% macro cex_flows(blockchain, transfers, addresses) %}
+{% macro cex_evms(cex_addresses, blockchain, traces) %}
 
-WITH cex_addresses AS (
-    SELECT
-        address,
-        cex_name,
-        distinct_name
-    FROM {{addresses}}
-),
-
-transfer_data AS (
-    SELECT
-        block_time,
-        block_number,
-        contract_address,
-        symbol,
-        token_standard,
-        amount,
-        amount_raw,
-        amount_usd,
-        "from",
-        to,
-        tx_from,
-        tx_to,
-        tx_index,
-        tx_hash,
-        evt_index,
-        unique_key
-    FROM {{transfers}}
+WITH new_addresses AS (
+    SELECT 
+        '{{blockchain}}' AS blockchain,
+        a.address,
+        a.cex_name,
+        a.distinct_name,
+        a.added_by,
+        a.added_date,
+        MIN(t.block_time) AS first_used
+    FROM {{traces}} t
+    INNER JOIN {{cex_addresses}} a ON a.address = t.to
     {% if is_incremental() %}
-    WHERE {{incremental_predicate('block_time')}}
+    WHERE {{incremental_predicate('t.block_time')}}
     {% endif %}
+    GROUP BY a.address, a.cex_name, a.distinct_name, a.added_by, a.added_date
 )
 
-SELECT DISTINCT '{{blockchain}}' AS blockchain
-, CAST(date_trunc('month', t.block_time) AS date) AS block_month
-, t.block_time
-, t.block_number
-, COALESCE(c.cex_name, a.cex_name, d.cex_name, b.cex_name) AS cex_name
-, a.distinct_name
-, t.contract_address AS token_address
-, t.symbol AS token_symbol
-, t.token_standard
-, CASE WHEN a.cex_name=c.cex_name AND a.cex_name IS NOT NULL THEN 'Internal'
-    WHEN a.cex_name<>c.cex_name AND a.cex_name IS NOT NULL AND c.cex_name IS NOT NULL THEN 'Cross-CEX'
-    WHEN a.cex_name IS NOT NULL THEN 'Outflow'
-    WHEN c.cex_name IS NOT NULL THEN 'Inflow'
-    WHEN d.cex_name IS NOT NULL THEN 'Executed Contract'
-    WHEN b.cex_name IS NOT NULL THEN 'Executed'
-    END AS flow_type
-, CASE WHEN a.address=t."from" AND b.address!=t.to THEN -t.amount ELSE t.amount END AS amount
-, t.amount_raw
-, CASE WHEN a.address=t."from" AND b.address!=t.to THEN -t.amount_usd ELSE t.amount_usd END AS amount_usd
-, t."from"
-, t.to
-, t.tx_from
-, t.tx_to
-, t.tx_index
-, t.tx_hash
-, t.evt_index
-, t.unique_key
-FROM transfer_data t
-LEFT JOIN cex_addresses a ON a.address = t."from"
-LEFT JOIN cex_addresses b ON b.address = t.tx_from
-LEFT JOIN cex_addresses c ON c.address = t.to
-LEFT JOIN cex_addresses d ON d.address = t.tx_to
-WHERE a.cex_name IS NOT NULL 
-    OR b.cex_name IS NOT NULL 
-    OR c.cex_name IS NOT NULL 
-    OR d.cex_name IS NOT NULL
+SELECT 
+    n.blockchain,
+    n.address,
+    n.cex_name,
+    n.distinct_name,
+    n.added_by,
+    n.added_date,
+    {% if is_incremental() %}
+    COALESCE(b.first_used, n.first_used) AS first_used
+    FROM new_addresses n
+    LEFT JOIN {{this}} b ON 
+        n.blockchain = b.blockchain AND 
+        n.address = b.address
+    {% else %}
+    n.first_used
+    FROM new_addresses n
+    {% endif %}
+
 {% endmacro %}
