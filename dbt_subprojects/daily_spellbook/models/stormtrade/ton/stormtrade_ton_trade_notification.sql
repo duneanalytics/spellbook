aac54ad093ab93c838049ec775712c2b3898f3c1
Pavel Shuvalov <pavel.s@ton.org>
2025-04-10 11:13:23 +0400

    Add macros with vaults

--- dbt_subprojects/daily_spellbook/models/stormtrade/ton/stormtrade_ton_trade_notification.sql
+++ dbt_subprojects/daily_spellbook/models/stormtrade/ton/stormtrade_ton_trade_notification.sql
@@ -15,15 +15,10 @@
 
 
 -- trade_notification#3475fdd2 is sent from the AMM to the vault
--- dbt_subprojects/daily_spellbook/models/stormtrade/ton/evaa_ton_supply.sql
+-- https://github.com/Tsunami-Exchange/storm-contracts-specs/blob/3da4852/scheme.tlb#L143-L148
 
 WITH stormtrade_vaults AS (
-    SELECT vault, vault_token FROM (VALUES 
-    (upper('0:33e9e84d7cbefff0d23b395875420e3a1ecb82e241692be89c7ea2bd27716b77'), 'USDT'),
-    (upper('0:f29d17a209e2bcc652916d802ba69e23cb366e17afad61f8453343b9ba53ace4'), 'jUSDT'),
-    (upper('0:e926764ff3d272c73ddeb836975c5521c025ad68e7919a25094e2de3198805f1'), 'TON'),
-    (upper('0:06f3f073c255a49aa6fdcc89abf512638e065908b30e4173fd3d1d01d4f607bd'), 'NOT')
-    ) AS T(vault, vault_token)
+    {{ stormtrade_ton_vaults() }}
 ),
 parsed_boc AS (
     SELECT M.block_date, M.tx_hash, M.trace_id, M.tx_now, M.tx_lt, vault, vault_token, M.source as amm, body_boc
