9ddc18a122c5bb06bfd1ca9ccb949a57be6f6abe
Boxer <Floba.hs@gmail.com>
2025-04-10 14:39:00 +0100

    fix upstream uniq key

--- dbt_subprojects/solana/models/_sector/transfers/pre_computed_transfers.sql
+++ dbt_subprojects/solana/models/_sector/transfers/pre_computed_transfers.sql
@@ -3,8 +3,7 @@
     schema='tokens_solana', 
     alias='pre_computed_transfers', 
     materialized='table',
-    partition_by = ['from_token_account_prefix'],
-    cluster_by = ['from_token_account_prefix']
+    partition_by = ['block_month']
   )
 }}
 
@@ -12,7 +11,6 @@ SELECT
     call_block_time as block_time
     , cast(date_trunc('day', call_block_time) as date) as block_date
     , cast(date_trunc('month', call_block_time) as date) as block_month
-    , cast(date_trunc('year', call_block_time) as date) as block_year -- Added block_year
     , call_block_slot as block_slot
     , call_tx_index as tx_index
     , 'transfer' as action
@@ -24,9 +22,16 @@ SELECT
     , 'spl_token' as token_version
     , call_tx_signer as tx_signer
     , call_tx_id as tx_id
-    , call_outer_instruction_index as outer_instruction_index
-    , call_inner_instruction_index as inner_instruction_index
     , call_outer_executing_account as outer_executing_account
+    -- constructing an artificial instruction_uniq_id to order instructions using one string column
+    -- lpads are chosen carefully to be lexicographically sortable
+    -- we can sort the instructions by this column to get the correct order
+    , CONCAT(
+        lpad(cast(call_block_slot as varchar), 12, '0'), '-', 
+        lpad(cast(call_tx_index as varchar), 6, '0'), '-', 
+        lpad(cast(coalesce(call_outer_instruction_index, 0) as varchar), 4, '0'), '-', 
+        lpad(cast(coalesce(call_inner_instruction_index, 0) as varchar), 4, '0')
+      ) AS instruction_uniq_id
 FROM
     {{ source('spl_token_solana', 'spl_token_call_transfer') }} 
 WHERE
