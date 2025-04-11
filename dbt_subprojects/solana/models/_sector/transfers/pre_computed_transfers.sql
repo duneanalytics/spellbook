a8254a2cf77067c03c3003ac2c5a79983ae7c82a
Boxer <Floba.hs@gmail.com>
2025-04-11 12:30:53 +0100

    try from start of march with year filter

--- dbt_subprojects/solana/models/_sector/transfers/pre_computed_transfers.sql
+++ dbt_subprojects/solana/models/_sector/transfers/pre_computed_transfers.sql
@@ -37,4 +37,4 @@ FROM
     {{ source('spl_token_solana', 'spl_token_call_transfer') }} 
 WHERE
     1=1
-    and call_block_time > TIMESTAMP '2025-04-01' 
+    and call_block_time > TIMESTAMP '2025-03-01' 
