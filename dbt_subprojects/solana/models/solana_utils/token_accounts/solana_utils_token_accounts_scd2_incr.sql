d6d2d5881c61e5487c4340c4596254081de56759
jeff-dude <jeff@dune.xyz>
2025-04-11 15:25:12 -0400

    try to use the existing macro for properties

--- dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_accounts_scd2_incr.sql
+++ dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_accounts_scd2_incr.sql
@@ -5,7 +5,9 @@
     , partition_by=['token_account_prefix']
     , materialized='incremental'
     , file_format='delta'
-    , pre_hook = 'SET SESSION hive.insert-existing-partitions-behavior = "OVERWRITE"'
+    , pre_hook=[
+        set_trino_session_property(true, 'hive.insert_existing_partitions_behavior', 'OVERWRITE')
+    ]
   )
 }}
 
