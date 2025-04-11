fbd147c2933f6a119dba67221d9b04450d361707
Poly <cypherpepe33@gmail.com>
2025-04-11 13:08:37 +0300

    Update prices_solana_tokens.sql

--- dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
+++ dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
@@ -743,6 +743,5 @@ FROM
         ('ldo-lido-dao','solana','LDO','HZRCwxP2Vq9PCpPXooayhJ2bxTpo5xfpQrwB1svh332p',8),
         ('dbr-debridge','solana','DBR','DBRiDgJAMsM95moTzJs7M9LnkGErpbv9v6CUR1DXnUu5',6),
         ('grt-the-graph','solana','GRT','HGsLG4PnZ28L8A4R5nPqKgZd86zUUdmfnkTRnuFJ5dAX',8),
-        ('mana-decentraland','solana','MANA','7dgHoN8wBZCc5wbnQ2C47TDnBMAxG4Q5L3KjP67z8kNi',8),
-        ('w-wormhole','solana','W','85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ',6)
+        ('mana-decentraland','solana','MANA','7dgHoN8wBZCc5wbnQ2C47TDnBMAxG4Q5L3KjP67z8kNi',8)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
