b4ac2bc63e48024941a30fa74713096f2010a440
jeff-dude <jeff@dune.xyz>
2025-04-10 22:55:05 -0400

    fix syntax for accounts model

--- dbt_subprojects/solana/models/solana_utils/solana_utils_token_accounts.sql
+++ dbt_subprojects/solana/models/solana_utils/solana_utils_token_accounts.sql
@@ -17,14 +17,12 @@ with ranked_src as (
 		, token_account
 		, event_type
 		, account_owner
-		, coalesce(
-			account_mint
-			, last_value(account_mint ignore nulls) over (
+		, max(case when account_mint is not null then account_mint end)
+			over (
 				partition by token_account
 				order by unique_instruction_key
 				rows between unbounded preceding and current row
-			)
-		) as account_mint
+			) as account_mint
 		, unique_instruction_key
 		, block_time as valid_from
 		, lead(block_time) over (
@@ -35,11 +33,10 @@ with ranked_src as (
 			partition by token_account
 			order by unique_instruction_key desc
 		) as row_num
-		, md5(token_account || unique_instruction_key) as scd_id
 	from {{ ref('solana_utils_token_accounts_raw') }}
-	{%- if is_incremental() -%}
+	{% if is_incremental() or true -%}
 	where {{ incremental_predicate('block_time') }}
-	{%- endif -%}
+	{%- endif %}
 )
 , src as (
 	select
@@ -52,10 +49,9 @@ with ranked_src as (
 		, valid_from
 		, coalesce(next_valid_from, TIMESTAMP '9999-12-31 23:59:59') as valid_to
 		, row_num = 1 as is_current
-		, scd_id
 	from ranked_src
 )
-{%- if is_incremental() -%}
+{% if is_incremental() or true -%}
 -- Step 2: Active rows from current table
 , dst as (
 	select
@@ -68,7 +64,6 @@ with ranked_src as (
 		, event_type
 		, account_owner
 		, account_mint
-		, md5(token_account || unique_instruction_key) as scd_id
 	from {{ this }}
     where is_current = true -- consider building this as model, if slow, and read here instead
 )
@@ -107,8 +102,7 @@ with ranked_src as (
 		, d.unique_instruction_key
 		, d.valid_from
 		, coalesce(e.new_valid_to, d.valid_to) as valid_to
-		, case when e.new_valid_from is not null then false else d.is_current end as is_current
-		, d.scd_id
+		, case when e.new_valid_to is not null then false else d.is_current end as is_current
 	from dst d
 	left join to_expire e
 		on d.token_account = e.token_account
@@ -117,17 +111,20 @@ with ranked_src as (
 )
 -- Step 6: Filter out overlapping src rows already handled by updated_dst
 , final_src as (
-	select *
-	from src
-	where scd_id not in (
-		select scd_id from updated_dst
-	)
+	select s.*
+	from src s
+	left join updated_dst d
+		on s.token_account = d.token_account
+        and s.token_account_prefix = d.token_account_prefix
+		and s.unique_instruction_key = d.unique_instruction_key
+	where d.token_account is null
 )
+
 -- Step 7: Merge-ready output
 select * from updated_dst
 union all
 select * from final_src
-{%- else -%}
+{% else -%}
 -- Full refresh: just emit all source rows with SCD logic applied
 select * from src
-{%- endif -%}
+{%- endif %}
