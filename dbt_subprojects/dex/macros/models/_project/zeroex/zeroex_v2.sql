c4c6ed692f8c3c29df48d3d429b0ba0f2987b897
RantumBits <rantum@rantum.xyz>
2025-04-16 10:50:47 -0700

    cow trade log selection update

--- dbt_subprojects/dex/macros/models/_project/zeroex/zeroex_v2.sql
+++ dbt_subprojects/dex/macros/models/_project/zeroex/zeroex_v2.sql
@@ -110,12 +110,6 @@ tbl_all_logs AS (
                 OR logs.contract_address = settler_address
                 OR swap_signatures.signature is not null
         )
-        and  case when cow_rn is not null then (
-             bytearray_substring(logs.topic1, 13,20) = settler_address 
-          or bytearray_substring(logs.topic2, 13,20) = settler_address ) 
-          or (bytearray_substring(logs.topic1, 13,20) = st.contract_address
-              and bytearray_substring(logs.topic1, 13,20) = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41 )
-          else 1=1 end 
         AND zid != 0xa00000000000000000000000
 ),
 cow_log_range as (
@@ -200,6 +194,12 @@ taker_logs as (
                             and bytearray_substring(logs.topic1,13,20)  = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41
                             )
         ) 
+           and  case when cow_rn is not null then (
+             bytearray_substring(logs.topic1, 13,20) = settler_address 
+          or bytearray_substring(logs.topic2, 13,20) = settler_address ) 
+          or (bytearray_substring(logs.topic2, 13,20) = st.contract_address
+              and bytearray_substring(logs.topic1, 13,20) = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41 )
+          else 1=1 end 
     )
     select * 
     from tbl_base
