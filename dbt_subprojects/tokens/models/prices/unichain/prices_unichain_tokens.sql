de2e6635e627fb607205d1e6da5f164e774f9311
ppclunghe <pipistrella@list.ru>
2025-04-17 11:46:16 +0200

    prices update

--- dbt_subprojects/tokens/models/prices/unichain/prices_unichain_tokens.sql
+++ dbt_subprojects/tokens/models/prices/unichain/prices_unichain_tokens.sql
@@ -22,4 +22,6 @@ FROM
     , ('usdc-usd-coin', 'USDC', 0x078d782b760474a361dda0af3839290b0ef57ad6, 6)
     , ('uni-uniswap', 'UNI', 0x8f187aa05619a017077f5308904739877ce9ea21, 18)
     , ('dai-dai', 'DAI', 0x20cab320a855b39f724131c69424240519573f81, 18)
+    , ('wsteth-wrapped-liquid-staked-ether-20', 'wstETH', 0xc02fe7317d4eb8753a02c35fe019786854a92001, 18) 
+
 ) as temp (token_id, symbol, contract_address, decimals) 
