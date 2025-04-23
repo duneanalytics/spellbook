8696076b502f1e0fe8c14da9d229d24f31e1b24f
Boxer <Floba.hs@gmail.com>
2025-04-23 14:55:53 +0200

    remove nft join

--- dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_spl_token_accounts_state_history.sql
+++ dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_spl_token_accounts_state_history.sql
@@ -34,14 +34,6 @@ with ranked_src as (
         ) as rn
     from {{ ref('solana_utils_spl_token_accounts_raw') }}
 )
-, nft as (
-    select distinct
-      account_mint
-    from
-      {{ ref('tokens_solana_nft')}}
-    where
-      account_mint is not null
-)
 , base_state as ( -- Calculate the definitive token_mint_address first
     select
         ranked_src.address_prefix
@@ -59,34 +51,27 @@ with ranked_src as (
         , ranked_src.last_non_null_token_mint_address -- Keep for debugging if needed, or remove
     from ranked_src
 )
-, final_state_calc as ( -- Now perform the join and calculate account_type
+, final_state_calc as ( -- Simplified CTE without NFT join
     select
         base_state.address_prefix
         , base_state.address
         , base_state.event_type
         , base_state.token_balance_owner
         , base_state.token_mint_address -- Use pre-calculated address
-        , case when nft.account_mint is not null
-          then 'nft'
-          else 'fungible'
-        end as account_type
         , base_state.block_date
         , base_state.valid_from_unique_instruction_key
         , coalesce(base_state.valid_to_unique_instruction_key, '999999999-999999-9999-9999') as valid_to_unique_instruction_key
         , if(base_state.rn = 1, 1, 0) as is_active
     from base_state
-    left join nft
-      on base_state.token_mint_address = nft.account_mint -- Direct join condition
 )
 
--- Final selection with filtering based on the provided logic
+
 select
     address_prefix
     , address
     , event_type
     , token_balance_owner
     , token_mint_address
-    , account_type
     , block_date
     , valid_from_unique_instruction_key
     , valid_to_unique_instruction_key
