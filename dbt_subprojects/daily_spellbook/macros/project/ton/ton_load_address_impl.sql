0ddd4f71ba2fbed8e6ec63f408cad0b9d4414d48
markysha <mark.kornejchik@gmail.com>
2025-04-23 18:15:17 +0300

    ton dns init

--- dbt_subprojects/daily_spellbook/macros/project/ton/ton_load_address_impl.sql
+++ dbt_subprojects/daily_spellbook/macros/project/ton/ton_load_address_impl.sql
@@ -32,7 +32,7 @@ CASE
       state.refs_indexes,
       state.cursor_bit_offset + {{ 3 + 8 + 256 }}, state.cursor_ref_offset,
       map_concat(state.output,
-      map_from_entries(ARRAY[({{ field_name }}, CAST(CAST('address format is not supported' AS BIGINT) AS JSON))]))
+      map_from_entries(ARRAY[({{ field_name }}, CAST(CAST('address format is not supported' AS varchar) AS JSON))]))
     )
 END
 AS {{ ton_boc_state_type() }})
