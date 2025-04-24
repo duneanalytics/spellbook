19f3a756daa8a4b16b17fde6a1e9b600e5194680
jeff-dude <jeff@dune.xyz>
2025-04-24 16:46:31 -0400

    update spl transfers macro and models

--- dbt_subprojects/solana/macros/_sector/transfers/solana_spl_transfers_macro.sql
+++ dbt_subprojects/solana/macros/_sector/transfers/solana_spl_transfers_macro.sql
@@ -4,7 +4,6 @@ WITH transfers_raw AS (
     SELECT
         call_block_time as block_time
         , cast(date_trunc('day', call_block_time) as date) as block_date
-        , cast(date_trunc('month', call_block_time) as date) as block_month
         , call_block_slot as block_slot
         , 'transfer' as action
         , amount
@@ -15,7 +14,7 @@ WITH transfers_raw AS (
         , call_tx_id as tx_id
         , call_tx_index as tx_index
         , call_outer_instruction_index as outer_instruction_index
-        , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
+        , call_inner_instruction_index as inner_instruction_index
         , call_outer_executing_account as outer_executing_account
     FROM 
         {{ source('spl_token_solana','spl_token_call_transferChecked') }}
@@ -33,7 +32,6 @@ WITH transfers_raw AS (
     SELECT
         call_block_time as block_time
         , cast (date_trunc('day', call_block_time) as date) as block_date
-        , cast (date_trunc('month', call_block_time) as date) as block_month
         , call_block_slot as block_slot
         , 'mint' as action
         , amount
@@ -44,7 +42,7 @@ WITH transfers_raw AS (
         , call_tx_id as tx_id
         , call_tx_index as tx_index
         , call_outer_instruction_index as outer_instruction_index
-        , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
+        , call_inner_instruction_index as inner_instruction_index
         , call_outer_executing_account as outer_executing_account
     FROM 
         {{ source('spl_token_solana','spl_token_call_mintTo') }}
@@ -62,7 +60,6 @@ WITH transfers_raw AS (
     SELECT
         call_block_time as block_time
         , cast (date_trunc('day', call_block_time) as date) as block_date
-        , cast (date_trunc('month', call_block_time) as date) as block_month
         , call_block_slot as block_slot
         , 'mint' as action
         , amount
@@ -73,7 +70,7 @@ WITH transfers_raw AS (
         , call_tx_id as tx_id
         , call_tx_index as tx_index
         , call_outer_instruction_index as outer_instruction_index
-        , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
+        , call_inner_instruction_index as inner_instruction_index
         , call_outer_executing_account as outer_executing_account
     FROM 
         {{ source('spl_token_solana','spl_token_call_mintToChecked') }}
@@ -91,7 +88,6 @@ WITH transfers_raw AS (
     SELECT
         call_block_time as block_time
         , cast (date_trunc('day', call_block_time) as date) as block_date
-        , cast (date_trunc('month', call_block_time) as date) as block_month
         , call_block_slot as block_slot
         , 'burn' as action
         , amount
@@ -102,7 +98,7 @@ WITH transfers_raw AS (
         , call_tx_id as tx_id
         , call_tx_index as tx_index
         , call_outer_instruction_index as outer_instruction_index
-        , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
+        , call_inner_instruction_index as inner_instruction_index
         , call_outer_executing_account as outer_executing_account
     FROM 
         {{ source('spl_token_solana','spl_token_call_burn') }}
@@ -120,7 +116,6 @@ WITH transfers_raw AS (
     SELECT
         call_block_time as block_time
         , cast (date_trunc('day', call_block_time) as date) as block_date
-        , cast (date_trunc('month', call_block_time) as date) as block_month
         , call_block_slot as block_slot
         , 'burn' as action
         , amount
@@ -131,7 +126,7 @@ WITH transfers_raw AS (
         , call_tx_id as tx_id
         , call_tx_index as tx_index
         , call_outer_instruction_index as outer_instruction_index
-        , COALESCE(call_inner_instruction_index,0) as inner_instruction_index
+        , call_inner_instruction_index as inner_instruction_index
         , call_outer_executing_account as outer_executing_account
     FROM 
         {{ source('spl_token_solana','spl_token_call_burnChecked') }}
@@ -148,7 +143,6 @@ WITH transfers_raw AS (
     SELECT
         block_time
         , block_date
-        , block_month
         , block_slot
         , action
         , amount
@@ -172,76 +166,26 @@ WITH transfers_raw AS (
     FROM
         transfers_raw
 )
-, prices AS (
-    SELECT
-        contract_address
-        , minute
-        , price
-        , decimals
-        , symbol
-    FROM 
-        {{ source('prices', 'usd_forward_fill') }}
-    WHERE 
-        blockchain = 'solana'
-        AND minute >= TIMESTAMP '2020-10-02 00:00' --solana start date
-        {% if is_incremental() or true -%}
-        AND {{incremental_predicate('minute')}}
-        {% else -%}
-        AND minute >= {{start_date}}
-        AND minute < {{end_date}}
-        {% endif -%}
+, final AS (
+    select
+        transfers.*
+    from
+        transfers
+    {% if is_incremental() -%}
+    left join
+        {{ this }} as existing
+        on existing.block_date = transfers.block_date
+        and existing.block_slot = transfers.block_slot
+        and existing.tx_index = transfers.tx_index
+        and existing.inner_instruction_index = transfers.inner_instruction_index
+        and existing.outer_instruction_index = transfers.outer_instruction_index
+        and {{incremental_predicate('existing.block_time')}}
+    where
+        existing.block_date is null -- only insert new rows
+    {% endif -%}
 )
-SELECT
-    t.block_time
-    , t.block_date
-    , t.block_month
-    , t.block_slot
-    , t.action
-    , t.amount
-    , CASE  
-        WHEN tk_f.decimals is null THEN null
-        WHEN tk_f.decimals = 0 THEN t.amount
-        ELSE t.amount / power(10, tk_f.decimals)
-      END as amount_display
-    , t.from_token_account
-    , t.to_token_account
-    , tk_s.token_balance_owner as from_owner
-    , tk_d.token_balance_owner as to_owner
-    , t.token_version
-    , t.tx_signer
-    , t.tx_id
-    , t.tx_index
-    , t.outer_instruction_index
-    , t.inner_instruction_index
-    , t.outer_executing_account
-    , COALESCE(tk_s.token_mint_address, tk_d.token_mint_address) as token_mint_address
-    , p.price as price_usd
-    , CASE 
-        WHEN tk_f.decimals is null THEN null
-        WHEN tk_f.decimals = 0 THEN p.price * t.amount
-        ELSE p.price * t.amount / power(10, tk_f.decimals)
-      END as amount_usd
-    , tk_f.symbol as symbol
-FROM transfers as t
-LEFT JOIN 
-    {{ ref('solana_utils_token_accounts_state_history') }} tk_s 
-    ON t.from_token_account_prefix = tk_s.address_prefix
-    and t.from_token_account = tk_s.address
-    and t.unique_instruction_key >= tk_s.valid_from_unique_instruction_key
-    and t.unique_instruction_key < tk_s.valid_to_unique_instruction_key
-LEFT JOIN 
-    {{ ref('solana_utils_token_accounts_state_history') }} tk_d 
-    ON t.to_token_account_prefix = tk_d.address_prefix
-    and t.to_token_account = tk_d.address
-    and t.unique_instruction_key >= tk_d.valid_from_unique_instruction_key
-    and t.unique_instruction_key < tk_d.valid_to_unique_instruction_key
-LEFT JOIN 
-    {{ ref('solana_utils_token_address_mapping') }} tk_m
-    ON tk_m.base58_address = COALESCE(tk_s.token_mint_address, tk_d.token_mint_address)
-LEFT JOIN prices p
-    ON p.contract_address = tk_m.binary_address
-    AND p.minute = date_trunc('minute', t.block_time)
-LEFT JOIN 
-    {{ ref('tokens_solana_fungible') }} tk_f
-    ON COALESCE(tk_s.token_mint_address, tk_d.token_mint_address) = tk_f.token_mint_address
+select
+    *
+from
+    final
 {%- endmacro %}
--- dbt_subprojects/solana/models/_sector/transfers/backfills/spl_transfers/tokens_solana_spl_transfers_current.sql
+++ dbt_subprojects/solana/models/_sector/transfers/backfills/spl_transfers/tokens_solana_spl_transfers_current.sql
@@ -4,11 +4,11 @@
     partition_by = ['block_date'],
     materialized = 'incremental',
     file_format = 'delta',
-    incremental_strategy = 'delete+insert',
-    unique_key = ['block_date', 'tx_id', 'outer_instruction_index', 'inner_instruction_index', 'block_slot']
+    incremental_strategy = 'append',
+    unique_key = ['block_date', 'unique_instruction_key']
 ) }}
 
 {{ solana_spl_transfers_macro(
-    "cast('2024-10-01' as timestamp)",
+    "cast('2025-04-01' as timestamp)",
     "now()"
 ) }} 
