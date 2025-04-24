3cc41f7e25a9b950f93af08eb6faa77e8b4614bc
jeff-dude <jeff@dune.xyz>
2025-04-24 15:31:46 -0400

    finalize columns in state history models

--- dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_spl_token_2022_accounts_state_history.sql
+++ dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_spl_token_2022_accounts_state_history.sql
@@ -11,13 +11,18 @@
 
 with full_history as (
     select
-        address_prefix
+        block_date
+        , block_time
         , address
+        , address_prefix
         , event_type
         , token_balance_owner
         , token_mint_address
-        , block_date
         , unique_instruction_key
+        , block_slot
+        , tx_index
+        , outer_instruction_index
+        , inner_instruction_index
     from {{ ref('solana_utils_spl_token_2022_accounts_raw') }}
 )
 , ranked_src as (
@@ -33,29 +38,41 @@ with full_history as (
                 order by unique_instruction_key asc
                 rows between unbounded preceding and current row
         ) as last_non_null_token_mint_address -- get the latest non-null token_mint_address up to this point
-        , block_date
+        , block_date as valid_from_block_date
+        , block_time as valid_from_block_time
         , unique_instruction_key as valid_from_unique_instruction_key
+        , lead(block_date) over (
+            partition by address_prefix, address
+            order by unique_instruction_key asc
+            ) as valid_to_block_date
+        , lead(block_time) over (
+            partition by address_prefix, address
+            order by unique_instruction_key asc
+            ) as valid_to_block_time
         , lead(unique_instruction_key) over (
             partition by address_prefix, address
             order by unique_instruction_key asc
-        ) as valid_to_unique_instruction_key
+            ) as valid_to_unique_instruction_key
         , row_number() over (
             partition by address_prefix, address
             order by unique_instruction_key desc
-        ) as rn
+            ) as rn
     from full_history
 )
 select
-    ranked_src.address_prefix
-    , ranked_src.address
-    , ranked_src.event_type
-    , ranked_src.token_balance_owner
+    address_prefix
+    , address
+    , event_type
+    , token_balance_owner
     , CASE
-        WHEN ranked_src.event_type = 'owner_change' THEN ranked_src.last_non_null_token_mint_address
-        ELSE ranked_src.token_mint_address
+        WHEN event_type = 'owner_change' THEN last_non_null_token_mint_address
+        ELSE token_mint_address
     END AS token_mint_address
-    , ranked_src.block_date
-    , ranked_src.valid_from_unique_instruction_key
-    , coalesce(ranked_src.valid_to_unique_instruction_key, '999999999-999999-9999-9999') as valid_to_unique_instruction_key
-    , if(ranked_src.rn = 1, 1, 0) as is_active
+    , valid_from_block_date
+    , valid_from_block_time
+    , valid_from_unique_instruction_key
+    , coalesce(valid_to_block_date, date '9999-12-31') as valid_to_block_date
+    , coalesce(valid_to_block_time, timestamp '9999-12-31 23:59:59') as valid_to_block_time
+    , coalesce(valid_to_unique_instruction_key, '999999999-999999-9999-9999') as valid_to_unique_instruction_key
+    , if(rn = 1, 1, 0) as is_active
 from ranked_src
