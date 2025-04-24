239860f4da0383fe4aef51868b7b9d513ac9541a
jeff-dude <jeff@dune.xyz>
2025-04-24 15:33:15 -0400

    finalize columns downstream for token accounts

--- dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_accounts.sql
+++ dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_accounts.sql
@@ -7,7 +7,18 @@
 }}
 
 select
-    *
+    address_prefix
+    , address
+    , event_type
+    , token_balance_owner
+    , token_mint_address
+    , valid_from_block_date
+    , valid_from_block_time
+    , valid_from_unique_instruction_key
+    , valid_to_block_date
+    , valid_to_block_time
+    , valid_to_unique_instruction_key
+    , is_active
 from
     {{ ref('solana_utils_token_accounts_state_history')}}
 where
--- dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_accounts_state_history.sql
+++ dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_accounts_state_history.sql
@@ -7,11 +7,33 @@
 }}
 
 select
-    *
+    address_prefix
+    , address
+    , event_type
+    , token_balance_owner
+    , token_mint_address
+    , valid_from_block_date
+    , valid_from_block_time
+    , valid_from_unique_instruction_key
+    , valid_to_block_date
+    , valid_to_block_time
+    , valid_to_unique_instruction_key
+    , is_active
 from
     {{ ref('solana_utils_spl_token_accounts_state_history')}}
 union all
 select
-    *
+    address_prefix
+    , address
+    , event_type
+    , token_balance_owner
+    , token_mint_address
+    , valid_from_block_date
+    , valid_from_block_time
+    , valid_from_unique_instruction_key
+    , valid_to_block_date
+    , valid_to_block_time
+    , valid_to_unique_instruction_key
+    , is_active
 from
     {{ ref('solana_utils_spl_token_2022_accounts_state_history')}}
