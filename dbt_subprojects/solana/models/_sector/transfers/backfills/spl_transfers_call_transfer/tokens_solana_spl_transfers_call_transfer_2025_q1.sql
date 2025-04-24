8b1d8e48669b781a23fe0e73c5c3c3c001cefabe
jeff-dude <jeff@dune.xyz>
2025-04-24 16:35:57 -0400

    updated spl transfers call transfer models

---
+++ dbt_subprojects/solana/models/_sector/transfers/backfills/spl_transfers_call_transfer/tokens_solana_spl_transfers_call_transfer_2025_q1.sql
@@ -0,0 +1,13 @@
+{{ config(
+    schema = 'tokens_solana',
+    alias = 'spl_transfers_call_transfer_2025_q1',
+    tags = ['static', 'prod_exclude'],
+    partition_by = ['block_date'],
+    materialized = 'table',
+    file_format = 'delta'
+) }}
+
+{{ solana_spl_transfers_call_transfer_macro(
+    "cast('2025-01-01' as timestamp)",
+    "cast('2025-04-01' as timestamp)"
+) }} 
