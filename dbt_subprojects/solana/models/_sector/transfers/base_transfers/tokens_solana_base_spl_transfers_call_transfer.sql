59681a76ce6a3db49dd93019be07380c8555cc1d
jeff-dude <jeff@dune.xyz>
2025-04-24 18:14:00 -0400

    list columns explicitly in base_ models

--- dbt_subprojects/solana/models/_sector/transfers/base_transfers/tokens_solana_base_spl_transfers_call_transfer.sql
+++ dbt_subprojects/solana/models/_sector/transfers/base_transfers/tokens_solana_base_spl_transfers_call_transfer.sql
@@ -10,7 +10,24 @@
 
 {%- for model in models %}
 SELECT 
-    *
+    blockchain
+    , block_date
+    , block_time
+    , block_slot
+    , tx_id
+    , tx_index
+    , inner_instruction_index
+    , outer_instruction_index
+    , tx_signer
+    , amount
+    , outer_executing_account
+    , inner_executing_account
+    , from_token_account_prefix
+    , from_token_account
+    , to_token_account_prefix
+    , to_token_account
+    , token_version
+    , unique_instruction_key
 FROM 
     {{ ref(model) }}
 {%- if not loop.last %}
