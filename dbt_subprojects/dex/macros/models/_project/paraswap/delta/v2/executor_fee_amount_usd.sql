eaa526b091b771a6703f231bf33ae141514e8f32
alexshchur <hi@alexeyshchur.com>
2025-04-25 13:40:12 +0300

    fix typo

--- dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/executor_fee_amount_usd.sql
+++ dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/executor_fee_amount_usd.sql
@@ -1,6 +1,6 @@
 {% macro gas_fee_usd() %}
  COALESCE( 
-        d.price * w.executorFeeAmount / POWER(10, d.decimals), -- compute diretcly (dest token is gas fee compensation if it's augustus axecutor)
+        d.price * w.executorFeeAmount / POWER(10, d.decimals), -- compute directly (dest token is gas fee compensation if it's augustus axecutor)
         s.price *  w.srcAmount / POWER(10, s.decimals) * CAST (w.executorFeeAmount AS DECIMAL) / (CAST (w.destAmount AS DECIMAL)), -- compute pro-rata based on src token if it's augustus executor
         -- TODO: also add 3rd party executor fee compensation -- based on spent native token, divided by amount of orders if multiple
         0
