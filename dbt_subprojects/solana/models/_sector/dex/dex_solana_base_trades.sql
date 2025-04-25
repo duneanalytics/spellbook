4df1923646485bc88e07809ee9feded0775a3bd1
Boxer <Floba.hs@gmail.com>
2025-04-25 17:50:38 +0200

    trigger full lineage run with changes

--- dbt_subprojects/solana/models/_sector/dex/dex_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/dex_solana_base_trades.sql
@@ -12,7 +12,15 @@
 }}
 
 {% set solana_dexes = [
-     ref('raydium_v3_base_trades')
+    
+   , ref('meteora_v2_solana_base_trades')
+   
+] %}
+
+/*
+   , ref('goosefx_ssl_v2_solana_base_trades')
+   , ref('pumpdotfun_solana_base_trades')
+   , ref('raydium_v3_base_trades')
    , ref('raydium_v4_base_trades')
    , ref('raydium_v5_base_trades')
    , ref('orca_whirlpool_base_trades')
@@ -21,11 +29,7 @@
    , ref('lifinity_v1_base_trades')
    , ref('lifinity_v2_base_trades')
    , ref('meteora_v1_solana_base_trades')
-   , ref('meteora_v2_solana_base_trades')
-   , ref('goosefx_ssl_v2_solana_base_trades')
-   , ref('pumpdotfun_solana_base_trades')
-] %}
-
+*/
 -- excluded:    , ref('sanctum_router_base_trades')
 
 {% for dex in solana_dexes %}
--- dbt_subprojects/solana/models/_sector/dex/dex_solana_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/dex_solana_trades.sql
@@ -100,3 +100,4 @@ LEFT JOIN
     {{ source('prices','trusted_tokens') }} tt_bought
     ON bt.token_bought_mint_address = toBase58(tt_bought.contract_address)
     AND bt.blockchain = tt_bought.blockchain
+Where block_time > CAST('2025-01-01' AS TIMESTAMP)
