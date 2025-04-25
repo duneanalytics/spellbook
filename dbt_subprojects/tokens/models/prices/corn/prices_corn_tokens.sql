a340f89298e73612f76acf60df7a67bb6824af46
youyyytrok <d.ionov.beats@gmail.com>
2025-04-25 13:06:00 +0200

    add tokens corn, baby prices_corn_tokens.sql

--- dbt_subprojects/tokens/models/prices/corn/prices_corn_tokens.sql
+++ dbt_subprojects/tokens/models/prices/corn/prices_corn_tokens.sql
@@ -22,4 +22,6 @@ FROM
     , ('lbtc-lombard-staked-btc', 'LBTC', 0xecAc9C5F704e954931349Da37F60E39f515c11c1, 8)
     , ('usdce-usd-coine', 'USDC.e', 0xDF0B24095e15044538866576754F3C964e902Ee6, 6)
     , ('pumpbtc-pumpbtc', 'pumpBTC', 0xF469fBD2abcd6B9de8E169d128226C0Fc90a012e, 8)
+    , ('corn-corn2', 'CORN', 0x44f49ff0da2498bCb1D3Dc7C0f999578F67FD8C6, 18)
+    , ('baby-babylon', 'BABY', 0xe53dCec07d16D88e386AE0710E86d9a400f83c31, 6)
 ) as temp (token_id, symbol, contract_address, decimals)
