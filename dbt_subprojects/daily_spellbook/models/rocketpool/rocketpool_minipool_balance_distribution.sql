111a8d682f69235aece27c54d95b8333bc734bf1
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2025-04-28 11:12:39 -0400

    Update dbt_subprojects/daily_spellbook/models/rocketpool/rocketpool_minipool_balance_distribution.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/daily_spellbook/models/rocketpool/rocketpool_minipool_balance_distribution.sql
+++ dbt_subprojects/daily_spellbook/models/rocketpool/rocketpool_minipool_balance_distribution.sql
@@ -16,6 +16,13 @@ from
 right join {{ source('rocketpool_ethereum','RocketMinipoolDelegate_call_distributeBalance') }} as dist
     on dist.call_tx_hash = trans.hash
         and dist.call_block_number = trans.block_number
+        and dist._rewardsOnly = false -- Only full minipool distributions
+        and dist.call_success = true
+        {% if is_incremental() -%}
+        and {{ incremental_predicate('dist.call_block_time') }}
+        {% else -%}
+        and dist.call_block_time > timestamp '2023-04-01'
+        {% endif -%}
 where
     dist.call_block_time > timestamp '2023-04-01'
     and trans.block_time > timestamp '2023-04-01'
