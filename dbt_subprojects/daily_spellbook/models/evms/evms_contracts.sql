a05460dbb8738a5bca0a7fbb648ae596eb7d2228
los-xyz <alessandro.losi1991@gmail.com>
2025-04-30 10:15:40 +0200

    add evms_contracts

--- dbt_subprojects/daily_spellbook/models/evms/evms_contracts.sql
+++ dbt_subprojects/daily_spellbook/models/evms/evms_contracts.sql
@@ -3,71 +3,96 @@
         alias = 'contracts',
         unique_key=['blockchain', 'address'],
         post_hook='{{ expose_spells(\'[
-                                        "abstract",
-                                        "apechain",
-                                        "arbitrum",
-                                        "avalanche_c",
-                                        "b3",
-                                        "base",
-                                        "berachain",
-                                        "blast",
-                                        "bnb",
-                                        "boba",
-                                        "celo",
-                                        "ethereum",
-                                        "fantom",
-                                        "gnosis",
-                                        "kaia",
-                                        "linea",
-                                        "mantle",
-                                        "optimism",
-                                        "polygon",
-                                        "ronin",
-                                        "scroll",
-                                        "sonic",
-                                        "zkevm",
-                                        "zksync",
-                                        "zora",
-                                        "ink"
+                                        "abstract"
+                                        , "apechain"
+                                        , "arbitrum"
+                                        , "avalanche_c"
+                                        , "b3"
+                                        , "base"
+                                        , "berachain"
+                                        , "blast"
+                                        , "bnb"
+                                        , "bob"
+                                        , "boba"
+                                        , "celo"
+                                        , "corn"
+                                        , "degen"
+                                        , "ethereum"
+                                        , "fantom"
+                                        , "flare"
+                                        , "gnosis"
+                                        , "ink"
+                                        , "kaia"
+                                        , "lens"
+                                        , "linea"
+                                        , "mantle"
+                                        , "nova"
+                                        , "opbnb"
+                                        , "optimism"
+                                        , "polygon"
+                                        , "ronin"
+                                        , "scroll"
+                                        , "sei"
+                                        , "shape"
+                                        , "sonic"
+                                        , "sophon"
+                                        , "unichain"
+                                        , "viction"
+                                        , "worldchain"
+                                        , "zkevm"
+                                        , "zksync"
+                                        , "zora"
                                         ]\',
                                     "sector",
                                     "evms",
-                                    \'["hildobby"]\') }}'
+                                    \'["hildobby", "allelosi"]\') }}'
         )
 }}
 
 {% set contracts_models = [
-     ('ethereum', source('ethereum', 'contracts'))
-     , ('polygon', source('polygon', 'contracts'))
-     , ('bnb', source('bnb', 'contracts'))
-     , ('avalanche_c', source('avalanche_c', 'contracts'))
-     , ('gnosis', source('gnosis', 'contracts'))
-     , ('fantom', source('fantom', 'contracts'))
-     , ('optimism', source('optimism', 'contracts'))
+     ('abstract', source('abstract', 'contracts'))
+     , ('apechain', source('apechain', 'contracts'))
      , ('arbitrum', source('arbitrum', 'contracts'))
-     , ('celo', source('celo', 'contracts'))
+     , ('avalanche_c', source('avalanche_c', 'contracts'))
+     , ('b3', source('b3', 'contracts'))
      , ('base', source('base', 'contracts'))
+     , ('berachain', source('berachain', 'contracts'))
+     , ('blast', source('blast', 'contracts'))
+     , ('bnb', source('bnb', 'contracts'))
+     , ('bob', source('bob', 'contracts'))
+     , ('boba', source('boba', 'contracts'))
+     , ('celo', source('celo', 'contracts'))
+     , ('corn', source('corn', 'contracts'))
+     , ('degen', source('degen', 'contracts'))
+     , ('ethereum', source('ethereum', 'contracts'))
+     , ('fantom', source('fantom', 'contracts'))
+     , ('flare', source('flare', 'contracts'))
+     , ('gnosis', source('gnosis', 'contracts'))
+     , ('ink', source('ink', 'contracts'))
+     , ('kaia', source('kaia', 'contracts'))
+     , ('lens', source('lens', 'contracts'))
+     , ('linea', source('linea', 'contracts'))
+     , ('mantle', source('mantle', 'contracts'))
+     , ('nova', source('nova', 'contracts'))
+     , ('opbnb', source('opbnb', 'contracts'))
+     , ('optimism', source('optimism', 'contracts'))
+     , ('polygon', source('polygon', 'contracts'))
+     , ('ronin', source('ronin', 'contracts'))
+     , ('scroll', source('scroll', 'contracts'))
+     , ('sei', source('sei', 'contracts'))
+     , ('shape', source('shape', 'contracts'))
+     , ('sonic', source('sonic', 'contracts'))
+     , ('sophon', source('sophon', 'contracts'))
+     , ('unichain', source('unichain', 'contracts'))
+     , ('viction', source('viction', 'contracts'))
+     , ('worldchain', source('worldchain', 'contracts'))
+     , ('zkevm', source('zkevm', 'contracts'))
      , ('zksync', source('zksync', 'contracts'))
      , ('zora', source('zora', 'contracts'))
-     , ('scroll', source('scroll', 'contracts'))
-     , ('linea', source('linea', 'contracts'))
-     , ('zkevm', source('zkevm', 'contracts'))
-     , ('blast', source('blast', 'contracts'))
-     , ('mantle', source('mantle', 'contracts'))
-     , ('sei', source('sei', 'contracts'))
-     , ('ronin', source('ronin', 'contracts'))
-     , ('abstract', source('abstract', 'contracts'))
-     , ('boba', source('boba', 'contracts'))
-     , ('kaia', source('kaia', 'contracts'))
-     , ('apechain', source('apechain', 'contracts'))
-     , ('berachain', source('berachain', 'contracts'))
-     , ('b3', source('b3', 'contracts'))
-     , ('ink', source('ink', 'contracts'))
 ] %}
 
 SELECT *
-    FROM
-    (
+FROM (
         {% for contracts_model in contracts_models %}
         SELECT
             '{{ contracts_model[0] }}' AS blockchain
@@ -86,9 +111,10 @@ SELECT *
             , row_number() over (partition by address order by created_at desc) as duplicates_rank
         FROM {{ contracts_model[1] }}
         {% if not loop.last %}
+        {% if is_incremental() %}
+        {% endif %}
         UNION ALL
         {% endif %}
         {% endfor %}
-    )
-    WHERE
-        duplicates_rank = 1
+        )
+WHERE duplicates_rank = 1
