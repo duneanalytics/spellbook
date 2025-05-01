4307691031fc5d9e0196af8b0a309308e5bb77f7
butterbeer@pancakeswap.com <butterbeer@pancakeswap.com>
2025-05-01 20:37:18 +0800

    Revert "[Test] add a comment test next to blockchain column"
    This reverts commit e58f07641dab0a2cb805efdb4f2ab3794bdde528.

--- dbt_subprojects/dex/models/trades/bnb/dex_bnb_base_trades.sql
+++ dbt_subprojects/dex/models/trades/bnb/dex_bnb_base_trades.sql
@@ -48,7 +48,7 @@ WITH base_union AS (
     FROM (
         {% for base_model in base_models %}
         SELECT
-            blockchain --test
+            blockchain
             , project
             , version
             , block_month
