6def7d7e46dc77797e60a9476ad9460360de7367
alexshchur <hi@alexeyshchur.com>
2025-05-02 15:21:57 +0300

    feat: draft: start adjusting delta v2 master template for dex integration

--- dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/master.sql
+++ dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/master.sql
@@ -1,14 +1,45 @@
 {% macro delta_v2_master(blockchain) %}
 with
     {{ delta_v2_swap_settle(blockchain) }},
-    {{ delta_v2_swap_settle_batch(blockchain) }}
-select
-    date_trunc('month', call_block_time) AS block_month,        
-    *
-from delta_v2_swapSettle
-    union all   
-select 
-    date_trunc('month', call_block_time) AS block_month,        
-    *
-from delta_v2_swapSettleBatch
+    {{ delta_v2_swap_settle_batch(blockchain) }},
+    delta_v2_master as (
+        select
+            date_trunc('month', call_block_time) AS block_month,        
+            *
+        from delta_v2_swapSettle
+            union all   
+        select 
+            date_trunc('month', call_block_time) AS block_month,        
+            *
+        from delta_v2_swapSettleBatch
+    )
+    select 
+
+-- SELECT
+        blockchain,
+        'velora_delta' as project,
+        'v2' as version,
+        block_month,
+        block_date,
+        block_time,
+        token_bought_symbol,
+        token_sold_symbol,
+        token_pair,
+        token_bought_amount,
+        token_sold_amount,
+        token_bought_amount_raw,
+        token_sold_amount_raw,
+        amount_usd,
+        token_bought_address,
+        token_sold_address,
+        taker,
+        maker,
+        project_contract_address,
+        tx_hash,
+        tx_from,
+        tx_to,
+        trace_address,
+        evt_index
+--     FROM {{ dex_model }}
+    from delta_v2_master
 {% endmacro %}
