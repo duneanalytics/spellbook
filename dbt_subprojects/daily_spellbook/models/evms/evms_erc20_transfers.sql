750b0a2dffc985068e78dd095ac88cf519d1a61a
agaperste <agapehumilduro@gmail.com>
2025-05-02 12:09:30 -0400

    Add unichain to evms.{} tables, and fixing a bunch of unichain base spellbook initialization

--- dbt_subprojects/daily_spellbook/models/evms/evms_erc20_transfers.sql
+++ dbt_subprojects/daily_spellbook/models/evms/evms_erc20_transfers.sql
@@ -29,7 +29,8 @@
                                         "zksync",
                                         "zora",
                                         "opbnb",
-                                        "ink"
+                                        "ink",
+                                        "unichain"
                                         ]\',
                                     "sector",
                                     "evms",
