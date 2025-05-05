0d162dbaf9cf9fd0383f1007c0543ca30cd7d72b
whalehunting <143016036+whalehunting@users.noreply.github.com>
2025-05-05 17:42:09 +0200

    chore: reformat

--- dbt_subprojects/dex/models/trades/dex_base_trades.sql
+++ dbt_subprojects/dex/models/trades/dex_base_trades.sql
@@ -45,42 +45,48 @@
     , ref('dex_zora_base_trades')
 ] %}
 
-with base_union as (
-    SELECT *
-    FROM
-    (
-        {% for model in models %}
-        SELECT
-            blockchain
-            , project
-            , version
-            , block_month
-            , block_date
-            , block_time
-            , block_number
-            , cast(token_bought_amount_raw as uint256) as token_bought_amount_raw
-            , cast(token_sold_amount_raw as uint256) as token_sold_amount_raw
-            , token_bought_address
-            , token_sold_address
-            , taker
-            , maker
-            , project_contract_address
-            , tx_hash
-            , evt_index
-            , tx_from
-            , tx_to
-            , row_number() over (partition by tx_hash, evt_index order by tx_hash) as duplicates_rank
-        FROM
-            {{ model }}
-        WHERE
-           token_sold_amount_raw >= 0 and token_bought_amount_raw >= 0
-        {% if is_incremental() %}
-            AND {{ incremental_predicate('block_time') }}
-        {% endif %}
-        {% if not loop.last %}
-        UNION ALL
-        {% endif %}
-        {% endfor %}
+with
+    base_union as (
+        select *
+        from
+            (
+                {% for model in models %}
+                    select
+                        blockchain,
+                        project,
+                        version,
+                        block_month,
+                        block_date,
+                        block_time,
+                        block_number,
+                        cast(
+                            token_bought_amount_raw as uint256
+                        ) as token_bought_amount_raw,
+                        cast(token_sold_amount_raw as uint256) as token_sold_amount_raw,
+                        token_bought_address,
+                        token_sold_address,
+                        taker,
+                        maker,
+                        project_contract_address,
+                        tx_hash,
+                        evt_index,
+                        tx_from,
+                        tx_to,
+                        row_number() over (
+                            partition by tx_hash, evt_index order by tx_hash
+                        ) as duplicates_rank
+                    from {{ model }}
+                    where
+                        token_sold_amount_raw >= 0 and token_bought_amount_raw >= 0
+                        {% if is_incremental() %}
+                            and {{ incremental_predicate('block_time') }}
+                        {% endif %}
+                    {% if not loop.last %}
+                        union all
+                    {% endif %}
+                {% endfor %}
+            )
+        where duplicates_rank = 1
     )
 select *
 from base_union
