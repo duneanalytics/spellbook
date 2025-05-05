ecdf294eb69aab12b2e6f5a341230196e0598555
alexshchur <hi@alexeyshchur.com>
2025-05-05 10:08:47 +0300

    fix: "Column 'call_tx_hash' cannot be resolved" and more fileds alike / in the model paraswap_delta_v2_ethereum_trades

--- dbt_subprojects/dex/models/_projects/paraswap/ethereum/paraswap_delta_v2_ethereum_trades.sql
+++ dbt_subprojects/dex/models/_projects/paraswap/ethereum/paraswap_delta_v2_ethereum_trades.sql
@@ -7,7 +7,7 @@
     file_format = 'delta',
     incremental_strategy = 'merge',
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.call_block_time')],
-    unique_key = ['method', 'call_tx_hash', 'call_trace_address', 'order_index'],
+    unique_key = ['method', 'tx_hash', 'trace_address', 'order_index'],
     post_hook='{{ expose_spells(blockchains = \'["ethereum"]\',
                                 spell_type = "project",
                                 spell_name = "paraswap_delta_v2",
