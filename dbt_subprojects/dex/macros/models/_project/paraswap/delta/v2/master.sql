6587c0e581040f8f96b818a999df469d9566fbc4
alexshchur <hi@alexeyshchur.com>
2025-05-05 07:36:41 +0300

    feat: delta v2 - more columns to conform DEX trades shape

--- dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/master.sql
+++ dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/master.sql
@@ -16,28 +16,43 @@ with
     select 
 
 -- SELECT
-        blockchain,
+        delta_v2_master.blockchain,
         'velora_delta' as project,
         'v2' as version,
         block_month,
         DATE_TRUNC('day', call_block_time) as block_date,
-        call_block_time as block_time,
-        token_pair,
-        token_bought_amount,
-        token_sold_amount,
-        token_bought_amount_raw,
-        token_sold_amount_raw,
-        amount_usd,
-        token_bought_address,
-        token_sold_address,
-        taker,
-        maker,
-        project_contract_address,
-        tx_hash,
-        tx_from,
-        tx_to,
-        trace_address,
-        evt_index
+        call_block_time as block_time,        
+        CASE
+            WHEN lower(t_dest_token.symbol) > lower(t_src_token.symbol)
+            THEN concat(t_src_token.symbol, '-', t_dest_token.symbol)
+            ELSE concat(t_dest_token.symbol, '-', t_src_token.symbol)
+        END as token_pair
+        -- token_bought_amount,
+        -- token_sold_amount,
+        -- token_bought_amount_raw,
+        -- token_sold_amount_raw,
+        -- amount_usd,
+        -- token_bought_address,
+        -- token_sold_address,
+        -- taker,
+        -- maker,
+        -- project_contract_address,
+        -- tx_hash,
+        -- tx_from,
+        -- tx_to,
+        -- trace_address,
+        -- evt_index
 --     FROM {{ dex_model }}
-    from delta_v2_master
+    from delta_v2_master  
+        LEFT JOIN 
+        {{ source('tokens', 'erc20') }} t_src_token 
+            ON t_src_token.blockchain = '{{blockchain}}'
+            AND t_src_token.contract_address = src_token
+        LEFT JOIN 
+        {{ source('tokens', 'erc20') }} t_dest_token
+            ON t_dest_token.blockchain = '{{blockchain}}'
+            AND t_dest_token.contract_address = dest_token
+    
+    order by block_time desc
+    limit 1 
 {% endmacro %}
