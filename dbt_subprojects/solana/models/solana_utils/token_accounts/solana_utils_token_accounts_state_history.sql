c82fc228f102bac0ef61062bcfdeb0450f2ae9d7
jeff-dude <jeff@dune.xyz>
2025-05-06 13:59:30 -0400

    add valid from/to month to union model

--- dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_accounts_state_history.sql
+++ dbt_subprojects/solana/models/solana_utils/token_accounts/solana_utils_token_accounts_state_history.sql
@@ -2,10 +2,10 @@
   config(
     schema='solana_utils'
     , alias='token_accounts_state_history'
-    , partition_by=['address_prefix']
+    , partition_by=['valid_from_block_month', 'address_prefix']
     , materialized='table'
     , file_format='delta'
-    , unique_key=['address', 'address_prefix', 'unique_instruction_key']
+    , unique_key=['valid_from_block_month', 'address', 'address_prefix', 'unique_instruction_key']
   )
 }}
 
@@ -15,9 +15,11 @@ select
     , event_type
     , token_balance_owner
     , token_mint_address
+    , valid_from_block_month
     , valid_from_block_date
     , valid_from_block_time
     , valid_from_unique_instruction_key
+    , valid_to_block_month
     , valid_to_block_date
     , valid_to_block_time
     , valid_to_unique_instruction_key
@@ -31,9 +33,11 @@ select
     , event_type
     , token_balance_owner
     , token_mint_address
+    , valid_from_block_month
     , valid_from_block_date
     , valid_from_block_time
     , valid_from_unique_instruction_key
+    , valid_to_block_month
     , valid_to_block_date
     , valid_to_block_time
     , valid_to_unique_instruction_key
