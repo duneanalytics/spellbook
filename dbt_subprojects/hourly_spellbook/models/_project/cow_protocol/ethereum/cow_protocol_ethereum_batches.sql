237f76a921d32ff94d4d0a0e7b209a16834edf23
harisang <harisangelidakis@gmail.com>
2025-05-06 08:26:40 +0300

    update tables for all chains

--- dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/ethereum/cow_protocol_ethereum_batches.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/ethereum/cow_protocol_ethereum_batches.sql
@@ -15,7 +15,7 @@
     )
 }}
 
-WITH
+with
 -- Find the PoC Query here: https://dune.com/queries/1290518
 batch_counts as (
     select try_cast(date_trunc('day', s.evt_block_time) as date) as block_date,
@@ -31,7 +31,7 @@ batch_counts as (
                        then 1
                    else 0
                 end
-            ) as dex_swaps,
+           ) as dex_swaps,
            sum(case when selector = 0x2e1a7d4d then 1 else 0 end) as unwraps,
            sum(case when selector = 0x095ea7b3 then 1 else 0 end) as token_approvals
     from {{ source('gnosis_protocol_v2_ethereum', 'GPv2Settlement_evt_Settlement') }} s
