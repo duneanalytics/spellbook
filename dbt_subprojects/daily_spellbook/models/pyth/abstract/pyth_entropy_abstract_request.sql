da665db0967d906d1b767fd0e60fc53701ebff69
GUNBOATs <92352063+gunboatsss@users.noreply.github.com>
2025-05-06 11:56:06 +0700

    zksync chain macro

--- dbt_subprojects/daily_spellbook/models/pyth/abstract/pyth_entropy_abstract_request.sql
+++ dbt_subprojects/daily_spellbook/models/pyth/abstract/pyth_entropy_abstract_request.sql
@@ -9,12 +9,8 @@
     )
 }}
 
-{{pyth_entropy_request(
+{{pyth_entropy_request_zksync_chain(
     blockchain='abstract',
     symbol='ETH',
     entropy_address='0x5a4a369F4db5df2054994AF031b7b23949b98c0e'
 )}}
-and slice(trace_address, -1, 1) = 0
--- Zksync does a weird thing where native ether need to be handled by system contract
--- before it called the contract again??? this filter out to be the second call
--- https://docs.zksync.io/zksync-protocol/differences/evm-instructions#call-staticcall-delegatecall
