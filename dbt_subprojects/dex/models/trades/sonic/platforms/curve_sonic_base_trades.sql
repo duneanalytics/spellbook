3ec2c68c749bd25099bb91e0c906eb8464c3b8df
Boxer <Floba.hs@gmail.com>
2025-05-07 17:06:29 +0200

    remove exchange underlying

--- dbt_subprojects/dex/models/trades/sonic/platforms/curve_sonic_base_trades.sql
+++ dbt_subprojects/dex/models/trades/sonic/platforms/curve_sonic_base_trades.sql
@@ -30,25 +30,6 @@ exchange_evt_all as (
     where {{ incremental_predicate('t.evt_block_time') }}
     {% endif %}
 ),
- 
-exchange_und_evt_all as (
-    select
-        t.evt_block_number as block_number,
-        t.evt_block_time as block_time,
-        t.bought_id,
-		t.sold_id,
-		t.tokens_bought AS token_bought_amount_raw,
-		t.tokens_sold AS token_sold_amount_raw,
-        t.buyer as taker,
-        cast(null as varbinary) as maker,
-        t.contract_address as project_contract_address,
-        t.evt_tx_hash as tx_hash,
-        t.evt_index
-    from {{ source('curvefi_sonic', 'curvestableswap_evt_TokenExchangeUnderlying') }} t
-    {% if is_incremental() %}
-    where {{ incremental_predicate('t.evt_block_time') }}
-    {% endif %}
-),
 
 dexs as (
     select
@@ -72,30 +53,6 @@ dexs as (
         on t.sold_id = cast(pt_sold.token_id as int256)
         and t.project_contract_address = pt_sold.pool
         and pt_sold.token_type = 'pool_token'
-
-    union all
-
-    select
-        t.block_number,
-        t.block_time,
-        pt_bought.token_address as token_bought_address,
-        pt_sold.token_address as token_sold_address,
-		t.token_bought_amount_raw,
-		t.token_sold_amount_raw,
-        t.taker,
-        t.maker,
-        t.project_contract_address,
-        t.tx_hash,
-        t.evt_index
-    from exchange_und_evt_all t
-    inner join {{ ref('curve_sonic_pools') }} pt_bought
-        on t.bought_id = cast(pt_bought.token_id as int256)
-        and t.project_contract_address = pt_bought.pool
-        and pt_bought.token_type = 'underlying_token_bought'
-    inner join {{ ref('curve_sonic_pools') }} pt_sold
-        on t.sold_id = cast(pt_sold.token_id as int256)
-        and t.project_contract_address = pt_sold.pool
-        and pt_sold.token_type = 'underlying_token_sold'
 )
 
 select
