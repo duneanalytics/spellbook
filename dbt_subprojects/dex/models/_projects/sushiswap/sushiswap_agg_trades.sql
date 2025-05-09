96a7976c5bd49bfa916737ca84d31de8d4a5cb87
OlaStenberg <hello@olastenberg.com>
2025-05-09 20:13:24 +0200

    Refactor sushi aggregator trades

--- dbt_subprojects/dex/models/_projects/sushiswap/sushiswap_agg_trades.sql
+++ dbt_subprojects/dex/models/_projects/sushiswap/sushiswap_agg_trades.sql
@@ -1,12 +1,14 @@
 {{ config(
-        schema = 'sushiswap_agg',
-        alias = 'trades',
-        materialized = 'view',
-        post_hook='{{ expose_spells(blockchains = \'["arbitrum", "avalanche_c", "apechain", "base", "blast", "bnb", "celo", "ethereum", "fantom", "gnosis", "linea", "mantle", "nova", "optimism", "polygon", "scroll", "sonic", "zkevm", "zksync"]\',
-                                    spell_type = "project",
-                                    spell_name = "sushiswap_agg",
-                                    contributors = \'["olastenberg"]\') }}'
-)}}
+    schema = 'sushiswap_agg',
+    alias = 'trades',
+    materialized = 'view',
+    post_hook='{{ expose_spells(
+        blockchains   = \'["arbitrum","avalanche_c","apechain","base","blast","bnb","celo","ethereum","fantom","gnosis","linea","mantle","nova","optimism","polygon","scroll","sonic","zkevm","zksync"]\',
+        spell_type    = "project",
+        spell_name    = "sushiswap_agg",
+        contributors  = \'["olastenberg"]\'
+    ) }}'
+) }}
 
 
 {% set sushiswap_agg_models = [
@@ -31,38 +33,37 @@
     ref('sushiswap_agg_zksync_trades')
 ] %}
 
-SELECT *
-FROM (
-    {% for dex_model in sushiswap_agg_models %}
-    SELECT
-        blockchain,
-        project,
-        version,
-        block_month,
-        block_date,
-        block_time,
-        trade_type,
-        token_bought_symbol,
-        token_sold_symbol,
-        token_pair,
-        token_bought_amount,
-        token_sold_amount,
-        token_bought_amount_raw,
-        token_sold_amount_raw,
-        amount_usd,
-        token_bought_address,
-        token_sold_address,
-        taker,
-        maker,
-        project_contract_address,
-        tx_hash,
-        tx_from,
-        tx_to,
-        trace_address,
-        evt_index
-    FROM {{ dex_model }}
-    {% if not loop.last %}
-    UNION ALL
-    {% endif %}
-    {% endfor %}
-)
+select *
+from (
+  {% for dex_model in sushiswap_agg_models %}
+    select
+      blockchain,
+      project,
+      version,
+      function_name,
+      block_month,
+      block_date,
+      block_time,
+      trade_type,
+      token_bought_symbol,
+      token_sold_symbol,
+      token_pair,
+      token_bought_amount,
+      token_sold_amount,
+      token_bought_amount_raw,
+      token_sold_amount_raw,
+      amount_usd,
+      token_bought_address,
+      token_sold_address,
+      taker,
+      maker,
+      project_contract_address,
+      tx_hash,
+      tx_from,
+      tx_to,
+      trace_address,
+      evt_index
+    from {{ dex_model }}
+    {% if not loop.last %} union all {% endif %}
+  {% endfor %}
+) as combined_trades 
