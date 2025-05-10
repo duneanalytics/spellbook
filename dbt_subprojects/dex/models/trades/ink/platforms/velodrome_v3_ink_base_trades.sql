aae1787d286920f7f0366de4e8df1a8534f859f9
ShawnEdwards23 <130678374+ShawnEdwards23@users.noreply.github.com>
2025-05-10 12:10:28 +0700

    Create velodrome_v3_ink_base_trades.sql

---
+++ dbt_subprojects/dex/models/trades/ink/platforms/velodrome_v3_ink_base_trades.sql
@@ -0,0 +1,20 @@
+{{ config(
+    schema = 'velodrome_v3_ink'
+    , alias = 'base_trades'
+    , materialized = 'incremental'
+    , file_format = 'delta'
+    , incremental_strategy = 'merge'
+    , unique_key = ['tx_hash', 'evt_index']
+    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v3_trades(
+        blockchain = 'ink'
+        , project = 'velodrome'
+        , version = '3'
+        , Pair_evt_Swap = source('velodrome_ink', 'clpool_evt_swap')
+        , Factory_evt_PoolCreated = source('velodrome_ink', 'clfactory_evt_poolcreated')
+    )
+}}
