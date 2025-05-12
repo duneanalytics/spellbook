fe1a34535d9b957de2b24a186fe5ee7c041fe947
VolodymyrBg <aqdrgg19@gmail.com>
2025-05-12 21:58:37 +0300

    feat(dex): Add 3rd party executor fee compensation calculation
    revert
    
    feat(dex): Add 3rd party executor fee compensation calculation

--- dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/executor_fee_amount_usd.sql
+++ dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/executor_fee_amount_usd.sql
@@ -2,7 +2,7 @@
  COALESCE( 
         d.price * w.executorFeeAmount / POWER(10, d.decimals), -- compute directly (dest token is gas fee compensation if it's augustus axecutor)
         s.price *  w.srcAmount / POWER(10, s.decimals) * CAST (w.executorFeeAmount AS DECIMAL) / (CAST (w.destAmount AS DECIMAL)), -- compute pro-rata based on src token if it's augustus executor
-        -- TODO: also add 3rd party executor fee compensation -- based on spent native token, divided by amount of orders if multiple
+        n.price * tx.gas_used * tx.gas_price / POWER(10, n.decimals) / COALESCE(tx.order_count, 1), -- divide by number of orders in tx if multiple
         0
     )  AS gas_fee_usd
 {% endmacro %}
