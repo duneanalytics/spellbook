83a321bde0d237fb2f0e24ac11b3a7bbabc2d529
Boxer <Floba.hs@gmail.com>
2025-05-13 19:26:16 +0200

    test if taking this out changes anything

--- dbt_subprojects/tokens/models/transfers_and_balances/base/tokens_base_balances_daily_agg_base.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/base/tokens_base_balances_daily_agg_base.sql
@@ -1,12 +1,7 @@
 {{ config(
         schema = 'tokens_base',
         alias = 'balances_daily_agg_base',
-        file_format = 'delta',
-        materialized='incremental',
-        incremental_strategy='merge',
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-        unique_key = ['day', 'unique_key'],
-        partition_by = ['day'],
+        materialized='view'
         )
 }}
 
