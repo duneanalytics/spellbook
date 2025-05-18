9fcbb7b35976a2c04cfcbebcc068db52860595aa
tomfutago <tomfutago@gmail.com>
2025-05-18 09:51:17 +0100

    refactor: use daily active stake & utils.days

--- dbt_subprojects/daily_spellbook/models/nexusmutual/ethereum/staking/nexusmutual_ethereum_staked_per_pool.sql
+++ dbt_subprojects/daily_spellbook/models/nexusmutual/ethereum/staking/nexusmutual_ethereum_staked_per_pool.sql
@@ -14,33 +14,42 @@
 with
 
 staking_pools as (
-  select distinct
+  select
     sp.pool_id,
     sp.pool_address,
     se.first_stake_event_date
-  from {{ ref('nexusmutual_ethereum_staking_pools') }} sp
+  from {{ ref('nexusmutual_ethereum_staking_pools_list') }} sp
     inner join (
       select
-        pool_address,
+        pool_id,
         cast(min(block_time) as date) as first_stake_event_date
       from {{ ref('nexusmutual_ethereum_staking_events') }}
       group by 1
-    ) se on sp.pool_address = se.pool_address
+    ) se on sp.pool_id = se.pool_id
+),
+
+active_stake_daily as (
+  select
+    pool_id,
+    pool_address,
+    block_date,
+    active_stake as total_staked_nxm,
+    row_number() over (partition by pool_id order by block_date) as active_stake_event_rn
+  from {{ ref('nexusmutual_ethereum_active_stake_daily') }}
 ),
 
 staking_pool_day_sequence as (
   select
     sp.pool_id,
     sp.pool_address,
-    s.block_date
-  from staking_pools sp
-    cross join unnest (
-      sequence(
-        cast(date_trunc('day', sp.first_stake_event_date) as timestamp),
-        cast(date_trunc('day', now()) as timestamp),
-        interval '1' day
-      )
-    ) as s(block_date)
+    d.timestamp as block_date,
+    if(asu.block_date is null, true, false) as is_pre_active_stake_events
+  from {{ source('utils', 'days') }} d
+    inner join staking_pools sp on d.timestamp >=sp.first_stake_event_date
+    left join active_stake_daily asu
+      on sp.pool_id = asu.pool_id
+      and asu.active_stake_event_rn = 1
+      and d.timestamp >= asu.block_date
 ),
 
 staked_nxm_per_pool as (
@@ -48,8 +57,7 @@ staked_nxm_per_pool as (
     block_date,
     pool_id,
     pool_address,
-    sum(coalesce(total_amount, 0)) as total_staked_nxm,
-    dense_rank() over (partition by pool_id order by block_date desc) as pool_date_rn
+    sum(coalesce(total_amount, 0)) as total_staked_nxm
   from (
       -- deposits & deposit extensions
       select
@@ -59,9 +67,10 @@ staked_nxm_per_pool as (
         sum(se.amount) as total_amount
       from staking_pool_day_sequence d
         left join {{ ref('nexusmutual_ethereum_staking_deposit_extensions') }} se
-          on d.pool_address = se.pool_address
+          on d.pool_id = se.pool_id
          and d.block_date >= se.stake_start_date
          and d.block_date < se.stake_end_date
+      where d.is_pre_active_stake_events
       group by 1, 2, 3
       union all
       -- withdrawals & burns
@@ -72,13 +81,40 @@ staked_nxm_per_pool as (
         sum(se.amount) as total_amount
       from staking_pool_day_sequence d
         left join {{ ref('nexusmutual_ethereum_staking_events') }} se
-          on d.pool_address = se.pool_address
-         and d.block_date >= date_trunc('day', se.block_time)
+          on d.pool_id = se.pool_id
+         and d.block_date >= se.block_date
          and d.block_date < coalesce(se.tranche_expiry_date, current_date)
       where flow_type in ('withdraw', 'stake burn')
+        and d.is_pre_active_stake_events
       group by 1, 2, 3
     ) t
   group by 1, 2, 3
+),
+
+staked_nxn_per_pool_combined as (
+  select
+    block_date,
+    pool_id,
+    pool_address,
+    total_staked_nxm
+  from staked_nxm_per_pool
+  union all
+  select
+    block_date,
+    pool_id,
+    pool_address,
+    total_staked_nxm
+  from active_stake_daily
+),
+
+staked_nxm_per_pool_final as (
+  select
+    block_date,
+    pool_id,
+    pool_address,
+    total_staked_nxm,
+    row_number() over (partition by pool_id order by block_date desc) as pool_date_rn
+  from staked_nxn_per_pool_combined
 )
 
 select
@@ -87,4 +123,4 @@ select
   pool_address,
   total_staked_nxm,
   pool_date_rn
-from staked_nxm_per_pool
+from staked_nxm_per_pool_final
