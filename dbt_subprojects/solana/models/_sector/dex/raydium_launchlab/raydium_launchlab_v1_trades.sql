85974076e0eac42a142ad0c5c624a463c8442ce0
beve0x <gk0001va@gmail.com>
2025-05-19 16:42:59 -0600

    insert platform name to the v1 trades

--- dbt_subprojects/solana/models/_sector/dex/raydium_launchlab/raydium_launchlab_v1_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/raydium_launchlab/raydium_launchlab_v1_trades.sql
@@ -41,5 +41,14 @@ select
       , outer_instruction_index
       , inner_instruction_index
       , tx_index
-from {{ref('dex_solana_trades')}}
+      , base.platform_name
+      , base.platform_params
+from {{ref('dex_solana_trades')}} as dex_trades
+left join {{ref('raydium_launchlab_v1_base_trades')}} as base
+      on dex_trades.tx_id = base.tx_id 
+      and dex_trades.outer_instruction_index = base.outer_instruction_index
+      and COALESCE(dex_trades.inner_instruction_index, 0) = COALESCE(base.inner_instruction_index, 0)
+      and dex_trades.tx_index = base.tx_index
+      and dex_trades.block_slot = base.block_slot
+      and dex_trades.block_time = base.block_time
 where project = 'raydium_launchlab' and version = 1
