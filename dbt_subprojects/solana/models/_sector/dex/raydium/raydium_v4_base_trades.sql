cab99533a0f12b0f76dd0dfaab90d81e2f9e4ecc
jeff-dude <jeff@dune.xyz>
2025-05-20 17:38:55 -0400

    Revert "force short runtimes"
    This reverts commit 883e8126c228df331e8da47bf829ae432868680a.

--- dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v4_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v4_base_trades.sql
@@ -53,7 +53,7 @@
             AND ((sp.call_is_inner = false AND (trs_1.inner_instruction_index = 1 OR trs_1.inner_instruction_index = 2))
                 OR (sp.call_is_inner = true AND (trs_1.inner_instruction_index = sp.call_inner_instruction_index + 1 OR trs_1.inner_instruction_index = sp.call_inner_instruction_index + 2))
                 )
-            {% if is_incremental() or true %}
+            {% if is_incremental() %}
             AND {{incremental_predicate('trs_1.block_time')}}
             {% else %}
             AND trs_1.block_time >= TIMESTAMP '{{project_start_date}}'
@@ -65,7 +65,7 @@
             AND ((sp.call_is_inner = false AND (trs_2.inner_instruction_index = 2 OR trs_2.inner_instruction_index = 3))
                 OR (sp.call_is_inner = true AND (trs_2.inner_instruction_index = sp.call_inner_instruction_index + 2 OR trs_2.inner_instruction_index = sp.call_inner_instruction_index + 3))
                 )
-            {% if is_incremental() or true %}
+            {% if is_incremental() %}
             AND {{incremental_predicate('trs_2.block_time')}}
             {% else %}
             AND trs_2.block_time >= TIMESTAMP '{{project_start_date}}'
@@ -74,7 +74,7 @@
         WHERE 1=1
         and trs_1.token_mint_address != trs_2.token_mint_address --gets rid of dupes from the OR statement in transfer joins
         and tk_2.token_balance_owner = '5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1' --raydium pool v4 authority. makes sure we don't accidently catch some fee transfer or something after the swap. should add for lifinity too later.
-        {% if is_incremental() or true %}
+        {% if is_incremental() %}
         AND {{incremental_predicate('sp.call_block_time')}}
         {% else %}
         AND sp.call_block_time >= TIMESTAMP '{{project_start_date}}'
