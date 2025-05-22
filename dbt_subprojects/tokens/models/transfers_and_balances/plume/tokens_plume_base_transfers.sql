08925cbe8ab307c9706927073914cfeff8b33ff1
thecaptncrunch <138029744+thecaptncrunch@users.noreply.github.com>
2025-05-22 08:26:18 -0400

    Update dbt_subprojects/tokens/models/transfers_and_balances/plume/tokens_plume_base_transfers.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/tokens/models/transfers_and_balances/plume/tokens_plume_base_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/plume/tokens_plume_base_transfers.sql
@@ -15,6 +15,6 @@
     traces = source('plume','traces'),
     transactions = source('plume','transactions'),
     erc20_transfers = source('erc20_plume','evt_Transfer'),
-    native_contract_address = null
+    native_contract_address = var('ETH_ERC20_ADDRESS')
 )
 }}
