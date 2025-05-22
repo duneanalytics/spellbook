6dc10e093f2a2219e1eae4c02d198366c817987b
jeff-dude <jeff@dune.xyz>
2025-05-22 18:14:23 -0400

    fix group by

--- dbt_subprojects/daily_spellbook/models/sushiswap/optimism/sushiswap_optimism_pool_incentives_mappings.sql
+++ dbt_subprojects/daily_spellbook/models/sushiswap/optimism/sushiswap_optimism_pool_incentives_mappings.sql
@@ -14,7 +14,7 @@ FROM (
         chain as blockchain, contract_address, pid, lpToken AS lp_address
         FROM {{ source('sushi_multichain','minichefv2_evt_logpooladdition') }}
         WHERE chain = 'optimism'
-        GROUP BY 1,2,3
+        GROUP BY 1,2,3,4
 
 
 ) a (contract_address, pid, lp_address)
