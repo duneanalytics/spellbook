231d66b3612ef4ab8f4c538b59966e46436fb8da
Outputlayer <pavlo1crypto@gmail.com>
2025-05-23 19:56:44 +0200

    chore: remove metis chain from stargate bridge tracking

--- dbt_subprojects/daily_spellbook/models/stargate/metis/stargate_bridge_transfers_metis.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-  materialized='incremental',
-  unique_key=['block_time', 'block_number', 'user', 'dest', 'amount_raw'],
-  schema='stargate',
-  alias='stargate_bridge_transfers_metis'
-) }}
-
-{{ stargate_bridge_transfers('metis') }} 
