f907697cc15f5b27b439a35579d97ff9a1313e66
Outputlayer <pavlo1crypto@gmail.com>
2025-05-23 20:14:20 +0200

    chore: remove unused chains (moonriver, moonbeam, metis, kava, telos, bsc) from stargate bridge tracking

--- dbt_subprojects/daily_spellbook/models/stargate/telos/stargate_bridge_transfers_telos.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-  materialized='incremental',
-  unique_key=['block_time', 'block_number', 'user', 'dest', 'amount_raw'],
-  schema='stargate',
-  alias='stargate_bridge_transfers_telos'
-) }}
-
-{{ stargate_bridge_transfers('telos') }} 
