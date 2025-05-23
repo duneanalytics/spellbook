b3d7ba7426171b12b05037a1694f5b6cbc7e6c08
Outputlayer <pavlo1crypto@gmail.com>
2025-05-23 20:06:50 +0200

    chore: remove moonbeam chain from stargate bridge tracking

--- dbt_subprojects/daily_spellbook/models/stargate/moonbeam/stargate_bridge_transfers_moonbeam.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-  materialized='incremental',
-  unique_key=['block_time', 'block_number', 'user', 'dest', 'amount_raw'],
-  schema='stargate',
-  alias='stargate_bridge_transfers_moonbeam'
-) }}
-
-{{ stargate_bridge_transfers('moonbeam') }} 
