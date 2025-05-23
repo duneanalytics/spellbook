d315b5b5b7619e03cdc56a91be483f284b5f1aa1
Outputlayer <43842477+outputlayer@users.noreply.github.com>
2025-05-23 15:12:16 +0200

    Create stargate_bridge_transfers.sql

---
+++ dbt_subprojects/daily_spellbook/models/stargate/arbitrum/stargate_bridge_transfers.sql
@@ -0,0 +1,6 @@
+{{ config(
+  materialized='incremental',
+  unique_key=['block_time', 'block_number', 'user', 'dest', 'amount_raw'],
+  schema='stargate',
+  alias='stargate_bridge_transfers_arbitrum'
+) }}
