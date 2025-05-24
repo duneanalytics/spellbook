187b014ad1ae5d4f3b4487507d6c2af5b9388c8e
Outputlayer <43842477+outputlayer@users.noreply.github.com>
2025-05-24 17:10:52 +0200

    Update relay_token_transfers.sql

--- dbt_subprojects/daily_spellbook/macros/project/relay/relay_token_transfers.sql
+++ dbt_subprojects/daily_spellbook/macros/project/relay/relay_token_transfers.sql
@@ -30,7 +30,6 @@ excluded_addresses AS (
     ) AS t(address)
 ),
 
-contract_address
 txs AS (
     SELECT block_time, '{{ blockchain }}' AS blockchain, hash, block_number
     FROM {{ source(blockchain, 'transactions') }}
@@ -43,7 +42,23 @@ txs_erc20 AS (
     FROM {{ source(blockchain, 'transactions') }}
     WHERE "from" = 0xf70da97812CB96acDF810712Aa562db8dfA3dbEF
     AND "to" in ( SELECT contract_address FROM {{ ref('relay_token_metadata') }} WHERE blockchain = '{{ blockchain }}')
-    AND "to" in (SELECT address FROM excluded_addresses)
+    AND "to" in (
+        0xeeeeee9eC4769A09a76A83C7bC42b185872860eE,
+        0xe0b062d028236fa09fe33db8019ffeeee6bf79ed,
+        0xa1bea5fe917450041748dbbbe7e9ac57a4bbebab,
+        0x435bc1fa302256f0c4b704ae3b7ff322d5c1490c,
+        0xf605a9345401a0a3caacbcf9d891949218e0142d,
+        0x3db750fd20d09f7988b7331f412a516c524caf13,
+        0xa72dc2c494b030894699fa081b8974bdb094d3bd,
+        0x0649f2daa72a6524f0eb7f5f65e13cffc8082b10,
+        0x1d3a594eaf472ca2cec2a8ae44478c06d6a37e22,
+        0x610f3927901c41c219a1ad267df8073dbc883464,
+        0x6b7b5e5a75eb1c26689618c571143d255694c134,
+        0x85412871bce7432b9bd32eb9ff8c58997dd5a96c,
+        0x57732abcad29648c977c18a39bdf474436bba973,
+        0xe377e13256002ab260e8ab59478652710a79ac5c,
+        0x836caf2409d91df0bda01bc9f3cec524ba1c571d
+    )
       AND success = TRUE
 ),
 
