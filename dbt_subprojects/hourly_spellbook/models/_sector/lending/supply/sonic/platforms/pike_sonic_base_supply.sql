04cc44c1a7f0466ca6a7aaa8e0811916eaf35698
brunota20 <brunot.anjos@gmail.com>
2025-05-28 08:44:33 -0300

    chore: comment pike_sonic supply spell

--- dbt_subprojects/hourly_spellbook/models/_sector/lending/supply/sonic/platforms/pike_sonic_base_supply.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/lending/supply/sonic/platforms/pike_sonic_base_supply.sql
@@ -1,21 +1,21 @@
-{{
-  config(
-    schema = 'pike_sonic',
-    alias = 'base_supply',
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    unique_key = ['transaction_type', 'token_address', 'tx_hash', 'evt_index'],
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
-  )
-}}
+-- {{
+--   config(
+--     schema = 'pike_sonic',
+--     alias = 'base_supply',
+--     materialized = 'incremental',
+--     file_format = 'delta',
+--     incremental_strategy = 'merge',
+--     unique_key = ['transaction_type', 'token_address', 'tx_hash', 'evt_index'],
+--     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+--   )
+-- }}
 
-{{
-  lending_pike_compatible_supply(
-    blockchain = 'sonic',
-    project = 'pike',
-    version = '1',
-    deploy_market_table = 'factory_call_deploymarket',
-    evt_transfer_table = 'ptoken_evt_transfer'
-  )
-}}
+-- {{
+--   lending_pike_compatible_supply(
+--     blockchain = 'sonic',
+--     project = 'pike',
+--     version = '1',
+--     deploy_market_table = 'factory_call_deploymarket',
+--     evt_transfer_table = 'ptoken_evt_transfer'
+--   )
+-- }}
