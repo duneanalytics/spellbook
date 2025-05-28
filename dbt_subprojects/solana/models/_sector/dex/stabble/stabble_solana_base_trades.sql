529f94be43c3350e4fd991c7007b8b0ea75db157
beve0x <gk0001va@gmail.com>
2025-05-28 12:17:04 -0600

    w/o left join

--- dbt_subprojects/solana/models/_sector/dex/stabble/stabble_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/stabble/stabble_solana_base_trades.sql
@@ -16,26 +16,28 @@
 WITH all_swaps AS (
     -- Stable Swap V1
     SELECT
-        'stable_swap_v1' AS swap_type,
-        call_block_time AS block_time,
-        call_block_slot AS block_slot,
-        'stabble' AS project,
-        1 AS version,
-        'solana' AS blockchain,
-        CASE WHEN call_is_inner = FALSE THEN 'direct' ELSE call_outer_executing_account END AS trade_source,
-        amount_in AS token_sold_amount_raw,
-        account_pool AS pool_id,
-        call_tx_signer AS trader_id,
-        call_tx_id AS tx_id,
-        call_outer_instruction_index AS outer_instruction_index,
-        COALESCE(call_inner_instruction_index, 0) AS inner_instruction_index,
-        call_tx_index AS tx_index,
-        CAST(NULL AS VARCHAR) AS token_sold_mint_address,      
-        CAST(NULL AS VARCHAR) AS token_bought_mint_address,    
-        account_user_token_in,
-        account_user_token_out,    
-        account_vault_token_in AS token_sold_vault,
-        account_vault_token_out AS token_bought_vault
+        'stable_swap_v1' AS swap_type
+        , 'stabble' AS project
+        , 1 AS version
+        , 'solana' AS blockchain
+        , 'stable_swap_solana' AS project_program_id
+        , 'vo1tWgqZMjG61Z2T9qUaMYKqZ75CYzMuaZ2LZP1n7HV' AS project_main_id
+        , call_block_time AS block_time
+        , call_block_slot AS block_slot
+        , CASE WHEN call_is_inner = FALSE THEN 'direct' ELSE call_outer_executing_account END AS trade_source
+        , amount_in AS token_sold_amount_raw
+        , account_pool AS pool_id
+        , call_tx_signer AS trader_id
+        , call_tx_id AS tx_id
+        , call_outer_instruction_index AS outer_instruction_index
+        , COALESCE(call_inner_instruction_index, 0) AS inner_instruction_index
+        , call_tx_index AS tx_index
+        , CAST(NULL AS VARCHAR) AS token_sold_mint_address      
+        , CAST(NULL AS VARCHAR) AS token_bought_mint_address    
+        , account_user_token_in
+        , account_user_token_out    
+        , account_vault_token_in AS token_sold_vault
+        , account_vault_token_out AS token_bought_vault
     FROM {{ source('stable_swap_solana', 'stable_swap_call_swap') }}
     {% if is_incremental() %}
     WHERE {{incremental_predicate('call_block_time')}}
@@ -47,26 +49,26 @@ WITH all_swaps AS (
 
     -- Stable Swap V2
     SELECT
-        'stable_swap_v2' AS swap_type,
-        call_block_time AS block_time,
-        call_block_slot AS block_slot,
-        'stabble' AS project,
-        1 AS version,
-        'solana' AS blockchain,
-        CASE WHEN call_is_inner = FALSE THEN 'direct' ELSE call_outer_executing_account END,
-        amount_in AS token_sold_amount_raw,
-        account_pool AS pool_id,
-        call_tx_signer AS trader_id,
-        call_tx_id AS tx_id,
-        call_outer_instruction_index AS outer_instruction_index,
-        COALESCE(call_inner_instruction_index, 0) AS inner_instruction_index,
-        call_tx_index AS tx_index,
-        account_mint_in AS token_sold_mint_address,      
-        account_mint_out AS token_bought_mint_address,    
-        account_user_token_in,      
-        account_user_token_out,    
-        account_vault_token_in AS token_sold_vault,
-        account_vault_token_out AS token_bought_vault
+        'stable_swap_v2' AS swap_type
+        , call_block_time AS block_time
+        , call_block_slot AS block_slot
+        , 'stabble' AS project
+        , 1 AS version
+        , 'solana' AS blockchain
+        , CASE WHEN call_is_inner = FALSE THEN 'direct' ELSE call_outer_executing_account END
+        , amount_in AS token_sold_amount_raw
+        , account_pool AS pool_id
+        , call_tx_signer AS trader_id
+        , call_tx_id AS tx_id
+        , call_outer_instruction_index AS outer_instruction_index
+        , COALESCE(call_inner_instruction_index, 0) AS inner_instruction_index
+        , call_tx_index AS tx_index
+        , account_mint_in AS token_sold_mint_address      
+        , account_mint_out AS token_bought_mint_address    
+        , account_user_token_in      
+        , account_user_token_out    
+        , account_vault_token_in AS token_sold_vault
+        , account_vault_token_out AS token_bought_vault
     FROM {{ source('stable_swap_solana', 'stable_swap_call_swap_v2') }}
     {% if is_incremental() %}
     WHERE {{incremental_predicate('call_block_time')}}
@@ -78,26 +80,26 @@ WITH all_swaps AS (
 
     -- Weighted Swap V1
     SELECT
-        'weighted_swap_v1' AS swap_type,
-        call_block_time AS block_time,
-        call_block_slot AS block_slot,
-        'stabble' AS project,
-        1 AS version,
-        'solana' AS blockchain,
-        CASE WHEN call_is_inner = FALSE THEN 'direct' ELSE call_outer_executing_account END AS trade_source,
-        amount_in AS token_sold_amount_raw,
-        account_pool AS pool_id,
-        call_tx_signer AS trader_id,
-        call_tx_id AS tx_id,
-        call_outer_instruction_index AS outer_instruction_index,
-        COALESCE(call_inner_instruction_index, 0) AS inner_instruction_index,
-        call_tx_index AS tx_index,
-        CAST(NULL AS VARCHAR) AS token_sold_mint_address,      
-        CAST(NULL AS VARCHAR) AS token_bought_mint_address,    
-        account_user_token_in,      
-        account_user_token_out,    
-        account_vault_token_in AS token_sold_vault,
-        account_vault_token_out AS token_bought_vault
+        'weighted_swap_v1' AS swap_type
+        , call_block_time AS block_time
+        , call_block_slot AS block_slot
+        , 'stabble' AS project
+        , 1 AS version
+        , 'solana' AS blockchain
+        , CASE WHEN call_is_inner = FALSE THEN 'direct' ELSE call_outer_executing_account END AS trade_source
+        , amount_in AS token_sold_amount_raw
+        , account_pool AS pool_id
+        , call_tx_signer AS trader_id
+        , call_tx_id AS tx_id
+        , call_outer_instruction_index AS outer_instruction_index
+        , COALESCE(call_inner_instruction_index, 0) AS inner_instruction_index
+        , call_tx_index AS tx_index
+        , CAST(NULL AS VARCHAR) AS token_sold_mint_address      
+        , CAST(NULL AS VARCHAR) AS token_bought_mint_address    
+        , account_user_token_in      
+        , account_user_token_out    
+        , account_vault_token_in AS token_sold_vault
+        , account_vault_token_out AS token_bought_vault
     FROM {{ source('stable_swap_solana', 'weighted_swap_call_swap') }}
     {% if is_incremental() %}
     WHERE {{incremental_predicate('call_block_time')}}
@@ -109,26 +111,26 @@ WITH all_swaps AS (
 
     -- Weighted Swap V2
     SELECT
-        'weighted_swap_v2' AS swap_type,
-        call_block_time AS block_time,
-        call_block_slot AS block_slot,
-        'stabble' AS project,
-        1 AS version,
-        'solana' AS blockchain,
-        CASE WHEN call_is_inner = FALSE THEN 'direct' ELSE call_outer_executing_account END AS trade_source,
-        amount_in AS token_sold_amount_raw,
-        account_pool AS pool_id,
-        call_tx_signer AS trader_id,
-        call_tx_id AS tx_id,
-        call_outer_instruction_index AS outer_instruction_index,
-        COALESCE(call_inner_instruction_index, 0) AS inner_instruction_index,
-        call_tx_index AS tx_index,
-        account_mint_in AS token_sold_mint_address,      
-        account_mint_out AS token_bought_mint_address,    
-        account_user_token_in,      
-        account_user_token_out,    
-        account_vault_token_in AS token_sold_vault,
-        account_vault_token_out AS token_bought_vault
+        'weighted_swap_v2' AS swap_type
+        , call_block_time AS block_time
+        , call_block_slot AS block_slot
+        , 'stabble' AS project
+        , 1 AS version
+        , 'solana' AS blockchain
+        , CASE WHEN call_is_inner = FALSE THEN 'direct' ELSE call_outer_executing_account END AS trade_source
+        , amount_in AS token_sold_amount_raw
+        , account_pool AS pool_id
+        , call_tx_signer AS trader_id
+        , call_tx_id AS tx_id
+        , call_outer_instruction_index AS outer_instruction_index
+        , COALESCE(call_inner_instruction_index, 0) AS inner_instruction_index
+        , call_tx_index AS tx_index
+        , account_mint_in AS token_sold_mint_address      
+        , account_mint_out AS token_bought_mint_address    
+        , account_user_token_in      
+        , account_user_token_out    
+        , account_vault_token_in AS token_sold_vault
+        , account_vault_token_out AS token_bought_vault
     FROM {{ source('stable_swap_solana', 'weighted_swap_call_swap_v2') }}
     {% if is_incremental() %}
     WHERE {{incremental_predicate('call_block_time')}}
@@ -139,13 +141,26 @@ WITH all_swaps AS (
 
 , transfers AS (
     SELECT
-        s.tx_id,
-        s.block_slot,
-        s.outer_instruction_index,
-        s.inner_instruction_index as swap_inner_idx,
-        t.inner_instruction_index as transfer_inner_idx,
-        t.amount,
-        t.token_mint_address
+        s.blockchain
+        , s.project
+        , s.version
+        , s.block_time
+        , s.block_slot
+        , s.trade_source
+        , t.amount AS token_bought_amount_raw
+        , s.token_sold_amount_raw
+        , CAST(NULL AS DOUBLE) AS fee_tier
+        , COALESCE(s.token_sold_mint_address, t.token_mint_address) AS token_sold_mint_address
+        , COALESCE(s.token_bought_mint_address, t.token_mint_address) AS token_bought_mint_address
+        , s.token_sold_vault
+        , s.token_bought_vault
+        , s.pool_id AS project_program_id
+        , 'vo1tWgqZMjG61Z2T9qUaMYKqZ75CYzMuaZ2LZP1n7HV' AS project_main_id
+        , s.trader_id
+        , s.tx_id
+        , s.outer_instruction_index
+        , s.inner_instruction_index
+        , s.tx_index
     FROM all_swaps s
     INNER JOIN {{ ref('tokens_solana_transfers') }} t
         ON t.tx_id = s.tx_id 
@@ -167,30 +182,25 @@ WITH all_swaps AS (
 )
 
 SELECT
-    s.blockchain,
-    s.project,
-    s.version,
-    CAST(DATE_TRUNC('month', s.block_time) AS DATE) AS block_month,
-    s.block_time,
-    s.block_slot,
-    s.trade_source,
-    ot.amount AS token_bought_amount_raw, 
-    s.token_sold_amount_raw, 
-    CAST(NULL AS DOUBLE) AS fee_tier,
-    COALESCE(s.token_sold_mint_address, ot.token_mint_address) AS token_sold_mint_address,
-    COALESCE(s.token_bought_mint_address, ot.token_mint_address) AS token_bought_mint_address,
-    s.token_sold_vault,
-    s.token_bought_vault,
-    s.pool_id AS project_program_id,
-    'vo1tWgqZMjG61Z2T9qUaMYKqZ75CYzMuaZ2LZP1n7HV' AS project_main_id,
-    s.trader_id,
-    s.tx_id,
-    s.outer_instruction_index,
-    s.inner_instruction_index,
-    s.tx_index
-FROM all_swaps s
-LEFT JOIN transfers ot
-    ON ot.tx_id = s.tx_id
-    AND ot.block_slot = s.block_slot
-    AND ot.outer_instruction_index = s.outer_instruction_index
-    AND ot.swap_inner_idx = s.inner_instruction_index
+    blockchain
+    , project
+    , version
+    , CAST(DATE_TRUNC('month', block_time) AS DATE) AS block_month
+    , block_time
+    , block_slot
+    , trade_source
+    , token_bought_amount_raw 
+    , token_sold_amount_raw 
+    , fee_tier
+    , token_sold_mint_address
+    , token_bought_mint_address
+    , token_sold_vault
+    , token_bought_vault
+    , project_program_id
+    , project_main_id
+    , trader_id
+    , tx_id
+    , outer_instruction_index
+    , inner_instruction_index
+    , tx_index
+FROM transfers s
