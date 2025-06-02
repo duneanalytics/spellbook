2320060694b79bdb55f080c10d2eff316f823633
beve0x <gk0001va@gmail.com>
2025-06-02 14:59:33 -0600

    changed appraoch to prices_usd_native

--- dbt_subprojects/hourly_spellbook/models/_sector/prices/prices_usd_native.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/prices/prices_usd_native.sql
@@ -6,26 +6,24 @@
 -- this is a TEMPORARY spell that should be incorporated in the general prices models.
 -- more discussion here: https://github.com/duneanalytics/spellbook/issues/6577
 
-WITH blockchains as
-(
-select
-    evm.blockchain
-    ,evm.native_token_symbol as symbol
-    ,{{var('ETH_ERC20_ADDRESS')}} as contract_address -- 0x00..00
-    ,18 as decimals
-from {{source('evms','info')}} evm
-inner join {{source('prices_native','tokens')}} p
-on native_token_symbol = p.symbol
+WITH native_tokens as (
+    SELECT
+        b.name AS blockchain,
+        b.token_symbol AS symbol,
+        b.token_address AS contract_address,
+        b.token_decimals AS decimals
+    FROM {{ source('dune', 'blockchains') }}
+    WHERE is_evm = true
 )
 
 SELECT
-  b.blockchain
-, b.contract_address
-, b.decimals
-, b.symbol
-, p.minute
-, p.price
-FROM {{ source('prices', 'usd') }} p
-INNER JOIN blockchains b
-ON b.symbol = p.symbol
-and p.blockchain is null
+    t.blockchain,
+    t.contract_address,
+    t.decimals,
+    t.symbol,
+    p.timestamp,
+    p.price
+FROM {{ source('prices', 'minute') }} p
+INNER JOIN native_tokens t
+ON t.blockchain = p.blockchain
+and t.contract_address = p.contract_address
--- dbt_subprojects/hourly_spellbook/models/_sector/prices/prices_usd_with_native.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/prices/prices_usd_with_native.sql
@@ -6,72 +6,20 @@
 -- this is a TEMPORARY spell that should be incorporated in the general prices models.
 -- more discussion here: https://github.com/duneanalytics/spellbook/issues/6577
 
-
-WITH native_token_prices AS (
-    SELECT
-        p.timestamp AS minute,
-        b.name AS blockchain,
-        b.token_address AS contract_address,
-        b.token_decimals AS decimals,
-        b.token_symbol AS symbol,
-        p.price
-    FROM {{ source('prices', 'day') }} p
-    INNER JOIN {{ source('dune', 'blockchains') }} b
-        ON p.blockchain = b.name
-        AND p.contract_address = b.token_address
-),
-
-fallback_native_prices AS (
-    SELECT
-        f.minute,
-        f.blockchain,
-        f.contract_address,
-        f.decimals,
-        f.symbol,
-        f.price
-    FROM (
-        SELECT
-            minute,
-            blockchain,
-            contract_address,
-            decimals,
-            symbol,
-            price
-        FROM {{ source('prices', 'usd') }}
-        UNION ALL
-        SELECT
-            minute,
-            blockchain,
-            contract_address,
-            decimals,
-            symbol,
-            price
-        FROM {{ ref('prices_usd_native') }}
-    ) f
-    LEFT JOIN (
-        SELECT DISTINCT blockchain, contract_address FROM native_token_prices
-    ) n
-    ON f.blockchain = n.blockchain
-    AND f.contract_address = n.contract_address
-    WHERE n.blockchain IS NULL  
-)
-
-SELECT 
-    minute,
-    blockchain,
-    contract_address,
-    decimals,
-    symbol,
-    price
-FROM native_token_prices
-
-UNION ALL
-
-SELECT 
-    minute,
-    blockchain,
-    contract_address,
-    decimals,
-    symbol,
-    price
-FROM fallback_native_prices
+select
+    minute
+    ,blockchain
+    ,contract_address
+    ,decimals
+    ,symbol
+    ,price
+from {{source('prices','usd')}}
+union all
+select
+    minute
+    ,blockchain
+    ,contract_address
+    ,decimals
+    ,symbol
+    ,price
+from {{ref('prices_usd_native')}}
