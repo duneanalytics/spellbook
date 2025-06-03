974b43551daa9210d5f56755cc5b1991fdb2d7cd
jeff-dude <jeff@dune.xyz>
2025-06-03 18:25:36 -0400

    fix ink

--- dbt_subprojects/tokens/models/tokens/ink/tokens_ink_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/ink/tokens_ink_erc20.sql
@@ -15,4 +15,5 @@ FROM
 (
     VALUES
     -- placeholder rows to give example of format, tokens missing in automated tokens.erc20
+    (0x4200000000000000000000000000000000000006, 'WETH', 18)
 ) AS temp_table (contract_address, symbol, decimals)
