964f6626ae3af7d83a165c2310bbac0320c9d4ee
brunota20 <brunot.anjos@gmail.com>
2025-06-05 14:22:55 -0300

    fix: fetch data from enriched query

--- dbt_subprojects/dex/models/_projects/tapio/tapio_trades.sql
+++ dbt_subprojects/dex/models/_projects/tapio/tapio_trades.sql
@@ -1,55 +1,37 @@
 {{ config(
-        schema = 'tapio',
-        alias = 'trades',
-        materialized = 'view',
-        post_hook='{{ expose_spells(blockchains = \'["base", "sonic"]\',
-                                      spell_type = "project", 
-                                      spell_name = "tapio", 
-                                      contributors = \'["brunota20"]\') }}'
-        )
-}}
+    schema = 'tapio',
+    alias = 'trades',
+    materialized = 'view',
+    post_hook='{{ expose_spells(blockchains = \'["base", "sonic"]\',
+                                  spell_type = "project",
+                                  spell_name = "tapio",
+                                  contributors = \'["brunota20"]\') }}'
+) }}
 
-{% set tapio_models = [
-    ref('tapio_base_base_trades'),
-    ref('tapio_sonic_base_trades')
-] %}
-
-
-SELECT *
-FROM (
-    {% for model in tapio_models %}
-        SELECT 
-                blockchain,
-                project,
-                version,
-                block_date,
-                block_number,
-                block_month,
-                block_time,
-                token_bought_symbol,
-                token_sold_symbol,
-                token_pair,
-                token_bought_amount,
-                token_sold_amount,
-                token_bought_amount_raw,
-                token_sold_amount_raw,
-                amount_usd,
-                token_bought_address,
-                token_sold_address,
-                taker,
-                maker,
-                project_contract_address,
-                pool_id,
-                swap_fee,
-                pool_symbol,
-                pool_type,
-                tx_hash,
-                tx_from,
-                tx_to,
-                evt_index
-        FROM {{ model }}
-        {% if not loop.last %}
-        UNION ALL
-        {% endif %}
-        {% endfor %}
-)
+SELECT  
+    blockchain,
+    project,
+    version,
+    block_month,
+    block_date,
+    block_time,
+    block_number,
+    token_bought_symbol,
+    token_sold_symbol,
+    token_pair,
+    token_bought_amount,
+    token_sold_amount,
+    token_bought_amount_raw,
+    token_sold_amount_raw,
+    amount_usd,
+    token_bought_address,
+    token_sold_address,
+    taker,
+    maker,
+    project_contract_address,
+    tx_hash,
+    tx_from,
+    tx_to,
+    evt_index
+FROM {{ ref('dex_trades') }}
+WHERE project = 'tapio'
