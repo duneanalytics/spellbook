d694cc99134e22a3eace0f7562c69d327e71e88a
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2025-06-07 11:24:11 -0400

    Update fluid_v1_polygon_base_trades.sql

--- dbt_subprojects/dex/models/trades/polygon/platforms/fluid_v1_polygon_base_trades.sql
+++ dbt_subprojects/dex/models/trades/polygon/platforms/fluid_v1_polygon_base_trades.sql
@@ -37,7 +37,7 @@ WITH
       FROM {{ source('fluid_polygon', 'FluidDexT1_evt_Swap') }} t
       INNER JOIN {{ source('fluid_polygon', 'FluidDexFactory_evt_LogDexDeployed') }} p
           ON t.contract_address = p.dex
-      {% IF is_incremental() %}
+      {% if is_incremental() %}
       WHERE {{ incremental_predicate('t.evt_block_time') }}
       {% endif %}
   )
