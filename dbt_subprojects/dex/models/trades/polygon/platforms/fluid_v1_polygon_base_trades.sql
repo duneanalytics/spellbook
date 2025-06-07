65b668731a4326c71133166553838255032fd3cf
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2025-06-07 11:08:27 -0400

    Create fluid_v1_polygon_base_trades.sql

---
+++ dbt_subprojects/dex/models/trades/polygon/platforms/fluid_v1_polygon_base_trades.sql
@@ -0,0 +1,62 @@
+{{
+    config(
+        schema = 'fluid_v1_polygon',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+WITH 
+  decoded_events AS (
+      SELECT
+          '1' AS version
+          , t.evt_block_number AS block_number
+          , t.evt_block_time AS block_time
+          , t.to AS taker
+          , CAST(NULL AS VARBINARY) AS maker
+          , t.amountOut AS token_bought_amount_raw
+          , t.amountIn AS token_sold_amount_raw
+          , CASE 
+              WHEN swap0to1 
+              THEN p.borrow_token
+              ELSE p.supply_token 
+          END AS token_bought_address
+          , CASE 
+              WHEN 
+              NOT(swap0to1) 
+              THEN p.borrow_token
+              ELSE p.supply_token 
+          END AS token_sold_address
+          , t.contract_address AS project_contract_address
+          , t.evt_tx_hash AS tx_hash
+          , t.evt_index
+      FROM {{ source('fluid_polygon', 'FluidDexT1_evt_Swap') }} t
+      INNER JOIN {{ source('fluid_polygon', 'FluidDexFactory_evt_LogDexDeployed') }} p
+          ON t.contract_address = p.dex
+      {% IF is_incremental() %}
+      WHERE {{ incremental_predicate('t.evt_block_time') }}
+      {% endif %}
+  )
+
+SELECT
+    'polygon' AS blockchain
+    , 'fluid' AS project
+    , dexs.version
+    , CAST(date_trunc('month', dexs.block_time) AS date) AS block_month
+    , CAST(date_trunc('day', dexs.block_time) AS date) AS block_date
+    , dexs.block_time
+    , dexs.block_number
+    , dexs.token_bought_amount_raw
+    , dexs.token_sold_amount_raw
+    , dexs.token_bought_address
+    , dexs.token_sold_address
+    , dexs.taker
+    , dexs.maker
+    , dexs.project_contract_address
+    , dexs.tx_hash
+    , dexs.evt_index
+FROM decoded_events dexs 
