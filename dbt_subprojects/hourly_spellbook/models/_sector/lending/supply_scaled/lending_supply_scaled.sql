9c036e2e20980e0ba856ed49529d07c77341adba
tomfutago <tomfutago@gmail.com>
2025-06-10 15:26:50 +0100

    fix: temp remove high volume chains

--- dbt_subprojects/hourly_spellbook/models/_sector/lending/supply_scaled/lending_supply_scaled.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/lending/supply_scaled/lending_supply_scaled.sql
@@ -12,18 +12,13 @@
 
 {%
   set models = [
-    ref('lending_arbitrum_base_supply_scaled'),
     ref('lending_avalanche_c_base_supply_scaled'),
-    ref('lending_base_base_supply_scaled'),
     ref('lending_bnb_base_supply_scaled'),
     ref('lending_celo_base_supply_scaled'),
     ref('lending_ethereum_base_supply_scaled'),
     ref('lending_fantom_base_supply_scaled'),
     ref('lending_gnosis_base_supply_scaled'),
     ref('lending_linea_base_supply_scaled'),
-    ref('lending_optimism_base_supply_scaled'),
-    ref('lending_polygon_base_supply_scaled'),
-    ref('lending_scroll_base_supply_scaled'),
     ref('lending_sonic_base_supply_scaled'),
     ref('lending_zksync_base_supply_scaled')
   ]
