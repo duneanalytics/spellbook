56f2f73192b19b89a340006ee5b4ccc66594bdb7
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2025-06-13 11:52:24 -0400

    refactor likely bots & add chains

--- dbt_subprojects/daily_spellbook/models/_sector/labels/addresses/__single_category_labels__/likely_bots/base/labels_base_likely_bot_contracts.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/labels/addresses/__single_category_labels__/likely_bots/base/labels_base_likely_bot_contracts.sql
@@ -2,11 +2,11 @@
     config(
         alias = 'likely_bot_contracts',
 
-        post_hook='{{ expose_spells('["optimism"]',
+        post_hook='{{ expose_spells('["base"]',
         "sector",
         "labels",
         '["msilb7"]') }}'
     )
 }}
 
-{{ get_likely_bot_contracts('optimism') }}
+{{ get_likely_bot_contracts('base') }}
--- dbt_subprojects/daily_spellbook/models/_sector/labels/addresses/__single_category_labels__/likely_bots/bob/labels_bob_likely_bot_contracts.sql
+++ dbt_subprojects/daily_spellbook/models/_sector/labels/addresses/__single_category_labels__/likely_bots/bob/labels_bob_likely_bot_contracts.sql
@@ -2,11 +2,11 @@
     config(
         alias = 'likely_bot_contracts',
 
-        post_hook='{{ expose_spells('["optimism"]',
+        post_hook='{{ expose_spells('["bob"]',
         "sector",
         "labels",
         '["msilb7"]') }}'
     )
 }}
 
-{{ get_likely_bot_contracts('optimism') }}
+{{ get_likely_bot_contracts('bob') }} 
