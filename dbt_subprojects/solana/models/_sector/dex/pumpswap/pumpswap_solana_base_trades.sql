e4bda9d5d86ca3130c037a1cb6521b471c9b1761
beve0x <gk0001va@gmail.com>
2025-06-16 14:10:17 -0400

    Revert "test"
    This reverts commit 17044dae44ec6189b7090c4fe34e5132945156f0.

--- dbt_subprojects/solana/models/_sector/dex/pumpswap/pumpswap_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/pumpswap/pumpswap_solana_base_trades.sql
@@ -51,9 +51,8 @@ WITH pools AS (
     WHERE {{incremental_predicate('call_block_time')}}
     {% else %}
     WHERE call_block_time >= TIMESTAMP '{{project_start_date}}'
-    
     {% endif %}
-    AND call_block_time >= now() - interval '5' day
+    
     UNION ALL
     
     -- Sell operations
@@ -83,7 +82,6 @@ WITH pools AS (
     {% else %}
     WHERE call_block_time >= TIMESTAMP '{{project_start_date}}'
     {% endif %}
-    AND call_block_time >= now() - interval '5' day
 )
 
 , fee_configs_with_time_ranges AS (
@@ -143,8 +141,6 @@ WITH pools AS (
         {% else %}
         AND t.block_time >= TIMESTAMP '{{project_start_date}}'
         {% endif %}
-        AND t.block_time >= now() - interval '5' day
-
 )
 
 , trades_base as (
