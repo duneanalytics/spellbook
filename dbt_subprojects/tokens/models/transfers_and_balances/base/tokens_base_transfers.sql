a537ae3db13adacac826bf8a12bdb636e998f3ea
jeff-dude <jeff@dune.xyz>
2025-06-16 20:52:04 -0400

    update base model back to normal config, update new prices

--- dbt_subprojects/tokens/models/transfers_and_balances/base/tokens_base_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/base/tokens_base_transfers.sql
@@ -1,6 +1,6 @@
 {{config(
-    schema = 'test_schema',
-    alias = 'jeff_test_tokens_base_transfers_minute',
+    schema = 'tokens_base',
+    alias = 'transfers',
     partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
@@ -18,8 +18,8 @@
     transfers_enrich(
         base_transfers = ref('tokens_base_base_transfers')
         , tokens_erc20_model = source('tokens', 'erc20')
-        , prices_model = source('prices', 'minute')
-        , transfers_start_date = '2025-05-01'
+        , prices_model = source('prices', 'hour')
+        , transfers_start_date = '2023-06-15'
         , blockchain = 'base'
     )
 }}
