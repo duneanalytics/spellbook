2a2062bb88f013b9542d3138b97842a4c2ef239d
jeff-dude <jeff@dune.xyz>
2025-06-17 12:19:08 -0400

    test append strat

--- dbt_subprojects/tokens/macros/transfers/transfers_enrich.sql
+++ dbt_subprojects/tokens/macros/transfers/transfers_enrich.sql
@@ -74,35 +74,50 @@ WITH base_transfers as (
         AND t.blockchain = prices.blockchain
         AND t.contract_address = prices.contract_address
 )
+, final as (
+    SELECT
+        unique_key
+        , blockchain
+        , block_month
+        , block_date
+        , block_time
+        , block_number
+        , tx_hash
+        , evt_index
+        , trace_address
+        , token_standard
+        , tx_from
+        , tx_to
+        , tx_index
+        , "from"
+        , to
+        , contract_address
+        , symbol
+        , amount_raw
+        , amount
+        , price_usd
+        , amount_usd
+        /*
+        , CASE
+            WHEN amount_usd >= {{ usd_amount_threshold }}
+                THEN CAST(NULL as double)
+                ELSE amount_usd -- Select only transfers where USD amount is less than the threshold
+            END AS amount_usd
+        */
+    FROM
+        transfers
+)
 SELECT
-    unique_key
-    , blockchain
-    , block_month
-    , block_date
-    , block_time
-    , block_number
-    , tx_hash
-    , evt_index
-    , trace_address
-    , token_standard
-    , tx_from
-    , tx_to
-    , tx_index
-    , "from"
-    , to
-    , contract_address
-    , symbol
-    , amount_raw
-    , amount
-    , price_usd
-    , amount_usd
-    /*
-    , CASE
-        WHEN amount_usd >= {{ usd_amount_threshold }}
-            THEN CAST(NULL as double)
-            ELSE amount_usd -- Select only transfers where USD amount is less than the threshold
-        END AS amount_usd
-    */
+    src.*
 FROM
-    transfers
+    final as src
+{% if is_incremental() -%}
+LEFT JOIN
+    {{ this }} as tgt
+    ON src.unique_key = tgt.unique_key
+    AND src.block_month = tgt.block_month
+    AND src.block_date = tgt.block_date
+WHERE
+    tgt.unique_key IS NULL
+{% endif -%}
 {%- endmacro %}
--- dbt_subprojects/tokens/models/transfers_and_balances/b3/tokens_b3_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/b3/tokens_b3_transfers.sql
@@ -4,7 +4,7 @@
      , partition_by = ['block_month']
      , materialized = 'incremental'
      , file_format = 'delta'
-     , incremental_strategy = 'merge'
+     , incremental_strategy = 'append'
      , unique_key = ['block_date','unique_key']
      , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
      , post_hook='{{ expose_spells(blockchains = \'["b3"]\',
