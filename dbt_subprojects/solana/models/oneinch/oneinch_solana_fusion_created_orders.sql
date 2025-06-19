4725de89f410290c2686f9a71b5d79bbbc97e254
Eduard Horch <40689054+grkhr@users.noreply.github.com>
2025-06-20 05:18:27 +0700

    Update dbt_subprojects/solana/models/oneinch/oneinch_solana_fusion_created_orders.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/solana/models/oneinch/oneinch_solana_fusion_created_orders.sql
+++ dbt_subprojects/solana/models/oneinch/oneinch_solana_fusion_created_orders.sql
@@ -7,7 +7,7 @@
         file_format = 'delta',
         incremental_strategy = 'merge',
         incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-        unique_key = ['tx_id', 'order_hash']
+        unique_key = ['block_month', 'tx_id', 'order_hash']
     )
 }}
 
