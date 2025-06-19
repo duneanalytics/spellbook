14312ba1dbc9858250599fd5cdb9b3cb33132ee9
jeff-dude <jeff@dune.xyz>
2025-06-19 13:45:22 -0400

    update all relevant EVM macros + models

--- dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/apechain/gas_apechain_fees.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/apechain/gas_apechain_fees.sql
@@ -12,7 +12,12 @@
     )
 }}
 
-WITH base_model as (
+{% set test_short_ci = true %}
+
+WITH native_token_prices as (
+    {{ native_token_prices(blockchain) }}
+)
+, base_model as (
     SELECT
         txns.block_time
         ,txns.block_number
@@ -23,7 +28,6 @@ WITH base_model as (
         ,txns.gas_used as gas_used
         ,cast(base_fee_per_gas as uint256) * cast(txns.gas_used as uint256) as tx_fee_raw
         ,map(array['base_fee'], array[cast(base_fee_per_gas as uint256) * cast(txns.gas_used as uint256)]) as tx_fee_breakdown_raw
-        ,0x7f9fbf9bdd3f4105c478b996b648fe6e828a1e98 as tx_fee_currency
         ,blocks.miner AS block_proposer
         ,txns.max_fee_per_gas
         ,txns.priority_fee_per_gas
@@ -34,9 +38,7 @@ WITH base_model as (
             WHEN txns.gas_limit = 0 THEN NULL
             WHEN txns.gas_limit != 0 THEN cast(txns.gas_used as double) / cast(txns.gas_limit as double)
         END AS gas_limit_usage
-        {% if blockchain == 'apechain' %}
-        ,l1_gas_used
-        {% endif %}
+        ,txns.l1_gas_used
     FROM {{ source(blockchain, 'transactions') }} txns
     INNER JOIN {{ source(blockchain, 'blocks') }} blocks
         ON txns.block_number = blocks.number
@@ -53,41 +55,34 @@ WITH base_model as (
 
 SELECT
     '{{blockchain}}' as blockchain
-    ,CAST(date_trunc('month', block_time) AS DATE) AS block_month
-    ,CAST(date_trunc('day', block_time) AS DATE) AS block_date
-    ,block_time
-    ,block_number
-    ,tx_hash
-    ,tx_from
-    ,tx_to
-    ,gas_price
-    ,gas_used
+    ,CAST(date_trunc('month', b.block_time) AS DATE) AS block_month
+    ,CAST(date_trunc('day', b.block_time) AS DATE) AS block_date
+    ,b.block_time
+    ,b.block_number
+    ,b.tx_hash
+    ,b.tx_from
+    ,b.tx_to
+    ,b.gas_price
+    ,b.gas_used
     ,p.symbol as currency_symbol
-    ,coalesce(tx_fee_raw, 0) as tx_fee_raw
-    ,coalesce(tx_fee_raw, 0) / pow(10,p.decimals) as tx_fee
-    ,coalesce(tx_fee_raw, 0) / pow(10,p.decimals) * p.price as tx_fee_usd
-    ,transform_values(tx_fee_breakdown_raw,
+    ,coalesce(b.tx_fee_raw, 0) as tx_fee_raw
+    ,coalesce(b.tx_fee_raw, 0) / pow(10,p.decimals) as tx_fee
+    ,coalesce(b.tx_fee_raw, 0) / pow(10,p.decimals) * p.price as tx_fee_usd
+    ,transform_values(b.tx_fee_breakdown_raw,
             (k,v) -> coalesce(v, cast(0 as uint256))) as tx_fee_breakdown_raw
-    ,transform_values(tx_fee_breakdown_raw,
+    ,transform_values(b.tx_fee_breakdown_raw,
             (k,v) -> coalesce(v, cast(0 as uint256)) / pow(10,p.decimals) ) as tx_fee_breakdown
-    ,transform_values(tx_fee_breakdown_raw,
+    ,transform_values(b.tx_fee_breakdown_raw,
             (k,v) -> coalesce(v, cast(0 as uint256)) / pow(10,p.decimals) * p.price) as tx_fee_breakdown_usd
-    ,tx_fee_currency
-    ,block_proposer
-    ,max_fee_per_gas
-    ,priority_fee_per_gas
-    ,max_priority_fee_per_gas
-    ,base_fee_per_gas
-    ,gas_limit
-    ,gas_limit_usage
-    {% if blockchain == 'apechain' %}
-    ,l1_gas_used
-    {% endif %}
-FROM base_model
-LEFT JOIN {{ ref('prices_usd_with_native') }} p
-    ON p.blockchain = '{{blockchain}}'
-    AND p.contract_address = tx_fee_currency
-    AND p.minute = date_trunc('minute', block_time)
-    {% if is_incremental() %}
-    AND {{ incremental_predicate('p.minute') }}
-    {% endif %}
+    ,p.contract_address as tx_fee_currency
+    ,b.block_proposer
+    ,b.max_fee_per_gas
+    ,b.priority_fee_per_gas
+    ,b.max_priority_fee_per_gas
+    ,b.base_fee_per_gas
+    ,b.gas_limit
+    ,b.gas_limit_usage
+    ,b.l1_gas_used
+FROM base_model as b
+INNER JOIN native_token_prices as p
+    ON p.timestamp = date_trunc('hour', b.block_time)
--- dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/base/gas_base_fees.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/gas/fees/base/gas_base_fees.sql
@@ -12,9 +12,12 @@
     )
 }}
 
-{% set test_short_ci = false %}
+{% set test_short_ci = true %}
 
-WITH base_model as (
+WITH native_token_prices as (
+    {{ native_token_prices(blockchain) }}
+)
+, base_model as (
     SELECT
         txns.block_time
         ,txns.block_number
@@ -35,7 +38,6 @@ WITH base_model as (
                          )
                 end
         ) as tx_fee_breakdown_raw
-        ,{{var('ETH_ERC20_ADDRESS')}} as tx_fee_currency
         ,blocks.miner AS block_proposer
         ,txns.max_fee_per_gas
         ,txns.priority_fee_per_gas
@@ -65,42 +67,37 @@ WITH base_model as (
     )
 SELECT
     '{{blockchain}}' as blockchain
-    ,CAST(date_trunc('month', block_time) AS DATE) AS block_month
-    ,CAST(date_trunc('day', block_time) AS DATE) AS block_date
-    ,block_time
-    ,block_number
-    ,tx_hash
-    ,tx_from
-    ,tx_to
-    ,gas_price
-    ,gas_used
+    ,CAST(date_trunc('month', b.block_time) AS DATE) AS block_month
+    ,CAST(date_trunc('day', b.block_time) AS DATE) AS block_date
+    ,b.block_time
+    ,b.block_number
+    ,b.tx_hash
+    ,b.tx_from
+    ,b.tx_to
+    ,b.gas_price
+    ,b.gas_used
     ,p.symbol as currency_symbol
-    ,coalesce(tx_fee_raw, 0) as tx_fee_raw
-    ,coalesce(tx_fee_raw, 0) / pow(10,p.decimals) as tx_fee
-    ,coalesce(tx_fee_raw, 0) / pow(10,p.decimals) * p.price as tx_fee_usd
-    ,transform_values(tx_fee_breakdown_raw,
+    ,coalesce(b.tx_fee_raw, 0) as tx_fee_raw
+    ,coalesce(b.tx_fee_raw, 0) / pow(10,p.decimals) as tx_fee
+    ,coalesce(b.tx_fee_raw, 0) / pow(10,p.decimals) * p.price as tx_fee_usd
+    ,transform_values(b.tx_fee_breakdown_raw,
             (k,v) -> coalesce(v,0)) as tx_fee_breakdown_raw
-    ,transform_values(tx_fee_breakdown_raw,
+    ,transform_values(b.tx_fee_breakdown_raw,
             (k,v) -> coalesce(v, 0) / pow(10,p.decimals) ) as tx_fee_breakdown
-    ,transform_values(tx_fee_breakdown_raw,
+    ,transform_values(b.tx_fee_breakdown_raw,
             (k,v) -> coalesce(v, 0) / pow(10,p.decimals) * p.price) as tx_fee_breakdown_usd
-    ,tx_fee_currency
-    ,block_proposer
-    ,max_fee_per_gas
-    ,priority_fee_per_gas
-    ,max_priority_fee_per_gas
-    ,base_fee_per_gas
-    ,gas_limit
-    ,gas_limit_usage    
-    ,l1_fee
-    ,l1_gas_used
-    ,l1_gas_price
-    ,l1_fee_scalar
-FROM base_model
-LEFT JOIN {{ ref('prices_usd_with_native') }} p
-    ON p.blockchain = '{{blockchain}}'
-    AND p.contract_address = tx_fee_currency
-    AND p.minute = date_trunc('minute', block_time)
-    {% if is_incremental() %}
-    AND {{ incremental_predicate('p.minute') }}
-    {% endif %}
+    ,p.contract_address as tx_fee_currency
+    ,b.block_proposer
+    ,b.max_fee_per_gas
+    ,b.priority_fee_per_gas
+    ,b.max_priority_fee_per_gas
+    ,b.base_fee_per_gas
+    ,b.gas_limit
+    ,b.gas_limit_usage    
+    ,b.l1_fee
+    ,b.l1_gas_used
+    ,b.l1_gas_price
+    ,b.l1_fee_scalar
+FROM base_model as b
+INNER JOIN native_token_prices as p
+    ON p.timestamp = date_trunc('hour', b.block_time)
