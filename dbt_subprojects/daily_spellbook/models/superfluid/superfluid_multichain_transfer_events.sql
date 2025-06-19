f9618b706aab791560da70e8dc517c179ec04301
Aaron Ho <cincau.hangus@gmail.com>
2025-06-20 04:02:35 +0800

    fix: streamline columns, fix renamed query

--- dbt_subprojects/daily_spellbook/models/superfluid/superfluid_multichain_transfer_events.sql
+++ dbt_subprojects/daily_spellbook/models/superfluid/superfluid_multichain_transfer_events.sql
@@ -14,11 +14,11 @@ SELECT
     evt_block_date AS block_date,
     evt_tx_hash AS tx_hash,
     evt_index AS index,
-    contract_address,
+    contract_address AS token,
     "from",
     "to",
     "value"
 FROM {{ ref('evms_erc20_transfers') }} transfers
 WHERE (blockchain, contract_address) IN (
-    SELECT blockchain, token_address AS contract_address FROM {{ ref('superfluid_multichain_supertoken_addresses') }}
+    SELECT blockchain, token_address AS contract_address FROM {{ ref('superfluid_multichain_supertoken_created_events') }}
 )
