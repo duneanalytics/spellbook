a06f2ce7e0d570cffb15ad0a2e817cd5ad285592
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-06-19 18:33:44 +0100

    add liquidity_v2

--- dbt_subprojects/dex/macros/models/_project/uniswap_compatible_liquidity_v2.sql
+++ dbt_subprojects/dex/macros/models/_project/uniswap_compatible_liquidity_v2.sql
@@ -164,7 +164,6 @@ sort_table as (
     
 
     select 
-
         '{{blockchain}}' as blockchain
         , '{{project}}'  as project
         , '{{version}}' as version
@@ -179,5 +178,210 @@ sort_table as (
     get_events 
 
 {% endif %}
+
+{% endmacro %}
+
+{% macro uniswap_compatible_v4_base_liquidity( 
+    blockchain = null
+    , project = 'uniswap'
+    , version = '4'
+    , PoolManager_evt_ModifyLiquidity = null
+    , PoolManager_evt_Swap = null
+    , liquidity_pools = null
+    , liquidity_sqrtpricex96 = null
+    )
+%}
+
+
+with 
+
+get_pools as (
+    select 
+        blockchain
+        , id
+        , token0
+        , token1
+    from 
+    {{ liquidity_pools }}
+),
+
+get_prices_tmp as (
+    select
+        blockchain
+        , id
+        , block_index_sum 
+        , previous_block_index_sum
+        , sqrtpricex96
+    from 
+    {{ liquidity_sqrtpricex96 }}
+),
+
+get_latest_prices as (
+    select 
+        blockchain
+        , id 
+        , max(block_index_sum) as block_index_sum 
+        , max(previous_block_index_sum) as previous_block_index_sum 
+        , max_by(sqrtpricex96, block_index_sum) as sqrtpricex96
+    from 
+    get_prices_tmp 
+    group by 1, 2 
+),
+
+get_prices as (
+    select 
+        blockchain
+        , id
+        , block_index_sum 
+        , previous_block_index_sum
+        , sqrtpricex96
+    from 
+    get_prices_tmp 
+
+    union all 
+
+    select 
+        blockchain
+        , id
+        , block_index_sum + block_index_sum as block_index_sum 
+        , block_index_sum as previous_block_index_sum -- for filling
+        , sqrtpricex96
+    from 
+    get_latest_prices
+),
+
+modify_liquidity_events as (
+    with 
+    
+    get_events as (
+        select 
+              evt_block_time
+            , evt_block_number
+            , evt_block_number + evt_index/1e6 as block_index_sum
+            , id
+            , evt_tx_hash
+            , evt_index
+            , 'modify_liquidity' as event_type
+            , salt
+            , tickLower
+            , tickUpper
+            , liquidityDelta
+        from 
+        {{ PoolManager_evt_ModifyLiquidity }}
+        {%- if is_incremental() %}
+        where {{ incremental_predicate('evt_block_time') }}
+        {%- endif %} 
+    ),
+
+    add_latest_price as (
+        select 
+            ge.*
+            , gp.sqrtpricex96
+        from 
+        get_events ge 
+        left join 
+        get_prices gp 
+            on ge.id = gp.id 
+            and ge.block_index_sum >= gp.previous_block_index_sum
+            and ge.block_index_sum < gp.block_index_sum 
+    ),
+
+    prep_for_calculations as (
+        select 
+            * 
+            , sqrtpricex96 / power(2, 96) AS sqrtprice
+            , sqrt(power(1.0001, tickLower)) AS sqrtRatioL
+            , sqrt(power(1.0001, tickUpper)) AS sqrtRatioU
+        from 
+        add_latest_price
+    )
+
+    select 
+        *
+        , case
+            when sqrtPrice <= sqrtRatioL then liquidityDelta * ((sqrtRatioU - sqrtRatioL)/(sqrtRatioL*sqrtRatioU))
+            when sqrtPrice >= sqrtRatioU then 0
+            else liquidityDelta * ((sqrtRatioU - sqrtPrice) / (sqrtPrice * sqrtRatioU))
+          end as amount0
+        , case
+            when sqrtPrice <= sqrtRatioL then 0
+            when sqrtPrice >= sqrtRatioU then liquidityDelta * (sqrtRatioU - sqrtRatioL)
+            else liquidityDelta * (sqrtPrice - sqrtRatioL)
+          end as amount1
+    from 
+    modify_liquidity_events
+),
+
+swap_events as (
+    select 
+        evt_block_time
+        , evt_block_number 
+        , evt_tx_hash 
+        , evt_index 
+        , id 
+        , -1 * amount0 as amount0
+        , -1 * amount1 as amount1
+    from 
+    {{ PoolManager_evt_Swap }}
+    {%- if is_incremental() %}
+    where {{ incremental_predicate('evt_block_time') }}
+    {%- endif %}
+),
+
+liquidity_change_base as (
+    select 
+        ml.id
+        , ml.evt_block_time as block_time
+        , ml.evt_block_number as block_number 
+        , ml.evt_tx_hash as tx_hash 
+        , ml.evt_index 
+        , ml.event_type 
+        , gp.token0 
+        , gp.token1 
+        , ml.amount0 
+        , ml.amount1 
+    from 
+    modify_liquidity_events ml 
+    inner join 
+    get_pools gp 
+        on ml.id = gp.id 
+
+    union all 
+
+    select 
+        se.id
+        , se.evt_block_time as block_time
+        , se.evt_block_number as block_number 
+        , se.evt_tx_hash as tx_hash 
+        , se.evt_index 
+        , 'swap' as event_type 
+        , gp.token0 
+        , gp.token1 
+        , se.amount0 
+        , se.amount1 
+    from 
+    swap_events se
+    inner join 
+    get_pools gp 
+        on se.id = gp.id 
+)
+    select 
+          '{{blockchain}}' as blockchain
+        , '{{project}}'  as project
+        , '{{version}}' as version
+        , cast(date_trunc('month', base.block_time) as date) as block_month
+        , cast(date_trunc('day', base.block_time) as date) as block_date
+        , date_trunc('minute', block_time) as block_time -- for prices
+        , block_number
+        , id
+        , tx_hash
+        , evt_index
+        , event_type
+        , token0
+        , token1
+        , CAST(amount0 AS double) as amount0_raw
+        , CAST(amount1 AS double) as amount1_raw
+    from 
+    liquidity_change_base 
 
 {% endmacro %}
---
+++ dbt_subprojects/dex/models/liquidity/unichain/platforms/uniswap/uniswap_v4_unichain_liquidity.sql
@@ -0,0 +1,22 @@
+{{ config(
+    schema = 'uniswap_v4_unichain'
+    , alias = 'liquidity'
+    , materialized = 'incremental'
+    , file_format = 'delta'
+    , incremental_strategy = 'merge'
+    , unique_key = ['tx_hash', 'evt_index']
+    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v4_base_liquidity(
+          blockchain = 'unichain'
+        , project = 'uniswap'
+        , version = '4'
+        , PoolManager_evt_ModifyLiquidity = source ('uniswap_v4_unichain', 'PoolManager_evt_ModifyLiquidity')
+        , PoolManager_evt_Swap = source('uniswap_v4_unichain', 'PoolManager_evt_Swap') 
+        , liquidity_pools = ref('uniswap_v4_unichain_pools')
+        , liquidity_sqrtpricex96 = ref('uniswap_v4_unichain_sqrtpricex96')
+    )
+}}
