3c99eb45529ae17dfd9220ed00f5d2c30c4b33d9
Hildebert Moulié <h.moulie@gmail.com>
2025-06-20 16:56:43 +0200

    fix

--- dbt_subprojects/hourly_spellbook/macros/sector/cex/cex_deposit_addresses.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/cex/cex_deposit_addresses.sql
@@ -37,6 +37,58 @@ WITH unique_inflows AS (
     )
 
 , in_and_out AS (
+    {% if blockchain == 'ethereum' %}
+
+    SELECT suspected_deposit_address
+    , cex_name
+    , amount_consolidated
+    , consolidation_block_time
+    , SUM(amount_deposited) AS amount_deposited
+    , MIN(deposit_first_block_time) AS deposit_first_block_time
+    , MAX(deposit_last_block_time) AS deposit_last_block_time
+        FROM (
+        SELECT i.suspected_deposit_address
+        , i.cex_name
+        , i.amount AS amount_consolidated
+        , i.block_time AS consolidation_block_time
+        , SUM(t.amount) AS amount_deposited
+        , MIN(t.block_time) AS deposit_first_block_time
+        , MAX(t.block_time) AS deposit_last_block_time
+        FROM {{ source('tokens_'~blockchain,'transfers') }} t
+        INNER JOIN unique_inflows_expanded i ON t.to=i.suspected_deposit_address
+            AND t.token_standard=i.token_standard
+            AND t.contract_address=i.token_address
+            AND t.block_time BETWEEN i.block_time - interval '1' day AND i.block_time
+            --AND t.block_number<i.block_number
+            --AND i.amount_raw BETWEEN t.amount_raw*0.9 AND t.amount_raw*1.1
+        {% if is_incremental() %}
+        WHERE {{ incremental_predicate('t.block_time') }}
+        {% endif %}
+        GROUP BY 1, 2, 3, 4
+
+        UNION ALL
+
+        SELECT i.suspected_deposit_address
+        , i.cex_name
+        , i.amount AS amount_consolidated
+        , i.block_time AS consolidation_block_time
+        , SUM(w.amount/1e9) AS amount_deposited
+        , MIN(w.block_time) AS deposit_first_block_time
+        , MAX(w.block_time) AS deposit_last_block_time
+        FROM {{source('ethereum', 'withdrawals')}} w
+        INNER JOIN unique_inflows_expanded i ON w.block_number<i.block_number
+            AND w.address=i.suspected_deposit_address
+            AND i.token_standard = 'native'
+            AND w.block_time BETWEEN i.block_time - interval '1' day AND i.block_time
+        {% if is_incremental() %}
+        WHERE {{ incremental_predicate('w.block_time') }}
+        {% endif %}
+        GROUP BY 1, 2, 3, 4
+        )
+    GROUP BY 1, 2, 3, 4
+
+    {% else %}
+
     SELECT i.suspected_deposit_address
     , i.cex_name
     , i.amount AS amount_consolidated
@@ -56,25 +108,6 @@ WITH unique_inflows AS (
     {% endif %}
     GROUP BY 1, 2, 3, 4
 
-    {% if blockchain == 'ethereum' %}
-    UNION ALL
-
-    SELECT i.suspected_deposit_address
-    , i.cex_name
-    , i.amount AS amount_consolidated
-    , i.block_time AS consolidation_block_time
-    , SUM(w.amount/1e9) AS amount_deposited
-    , MIN(w.block_time) AS deposit_first_block_time
-    , MAX(w.block_time) AS deposit_last_block_time
-    FROM {{source('ethereum', 'withdrawals')}} w
-    INNER JOIN unique_inflows_expanded i ON w.block_number<i.block_number
-        AND w.address=i.suspected_deposit_address
-        AND i.token_standard = 'native'
-        AND w.block_time BETWEEN i.block_time - interval '1' day AND i.block_time
-    {% if is_incremental() %}
-    WHERE {{ incremental_predicate('w.block_time') }}
-    {% endif %}
-    GROUP BY 1, 2, 3, 4
     {% endif %}
     )
 
