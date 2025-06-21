7a8b339b0474311283a22a5a79b7502498e6e75c
PatelPrinci <principatel@lampros.tech>
2025-06-21 19:33:25 +0530

    Fixed issue of swapping sql files

--- dbt_subprojects/dex/models/trades/arbitrum/platforms/treasure_magicswap_arbitrum_base_trades.sql
+++ dbt_subprojects/dex/models/trades/arbitrum/platforms/treasure_magicswap_arbitrum_base_trades.sql
@@ -1,69 +1,108 @@
 {{ config(
-    schema = 'defi_money_arbitrum',
+    schema = 'treasure_magicswap_arbitrum',
     alias = 'base_trades',
     partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_date', 'blockchain', 'project', 'version', 'tx_hash', 'evt_index']
+    unique_key = ['tx_hash', 'evt_index']
 ) }}
 
--- Step 1: Token Swap Events
-WITH token_swaps AS (
-    SELECT
-        evt_block_number AS block_number,
-        CAST(evt_block_time AS timestamp(3) with time zone) AS block_time,
-        buyer AS maker,
-        evt_tx_to AS taker,
-        tokens_sold AS token_sold_amount_raw,
-        tokens_bought AS token_bought_amount_raw,
-        sold_id,
-        bought_id,
-        contract_address AS project_contract_address,
-        evt_tx_hash AS tx_hash,
-        evt_index
-    FROM {{ source('defi_money_arbitrum', 'arb_amm_evt_tokenexchange') }}
-    {% if is_incremental() %}
-    WHERE {{ incremental_predicate('evt_block_time') }}
-    {% endif %}
+WITH token0_calls AS (
+  SELECT contract_address AS pool_address, output_0 AS token0_address, call_block_time, call_block_number
+  FROM "delta_prod"."treasure_magicswap_arbitrum"."magicgflypair_call_token0"
+  WHERE call_success = TRUE
+  UNION ALL
+  SELECT contract_address AS pool_address, output_0 AS token0_address, call_block_time, call_block_number
+  FROM "delta_prod"."treasure_magicswap_arbitrum"."magicelmpair_call_token0"
+  WHERE call_success = TRUE
 ),
 
--- Step 2: Coin Mapping (i => token address)
-coin_mapping AS (
-    SELECT
-        contract_address AS pool_address,
-        i AS token_index,
-        output_0 AS token_address
-    FROM {{ source('defi_money_arbitrum', 'arb_amm_call_coins') }}
-    WHERE call_success = TRUE
-    {% if is_incremental() %}
-    AND {{ incremental_predicate('call_block_time') }}
-    {% endif %}
-    GROUP BY 1, 2, 3
+latest_token0 AS (
+  SELECT pool_address, token0_address,
+         ROW_NUMBER() OVER (PARTITION BY pool_address ORDER BY call_block_number DESC, call_block_time DESC) AS rn
+  FROM token0_calls
+),
+
+token1_calls AS (
+  SELECT contract_address AS pool_address, output_0 AS token1_address, call_block_time, call_block_number
+  FROM "delta_prod"."treasure_magicswap_arbitrum"."magicgflypair_call_token1"
+  WHERE call_success = TRUE
+  UNION ALL
+  SELECT contract_address AS pool_address, output_0 AS token1_address, call_block_time, call_block_number
+  FROM "delta_prod"."treasure_magicswap_arbitrum"."magicelmpair_call_token1"
+  WHERE call_success = TRUE
+),
+
+latest_token1 AS (
+  SELECT pool_address, token1_address,
+         ROW_NUMBER() OVER (PARTITION BY pool_address ORDER BY call_block_number DESC, call_block_time DESC) AS rn
+  FROM token1_calls
+),
+
+pool_tokens AS (
+  SELECT t0.pool_address, t0.token0_address, t1.token1_address
+  FROM latest_token0 t0
+  JOIN latest_token1 t1 ON t0.pool_address = t1.pool_address
+  WHERE t0.rn = 1 AND t1.rn = 1
+),
+
+swap_events AS (
+  SELECT
+    evt_block_number AS block_number,
+    TRY_CAST(evt_block_time AS TIMESTAMP(3) WITH TIME ZONE) AS block_time,
+    evt_tx_from AS maker,
+    contract_address AS pool_address,
+    COALESCE(amount0In, 0) AS amount0_in,
+    COALESCE(amount1In, 0) AS amount1_in,
+    COALESCE(amount0Out, 0) AS amount0_out,
+    COALESCE(amount1Out, 0) AS amount1_out,
+    sender,
+    to,
+    evt_tx_hash AS tx_hash,
+    evt_index AS evt_index
+  FROM "delta_prod"."treasure_magicswap_arbitrum"."magicgflypair_evt_swap"
+  {% if is_incremental() %}
+  WHERE {{ incremental_predicate('evt_block_time') }}
+  {% endif %}
+  
+  UNION ALL
+
+  SELECT
+    evt_block_number,
+    TRY_CAST(evt_block_time AS TIMESTAMP(3) WITH TIME ZONE),
+    evt_tx_from,
+    contract_address,
+    COALESCE(amount0In, 0),
+    COALESCE(amount1In, 0),
+    COALESCE(amount0Out, 0),
+    COALESCE(amount1Out, 0),
+    sender,
+    to,
+    evt_tx_hash,
+    evt_index
+  FROM "delta_prod"."treasure_magicswap_arbitrum"."magicelmpair_evt_swap"
+  {% if is_incremental() %}
+  WHERE {{ incremental_predicate('evt_block_time') }}
+  {% endif %}
 )
 
--- Step 3: Final Output
 SELECT
-    'arbitrum' AS blockchain,
-    'defi_money' AS project,
-    '1' AS version,
-    CAST(date_trunc('month', ts.block_time) AS date) AS block_month,
-    CAST(date_trunc('day', ts.block_time) AS date) AS block_date,
-    ts.block_time,
-    ts.block_number,
-    ts.token_sold_amount_raw,
-    ts.token_bought_amount_raw,
-    sold_coins.token_address AS token_sold_address,
-    bought_coins.token_address AS token_bought_address,
-    ts.maker,
-    ts.taker,
-    ts.project_contract_address,
-    ts.tx_hash,
-    ts.evt_index
-FROM token_swaps ts
-LEFT JOIN coin_mapping sold_coins
-    ON ts.project_contract_address = sold_coins.pool_address
-    AND ts.sold_id = sold_coins.token_index
-LEFT JOIN coin_mapping bought_coins
-    ON ts.project_contract_address = bought_coins.pool_address
-    AND ts.bought_id = bought_coins.token_index
+  'arbitrum' AS blockchain,
+  'treasure_magicswap' AS project,
+  '1' AS version,
+  TRY_CAST(DATE_TRUNC('month', se.block_time) AS DATE) AS block_month,
+  TRY_CAST(DATE_TRUNC('day', se.block_time) AS DATE) AS block_date,
+  se.block_time,
+  se.block_number,
+  CASE WHEN se.amount0_in > 0 THEN se.amount0_in ELSE se.amount1_in END AS token_sold_amount_raw,
+  CASE WHEN se.amount0_out > 0 THEN se.amount0_out ELSE se.amount1_out END AS token_bought_amount_raw,
+  CASE WHEN se.amount0_in > 0 THEN pt.token0_address ELSE pt.token1_address END AS token_sold_address,
+  CASE WHEN se.amount0_out > 0 THEN pt.token0_address ELSE pt.token1_address END AS token_bought_address,
+  se.maker,
+  se.to AS taker,
+  se.pool_address AS project_contract_address,
+  se.tx_hash,
+  se.evt_index
+FROM swap_events se
+JOIN pool_tokens pt ON se.pool_address = pt.pool_address
