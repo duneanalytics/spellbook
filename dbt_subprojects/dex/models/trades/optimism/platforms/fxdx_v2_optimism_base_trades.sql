093e90565fcb3c611ce6afbbf000572c577a2c86
PatelPrinci <principatel@lampros.tech>
2025-06-23 10:07:36 +0530

    FXDX V2 added for optimism

---
+++ dbt_subprojects/dex/models/trades/optimism/platforms/fxdx_v2_optimism_base_trades.sql
@@ -0,0 +1,50 @@
+{{
+    config(
+        schema = 'fxdx_v2_optimism',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['evt_tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('block_time')]
+    )
+}}
+
+WITH swaps AS (
+    SELECT
+        s.evt_block_number AS block_number,
+        s.evt_block_time AS block_time,
+        s.account,
+        s.amountIn,
+        s.amountOut,
+        s.amountOutAfterFees,
+        s.feeBasisPoints,
+        s.tokenIn,
+        s.tokenOut,
+        s.contract_address,
+        s.evt_tx_hash AS tx_hash,
+        s.evt_index
+    FROM {{ source('fxdxdex_v2_optimism', 'vault_evt_swap') }} s
+    {% if is_incremental() %}
+        WHERE {{ incremental_predicate('block_time') }}
+    {% endif %}
+)
+
+SELECT
+    'optimism' AS blockchain,
+    'fxdx' AS project,
+    '2' AS version,
+    CAST(date_trunc('month', swaps.block_time) AS date) AS block_month,
+    CAST(date_trunc('day', swaps.block_time) AS date) AS block_date,
+    swaps.block_time,
+    swaps.block_number,
+    swaps.amountIn AS token_sold_amount_raw,
+    swaps.amountOut AS token_bought_amount_raw,
+    swaps.tokenIn AS token_sold_address,
+    swaps.tokenOut AS token_bought_address,
+    swaps.account AS taker,
+    swaps.contract_address AS maker,
+    swaps.contract_address AS project_contract_address,
+    swaps.tx_hash,
+    swaps.evt_index
+FROM swaps
