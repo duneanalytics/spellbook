4b7d1f80376e28615eaf2a1c585dcafaded24ac6
Jacob Sharples <45126111+JacobSharples@users.noreply.github.com>
2025-06-29 19:24:24 -0400

    Update prices_solana_tokens.sql

--- dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
+++ dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
@@ -769,4 +769,5 @@ FROM
         ('troll-troll2', 'solana', 'TROLL', '5UUH9RTDiSpq6HKS6bp4NdU9PNJpXRXuiw6ShBTBhgH2', 6),
         ('fdusd-first-digital-usd', 'solana', 'FDUSD', '9zNQRsGLjNKwCUU5Gq5LR8beUCPzQMVMqKAi3SSZh54u', 6),
         ('me-magic-eden', 'solana', 'ME', 'MEFNBXixkEbait3xn9bkm8WsJzXtVsaJEn4c8Sam21u', 6)
+        ('bbsol-bybit-staked-sol', 'solana', 'bbSOL', 'Bybit2vBJGhPF52GBdNaQfUJ6ZpThSgHBobjWZpLPb4B', 9)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
