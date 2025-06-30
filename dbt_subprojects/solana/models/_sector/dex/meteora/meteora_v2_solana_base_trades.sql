eb55f0538fa8d88faa86b481fb891bbc67b2633a
sandeshsk12 <sksandeshkandagal@gmail.com>
2025-06-30 23:47:41 +0530

    Fixed meteora v2 swaps

--- dbt_subprojects/solana/models/_sector/dex/meteora/meteora_v2_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/meteora/meteora_v2_solana_base_trades.sql
@@ -13,80 +13,101 @@
 
 {% set project_start_date = '2023-11-01' %} --grabbed program deployed at time (account created at).
 
-WITH
-    all_swaps as (
-        SELECT 
-            sp.call_block_time as block_time
-            , sp.call_block_slot as block_slot
-            , 'meteora' as project
-            , 2 as version
-            , 'solana' as blockchain
-            , case when sp.call_is_inner = False then 'direct'
-                else sp.call_outer_executing_account
-                end as trade_source
-            -- -- token bought is always the second instruction (transfer) in the inner instructions
-            , trs_2.amount as token_bought_amount_raw
-            , trs_1.amount as token_sold_amount_raw
-            , sp.account_lbPair as pool_id --p.pool_id
-            , sp.call_tx_signer as trader_id
-            , sp.call_tx_id as tx_id
-            , sp.call_outer_instruction_index as outer_instruction_index
-            , COALESCE(sp.call_inner_instruction_index, 0) as inner_instruction_index
-            , sp.call_tx_index as tx_index
-            , COALESCE(trs_2.token_mint_address, cast(null as varchar)) as token_bought_mint_address
-            , COALESCE(trs_1.token_mint_address, cast(null as varchar)) as token_sold_mint_address
-            , trs_2.from_token_account as token_bought_vault
-            , trs_1.to_token_account as token_sold_vault
-        FROM 
-            {{ source('meteora_solana','lb_clmm_call_swap') }}  sp
-        INNER JOIN 
-            {{ ref('tokens_solana_transfers') }} trs_1 
-            ON trs_1.tx_id = sp.call_tx_id 
-            AND trs_1.block_date = sp.call_block_date
-            AND trs_1.block_time = sp.call_block_time
-            AND trs_1.outer_instruction_index = sp.call_outer_instruction_index 
-            AND trs_1.inner_instruction_index = COALESCE(sp.call_inner_instruction_index,0) + 1
+{% set project_start_date = '2025-06-29' %} 
+
+with swaps_events_data as
+(
+select
+    call_block_slot as block_slot
+    , call_block_time as block_time
+    , coalesce(call_tx_index,0) as tx_index
+    , coalesce(call_outer_instruction_index,0) as outer_instruction_index
+    , coalesce(call_inner_instruction_index,0) as inner_instruction_index
+    , call_tx_id as tx_id
+    , call_tx_signer as trader_id
+    , call_outer_executing_account as outer_executing_account
+    , account_tokenXMint
+    , account_tokenYMint
+    , account_reserveX
+    , account_reserveY
+    , account_lbPair
+    , call_is_inner as is_inner_swap
+    , row_number() over (partition by call_tx_id order by call_tx_index asc, call_outer_instruction_index asc, call_inner_instruction_index) as swap_number
+from {{ source ('dlmm_solana','lb_clmm_call_swap') }}
+where 1=1
+-- and call_block_time >= timestamp '{{project_start_date}}'
             {% if is_incremental() %}
-            AND {{incremental_predicate('trs_1.block_time')}}
+            AND {{incremental_predicate('call_block_time')}}
             {% else %}
-            AND trs_1.block_time >= TIMESTAMP '{{project_start_date}}'
+            AND call_block_time >= TIMESTAMP '{{project_start_date}}'
             {% endif %}
-        INNER JOIN 
-            {{ ref('tokens_solana_transfers') }} trs_2 
-            ON trs_2.tx_id = sp.call_tx_id 
-            AND trs_2.block_date = sp.call_block_date
-            AND trs_2.block_time = sp.call_block_time
-            AND trs_2.outer_instruction_index = sp.call_outer_instruction_index 
-            AND trs_2.inner_instruction_index = COALESCE(sp.call_inner_instruction_index,0) + 2
-            {% if is_incremental() %}
-            AND {{incremental_predicate('trs_2.block_time')}}
+),
+inner_instruct_data as 
+(
+select
+    tx_id
+    , block_time
+    , coalesce(tx_index,0) as tx_index
+    , coalesce(outer_instruction_index,0) as outer_instruction_index
+    , coalesce(inner_instruction_index,0) as  inner_instruction_index
+    , row_number() over (partition by tx_id order by tx_index asc, outer_instruction_index asc, inner_instruction_index) as swap_number
+    , data
+from {{ source ('solana','instruction_calls') }}
+where 1=1
+{% if is_incremental() %}
+            AND {{incremental_predicate('block_time')}}
             {% else %}
-            AND trs_2.block_time >= TIMESTAMP '{{project_start_date}}'
+            AND block_time >= TIMESTAMP '{{project_start_date}}'
             {% endif %}
-        WHERE 
-            1=1
-    )
-    
-SELECT
-    tb.blockchain
-    , tb.project 
-    , tb.version
-    , CAST(date_trunc('month', tb.block_time) AS DATE) as block_month
-    , tb.block_time
-    , tb.block_slot
-    , tb.trade_source
-    , tb.token_bought_amount_raw
-    , tb.token_sold_amount_raw
-    , cast(null as double) as fee_tier
-    , tb.token_sold_mint_address
-    , tb.token_bought_mint_address
-    , tb.token_sold_vault
-    , tb.token_bought_vault
-    , tb.pool_id as project_program_id
-    , 'LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo' as project_main_id
-    , tb.trader_id
-    , tb.tx_id
-    , tb.outer_instruction_index
-    , tb.inner_instruction_index
-    , tb.tx_index
-FROM all_swaps tb
+and tx_success
+and is_inner
+and inner_executing_account='LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo' 
+and cardinality(account_arguments)=1
+)
+select 
+    'solana' as blockchain
+    , 'meteora' as project
+    , '2' as version
+    , DATE_TRUNC('month', sw.block_time) AS block_month
+    , sw.block_time
+    , sw.block_slot
+    , CASE WHEN is_inner_swap = FALSE THEN 'direct' ELSE sw.outer_executing_account END AS trade_source
+    , bytearray_to_bigint(bytearray_reverse(bytearray_substring(('0x'||substr(cast(ic.data as varchar),211,2)), 1, 2)))  as swapforY
+    , bytearray_to_bigint(bytearray_reverse(bytearray_substring(('0x'||substr(cast(ic.data as VARCHAR),195,16)),1,16))) as token_bought_amount_raw
+    , bytearray_to_bigint(bytearray_reverse(bytearray_substring(('0x'||substr(cast(ic.data as VARCHAR),179,16)),1,16))) as token_sold_amount_raw
+    , NULL AS fee_tier
+    , case when bytearray_to_bigint(bytearray_reverse(bytearray_substring(('0x'||substr(cast(ic.data as varchar),211,2)), 1, 2))) =1 
+      then account_tokenXMint
+      else account_tokenYMint
+      end as token_sold_mint_address
+    , case when bytearray_to_bigint(bytearray_reverse(bytearray_substring(('0x'||substr(cast(data as varchar),211,2)), 1, 2))) =0 
+      then account_tokenXMint
+      else account_tokenYMint
+      end as token_bought_mint_address
+    , case when bytearray_to_bigint(bytearray_reverse(bytearray_substring(('0x'||substr(cast(ic.data as varchar),211,2)), 1, 2))) =1 
+      then account_reserveX
+      else account_reserveY
+      end AS token_sold_vault
+    , case when bytearray_to_bigint(bytearray_reverse(bytearray_substring(('0x'||substr(cast(data as varchar),211,2)), 1, 2))) =0 
+      then account_reserveX
+      else account_reserveY
+      end as token_bought_vault
+    , sw.account_lbPair AS project_program_id
+    , 'LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo' AS project_main_id
+    , sw.trader_id
+    , sw.tx_id
+    , sw.outer_instruction_index
+    , sw.inner_instruction_index
+    , sw.tx_index
+  
+from 
+swaps_events_data sw 
+left join inner_instruct_data ic 
+on (sw.tx_id=ic.tx_id and sw.block_time=ic.block_time and sw.swap_number=ic.swap_number)
+where 1=1 
+and sw.tx_id='3kDZJQo2fyyE6kiViKtmGbxCcTh7EDjADux9xkah5ytNhsSBbHKx4h1QcJNWTT5BBD4nyxqdN7hahzW4pE4AYTZT'
+{% if is_incremental() %}
+            AND {{incremental_predicate('sw.block_time')}}
+            {% else %}
+            AND sw.block_time >= TIMESTAMP '{{project_start_date}}'
+            {% endif %}
