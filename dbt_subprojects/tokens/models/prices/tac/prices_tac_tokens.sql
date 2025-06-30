d0e3bf4ca7edac78eaaa117e8fb230f8f7c0dd06
beve0x <gk0001va@gmail.com>
2025-06-30 12:15:53 -0400

    fixed syntax

--- dbt_subprojects/tokens/models/prices/tac/prices_tac_tokens.sql
+++ dbt_subprojects/tokens/models/prices/tac/prices_tac_tokens.sql
@@ -18,7 +18,7 @@ SELECT
 FROM
 (
     VALUES
-    (('wtac-wrapped-tac', 'WTAC', 0x07840B012d84095397Fd251Ea619cee6F866bC39, 18)
+    ('wtac-wrapped-tac', 'WTAC', 0x07840B012d84095397Fd251Ea619cee6F866bC39, 18)
     , ('ton-ton-token', 'TON', 0xbE3C16e14d578a24eF4B124fAf9CD1bb5F1e964B, 9)
     , ('usdt-tether-usd', 'USDT', 0xd7d11F0CA352a83aB11fAed983Cf28ACB5AB61aD, 6)
 ) as temp (token_id, symbol, contract_address, decimals)
