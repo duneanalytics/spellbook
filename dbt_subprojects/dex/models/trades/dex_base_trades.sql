346222d2f304098de60795e0327f08796ca9a05c
thecaptncrunch <nia_whitaker@protonmail.com>
2025-06-30 15:23:17 -0400

    syntax error

--- dbt_subprojects/dex/models/trades/dex_base_trades.sql
+++ dbt_subprojects/dex/models/trades/dex_base_trades.sql
@@ -32,8 +32,8 @@
     , ref('dex_mantle_base_trades')
     , ref('dex_nova_base_trades')
     , ref('dex_opbnb_base_trades')
-    , ref('dex_optimism_base_trades'
-    , ref('dex_plume_base_trades'))
+    , ref('dex_optimism_base_trades')
+    , ref('dex_plume_base_trades')
     , ref('dex_polygon_base_trades')
     , ref('dex_ronin_base_trades')
     , ref('dex_scroll_base_trades')
