ffb3555947589c7a646eb80b7ae0d94c923d69f1
Hildebert Moulié <h.moulie@gmail.com>
2025-07-04 15:46:06 +0200

    fix culmination spells

--- dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/bridges_deposits.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/bridges_deposits.sql
@@ -48,71 +48,31 @@ WITH grouped_initiated_events AS (
         )
     )
 
-, deposit_filled AS (
-    SELECT deposit_chain
-    , withdrawal_chain
-    , bridge_name
-    , bridge_version
-    , event_side
-    , block_date
-    , block_time
-    , block_number
-    , sender
-    , recipient
-    , deposit_amount_raw
-    , deposit_amount_raw/POWER(10, pus.decimals) AS deposit_amount
-    , pus.price*deposit_amount_raw/POWER(10, pus.decimals) AS deposit_amount_usd
-    , deposit_token_address
-    , deposit_token_standard
-    , pus.symbol AS deposit_token_symbol
-    , withdrawal_amount_raw
-    , withdrawal_token_address
-    , withdrawal_token_standard
-    , tx_from
-    , tx_hash
-    , evt_index
-    , i.contract_address
-    , bridge_id
-    FROM grouped_initiated_events i
-    INNER JOIN {{ source('prices', 'usd') }} pus ON pus.blockchain=i.deposit_chain
-        AND pus.contract_address=i.deposit_token_address
-        AND pus.minute=date_trunc('minute', block_time)
-        {% if is_incremental() %}
-        AND  {{ incremental_predicate('pus.minute') }}
-        {% endif %}
-    )
-
 SELECT deposit_chain
 , withdrawal_chain
 , bridge_name
 , bridge_version
-, event_side
+, canonical_bridge
 , block_date
 , block_time
 , block_number
+, deposit_amount_raw
+, deposit_amount_raw/POWER(10, pus.decimals) AS deposit_amount
+, pus.price*deposit_amount_raw/POWER(10, pus.decimals) AS deposit_amount_usd
 , sender
 , recipient
-, deposit_amount_raw
-, deposit_amount
-, deposit_amount_usd
-, deposit_token_address
 , deposit_token_standard
-, deposit_token_symbol
-, withdrawal_amount_raw
-, withdrawal_amount_raw/POWER(10, pud.decimals) AS withdrawal_amount
-, pud.price*withdrawal_amount_raw/POWER(10, pud.decimals) AS withdrawal_amount_usd
-, withdrawal_token_address
 , withdrawal_token_standard
-, pud.symbol AS withdrawal_token_symbol
+, deposit_token_address
 , tx_from
 , tx_hash
 , evt_index
-, i.contract_address
-, bridge_id
-FROM deposit_filled i
-INNER JOIN {{ source('prices', 'usd') }} pud ON pud.blockchain=i.withdrawal_chain
-    AND pud.contract_address=i.withdrawal_token_address
-    AND pud.minute=date_trunc('minute', block_time)
-        {% if is_incremental() %}
-        AND  {{ incremental_predicate('pud.minute') }}
-        {% endif %}
+, contract_address
+, transfer_id
+FROM grouped_initiated_events i
+INNER JOIN {{ source('prices', 'usd') }} pus ON pus.blockchain=i.deposit_chain
+    AND pus.contract_address=i.deposit_token_address
+    AND pus.minute=date_trunc('minute', block_time)
+    {% if is_incremental() %}
+    AND  {{ incremental_predicate('pus.minute') }}
+    {% endif %}
