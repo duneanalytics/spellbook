3fb3b95d213a8c71be58f034e86ef05f9c034965
Hildebert Moulié <h.moulie@gmail.com>
2025-07-04 12:33:24 +0200

    rm filter used to finish ci tests

--- dbt_subprojects/hourly_spellbook/macros/sector/addresses_events/addresses_events_first_token_received.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/addresses_events/addresses_events_first_token_received.sql
@@ -9,8 +9,6 @@ WITH finding_transfer AS (
     LEFT JOIN {{this}} t ON t.address=tt.to
     WHERE t.address IS NULL
     AND {{ incremental_predicate('tt.block_time') }}
-    {% else %}
-    WHERE tt.block_time > NOW() - interval '1' month
     {% endif %}
     GROUP BY 1
     )
@@ -33,8 +31,6 @@ FROM {{token_transfers}} tt
 INNER JOIN finding_transfer ft USING (unique_key)
 {% if is_incremental() %}
 WHERE {{ incremental_predicate('tt.block_time') }}
-{% else %}
-WHERE tt.block_time > NOW() - interval '1' month
 {% endif %}
 
 {% endmacro %}
