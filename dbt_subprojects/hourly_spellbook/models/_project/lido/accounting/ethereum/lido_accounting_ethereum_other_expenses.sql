5cb07f37e918d5d0d0ef8d2e420580fae7799cb8
Huang Geyang <Sukebeta@outlook.com>
2025-07-04 23:08:22 +0800

    rename source

--- dbt_subprojects/hourly_spellbook/models/_project/lido/accounting/ethereum/lido_accounting_ethereum_other_expenses.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/lido/accounting/ethereum/lido_accounting_ethereum_other_expenses.sql
@@ -126,10 +126,10 @@ excluded_addresses AS (
     SELECT address FROM ldo_referral_payments_addr
     UNION ALL
     SELECT _recipient AS address
-    FROM {{ source('lido_ethereum', 'AllowedRecipientsRegistry_evt_RecipientAdded') }}
+    FROM {{ source('lido_ethereum', 'allowedrecipientsregistry_evt_recipientadded') }}
     UNION ALL
     SELECT _recipient AS address
-    FROM {{ source('lido_ethereum', 'AllowedRecipientsRegistry_RevShare_evt_RecipientAdded') }}
+    FROM {{ source('lido_ethereum', 'allowedrecipientsregistry_revshare_evt_recipientadded') }}
     UNION ALL
     SELECT 0x0000000000000000000000000000000000000000
     UNION ALL
