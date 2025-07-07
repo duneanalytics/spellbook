185cbb91883c91173895ed7229b497fe0907c01b
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2025-07-07 19:26:24 -0400

    add block_month on final join

--- dbt_subprojects/hourly_spellbook/macros/sector/addresses_events/addresses_events_first_token_received.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/addresses_events/addresses_events_first_token_received.sql
@@ -31,7 +31,7 @@ SELECT '{{blockchain}}' as blockchain
 , tt.block_month AS block_month
 , unique_key
 FROM {{token_transfers}} tt
-INNER JOIN finding_transfer ft USING (unique_key)
+INNER JOIN finding_transfer ft USING (block_month, unique_key)
 {% if is_incremental() %}
 WHERE {{ incremental_predicate('tt.block_time') }}
 {% endif %}
