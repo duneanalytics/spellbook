277897334a2a412f52f2d7c76f7be275054f089b
beve0x <gk0001va@gmail.com>
2025-07-07 13:19:44 -0400

    fixing decimals

--- dbt_subprojects/solana/models/_sector/dex/pumpswap/pumpswap_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/pumpswap/pumpswap_solana_base_trades.sql
@@ -168,18 +168,36 @@ WITH pools AS (
           end as trade_source
         --bought
         ,CASE
+        -- For buys: get the non-SOL token (meme token)
         WHEN is_buy = 1 AND p.baseMint != 'So11111111111111111111111111111111111111112' THEN p.baseMint
         WHEN is_buy = 1 AND p.quoteMint != 'So11111111111111111111111111111111111111112' THEN p.quoteMint
-        ELSE 'So11111111111111111111111111111111111111112'
+        -- For sells: get SOL
+        WHEN is_buy = 0 THEN 'So11111111111111111111111111111111111111112'  
         END as token_bought_mint_address
-        --sold
+        --sold  
         ,CASE
+        -- For sells: give the non-SOL token (meme token)
         WHEN is_buy = 0 AND p.baseMint != 'So11111111111111111111111111111111111111112' THEN p.baseMint
         WHEN is_buy = 0 AND p.quoteMint != 'So11111111111111111111111111111111111111112' THEN p.quoteMint
-        ELSE 'So11111111111111111111111111111111111111112'
+        -- For buys: give SOL
+        WHEN is_buy = 1 THEN 'So11111111111111111111111111111111111111112'
         END as token_sold_mint_address
         , case when sp.is_buy = 0 then sp.base_token_amount else sp.quote_token_amount end AS token_sold_amount_raw
         , case when sp.is_buy = 1 then sp.base_token_amount else sp.quote_token_amount end AS token_bought_amount_raw
+        , CASE
+            -- For buys: get decimals for non-SOL token (meme token)
+            WHEN is_buy = 1 AND p.baseMint != 'So11111111111111111111111111111111111111112' THEN p.baseMintDecimals
+            WHEN is_buy = 1 AND p.quoteMint != 'So11111111111111111111111111111111111111112' THEN p.quoteMintDecimals
+            -- For sells: get SOL decimals
+            WHEN is_buy = 0 THEN 9
+        END as token_bought_decimal_project_specific
+        , CASE
+            -- For sells: give decimals for non-SOL token (meme token)
+            WHEN is_buy = 0 AND p.baseMint != 'So11111111111111111111111111111111111111112' THEN p.baseMintDecimals
+            WHEN is_buy = 0 AND p.quoteMint != 'So11111111111111111111111111111111111111112' THEN p.quoteMintDecimals
+            -- For buys: give SOL decimals
+            WHEN is_buy = 1 THEN 9
+        END as token_sold_decimal_project_specific
         , cast(sp.total_fee_rate as double) as fee_tier
         , sp.pool as pool_id
         , 'pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA' as project_main_id
@@ -216,6 +234,8 @@ SELECT
     , tb.fee_tier
     , tb.token_sold_mint_address
     , tb.token_bought_mint_address
+    , tb.token_bought_decimal_project_specific
+    , tb.token_sold_decimal_project_specific
     , tb.token_sold_vault
     , tb.token_bought_vault
     , tb.pool_id as project_program_id
