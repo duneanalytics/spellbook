c765d2922549df54977282d53e1944e4fe91679f
jeff-dude <jeff@dune.xyz>
2025-07-08 16:28:27 -0400

    update katana model

--- dbt_subprojects/tokens/models/transfers_and_balances/katana/tokens_katana_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/katana/tokens_katana_transfers.sql
@@ -4,7 +4,7 @@
     partition_by = ['block_month'],
     materialized = 'incremental',
     file_format = 'delta',
-    incremental_strategy = 'merge',
+    incremental_strategy = 'append',
     unique_key = ['block_date','unique_key'],
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
     post_hook='{{ expose_spells(\'["katana"]\',
@@ -18,8 +18,7 @@
     transfers_enrich(
         base_transfers = ref('tokens_katana_base_transfers')
         , tokens_erc20_model = source('tokens', 'erc20')
-        , prices_model = source('prices', 'usd')
-        , evms_info_model = source('evms','info')
+        , prices_model = source('prices', 'hour')
         , transfers_start_date = '2025-05-08'
         , blockchain = 'katana'
     )
