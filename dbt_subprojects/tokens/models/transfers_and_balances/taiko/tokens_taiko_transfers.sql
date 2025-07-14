bd6d32e308ac4f366a49bc42d75a3999b17fa7f7
beve0x <gk0001va@gmail.com>
2025-07-13 23:25:38 -0400

    typo

--- dbt_subprojects/tokens/models/transfers_and_balances/taiko/tokens_taiko_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/taiko/tokens_taiko_transfers.sql
@@ -7,7 +7,7 @@
      , incremental_strategy = 'merge'
      , unique_key = ['block_date','unique_key']
      , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
-     , post_hook='{{ expose_spells(blockchains = \'["shape"]\',
+     , post_hook='{{ expose_spells(blockchains = \'["taiko"]\',
                                 spell_type = "sector",
                                 spell_name = "tokens",
                                 contributors = \'["krishhh"]\') }}'
