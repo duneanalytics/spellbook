142e3e5a04065728442e862adea42d5196ad35c7
Abhijeet <abhijeet@useblitz.com>
2025-07-15 12:17:24 +0530

    Add blackhole_v2 and blackhole_v3 avalanche_c base trades models with incremental configurations and updated contributors in schema.yml

---
+++ dbt_subprojects/dex/models/trades/avalanche_c/platforms/blackhole_v3_avalanche_c_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'blackhole_v3_avalanche_c',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v3_trades(
+        blockchain = 'avalanche_c',
+        project = 'blackhole',
+        version = '2',
+        Pair_evt_Swap =  source('blackhole_avalanche_c', 'algebrapool_evt_swap'),
+        Factory_evt_PairCreated = source('blackhole_avalanche_c', 'algebrafactory_evt_custompool'),
+    )
+}}
