0b662b486ce52b3739069bfc825ca60f26f1fedc
Abhijeet <abhijeet@useblitz.com>
2025-07-15 10:27:12 +0530

    Fix column name in blackhole avalanche_c base trades model

--- dbt_subprojects/dex/models/trades/avalanche_c/platforms/blackhole_avalanche_c_base_trades.sql
+++ dbt_subprojects/dex/models/trades/avalanche_c/platforms/blackhole_avalanche_c_base_trades.sql
@@ -16,7 +16,7 @@
         project = 'blackhole',
         version = '1',
         Pairgenerator_evt_Paircreated = source('blackhole_avalanche_c', 'pairgenerator_evt_paircreated'),
-        Pair_evt_Awap = source('blackhole_avalanche_c', 'pair_evt_swap')
+        Pair_evt_Swap = source('blackhole_avalanche_c', 'pair_evt_swap')
         Algebrafactory_evt_Custompool = source('blackhole_avalanche_c', 'algebrafactory_evt_custompool'),
         Algebrapool_evt_Swap = source('blackhole_avalanche_c', 'algebrapool_evt_swap')
     )
