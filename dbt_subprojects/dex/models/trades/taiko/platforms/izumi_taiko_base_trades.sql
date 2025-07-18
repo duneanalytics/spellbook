b42c2c064e857ac72ddccaf644ea79b70b8d7a31
beve0x <gk0001va@gmail.com>
2025-07-18 11:15:47 -0400

    test output from base trades

---
+++ dbt_subprojects/dex/models/trades/taiko/platforms/izumi_taiko_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'izumi_taiko',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v3_trades(
+        blockchain = 'taiko',
+        project = 'izumi',
+        version = '1',
+        Pool_evt_Swap = source('izumi_taiko', 'iZiSwapPool_evt_Swap'),
+        Factory_evt_PoolCreated = source('izumi_taiko', 'iZiSwapFactory_evt_NewPool')
+    )
+}}
