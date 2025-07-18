60f1f83b40ca2d1ba667b94f730bd24b222cfbc1
grkhr <eduardgorkh@gmail.com>
2025-07-18 15:19:53 +0300

    try w bnb

---
+++ dbt_subprojects/daily_spellbook/models/_projects/oneinch/bnb/1/oneinch_bnb_mapped_contracts_1.sql
@@ -0,0 +1,14 @@
+{% set blockchain = 'bnb' %}
+
+
+
+{{
+    config(
+        schema = 'oneinch_' + blockchain,
+        alias = 'mapped_contracts_1',
+        materialized = 'table',
+        unique_key = ['blockchain', 'address'],
+    )
+}}
+
+{{ oneinch_mapped_contracts_macro_test(blockchain, '1') }}
