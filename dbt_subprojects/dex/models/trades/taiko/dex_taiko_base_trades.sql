2287dcec7517e72a30b7bfe04b1209a254763545
beve0x <gk0001va@gmail.com>
2025-07-21 11:52:52 -0400

    removd unagi v2 and izumi

--- dbt_subprojects/dex/models/trades/taiko/dex_taiko_base_trades.sql
+++ dbt_subprojects/dex/models/trades/taiko/dex_taiko_base_trades.sql
@@ -7,9 +7,7 @@
 
 {% set base_models = [
     ref('oku_v3_taiko_base_trades')
-    , ref('unagi_v2_taiko_base_trades')
     , ref('unagi_v3_taiko_base_trades')
-    , ref('izumi_multichain_base_trades')
 ] %}
 
 WITH base_union AS (
