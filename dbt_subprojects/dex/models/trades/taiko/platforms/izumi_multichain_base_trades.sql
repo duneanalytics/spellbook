72a7c9f7a19df1698c11acb1c67687202852c617
beve0x <gk0001va@gmail.com>
2025-07-21 10:59:19 -0400

    izumi schema column naming fix

--- dbt_subprojects/dex/models/trades/taiko/platforms/izumi_multichain_base_trades.sql
+++ dbt_subprojects/dex/models/trades/taiko/platforms/izumi_multichain_base_trades.sql
@@ -10,12 +10,23 @@
     )
 }}
 
+with izumi_swaps as (
+    select
+        *,
+        amountX as amount0,
+        amountY as amount1,
+        tokenX as token0,
+        tokenY as token1
+    from {{ source('izumi_multichain', 'iziswappool_evt_swap') }}
+)
+
 {{
     uniswap_compatible_v3_trades(
         blockchain = 'taiko',
         project = 'izumi',
         version = '1',
-        Pair_evt_Swap = source('izumi_multichain', 'iziswappool_evt_swap'),
-        Factory_evt_PoolCreated = source('izumi_multichain', 'iziswapfactory_evt_newpool')
+        Pair_evt_Swap = 'izumi_swaps',
+        Factory_evt_PoolCreated = source('izumi_multichain', 'iziswapfactory_evt_newpool'),
+        taker_column_name = 'evt_tx_to'
     )
 }}
