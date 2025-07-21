3b9c1059c41b653af151ae9dd761ada22cee020f
beve0x <gk0001va@gmail.com>
2025-07-21 15:04:51 -0400

    resolving amount0

--- dbt_subprojects/dex/models/trades/taiko/platforms/stg_izumi_finance_taiko_swaps.sql
+++ dbt_subprojects/dex/models/trades/taiko/platforms/stg_izumi_finance_taiko_swaps.sql
@@ -12,34 +12,37 @@
 
 WITH base_swaps AS (
   SELECT
-    -- All raw columns from the source table
-    contract_address,
-    evt_tx_hash,
-    evt_tx_from,
-    evt_tx_to,
-    evt_tx_index,
-    evt_index,
-    evt_block_time,
-    evt_block_number,
-    evt_block_date,
-    amountX,
-    amountY,
-    currentPoint,
-    fee,
-    sellXEarnY,
-    tokenX,
-    tokenY,
+      contract_address
+    , evt_tx_hash
+    , evt_tx_from
+    , evt_tx_to
+    , evt_tx_index
+    , evt_index
+    , evt_block_time
+    , evt_block_number
+    , evt_block_date
+    , amountX
+    , amountY
+    , currentPoint
+    , fee
+    , sellXEarnY
+    , tokenX
+    , tokenY
     -- Standardized/derived columns for downstream compatibility
-    evt_tx_hash AS tx_hash,
-    evt_block_number AS block_number,
-    evt_block_date AS block_date,
-    CASE WHEN sellXEarnY THEN tokenY ELSE tokenX END AS token_bought_address,
-    CASE WHEN sellXEarnY THEN tokenX ELSE tokenY END AS token_sold_address,
-    CASE WHEN sellXEarnY THEN amountY ELSE amountX END AS token_bought_amount_raw,
-    CASE WHEN sellXEarnY THEN amountX ELSE amountY END AS token_sold_amount_raw,
-    'taiko' AS blockchain,
-    'izumi_finance' AS project,
-    'v3' AS version
+    , evt_tx_hash AS tx_hash
+    , evt_block_number AS block_number
+    , evt_block_date AS block_date
+    , amountX AS amount0
+    , amountY AS amount1
+    , tokenX AS token0
+    , tokenY AS token1
+    , CASE WHEN sellXEarnY THEN tokenY ELSE tokenX END AS token_bought_address
+    , CASE WHEN sellXEarnY THEN tokenX ELSE tokenY END AS token_sold_address
+    , CASE WHEN sellXEarnY THEN amountY ELSE amountX END AS token_bought_amount_raw
+    , CASE WHEN sellXEarnY THEN amountX ELSE amountY END AS token_sold_amount_raw
+    , 'taiko' AS blockchain
+    , 'izumi_finance' AS project
+    , 'v3' AS version
   FROM {{ source('izumi_finance_taiko', 'iziswappool_evt_swap') }}
 )
 SELECT * FROM base_swaps
