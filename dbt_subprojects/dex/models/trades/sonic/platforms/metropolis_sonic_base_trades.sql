72cad383f62d7622ae23880b632743469d29188d
okolicodes <okolifavour50@gmail.com>
2025-07-23 07:13:43 +0100

    new correction

--- dbt_subprojects/dex/models/trades/sonic/platforms/metropolis_sonic_base_trades.sql
+++ dbt_subprojects/dex/models/trades/sonic/platforms/metropolis_sonic_base_trades.sql
@@ -28,37 +28,39 @@ WITH all_swaps AS (
 
 ),
 
+WITH 
 swap_events AS (
     SELECT
-        buyer,
         contract_address,
-        evt_block_number,
-        evt_block_time,
-        evt_tx_hash,
-        evt_index,
-        element_at(amounts, 1) AS amount0,
-        element_at(amounts, 2) AS amount1,
-        swapAmount,
-        feeAmount
+        call_block_number,
+        call_block_time,
+        call_tx_hash,
+        call_tx_index,
+        call_trace_address,
+        call_success,
+        call_tx_from,
+        call_tx_to,
+        call_block_date,
+        amountIn,
+        amountOutMin,
+        deadline,
+        output_amounts,
+        path,
+        to
     FROM all_swaps
     {% if is_incremental() %}
-    WHERE {{ incremental_predicate('evt_block_time') }}
+    WHERE {{ incremental_predicate('call_block_time') }}
     {% endif %}
 ),
 
 swap_direction AS (
     SELECT
         s.*,
-        CASE
-            WHEN ABS(amount0) = swapAmount THEN 0
-            ELSE 1
-        END AS tokenIn,
-        CASE
-            WHEN ABS(amount0) = swapAmount THEN 1
-            ELSE 0
-        END AS tokenOut,
-        ABS(amount0) AS amount0_abs,
-        ABS(amount1) AS amount1_abs
+        -- Use amountIn as the swap amount for inference
+        0 AS tokenIn,  -- You can refine this logic based on path[1] etc. later
+        1 AS tokenOut,
+        amountIn AS amount0_abs,
+        element_at(output_amounts, cardinality(output_amounts)) AS amount1_abs
     FROM swap_events s
 ),
 
@@ -84,11 +86,22 @@ dexs AS (
             WHEN s.tokenOut = 1 THEN s.amount1_abs
             ELSE CAST(NULL AS uint256)
         END AS token_bought_amount_raw,
-        s.contract_address AS project_contract_address,
-        s.evt_block_number AS block_number,
-        s.evt_block_time AS block_time,
-        s.evt_tx_hash AS tx_hash,
-        s.evt_index
+        s.contract_address,
+        s.call_block_number AS block_number,
+        s.call_block_time AS block_time,
+        s.call_tx_hash AS tx_hash,
+        s.call_tx_index AS evt_index,
+        s.call_success,
+        s.call_tx_from,
+        s.call_tx_to,
+        s.call_trace_address,
+        s.call_block_date,
+        s.amountIn,
+        s.amountOutMin,
+        s.deadline,
+        s.output_amounts,
+        s.path,
+        s.to
     FROM swap_direction s
     LEFT JOIN (SELECT * FROM unique_pool_tokens WHERE rn = 1) p 
         ON s.contract_address = p.contract_address
@@ -120,7 +133,22 @@ SELECT
     dexs.token_sold_address,
     CAST(NULL AS VARBINARY) AS taker,
     CAST(NULL AS VARBINARY) AS maker,
-    dexs.project_contract_address,
+    dexs.contract_address AS project_contract_address,
     dexs.tx_hash,
-    dexs.evt_index
+    dexs.evt_index,
+    dexs.call_success,
+    dexs.call_tx_hash,
+    dexs.call_tx_from,
+    dexs.call_tx_to,
+    dexs.call_tx_index,
+    dexs.call_trace_address,
+    dexs.call_block_time,
+    dexs.call_block_number,
+    dexs.call_block_date,
+    dexs.amountIn,
+    dexs.amountOutMin,
+    dexs.deadline,
+    dexs.output_amounts,
+    dexs.path,
+    dexs.to
 FROM dexs
