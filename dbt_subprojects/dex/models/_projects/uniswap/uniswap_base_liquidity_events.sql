94c8ca71f1ac24b7c7aaf5abf3c2873052f1a58e
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-07-25 17:09:19 +0100

    push changes for all chains

--- dbt_subprojects/dex/models/_projects/uniswap/uniswap_base_liquidity_events.sql
+++ dbt_subprojects/dex/models/_projects/uniswap/uniswap_base_liquidity_events.sql
@@ -11,18 +11,20 @@
 }}
 
 {% set models = [
-    ref('uniswap_v4_ethereum_base_liquidity_events')
-   ,ref('uniswap_arbitrum_base_liquidity_events')
-   ,ref('uniswap_v4_optimism_base_liquidity_events')
-   ,ref('uniswap_blast_base_liquidity_events')
-   ,ref('uniswap_v4_zora_base_liquidity_events')
+    ref('uniswap_arbitrum_base_liquidity_events')
    ,ref('uniswap_avalanche_c_base_liquidity_events')
-   ,ref('uniswap_v4_ink_base_liquidity_events')
-   ,ref('uniswap_v4_unichain_base_liquidity_events')
    ,ref('uniswap_base_base_liquidity_events')
-   ,ref('uniswap_v4_polygon_base_liquidity_events')
+   ,ref('uniswap_blast_base_liquidity_events')
    ,ref('uniswap_bnb_base_liquidity_events')
-   ,ref('uniswap_v4_worldchain_base_liquidity_events')
+   ,ref('uniswap_celo_base_liquidity_events')
+   ,ref('uniswap_ethereum_base_liquidity_events')
+   ,ref('uniswap_gnosis_base_liquidity_events')
+   ,ref('uniswap_ink_base_liquidity_events')
+   ,ref('uniswap_optimism_base_liquidity_events')
+   ,ref('uniswap_polygon_base_liquidity_events')
+   ,ref('uniswap_unichain_base_liquidity_events')
+   ,ref('uniswap_worldchain_base_liquidity_events')
+   ,ref('uniswap_zora_base_liquidity_events')
 ] %}
 
 with base_union as (
