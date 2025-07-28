2dcff0bf7ba5d4f3606c01f2da88b263f397009f
Hildebert Moulié <h.moulie@gmail.com>
2025-07-28 03:27:49 +0200

    fix

--- dbt_subprojects/hourly_spellbook/macros/sector/bridges/celer_v1_deposits.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/bridges/celer_v1_deposits.sql
@@ -11,6 +11,7 @@ SELECT '{{blockchain}}' AS deposit_chain
 , sender
 , receiver AS recipient
 , 'erc20' AS deposit_token_standard
+, 'erc20' AS withdrawal_token_standard
 , token AS deposit_token_address
 , evt_tx_from AS tx_from
 , evt_tx_hash AS tx_hash
