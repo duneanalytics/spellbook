9642d78e83a80df4bae005d7c8382ed8238b8cc7
hildobby <h.moulie@gmail.com>
2025-07-29 21:04:35 +0200

    cctp bridge on arbitrum, avalanche, optimism, polygon and unichain (#8550)

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/arbitrum/platforms/bridges_arbitrum_cctp_v1_withdrawals.sql
@@ -0,0 +1,10 @@
+{% set blockchain = 'arbitrum' %}
+
+{{ config(
+    schema = 'bridges_' + blockchain,
+    alias = 'cctp_v1_withdrawals',
+    materialized = 'view',
+    )
+}}
+
+{{cctp_v1_withdrawals(blockchain = blockchain)}}
