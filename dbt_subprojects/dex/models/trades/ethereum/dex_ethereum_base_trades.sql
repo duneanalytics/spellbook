cae16c89ea48297598ba9f091bc793610182728c
sandeshsk12 <sksandeshkandagal@gmail.com>
2025-07-30 00:16:11 +0530

    Added ekubo to eth models

--- dbt_subprojects/dex/models/trades/ethereum/dex_ethereum_base_trades.sql
+++ dbt_subprojects/dex/models/trades/ethereum/dex_ethereum_base_trades.sql
@@ -43,6 +43,7 @@
     , ref('xchange_v2_ethereum_base_trades')
     , ref('fluid_v1_ethereum_base_trades')
     , ref('native_ethereum_base_trades')
+    , ref('ekubo_v1_ethereum_base_trades')
 ] %}
 
 WITH base_union AS (
