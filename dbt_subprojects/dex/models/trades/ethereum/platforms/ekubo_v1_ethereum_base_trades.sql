4d351fc0069a7689eb3162518d2e49e37837c2dd
sandeshsk12 <sksandeshkandagal@gmail.com>
2025-07-29 20:29:47 +0530

    Added comments and also filtered zero amount trades

--- dbt_subprojects/dex/models/trades/ethereum/platforms/ekubo_v1_ethereum_base_trades.sql
+++ dbt_subprojects/dex/models/trades/ethereum/platforms/ekubo_v1_ethereum_base_trades.sql
@@ -1,3 +1,31 @@
+-- #############################################################################################################################
+-- ##                                          Methodology & Design Choices                                                   ##
+-- #############################################################################################################################
+-- Ekubo is a high-efficiency Automated Market Maker (AMM) that utilizes a concentrated liquidity model.
+-- Primarily built using cairo for the starknet community. The have also included Ethereum in March 2025
+
+-- Links : 
+-- 1. Website : https://ekubo.org/ 
+-- 2. Docs: https://docs.ekubo.org/ 
+-- 3. Contract address: https://docs.ekubo.org/integration-guides/reference/contract-addresses 
+-- 4. Github: https://github.com/EkuboProtocol
+-- 5. Twitter: https://x.com/EkuboProtocol 
+
+-- This model deviates from standard DEX abstractions by sourcing primary trade data from `traces` instead of `events`.
+-- The Ekubo contract's `swap` function does not emit token amounts, making traces the only reliable source for this critical data.
+--
+-- To construct a complete trade record, we join trace data with event logs. The `INNER JOIN` ensures we only include trades that have both a successful call trace
+-- and a corresponding event log. This acts as a safeguard, as swaps that appear successful in traces but lack an
+-- event log may represent failed or reverted state changes within the transaction.
+--
+-- We also explicitly filter out trades where both `output_delta0` and `output_delta1` are zero. These represent successful
+-- calls that resulted in no actual token movement. This explains why the final trade count is slightly lower than the total
+-- number of successful swap calls (e.g., 20 transaction calls over 5 months as of July 2025), without affecting the total volume.
+-- Example of a zero-amount swap transaction: 0x87f217eba253b23288deebfc434f92c5018d9b90a906e8627589b3172578b29e
+--
+-- #############################################################################################################################
+
+
 {{
     config(
         schema = 'ekubo_v1_ethereum',
@@ -10,21 +38,17 @@
     )
 }}
 
-{% set project_deployed_on = '2025-03-14 23:49' %}
+-- The project was deployed on 2025-03-14 07:41 PM. https://etherscan.io/tx/0x15cc649ad5dea0943e4d0ea81df42e0dcb11e46766ebd6aeeffd6e91aecaf9c9
+{% set project_deployed_on = '2025-03-14 19:00' %}
 {% set contract_addresses = [
     '0xe0e0e08a6a4b9dc7bd67bcb7aade5cf48157d444'
 ] %}
 
 
-/* mev transaction (0xbd6f5765d7bcf3cb3a4d6ae71225c3a9bb50354d5544f1e8aef95aad2f6215aa), so here the user removed liquidity + collected fees+ swapped, giving the total token0 as 1,86,73,09,51,582 */
-/* tough one to verify 0x802b84d9900f577d3b535b982717c3c2acf8d45d45b0b0629cceb810291c49c6 */
 with trace_trades as 
 (
     SELECT
-        'ethereum' AS blockchain
-        , 'ekubo' AS project
-        , '1' AS version
-        , call_block_time as block_time
+        call_block_time as block_time
         , call_block_number AS block_number
         , CASE 
             WHEN isToken1 = TRUE THEN ABS(output_delta0)
@@ -44,10 +68,10 @@ with trace_trades as
         END AS token_sold_address
         , contract_address AS project_contract_address
         , call_tx_hash AS tx_hash
-        , cast(call_tx_from as varchar) AS taker
-        , cast(null as varbinary) AS maker
-        -- , cast(JSON_EXTRACT(poolKey, '$.config') as varchar) as config  
-        , row_number() over (partition by call_tx_hash order by call_trace_address asc) as swap_number
+        , CAST(call_tx_from AS VARCHAR) AS taker 
+        , CAST(null AS VARBINARY) AS maker
+        -- , cast(JSON_EXTRACT(poolKey, '$.config') AS varchar) as config  This is the pool config. So if you are looking to calculate the colume of pool, please use this.
+        , ROW_NUMBER() OVER (PARTITION BY call_tx_hash ORDER BY call_trace_address ASC) AS swap_number -- Row number is needed to match each trace call with a matching event in logs. Very helpful in multi step swaps
 
     FROM {{ source('ekubo_ethereum', 'ekubo_core_call_swap_611415377') }}
     WHERE 1=1
@@ -55,56 +79,54 @@ with trace_trades as
         {% if is_incremental() %}
         and {{ incremental_predicate('call_block_time') }}
         {% endif %}
+        AND (ABS(output_delta1) > 0 or ABS(output_delta0) > 0) -- Removes trades where no tokens are bought or sold.
 )
 , evt_trades as 
 (
-    select
+    SELECT
         block_time
         , tx_hash
         , index as evt_index
-        , tx_index 
-        -- , substr(data,21,32)  as pool_id 
-        , row_number() over (partition by tx_hash order by index asc) as swap_number
-    from {{ source('ethereum', 'logs') }}
-    where 1=1
-        and cast(contract_address as varchar) in 
+        , ROW_NUMBER() OVER (PARTITION BY tx_hash ORDER BY index ASC) AS swap_number
+    FROM {{ source('ethereum', 'logs') }}
+    WHERE 1=1
+        AND block_time >= timestamp '{{ project_deployed_on }}'
+        AND cast(contract_address as varchar) in 
         (
             {% for contract_address in contract_addresses %}
                 '{{ contract_address }}'
             {% endfor %}
         )
-        and block_time >= timestamp '{{ project_deployed_on }}'
         {% if is_incremental() %}
-        and {{ incremental_predicate('block_time') }}
+        AND {{ incremental_predicate('block_time') }}
         {% endif %}
-        and topic0 is null 
+        AND topic0 is null 
 )
-select 
-    cast(tt.blockchain as varchar) as blockchain
-    , cast(tt.project as varchar) as project
-    , cast(tt.version as varchar) as version
-    , date_trunc( 'month', tt.block_time) as block_month
-    , date_trunc( 'day', tt.block_time) as block_date
-    , tt.block_time as block_time
-    , cast(tt.block_number as uint256) as block_number
-    , cast(tt.token_bought_amount_raw as uint256) as token_bought_amount_raw
-    , cast(tt.token_sold_amount_raw as uint256) as token_sold_amount_raw
-    , from_hex(token_bought_address) as token_bought_address
-    , from_hex(token_sold_address) as token_sold_address
-    , from_hex(tt.taker) as taker
-    , from_hex(tt.maker) as maker
+SELECT 
+
+    CAST('ethereum' AS VARCHAR) AS blockchain
+    , CAST('ekubo' AS VARCHAR) AS project
+    , CAST('1' AS VARCHAR) AS version
+    , date_trunc( 'month', tt.block_time) AS block_month
+    , date_trunc( 'day', tt.block_time) AS block_date
+    , tt.block_time AS block_time
+    , CAST(tt.block_number AS uint256) AS block_number
+    , CAST(tt.token_bought_amount_raw AS uint256) AS token_bought_amount_raw
+    , CAST(tt.token_sold_amount_raw AS uint256) AS token_sold_amount_raw
+    , from_hex(token_bought_address) AS token_bought_address
+    , from_hex(token_sold_address) AS token_sold_address
+    , from_hex(tt.taker) AS taker
+    , from_hex(tt.maker) AS maker
     , project_contract_address
     , tt.tx_hash
-    , cast(coalesce(et.evt_index, 1000000+tt.swap_number) as uint256) as evt_index 
+    , CAST(et.evt_index AS uint256) AS evt_index 
 
-
-    
-from trace_trades tt
-left join evt_trades et 
-on (
+FROM trace_trades tt
+INNER JOIN evt_trades et 
+ON (
     tt.tx_hash=et.tx_hash 
-    and tt.block_time=et.block_time 
-    and tt.swap_number=et.swap_number
+    AND tt.block_time=et.block_time 
+    AND tt.swap_number=et.swap_number
    )
-where 1=1      
+ 
 
