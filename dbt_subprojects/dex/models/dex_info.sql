b7651f19f85e7ed673e7d6b43abb029b09c2bd81
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-07-29 12:27:37 +0100

    add to dex_info and remove uni-v4 seed

--- dbt_subprojects/dex/models/dex_info.sql
+++ dbt_subprojects/dex/models/dex_info.sql
@@ -226,4 +226,5 @@ FROM (VALUES
     , ('yeiswap', 'YeiSwap', 'Direct', 'YeiFinance')
     , ('bulletx', 'BulletX', 'Direct', 'bulletx_io')
     , ('native', 'Native', 'Direct', 'Native')
+    , ('eulerswap', 'Eulerswap', 'Direct & Aggregator', 'eulerfinance')
 ) AS temp_table (project, name, marketplace_type, x_username)
