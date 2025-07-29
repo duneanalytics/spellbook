16024d4bab6d2bdd547ffa2923827e0e29d91870
jeff-dude <jeff@dune.xyz>
2025-07-29 16:05:48 -0400

    force short run time

--- dbt_subprojects/dex/models/trades/gnosis/dex_gnosis_base_trades.sql
+++ dbt_subprojects/dex/models/trades/gnosis/dex_gnosis_base_trades.sql
@@ -51,7 +51,7 @@ with base_union as (
             {{ base_model }}
         WHERE
            token_sold_amount_raw >= 0 and token_bought_amount_raw >= 0
-        {% if is_incremental() %}
+        {% if is_incremental() or true %}
             AND {{ incremental_predicate('block_time') }}
         {% endif %}
         {% if not loop.last %}
