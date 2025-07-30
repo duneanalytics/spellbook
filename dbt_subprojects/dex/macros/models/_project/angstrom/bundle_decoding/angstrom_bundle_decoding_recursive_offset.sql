806bdd3fb0d88f9267b2b6f9c2ffea63ad5842a8
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-07-30 14:34:37 -0400

    added recursive decoding

--- dbt_subprojects/dex/macros/models/_project/angstrom/bundle_decoding/angstrom_bundle_decoding_recursive_offset.sql
+++ dbt_subprojects/dex/macros/models/_project/angstrom/bundle_decoding/angstrom_bundle_decoding_recursive_offset.sql
@@ -1,40 +1,93 @@
--- the idea of this is to get the offset of the buffer given a specific field to be decoded. This may not be possible or necessary?
-
 {% macro
     angstrom_decoding_recursive(
         input_hex,
-        field_index -- 0. assets, 1. pairs, 2. pool_updates, 3. top_of_block_orders, 4. user_orders
+        field_step -- 0. assets, 1. pairs, 2. pool_updates, 3. top_of_block_orders, 4. user_orders
     )
 %}
 
 
 
-
-
 WITH
+    trimmed_input AS (
+        SELECT 
+            1 AS next_offset,
+            varbinary_substring(input_hex, 69) AS next_buf
+    ),
     -- assets
     step0 AS (
         SELECT
-            1 AS offset,
-            varbinary_to_integer(varbinary_substring(input_hex, offset, 3)) / 68 AS len, -- 68 bytes per `Asset`
-            len + 3 AS next_offset,
-            varbinary_substring(input_hex, offset, next_offset) AS next_buf
-
+            len + 3 + offset AS next_offset,
+            varbinary_substring(next_buf, offset, len + 3) AS buf,
+            next_buf
+        FROM (
+            SELECT 
+                next_offset AS offset,
+                varbinary_to_integer(varbinary_substring(next_buf, next_offset, 3)) AS len,
+                next_buf
+            FROM trimmed_input
+        )
     ),
     -- pairs
     step1 AS (
         SELECT
-            next_offset AS offset,
-            varbinary_to_integer(varbinary_substring(next_buf, offset, 3)) / 38 AS len, -- 38 bytes per `Pair`
-            len + 3 AS next_offset,
-            varbinary_substring(next_buf, offset, next_offset) AS next_buf
-        FROM step0
+            len + 3 + offset AS next_offset,
+            varbinary_substring(next_buf, offset, len + 3) AS buf,
+            next_buf
+        FROM (
+            SELECT 
+                next_offset AS offset,
+                varbinary_to_integer(varbinary_substring(next_buf, next_offset, 3)) AS len,
+                next_buf
+            FROM step0
+        )
+    ),
+    -- pool updates
+    step2 AS (
+        SELECT
+            len + 3 + offset AS next_offset,
+            varbinary_substring(next_buf, offset, len + 3) AS buf,
+            next_buf
+        FROM (
+            SELECT 
+                next_offset AS offset,
+                varbinary_to_integer(varbinary_substring(next_buf, next_offset, 3)) AS len,
+                next_buf
+            FROM step1
+        )
+    ),
+    -- top of block orders
+    step3 AS (
+        SELECT
+            len + 3 + offset AS next_offset,
+            varbinary_substring(next_buf, offset, len + 3) AS buf,
+            next_buf
+        FROM (
+            SELECT 
+                next_offset AS offset,
+                varbinary_to_integer(varbinary_substring(next_buf, next_offset, 3)) AS len,
+                next_buf
+            FROM step2
+        )
+    ),
+    -- user orders
+    step4 AS (
+        SELECT
+            len + 3 + offset AS next_offset,
+            varbinary_substring(next_buf, offset, len + 3) AS buf,
+            next_buf
+        FROM (
+            SELECT 
+                next_offset AS offset,
+                varbinary_to_integer(varbinary_substring(next_buf, next_offset, 3)) AS len,
+                next_buf
+            FROM step3
+        )
     )
-    -- TODO: recusively trace down to step 4
 SELECT
-    offset
-FROM ... -- Can we pass in the recusive step to query from?
+    *
+FROM {{field_step}}
 
 
 
 {% endmacro %}
+
