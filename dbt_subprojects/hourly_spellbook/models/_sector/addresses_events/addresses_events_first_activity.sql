ff75d00403cf53738d62af85f2de71904d0c9c52
Hildebert Moulié <h.moulie@gmail.com>
2025-07-30 23:14:36 +0200

    add opbnb

--- dbt_subprojects/hourly_spellbook/models/_sector/addresses_events/addresses_events_first_activity.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/addresses_events/addresses_events_first_activity.sql
@@ -2,7 +2,7 @@
 (
     alias = 'first_activity',
     schema = 'addresses_events',
-    post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "bnb", "ethereum", "fantom", "gnosis", "optimism", "polygon", "celo", "zksync", "zora", "base", "scroll", "sei", "mantle", "blast", "ronin", "nova", "abstract", "apechain", "berachain", "katana", "ink"]\',
+    post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "bnb", "ethereum", "fantom", "gnosis", "optimism", "polygon", "celo", "zksync", "zora", "base", "scroll", "sei", "mantle", "blast", "ronin", "nova", "abstract", "apechain", "berachain", "katana", "ink", "opbnb"]\',
                                     "sector",
                                     "addresses_events",
                                     \'["Henrystats", "hildobby", "0xr3x"]\') }}'
@@ -35,6 +35,7 @@
 , ref('addresses_events_berachain_first_activity')
 , ref('addresses_events_katana_first_activity')
 , ref('addresses_events_ink_first_activity')
+, ref('addresses_events_opbnb_first_activity')
 ] %}
 
 SELECT *
