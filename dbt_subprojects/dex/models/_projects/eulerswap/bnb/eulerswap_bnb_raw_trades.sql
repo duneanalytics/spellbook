dde718387e5c88a7ebe83638526202cad3329bdc
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-07-31 02:35:04 +0100

    fix seed and update raw trades

--- dbt_subprojects/dex/models/_projects/eulerswap/bnb/eulerswap_bnb_raw_trades.sql
+++ dbt_subprojects/dex/models/_projects/eulerswap/bnb/eulerswap_bnb_raw_trades.sql
@@ -9,6 +9,9 @@
     )
 }}
 
+with 
+
+base_model as (
 {{
     eulerswap_compatible_trades(
         blockchain = 'bnb'
@@ -17,5 +20,52 @@
         , eulerswapinstance_evt_swap = source('eulerswap_bnb', 'eulerswapinstance_evt_swap')
         , eulerswap_pools_created = ref('eulerswap_bnb_pool_creations')
         , filter = "(1 = 1)"
+    )
+}}
+)
+
+, base_union as (
+    select 
+        * 
+    from (
+        select 
+            blockchain
+            , project
+            , version
+            , block_month
+            , block_date
+            , block_time
+            , block_number
+            , cast(token_bought_amount_raw as uint256) as token_bought_amount_raw
+            , cast(token_sold_amount_raw as uint256) as token_sold_amount_raw
+            , token_bought_address
+            , token_sold_address
+            , taker
+            , maker
+            , project_contract_address
+            , tx_hash
+            , evt_index
+            , fee 
+            , protocolFee 
+            , instance 
+            , eulerAccount 
+            , factory_address 
+            , sender 
+            , source
+            , row_number() over (partition by tx_hash, evt_index order by tx_hash) as duplicates_rank
+        from 
+        base_model 
+        where
+           token_sold_amount_raw >= 0 and token_bought_amount_raw >= 0
+    ) 
+    where
+        duplicates_rank = 1
+) 
+
+{{
+    add_tx_columns(
+        model_cte = 'base_union'
+        , blockchain = 'bnb'
+        , columns = ['from', 'to', 'index']
     )
 }}
