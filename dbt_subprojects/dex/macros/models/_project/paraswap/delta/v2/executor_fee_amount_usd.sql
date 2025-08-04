446746d8212eb75baee0cf58ffb56f825c11dd21
Alexey Shchur <hi@alexeyshchur.com>
2025-08-04 07:43:51 +0300

    fix: syntax / redundant comma

--- dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/executor_fee_amount_usd.sql
+++ dbt_subprojects/dex/macros/models/_project/paraswap/delta/v2/executor_fee_amount_usd.sql
@@ -4,7 +4,7 @@
       then
         COALESCE( 
                 d.price * w.executorFeeAmount / POWER(10, d.decimals), -- compute directly (dest token is gas fee compensation if it's augustus axecutor)
-                s.price *  w.srcAmount / POWER(10, s.decimals) * CAST (w.executorFeeAmount AS DECIMAL) / (CAST (w.destAmount AS DECIMAL)), -- compute pro-rata based on src token if it's augustus executor        
+                s.price *  w.srcAmount / POWER(10, s.decimals) * CAST (w.executorFeeAmount AS DECIMAL) / (CAST (w.destAmount AS DECIMAL)) -- compute pro-rata based on src token if it's augustus executor        
         )
       else
         -- if can't parse executorFeeAmount - fall back to spent native token, divided by amount of orders if multiple
