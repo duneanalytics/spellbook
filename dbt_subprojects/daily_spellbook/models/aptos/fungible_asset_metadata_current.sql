ebb69947f3457d78a2bcc4f21165cff847dacf9e
Ying Wu <ying-w@users.noreply.github.com>
2025-08-06 13:53:45 -0700

    no qualify on Trino db

--- dbt_subprojects/daily_spellbook/models/aptos/fungible_asset_metadata_current.sql
+++ dbt_subprojects/daily_spellbook/models/aptos/fungible_asset_metadata_current.sql
@@ -1,2 +1,26 @@
-SELECT * FROM {{ ref('fungible_asset_metadata') }}
-QUALIFY ROW_NUMBER() OVER (PARTITION BY asset_type ORDER BY block_time DESC) = 1
+WITH latest_metadata AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY asset_type ORDER BY block_time DESC) AS rn
+    FROM {{ ref('fungible_asset_metadata') }}
+)
+
+SELECT
+    block_date,
+    tx_version,
+    block_time,
+    tx_hash,
+    --
+    write_set_change_index,
+    asset_type,
+    owner_address,
+    asset_name,
+    asset_symbol,
+    decimals,
+    supply,
+    --
+    token_standard,
+    asset_type_migrated,
+    icon_uri,
+    project_uri,
+    maximum
+FROM latest_metadata
+WHERE rn = 1 -- no easy way to exclude
