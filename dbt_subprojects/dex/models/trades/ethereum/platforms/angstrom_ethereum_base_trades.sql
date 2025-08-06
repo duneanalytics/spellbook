a3a0d591694c5899c433418a16d006c762c09e64
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-08-06 16:31:05 -0700

    angstrom base

--- dbt_subprojects/dex/models/trades/ethereum/platforms/angstrom_ethereum_base_trades.sql
+++ dbt_subprojects/dex/models/trades/ethereum/platforms/angstrom_ethereum_base_trades.sql
@@ -18,7 +18,7 @@ WITH dexs AS
         controller_v1_contract_addr = '0xFE77113460CF1833c4440FD17B4463f472010e10',
         blockchain = 'ethereum',
         project = 'angstrom',
-        version = '2'
+        version = '1'
     )
     }}
 )
