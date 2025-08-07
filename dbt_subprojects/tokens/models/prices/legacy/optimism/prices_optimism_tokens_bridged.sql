ae8618f64a004e06b9f18cb9049786a923b748b7
jeff-dude <jeff@dune.xyz>
2025-08-07 14:34:54 -0400

    ensure no dupes in optimism

--- dbt_subprojects/tokens/models/prices/legacy/optimism/prices_optimism_tokens_bridged.sql
+++ dbt_subprojects/tokens/models/prices/legacy/optimism/prices_optimism_tokens_bridged.sql
@@ -6,12 +6,18 @@
         
         )
 }}
-SELECT
-    e.token_id
-    , 'optimism' as blockchain
-    , e.symbol as symbol
-    , o.l2_token as contract_address
-    , e.decimals
-FROM {{ source('tokens_optimism', 'erc20_bridged_mapping') }} o
-INNER JOIN {{ ref('prices_ethereum_tokens') }} e
-ON e.contract_address = o.l1_token
+with tokens as (
+    SELECT
+        e.token_id
+        , 'optimism' as blockchain
+        , e.symbol as symbol
+        , o.l2_token as contract_address
+        , e.decimals
+        , row_number() over (partition by o.l2_token order by e.token_id) as rn
+    FROM {{ source('tokens_optimism', 'erc20_bridged_mapping') }} o
+    INNER JOIN {{ ref('prices_ethereum_tokens') }} e
+    ON e.contract_address = o.l1_token
+)
+select *
+from tokens
+where rn = 1
