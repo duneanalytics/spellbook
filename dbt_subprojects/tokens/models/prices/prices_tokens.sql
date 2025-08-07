1fc1ef80e7f89c2a12f00e14ccd0af7a57d96fc2
jeff-dude <jeff@dune.xyz>
2025-08-07 14:13:55 -0400

    update prices_tokens to use manual coinpaprika trusted tokens

--- dbt_subprojects/tokens/models/prices/prices_tokens.sql
+++ dbt_subprojects/tokens/models/prices/prices_tokens.sql
@@ -9,47 +9,7 @@
 }}
 
 {% set evm_models = [
-    ref('prices_abstract_tokens')
-    ,ref('prices_apechain_tokens')
-    ,ref('prices_arbitrum_tokens')
-    ,ref('prices_avalanche_c_tokens')
-    ,ref('prices_base_tokens')
-    ,ref('prices_berachain_tokens')
-    ,ref('prices_bnb_tokens')
-    ,ref('prices_blast_tokens')
-    ,ref('prices_boba_tokens')
-    ,ref('prices_celo_tokens')
-    ,ref('prices_corn_tokens')
-    ,ref('prices_ethereum_tokens')
-    ,ref('prices_fantom_tokens')
-    ,ref('prices_flare_tokens')
-    ,ref('prices_gnosis_tokens')
-    ,ref('prices_hemi_tokens')
-    ,ref('prices_ink_tokens')
-    ,ref('prices_katana_tokens')
-    ,ref('prices_kaia_tokens')
-    ,ref('prices_linea_tokens')
-    ,ref('prices_mantle_tokens')
-    ,ref('prices_nova_tokens')
-    ,ref('prices_opbnb_tokens')
-    ,ref('prices_optimism_tokens')
-    ,ref('prices_plume_tokens')
-    ,ref('prices_polygon_tokens')
-    ,ref('prices_ronin_tokens')
-    ,ref('prices_scroll_tokens')
-    ,ref('prices_sei_tokens')
-    ,ref('prices_shape_tokens')
-    ,ref('prices_sonic_tokens')
-    ,ref('prices_sophon_tokens')
-    ,ref('prices_superseed_tokens')
-    ,ref('prices_tac_tokens')
-    ,ref('prices_taiko_tokens')
-    ,ref('prices_unichain_tokens')
-    ,ref('prices_viction_tokens')
-    ,ref('prices_worldchain_tokens')
-    ,ref('prices_zkevm_tokens')
-    ,ref('prices_zksync_tokens')
-    ,ref('prices_zora_tokens')
+    ref('prices_coinpaprika_trusted_tokens')
 ] %}
 
 {% set non_evm_models = [
@@ -108,10 +68,6 @@ with native as (
         , pt.decimals
     from
         {{ model }} as pt
-    inner join
-        {{ source('prices', 'trusted_tokens') }} as trusted
-        on pt.blockchain = trusted.blockchain
-        and pt.contract_address = trusted.contract_address
     left join
         {{ source('dune', 'blockchains') }} as anti
         on anti.name = pt.blockchain
