a08fb90a3c3d90fb4525917223c1dd7c31d9de12
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:13:02 +0200

    refactor: blast network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/blast/safe_blast_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/blast/safe_blast_singletons.sql
@@ -1,22 +1,13 @@
-{{
+{{ 
     config(
         materialized='table',
-        schema = 'safe_blast',
-        alias= 'singletons',
-        post_hook = '{{ expose_spells(
-                        blockchains = \'["blast"]\',
-                        spell_type = "project",
-                        spell_name = "safe",
-                        contributors = \'["danielpartida"]\') }}'
-    )
+        schema = 'gnosis_safe_blast',
+        alias = 'singletons',
+        post_hook='{{ expose_spells(\'["blast"]\',
+                                    "project",
+                                    "safe",
+                                    \'["danielpartida"]\') }}'
+    ) 
 }}
 
-
--- Fetch all known singleton/mastercopy addresses used via factories.
-select distinct singleton as address
-from {{ source('gnosis_safe_blast', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
-
-union
-
-select distinct singleton as address
-from {{ source('gnosis_safe_blast', 'SafeProxyFactory_v1_4_1_evt_ProxyCreation') }}
+{{ safe_singletons_by_network('blast') }}
