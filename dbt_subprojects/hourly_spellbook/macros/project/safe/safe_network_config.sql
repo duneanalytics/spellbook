a3ea83aadb2d35544a46bcc19789743e4ad5fece
Eike Caldeweyher <eike@safe.global>
2025-08-11 15:37:38 +0200

    refactor: use dbt macros

---
+++ dbt_subprojects/hourly_spellbook/macros/project/safe/safe_network_config.sql
@@ -0,0 +1,234 @@
+{% macro safe_network_config() %}
+    {%- set default_contributors = ["tschubotz", "hosuke", "danielpartida", "safehjc"] -%}
+    {%- set networks = {
+        'arbitrum': {
+            'start_date': '2021-06-20',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ],
+            'contributors': {
+                'safes': ["tschubotz", "hosuke", "danielpartida"],
+                'singletons': ["tschubotz", "peterrliem"],
+                'eth_transfers': ["tschubotz", "hosuke"],
+                'transactions': ["tschubotz", "hosuke", "danielpartida"]
+            }
+        },
+        'avalanche_c': {
+            'start_date': '2021-07-29',
+            'native_token': 'AVAX',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+            ],
+            'contributors': {
+                'safes': ["tschubotz", "hosuke"],
+                'singletons': ["tschubotz"]
+            }
+        },
+        'base': {
+            'start_date': '2023-06-15',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ],
+            'contributors': {
+                'safes': ["danielpartida"],
+                'singletons': ["danielpartida", "peterrliem"],
+                'eth_transfers': ["danielpartida", "hosuke"],
+                'transactions': ["danielpartida"]
+            }
+        },
+        'berachain': {
+            'start_date': '2024-01-11',
+            'native_token': 'BERA',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ],
+            'has_native_transfers': false
+        },
+        'blast': {
+            'start_date': '2024-02-24',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'bnb': {
+            'start_date': '2021-01-29',
+            'native_token': 'BNB',
+            'legacy_singleton_sources': [
+                {'table': 'ProxyFactory_v1_1_1_call_createProxy', 'column': 'masterCopy'},
+                {'table': 'ProxyFactory_v1_1_1_call_createProxyWithNonce', 'column': '_mastercopy'},
+                {'table': 'ProxyFactory_v1_1_1_call_createProxyWithCallback', 'column': '_mastercopy'}
+            ],
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+            ]
+        },
+        'celo': {
+            'start_date': '2021-07-20',
+            'native_token': 'CELO',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+            ]
+        },
+        'ethereum': {
+            'start_date': '2018-11-24',
+            'native_token': 'ETH',
+            'singleton_type': 'legacy_ethereum',
+            'contributors': {
+                'safes': ["sche", "tschubotz", "danielpartida", "hosuke"],
+                'singletons': ["tschubotz", "danielpartida"],
+                'eth_transfers': ["tschubotz", "hosuke"],
+                'transactions': ["sche", "tschubotz", "danielpartida", "hosuke"]
+            }
+        },
+        'fantom': {
+            'start_date': '2021-12-20',
+            'native_token': 'FTM',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+            ],
+            'has_native_transfers': false
+        },
+        'gnosis': {
+            'start_date': '2020-05-15',
+            'native_token': 'xDAI',
+            'legacy_singleton_sources': [
+                {'table': 'ProxyFactory_v1_1_1_call_createProxy', 'column': 'masterCopy'},
+                {'table': 'ProxyFactory_v1_1_1_call_createProxyWithNonce', 'column': '_mastercopy'},
+                {'table': 'ProxyFactory_v1_1_1_call_createProxyWithCallback', 'column': '_mastercopy'}
+            ],
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+            ]
+        },
+        'linea': {
+            'start_date': '2023-07-11',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'mantle': {
+            'start_date': '2023-07-14',
+            'native_token': 'MNT',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'optimism': {
+            'start_date': '2021-11-17',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'polygon': {
+            'start_date': '2021-03-07',
+            'native_token': 'MATIC',
+            'singleton_type': 'legacy',
+            'legacy_singleton_sources': [
+                {'table': 'ProxyFactory_v1_1_1_call_createProxy', 'column': 'masterCopy'},
+                {'table': 'ProxyFactory_v1_1_1_call_createProxyWithNonce', 'column': '_mastercopy'},
+                {'table': 'ProxyFactory_v1_1_1_call_createProxyWithCallback', 'column': '_mastercopy'}
+            ],
+            'modern_singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'ronin': {
+            'start_date': '2024-03-19',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+            ]
+        },
+        'scroll': {
+            'start_date': '2023-10-10',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'unichain': {
+            'start_date': '2024-11-19',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'worldchain': {
+            'start_date': '2024-10-21',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'zkevm': {
+            'start_date': '2023-03-24',
+            'native_token': 'MATIC',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+                'SafeProxyFactory_v1_4_1_evt_ProxyCreation'
+            ]
+        },
+        'zksync': {
+            'start_date': '2023-02-14',
+            'native_token': 'ETH',
+            'singleton_type': 'modern',
+            'singleton_sources': [
+                'SafeProxyFactory_v1_3_0_evt_ProxyCreation',
+            ]
+        }
+    } -%}
+    {{ return(networks) }}
+{% endmacro %}
+
+{% macro get_safe_network_config(blockchain) %}
+    {%- set all_networks = safe_network_config() -%}
+    {%- if blockchain in all_networks -%}
+        {{ return(all_networks[blockchain]) }}
+    {%- else -%}
+        {{ exceptions.raise_compiler_error("Network '" ~ blockchain ~ "' not found in safe_network_config") }}
+    {%- endif -%}
+{% endmacro %}
+
+{% macro get_safe_contributors(blockchain, model_type) %}
+    {%- set default_contributors = ["tschubotz", "hosuke", "danielpartida", "safehjc"] -%}
+    {%- set network_config = get_safe_network_config(blockchain) -%}
+    
+    {%- if 'contributors' in network_config and model_type in network_config.contributors -%}
+        {{ return(network_config.contributors[model_type]) }}
+    {%- else -%}
+        {{ return(default_contributors) }}
+    {%- endif -%}
+{% endmacro %}
