911e0413ab30969b610a82195ce25885f0ed22d8
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:18:31 +0200

    refactor: optimism network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/optimism/safe_optimism_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/optimism/safe_optimism_singletons.sql
@@ -1,8 +1,8 @@
 {{ 
     config(
         materialized='table',
+        schema = 'safe_optimism',
         alias = 'singletons',
-        
         post_hook='{{ expose_spells(\'["optimism"]\',
                                     "project",
                                     "safe",
@@ -10,12 +10,4 @@
     ) 
 }}
 
-
--- Fetch all known singleton addresses used via the factory.
-select distinct singleton as address 
-from {{ source('gnosis_safe_optimism', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
-
-union 
-
-select distinct singleton as address 
-from {{ source('gnosis_safe_optimism', 'SafeProxyFactory_v_1_4_1_evt_ProxyCreation') }}
+{{ safe_singletons_by_network('optimism') }}
