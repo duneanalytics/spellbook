dace8987b82a732f5173ed155c546c0685bbef23
AIDataMaster <cryptouserv1@gmail.com>
2025-08-11 13:49:48 +0300

    Add SyncConfig Events

---
+++ dbt_subprojects/hourly_spellbook/models/_project/gmx/event/arbitrum/gmx_v2_arbitrum_sync_config.sql
@@ -0,0 +1,172 @@
+{{
+  config(
+    schema = 'gmx_v2_arbitrum',
+    alias = 'sync_config',
+    materialized = 'incremental',
+    unique_key = ['tx_hash', 'index'],
+    incremental_strategy = 'merge'
+    )
+}}
+
+{%- set event_name = 'SyncConfig' -%}
+{%- set blockchain_name = 'arbitrum' -%}
+
+WITH evt_data_1 AS (
+    SELECT 
+        -- Main Variables
+        '{{ blockchain_name }}' AS blockchain,
+        evt_block_time AS block_time,
+        evt_block_number AS block_number, 
+        evt_tx_hash AS tx_hash,
+        evt_index AS index,
+        contract_address,
+        eventName AS event_name,
+        eventData AS data,
+        msgSender AS msg_sender
+    FROM {{ source('gmx_v2_arbitrum','EventEmitter_evt_EventLog1')}}
+    WHERE eventName = '{{ event_name }}'
+    {% if is_incremental() %}
+        AND {{ incremental_predicate('evt_block_time') }}
+    {% endif %}
+)
+
+, evt_data_2 AS (
+    SELECT 
+        -- Main Variables
+        '{{ blockchain_name }}' AS blockchain,
+        evt_block_time AS block_time,
+        evt_block_number AS block_number, 
+        evt_tx_hash AS tx_hash,
+        evt_index AS index,
+        contract_address,
+        eventName AS event_name,
+        eventData AS data,
+        msgSender AS msg_sender
+    FROM {{ source('gmx_v2_arbitrum','EventEmitter_evt_EventLog2')}}
+    WHERE eventName = '{{ event_name }}'
+    {% if is_incremental() %}
+        AND {{ incremental_predicate('evt_block_time') }}
+    {% endif %}
+)
+
+, evt_data_3 AS (
+    SELECT 
+        -- Main Variables
+        '{{ blockchain_name }}' AS blockchain,
+        evt_block_time AS block_time,
+        evt_block_number AS block_number, 
+        evt_tx_hash AS tx_hash,
+        evt_index AS index,
+        contract_address,
+        eventName AS event_name,
+        eventData AS data,
+        msgSender AS msg_sender
+    FROM {{ source('gmx_v2_arbitrum','EventEmitter_evt_EventLog')}}
+    WHERE eventName = '{{ event_name }}'
+    {% if is_incremental() %}
+        AND {{ incremental_predicate('evt_block_time') }}
+    {% endif %}
+)
+
+-- unite 3 tables
+, evt_data AS (
+    SELECT * 
+    FROM evt_data_1
+    UNION ALL
+    SELECT *
+    FROM evt_data_2
+    UNION ALL
+    SELECT *
+    FROM evt_data_3
+)
+
+, parsed_data AS (
+    SELECT
+        tx_hash,
+        index, 
+        json_query(data, 'lax $.uintItems' OMIT QUOTES) AS uint_items,
+        json_query(data, 'lax $.boolItems' OMIT QUOTES) AS bool_items
+    FROM
+        evt_data
+)
+
+, uint_items_parsed AS (
+    SELECT 
+        tx_hash,
+        index,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.key') AS key_name,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.value') AS value
+    FROM 
+        parsed_data,
+        UNNEST(
+            CAST(json_extract(uint_items, '$.items') AS ARRAY(JSON))
+        ) AS t(item)
+)
+
+, bool_items_parsed AS (
+    SELECT 
+        tx_hash,
+        index,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.key') AS key_name,
+        json_extract_scalar(CAST(item AS VARCHAR), '$.value') AS value
+    FROM 
+        parsed_data,
+        UNNEST(
+            CAST(json_extract(bool_items, '$.items') AS ARRAY(JSON))
+        ) AS t(item)
+)
+
+, combined AS (
+    SELECT *
+    FROM uint_items_parsed
+    UNION ALL
+    SELECT *
+    FROM bool_items_parsed
+)
+
+, evt_data_parsed AS (
+    SELECT
+        tx_hash,
+        index,
+        
+        MAX(CASE WHEN key_name = 'updateId' THEN value END) AS update_id,
+        MAX(CASE WHEN key_name = 'prevValue' THEN value END) AS prev_value,
+        MAX(CASE WHEN key_name = 'nextValue' THEN value END) AS next_value,
+        MAX(CASE WHEN key_name = 'updateApplied' THEN value END) AS update_applied
+
+    FROM
+        combined
+    GROUP BY tx_hash, index
+)
+
+-- full data 
+, full_data AS (
+    SELECT 
+        blockchain,
+        block_time,
+        block_number,
+        ED.tx_hash,
+        ED.index,
+        contract_address,
+        event_name,
+        msg_sender,
+        
+        TRY_CAST(EDP.update_id AS DOUBLE) AS update_id,
+        TRY_CAST(EDP.prev_value AS DOUBLE) / POWER(10, 30) AS prev_value,
+        TRY_CAST(EDP.next_value AS DOUBLE) / POWER(10, 30) AS next_value,
+        update_applied
+
+    FROM evt_data AS ED
+    LEFT JOIN evt_data_parsed AS EDP
+        ON ED.tx_hash = EDP.tx_hash
+            AND ED.index = EDP.index
+)
+
+--can be removed once decoded tables are fully denormalized
+{{
+    add_tx_columns(
+        model_cte = 'full_data'
+        , blockchain = blockchain_name
+        , columns = ['from', 'to']
+    )
+}}
