126c89299af3dcd5c04807a5be8d0e854494bbaa
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:19:45 +0200

    refactor: unichain network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/unichain/safe_unichain_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/unichain/safe_unichain_singletons.sql
@@ -1,7 +1,7 @@
 {{ 
     config(
         materialized='table',
-        
+        schema = 'safe_unichain',
         alias = 'singletons',
         post_hook='{{ expose_spells(\'["unichain"]\',
                                     "project",
@@ -10,12 +10,4 @@
     ) 
 }}
 
-
--- Fetch all known singleton addresses used via the factory.
-select distinct singleton as address 
-from {{ source('gnosis_safe_unichain', 'safeproxyfactory_v1_3_0_evt_proxycreation') }}
-
-union 
-
-select distinct singleton as address 
-from {{ source('gnosis_safe_unichain', 'safeproxyfactory_v1_4_1_evt_proxycreation') }}
+{{ safe_singletons_by_network('unichain') }}
