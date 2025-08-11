11323a5c85c375f9816b6cc2b5064b3868fe43d8
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:19:26 +0200

    refactor: scroll network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/scroll/safe_scroll_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/scroll/safe_scroll_singletons.sql
@@ -1,21 +1,13 @@
-{{
+{{ 
     config(
         materialized='table',
         schema = 'safe_scroll',
-        alias= 'singletons',
-        post_hook = '{{ expose_spells(
-                        blockchains = \'["scroll"]\',
-                        spell_type = "project",
-                        spell_name = "safe",
-                        contributors = \'["danielpartida"]\') }}'
-    )
+        alias = 'singletons',
+        post_hook='{{ expose_spells(\'["scroll"]\',
+                                    "project",
+                                    "safe",
+                                    \'["danielpartida"]\') }}'
+    ) 
 }}
 
-
--- Fetch all known singleton/mastercopy addresses used via factories.
-select distinct singleton as address
-from {{ source('gnosis_safe_scroll', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
-
-union
-select distinct singleton as address
-from {{ source('gnosis_safe_scroll', 'SafeProxyFactory_v1_4_1_evt_ProxyCreation') }}
+{{ safe_singletons_by_network('scroll') }}
--- dbt_subprojects/hourly_spellbook/models/_project/safe/scroll/safe_scroll_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/scroll/safe_scroll_transactions.sql
@@ -1,20 +1,5 @@
-{{
-    config(
-        materialized='incremental',
-        schema = 'safe_scroll',
-        alias= 'transactions',
-        partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'],
-        file_format ='delta',
-        incremental_strategy='merge',
-        post_hook = '{{ expose_spells(
-                        blockchains = \'["scroll"]\',
-                        spell_type = "project",
-                        spell_name = "safe",
-                        contributors = \'["danielpartida"]\') }}'
+{{ 
+    safe_transactions_wrapper(
+        blockchain = 'scroll'
     )
 }}
-
-{% set project_start_date = '2023-10-15' %}
-
-{{ safe_transactions('scroll', project_start_date) }}
