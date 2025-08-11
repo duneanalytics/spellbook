6229b1610dee848e0b33b6fae2f883383336d32c
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:21:31 +0200

    refactor: zksync network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/zksync/safe_zksync_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/zksync/safe_zksync_singletons.sql
@@ -1,4 +1,4 @@
-{{
+{{ 
     config(
         materialized='table',
         schema = 'safe_zksync',
@@ -7,10 +7,7 @@
                                     "project",
                                     "safe",
                                     \'["danielpartida", "kryptaki"]\') }}'
-    )
+    ) 
 }}
 
-
--- Fetch all known singleton/mastercopy addresses used via factories.
-select distinct singleton as address
-from {{ source('gnosis_safe_zksync', 'GnosisSafeProxyFactoryv1_3_0_evt_ProxyCreation') }}
+{{ safe_singletons_by_network('zksync') }}
