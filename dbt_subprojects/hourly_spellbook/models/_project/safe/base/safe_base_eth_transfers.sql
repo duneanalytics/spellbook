e041b5593be28d4c7dccc43e8dc24f1d68edbe82
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:12:16 +0200

    refactor: base network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/base/safe_base_eth_transfers.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/base/safe_base_eth_transfers.sql
@@ -1,24 +1,14 @@
-{{
-    config(
-        schema = 'safe_base',
-        alias = 'eth_transfers',
-        partition_by = ['block_month'],
-        on_schema_change='fail',
-        materialized='incremental',
-        file_format ='delta',
-        incremental_strategy='merge',
-        unique_key = ['block_date', 'address', 'tx_hash', 'trace_address'],
-        post_hook='{{ expose_spells(blockchains = \'["base"]\',
-                                    spell_type = "project",
-                                    spell_name = "safe",
-                                    contributors = \'["danielpartida", "hosuke"]\') }}'
+{{ 
+    safe_config(
+        blockchain = 'base',
+        alias_name = 'eth_transfers'
     )
 }}
 
 {{
     safe_native_transfers(
-        blockchain = 'base'
-        , native_token_symbol = 'ETH'
-        , project_start_date = '2023-07-01'
+        blockchain = 'base',
+        native_token_symbol = 'ETH',
+        project_start_date = '2023-07-01'
     )
 }}
--- dbt_subprojects/hourly_spellbook/models/_project/safe/base/safe_base_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/base/safe_base_transactions.sql
@@ -1,16 +1,7 @@
-{{
-    config(
-        materialized='incremental',
-        schema='safe_base',
-        alias= 'transactions',
-        partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'],
-        file_format ='delta',
-        incremental_strategy='merge',
-        post_hook='{{ expose_spells(blockchains = \'["base"]\',
-                                    spell_type = "project",
-                                    spell_name = "safe",
-                                    contributors = \'["danielpartida"]\') }}'
+{{ 
+    safe_config(
+        blockchain = 'base',
+        alias_name = 'transactions'
     )
 }}
 
