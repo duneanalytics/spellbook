b88b3732645cf2fc81d350ed0036c9babc202a56
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:13:33 +0200

    refactor: celo network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/celo/safe_celo_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/celo/safe_celo_transactions.sql
@@ -1,17 +1,5 @@
-{{
-    config(
-        materialized='incremental',
-        schema='safe_celo',
-        alias = 'transactions',
-        partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'],
-        file_format ='delta',
-        incremental_strategy='merge',
-        post_hook='{{ expose_spells(blockchains = \'["celo"]\',
-                                    spell_type = "project",
-                                    spell_name = "safe",
-                                    contributors = \'["danielpartida", "hosuke"]\') }}'
+{{ 
+    safe_transactions_wrapper(
+        blockchain = 'celo'
     )
 }}
-
-{{ safe_transactions('celo', '2021-06-20') }}
