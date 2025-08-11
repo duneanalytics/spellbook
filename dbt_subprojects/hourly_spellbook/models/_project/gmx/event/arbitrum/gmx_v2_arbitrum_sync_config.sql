f12dc065974f0bb81382dee7de543ddb84eca947
AIDataMaster <cryptouserv1@gmail.com>
2025-08-11 14:32:30 +0300

    Update gmx_v2_arbitrum_sync_config.sql

--- dbt_subprojects/hourly_spellbook/models/_project/gmx/event/arbitrum/gmx_v2_arbitrum_sync_config.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/gmx/event/arbitrum/gmx_v2_arbitrum_sync_config.sql
@@ -156,7 +156,6 @@ WITH evt_data_1 AS (
         TRY_CAST(EDP.prev_value AS DECIMAL(38,0)) / POWER(10, 30) AS prev_value,
         TRY_CAST(EDP.next_value AS DECIMAL(38,0)) / POWER(10, 30) AS next_value,
         TRY_CAST(EDP.update_applied AS BOOLEAN) AS update_applied
-
     FROM evt_data AS ED
     LEFT JOIN evt_data_parsed AS EDP
         ON ED.tx_hash = EDP.tx_hash
