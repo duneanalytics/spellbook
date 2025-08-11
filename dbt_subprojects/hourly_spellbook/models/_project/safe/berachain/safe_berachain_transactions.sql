e5279694bdf9e3cddc2eb92e2170a2674272b76d
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:12:39 +0200

    refactor: berachain network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/berachain/safe_berachain_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/berachain/safe_berachain_transactions.sql
@@ -1,20 +1,5 @@
-{{
-    config(
-        materialized='incremental',
-        schema = 'safe_berachain',
-        alias= 'transactions',
-        partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'],
-        file_format ='delta',
-        incremental_strategy='merge',
-        post_hook = '{{ expose_spells(
-                        blockchains = \'["berachain"]\',
-                        spell_type = "project",
-                        spell_name = "safe",
-                        contributors = \'["petertherock"]\') }}'
+{{ 
+    safe_transactions_wrapper(
+        blockchain = 'berachain'
     )
 }}
-
-{% set project_start_date = '2025-01-01' %}
-
-{{ safe_transactions('berachain', project_start_date) }}
