2aece2d56d0e60a685c83cd57b49a4539757344f
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:12:16 +0200

    refactor: base network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/base/safe_base_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/base/safe_base_singletons.sql
@@ -1,8 +1,8 @@
 {{
     config(
         materialized='table',
-        
-        alias= 'singletons',
+        schema = 'safe_base',
+        alias = 'singletons',
         post_hook='{{ expose_spells(\'["base"]\',
                                     "project",
                                     "safe",
@@ -10,12 +10,4 @@
     )
 }}
 
-
--- Fetch all known singleton/mastercopy addresses used via factories.
-select distinct singleton as address
-from {{ source('gnosis_safe_base', 'GnosisSafeProxyFactoryv_1_3_0_evt_ProxyCreation') }}
-
-union
-
-select distinct singleton as address
-from {{ source('gnosis_safe_base', 'SafeProxyFactory_v_1_4_1_evt_ProxyCreation') }}
+{{ safe_singletons_by_network('base') }}
