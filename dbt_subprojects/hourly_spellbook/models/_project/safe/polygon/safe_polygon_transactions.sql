a8f2f0b06e654fc0f4e822000260d42921fb65f8
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:18:49 +0200

    refactor: polygon network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/polygon/safe_polygon_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/polygon/safe_polygon_transactions.sql
@@ -1,17 +1,5 @@
 {{ 
-    config(
-        materialized='incremental',
-        schema='safe_polygon',
-        alias = 'transactions',
-        partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'],
-        file_format ='delta',
-        incremental_strategy='merge',
-        post_hook='{{ expose_spells(blockchains = \'["polygon"]\',
-                                    spell_type = "project",
-                                    spell_name = "safe",
-                                    contributors = \'["tschubotz", "hosuke", "danielpartida"]\') }}'
+    safe_transactions_wrapper(
+        blockchain = 'polygon'
     )
 }}
-
-{{ safe_transactions('polygon', '2021-03-07') }}
