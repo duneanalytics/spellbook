c4b0a54fb04365df5ade9ad28be51a497a97a062
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:11:41 +0200

    refactor: avalance c network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/avalanche_c/safe_avalanche_c_avax_transfers.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/avalanche_c/safe_avalanche_c_avax_transfers.sql
@@ -1,24 +1,14 @@
 {{ 
-    config(
-        schema = 'safe_avalanche_c',
-        alias = 'avax_transfers',
-        partition_by = ['block_month'],
-        on_schema_change='fail',
-        materialized='incremental',
-        file_format ='delta',
-        incremental_strategy='merge',
-        unique_key = ['block_date', 'address', 'tx_hash', 'trace_address'],
-        post_hook='{{ expose_spells(blockchains = \'["avalanche_c"]\',
-                                    spell_type = "project",
-                                    spell_name = "safe",
-                                    contributors = \'["tschubotz", "hosuke"]\') }}'
-    ) 
+    safe_config(
+        blockchain = 'avalanche_c',
+        alias_name = 'avax_transfers'
+    )
 }}
 
 {{
     safe_native_transfers(
-        blockchain = 'avalanche_c'
-        , native_token_symbol = 'AVAX'
-        , project_start_date = '2021-10-05'
+        blockchain = 'avalanche_c',
+        native_token_symbol = 'AVAX',
+        project_start_date = '2021-10-05'
     )
 }}
--- dbt_subprojects/hourly_spellbook/models/_project/safe/avalanche_c/safe_avalanche_c_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/avalanche_c/safe_avalanche_c_singletons.sql
@@ -1,16 +1,13 @@
-{{ 
+{{
     config(
         materialized='table',
-        
+        schema = 'safe_avalanche_c',
         alias = 'singletons',
         post_hook='{{ expose_spells(\'["avalanche_c"]\',
                                     "project",
                                     "safe",
                                     \'["tschubotz"]\') }}'
-    ) 
+    )
 }}
 
-
--- Fetch all known singleton addresses used via the factory.
-select distinct singleton as address 
-from {{ source('gnosis_safe_avalanche_c', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
+{{ safe_singletons_by_network('avalanche_c') }}
