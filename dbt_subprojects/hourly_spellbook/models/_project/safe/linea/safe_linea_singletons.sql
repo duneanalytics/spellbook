6c224be4b8a5d0282bc0211a8ddaa7c9f6543438
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:14:49 +0200

    refactor: linea network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/linea/safe_linea_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/linea/safe_linea_singletons.sql
@@ -1,22 +1,13 @@
-{{
+{{ 
     config(
         materialized='table',
         schema = 'safe_linea',
-        alias= 'singletons',
-        post_hook = '{{ expose_spells(
-                        blockchains = \'["linea"]\',
-                        spell_type = "project",
-                        spell_name = "safe",
-                        contributors = \'["danielpartida"]\') }}'
-    )
+        alias = 'singletons',
+        post_hook='{{ expose_spells(\'["linea"]\',
+                                    "project",
+                                    "safe",
+                                    \'["danielpartida"]\') }}'
+    ) 
 }}
 
-
--- Fetch all known singleton/mastercopy addresses used via factories.
-select distinct singleton as address
-from {{ source('gnosis_safe_linea', 'SafeProxyFactoryv_1_3_0_evt_ProxyCreation') }}
-
-union
-
-select distinct singleton as address
-from {{ source('gnosis_safe_linea', 'SafeProxyFactoryv_1_4_1_evt_ProxyCreation') }}
+{{ safe_singletons_by_network('linea') }}
