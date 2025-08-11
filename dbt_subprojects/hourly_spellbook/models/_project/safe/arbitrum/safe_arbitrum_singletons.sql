4f6b4ba05ce0af3e3ea5e67379001749220dd9a4
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:11:14 +0200

    refactor: arbitrum network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/arbitrum/safe_arbitrum_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/arbitrum/safe_arbitrum_singletons.sql
@@ -1,7 +1,7 @@
 {{ 
     config(
         materialized='table',
-        
+        schema = 'safe_arbitrum',
         alias = 'singletons',
         post_hook='{{ expose_spells(\'["arbitrum"]\',
                                     "project",
@@ -10,12 +10,4 @@
     ) 
 }}
 
-
--- Fetch all known singleton addresses used via the factory.
-select distinct singleton as address 
-from {{ source('gnosis_safe_arbitrum', 'GnosisSafeProxyFactory_v1_3_0_evt_ProxyCreation') }}
-
-union 
-
-select distinct singleton as address 
-from {{ source('gnosis_safe_arbitrum', 'SafeProxyFactory_v_1_4_1_evt_ProxyCreation') }}
+{{ safe_singletons_by_network('arbitrum') }}
--- dbt_subprojects/hourly_spellbook/models/_project/safe/arbitrum/safe_arbitrum_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/arbitrum/safe_arbitrum_transactions.sql
@@ -1,17 +1,5 @@
 {{ 
-    config(
-        materialized='incremental',
-        schema='safe_arbitrum',
-        alias = 'transactions',
-        partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'], 
-        file_format ='delta',
-        incremental_strategy='merge',
-        post_hook='{{ expose_spells(blockchains =  \'["arbitrum"]\',
-                                    spell_type = "project",
-                                    spell_name = "safe",
-                                    contributors = \'["tschubotz", "hosuke", "danielpartida"]\') }}'
-    ) 
+    safe_transactions_wrapper(
+        blockchain = 'arbitrum'
+    )
 }}
-
-{{ safe_transactions('arbitrum', '2021-06-20') }}
