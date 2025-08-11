f8f26d340e7a58ac8b4df5b1023e10697b8dea2f
Eike Caldeweyher <eike@safe.global>
2025-08-11 16:20:05 +0200

    refactor: worldchain network

--- dbt_subprojects/hourly_spellbook/models/_project/safe/worldchain/safe_worldchain_transactions.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/worldchain/safe_worldchain_transactions.sql
@@ -1,20 +1,5 @@
-{{
-    config(
-        materialized='incremental',
-        schema = 'safe_worldchain',
-        alias= 'transactions',
-        partition_by = ['block_month'],
-        unique_key = ['block_date', 'tx_hash', 'trace_address'],
-        file_format ='delta',
-        incremental_strategy='merge',
-        post_hook = '{{ expose_spells(
-                        blockchains = \'["worldchain"]\',
-                        spell_type = "project",
-                        spell_name = "safe",
-                        contributors = \'["danielpartida"]\') }}'
+{{ 
+    safe_transactions_wrapper(
+        blockchain = 'worldchain'
     )
 }}
-
-{% set project_start_date = '2024-10-01' %}
-
-{{ safe_transactions('worldchain', project_start_date) }}
