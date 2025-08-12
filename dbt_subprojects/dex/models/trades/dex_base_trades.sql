749fdb225cd158919658788dca95fc54bf5c14fe
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-08-13 00:05:21 +0100

    add time filter to dex base trades

--- dbt_subprojects/dex/models/trades/dex_base_trades.sql
+++ dbt_subprojects/dex/models/trades/dex_base_trades.sql
@@ -70,6 +70,7 @@ with base_union as (
         , tx_index
     FROM
         {{ model }}
+    WHERE block_time >= date '2025-06-01'
     {% if not loop.last %}
     UNION ALL
     {% endif %}
--- dbt_subprojects/dex/models/trades/dex_trades.sql
+++ dbt_subprojects/dex/models/trades/dex_trades.sql
@@ -94,8 +94,7 @@ WITH balancer_v3 AS (
         , evt_index
     FROM
         {{ model }}
-    WHERE 1 = 1 
-    AND block_time >= date '2025-06-01'
+    WHERE block_time >= date '2025-06-01'
     {% if is_incremental() %}
     AND
         {{ incremental_predicate('block_time') }}
