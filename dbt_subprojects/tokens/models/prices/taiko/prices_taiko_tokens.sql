5e694386542054fdde5eb54a46bca040de3e3867
crStiv <cryptostiv7@gmail.com>
2025-08-13 12:18:14 +0200

    Update prices_taiko_tokens.sql

--- dbt_subprojects/tokens/models/prices/taiko/prices_taiko_tokens.sql
+++ dbt_subprojects/tokens/models/prices/taiko/prices_taiko_tokens.sql
@@ -19,5 +19,6 @@ FROM
     ('weth-weth', 'taiko', 'WETH', 0xa51894664a773981c6c112c43ce576f315d5b1b6, 18),
     ('taiko-taiko', 'taiko', 'TAIKO', 0xa9d23408b9ba935c230493c40c73824df71a0975, 18),
     ('usdce-usd-coine', 'taiko', 'USDC.e', 0x19e26b0638bf63aa9fa4d14c6baf8d52ebe86c5c, 6),
-    ('usdt-tether', 'taiko', 'USDT', 0x2def195713cf4a606b49d07e520e22c17899a736, 6)
+    ('usdt-tether', 'taiko', 'USDT', 0x2def195713cf4a606b49d07e520e22c17899a736, 6),
+    ('usdte-tether-usde', 'taiko', 'USDT.e', 0x9c2dc7377717603eb92b2655c5f2e7997a4945bd, 6)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
