9186d3755f7afdb4af404997666424d873828b4c
beve0x <gk0001va@gmail.com>
2025-08-14 17:12:07 -0400

    test efficient method to run

--- dbt_subprojects/dex/macros/models/dex_wash_trades.sql
+++ dbt_subprojects/dex/macros/models/dex_wash_trades.sql
@@ -1,38 +1,52 @@
 {% macro dex_wash_trades(blockchain) %}
 
-WITH filter_1_same_wallet AS (
-    -- Same wallet buys and sells to itself
-    SELECT dt1.tx_hash, true AS same_wallet_trading
-    FROM {{ ref('dex_trades') }} dt1
-    INNER JOIN {{ ref('dex_trades') }} dt2 
-        ON dt1.tx_hash = dt2.tx_hash
-        AND dt1.tx_from = dt2.tx_from
-        AND dt1.token_bought_address = dt2.token_sold_address
-        AND dt1.token_sold_address = dt2.token_bought_address
-        AND dt1.blockchain = '{{blockchain}}'
-        AND dt2.blockchain = '{{blockchain}}'
-    WHERE dt1.blockchain = '{{blockchain}}'
+WITH filtered_trades AS (
+    SELECT *
+    FROM {{ ref('dex_trades') }}
+    WHERE blockchain = '{{blockchain}}'
     {% if is_incremental() %}
-    AND dt1.block_time >= date_trunc('day', NOW() - interval '7' day)
+    AND block_time >= date_trunc('day', NOW() - interval '7' day)
     {% endif %}
+),
+
+filter_1_same_wallet AS (
+    -- Same wallet buys and sells the same tokens in same transaction
+    SELECT tx_hash, true AS same_wallet_trading
+    FROM (
+        SELECT 
+            tx_hash,
+            tx_from,
+            -- Check if this wallet both bought and sold the same tokens
+            COUNT(CASE WHEN token_bought_address = token_sold_address THEN 1 END) as same_token_trades,
+            COUNT(DISTINCT token_bought_address) as unique_tokens_bought,
+            COUNT(DISTINCT token_sold_address) as unique_tokens_sold
+        FROM filtered_trades
+        GROUP BY tx_hash, tx_from
+    ) t
+    WHERE same_token_trades > 1  -- Multiple trades of same tokens
+    AND unique_tokens_bought = unique_tokens_sold  -- Same tokens involved
 ),
 
 filter_2_back_forth AS (
     -- Rapid back-and-forth between two wallets
-    SELECT dt1.tx_hash, true AS back_forth_trading
-    FROM {{ ref('dex_trades') }} dt1
-    INNER JOIN {{ ref('dex_trades') }} dt2 
-        ON dt1.tx_from = dt2.tx_to
-        AND dt1.tx_to = dt2.tx_from
-        AND dt1.token_bought_address = dt2.token_sold_address
-        AND dt1.token_sold_address = dt2.token_bought_address
-        AND date_diff('second', dt1.block_time, dt2.block_time) BETWEEN -300 AND 300 -- 5 minutes arbitrary threshold
-        AND dt1.blockchain = '{{blockchain}}'
-        AND dt2.blockchain = '{{blockchain}}'
-    WHERE dt1.blockchain = '{{blockchain}}'
-    {% if is_incremental() %}
-    AND dt1.block_time >= date_trunc('day', NOW() - interval '7' day)
-    {% endif %}
+    SELECT tx_hash, true AS back_forth_trading
+    FROM (
+        SELECT 
+            tx_hash,
+            tx_from,
+            tx_to,
+            token_bought_address,
+            token_sold_address,
+            block_time,
+            -- Find matching reverse trades within 5 minutes
+            LEAD(tx_to) OVER (PARTITION BY tx_from, token_bought_address ORDER BY block_time) as next_tx_to,
+            LEAD(tx_from) OVER (PARTITION BY tx_to, token_sold_address ORDER BY block_time) as next_tx_from,
+            LEAD(block_time) OVER (PARTITION BY tx_from, token_bought_address ORDER BY block_time) as next_block_time
+        FROM filtered_trades
+    ) t
+    WHERE next_tx_to = tx_from 
+    AND next_tx_from = tx_to
+    AND date_diff('second', block_time, next_block_time) BETWEEN -300 AND 300
 ),
 
 filter_3_high_frequency AS (
@@ -40,14 +54,10 @@ filter_3_high_frequency AS (
     SELECT tx_hash, true AS high_frequency_trading
     FROM (
         SELECT tx_hash, COUNT(*) as swap_count
-        FROM {{ ref('dex_trades') }}
-        WHERE blockchain = '{{blockchain}}'
-        {% if is_incremental() %}
-        AND block_time >= date_trunc('day', NOW() - interval '7' day)
-        {% endif %}
+        FROM filtered_trades
         GROUP BY tx_hash
     ) t
-    WHERE swap_count > 50 -- Assumed threshold for high frequency
+    WHERE swap_count > 50
 ),
 
 filter_4_circular_trading AS (
@@ -57,58 +67,28 @@ filter_4_circular_trading AS (
         SELECT tx_hash, 
                COUNT(DISTINCT tx_from) as unique_traders,
                COUNT(*) as total_trades
-        FROM {{ ref('dex_trades') }}
-        WHERE blockchain = '{{blockchain}}'
-        {% if is_incremental() %}
-        AND block_time >= date_trunc('day', NOW() - interval '7' day)
-        {% endif %}
+        FROM filtered_trades
         GROUP BY tx_hash
     ) t
-    WHERE unique_traders <= 3 AND total_trades >= 5 -- This is an arbitrary threshold
+    WHERE unique_traders <= 3 AND total_trades >= 5
 ),
 
 filter_5_net_zero_pnl AS (
-    -- Net zero/minimal PnL after massive volume using USD amounts
+    -- Net zero/minimal PnL after massive volume
     SELECT tx_hash, true AS net_zero_pnl
     FROM (
         SELECT 
             tx_hash,
-            SUM(trader_volume) as total_volume,
-            -- Calculate net position change for each trader in the transaction
-            MAX(ABS(net_position_change)) as max_net_change
-        FROM (
-            -- Calculate net position change per trader per transaction
-            SELECT 
-                tx_hash,
-                trader_address,
-                SUM(amount_usd) as trader_volume,
-                SUM(CASE 
-                    WHEN trader_address = tx_from THEN -amount_usd  -- Selling
-                    WHEN trader_address = tx_to THEN amount_usd     -- Buying
-                    ELSE 0 
-                END) as net_position_change
-            FROM (
-                -- Get all trades with their trader addresses
-                SELECT tx_hash, tx_from as trader_address, amount_usd, blockchain, tx_from, tx_to
-                FROM {{ ref('dex_trades') }} dt1
-                WHERE dt1.blockchain = '{{blockchain}}'
-                {% if is_incremental() %}
-                AND dt1.block_time >= date_trunc('day', NOW() - interval '7' day)
-                {% endif %}
-                UNION ALL
-                SELECT tx_hash, tx_to as trader_address, amount_usd, blockchain, tx_from, tx_to
-                FROM {{ ref('dex_trades') }} dt2
-                WHERE dt2.blockchain = '{{blockchain}}'
-                {% if is_incremental() %}
-                AND dt2.block_time >= date_trunc('day', NOW() - interval '7' day)
-                {% endif %}
-            ) all_trades
-            GROUP BY tx_hash, trader_address
-        ) trader_positions
+            SUM(amount_usd) as total_volume,
+            SUM(CASE 
+                WHEN tx_from = tx_to THEN 0  -- Self-trades
+                ELSE amount_usd 
+            END) as net_position_change
+        FROM filtered_trades
         GROUP BY tx_hash
     ) t
     WHERE total_volume > 100000 
-        AND (max_net_change / total_volume) < 0.002 -- Less than 0.2% net change - this is an arbitrary threshold
+    AND (ABS(net_position_change) / total_volume) < 0.002
 )
 
 SELECT 
@@ -151,15 +131,11 @@ SELECT
          THEN true
          ELSE false
     END AS is_wash_trade
-FROM {{ ref('dex_trades') }} dt
+FROM filtered_trades dt
 LEFT JOIN filter_1_same_wallet f1 ON dt.tx_hash = f1.tx_hash
 LEFT JOIN filter_2_back_forth f2 ON dt.tx_hash = f2.tx_hash
 LEFT JOIN filter_3_high_frequency f3 ON dt.tx_hash = f3.tx_hash
 LEFT JOIN filter_4_circular_trading f4 ON dt.tx_hash = f4.tx_hash
 LEFT JOIN filter_5_net_zero_pnl f5 ON dt.tx_hash = f5.tx_hash
-WHERE dt.blockchain = '{{blockchain}}'
-{% if is_incremental() %}
-AND dt.block_time >= date_trunc('day', NOW() - interval '7' day)
-{% endif %}
 
 {% endmacro %}
