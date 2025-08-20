2d5709f41d8e5a7792b24a810ad4bde3e1cbcc97
disco chuck <uv@househeads.org>
2025-08-20 07:30:32 -0500

    Update etherex_v3_linea_base_trades.sql
    removed artifacts from pancakeswaps PR (everything for etherex is under etherex_linea)

--- dbt_subprojects/dex/models/trades/linea/platforms/etherex_v3_linea_base_trades.sql
+++ dbt_subprojects/dex/models/trades/linea/platforms/etherex_v3_linea_base_trades.sql
@@ -1,6 +1,6 @@
 {{
     config(
-        schema = 'etherex_v3_linea',
+        schema = 'etherex_linea',
         alias = 'base_trades',
         materialized = 'incremental',
         file_format = 'delta',
