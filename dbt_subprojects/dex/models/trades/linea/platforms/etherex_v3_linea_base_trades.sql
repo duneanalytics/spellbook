7d0396e6979fa911eb34c035672819ae6ef258ee
disco chuck <uv@househeads.org>
2025-08-20 08:29:18 -0500

    Update etherex_v3_linea_base_trades.sql

--- dbt_subprojects/dex/models/trades/linea/platforms/etherex_v3_linea_base_trades.sql
+++ dbt_subprojects/dex/models/trades/linea/platforms/etherex_v3_linea_base_trades.sql
@@ -1,6 +1,6 @@
 {{
     config(
-        schema = 'etherex_linea',
+        schema = 'etherex_v3_linea',
         alias = 'base_trades',
         materialized = 'incremental',
         file_format = 'delta',
