1e822a3f56dc115b60c02952465d0cdef2218144
beve0x <gk0001va@gmail.com>
2025-08-20 16:54:13 -0400

    handling duplicates

--- dbt_subprojects/dex/models/trades/sei/platforms/dragon_swap_v3_sei_base_trades.sql
+++ dbt_subprojects/dex/models/trades/sei/platforms/dragon_swap_v3_sei_base_trades.sql
@@ -10,13 +10,40 @@
     )
 }}
 
-SELECT DISTINCT *
-FROM {{
-    uniswap_compatible_v3_trades(
-        blockchain = 'sei',
-        project = 'dragon_swap',
-        version = '3',
-        Pair_evt_Swap = source('dragon_swap_sei_v2_sei', 'dragonswapv2pool_evt_swap'),
-        Factory_evt_PoolCreated = source('dragon_swap_sei_v2_sei', 'dragonswapv2factory_evt_poolcreated')
-    )
-}}
+WITH base_trades AS (
+    {{
+        uniswap_compatible_v3_trades(
+            blockchain = 'sei',
+            project = 'dragon_swap',
+            version = '3',
+            Pair_evt_Swap = source('dragon_swap_sei_v2_sei', 'dragonswapv2pool_evt_swap'),
+            Factory_evt_PoolCreated = source('dragon_swap_sei_v2_sei', 'dragonswapv2factory_evt_poolcreated')
+        )
+    }}
+),
+
+deduplicated_trades AS (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY tx_hash, evt_index ORDER BY block_time) as rn
+    FROM base_trades
+)
+
+SELECT 
+    blockchain,
+    project,
+    version,
+    block_month,
+    block_date,
+    block_time,
+    block_number,
+    token_bought_amount_raw,
+    token_sold_amount_raw,
+    token_bought_address,
+    token_sold_address,
+    taker,
+    maker,
+    project_contract_address,
+    tx_hash,
+    evt_index
+FROM deduplicated_trades
+WHERE rn = 1
