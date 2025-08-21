13cf16869364b7bb33b68c397dd29e535a16a83a
Hildebert Moulié <h.moulie@gmail.com>
2025-08-22 00:54:30 +0200

    split out spell for platform specific blockchain indexes

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/chain_indexes/bridges_cctp_chain_indexes.sql
@@ -0,0 +1,25 @@
+{{ config(
+    schema = 'bridges',
+    alias = 'cctp_chain_indexes',
+    materialized = 'view',
+    )
+}}
+
+SELECT id, blockchain
+    FROM (VALUES
+    (0, 'ethereum')
+    , (1, 'avalanche_c')
+    , (2, 'optimism')
+    , (3, 'arbitrum')
+    , (4, 'noble')
+    , (5, 'solana')
+    , (6, 'base')
+    , (7, 'polygon')
+    , (8, 'sui')
+    , (9, 'aptos')
+    , (10, 'unichain')
+    , (11, 'linea')
+    , (12, 'codex')
+    , (13, 'sonic')
+    , (14, 'worldchain')
+    ) AS x (id, blockchain)
