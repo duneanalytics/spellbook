0c9a332887954a6ee962e9f9593abf120c04d233
Max-Morrow <amidmoro@gmail.com>
2025-08-21 11:47:22 +0300

    transfers prematerialization

---
+++ dbt_subprojects/dex/models/_projects/oneinch/arbitrum/oneinch_arbitrum_transfers.sql
@@ -0,0 +1,24 @@
+{% set blockchain = 'arbitrum' %}
+
+
+
+{{
+    config(
+        schema = 'oneinch_' + blockchain,
+        alias = 'transfers',
+        partition_by = ['block_month', 'block_date'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        unique_key = ['blockchain', 'tx_hash', 'transfer_trace_address']
+    )
+}}
+
+
+
+{{
+    oneinch_transfers_macro(
+        blockchain = blockchain
+    )
+}}
