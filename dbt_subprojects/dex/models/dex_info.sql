f8d0f7555bd7ebca5a77bd0bf7ea668d17c0da7a
Cuauhtemoc Ramirez Hernandez <660153+ktemo@users.noreply.github.com>
2025-08-29 15:45:28 -0600

    add project info

--- dbt_subprojects/dex/models/dex_info.sql
+++ dbt_subprojects/dex/models/dex_info.sql
@@ -230,4 +230,5 @@ FROM (VALUES
     , ('blackhole', 'Blackhole', 'Direct', 'BlackholeDEX')
     , ('ekubo', 'Ekubo', 'Direct', 'EkuboProtocol')
     , ('angstrom', 'Angstrom', 'Direct', 'angstromxyz')
+    , ('blade', 'Blade', 'Direct', 'SushiSwap')
 ) AS temp_table (project, name, marketplace_type, x_username)
