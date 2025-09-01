aca0a09edd311960b66e88688f38f1749c1dd73e
PatelPrinci <principatel@lampros.tech>
2025-09-01 11:49:12 +0530

    Updated all sources to jinja syntax

--- dbt_subprojects/dex/models/trades/apechain/platforms/saru_apechain_base_trades.sql
+++ dbt_subprojects/dex/models/trades/apechain/platforms/saru_apechain_base_trades.sql
@@ -8,38 +8,35 @@
     unique_key = ['tx_hash', 'evt_index']
 ) }}
 
-WITH token0_calls 
- AS (
+WITH token0_calls AS (
   SELECT
     contract_address AS pool_address,
     output_0 AS token0_address,
     call_block_time,
     call_block_number
-  FROM "delta_prod"."saru_apechain"."sarupair_call_token0"
-  WHERE
-    call_success = TRUE
+  FROM {{ source('saru_apechain', 'sarupair_call_token0') }}
+  WHERE call_success = TRUE
 ), latest_token0 AS (
   SELECT
     pool_address,
     token0_address,
     ROW_NUMBER() OVER (PARTITION BY pool_address ORDER BY call_block_number DESC, call_block_time DESC) AS rn
   FROM token0_calls
-), token1_calls /* Latest successful token1() calls per pool */ AS (
+), token1_calls AS (
   SELECT
     contract_address AS pool_address,
     output_0 AS token1_address,
     call_block_time,
     call_block_number
-  FROM "delta_prod"."saru_apechain"."sarupair_call_token1"
-  WHERE
-    call_success = TRUE
+  FROM {{ source('saru_apechain', 'sarupair_call_token1') }}
+  WHERE call_success = TRUE
 ), latest_token1 AS (
   SELECT
     pool_address,
     token1_address,
     ROW_NUMBER() OVER (PARTITION BY pool_address ORDER BY call_block_number DESC, call_block_time DESC) AS rn
   FROM token1_calls
-), pool_tokens /* Final pool to tokens mapping */ AS (
+), pool_tokens AS (
   SELECT
     t0.pool_address,
     t0.token0_address,
@@ -47,9 +44,8 @@ WITH token0_calls
   FROM latest_token0 AS t0
   JOIN latest_token1 AS t1
     ON t0.pool_address = t1.pool_address
-  WHERE
-    t0.rn = 1 AND t1.rn = 1
-), swap_events /* Swap events with raw amounts */ AS (
+  WHERE t0.rn = 1 AND t1.rn = 1
+), swap_events AS (
   SELECT
     evt_block_number AS block_number,
     TRY_CAST(evt_block_time AS TIMESTAMP(3) WITH TIME ZONE) AS block_time,
@@ -63,9 +59,9 @@ WITH token0_calls
     to,
     evt_tx_hash AS tx_hash,
     evt_index AS evt_index
-  FROM "delta_prod"."saru_apechain"."sarupair_evt_swap"
-
+  FROM {{ source('saru_apechain', 'sarupair_evt_swap') }}
 )
+
 SELECT
   'apechain' AS blockchain,
   'saru' AS project,
