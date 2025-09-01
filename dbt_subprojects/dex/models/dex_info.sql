b9acf087408573bb557f44bbd85dec117b852ea4
PatelPrinci <principatel@lampros.tech>
2025-09-01 15:57:45 +0530

    SwapFish DEX Trades Added on Arbitrum

--- dbt_subprojects/dex/models/dex_info.sql
+++ dbt_subprojects/dex/models/dex_info.sql
@@ -230,4 +230,5 @@ FROM (VALUES
     , ('blackhole', 'Blackhole', 'Direct', 'BlackholeDEX')
     , ('ekubo', 'Ekubo', 'Direct', 'EkuboProtocol')
     , ('angstrom', 'Angstrom', 'Direct', 'angstromxyz')
+    , ('swapfish', 'SwapFish', 'Direct', 'SwapFishFi')
 ) AS temp_table (project, name, marketplace_type, x_username)
