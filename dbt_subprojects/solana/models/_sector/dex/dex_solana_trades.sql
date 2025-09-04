099a274928676e1eaa4725c33a813e5fc889c432
beve0x <gk0001va@gmail.com>
2025-09-04 11:06:37 -0400

    revert changes

--- dbt_subprojects/solana/models/_sector/dex/dex_solana_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/dex_solana_trades.sql
@@ -86,7 +86,6 @@ LEFT JOIN
     {% if is_incremental() %}
     AND {{incremental_predicate('p_bought.minute')}}
     {% endif %}
-    -- test run, remove this
 LEFT JOIN 
     {{ source('prices', 'usd') }} p_sold
     ON p_sold.blockchain = 'solana'
