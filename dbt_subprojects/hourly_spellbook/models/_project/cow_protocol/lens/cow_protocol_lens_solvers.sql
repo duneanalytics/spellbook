09891bc5adc9efb5c8f1a15fa9b31999574e531b
tamir <tamir@cow.fi>
2025-09-04 10:12:27 +0300

    This PR adds new solvers to cow protocol and initializes lens
    Helixbox:
    Lens

---
+++ dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/lens/cow_protocol_lens_solvers.sql
@@ -0,0 +1,50 @@
+{{ config(
+        schema = 'cow_protocol_lens',
+        alias='solvers',
+        post_hook='{{ expose_spells(blockchains = \'["lens"]\',
+                                    spell_type = "project",
+                                    spell_name = "cow_protocol",
+                                    contributors = \'["tamir-cow"]\') }}'
+)}}
+
+WITH
+-- Aggregate the solver added and removed events into a single table
+-- with true/false for adds/removes respectively
+solver_activation_events as (
+    select solver, evt_block_number, evt_index, True as activated
+    from {{ source('gnosis_protocol_v2_lens', 'GPv2AllowListAuthentication_evt_SolverAdded') }}
+    union
+    select solver, evt_block_number, evt_index, False as activated
+    from {{ source('gnosis_protocol_v2_lens', 'GPv2AllowListAuthentication_evt_SolverRemoved') }}
+),
+-- Sorting by (evt_block_number, evt_index) allows us to pick the most recent activation status of each unique solver
+ranked_solver_events as (
+    select
+        rank() over (partition by solver order by evt_block_number desc, evt_index desc) as rk,
+        solver,
+        evt_block_number,
+        evt_index,
+        activated as active
+    from solver_activation_events
+),
+registered_solvers as (
+    select solver, active
+    from ranked_solver_events
+    where rk = 1
+),
+-- Manually inserting environment and name for each "known" solver
+known_solver_metadata (address, environment, name) as (
+    select *
+    from (
+      VALUES
+                (0x1939B77693c4c477B4c6b75965Bef05356A756EF, 'barn', 'Helixbox'),
+                (0x2843af285B7e82f2CBc96A45289884F55a460EDe, 'prod', 'Helixbox')
+    ) as _
+)
+-- Combining the metadata with current activation status for final table
+select solver as address,
+       case when environment is not null then environment else 'new' end as environment,
+       case when name is not null then name else 'Uncatalogued'      end as name,
+      active
+from registered_solvers
+left outer join known_solver_metadata on solver = address
