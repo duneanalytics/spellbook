6c9e3bde0c59a122786f4321d2fe49610933bd86
Krish <113653936+krishgka@users.noreply.github.com>
2025-09-04 16:56:52 -0400

    Revert "CUR2-474 Solfi fix again (#8674)"
    This reverts commit 2f5c2dfdaa50cb257030ebd74f866f45ac543d42.

--- dbt_subprojects/solana/models/_sector/dex/dex_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/dex_solana_base_trades.sql
@@ -30,7 +30,7 @@ ref('raydium_v3_base_trades')
   , ref('pumpswap_solana_base_trades')
   , ref('pancakeswap_v3_solana_base_trades') 
   , ref('stabble_solana_base_trades')
-  , ref('solfi_solana_base_trades')
+  , ref('solfi_solana_base_trades') 
 ] %}
 
 /*
