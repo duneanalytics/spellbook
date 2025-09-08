b6c9476f8cd5895bb968bdcd0f1065f464419a82
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-09-08 22:32:05 +0100

    smol fix

--- dbt_subprojects/dex/models/_projects/fluid/fluid_daily_agg_liquidity_events.sql
+++ dbt_subprojects/dex/models/_projects/fluid/fluid_daily_agg_liquidity_events.sql
@@ -39,7 +39,7 @@ select
     , sum(case when fp.supply_token = ep.token_address then ep.supply_amount_raw / pow(10, fp.supply_token_decimals) else 0 end) as amount0
     , max(case when fp.supply_token = ep.token_address then ep.supply_exchange_price else 0 end) as amount0_price
     , sum(case when fp.borrow_token = ep.token_address then ep.supply_amount_raw else 0 end) as amount1_raw 
-    , sum(case when fp.supply_token = ep.token_address then ep.supply_amount_raw / pow(10, fp.borrow_token_decimals) else 0 end) as amount1
+    , sum(case when fp.borrow_token = ep.token_address then ep.supply_amount_raw / pow(10, fp.borrow_token_decimals) else 0 end) as amount1
     , max(case when fp.borrow_token = ep.token_address then ep.supply_exchange_price else 0 end) as amount1_price
 from 
 enrich_prices ep 
