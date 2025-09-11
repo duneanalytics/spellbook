4b54af9cadebaeef07ad492b0fc1c06d03365326
Eike Caldeweyher <eike@safe.global>
2025-09-11 11:41:11 +0200

    chore: incremental safe config and singleton models

--- dbt_subprojects/hourly_spellbook/models/_project/safe/berachain/safe_berachain_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/berachain/safe_berachain_singletons.sql
@@ -1,6 +1,6 @@
-{{ safe_table_config(
+{{ safe_incremental_singleton_config(
     blockchain = 'berachain',
     alias_name = 'singletons'
 ) }}
 
-{{ safe_singletons_by_network_validated('berachain', only_official=true, date_filter=true) }}
+{{ safe_singletons_by_network_validated('berachain', only_official=true) }}
--- dbt_subprojects/hourly_spellbook/models/_project/safe/linea/safe_linea_singletons.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/linea/safe_linea_singletons.sql
@@ -1,6 +1,6 @@
-{{ safe_table_config(
+{{ safe_incremental_singleton_config(
     blockchain = 'linea',
     alias_name = 'singletons'
 ) }}
 
-{{ safe_singletons_by_network_validated('linea', only_official=true, date_filter=true) }}
+{{ safe_singletons_by_network_validated('linea', only_official=true) }}
