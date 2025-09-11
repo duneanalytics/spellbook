84588c51b51c209ab0cbfc38183a159ded27256c
markysha <mark.kornejchik@gmail.com>
2025-09-11 17:46:45 +0300

    fix username

--- dbt_subprojects/daily_spellbook/models/tonco/ton/tonco_ton_pool_init.sql
+++ dbt_subprojects/daily_spellbook/models/tonco/ton/tonco_ton_pool_init.sql
@@ -9,7 +9,7 @@
        , post_hook='{{ expose_spells(\'["ton"]\',
                                    "project",
                                    "tonco",
-                                   \'["markysha"]\') }}'
+                                   \'["markfromton"]\') }}'
    )
  }}
 
