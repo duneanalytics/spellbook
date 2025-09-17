4587bdcaa4317fcf3be3ef4cdb5f0258512a8a21
0xRob <83790096+0xRobin@users.noreply.github.com>
2025-09-17 12:24:51 +0200

    ignore native + match on block_time

--- dbt_subprojects/solana/models/_sector/dex/solfi/solfi_solana_token_transfers.sql
+++ dbt_subprojects/solana/models/_sector/dex/solfi/solfi_solana_token_transfers.sql
@@ -16,7 +16,7 @@
 -- Base swaps from solfi_call_swap table
 WITH solfi_swaps AS (
     SELECT distinct
-        date_trunc('day', call_block_time) as block_date
+        call_block_time as block_time
         , call_block_slot as block_slot
         , call_tx_index as tx_index
         , call_outer_instruction_index as outer_instruction_index
@@ -32,6 +32,7 @@ WITH solfi_swaps AS (
     select * 
     from {{ source('tokens_solana','transfers') }}
     where 1=1
+        AND token_version != 'native'
         {% if is_incremental() %}
         AND {{ incremental_predicate('block_time') }}
         {% else %}
@@ -41,4 +42,4 @@ WITH solfi_swaps AS (
 
 select *
 from token_transfers
-inner join solfi_swaps using (block_date, block_slot, tx_index, outer_instruction_index)
+inner join solfi_swaps using (block_time, block_slot, tx_index, outer_instruction_index)
