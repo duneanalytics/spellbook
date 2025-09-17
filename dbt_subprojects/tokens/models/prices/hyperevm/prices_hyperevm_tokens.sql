f72189610fb9eb26e1ef1130862dee4559257dd1
beve0x <gk0001va@gmail.com>
2025-09-16 22:02:36 -0400

    updated decimal

--- dbt_subprojects/tokens/models/prices/hyperevm/prices_hyperevm_tokens.sql
+++ dbt_subprojects/tokens/models/prices/hyperevm/prices_hyperevm_tokens.sql
@@ -22,6 +22,6 @@ FROM
     , ('hype-hyperliquid', 'WHYPE', 0x5555555555555555555555555555555555555555, 18)
     , ('eth-ethereum', 'UETH', 0xbe6727b535545c67d5caa73dea54865b92cf7907, 18)
     , ('btc-bitcoin', 'UBTC', 0x9fdbda0a5e284c32744d2f17ee5c74b284993463, 8)
-    , ('pump-pumpfun', 'UPUMP', 0x27ec642013bcb3d80ca3706599d3cda04f6f4452, 18)
+    , ('pump-pumpfun', 'UPUMP', 0x27ec642013bcb3d80ca3706599d3cda04f6f4452, 6)
     , ('sol-solana', 'USOL', 0x068f321fa8fb9f0d135f290ef6a3e2813e1c8a29, 9)
 ) as temp (token_id, symbol, contract_address, decimals)
