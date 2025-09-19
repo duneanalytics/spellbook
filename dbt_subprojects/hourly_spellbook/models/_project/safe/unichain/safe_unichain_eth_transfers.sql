4e4f050b1c757ab56cfe78d0f1f36dedd86c4079
danielpartida <partidapd@gmail.com>
2025-09-19 14:41:45 +0200

    Implement spell for tracking native transfers for Safe on unichain

---
+++ dbt_subprojects/hourly_spellbook/models/_project/safe/unichain/safe_unichain_eth_transfers.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        schema = 'safe_unichain',
+        alias= 's_transfers',
+        partition_by = ['block_month'],
+        on_schema_change='fail',
+        materialized='incremental',
+        file_format ='delta',
+        incremental_strategy='merge',
+        unique_key = ['block_date', 'address', 'tx_hash', 'trace_address'],
+        post_hook = '{{ expose_spells(blockchains = \'["unichain"]\',
+                                    spell_type = "project",
+                                    spell_name = "safe",
+                                    contributors = \'["danielpartida"]\') }}'
+    )
+}}
+
+{% set project_start_date = '2025-11-04' %}
+
+{{
+    safe_native_transfers(
+        blockchain = 'unichain',
+        native_token_symbol = 'ETH',
+        project_start_date = project_start_date
+    )
+}}
