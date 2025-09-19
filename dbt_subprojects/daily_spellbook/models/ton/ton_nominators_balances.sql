d2ce5af066f3fe484843928fc81730a3537e3cd8
Pavel Shuvalov <pavel.s@ton.org>
2025-09-19 10:57:58 +0400

    minor fix

--- dbt_subprojects/daily_spellbook/models/ton/ton_nominators_balances.sql
+++ dbt_subprojects/daily_spellbook/models/ton/ton_nominators_balances.sql
@@ -5,7 +5,7 @@
     )
 }}
 
-SELECT pool, user_address, pool_type, greatest(0, sum(value * case when direction = 'in' then 1 else -1 end) )/ 1e9 as current_balance,
+SELECT pool_address, user_address, pool_type, greatest(0, sum(value * case when direction = 'in' then 1 else -1 end) )/ 1e9 as current_balance,
 min_by(tx_hash, block_time) as first_tx,
 max_by(tx_hash, block_time) as last_tx,
 count(1) as tx_count
