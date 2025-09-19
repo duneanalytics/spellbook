a1a95fa912de2b77acc095799d4699b0d76ee0bb
Broderick Bonelli <broderickbonelli@gmail.com>
2025-09-19 13:00:29 -0500

    add block_time

--- dbt_subprojects/dex/models/_projects/aerodrome/aerodrome_pool_fees.sql
+++ dbt_subprojects/dex/models/_projects/aerodrome/aerodrome_pool_fees.sql
@@ -4,7 +4,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_number', 'pool'],
+    unique_key = ['block_number', 'pool', 'block_time'],
     post_hook='{{ expose_spells(blockchains = \'["base"]\',
                                   spell_type = "project", 
                                   spell_name = "aerodrome", 
@@ -16,26 +16,28 @@ WITH cl_pool_fee_per_block AS(
     SELECT 
         pool, 
         call_block_number AS block_number,
+        call_block_time AS block_time,
         MAX(output_0) AS fee
     FROM {{ source('aerodrome_base', 'CLFactory_call_getSwapFee') }}
     WHERE call_success = True
     {% if is_incremental() %}
         AND {{ incremental_predicate('call_block_time') }}
     {% endif %}
-    GROUP BY 1,2
+    GROUP BY 1,2,3
 ),
 
 non_cl_pool_fee_per_block AS(
     SELECT 
         pool, 
         call_block_number AS block_number,
-         MAX(output_0) AS fee
+        call_block_time AS block_time,
+        MAX(output_0) AS fee
     FROM {{ source('aerodrome_base', 'poolfactory_call_getfee') }}
     WHERE call_success = True
     {% if is_incremental() %}
         AND {{ incremental_predicate('call_block_time') }}
     {% endif %}
-    GROUP BY 1,2
+    GROUP BY 1,2,3
 )
 
 SELECT * FROM cl_pool_fee_per_block
