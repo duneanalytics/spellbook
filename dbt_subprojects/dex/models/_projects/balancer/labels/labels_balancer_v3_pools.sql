c1fbe83161b3286a60481fd00eaaca0c71cd8531
Gosuto Inzasheru <gosuto@inzasheru.email>
2025-09-19 18:43:30 +0200

    feat: add balancer v3 on avalanche

--- dbt_subprojects/dex/models/_projects/balancer/labels/labels_balancer_v3_pools.sql
+++ dbt_subprojects/dex/models/_projects/balancer/labels/labels_balancer_v3_pools.sql
@@ -1,10 +1,10 @@
 {{config(
         schema = 'labels',
-        alias = 'balancer_v3_pools',        
-        post_hook='{{ expose_spells(\'["arbitrum", "base", "ethereum", "gnosis"]\',
+        alias = 'balancer_v3_pools',
+        post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "ethereum", "gnosis"]\',
                                     "sector",
                                     "labels",
-                                    \'["balancerlabs", "viniabussafi"]\') }}')}}
+                                    \'["balancerlabs", "viniabussafi", "gosuto"]\') }}')}}
 
 SELECT * FROM  {{ ref('labels_balancer_v3_pools_ethereum') }}
 UNION
@@ -13,3 +13,5 @@ UNION
 SELECT * FROM  {{ ref('labels_balancer_v3_pools_arbitrum') }}
 UNION
 SELECT * FROM  {{ ref('labels_balancer_v3_pools_base') }}
+UNION
+SELECT * FROM  {{ ref('labels_balancer_v3_pools_avalanche_c') }}
