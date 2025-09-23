fafe3d2a2b29c2c5c59f9b5881c003db8d8e4e4f
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-09-23 14:58:53 +0100

    curve pool liquidity

---
+++ dbt_subprojects/dex/models/_projects/curvefi/curvefi_tvl_daily.sql
@@ -0,0 +1,32 @@
+{{
+  config(
+    schema = 'curvefi',
+    alias = 'tvl_daily',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['address', 'token_address', 'day'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
+  )
+}}
+
+WITH
+
+pool_addresses AS (
+  select 
+    pool_address as address,
+    coin0 as token_address
+),
+
+filtered_balances AS (
+  {{ balances_incremental_subset_daily(
+       blockchain='ethereum',
+       start_date='2021-11-11',
+       address_token_list='pool_addresses'
+  ) }}
+)
+
+select 
+    * 
+from 
+filtered_balances
