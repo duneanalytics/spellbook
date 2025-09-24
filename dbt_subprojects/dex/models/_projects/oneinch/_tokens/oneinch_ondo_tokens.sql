756c0e468ff8fada0b9f236eaad4802bfc623718
grkhr <eduardgorkh@gmail.com>
2025-09-24 13:44:14 +0300

    ondo uniqueness

--- dbt_subprojects/dex/models/_projects/oneinch/_tokens/oneinch_ondo_tokens.sql
+++ dbt_subprojects/dex/models/_projects/oneinch/_tokens/oneinch_ondo_tokens.sql
@@ -12,10 +12,14 @@
 -- currently view as just one source table and it's light (100 rows), if more sources added in future we can switch to table / incremental
 
 
-select
-    'ethereum' as blockchain
-    , proxy as contract_address
-    , ticker as symbol
-    , name
-    , evt_block_time as block_time 
-from {{ source('ondo_finance_ethereum', 'GMTokenFactory_evt_NewGMTokenDeployed') }}
+select * from (
+    select
+        'ethereum' as blockchain
+        , proxy as contract_address
+        , ticker as symbol
+        , name
+        , evt_block_time as block_time 
+        , row_number() over (partition by blockchain, proxy order by evt_block_time desc) as rn
+    from {{ source('ondo_finance_ethereum', 'GMTokenFactory_evt_NewGMTokenDeployed') }}
+)
+where rn = 1 -- take latest event only in case of re-deployments
