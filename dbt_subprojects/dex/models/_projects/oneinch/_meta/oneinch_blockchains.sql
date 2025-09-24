0f91b0bcd814b81750b9f65cf903244aa220f20b
Max-Morrow <amidmoro@gmail.com>
2025-09-24 17:05:35 +0300

    fix

--- dbt_subprojects/dex/models/_projects/oneinch/_meta/oneinch_blockchains.sql
+++ dbt_subprojects/dex/models/_projects/oneinch/_meta/oneinch_blockchains.sql
@@ -15,12 +15,12 @@ from (
         select
             '{{ blockchain }}' as blockchain
             , {{ meta.chain_id.get(blockchain, 'null') }} as chain_id
-            , date('{{ meta['start'].get(blockchain, 'null') }}') as first_deployed_at
-            , '{{ meta.native_token_symbol.get(blockchain, '') }}' as native_token_symbol
-            , {{ meta['wrapped_native_token_address'].get(blockchain, 'null') }} as wrapped_native_token_address
-            , '{{ meta['explorer_link'].get(blockchain, 'null') }}' as explorer_link
-            , array[{{ meta['fusion_settlement_addresses'].get(blockchain, 'null') | join(', ') }}] as fusion_settlement_addresses
-            , array[{{ meta['escrow_factory_addresses'].get(blockchain, 'null') | join(', ') }}] as escrow_factory_addresses
+            , date('{{ meta['start'][blockchain] }}') as first_deployed_at
+            , {{ meta.native_token_symbol.get(blockchain, 'null') }} as native_token_symbol
+            , {{ meta['wrapped_native_token_address'].get(blockchain, 'cast(null as varbinary)') }} as wrapped_native_token_address
+            , {{ meta['explorer_link'].get(blockchain, 'null') }} as explorer_link
+            , array[{{ meta['fusion_settlement_addresses'].get(blockchain, []) | join(', ') }}] as fusion_settlement_addresses
+            , array[{{ meta['escrow_factory_addresses'].get(blockchain, []) | join(', ') }}] as escrow_factory_addresses
             , '{{ meta['exposed'].get(blockchain, '') }}' as exposed
         {% if not loop.last %}union{% endif %}
     {% endfor %}
