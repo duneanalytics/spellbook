f94dfb88da9e1bb8dca2b26ce0c5522bd7eb0f31
sandeshsk12 <sksandeshkandagal@gmail.com>
2025-09-27 00:32:21 +0530

    compiled files checked. Generating seed next

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/lending/supply/plasma/platforms/aave_v3_plasma_base_supply.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    schema = 'aave_v3_plasma',
+    alias = 'base_supply',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['transaction_type', 'token_address', 'tx_hash', 'evt_index'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+  )
+}}
+
+{{
+  lending_aave_v3_compatible_supply(
+    blockchain = 'plasma',
+    project = 'aave',
+    version = '3',
+    decoded_contract_name='poolinstance',
+    wrapped_token_gateway_available=false
+  )
+}}
+
