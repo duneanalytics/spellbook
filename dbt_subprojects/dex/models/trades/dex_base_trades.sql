9d153112c923db0ed28e9ffedb3c1662b211b342
beve0x <gk0001va@gmail.com>
2025-09-29 23:58:03 -0400

    test dex trades plasma

--- dbt_subprojects/dex/models/trades/dex_base_trades.sql
+++ dbt_subprojects/dex/models/trades/dex_base_trades.sql
@@ -31,6 +31,7 @@
     , ref('dex_nova_base_trades')
     , ref('dex_opbnb_base_trades')
     , ref('dex_optimism_base_trades')
+    , ref('dex_plasma_base_trades')
     , ref('dex_plume_base_trades')
     , ref('dex_polygon_base_trades')
     , ref('dex_ronin_base_trades')
