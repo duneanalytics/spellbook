4e35ad613506350347ee08efa62f2b1934ac0b8f
Boxer <Floba.hs@gmail.com>
2025-09-30 20:38:36 +0200

    commit

--- dbt_subprojects/tokens/models/transfers_and_balances/tron/tokens_tron_base_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/tron/tokens_tron_base_transfers.sql
@@ -10,7 +10,7 @@
 )
 }}
 
--- this is only including TRC20 (ERC20) transfers
+-- TRC20 (ERC20) transfers
 SELECT
     {{dbt_utils.generate_surrogate_key(['t.evt_block_number', 'tx.index', 't.evt_index'])}} as unique_key
     , 'tron' as blockchain
@@ -34,9 +34,40 @@ INNER JOIN {{ source('tron','transactions') }} tx ON
     tx.block_time = t.evt_block_time -- raw base tables partitioned by time
     AND tx.block_number = t.evt_block_number
     AND tx.hash = t.evt_tx_hash
+    
     {% if is_incremental() %}
     AND {{incremental_predicate('tx.block_time')}}
     {% endif %}
 {% if is_incremental() %}
 WHERE {{incremental_predicate('t.evt_block_time')}}
 {% endif %}
+WHERE tx.block_time >= now() - interval '2' day
+
+UNION ALL
+
+-- Native TRX transfers
+SELECT
+    {{dbt_utils.generate_surrogate_key(['tx.block_number', 'tx.index'])}} as unique_key
+    , 'tron' as blockchain
+    , cast(date_trunc('month', tx.block_time) as date) AS block_month
+    , cast(date_trunc('day', tx.block_time) as date) AS block_date
+    , tx.block_time AS block_time
+    , tx.block_number AS block_number
+    , tx.hash AS tx_hash
+    , CAST(NULL AS BIGINT) AS evt_index
+    , CAST(NULL AS ARRAY<BIGINT>) AS trace_address
+    , (SELECT token_address FROM {{source('dune','blockchains')}} WHERE name = 'tron') AS contract_address
+    , 'native' AS token_standard
+    , tx."from"
+    , tx.to
+    , tx."index" AS tx_index
+    , tx."from" as tx_from
+    , tx.to as tx_to
+    , tx.value AS amount_raw
+FROM {{ source('tron','transactions') }} tx
+WHERE tx.success = true
+    AND tx.value > UINT256 '0'
+    AND tx.block_time >= now() - interval '2' day
+    {% if is_incremental() %}
+    AND {{incremental_predicate('tx.block_time')}}
+    {% endif %}
