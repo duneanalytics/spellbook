95d2a1c37acacbb8307a271a223906f865d2c90f
beve0x <gk0001va@gmail.com>
2025-09-30 10:00:07 -0400

    peaq update

--- dbt_subprojects/dex/models/trades/peaq/platforms/machinex_v3_peaq_base_trades.sql
+++ dbt_subprojects/dex/models/trades/peaq/platforms/machinex_v3_peaq_base_trades.sql
@@ -12,7 +12,22 @@
 
 {% set machinex_start_date = "2025-05-25" %}
 
-WITH dexs AS (
+WITH pool_tokens AS (
+    SELECT 
+        i.contract_address as pool,
+        c0.output_0 as token0,
+        c1.output_0 as token1
+    FROM {{ source('machinex_peaq', 'machinexv3pool_evt_initialize') }} i
+    LEFT JOIN {{ source('machinex_peaq', 'machinexv3pool_call_token0') }} c0
+        ON i.contract_address = c0.contract_address 
+        AND c0.call_success = true
+    LEFT JOIN {{ source('machinex_peaq', 'machinexv3pool_call_token1') }} c1
+        ON i.contract_address = c1.contract_address 
+        AND c1.call_success = true
+    GROUP BY 1, 2, 3
+),
+
+dexs AS (
     SELECT
         t.evt_block_number AS block_number,
         t.evt_block_time AS block_time,
@@ -20,16 +35,16 @@ WITH dexs AS (
         CAST(NULL AS VARBINARY) AS maker,
         CASE WHEN amount0 < INT256 '0' THEN abs(amount0) ELSE abs(amount1) END AS token_bought_amount_raw,
         CASE WHEN amount0 < INT256 '0' THEN abs(amount1) ELSE abs(amount0) END AS token_sold_amount_raw,
-        CASE WHEN amount0 < INT256 '0' THEN f.tokena ELSE f.tokenb END AS token_bought_address,
-        CASE WHEN amount0 < INT256 '0' THEN f.tokenb ELSE f.tokena END AS token_sold_address,
+        CASE WHEN amount0 < INT256 '0' THEN p.token0 ELSE p.token1 END AS token_bought_address,
+        CASE WHEN amount0 < INT256 '0' THEN p.token1 ELSE p.token0 END AS token_sold_address,
         t.contract_address AS project_contract_address,
         t.evt_tx_hash AS tx_hash,
         t.evt_index
     FROM
         {{ source('machinex_peaq', 'machinexv3pool_evt_swap') }} t
     INNER JOIN
-        {{ source('machinex_peaq', 'machinexv3factory_call_createpool') }} f
-        ON f.output_pool = t.contract_address
+        pool_tokens p
+        ON p.pool = t.contract_address
     {% if is_incremental() %}
     WHERE
         {{ incremental_predicate('t.evt_block_time') }}
