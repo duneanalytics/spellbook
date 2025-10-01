83996f57ec564a261ba0132b6244e526f41e6a72
beve0x <gk0001va@gmail.com>
2025-09-30 20:23:51 -0400

    added tests

--- dbt_subprojects/dex/models/trades/somnia/platforms/somnex_v2_somnia_base_trades.sql
+++ dbt_subprojects/dex/models/trades/somnia/platforms/somnex_v2_somnia_base_trades.sql
@@ -18,26 +18,37 @@ WITH dexs AS (
         t.evt_block_time AS block_time,
         t.to AS taker,
         t.contract_address AS maker,
-        -- Universal fix for Somnia DEX event amount inflation issue
-        -- amount1 fields are systematically inflated by 1e10, amount0 fields are correct
-        CASE WHEN amount0Out = UINT256 '0' THEN amount1Out / 1e10 ELSE amount0Out END AS token_bought_amount_raw,
-        CASE WHEN amount0In = UINT256 '0' OR amount1Out = UINT256 '0' THEN amount1In / 1e10 ELSE amount0In END AS token_sold_amount_raw,
+        -- amount1 fields are inflated by 1e10 ONLY for 18-decimal tokenA pairs
+        -- For 6-decimal tokenA (like USDC), amount1 is already correct
+        CASE 
+            WHEN amount0Out = UINT256 '0' AND COALESCE(tokena_decimals.decimals, 18) = 18 
+                THEN amount1Out / 1e10
+            WHEN amount0Out = UINT256 '0' 
+                THEN amount1Out
+            ELSE amount0Out 
+        END AS token_bought_amount_raw,
+        CASE 
+            WHEN (amount0In = UINT256 '0' OR amount1Out = UINT256 '0') AND COALESCE(tokena_decimals.decimals, 18) = 18 
+                THEN amount1In / 1e10
+            WHEN amount0In = UINT256 '0' OR amount1Out = UINT256 '0' 
+                THEN amount1In
+            ELSE amount0In 
+        END AS token_sold_amount_raw,
         CASE WHEN amount0Out = UINT256 '0' THEN f.tokenb ELSE f.tokena END AS token_bought_address,
         CASE WHEN amount0In = UINT256 '0' OR amount1Out = UINT256 '0' THEN f.tokenb ELSE f.tokena END AS token_sold_address,
         t.contract_address AS project_contract_address,
         t.evt_tx_hash AS tx_hash,
         t.evt_index AS evt_index
-    FROM
-        {{ source('somnex_somnia', 'somnexammpair_evt_swap') }} t
-    INNER JOIN
-        {{ source('somnex_somnia', 'somnexammfactory_call_createpair') }} f
+    FROM {{ source('somnex_somnia', 'somnexammpair_evt_swap') }} t
+    INNER JOIN {{ source('somnex_somnia', 'somnexammfactory_call_createpair') }} f
         ON f.output_pair = t.contract_address
+    LEFT JOIN {{ source('tokens', 'erc20') }} tokena_decimals
+        ON tokena_decimals.contract_address = f.tokena 
+        AND tokena_decimals.blockchain = 'somnia'
     {% if is_incremental() %}
-    WHERE
-        {{ incremental_predicate('t.evt_block_time') }}
+    WHERE {{ incremental_predicate('t.evt_block_time') }}
     {% else %}
-    WHERE
-        t.evt_block_time >= TIMESTAMP '{{somnex_start_date}}'
+    WHERE t.evt_block_time >= TIMESTAMP '{{somnex_start_date}}'
     {% endif %}
 )
 
