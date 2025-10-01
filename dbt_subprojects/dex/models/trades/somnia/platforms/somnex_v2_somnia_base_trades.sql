decd54805025fd7f4f9544c9e6e6200d53367a28
beve0x <gk0001va@gmail.com>
2025-09-30 20:26:13 -0400

    prod exclude somnex

--- dbt_subprojects/dex/models/trades/somnia/platforms/somnex_v2_somnia_base_trades.sql
+++ dbt_subprojects/dex/models/trades/somnia/platforms/somnex_v2_somnia_base_trades.sql
@@ -6,7 +6,8 @@
         file_format = 'delta',
         incremental_strategy = 'merge',
         unique_key = ['tx_hash', 'evt_index'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        tags = ['prod_exclude']
     )
 }}
 
