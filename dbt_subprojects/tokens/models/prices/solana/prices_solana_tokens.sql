5715381a183e629f3a0d497fbe1caa0f1aad70f1
Jacob Sharples <45126111+JacobSharples@users.noreply.github.com>
2025-10-01 16:00:23 -0400

    Add USD1 to Solana price table

--- dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
+++ dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
@@ -778,5 +778,6 @@ FROM
         ('aura-auraonsol', 'solana', 'AURA', 'DtR4D9FtVoTX2569gaL837ZgrB6wNjj6tkmnX9Rdk9B2', 6),
         ('wct-walletconnect-token', 'solana', 'WCT', 'WCTk5xWdn5SYg56twGj32sUF3W4WFQ48ogezLBuYTBY', 9),
         ('holo-holoworld-ai', 'solana', 'HOLO', '69RX85eQoEsnZvXGmLNjYcWgVkp9r2JjahVm99KbJETU', 9),
-        ('yzy-yzy', 'solana', 'YZY', 'DrZ26cKJDksVRWib3DVVsjo9eeXccc7hKhDJviiYEEZY', 6)
+        ('yzy-yzy', 'solana', 'YZY', 'DrZ26cKJDksVRWib3DVVsjo9eeXccc7hKhDJviiYEEZY', 6),
+        ('usd1-usd1', 'solana', 'USD1', 'USD1ttGY1N17NEEHLmELoaybftRBUSErhqYiQzvEmuB', 6)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
