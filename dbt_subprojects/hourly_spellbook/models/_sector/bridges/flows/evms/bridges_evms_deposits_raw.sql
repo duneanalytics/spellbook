c57428dc2370c3af2ee311da0e90ed47d5f48eb8
Hildebert Moulié <h.moulie@gmail.com>
2025-10-03 01:42:56 +0200

    move duplicate_index to deposits_raw file

--- dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/evms/bridges_evms_deposits_raw.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/evms/bridges_evms_deposits_raw.sql
@@ -4,7 +4,7 @@
     , materialized = 'incremental'
     , file_format = 'delta'
     , incremental_strategy='merge'
-    , unique_key = ['deposit_chain','withdrawal_chain','bridge_name','bridge_version','bridge_transfer_id']
+    , unique_key = ['deposit_chain','withdrawal_chain','bridge_name','bridge_version','bridge_transfer_id', 'duplicate_index']
     , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
 )
 }}
@@ -19,8 +19,9 @@
     , 'unichain'
 ] %}
 
-SELECT *
-FROM (
+WITH raw_deposits AS (
+    SELECT *
+    FROM (
         {% for chain in chains %}
         SELECT deposit_chain
         , withdrawal_chain
@@ -48,3 +49,39 @@ FROM (
         {% endif %}
         {% endfor %} 
         )
+    )
+
+{% if is_incremental() %}
+WITH check_dupes AS (
+    SELECT deposit_chain
+    , withdrawal_chain
+    , bridge_name
+    , bridge_version
+    , bridge_transfer_id
+    , MAX(duplicate_index) AS duplicate_index
+    FROM raw_deposits rd
+    INNER JOIN {{ this }} t USING (deposit_chain, withdrawal_chain, bridge_name, bridge_version, bridge_transfer_id)
+    WHERE {{ incremental_predicate('rd.block_time') }}
+    GROUP BY 1, 2, 3, 4, 5
+    )
+{% endif %}
+
+    SELECT deposit_chain
+, withdrawal_chain
+, bridge_name
+, bridge_version
+, bridge_transfer_id
+{% if is_incremental() %}
+, COALESCE(cd.duplicate_index, 0) + ROW_NUMBER() OVER (PARTITION BY d.deposit_chain, d.withdrawal_chain, d.bridge_name, d.bridge_version, d.bridge_transfer_id ORDER BY d.block_number, d.evt_index ) AS duplicate_index
+{% else %}
+, ROW_NUMBER() OVER (PARTITION BY d.deposit_chain, d.withdrawal_chain, d.bridge_name, d.bridge_version, d.bridge_transfer_id ORDER BY d.block_number, d.evt_index ) AS duplicate_index
+{% endif %}
+, ROW_NUMBER() OVER (PARTITION BY d.deposit_chain, d.withdrawal_chain, d.bridge_name, d.bridge_version, d.bridge_transfer_id ORDER BY d.block_number, d.evt_index) AS duplicate_index
+FROM raw_deposits rd
+{% if is_incremental() %}
+INNER JOIN check_dupes cd ON rd.deposit_chain = cd.deposit_chain
+    AND rd.withdrawal_chain = cd.withdrawal_chain
+    AND rd.bridge_name = cd.bridge_name
+    AND rd.bridge_version = cd.bridge_version
+    AND rd.bridge_transfer_id = cd.bridge_transfer_id
+{% endif %}
