b342d7fa4a97273a7dfa38c74440c6f2cb60ef38
= <bram@cow.fi>
2025-10-07 08:50:32 +0200

    Add CoW Protocol solvers

--- dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/base/cow_protocol_base_solvers.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/base/cow_protocol_base_solvers.sql
@@ -87,7 +87,11 @@ known_solver_metadata (address, environment, name) as (
                 (0xB222Da076C21B7784A975dD54Fd09C0f7c21262f, 'barn', 'BRRRolver'),
                 (0x4dd1be0Cd607E5382Dd2844fA61D3a17e3e83D56, 'prod', 'Rizzolver'),
                 (0x100bc2a159BFacaD3e7D4D8ED26Cc3B7983bade9, 'barn', 'Bebop'),
-                (0xe1B77774A2b69D74F5e93760ddC604Fabe641272, 'prod', 'Bebop')
+                (0xe1B77774A2b69D74F5e93760ddC604Fabe641272, 'prod', 'Bebop'),
+                (0xb3890d27ea75b4c3b074b09c14a32a76cd73678e, 'barn', 'MXTrading'),
+                (0xfe1f652015930724cd8d927ed1666468b2f07718, 'barn', 'Kamaji'),
+                (0xfd12ebfbab23d5f364c15f1d2962f3b19105671a, 'prod', 'Kamaji'),
+                (0x3980daa7eaad0b7e0c53cfc5c2760037270da54d, 'prod', 'Tsolver')
     ) as _
 )
 -- Combining the metadata with current activation status for final table
