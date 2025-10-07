76ab27a30f56f141c99c78a8763f2f8a54ef1f9c
Hildebert Moulié <h.moulie@gmail.com>
2025-10-07 20:12:18 +0200

    rm distinct and add plasma to across

--- dbt_subprojects/hourly_spellbook/macros/sector/bridges/across_v2_withdrawals.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/bridges/across_v2_withdrawals.sql
@@ -24,7 +24,7 @@ WITH ranked AS (
     LEFT JOIN {{ ref('bridges_across_chain_indexes') }} m ON d.originChainId=m.id
     )
 
-SELECT DISTINCT deposit_chain
+SELECT deposit_chain
 , withdrawal_chain
 , bridge_name
 , bridge_version
