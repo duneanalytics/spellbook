84ac5871a4a7b5f9c44afb74cff13d83d1613fe9
beve0x <gk0001va@gmail.com>
2025-10-09 14:01:13 -0400

    removed timestamp for block_time

--- dbt_subprojects/daily_spellbook/models/thorchain/silver/thorchain_silver_rune_price.sql
+++ dbt_subprojects/daily_spellbook/models/thorchain/silver/thorchain_silver_rune_price.sql
@@ -4,7 +4,7 @@
     materialized = 'incremental',
     file_format = 'delta',
     incremental_strategy = 'merge',
-    unique_key = ['block_timestamp'],
+    unique_key = ['block_time'],
     partition_by = ['block_month'],
     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
     tags = ['thorchain', 'rune', 'prices']
