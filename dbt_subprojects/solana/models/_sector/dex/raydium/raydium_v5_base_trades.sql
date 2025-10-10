4b82c0451dc600dfb445fffabea908c467ea618e
Boxer <Floba.hs@gmail.com>
2025-10-10 23:25:40 +0200

    wider date range

--- dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v5_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v5_base_trades.sql
@@ -12,7 +12,7 @@
         )
 }}
 
-{% set project_start_date = '2025-10-08' %} --grabbed program deployed at time (account created at).
+{% set project_start_date = '2025-08-21' %} --grabbed program deployed at time (account created at).
 
   WITH
     all_swaps as (
