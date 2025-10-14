85e6f95f1625c70f48b396464ffbf6cbe5d36ef1
max-morrow <amidmoro@gmail.com>
2025-10-14 11:00:59 +0300

    transfers_from_traces to other blockchains

---
+++ dbt_subprojects/tokens/models/transfers_and_balances/tokens_transfers_from_traces.sql
@@ -0,0 +1,48 @@
+{%- set exposed: [
+    "arbitrum",
+    "avalanche_c",
+    "base",
+    "bnb",
+    "ethereum",
+    "fantom",
+    "gnosis",
+    "linea",
+    "optimism",
+    "polygon",
+    "sonic",
+    "unichain",
+    "zksync",
+] -%}
+
+{{-
+    config(
+        schema = 'tokens',
+        alias = 'transfers_from_traces',
+        post_hook = '{{ expose_spells(
+            blockchains = \'exposed\', 
+            spell_type = "sector",
+            spell_name = "tokens",
+            contributors = \'["max-morrow", "grkhr"]\'
+        ) }}'
+    )
+-}}
+
+select
+    blockchain
+    , block_month
+    , block_date
+    , block_time
+    , block_number
+    , tx_hash
+    , trace_address
+    , type
+    , token_standard
+    , contract_address
+    , amount_raw
+    , transfer_from
+    , transfer_to
+    , unique_key
+from({% for blockchain in exposed %}
+    select * from {{ ref('tokens_' + blockchain + '_transfers_from_traces') }}
+    {% if not loop.last %}union all{% endif %}
+{% endfor %})
