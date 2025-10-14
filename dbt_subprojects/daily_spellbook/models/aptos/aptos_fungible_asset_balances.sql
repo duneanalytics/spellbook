f0d6a85bf51a3e35934655484da903fcb7f75da9
Ying Wu <ying-w@users.noreply.github.com>
2025-10-14 14:50:44 -0700

    change_index for deleted fs use ObjectCore instead of virtual

--- dbt_subprojects/daily_spellbook/models/aptos/aptos_fungible_asset_balances.sql
+++ dbt_subprojects/daily_spellbook/models/aptos/aptos_fungible_asset_balances.sql
@@ -102,20 +102,29 @@ WITH coin_balances AS (
     -- Store needs to have 0 balance to be deleted
     -- Ex 2975888978 store 0x1198c59f6c6f84f0a52f1a8524fbaac60946c7e2ac74a87bf7632fa50e23f34d
         SELECT
-            tx_version,
-            tx_hash,
-            block_date,
-            block_time,
+            ev.tx_version,
+            ev.tx_hash,
+            ev.block_date,
+            ev.block_time,
             --
+            mr.write_set_change_index,  -- use ObjectCore change index
             '0x' || LPAD(LTRIM(json_extract_scalar(data, '$.metadata'), '0x'), 64, '0') AS asset_type,
-            '0x' || LPAD(LTRIM(json_extract_scalar(data, '$.store'), '0x'), 64, '0') AS storage_id,
+            address_32_from_hex(json_extract_scalar(data, '$.store')) AS storage_id,
             '0x' || LPAD(LTRIM(json_extract_scalar(data, '$.owner'), '0x'), 64, '0') AS owner_address,
-            TRUE AS move_is_deletion,
+            TRUE AS move_is_deletion, -- object isn't deleted but store is
             CAST(0 AS UINT256) AS amount
-        FROM {{ source('aptos', 'events') }}
+        FROM {{ source('aptos', 'events') }} ev
+        LEFT JOIN {{ source('aptos', 'move_resources') }} mr
+            ON mr.tx_version = ev.tx_version
+            AND mr.move_address = address_32_from_hex(json_extract_scalar(ev.data, '$.store'))
+            AND move_module_address = 0x0000000000000000000000000000000000000000000000000000000000000001
+            AND move_resource_module = 'object'
+            AND move_resource_name = 'ObjectCore'
+            AND mr.block_date = ev.block_date -- optimization
         WHERE 1=1
-        AND event_type = '0x1::fungible_asset::FungibleStoreDeletion'
-        AND block_date >= DATE('2025-04-28') -- date enabled
+        AND ev.event_type = '0x1::fungible_asset::FungibleStoreDeletion'
+        AND ev.block_date >= DATE('2025-04-28') -- date enabled
+        AND mr.block_date >= DATE('2025-04-28') -- date enabled
     {% if is_incremental() or true %}
         AND {{ incremental_predicate('block_time') }}
     {% endif %}
@@ -168,10 +177,10 @@ SELECT
     block_time,
     date(date_trunc('month', block_time)) as block_month,
     --
-    -1 AS write_set_change_index, -- virtual
+    write_set_change_index, -- uses object core
     asset_type,
     from_hex(owner_address) AS owner_address,
-    from_hex(storage_id) AS storage_id,
+    storage_id,
     amount,
     NULL AS is_frozen,
     'v2' AS token_standard
