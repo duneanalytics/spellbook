a48d9983dcbfde5be5aa38184e3e867b1d7b7737
beve0x <gk0001va@gmail.com>
2025-10-14 16:25:10 -0400

    update, added more files

---
+++ dbt_subprojects/daily_spellbook/models/thorchain/gold/thorchain_defi_fact_bond_actions.sql
@@ -0,0 +1,81 @@
+{{ config(
+    schema = 'thorchain_defi',
+    alias = 'fact_bond_actions',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['fact_bond_actions_id'],
+    partition_by = ['block_month'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    tags = ['thorchain', 'defi', 'bond_actions', 'fact']
+) }}
+
+WITH block_prices AS (
+    SELECT
+        AVG(rune_usd) AS rune_usd,
+        block_id
+    FROM {{ ref('thorchain_silver_prices') }}
+    GROUP BY block_id
+),
+
+bond_events AS (
+    SELECT
+        block_timestamp,
+        block_time,
+        block_month,
+        tx_id,
+        from_address,
+        to_address,
+        asset,
+        blockchain,
+        bond_type,
+        asset_e8,
+        e8,
+        memo,
+        event_id,
+        _tx_type,
+        _inserted_timestamp
+    FROM {{ ref('thorchain_silver_bond_events') }}
+    {% if is_incremental() %}
+        WHERE {{ incremental_predicate('block_time') }}
+    {% endif %}
+)
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'be.tx_id',
+        'be.from_address',
+        'be.to_address',
+        'be.asset_e8',
+        'be.bond_type',
+        'be.e8',
+        'be.block_timestamp',
+        'be.blockchain',
+        'be.asset',
+        'be.memo'
+    ]) }} AS fact_bond_actions_id,
+    b.block_time,
+    b.block_timestamp,  -- Include for compatibility
+    b.block_month,
+    COALESCE(b.dim_block_id, '-1') AS dim_block_id,
+    be.tx_id,
+    be.from_address,
+    be.to_address,
+    be.asset,
+    be.blockchain,
+    be.bond_type,
+    COALESCE(be.e8 / power(10, 8), 0) AS asset_amount,
+    COALESCE(p.rune_usd * be.asset_e8, 0) AS asset_usd,
+    be.memo,
+    be.event_id,
+    be._tx_type,
+    be._inserted_timestamp,
+    cast('{{ invocation_id }}' as varchar) AS _audit_run_id,
+    current_timestamp AS inserted_timestamp,
+    current_timestamp AS modified_timestamp
+FROM bond_events be
+JOIN {{ ref('thorchain_core_dim_block') }} b
+    ON be.block_timestamp = b.timestamp
+LEFT JOIN block_prices p
+    ON b.block_id = p.block_id
+
---
+++ dbt_subprojects/daily_spellbook/models/thorchain/silver/thorchain_silver_upgrades.sql
@@ -0,0 +1,73 @@
+{{ config(
+    schema = 'thorchain_silver',
+    alias = 'upgrades',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['_unique_key'],
+    partition_by = ['block_month'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    tags = ['thorchain', 'upgrades', 'silver']
+) }}
+
+-- Switch events enriched with pricing for token upgrades
+WITH block_prices AS (
+    SELECT
+        AVG(rune_usd) AS rune_usd,
+        block_id
+    FROM {{ ref('thorchain_silver_prices') }}
+    GROUP BY block_id
+),
+
+switch_events AS (
+    SELECT
+        tx AS tx_id,
+        from_addr AS from_address,
+        to_addr AS to_address,
+        burn_asset,
+        burn_e8,
+        mint_e8,
+        event_id,
+        block_timestamp,
+        _updated_at,
+        ROW_NUMBER() OVER (
+            PARTITION BY tx, from_addr, to_addr, burn_asset, burn_e8, mint_e8, block_timestamp
+            ORDER BY _updated_at DESC
+        ) AS rn
+    FROM {{ source('thorchain', 'switch_events') }}
+    WHERE cast(from_unixtime(cast(block_timestamp / 1e9 as bigint)) as timestamp) >= current_date - interval '14' day
+)
+
+SELECT
+    bl.height AS block_id,
+    cast(from_unixtime(cast(se.block_timestamp / 1e9 as bigint)) as timestamp) AS block_time,
+    date(from_unixtime(cast(se.block_timestamp / 1e9 as bigint))) AS block_date,
+    date_trunc('month', from_unixtime(cast(se.block_timestamp / 1e9 as bigint))) AS block_month,
+    se.tx_id,
+    se.from_address,
+    se.to_address,
+    se.burn_asset,
+    se.burn_e8 / power(10, 8) AS rune_amount,
+    (se.burn_e8 / power(10, 8)) * COALESCE(p.rune_usd, 0) AS rune_amount_usd,
+    se.mint_e8 / power(10, 8) AS mint_amount,
+    (se.mint_e8 / power(10, 8)) * COALESCE(p.rune_usd, 0) AS mint_amount_usd,
+    concat_ws(
+        '-',
+        se.tx_id,
+        cast(se.block_timestamp as varchar),
+        se.from_address,
+        se.to_address,
+        se.burn_asset
+    ) AS _unique_key,
+    current_timestamp AS _inserted_timestamp
+FROM switch_events se
+JOIN {{ source('thorchain', 'block_log') }} bl
+    ON se.block_timestamp = bl.timestamp
+LEFT JOIN block_prices p
+    ON bl.height = p.block_id
+WHERE se.rn = 1
+  AND cast(from_unixtime(cast(se.block_timestamp / 1e9 as bigint)) as timestamp) >= current_date - interval '14' day
+{% if is_incremental() %}
+  AND {{ incremental_predicate('cast(from_unixtime(cast(se.block_timestamp / 1e9 as bigint)) as timestamp)') }}
+{% endif %}
+
