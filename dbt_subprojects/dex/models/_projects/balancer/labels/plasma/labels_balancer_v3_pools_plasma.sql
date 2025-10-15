c2797e4c066665e344215239fc162398d870afa1
João Bruno <joaobrunoah@gmail.com>
2025-10-15 13:11:19 -0300

    Add Reclamm factory

--- dbt_subprojects/dex/models/_projects/balancer/labels/plasma/labels_balancer_v3_pools_plasma.sql
+++ dbt_subprojects/dex/models/_projects/balancer/labels/plasma/labels_balancer_v3_pools_plasma.sql
@@ -50,6 +50,19 @@ WITH token_data AS (
         t.tokens,
         0 AS weights,
         cc.symbol,
+        'reclamm' AS pool_type
+      FROM token_data c
+      INNER JOIN {{ source('balancer_v3_plasma', 'ReclammPoolFactory_call_create') }} cc
+        ON c.pool = cc.output_pool
+      CROSS JOIN UNNEST(c.tokens) AS t(tokens)
+
+      UNION ALL
+
+      SELECT
+        c.pool AS pool_id,
+        t.tokens,
+        0 AS weights,
+        cc.symbol,
         'stable' AS pool_type
       FROM token_data c
       INNER JOIN {{ source('balancer_v3_plasma', 'StablePoolFactory_call_create') }} cc
