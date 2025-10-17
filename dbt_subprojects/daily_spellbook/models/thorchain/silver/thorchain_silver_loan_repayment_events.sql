e8db2eaba5d313d824b4126b8f03ba987f119904
Krish <113653936+krishgka@users.noreply.github.com>
2025-10-17 19:28:35 -0400

    CUR2-743 thorchain tables (#8856)
    * thorchain phase 1 commit
    
    * changed ref to tokens prices
    
    * prices.usd update
    
    * adjustments
    
    * fix
    
    * added partitioning
    
    * update fix
    
    * last 7 days run
    
    * adjusted last 7 days
    
    * adjusted prices to last 7d
    
    * updated for all joins
    
    * update
    
    * added block_month partitioning
    
    * reverted unique keys
    
    * fixed unique key
    
    * fixes
    
    * remove previously held columns from schema
    
    * removed duplicate variables
    
    * changed txhash to txid
    
    * removed extra incremental
    
    * update fixes
    
    * sorting spellbook structure
    
    * removed timestamp for block_time
    
    * removed duplicate joins
    
    * schema fix
    
    * added Pool & TVL tables
    
    * changed to lowercase
    
    * fixes for pools and tvl
    
    * changes update
    
    * fixes
    
    * fix
    
    * update on pools
    
    * removed meta
    
    * balances fixes to downstream
    
    * updates prices to include block_id
    
    * fixes again
    
    * updated config
    
    * block_time/date consistency
    
    * minor fix
    
    * downstream changes made
    
    * fix
    
    * changed date to be from 15 days ago
    
    * pool block update
    
    * fixed pool block stat naming
    
    * update the stats
    
    * test runs with 2 new lineages
    
    * added 60 models
    
    * update, added more files
    
    * fixed error
    
    * updated changes
    
    * changed mapping for slash amounts/events
    
    * scheduled_outbound_events fix
    
    * update rune prices
    
    * removed tx_id null tests from schema
    
    * removed last incre. run
    
    * rerun with more data
    
    * still no data, changing lookback time
    
    * Remove gold folder - will be added in separate thorchain_gold PR
    
    * Update schema.yml to only include silver models
    
    * add back core dim block
    
    * refined the swaps model
    
    * updated the joins
    
    * removed snowflake func
    
    * fixed array
    
    * rearranged arrays on trino
    
    * added element_at func
    
    * removed comments, ready
    
    * review changes
    
    * fixed schema
    
    * fixed pool block fees
    
    * doing checks again
    
    * updated unique key
    
    * made fixes
    
    * fixed stats
    
    * change
    
    * fixed unique key in block stats
    
    * using dedup method
    
    * corrected positioning in the window cte
    
    * found root cause of duplicates
    
    * added block_month to surrogate key
    
    * changed position of lag func
    
    * rearranged CTEs
    
    * simplified
    
    * removed window func
    
    * removed lag func
    
    * fixed stats
    
    * fixed the table
    
    * added incremental
    
    * removed last 17 day run
    
    * fixed tables historical
    
    * fixed schema
    
    * incremental fixes- historical
    
    * fixes

---
+++ dbt_subprojects/daily_spellbook/models/thorchain/silver/thorchain_silver_loan_repayment_events.sql
@@ -0,0 +1,52 @@
+{{ config(
+    schema = 'thorchain_silver',
+    alias = 'loan_repayment_events',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_month', 'event_id'],
+    partition_by = ['block_month'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    tags = ['thorchain', 'loan_repayment_events', 'silver']
+) }}
+
+WITH deduplicated AS (
+    SELECT
+        owner,
+        collateral_asset,
+        event_id,
+        block_timestamp,
+        collateral_withdrawn,
+        debt_repaid,
+        tx_id,
+        _updated_at,
+        ROW_NUMBER() OVER (
+            PARTITION BY event_id
+            ORDER BY _updated_at DESC
+        ) AS rn
+    FROM {{ source('thorchain', 'loan_repayment_events') }}
+    {% if is_incremental() %}
+    WHERE {{ incremental_predicate('cast(from_unixtime(cast(block_timestamp / 1e9 as bigint)) as timestamp)') }}
+    {% endif %}
+),
+
+base AS (
+    SELECT
+        owner,
+        collateral_asset,
+        event_id,
+        collateral_withdrawn,
+        debt_repaid,
+        tx_id,
+        block_timestamp,
+        cast(from_unixtime(cast(block_timestamp / 1e9 as bigint)) as timestamp) AS block_time,
+        DATE(cast(from_unixtime(cast(block_timestamp / 1e9 as bigint)) as timestamp)) AS block_date,
+        date_trunc('month', cast(from_unixtime(cast(block_timestamp / 1e9 as bigint)) as timestamp)) AS block_month,
+        date_trunc('hour', cast(from_unixtime(cast(block_timestamp / 1e9 as bigint)) as timestamp)) AS block_hour,
+        current_timestamp AS _inserted_timestamp
+    FROM deduplicated
+    WHERE rn = 1
+)
+
+SELECT * FROM base
+
