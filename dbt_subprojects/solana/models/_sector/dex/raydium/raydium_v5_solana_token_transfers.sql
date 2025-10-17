1f5cff7c7ce13f4534377608faddd8c2fc00ceda
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2025-10-17 14:29:06 -0400

    raydium v4/5: init staging transfers models for performance optimization (#8884)
    * init staging transfers for raydium for performance
    
    * force short run time
    
    * try to add inner instruction to staging transfers join
    
    * fix col name
    
    * explicitly list cols
    
    * Revert "explicitly list cols"
    
    This reverts commit 011cf3de5372b8bbcdb37b19ed23803ab1a1aba3.
    
    * Revert "fix col name"
    
    This reverts commit 51115ebfc5de807f4665b0aacc3501cffb9a426d.
    
    * Revert "try to add inner instruction to staging transfers join"
    
    This reverts commit 6c65e210dd27da3b5a3c97a14194abb73169e21d.
    
    * grab distinct after unioning out/in source
    
    * Revert "force short run time"
    
    This reverts commit 290aa4f4725dad78fa695a6eaa94ea09c978cee5.

---
+++ dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v5_solana_token_transfers.sql
@@ -0,0 +1,73 @@
+{{
+  config(
+    schema = 'raydium_v5_solana'
+    , alias = 'token_transfers'
+    , partition_by = ['block_date']
+    , materialized = 'incremental'
+    , file_format = 'delta'
+    , incremental_strategy = 'merge'
+    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    , unique_key = ['block_date', 'unique_instruction_key']
+  )
+}}
+
+{% set project_start_date = '2024-05-16' %}
+
+-- Base swaps from raydium v5
+with all_raydium_swaps as (
+	select distinct
+		date_trunc('day', call_block_time) as block_date
+		, call_block_slot as block_slot
+		, call_tx_index as tx_index
+		, call_outer_instruction_index as outer_instruction_index
+	from
+		{{ source('raydium_cp_solana', 'raydium_cp_swap_call_swapBaseOutput') }}
+	where
+		1=1
+		{% if is_incremental() -%}
+		and {{incremental_predicate('call_block_time')}}
+		{% else -%}
+		and call_block_date >= date '{{project_start_date}}'
+		{% endif -%}
+	union all
+	select distinct
+		date_trunc('day', call_block_time) as block_date
+		, call_block_slot as block_slot
+		, call_tx_index as tx_index
+		, call_outer_instruction_index as outer_instruction_index
+	from
+		{{ source('raydium_cp_solana', 'raydium_cp_swap_call_swapBaseInput') }}
+	where
+		1=1
+		{% if is_incremental() -%}
+		and {{incremental_predicate('call_block_time')}}
+		{% else -%}
+		and call_block_date >= date '{{project_start_date}}'
+		{% endif -%}
+)
+, raydium_swaps as (
+	select
+		distinct *
+	from
+		all_raydium_swaps
+)
+, token_transfers as (
+	select
+		*
+	from
+		{{ source('tokens_solana','transfers') }}
+	where
+		1=1
+		and token_version != 'native'
+		{% if is_incremental() %}
+		and {{ incremental_predicate('block_time') }}
+		{% else %}
+		and block_time >= timestamp '{{ project_start_date }}'
+		{% endif %}
+)
+
+select
+	*
+from
+	token_transfers
+inner join raydium_swaps using (block_date, block_slot, tx_index, outer_instruction_index)
