d2ac272e24723cbb56a755fbcdeb09a8a609411e
jeff-dude <jeff@dune.xyz>
2025-10-22 15:50:28 -0400

    refactor to prematerialize decoded swaps

--- dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v4_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v4_base_trades.sql
@@ -39,28 +39,8 @@ all_swaps as (
         , trs_1.token_mint_address as token_sold_mint_address
         , trs_2.from_token_account as token_bought_vault
         , trs_1.to_token_account as token_sold_vault
-    FROM (
-        SELECT account_amm, call_is_inner, call_outer_instruction_index, call_inner_instruction_index, call_tx_id, call_block_time, call_block_slot, call_block_date, call_outer_executing_account, call_tx_signer, call_tx_index, account_poolCoinTokenAccount, account_poolPcTokenAccount, account_uerSourceTokenAccount, account_uerDestinationTokenAccount
-        FROM {{ source('raydium_amm_solana', 'raydium_amm_call_swapBaseOut') }}
-        WHERE
-            1=1
-            {% if is_incremental() -%}
-            AND {{incremental_predicate('call_block_time')}}
-            {% else -%}
-            AND call_block_date >= DATE '{{project_start_date}}'
-            {% endif -%}
-        UNION ALL
-        SELECT account_amm, call_is_inner, call_outer_instruction_index, call_inner_instruction_index, call_tx_id, call_block_time, call_block_slot, call_block_date, call_outer_executing_account, call_tx_signer, call_tx_index, account_poolCoinTokenAccount, account_poolPcTokenAccount, account_uerSourceTokenAccount, account_uerDestinationTokenAccount
-        FROM {{ source('raydium_amm_solana', 'raydium_amm_call_swapBaseIn') }}
-        WHERE
-            1=1
-            {% if is_incremental() -%}
-            AND {{incremental_predicate('call_block_time')}}
-            {% else -%}
-            AND call_block_date >= DATE '{{project_start_date}}'
-            {% endif -%}
-    ) sp
-    INNER JOIN {{ ref('raydium_v4_solana_token_transfers') }} trs_1
+    FROM {{ ref('raydium_v4_solana_stg_decoded_swaps') }} sp
+    INNER JOIN {{ source('tokens_solana','transfers') }} trs_1
         ON trs_1.tx_id = sp.call_tx_id
         AND trs_1.block_date = sp.call_block_date
         AND trs_1.block_slot = sp.call_block_slot
@@ -68,12 +48,13 @@ all_swaps as (
         AND ((sp.call_is_inner = false AND trs_1.inner_instruction_index = 1)
             OR (sp.call_is_inner = true AND trs_1.inner_instruction_index = sp.call_inner_instruction_index + 1))
         AND trs_1.from_token_account = sp.account_uerSourceTokenAccount
+        AND trs_1.token_version != 'native'
         {% if is_incremental() %}
         AND {{incremental_predicate('trs_1.block_time')}}
         {% else %}
         AND trs_1.block_date >= DATE '{{project_start_date}}'
         {% endif %}
-    INNER JOIN {{ ref('raydium_v4_solana_token_transfers') }} trs_2
+    INNER JOIN {{ source('tokens_solana','transfers') }} trs_2
         ON trs_2.tx_id = sp.call_tx_id
         AND trs_2.block_date = sp.call_block_date
         AND trs_2.block_slot = sp.call_block_slot
@@ -86,6 +67,13 @@ all_swaps as (
         {% else %}
         AND trs_2.block_date >= DATE '{{project_start_date}}'
         {% endif %}
+	where
+		1=1
+		{% if is_incremental() -%}
+		and {{incremental_predicate('sp.call_block_time')}}
+		{% else -%}
+		and sp.call_block_date >= date '{{project_start_date}}'
+		{% endif -%}
 )
 
 SELECT
@@ -111,5 +99,5 @@ SELECT
     , tb.outer_instruction_index
     , tb.inner_instruction_index
     , tb.tx_index
-    , {{ dbt_utils.generate_surrogate_key(['tb.tx_id', 'tb.tx_index', 'tb.outer_instruction_index', 'tb.inner_instruction_index']) }} as surrogate_key
+    , {{ dbt_utils.generate_surrogate_key(['tb.block_slot', 'tb.tx_id', 'tb.tx_index', 'tb.outer_instruction_index', 'tb.inner_instruction_index']) }} as surrogate_key
 FROM all_swaps tb
--- dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v5_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/raydium/raydium_v5_base_trades.sql
@@ -13,8 +13,7 @@
 
 {% set project_start_date = '2024-05-16' %} --grabbed program deployed at time (account created at).
 
-WITH
-all_swaps as (
+WITH all_swaps as (
     SELECT
         sp.call_block_time as block_time
         , sp.call_block_slot as block_slot
@@ -38,28 +37,8 @@ all_swaps as (
         , sp.account_inputTokenMint as token_sold_mint_address
         , sp.account_outputVault as token_bought_vault
         , sp.account_inputVault as token_sold_vault
-    FROM (
-        SELECT account_poolState, call_is_inner, call_outer_instruction_index, call_inner_instruction_index, call_tx_id, call_block_time, call_block_slot, call_block_date, call_outer_executing_account, call_tx_signer, call_tx_index, account_inputTokenMint, account_outputTokenMint, account_inputVault, account_outputVault
-        FROM {{ source('raydium_cp_solana', 'raydium_cp_swap_call_swapBaseOutput') }}
-        WHERE
-            1=1
-            {% if is_incremental() -%}
-            AND {{incremental_predicate('call_block_time')}}
-            {% else -%}
-            AND call_block_date >= DATE '{{project_start_date}}'
-            {% endif -%}
-        UNION ALL
-        SELECT account_poolState, call_is_inner, call_outer_instruction_index, call_inner_instruction_index, call_tx_id, call_block_time, call_block_slot, call_block_date, call_outer_executing_account, call_tx_signer, call_tx_index, account_inputTokenMint, account_outputTokenMint, account_inputVault, account_outputVault
-        FROM {{ source('raydium_cp_solana', 'raydium_cp_swap_call_swapBaseInput') }}
-        WHERE
-            1=1
-            {% if is_incremental() -%}
-            AND {{incremental_predicate('call_block_time')}}
-            {% else -%}
-            AND call_block_date >= DATE '{{project_start_date}}'
-            {% endif -%}
-    ) sp
-    INNER JOIN {{ ref('raydium_v5_solana_token_transfers') }} trs_1
+    FROM {{ ref('raydium_v5_solana_stg_decoded_swaps') }} sp
+    INNER JOIN {{ source('tokens_solana','transfers') }} trs_1
         ON trs_1.tx_id = sp.call_tx_id
         AND trs_1.block_date = sp.call_block_date
         AND trs_1.block_slot = sp.call_block_slot
@@ -68,12 +47,13 @@ all_swaps as (
             OR (sp.call_is_inner = true AND trs_1.inner_instruction_index = sp.call_inner_instruction_index + 1))
         AND trs_1.token_mint_address = sp.account_inputTokenMint
         AND trs_1.to_token_account = sp.account_inputVault
+        AND trs_1.token_version != 'native'
         {% if is_incremental() -%}
         AND {{incremental_predicate('trs_1.block_time')}}
         {% else -%}
         AND trs_1.block_date >= DATE '{{project_start_date}}'
         {% endif -%}
-    INNER JOIN {{ ref('raydium_v5_solana_token_transfers') }} trs_2
+    INNER JOIN {{ source('tokens_solana','transfers') }} trs_2
         ON trs_2.tx_id = sp.call_tx_id
         AND trs_2.block_date = sp.call_block_date
         AND trs_2.block_slot = sp.call_block_slot
@@ -82,11 +62,19 @@ all_swaps as (
             OR (sp.call_is_inner = true AND trs_2.inner_instruction_index = sp.call_inner_instruction_index + 2))
         AND trs_2.token_mint_address = sp.account_outputTokenMint
         AND trs_2.from_token_account = sp.account_outputVault
+        AND trs_2.token_version != 'native'
         {% if is_incremental() -%}
         AND {{incremental_predicate('trs_2.block_time')}}
         {% else -%}
         AND trs_2.block_date >= DATE '{{project_start_date}}'
         {% endif -%}
+    where
+		1=1
+		{% if is_incremental() -%}
+		and {{incremental_predicate('sp.call_block_time')}}
+		{% else -%}
+		and sp.call_block_date >= date '{{project_start_date}}'
+		{% endif -%}
 )
 
 SELECT
@@ -112,5 +100,5 @@ SELECT
     , tb.outer_instruction_index
     , tb.inner_instruction_index
     , tb.tx_index
-    , {{ dbt_utils.generate_surrogate_key(['tb.tx_id', 'tb.tx_index', 'tb.outer_instruction_index', 'tb.inner_instruction_index']) }} as surrogate_key
+    , {{ dbt_utils.generate_surrogate_key(['tb.block_slot', 'tb.tx_id', 'tb.tx_index', 'tb.outer_instruction_index', 'tb.inner_instruction_index']) }} as surrogate_key
 FROM all_swaps tb
