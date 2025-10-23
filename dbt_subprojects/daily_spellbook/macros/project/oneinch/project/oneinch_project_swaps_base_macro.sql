0ac27ca84181e1e1b9dee80f1963717a689e5175
max-morrow <amidmoro@gmail.com>
2025-10-23 14:28:51 +0300

    split sides

--- dbt_subprojects/daily_spellbook/macros/project/oneinch/project/oneinch_project_swaps_base_macro.sql
+++ dbt_subprojects/daily_spellbook/macros/project/oneinch/project/oneinch_project_swaps_base_macro.sql
@@ -1,8 +1,8 @@
-{%- macro 
-    oneinch_project_swaps_macro(
+{%- macro
+    oneinch_project_swaps_base_macro(
         blockchain
         , date_from = '2025-10-01'
-    ) 
+    )
 -%}
 
 {%- set native_addresses = '(0x0000000000000000000000000000000000000000, 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee)' -%}
@@ -68,6 +68,19 @@ meta as (
 
 , calls as (
     select
+        *
+        , array_agg(call_trace_address) over(partition by block_month, block_number, tx_hash, project) as call_trace_addresses
+    from {{ ref('oneinch_' + blockchain + '_project_calls') }}
+    where true
+        and call_success
+        and (tx_success or tx_success is null)
+        and (flags['cross_chain'] or not flags['cross_chain_method']) -- without cross-chain methods calls in non cross-chain protocols
+        and block_time >= timestamp '{{ date_from }}'
+        {% if is_incremental() %}and {{ incremental_predicate('block_time') }}{% endif %}
+)
+
+, swaps as (
+    select
         blockchain
         , block_month
         , block_number
@@ -94,18 +107,7 @@ meta as (
         , if(maker_asset in {{native_addresses}}, wrapped_native_token_address, maker_asset) as _maker_asset
         , if(taker_asset in {{native_addresses}}, wrapped_native_token_address, taker_asset) as _taker_asset
         , coalesce(order_hash, concat(tx_hash, to_utf8(array_join(call_trace_address, ',')))) as call_trade_id -- without call_trade for the correctness of the max transfer approach
-    from (
-        select
-            *
-            , array_agg(call_trace_address) over(partition by block_month, block_number, tx_hash, project) as call_trace_addresses
-        from {{ ref('oneinch_' + blockchain + '_project_calls') }}
-        where true
-            and call_success
-            and (tx_success or tx_success is null)
-            and (flags['cross_chain'] or not flags['cross_chain_method']) -- without cross-chain methods calls in non cross-chain protocols
-            and block_time >= timestamp '{{ date_from }}'
-            {% if is_incremental() %}and {{ incremental_predicate('block_time') }}{% endif %}
-    )
+    from calls
     left join orders using(block_month, block_number, tx_hash, call_trace_address), meta
     where
         reduce(call_trace_addresses, true, (r, x) -> if(r and x <> call_trace_address and slice(call_trace_address, 1, cardinality(x)) = x, false, r), r -> r) -- only not nested calls of the project in tx
@@ -183,7 +185,70 @@ meta as (
         {% if is_incremental() %}and {{ incremental_predicate('block_time') }}{% endif %}
 )
 
-, swaps as (
+, joined as (
+    select
+        blockchain
+        , calls.block_month
+        , calls.block_number
+        , calls.tx_hash
+        , calls.call_trace_address
+        , calls.call_trade_id
+        , any_value(block_time) as block_time
+        , any_value(tx_from) as tx_from
+        , any_value(tx_to) as tx_to
+        , any_value(project) as project
+        , any_value(tag) as tag
+        , any_value(flags) as flags
+        , any_value(call_from) as call_from
+        , any_value(call_to) as call_to
+        , any_value(call_selector) as call_selector
+        , any_value(method) as method
+        , any_value(order_hash) as order_hash
+        , any_value(maker) as maker
+        , any_value(maker_asset) as maker_asset
+        , any_value(making_amount) as making_amount
+        , any_value(taker_asset) as taker_asset
+        , any_value(taking_amount) as taking_amount
+        , any_value(order_flags) as order_flags
+        , array_agg(distinct
+            cast(row(if(native, native_symbol, symbol), contract_address_raw)
+                as row(symbol varchar, contract_address_raw varbinary))
+        ) as tokens
+        , array_agg(distinct
+            cast(row(if(native, native_symbol, symbol), contract_address_raw)
+                as row(symbol varchar, contract_address_raw varbinary))
+        ) filter(where creations_from.block_number is null or creations_to.block_number is null) as user_tokens
+        , array_agg(distinct
+            cast(row(if(native, native_symbol, symbol), contract_address_raw)
+                as row(symbol varchar, contract_address_raw varbinary))
+        ) filter(where transfer_from = call_from or transfer_to = call_from) as caller_tokens
+        , max(amount * price / pow(10, decimals)) as call_amount_usd
+        , max(amount * price / pow(10, decimals)) filter(where trusted) as call_amount_usd_trusted
+        , max(amount * price / pow(10, decimals)) filter(where creations_from.block_number is null or creations_to.block_number is null) as user_amount_usd
+        , max(amount * price / pow(10, decimals)) filter(where (creations_from.block_number is null or creations_to.block_number is null) and trusted) as user_amount_usd_trusted
+        , max(amount * price / pow(10, decimals)) filter(where transfer_from = call_from or transfer_to = call_from) as caller_amount_usd
+        , max(amount * price / pow(10, decimals)) filter(where (transfer_from = call_from or transfer_to = call_from) and trusted) as caller_amount_usd_trusted
+        , max(amount * price / pow(10, decimals)) filter(where transfer_from = call_to or transfer_to = call_to) as contract_amount_usd
+        , max(amount * price / pow(10, decimals)) filter(where (transfer_from = call_to or transfer_to = call_to) and trusted) as contract_amount_usd_trusted
+        , array_agg(distinct transfer_from) filter(where creations_from.block_number is null) as senders
+        , array_agg(distinct transfer_to) filter(where creations_to.block_number is null) as receivers
+    from swaps
+    join transfers on true
+        and calls.block_month = transfers.block_month
+        and calls.block_number = transfers.block_number
+        and calls.tx_hash = transfers.tx_hash
+        and slice(transfer_trace_address, 1, cardinality(call_trace_address)) = call_trace_address -- nested transfers only
+        and reduce(array_distinct(call_trace_addresses), call_trace_address, (r, x) -> if(slice(transfer_trace_address, 1, cardinality(x)) = x and x > r, x, r), r -> r) = call_trace_address -- transfers related to the call only
+        and (order_hash is null or contract_address in (_maker_asset, _taker_asset) and cardinality(array_intersect(array[call_from, maker, taker], array[transfer_from, transfer_to])) > 0) -- transfers related to the order only
+    left join prices using(contract_address, minute)
+    left join tokens using(contract_address)
+    left join trusted_tokens using(contract_address)
+    left join creations as creations_from on creations_from.address = transfers.transfer_from
+    left join creations as creations_to on creations_to.address = transfers.transfer_to
+    group by 1, 2, 3, 4, 5, 6
+)
+
+, processing as (
     select
         *
         , array_union(senders, receivers) as users
@@ -219,73 +284,12 @@ meta as (
         , coalesce(element_at(order_flags, 'cross_chain'), false) -- 1inch cross-chain
             or coalesce(element_at(flags, 'cross_chain'), false) and not coalesce(element_at(flags, 'multi'), false) -- any suitable swap method call of exclusively cross-chain protocol
             or coalesce(element_at(flags, 'cross_chain'), false) and coalesce(element_at(flags, 'cross_chain_method'), false) -- calls of exclusively cross-chain methods of any cross-chain protocol
-        as cross_chain_swap
+        as cross_chain
         , not flags['user']
             or position('RFQ' in method) > 0
             or coalesce(element_at(order_flags, 'partial') and not element_at(order_flags, 'multiple'), false)
         as contracts_only
-    from (
-        select
-            blockchain
-            , calls.block_month
-            , calls.block_number
-            , calls.tx_hash
-            , calls.call_trace_address
-            , calls.call_trade_id
-            , any_value(block_time) as block_time
-            , any_value(tx_from) as tx_from
-            , any_value(tx_to) as tx_to
-            , any_value(project) as project
-            , any_value(tag) as tag
-            , any_value(flags) as flags
-            , any_value(call_from) as call_from
-            , any_value(call_to) as call_to
-            , any_value(call_selector) as call_selector
-            , any_value(method) as method
-            , any_value(order_hash) as order_hash
-            , any_value(maker) as maker
-            , any_value(maker_asset) as maker_asset
-            , any_value(making_amount) as making_amount
-            , any_value(taker_asset) as taker_asset
-            , any_value(taking_amount) as taking_amount
-            , any_value(order_flags) as order_flags
-            , array_agg(distinct
-                cast(row(if(native, native_symbol, symbol), contract_address_raw)
-                  as row(symbol varchar, contract_address_raw varbinary))
-            ) as tokens
-            , array_agg(distinct
-                cast(row(if(native, native_symbol, symbol), contract_address_raw)
-                  as row(symbol varchar, contract_address_raw varbinary))
-            ) filter(where creations_from.block_number is null or creations_to.block_number is null) as user_tokens
-            , array_agg(distinct
-                cast(row(if(native, native_symbol, symbol), contract_address_raw)
-                  as row(symbol varchar, contract_address_raw varbinary))
-            ) filter(where transfer_from = call_from or transfer_to = call_from) as caller_tokens
-            , max(amount * price / pow(10, decimals)) as call_amount_usd
-            , max(amount * price / pow(10, decimals)) filter(where trusted) as call_amount_usd_trusted
-            , max(amount * price / pow(10, decimals)) filter(where creations_from.block_number is null or creations_to.block_number is null) as user_amount_usd
-            , max(amount * price / pow(10, decimals)) filter(where (creations_from.block_number is null or creations_to.block_number is null) and trusted) as user_amount_usd_trusted
-            , max(amount * price / pow(10, decimals)) filter(where transfer_from = call_from or transfer_to = call_from) as caller_amount_usd
-            , max(amount * price / pow(10, decimals)) filter(where (transfer_from = call_from or transfer_to = call_from) and trusted) as caller_amount_usd_trusted
-            , max(amount * price / pow(10, decimals)) filter(where transfer_from = call_to or transfer_to = call_to) as contract_amount_usd
-            , max(amount * price / pow(10, decimals)) filter(where (transfer_from = call_to or transfer_to = call_to) and trusted) as contract_amount_usd_trusted
-            , array_agg(distinct transfer_from) filter(where creations_from.block_number is null) as senders
-            , array_agg(distinct transfer_to) filter(where creations_to.block_number is null) as receivers
-        from calls
-        join transfers on true
-            and calls.block_month = transfers.block_month
-            and calls.block_number = transfers.block_number
-            and calls.tx_hash = transfers.tx_hash
-            and slice(transfer_trace_address, 1, cardinality(call_trace_address)) = call_trace_address -- nested transfers only
-            and reduce(array_distinct(call_trace_addresses), call_trace_address, (r, x) -> if(slice(transfer_trace_address, 1, cardinality(x)) = x and x > r, x, r), r -> r) = call_trace_address -- transfers related to the call only
-            and (order_hash is null or contract_address in (_maker_asset, _taker_asset) and cardinality(array_intersect(array[call_from, maker, taker], array[transfer_from, transfer_to])) > 0) -- transfers related to the order only
-        left join prices using(contract_address, minute)
-        left join tokens using(contract_address)
-        left join trusted_tokens using(contract_address)
-        left join creations as creations_from on creations_from.address = transfers.transfer_from
-        left join creations as creations_to on creations_to.address = transfers.transfer_to
-        group by 1, 2, 3, 4, 5, 6
-    )
+    from joined
 )
 
 , sides as (
@@ -296,7 +300,7 @@ meta as (
                 entry
                 and order_hash is null
                 and not auction
-                and not cross_chain_swap
+                and not cross_chain
                 and not contracts_only
                 or second_side
             )
@@ -305,16 +309,16 @@ meta as (
                 and order_hash is null
                 and not auction
                 and not contracts_only
-                and not cross_chain_swap
+                and not cross_chain
             )
             , ('intra-chain: intents: auction',
                 auction
-                and not cross_chain_swap
+                and not cross_chain
             )
             , ('intra-chain: intents: user limit order',
                 order_hash is not null
                 and not auction
-                and not cross_chain_swap
+                and not cross_chain
                 and not contracts_only
                 and not second_side
             )
@@ -322,7 +326,7 @@ meta as (
                 contracts_only
                 and not second_side
             )
-            , ('cross-chain', cross_chain_swap)
+            , ('cross-chain', cross_chain)
         ]) as modes
     from (
         select
@@ -330,23 +334,7 @@ meta as (
             , coalesce(maker, tx_from) as user
             , false as second_side
             , flags['direct'] or reduce(tx_swaps, false, (r, x) -> x.call_trace_address <> call_trace_address and slice(call_trace_address, 1, cardinality(x.call_trace_address)) = x.call_trace_address or r, r -> not r) as entry
-        from swaps
-        
-        union all
-        
-        select
-            *
-            , tx_from as user
-            , true as second_side
-            , false as entry
-        from swaps
-        where
-            true
-            and flags['direct']
-            and order_hash is not null -- intent
-            and maker is not null
-            and not auction
-            and not cross_chain_swap
+        from processing
     )
 )
 
@@ -363,10 +351,8 @@ select
     , project
     , tag
     , map_concat(flags, map_from_entries(array[
-        ('intent', order_hash is not null and not second_side)
-        , ('auction', auction and not cross_chain_swap)
-        , ('cross_chain_swap', cross_chain_swap)
-        , ('entry', entry)
+          ('auction', auction and not cross_chain)
+        , ('cross_chain', cross_chain)
     ])) as flags
     , call_selector
     , method
