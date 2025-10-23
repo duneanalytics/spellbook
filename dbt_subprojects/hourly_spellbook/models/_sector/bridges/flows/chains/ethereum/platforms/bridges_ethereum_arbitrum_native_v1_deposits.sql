827a0d287f80eeaefa445561a0a3796d2bcfc7d3
hildobby <h.moulie@gmail.com>
2025-10-23 16:02:29 +0200

    add arbitrum native bridge on ethereum

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/ethereum/platforms/bridges_ethereum_arbitrum_native_v1_deposits.sql
@@ -0,0 +1,27 @@
+{% set blockchain = 'ethereum' %}
+
+{{ config(
+    schema = 'bridges_' + blockchain,
+    alias = 'arbitrum_native_v1_deposits',
+    materialized = 'view',
+    )
+}}
+
+SELECT 'ethereum' AS deposit_chain
+, NULL AS withdrawal_chain
+, 'Arbitrum' AS bridge_name
+, '1' AS bridge_version
+, evt_block_date AS block_date
+, evt_block_time AS block_time
+, evt_block_number AS block_number
+, _amount AS deposit_amount_raw
+, _from AS sender
+, _to AS recipient
+, 'erc20' AS deposit_token_standard
+, l1Token AS deposit_token_address
+, evt_tx_from AS tx_from
+, evt_tx_hash AS  tx_hash
+, evt_index
+, contract_address
+, _sequenceNumber AS bridge_id
+FROM {{ source('arbitrum_ethereum', 'l1erc20gateway_evt_depositinitiated') }}
