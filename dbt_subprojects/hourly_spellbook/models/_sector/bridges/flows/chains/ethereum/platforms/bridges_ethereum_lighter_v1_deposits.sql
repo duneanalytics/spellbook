7d2f558d017978eb6c1b3f9fd3b19d407533bcb6
hildobby <h.moulie@gmail.com>
2025-10-24 14:53:10 +0200

    bridges: add lighter bridge (ethereum side)

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/ethereum/platforms/bridges_ethereum_lighter_v1_deposits.sql
@@ -0,0 +1,28 @@
+{% set blockchain = 'ethereum' %}
+
+{{ config(
+    schema = 'bridges_' + blockchain,
+    alias = 'lighter_v1_deposits',
+    materialized = 'view',
+    )
+}}
+
+SELECT '{{blockchain}}' AS deposit_chain
+, d.dstChainId AS withdrawal_chain_id
+, i.blockchain AS withdrawal_chain
+, 'Lighter' AS bridge_name
+, '1' AS bridge_version
+, evt_block_date AS block_date
+, evt_block_time AS block_time
+, evt_block_number AS block_number
+, amount AS deposit_amount_raw
+, evt_tx_from AS sender
+, toAddress AS recipient
+, 'erc20' AS deposit_token_standard
+, 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 AS deposit_token_address
+, evt_tx_from AS tx_from
+, evt_tx_hash AS tx_hash
+, evt_index
+, contract_address
+, {{ dbt_utils.generate_surrogate_key(['evt_tx_hash', 'evt_index']) }} as bridge_transfer_id
+FROM {{ source('lighter_v2_ethereum', 'zklighter_evt_deposit') }} d
