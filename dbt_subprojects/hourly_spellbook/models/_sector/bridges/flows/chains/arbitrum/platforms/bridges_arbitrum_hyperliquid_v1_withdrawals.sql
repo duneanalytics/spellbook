5aedf3f43f10bd2431d8e9d58b13d321f6d35658
hildobby <h.moulie@gmail.com>
2025-10-24 15:38:58 +0200

    bridges: add hyperliquid v1 and v2 (arbitrum side)

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/arbitrum/platforms/bridges_arbitrum_hyperliquid_v1_withdrawals.sql
@@ -0,0 +1,28 @@
+{% set blockchain = 'arbitrum' %}
+
+{{ config(
+    schema = 'bridges_' + blockchain,
+    alias = 'hyperliquid_v1_withdrawals',
+    materialized = 'view',
+    )
+}}
+
+SELECT CAST(NULL AS DOUBLE) AS deposit_chain_id
+, 'hyperliquid' AS deposit_chain
+, '{{blockchain}}' AS withdrawal_chain
+, 'Hyperliquid' AS bridge_name
+, '1' AS bridge_version
+, evt_block_date AS block_date
+, evt_block_time AS block_time
+, evt_block_number AS block_number
+, json_extract_scalar(e, '$.usdc') AS withdrawal_amount_raw
+, json_extract_scalar(e, '$.user') AS sender
+, json_extract_scalar(e, '$.user') AS recipient
+, 'erc20' AS withdrawal_token_standard
+, 0xff970a61a04b1ca14834a43f5de4533ebddb5cc8 AS withdrawal_token_address
+, evt_tx_from AS tx_from
+, evt_tx_hash AS tx_hash
+, evt_index
+, contract_address
+, {{ dbt_utils.generate_surrogate_key(['evt_tx_hash', 'evt_index']) }} as bridge_transfer_id
+FROM {{ source('hyperliquid_arbitrum', 'hyperliquid_bridge_evt_withdraw') }} d
---
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/arbitrum/platforms/bridges_arbitrum_hyperliquid_v2_deposits.sql
@@ -0,0 +1,30 @@
+{% set blockchain = 'arbitrum' %}
+
+{{ config(
+    schema = 'bridges_' + blockchain,
+    alias = 'hyperliquid_v2_deposits',
+    materialized = 'view',
+    )
+}}
+
+SELECT 'arbitrum' AS deposit_chain
+, CAST(NULL AS DOUBLE) AS withdrawal_chain_id
+, 'hyperliquid' AS withdrawal_chain
+, 'Hyperliquid' AS bridge_name
+, '2' AS bridge_version
+, block_date AS block_date
+, block_time AS block_time
+, block_number AS block_number
+, amount AS deposit_amount_raw
+, tx_from AS sender
+, tx_from AS recipient
+, 'erc20' AS deposit_token_standard
+, 0xaf88d065e77c8cc2239327c5edb3a432268e5831 AS deposit_token_address
+, tx_from AS tx_from
+, tx_hash AS tx_hash
+, evt_index
+, contract_address
+, {{ dbt_utils.generate_surrogate_key(['tx_hash', 'evt_index']) }} as bridge_transfer_id
+FROM {{ source('tokens_arbitrum', 'transfers') }}
+WHERE to = 0x2df1c51e09aecf9cacb7bc98cb1742757f163df7
+AND contract_address = 0xaf88d065e77c8cc2239327c5edb3a432268e5831
