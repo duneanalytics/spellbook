41ae82211689d78096b6490f62eae7d987ad4f70
max-morrow <amidmoro@gmail.com>
2025-10-24 09:27:03 +0300

    cosmetic

--- dbt_subprojects/daily_spellbook/models/_projects/oneinch/bnb/project/oneinch_bnb_project_swaps.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/oneinch/bnb/project/oneinch_bnb_project_swaps.sql
@@ -5,7 +5,7 @@
         schema = 'oneinch_' + blockchain,
         alias = 'project_swaps',
         materialized = 'view',
-        unique_key = ['blockchain', 'block_month', 'block_number', 'tx_hash', 'second_side', 'call_trace_address', 'call_trade_id']
+        unique_key = ['blockchain', 'block_month', 'block_number', 'tx_hash', 'second_side', 'call_trace_address', 'call_trade_id'],
     )
 -}}
 
