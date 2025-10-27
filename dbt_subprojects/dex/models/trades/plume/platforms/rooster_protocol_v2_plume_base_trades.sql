dc7f9c75604556bea5a835c733b6ad9d220c14ae
thecaptncrunch <nia_whitaker@protonmail.com>
2025-10-27 14:08:49 -0400

    add izumi finance and rooster protocol v2 to Plume dexes

---
+++ dbt_subprojects/dex/models/trades/plume/platforms/rooster_protocol_v2_plume_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'rooster_protocol_plume',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v3_trades(
+        blockchain = 'plume',
+        project = 'rooster_protocol',
+        version = '3',
+        Pair_evt_Swap = source('rooster_protocol_plume', 'algebrapool_evt_swap'),
+        Factory_evt_PoolCreated = source('rooster_protocol_plume', 'algebrafactory_evt_pool')
+    )
+}}
