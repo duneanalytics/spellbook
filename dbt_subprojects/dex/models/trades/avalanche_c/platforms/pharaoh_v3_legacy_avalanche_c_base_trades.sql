06b9e926f6990f2ace2b09270f3221613828e744
disco chuck <uv@househeads.org>
2025-10-28 08:26:33 -0500

    Add files via upload

---
+++ dbt_subprojects/dex/models/trades/avalanche_c/platforms/pharaoh_v3_legacy_avalanche_c_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'pharaoh_v3_avalanche_c',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v2_trades(
+        blockchain = 'avalanche_c',
+        project = 'pharaoh_v3',
+        version = 'legacy',
+        Pair_evt_Swap = source('pharaoh_v3_avalanche_c', 'Pair_evt_Swap'),
+        Factory_evt_PairCreated = source('pharaoh_v3_avalanche_c', 'PairFactory_evt_PairCreated')
+    )
+}}
