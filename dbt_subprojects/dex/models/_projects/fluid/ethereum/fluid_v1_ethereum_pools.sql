3eaf3f7f89b3b37f085bd0365c852f53b75bb0c9
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-10-29 18:47:15 +0100

    hide spells

--- dbt_subprojects/dex/models/_projects/fluid/ethereum/fluid_v1_ethereum_pools.sql
+++ dbt_subprojects/dex/models/_projects/fluid/ethereum/fluid_v1_ethereum_pools.sql
@@ -6,7 +6,8 @@
         file_format = 'delta',
         incremental_strategy = 'merge',
         unique_key = ['dex'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        post_hook='{{ hide_spells() }}'
     )
 }}
 
