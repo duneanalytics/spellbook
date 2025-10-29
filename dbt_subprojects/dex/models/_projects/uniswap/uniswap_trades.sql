c4f36ee383e14318839d061797b121a39411f1af
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-10-29 01:54:04 +0100

    fix

--- dbt_subprojects/dex/models/_projects/uniswap/uniswap_trades.sql
+++ dbt_subprojects/dex/models/_projects/uniswap/uniswap_trades.sql
@@ -297,7 +297,7 @@ prices AS (
         , price as price_usd 
     from
     {{ source('prices','usd_with_native') }}
-    where block_date >= date '2025-08-01'
+    where minute >= date '2025-08-01'
     {% if is_incremental() %}
     and {{ incremental_predicate('minute') }}
     {% endif %}
