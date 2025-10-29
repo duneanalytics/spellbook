aad2990e4278e6ed9673592a3c55b88c9c2ee171
Henrystats <65398020+henrystats@users.noreply.github.com>
2025-10-29 17:10:02 +0100

    test celo

---
+++ dbt_subprojects/dex/models/_projects/uniswap/celo/uniswap_celo_trades.sql
@@ -0,0 +1,17 @@
+{{ config(
+    schema = 'uniswap_celo'
+    , alias = 'trades'
+    , partition_by = ['block_month', 'blockchain', 'project']
+    , materialized = 'incremental'
+    , file_format = 'delta'
+    , incremental_strategy = 'merge'
+    , unique_key = ['blockchain', 'project', 'version', 'tx_hash', 'evt_index']
+    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_downstream_trades(
+          blockchain = 'celo'
+    )
+}}
