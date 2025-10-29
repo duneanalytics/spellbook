fd4b814b15ec3d21bf027bde2011b425ec4f0d38
max-morrow <amidmoro@gmail.com>
2025-10-29 21:51:55 +0300

    tft expansion

---
+++ dbt_subprojects/tokens/models/transfers_and_balances/tac/tokens_tac_transfers_from_traces.sql
@@ -0,0 +1,11 @@
+{% set blockchain = 'tac' %}
+
+{{
+    config(
+        schema = 'tokens_' ~ blockchain,
+        alias = 'transfers_from_traces',
+        materialized = 'view',
+    )
+}}
+
+{{ transfers_from_traces_macro(blockchain=blockchain) }}
