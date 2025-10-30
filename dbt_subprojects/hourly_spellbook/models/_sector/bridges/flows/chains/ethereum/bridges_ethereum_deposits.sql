5e58fbb0ffe58ec52faf779efaacc017d44ead1f
hildobby <h.moulie@gmail.com>
2025-10-30 13:37:02 +0100

    bridges: across missing models

--- dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/ethereum/bridges_ethereum_deposits.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/ethereum/bridges_ethereum_deposits.sql
@@ -10,6 +10,7 @@
 {% set bridges_platforms = [
     'bridges_' + blockchain + '_cctp_v1_deposits'
     , 'bridges_' + blockchain + '_celer_v1_deposits'
+    , 'bridges_' + blockchain + '_across_v1_deposits'
     , 'bridges_' + blockchain + '_across_v2_deposits'
     , 'bridges_' + blockchain + '_across_v3_deposits'
     , 'bridges_' + blockchain + '_arbitrum_native_v1_deposits'
