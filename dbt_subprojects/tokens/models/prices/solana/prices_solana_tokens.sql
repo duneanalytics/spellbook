e28ea86b32dc54f8b39d960f869da73ed7c5fae3
thetroyharris <127905654+thetroyharris@users.noreply.github.com>
2025-11-03 15:11:22 -0500

    Update prices_solana_tokens.sql
    Add LBTC and PST

--- dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
+++ dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
@@ -783,5 +783,7 @@ FROM
         ('useless-useless-coin', 'solana', 'USELESS', 'Dz9mQ9NzkBcCsuGPFJ3r1bS4wgqKMHBPiVuniW8Mbonk', 6),
         ('onyc-onyc', 'solana', 'ONyc', '5Y8NV33Vv7WbnLfq3zBcKSdYPrk7g2KoiQoe7M2tcxp5', 9),
         ('yzy-yzy', 'solana', 'YZY', 'DrZ26cKJDksVRWib3DVVsjo9eeXccc7hKhDJviiYEEZY', 6),
-        ('xbtc-okx-wrapped-btc','solana','xBTC','CtzPWv73Sn1dMGVU3ZtLv9yWSyUAanBni19YWDaznnkn',8)
+        ('xbtc-okx-wrapped-btc','solana','xBTC','CtzPWv73Sn1dMGVU3ZtLv9yWSyUAanBni19YWDaznnkn',8),
+        ('lbtc-lombard-staked-btc','solana','LBTC','LBTCgU4b3wsFKsPwBn1rRZDx5DoFutM6RPiEt1TPDsY',8),
+        ('pstusdc-payfi-strategy-token-usdc','solana','PST','59obFNBzyTBGowrkif5uK7ojS58vsuWz3ZCvg6tfZAGw',6)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
