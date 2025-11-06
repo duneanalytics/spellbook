30f0be52538646779579f9e4e00a67b879e6b718
max-morrow <amidmoro@gmail.com>
2025-11-06 15:13:18 +0300

    fix

--- dbt_subprojects/dex/models/_projects/oneinch/_meta/oneinch_blockchains.sql
+++ dbt_subprojects/dex/models/_projects/oneinch/_meta/oneinch_blockchains.sql
@@ -12,12 +12,12 @@
 {% for blockchain in oneinch_blockchains_cfg_macro() %}
     select
         '{{ blockchain.name }}' as blockchain
-        , {{ blockchain.get(chain_id, 'null') }} as chain_id
+        , {{ blockchain.get('chain_id', 'null') }} as chain_id
         , date('{{ blockchain.start }}') as first_deployed_at
-        , {{ blockchain.get(native_token_symbol, 'null') }} as native_token_symbol
-        , {{ blockchain.get(wrapped_native_token_address, 'cast(null as varbinary)') }} as wrapped_native_token_address
-        , {{ blockchain.get(explorer_link, 'null') }} as explorer_link
-        , array[{{ blockchain.get(fusion_settlement_addresses, []) | join(', ') }}] as fusion_settlement_addresses
-        , array[{{ blockchain.get(escrow_factory_addresses, []) | join(', ') }}] as escrow_factory_addresses
+        , {{ blockchain.get('native_token_symbol', 'null') }} as native_token_symbol
+        , {{ blockchain.get('wrapped_native_token_address', 'cast(null as varbinary)') }} as wrapped_native_token_address
+        , {{ blockchain.get('explorer_link', 'null') }} as explorer_link
+        , array[{{ blockchain.get('fusion_settlement_addresses', []) | join(', ') }}] as fusion_settlement_addresses
+        , array[{{ blockchain.get('escrow_factory_addresses', []) | join(', ') }}] as escrow_factory_addresses
     {% if not loop.last %}union{% endif %}
 {% endfor %}
