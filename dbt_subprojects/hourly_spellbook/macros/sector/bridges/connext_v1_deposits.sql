b17435936fa7799d0183e5b5ab841251f91409ce
hildobby <h.moulie@gmail.com>
2025-11-06 18:37:09 +0100

    fix

--- dbt_subprojects/hourly_spellbook/macros/sector/bridges/connext_v1_deposits.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/bridges/connext_v1_deposits.sql
@@ -8,11 +8,11 @@ SELECT '{{blockchain}}' AS deposit_chain
 , evt_block_date AS block_date
 , evt_block_time AS block_time
 , evt_block_number AS block_number
-, json_extract_scalar(params, '$.bridgeAmt') AS deposit_amount_raw
-, json_extract_scalar(params, '$.originSender') AS sender
-, json_extract_scalar(params, '$.to') AS recipient
+, CAST(json_extract_scalar(params, '$.bridgeAmt') AS double) AS deposit_amount_raw
+, from_hex(json_extract_scalar(params, '$.originSender')) AS sender
+, from_hex(json_extract_scalar(params, '$.to')) AS recipient
 , CASE WHEN local=0x0000000000000000000000000000000000000000 THEN 'native' ELSE 'erc20' END AS deposit_token_standard
-, local AS deposit_token_address
+, from_hex(local) AS deposit_token_address
 , evt_tx_from AS tx_from
 , evt_tx_hash AS tx_hash
 , evt_index
