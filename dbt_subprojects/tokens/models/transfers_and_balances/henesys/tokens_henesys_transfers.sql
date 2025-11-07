1dfcb644b5647c65e2e4d037732247223e4e5c4d
tomfutago <tomfutago@gmail.com>
2025-11-07 15:54:37 +0000

    feat: henesys token transfers

---
+++ dbt_subprojects/tokens/models/transfers_and_balances/henesys/tokens_henesys_transfers.sql
@@ -0,0 +1,22 @@
+{{config(
+     schema = 'tokens_henesys',
+     alias = 'transfers',
+     partition_by = ['block_month'],
+     materialized = 'incremental',
+     file_format = 'delta',
+     incremental_strategy = 'merge',
+     unique_key = ['block_date','unique_key'],
+     incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
+     post_hook='{{ expose_spells(blockchains = \'["henesys"]\',
+                                 spell_type = "sector",
+                                 spell_name = "tokens",
+                                 contributors = \'["krishhh"]\') }}'
+  )
+}}
+
+{{ transfers_enrich(
+    base_transfers = ref('tokens_henesys_base_transfers'),
+    transfers_start_date = '2025-03-26',
+    blockchain = 'henesys'
+  ) 
+}}
