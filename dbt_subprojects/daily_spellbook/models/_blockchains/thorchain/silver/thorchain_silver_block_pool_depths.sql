c2ac6c2be33bdfe53e5ce855df86ce9a16749a96
jeff-dude <jeff@dune.xyz>
2025-11-07 14:49:33 -0500

    update silver block pool depths

--- dbt_subprojects/daily_spellbook/models/_blockchains/thorchain/silver/thorchain_silver_block_pool_depths.sql
+++ dbt_subprojects/daily_spellbook/models/_blockchains/thorchain/silver/thorchain_silver_block_pool_depths.sql
@@ -9,18 +9,9 @@ with base as (
         pool AS pool_name,
         asset_e8,
         rune_e8,
-        synth_e8,        
-        cast(from_unixtime(cast(block_timestamp / 1e9 as bigint)) as timestamp) as block_time,
-        date(from_unixtime(cast(block_timestamp / 1e9 as bigint))) as block_date,
-        date_trunc('month', from_unixtime(cast(block_timestamp / 1e9 as bigint))) as block_month,
-        date_trunc('hour', from_unixtime(cast(block_timestamp / 1e9 as bigint))) as block_hour,
-        block_timestamp as raw_block_timestamp,        
-        asset_e8 / 1e8 as asset_amount,
-        rune_e8 / 1e8 as rune_amount,
-        synth_e8 / 1e8 as synth_amount,        
-        _updated_at AS _inserted_timestamp,
-        _ingested_at,
-        _updated_at,        
+        synth_e8,
+        block_timestamp,
+        _ingested_at as _inserted_timestamp,
         ROW_NUMBER() OVER(
             PARTITION BY pool, block_timestamp 
             ORDER BY _ingested_at DESC
@@ -33,14 +24,7 @@ SELECT
     asset_e8,
     rune_e8,
     synth_e8,
-    asset_amount,
-    rune_amount,
-    synth_amount,
-    block_time,
-    block_date,
-    block_month,
-    block_hour,
-    raw_block_timestamp,
+    block_timestamp,
     _inserted_timestamp
 FROM base
 WHERE rn = 1
