6a57eeb18d8f6397db6759ec02832ceeca9693fb
Kofi <kufuorkofi@gmail.com>
2025-11-07 12:56:49 +0000

    feat: Add base app solana trades (#8923)
    * add base app solana trades
    
    * move date
    
    * add previous fee address activity
    
    * correct notes
    
    * correct notes
    
    * update sourceyml
    
    * reduce timeframe for test purposes
    
    * Update dbt_subprojects/solana/models/_sector/dex/base_app/base_app_swapper_solana_fee_payments_raw.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update dbt_subprojects/solana/models/_sector/dex/base_app/base_app_swapper_solana_trades.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update dbt_subprojects/solana/models/_sector/dex/base_app/base_app_swapper_solana_trades.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update dbt_subprojects/solana/models/_sector/dex/base_app/base_app_swapper_solana_trades.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update dbt_subprojects/solana/models/_sector/dex/base_app/base_app_swapper_solana_trades.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update dbt_subprojects/solana/models/_sector/dex/base_app/base_app_swapper_solana_trades.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update dbt_subprojects/solana/models/_sector/dex/base_app/base_app_swapper_solana_trades.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update dbt_subprojects/solana/models/_sector/dex/base_app/schema.yml
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * edited join
    
    * add block month to dex join
    
    * fix alias
    
    * reset dates
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

---
+++ dbt_subprojects/solana/models/_sector/dex/base_app/base_app_swapper_solana_trades.sql
@@ -0,0 +1,104 @@
+{{ config(
+    schema = 'base_app_swapper_solana',
+    alias = 'trades',
+    partition_by = ['block_month'],
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    unique_key = ['block_month', 'block_slot', 'tx_index', 'outer_instruction_index', 'inner_instruction_index']
+   )
+}}
+
+{% set query_start_date = '2024-04-01' %}
+{% set wsol_token = 'So11111111111111111111111111111111111111112' %}
+
+with filtered_transactions as (
+        select 
+            id, 
+            signer,
+            block_date
+        from {{ source('solana', 'transactions') }}
+        where
+            {% if is_incremental() %} 
+                {{ incremental_predicate('block_date') }}
+            {% else %} 
+                block_date >= timestamp '{{query_start_date}}'
+            {% endif %} 
+        and signer != '6ooVBXhnqAXaF91cu49YmWhoFuE6WLdZWTwNYvTuhyBd'
+    ),
+    trades as (
+        select
+            'solana' as blockchain,
+            trades.block_time,
+            cast(date_trunc('day', trades.block_time) as date) as block_date,
+            cast(date_trunc('month', trades.block_time) as date) as block_month,
+            trades.amount_usd,
+            if(token_sold_mint_address = '{{wsol_token}}', 'Buy', 'Sell') as type,
+            token_bought_amount,
+            token_bought_symbol,
+            token_bought_mint_address as token_bought_address,
+            token_sold_amount,
+            token_sold_symbol,
+            token_sold_mint_address as token_sold_address,
+            fee_payments.amount_usd as fee_usd,
+            fee_payments.amount as fee_token_amount,
+            fee_payments.token_symbol as fee_token_symbol,
+            fee_payments.token_address as fee_token_address,
+            project,
+            trades.version,
+            token_pair,
+            project_program_id as project_contract_address,
+            trader_id as user,
+            trades.tx_id,
+            trades.block_slot,
+            tx_index,
+            outer_instruction_index,
+            inner_instruction_index
+        from {{ source('dex_solana', 'trades') }} as trades
+        join
+            {{ ref('base_app_swapper_solana_fee_payments_usd') }} as fee_payments
+            on (
+                trades.tx_id = fee_payments.tx_id
+                and fee_payments.fee_receiver = '6ooVBXhnqAXaF91cu49YmWhoFuE6WLdZWTwNYvTuhyBd'
+                and fee_payments.block_time = trades.block_time
+                and fee_payments.index = 1  -- only get the first fee payment per tx
+                and trades.trader_id != fee_payments.fee_receiver
+                {% if is_incremental() %}
+                and {{ incremental_predicate('fee_payments.block_time') }}
+                {% else %}
+                and fee_payments.block_time >= timestamp '{{query_start_date}}'
+                {% endif %}
+            )
+        join filtered_transactions as tx
+            ON trades.block_date = tx.block_date
+            AND trades.tx_id = tx.id
+        where
+            trades.trader_id != '6ooVBXhnqAXaF91cu49YmWhoFuE6WLdZWTwNYvTuhyBd'
+            {% if is_incremental() %}
+                and {{ incremental_predicate('trades.block_time') }}
+            {% else %}
+                and trades.block_time >= timestamp '{{query_start_date}}'
+            {% endif %}
+    ),
+    highest_inner_instruction_index_for_each_trade as (
+        select
+            tx_id,
+            block_date,
+            outer_instruction_index,
+            max(inner_instruction_index) as highest_inner_instruction_index
+        from trades
+        group by tx_id, block_date, outer_instruction_index
+    )
+select
+    trades.*,
+    if(
+        inner_instruction_index = highest_inner_instruction_index, true, false
+    ) as is_last_trade_in_transaction
+from trades
+join
+    highest_inner_instruction_index_for_each_trade
+    on (
+        trades.block_date = highest_inner_instruction_index_for_each_trade.block_date
+        and trades.tx_id = highest_inner_instruction_index_for_each_trade.tx_id
+        and trades.outer_instruction_index = highest_inner_instruction_index_for_each_trade.outer_instruction_index
+    )
