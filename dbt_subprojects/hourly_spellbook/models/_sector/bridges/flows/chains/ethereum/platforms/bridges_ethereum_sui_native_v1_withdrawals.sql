e775403a5f5335e6a6a467b15240899b141b2eae
hildobby <h.moulie@gmail.com>
2025-11-09 20:36:00 +0100

    bridges: sui (ethereum side)

---
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/ethereum/platforms/bridges_ethereum_sui_native_v1_withdrawals.sql
@@ -0,0 +1,28 @@
+{% set blockchain = 'ethereum' %}
+
+{{ config(
+    schema = 'bridges_' + blockchain,
+    alias = 'sui_native_v1_withdrawals',
+    materialized = 'view',
+    )
+}}
+
+SELECT '{{blockchain}}' AS deposit_chain
+, sourceChainID AS withdrawal_chain_id
+, 'sui' AS withdrawal_chain
+, 'Sui' AS bridge_name
+, '1' AS bridge_version
+, evt_block_date AS block_date
+, evt_block_time AS block_time
+, evt_block_number AS block_number
+, suiAdjustedAmount AS deposit_amount_raw
+, senderAddress AS sender
+, recipientAddress AS recipient
+, 'erc20' AS deposit_token_standard
+, tokenID AS deposit_token_address
+, evt_tx_from AS tx_from
+, evt_tx_hash AS tx_hash
+, evt_index
+, contract_address
+, CAST(nonce AS varchar) as bridge_transfer_id
+FROM {{ source('suibridge_ethereum', 'suibridge_evt_tokensclaimed') }} d
