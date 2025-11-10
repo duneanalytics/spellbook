8d259e56a75d8a0fbf05dc229dc6347b2439c650
= <bram@cow.fi>
2025-11-10 11:29:33 +0100

    [CoW Protocol] Second part of Linea integration

---
+++ dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/linea/cow_protocol_linea_solvers.sql
@@ -0,0 +1,52 @@
+{{ config(
+        schema = 'cow_protocol_linea',
+        alias='solvers',
+        post_hook='{{ expose_spells(blockchains = \'["linea"]\',
+                                    spell_type = "project",
+                                    spell_name = "cow_protocol",
+                                    contributors = \'["harisang"]\') }}'
+)}}
+
+WITH
+-- Aggregate the solver added and removed events into a single table
+-- with true/false for adds/removes respectively
+solver_activation_events as (
+    select solver, evt_block_number, evt_index, True as activated
+    from {{ source('gnosis_protocol_v2_linea', 'GPv2AllowListAuthentication_evt_SolverAdded') }}
+    union
+    select solver, evt_block_number, evt_index, False as activated
+    from {{ source('gnosis_protocol_v2_linea', 'GPv2AllowListAuthentication_evt_SolverRemoved') }}
+),
+-- Sorting by (evt_block_number, evt_index) allows us to pick the most recent activation status of each unique solver
+ranked_solver_events as (
+    select
+        rank() over (partition by solver order by evt_block_number desc, evt_index desc) as rk,
+        solver,
+        evt_block_number,
+        evt_index,
+        activated as active
+    from solver_activation_events
+),
+registered_solvers as (
+    select solver, active
+    from ranked_solver_events
+    where rk = 1
+),
+-- Manually inserting environment and name for each "known" solver
+known_solver_metadata (address, environment, name) as (
+    select *
+    from (
+      VALUES
+                (0xeeD424986A909d3552CB6D108840E16B41DfD6ac, 'barn', 'Baseline'),
+                (0xD72728C165C2e941d41749026c41d414C6679636, 'prod', 'Baseline'),
+                (0x57dc4958b5691D3C6A5b58c71a5074eeC7eE7888, 'barn', 'Gnosis_0x'),
+                (0x920E180FbB32D2B14EA51D95630afe3F8205b278, 'prod', 'Gnosis_0x'),
+    ) as _
+)
+-- Combining the metadata with current activation status for final table
+select solver as address,
+       case when environment is not null then environment else 'new' end as environment,
+       case when name is not null then name else 'Uncatalogued'      end as name,
+      active
+from registered_solvers
+left outer join known_solver_metadata on solver = address
