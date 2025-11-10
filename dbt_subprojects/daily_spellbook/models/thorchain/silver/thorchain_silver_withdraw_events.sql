61f6db049239d7fe6e29bae285e332764d60637c
jeff-dude <jeff@dune.xyz>
2025-11-09 20:48:25 -0500

    fix missing col in silver withdraw events

--- dbt_subprojects/daily_spellbook/models/thorchain/silver/thorchain_silver_withdraw_events.sql
+++ dbt_subprojects/daily_spellbook/models/thorchain/silver/thorchain_silver_withdraw_events.sql
@@ -7,45 +7,25 @@
 
 with base as (
     SELECT
-    tx AS tx_id,
-    chain AS blockchain,
-    from_addr AS from_address,
-    to_addr AS to_address,
-    asset,
-    asset_e8,
-    emit_asset_e8,
-    emit_rune_e8,
-    memo,
-    pool AS pool_name,
-    stake_units,
-    basis_points,
-    asymmetry,
-    imp_loss_protection_e8,
-    _emit_asset_in_rune_e8,
-    event_id,
-    block_timestamp,
-    _TX_TYPE,
-    
-    cast(from_unixtime(cast(block_timestamp / 1e9 as bigint)) as timestamp) as block_time,
-    date(from_unixtime(cast(block_timestamp / 1e9 as bigint))) as block_date,
-    date_trunc('month', from_unixtime(cast(block_timestamp / 1e9 as bigint))) as block_month,
-    date_trunc('hour', from_unixtime(cast(block_timestamp / 1e9 as bigint))) as block_hour,
-    
-    CASE 
-        WHEN asset LIKE 'THOR.%' THEN cast(null as varbinary)
-        ELSE cast(asset as varbinary)
-    END as contract_address,
-    
-    CASE 
-        WHEN pool LIKE '%.%' THEN split_part(pool, '.', 1)
-        ELSE 'THOR'
-    END as pool_chain,
-    
-    CASE 
-        WHEN pool LIKE '%.%' THEN split_part(pool, '.', 2)
-        ELSE pool
-    END as pool_asset
-
+        tx AS tx_id,
+        chain AS blockchain,
+        from_addr AS from_address,
+        to_addr AS to_address,
+        asset,
+        asset_e8,
+        emit_asset_e8,
+        emit_rune_e8,
+        memo,
+        pool AS pool_name,
+        stake_units,
+        basis_points,
+        asymmetry,
+        imp_loss_protection_e8,
+        _emit_asset_in_rune_e8,
+        event_id,
+        block_timestamp,
+        _TX_TYPE,
+        _ingested_at AS _inserted_timestamp,
     FROM {{ source('thorchain', 'withdraw_events') }}
 )
 
