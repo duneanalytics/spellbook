a27a397a83987e0df5b7a7ece6a589eb98ee16b2
hildobby <h.moulie@gmail.com>
2025-11-11 23:43:03 +0100

    bridges: sui (ethereum side) (#8981)
    * bridges: sui (ethereum side)
    
    * add tokens from sui bridge config contract
    
    * fix
    
    * fix

--- dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/ethereum/bridges_ethereum_deposits.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/ethereum/bridges_ethereum_deposits.sql
@@ -19,6 +19,7 @@
     , 'bridges_' + blockchain + '_lighter_v1_deposits'
     , 'bridges_' + blockchain + '_avalanche_c_native_v2_deposits'
     , 'bridges_' + blockchain + '_zkync_native_v2_deposits'
+    , 'bridges_' + blockchain + '_sui_native_v1_deposits'
     , 'bridges_' + blockchain + '_synapse_rfq_deposits'
     , 'bridges_' + blockchain + '_zkbridge_v1_deposits'
     , 'bridges_' + blockchain + '_rainbow_v1_deposits'
