ebfc41769e85a0f69a6fed75611e986ec0e8b4e4
jeff-dude <jeff@dune.xyz>
2025-11-10 21:55:42 -0500

    clean up sol payments models, add new to schema yml

--- dbt_subprojects/solana/models/_sector/dex/base_app/fee_payments_raw/base_app_swapper_solana_stg_sol_payments.sql
+++ dbt_subprojects/solana/models/_sector/dex/base_app/fee_payments_raw/base_app_swapper_solana_stg_sol_payments.sql
@@ -9,34 +9,35 @@
    )
 }}
 
-with sol_payments as (
-    select
-        block_time,
-        block_month,
-        fee_receiver,
-        amount,
-        token_address,
-        tx_id
-    from {{ ref('base_app_swapper_solana_stg_sol_payments_fees_paid') }}
-    where
-        1 = 1
+{% set wsol_token = 'So11111111111111111111111111111111111111112' %}
+
+-- From '2025-10' onwards fees are paid directly to 6oo...yBd during each trade
+select
+    account_activity.block_time,
+    cast(date_trunc('month', account_activity.block_time) as date) as block_month,
+    address as fee_receiver,
+    account_activity.balance_change / 1e9 as amount,
+    '{{wsol_token}}' token_address,
+    account_activity.tx_id
+from {{ source('solana','account_activity') }} as account_activity
+where
+    {% if is_incremental() %} 
+    {{ incremental_predicate('account_activity.block_time') }}
+    {% else %} 
+    account_activity.block_time >= timestamp '2025-10-16'
+    {% endif %} 
+    and tx_success
+    and address = '6ooVBXhnqAXaF91cu49YmWhoFuE6WLdZWTwNYvTuhyBd'
+    and balance_change > 0
+    and exists (
+        select 1 
+        from {{ source('dex_solana', 'trades') }} as trades
+        where trades.tx_id = account_activity.tx_id
+        and trades.block_time = account_activity.block_time
+        and trades.block_month = cast(date_trunc('month', account_activity.block_time) as date)
         {% if is_incremental() %} 
-        and {{ incremental_predicate('block_time') }}
-        {% endif %}
-    union all
-    select
-        block_time,
-        block_month,
-        fee_receiver,
-        amount,
-        token_address,
-        tx_id
-    from {{ ref('base_app_swapper_solana_stg_sol_payments_fees_claimed') }}
-    where
-        1 = 1
-        {% if is_incremental() %} 
-        and {{ incremental_predicate('block_time') }}
-        {% endif %}
-)
-select *
-from sol_payments
+        and {{ incremental_predicate('trades.block_time') }}
+        {% else %} 
+        and trades.block_time >= timestamp '2025-10-16'
+        {% endif %} 
+    )
