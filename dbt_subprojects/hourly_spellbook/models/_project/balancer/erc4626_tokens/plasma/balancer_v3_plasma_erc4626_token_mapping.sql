811865643b1faae991be8a0ea525c38c13a5c081
amandadalle <amandadallepiane8@gmail.com>
2025-11-12 17:44:30 -0300

    feat: add plasma erc4626 token mapping model

--- dbt_subprojects/hourly_spellbook/models/_project/balancer/erc4626_tokens/plasma/balancer_v3_plasma_erc4626_token_mapping.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/erc4626_tokens/plasma/balancer_v3_plasma_erc4626_token_mapping.sql
@@ -4,28 +4,29 @@
         alias = 'erc4626_token_mapping', 
         materialized = 'table',
         file_format = 'delta'
-         )
+    )
 }}
 
-WITH aave_tokens AS(
-SELECT 
-    b.output_0 AS erc4626_token,
-    t2.symbol AS erc4626_token_name,
-    t2.symbol AS erc4626_token_symbol,
-    BYTEARRAY_SUBSTRING(b.salt, 13, 24) AS underlying_token,
-    t1.symbol AS underlying_token_symbol,
-    t2.decimals AS decimals
-FROM {{ source('aave_v3_plasma', 'StataTokenV2_evt_Initialized') }} a
-JOIN {{ source('aave_v3_plasma', 'StataTokenFactory_call_createDeterministic') }} b
-ON b.output_0 = a.contract_address
-JOIN {{ source('tokens', 'erc20') }} t1
-ON t1.blockchain = 'plasma'
-AND BYTEARRAY_SUBSTRING(b.salt, 13, 36) = t1.contract_address
-JOIN {{ source('tokens', 'erc20') }} t2
-ON t2.blockchain = 'plasma'
-AND b.output_0 = t2.contract_address)
+WITH aave_tokens AS (
+    SELECT 
+        b.output_0[1] AS erc4626_token,
+        t2.symbol AS erc4626_token_name,
+        t2.symbol AS erc4626_token_symbol,
+        b.underlyings[1] AS underlying_token,
+        t1.symbol AS underlying_token_symbol,
+        t2.decimals AS decimals
+    FROM {{ source('aave_v3_plasma', 'StataTokenV2_evt_Initialized') }} a
+    JOIN {{ source('aave_v3_plasma', 'StataTokenFactory_call_createstatatokens') }} b
+        ON b.output_0[1] = a.contract_address
+    JOIN {{ source('tokens', 'erc20') }} t1
+        ON t1.blockchain = 'plasma'
+        AND b.underlyings[1] = t1.contract_address
+    JOIN {{ source('tokens', 'erc20') }} t2
+        ON t2.blockchain = 'plasma'
+        AND b.output_0[1] = t2.contract_address
+)
 
 SELECT 
     'plasma' AS blockchain, 
     * 
-FROM aave_tokens 
+FROM aave_tokens
