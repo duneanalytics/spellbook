93e960d6b50209c0950fa0edd3c583bba40eb81b
amandadalle <amandadallepiane8@gmail.com>
2025-11-13 16:42:34 -0300

    refactor(plasma): simplify SQL query by removing deduplication logic

--- dbt_subprojects/hourly_spellbook/models/_project/balancer/protocol_fee/plasma/balancer_v3_plasma_protocol_fee.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/protocol_fee/plasma/balancer_v3_plasma_protocol_fee.sql
@@ -13,26 +13,14 @@ WITH base AS (
     {{
         balancer_v3_compatible_protocol_fee_macro(
             blockchain = blockchain,
-            version = '3',        
+            version = '3',
             project_decoded_as = 'balancer_v3',
             base_spells_namespace = 'balancer',
             pool_labels_model = 'balancer_v3_pools_plasma'
         )
     }}
-),
-deduplicated AS (
-    SELECT 
-        *,
-        ROW_NUMBER() OVER (
-            PARTITION BY 
-                day,
-                pool_id,
-                token_address,
-                fee_type
-            ORDER BY protocol_fee_collected_usd DESC
-        ) AS rn
-    FROM base
 )
-SELECT *
-FROM deduplicated
-WHERE rn = 1
+
+SELECT DISTINCT *
+FROM base
+
