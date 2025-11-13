c6daf38cccbae7ea143cb45eeb154b0ce12fa27c
amandadalle <amandadallepiane8@gmail.com>
2025-11-13 09:56:43 -0300

    refactor(plasma): restructure SQL query for clarity

--- dbt_subprojects/hourly_spellbook/models/_project/balancer/protocol_fee/plasma/balancer_v3_plasma_protocol_fee.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/protocol_fee/plasma/balancer_v3_plasma_protocol_fee.sql
@@ -9,15 +9,15 @@
     )
 }}
 
-{{ 
-    balancer_v3_compatible_protocol_fee_macro(
-        blockchain = blockchain,
-        version = '3',        
-        project_decoded_as = 'balancer_v3',
-        base_spells_namespace = 'balancer',
-        pool_labels_model = 'balancer_v3_pools_plasma'
-    )
-}}
-
 SELECT DISTINCT *
-FROM base
+FROM (
+    {{
+        balancer_v3_compatible_protocol_fee_macro(
+            blockchain = blockchain,
+            version = '3',        
+            project_decoded_as = 'balancer_v3',
+            base_spells_namespace = 'balancer',
+            pool_labels_model = 'balancer_v3_pools_plasma'
+        )
+    }}
+) base
