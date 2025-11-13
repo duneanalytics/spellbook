aedf795cfd40abc8eca58458b3fa5d5c85537081
jeff-dude <jeff@dune.xyz>
2025-11-13 18:17:50 -0500

    update all models to incremental tables

--- dbt_subprojects/tokens/models/transfers_and_balances/shape/tokens_shape_transfers_from_traces.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/shape/tokens_shape_transfers_from_traces.sql
@@ -4,7 +4,12 @@
     config(
         schema = 'tokens_' ~ blockchain,
         alias = 'transfers_from_traces',
-        materialized = 'view',
+        partition_by = ['block_month'],
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+        unique_key = ['block_date', 'unique_key'],
     )
 }}
 
