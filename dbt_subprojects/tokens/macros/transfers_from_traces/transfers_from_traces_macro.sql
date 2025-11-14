b2d41254ca4bf903dac6bd6bacef70701d1262b3
max-morrow <amidmoro@gmail.com>
2025-11-14 03:18:42 +0300

    cosmetic & visuality

--- dbt_subprojects/tokens/macros/transfers_from_traces/transfers_from_traces_macro.sql
+++ dbt_subprojects/tokens/macros/transfers_from_traces/transfers_from_traces_macro.sql
@@ -1,16 +1,21 @@
-{%- macro transfers_from_traces_macro(
-    blockchain
-    , transfers_start_date = '2000-01-01'
-    , easy_dates=true
-    , prices_interval='hour'
+{%- macro
+    transfers_from_traces_macro(
+        blockchain,
+        transfers_start_date='2000-01-01',
+        easy_dates=true,
+        prices_interval='hour'
     )
 -%}
 
 {%- if blockchain is none or blockchain == '' -%}
-    {{ exceptions.raise_compiler_error("blockchain parameter cannot be null or empty") }}
+    {{- exceptions.raise_compiler_error("blockchain parameter cannot be null or empty") -}}
 {%- endif -%}
 
-with base_traces as (
+
+
+with
+
+base_tft as (
     select
         blockchain
         , block_month
@@ -26,18 +31,15 @@ with base_traces as (
         , "from"
         , "to"
         , unique_key
-    from
-        {{ ref('tokens_' ~ blockchain ~ '_transfers_from_traces_base') }}
-    where
-        1=1
-        {% if easy_dates -%} and block_date > current_date - interval '10' day {%- endif %} -- easy_dates mode for dev, to prevent full scan
-        {% if is_incremental() -%}
-        and {{ incremental_predicate('block_date') }}
-        {% else -%}
+        , date_trunc('{{ prices_interval }}', block_time) as timestamp
+    from {{ ref('tokens_' ~ blockchain ~ '_transfers_from_traces_base') }}
+    where true
         and block_date >= timestamp '{{ transfers_start_date }}'
-        {% endif -%}
+        {% if easy_dates -%} and block_date > current_date - interval '10' day {%- endif %} -- easy_dates mode for dev, to prevent full scan
+        {% if is_incremental() -%} and {{ incremental_predicate('block_date') }} {%- endif %}
 )
-, base_traces_wrapper_deposits as (
+
+, base_tft_wrapper_deposits as (
     select
         blockchain
         , block_month
@@ -53,75 +55,70 @@ with base_traces as (
         , "from"
         , "to"
         , unique_key
-    from
-        {{ ref('tokens_' ~ blockchain ~ '_transfers_from_traces_base_wrapper_deposits') }}
-    where
-        1=1
-        {% if easy_dates -%} and block_date > current_date - interval '10' day {%- endif %} -- easy_dates mode for dev, to prevent full scan
-        {% if is_incremental() -%}
-        and {{ incremental_predicate('block_date') }}
-        {% else -%}
+        , date_trunc('{{ prices_interval }}', block_time) as timestamp
+    from {{ ref('tokens_' ~ blockchain ~ '_transfers_from_traces_base_wrapper_deposits') }}
+    where true
         and block_date >= timestamp '{{ transfers_start_date }}'
-        {% endif -%}
+        {% if easy_dates -%} and block_date > current_date - interval '10' day {%- endif %} -- easy_dates mode for dev, to prevent full scan
+        {% if is_incremental() -%} and {{ incremental_predicate('block_date') }} {%- endif %}
 )
-, union_traces as (
-    select * from base_traces
+
+, tft as (
+    select * from base_tft
     union all
-    select * from base_traces_wrapper_deposits
+    select * from base_tft_wrapper_deposits
 )
-, erc20 as (
+
+, tokens as (
     select
-        blockchain
-        , contract_address
-        , decimals
-        , symbol
-    from
-        {{ source('tokens', 'erc20') }}
-    where
-        blockchain = '{{ blockchain }}'
+        contract_address
+        , decimals as token_decimals
+        , symbol as token_symbol
+    from {{ source('tokens', 'erc20') }}
+    where true
+        and blockchain = '{{ blockchain }}'
 )
+
 , prices as (
     select
-        timestamp
-        , blockchain
-        , contract_address
+        contract_address
+        , timestamp
         , decimals
         , symbol
         , price
-    from
-        {{ source('prices_external', prices_interval) }}
-    where
-        blockchain = '{{ blockchain }}'
-        {% if easy_dates -%} and timestamp > current_date - interval '10' day {%- endif %} -- easy_dates mode for dev, to prevent full scan
-        {% if is_incremental() %}
-        and {{ incremental_predicate('timestamp') }}
-        {% else %}
+    from {{ source('prices_external', prices_interval) }}
+    where true
+        and blockchain = '{{ blockchain }}'
         and timestamp >= timestamp '{{ transfers_start_date }}'
-        {% endif %}
+        {% if easy_dates -%} and timestamp > current_date - interval '10' day {%- endif %} -- easy_dates mode for dev, to prevent full scan
+        {% if is_incremental() -%} and {{ incremental_predicate('timestamp') }} {%- endif %}
 )
+
+-- output --
+
 select
-    t.blockchain
-    , t.block_month
-    , t.block_date
-    , t.block_time
-    , t.block_number
-    , t.tx_hash
-    , t.trace_address
-    , t.type
-    , t.token_standard
-    , t.contract_address
-    , erc20.symbol
-    , t.amount_raw
-    , t.amount_raw / power(10, coalesce(erc20.decimals, p.decimals)) as amount
-    , p.price as price_usd
-    , t.amount_raw / power(10, coalesce(erc20.decimals, p.decimals)) * p.price as amount_usd
-    , t."from"
-    , t."to"
-    , t.unique_key
-from union_traces as t
-left join erc20
-    on erc20.contract_address = t.contract_address
-left join prices as p
-    on p.contract_address = t.contract_address
-    and p.timestamp = date_trunc('{{ prices_interval }}', t.block_time)
+    blockchain
+    , block_month
+    , block_date
+    , block_time
+    , block_number
+    , tx_hash
+    , trace_address
+    , type
+    , token_standard
+    , contract_address
+    , coalesce(token_symbol, symbol) as symbol
+    , amount_raw
+    , amount_raw / power(10, coalesce(token_decimals, decimals)) as amount
+    , price as price_usd
+    , amount_raw / power(10, coalesce(token_decimals, decimals)) * price as amount_usd
+    , "from"
+    , "to"
+    , unique_key
+from tft
+left join tokens using(contract_address)
+left join prices using(contract_address, timestamp)
+
+
+
 {%- endmacro -%}
