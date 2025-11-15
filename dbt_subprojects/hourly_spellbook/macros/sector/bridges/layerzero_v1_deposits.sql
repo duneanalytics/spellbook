f9020ca61dcedddc53474cd3b4db7266b71036c8
Hildebert Moulié <h.moulie@gmail.com>
2025-11-15 17:32:15 +0100

    fix

--- dbt_subprojects/hourly_spellbook/macros/sector/bridges/layerzero_v1_deposits.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/bridges/layerzero_v1_deposits.sql
@@ -74,7 +74,6 @@ SELECT distinct '{{blockchain}}' AS deposit_chain
 , COALESCE(t.evt_index, -sc.call_send_index) AS evt_index
 , sc.contract_address
 , {{ dbt_utils.generate_surrogate_key(['sc.tx_hash', 't.evt_index', 'sc.call_send_index']) }} as bridge_transfer_id
-, CAST(t.unique_key AS varchar) AS bridge_transfer_id
 FROM send_calls sc
 LEFT JOIN transfers t ON t.block_number=sc.block_number
         AND t.tx_hash=sc.tx_hash
