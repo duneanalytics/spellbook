b5b0d632cecb5d10cb9e2a4316a760025a723493
hildobby <h.moulie@gmail.com>
2025-11-18 17:57:33 -0300

    bridges: fix layerzero and prices join (#9048)
    * fix layerzero and prices join
    
    * add fix for allbridge
    
    * add sources
    
    * fix
    
    * account for token standard

--- dbt_subprojects/hourly_spellbook/macros/sector/bridges/allbridge_classic_deposits.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/bridges/allbridge_classic_deposits.sql
@@ -10,16 +10,17 @@ SELECT '{{blockchain}}' AS deposit_chain
 , evt_block_number AS block_number
 , amount AS deposit_amount_raw
 , sender
-, recipient
-, tokenSource AS deposit_token_address
+, CASE WHEN substr(recipient, 21) = 0x000000000000000000000000 THEN substr(recipient, 1, 20) ELSE recipient END AS recipient
+, nativeTokenAddress AS deposit_token_address
 , 'erc20' AS deposit_token_standard
 , 'erc20' AS withdrawal_token_standard
 , evt_tx_from AS tx_from
 , evt_tx_hash AS tx_hash
 , evt_index
-, contract_address
+, d.contract_address
 , CAST(lockId AS varchar) AS bridge_transfer_id
 FROM ({{ source('allbridge_' + blockchain, 'bridge_evt_sent') }}) d
+LEFT JOIN {{ source('allbridge_' + blockchain, 'bridge_call_addtoken') }} at USING (tokenSource)
 LEFT JOIN {{ ref('bridges_allbridge_classic_chain_indexes') }} ci ON trim(from_utf8(d.destination))=ci.allbridge_slug
 LEFT JOIN {{ source('evms','info') }} i ON ci.blockchain=i.blockchain
 
