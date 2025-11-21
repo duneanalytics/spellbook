5a5244c36bdd0d6944b40a2d0494bb316be12d17
jeff-dude <jeff@dune.xyz>
2025-11-21 14:33:48 -0300

    add filter for performance

--- dbt_subprojects/solana/models/_sector/dex/dex_solana_stg_max_block_slot.sql
+++ dbt_subprojects/solana/models/_sector/dex/dex_solana_stg_max_block_slot.sql
@@ -13,3 +13,6 @@ select
     , max(block_slot) as block_slot_filter
 from
     {{ source('tokens_solana', 'transfers') }}
+where
+    -- to prevent full scan, give short timeframe filter with some buffer
+    block_date >= now() - interval '3' day
