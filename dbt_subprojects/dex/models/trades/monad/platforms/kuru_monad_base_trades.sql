1c607090afe18ae81d9aac5d104fe2fe99bf1dd5
tomfutago <tomfutago@gmail.com>
2025-11-24 16:37:36 +0000

    feat: update source and dex logic + add monad to dex base trades

--- dbt_subprojects/dex/models/trades/monad/platforms/kuru_monad_base_trades.sql
+++ dbt_subprojects/dex/models/trades/monad/platforms/kuru_monad_base_trades.sql
@@ -10,12 +10,40 @@
     )
 }}
 
-{{
-    generic_spot_compatible_trades(
-        blockchain = 'monad',
-        project = 'kuru',
-        version = '1',
-        source_evt_swap = source('kuru_monad_testnet', 'kuruflow_evt_kuruflowswap'),
-        taker = 'user'
-    )
-}}
+WITH dexs AS (
+    SELECT
+        t.evt_block_time as block_time,
+        t.evt_block_number as block_number,
+        cast(null as varbinary) as taker,
+        cast(null as varbinary) as maker,
+        t.amountOut as token_bought_amount_raw,
+        t.amountIn as token_sold_amount_raw,
+        t.creditToken as token_bought_address,
+        t.debitToken as token_sold_address,
+        t.contract_address as project_contract_address,
+        t.evt_tx_hash as tx_hash,
+        t.evt_index
+    FROM {{ source('kuru_monad', 'router_evt_kururouterswap') }} t
+    {% if is_incremental() %}
+    WHERE {{ incremental_predicate('t.evt_block_time') }}
+    {% endif %}
+)
+
+SELECT
+    'monad' AS blockchain,
+    'kuru' AS project,
+    '1' AS version,
+    CAST(date_trunc('month', dexs.block_time) AS date) AS block_month,
+    CAST(date_trunc('day', dexs.block_time) AS date) AS block_date,
+    dexs.block_time,
+    dexs.block_number,
+    dexs.token_bought_amount_raw,
+    dexs.token_sold_amount_raw,
+    dexs.token_bought_address,
+    dexs.token_sold_address,
+    dexs.taker,
+    dexs.maker,
+    dexs.project_contract_address,
+    dexs.tx_hash,
+    dexs.evt_index
+FROM dexs
