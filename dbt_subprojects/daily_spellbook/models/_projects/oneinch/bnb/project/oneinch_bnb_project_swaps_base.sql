49146ecc3db2cb015e16ab46a56bb1e5279d1906
max-morrow <amidmoro@gmail.com>
2025-12-02 22:17:35 +0300

    fix

--- dbt_subprojects/daily_spellbook/models/_projects/oneinch/bnb/project/oneinch_bnb_project_swaps_base.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/oneinch/bnb/project/oneinch_bnb_project_swaps_base.sql
@@ -15,11 +15,11 @@
 
 -- depends_on: {{ ref('oneinch_' + blockchain + '_project_orders') }}
 
-{{-
+{{
     oneinch_project_swaps_base_macro(
         blockchain = blockchain,
         date_from = '2020-08-01'
     )
--}}
+}}
 where true
     and block_month < date('2020-09-01')
