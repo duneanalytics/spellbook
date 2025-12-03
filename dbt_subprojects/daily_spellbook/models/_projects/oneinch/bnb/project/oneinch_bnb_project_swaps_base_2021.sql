dd748f7138b6b9493df7b415e7eabc556c241e61
max-morrow <amidmoro@gmail.com>
2025-12-03 12:08:14 +0300

    next

---
+++ dbt_subprojects/daily_spellbook/models/_projects/oneinch/bnb/project/oneinch_bnb_project_swaps_base_2021.sql
@@ -0,0 +1,20 @@
+{%- set blockchain = 'bnb' -%}
+
+{{-
+    config(
+        schema = 'oneinch_' + blockchain,
+        alias = 'project_swaps_base_2021',
+        materialized = 'table',
+        unique_key = ['blockchain', 'id'],
+    )
+-}}
+
+-- depends_on: {{ ref('oneinch_' + blockchain + '_project_orders') }}
+
+{{
+    oneinch_project_swaps_base_u_macro(
+        blockchain = blockchain,
+        date_from = '2021-01-01',
+        date_to = '2022-01-01'
+    )
+}}
