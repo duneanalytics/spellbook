76fe73e5708fe6ae552e4232a36dc1ec2f9c88d7
tomfutago <tomfutago@gmail.com>
2025-12-05 14:14:53 +0000

    feat: base+enrich approach

---
+++ dbt_subprojects/daily_spellbook/models/_sector/stablecoins/bnb/stablecoins_bnb_base_balances.sql
@@ -0,0 +1,42 @@
+{{
+  config(
+    tags = ['prod_exclude'],
+    schema = 'stablecoins_bnb',
+    alias = 'base_balances',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['day', 'address', 'token_address'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
+  )
+}}
+
+with
+
+stablecoin_tokens as (
+  select
+    symbol,
+    contract_address as token_address
+  from {{ source('tokens_bnb', 'erc20_stablecoins')}}
+),
+
+balances as (
+  {{
+    balances_incremental_subset_daily_new(
+        blockchain = 'bnb',
+        token_list = 'stablecoin_tokens',
+        start_date = '2020-09-01'
+    )
+  }}
+)
+
+select
+  blockchain,
+  day,
+  address,
+  token_address,
+  token_standard,
+  token_id,
+  balance_raw,
+  last_updated
+from balances
---
+++ dbt_subprojects/daily_spellbook/models/_sector/stablecoins/polygon/stablecoins_polygon_base_balances.sql
@@ -0,0 +1,41 @@
+{{
+  config(
+    schema = 'stablecoins_polygon',
+    alias = 'base_balances',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['day', 'address', 'token_address'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.day')]
+  )
+}}
+
+with
+
+stablecoin_tokens as (
+  select
+    symbol,
+    contract_address as token_address
+  from {{ source('tokens_polygon', 'erc20_stablecoins')}}
+),
+
+balances as (
+  {{
+    balances_incremental_subset_daily_new(
+        blockchain = 'polygon',
+        token_list = 'stablecoin_tokens',
+        start_date = '2023-08-01'
+    )
+  }}
+)
+
+select
+  blockchain,
+  day,
+  address,
+  token_address,
+  token_standard,
+  token_id,
+  balance_raw,
+  last_updated
+from balances
