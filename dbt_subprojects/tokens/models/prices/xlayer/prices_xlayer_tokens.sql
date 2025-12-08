44477588fbd68f180a0f862dd4826ee245e19df0
tomfutago <tomfutago@gmail.com>
2025-12-08 09:39:14 +0000

    fix: symbols and dupes

--- dbt_subprojects/tokens/models/prices/xlayer/prices_xlayer_tokens.sql
+++ dbt_subprojects/tokens/models/prices/xlayer/prices_xlayer_tokens.sql
@@ -19,10 +19,10 @@ FROM
 (
     VALUES
     -- placeholder rows, add actual tokens as needed
-    ('okb-okb', 'WOKB', 0xe538905cf8410324e03a5a23c1c177a474d59b2b, 18)
+    ('okb-okb', 'OKB', 0xe538905cf8410324e03a5a23c1c177a474d59b2b, 18) -- WOKB
     , ('weth-weth', 'WETH', 0x5a77f1443d16ee5761d310e38b62f77f726bc71c, 18)
     , ('usdt-tether', 'USDT', 0x1e4a5963abfd975d8c9021ce480b42188849d41d, 6)
-    , ('usdt-tether', 'USDT0', 0x779ded0c9e1022225f8e0630b35a9b54be713736, 6)
+    , ('usdt-tether', 'USDT', 0x779ded0c9e1022225f8e0630b35a9b54be713736, 6) -- USDT0
     , ('usdc-usd-coin', 'USDC', 0x74b7f16337b8972027f6196a17a631ac6de26d22, 6)
     , ('usdce-usd-coine', 'USDC.e', 0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035, 6)
     , ('wbtc-wrapped-bitcoin', 'WBTC', 0xea034fb02eb1808c2cc3adbc15f447b93cbe08e1, 8)
