282edb7ec7257164ecc4bb6482003ac44b76a67e
tomfutago <tomfutago@gmail.com>
2025-12-09 09:25:58 +0000

    feat: stablecoin balances base+enrich approach

---
+++ dbt_subprojects/daily_spellbook/models/_sector/stablecoins/stablecoins_base_balances.sql
@@ -0,0 +1,45 @@
+{{
+   config(
+     schema = 'stablecoins',
+     alias = 'base_balances',
+     materialized = 'view'
+   )
+ }}
+
+{%
+  set models = [
+    ref('stablecoins_arbitrum_base_balances'),
+    ref('stablecoins_avalanche_c_base_balances'),
+    ref('stablecoins_base_base_balances'),
+    ref('stablecoins_ethereum_base_balances'),
+    ref('stablecoins_linea_base_balances'),
+    ref('stablecoins_polygon_base_balances'),
+    ref('stablecoins_scroll_base_balances')
+  ]
+%}
+
+/*
+temp removed:
+    ref('stablecoins_bnb_base_balances'),
+    ref('stablecoins_kaia_base_balances'),
+    ref('stablecoins_optimism_base_balances'),
+    ref('stablecoins_worldchain_base_balances'),
+    ref('stablecoins_zksync_base_balances')
+*/
+
+{% for model in models %}
+select
+  blockchain,
+  day,
+  address,
+  token_address,
+  token_standard,
+  token_id,
+  balance_raw,
+  last_updated
+from {{ model }}
+{% if not loop.last %}
+union all
+{% endif %}
+{% endfor %}
+
