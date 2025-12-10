f25cda4b6d489bd200df7f76384a2cb1638eb8b0
hildobby <h.moulie@gmail.com>
2025-12-10 01:40:58 +0100

    create users balance changes spell

---
+++ dbt_subprojects/daily_spellbook/models/_projects/polymarket/polygon/polymarket_polygon_protocol_tvl.sql
@@ -0,0 +1,71 @@
+{{
+  config(
+    schema = 'polymarket_polygon',
+    alias = 'protocol_tvl',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['hour']
+  )
+}}
+
+WITH latest_transfer_hour AS (
+  SELECT date_trunc('hour', MAX(block_time)) AS max_hour
+  FROM {{ source('tokens_polygon', 'transfers') }}
+  WHERE block_time > NOW() - interval '3' day
+  )
+
+
+{% if is_incremental() %}
+, last_spell_update AS (
+  SELECT MAX(hour) AS max_hour
+  , MAX_BY(tvl, hour) AS latest_tvl
+  FROM {{this}}
+  )
+{% endif %}
+
+, flows AS (
+  SELECT date_trunc('hour', block_time) AS hour
+  , SUM(amount) as flow
+  FROM {{ source('tokens_polygon', 'transfers') }} t
+  WHERE contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
+  AND to IN (0x4d97dcd97ec945f40cf65f87097ace5ea0476045, 0x3a3bd7bb9528e159577f7c2e685cc81a765002e2)
+  AND block_time > (SELECT max_hour FROM latest_transfer_hour)
+  AND block_number > 4023686
+  {% if is_incremental() %}
+  AND block_time > (SELECT max_hour FROM last_spell_update)
+  {% endif %}
+  GROUP BY 1
+
+  UNION ALL
+
+  SELECT date_trunc('hour', block_time) AS hour
+  , -SUM(amount) as flow
+  FROM {{ source('tokens_polygon', 'transfers') }} t
+  WHERE contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
+  AND "from" IN (0x4d97dcd97ec945f40cf65f87097ace5ea0476045, 0x3a3bd7bb9528e159577f7c2e685cc81a765002e2)
+  AND block_time > (SELECT max_hour FROM latest_transfer_hour)
+  AND block_number > 4023686
+  {% if is_incremental() %}
+  AND block_time > (SELECT max_hour FROM last_spell_update)
+  {% endif %}
+  GROUP BY 1
+  )
+
+
+{% if is_incremental() %}
+
+SELECT f.hour
+, SUM(f.flow) AS net_flow
+, SUM(SUM(lu.latest_tvl + f.flow)) OVER (ORDER BY f.hour) AS tvl
+FROM flows f
+INNER JOIN last_update lu ON f.hour > lu.max_hour
+
+{% else %}
+
+SELECT hour
+, SUM(flow) AS net_flow
+, SUM(SUM(flow)) OVER (ORDER BY hour) AS tvl
+FROM flows
+
+{% endif %}
---
+++ dbt_subprojects/daily_spellbook/models/_projects/polymarket/polygon/polymarket_polygon_users_balance_changes.sql
@@ -0,0 +1,62 @@
+{{
+  config(
+    schema = 'polymarket_polygon',
+    alias = 'users_balance_changes',
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    unique_key = ['block_number', 'tx_index', 'evt_index', 'direction']
+  )
+}}
+
+SELECT t.block_time
+, t.block_date
+, t.block_month
+, t.block_number
+, t.tx_hash
+, t.tx_from
+, t.tx_to
+, t.tx_index
+, t.evt_index
+, 'inflow' AS direction
+, a.polymarket_wallet
+, t.amount
+, "from"
+, CAST(NULL AS varbinary) AS to
+, t.unique_key
+FROM {{ source('tokens_polygon', 'transfers') }} t
+INNER JOIN {{ ref('polymarket_polygon.users_address_lookup') }} a ON t.to=a.polymarket_wallet
+  AND a.has_been_funded
+  AND t.block_time>=a.first_funded_time
+WHERE t.contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
+AND t.block_number > 5067840
+{% if is_incremental() %}
+AND {{ incremental_predicate('t.block_time') }}
+{% endif %}
+
+UNION ALL
+
+SELECT t.block_time
+, t.block_date
+, t.block_month
+, t.block_number
+, t.tx_hash
+, t.tx_from
+, t.tx_to
+, t.tx_index
+, t.evt_index
+, 'outflow' AS direction
+, a.polymarket_wallet
+, t.amount
+, CAST(NULL AS varbinary) AS "from"
+, to
+, t.unique_key
+FROM {{ source('tokens_polygon', 'transfers') }} t
+INNER JOIN {{ ref('polymarket_polygon.users_address_lookup') }} a ON t."from"=a.polymarket_wallet
+  AND a.has_been_funded
+  AND t.block_time>=a.first_funded_time
+WHERE contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
+AND t.block_number > 5067840
+{% if is_incremental() %}
+AND {{ incremental_predicate('t.block_time') }}
+{% endif %}
