3be098bbd7d15fdeb9a318c37dcb9bfd339ebd0d
hildobby <h.moulie@gmail.com>
2025-12-12 13:15:54 +0100

    fixes

--- dbt_subprojects/daily_spellbook/models/_projects/polymarket/polygon/polymarket_polygon_protocol_tvl.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/polymarket/polygon/polymarket_polygon_protocol_tvl.sql
@@ -9,13 +9,14 @@
   )
 }}
 
+-- to avoid incomplete data on the last computed hour with partial transfers data
 WITH latest_transfer_hour AS (
   SELECT date_trunc('hour', MAX(block_time)) AS max_hour
   FROM {{ source('tokens_polygon', 'transfers') }}
-  WHERE block_time > NOW() - interval '3' day
+  WHERE block_time > NOW() - interval '7' day
   )
 
-
+-- get the last computed hour in the spell
 {% if is_incremental() %}
 , last_spell_update AS (
   SELECT MAX(hour) AS max_hour
@@ -30,7 +31,7 @@ WITH latest_transfer_hour AS (
   FROM {{ source('tokens_polygon', 'transfers') }} t
   WHERE contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
   AND to IN (0x4d97dcd97ec945f40cf65f87097ace5ea0476045, 0x3a3bd7bb9528e159577f7c2e685cc81a765002e2)
-  AND block_time > (SELECT max_hour FROM latest_transfer_hour)
+  AND block_time < (SELECT max_hour FROM latest_transfer_hour)
   AND block_number > 4023686
   {% if is_incremental() %}
   AND block_time > (SELECT max_hour FROM last_spell_update)
@@ -44,7 +45,7 @@ WITH latest_transfer_hour AS (
   FROM {{ source('tokens_polygon', 'transfers') }} t
   WHERE contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
   AND "from" IN (0x4d97dcd97ec945f40cf65f87097ace5ea0476045, 0x3a3bd7bb9528e159577f7c2e685cc81a765002e2)
-  AND block_time > (SELECT max_hour FROM latest_transfer_hour)
+  AND block_time < (SELECT max_hour FROM latest_transfer_hour)
   AND block_number > 4023686
   {% if is_incremental() %}
   AND block_time > (SELECT max_hour FROM last_spell_update)
