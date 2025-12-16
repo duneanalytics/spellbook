233c9be802d4a1b74705dae976f2a0fbefeb353b
andrew <andrewmaury@0xproject.com>
2025-12-16 11:43:44 -0800

    add monad to multichain tables

--- dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
+++ dbt_subprojects/dex/models/_projects/zeroex/zeroex_api_fills_deduped.sql
@@ -1,7 +1,7 @@
 {{ config(
      schema = 'zeroex'
         , alias = 'api_fills_deduped'
-        , post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "bnb", "celo", "ethereum", "fantom", "optimism", "polygon","scroll", "linea","blast","mantle", "mode", "worldchain", "unichain", "berachain", "ink"]\',
+        , post_hook='{{ expose_spells(\'["arbitrum", "avalanche_c", "base", "bnb", "celo", "ethereum", "fantom", "optimism", "polygon","scroll", "linea","blast","mantle", "mode", "worldchain", "unichain", "berachain", "ink", "monad"]\',
                                 "project",
                                 "zeroex",
                                 \'["rantum","bakabhai993"]\') }}'
@@ -38,6 +38,7 @@
   ,ref('zeroex_v2_worldchain_trades')
   ,ref('zeroex_v2_berachain_trades')
   ,ref('zeroex_v2_ink_trades')
+  ,ref('zeroex_v2_monad_trades')
 ] %}
 
 
