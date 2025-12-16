7a625b5d5677dd639cce148798928817b6fd9943
tomfutago <tomfutago@gmail.com>
2025-12-16 17:29:28 +0000

    chore: move & reformat stablecoin lists

---
+++ dbt_subprojects/tokens/models/stablecoins/kaia/tokens_kaia_erc20_stablecoins.sql
@@ -0,0 +1,17 @@
+{% set chain = 'kaia' %}
+
+{{
+  config(
+    schema = 'tokens_' ~ chain,
+    alias = 'erc20_stablecoins',
+    tags = ['static'],
+    unique_key = ['contract_address']
+  )
+}}
+
+select '{{chain}}' as blockchain, contract_address, backing, symbol, decimals, name
+from (values
+
+     (0x18bc5bcc660cf2b9ce3cd51a404afe1a0cbd3c22, 'Fiat-backed stablecoin', 'IDRX', 18, '')
+
+) as temp_table (contract_address, backing, symbol, decimals, name)
