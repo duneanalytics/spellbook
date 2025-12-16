9592bca90eef1402a8d716053f49563ac5709352
Hildebert Moulié <h.moulie@gmail.com>
2025-12-16 21:58:31 +0100

    optimise balance_changes

--- dbt_subprojects/daily_spellbook/models/_projects/polymarket/polygon/polymarket_polygon_users_balance_changes.sql
+++ dbt_subprojects/daily_spellbook/models/_projects/polymarket/polygon/polymarket_polygon_users_balance_changes.sql
@@ -10,54 +10,102 @@
   )
 }}
 
-SELECT t.block_time
-, t.block_date
-, t.block_month
-, t.block_number
-, t.tx_hash
-, t.tx_from
-, t.tx_to
-, t.tx_index
-, t.evt_index
-, 'inflow' AS direction
-, a.polymarket_wallet
-, t.amount
+WITH polymarket_users_lookup AS (
+  SELECT polymarket_wallet
+  FROM {{ ref('polymarket_polygon_users') }}
+  )
+
+, first_funded_by_lookup AS (
+  SELECT address
+    , block_number AS first_funded_block_number
+  FROM {{ source('addresses_events_polygon', 'first_funded_by')}}
+  WHERE address IN (SELECT polymarket_wallet FROM polymarket_users_lookup)
+  )
+
+, relevant_transfers AS (
+  SELECT t.block_time
+  , t.block_date
+  , t.block_month
+  , t.block_number
+  , t.tx_hash
+  , t.tx_from
+  , t.tx_to
+  , t.tx_index
+  , t.evt_index
+  , t.amount
+  , t."from" AS from_address
+  , t."to" AS to_address
+  , t.unique_key
+  , to_user.polymarket_wallet AS to_polymarket_wallet
+  , from_user.polymarket_wallet AS from_polymarket_wallet
+  , to_ffb.first_funded_block_number AS to_first_funded_block
+  , from_ffb.first_funded_block_number AS from_first_funded_block
+  FROM {{ source('tokens_polygon', 'transfers') }} t
+  LEFT JOIN polymarket_users_lookup to_user on t."to" = to_user.polymarket_wallet
+  LEFT JOIN polymarket_users_lookup from_user on t."from" = from_user.polymarket_wallet
+  LEFT JOIN first_funded_by_lookup to_ffb on t."to" = to_ffb.address
+  LEFT JOIN first_funded_by_lookup from_ffb on t."from" = from_ffb.address
+  WHERE t.contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
+  AND t.block_number >= 5067840
+  AND (to_user.polymarket_wallet IS NOT NULL OR from_user.polymarket_wallet IS NOT NULL)
+  {% if is_incremental() %}
+  AND {{ incremental_predicate('t.block_time') }}
+  {% endif %}
+  )
+  
+SELECT block_time
+, block_date
+, block_month
+, block_number
+, tx_hash
+, tx_from
+, tx_to
+, tx_index
+, evt_index
+, direction
+, polymarket_wallet
+, amount
 , "from"
-, CAST(NULL AS varbinary) AS to
-, t.unique_key
-FROM {{ source('tokens_polygon', 'transfers') }} t
-INNER JOIN {{ ref('polymarket_polygon_users') }} a ON t.to=a.polymarket_wallet
-INNER JOIN {{ source('addresses_events_polygon', 'first_funded_by')}} ffb ON t.to=ffb.address
-  AND ffb.block_number<=t.block_number
-WHERE t.contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
-AND t.block_number >= 5067840
-{% if is_incremental() %}
-AND {{ incremental_predicate('t.block_time') }}
-{% endif %}
-
-UNION ALL
-
-SELECT t.block_time
-, t.block_date
-, t.block_month
-, t.block_number
-, t.tx_hash
-, t.tx_from
-, t.tx_to
-, t.tx_index
-, t.evt_index
-, 'outflow' AS direction
-, a.polymarket_wallet
-, t.amount
-, CAST(NULL AS varbinary) AS "from"
-, to
-, t.unique_key
-FROM {{ source('tokens_polygon', 'transfers') }} t
-INNER JOIN {{ ref('polymarket_polygon_users') }} a ON t."from"=a.polymarket_wallet
-INNER JOIN {{ source('addresses_events_polygon', 'first_funded_by')}} ffb ON t."from"=ffb.address
-  AND ffb.block_number<=t.block_number
-WHERE contract_address = 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 -- USDC.e
-AND t.block_number >= 5067840
-{% if is_incremental() %}
-AND {{ incremental_predicate('t.block_time') }}
-{% endif %}
+, "to"
+, unique_key
+FROM (
+  SELECT block_time
+  , block_date
+  , block_month
+  , block_number
+  , tx_hash
+  , tx_from
+  , tx_to
+  , tx_index
+  , evt_index
+  , 'inflow' AS direction
+  , to_polymarket_wallet AS polymarket_wallet
+  , amount
+  , from_address as "from"
+  , cast(NULL AS varbinary) AS "to"
+  , unique_key
+  FROM relevant_transfers
+  WHERE to_polymarket_wallet IS NOT NULL
+    AND (to_first_funded_block IS NULL OR to_first_funded_block <= block_number)
+  
+  UNION ALL
+  
+  SELECT block_time
+  , block_date
+  , block_month
+  , block_number
+  , tx_hash
+  , tx_from
+  , tx_to
+  , tx_index
+  , evt_index
+  , 'outflow' AS direction
+  , from_polymarket_wallet AS polymarket_wallet
+  , amount
+  , cast(NULL AS varbinary) AS "from"
+  , to_address AS "to"
+  , unique_key
+  FROM relevant_transfers
+  WHERE from_polymarket_wallet IS NOT NULL
+  AND (from_first_funded_block IS NULL OR from_first_funded_block <= block_number)
+) t
