fa79206ed2649bc513ed1a3733cd4fd22a861e08
DevEloperate <dmitriy.i3.jp@gmail.com>
2025-12-17 11:59:09 +0100

    Update dbt_subprojects/tokens/models/tokens/tron/tokens_tron_v1_erc20.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/tokens/models/tokens/tron/tokens_tron_v1_erc20.sql
+++ dbt_subprojects/tokens/models/tokens/tron/tokens_tron_v1_erc20.sql
@@ -30,5 +30,5 @@ FROM
     , ('BTT', 'TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4', 18)
     , ('SUN', 'TSSMHYeV2uE9qYH95DqyoCuNCzEL1NvU3S', 18)
     , ('USDD', 'TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn', 18)
-    , ('BUSD', 'TMz2SWatiAtZVVcH2ebpsbVtYwUPT9EdjH', 18)
+    , ('BUSDOLD', 'TMz2SWatiAtZVVcH2ebpsbVtYwUPT9EdjH', 18)
 ) as temp (symbol, contract_address, decimals)
