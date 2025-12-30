7d6692e80e5bc85cecaeeee250a43657d4fd352b
alicecha <110170331+alicecha@users.noreply.github.com>
2025-12-30 13:07:15 +0100

    turned dex_trades into view and removed tests on dex_trades

--- dbt_subprojects/dex/models/trades/dex_trades.sql
+++ dbt_subprojects/dex/models/trades/dex_trades.sql
@@ -60,12 +60,7 @@
 {{ config(
     schema = 'dex'
     , alias = 'trades'
-    , partition_by = ['block_month', 'blockchain', 'project']
-    , materialized = 'incremental'
-    , file_format = 'delta'
-    , incremental_strategy = 'merge'
-    , unique_key = ['blockchain', 'project', 'version', 'tx_hash', 'evt_index']
-    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    , materialized = 'view'
     , post_hook='{{ expose_spells(blockchains = \'["' + exposed_chains | join('","') + '"]\',
                                     spell_type = "sector",
                                     spell_name = "dex",
@@ -82,36 +77,9 @@
 WITH chain_trades AS (
     {% for chain in chains %}
     SELECT
-        blockchain
-        , project
-        , version
-        , block_month
-        , block_date
-        , block_time
-        , block_number
-        , token_bought_symbol
-        , token_sold_symbol
-        , token_pair
-        , token_bought_amount
-        , token_sold_amount
-        , token_bought_amount_raw
-        , token_sold_amount_raw
-        , amount_usd
-        , token_bought_address
-        , token_sold_address
-        , taker
-        , maker
-        , project_contract_address
-        , tx_hash
-        , tx_from
-        , tx_to
-        , evt_index
+        *
     FROM
         {{ ref('dex_'~chain~'_trades') }}
-    {% if is_incremental() %}
-    WHERE
-        {{ incremental_predicate('block_time') }}
-    {% endif %}
     {% if not loop.last %}
     UNION ALL
     {% endif %}
@@ -120,36 +88,9 @@ WITH chain_trades AS (
 , as_is_dexs AS (
     {% for model in as_is_models %}
     SELECT
-        blockchain
-        , project
-        , version
-        , block_month
-        , block_date
-        , block_time
-        , block_number
-        , token_bought_symbol
-        , token_sold_symbol
-        , token_pair
-        , token_bought_amount
-        , token_sold_amount
-        , token_bought_amount_raw
-        , token_sold_amount_raw
-        , amount_usd
-        , token_bought_address
-        , token_sold_address
-        , taker
-        , maker
-        , project_contract_address
-        , tx_hash
-        , tx_from
-        , tx_to
-        , evt_index
+        *
     FROM
         {{ model }}
-    {% if is_incremental() %}
-    WHERE
-        {{ incremental_predicate('block_time') }}
-    {% endif %}
     {% if not loop.last %}
     UNION ALL
     {% endif %}
