3df577701d0ae1e7fef4c92f2707a4e414c65167
alicecha <110170331+alicecha@users.noreply.github.com>
2026-01-05 10:29:30 +0100

    added token symbol to unique key

--- dbt_subprojects/dex/models/trades/taiko/dex_taiko_token_volumes_daily.sql
+++ dbt_subprojects/dex/models/trades/taiko/dex_taiko_token_volumes_daily.sql
@@ -5,7 +5,7 @@
     , materialized = 'incremental'
     , file_format = 'delta'
     , incremental_strategy = 'merge'
-    , unique_key = ['blockchain', 'token_address', 'block_date']
+    , unique_key = ['blockchain', 'token_address', 'symbol', 'block_date']
     , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
     )
 }}
