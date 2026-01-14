26ba6d10a51819f01a468d8aba23d6caa0a2f949
tomfutago <tomfutago@gmail.com>
2026-01-14 08:48:42 +0000

    chore: revert test start date

--- dbt_subprojects/tokens/models/stablecoins/svm/solana/transfers/stablecoins_solana_core_transfers.sql
+++ dbt_subprojects/tokens/models/stablecoins/svm/solana/transfers/stablecoins_solana_core_transfers.sql
@@ -14,9 +14,7 @@
 
 -- core transfers: tracks transfers for stablecoins in the frozen core list
 
--- TEST: start_date = '2026-01-01' -> revert to none for production
 {{ stablecoins_svm_transfers(
     blockchain = chain,
-    token_list = 'core',
-    start_date = '2026-01-11'
+    token_list = 'core'
 ) }}
