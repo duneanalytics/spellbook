ab5110c739be4b0e9244fe5b92e6ae0c3d6d3c50
jeff-dude <jeff@dune.xyz>
2026-01-13 20:31:02 -0500

    test the semi join filtered transfers design

--- dbt_subprojects/solana/models/_sector/dex/humidifi/humidifi_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/humidifi/humidifi_solana_base_trades.sql
@@ -37,6 +37,84 @@ WITH swaps AS (
 		{% endif -%}
 )
 
+, swap_transfer_keys AS (
+	SELECT DISTINCT
+		  tx_id
+		, block_date
+		, block_slot
+		, outer_instruction_index
+		, inner_instruction_index + 1 AS transfer_inner_instruction_index
+		, 1 AS transfer_side
+	FROM swaps
+
+	UNION ALL
+
+	SELECT DISTINCT
+		  tx_id
+		, block_date
+		, block_slot
+		, outer_instruction_index
+		, inner_instruction_index + 2 AS transfer_inner_instruction_index
+		, 2 AS transfer_side
+	FROM swaps
+)
+
+-- Step 1: filter transfers using a SEMI join (EXISTS) so the hash build is on swap_transfer_keys, not transfers
+, transfers_pruned AS (
+	SELECT
+		  tf.tx_id
+		, tf.block_date
+		, tf.block_slot
+		, tf.outer_instruction_index
+		, tf.inner_instruction_index
+		, tf.amount
+		, tf.from_token_account
+		, tf.to_token_account
+		, tf.token_mint_address
+	FROM {{ source('tokens_solana', 'transfers') }} tf
+	WHERE
+		1=1
+		AND tf.token_version IN ('spl_token', 'spl_token_2022')
+		-- keep this for partition pruning on transfers
+		{% if is_incremental() -%}
+		AND {{ incremental_predicate('tf.block_date') }}
+		{% else -%}
+		AND tf.block_date >= DATE '{{ project_start_date }}'
+		{% endif -%}
+		AND EXISTS (
+			SELECT 1
+			FROM swap_transfer_keys sk
+			WHERE
+				sk.tx_id = tf.tx_id
+				AND sk.block_date = tf.block_date
+				AND sk.block_slot = tf.block_slot
+				AND sk.outer_instruction_index = tf.outer_instruction_index
+				AND sk.transfer_inner_instruction_index = tf.inner_instruction_index
+		)
+)
+
+-- Step 2: attach transfer_side (now this join is only over the pruned set)
+, transfers_filtered AS (
+	SELECT
+		  sk.tx_id
+		, sk.block_date
+		, sk.block_slot
+		, sk.outer_instruction_index
+		, sk.transfer_inner_instruction_index AS inner_instruction_index
+		, sk.transfer_side
+		, tp.amount
+		, tp.from_token_account
+		, tp.to_token_account
+		, tp.token_mint_address
+	FROM swap_transfer_keys sk
+	INNER JOIN transfers_pruned tp
+		ON  tp.tx_id = sk.tx_id
+		AND tp.block_date = sk.block_date
+		AND tp.block_slot = sk.block_slot
+		AND tp.outer_instruction_index = sk.outer_instruction_index
+		AND tp.inner_instruction_index = sk.transfer_inner_instruction_index
+)
+
 , transfers AS (
 	SELECT
 		  s.block_date
@@ -46,12 +124,12 @@ WITH swaps AS (
 			WHEN s.is_inner = false THEN 'direct'
 			ELSE s.outer_executing_account
 		  END AS trade_source
-		, t_buy.amount AS token_bought_amount_raw
-		, t_sell.amount AS token_sold_amount_raw
-		, t_buy.from_token_account AS token_bought_vault
-		, t_sell.to_token_account AS token_sold_vault
-		, t_buy.token_mint_address AS token_bought_mint_address
-		, t_sell.token_mint_address AS token_sold_mint_address
+		, max(CASE WHEN tf.transfer_side = 2 THEN tf.amount END) AS token_bought_amount_raw
+		, max(CASE WHEN tf.transfer_side = 1 THEN tf.amount END) AS token_sold_amount_raw
+		, max(CASE WHEN tf.transfer_side = 2 THEN tf.from_token_account END) AS token_bought_vault
+		, max(CASE WHEN tf.transfer_side = 1 THEN tf.to_token_account END) AS token_sold_vault
+		, max(CASE WHEN tf.transfer_side = 2 THEN tf.token_mint_address END) AS token_bought_mint_address
+		, max(CASE WHEN tf.transfer_side = 1 THEN tf.token_mint_address END) AS token_sold_mint_address
 		, s.pool_id AS project_program_id
 		, s.tx_signer AS trader_id
 		, s.tx_id
@@ -60,34 +138,31 @@ WITH swaps AS (
 		, s.tx_index
 		, s.surrogate_key
 	FROM swaps s
-
-	-- sold (inner_instruction_index + 1)
-	INNER JOIN {{ source('tokens_solana', 'transfers') }} t_sell
-		ON  t_sell.tx_id = s.tx_id
-		AND t_sell.block_date = s.block_date
-		AND t_sell.block_slot = s.block_slot
-		AND t_sell.outer_instruction_index = s.outer_instruction_index
-		AND t_sell.inner_instruction_index = s.inner_instruction_index + 1
-		AND t_sell.token_version IN ('spl_token', 'spl_token_2022')
-		{% if is_incremental() -%}
-		AND {{ incremental_predicate('t_sell.block_date') }}
-		{% else -%}
-		AND t_sell.block_date >= DATE '{{ project_start_date }}'
-		{% endif -%}
-
-	-- bought (inner_instruction_index + 2)
-	INNER JOIN {{ source('tokens_solana', 'transfers') }} t_buy
-		ON  t_buy.tx_id = s.tx_id
-		AND t_buy.block_date = s.block_date
-		AND t_buy.block_slot = s.block_slot
-		AND t_buy.outer_instruction_index = s.outer_instruction_index
-		AND t_buy.inner_instruction_index = s.inner_instruction_index + 2
-		AND t_buy.token_version IN ('spl_token', 'spl_token_2022')
-		{% if is_incremental() -%}
-		AND {{ incremental_predicate('t_buy.block_date') }}
-		{% else -%}
-		AND t_buy.block_date >= DATE '{{ project_start_date }}'
-		{% endif -%}
+	INNER JOIN transfers_filtered tf
+		ON  tf.tx_id = s.tx_id
+		AND tf.block_date = s.block_date
+		AND tf.block_slot = s.block_slot
+		AND tf.outer_instruction_index = s.outer_instruction_index
+		AND tf.inner_instruction_index IN (s.inner_instruction_index + 1, s.inner_instruction_index + 2)
+	GROUP BY
+		  s.block_date
+		, s.block_time
+		, s.block_slot
+		, CASE
+			WHEN s.is_inner = false THEN 'direct'
+			ELSE s.outer_executing_account
+		  END
+		, s.pool_id
+		, s.tx_signer
+		, s.tx_id
+		, s.outer_instruction_index
+		, s.inner_instruction_index
+		, s.tx_index
+		, s.surrogate_key
+	HAVING
+		1=1
+		AND count_if(tf.transfer_side = 1) = 1
+		AND count_if(tf.transfer_side = 2) = 1
 )
 
 SELECT
