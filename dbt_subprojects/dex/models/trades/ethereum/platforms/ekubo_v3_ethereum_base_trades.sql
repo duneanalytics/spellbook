17c19cd2ed7ab48159196b19b5bc3bae629ce20b
Jacob Sharples <jacobsharples@gmail.com>
2026-01-14 08:57:24 -0600

    update case when logic

--- dbt_subprojects/dex/models/trades/ethereum/platforms/ekubo_v3_ethereum_base_trades.sql
+++ dbt_subprojects/dex/models/trades/ethereum/platforms/ekubo_v3_ethereum_base_trades.sql
@@ -18,10 +18,10 @@ SELECT
     , cast(date_trunc( 'day', t.block_time) as date) AS block_date
     , t.block_time AS block_time
     , t.block_number AS block_number
-    , abs(case when t.amount0_raw < 0 then t.amount0_raw else t.amount1_raw end) as token_bought_amount_raw
-    , abs(case when t.amount0_raw > 0 then t.amount0_raw else t.amount1_raw end) as token_sold_amount_raw
-    , case when t.amount0_raw < 0 then token0 else token1 END as token_bought_address
-    , case when t.amount0_raw > 0 then token0 else token1 END as token_sold_address
+    , abs(case when t.amount0_raw <= 0 then t.amount0_raw else t.amount1_raw end) as token_bought_amount_raw
+    , abs(case when t.amount0_raw >= 0 then t.amount0_raw else t.amount1_raw end) as token_sold_amount_raw
+    , case when t.amount0_raw <= 0 then token0 else token1 END as token_bought_address
+    , case when t.amount0_raw >= 0 then token0 else token1 END as token_sold_address
     , t.tx_from AS taker
     , t.id AS maker
     , 0x00000000000014aA86C5d3c41765bb24e11bd701 as project_contract_address
