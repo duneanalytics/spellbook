cc43fdef789bd051927c51911d856954139e580c
alicecha <110170331+alicecha@users.noreply.github.com>
2026-01-14 18:05:28 +0100

    added 12 more chains

---
+++ dbt_subprojects/daily_spellbook/models/contracts/linea/contracts_linea_info.sql
@@ -0,0 +1,16 @@
+{{
+  config(
+        schema = 'contracts_linea',
+        alias = 'info',
+        materialized = 'incremental',
+        file_format = 'delta',
+        partition_by = ['creation_block_month'],
+        unique_key = ['blockchain', 'contract_address'],
+        incremental_strategy = 'merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.creation_block_time')]
+  )
+}}
+
+{{
+    contracts_info(blockchain='linea')
+}}
