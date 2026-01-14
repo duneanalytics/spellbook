10c25492f534f9648f5b2e08071ae87a5e4f76fc
Isaac Izard <loopyloopy811@gmail.com>
2026-01-14 14:56:31 +0100

    Added USDS in prices solana list

--- dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
+++ dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
@@ -791,5 +791,6 @@ FROM
         ('pstusdc-payfi-strategy-token-usdc','solana','PST','59obFNBzyTBGowrkif5uK7ojS58vsuWz3ZCvg6tfZAGw',6),
         ('wet-humidifi','solana','WET','WETZjtprkDMCcUxPi9PfWnowMRZkiGGHDb9rABuRZ2U',6),
         ('zec-omnibridge-bridged-zcash-solana','solana','ZEC','A7bdiYdS5GjqGFtxf17ppRHtDKPkkRqbKtR27dxvQXaS',8),
+        ('usds-usds','solana','USDS','USDSwr9ApdHk5bvJKMjzff41FfuX8bSxdKcR81vTwcA',6),
         ('elizaos-elizaos','solana','elizaOS','DuMbhu7mvQvqQHGcnikDgb4XegXJRyhUBfdU22uELiZA',9)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
