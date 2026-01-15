c0c18f950990ecfe7348936f21eb0028c7050a11
tiagoponciano <tiagoponciano03@gmail.com>
2026-01-15 13:47:20 -0300

    fix: update average volume threshold in liquidity macro from 750K to 1M for Ethereum filtering fix: add conditional filtering for protocol fee based on blockchain type in protocol fee macro

--- dbt_macros/shared/balancer/balancer_protocol_fee_macro.sql
+++ dbt_macros/shared/balancer/balancer_protocol_fee_macro.sql
@@ -155,6 +155,11 @@ WITH pool_labels AS (
         ON r.day = f.day
     LEFT JOIN pool_labels l
         ON BYTEARRAY_SUBSTRING(f.pool_id,1,20) = l.address
+    WHERE CASE 
+        WHEN '{{blockchain}}' = 'gnosis' 
+        THEN (f.protocol_fee_collected_usd > 0 AND f.protocol_fee_collected_usd < 100000)
+        ELSE TRUE 
+    END
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14
 
 {% endmacro %}
