dad6250a76142ee1843bf8687f224d24ed6f01fa
tomfutago <tomfutago@gmail.com>
2026-01-15 09:06:17 +0000

    fix: revert to solana specific col names convention, move & use inst key macro

--- dbt_subprojects/tokens/macros/stablecoins/stablecoins_svm_transfers.sql
+++ dbt_subprojects/tokens/macros/stablecoins/stablecoins_svm_transfers.sql
@@ -1,7 +1,7 @@
 {%- macro stablecoins_svm_transfers(
   blockchain,
   token_list,
-  start_date=none
+  start_date = none
 ) %}
 
 select
@@ -9,34 +9,30 @@ select
   cast(date_trunc('month', t.block_date) as date) as block_month,
   t.block_date,
   t.block_time,
-  t.block_slot as block_number,
-  t.tx_id as tx_hash,
-  t.outer_instruction_index as evt_index,
-  array[cast(coalesce(t.inner_instruction_index, 0) as bigint)] as trace_address,
-  t.token_version as token_standard,
-  t.token_mint_address as token_address,
+  t.block_slot,
+  t.tx_id,
+  t.tx_index,
+  t.outer_instruction_index,
+  coalesce(t.inner_instruction_index, 0) as inner_instruction_index,
+  t.token_version,
+  t.token_mint_address,
   t.symbol as token_symbol,
   t.amount as amount_raw,
   t.amount_display as amount,
   t.amount_usd,
-  t.from_owner as "from",
-  t.to_owner as "to",
-  (
-    cast(t.block_slot as decimal(38,0)) * cast(1e21 as decimal(38,0))
-    + cast(t.tx_index as decimal(38,0)) * cast(1e12 as decimal(38,0))
-    + cast(coalesce(t.outer_instruction_index, 0) as decimal(38,0)) * cast(1e6 as decimal(38,0))
-    + cast(coalesce(t.inner_instruction_index, 0) as decimal(38,0))
-  ) as unique_key
+  t.from_owner,
+  t.to_owner,
+  {{ solana_instruction_key('t.block_slot', 't.tx_index', 't.outer_instruction_index', 't.inner_instruction_index') }} as unique_key
 from {{ source('tokens_' ~ blockchain, 'transfers') }} t
-where t.action = 'transfer'
+where 1 = 1
+{% if start_date is not none %}
+  and t.block_date >= date '{{ start_date }}'
+{% endif %}
   and exists (
     select 1
     from {{ ref('tokens_' ~ blockchain ~ '_spl_stablecoins_' ~ token_list) }} s
     where s.token_mint_address = t.token_mint_address
   )
-{% if start_date is not none %}
-  and t.block_date >= date '{{ start_date }}'
-{% endif %}
 {% if is_incremental() %}
   and {{ incremental_predicate('t.block_date') }}
 {% endif %}
--- dbt_subprojects/tokens/models/stablecoins/svm/solana/transfers/stablecoins_solana_transfers.sql
+++ dbt_subprojects/tokens/models/stablecoins/svm/solana/transfers/stablecoins_solana_transfers.sql
@@ -19,49 +19,49 @@ select
   t.block_month,
   t.block_date,
   t.block_time,
-  t.block_number,
-  t.tx_hash,
-  t.evt_index,
-  t.trace_address,
-  t.token_standard,
-  t.token_address,
+  t.block_slot,
+  t.tx_id,
+  t.tx_index,
+  t.outer_instruction_index,
+  t.inner_instruction_index,
+  t.token_version,
+  t.token_mint_address,
   t.token_symbol,
   m.backing as token_backing,
   m.name as token_name,
   t.amount_raw,
   t.amount,
   t.amount_usd,
-  t."from",
-  t."to",
+  t.from_owner,
+  t.to_owner,
   t.unique_key
 from {{ ref('stablecoins_' ~ chain ~ '_core_transfers') }} t
 left join {{ ref('tokens_spl_stablecoins_metadata') }} m
   on t.blockchain = m.blockchain
-  and t.token_address = m.token_mint_address
-
+  and t.token_mint_address = m.token_mint_address
 union all
-
 select
   t.blockchain,
   t.block_month,
   t.block_date,
   t.block_time,
-  t.block_number,
-  t.tx_hash,
-  t.evt_index,
-  t.trace_address,
-  t.token_standard,
-  t.token_address,
+  t.block_slot,
+  t.tx_id,
+  t.tx_index,
+  t.outer_instruction_index,
+  t.inner_instruction_index,
+  t.token_version,
+  t.token_mint_address,
   t.token_symbol,
   m.backing as token_backing,
   m.name as token_name,
   t.amount_raw,
   t.amount,
   t.amount_usd,
-  t."from",
-  t."to",
+  t.from_owner,
+  t.to_owner,
   t.unique_key
 from {{ ref('stablecoins_' ~ chain ~ '_extended_transfers') }} t
 left join {{ ref('tokens_spl_stablecoins_metadata') }} m
   on t.blockchain = m.blockchain
-  and t.token_address = m.token_mint_address
+  and t.token_mint_address = m.token_mint_address
