e09ed52742717b59a32a4e59dd068984b65521c0
tomfutago <tomfutago@gmail.com>
2026-01-15 10:20:42 +0000

    chore: back to prod start dates

--- dbt_subprojects/tokens/models/stablecoins/svm/solana/transfers/stablecoins_solana_extended_transfers.sql
+++ dbt_subprojects/tokens/models/stablecoins/svm/solana/transfers/stablecoins_solana_extended_transfers.sql
@@ -7,7 +7,7 @@
     materialized = 'incremental',
     incremental_strategy = 'microbatch',
     event_time = 'block_date',
-    begin = '2026-01-13',
+    begin = '2026-01-01',
     batch_size = 'day',
     partition_by = ['block_month'],
     unique_key = ['block_month', 'block_date', 'unique_key'],
@@ -16,6 +16,7 @@
 }}
 
 -- extended transfers: tracks transfers for newly added stablecoins (not in core list)
+-- update begin date when adding new stablecoins
 
 {{ stablecoins_svm_transfers(
     blockchain = chain,
