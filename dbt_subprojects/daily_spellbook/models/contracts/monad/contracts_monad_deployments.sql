407943ea5d4f09fe996e770f08e9c4e7fdb9e4e3
alicecha <110170331+alicecha@users.noreply.github.com>
2026-01-15 10:35:05 +0100

    added unique key for incremental build

--- dbt_subprojects/daily_spellbook/models/contracts/monad/contracts_monad_deployments.sql
+++ dbt_subprojects/daily_spellbook/models/contracts/monad/contracts_monad_deployments.sql
@@ -6,6 +6,7 @@
         file_format = 'delta',
         partition_by = ['creation_block_month'],
         incremental_strategy = 'merge',
+        unique_key = ['blockchain', 'contract_address', 'creation_tx_hash'],
         incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.creation_block_time')]
   )
 }}
