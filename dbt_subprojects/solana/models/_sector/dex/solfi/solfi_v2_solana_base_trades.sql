6af751de4f262d6b85e51dfa39c11e30bb519806
eekey-cloud <eekeyguy.eth@gmail.com>
2026-01-26 14:39:01 +0530

    Create solfi_v2_solana_base_trades.sql

---
+++ dbt_subprojects/solana/models/_sector/dex/solfi/solfi_v2_solana_base_trades.sql
@@ -0,0 +1,114 @@
+{{
+  config(
+    schema = 'solfi_solana'
+    , alias = 'v2_base_trades'
+    , partition_by = ['block_month']
+    , materialized = 'incremental'
+    , file_format = 'delta'
+    , incremental_strategy = 'merge'
+    , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    , unique_key = ['block_month', 'surrogate_key']
+  )
+}}
+
+{% set project_start_date = '2025-08-07' %}
+
+-- solfi v2 swap data from staging table
+WITH swaps AS (
+    SELECT
+          block_slot
+        , block_date
+        , block_time
+        , inner_instruction_index
+        , outer_instruction_index
+        , outer_executing_account
+        , is_inner
+        , tx_id
+        , tx_signer
+        , tx_index
+        , pool_id
+    FROM {{ ref('solfi_v2_solana_stg_raw_swaps') }}
+    WHERE 1=1
+        {% if is_incremental() -%}
+        AND {{ incremental_predicate('block_time') }}
+        {% else -%}
+        AND block_time >= TIMESTAMP '{{ project_start_date }}'
+        {% endif -%}
+)
+
+-- Inner join on token_transfers initiated by amm swap instructions.
+, transfers AS (
+    SELECT
+        s.block_date
+      , s.block_time
+      , s.block_slot
+      , CASE
+          WHEN s.is_inner = false THEN 'direct'
+          ELSE s.outer_executing_account
+        END as trade_source
+      , t.amount as token_bought_amount_raw
+      , t1.amount as token_sold_amount_raw
+      , t.from_token_account as token_bought_vault
+      , t1.to_token_account as token_sold_vault
+      , t.token_mint_address as token_bought_mint_address
+      , t1.token_mint_address as token_sold_mint_address
+      , s.pool_id AS project_program_id
+      , s.tx_signer as trader_id
+      , s.tx_id
+      , s.outer_instruction_index
+      , s.inner_instruction_index
+      , s.tx_index
+    FROM swaps s
+    INNER JOIN {{ source('tokens_solana','transfers') }} t  ON t.tx_id = s.tx_id --buy
+        AND t.block_date = s.block_date
+        AND t.block_slot = s.block_slot
+        AND t.outer_instruction_index = s.outer_instruction_index
+        AND t.inner_instruction_index = s.inner_instruction_index + 2
+        AND (t.token_version = 'spl_token' or t.token_version = 'spl_token_2022')
+        {% if is_incremental() -%}
+        AND {{ incremental_predicate('t.block_time') }}
+        {% else -%}
+        AND t.block_time >= TIMESTAMP '{{ project_start_date }}'
+        {% endif -%}
+    INNER JOIN {{ source('tokens_solana','transfers') }} t1  ON t1.tx_id = s.tx_id --sell
+        AND t1.block_date = s.block_date
+        AND t1.block_slot = s.block_slot
+        AND t1.outer_instruction_index = s.outer_instruction_index
+        AND t1.inner_instruction_index = s.inner_instruction_index + 1
+        AND (t1.token_version = 'spl_token' or t1.token_version = 'spl_token_2022')
+        {% if is_incremental() -%}
+        AND {{ incremental_predicate('t1.block_time') }}
+        {% else -%}
+        AND t1.block_time >= TIMESTAMP '{{ project_start_date }}'
+        {% endif -%}
+
+)
+
+
+--add pertinent info in prep for union on solana_base_trades
+SELECT
+      'solana' as blockchain
+    , 'solfi' AS project
+    , 2 AS version
+    , 'v2' as version_name
+    , date_trunc('month',s.block_date) as block_month
+    , s.block_time
+    , s.block_slot
+    , s.block_date
+    , s.trade_source
+    , s.token_bought_amount_raw
+    , s.token_sold_amount_raw
+    , CAST(NULL AS DOUBLE) as fee_tier
+    , s.token_bought_mint_address
+    , s.token_sold_mint_address
+    , s.token_bought_vault
+    , s.token_sold_vault
+    , s.project_program_id
+    , 'SV2EYYJyRz2YhfXwXnhNAevDEui5Q6yrfyo13WtupPF' AS project_main_id
+    , s.trader_id
+    , s.tx_id
+    , s.outer_instruction_index
+    , s.inner_instruction_index
+    , s.tx_index
+    , {{ dbt_utils.generate_surrogate_key(['tx_id', 'tx_index', 'outer_instruction_index', 'inner_instruction_index']) }} as surrogate_key
+FROM transfers s
