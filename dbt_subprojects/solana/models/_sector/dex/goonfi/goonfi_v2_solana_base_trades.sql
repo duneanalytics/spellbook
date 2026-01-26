59f3b41470e9038f84d4c22d50fe2175d91350a2
eekey-cloud <eekeyguy.eth@gmail.com>
2026-01-26 21:34:35 +0530

    Update goonfi_v2_solana_base_trades.sql

--- dbt_subprojects/solana/models/_sector/dex/goonfi/goonfi_v2_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/goonfi/goonfi_v2_solana_base_trades.sql
@@ -36,79 +36,77 @@ WITH swaps AS (
         {% endif -%}
 )
 
--- Inner join on token_transfers initiated by amm swap instructions.
 , transfers AS (
     SELECT
-        s.block_date
-      , s.block_time
-      , s.block_slot
-      , CASE
-          WHEN s.is_inner = false THEN 'direct'
-          ELSE s.outer_executing_account
-        END as trade_source
-      , t.amount as token_bought_amount_raw
-      , t1.amount as token_sold_amount_raw
-      , t.from_token_account as token_bought_vault
-      , t1.to_token_account as token_sold_vault
-      , t.token_mint_address as token_bought_mint_address
-      , t1.token_mint_address as token_sold_mint_address
-      , s.pool_id AS project_program_id
-      , s.tx_signer as trader_id
-      , s.tx_id
-      , s.outer_instruction_index
-      , s.inner_instruction_index
-      , s.tx_index
+          s.block_date
+        , s.block_time
+        , s.block_slot
+        , CASE WHEN s.is_inner = false THEN 'direct' ELSE s.outer_executing_account END AS trade_source
+        , MAX(CASE WHEN tf.inner_instruction_index = s.inner_instruction_index + 2 THEN tf.amount END) AS token_bought_amount_raw
+        , MAX(CASE WHEN tf.inner_instruction_index = s.inner_instruction_index + 1 THEN tf.amount END) AS token_sold_amount_raw
+        , MAX(CASE WHEN tf.inner_instruction_index = s.inner_instruction_index + 2 THEN tf.from_token_account END) AS token_bought_vault
+        , MAX(CASE WHEN tf.inner_instruction_index = s.inner_instruction_index + 1 THEN tf.to_token_account END) AS token_sold_vault
+        , MAX(CASE WHEN tf.inner_instruction_index = s.inner_instruction_index + 2 THEN tf.token_mint_address END) AS token_bought_mint_address
+        , MAX(CASE WHEN tf.inner_instruction_index = s.inner_instruction_index + 1 THEN tf.token_mint_address END) AS token_sold_mint_address
+        , s.pool_id AS project_program_id
+        , s.tx_signer AS trader_id
+        , s.tx_id
+        , s.outer_instruction_index
+        , s.inner_instruction_index
+        , s.tx_index
+        , s.surrogate_key
     FROM swaps s
-    INNER JOIN {{ source('tokens_solana','transfers') }} t  ON t.tx_id = s.tx_id --buy
-        AND t.block_date = s.block_date
-        AND t.block_slot = s.block_slot
-        AND t.outer_instruction_index = s.outer_instruction_index
-        AND t.inner_instruction_index = s.inner_instruction_index + 2
-        AND (t.token_version = 'spl_token' or t.token_version = 'spl_token_2022')
+    INNER JOIN {{ source('tokens_solana', 'transfers') }} tf
+        ON  tf.tx_id = s.tx_id
+        AND tf.block_date = s.block_date
+        AND tf.block_slot = s.block_slot
+        AND tf.outer_instruction_index = s.outer_instruction_index
+        AND tf.inner_instruction_index IN (s.inner_instruction_index + 1, s.inner_instruction_index + 2)
+    WHERE tf.token_version IN ('spl_token', 'spl_token_2022')
         {% if is_incremental() -%}
-        AND {{ incremental_predicate('t.block_time') }}
+        AND {{ incremental_predicate('tf.block_date') }}
         {% else -%}
-        AND t.block_time >= TIMESTAMP '{{ project_start_date }}'
+        AND tf.block_date >= DATE '{{ project_start_date }}'
         {% endif -%}
-    INNER JOIN {{ source('tokens_solana','transfers') }} t1  ON t1.tx_id = s.tx_id --sell
-        AND t1.block_date = s.block_date
-        AND t1.block_slot = s.block_slot
-        AND t1.outer_instruction_index = s.outer_instruction_index
-        AND t1.inner_instruction_index = s.inner_instruction_index + 1
-        AND (t1.token_version = 'spl_token' or t1.token_version = 'spl_token_2022')
-        {% if is_incremental() -%}
-        AND {{ incremental_predicate('t1.block_time') }}
-        {% else -%}
-        AND t1.block_time >= TIMESTAMP '{{ project_start_date }}'
-        {% endif -%}
-
+    GROUP BY
+          s.block_date
+        , s.block_time
+        , s.block_slot
+        , CASE WHEN s.is_inner = false THEN 'direct' ELSE s.outer_executing_account END
+        , s.pool_id
+        , s.tx_signer
+        , s.tx_id
+        , s.outer_instruction_index
+        , s.inner_instruction_index
+        , s.tx_index
+        , s.surrogate_key
+    HAVING COUNT_IF(tf.inner_instruction_index = s.inner_instruction_index + 2) = 1
+       AND COUNT_IF(tf.inner_instruction_index = s.inner_instruction_index + 1) = 1
 )
 
-
---add pertinent info in prep for union on solana_base_trades
 SELECT
-      'solana' as blockchain
+      'solana' AS blockchain
     , 'goonfi' AS project
     , 2 AS version
-    , 'v2' as version_name
-    , date_trunc('month',s.block_date) as block_month
-    , s.block_time
-    , s.block_slot
-    , s.block_date
-    , s.trade_source
-    , s.token_bought_amount_raw
-    , s.token_sold_amount_raw
-    , CAST(NULL AS DOUBLE) as fee_tier
-    , s.token_bought_mint_address
-    , s.token_sold_mint_address
-    , s.token_bought_vault
-    , s.token_sold_vault
-    , s.project_program_id
+    , 'v2' AS version_name
+    , CAST(DATE_TRUNC('month', block_date) AS DATE) AS block_month
+    , block_time
+    , block_slot
+    , block_date
+    , trade_source
+    , token_bought_amount_raw
+    , token_sold_amount_raw
+    , CAST(NULL AS DOUBLE) AS fee_tier
+    , token_bought_mint_address
+    , token_sold_mint_address
+    , token_bought_vault
+    , token_sold_vault
+    , project_program_id
     , 'goonuddtQRrWqqn5nFyczVKaie28f3kDkHWkHtURSLE' AS project_main_id
-    , s.trader_id
-    , s.tx_id
-    , s.outer_instruction_index
-    , s.inner_instruction_index
-    , s.tx_index
-    , {{ dbt_utils.generate_surrogate_key(['tx_id', 'tx_index', 'outer_instruction_index', 'inner_instruction_index']) }} as surrogate_key
-FROM transfers s
+    , trader_id
+    , tx_id
+    , outer_instruction_index
+    , inner_instruction_index
+    , tx_index
+    , surrogate_key
+FROM transfers
