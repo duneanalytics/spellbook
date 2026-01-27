ab2e28401530c2e1306c48cb44bb5a92b91127ab
eekey-cloud <eekeyguy.eth@gmail.com>
2026-01-27 11:55:51 +0530

    Update dex_solana_base_trades.sql

--- dbt_subprojects/solana/models/_sector/dex/dex_solana_base_trades.sql
+++ dbt_subprojects/solana/models/_sector/dex/dex_solana_base_trades.sql
@@ -41,6 +41,7 @@
       , ref('bisonfi_solana_base_trades')
       , ref('goonfi_v2_solana_base_trades')
       , ref('solfi_v2_solana_base_trades')
+      , ref('whalestreet_solana_base_trades')
       ]
 %}
 
