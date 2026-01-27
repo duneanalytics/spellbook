70b4751596e2714f86f6f7d2a1bbc3c3fc648733
eekey-cloud <eekeyguy.eth@gmail.com>
2026-01-27 11:32:38 +0530

    Update whalestreet_solana_stg_raw_swaps.sql

--- dbt_subprojects/solana/models/_sector/dex/whalestreet/whalestreet_solana_stg_raw_swaps.sql
+++ dbt_subprojects/solana/models/_sector/dex/whalestreet/whalestreet_solana_stg_raw_swaps.sql
@@ -29,7 +29,7 @@ WITH swaps AS (
     , tx_id
     , tx_signer
     , tx_index
-    , CAST(NULL as VARCHAR) AS pool_id -- AlphaQ does not use a pool system like other AMM's
+    , CAST(NULL as VARCHAR) AS pool_id -- Whale Street does not use a pool system like other AMM's
     , {{ solana_instruction_key(
           'block_slot'
         , 'tx_index'
