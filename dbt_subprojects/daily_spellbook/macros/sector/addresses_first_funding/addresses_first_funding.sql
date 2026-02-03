41eed353ff29aed1e7c10a2f7ea69eb0a57ec068
0xRob <83790096+0xRobin@users.noreply.github.com>
2026-02-03 11:54:34 +0100

    Update addresses_first_funding.sql (#9270)

--- dbt_subprojects/daily_spellbook/macros/sector/addresses_first_funding/addresses_first_funding.sql
+++ dbt_subprojects/daily_spellbook/macros/sector/addresses_first_funding/addresses_first_funding.sql
@@ -7,10 +7,6 @@ SELECT
     , i.first_funded_by_block_time AS first_funded_at
     , CASE WHEN i.is_smart_contract THEN true ELSE false END AS is_smart_contract
     , CASE WHEN i.is_smart_contract THEN false ELSE true END AS is_eoa
-    , now() AS _updated_at
 FROM {{ ref('addresses_' + blockchain + '_info') }} i
-{% if is_incremental() %}
-WHERE {{ incremental_predicate('i.last_seen') }}
-{% endif %}
 
 {% endmacro %}
