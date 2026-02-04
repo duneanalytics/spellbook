56d204b3ead575d672020307e3d807884c634bb2
tomfutago <35136350+tomfutago@users.noreply.github.com>
2026-02-04 08:40:24 +0000

    fix: align start_date with min(block_date) from stablecoins_transfers (#9272)

--- dbt_subprojects/daily_spellbook/models/stablecoins/evm/ethereum/balances/stablecoins_ethereum_core_balances.sql
+++ dbt_subprojects/daily_spellbook/models/stablecoins/evm/ethereum/balances/stablecoins_ethereum_core_balances.sql
@@ -17,5 +17,5 @@
 
 {{ stablecoins_balances_from_transfers(
     transfers = source('stablecoins_' ~ chain, 'core_transfers'),
-    start_date = '2023-01-01'
+    start_date = '2017-11-28'
 ) }}
--- dbt_subprojects/daily_spellbook/models/stablecoins/evm/flare/balances/stablecoins_flare_core_balances.sql
+++ dbt_subprojects/daily_spellbook/models/stablecoins/evm/flare/balances/stablecoins_flare_core_balances.sql
@@ -17,5 +17,5 @@
 
 {{ stablecoins_balances_from_transfers(
     transfers = source('stablecoins_' ~ chain, 'core_transfers'),
-    start_date = '2023-07-14'
+    start_date = '2024-07-16'
 ) }}
