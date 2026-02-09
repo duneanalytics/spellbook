7bfa7e5ee9802b7493db1e0f18dbf7b193d8dd46
gustavobftorres <gutobevtorres@gmail.com>
2026-02-09 16:06:05 -0300

    add support for avalanche on balancer_v3 spells

---
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/balances/avalanche_c/balancer_v3_avalanche_c_token_balance_changes_daily.sql
@@ -0,0 +1,19 @@
+{% set blockchain = 'avalanche_c' %}
+
+{{
+    config(
+        schema = 'balancer_v3_avalanche_c',
+        alias = 'token_balance_changes_daily', 
+        materialized = 'table',
+        file_format = 'delta'
+    )
+}}
+
+{{ 
+    balancer_v2_compatible_token_balance_changes_daily_agg_macro(
+        blockchain = blockchain,
+        version = '3',
+        project_decoded_as = 'balancer_v3',
+        base_spells_namespace = 'balancer'
+    )
+}}
---
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/bpt/avalanche_c/balancer_v3_avalanche_c_bpt_supply_changes.sql
@@ -0,0 +1,23 @@
+{% set blockchain = 'avalanche_c' %}
+
+{{
+    config(
+        schema = 'balancer_v3_avalanche_c',
+        alias = 'bpt_supply_changes', 
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['block_date', 'evt_tx_hash', 'evt_index', 'label'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.evt_block_time')]
+    )
+}}
+
+{{ 
+    balancer_v2_compatible_bpt_supply_changes_macro(
+        blockchain = blockchain,
+        version = '3',
+        project_decoded_as = 'balancer_v3',
+        base_spells_namespace = 'balancer',
+        pool_labels_model = 'balancer_v3_pools_avalanche_c'
+    )
+}}
