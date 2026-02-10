ab8cb015ce21e3ea5d353d207cc18cbe2c4fea45
tomfutago <tomfutago@gmail.com>
2026-02-10 17:02:32 +0000

    chore: more cleaning up

--- dbt_subprojects/tokens/models/stablecoins/tron/transfers/stablecoins_tron_transfers.sql
+++ dbt_subprojects/tokens/models/stablecoins/tron/transfers/stablecoins_tron_transfers.sql
@@ -12,75 +12,65 @@
 -- union of core and extended transfers; columns = EVM macro + tron extras + metadata
 
 select
-  t.blockchain
-  , t.block_month
-  , t.block_date
-  , t.block_time
-  , t.block_number
-  , t.tx_hash
-  , t.evt_index
-  , t.trace_address
-  , t.token_standard
-  , t.token_address
-  , t.token_symbol
-  , m.backing as token_backing
-  , m.name as token_name
-  , t.amount_raw
-  , t.amount
-  , t.price_usd
-  , t.amount_usd
-  , t."from"
-  , t.to
-  , t.unique_key
-  , t.tx_from
-  , t.tx_to
-  , t.tx_index
-  , t.contract_address
-  , t.tx_hash_varchar
-  , t.contract_address_varchar
-  , t.from_varchar
-  , t.to_varchar
-  , t.tx_from_varchar
-  , t.tx_to_varchar
+  t.blockchain,
+  t.block_month,
+  t.block_date,
+  t.block_time,
+  t.block_number,
+  t.tx_hash,
+  t.evt_index,
+  t.trace_address,
+  t.token_standard,
+  t.token_address,
+  t.token_symbol,
+  t.amount_raw,
+  t.amount,
+  t.price_usd,
+  t.amount_usd,
+  t."from",
+  t."to",
+  t.unique_key,
+  t.tx_from,
+  t.tx_to,
+  t.tx_index,
+  t.contract_address,
+  t.tx_hash_varchar,
+  t.contract_address_varchar,
+  t.from_varchar,
+  t.to_varchar,
+  t.tx_from_varchar,
+  t.tx_to_varchar
 from {{ ref('stablecoins_' ~ chain ~ '_core_transfers') }} t
-left join {{ ref('tokens_erc20_stablecoins_metadata') }} m
-  on t.blockchain = m.blockchain
-  and t.token_address = m.contract_address
 
 union all
 
 select
-  t.blockchain
-  , t.block_month
-  , t.block_date
-  , t.block_time
-  , t.block_number
-  , t.tx_hash
-  , t.evt_index
-  , t.trace_address
-  , t.token_standard
-  , t.token_address
-  , t.token_symbol
-  , m.backing as token_backing
-  , m.name as token_name
-  , t.amount_raw
-  , t.amount
-  , t.price_usd
-  , t.amount_usd
-  , t."from"
-  , t.to
-  , t.unique_key
-  , t.tx_from
-  , t.tx_to
-  , t.tx_index
-  , t.contract_address
-  , t.tx_hash_varchar
-  , t.contract_address_varchar
-  , t.from_varchar
-  , t.to_varchar
-  , t.tx_from_varchar
-  , t.tx_to_varchar
+  t.blockchain,
+  t.block_month,
+  t.block_date,
+  t.block_time,
+  t.block_number,
+  t.tx_hash,
+  t.evt_index,
+  t.trace_address,
+  t.token_standard,
+  t.token_address,
+  t.token_symbol,
+  t.amount_raw,
+  t.amount,
+  t.price_usd,
+  t.amount_usd,
+  t."from",
+  t."to",
+  t.unique_key,
+  t.tx_from,
+  t.tx_to,
+  t.tx_index,
+  t.contract_address,
+  t.tx_hash_varchar,
+  t.contract_address_varchar,
+  t.from_varchar,
+  t.to_varchar,
+  t.tx_from_varchar,
+  t.tx_to_varchar
 from {{ ref('stablecoins_' ~ chain ~ '_extended_transfers') }} t
-left join {{ ref('tokens_erc20_stablecoins_metadata') }} m
-  on t.blockchain = m.blockchain
-  and t.token_address = m.contract_address
