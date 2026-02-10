7cec37336a177eb984f4ba3205a712a1211362b6
gustavobftorres <gutobevtorres@gmail.com>
2026-02-10 10:11:06 -0300

    Stub added for BPT supply

--- dbt_subprojects/hourly_spellbook/models/_project/balancer/bpt/avalanche_c/balancer_v3_avalanche_c_bpt_supply.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/bpt/avalanche_c/balancer_v3_avalanche_c_bpt_supply.sql
@@ -10,12 +10,19 @@
     )
 }}
 
-{{ 
-    balancer_v3_compatible_bpt_supply_macro(
-        blockchain = blockchain,
-        version = '3',        
-        project_decoded_as = 'balancer_v3',
-        pool_labels_model = 'balancer_v3_pools_avalanche_c',
-        transfers_spell = ref('balancer_v3_avalanche_c_transfers_bpt')
-    )
-}}
+-- Stub implementation for Avalanche C v3 BPT supply.
+-- This model is kept to satisfy dependencies and will be
+-- replaced once labels.balancer_v3_pools_avalanche_c exists
+-- and the v3 BPT logic is fully wired for Avalanche.
+WITH stub AS (
+    SELECT
+        CAST(NULL AS DATE)      AS day,
+        CAST(NULL AS VARCHAR)   AS pool_type,
+        '3'                     AS version,
+        'avalanche_c'           AS blockchain,
+        CAST(NULL AS VARBINARY) AS token_address,
+        CAST(NULL AS DOUBLE)    AS supply
+    WHERE 1 = 0
+)
+
+SELECT * FROM stub
