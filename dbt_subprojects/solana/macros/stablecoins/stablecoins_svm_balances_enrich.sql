4b596ffe5b772cba7bd5f1c634e43ca77db9bfaa
0xBoxer <71930326+0xBoxer@users.noreply.github.com>
2026-02-12 16:04:04 +0100

    feat: apply FX rate pricing to Solana stablecoins pipeline (#9307)

--- dbt_subprojects/solana/macros/stablecoins/stablecoins_svm_balances_enrich.sql
+++ dbt_subprojects/solana/macros/stablecoins/stablecoins_svm_balances_enrich.sql
@@ -7,22 +7,26 @@ select
   b.blockchain,
   b.day,
   b.address,
-  coalesce(f.symbol, p.symbol) as token_symbol,
+  f.symbol as token_symbol,
   b.token_mint_address as token_address,
   'spl_token' as token_standard,
   cast(null as uint256) as token_id,
+  s.currency,
   b.balance_raw,
-  cast(b.balance_raw as double) / power(10, coalesce(f.decimals, p.decimals)) as balance,
-  cast(b.balance_raw as double) / power(10, coalesce(f.decimals, p.decimals)) * p.price as balance_usd,
+  cast(b.balance_raw as double) / power(10, f.decimals) as balance,
+  cast(b.balance_raw as double) / power(10, f.decimals) * fx.exchange_rate as balance_usd,
   b.last_updated
 from {{ base_balances }} b
-left join {{ source('tokens_solana', 'fungible') }} f on b.token_mint_address = f.token_mint_address
-left join {{ source('prices_external', 'day') }} p
-  on cast(b.day as timestamp) = p.timestamp
-  and from_base58(b.token_mint_address) = p.contract_address
-  and p.blockchain = '{{ blockchain }}'
+inner join {{ ref('tokens_' ~ blockchain ~ '_spl_stablecoins') }} s
+  on s.token_mint_address = b.token_mint_address
+left join {{ source('tokens_solana', 'fungible') }} f
+  on b.token_mint_address = f.token_mint_address
+left join {{ source('prices', 'fx_exchange_rates') }} fx
+  on fx.base_currency = s.currency
+  and fx.target_currency = 'USD'
+  and fx.date = b.day
   {% if is_incremental() %}
-  and {{ incremental_predicate('p.timestamp') }}
+  and {{ incremental_predicate('fx.date') }}
   {% endif %}
 {% if is_incremental() %}
 where {{ incremental_predicate('b.day') }}
