db3e901af406df4aa32b2b4d1a4ebb297a80ba40
jeff-dude <jeff@dune.xyz>
2026-02-16 20:02:44 -0500

    Revert "temp: ci short run time"
    This reverts commit 98695982d949e48add74bdd53198f7896b1d48f3.

--- dbt_subprojects/hourly_spellbook/models/_project/lido/accounting/ethereum/lido_accounting_ethereum_other_income.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/lido/accounting/ethereum/lido_accounting_ethereum_other_income.sql
@@ -8,7 +8,7 @@
 	incremental_predicates=[incremental_predicate('DBT_INTERNAL_DEST.period')],
 ) }}
 
-{% set project_start_date = '2026-01-01' %}
+{% set project_start_date = '2020-12-17' %}
 
 with tokens as (
 	select
