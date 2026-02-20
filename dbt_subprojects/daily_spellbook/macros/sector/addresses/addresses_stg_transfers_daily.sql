3aa7e4da12292586729f98ece7145ea908882b25
jeff-dude <jeff@dune.xyz>
2026-02-20 13:05:51 -0500

    break out transfers

--- dbt_subprojects/daily_spellbook/macros/sector/addresses/addresses_stg_transfers_daily.sql
+++ dbt_subprojects/daily_spellbook/macros/sector/addresses/addresses_stg_transfers_daily.sql
@@ -1,5 +1,69 @@
-{% macro addresses_stg_transfers_daily(token_transfers) %}
-{# Incremental by block_date: rolling window, full days only (exclude current day). Grain: address, address_prefix, block_date. Partition: block_month. #}
+{% macro addresses_stg_transfers_daily_received(token_transfers) %}
+{# Single read from transfers: received side only. Grain: address, address_prefix, block_date. Same incremental/filter as daily. #}
+select
+	tt."to" as address
+	, varbinary_to_integer(varbinary_substring(tt."to", 1, 1)) as address_prefix
+	, tt.block_date
+	, cast(date_trunc('month', tt.block_date) as date) as block_month
+	, count(*) as tokens_received_count
+	, count(distinct tt.tx_hash) as tokens_received_tx_count
+	, 0 as tokens_sent_count
+	, 0 as tokens_sent_tx_count
+	, min(tt.block_time) as first_transfer_block_time
+	, max(tt.block_time) as last_transfer_block_time
+	, cast(null as bigint) as first_received_block_number
+	, cast(null as bigint) as last_received_block_number
+	, min(tt.block_number) as first_sent_block_number
+	, max(tt.block_number) as last_sent_block_number
+	, sum(tt.amount_usd) as received_volume_usd
+	, 0 as sent_volume_usd
+from
+	{{ token_transfers }} as tt
+where tt.block_date < cast(now() as date)
+{% if is_incremental() -%}
+and {{ incremental_predicate('tt.block_date') }}
+{% endif -%}
+group by
+	tt."to"
+	, 2
+	, tt.block_date
+{% endmacro %}
+
+
+{% macro addresses_stg_transfers_daily_sent(token_transfers) %}
+{# Single read from transfers: sent side only. Grain: address, address_prefix, block_date. Same incremental/filter as daily. #}
+select
+	tt."from" as address
+	, varbinary_to_integer(varbinary_substring(tt."from", 1, 1)) as address_prefix
+	, tt.block_date
+	, cast(date_trunc('month', tt.block_date) as date) as block_month
+	, 0 as tokens_received_count
+	, 0 as tokens_received_tx_count
+	, count(*) as tokens_sent_count
+	, count(distinct tt.tx_hash) as tokens_sent_tx_count
+	, min(tt.block_time) as first_transfer_block_time
+	, max(tt.block_time) as last_transfer_block_time
+	, min(tt.block_number) as first_received_block_number
+	, max(tt.block_number) as last_received_block_number
+	, cast(null as bigint) as first_sent_block_number
+	, cast(null as bigint) as last_sent_block_number
+	, 0 as received_volume_usd
+	, sum(tt.amount_usd) as sent_volume_usd
+from
+	{{ token_transfers }} as tt
+where tt.block_date < cast(now() as date)
+{% if is_incremental() -%}
+and {{ incremental_predicate('tt.block_date') }}
+{% endif -%}
+group by
+	tt."from"
+	, 2
+	, tt.block_date
+{% endmacro %}
+
+
+{% macro addresses_stg_transfers_daily_union(received_model, sent_model) %}
+{# Union received + sent daily staging and aggregate to same grain. No read from transfers. Incremental: filter by block_date when merging. #}
 select
 	address
 	, address_prefix
@@ -19,61 +83,51 @@ select
 	, sum(sent_volume_usd) as sent_volume_usd
 from (
 	select
-		tt."from" as address
-		, varbinary_to_integer(varbinary_substring(tt."from", 1, 1)) as address_prefix
-		, cast(tt.block_time as date) as block_date
-		, 0 as tokens_received_count
-		, 0 as tokens_received_tx_count
-		, count(*) as tokens_sent_count
-		, count(distinct tt.tx_hash) as tokens_sent_tx_count
-		, min(tt.block_time) as first_transfer_block_time
-		, max(tt.block_time) as last_transfer_block_time
-		, min(tt.block_number) as first_received_block_number
-		, max(tt.block_number) as last_received_block_number
-		, cast(null as bigint) as first_sent_block_number
-		, cast(null as bigint) as last_sent_block_number
-		, 0 as received_volume_usd
-		, sum(tt.amount_usd) as sent_volume_usd
+		r.address
+		, r.address_prefix
+		, r.block_date
+		, r.block_month
+		, r.tokens_received_count
+		, r.tokens_received_tx_count
+		, r.tokens_sent_count
+		, r.tokens_sent_tx_count
+		, r.first_transfer_block_time
+		, r.last_transfer_block_time
+		, r.first_received_block_number
+		, r.last_received_block_number
+		, r.first_sent_block_number
+		, r.last_sent_block_number
+		, r.received_volume_usd
+		, r.sent_volume_usd
 	from
-		{{ token_transfers }} as tt
-	where cast(tt.block_time as date) < cast(now() as date)
+		{{ received_model }} as r
 	{% if is_incremental() -%}
-	and {{ incremental_predicate('cast(tt.block_time as date)') }}
+	where {{ incremental_predicate('r.block_date') }}
 	{% endif -%}
-	group by
-		tt."from"
-		, 2
-		, cast(tt.block_time as date)
-
 	union all
-
 	select
-		tt."to" as address
-		, varbinary_to_integer(varbinary_substring(tt."to", 1, 1)) as address_prefix
-		, cast(tt.block_time as date) as block_date
-		, count(*) as tokens_received_count
-		, count(distinct tt.tx_hash) as tokens_received_tx_count
-		, 0 as tokens_sent_count
-		, 0 as tokens_sent_tx_count
-		, min(tt.block_time) as first_transfer_block_time
-		, max(tt.block_time) as last_transfer_block_time
-		, cast(null as bigint) as first_received_block_number
-		, cast(null as bigint) as last_received_block_number
-		, min(tt.block_number) as first_sent_block_number
-		, max(tt.block_number) as last_sent_block_number
-		, sum(tt.amount_usd) as received_volume_usd
-		, 0 as sent_volume_usd
+		s.address
+		, s.address_prefix
+		, s.block_date
+		, s.block_month
+		, s.tokens_received_count
+		, s.tokens_received_tx_count
+		, s.tokens_sent_count
+		, s.tokens_sent_tx_count
+		, s.first_transfer_block_time
+		, s.last_transfer_block_time
+		, s.first_received_block_number
+		, s.last_received_block_number
+		, s.first_sent_block_number
+		, s.last_sent_block_number
+		, s.received_volume_usd
+		, s.sent_volume_usd
 	from
-		{{ token_transfers }} as tt
-	where cast(tt.block_time as date) < cast(now() as date)
+		{{ sent_model }} as s
 	{% if is_incremental() -%}
-	and {{ incremental_predicate('cast(tt.block_time as date)') }}
+	where {{ incremental_predicate('s.block_date') }}
 	{% endif -%}
-	group by
-		tt."to"
-		, 2
-		, cast(tt.block_time as date)
-	)
+)
 group by
 	1
 	, 2
