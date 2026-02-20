6b6d7c10fd210ce4acde3b073e6f9f843995426b
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2026-02-19 21:12:54 -0500

    Further enhance addresses info (#9340)
    * further break out staging to be more efficient
    
    * temp: force short run time
    
    * ensure incremental universally
    
    * temp: short run time
    
    * fix bugs
    
    * refactor to help run and keep data quality
    
    * temp: short run time
    
    * restore claude md file
    
    * Revert "temp: short run time"
    
    This reverts commit 6b25ad63820c0ef3ca0948aee6632f5b34fe9476.

---
+++ dbt_subprojects/daily_spellbook/models/_sector/addresses/chains/avalanche_c/addresses_avalanche_c_stg_executed_txs_daily.sql
@@ -0,0 +1,20 @@
+{% set blockchain = 'avalanche_c' %}
+
+{{
+	config(
+		schema = 'addresses_' + blockchain,
+		alias = 'stg_executed_txs_daily',
+		materialized = 'incremental',
+		file_format = 'delta',
+		incremental_strategy = 'merge',
+		partition_by = ['block_month'],
+		unique_key = ['address_prefix', 'address', 'block_date'],
+		incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')],
+	)
+}}
+
+{{
+	addresses_stg_executed_txs_daily(
+		transactions = source(blockchain, 'transactions'),
+	)
+}}
