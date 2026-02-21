d025c83925120b984020ba14ff1d7d2d3bab021d
jeff-dude <jeff@dune.xyz>
2026-02-21 12:26:51 -0500

    temp: fix bug in megaeth source data for clashing tx_hash values

--- dbt_subprojects/hourly_spellbook/macros/sector/gas/op_stack_gas_fees.sql
+++ dbt_subprojects/hourly_spellbook/macros/sector/gas/op_stack_gas_fees.sql
@@ -7,6 +7,9 @@ WITH native_token_prices as (
         txns.block_time
         ,txns.block_number
         ,txns.hash AS tx_hash
+        {% if blockchain == 'megaeth' -%}
+        ,txns."index" AS tx_index
+        {%- endif %}
         ,txns."from" AS tx_from
         ,txns.to AS tx_to
         ,cast(gas_price as uint256) as gas_price
@@ -65,6 +68,9 @@ SELECT
     ,b.block_time
     ,b.block_number
     ,b.tx_hash
+    {% if blockchain == 'megaeth' -%}
+    ,b.tx_index
+    {%- endif %}
     ,b.tx_from
     ,b.tx_to
     ,b.gas_price
