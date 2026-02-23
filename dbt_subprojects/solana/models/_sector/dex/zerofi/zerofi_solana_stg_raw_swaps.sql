750c6b78a3cb15a431ddbba8bcfe3c5c97177f93
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2026-02-22 19:59:09 -0500

    Revert "feat(solana): AMM macro refactor + 8 new DEX integrations (AlphaQ, BisonFi, GoonFi v2, SolFi v2, WhaleStreet, Manifest, Byreal, Scorch) (#9303)" (#9346)
    This reverts commit 3c340a63d2323d2826a23b7b40a85213c4d89384.

--- dbt_subprojects/solana/models/_sector/dex/zerofi/zerofi_solana_stg_raw_swaps.sql
+++ dbt_subprojects/solana/models/_sector/dex/zerofi/zerofi_solana_stg_raw_swaps.sql
@@ -11,9 +11,45 @@
   )
 }}
 
-{{ solana_amm_stg_raw_swaps(
-    program_id = 'ZERor4xhbUycZ6gb9ntrhqscUcZmAbQDjEAtCf4hbZY'
-    , discriminator_filter = "BYTEARRAY_SUBSTRING(data, 1, 1) = 0x06"
-    , project_start_date = '2024-12-12'
-    , pool_id_expression = "account_arguments[1]"
-) }}
+{% set project_start_date = '2024-12-12' %}
+
+-- zerofi swap data from instruction_calls table
+WITH swaps AS (
+  SELECT
+    block_slot
+    , cast(date_trunc('month', block_date) AS DATE) AS block_month
+    , block_date
+    , block_time
+    , COALESCE(inner_instruction_index,0) as inner_instruction_index -- adjust to index 0 for non aggregated trades
+    , outer_instruction_index
+    , inner_executing_account
+    , outer_executing_account
+    , executing_account
+    , is_inner
+    , tx_id
+    , tx_signer
+    , tx_index
+    , account_arguments[8] AS transfer_authority
+    , account_arguments[1] AS pool_id
+    , account_arguments[3] AS token_a_vault
+    , account_arguments[5] as token_b_vaul
+    , {{ solana_instruction_key(
+          'block_slot'
+        , 'tx_index'
+        , 'outer_instruction_index'
+        , 'inner_instruction_index'
+      ) }} as surrogate_key
+  FROM {{ source('solana','instruction_calls') }}
+  WHERE
+    1=1
+    AND executing_account = 'ZERor4xhbUycZ6gb9ntrhqscUcZmAbQDjEAtCf4hbZY'
+    AND BYTEARRAY_SUBSTRING(data, 1, 1) = 0x06 --ZeroFi Swap discriminator. See: https://dune.com/queries/5857401
+    AND tx_success = true
+    {% if is_incremental() -%}
+    AND {{ incremental_predicate('block_date') }}
+    {% else -%}
+    AND block_date >= DATE '{{ project_start_date }}'
+    {% endif -%}
+)
+select *
+from swaps
