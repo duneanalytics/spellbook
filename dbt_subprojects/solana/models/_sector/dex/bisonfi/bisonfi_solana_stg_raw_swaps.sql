078c7138719093e6e6a294d14fd3df84270b898a
jeff-dude <jeff@dune.xyz>
2026-02-23 07:15:26 -0500

    Reapply "feat(solana): AMM macro refactor + 8 new DEX integrations (AlphaQ, BisonFi, GoonFi v2, SolFi v2, WhaleStreet, Manifest, Byreal, Scorch) (#9303)" (#9346)
    This reverts commit 750c6b78a3cb15a431ddbba8bcfe3c5c97177f93.

--- dbt_subprojects/solana/models/_sector/dex/bisonfi/bisonfi_solana_stg_raw_swaps.sql
+++ dbt_subprojects/solana/models/_sector/dex/bisonfi/bisonfi_solana_stg_raw_swaps.sql
@@ -1,6 +1,6 @@
 {{
   config(
-    schema = 'zerofi_solana'
+    schema = 'bisonfi_solana'
     , alias = 'stg_raw_swaps'
     , partition_by = ['block_month']
     , materialized = 'incremental'
@@ -12,8 +12,8 @@
 }}
 
 {{ solana_amm_stg_raw_swaps(
-    program_id = 'ZERor4xhbUycZ6gb9ntrhqscUcZmAbQDjEAtCf4hbZY'
-    , discriminator_filter = "BYTEARRAY_SUBSTRING(data, 1, 1) = 0x06"
-    , project_start_date = '2024-12-12'
-    , pool_id_expression = "account_arguments[1]"
+    program_id = 'BiSoNHVpsVZW2F7rx2eQ59yQwKxzU5NvBcmKshCSUypi'
+    , discriminator_filter = "(BYTEARRAY_SUBSTRING(data, 1, 1) IN (0x02, 0x07)) AND cardinality(account_arguments) = 9"
+    , project_start_date = '2025-11-05'
+    , pool_id_expression = "account_arguments[2]"
 ) }}
