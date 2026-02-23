b073bbfd6978af2d0d2b9c57669e81341da13d63
Desant pivo <pivasdesant@gmail.com>
2026-02-23 15:55:11 +0100

    Update prices_solana_tokens.sql

--- dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
+++ dbt_subprojects/tokens/models/prices/solana/prices_solana_tokens.sql
@@ -782,5 +782,6 @@ FROM
         ('2z-doublezero', 'solana', '2Z', 'J6pQQ3FAcJQeWPPGppWRb4nM8jU3wLyYbRrLh7feMfvd', 8),
         ('useless-useless-coin', 'solana', 'USELESS', 'Dz9mQ9NzkBcCsuGPFJ3r1bS4wgqKMHBPiVuniW8Mbonk', 6),
         ('onyc-onyc', 'solana', 'ONyc', '5Y8NV33Vv7WbnLfq3zBcKSdYPrk7g2KoiQoe7M2tcxp5', 9),
-        ('yzy-yzy', 'solana', 'YZY', 'DrZ26cKJDksVRWib3DVVsjo9eeXccc7hKhDJviiYEEZY', 6)
+        ('yzy-yzy', 'solana', 'YZY', 'DrZ26cKJDksVRWib3DVVsjo9eeXccc7hKhDJviiYEEZY', 6),
+        ('chsb-swissborg', 'solana', 'BORG', '3dQTr7ror2QPKQ3GbBCokJUmjErGg8kTJzdnYjNfvi3Z', 9)
 ) as temp (token_id, blockchain, symbol, contract_address, decimals)
