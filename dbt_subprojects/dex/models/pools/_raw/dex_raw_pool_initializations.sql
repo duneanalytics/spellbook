0173b5ac44502378bb8788747e088317b64063b2
sashass1315 <sashass1315@gmail.com>
2026-02-25 22:32:02 +0200

    fix(dex): remove redundant wrapper in raw pool initializations

--- dbt_subprojects/dex/models/pools/_raw/dex_raw_pool_initializations.sql
+++ dbt_subprojects/dex/models/pools/_raw/dex_raw_pool_initializations.sql
@@ -10,23 +10,21 @@
 
 
 
-select * from (
-    -- only uni v2. re-initialization is restricted on v3
-    select
-        blockchain
-        , 'uniswap_compatible' as type
-        , 'v2' as version
-        , block_time
-        , "to" as pool
-        , substr(input, 17, 20) token0
-        , substr(input, 49, 20) token1
-        , tx_hash
-        , trace_address call_trace_address
-    from {{ ref('dex_raw_pool_pre_materialized_traces') }}
-    where 
-        substr(input, 1, 4) = 0x485cc955 
-        {% if is_incremental() %}
-            and {{ incremental_predicate('block_time') }}
-        {% endif %}
-)
+-- only uni v2. re-initialization is restricted on v3
+select
+    blockchain
+    , 'uniswap_compatible' as type
+    , 'v2' as version
+    , block_time
+    , "to" as pool
+    , substr(input, 17, 20) token0
+    , substr(input, 49, 20) token1
+    , tx_hash
+    , trace_address call_trace_address
+from {{ ref('dex_raw_pool_pre_materialized_traces') }}
+where 
+    substr(input, 1, 4) = 0x485cc955 
+    {% if is_incremental() %}
+        and {{ incremental_predicate('block_time') }}
+    {% endif %}
 
