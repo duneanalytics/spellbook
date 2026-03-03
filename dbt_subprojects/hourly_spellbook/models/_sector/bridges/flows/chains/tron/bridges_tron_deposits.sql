38c1f6cb83e5ee36eb8b8ebdf567ca4992336dfc
tomfutago <tomfutago@gmail.com>
2026-03-03 15:39:47 +0000

    fix: clean up

--- dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/tron/bridges_tron_deposits.sql
+++ dbt_subprojects/hourly_spellbook/models/_sector/bridges/flows/chains/tron/bridges_tron_deposits.sql
@@ -8,8 +8,8 @@
     file_format = 'delta',
     partition_by = ['block_month'],
     incremental_strategy = 'merge',
-    unique_key = ['tx_hash', 'evt_index', 'bridge_transfer_id'],
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    unique_key = ['tx_hash', 'evt_index', 'bridge_transfer_id', 'block_month'],
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
   )
 }}
 
