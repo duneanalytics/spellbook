6e3b5341c3808d1dfe0729f75fa025396a0143d1
data-cowwboy <nitish@cow.fi>
2026-03-03 13:05:03 +0530

    Update dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/ink/cow_protocol_ink_batches.sql
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/ink/cow_protocol_ink_batches.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/cow_protocol/ink/cow_protocol_ink_batches.sql
@@ -8,7 +8,6 @@
         file_format ='delta',
         incremental_strategy='merge',
         incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
-        , post_hook='{{ hide_spells() }}'
     )
 }}
 
