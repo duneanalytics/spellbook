d8df26067effb3be115daf408d10cfc3e6092e98
Gustavo Torres <130374136+gustavobftorres@users.noreply.github.com>
2026-03-03 21:34:31 -0300

    Add Avalanche C chain  for Balancer  V3 (#9300)
    * add support for avalanche on balancer_v3 spells
    
    * add sources to avalanche on v3
    
    * fix erc4626 typo
    
    * use balancer_v3 compatible token balance changes daily instead of v2
    
    * include avalanche_c models on the cross chain aggregators
    
    * add reclamm pool factory to avalanche sources
    
    * added for the six columns that the second model references
    
    * fix typo on string, missing quotation marks
    
    * add fee_type and treasury_fee_usd to protocol fee schema
    
    * fix bugs reported by bot
    
    * use gyroscope decoded contract instead of balancer
    
    * fix avalanche labels to use gyroscope decoded contract
    
    * drop connection with aave tokens
    
    * Stubbed balancer_v3_avalanche_c_token_balance_changes so it no longer calls balancer_v3_compatible_token_balance_changes_macro or touches any labels tables
    
    * Stub added for BPT supply
    
    * stub bpt supply changes and prices
    
    * stib liquidity
    
    * remove horly_spellbook files
    
    * fix schemas
    
    * remove v3 hourly from token weights
    
    * remove v3 from procol_fee schema
    
    * add blockchain and version in schema key
    
    * add avalanche to dex model
    
    * Update dbt_subprojects/dex/models/_projects/balancer/labels/avalanche_c/labels_balancer_v3_pools_avalanche_c.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    * Update dbt_subprojects/dex/models/_projects/balancer/trades/avalanche_c/balancer_v3_avalanche_c_trades.sql
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: jeff-dude <102681548+jeff-dude@users.noreply.github.com>

--- dbt_subprojects/dex/models/_projects/balancer/labels/labels_balancer_v3_pools.sql
+++ dbt_subprojects/dex/models/_projects/balancer/labels/labels_balancer_v3_pools.sql
@@ -13,6 +13,8 @@ SELECT * FROM  {{ ref('labels_balancer_v3_pools_arbitrum') }}
 UNION
 SELECT * FROM  {{ ref('labels_balancer_v3_pools_base') }}
 UNION
+SELECT * FROM {{ ref('labels_balancer_v3_pools_avalanche_c')}}
+UNION
 SELECT * FROM {{ ref('labels_balancer_v3_pools_hyperevm') }}
 UNION
 SELECT * FROM  {{ ref('labels_balancer_v3_pools_monad') }}
