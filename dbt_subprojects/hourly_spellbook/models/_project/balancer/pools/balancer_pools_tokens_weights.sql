f72396900ead82ace41e4961d7deb063ea4f873d
gustavobftorres <gutobevtorres@gmail.com>
2026-03-06 17:44:22 -0300

    add files to connect plasma into balancerv3 hourly spellbook

--- dbt_subprojects/hourly_spellbook/models/_project/balancer/pools/balancer_pools_tokens_weights.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/pools/balancer_pools_tokens_weights.sql
@@ -13,7 +13,8 @@
     ref('balancer_gnosis_pools_tokens_weights'),
     ref('balancer_optimism_pools_tokens_weights'),
     ref('balancer_polygon_pools_tokens_weights'),
-    ref('balancer_zkevm_pools_tokens_weights')
+    ref('balancer_zkevm_pools_tokens_weights'),
+    ref('balancer_plasma_pools_tokens_weights')
 ] %}
 
 
