9c24f5d9da5767ae3ee29cee4a5ce78886cd330c
0xRob <83790096+0xRobin@users.noreply.github.com>
2026-03-12 12:15:34 +0100

    add ethereum withdrawals and miner rewards to base transfers

--- dbt_subprojects/tokens/models/transfers_and_balances/ethereum/tokens_ethereum_base_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/ethereum/tokens_ethereum_base_transfers.sql
@@ -30,3 +30,78 @@ FROM
     )
     }}
 )
+
+UNION ALL
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['w.block_number', "'withdrawal'", 'w.withdrawal_index']) }} AS unique_key
+    , 'ethereum' AS blockchain
+    , cast(date_trunc('month', w.block_time) as date) AS block_month
+    , cast(date_trunc('day', w.block_time) as date) AS block_date
+    , w.block_time
+    , w.block_number
+    , cast(NULL as varbinary) AS tx_hash
+    , cast(NULL as integer) AS evt_index
+    , cast(NULL as array(bigint)) AS trace_address
+    , 'native' AS token_standard
+    , cast(NULL as varbinary) AS tx_from
+    , cast(NULL as varbinary) AS tx_to
+    , cast(NULL as integer) AS tx_index
+    , 0x0000000000000000000000000000000000000000 AS "from"
+    , w.address AS to
+    , (select token_address from {{ source('dune','blockchains') }} where name = 'ethereum') AS contract_address
+    , (cast(w.amount as uint256) * uint256 '1000000000') AS amount_raw
+FROM (
+    SELECT
+        block_time
+        , block_number
+        , "index" AS withdrawal_index
+        , address
+        , amount
+    FROM {{ source('ethereum', 'withdrawals') }}
+) w
+WHERE w.address IS NOT NULL
+    AND w.amount > 0
+{% if is_incremental() %}
+    AND {{ incremental_predicate('w.block_time') }}
+{% endif %}
+
+UNION ALL
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['b.number', "'miner_reward'"]) }} AS unique_key
+    , 'ethereum' AS blockchain
+    , cast(date_trunc('month', b.time) as date) AS block_month
+    , cast(date_trunc('day', b.time) as date) AS block_date
+    , b.time AS block_time
+    , b.number AS block_number
+    , cast(NULL as varbinary) AS tx_hash
+    , cast(NULL as integer) AS evt_index
+    , cast(NULL as array(bigint)) AS trace_address
+    , 'native' AS token_standard
+    , cast(NULL as varbinary) AS tx_from
+    , cast(NULL as varbinary) AS tx_to
+    , cast(NULL as integer) AS tx_index
+    , 0x0000000000000000000000000000000000000000 AS "from"
+    , b.miner AS to
+    , (select token_address from {{ source('dune','blockchains') }} where name = 'ethereum') AS contract_address
+    , r.amount_raw
+FROM {{ source('ethereum', 'blocks') }} b
+INNER JOIN (
+    SELECT
+        block_number
+        , max(value) AS amount_raw -- use max reward trace per block for miner payout; sum(value) would include uncle rewards and overstate miner amount
+    FROM {{ source('ethereum', 'traces') }}
+    WHERE type = 'reward'
+        AND value > uint256 '0'
+        AND block_number <= 15537393
+        {% if is_incremental() %}
+        AND {{ incremental_predicate('block_time') }}
+        {% endif %}
+    GROUP BY 1
+) r
+    ON r.block_number = b.number
+WHERE b.number <= 15537393
+{% if is_incremental() %}
+    AND {{ incremental_predicate('b.time') }}
+{% endif %}
