756a8839747e90ec0dfc8d473df075bbc107767a
jeff-dude <jeff@dune.xyz>
2026-03-25 15:41:09 -0400

    test incremental

--- dbt_subprojects/tokens/models/transfers_and_balances/tokens_transfers_last_updated.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/tokens_transfers_last_updated.sql
@@ -1,16 +1,56 @@
 {{ config(
         schema = 'tokens'
         , alias = 'transfers_last_updated'
-        , materialized = 'table'
+        , materialized = 'incremental'
         , file_format = 'delta'
+        , incremental_strategy = 'merge'
+        , unique_key = ['blockchain', 'block_month']
+        , merge_skip_unchanged = true
         )
 }}
 
-SELECT
-    blockchain
-    , block_month
-    , max(_updated_at) AS last_update_date
-FROM {{ ref('tokens_transfers') }}
-GROUP BY
-    blockchain
-    , block_month
+{% if is_incremental() -%}
+with changed_keys as (
+	select distinct
+		t.blockchain
+		, t.block_month
+	from
+		{{ ref('tokens_transfers') }} as t
+	left join {{ this }} as d
+		on t.blockchain = d.blockchain
+		and t.block_month = d.block_month
+	where
+		t._updated_at > coalesce(d.last_update_date, timestamp '1900-01-01')
+)
+, recalc as (
+	select
+		t.blockchain
+		, t.block_month
+		, max(t._updated_at) as last_update_date
+	from
+		{{ ref('tokens_transfers') }} as t
+	inner join changed_keys as k
+		on t.blockchain = k.blockchain
+		and t.block_month = k.block_month
+	group by
+		t.blockchain
+		, t.block_month
+)
+
+select
+	r.blockchain
+	, r.block_month
+	, r.last_update_date
+from
+	recalc as r
+{% else -%}
+select
+	t.blockchain
+	, t.block_month
+	, max(t._updated_at) as last_update_date
+from
+	{{ ref('tokens_transfers') }} as t
+group by
+	t.blockchain
+	, t.block_month
+{% endif -%}
