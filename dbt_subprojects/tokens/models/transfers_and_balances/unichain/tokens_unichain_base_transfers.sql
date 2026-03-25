6e71d9ca7118e00fc2c20843244a6f88d85a8522
0xRob <83790096+0xRobin@users.noreply.github.com>
2026-03-25 11:16:03 +0100

    fix(tokens): add wrapped native coverage for shape, somnia, superseed, taiko, unichain, worldchain, zkevm (#9478)

--- dbt_subprojects/tokens/models/transfers_and_balances/unichain/tokens_unichain_base_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/unichain/tokens_unichain_base_transfers.sql
@@ -16,3 +16,17 @@
 	transactions=source('unichain', 'transactions'),
 	erc20_transfers=source('erc20_unichain', 'evt_Transfer'),
 ) }}
+
+union all
+
+select
+	*
+from
+	(
+		{{ transfers_base_wrapped_token(
+			blockchain='unichain',
+			transactions=source('unichain', 'transactions'),
+			wrapped_token_deposit=source('weth_unichain', 'weth_evt_deposit'),
+			wrapped_token_withdrawal=source('weth_unichain', 'weth_evt_withdrawal'),
+		) }}
+	)
