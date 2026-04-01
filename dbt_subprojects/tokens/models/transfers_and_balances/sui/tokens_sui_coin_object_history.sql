407c34878320abfb6e9f7b0abd2f8f8b290df91a
tomfutago <tomfutago@gmail.com>
2026-04-01 10:46:28 +0100

    chore: prod chain start date

--- dbt_subprojects/tokens/models/transfers_and_balances/sui/tokens_sui_coin_object_history.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/sui/tokens_sui_coin_object_history.sql
@@ -12,8 +12,7 @@
   )
 }}
 
--- temp filter to unblock ci run (original start date '2023-04-12')
-{% set sui_transfer_start_date = '2026-01-01' %}
+{% set sui_transfer_start_date = '2023-04-12' %}
 
 with
 
