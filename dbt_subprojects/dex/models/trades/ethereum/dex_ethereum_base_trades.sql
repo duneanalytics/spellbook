c0776dae09161efb7f046cc2eee40d1b8a04a7b5
Dennis <dennis@dune.com>
2026-04-02 13:48:06 +0300

    add comment for excluded ethereum tx_hash

--- dbt_subprojects/dex/models/trades/ethereum/dex_ethereum_base_trades.sql
+++ dbt_subprojects/dex/models/trades/ethereum/dex_ethereum_base_trades.sql
@@ -62,4 +62,5 @@ WITH base AS (
     ) }}
 )
 SELECT * FROM base
+-- exclude tx with inflated volume (~10 trillion)
 WHERE tx_hash != 0x1c27c4d625429acfc0f97e466eda725fd09ebdc77550e529ba4cbdbc33beb97b
