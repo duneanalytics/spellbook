0429556e217d6dd2b86a539319da5159768c6dec
tomfutago <tomfutago@gmail.com>
2026-04-05 14:01:16 +0100

    chore: ci test dates

--- dbt_subprojects/tokens/models/transfers_and_balances/sui/sui_coin_info.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/sui/sui_coin_info.sql
@@ -10,7 +10,7 @@
   )
 }}
 
-{% set sui_transfer_start_date = '2023-04-12' %}
+{% set sui_transfer_start_date = '2026-01-01' %} -- just ci test
 
 -- ranking policy:
 -- choose the most recent metadata snapshot first (checkpoint/version),
