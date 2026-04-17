9984aa4af44757bec2af263081bfb96206ca456e
André Monteiro <andre@dune.com>
2026-04-17 18:34:55 +0100

    Remove DWH-421 test models after CI verification

--- dbt_subprojects/daily_spellbook/models/dwh_421_test/dwh_421_test_table.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    schema = 'dwh_421_test',
-    alias = 'test_table',
-    materialized = 'table',
-    file_format = 'delta',
-    tags = ['dwh_421_test']
-) }}
-
-SELECT 1 AS n, 'hello' AS label
--- dbt_subprojects/daily_spellbook/models/dwh_421_test/dwh_421_test_view.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-    schema = 'dwh_421_test',
-    alias = 'test_view',
-    materialized = 'view',
-    tags = ['dwh_421_test']
-) }}
-
-SELECT * FROM {{ ref('dwh_421_test_table') }}
