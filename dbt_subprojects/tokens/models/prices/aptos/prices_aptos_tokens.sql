b139a45c7215532ad8e0bb4dcef67d856bc11feb
tomfutago <tomfutago@gmail.com>
2026-04-21 16:47:52 +0100

    feat: aptos prices

---
+++ dbt_subprojects/tokens/models/prices/aptos/prices_aptos_tokens.sql
@@ -0,0 +1,27 @@
+{% set blockchain = 'aptos' %}
+
+{{ config(
+    schema = 'prices_' + blockchain,
+    alias = 'tokens',
+    materialized = 'table',
+    file_format = 'delta',
+    tags = ['static']
+    )
+}}
+
+select
+    token_id
+    , '{{ blockchain }}' as blockchain
+    , symbol
+    , to_utf8(contract_address) as contract_address
+    , contract_address as contract_address_native
+    , cast(decimals as integer) as decimals
+from
+(
+    values
+    ('usdc-usd-coin', 'USDC', '0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC', 6)
+    , ('usdt-tether', 'USDT', '0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT', 6)
+    , ('weth-weth', 'WETH', '0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::WETH', 6)
+    , ('wbtc-wrapped-bitcoin', 'WBTC', '0xae478ff7d83ed072dbc5e264250e67ef58f57c99d89b447efd8a0a2e8b2be76e::coin::T', 8)
+    , ('thl-thala', 'THL', '0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL', 8)
+) as temp (token_id, symbol, contract_address, decimals)
