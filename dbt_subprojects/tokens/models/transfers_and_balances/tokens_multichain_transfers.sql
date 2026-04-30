4cf668ebb8bba760e9edbebe401d938ab21fafc3
tomfutago <tomfutago@gmail.com>
2026-04-30 14:19:46 +0100

    fix: aptos block_number blank for now

--- dbt_subprojects/tokens/models/transfers_and_balances/tokens_multichain_transfers.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/tokens_multichain_transfers.sql
@@ -136,7 +136,7 @@ select
   block_time,
   block_date,
   block_month,
-  cast(tx_version as bigint) as block_number,
+  cast(null as bigint) as block_number, -- TODO: add aptos block_height downstream
   cast(tx_hash as varchar) as tx_id,
   cast(tx_index as bigint) as tx_index,
   cast(event_index as bigint) as event_index,
