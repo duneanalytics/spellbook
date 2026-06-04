effca3a197a82eb5954a51a1c0b12e97eb4fd0fd
tomfutago <35136350+tomfutago@users.noreply.github.com>
2026-06-04 13:22:36 +0100

    CUR2-2601: prepare XRPL token transfer cutover (#9714)

--- dbt_subprojects/tokens/models/transfers_and_balances/xrpl/tokens_xrpl_affected_nodes.sql
+++ dbt_subprojects/tokens/models/transfers_and_balances/xrpl/tokens_xrpl_affected_nodes.sql
@@ -2,6 +2,7 @@
   config(
     schema = 'tokens_xrpl',
     alias = 'affected_nodes',
+    tags = ['prod_exclude'],
     materialized = 'incremental',
     file_format = 'delta',
     partition_by = ['block_month'],
