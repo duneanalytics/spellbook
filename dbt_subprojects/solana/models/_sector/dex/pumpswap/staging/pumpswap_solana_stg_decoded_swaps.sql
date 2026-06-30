861cedfc8359f0fb2f3a56e46ead6e37c2c70bb3
0xRob <83790096+0xRobin@users.noreply.github.com>
2026-06-30 09:43:39 +0200

    Split PumpSwap BuyExactQuoteIn trades

--- dbt_subprojects/solana/models/_sector/dex/pumpswap/staging/pumpswap_solana_stg_decoded_swaps.sql
+++ dbt_subprojects/solana/models/_sector/dex/pumpswap/staging/pumpswap_solana_stg_decoded_swaps.sql
@@ -6,88 +6,14 @@
     , materialized = 'incremental'
     , file_format = 'delta'
     , incremental_strategy = 'merge'
-    , on_schema_change = 'sync_all_columns'
     , incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_date')]
     , unique_key = ['block_month', 'block_date', 'surrogate_key']
   )
 }}
 
 {% set project_start_date = '2025-02-20' %}
-{% set event_start_date = '2025-11-01' %}
 
-WITH event_only_swaps AS (
-    SELECT
-          e.evt_block_time
-        , e.evt_block_slot
-        , e.evt_block_date
-        , e.evt_outer_instruction_index
-        , e.evt_inner_instruction_index
-        , e.evt_tx_id
-        , e.evt_tx_index
-        , e.evt_outer_executing_account
-        , e.pool
-        , e.user
-        , e.user_base_token_account
-        , e.user_quote_token_account
-        , e.protocol_fee_recipient_token_account
-        , e.base_amount_out
-        , e.quote_amount_in
-    FROM {{ source('pumpdotfun_solana', 'pump_amm_evt_buyevent') }} e
-    LEFT JOIN {{ source('pumpdotfun_solana', 'pump_amm_call_buy') }} c
-        ON c.call_block_date = e.evt_block_date
-        AND c.call_tx_id = e.evt_tx_id
-        AND c.call_outer_instruction_index = e.evt_outer_instruction_index
-        AND c.account_pool = e.pool
-        AND c.base_amount_out = e.base_amount_out
-        {% if is_incremental() %}
-        AND {{ incremental_predicate('c.call_block_date') }}
-        {% else %}
-        AND c.call_block_date >= DATE '{{ event_start_date }}'
-        {% endif %}
-    WHERE c.call_tx_id IS NULL
-        {% if is_incremental() %}
-        AND {{ incremental_predicate('e.evt_block_date') }}
-        {% else %}
-        AND e.evt_block_date >= DATE '{{ event_start_date }}'
-        {% endif %}
-)
-
-, pool_accounts AS (
-    SELECT
-          account_pool AS pool
-        , arbitrary(account_pool_base_token_account) AS pool_base_token_account
-        , arbitrary(account_pool_quote_token_account) AS pool_quote_token_account
-    FROM {{ source('pumpdotfun_solana', 'pump_amm_call_create_pool') }}
-    WHERE call_block_time >= TIMESTAMP '{{ project_start_date }}'
-    GROUP BY 1
-)
-
-, event_swaps AS (
-    SELECT
-          e.evt_block_time AS call_block_time
-        , e.evt_block_slot AS call_block_slot
-        , e.evt_block_date AS call_block_date
-        , e.evt_outer_instruction_index AS call_outer_instruction_index
-        , e.evt_inner_instruction_index AS call_inner_instruction_index
-        , e.evt_tx_id AS call_tx_id
-        , e.evt_tx_index AS call_tx_index
-        , e.evt_outer_executing_account AS call_outer_executing_account
-        , e.pool AS account_pool
-        , e.user AS account_user
-        , e.user_base_token_account AS account_user_base_token_account
-        , e.user_quote_token_account AS account_user_quote_token_account
-        , p.pool_base_token_account AS account_pool_base_token_account
-        , p.pool_quote_token_account AS account_pool_quote_token_account
-        , e.protocol_fee_recipient_token_account AS account_protocol_fee_recipient_token_account
-        , e.base_amount_out AS base_amount
-        , e.quote_amount_in AS quote_amount
-        , 1 AS is_buy
-    FROM event_only_swaps e
-    INNER JOIN pool_accounts p
-        ON p.pool = e.pool
-)
-
-, swaps AS (
+WITH swaps AS (
     SELECT
           call_block_time
         , call_block_slot
@@ -105,7 +31,6 @@ WITH event_only_swaps AS (
         , account_pool_quote_token_account
         , account_protocol_fee_recipient_token_account
         , base_amount_out AS base_amount
-        , CAST(NULL AS UINT256) AS quote_amount
         , 1 AS is_buy
     FROM {{ source('pumpdotfun_solana', 'pump_amm_call_buy') }}
     WHERE 1=1
@@ -134,7 +59,6 @@ WITH event_only_swaps AS (
         , account_pool_quote_token_account
         , account_protocol_fee_recipient_token_account
         , base_amount_in AS base_amount
-        , CAST(NULL AS UINT256) AS quote_amount
         , 0 AS is_buy
     FROM {{ source('pumpdotfun_solana', 'pump_amm_call_sell') }}
     WHERE 1=1
@@ -143,29 +67,6 @@ WITH event_only_swaps AS (
         {% else %}
         AND call_block_time >= TIMESTAMP '{{ project_start_date }}'
         {% endif %}
-
-    UNION ALL
-
-    SELECT
-          call_block_time
-        , call_block_slot
-        , call_block_date
-        , call_outer_instruction_index
-        , call_inner_instruction_index
-        , call_tx_id
-        , call_tx_index
-        , call_outer_executing_account
-        , account_pool
-        , account_user
-        , account_user_base_token_account
-        , account_user_quote_token_account
-        , account_pool_base_token_account
-        , account_pool_quote_token_account
-        , account_protocol_fee_recipient_token_account
-        , base_amount
-        , quote_amount
-        , 1 AS is_buy
-    FROM event_swaps
 )
 
 SELECT
@@ -187,7 +88,6 @@ SELECT
     , sp.account_pool_quote_token_account
     , sp.account_protocol_fee_recipient_token_account
     , sp.base_amount
-    , sp.quote_amount
     , sp.is_buy
     , {{ solana_instruction_key(
           'sp.call_block_slot'
