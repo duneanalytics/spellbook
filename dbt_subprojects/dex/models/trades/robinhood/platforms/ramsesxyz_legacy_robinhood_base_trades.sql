5d9f2dd2b61a73b645aea0a08030895cbb735a47
nyx-discochuck <nyx@househeads.org>
2026-07-27 14:30:55 +0000

    Add RamsesXYZ Robinhood dex trades

---
+++ dbt_subprojects/dex/models/trades/robinhood/platforms/ramsesxyz_legacy_robinhood_base_trades.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema = 'ramsesxyz_legacy_robinhood',
+        alias = 'base_trades',
+        materialized = 'incremental',
+        file_format = 'delta',
+        incremental_strategy = 'merge',
+        unique_key = ['tx_hash', 'evt_index'],
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')]
+    )
+}}
+
+{{
+    uniswap_compatible_v2_trades(
+        blockchain = 'robinhood',
+        project = 'ramsesxyz',
+        version = 'legacy',
+        Pair_evt_Swap = source('ramsesxyz_robinhood', 'pair_evt_swap'),
+        Factory_evt_PairCreated = source('ramsesxyz_robinhood', 'pairfactory_evt_paircreated')
+    )
+}}
