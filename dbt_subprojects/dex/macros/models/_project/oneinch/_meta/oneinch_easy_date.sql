2226dd599aead7de0d871d49992b0a619557e56e
jeff-dude <jeff@dune.xyz>
2026-08-17 18:33:51 -0400

    chore(oneinch): remove temporary CI date clamp

--- dbt_subprojects/dex/macros/models/_project/oneinch/_meta/oneinch_easy_date.sql
+++
@@ -1,6 +0,0 @@
--- TEMP, CI ONLY -- REVERT THIS COMMIT BEFORE MERGE --
--- Clamps every oneinch model's start date to a rolling two-day window so CI builds only
--- recent data while shared macro changes force the complete oneinch lineage to rebuild.
-{% macro oneinch_easy_date() %}
-    {{ return((modules.datetime.date.today() - modules.datetime.timedelta(days=2)).isoformat()) }}
-{% endmacro %}
