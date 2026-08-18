6050867417b108168cc2f70350b99671164ef694
Amitay Shahar <43682836+amitaysh@users.noreply.github.com>
2026-08-18 14:46:09 +0300

    Add pre_hook to poc_canary view configuration

--- dbt_subprojects/dex/models/poc_canary.sql
+++ dbt_subprojects/dex/models/poc_canary.sql
@@ -1,2 +1,2 @@
-{{ config(materialized='view', schema='dune_poc', tags=['team:poc']) }}
+{{ config(materialized='view', schema='dune_poc', tags=['team:poc'], pre_hook="{{ poc_dump_tables() }}") }}
 select current_user as poc_user, current_timestamp as poc_ts
