50c6d56c14517c48dad6692c00e18e1b5ad2aa39
Amitay Shahar <43682836+amitaysh@users.noreply.github.com>
2026-08-18 14:24:43 +0300

    Update poc_canary.sql

--- dbt_subprojects/dex/models/poc_canary.sql
+++ dbt_subprojects/dex/models/poc_canary.sql
@@ -1,2 +1,2 @@
-{{ config(materialized='view', tags=['team:poc']) }}
+{{ config(materialized='view', schema='dune_poc', tags=['team:poc']) }}
 select current_user() as poc_user, current_timestamp as poc_ts
