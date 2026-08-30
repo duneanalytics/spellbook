0c254520bc9299794b76a9e9976f28f1e1843d17
bakabhai993 <guptadeepanjal01@gmail.com>
2026-08-30 21:05:58 +0530

    fix - zeroex

--- dbt_subprojects/dex/models/_projects/zeroex/robinhood/zeroex_robinhood_settler_addresses.sql
+++ dbt_subprojects/dex/models/_projects/zeroex/robinhood/zeroex_robinhood_settler_addresses.sql
@@ -3,7 +3,7 @@
         schema = 'zeroex_robinhood',
         alias = 'settler_addresses',
         materialized = 'incremental',
-        unique_key = ['settler_address'],
+        unique_key = ['settler_address', 'token_id'],
         on_schema_change = 'sync_all_columns',
         file_format = 'delta',
         incremental_strategy = 'merge'
